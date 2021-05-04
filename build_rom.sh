9c93ada97479bbadb342859dc31ed82c4ce3e10a
Fedor Korotkov <fedor.korotkov@gmail.com>
2021-05-04 11:36:30 -0400

    Use all threads

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 set -e
+set -x
 
 # sync rom
 repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x
 git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # build rom
 source build/envsetup.sh
