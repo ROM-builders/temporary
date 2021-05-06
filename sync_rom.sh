29844f55fbe804a4020b09a3a675550e17d476eb
Apon77 <khalakuzzamanapon5@gmail.com>
2021-05-07 04:53:29 +0600

    rom name

---
+++ sync_rom.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# sync rom
+repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x
+git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+
+# build rom
+source build/envsetup.sh
+lunch aosp_mido-user
+m aex -j$(nproc --all)
+
+# upload rom
+up(){
+	curl --upload-file $1 https://transfer.sh/$(basename $1); echo
+	# 14 days, 10 GB limit
+}
+
+up out/target/product/mido/*.zip
