1b4a6324205d3ea94799c9589ecc79217b5074bf
Apon77 <khalakuzzamanapon5@gmail.com>
2021-05-06 13:12:07 +0600

    Ebin_A fix 1

--- build_rom.sh
+++ build_rom.sh
@@ -11,10 +11,11 @@ repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 git clone https://github.com/arulebin/device_xiaomi_rosy.git -b styx device/xiaomi/rosy
 git clone https://github.com/arulebin/vendor_xiaomi_rosy.git vendor/xiaomi/rosy
 git clone https://github.com/arulebin/kernel_xiaomi_rosy.git kernel/xiaomi/rosy
+rm -rf hardware/qcom-caf/msm8996
 
 # build rom
 source build/envsetup.sh
-lunch styx_rosy-userdebug
+lunch styx_rosy-user
 m styx-ota -j$(nproc --all)
 
 # upload rom
@@ -24,3 +25,4 @@ up(){
 }
 
 up out/target/product/rosy/*.zip
+
