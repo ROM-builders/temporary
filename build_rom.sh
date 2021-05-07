e08d18454218c849e45b7c4e42eed71ab00158a3
Apon77 <khalakuzzamanapon5@gmail.com>
2021-05-07 23:34:29 +0600

    sarthakroy2002 LineageOS

--- build_rom.sh
+++ build_rom.sh
@@ -4,5 +4,10 @@ set -exv
 
 # build rom
 source build/envsetup.sh
-lunch p404_whyred-userdebug
-m bacon
+lunch lineage_RMX2020-userdebug
+export SKIP_ABI_CHECKS=true
+export SKIP_API_CHECKS=true
+mka api-stubs-docs
+mka system-api-stubs-docs
+mka test-api-stubs-docs
+mka bacon -j8
--- sync_rom.sh
+++ sync_rom.sh
@@ -3,6 +3,11 @@
 set -exv
 
 # sync rom
-repo init -u https://github.com/P-404/platform_manifest -b rippa --depth=1
-git clone https://github.com/Project404-whyred/local_manifests.git --depth=1 .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+repo init --depth=1 -u git://github.com/ForkLineageOS/android.git -b lineage-18.1 --depth=1
+repo sync
+
+#Setup
+git clone https://github.com/Realme-G70-Series/vendor_realme_RMX2020.git vendor/realme/RMX2020 --depth 1
+git clone https://github.com/Realme-G70-Series/device_realme_RMX2020.git device/realme/RMX2020 --depth 1
+cd external/selinux && wget https://raw.githubusercontent.com/SamarV-121/android_vendor_extra/lineage-18.1/patches/external/selinux/0001-Revert-libsepol-Make-an-unknown-permission-an-error-.patch && patch -p1 < *.patch && cd ../..
+cd frameworks/av && wget https://github.com/phhusson/platform_frameworks_av/commit/624cfc90b8bedb024f289772960f3cd7072fa940.patch && patch -p1 < *.patch && cd -
