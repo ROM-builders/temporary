ab40bc8cd15bac2156b6b08f4495a5488dba2583
Apon77 <khalakuzzamanapon5@gmail.com>
2021-05-08 03:56:15 +0600

    UNKNOWN_MEMBER_69 XOSP-Reborn

--- sync_rom.sh
+++ sync_rom.sh
@@ -6,3 +6,4 @@ set -exv
 repo init -u git://github.com/XOSP-Reborn/manifest.git -b eleven --depth=1
 git clone https://github.com/sasukeuchiha-clan/Begonia --depth=1 -b soni .repo/local_manifests
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+rm -rf hardware/qcom-caf*
