9c5de69c8f4dd8655ca2db5e18b30cd8bdf3a39a
Apon77 <khalakuzzamanapon5@gmail.com>
2021-05-07 14:05:51 +0600

    cck

--- build_rom.sh
+++ build_rom.sh
@@ -7,6 +7,3 @@ source build/envsetup.sh
 lunch aosp_mido-user
 ccache -z
 m aex -j$(nproc --all)
-ccache -s
-ccache -V
-which ccache
--- sync_rom.sh
+++ sync_rom.sh
@@ -2,6 +2,11 @@
 
 set -exv
 
+ccache -s
+which ccache
+ccache -V
+sleep 2m
+
 #sync rom
 repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x
 git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
