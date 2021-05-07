6c4070c8f98273a592db5506ac5566ecefd0c974
Apon77 <khalakuzzamanapon5@gmail.com>
2021-05-08 00:06:39 +0600

    runrunrun00 LineageOS

--- sync_rom.sh
+++ sync_rom.sh
@@ -12,4 +12,3 @@ git clone --depth=1 https://github.com/LinkBoi00/device_xiaomi_daisy-eleven devi
 git clone --depth=1 https://github.com/Couchpotato-sauce/kernel_xiaomi_sleepy kernel/xiaomi/msm8953 --depth 1
 git clone https://github.com/kdrag0n/proton-clang --depth=1 prebuilts/clang/host/linux-x86/clang-proton --depth 1
 rm -rf hardware/qcom-caf/msm8996/media hardware/qcom-caf/msm8996/audio hardware/qcom-caf/msm8996/display && git clone --single-branch https://github.com/Jabiyeff/android_hardware_qcom_media hardware/qcom-caf/msm8996/media && git clone --single-branch https://github.com/Jabiyeff/android_hardware_qcom_display hardware/qcom-caf/msm8996/display &&  git clone https://github.com/ItsVixano/android_hardware_qcom_audio --single-branch hardware/qcom-caf/msm8996/audio
-git clone https://github.com/Project-Fluid/packages_apps_Snap packages/apps/Snap --depth 1
