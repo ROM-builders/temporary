f4d80b68b9e3f4d1c1ff50ecb34c2555a878a10d
Apon77 <khalakuzzamanapon5@gmail.com>
2021-05-07 06:13:15 +0600

    why vim

--- sync_rom.sh
+++ sync_rom.sh
@@ -7,5 +7,4 @@ pwd
 repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x
 pwd
 git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync
--j$(nproc --all)
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
