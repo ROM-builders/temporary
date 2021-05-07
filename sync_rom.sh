cbf1c027644c0a0f9a6b5293f80399d18abbf495
Apon77 <khalakuzzamanapon5@gmail.com>
2021-05-07 14:28:22 +0600

    ccache -s

--- sync_rom.sh
+++ sync_rom.sh
@@ -2,8 +2,6 @@
 
 set -exv
 
-ccache -p
-sleep 2m
 #sync rom
 repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x
 git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
