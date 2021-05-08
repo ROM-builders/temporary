bbce2b96f5d96d6974b6640eacd99c78cf21022c
Apon77 <khalakuzzamanapon5@gmail.com>
2021-05-08 10:27:34 +0600

    sarthakroy2002 ForkLineageOS

--- sync_rom.sh
+++ sync_rom.sh
@@ -4,7 +4,7 @@ set -exv
 
 # sync rom
 repo init -u git://github.com/ForkLineageOS/android.git --depth=1 -b lineage-18.1
-git clone https://github.com/Realme-G70-Series/local_mainfest.git --depth=1 -b fluid .repo/local_mainfests
+git clone https://github.com/Realme-G70-Series/local_manifest.git --depth=1 -b fluid .repo/local_mainfests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 echo finished sync
 cd external/selinux && wget https://raw.githubusercontent.com/SamarV-121/android_vendor_extra/lineage-18.1/patches/external/selinux/0001-Revert-libsepol-Make-an-unknown-permission-an-error-.patch && patch -p1 < *.patch
