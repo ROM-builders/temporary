01c21166557c14c372737b646fabe80ed64921b8
Apon77 <khalakuzzamanapon5@gmail.com>
2021-05-08 16:37:46 +0600

    sarthakroy2002 ForkLineageOS

--- build_rom.sh
+++ build_rom.sh
@@ -5,6 +5,6 @@ set -exv
 # build rom
 source build/envsetup.sh
 lunch lineage_RMX2020-userdebug
-export SKIP_ABI_CHECKS=true
+export SKIP_ABI_CHECKS=true 
 export SKIP_API_CHECKS=true
 mka bacon -j64
--- sync_rom.sh
+++ sync_rom.sh
@@ -6,7 +6,3 @@ set -exv
 repo init -u git://github.com/ForkLineageOS/android.git --depth=1 -b lineage-18.1
 git clone https://github.com/Realme-G70-Series/local_manifest.git --depth=1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
-echo finished sync
-cd frameworks/av && wget https://github.com/phhusson/platform_frameworks_av/commit/624cfc90b8bedb024f289772960f3cd7072fa940.patch && patch -p1 < *.patch
-cd ../..
-echo finished patches
