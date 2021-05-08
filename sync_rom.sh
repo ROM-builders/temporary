39e3c528ca55ba1687f146b4b813fa5523fa50d2
Apon77 <khalakuzzamanapon5@gmail.com>
2021-05-08 12:07:23 +0600

    sarthakroy2002 ForkLineageOS device

--- sync_rom.sh
+++ sync_rom.sh
@@ -3,6 +3,7 @@
 set -exv
 
 # sync rom
+rm -rf device
 repo init -u git://github.com/ForkLineageOS/android.git --depth=1 -b lineage-18.1
 git clone https://github.com/Realme-G70-Series/local_manifest.git --depth=1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
