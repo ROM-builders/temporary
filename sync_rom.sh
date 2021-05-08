fe5248d71c42d4c6a9a0b45d821507393a7d31f2
Apon77 <khalakuzzamanapon5@gmail.com>
2021-05-08 11:46:24 +0600

    P4042 Project-Fluid

--- sync_rom.sh
+++ sync_rom.sh
@@ -4,6 +4,7 @@ set -exv
 
 # sync rom
 repo init -u https://github.com/Project-Fluid/manifest.git --depth=1 -b fluid-11
-git clone https://github.com/adrian-8901/local_mainfest.git --depth=1 -b fluid .repo/local_mainfests
+rm -rf .repo/local_mainfests
+git clone https://github.com/adrian-8901/local_mainfest.git --depth=1 -b fluid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 echo finished sync
