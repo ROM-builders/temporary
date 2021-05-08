d813d169e146379aa8543630676537b95a4fea7b
Apon77 <khalakuzzamanapon5@gmail.com>
2021-05-08 14:50:41 +0600

    flashokiller_gacha AOSiP

--- sync_rom.sh
+++ sync_rom.sh
@@ -2,8 +2,6 @@
 
 set -exv
 
-# sync rom
 repo init -u  git://github.com/AOSiP/platform_manifest.git -b eleven --depth=1
 git clone https://github.com/flashokiller/android_.repo_local_manifests --depth=1  .repo/local_manifests -b aex
 repo sync --force-sync --no-tags --no-clone-bundle
-
