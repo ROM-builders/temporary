7159cd8dea3e326c46386f831715f1955a258036
Apon77 <khalakuzzamanapon5@gmail.com>
2021-05-08 09:49:43 +0600

    flashokiller_gacha AOSiP

--- build_rom.sh
+++ build_rom.sh
@@ -6,5 +6,4 @@ set -exv
 . build/envsetup.sh
 lunch aosip_ysl-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
-export WITH_GAPPS=true
-time m kronic -j$(nproc --all)
+time m kronic
--- sync_rom.sh
+++ sync_rom.sh
@@ -3,14 +3,12 @@
 set -exv
 
 # sync rom
-repo init -u git://github.com/AOSiP/platform_manifest.git -b eleven --depth=1
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-nproc --all
+repo init -u  git://github.com/AOSiP/platform_manifest.git -b eleven --depth=1
+git clone https://github.com/flashokiller/android_.repo_local_manifests --depth1  .repo/local_manifetsts -b aex
+repo sync --force-sync --no-tags --no-clone-bundle
 #Setup
-git clone https://github.com/flashokiller/android_device_xiaomi_ysl device/xiaomi/ysl --depth 1
-git clone https://github.com/ItsVixano/android_vendor_xiaomi_ysl vendor/xiaomi/ysl --depth 1
-git clone https://github.com/flashokiller/flasho_Ysl kernel/xiaomi/ysl --depth 1
 # fix
+rm -rf device/generic/opengl-transport
 rm  -rf vendor/codeaurora/telephony hardware/qcom-caf/msm8996/media hardware/qcom-caf/msm8996/audio hardware/qcom-caf/msm8996/display && git clone https://github.com/wave-project/vendor_codeaurora_telephony --depth=1 --single-branch vendor/codeaurora/telephony/ && git clone --single-branch https://github.com/Jabiyeff/android_hardware_qcom_media hardware/qcom-caf/msm8996/media && git clone --single-branch https://github.com/Jabiyeff/android_hardware_qcom_display hardware/qcom-caf/msm8996/display &&  git clone https://github.com/LineageOS/android_hardware_qcom_audio --single-branch -b lineage-18.1-caf-msm8996 hardware/qcom-caf/msm8996/audio
 cd device/xiaomi/ysl
 rm -rf overlay/packages/services/Telephony/res/xml/telephony_injection.xml
