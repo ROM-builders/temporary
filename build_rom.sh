6fa92354af614984e0692186141e8bc24821efbf
Apon77 <khalakuzzamanapon5@gmail.com>
2021-05-09 02:15:37 +0600

    sarthakroy2002 ForkLineageOS

--- build_rom.sh
+++ build_rom.sh
@@ -2,9 +2,11 @@
 
 set -exv
 
+
 # build rom
 source build/envsetup.sh
 lunch lineage_RMX2020-userdebug
-export SKIP_ABI_CHECKS=true 
+export SKIP_ABI_CHECKS=true
 export SKIP_API_CHECKS=true
 mka bacon -j64
+
