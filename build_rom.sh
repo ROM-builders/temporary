f51f028e7c5b2c275d97d6f3b4e421efc248f761
Apon77 <khalakuzzamanapon5@gmail.com>
2021-05-08 12:55:24 +0600

    rahul9999xda DerpFest-11

--- build_rom.sh
+++ build_rom.sh
@@ -5,5 +5,4 @@ set -exv
 # build rom
 source build/envsetup.sh
 lunch derp_vayu-userdebug
-make installclean
 mka derp
--- sync_rom.sh
+++ sync_rom.sh
@@ -2,10 +2,13 @@
 
 set -exv
 
-#sync rom
+# sync rom
 repo init -u git://github.com/DerpFest-11/manifest.git -b 11 --depth=1
 git clone https://github.com/pocox3pro/Local-Manifests --depth=1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 #Setup
+curl https://gist.githubusercontent.com/SuperCosmicBeing/c6db35d8abfa480e183a66b71ffca563/raw/3f5f1fc6d02ae767479bda37c04ea417a229f7eb/gms_full.mk >> gms_full.mk
+mv gms_full.mk vendor/gms/
 rm -rf device/generic/opengl-transport
+
