518f8042c40fbb3534894e0ae27002bd32e2638e
Apon77 <khalakuzzamanapon5@gmail.com>
2021-05-08 10:07:17 +0600

    sarthakroy2002 ForkLineageOS

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,4 @@ source build/envsetup.sh
 lunch lineage_RMX2020-userdebug
 export SKIP_ABI_CHECKS=true
 export SKIP_API_CHECKS=true
-mka api-stubs-docs
-mka system-api-stubs-docs
-mka test-api-stubs-docs
-mka bacon -j$(nproc --all)
+mka bacon -j64
--- sync_rom.sh
+++ sync_rom.sh
@@ -3,11 +3,12 @@
 set -exv
 
 # sync rom
-repo init --depth=1 -u git://github.com/ForkLineageOS/android.git -b lineage-18.1 --depth=1
-repo sync
-
-#Setup
-git clone https://github.com/Realme-G70-Series/vendor_realme_RMX2020.git vendor/realme/RMX2020 --depth 1
-git clone https://github.com/Realme-G70-Series/device_realme_RMX2020.git device/realme/RMX2020 --depth 1
-cd external/selinux && wget https://raw.githubusercontent.com/SamarV-121/android_vendor_extra/lineage-18.1/patches/external/selinux/0001-Revert-libsepol-Make-an-unknown-permission-an-error-.patch && patch -p1 < *.patch && cd ../..
-cd frameworks/av && wget https://github.com/phhusson/platform_frameworks_av/commit/624cfc90b8bedb024f289772960f3cd7072fa940.patch && patch -p1 < *.patch && cd -
+repo init -u git://github.com/ForkLineageOS/android.git --depth=1 -b lineage-18.1
+git clone https://github.com/Realme-G70-Series/local_mainfest.git --depth=1 -b fluid .repo/local_mainfests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+echo finished sync
+cd external/selinux && wget https://raw.githubusercontent.com/SamarV-121/android_vendor_extra/lineage-18.1/patches/external/selinux/0001-Revert-libsepol-Make-an-unknown-permission-an-error-.patch && patch -p1 < *.patch
+cd ../..
+cd frameworks/av && wget https://github.com/phhusson/platform_frameworks_av/commit/624cfc90b8bedb024f289772960f3cd7072fa940.patch && patch -p1 < *.patch
+cd ../..
+echo finished patches
