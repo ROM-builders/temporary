3f1484e8f659f52afc34160c135c7f7a5d16665c
Sarthak <sarthakroy2002@gmail.com>
2021-05-09 16:23:24 +0530

    Project Sakura RMX2020

--- sync_rom.sh
+++ sync_rom.sh
@@ -6,6 +6,5 @@ set -exv
 repo init --depth=1 -u git://github.com/ProjectSakura/android.git -b 11
 git clone https://github.com/Realme-G70-Series/local_manifest.git --depth=1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
-git clone https://github.com/Realme-G70-Series/android_packages_apps_RealmeParts packages/apps/RealmeParts
 
 
