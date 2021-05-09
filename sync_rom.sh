93ef6ce9eafa7074a40462b8dbdf1cbc3d7c7f9f
DhruvChhura <73804079+DhruvChhura@users.noreply.github.com>
2021-05-09 20:22:39 +0530

    Update sync

--- sync_rom.sh
+++ sync_rom.sh
@@ -8,3 +8,4 @@ git clone https://github.com/DhruvChhura/mainfest_personal.git --depth=1 -b cph 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 
+rm -rf vendor/codeaurora/telephony hardware/qcom-caf/msm8996/media hardware/qcom-caf/msm8996/audio hardware/qcom-caf/msm8996/display && git clone https://github.com/wave-project/vendor_codeaurora_telephony --depth=1 --single-branch vendor/codeaurora/telephony/ && git clone --single-branch https://github.com/Jabiyeff/android_hardware_qcom_media hardware/qcom-caf/msm8996/media && git clone --single-branch https://github.com/Jabiyeff/android_hardware_qcom_display hardware/qcom-caf/msm8996/display &&  git clone https://github.com/LineageOS/android_hardware_qcom_audio --single-branch -b lineage-18.1-caf-msm8996 hardware/qcom-caf/msm8996/audio
