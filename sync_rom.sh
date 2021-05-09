5c658b6ee82e5351575c372085b072d09bc5b1b5
nnippon <adameyyad999@gmail.com>
2021-05-09 18:33:29 +0000

    Fix derp

--- sync_rom.sh
+++ sync_rom.sh
@@ -4,9 +4,5 @@ set -exv
 
 # sync rom
 repo init -u https://github.com/StyxProject/manifest.git --depth=1 -b R
-git clone https://github.com/nnippon/local_buildbot.git --depth=1 -b fluid .repo/local_manifests
+git clone https://github.com/nnippon/local_buildbot.git --depth=1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
-echo finished sync
-
-# Fix errors
-rm -rf hardware/ril/libril vendor/qcom/opensource/power && git clone https://github.com/nnippon/android_vendor_qcom_opensource_power -b 11 vendor/qcom/opensource/power
