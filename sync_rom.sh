9b840cf152c3eb4c3b0669696e2e053396bbdb7d
Ebin <ebinarul@protonmail.com>
2021-05-09 22:57:16 +0530

    Fix typo

--- sync_rom.sh
+++ sync_rom.sh
@@ -3,6 +3,6 @@
 set -exv
 
 # Sync Everything
-repo init -u --depth=1 https://github.com/arulebin/android_manifest.git -b eleven
-git clone https://github.com/arulebin/local_manifest.git --depth=1 .repo/local_manifests
+repo init --depth=1 -u https://github.com/arulebin/android_manifest.git -b eleven
+git clone https://github.com/arulebin/local_manifest.git -b cph .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
