e1de7a3dbe33746effc17fe9eff87640de05b16e
devil-black-786 <fahimahmad904@gmail.com>
2021-05-09 18:51:29 +0530

    sax

--- build_rom.sh
+++ build_rom.sh
@@ -4,17 +4,14 @@ set -e
 set -x
 
 # sync rom
-repo init --depth=1 https://github.com/PotatoProject/manifest -b dumaloo-release
+repo init --depth=1 -u git://github.com/ForkLineageOS/android.git -b lineage-18.1
+git clone https://github.com/CPH1859/local_manifest/blob/main/local_manifest.xml --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
-
-#clone
-git clone --depth=1 https://github.com/devil-black-786/octavi_oppo_CPH1859 -b posp device/oppo/CPH1859
-git clone --depth=1 https://github.com/CPH1859/proprietary_vendor_oppo_CPH1859 vendor/oppo/CPH1859
 
 # build rom
 source build/envsetup.sh
-lunch potato_mido-userdebug
-brunch CPH1859 -j$(nproc --all)
+lunch lunch_CPH1859-userdebug
+make bacon  -j$(nproc --all)
 
 # upload rom
 up(){
