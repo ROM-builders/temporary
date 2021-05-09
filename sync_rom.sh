f74f2b138a62e504cf85ab65d3352cac011ba4ce
DhruvChhura <73804079+DhruvChhura@users.noreply.github.com>
2021-05-09 15:52:55 +0530

    Update sync_rom.sh

--- sync_rom.sh
+++ sync_rom.sh
@@ -3,7 +3,7 @@
 # sync rom
 repo init --depth=1 -u git://github.com/CipherOS/android_manifest.git -b eleven
 
-git clone https://github.com/DhruvChhura/frostmanifest.git --depth=1 -b derp .repo/local_manifests
+git clone https://github.com/DhruvChhura/mainfest_personal.git --depth=1 -b cph .repo/local_manifests
 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
