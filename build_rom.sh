6e190c49d625475f20dfd1f9478aaa8a4835bc62
devil-black-786 <fahimahmad904@gmail.com>
2021-05-09 18:15:42 +0530

    Adapted script for CPH1859

--- build_rom.sh
+++ build_rom.sh
@@ -4,14 +4,17 @@ set -e
 set -x
 
 # sync rom
-repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x
-git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
+repo init --depth=1 https://github.com/PotatoProject/manifest -b dumaloo-release
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+
+#clone
+git clone --depth=1 https://github.com/devil-black-786/octavi_oppo_CPH1859 -b posp device/oppo/CPH1859
+git clone --depth=1 https://github.com/CPH1859/proprietary_vendor_oppo_CPH1859 vendor/oppo/CPH1859
 
 # build rom
 source build/envsetup.sh
-lunch aosp_mido-user
-m aex -j$(nproc --all)
+lunch potato_mido-userdebug
+brunch CPH1859 -j$(nproc --all)
 
 # upload rom
 up(){
@@ -19,4 +22,4 @@ up(){
 	# 14 days, 10 GB limit
 }
 
-up out/target/product/mido/*.zip
+up out/target/product/CPH1859/*.zip
