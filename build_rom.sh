92ee5cf788c404f78bdc6ed8e13349ab2cc9bfbc
mukulsharma05175 <73372165+mukulsharma05175@users.noreply.github.com>
2021-05-10 22:35:55 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
+set -e
+set -x
+
 # sync rom
-repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests --depth=1
+repo init --depth=1 -u git://github.com/CipherOS/android_manifest.git -b eleven
+
+git clone https://github.com/whtisusername/frostmanifest.git --depth=1 -b derp .repo/local_manifests
+
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+
 
 # build rom
 source build/envsetup.sh
-lunch aosp_mido-user
-m aex -j10
+export CIPHER_OFFICIAL=true
+export CIPHER_GAPPS=true
+lunch lineage_lavender-userdebug
+make bacon
 
 # upload rom
-time rclone copy out/target/product/mido/AospExtended*.zip cirrus:mido -P
+up(){
+	curl --upload-file $1 https://transfer.sh/$(basename $1); echo
+	# 14 days, 10 GB limit
+}
+
+up out/target/product/lavender/*.zip
