23d5486acef45bae27d0fc5fe02e7368222443d4
DhruvChhura <73804079+DhruvChhura@users.noreply.github.com>
2021-05-10 08:33:37 +0530

    Sync Pe+

--- sync_rom.sh
+++ sync_rom.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # sync rom
-repo init --depth=1 -u git://github.com/CipherOS/android_manifest.git -b eleven
+repo init --depth=1 -u https://github.com/PixelExperience/manifest -b eleven-plus
 
-git clone https://github.com/DhruvChhura/mainfest_personal.git --depth=1 -b cph .repo/local_manifests
+git clone https://github.com/DhruvChhura/mainfest_personal.git --depth=1 -b pe .repo/local_manifests
 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
