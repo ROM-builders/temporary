220b237e0203ac77849bd7ee4e3e8c743679d736
Khalakuzzaman Apon <61594531+Apon77@users.noreply.github.com>
2021-05-10 22:25:15 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -8,7 +8,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch aosp_mido-user
-m init
+m aex
 
 # upload rom
 time rclone copy out/target/product/mido/AospExtended*.zip cirrus:mido -P
