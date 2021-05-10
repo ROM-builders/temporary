835429a8bd169c2d9a758f253899cfd5e731f69f
Apon77 <khalakuzzamanapon5@gmail.com>
2021-05-10 20:39:26 +0600

    mojito-crdroidandroid-boedhack

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 # sync rom
-repo init --depth=1 -u git://github.com/crdroidandroid/android.git -b 11.0 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/boedhack/local_manifest.git --depth 1 -b 11.0 .repo/local_manifests
+repo init -u git://github.com/crdroidandroid/android.git --depth=1 -b 11.0 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/boedhack/local_manifest.git -b 11.0 --depth 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # build rom
 source build/envsetup.sh
 lunch lineage_mojito-userdebug
+export SKIP_API_CHECKS=true
+export SKIP_ABI_CHECKS=true
 mka bacon
 
 # upload rom
