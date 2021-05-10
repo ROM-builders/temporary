19ff613524b8084a8929b2de7d3499933efe0061
Sarthak <sarthakroy2002@gmail.com>
2021-05-10 09:36:38 +0530

    fork los rmx2020

--- build_rom.sh
+++ build_rom.sh
@@ -8,6 +8,5 @@ source build/envsetup.sh
 lunch lineage_RMX2020-userdebug
 export SKIP_ABI_CHECKS=true
 export SKIP_API_CHECKS=true
-export SAKURA_MAINTAINER=sarthakroy2002
 mka bacon -j10
 
--- sync_rom.sh
+++ sync_rom.sh
@@ -3,8 +3,8 @@
 set -exv
 
 # sync rom
-repo init --depth=1 -u git://github.com/ProjectSakura/android.git -b 11
-git clone https://github.com/Realme-G70-Series/local_manifest.git --depth=1 .repo/local_manifests
+repo init --depth=1 -u git://github.com/ForkLineageOS/android.git -b lineage-18.1
+git clone https://github.com/Realme-G70-Series/local_manifest.git -b 11-staging --depth=1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 
