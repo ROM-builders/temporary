e4a9a8aa652daf8ca057cb2b5219ab64994d20a8
Ahmed Elwakil <35235801+PotatoAhmed@users.noreply.github.com>
2021-05-10 21:19:19 +0200

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# sync rom
-repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests --depth=1
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
-
-# build rom
+# Initializing build
 source build/envsetup.sh
-lunch aosp_mido-user
-m aex -j10
+lunch evolution_mido-user
 
-# upload rom
-time rclone copy out/target/product/mido/AospExtended*.zip cirrus:mido -P
+# Make Metalava
+make api-stubs-docs || echo no problem
+make system-api-stubs-docs || echo no problem
+make test-api-stubs-docs || echo no problem
+
+# Build Rom
+make bacon -j10
+
