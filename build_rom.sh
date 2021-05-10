e0537092ada809abb6fed4b4db59c056042fb0ef
Apon77 <khalakuzzamanapon5@gmail.com>
2021-05-10 17:27:43 +0600

    14

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,6 @@ repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x -g def
 git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests --depth=1
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
-
 # build rom
 source build/envsetup.sh
 lunch aosp_mido-user
