ddcad257a257d31b96ce813e9d7de26af0cbab44
Nippon <37734266+nnippon@users.noreply.github.com>
2021-05-10 22:47:33 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -2,6 +2,9 @@
 
 set -exv
 
+#Setup
+rm -rf hardware/ril/libril vendor/qcom/opensource/power && git clone https://github.com/nnippon/android_vendor_qcom_opensource_power -b 11 vendor/qcom/opensource/power
+
 # build rom
 source build/envsetup.sh
 lunch styx_j4primelte-userdebug
