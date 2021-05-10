1715de2bde2f04e96e0e27ea00aa643c913a1e51
ZèD <imzihad@gmail.com>
2021-05-10 23:39:02 +0600

    Fetch from main source
    Signed-off-by: ZèD <imzihad@gmail.com>

--- sync_rom.sh
+++ sync_rom.sh
@@ -4,7 +4,6 @@ set -exv
 
 # Init Repo
 repo init --no-repo-verify --depth=1 -u https://github.com/AOSPA/manifest -b ruby -g default,-device,-mips,-darwin,-notdefault
-sed -i 's/source.codeaurora.org/portland.source.codeaurora.org/g' .repo/manifests/default.xml
 
 # Device sources
 git clone https://github.com/AOSPA-Sakura/sakura_local_manifests .repo/local_manifests
