dc407196363c8c27f12ef27b7db8d70becda9504
Ahmed Elwakil <35235801+PotatoAhmed@users.noreply.github.com>
2021-05-10 21:16:53 +0200

    Create upload_rom.sh

---
+++ upload_rom.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Upload Function
+up() {
+      curl --upload-file $1 https://transfer.sh/$(basename $1); echo
+      # 14 days, 10 GB limit
+    }
+# Upload Rom
+    up out/target/product/mido/*.zip
