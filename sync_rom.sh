61e8bc7ceff4a11e2a615c319af5efe38e56f066
Ebin <ebinarul@protonmail.com>
2021-05-10 23:42:43 +0530

    rerun

--- sync_rom.sh
+++ sync_rom.sh
@@ -6,3 +6,4 @@ set -exv
 repo init --depth=1 -u https://github.com/arulebin/android_manifest.git -b eleven
 git clone https://github.com/arulebin/local_manifest.git -b cph .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+
