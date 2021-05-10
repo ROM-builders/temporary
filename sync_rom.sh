ec98520024dcdad913adc69fe7e1af47628e9291
ZèD <imzihad@gmail.com>
2021-05-10 23:55:31 +0600

    Temporary patch
    Signed-off-by: ZèD <imzihad@gmail.com>

--- sync_rom.sh
+++ sync_rom.sh
@@ -8,6 +8,9 @@ repo init --no-repo-verify --depth=1 -u https://github.com/AOSPA/manifest -b rub
 # Device sources
 git clone https://github.com/AOSPA-Sakura/sakura_local_manifests .repo/local_manifests
 
+# TMP fix for sync fail
+rm -rf cts
+
 # Sync Repo
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
