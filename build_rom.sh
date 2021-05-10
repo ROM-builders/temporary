e81c7298f3521d395c9c985c6d0f22a5ed2ea0e9
Apon77 <khalakuzzamanapon5@gmail.com>
2021-05-10 15:19:13 +0600

    check

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-set -exv
+# sync rom
+repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests --depth=1
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # build rom
 source build/envsetup.sh
 lunch aosp_mido-user
-#m aex -j$(nproc --all)
-make init
+m aex -j10
+
+# upload rom
+time rclone copy out/target/product/mido/AospExtended*.zip cirrus:mido -P
