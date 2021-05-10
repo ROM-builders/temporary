f6b45dd829be6632c5f82090830d39ebbf889b85
AryanAhire <66762638+sasukeuchiha-clan@users.noreply.github.com>
2021-05-10 10:55:30 +0530

    Delete sync_rom.sh

--- sync_rom.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -exv
-
-# sync rom
-repo init --depth=1 -u https://github.com/CherishOS/android_manifest.git -b eleven
-git clone https://github.com/sasukeuchiha-clan/Begonia --depth 1 -b CherishOS .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
