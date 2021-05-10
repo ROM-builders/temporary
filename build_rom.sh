dcda024d7ea8a6ed84af64a02b872900be9eddac
Apon77 <khalakuzzamanapon5@gmail.com>
2021-05-10 19:09:56 +0600

    mojito-crdroidandroid-boedhack

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # sync rom
-repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests --depth=1
+repo init --depth=1 -u git://github.com/crdroidandroid/android.git -b 11.0 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/boedhack/local_manifest.git --depth 1 -b 11.0 .repo/local_manifests --depth=1
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # build rom
 source build/envsetup.sh
-lunch aosp_mido-user
-m init
+lunch lineage_mojito-userdebug
+mka bacon
 
 # upload rom
-time rclone copy out/target/product/mido/AospExtended*.zip cirrus:mido -P
+time rclone copy out/target/product/mojito/*Unofficial-test.zip cirrus:mojito -P
