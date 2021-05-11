7d4c9d8298ac9c36f335fa8c67279865f48d3538
Hotorou <37245252+ZunayedDihan@users.noreply.github.com>
2021-05-11 16:42:05 +0600

    LineageOS- daisy(test-1)

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
 # sync rom
-repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests --depth=1
+repo init --depth=1 -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/ZunayedDihan/hotorou_manifest --depth 1 -b eleven .repo/local_manifests --depth=1
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # build rom
 source build/envsetup.sh
-lunch aosp_mido-user
-m aex -j10
+lunch lineage_daisy-userdebug
+mka bacon -j10
 
 # upload rom
-time rclone copy out/target/product/mido/AospExtended*.zip cirrus:mido -P
+set -exv
+
+# Upload ROM
+upload(){
+	curl --upload-file $1 https://transfer.sh/$(basename $1); echo
+	# 14 days, 10 GB limit
+}
+
+upload out/target/product/daisy/*.zip
