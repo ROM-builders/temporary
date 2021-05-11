cd58189907803b70b67174fe0bae20470a6a5e75
Sarthak <sarthakroy2002@gmail.com>
2021-05-11 07:43:57 +0530

    los rmx2020

--- sync_rom.sh
+++ sync_rom.sh
@@ -3,7 +3,7 @@
 set -exv
 
 # sync rom
-repo init --depth=1 -u git://github.com/ForkLineageOS/android.git -b lineage-18.1
+repo init --depth=1 -u git://github.com/LineageOS/android.git -b lineage-18.1
 git clone https://github.com/Realme-G70-Series/local_manifest.git -b 11-staging --depth=1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
