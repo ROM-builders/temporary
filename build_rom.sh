42b287211bec20bb4de8264bfcca33252b9274bf
Apon77 <khalakuzzamanapon5@gmail.com>
2021-05-12 02:17:23 +0600

    daisy-LineageOS-LinkBoi00

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,12 @@
 # sync rom
 repo init --depth=1 -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/sarthakroy2002/local_manifest.git --depth=1 .repo/local_manifests
+git clone https://github.com/LinkBoi00/linkmanifest -b eleven --depth=1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # build rom
 source build/envsetup.sh
-lunch lineage_RMX2050-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
-export SKIP_ABI_CHECKS=true
-export SKIP_API_CHECKS=true
+lunch lineage_daisy-userdebug
 mka bacon
 
 # upload rom
-rclone copy out/target/product/RMX2050/*UNOFFICIAL*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d _ -f 2 | cut -d - -f 1) -P
+rclone copy out/target/product/daily/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d _ -f 2 | cut -d - -f 1) -P
