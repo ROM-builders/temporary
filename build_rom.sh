e68e4a469d1b8455e3a010d63b3c8fdd3a1099a8
mukulsharma05175 <73372165+mukulsharma05175@users.noreply.github.com>
2021-05-11 12:01:14 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,24 +4,20 @@ set -e
 set -x
 
 # sync rom
-repo init --depth=1 -u git://github.com/CipherOS/android_manifest.git -b eleven
-
-git clone https://github.com/whtisusername/frostmanifest.git --depth=1 -b derp .repo/local_manifests
-
+repo init --depth=1 -u git://github.com/Octavi-OS/android_manifest.git -b eleven
+git clone https://github.com/mukulsharma05175/frostmanifest.git --depth=1 -b derp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 
 # build rom
 source build/envsetup.sh
-export CIPHER_GAPPS=true
-lunch lineage_lavender-userdebug
+lunch octavi_lavender-userdebug
 make bacon
 
 # upload rom
 up(){
-	curl --upload-file $1 https://transfer.sh/$(basename $1); echo
-	# 14 days, 10 GB limit
+  curl --upload-file $1 https://transfer.sh/$(basename $1); echo
+  # 14 days, 10 GB limit
 }
 
 up out/target/product/lavender/*.zip
-
