63c8f2303763c4fd75ec0322c7912d30b28e4ae2
adrian-8901 <adrianlam230@gmail.com>
2021-05-11 18:48:53 +0800

    Trigger

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+rm -rf /tmp/rom
+mkdir -p /tmp/rom
+cd tmp/rom
+
+df -h 
+
+
 repo init --depth=1 -u https://github.com/Project-Fluid/manifest.git -b fluid-11  || true
 
     rm -rf .repo/local_manifests || true
