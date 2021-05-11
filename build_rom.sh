191dad30830eac60ac5fa0c0f67d125fade8b69e
adrian-8901 <adrianlam230@gmail.com>
2021-05-11 18:44:00 +0800

    Update

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
+repo init --depth=1 -u https://github.com/Project-Fluid/manifest.git -b fluid-11  || true
 
-# sync rom
-repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests --depth=1
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+    rm -rf .repo/local_manifests || true
+     git clone https://github.com/adrian-8901/.repo-local_manifest.git --depth 1 -b fluid .repo/local_manifests
+     repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+     source build/envsetup.sh
+     lunch fluid_umi-userdebug
+     mka bacon -j$(nproc --all)
+    ccache -s
+    up() {
+      curl --upload-file $1 https://transfer.sh/$(basename $1); echo
+      # 14 days, 10 GB limit
+    }
 
-# build rom
-source build/envsetup.sh
-lunch aosp_mido-user
-m aex -j10
-
-# upload rom
-time rclone copy out/target/product/mido/AospExtended*.zip cirrus:mido -P
+    up out/target/product/umi/*.zip
