4a7305e265f4b7a92d7d8420492382891919430a
Apon77 <khalakuzzamanapon5@gmail.com>
2021-05-12 01:09:38 +0600

    check

--- build_rom.sh
+++ build_rom.sh
@@ -9,7 +9,7 @@ lunch lineage_RMX2050-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export SKIP_ABI_CHECKS=true
 export SKIP_API_CHECKS=true
-mka bacon
+mka init
 
 # upload rom
 rclone copy out/target/product/RMX2050/*UNOFFICIAL*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d _ -f 2 | cut -d - -f 1) -P
