06c1172cad936a687fffb0a2faebac4838d6fcf0
SriBalaji <sribalajisenthilkumar@gmail.com>
2021-05-13 03:06:47 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -23,3 +23,4 @@ mka bacon
 # upload rom
 device=$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d _ -f 2 | cut -d - -f 1)
 rclone copy out/target/product/RMX2001/crdroidandroid*.zip cirrus:$device -P
+
