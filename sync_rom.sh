f31a268a98617f104a56bbdc83a450ca208a247c
boedhack99 <boedhack@gmail.com>
2021-05-12 19:43:46 +0700

    test sync

--- sync_rom.sh
+++ sync_rom.sh
@@ -6,3 +6,4 @@ set -exv
 repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x
 git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+
