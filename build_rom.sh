b8f2f362c7af76afd8a63d721cc4408380d1f7d3
boedhack99 <boedhack@gmail.com>
2021-05-12 15:34:53 +0700

    back to stock

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@
 # sync rom
 repo init -u git://github.com/crdroidandroid/android.git --depth=1 -b 11.0 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/boedhack/local_manifest.git -b 11.0 --depth 1 .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync --fail-fast -j$(nproc --all)
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # build rom
 source build/envsetup.sh
