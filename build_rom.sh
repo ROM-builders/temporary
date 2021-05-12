0ce42d61cbefe8f38289b491b564c4c1f7d1aa4c
Apon77 <khalakuzzamanapon5@gmail.com>
2021-05-12 09:57:51 +0600

    Making better logs

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,13 @@
-#!/bin/bash
-
 # sync rom
 repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests --depth=1
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
 
 # build rom
 source build/envsetup.sh
 lunch aosp_mido-user
-m aex -j10
+m aex
 
 # upload rom
-time rclone copy out/target/product/mido/AospExtended*.zip cirrus:mido -P
+device=$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d _ -f 2 | cut -d - -f 1)
+rclone copy out/target/product/mido/AospExtended*.zip cirrus:$device -P
