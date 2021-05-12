6b3765a8544b9ced92e6d5b02e94b672eee9d0f7
Apon77 <khalakuzzamanapon5@gmail.com>
2021-05-12 09:28:29 +0600

    Trigger 1 - 12 may 9:28

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,13 @@
 # sync rom
-repo init --depth=1 -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/sarthakroy2002/local_manifest.git --depth=1 .repo/local_manifests
+repo init --depth=1 -u https://github.com/PixelExperience/manifest -b eleven-plus -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/DhruvChhura/mainfest_personal.git --depth=1 -b pe .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # build rom
 source build/envsetup.sh
-lunch lineage_RMX2050-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
-export SKIP_ABI_CHECKS=true
-export SKIP_API_CHECKS=true
-mka bacon
+export ALLOW_MISSING_DEPENDENCIES=true
+lunch aosp_ysl-user
+make bacon
 
 # upload rom
-rclone copy out/target/product/RMX2050/*UNOFFICIAL*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d _ -f 2 | cut -d - -f 1) -P
+rclone copy out/target/product/ysl/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d _ -f 2 | cut -d - -f 1) -P
