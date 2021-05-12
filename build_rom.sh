7588e1ab8e725b888ac03ac014236dfa75869973
Khalakuzzaman Apon <61594531+Apon77@users.noreply.github.com>
2021-05-12 21:07:39 +0600

    Fix download link [skip ci]

--- build_rom.sh
+++ build_rom.sh
@@ -9,5 +9,4 @@ lunch aosp_mido-user
 m aex
 
 # upload rom
-device=$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d _ -f 2 | cut -d - -f 1)
-rclone copy out/target/product/mido/AospExtended*.zip cirrus:$device -P
+rclone copy out/target/product/mido/AospExtended*.zip cirrus:mido -P
