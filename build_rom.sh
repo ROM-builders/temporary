414a7710f6a2e02581dd9abd6f774af2b401b706
rahul9999xda <rahul9999xda@gmail.com>
2021-05-12 17:34:54 +0000

    Fix Build Script
    Signed-off-by: rahul9999xda <rahul9999xda@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,12 @@
-#!/bin/bash
-
-set -exv
+# sync rom
+repo init --depth=1 -u git://github.com/Corvus-R/android_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/AhmedElwakil2004/local_manifest_test.git --depth=1 -b corvus .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch evolution_mido-user
-mka bacon
+lunch corvus_mido-userdebug
+mka corvus
 
+# upload rom
+rclone copy out/target/product/mido/Corvus-R*.zip cirrus:mido -P
