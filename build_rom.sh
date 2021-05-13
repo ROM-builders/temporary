df1670fa6a772a655cdbdceb4bac451f93c35488
boedhack99 <boedhack@gmail.com>
2021-05-13 23:45:11 +0700

    remove no need

--- build_rom.sh
+++ build_rom.sh
@@ -1,17 +1,15 @@
-#!/bin/bash
-
 # sync rom
 repo init -u git://github.com/crdroidandroid/android.git --depth=1 -b 11.0 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/boedhack/local_manifest.git -b 11.0 --depth 1 .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
 
 # build rom
 . build/envsetup.sh
-lunch lineage_mojito-userdebug
+lunch lineage_mido-userdebug
 export SKIP_API_CHECKS=true
 export SKIP_ABI_CHECKS=true
 mka bacon
 
 # upload rom
-time rclone copy out/target/product/mojito/*.zip cirrus:mojito -P
-
+device=$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d _ -f 2 | cut -d - -f 1)
+rclone copy out/target/product/mojito/*.zip cirrus:$device -P
