addb115fed94f8ff2580c1bfc7b554cf1b5943d8
Flashokiller <75789819+flashokiller@users.noreply.github.com>
2021-05-13 14:42:36 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,12 +3,7 @@
 set -exv
 
 # sync rom
-repo init -u  git://github.com/AOSiP/platform_manifest.git -b eleven --depth=1
-
-
-
-
-
+repo init -u  git://github.com/AOSiP/platform_manifest.git -b eleven --depth=1 -g default,-device,-mips,-darwin,-notdefault
 git clone  https://github.com/flashokiller/mainfest_personal --depth=1  .repo/local_manifests -b master
 repo sync --force-sync --no-tags --no-clone-bundle
 
@@ -22,3 +17,6 @@ rm  -rf vendor/codeaurora/telephony hardware/qcom-caf/msm8996/media hardware/qco
 source build/envsetup.sh
 lunch aosip_ysl-userdebug
 time m kronic
+
+# upload rom
+rclone copy out/target/product/ysl/Aosip*.zip
