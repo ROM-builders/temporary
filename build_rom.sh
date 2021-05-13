fdfc0d0501755e934a05b17892163f2d91872f53
Flashokiller <75789819+flashokiller@users.noreply.github.com>
2021-05-13 14:15:31 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,8 +7,8 @@ repo init -u git://github.com/AOSiP/platform_manifest.git -b eleven --depth=1 -g
 git clone https://github.com/flashokiller/mainfest_personal --depth=1  .repo/local_manifests -b master
 repo sync --force-sync --no-tags --no-clone-bundle 
 
-#huls
-hardware/qcom-caf/msm8996-ysl/media hardware/qcom-caf/msm8996-ysl/audio hardware/qcom-caf/msm8996-ysl/display && git clone https://github.com/wave-project/vendor_codeaurora_telephony --depth=1 --single-branch vendor/codeaurora/telephony/ && git clone --single-branch https://github.com/Jabiyeff/android_hardware_qcom_media hardware/qcom-caf/msm8996-ysl/media && git clone --single-branch https://github.com/Jabiyeff/android_hardware_qcom_display hardware/qcom-caf/msm8996-ysl/display &&  git clone https://github.com/LineageOS/android_hardware_qcom_audio --single-branch -b lineage-18.1-caf-msm8996 hardware/qcom-caf/msm8996-ysl/audio
+#hals 
+rm -rf vendor/codeaurora/telephony && git clone https://github.com/wave-project/vendor_codeaurora_telephony --depth=1 --single-branch vendor/codeaurora/telephony/ && git clone --single-branch https://github.com/Jabiyeff/android_hardware_qcom_media hardware/qcom-caf/msm8996-ysl/media && git clone --single-branch https://github.com/Jabiyeff/android_hardware_qcom_display hardware/qcom-caf/msm8996-ysl/display &&  git clone https://github.com/LineageOS/android_hardware_qcom_audio --single-branch -b lineage-18.1-caf-msm8996 hardware/qcom-caf/msm8996-ysl/audio
 
 #uild rom
 . build/envsetup.sh
