af89ab3eda363b9fb1d529670ec7ec11865a51a3
Flashokiller <75789819+flashokiller@users.noreply.github.com>
2021-05-13 21:54:54 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,6 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
+export ALLOW_MISSING_DEPENDENCIES=true
 lunch aosip_ysl-user
 time m kronic
 
