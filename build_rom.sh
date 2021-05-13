c4ec5713b9beb601434440a580f32392bb927bc2
P-Salik <peersalik16@gmail.com>
2021-05-13 12:37:15 +0530

    Pixel Experience

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ set -x
 
 
 # sync rom
-repo init -u https://github.com/PixelExperience/manifest --depth=1 -b eleven
+repo init -u https://github.com/PixelExperience/manifest --depth=1 -b eleven -g default,-device,-mips,-darwin,-notdefault
 
 git clone https://github.com/P-Salik/local_manifest --depth=1 -b main .repo/local_manifests
 
