6fc759432aa19ab5d4e2ffa438cbd4e2ed97b923
P-Salik <peersalik16@gmail.com>
2021-05-13 09:37:10 +0530

    PixelExperience

--- build_rom.sh
+++ build_rom.sh
@@ -6,11 +6,11 @@ set -x
 
 
 # sync rom
-#repo init -u https://github.com/PixelExperience/manifest --depth=1 -b eleven
+repo init -u https://github.com/PixelExperience/manifest --depth=1 -b eleven
 
-#git clone https://github.com/P-Salik/local_manifest --depth=1 -b main .repo/local_manifests
+git clone https://github.com/P-Salik/local_manifest --depth=1 -b main .repo/local_manifests
 
-#repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # build
 . build/envsetup.sh
@@ -19,9 +19,9 @@ chmod -R 660 out/target/product/RMX1941/
 mka bacon -j$(nproc --all)
 
 # upload
-#up(){
-#        curl --upload-file $1 https://transfer.sh/$(basename $1); echo
-#        # 14 days, 10 GB limit
-#}
+up(){
+        curl --upload-file $1 https://transfer.sh/$(basename $1); echo
+        # 14 days, 10 GB limit
+}
 
-#up out/target/product/RMX1941/*.zip
+up out/target/product/RMX1941/*.zip
