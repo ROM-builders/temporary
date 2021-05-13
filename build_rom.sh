809c24de02ae338b30be92ad5f006e02398f5f60
Unknownbitch <68426327+Unknownbitch07@users.noreply.github.com>
2021-05-13 15:55:23 +0530

    update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
+repo init --depth=1 -u git://github.com/ShapeShiftOS/android_manifest.git -b android_11 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/Unknownbitch07/local_manifest.git --depth=1 -b ssos .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
 
 # build rom
 source build/envsetup.sh
-lunch aosp_mido-user
-m aex
+lunch ssos_tissot-userdebug
+make bacon
 
 # upload rom
-rclone copy out/target/product/mido/AospExtended*.zip cirrus:mido -P
+rclone copy out/target/product/tissot/ShapeShiftOS*.zip cirrus:mido -P
