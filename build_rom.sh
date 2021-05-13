d4cb9b836e6a4671450183d738f7c9d41c8ad338
BOND007 <mhashimkp@gmail.com>
2021-05-13 22:48:00 +0530

    #Upload Rom

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
+repo init --depth=1 -u https://github.com/RevengeOS/android_manifest -b r11.0 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/Hashimkp/local_manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
 
 # build rom
 source build/envsetup.sh
-lunch aosp_mido-user
-m aex
+lunch revengeos_RMX1971-userdebug
+make bacon
 
 # upload rom
-rclone copy out/target/product/mido/AospExtended*.zip cirrus:mido -P
+rclone copy out/target/product/RMX1971/http://RevengeOS-4.0-R-UNOFFICIAL-RMX1971-20210513-1418.zip cirrus:RMX1971 -P
