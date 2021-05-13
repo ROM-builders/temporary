6626babed7ea8c60d25f76ad5cf8db061587319e
Flashokiller <75789819+flashokiller@users.noreply.github.com>
2021-05-13 12:48:45 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,13 @@
 
 set -exv
 
+# sync rom
+repo init -u  git://github.com/crdroidandroid/android.git -b 11.0 --depth=1
+git clone https://github.com/flashokiller/mainfest_presonal--depth=1  .repo/local_manifests -b aex
+repo sync --force-sync --no-tags --no-clone-bundle
+
+
 #uild rom
 . build/envsetup.sh
-lunch crdroid_ysl-userdebug
-brunch ysl
+lunch aosip_ysl-userdebug
+time m kronic
