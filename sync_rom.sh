c19f1c665845e37ef6033f818f2cdebc8f47700f
Flashokiller <75789819+flashokiller@users.noreply.github.com>
2021-05-13 09:06:20 +0530

    Update sync_rom.sh

--- sync_rom.sh
+++ sync_rom.sh
@@ -16,5 +16,5 @@ repo sync --force-sync --no-tags --no-clone-bundle
 
 #huls
 rm  -rf vendor/codeaurora/telephony hardware/qcom-caf/msm8996/media hardware/qcom-caf/msm8996/audio hardware/qcom-caf/msm8996/display && git clone https://github.com/wave-project/vendor_codeaurora_telephony --depth=1 --single-branch vendor/codeaurora/telephony/ && git clone --single-branch https://github.com/Jabiyeff/android_hardware_qcom_media hardware/qcom-caf/msm8996/media && git clone --single-branch https://github.com/Jabiyeff/android_hardware_qcom_display hardware/qcom-caf/msm8996/display &&  git clone https://github.com/LineageOS/android_hardware_qcom_audio --single-branch -b lineage-18.1-caf-msm8996 hardware/qcom-caf/msm8996/audio
-
+#hmm
 
