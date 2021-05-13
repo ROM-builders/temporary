89a813c6c14406839ed2041db2ecc681ca66e6d3
P-Salik <peersalik16@gmail.com>
2021-05-13 18:39:27 +0530

    DotOS

--- build_rom.sh
+++ build_rom.sh
@@ -18,15 +18,15 @@ wget https://github.com/PixelExperience/external_selinux/commit/9d6ebe89430ffe0a
 patch -p1 < *.patch
 cd ../..
 
-cd frameworks/base
-wget https://github.com/PixelExperience/frameworks_base/commit/37f5a323245b0fd6269752742a2eb7aa3cae24a7.patch
-patch -p1 < *.patch
-cd ../..
+#cd frameworks/base
+#wget https://github.com/PixelExperience/frameworks_base/commit/37f5a323245b0fd6269752742a2eb7aa3cae24a7.patch
+#patch -p1 < *.patch
+#cd ../..
 
-cd frameworks/opt/net/wifi
-wget https://github.com/PixelExperience/frameworks_opt_net_wifi/commit/3bd2c14fbda9c079a4dc39ff4601ba54da589609.patch
-patch -p1 < *.patch
-cd ../../../..
+#cd frameworks/opt/net/wifi
+#wget https://github.com/PixelExperience/frameworks_opt_net_wifi/commit/3bd2c14fbda9c079a4dc39ff4601ba54da589609.patch
+#patch -p1 < *.patch
+#cd ../../../..
 
 cd frameworks/opt/net/ims
 wget https://github.com/PixelExperience/frameworks_opt_net_ims/commit/661ae9749b5ea7959aa913f2264dc5e170c63a0a.patch
