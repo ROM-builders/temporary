5779c5856412b1c9cea16be5fbc4b67a5ecdba82
adrian-8901 <adrianlam230@gmail.com>
2021-05-13 21:16:41 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@
 set -exv
 
 # sync rom
-repo init -u https://github.com/Project-Fluid/manifest.git --depth=1 -b fluid-11
+repo init -u https://github.com/adrian-8901/manifest.git --depth=1 -b fluid-11
 git clone https://github.com/adrian-8901/local_mainfest.git --depth=1 -b fluid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 echo finished sync
