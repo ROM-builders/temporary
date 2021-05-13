185fae5c82a2364faa3ed49cd1c4fe02df5c5bf5
boedhack99 <boedhack@gmail.com>
2021-05-13 16:32:18 +0700

    ppui bringup

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 # sync rom
-repo init -u git://github.com/crdroidandroid/android.git --depth=1 -b 11.0 -g default,-device,-mips,-darwin,-notdefault
+repo init -u git://github.com/PixelPlusUI/manifest.git --depth=1 -b eleven -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/boedhack/local_manifest.git -b 11.0 --depth 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # build rom
 . build/envsetup.sh
-lunch lineage_mojito-userdebug
-export SKIP_API_CHECKS=true
-export SKIP_ABI_CHECKS=true
+lunch aosp_mojito-userdebug
 mka bacon
 
 # upload rom
