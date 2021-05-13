532cf2a650b45ed3043e170f4bde2fb8bc21c98b
Erik Garcia <eriikgarciia1999@gmail.com>
2021-05-13 16:12:02 -0300

    beyond0lte: BUILD AEX (01 - 13/04)

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
 repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
+git clone https://github.com/ECr34T1v3/android_.repo_local_manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
 
 # build rom
 source build/envsetup.sh
-lunch aosp_mido-user
+lunch aosp_beyond0lte-userdebug
 m aex
 
 # upload rom
-rclone copy out/target/product/mido/AospExtended*.zip cirrus:mido -P
+rclone copy out/target/product/beyond0lte/AospExtended*.zip cirrus:beyond0lte -P
