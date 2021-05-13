35d768672b60946e5677adef55871ac49e805aa7
LinkBoi00 <50962670+LinkBoi00@users.noreply.github.com>
2021-05-13 18:55:23 +0300

    daisy: Add LineageOS vendor source patch
    - Original commit: https://github.com/PotatoProject/vendor_potato/commit/98564698591bc2420da7de16a724e83d47b59006

--- build_rom.sh
+++ build_rom.sh
@@ -3,6 +3,12 @@ repo init --depth=1 -u git://github.com/LineageOS/android.git -b lineage-18.1 -g
 git clone https://github.com/LinkBoi00/linkmanifest -b eleven --depth=1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
+# Apply source patch(es)
+cd vendor/lineage
+wget https://oshi.at/CJHLPo -O "1.patch"
+git apply 1.patch
+cd .. && cd ..
+
 # build rom
 source build/envsetup.sh
 lunch lineage_daisy-user
