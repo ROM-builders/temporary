b829389a960165992e2fbf82c571a383876a620d
Flashokiller <75789819+flashokiller@users.noreply.github.com>
2021-05-13 21:43:03 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,12 @@
-#!/bin/bash
+# sync rom
+repo init --depth=1 -u git://github.com/AOSiP/platform_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
+git clone  --depth 1 https://github.com/flashokiller/mainfest_personal -b master .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
 
-set -exv
-
-. build/envsetup.sh
-lunch aosip_ysl-userdebug
+# build rom
+source build/envsetup.sh
+lunch aosip_ysl-user
 time m kronic
+
+# upload rom
+rclone copy out/target/product/ysl/Aosip*.zip 
