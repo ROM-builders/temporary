1900acfbb1a9cfd5f0f1f215203e7f9bc0e1f5a6
P-Salik <peersalik16@gmail.com>
2021-05-14 02:10:42 -0700

    Pixel Experience Enforcing

--- build_rom.sh
+++ build_rom.sh
@@ -3,40 +3,39 @@
 set -e
 set -x
 
-#sudo apt install wget
 
 # sync rom
-repo init -u https://github.com/ArrowOS/android_manifest.git -b arrow-11.0 --depth=1 -g default,-device,-mips,-darwin,-notdefault
+repo init -u https://github.com/PixelExperience/manifest -b eleven --depth=1 -g default,-device,-mips,-darwin,-notdefault
 
 git clone https://github.com/P-Salik/local_manifest --depth=1 -b main .repo/local_manifests
 
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j4
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # patches
-cd external/selinux
-curl -LO https://github.com/PixelExperience/external_selinux/commit/9d6ebe89430ffe0aeeb156f572b2a810f9dc98cc.patch
-patch -p1 < *.patch
-cd ../..
+#cd external/selinux
+#curl -LO https://github.com/PixelExperience/external_selinux/commit/9d6ebe89430ffe0aeeb156f572b2a810f9dc98cc.patch
+#patch -p1 < *.patch
+#cd ../..
 
-cd frameworks/base
-curl -LO https://github.com/PixelExperience/frameworks_base/commit/37f5a323245b0fd6269752742a2eb7aa3cae24a7.patch
-patch -p1 < *.patch
-cd ../..
+#cd frameworks/base
+#curl -LO https://github.com/PixelExperience/frameworks_base/commit/37f5a323245b0fd6269752742a2eb7aa3cae24a7.patch
+#patch -p1 < *.patch
+#cd ../..
 
-cd frameworks/opt/net/wifi
-curl -LO https://github.com/PixelExperience/frameworks_opt_net_wifi/commit/3bd2c14fbda9c079a4dc39ff4601ba54da589609.patch
-patch -p1 < *.patch
-cd ../../../..
+#cd frameworks/opt/net/wifi
+#curl -LO https://github.com/PixelExperience/frameworks_opt_net_wifi/commit/3bd2c14fbda9c079a4dc39ff4601ba54da589609.patch
+#patch -p1 < *.patch
+#cd ../../../..
 
-cd frameworks/opt/net/ims
-curl -LO https://github.com/PixelExperience/frameworks_opt_net_ims/commit/661ae9749b5ea7959aa913f2264dc5e170c63a0a.patch
-patch -p1 < *.patch
-cd ../../../..
+#cd frameworks/opt/net/ims
+#curl -LO https://github.com/PixelExperience/frameworks_opt_net_ims/commit/661ae9749b5ea7959aa913f2264dc5e170c63a0a.patch
+#patch -p1 < *.patch
+#cd ../../../..
 
 # build
 . build/envsetup.sh
-lunch arrow_RMX1941-userdebug
-m bacon
+lunch aosp_RMX1941-userdebug
+mka bacon -j$(nproc --all)
 
 # upload
 rclone copy out/target/product/RMX1941/*UNOFFICIAL*.zip cirrus:RMX1941 -P
