2c4ad8fc786cfb18adb0643bf93bcbf435e6307d
adrian-8901 <adrianlam230@gmail.com>
2021-05-14 20:56:15 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -2,20 +2,18 @@
 
 df -h 
 
-repo init --depth=1 -u https://github.com/adrian-8901/manifest.git -b fluid-11  || true
-
-    rm -rf .repo/local_manifests || true
-     git clone https://github.com/adrian-8901/.repo-local_manifest.git --depth 1 -b fluid .repo/local_manifests
-     repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
-     source build/envsetup.sh
-     lunch fluid_umi-userdebug
-     mka bacon -j$(nproc --all)
-    ccache -s
-    up() {
+## sync rom
+repo init -u https://github.com/Project-Fluid/manifest.git --depth=1 -b fluid-11
+git clone https://github.com/adrian-8901/local_mainfest.git --depth=1 -b fluid .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+bash .repo/local_manifests/hal.sh
+echo finished sync
+lunch fluid_umi-userdebug
+mka bacon -j$(nproc --all)
+ccache -s
+up() {
       curl --upload-file $1 https://transfer.sh/$(basename $1); echo
       # 14 days, 10 GB limit
     }
 
-    up out/target/product/umi/*.zip
-
-#
+up out/target/product/umi/*.zip
