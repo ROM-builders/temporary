ef0add110cf39ff070cc279a78f3fdfd2542ba61
Baibhab <60211800+baibhab34@users.noreply.github.com>
2021-05-14 22:58:40 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,21 +1,12 @@
-#!/bin/bash
-
-set -e
-set -x
-
 # sync rom
+repo init --depth=1 -u https://github.com/LineageOS/android -b lineage-17.1 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/baibhab34/local_manifest --depth 1 -b los .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
 
-repo init -u git://github.com/LineageOS/android.git -b lineage-17.1 --depth=1 -g default,-device,-mips,-darwin,-notdefault
-
-git clone https://github.com/baibhab34/local_manifest --depth=1 -b los .repo/local_manifests
-
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
-
-# build
-. build/envsetup.sh
-lunch lineage_RMX1805-userdebug
+# build rom
+source build/envsetup.sh
+lunch lineage_RMX1805-user
 mka bacon
 
-# upload
+# upload rom
 rclone copy out/target/product/RMX1805/lineage*.zip cirrus:RMX1805 -P
