827324670286643b9547b2ed9f2d5590e5ed3c4e
Rehan Singh <54594678+theRay1s@users.noreply.github.com>
2021-05-14 20:42:14 +0530

    Delete sync_rom.sh

--- sync_rom.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -exv
-
-#sync rom
-repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x
-git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
