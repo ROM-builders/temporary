f8260ba6170c32f420c7c31e242926ce8b7d48f3
boedhack99 <boedhack@gmail.com>
2021-05-14 08:43:03 +0700

    unofficial build

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/boedhack/local_manifest.git -b 11.0 --depth=1 .repo
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
 
 # build rom
-. build/envsetup.sh
+suorce build/envsetup.sh
 lunch lineage_mojito-userdebug
 export SKIP_API_CHECKS=true
 export SKIP_ABI_CHECKS=true
@@ -12,4 +12,4 @@ mka bacon
 
 # upload rom
 device=$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d _ -f 2 | cut -d - -f 1)
-rclone copy out/target/product/mojito/*.zip cirrus:$device -P
+rclone copy out/target/product/mojito/crDroid*.zip cirrus:$device -P
