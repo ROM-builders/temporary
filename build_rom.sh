ece8aa3fa9a0b5cc0b71d4e7ad81e08af91382c1
boedhack99 <boedhack@gmail.com>
2021-05-14 16:51:32 +0700

    trrigered 05:00 pm

--- build_rom.sh
+++ build_rom.sh
@@ -13,4 +13,3 @@ mka bacon
 # upload rom
 device=$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d _ -f 2 | cut -d - -f 1)
 rclone copy out/target/product/mojito/crDroid*.zip cirrus:$device -P
-
