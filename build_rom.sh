ec2e469f6acd732e1e864f09db3f36db83f4873c
Anoos Ragh <83896147+cArN4gEisDeD@users.noreply.github.com>
2021-05-14 09:56:08 +0530

    RMX1941-los

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,24 @@
+#!/bin/bash
+
+set -e
+set -x
+
+sudo apt install wget -y
+
+rm -rf .repo/local_manifest
+
 # sync rom
-repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
+repo init -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
 
-# build rom
-source build/envsetup.sh
-lunch aosp_mido-user
-m aex
+git clone https://github.com/cArN4gEisDeD/local_manifest
 
-# upload rom
-rclone copy out/target/product/mido/AospExtended*.zip cirrus:mido -P
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+
+# build
+. build/envsetup.sh
+lunch lineage_RMX1941-userdebug
+
+mka bacon -j$(nproc --all)
+
+#upload rom
+rclone copy out/target/product/RMX1941/*UNOFFICIAL*.zip cirrus:RMX1941 -P
