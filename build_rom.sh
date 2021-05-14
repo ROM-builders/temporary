ba7dc8e5c018366fbc4bc4a228efcc51b3cd2cda
Anoos Ragh <83896147+cArN4gEisDeD@users.noreply.github.com>
2021-05-14 19:25:12 +0530

    RMX1941-PE

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,22 @@
+#!/bin/bash
+
+set -e
+set -x
+
+
 # sync rom
-repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
 
-# build rom
-source build/envsetup.sh
-lunch aosp_mido-user
-m aex
+repo init -u https://github.com/PixelExperience/manifest -b eleven --depth=1 -g default,-device,-mips,-darwin,-notdefault
 
-# upload rom
-rclone copy out/target/product/mido/AospExtended*.zip cirrus:mido -P
+git clone https://github.com/cArN4gEisDeD/local_manifest --depth=1 -b main .repo/local_manifests
+
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
+
+# build
+. build/envsetup.sh
+lunch aosp_RMX1941-userdebug
+mka bacon
+
+# upload
+rclone copy out/target/product/RMX1941/*UNOFFICIAL*.zip cirrus:RMX1941 -P
