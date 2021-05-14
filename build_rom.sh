cb1b6c03b6cb4c9000e5d2a644ca0eb752dcedef
susandahal <susandahal53@gmail.com>
2021-05-14 10:58:22 +0545

    Lavender:Build styxOS

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
+repo init --depth=1 -u https://github.com/StyxProject/manifest -b R -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/susandahal/local_manifest --depth 1 -b styx .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_mido-user
-m aex
+lunch styx_lavender-userdebug
+m styx-ota
 
 # upload rom
-rclone copy out/target/product/mido/AospExtended*.zip cirrus:mido -P
+rclone copy out/target/product/lavender/styxOS-*.zip cirrus:lavender -P
