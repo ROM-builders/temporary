240f84f447af304d26f86754cb56815f0b347236
P-Salik <peersalik16@gmail.com>
2021-05-14 05:01:36 -0700

    Corvus

--- build_rom.sh
+++ build_rom.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-set -e
-set -x
-
 
 # sync rom
-repo init -u https://github.com/PixelExperience/manifest -b eleven --depth=1 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 -u https://github.com/Corvus-ROM/android_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
 
 git clone https://github.com/P-Salik/local_manifest --depth=1 -b main .repo/local_manifests
 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # patches
+# boot patch
 #cd external/selinux
 #curl -LO https://github.com/PixelExperience/external_selinux/commit/9d6ebe89430ffe0aeeb156f572b2a810f9dc98cc.patch
 #patch -p1 < *.patch
 #cd ../..
 
+# ims patches
 #cd frameworks/base
 #curl -LO https://github.com/PixelExperience/frameworks_base/commit/37f5a323245b0fd6269752742a2eb7aa3cae24a7.patch
 #patch -p1 < *.patch
@@ -34,8 +33,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build
 . build/envsetup.sh
-lunch aosp_RMX1941-userdebug
-mka bacon -j$(nproc --all)
+lunch corvus_RMX1941-userdebug
+make corvus
 
 # upload
 rclone copy out/target/product/RMX1941/*UNOFFICIAL*.zip cirrus:RMX1941 -P
