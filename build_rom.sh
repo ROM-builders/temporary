07ef878f3c25f6246aec45cbe8e8a39195694033
Rehan Singh <54594678+theRay1s@users.noreply.github.com>
2021-05-14 21:12:38 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # sync rom
-repo init -u git://github.com/crdroidandroid/android.git -b 10.0 --depth=1 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/theRay1s/android_manifest --depth 1 -b lineage-17.1 .repo/local_manifests
+repo init -u git://github.com/crdroidandroid/android.git -b 11.0 --depth=1 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/theRay1s/android_manifest --depth 1 -b lineage-18.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # build rom
