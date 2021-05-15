1fd9fdaf9f0a69feb3a90071cabebc790ca1a1ac
adrian-8901 <adrianlam230@gmail.com>
2021-05-15 13:41:23 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ df -h
 repo init -u https://github.com/Project-Fluid/manifest.git --depth=1 -b fluid-11
 git clone https://github.com/adrian-8901/local_mainfest.git --depth=1 -b fluid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-bash .repo/local_manifests/hal.sh
 ls 
 echo finished sync
 . build/envsetup.sh
