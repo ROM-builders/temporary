1b076777a90b94a9bb2483a518b90c8c65db21c1
Sarthak <sarthakroy2002@gmail.com>
2021-05-15 07:51:35 +0530

    los fe rmx2020 #1

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,18 @@
 # sync rom
-repo init --depth=1 -u git://github.com/ForkLineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/CPH1859/local_manifest.git --depth=1 .repo/local_manifests
+repo init --depth=1 -u git://github.com/Lineage-FE/manifest.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/Realme-G70-Series/local_manifest.git --depth=1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # build rom
 source build/envsetup.sh
-lunch lineage_CPH1859-userdebug
+lunch lineage_RMX2020-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export SKIP_ABI_CHECKS=true
 export SKIP_API_CHECKS=true
 mka bacon -j$(nproc --all)
 
 # upload rom
-rclone copy out/target/product/CPH1859/*UNOFFICIAL*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d _ -f 2 | cut -d - -f 1) -P
+curl -sL https://git.io/file-transfer | sh
+./transfer wet out/target/product/RMX2020/*UNOFFICIAL*.zip
+rclone copy out/target/product/RMX2020/*UNOFFICIAL*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d _ -f 2 | cut -d - -f 1) -P
 
