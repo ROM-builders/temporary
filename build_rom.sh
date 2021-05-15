36944401f6a024c526f6d380a82eb17adccd6502
ItsVixano <vixanonew@gmail.com>
2021-05-15 18:48:57 +0200

    Update for los 16 daisy
    Change-Id: I1e67407e678df1a3abb73bbb1a8c6d1e72dc9595

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,17 @@
 # sync rom
-repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
+repo init --depth=1 -u git://github.com/LineageOS/android.git -b lineage-16.0 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/ItsVixano/local_manifest --depth 1 -b daisy-pie .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
+
+# Install imagemagick
+sudo apt update -y && sudo apt install imagemagick -y
 
 # build rom
 source build/envsetup.sh
-lunch aosp_mido-user
-m aex
+lunch lineage_daisy-userdebug
+#make clean
+croot
+brunch daisy
 
 # upload rom
-rclone copy out/target/product/mido/AospExtended*.zip cirrus:mido -P
+rclone copy out/target/product/daisy/lineage-16.0*.zip cirrus:giovannirn5 -P
