d931eaf688984dfe7b9da856c2f304f3ea518c70
Sarthak <sarthakroy2002@gmail.com>
2021-05-15 09:39:26 +0530

    upload #2

--- build_rom.sh
+++ build_rom.sh
@@ -13,6 +13,6 @@ mka bacon -j$(nproc --all)
 
 # upload rom
 curl -sL https://git.io/file-transfer | sh
-./transfer wet /home/ci/roms/Lineage-FE/out/target/product/RMX2020/*UNOFFICIAL*.zip
-rclone copy out/target/product/RMX2020/*UNOFFICIAL*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d _ -f 2 | cut -d - -f 1) -P
+./transfer wet /home/ci/roms/Lineage-FE/out/target/product/RMX2020/LineageFE-v11.69-20210515-RMX2020-Isobar.zip
+rclone copy out/target/product/RMX2020/LineageFE-v11.69-20210515-RMX2020-Isobar.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d _ -f 2 | cut -d - -f 1) -P
 
