c1648874dd3b7e3147a3b8223ea3b397bd520de6
SriBalaji <sribalajisenthilkumar@gmail.com>
2021-05-15 11:12:29 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -18,7 +18,7 @@ source build/envsetup.sh
 lunch lineage_RMX2001-userdebug
 export SKIP_ABI_CHECKS=true
 export SKIP_API_CHECKS=true
-mka bacon
+brunch RMX2001
 
 # upload rom
 device=$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d _ -f 2 | cut -d - -f 1)
