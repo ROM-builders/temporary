40a063c1622e6043e1c0e4c3dbe0ecbe42e0bdfc
Baibhab <60211800+baibhab34@users.noreply.github.com>
2021-05-15 10:37:15 +0530

    Build userdebug

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_RMX1805-user
+lunch lineage_RMX1805-userdebug
 mka bacon
 
 # upload rom
