6d1249d5760e9219656114a804f37852ab770257
Baibhab <60211800+baibhab34@users.noreply.github.com>
2021-05-15 12:09:56 +0530

    Make installclean

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_RMX1805-userdebug
+make installclean
 mka bacon
 
 # upload rom
