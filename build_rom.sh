ce9be5327da6a6592fa533525babe319ea450dd7
Baibhab <60211800+baibhab34@users.noreply.github.com>
2021-05-15 16:52:23 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_RMX1805-userdebug
-make clean
 mka bacon
 
 # upload rom
