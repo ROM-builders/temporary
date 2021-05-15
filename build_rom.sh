84a8c13fb4b09a3a65cecea94ba67a8503e329ad
fourninesix <klmajor2213@gmail.com>
2021-05-15 07:12:41 +0000

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,20 @@
 # sync rom
-repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
+repo init --depth=1 -u git://github.com/DerpFest-11/manifest.git -b 11-aosp -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/fourninesix/local_manifests -b derp --depth=1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
+
+# apply patch
+cd frameworks/base && curl -LO https://github.com/PixelExperience/frameworks_base/commit/37f5a323245b0fd6269752742a2eb7aa3cae24a7.patch && git apply 37f5a323245b0fd6269752742a2eb7aa3cae24a7.patch
+cd ../..
+cd frameworks/opt/net/ims && curl -LO https://github.com/PixelExperience/frameworks_opt_net_ims/commit/661ae9749b5ea7959aa913f2264dc5e170c63a0a.patch && git apply 661ae9749b5ea7959aa913f2264dc5e170c63a0a.patch
+cd ../../../..
+cd frameworks/opt/net/wifi && curl -LO https://github.com/PixelExperience/frameworks_opt_net_wifi/commit/3bd2c14fbda9c079a4dc39ff4601ba54da589609.patch && git apply 3bd2c14fbda9c079a4dc39ff4601ba54da589609.patch
+cd ../../../..
 
 # build rom
 source build/envsetup.sh
-lunch aosp_mido-user
-m aex
+lunch derp_merlin-userdebug
+mka derp
 
 # upload rom
-rclone copy out/target/product/mido/AospExtended*.zip cirrus:mido -P
+rclone copy out/target/product/merlin/DerpFest*.zip cirrus:merlin -P
