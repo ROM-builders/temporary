1061a20dea76354583f9a3df2be5bdce00b88fcf
Baibhab <60211800+baibhab34@users.noreply.github.com>
2021-05-15 12:40:29 +0530

    Make 17.1

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 -u https://github.com/LineageOS/android -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 -u https://github.com/LineageOS/android -b lineage-17.1 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/baibhab34/local_manifest --depth 1 -b los .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
 
 # build rom
 source build/envsetup.sh
 lunch lineage_RMX1805-userdebug
-make installclean
+make clean
 mka bacon
 
 # upload rom
