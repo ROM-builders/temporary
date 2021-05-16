c1f244d8e027ddbe37457fc6256140b868c8302c
Shazu <81510399+GangsterPlayz1@users.noreply.github.com>
2021-05-16 20:15:26 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init -u https://github.com/Kang-OS-R/android_manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
+repo init -u git://github.com/TenX-OS/manifest_TenX -b eleven -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/thebuildbomt/local_manifest.git --depth 1 -b los .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch kangos_RMX1801-userdebug
-make bacon 
+lunch aosl_RMX1801-userdebug
+make bacon -j8
 
 # upload rom
-rclone copy out/target/product/RMX1801/kangos*.zip cirrus:RMX1801 -P
+rclone copy out/target/product/RMX1801/tenx*.zip cirrus:RMX1801 -P
