9f24e364920be80cb2dc00f64b684478db69edde
MLZ94 <zugutoroberto@gmail.com>
2021-05-16 08:23:51 +0200

    add some -g flags

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 -u https://github.com/NezukoOS/manifest -b eleven
+repo init --depth=1 -u https://github.com/NezukoOS/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/MLZ94/local_manifest -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
