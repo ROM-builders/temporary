c5c55946c01ef53ee334b16e81d7d634a06ad191
Shazu <81510399+GangsterPlayz1@users.noreply.github.com>
2021-05-16 18:21:06 +0530

    Kek

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init -u https://github.com/Corvus-ROM/android_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/GangsterPlayz1/local_manifests.git --depth 1 -b aex .repo/local_manifests
+repo init -u https://github.com/PixelExperience/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/Shazu_xD/local_manifests.git --depth 1 -b aex .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch corvus_RMX1801-userdebug
-make corvus
+lunch aosp_RMX1801-userdebug
+mka bacon -jx
 
 # upload rom
-rclone copy out/target/product/RMX1801/corvus*.zip cirrus:RMX1801 -P
+rclone copy out/target/product/RMX1801/aosp*.zip cirrus:RMX1801 -P
