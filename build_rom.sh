23908fcf902d380c9a3db7b7c259a9e83ce14147
Shazu <81510399+GangsterPlayz1@users.noreply.github.com>
2021-05-16 22:25:54 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init -u https://github.com/Project-Xtended/manifest.git -b xr -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/GangsterPlayz1/local_manifests.git --depth 1 .repo/local_manifests
-repo sync --force-sync --no-tags --no-clone-bundle -jX
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 
 # build rom
