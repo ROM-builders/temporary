c647593e647c370e355ac03ef33d75687d6ce628
Sakthivel Nadar <55133980+SparXFusion@users.noreply.github.com>
2021-05-16 12:44:59 +0530

    redo rmx1941 build

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,14 @@
 # sync rom
-repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo init --depth=1 -u https://github.com/PixelExperience/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
 
-# build rom
-source build/envsetup.sh
-lunch aosp_mido-user
-m aex
+git clone https://github.com/SparXFusion/local_manifests.git --depth=1 -b main .repo/local_manifests
 
-# upload rom
-rclone copy out/target/product/mido/AospExtended*.zip cirrus:mido -P
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
+# build
+. build/envsetup.sh
+lunch aosp_RMX1941-userdebug
+mka bacon -j$(nproc --all)
+
+# upload build
+rclone copy out/target/product/RMX1941/*UNOFFICIAL*.zip cirrus:RMX1941 -P
