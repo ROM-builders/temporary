b2d608d98f4287a4a46634490022190795ab13c9
thebuildbomt <84271431+thebuildbomt@users.noreply.github.com>
2021-05-16 17:57:11 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom plz
+# sync rom 
 repo init -u https://github.com/PixelExperience/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/Shazu-xD/local_manifest --depth 1 -b los .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
