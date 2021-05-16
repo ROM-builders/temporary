7d9342c19177be0b83493d46d42e635a25f2fe67
Shazu khan <81506619+Sha-zu@users.noreply.github.com>
2021-05-16 13:53:21 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch corvus_RMX1801-userdebug
-m aex
+make corvus
 
 # upload rom
 rclone copy out/target/product/RMX1801/corvus*.zip cirrus:RMX1801 -P
