226a9743fca7ac1ddee9b16d1ff30b6ffdc20736
Shazu <81510399+GangsterPlayz1@users.noreply.github.com>
2021-05-16 19:18:21 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init -u https://github.com/PixelExperience/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Shazu_xD/local_manifests.git --depth 1 -b aex .repo/local_manifests
+repo init -u https://github.com/ResurrectionRemix/platform_manifest.git -b Q -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/Shazu_xD/local_manifests.git --depth 1 -b los .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_RMX1801-userdebug
-mka bacon -jx
+lunch rr_RMX1801-userdebug
+mka bacon 
 
 # upload rom
-rclone copy out/target/product/RMX1801/aosp*.zip cirrus:RMX1801 -P
+rclone copy out/target/product/RMX1801/rr*.zip cirrus:RMX1801 -P
