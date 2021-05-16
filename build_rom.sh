ef6259349f867560e95e14ba2750037964e56f65
Abhinavftp <abhinavftp98@gmail.com>
2021-05-16 11:21:12 +0530

    Any comment

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,14 @@
 # sync rom
-repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
+repo init -u https://github.com/Project-Fluid/manifest.git -b fluid-11 -g default,-device,-mips,-darwin,-notdefault
+
+git clone https://github.com/RMX1805/device_oppo_RMX1805 -b lineage-18.1 device/oppo/RMX1805 && git clone https://github.com/RMX1805/vendor_oppo -b lineage-18.1 vendor/oppo
+
+repo sync --force-sync --no-tags --no-clone-bundle -j$(nproc --all)
 
 # build rom
 source build/envsetup.sh
-lunch aosp_mido-user
-m aex
+lunch fluid_RMX1805-userdebug
+mka bacon 
 
 # upload rom
-rclone copy out/target/product/mido/AospExtended*.zip cirrus:mido -P
+rclone copy out/target/product/RMX1805/*UNOFFICIAL*.zip cirrus:RMX1805 -P
