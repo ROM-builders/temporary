f7f6dcb5cc331bac62974291ae362320cb8f96d8
Apon77 <khalakuzzamanapon5@gmail.com>
2021-05-16 15:51:01 +0600

    Follow main branch [skip ci]

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,14 @@
 # sync rom
-repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
+repo init --depth=1 -u git://github.com/crdroidandroid/android.git -b 11.0 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/boedhack/local_manifest.git --depth 1 -b 11.0 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
-lunch aosp_mido-user
-m aex
+. build/envsetup.sh
+lunch lineage_mojito-userdebug
+export SKIP_API_CHECKS=true
+export SKIP_ABI_CHECKS=true
+mka bacon
 
 # upload rom
-rclone copy out/target/product/mido/AospExtended*.zip cirrus:mido -P
+rclone copy out/target/product/mojito/crDroidandroid*.zip cirrus:mojito -P
