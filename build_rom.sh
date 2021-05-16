1754287b362303a0b86d51364ea446c830b47e7c
MLZ94 <zugutoroberto@gmail.com>
2021-05-16 08:15:17 +0200

    depth 1 as well

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init -u https://github.com/NezukoOS/manifest -b eleven
+repo init --depth=1 -u https://github.com/NezukoOS/manifest -b eleven
 git clone https://github.com/MLZ94/local_manifest -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
