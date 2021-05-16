c55c0cf3d8ab0167cf4bbe010f613dfb37e60c7c
Shazu <81510399+GangsterPlayz1@users.noreply.github.com>
2021-05-16 19:51:40 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init -u https://github.com/Kang-OS-R/android_manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/thebuildbomt/local_manifests.git --depth 1 -b los .repo/local_manifests
+git clone https://github.com/thebuildbomt/local_manifest.git --depth 1 -b los .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
