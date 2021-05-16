a3b744210dd2c80b956942691034c84906021c0f
thebuildbomt <84271431+thebuildbomt@users.noreply.github.com>
2021-05-16 21:00:11 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,13 @@
 # sync rom 
-repo init -u https://github.com/PixelExperience/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Shazu-xD/local_manifest --depth 1 -b los .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo init -u git://github.com/TenX-OS/manifest_TenX -b eleven -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/thebuildbomt/local_manifest --depth 1 -b los .repo/local_manifests
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+
 
 # build rom pe
 source build/envsetup.sh
 lunch aosp_RMX1801-userdebug
-mka bacon -jx
+brunch RMX1801
 
 # upload rom
 rclone copy out/target/product/RMX1801/aosp*RMX1801.zip cirrus:RMX1801 -P
