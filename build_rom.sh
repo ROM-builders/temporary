061ce71d18b86aa67bd0b23f5bbe5eb5b70b96b3
Shazu <81510399+GangsterPlayz1@users.noreply.github.com>
2021-05-17 08:06:05 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init -u https://github.com/Project-Xtended/manifest.git -b xr -g default,-device,-mips,-darwin,-notdefault
+repo init -u https://github.com/Kang-OS-R/android_manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/GangsterPlayz1/local_manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
@@ -7,8 +7,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch xtended_RMX1801-userdebug
-mka xtended
+lunch kangos_RMX1801-userdebug
+make bacon
 
 # upload rom
-rclone copy out/target/product/RMX1801/xtended*.zip cirrus:RMX1801 -P
+rclone copy out/target/product/RMX1801/kangos*.zip cirrus:RMX1801 -P
