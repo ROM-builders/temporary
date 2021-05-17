b0cf0172a8445acb7a32f48947fe5bf3340956a0
Baibhab <60211800+baibhab34@users.noreply.github.com>
2021-05-17 19:13:01 +0530

    Build losfE

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
+repo init --depth=1 -u https://github.com/Lineage-FE/manifest -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/baibhab34/local_manifest --depth 1 -b los .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_mido-user
-m aex
+lunch lineage_RMX1805-userdebug
+mka bacon
 
 # upload rom
-rclone copy out/target/product/mido/*.zip cirrus:mido -P
+rclone copy out/target/product/RMX1805/*UNOFFICIAL*.zip cirrus:RMX1805 -P
