9b7ec1520e034d89201024a254ea986106419445
dustxyz <dustxyz@criptext.com>
2021-05-17 08:45:20 -0400

    Build userdebug for now.
    Signed-off-by: dustxyz <dustxyz@criptext.com>

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch aosp_ysl-user
+lunch aosp_ysl-userdebug
 mka bacon 
 
 # upload rom
