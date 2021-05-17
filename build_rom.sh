0de321468dc40eba9adabffca4d30ffce664b339
MiDoNaSR545 <57400341+MiDoNaSR545@users.noreply.github.com>
2021-05-18 00:30:32 +0200

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,16 @@
 # sync rom
-repo init --depth=1 -u git://github.com/ShapeShiftOS/android_manifest.git -b android_11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/MiDoNaSR545/mainfest_personal.git --depth 1 -b ssos .repo/local_manifests
+repo init --depth=1 -u https://github.com/descendant-xi/manifests.git -b eleven-staging -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/MiDoNaSR545/mainfest_personal.git --depth 1 -b de .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch ssos_ysl-user
-make bacon
+lunch descendant_ysl-user
+mka descendant
 
 # upload rom
-rclone copy out/target/product/ysl/*.zip cirrus:mido -P
+rclone copy out/target/product/ysl/*.zip cirrus:ysl -P
+
+# upload again if rclone fail
+cd out/target/product/ysl
+curl -sL https://git.io/file-transfer | sh && ./transfer wet *.zip
