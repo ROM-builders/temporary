a6a01be721124a04007cf87eb82826f09603ef9f
Sushmit <80753594+sushmit1@users.noreply.github.com>
2021-05-17 13:35:47 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/sushmit1/mainfest_personal.git --depth 1 -b master 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+. build/envsetup.sh
 export SELINUX_IGNORE_NEVERALLOWS=true
 lunch corvus_ysl-userdebug
 make corvus
