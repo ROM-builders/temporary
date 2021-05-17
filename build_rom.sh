a16dc0e8e3405941c62171a3e47de0084dfafc9c
Shazu-xD <84271431+Shazu-xD@users.noreply.github.com>
2021-05-17 20:10:33 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom 
 repo init -u git://github.com/TenX-OS/manifest_TenX -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/thebuildbomt/local_manifest --depth 1 -b los .repo/local_manifests
+git clone https://github.com/Shazu-xD/local_manifest --depth 1 -b main.repo/local_manifests
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
 
