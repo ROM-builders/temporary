58f63631548cebea4b8d57b746dda2d824452016
dustxyz <dustxyz@criptext.com>
2021-05-17 10:21:26 -0400

    Don't sync 2 times.
    Signed-off-by: dustxyz <dustxyz@criptext.com>

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 -u https://github.com/HyconOS/manifest.git  -b eleven -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/Dustxyz/personal_manifest.git --depth 1 -b master .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
 
 # build rom
 source build/envsetup.sh
