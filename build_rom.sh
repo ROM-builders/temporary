944e0e42ab7763457471fe73d56e85262b34f2ea
Sarthak <sarthakroy2002@gmail.com>
2021-05-18 20:24:08 +0530

    Fix sync

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 -u git://github.com/Lineage-FE/manifest.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/sarthakroy2002/local_manifest.git --depth=1 .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+repo sync --force-sync -j$(nproc --all)
 
 # build rom
 source build/envsetup.sh
