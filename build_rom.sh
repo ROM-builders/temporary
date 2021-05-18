8af4ef4349fe427c0dec88b86de5af2f3b140bbf
P-Salik <peersalik16@gmail.com>
2021-05-18 23:53:36 +0530

    DotOS RMX1941

--- build_rom.sh
+++ build_rom.sh
@@ -1,20 +1,18 @@
-#!/bin/bash
-
 
 # sync rom
-repo init --depth=1 -u https://github.com/PixelExperience/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 -u git://github.com/DotOS/manifest.git -b dot11 -g default,-device,-mips,-darwin,-notdefault
 
-git clone https://github.com/P-Salik/local_manifest --depth=1 -b main .repo/local_manifests
+git clone https://github.com/P-Salik/local_manifest --depth=1 -b DotOS .repo/local_manifests
 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-cd ../..
 
 # build
-. build/envsetup.sh
-lunch aosp_RMX1941-userdebug
-mka bacon -j$(nproc --all)
-# Pixel Experience
+source build/envsetup.sh
+lunch dot_RMX1941-userdebug
+export WITH_GAPPS=true
+make bacon
+
 
 # upload build
 rclone copy out/target/product/RMX1941/*UNOFFICIAL*.zip cirrus:RMX1941 -P
