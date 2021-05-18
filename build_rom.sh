6c626a228c8e8ce366d541617702c0ce99a81fa6
Erik Garcia <eriikgarciia1999@gmail.com>
2021-05-17 22:21:16 -0300

    beyond0lte: Trying compile PE for Test - 5

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 -u git://github.com/PixelExperience/manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/ECr34T1v3/android_.repo_local_manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
 
 # build rom
-source build/envsetup.sh
+. build/envsetup.sh
 lunch aosp_beyond0lte-userdebug
-m aex
+mka bacon
 
 # upload rom
-rclone copy out/target/product/beyond0lte/AospExtended*.zip cirrus:beyond0lte -P
+rclone copy out/target/product/beyond0lte/PixelExperience*.zip cirrus:beyond0lte -P
