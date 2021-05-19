f49bc93e6860e03140366874d210c31b2705cf3e
adrian-8901 <adrianlam230@gmail.com>
2021-05-19 20:33:58 +0800

    Update

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@
 df -h 
 
 ## sync rom
-repo init -u https://github.com/Project-Fluid/manifest.git --depth=1 -b fluid-11
+repo init -u https://github.com/Project-Fluid/manifest.git --depth=1 -b fluid-11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/adrian-8901/local_mainfest.git --depth=1 -b fluid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 ls 
@@ -11,12 +11,7 @@ echo finished sync
 . build/envsetup.sh
 lunch fluid_umi-userdebug
 mka bacon -j$(nproc --all)
-ccache -s
-up() {
-      curl --upload-file $1 https://transfer.sh/$(basename $1); echo
-      # 14 days, 10 GB limit
-    }
 
-up out/target/product/umi/*.zip
 
-# 1
+# upload rom
+rclone copy out/target/product/mido/*.zip cirrus:mido -P
