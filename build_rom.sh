a1a71d0571d8042e33cc1cccfcf7ec53b1eeced6
yash <73551736+yashlearnpython@users.noreply.github.com>
2021-05-20 00:48:22 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,9 @@
 # sync rom
-repo init -u https://github.com/Ancient-Lab/manifest -b ten && repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && git clone https://github.com/smokey18/device_xiaomi_mido -b ten device/xiaomi/mido && git clone https://github.com/smokey18/vendor_xiaomi_mido -b ten vendor/xiaomi && git clone https://github.com/Scarlett-Project/kernel_xiaomi_msm8953-4.9 -b lineage-17.1 kernel/xiaomi/mido && rm -rf hardware/qcom-caf/msm8996/display && git clone https://github.com/smokey18/hardware_qcom_display -b ten-caf-msm8996 hardware/qcom-caf/msm8996/display && rm -rf hardware/qcom-caf/msm8996/media && git clone https://github.com/smokey18/hardware_qcom_media -b ten-caf-msm8996 hardware/qcom-caf/msm8996/media && rm -rf hardware/qcom-caf/msm8996/audio && git clone https://github.com/smokey18/hardware_qcom_audio -b ten-caf-msm8996 hardware/qcom-caf/msm8996/audio && git clone https://github.com/kdrag0n/proton-clang prebuilts/clang/host/linux-x86/clang-proton
+git clone https://github.com/phhusson/treble_experimentations
 
 # build rom
-. build/envsetup.sh
-lunch ancient_<devicecodename>-userdebug
-mka bacon -j$(nproc --all)
+mkdir AOSP11; cd AOSP11
+bash ../treble_experimentations/build.sh android-11.0
 
 # upload rom
-device=$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d _ -f 2 | cut -d - -f 1)
-rclone copy out/target/product/mido/Ancient*.zip cirrus:$device -P
+rclone copy out/target/product/mido/*.zip cirrus:mido -P
