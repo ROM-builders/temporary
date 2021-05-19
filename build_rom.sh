f2ba3b0561e402edd53e99d7c9f32c3c81c4bfad
Shoury Sharma <58800160+galanteria01@users.noreply.github.com>
2021-05-20 00:03:36 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -2,6 +2,12 @@
 repo init --depth=1 -u https://github.com/PotatoProject/manifest -b dumaloo-release; -g default,-device,-mips,-darwin,-notdefault
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+git clone https://github.com/PotatoDevices/device_xiaomi_violet.git device/xiaomi/violet
+git clone https://github.com/PotatoDevices/kernel_xiaomi_violet.git kernel/xiaomi/violet --depth 1
+git clone https://github.com/PotatoDevices/vendor_xiaomi_violet.git vendor/xiaomi/violet --depth 1
+git clone https://github.com/adi8900/vendor_xiaomi_dirac.git vendor/xiaomi/dirac --depth 1
+
+
 # build rom
 source build/envsetup.sh
 lunch potato_violet-user
