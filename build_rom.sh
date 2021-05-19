6f683ef5826f4a83411cfc7679544d44cb09fb79
Shazu <81510399+GangsterPlayz1@users.noreply.github.com>
2021-05-19 23:46:48 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 -u https://github.com/CherishOS/android_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 -u https://github.com/Project-Fluid/manifest.git -b fluid-11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/GangsterPlayz1/local_manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-brunch RMX1801
+lunch fluid_RMX1801-userdebug
 
 # upload rom
-rclone copy out/target/product/RMX1801/UNOFFICIAL*.zip cirrus:RMX1801 -P
+rclone copy out/target/product/RMX1801/Fluid*.zip cirrus:RMX1801 -P
