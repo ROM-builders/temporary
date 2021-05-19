7f0aff74d62e6250b80ef374e9bd71992df7ad05
adrian-8901 <adrianlam230@gmail.com>
2021-05-19 20:29:55 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,14 +5,13 @@ set -exv
 # sync rom
 repo init -u https://github.com/Project-Fluid/manifest.git --depth=1 -b fluid-11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/adrian-8901/local_mainfest.git --depth=1 -b fluid .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j16
-echo finished sync
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j24
 
 
 # build rom
 source build/envsetup.sh
 lunch fluid_umi-userdebug
-mka bacon -j$(nproc -all)
+mka bacon -j24
 
 
 rclone copy out/target/product/umi/*.zip cirrus:umi -P
