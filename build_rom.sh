92760c2bf8ee9ca6ab4ccee63a77092636517556
adrian-8901 <adrianlam230@gmail.com>
2021-05-19 20:32:31 +0800

    Revert "Merge branch 'Umi-ProjectFluid-P4042' into main"
    This reverts commit f53c28b6acd0e002c1687aeac43d8d32016e9415, reversing
    changes made to d24029c6911698201ede80f67e2bece67830612a.

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 df -h 
 
 ## sync rom
@@ -8,8 +7,16 @@ repo init -u https://github.com/Project-Fluid/manifest.git --depth=1 -b fluid-11
 git clone https://github.com/adrian-8901/local_mainfest.git --depth=1 -b fluid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 ls 
+echo finished sync
 . build/envsetup.sh
 lunch fluid_umi-userdebug
 mka bacon -j$(nproc --all)
+ccache -s
+up() {
+      curl --upload-file $1 https://transfer.sh/$(basename $1); echo
+      # 14 days, 10 GB limit
+    }
 
+up out/target/product/umi/*.zip
 
+# 1
