d20c199b5fcd306460a83368757f130def0570b7
Fraschze97 <Anprasetya97@gmail.com>
2021-05-20 20:24:10 +0700

    RMX1941 : NUSANTARA
    Signed-off-by: Fraschze97 <Anprasetya97@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,15 @@
 # sync rom
-repo init --depth=1 -u https://github.com/PixelPlusUI/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
+repo init -u https://github.com/NusantaraProject-ROM/android_manifest -b 11 --depth=1 -g default,-device,-mips,-darwin,-notdefault
 
 git clone https://github.com/Fraschze97/local_manifest.git --depth 1 -b main .repo/local_manifests
 
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
+export USE_GAPPS = true
 lunch aosp_RMX1941-userdebug
-mka bacon -jX
+mka nad
 
 # upload rom
 rclone copy out/target/product/RMX1941/*UNOFFICIAL*.zip cirrus:RMX1941 -P  
