943e8507cb56c4ed702e5d7e25d89870e658b011
Fraschze97 <Anprasetya97@gmail.com>
2021-05-20 16:43:41 +0700

    RMX1941 : NAD
    Signed-off-by: Fraschze97 <Anprasetya97@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 -u https://github.com/PixelPlusUI/manifest.git -b eleven.x -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Frachze97/local_manifests.git --depth 1 -b main .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
+repo init -u https://github.com/NusantaraProject-ROM/android_manifest -b 11 --depth=1 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/Fraschze97/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 # build rom
 source build/envsetup.sh
-lunch aosp_RMX1941-userdebug
-mka bacon -jX
+lunch nad_RMX1941-userdebug
+export WITH_GAPPS=true
+mka nad
 
 # upload rom
-rclone copy out/target/product/RMX1941/*UNOFFICIAL*.zip cirrus:RMX1941 -P  
+rclone copy out/target/product/RMX1941/*UNOFFICIAL*.zip cirrus:RMX1941 -P   
