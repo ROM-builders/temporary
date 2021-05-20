b516fe92de04a19217addbb3fd5be7fdca2c7221
Shazu <81510399+Shazu-xD@users.noreply.github.com>
2021-05-20 19:21:51 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 -u https://github.com/Corvus-ROM/android_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 -u https://github.com/Project-Fluid/manifest.git -b fluid-11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/Shazu-xD/local_manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch corvus_RMX1801-userdebug
-make corvus
+lunch fluid_RMX1801-userdebug
+mka bacon
 
 # upload rom
-rclone copy out/target/product/RMX1801/Corvus*.zip cirrus:RMX1801 -P
+rclone copy out/target/product/RMX1801/Fluid*.zip cirrus:RMX1801 -P
