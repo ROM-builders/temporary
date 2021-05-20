013387a36370b8d013e391396ce7c410f95e48e2
Hadad <repo-sync@outlook.com>
2021-05-20 13:23:07 +0700

    [skip ci] temporary: use local manifest ifor device sources.
    Signed-off-by: Hadad <repo-sync@outlook.com>

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-# ROM sources
+# Sources
 repo init -u https://github.com/NusantaraProject-ROM/android_manifest.git -b 11 --groups=all,-notdefault,-darwin,-mips,-device --depth=1
+git clone https://github.com/hadad/android_repo_local_manifest.git .repo/local_manifests --depth=1
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
-# Device sources
-git clone https://github.com/NusantaraROM-Devices/device_xiaomi_onclite.git -b 11 device/xiaomi/onclite --depth=1
-git clone https://github.com/NusantaraROM-Devices/kernel_xiaomi_onclite.git -b 11 kernel/xiaomi/onclite --depth=1
-git clone https://github.com/NusantaraROM-Devices/vendor_xiaomi_onclite.git -b 11 vendor/xiaomi/onclite --depth=1
 
 # Build
 source build/envsetup.sh
