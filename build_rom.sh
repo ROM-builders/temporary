201e811c28ae8ee7fe2703ac0663f968dde09fa4
yash <73551736+yashlearnpython@users.noreply.github.com>
2021-05-20 15:28:28 +0530

    try lineage gsi

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,15 @@
 # sync rom
-repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo init --depth=1 -u https://github.com/LineageOS/android.git -b lineage-18.1
+git clone https://github.com/phhusson/treble_manifest .repo/local_manifests  -b android-11.0
+repo sync -c -j4 --force-sync --no-tags --no-clone-bundle
+git clone https://github.com/phhusson/treble_patches -b android-11.0
+git am patch
 
 # build rom
-source build/envsetup.sh
-lunch aosp_mido-user
-m aex
+. build/envsetup.sh
+lunch treble_arm64_avN-userdebug
+WITHOUT_CHECK_API=true make -j8 systemimage
 
 # upload rom
 # If you need to upload json/multiple files too then put like this 'rclone copy out/target/product/mido/*.zip cirrus:mido -P && rclone copy out/target/product/mido/*.zip.json cirrus:mido -P'
-rclone copy out/target/product/mido/*.zip cirrus:mido -P
+rclone copy out/target/../../*.img cirrus:mido -P
