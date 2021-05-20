5436b67d02feae312fee54164fd62fa44943338c
Sarthak <sarthakroy2002@gmail.com>
2021-05-20 10:13:33 +0530

    LOS FE RMX2050

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 -u git://github.com/Lineage-FE/manifest.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/sarthakroy2002/local_manifest.git --depth=1 .repo/local_manifests
-repo sync --force-sync -j$(nproc --all)
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
@@ -12,7 +12,5 @@ export SKIP_API_CHECKS=true
 mka bacon -j$(nproc --all)
 
 # upload rom
-curl -sL https://git.io/file-transfer | sh
-./transfer wet /home/ci/roms/Lineage-FE/out/target/product/RMX2050/*LineageFE*.zip
 rclone copy out/target/product/RMX2050/*LineageFE*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d _ -f 2 | cut -d - -f 1) -P
 
