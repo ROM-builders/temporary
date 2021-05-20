18b6fa76a3b21dd95ebe36f796777c9ef9f597b8
Hadad <repo-sync@outlook.com>
2021-05-20 10:42:47 +0700

    temporary: add onclite support.
    * with Nusantara ROM
    
    Signed-off-by: Hadad <repo-sync@outlook.com>

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,27 @@
-# sync rom
-repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+#!/bin/bash
+rom_sources(){
+	repo init -u https://github.com/NusantaraProject-ROM/android_manifest.git -b 11 --groups=all,-notdefault,-darwin,-mips,-device --depth=1
+	repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+	repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+}
 
-# build rom
-source build/envsetup.sh
-lunch aosp_mido-user
-m aex
+device_sources(){
+	git clone https://github.com/NusantaraROM-Devices/device_xiaomi_onclite.git -b 11 device/xiaomi/onclite --depth=1
+	git clone https://github.com/NusantaraROM-Devices/kernel_xiaomi_onclite.git -b 11 kernel/xiaomi/onclite --depth=1
+	git clone https://github.com/NusantaraROM-Devices/vendor_xiaomi_onclite.git -b 11 vendor/xiaomi/onclite --depth=1
+}
 
-# upload rom
-# If you need to upload json/multiple files too then put like this 'rclone copy out/target/product/mido/*.zip cirrus:mido -P && rclone copy out/target/product/mido/*.zip.json cirrus:mido -P'
-rclone copy out/target/product/mido/*.zip cirrus:mido -P
+build_rom(){
+	source build/envsetup.sh
+	lunch nad_onclite-userdebug
+	mka nad -j$(nproc --all)
+}
+
+upload_rom(){
+	curl --upload-file out/target/product/onclite/Nusantara_v2.9-11-onclite*.zip https://transfer.sh
+}
+
+rom_sources
+device_sources
+build_rom
+upload_rom
