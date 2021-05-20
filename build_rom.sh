39f06cfc7a6825fb8f90aaaf5a0c0fda80c3b1cb
Fraschze97 <Anprasetya97@gmail.com>
2021-05-21 06:36:50 +0700

    RMX1941 : NAD
    Signed-off-by: Fraschze97 <Anprasetya97@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -1,10 +1,7 @@
 # sync rom
 repo init -u https://github.com/NusantaraProject-ROM/android_manifest -b 11 --depth=1 -g default,-device,-mips,-darwin,-notdefault
-
 git clone https://github.com/Fraschze97/local_manifest.git --depth 1 -b main .repo/local_manifests
-
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
 # build rom
 source build/envsetup.sh
 lunch nad_RMX1941-userdebug
@@ -12,4 +9,4 @@ export WITH_GAPPS=true
 mka nad
 
 # upload rom
-rclone copy out/target/product/RMX1941/*UNOFFICIAL*.zip cirrus:RMX1941 -P  
+rclone copy out/target/product/RMX1941/*UNOFFICIAL*.zip cirrus:RMX1941 -P   
