dc1d502347e0874853d70704f988f1ff8cfc3844
Rishawn Iyer <70256146+Stealth1226@users.noreply.github.com>
2021-05-21 10:52:46 +0530

    Lighthouse rs988 Testing Build

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 -u git://github.com/lighthouse-os/manifest.git -b raft -g default,-device,-mips,-darwin,-notdefault
-#git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
+git clone https://github.com/Stealth1226/local_manifest --depth 1 -b raft .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
