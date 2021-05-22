d1c7ac6b43d9ae4994aaf2d14851e9d7664435bd
yash <73551736+yashlearnpython@users.noreply.github.com>
2021-05-22 15:04:49 +0530

    Update sync_rom.sh

--- sync_rom.sh
+++ sync_rom.sh
@@ -3,6 +3,6 @@
 set -exv
 
 #sync rom
-repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x
+repo init --depth=1 -u https://github.com/Wave-Project/manifest -b r
 git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
