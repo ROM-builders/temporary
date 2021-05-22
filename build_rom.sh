36af7479ee58642667a4a07ebc144090fbcbb977
himanshu0218 <70850303+himanshu0218@users.noreply.github.com>
2021-05-22 14:51:24 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -11,3 +11,4 @@ mka bacon
 # upload rom
 # If you need to upload json/multiple files too then put like this 'rclone copy out/target/product/mido/*.zip cirrus:mido -P && rclone copy out/target/product/mido/*.zip.json cirrus:mido -P'
 rclone copy out/target/product/begonia/*UNOFFICIAL*.zip cirrus:begonia -P
+
