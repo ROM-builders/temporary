ba5d8702563b3459b68201f7acd8e23fd5794ffc
himanshu0218 <70850303+himanshu0218@users.noreply.github.com>
2021-05-22 17:47:23 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 -u git://github.com/Project-Awaken/android_manifest -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/himanshu0218/local_manifests.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/himanshu0218/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
