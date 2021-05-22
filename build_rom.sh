73eb2e7ba0466f720ae8de4c1fa4684b26b9854a
Shoury Sharma <58800160+galanteria01@users.noreply.github.com>
2021-05-22 09:30:29 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,6 @@
 # sync rom
 repo init --depth=1 -u https://github.com/PotatoProject/manifest -b dumaloo-release -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/galanteria01/local_manifest.git -b posp --depth 1 .repo/local_manifests 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -10,4 +11,4 @@ brunch violet
 
 # upload rom
 # If you need to upload json/multiple files too then put like this 'rclone copy out/target/product/mido/*.zip cirrus:mido -P && rclone copy out/target/product/mido/*.zip.json cirrus:mido -P'
-rclone copy out/target/product/violet/*.zip cirrus:mido -P
+rclone copy out/target/product/violet/Potato_violet*Community.zip cirrus:violet -P
