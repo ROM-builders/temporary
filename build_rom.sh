374ac7c2d356127c00d2b612adce008491f2e486
himanshu0218 <70850303+himanshu0218@users.noreply.github.com>
2021-05-22 22:58:01 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch awaken_surya-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
 brunch surya
 
 # upload rom
