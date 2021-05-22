52530e32c0b216c378c552b09a0a9de2891ba487
Shazu <81510399+Shazu-xD@users.noreply.github.com>
2021-05-22 16:29:33 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,11 @@
 # sync rom
-repo init --depth=1 -u https://github.com/Kang-OS-R/android_manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 -u https://github.com/CherishOS/android_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/Shazu-xD/local_manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch kangos_RMX1801-userdebug
-make bacon 
+brunch RMX1801
 
 # upload rom
 rclone copy out/target/product/RMX1801/*UNOFFICIAL*.zip cirrus:RMX1801 -P
