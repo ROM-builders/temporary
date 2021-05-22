ff0cb257170e6b87936abc0da0f3d1c7ede5cafa
Sarthak <sarthakroy2002@gmail.com>
2021-05-22 12:19:43 +0530

    cipher

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 -u git://github.com/Lineage-FE/manifest.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 -u git://github.com/CipherOS/android_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/sarthakroy2002/local_manifest.git --depth=1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
@@ -12,5 +12,5 @@ export SKIP_API_CHECKS=true
 mka bacon -j$(nproc --all)
 
 # upload rom to rclone
-rclone copy out/target/product/RMX2050/*LineageFE*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d _ -f 2 | cut -d - -f 1) -P
+rclone copy out/target/product/RMX2050/*UNOFFICIAL*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d _ -f 2 | cut -d - -f 1) -P
 
