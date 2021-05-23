28b852ce75c80fa27bbdcba2846c0792cfaefb13
yash <73551736+yashlearnpython@users.noreply.github.com>
2021-05-23 13:14:46 +0530

    take 18

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,17 @@
-# sync rom
-repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+#Sync ROM
+repo init --depth=1 -u https://github.com/Wave-Project/manifest -b r -g default,-device,-mips,-darwin,-notdefault
 
-# build rom
-source build/envsetup.sh
-lunch aosp_mido-user
-m aex
+git clone https://github.com/yashlearnpython/local_manifest.git --depth=1 -b wave-os .repo/local_manifests
 
-# upload rom
-# If you need to upload json/multiple files too then put like this 'rclone copy out/target/product/mido/*.zip cirrus:mido -P && rclone copy out/target/product/mido/*.zip.json cirrus:mido -P'
-rclone copy out/target/product/mido/*.zip cirrus:mido -P
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
+# Apply Patch
+#cd device/xiaomi/msm8937-common && curl -LO https://raw.githubusercontent.com/MinatiScape/scripts/main/selinux.patch && git am selinux.patch && cd ../../..
+# Build ROM
+. build/envsetup.sh
+lunch wave_mido-user
+mka clean
+mka bacon -j$(nproc --all)
+
+# Upload build
+rclone copy out/target/product/tiare/*.zip cirrus:mido -P
