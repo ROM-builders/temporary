39b215a1d2cb2c7397479f1886a5a19745744b99
boedhack99 <boedhack@gmail.com>
2021-05-23 12:20:17 +0700

    Use brunch to find directory

--- build_rom.sh
+++ build_rom.sh
@@ -5,8 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-brunch havoc_mojito-userdebug
-mka bacon -j$(nproc --all)
+brunch
 
 # upload rom
 # If you need to upload json/multiple files too then put like this 'rclone copy out/target/product/mido/*.zip cirrus:mido -P && rclone copy out/target/product/mido/*.zip.json cirrus:mido -P'
