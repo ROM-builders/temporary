eca44910385afbb2bd12415ea2d390772c88e70c
AryanAhire <66762638+HyperNotAryanX97@users.noreply.github.com>
2021-05-23 17:35:17 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 -u https://github.com/CherishOS/android_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/HyperNotAryanX97/Begonia -b CherishOS .repo/local_manifests
+git clone https://github.com/HyperNotAryanX97/Begonia -b backup .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
