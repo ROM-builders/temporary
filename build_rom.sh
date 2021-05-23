87a60c0b20e7bc87030b4bf2799b705b219c38cb
himanshu0218 <70850303+himanshu0218@users.noreply.github.com>
2021-05-23 13:06:18 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,15 @@
 # sync rom
-repo init --depth=1 -u git://github.com/Project-Awaken/android_manifest -b 11 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 -u git://github.com/StyxProject/manifest -b R -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/himanshu0218/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch awaken_surya-userdebug
+lunch styx_begonia-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
-brunch surya
+m styx-ota
 
 # upload rom
 # If you need to upload json/multiple files too then put like this 'rclone copy out/target/product/mido/*.zip cirrus:mido -P && rclone copy out/target/product/mido/*.zip.json cirrus:mido -P'
-rclone copy out/target/product/surya/*UNOFFICIAL*.zip cirrus:surya -P
+rclone copy out/target/product/begonia/*UNOFFICIAL*.zip cirrus:begonia -P
 
