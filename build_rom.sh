4a8513d18b9f0541774d4d11e8cc14cc511aca2d
Shoury Sharma <58800160+galanteria01@users.noreply.github.com>
2021-05-23 21:35:03 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,15 @@
 # sync rom
-repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
+repo init -u https://github.com/ShapeShiftOS/android_manifest.git -b android_11 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/galanteria01/local_manifest.git --depth 1 -b ssos .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
+sudo apt update && sudo apt -y install cpio
 source build/envsetup.sh
-lunch aosp_mido-user
-m aex
+export BUILD_BROKEN_DUP_RULES=true
+lunch ssos_violet-userdebug
+brunch violet
 
 # upload rom
 # If you need to upload json/multiple files too then put like this 'rclone copy out/target/product/mido/*.zip cirrus:mido -P && rclone copy out/target/product/mido/*.zip.json cirrus:mido -P'
-rclone copy out/target/product/mido/*.zip cirrus:mido -P
+rclone copy out/target/product/violet/ShapeShiftOS*.zip cirrus:violet -P
