0f87b6c704d88343c62cd04d89771ea84d7d3804
Fraschze97 <83362952+Fraschze97@users.noreply.github.com>
2021-05-24 10:31:39 +0700

    take 35 DT ERROR

--- build_rom.sh
+++ build_rom.sh
@@ -11,18 +11,8 @@ curl -LO https://github.com/PixelExperience/external_selinux/commit/9d6ebe89430f
 patch -p1 < *.patch
 cd ../..
 
-cd frameworks/base
-curl -LO  https://github.com/PixelExperience/frameworks_base/commit/37f5a323245b0fd6269752742a2eb7aa3cae24a7.patch
-patch -p1 < *.patch
-cd ../..
-
 cd frameworks/opt/net/ims
 curl -LO  https://github.com/PixelExperience/frameworks_opt_net_ims/commit/661ae9749b5ea7959aa913f2264dc5e170c63a0a.patch
-patch -p1 < *.patch
-cd ../..
-
-cd frameworks/opt/net/wifi
-curl -LO  https://github.com/PixelExperience/frameworks_opt_net_wifi/commit/3bd2c14fbda9c079a4dc39ff4601ba54da589609.patch
 patch -p1 < *.patch
 cd ../..
 
