2ea4ebbeee8b57b0b073c5ac2948fe3d38c6f4e4
Fraschze97 <83362952+Fraschze97@users.noreply.github.com>
2021-05-24 15:56:10 +0700

    new commit

--- build_rom.sh
+++ build_rom.sh
@@ -5,11 +5,13 @@ git clone https://github.com/Fraschze97/local_manifest --depth=1 -b main .repo/l
 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+
 # patches
 cd external/selinux
 curl -LO https://github.com/PixelExperience/external_selinux/commit/9d6ebe89430ffe0aeeb156f572b2a810f9dc98cc.patch
 patch -p1 < *.patch
 cd ../..
+
 
 cd frameworks/opt/net/ims
 curl -LO  https://github.com/PixelExperience/frameworks_opt_net_ims/commit/661ae9749b5ea7959aa913f2264dc5e170c63a0a.patch
