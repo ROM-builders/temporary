5095fd14eb892f339e0658686617504f0c0d0546
Sarthak <sarthakroy2002@gmail.com>
2021-05-24 15:38:28 +0530

    NEZUKO RMX2151

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,17 @@
 # sync rom
-repo init --depth=1 -u git://github.com/CipherOS/android_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/sarthakroy2002/local_manifest.git --depth=1 .repo/local_manifests
+repo init --depth=1 -u https://github.com/NezukoOS/manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/noobyysauraj/local_manifest.git --depth=1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_RMX2050-userdebug
+lunch nezuko_RMX2151-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export SKIP_ABI_CHECKS=true
 export SKIP_API_CHECKS=true
+export CUSTOM_BUILD_TYPE=UNOFFICIAL
 mka bacon -j$(nproc --all)
 
 # upload rom to rclone
-rclone copy out/target/product/RMX2050/*UNOFFICIAL*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d _ -f 2 | cut -d - -f 1) -P
+rclone copy out/target/product/RMX2151/*NEZUKO*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d _ -f 2 | cut -d - -f 1) -P
 
