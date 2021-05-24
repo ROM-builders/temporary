d6a79ad4368b3d470bf927b98cf99833afbb3130
Endi Prytna <65471980+Darkbreaker25@users.noreply.github.com>
2021-05-24 08:30:25 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,28 @@
 # sync rom
 repo init --depth=1 -u https://github.com/Havoc-OS/android_manifest.git -b eleven
-git clone https://github.com/Fraschze97/local_manifest --depth=1 -b main .repo/local_manifests
+git clone https://github.com/Darkbreaker25/local_manifest --depth=1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
+# patches
+cd external/selinux
+curl -LO https://github.com/PixelExperience/external_selinux/commit/9d6ebe89430ffe0aeeb156f572b2a810f9dc98cc.patch
+patch -p1 < *.patch
+cd ../..
+
+cd frameworks/base
+curl -LO  https://github.com/PixelExperience/frameworks_base/commit/37f5a323245b0fd6269752742a2eb7aa3cae24a7.patch
+patch -p1 < *.patch
+cd ../..
+
+cd frameworks/opt/net/ims
+curl -LO  https://github.com/PixelExperience/frameworks_opt_net_ims/commit/661ae9749b5ea7959aa913f2264dc5e170c63a0a.patch
+patch -p1 < *.patch
+cd ../..
+
+cd frameworks/opt/net/wifi
+curl -LO  https://github.com/PixelExperience/frameworks_opt_net_wifi/commit/3bd2c14fbda9c079a4dc39ff4601ba54da589609.patch
+patch -p1 < *.patch
+cd ../..
 
 # build rom
 . build/envsetup.sh
