8bd1784858e4115ad89559b311a3edea20f098bf
boedhack99 <boedhack@gmail.com>
2021-05-24 21:56:58 +0700

    Trriger: Trying brunch

--- build_rom.sh
+++ build_rom.sh
@@ -12,6 +12,3 @@ brunch mojito
 # upload rom
 # If you need to upload json/multiple files too then put like this 'rclone copy out/target/product/mido/*.zip cirrus:mido -P && rclone copy out/target/product/mido/*.zip.json cirrus:mido -P'
 rclone copy out/target/product/mojito/Havoc-OS*.zip cirrus:mojito -P
-
-
-
