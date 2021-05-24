5d59084f8823d84238518a51fe9e68b525a474b7
himanshu0218 <70850303+himanshu0218@users.noreply.github.com>
2021-05-24 23:12:20 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,16 @@
 # sync rom
-repo init --depth=1 -u git://github.com/Havoc-OS/android_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 -u git://github.com/yaap/manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/himanshu0218/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch havoc_begonia-userdebug
+lunch yaap_surya-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
-brunch begonia
+make yaap
 
 #if you are a patch user (which is really not normal and not recommended), then must put like this, `m aex || repo forall -c 'git checkout .'
 
 # upload rom
 # If you need to upload json/multiple files too then put like this 'rclone copy out/target/product/mido/*.zip cirrus:mido -P && rclone copy out/target/product/mido/*.zip.json cirrus:mido -P'
-rclone copy out/target/product/begonia/*Unofficial*.zip cirrus:begonia -P
+rclone copy out/target/product/surya/*Rocinante*.zip cirrus:surya -P
