678cd4552f3dfcfc3177e01dacf2bc4d7882daf3
himanshu0218 <70850303+himanshu0218@users.noreply.github.com>
2021-05-24 07:51:51 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -11,4 +11,4 @@ brunch begonia
 
 # upload rom
 # If you need to upload json/multiple files too then put like this 'rclone copy out/target/product/mido/*.zip cirrus:mido -P && rclone copy out/target/product/mido/*.zip.json cirrus:mido -P'
-rclone copy out/target/product/begonia/*UNOFFICIAL*.zip cirrus:begonia -P
+rclone copy out/target/product/begonia/*UNOFFICIAL*.zip cirrus:begonia -P 
