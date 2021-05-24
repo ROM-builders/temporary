1e101a3cad08632890b7746ef27e0130611968a2
Endi Prytna <65471980+Darkbreaker25@users.noreply.github.com>
2021-05-24 09:32:24 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -27,7 +27,7 @@ cd ../..
 # build rom
 . build/envsetup.sh
 lunch havoc_RMX1941-userdebug
-brunch
+brunch RMX1941
 
 # upload rom
 # If you need to upload json/multiple files too then put like this 'rclone copy out/target/product/mido/*.zip cirrus:mido -P && rclone copy out/target/product/mido/*.zip.json cirrus:mido -P'
