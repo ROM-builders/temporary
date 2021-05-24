1d44dbc56f48f531bd3a33d065ffee95bf930e30
himanshu0218 <70850303+himanshu0218@users.noreply.github.com>
2021-05-24 07:20:59 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch havoc_begonia-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
-mka bacon
+brunch begonia
 
 # upload rom
 # If you need to upload json/multiple files too then put like this 'rclone copy out/target/product/mido/*.zip cirrus:mido -P && rclone copy out/target/product/mido/*.zip.json cirrus:mido -P'
