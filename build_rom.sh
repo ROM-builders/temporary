4d8def500b0ea00c7c22c229d7eef72ac12b54c1
himanshu0218 <70850303+himanshu0218@users.noreply.github.com>
2021-05-24 06:00:02 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -12,3 +12,4 @@ mka bacon
 # upload rom
 # If you need to upload json/multiple files too then put like this 'rclone copy out/target/product/mido/*.zip cirrus:mido -P && rclone copy out/target/product/mido/*.zip.json cirrus:mido -P'
 rclone copy out/target/product/surya/*UNOFFICIAL*.zip cirrus:surya -P
+
