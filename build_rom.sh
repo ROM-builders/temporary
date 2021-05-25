5eb5227036251abe6401e762c3f2f2b4124e403f
himanshu0218 <70850303+himanshu0218@users.noreply.github.com>
2021-05-25 06:20:52 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,17 +1,17 @@
 # sync rom
-repo init --depth=1 -u git://github.com/Komodo-OS-Rom/manifest -b 11 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 -u git://github.com/SpiceOS/android.git -b 11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/himanshu0218/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch komodo_surya-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
-masak komodo
+breakfast surya
+mka bacon
 
 #if you are a patch user (which is really not normal and not recommended), then must put like this, `m aex || repo forall -c 'git checkout .'
 
 # upload rom
 # If you need to upload json/multiple files too then put like this 'rclone copy out/target/product/mido/*.zip cirrus:mido -P && rclone copy out/target/product/mido/*.zip.json cirrus:mido -P'
-rclone copy out/target/product/surya/*surya*.zip cirrus:surya -P
+rclone copy out/target/product/surya/*UNOFFICIAL*.zip cirrus:surya -P
 
