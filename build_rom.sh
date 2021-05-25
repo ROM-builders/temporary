a7d08708a56eac2511c1a7130b6018539377b11b
himanshu0218 <70850303+himanshu0218@users.noreply.github.com>
2021-05-25 06:35:08 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,6 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
+lunch lineage_surya-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 breakfast surya
 mka bacon
