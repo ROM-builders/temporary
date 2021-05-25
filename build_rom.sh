697bd6583f779d2dd4431043f753c15a3de07927
noobyysauraj <81681419+noobyysauraj@users.noreply.github.com>
2021-05-25 10:34:29 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 -u https://github.com/NezukoOS/manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/noobyysauraj/local_manifest.git --depth=1 .repo/local_manifests
+repo init -u https://github.com/PixelExperience/manifest -b eleven-plus -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/noobyysauraj/local_manifest.git -b aosp --depth=1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
-lunch nezuko_RMX2151-userdebug
+. build/envsetup.sh
+lunch aosp_RMX2151-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export SKIP_ABI_CHECKS=true
 export SKIP_API_CHECKS=true
@@ -13,5 +13,5 @@ export CUSTOM_BUILD_TYPE=UNOFFICIAL
 mka bacon -j$(nproc --all)
 
 # upload rom to rclone
-rclone copy out/target/product/RMX2151/*NEZUKO*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d _ -f 2 | cut -d - -f 1) -P
+rclone copy out/target/product/RMX2151/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d _ -f 2 | cut -d - -f 1) -P
 
