5d685dea1801f82a84f4ac91f317591b8a7b116e
Khalakuzzaman Apon <61594531+Apon77@users.noreply.github.com>
2021-05-25 18:26:43 +0600

    [skip ci] Fix typing mistake

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch aosp_mido-user
 m aex
-#if you are a patch user (which is really not normal and not recommended), then must put like this, `m aex || repo forall -c 'git checkout .'
+#if you are a patch user (which is really not normal and not recommended), then must put like this, " m aex || repo forall -c 'git checkout .' "
 
 # upload rom
 # If you need to upload json/multiple files too then put like this 'rclone copy out/target/product/mido/*.zip cirrus:mido -P && rclone copy out/target/product/mido/*.zip.json cirrus:mido -P'
