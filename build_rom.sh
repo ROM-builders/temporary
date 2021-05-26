c9a90d72a7779bf0a04fd28468e85e88dcde0ae9
Sundram <63789329+Yasundram@users.noreply.github.com>
2021-05-27 02:57:50 +1000

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,17 +1,11 @@
 # sync rom
-repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo init -u https://github.com/CherishOS/android_manifest.git -b eleven 
+git clone https://github.com/Yasundram/local_manifest --depth 1 -b aex .repo/local_manifests
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
 # build rom
-source build/envsetup.sh
-lunch aosp_mido-user
-# if you are a patch user, then dont remove the \ and next line , it helps patch users to get sync propely next time
-m aex \
-	&& repo forall -c 'git checkout .' || repo forall -c 'git checkout .'
+. build/envsetup.sh
+ brunch RMX1941-userdebug
 
 # upload rom
-# If you need to upload json/multiple files too then put like this
-#rclone copy out/target/product/mido/*.zip cirrus:mido -P
-#rclone copy out/target/product/mido/*.zip.json cirrus:mido -P'
-rclone copy out/target/product/mido/*.zip cirrus:mido -P
+rclone copy out/target/product/RMX1941/*UNOFFICIAL*.zip cirrus:RMX1941 -P
