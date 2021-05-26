13c148ce0db656f84df2f83d4f476ae4cc46514b
Apon77 <khalakuzzamanapon5@gmail.com>
2021-05-26 18:18:46 +0600

    Migrating to new script

--- build_rom.sh
+++ build_rom.sh
@@ -6,9 +6,12 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch aosp_mido-user
-m aex
-#if you are a patch user (which is really not normal and not recommended), then must put like this, " m aex || repo forall -c 'git checkout .' "
+#dont remove the \ , it helps patch users
+m init \
+	&& repo forall -c 'git checkout .' || repo forall -c 'git checkout'
 
 # upload rom
-# If you need to upload json/multiple files too then put like this 'rclone copy out/target/product/mido/*.zip cirrus:mido -P && rclone copy out/target/product/mido/*.zip.json cirrus:mido -P'
+# If you need to upload json/multiple files too then put like this
+#rclone copy out/target/product/mido/*.zip cirrus:mido -P
+#rclone copy out/target/product/mido/*.zip.json cirrus:mido -P'
 rclone copy out/target/product/mido/*.zip cirrus:mido -P
