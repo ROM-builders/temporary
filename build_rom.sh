038b33fd934e00d075f8499e9e2edc1cec2c7d89
Sundram <63789329+Yasundram@users.noreply.github.com>
2021-05-27 00:23:39 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init -u https://github.com/CherishOS/android_manifest.git -b eleven 
+repo init -u https://github.com/CherishOS/android_manifest.git -b eleven --depth=1 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/Yasundram/local_manifest --depth 1 -b aex .repo/local_manifests
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
