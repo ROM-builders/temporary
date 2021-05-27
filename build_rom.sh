b8909353185a3ee6ba50705e371c59c38c8adcc4
P-Salik <peersalik16@gmail.com>
2021-05-27 09:08:22 +0530

    Added correct branch name

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init -u https://github.com/CherishOS/android_manifest.git -b eleven --depth=1 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Yasundram/local_manifest --depth 1 -b aex .repo/local_manifests
+git clone https://github.com/Yasundram/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
 # build rom
 . build/envsetup.sh
-export CHERISH_MAINTAINER=Sundram 
+export CHERISH_MAINTAINER=Sundram
 export CHERISH_NONGAPPS=true
 brunch RMX1941-userdebug
 
