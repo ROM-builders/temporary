12d5e3775a2f0f8e36ddaca6dbd52dabb56b9146
MiDoNaSR545 <57400341+MiDoNaSR545@users.noreply.github.com>
2021-05-27 19:29:27 +0200

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/DotOS/manifest.git -b dot11 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/MiDoNaSR545/mainfest_personal.git --depth 1 -b dot .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
-mka derp
+lunch dot_ysl-user
+make bacon
+export WITH_GAPPS=true
+make bacon
 
 # upload rom
-rclone copy out/target/product/vayu/DerpFest*.zip cirrus:vayu -P
+rclone copy out/target/product/ysl/*.zip cirrus:ysl -P
