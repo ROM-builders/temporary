6b344a17f96223e89188135702702d8058f4513b
Fraschze97 <83362952+Fraschze97@users.noreply.github.com>
2021-05-27 10:41:36 +0700

    hoy

--- build_rom.sh
+++ build_rom.sh
@@ -4,6 +4,7 @@ repo init -u https://github.com/NusantaraProject-ROM/android_manifest -b 11 --de
 git clone https://github.com/Fraschze97/local_manifest --depth 1 -b nusantara .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+
 # build rom
 source build/envsetup.sh
 lunch nad_RMX1941-userdebug
