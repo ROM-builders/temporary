9cd0020ed7a6967979d86d6b3d19c41daeca25f9
Khalakuzzaman Apon <61594531+Apon77@users.noreply.github.com>
2021-05-27 08:22:58 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,5 @@
 # sync rom
+env
 repo init -u https://github.com/NusantaraProject-ROM/android_manifest -b 11 --depth=1 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/Fraschze97/local_manifest --depth 1 -b nusantara .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
