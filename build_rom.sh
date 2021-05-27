00a7916a628ed9a1df09e77ab041dcbc80917988
Shazu <81510399+Shazu-xD@users.noreply.github.com>
2021-05-27 13:02:55 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/hentaiOS/platform_manifest -b Rika -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/Shazu-xD/local_manifest.git --depth 1 -b Hentai .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
-mka derp
+lunch hentai_RMX1801-userdebug
+make otapackage -jX
 
 # upload rom
-rclone copy out/target/product/vayu/DerpFest*.zip cirrus:vayu -P
+rclone copy out/target/product/RMX1801/*2021*.zip cirrus:RMX1801 -P
