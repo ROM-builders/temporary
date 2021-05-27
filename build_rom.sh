b34702b29ac3dfbc324551e11d016d5b5ce41b9f
Fraschze97 <Anprasetya97@gmail.com>
2021-05-27 13:26:41 +0700

    Nusantara

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,16 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init -u https://github.com/NusantaraProject-ROM/android_manifest -b 11 --depth=1 -g default,-device,-mips,-darwin,-notdefault
+
+git clone https://github.com/Fraschze97/local_manifest.git --depth 1 -b nusantara .repo/local_manifests
+
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
-mka derp
+lunch nad_RMX1941-userdebug
+export USE_GAPPS=true
+export SELINUX_IGNORE_NEVERALLOWS=true
+mka nad
 
 # upload rom
-rclone copy out/target/product/vayu/DerpFest*.zip cirrus:vayu -P
+rclone copy out/target/product/RMX1941/*.zip cirrus:RMX1941 -P
