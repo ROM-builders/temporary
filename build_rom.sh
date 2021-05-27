448fe6c34ab2973a38dd0435f11ec6c19e11d81b
Rahif M <faizel326@gmail.com>
2021-05-28 02:36:29 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ProtonAOSP/android_manifest -b rvc -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/RahifM/local_manifests --depth 1 -b staging/proton-rvc .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
-mka derp
+lunch mido-userdebug
+make -j8 otapackage
 
 # upload rom
-rclone copy out/target/product/vayu/DerpFest*.zip cirrus:vayu -P
+rclone copy out/target/product/target/proton-aosp*.zip cirrus:mido -P
