f3df7e81f057c5b2eac8634993d4c6e5f8ba9b4d
Fraschze97 <83362952+Fraschze97@users.noreply.github.com>
2021-05-27 09:15:59 +0700

    Name Chiper but Nusantara

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,14 @@
 # sync rom
 repo init -u https://github.com/NusantaraProject-ROM/android_manifest -b 11 --depth=1 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Fraschze97/local-Manifests.git --depth 1 -b nusantara .repo/local_manifests
+git clone https://github.com/Fraschze97/local-manifest.git --depth 1 -b nusantara .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch nad_RMX1941-userdebug
+export USE_GAPPS=true
+export SELINUX_IGNORE_NEVERALLOWS=true
 mka nad
 
 # upload rom
-rclone copy out/target/product/RMX1941/DerpFest*.zip cirrus:vayu -P
+rclone copy out/target/product/RMX1941/*.zip cirrus:RMX1941 -P
