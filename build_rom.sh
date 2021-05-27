569bd2e9deee6876d7251d2787a84f55aa71bd43
Sundram <63789329+Yasundram@users.noreply.github.com>
2021-05-27 12:41:13 +1000

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 . build/envsetup.sh
 export CHERISH_MAINTAINER=Sundram 
 export CHERISH_NONGAPPS=true
- brunch RMX1941-userdebug
+brunch RMX1941-userdebug
 
 # upload rom
 rclone copy out/target/product/RMX1941/*UNOFFICIAL*.zip cirrus:RMX1941 -P
