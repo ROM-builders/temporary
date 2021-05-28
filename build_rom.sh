ed773cd734d569c535c7ae316f325aeb85e23823
Sundram <63789329+Yasundram@users.noreply.github.com>
2021-05-28 13:55:29 +1000

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,18 @@
 # sync rom
-repo init -u https://github.com/CherishOS/android_manifest.git -b eleven --depth=1 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 -u https://github.com/CherishOS/android_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
 
 git clone https://github.com/Yasundram/local_manifest --depth 1 -b main .repo/local_manifests
 
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags || repo sync -c --force-sync --no-clone-bundle --no-tags
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 
 # build rom
-. build/envsetup.sh
+source build/envsetup.sh
 export CHERISH_MAINTAINER=Sundram
 export CHERISH_NONGAPPS=true
-brunch cherish_RMX1941-userdebug
+lunch cherish_RMX1941-userdebug
+mka bacon
+
 
 # upload rom
 rclone copy out/target/product/RMX1941/*UNOFFICIAL*.zip cirrus:RMX1941 -P
