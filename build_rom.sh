189c0e08b5c75c6fe81480d02e2476fdfd253caa
Fraschze97 <Anprasetya97@gmail.com>
2021-05-29 02:58:44 +0700

    following main

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,18 @@
+# _______  _______           __     _____      ___  __   
+#(  ____ )(       )|\     /|/  \   / ___ \    /   )/  \  
+#| (    )|| () () |( \   / )\/) ) ( (   ) )  / /) |\/) ) 
+#| (____)|| || || | \ (_) /   | | ( (___) | / (_) (_ | | 
+#|     __)| |(_)| |  ) _ (    | |  \____  |(____   _)| | 
+#| (\ (   | |   | | / ( ) \   | |       ) |     ) (  | | 
+#| ) \ \__| )   ( |( /   \ )__) (_/\____) )     | |__) (_
+#|/   \__/|/     \||/     \|\____/\______/      (_)\____/
+#                                                       
+ 
 # sync rom
 repo init -u https://github.com/NusantaraProject-ROM/android_manifest -b 11 --depth=1 -g default,-device,-mips,-darwin,-notdefault
-
-git clone https://github.com/Fraschze97/local_manifest.git --depth 1 -b nusantara .repo/local_manifests
-
+git clone https://github.com/Fraschze97/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
 source build/envsetup.sh
 lunch nad_RMX1941-userdebug
 export USE_GAPPS=true
@@ -13,5 +20,5 @@ export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 mka nad
 
-# upload rom
-rclone copy out/target/product/RMX1941/*.zip cirrus:RMX1941 -P
+# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+rclone copy out/target/product/RMX1941/NusantaraProject*.zip cirrus:RMX1941 -P
