2919cec183efd7d1937444c47e7745f1fa86864d
Apon77 <khalakuzzamanapon5@gmail.com>
2021-05-29 03:19:01 +0600

    Remove clean, clobber, fix branch naming

--- test.sh
+++ test.sh
@@ -4,22 +4,57 @@ set -e
 init_check=$(grep 'repo init' $CIRRUS_WORKING_DIR/build_rom.sh | grep 'depth=1')
 if [[ $init_check != *default,-device,-mips,-darwin,-notdefault* ]]
 then
-	echo Please use depth=1 and -g default,-device,-mips,-darwin,-notdefault tags in repo init line
+	echo Please use --depth=1 and -g default,-device,-mips,-darwin,-notdefault tags in repo init line.
 	exit 1
 fi
 
 clone_check=$(grep 'git clone' $CIRRUS_WORKING_DIR/build_rom.sh | wc -l)
 if [[ $clone_check -gt 1 ]]
 then
-	echo Please use local manifest to clone trees and other repositories, we dont allow git clone to clone trees
+	echo Please use local manifest to clone trees and other repositories, we dont allow git clone to clone trees.
 	exit 1
 fi
 
 rm_check=$(grep 'rm -rf' $CIRRUS_WORKING_DIR/build_rom.sh | wc -l)
 if [[ $rm_check -gt 0 ]]
 then
-	echo Please dont use rm -rf inside script, use local manifest for this purpose
+	echo Please dont use rm -rf inside script, use local manifest for this purpose.
 	exit 1
+fi
+
+clean_check=$(grep ' clean' $CIRRUS_WORKING_DIR/build_rom.sh | wc -l)
+if [[ $rm_check -gt 0 ]]
+then
+	echo Please dont use make clean. Server does make installclean by default, which is enough for most of the cases. 
+	exit 1
+fi
+
+clobber_check=$(grep ' clobber' $CIRRUS_WORKING_DIR/build_rom.sh | wc -l)
+if [[ $rm_check -gt 0 ]]
+then
+	echo Please dont use make clobber. Server does make installclean by default, which is enough for most of the cases. 
+	exit 1
+fi
+
+rom_name=$(grep init $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d / -f 4)
+device=$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)
+
+if [[ $CIRRUS_BRANCH == 'pull/.*' ]]
+then
+	if [[ $CIRRUS_CHANGE_MESSAGE != $device-$rom_name-* ]]
+	then
+		echo "Please use PR title like this device-Rom-Builder (Rom name must be matched from repo init line)"
+		exit 1
+	fi
+fi
+
+if [[ $CIRRUS_BRANCH != 'pull/.*' ]]
+then
+	if [[ $CIRRUS_BRANCH != $device-$rom_name-* ]]
+	then
+		echo "Please use branch name like this device-Rom-Builder (Rom name must be matched from repo init line)"
+		exit 1
+	fi
 fi
 
 echo Test passed
