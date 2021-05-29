4061d5b847dd9a78eb2fafdea4b78d7242dd750d
Sundram <63789329+Yasundram@users.noreply.github.com>
2021-05-30 03:08:58 +1000

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,17 +1,15 @@
 # sync rom
-repo init --depth=1 -u https://github.com/CherishOS/android_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
+repo init -u https://github.com/descendant-xi/manifests.git -b eleven-staging --depth=1 -g default,-device,-mips,-darwin,-notdefault
 
 git clone https://github.com/Yasundram/local_manifest --depth 1 -b main .repo/local_manifests
 
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-export CHERISH_MAINTAINER=Sundram
-export WITH_GAPPS=true
-lunch cherish_RMX1941-userdebug
-mka bacon
+lunch descendant_RMX1941-userdebug
+mka descendant
 
 
 # upload rom
