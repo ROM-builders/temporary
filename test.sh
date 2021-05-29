76ac391eb5b2158bd4c57d0c1e987704c6904a7d
adrian-8901 <adrianlam230@gmail.com>
2021-05-29 14:36:37 +0800

    Revert "Update test.sh"
    This reverts commit b2226ed9e5e2060d1ed825ffa2a2aeccfdda8e68.

--- test.sh
+++ test.sh
@@ -4,54 +4,38 @@ set -e
 init_check=$(grep 'repo init' $CIRRUS_WORKING_DIR/build_rom.sh | grep 'depth=1')
 if [[ $init_check != *default,-device,-mips,-darwin,-notdefault* ]]
 then
-	echo Please use --depth=1 and -g default,-device,-mips,-darwin,-notdefault tags in repo init line.
+	echo Please use depth=1 and -g default,-device,-mips,-darwin,-notdefault tags in repo init line
 	exit 1
 fi
 
 clone_check=$(grep 'git clone' $CIRRUS_WORKING_DIR/build_rom.sh | wc -l)
 if [[ $clone_check -gt 1 ]]
 then
-	echo Please use local manifest to clone trees and other repositories, we dont allow git clone to clone trees.
+	echo Please use local manifest to clone trees and other repositories, we dont allow git clone to clone trees
 	exit 1
 fi
 
 rm_check=$(grep 'rm -rf' $CIRRUS_WORKING_DIR/build_rom.sh | wc -l)
 if [[ $rm_check -gt 0 ]]
 then
-	echo Please dont use rm -rf inside script, use local manifest for this purpose.
+	echo Please dont use rm -rf inside script, use local manifest for this purpose
 	exit 1
 fi
 
-clean_check=$(grep ' clean' $CIRRUS_WORKING_DIR/build_rom.sh | wc -l)
-if [[ $rm_check -gt 0 ]]
-then
-	echo Please dont use make clean. Server does make installclean by default, which is enough for most of the cases. 
-	exit 1
-fi
-
-clobber_check=$(grep ' clobber' $CIRRUS_WORKING_DIR/build_rom.sh | wc -l)
-if [[ $rm_check -gt 0 ]]
-then
-	echo Please dont use make clobber. Server does make installclean by default, which is enough for most of the cases. 
-	exit 1
-fi
-
-rom_name=$(grep init $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d / -f 4)
 device=$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)
+rom_name=$(grep init $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d / -f 4)
+username=$(grep 'git clone' $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d / -f 4)
 
-if [[ $CIRRUS_BRANCH =~ 'pull/*' ]]
+ls ~/roms/$device-$rom_name-$username/*/*/*/vendorsetup.sh > /dev/null 2>&1 && echo Please remove vendorsetup.sh from device tree, use local manifest for this purpose https://github.com/Apon77Lab/android_.repo_local_manifests/tree/tutorial || true
+
+if [ -z "$CIRRUS_PR" ]
 then
-	if [[ $CIRRUS_CHANGE_MESSAGE != $device-$rom_name-* ]]
+	if [[ $CIRRUS_BRANCH != $device-$rom_name-$username ]]
 	then
-		echo "Please use PR title/branch name like this device-Rom-Builder (Rom name must be matched from repo init line)"
-		exit 1
+		echo "Please fix branch naming as device-rom_name-username. device=comes from lunch line (mido), rom_name=comes from repo init line (AospExtended), username=comes from git clone localmanifest's username(Apon77) i.e. branch name should be mido-AospExtended-Apon77"
 	fi
 else
-	if [[ $CIRRUS_BRANCH != $device-$rom_name-* ]]
-	then
-		echo "Please use branch name/PR title like this device-Rom-Builder (Rom name must be matched from repo init line)"
-		exit 1
-	fi
+      true
 fi
 
 echo Test passed
