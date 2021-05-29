562700f8946a096ac8a13d9a1ebfcd6120c1c6df
Fraschze97 <Anprasetya97@gmail.com>
2021-05-29 19:54:23 +0700

    sorry wrong build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -8,9 +8,6 @@
 #|/   \__/|/     \||/     \|\____/\______/      (_)\____/
 #          
 
-mkdir -p /tmp/rom # Where to sync source
-cd /tmp/rom                                             
- 
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/NusantaraProject-ROM/android_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/Fraschze97/local_manifest.git --depth 1 -b nusantara .repo/local_manifests
@@ -21,7 +18,7 @@ lunch nad_RMX1941-userdebug
 export USE_GAPPS=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
-mka nad -j$(nproc --all)
+mka nad
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/RMX1941/NusantaraProject*.zip cirrus:RMX1941 -P
