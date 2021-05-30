ae63edf083c1ad1800f94d9dd8476212cfae759c
Fraschze97 <Anprasetya97@gmail.com>
2021-05-30 13:15:30 +0700

    Up
    Signed-off-by: Fraschze97 <Anprasetya97@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -10,14 +10,13 @@
 
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/NusantaraProject-ROM/android_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
+
 git clone https://github.com/Fraschze97/local_manifest.git --depth 1 -b nusantara .repo/local_manifests
+
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-source build/envsetup.sh
+. build/envsetup.sh
 lunch nad_RMX1941-userdebug
-export USE_GAPPS=true
-export SELINUX_IGNORE_NEVERALLOWS=true
-export ALLOW_MISSING_DEPENDENCIES=true
 mka nad
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
