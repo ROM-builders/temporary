177fce53fba46ea27b72ebfe4c2f1904d026b301
adrian-8901 <adrianlam230@gmail.com>
2021-05-30 12:06:17 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-df -h 
+
 
 ## sync rom
 repo init -u https://github.com/Project-Fluid/manifest.git --depth=1 -b fluid-11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/adrian-8901/local_mainfest.git --depth=1 -b fluid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-ls 
-echo finished sync
 . build/envsetup.sh
 lunch fluid_umi-user 
 export ALLOW_MISSING_DEPENDENCIES=true
