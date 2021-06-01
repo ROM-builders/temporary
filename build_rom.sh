591c71e2539d27ad91d114272f54f56da45afaaf
Abhinav Verma <79688470+Abhinavftp@users.noreply.github.com>
2021-06-01 15:16:55 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/NezukoOS/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
-https://github.com/Abhinavftp/local_manifest.git --depth 1 -b los .repo/local_manifests
+git clone https://github.com/Abhinavftp/local_manifest.git --depth 1 -b los .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch nezuko_RMX1805-userdebug
-mka nezuko
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/RMX1805/*UNOFFICIAL*.zip cirrus:RMX1805 -P
