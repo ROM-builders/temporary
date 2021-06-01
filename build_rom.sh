1f0eb1fc0e262279ff33a3c38b2433b73a72d658
SayanAcharya <rowdysayan99@gmail.com>
2021-06-01 17:26:10 +0000

    Fix script
    Signed-off-by: SayanAcharya <rowdysayan99@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -7,11 +7,11 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 cd device/qcom/common && curl -L -o mypatch.patch https://github.com/AOSPA/android_device_qcom_common/commit/b42838e08f2e21cc1ce78961a8bca3a7e40d914e.patch && patch -p1 < mypatch.patch && cd -
 cd vendor/pa && curl -L -o mypatch.patch https://github.com/AOSPA/android_vendor_pa/commit/ec27f18b4a374f7041e1693058e205e0fcfbe734.patch && patch -p1 < mypatch.patch && cd -
 cd vendor/pa && curl -L -o mypatch.patch https://github.com/AOSPA/android_vendor_pa/commit/f0bfe716960ee8498d6a9442d734615909163d6b.patch && patch -p1 < mypatch.patch && cd -
-cd device/qcom/kernelscripts && curl -L -o mypatch.patch https://github.com/AOSPA/android_kernel_build/commit/0dfbc9cdf2f6ca505604859ec275ba4a0b81f038.patch && patch -p1 < mypatch.patch && cd -
 
 # build rom
 #build/envsetup.sh
 #lunch aosp_X00TD-user
+export TZ=Asia/Dhaka #put before last build command
 ./rom-build.sh X00TD -t user
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
