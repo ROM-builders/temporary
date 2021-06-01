731ab239b4a02501d14b35e341acd6bc267fc1e6
Apon77 <khalakuzzamanapon5@gmail.com>
2021-06-01 22:27:33 +0600

    Support for timezone [skip ci]

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch derp_vayu-user
+export TZ=Asia/Dhaka #put before last build command
 mka derp
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
