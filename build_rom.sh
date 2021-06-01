e33253fcc25e006a0c25f1292a0d510d4b4fd620
Rishawn Iyer <70256146+Stealth1226@users.noreply.github.com>
2021-06-01 22:49:38 +0530

    Lighthouse rs988 build

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,9 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-brunch rs988
+lunch lighthouse_rs988-user
+mka lighthouse
+
 # upload rom
 # If you need to upload json/multiple files too then put like this 'rclone copy out/target/product/mido/*.zip cirrus:mido -P && rclone copy out/target/product/mido/*.zip.json cirrus:mido -P'
 rclone copy out/target/product/rs988/*OFFICIAL*.zip cirrus:rs988 -P
