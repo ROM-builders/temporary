21785069f071a51b68f992a0e7f9ee4a689db5a7
D4rkKnight21 <yusuff8621@gmail.com>
2021-06-02 05:12:12 +0000

    platina lineage-fe-d4rkknight21: build
    -. fix dt2w
    -. fix cts
    -. add iorapd
    -. delete some typo on this scipt

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/Lineage-FE/manifest.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/D4rkKnight21/local_manifest.git  --depth 1 -b los .repo/local_manifests
+git clone https://github.com/D4rkKnight21/local_manifest.git --depth 1 -b los .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
