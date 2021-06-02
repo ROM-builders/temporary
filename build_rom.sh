2547808ab95de4af6d1daeff93cd9951bb1ec642
Abhinav Verma <79688470+Abhinavftp@users.noreply.github.com>
2021-06-02 20:24:17 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch wave_RMX1805-userdebug
-make wave
+make brunch
 
 # upload rom 
 rclone copy out/target/product/RMX1805/*UNOFFICIAL*.zip cirrus:RMX1805 -P
