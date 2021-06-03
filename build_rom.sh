ced0a2fdad51c418380d5fce79f4148fc8fde5b2
Abhinav Verma <79688470+Abhinavftp@users.noreply.github.com>
2021-06-03 11:44:36 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom 
-repo init --depth=1 --no-repo-verify -u https://github.com/Havoc-OS/android_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Fluid/manifest.git -b fluid-11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/Abhinavftp/local_manifest.git --depth 1 -b los .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch havoc_RMX1805-userdebug
+lunch fluid_RMX1805-userdebug
 make bacon
 
 # upload rom 
@@ -13,3 +13,4 @@ rclone copy out/target/product/RMX1805/*Unofficial*.zip cirrus:RMX1805 -P
  
       
    
+
