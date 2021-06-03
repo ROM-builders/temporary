649f2184d48089bf8abad2f56fa50038d4a67b3e
Fraschze97 <83362952+Fraschze97@users.noreply.github.com>
2021-06-03 14:30:05 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -13,4 +13,4 @@ export TZ=Asia/Jakarta
 mka nad
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
-rclone copy out/target/product/RMX1941/*UNOFFICIAL*.zip cirrus:RMX1941 -P
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
