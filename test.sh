7cc2c4b2daac75078b8465d366d357fa57affed8
Apon77 <khalakuzzamanapon5@gmail.com>
2021-06-03 23:04:04 +0600

    rclone copy and upload fix for all [skip ci]

--- test.sh
+++ test.sh
@@ -36,7 +36,12 @@ then
 	exit 1
 fi
 
-rom_name=$(grep init $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d / -f 4)
-device=$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)
+rclone_check=$(grep 'rclone copy' $CIRRUS_WORKING_DIR/build_rom.sh)
+rclone_string="rclone copy out/target/product/\$(grep unch \$CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:\$(grep unch \$CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P"
+if [[ $rclone_check != *$rclone_string* ]]
+then
+	echo Please follow rclone copy line of main branch.
+	exit 1
+fi
 
 echo Test passed
