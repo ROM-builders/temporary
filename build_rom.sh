c4d4cca5bf4f29517400b6ee7bf7454b33667104
Abhinav Verma <79688470+Abhinavftp@users.noreply.github.com>
2021-06-03 13:06:17 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,10 +7,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch fluid_RMX1805-userdebug
 make bacon
-
-# upload rom 
-rclone copy out/target/product/RMX1805/*UNOFFICIAL*.zip cirrus:RMX1805 -P
  
-      
-   
-
+# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
