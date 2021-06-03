65cbfe58746b42eb6c7d41483bd59c82d2af2dd6
Abhinav Verma <79688470+Abhinavftp@users.noreply.github.com>
2021-06-04 05:03:38 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom 
-repo init --depth=1 --no-repo-verify -u https://github.com/Havoc-OS/android_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/ShapeShiftOS/android_manifest.git -b android_11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/Abhinavftp/local_manifest.git --depth 1 -b los .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch havoc_RMX1805-userdebug
+lunch ssos_RMX1805-userdebug
 make bacon
  
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
