4544fb459bab3aa39ee86ca5c0ea5dfa137a8926
Rishawn Iyer <70256146+Stealth1226@users.noreply.github.com>
2021-06-04 16:16:34 +0530

    Lighthouse rs988 build

--- build_rom.sh
+++ build_rom.sh
@@ -11,6 +11,3 @@ brunch rs988
 # upload rom
 # If you need to upload json/multiple files too then put like this 'rclone copy out/target/product/mido/*.zip cirrus:mido -P && rclone copy out/target/product/mido/*.zip.json cirrus:mido -P'
 rclone copy out/target/product/rs988/*OFFICIAL*.zip cirrus:rs988 -P
-
-
-
