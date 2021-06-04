a6fa883f12fbea09fb4d05b35e023629836f0c2f
Fraschze97 <83362952+Fraschze97@users.noreply.github.com>
2021-06-04 11:57:52 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,12 +6,21 @@ git clone https://github.com/Fraschze97/local_manifest --depth=1 -b nusantara .r
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-
 . build/envsetup.sh
 lunch nad_RMX1941-userdebug
 export USE_GAPPS=true
 export TZ=Asia/Jakarta
+
+# metalava
+make_metalava(){
+	make sepolicy
+	make init
+	make booimage
+}
+
+make_metalava
 mka nad
 
+
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
-rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+rclone copy out/target/product/RMX1941/*UNOFFICIAL*.zip cirrus:RMX1941 -P
