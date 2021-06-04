13483530f6b33c2d8540898e29f1d54585e6238c
Fraschze97 <83362952+Fraschze97@users.noreply.github.com>
2021-06-05 00:51:24 +0700

    Just leave a Bug in Hotspot

--- build_rom.sh
+++ build_rom.sh
@@ -11,13 +11,19 @@ curl -LO https://github.com/wulan17/android_device_xiaomi_certus/commit/1b34da9a
 patch -p1 < *.patch
 cd ../../../..
 
+cd frameworks/opt/net/wifi
+curl -LO https://github.com/wulan17/patches/commit/8e28743ebd7d05cedc25aa5b34e280d56d318d80.patch
+patch -p1 < *.patch
+cd ../../../..
+
 # build rom
 . build/envsetup.sh
 lunch nad_RMX1941-userdebug
+export SKIP_API_CHECKS=true
+export SKIP_ABI_CHECKS=true
 export USE_GAPPS=true
 export TZ=Asia/Jakarta
 mka nad
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-
