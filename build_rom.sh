63317d7c6004943d1f40e8b2b0df3dff961e2a2b
Abhinav Verma <79688470+Abhinavftp@users.noreply.github.com>
2021-06-04 19:38:15 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,8 @@
 # sync rom 
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExtended/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/Abhinavftp/local_manifest.git --depth 1 -b los .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -f
+
 #build
 source build/envsetup.sh
 lunch aosp_RMX1805-userdebug
