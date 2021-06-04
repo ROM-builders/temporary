d9fe01da23d977c50e7f4271d4dc585e0bf3a88f
Abhinav Verma <79688470+Abhinavftp@users.noreply.github.com>
2021-06-04 14:00:50 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,8 +6,12 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch aosp_RMX1805-userdebug
-export TZ=Asia/Dhaka #put before last build command
-m aex -j$(nproc --all)
+export SKIP_API_CHECKS=true
+export SKIP_ABI_CHECKS=true
+export ALLOW_MISSING_DEPENDENCIES=true
+export SELINUX_IGNORE_NEVERALLOWS=true
+export TZ=Asia/Jakarta #put before last build command
+m aex
  
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
