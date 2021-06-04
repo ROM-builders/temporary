0f33db223a3c142a50dfa91c02f27973cb0ec196
Abhinav Verma <79688470+Abhinavftp@users.noreply.github.com>
2021-06-04 18:04:56 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -2,13 +2,6 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExtended/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/Abhinavftp/local_manifest.git --depth 1 -b los .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
-# patch
-cd frameworks/native && curl -O https://github.com/phhusson/platform_frameworks_native/commit/cc94e422c0a8b2680e7f9cfc391b2b03a56da765.patch && patch -p1 < *.patch && cd ../..
-
-
-
-
 #build
 source build/envsetup.sh
 lunch aosp_RMX1805-userdebug
@@ -18,5 +11,12 @@ mka bacon
  
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+
+
+
+
+
+
+
 
 
