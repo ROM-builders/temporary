a00a7af3c716463201133ac7bedaef8e06240996
Fraschze97 <83362952+Fraschze97@users.noreply.github.com>
2021-06-05 22:07:26 +0700

    1312264557

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,10 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/NusantaraProject-ROM/android_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
+
 git clone https://github.com/Fraschze97/local_manifest --depth=1 -b nusantara .repo/local_manifests
+
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
 
 # build rom
 . build/envsetup.sh
@@ -11,6 +14,7 @@ export SKIP_ABI_CHECKS=true
 export USE_GAPPS=true
 export TZ=Asia/Jakarta
 mka nad
+
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
