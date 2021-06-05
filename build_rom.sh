b2d1e3f246959baab57a1a5b7b16d45ea0a6ed4b
Abhinav Verma <79688470+Abhinavftp@users.noreply.github.com>
2021-06-05 06:10:37 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,16 @@
 # sync rom 
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExtended/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b elle -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/Abhinavftp/local_manifest.git --depth 1 -b los .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-#build
-source build/envsetup.sh
-lunch aosp_RMX1805-userdebug
+# patch
+cd frameworks/native && curl -O
+https://github.com/CherishOS/android_frameworks_native/commit/1f63eeea82e8f746cd5ef5859087906a9d0cf2a5.patch 
+&& patch -p1 < *.patch && cd ../..
 
+#build
+. build/envsetup.sh
+lunch evolution_RMX1805-userdebug
 export TZ=Asia/Jakarta #put before last build command
 mka bacon
  
