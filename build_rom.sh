b979b10cb6ea259268f64a2334b47443c5d05006
hello boi <18100664+androidgreek@users.noreply.github.com>
2021-06-05 20:45:51 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -2,11 +2,13 @@
 # sync rom
 repo init -u https://github.com/ArrowOS/android_manifest.git -b arrow-11.0 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/androidgreek/local_manifest.git --depth=1 -b arrow .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 build/envsetup.sh
 lunch arrow_devicefajita-userdebug
+export TZ=Asia/Jakarta #put before last build command
+croot
 m otapackage
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
