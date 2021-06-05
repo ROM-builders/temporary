6b807c13ac8f5ac9d6f550946ca60cbeacb8f068
hello boi <18100664+androidgreek@users.noreply.github.com>
2021-06-05 21:06:31 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,12 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/ConquerOS/manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/brucetech627/local_manifest.git --depth 1 .repo/manifest.xml
+git clone https://github.com/androidgreek/local_manifest.git --depth 1 .repo/manifest.xml
+
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch conquer_sweet-eng
+lunch arrow_fajita-user
 export TZ=Asia/Dhaka #put before last build command
 make carthage -j$(nproc --all)
 
