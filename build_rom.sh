aba72b44228aed19c4105850c0dbce67ceccf9e0
HyperNotShroudX97 <81577009+HyperNotShroudX97@users.noreply.github.com>
2021-06-06 00:24:51 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/PotatoProject/manifest -b dumaloo-release -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/HyperNotShroudX97/localmanifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/HyperNotShroudX97/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
