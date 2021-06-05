ab9fa05ba7b4f693ed67c15f8f6bc04cdbce47c2
brucetech627 <66194009+brucetech627@users.noreply.github.com>
2021-06-05 09:28:02 -0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/ConquerOS/manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/brucetech627/local_manifest.git --depth 1 .repo/manifest.xml
+git clone https://github.com/brucetech627/local_manifest.git --depth 1 local_manifests/roomservice.xml
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
