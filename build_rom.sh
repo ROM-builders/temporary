fec3c226138f72a3aac193f3692e0f7ffbdf5474
Abhinav Verma <79688470+Abhinavftp@users.noreply.github.com>
2021-06-05 06:57:05 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,10 +3,6 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest 
 git clone https://github.com/Abhinavftp/local_manifest.git --depth 1 -b los .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# patch
-cd frameworks/native && curl -O https://github.com/phhusson/platform_frameworks_native/commit/cc94e422c0a8b2680e7f9cfc391b2b03a56da765.patch && patch -p1 < *.patch && cd ../..
-cd external/selinux && curl -O https://github.com/phhusson/platform_external_selinux/commit/38d614ec61d610459a7f8e3a243a3dab7a20d356.patch && patch -p1 < *.patch && cd ../..
-
 #build
 . build/envsetup.sh
 lunch evolution_RMX1805-userdebug
