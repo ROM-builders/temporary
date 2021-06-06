1084c1f45a9f3d0b193a05718c43ac55ba538a93
nathanielwesley <82140646+nathanielwesley@users.noreply.github.com>
2021-06-06 14:47:48 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_Z00L-userdebug
-export TZ=Asia/Kolkata
+export TZ=Asia/Dhaka
 mka bacon -j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
