29a70751d9f85d250fe0657848ec2ee0e3a014cb
iAboothahir <65064906+iAboothahir@users.noreply.github.com>
2021-06-06 23:41:39 +0530

    lineage 2

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git
 git clone https://github.com/iAboothahir/manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom lineage
 source build/envsetup.sh
 lunch lineage_X00TD-userdebug
 export TZ=Asia/Dhaka #put before last build command
