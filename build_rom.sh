60bcc690922c1565ae81a1c0780418a0ca4e9319
Abhinav Verma <79688470+Abhinavftp@users.noreply.github.com>
2021-06-06 08:57:32 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom 
-repo init --depth=1 --no-repo-verify -u https://github.com/BlissRoms/platform_manifest.git -b r -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/Abhinavftp/local_manifest.git --depth 1 -b los .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
@@ -10,9 +10,9 @@ cd external/selinux && curl -O https://github.com/phhusson/platform_external_sel
 
 #build
 . build/envsetup.sh
-lunch bliss_RMX1805-userdebug
+lunch lineage_RMX1805-userdebug
 export TZ=Asia/Jakarta #put before last build command
-blissify -g RMX1805
+
  
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
