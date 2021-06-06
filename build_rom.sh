3bcd3ca86a4855a3d03457bf96764078f689c6df
nathanielwesley <82140646+nathanielwesley@users.noreply.github.com>
2021-06-06 13:58:50 +0530

    Build LOS18.1 Z00L

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/nathanielwesley/local_manifests.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/nathanielwesley/local_manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
