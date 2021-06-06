525a0f8afda560caedd760946b4c0cc8313d4bbe
Fraschze97 <83362952+Fraschze97@users.noreply.github.com>
2021-06-07 01:53:28 +0700

    under me gey

--- build_rom.sh
+++ build_rom.sh
@@ -13,6 +13,5 @@ export USE_GAPPS=true
 export TZ=Asia/Jakarta
 mka nad
 
-
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
