03642924388a8f900958e98c7f520b88e5d90ab0
Abhinav Verma <79688470+Abhinavftp@users.noreply.github.com>
2021-06-06 17:48:20 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -15,6 +15,9 @@ rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1
 
 
 
+# patch
+cd frameworks/native && curl -O https://github.com/phhusson/platform_frameworks_native/commit/cc94e422c0a8b2680e7f9cfc391b2b03a56da765.patch && patch -p1 < *.patch && cd ../..
+cd external/selinux && curl -O https://github.com/phhusson/platform_external_selinux/commit/38d614ec61d610459a7f8e3a243a3dab7a20d356.patch && patch -p1 < *.patch && cd ../..
 
 
 
