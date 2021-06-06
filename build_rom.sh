2f246d61771f1fdd95fbb1c080ba27f87137f546
nathanielwesley <82140646+nathanielwesley@users.noreply.github.com>
2021-06-06 14:07:06 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_Z00L_userdebug
+lunch lineage_Z00L-userdebug
 export TZ=Asia/Dhaka
 mka bacon -j$(nproc --all)
 
