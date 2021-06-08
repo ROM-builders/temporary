76fd88a955db514a728973e176d6ddf996996272
mukulsharma06175 <83113849+mukulsharma06175@users.noreply.github.com>
2021-06-08 21:29:20 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Octavi-OS/platform_manifest -b 11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/mukulsharma06175/frostmanifest/tree/Octiva-4.19 --depth 1 -b Octiva-4.19 .repo/local_manifests
-repo sync -c -f --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
  . build/envsetup.sh
