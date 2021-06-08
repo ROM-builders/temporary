c472fc596eaa7dd836bc223c479b91860a9a8f98
Abhinav Verma <79688470+Abhinavftp@users.noreply.github.com>
2021-06-08 07:52:59 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -9,7 +9,6 @@ lunch aosp_RMX1805-userdebug
 export TZ=Asia/Jakarta #put before last build command
 m aex -j$(nproc --all) 
  
-rm -rf device/aosp/sepolicy/common/dynamic/hal_aosp_smartcharge.te
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
