35dce29451df51907d20c6be6ce1dca0bedc673b
D4rkKnight21 <yusuff8621@gmail.com>
2021-06-08 11:53:27 +0700

    Build nitrogen

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/nitrogen-os-fan-edition/android_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/nitrogen-project/android_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/D4rkKnight21/local_manifest.git --depth 1 -b nosfe .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
