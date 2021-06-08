b67e52e5d898a60116c4642980155f18533b65f9
D4rkKnight21 <yusuff8621@gmail.com>
2021-06-08 08:21:46 +0700

    Build nos fe platina

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 . build/envsetup.sh
 export SELINUX_IGNORE_NEVERALLOWS=true
 lunch nitrogen_platina-userdebug
-export ALLOW_MISSING_DEPENDENCIES=true
 export SKIP_API_CHECKS=true
 export SKIP_ABI_CHECKS=true
 export TZ=Asia/Jakarta #put before last build command
