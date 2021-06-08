9b4dce6d6779c435ad0ade0d288b306da44b3511
Rishawn <70256146+Stealth1226@users.noreply.github.com>
2021-06-08 12:43:24 +0530

    Fix local manifest branch name , Lighthouse rs988

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 -u git://github.com/lighthouse-os/manifest.git -b raft -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Stealth1226/local_manifest --depth 1 -b aod .repo/local_manifests
+git clone https://github.com/Stealth1226/local_manifest --depth 1 -b raft .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
