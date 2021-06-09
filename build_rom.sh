ce7cea1644e49ad51637152893988d404dc83821
Skynet <67794738+TasinAyon@users.noreply.github.com>
2021-06-09 17:48:48 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/TasinAyon/manifes-local.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-git clone https://github.com/TasinAyon/device_xiaomi_sdm660-common --depth=1 -b dumaloo-release device/xiaomi/sdm660-common
-git clone https://github.com/TasinAyon/kernel_xiaomi_lavender --depth=1 -b dumaloo-release kernel/xiaomi/lavender
-git clone https://github.com/TasinAyon/vendor_xiaomi_lavender --depth=1 -b dumaloo-release vendor/xiaomi/lavender
-git clone https://github.com/TasinAyon/vendor_xiaomi_sdm660-common --depth=1 -b dumaloo-release vendor/xiaomi/sdm660-common
-git clone https://github.com/TasinAyon/device_xiaomi_lavender-1 --depth=1 -b dumaloo-release device/xiaomi/lavender
-git clone https://github.com/TasinAyon/android_device_qcom_sepolicy --depth=1 -b lineage-18.1-legacy-um device/qcom/sepolicy-legacy-um
 
 # build rom
 source build/envsetup.sh
