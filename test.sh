4bfe1f2c1f8a5bca3c6cab6095963d76ffa93a02
Trần Nguyễn Tuấn Anh <43093213+dopaemon@users.noreply.github.com>
2021-06-11 02:44:26 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -e
 
-init_check=$(grep 'repo init' $CIRRUS_WORKING_DIR/build_rom.sh | grep 'depth=1')
-if [[ $init_check != *default,-device,-mips,-darwin,-notdefault* ]]
-then
-	echo Please use --depth=1 and -g default,-device,-mips,-darwin,-notdefault tags in repo init line.
-	exit 1
-fi
-
 clone_check=$(grep 'git clone' $CIRRUS_WORKING_DIR/build_rom.sh | wc -l)
 if [[ $clone_check -gt 1 ]]
 then
