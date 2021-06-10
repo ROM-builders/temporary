2843a1bd25d509aadcf78a79f12c93431df7c12a
mukulsharma06175 <83113849+mukulsharma06175@users.noreply.github.com>
2021-06-10 13:37:41 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,12 @@
 # sync rom.  
 repo init --depth=1 --no-repo-verify -u https://github.com/exthmui/android.git -b exthm-11 -g default,-device,-mips,-darwin,-notdefault
-
 git clone https://github.com/mukulsharma06175/frostmanifest --depth 1 -b derp .repo/local_manifests
 repo sync -c -f --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j8
 
-# build rom
-$ . build/envsetup.sh
-$ lunch octavi_lavender-userdebug
-$ brunch lavender
+build rom
+. build/envsetup.sh
+lunch octavi_lavender-userdebug
+brunch lavender
 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
