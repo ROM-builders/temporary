07b94c29bbcaff07450dfb7b4d2ef9e998d75c37
Wiley Lau <henloboii@protonmail.ch>
2021-06-10 09:51:51 +0800

    wat

--- build_rom.sh
+++ build_rom.sh
@@ -3,6 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/exthmui/android.git -b 
 git clone https://github.com/exthm-miatoll/manifest --depth=1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+
 # build rom
 source build/envsetup.sh
 lunch exthm_miatoll-userdebug
