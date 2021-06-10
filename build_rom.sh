480eedfb39e11819c541bfa282dbd5e39093d72b
mukulsharma06175 <83113849+mukulsharma06175@users.noreply.github.com>
2021-06-10 13:36:36 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom.  
 repo init --depth=1 --no-repo-verify -u https://github.com/exthmui/android.git -b exthm-11 -g default,-device,-mips,-darwin,-notdefault
 
-git clone ps://github.com/mukulsharma06175/frostmanifest/frostmanifest.git --depth 1 -b Octiva-4.19 .repo/local_manifests
+git clone https://github.com/mukulsharma06175/frostmanifest --depth 1 -b derp .repo/local_manifests
 repo sync -c -f --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j8
 
 # build rom
