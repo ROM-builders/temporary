05b543a93d6d989c5ff23cc5e71194be7a4fcf6e
Trần Nguyễn Tuấn Anh <43093213+dopaemon@users.noreply.github.com>
2021-06-11 14:06:34 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,9 +5,9 @@ repo sync -v -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sy
 
 # build rom
 source build/envsetup.sh
-lunch cherish_whyred-userdebug
+export SKIP_ABI_CHECKS=true
 export TZ=Asia/Ho_Chi_Minh #put before last build command
-mka bacon -j$(nproc --all)
+brunch whyred
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
