468e9410ea34ac4dcbe29ff7d493e993da910364
mukulsharma06175 <83113849+mukulsharma06175@users.noreply.github.com>
2021-06-11 07:50:07 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 #build rom
 . build/envsetup.sh
 lunch lineage_lavender-userdebug
-export TZ=Asia/Dhaka #put before last build comman
+export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
