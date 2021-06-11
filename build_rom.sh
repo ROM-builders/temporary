09c83b54096ef3cc8ec0a83b9652fed9e1462973
mukulsharma06175 <83113849+mukulsharma06175@users.noreply.github.com>
2021-06-11 17:56:14 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom.  
-repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b fire -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Kang-OS-R/android_manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/mukulsharma06175/frostmanifest --depth 1 -b derp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 #build rom
-. build/env*
-lunch spark_lavender-userdebug
-mka spark -j$(nproc --all)
+. build/envsetup.sh
+lunch kangos_lavender-userdebug
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
