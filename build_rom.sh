2e68bbb16f989e0557955fc86b65cb4cd53da540
mukulsharma06175 <83113849+mukulsharma06175@users.noreply.github.com>
2021-06-11 07:40:35 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom.  
 repo init --depth=1 --no-repo-verify -u git://github.com/CipherOS/android_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone git@github.com:mukulsharma06175/frostmanifest.git --depth 1 -b derp .repo/local_manifests
+git clone https://github.com/mukulsharma06175/frostmanifest --depth 1 -b derp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 #build rom
