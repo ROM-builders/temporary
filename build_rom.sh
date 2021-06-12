c34d5b5e59f08ab9d7648a5dd015c96f9c7a4532
Abhinav Verma <79688470+RemuruSama@users.noreply.github.com>
2021-06-12 15:34:23 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -9,11 +9,6 @@ lunch cherish_RMX1805-userdebug
 export TZ=Asia/Jakarta #put before last build command
 brunch RMX1805
 
-# patch
-cd frameworks/native && curl -O https://github.com/phhusson/platform_frameworks_native/commit/cc94e422c0a8b2680e7f9cfc391b2b03a56da765.patch && patch -p1 < *.patch && cd ../..
-cd external/selinux && curl -O https://github.com/phhusson/platform_external_selinux/commit/38d614ec61d610459a7f8e3a243a3dab7a20d356.patch && patch -p1 < *.patch && cd ../..
-
-
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
 
