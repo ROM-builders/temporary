266e79de58ed5055e601b6076a10e8378f7ee831
glenn <glennib@outlook.it>
2021-06-12 11:31:50 +0200

    pain

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch lineage_RMX2176-userdebug
 export TZ=Asia/Dhaka #put before last build command
-mka clean
+rm -rf out/.lock
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
