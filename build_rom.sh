93394bf7cdeb62eacb485bbfff473863c61b64e7
MiDoNaSR545 <57400341+MiDoNaSR545@users.noreply.github.com>
2021-06-13 14:17:17 +0200

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch dot_ysl-user
-export WITH_GAPPS=true
+export WITH_GAPPS=false
 make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
