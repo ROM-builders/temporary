09b260e68431fa48407fa81fa3585bfcde693b71
mukulsharma06175 <83113849+mukulsharma06175@users.noreply.github.com>
2021-06-16 21:05:03 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/CherishOS/android_manifest.git -b test -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/hungphan2001/local_manifests.git --depth 1 -b lavender .repo/local_manifests
+git clone https://github.com/mukulsharma06175/local_manifests.git --depth 1 -b lavender .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
