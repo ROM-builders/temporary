82f0c6b96d391154a0ad766d8043312dbb5a7e59
Rishawn <70256146+Stealth1226@users.noreply.github.com>
2021-06-16 12:17:44 +0530

    Remove && from script Lighthouse h850 build

--- build_rom.sh
+++ build_rom.sh
@@ -10,5 +10,5 @@ mka lighthouse
 
 
 # upload rom
-# If you need to upload json/multiple files too then put like this 'rclone copy out/target/product/mido/*.zip cirrus:mido -P && rclone copy out/target/product/mido/*.zip.json cirrus:mido -P'
+# If you need to upload json/multiple files too then put like this 'rclone copy out/target/product/mido/*.zip cirrus:mido -P  rclone copy out/target/product/mido/*.zip.json cirrus:mido -P'
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P 
