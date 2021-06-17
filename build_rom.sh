24d55e0931fddafdb0a3e4ec219258b4294375e1
mukulsharma06175 <83113849+mukulsharma06175@users.noreply.github.com>
2021-06-17 16:36:27 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch cherish_lavender-userdebug
-export TZ=Asia/Ho_Chi_Minh #put before last build command
+export TZ=asia/kolkata #put before last build command
 mka bacon -j$(nproc --all) 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
