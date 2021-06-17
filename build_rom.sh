2e3a1cae4a3847355d6ac72a34e3d6775e0cda73
mukulsharma06175 <83113849+mukulsharma06175@users.noreply.github.com>
2021-06-17 21:28:16 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/Komodo-OS-Rom/manifest -b 11 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Komodo-OS-Rom/manifest -b 11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/mukulsharma06175/local_manifests.git --depth 1 -b Komodo .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
