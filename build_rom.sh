db7c9ef7d987b5d06f08d24223f40759489fdda5
mukulsharma06175 <83113849+mukulsharma06175@users.noreply.github.com>
2021-06-18 13:30:44 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch fluid_lavender-userdebug
+export TZ=Asia/Kolkata #put before last build command (setting timezone)
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
