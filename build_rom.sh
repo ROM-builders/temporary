69278bf252873ac34fb4fbf07bac128632ec9e21
Prashant1873 <shuklaprashant847@gmail.com>
2021-06-18 20:01:05 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-brunch
+lunch havoc_jasmine_sprout-userdebug
+make bacon
 export TZ=Asia/Dhaka #put before last build command
 
 
