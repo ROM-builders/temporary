523406f2197ff729e7085fecf340d9a340ef4206
Fraschze97 <83362952+Fraschze97@users.noreply.github.com>
2021-06-20 00:00:49 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/NusantaraProject-ROM/android_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Fraschze97/local_manifest --depth=1 -b nusantara11 .repo/local_manifests
+git clone https://github.com/Fraschze97/local_manifest --depth=1 -b nad11 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
 lunch nad_RMX1941-userdebug
-export SKIP_API_CHECKS=true
-export SKIP_ABI_CHECKS=true
-export USE_GAPPS=true
 export TZ=Asia/Jakarta
 mka nad
 
