6b8fadab1737e4ce5a92df2995cfa7e0f9a89cd1
Prashant1873 <shuklaprashant847@gmail.com>
2021-06-19 10:26:48 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Havoc-OS/android_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Descendant-XI/manifests.git -b eleven-staging -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/Prashant1873/manifest_personal.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
