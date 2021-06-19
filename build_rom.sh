68e880858ece6c57b93aa63bdf06e654b5d01d96
Prashant Shukla <76157174+Prashant1873@users.noreply.github.com>
2021-06-20 04:54:06 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch descendant_jasmine_sprout-userdebug
-make bacon
+mka descendant
+
 export TZ=Asia/Dhaka #put before last build command
 
 
