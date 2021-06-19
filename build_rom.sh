9ed83e38834408b6556220e4db9e163b522d88ec
mukulsharma06175 <83113849+mukulsharma06175@users.noreply.github.com>
2021-06-19 16:11:25 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -8,7 +8,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 lunch aosp_lavender-userdebug
 mka bacon 
 export SELINUX_IGNORE_NEVERALLOWS=true
-export TZ=Asia/Kolkata #put before make command (Time)
+export TZ=Asia/Kolkata #put before make command (Time zone)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
