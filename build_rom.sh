f8746775a426462610c9016abf18d9e6a68ecf0e
mukulsharma06175 <83113849+mukulsharma06175@users.noreply.github.com>
2021-06-20 17:38:37 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -8,6 +8,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 lunch corvus_lavender-userdebug
 export TZ=Asia/Dhaka #put before last build command (Time zone)
 export SELINUX_IGNORE_NEVERALLOWS=true
+export RAVEN_LAIR=BETS
 make corvus
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
