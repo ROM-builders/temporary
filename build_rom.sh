dda995e21a46bd001e5b37cc78aa6b60bc44d4da
Prashant Shukla <76157174+Prashant1873@users.noreply.github.com>
2021-06-20 05:56:51 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-brunch
+brunch jasmine_sprout 
 
 export TZ=Asia/Dhaka #put before last build command
 
