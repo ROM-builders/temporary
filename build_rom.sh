567220517c2339e01c3ff08b6c9d54e22e4da7d3
Prashant Shukla <76157174+Prashant1873@users.noreply.github.com>
2021-06-20 05:54:02 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,8 +5,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch havoc_jasmine_sprout-userdebug
-mka havoc
+brunch
+
 export TZ=Asia/Dhaka #put before last build command
 
 
