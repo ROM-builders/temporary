66230692c3c647dbb6832c1c4df7491de352d6a5
Prashant Shukla <76157174+Prashant1873@users.noreply.github.com>
2021-06-22 02:47:24 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch cherish_jasmine_sprout-userdebug
-export TZ=Asia/Dhaka 
+export TZ=Asia/Kolkata 
 mka bacon -j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
