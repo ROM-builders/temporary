2ad84bce6c7f5b7ea845deff0aad2a278b1d0732
Prashant Shukla <76157174+Prashant1873@users.noreply.github.com>
2021-06-22 02:47:03 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/CherishOS/android_manifest.git -b test -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Prashant1873/manifest_personal --depth=1 -b main .repo/local_manifests
+git clone https://github.com/Prashant1873/manifest_personal --depth=1 -b cherish .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
