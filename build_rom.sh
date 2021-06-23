75df55e18e7e9f1996d56032e60ac8518a9057a6
mukulsharma06175 <83113849+mukulsharma06175@users.noreply.github.com>
2021-06-23 16:37:07 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch lighthouse_lavender-userdebug
-export TZ=Asia/Dhaka #put before last build command
+export TZ=Asia/Dhaka #put before last build command (Time zone)
 mka lighthouse
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
