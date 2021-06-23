248cced8034272ee85e15bb8ea3234676c899a05
Fraschze97 <83362952+Fraschze97@users.noreply.github.com>
2021-06-24 01:40:10 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch nad_RMX1941-userdebug
+export USE_GAPPS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Jakarta
 mka nad
