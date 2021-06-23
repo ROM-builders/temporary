6ab7950502dbfdcdd00e698ae54a33b257c79654
Abhinav Verma <77457859+abhinavftp98@users.noreply.github.com>
2021-06-23 17:48:55 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,6 +3,13 @@ repo init --depth=1 --no-repo-verify -u https://github.com/RevengeOS/android_man
 git clone https://github.com/abhinavftp98/local-manifest.git --depth 1 -b los .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+# patch
+cd packages/apps/Settings && curl -O https://github.com/random-aosp-stuff/android_packages_apps_Settings/commit/32752dae1ea3d61a0f7603432bd9b662d062a1f0.patch && patch -p1 < *.patch && cd ../../..
+
+cd frameworks/native && curl -O https://github.com/phhusson/platform_frameworks_native/commit/cc94e422c0a8b2680e7f9cfc391b2b03a56da765.patch && patch -p1 < *.patch && cd ../..
+
+cd external/selinux && curl -O https://github.com/phhusson/platform_external_selinux/commit/38d614ec61d610459a7f8e3a243a3dab7a20d356.patch && patch -p1 < *.patch && cd ../..
+
 # build rom
 source build/envsetup.sh
 lunch revengeos_RMX1805-userdebug
