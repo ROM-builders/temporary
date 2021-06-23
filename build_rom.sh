e6e934715b427df6825f15f54be8b7955b6d04f9
Prashant Shukla <76157174+Prashant1873@users.noreply.github.com>
2021-06-23 22:51:20 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -10,5 +10,7 @@ export TZ=Asia/Kolkata #put before last build command
 mka nad
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+rm out/target/product/jasmine/*-eng*.zip
+rm out/target/product/jasmine/*-ota-*.zip
 # rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
 rclone copy out/target/product/jasmine_sprout/*zip cirrus:jasmine_sprout -P
