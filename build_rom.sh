113f18b60a9aadfdacdb9c8a2f9c0be2f5585292
Abhinav Verma <77457859+abhinavftp98@users.noreply.github.com>
2021-06-23 09:09:06 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,6 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch revengeos_RMX1805-userdebug
 export TZ=Asia/Dhaka #put before last build command
+export IS_CIENV=true
 make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
