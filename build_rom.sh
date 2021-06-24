2e1d87d0185b20893a6963b20cbf0ef09bf83034
mukulsharma06175 <83113849+mukulsharma06175@users.noreply.github.com>
2021-06-24 12:01:05 +0530

    lavender_neruko-mukulsharma05175

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Komodo-OS-Rom/manifest -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/mukulsharma06175/local_manifests.git --depth 1 -b Komodo .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/NezukoOS/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/mukulsharma06175/frostmanifest.git --depth 1 -b Nezuko .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch komodo_lavender-userdebug
+lunch nezuko_lavender-userdebug
 export TZ=Asia/Ho_Chi_Minh #put before last build command
-masak komodo -j$(nproc --all) 
+mka bacon 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
