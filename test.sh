8b35b2b32e8208df5fbb36261e56d76de33070b3
Fraschze97 <83362952+Fraschze97@users.noreply.github.com>
2021-06-25 02:23:36 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -22,13 +22,6 @@ then
 	exit 1
 fi
 
-mv_check=$(grep 'mv ' $CIRRUS_WORKING_DIR/build_rom.sh | wc -l)
-if [[ $mv_check -gt 0 ]]
-then
-	echo Please dont use mv inside script, use local manifest for this purpose.
-	exit 1
-fi
-
 clean_check=$(grep ' clean' $CIRRUS_WORKING_DIR/build_rom.sh | wc -l)
 if [[ $clean_check -gt 0 ]]
 then
@@ -40,27 +33,6 @@ clobber_check=$(grep ' clobber' $CIRRUS_WORKING_DIR/build_rom.sh | wc -l)
 if [[ $clobber_check -gt 0 ]]
 then
 	echo Please dont use make clobber. Server does make installclean by default, which is enough for most of the cases. 
-	exit 1
-fi
-
-installclean_check=$(grep ' installclean' $CIRRUS_WORKING_DIR/build_rom.sh | wc -l)
-if [[ $installclean_check -gt 0 ]]
-then
-	echo Please dont use make installclean. Server does make installclean by default, which is enough for most of the cases. 
-	exit 1
-fi
-
-patch_check=$(grep 'patch ' $CIRRUS_WORKING_DIR/build_rom.sh | wc -l)
-if [[ $patch_check -gt 0 ]]
-then
-	echo Please dont use patch inside script, use local manifest for this purpose.
-	exit 1
-fi
-
-and_check=$(grep ' && ' $CIRRUS_WORKING_DIR/build_rom.sh | wc -l)
-if [[ $and_check -gt 0 ]]
-then
-	echo 'Please dont use && inside script, put that command in next line for this purpose.'
 	exit 1
 fi
 
@@ -77,20 +49,6 @@ sync_string="repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune 
 if [[ $sync_check != *$sync_string* ]]
 then
 	echo Please follow repo sync line of main branch.
-	exit 1
-fi
-
-fetch_check=$(grep 'fetch ' $CIRRUS_WORKING_DIR/build_rom.sh | wc -l)
-if [[ $fetch_check -gt 0 ]]
-then
-	echo Please dont use fetch inside script, use local manifest for this purpose.
-	exit 1
-fi
-
-cd_check=$(grep 'cd ' $CIRRUS_WORKING_DIR/build_rom.sh | wc -l)
-if [[ $cd_check -gt 0 ]]
-then
-	echo Please dont use cd inside script, use local manifest for this purpose.
 	exit 1
 fi
 
