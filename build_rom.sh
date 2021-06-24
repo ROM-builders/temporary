99d31ab8949c6800bec103ed467b53cb7bf9c7e6
Prashant Meena <71384284+Prashant-1695@users.noreply.github.com>
2021-06-24 15:45:49 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/NezukoOS/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Prashant-1695/frostmanifest --depth 1 -b master .repo/local_manifests
+git clone https://github.com/Prashant-1695/frostmanifest --depth 1 -b nezuko .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
