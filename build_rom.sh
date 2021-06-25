6a971c4e939e81c1048a3e5796c0687ee3e71ed2
Anmol Dhiman <73706789+Anmoldh@users.noreply.github.com>
2021-06-25 19:15:12 +0530

    typo

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch havoc_lavender-userdebug
+lunch statix_lavender-userdebug
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
