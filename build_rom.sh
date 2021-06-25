19923953cc2a1306ee77be0df81ada377a283c5f
Anmol Dhiman <73706789+Anmoldh@users.noreply.github.com>
2021-06-25 15:47:08 +0530

    typo

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/StatiXOS/android_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Anmoldh/local-lanifest.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/Anmoldh/local-manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
