035f56539bb3807f8bf7accfe06291581334940a
Endi Priyatna <65471980+Dreadwyrm@users.noreply.github.com>
2021-06-25 12:43:41 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-breakfast ginkgo
+lunch_lineage_ginkgo-userdebug
 export TZ=Asia/Jakarta
 mka bacon
 
