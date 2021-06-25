f5731af6cd01858971837dbeb69192403a66ba9b
Endi Priyatna <65471980+Dreadwyrm@users.noreply.github.com>
2021-06-25 11:25:03 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 -u https://github.com/SpiceOS/android.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Dreadwyrm/local_manifests --depth=1 -b master .repo/local_manifests
+git clone https://github.com/Dreadwyrm/local_manifests --depth=1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
