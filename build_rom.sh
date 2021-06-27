53dc8fa8720f2aece7f52d37e3a9bb757ce82163
Sandeep Chahal <sandeeps567ingh@gmail.com>
2021-06-27 08:51:01 +0530

    Resync [skip ci]

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/Noob-214/local_manifest.git --depth 1 -b master .repo/local_manifests
-git checkout .
+
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
