91faea005ff51c2fd9e91393a7827e098a2bcdde
Sandeep Chahal <sandeeps567ingh@gmail.com>
2021-06-27 08:34:03 +0000

    build aex-ysl

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/Noob-214/local_manifest.git --depth 1 -b master .repo/local_manifests
-#
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
-
-git clone https://github.com/LineageOS/android_packages_resources_devicesettings --depth=1 packages/resources/devicesettings
 
 # build rom
 source build/envsetup.sh
