236c364bd3ed132f32c75f2d8b4558ba6bd11c42
NganuCoeg <starterpackmeh@gmail.com>
2021-06-28 12:34:40 +0000

    Revert "Add patches to build_rom.sh"
    This reverts commit 2840bf3af57d3237c7bccc21d81d7d3d224f4731.

--- build_rom.sh
+++ build_rom.sh
@@ -3,17 +3,6 @@ repo init --depth=1 --no-repo-verify -u https://github.com/exthmui/android.git -
 git clone https://github.com/NganuCoeg/local_manifests --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# patches
-cd external/selinux
-wget https://raw.githubusercontent.com/SamarV-121/android_vendor_extra/lineage-18.1/patches/external/selinux/0001-Revert-libsepol-Make-an-unknown-permission-an-error-.patch
-patch -p1 < *.patch
-cd ../..
-
-cd frameworks/av
-wget https://github.com/phhusson/platform_frameworks_av/commit/624cfc90b8bedb024f289772960f3cd7072fa940.patch
-patch -p1 < *.patch
-cd ../..
-
 # build rom
 . build/envsetup.sh
 lunch exthm_RMX3171-userdebug
