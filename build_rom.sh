4cff3b1072e453b85d020676077289e76bca35ed
nhAsif <49162687+nhAsif@users.noreply.github.com>
2021-06-28 11:20:43 +0600

    rosy-CherishOS-nhAsif
    Building cherish os vanilla for rosy

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,9 @@ git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b main .repo/l
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
+
 source build/envsetup.sh
+export CHERISH_NONGAPPS=true
 lunch cherish_rosy-userdebug
 export TZ=Asia/Dhaka #put before last build command
 brunch rosy
