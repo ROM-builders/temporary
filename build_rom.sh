e19f5039c224662dcc400fca3f490a2dd6a74254
Prashant Shukla <76157174+Prashant1873@users.noreply.github.com>
2021-06-29 03:49:40 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -11,5 +11,5 @@ brunch jasmine_sprout
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 # rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-rm out/target/product/jasmine/*-ota-*.zip
+rm out/target/product/jasmine_sprout/*-ota-*.zip
 rclone copy out/target/product/jasmine_sprout/*zip cirrus:jasmine_sprout -P 
