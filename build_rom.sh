a530c3bca14a02989206eecc40943c399ce7a594
NganuCoeg <starterpackmeh@gmail.com>
2021-06-28 17:56:24 +0000

    Export SELINUX_IGNORE_NEVERALLOWS

--- build_rom.sh
+++ build_rom.sh
@@ -7,6 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 . build/envsetup.sh
 lunch exthm_RMX1805-userdebug
 export TZ=Asia/Jakarta
+export SELINUX_IGNORE_NEVERALLOWS=true
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
