7db74e5d0db0b128329a58d236259022ca51c9f4
satriojoyo <53747490+satriojoyo@users.noreply.github.com>
2021-06-28 23:49:17 +0700

    Update build_rom.sh
    Build with Gapps

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch exthm_lavender-userdebug
+export EXTHM_GAPPS=true
 export TZ=Asia/Jakarta
 mka bacon
 
