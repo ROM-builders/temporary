b63b37b70f91f38f622d1201eab3cb0a4d588f24
mukulsharma06175 <83113849+mukulsharma06175@users.noreply.github.com>
2021-06-30 16:17:54 +0530

    Final fix

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,7 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/Corvus-R/android_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/mukulsharma06175/frostmanifest.git --depth 1 -b Corvus .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-       
+        
 # build rom
 . build/envsetup.sh
 lunch corvus_lavender-userdebug
