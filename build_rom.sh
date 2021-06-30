467fb820bbe61f4151a6b378e0b5f3bad6aba7db
satriojoyo <53747490+satriojoyo@users.noreply.github.com>
2021-07-01 02:40:37 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u github.com/nitrogen-os-fan-edition/android_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/nitrogen-os-fan-edition/android_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/satriojoyo/frostmanifest.git --depth 1 -b exthm .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
