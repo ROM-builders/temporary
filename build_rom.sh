fecf8380af0ca17e7b5c49596d59cfe305790d95
Prashant Meena <71384284+Prashant-1695@users.noreply.github.com>
2021-06-30 07:27:05 +0530

    Legion gapps

--- build_rom.sh
+++ build_rom.sh
@@ -7,6 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 . build/envsetup.sh
 lunch legion_lavender-userdebug
 export TZ=Asia/Dhaka #put before last build command
+export USE_GAPPS=true
 make legion
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
