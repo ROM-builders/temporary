4393e287cf4d01d8676b3ded2e615c81014f00bc
mukulsharma06175 <83113849+mukulsharma06175@users.noreply.github.com>
2021-06-30 08:14:09 +0530

    I hop sync fixed

--- build_rom.sh
+++ build_rom.sh
@@ -2,13 +2,12 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/Corvus-R/android_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/mukulsharma06175/frostmanifest.git --depth 1 -b Corvus .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-   
+    
 # build rom
 . build/envsetup.sh
 lunch corvus_lavender-userdebug
 export TZ=Asia/kolkata #put before last build command (Time zone)
 export SELINUX_IGNORE_NEVERALLOWS=true
-export USE_GAPPS=true
 make corvus
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
