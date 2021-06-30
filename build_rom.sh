6231a732ec1d8329741f851324c5b319aedc373a
satriojoyo <53747490+satriojoyo@users.noreply.github.com>
2021-06-30 19:50:16 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/exthmui/android.git -b exthm-11 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u github.com/nitrogen-os-fan-edition/android_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/satriojoyo/frostmanifest.git --depth 1 -b exthm .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch exthm_lavender-userdebug
-export EXTHM_GAPPS=true
+lunch nitrogen_lavender-userdebug
 export TZ=Asia/Jakarta
-mka bacon
+make -j$(nproc --all) otapackage
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
