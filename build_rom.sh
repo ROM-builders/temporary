fb5e49e4fc6bb9f3bc05ea519ca7c05ff4437d52
Prashant Meena <71384284+Prashant-1695@users.noreply.github.com>
2021-07-02 09:40:38 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Project-LegionOS/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Prashant-1695/frostmanifest --depth 1 -b crdroid .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Fluid/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/Prashant-1695/frostmanifest --depth 1 -b octavi .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
 lunch legion_lavender-userdebug
 export TZ=Asia/Dhaka #put before last build command
-export USE_GAPPS=true
-make legion
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
