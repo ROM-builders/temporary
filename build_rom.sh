66133f9b9c4533245afa627da4d0c2fe243508e2
akira-vishal <vishal.rockstar7011@gmail.com>
2021-07-03 02:19:59 +0630

    update

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 . build/envsetup.sh
 lunch aosp_whyred-user
 export TZ=Asia/Dhaka #put before last build command
-export USE_GAPPS=true
 make syberia
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
