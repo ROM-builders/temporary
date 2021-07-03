7feafa03c27422c5bcf6daf540d133b05910ea61
mukulsharma06175 <83113849+mukulsharma06175@users.noreply.github.com>
2021-07-03 11:09:07 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,7 @@
 repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/mukulsharma06175/frostmanifest.git  --depth 1 -b derpfest .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
- 
+  
 # build rom
 . build/envsetup.sh
 lunch derp_lavender-userdebug
