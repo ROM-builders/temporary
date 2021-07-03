4b2072ec6138fc8aab3b7d52ad01343416b48576
Cristian-J <59663672+Cristian-J@users.noreply.github.com>
2021-07-03 12:59:25 -0400

    Build trigger
    fixed device tree issue

--- build_rom.sh
+++ build_rom.sh
@@ -4,6 +4,7 @@ git clone https://github.com/Cristian-J/local-manifest.git --depth 1 -b main .re
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
+# -
 source build/envsetup.sh
 lunch cherish_joan-userdebug
 mka bacon
