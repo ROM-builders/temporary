90d3c927d7b13960af7400fb4bc75ec2f53da0f8
brucetech627 <66194009+brucetech627@users.noreply.github.com>
2021-07-03 19:21:39 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/NezukoOS/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/brucetech627/local_manifests --depth 1 -b master .repo/local_manifests
+git clone https://github.com/brucetech627/local_manifests --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
