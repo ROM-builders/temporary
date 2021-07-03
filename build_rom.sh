4c2b4f0acdcf673e4dd9ceb432eee70f6122b2a6
Cristian-J <59663672+Cristian-J@users.noreply.github.com>
2021-07-03 09:42:35 -0400

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch cherishos_joan-userdebug
+lunch aosp_joan-userdebug
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
