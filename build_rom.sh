d40ad20e5f8f52c6337d99bfdc1cbdea92a755c6
satriojoyo <53747490+satriojoyo@users.noreply.github.com>
2021-07-03 10:40:12 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/AOSiP/platform_manifest.git -b ten -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/AOSiP/platform_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/satriojoyo/frostmanifest.git --depth 1 -b exthm .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
