7d1970cf18ab2620c50ee50988eac7769c58f5d8
Cristian-J <59663672+Cristian-J@users.noreply.github.com>
2021-07-03 13:00:59 -0400

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,6 @@ git clone https://github.com/Cristian-J/local-manifest.git --depth 1 -b main .re
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-# -
 source build/envsetup.sh
 lunch cherish_joan-userdebug
 mka bacon
