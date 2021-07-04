c8d69628de33d299b012a87e3279b55f217fef23
Cristian-J <59663672+Cristian-J@users.noreply.github.com>
2021-07-03 20:54:52 -0400

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch corvus_joan-userdebug
-export TZ=Asia/Dhaka
+export TZ=Asia/Dhaka #put before last build command
 make corvus
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
