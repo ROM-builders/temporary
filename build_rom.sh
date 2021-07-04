370220e33e4157b5ee48b8b737d84952714f6d5b
Cristian-J <59663672+Cristian-J@users.noreply.github.com>
2021-07-03 23:02:10 -0400

    last try for today

--- build_rom.sh
+++ build_rom.sh
@@ -4,10 +4,9 @@ git clone https://github.com/Cristian-J/local-manifest.git --depth 1 -b main .re
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-# 
-source build/envsetup.sh
+. build/envsetup.sh
 lunch corvus_joan-userdebug
-mka bacon
+make corvus
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
