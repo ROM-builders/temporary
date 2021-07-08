d775ebd18ebc252a6fe98403bfc542573a2a0798
Prashant Shukla <76157174+Prashant1873@users.noreply.github.com>
2021-07-09 03:55:24 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -11,5 +11,5 @@ mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 # rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P 
-rclone copy out/target/product/jasmine_sprout/*20210709*OFFICIAL-GApps.zip cirrus:jasmine_sprout -P  
+rclone copy out/target/product/jasmine_sprout/*20210708*OFFICIAL-GApps.zip cirrus:jasmine_sprout -P  
 
