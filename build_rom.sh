cb20dda9a39b064ad017d19ab083d77bea0fa2f8
Prashant Meena <71384284+Prashant-1695@users.noreply.github.com>
2021-07-09 10:14:13 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch nad_lavender-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true 
 export TZ=Asia/Dhaka #put before last build command
 mka nad
 
