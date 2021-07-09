f932474fb1090a7b7d5f373a7702f15bce3af9d7
Cristian-J <59663672+Cristian-J@users.noreply.github.com>
2021-07-09 14:29:34 -0400

    Fixed sync issue

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/DotOS/manifest.git -b d
 git clone https://github.com/Cristian-J/local-manifest.git --depth 1 -b dot .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom.
 source build/envsetup.sh
 lunch dot_joan-userdebug
 make bacon
