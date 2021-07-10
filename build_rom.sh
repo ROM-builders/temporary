ab6c65180091443ba869f58b52d0bc734792d648
Rishawn <70256146+Stealth1226@users.noreply.github.com>
2021-07-10 10:12:42 +0530

    Lighthouse h850 July update
    upload command was changed for this build only as last build's upload failed due to faulty json upload command so remove wasnt run.

--- build_rom.sh
+++ build_rom.sh
@@ -11,5 +11,5 @@ mka lighthouse
 
 # upload rom
 # If you need to upload json/multiple files too then put like this 'rclone copy out/target/product/mido/*.zip cirrus:mido -P  rclone copy out/target/product/mido/*.zip.json cirrus:mido -P'
-rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P 
-rclone copy out/target/product/h850/*json cirrus:h850 -P
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P || rclone copy out/target/product/h850/*20210710*zip cirrus:h850 -P
+rclone copy out/target/product/h850/*20210710*json cirrus:h850 -P
