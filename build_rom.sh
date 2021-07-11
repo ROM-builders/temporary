2b929a9d2bfef2a4f71ca866fa98f4ce4f170494
Lucifer-morning-star-96 <87255315+Lucifer-morning-star-96@users.noreply.github.com>
2021-07-11 19:08:33 +0530

    Sync fixed

--- build_rom.sh
+++ build_rom.sh
@@ -8,7 +8,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 lunch corvus_lavender-userdebug
 export TZ=Asia/kolkata #put before last build command (Time)
 export SELINUX_IGNORE_NEVERALLOWS=true
-make corvus
+make corvus 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
