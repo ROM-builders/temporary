3c3fb39c1db1a17e1ca940fda6e71f2b7ac3c259
akira-vishal <vishal.rockstar7011@gmail.com>
2021-07-11 10:11:51 +0530

    aosp to syberia

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch aosp_whyred-user
+lunch syberia_whyred-user
 export TZ=Asia/Dhaka #put before last build command
 make syberia
 
