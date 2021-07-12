ae982d4de7c0dd7c9ad31091716807707441cdef
yograjsingh-cmd <84258191+yograjsingh-cmd@users.noreply.github.com>
2021-07-12 20:04:22 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch derp_Z01R-user
+lunch derp_Z01R-userdebug
 
 mka derp
 
