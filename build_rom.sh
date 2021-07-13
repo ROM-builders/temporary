5e57b8d7575bc3efa4a8eb289d29b33d4d17aa74
Prashant Shukla <76157174+Prashant1873@users.noreply.github.com>
2021-07-13 16:17:19 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Havoc-OS/android_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Prashant1873/manifest_personal.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/Prashant1873/manifest_personal.git --depth 1 -b caf .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -12,4 +12,5 @@ export TZ=Asia/Kolkata #put before last build command
 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
-rclone copy out/target/product/jasmine_sprout/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+# rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P 
+rclone copy out/target/product/jasmine_sprout/*20210713*jasmine_sprout*.zip cirrus:jasmine_sprout -P 
