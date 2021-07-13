336141c94bfd33ab8d3b139d2f7bb021193704b2
yograjsingh-cmd <84258191+yograjsingh-cmd@users.noreply.github.com>
2021-07-13 13:10:21 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,9 +5,9 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch derp_Z01R-userdebug
-
-mka derp
+lunch corvus_Z01R-userdebug
+export TZ=Asia/Kolkata #put before last build command (setting timezone)
+make corvus
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
