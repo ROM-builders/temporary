c03c9a4e0ad75cb764364e0a6dda467b1087a544
Sandeep Chahal <sandeeps567ingh@gmail.com>
2021-07-13 22:36:42 +0530

    Build v8.0 xr

--- build_rom.sh
+++ build_rom.sh
@@ -8,5 +8,5 @@ source build/envsetup.sh
 lunch xtended_ysl-userdebug
 mka xtended
 
-# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+# upload rom (if you don't need to upload multiple files, then you don't need to edit next line).
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
