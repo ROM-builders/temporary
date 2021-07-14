391c0bd1f062596d80cfd08c67f441b53d65a4df
Lucifer-morning-star-96 <87255315+Lucifer-morning-star-96@users.noreply.github.com>
2021-07-14 20:36:07 +0530

    User build

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
   
 # build rom 
 . build/envsetup.sh
-lunch corvus_lavender-userdebug
+lunch corvus_lavender-user
 export TZ=Asia/kolkata #put before last build command (Time)
 make corvus 
 
