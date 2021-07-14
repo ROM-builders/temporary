160d7c6236ab0a44a972fdceea6c0f1327a6449c
Prashant Shukla <76157174+Prashant1873@users.noreply.github.com>
2021-07-14 08:30:57 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,11 +5,10 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-export ALLOW_MISSING_DEPENDENCIES=true
 lunch descendant_jasmine_sprout-userdebug
 export TZ=Asia/Kolkata #put before last build command
 mka descendant
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 # rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-rclone copy out/target/product/jasmine_sprout/*zip cirrus:jasmine_sprout -P
+rclone copy out/target/product/jasmine_sprout/20210714*jasmine_sprout*zip cirrus:jasmine_sprout -P
