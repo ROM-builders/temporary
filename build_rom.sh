95060853daa637c79ca0f10d3fadb1fc9b423321
Prashant Shukla <76157174+Prashant1873@users.noreply.github.com>
2021-07-17 02:31:18 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch cherish_jasmine_sprout-user
+lunch cherish_jasmine_sprout-userdebug
 export TZ=Asia/Kolkata 
 make bacon
 
