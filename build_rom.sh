b92931ead34a7c6f0bd6fd6c7dcc70dcd987f6ba
Yash Aggarwal <48090169+styloxyash@users.noreply.github.com>
2021-07-18 16:05:54 +0530

    Pixlish

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/styloxyash/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/styloxyash/Local-Manifests.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
-export TZ=Asia/Dhaka #put before last build command
+lunch aosp_lavender-userdebug
+export TZ=Asia/Kolkata #put before last build command
 mka derp
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
