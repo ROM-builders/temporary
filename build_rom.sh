4451191c6e8f08d3231c9a1898f8138e016ab030
Sarthak Roy <sarthakroy2002@gmail.com>
2021-07-18 23:27:30 +0530

    Start los build 1

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,15 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Realme-G70-Series/local_manifest.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/Realme-G70-Series/local_manifest.git --depth 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch lineage_RMX2020-userdebug
-mka bacon -j10
+export TZ=Asia/Dhaka #put before last build command
+export SKIP_ABI_CHECKS=true
+export SKIP_API_CHECKS=true
+mka bacon -j$(nproc --all) 
 
-# upload rom
-rclone copy out/target/product/RMX2020/*UNOFFICIAL*.zip cirrus:RMX2020 -P
+# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
