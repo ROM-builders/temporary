e9b667e752f4439b2df1eae3ca330c94a115cb46
Yash Aggarwal <48090169+styloxyash@users.noreply.github.com>
2021-07-18 17:09:32 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/styloxyash/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/styloxyash/android_.repo_local_manifests.git --depth 1 .repo/local_manifests -b pixlish
+git clone https://github.com/styloxyash/local_manifests --depth 1 .repo/local_manifests -b pixlish
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
