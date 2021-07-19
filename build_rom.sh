be36018ce6504d9c426dbf61098cfd19830d1e5f
linux <bdiez19@gmail.com>
2021-07-19 14:11:45 -0300

    Update

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,18 @@
+#!/bin/bash
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo init --depth=1 -u git://github.com/CherishOS/android_manifest.git -b eleven 
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+
+# Clone TREE
+git clone https://github.com/linuxmobile/android_device_xiaomi_chiron-common -b cherish-11 device/xiaomi/msm8998-common
+git clone https://github.com/linuxmobile/android_device_xiaomi_chiron -b cherish-11 device/xiaomi/chiron
+git clone https://github.com/linuxmobile/vendor-xiaomi -b cherish-11 vendor/xiaomi
+git clone https://github.com/linuxmobile/android_kernel_xiaomi_msm8998 -b cherish-11 kernel/xiaomi/msm8998
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
-export TZ=Asia/Dhaka #put before last build command
-mka derp
+lunch cherish_chiron-userdebug
+make bacon -j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
