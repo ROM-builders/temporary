d7ad2eae4827d74cd4f191124af972a7274499dd
VERY HAPPY <77049889+ajitlenka30@users.noreply.github.com>
2021-07-20 17:56:11 +0530

    Tissot cherish os V2.7.5

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,9 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/CherishOS/android_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/ajitlenka30/local_manifest.git --depth 1 -b 11 .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
+
 
 # build rom
 source build/envsetup.sh
