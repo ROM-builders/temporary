ba6d3fa35b256090c713b93ef6547eed59dcf4e0
Sarthak Roy <sarthakroy2002@gmail.com>
2021-07-21 15:42:54 +0530

    Fix sync and OSS Build

--- build_rom.sh
+++ build_rom.sh
@@ -9,7 +9,7 @@ lunch lineage_RMX2020-userdebug
 export TZ=Asia/Dhaka #put before last build command
 export SKIP_ABI_CHECKS=true
 export SKIP_API_CHECKS=true
-mka bacon -j$(nproc --all)  
+mka bacon -j$(nproc --all)   
 
-# upload rom (if you don't need to upload multiuple files, then you don't need to edit next yes line)
+# upload rom (if you don't need to upload multiuple files, then you don't need to edit next yeJs line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
