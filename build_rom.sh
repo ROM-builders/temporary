632ff6ac3c854e729677e47cf328399809348cda
Debayan Kar <69067988+kardebayan@users.noreply.github.com>
2021-07-23 01:29:05 +0530

    CPH1859: Change build command

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch potato_CPH1859-eng
 export TZ=Asia/Kolkata #put before last build command
-mka bacon -j$(nproc --all)
+brunch CPH1859
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
