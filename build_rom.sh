4a381ce5c04b9aa941a5d8a6385e380e69d02eff
xoo2001 <ekora2001@gmail.com>
2021-07-22 21:08:44 +0700

    update source

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/Los-FE/manifest.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/xoo2001/local_manifest.git --depth 1 -b lineage .repo/local_manifests
+git clone https://github.com/xoo2001/local_manifest.git --depth 1 -b losfe .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
