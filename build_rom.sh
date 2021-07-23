39e8d882eef771cbafc60eb30b51e63b84d01fe7
yograjsingh-cmd <84258191+yograjsingh-cmd@users.noreply.github.com>
2021-07-23 15:33:06 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,10 +1,10 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Corvus-R/android_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/yograjsingh-cmd/local_manifest.git --depth=1 -b main .repo/local_manifests
+git clone https://github.com/yograjsingh-cmd/local_manifest.git --depth=1 -b cvs .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+. build/envsetup.sh
 lunch corvus_Z01R-userdebug
 export TZ=Asia/Kolkata 
 make corvus
