1122599ba98c1a995966608adca43e2c1beef4ab
marsellinus <45103898+marsellinus@users.noreply.github.com>
2021-07-25 00:02:20 +0700

    Build: Initial build ArrowOS mojito with July SPL

--- build_rom.sh
+++ build_rom.sh
@@ -7,9 +7,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 . build/envsetup.sh
 lunch arrow_mojito-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
-export TZ=Asia/Jakarta #put before last build command
+export TZ=Asia/Jakarta # put before last build command
 m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-
