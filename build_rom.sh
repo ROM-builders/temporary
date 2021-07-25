fa023ea5f582bb0afbc6baafd035dc6e6d88dee0
arshinragh <79363212+arshinragh@users.noreply.github.com>
2021-07-25 10:03:20 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch arrow_tiare-userdebug
+lunch arrow_tiare-eng
 export TZ=Asia/kolkata #put before last build command
 m bacon
 
