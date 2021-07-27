9d04a9eebd2e089ad132e84c3e192a906cd5ac05
D34DPUL <77775456+D34DPUL@users.noreply.github.com>
2021-07-27 18:42:15 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/crdroidandroid/android.git -b 11.0
+repo init --depth=1 --no-repo-verify -u git://github.com/crdroidandroid/android.git -b 11.0 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/D34DPUL/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
