25b97cf86fcb29bdba06b0bd26f44975acb251bc
ᏗᏁᎧᏁᎩᎷᎧᏬᏕ <87353286+AnonyUnknownTg@users.noreply.github.com>
2021-07-27 10:32:47 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/Spark-Rom/manifest.git -b fire -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/AnonyUnknownTg/local_manifest.git --depth 1 -b spark .repo/local_manifests
+git clone https://github.com/cArN4gEisDeD/local_manifest.git --depth 1 -b spark .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
