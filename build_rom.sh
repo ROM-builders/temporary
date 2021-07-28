4b6d91acdfbee0e926dc4872c95cff899181bcf5
Aryan Karan <aryankaran28022004@gmail.com>
2021-07-28 12:15:10 +0530

    Intex Cloud Q11 initialized

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,26 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b cm-14.1 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/aryankaran/Intex_Cloud_Q11.git --depth 1 -b master .repo/local_manifests
+repo sync -c -j30 --force-sync --no-clone-bundle --no-tags || repo sync -c -j`expr 2 \* $(nproc --all)` --force-sync --no-clone-bundle --no-tags
+# Apply patches
+cd device/intex/Cloud_Q11/patches
+bash apply-patches.sh
+cd ../../../..
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
-export TZ=Asia/Dhaka #put before last build command
-mka derp
+export USE_CCACHE=1
+export TZ=Asia/Kolkata #put before last build command
+export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4096m"
 
-# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
-rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+./prebuilts/sdk/tools/jack-admin kill-server
+
+./prebuilts/sdk/tools/jack-admin start-server
+
+export LC_ALL=C
+
+brunch Cloud_Q11
+
+# upload rom
+export location="$(pwd)/out/target/product/Cloud_Q11/*Q11*.zip"
+curl --upload-file $location https://transfer.sh/$(basename $location)
