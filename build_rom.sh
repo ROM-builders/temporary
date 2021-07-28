cd5a99b13631f02d4a6f7238aa36bcd382322c07
Debayan Kar <69067988+kardebayan@users.noreply.github.com>
2021-07-28 13:33:29 +0530

    Build userdebug

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_CPH1859-eng
+lunch lineage_CPH1859-userdebug
 mka bacon -j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
