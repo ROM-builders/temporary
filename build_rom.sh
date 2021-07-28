a112eabf9405ab7ab126102059292339f6ae015b
xoo2001 <ekora2001@gmail.com>
2021-07-28 07:51:48 +0700

    fix path

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/NusantaraProject-ROM/android_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/xoo2001/local_manifest.git --depth 1 -b nad .repo/local_manifests
+git clone https://github.com/xoo2001/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
