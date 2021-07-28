1a736f799ffcc1e3e0abdc4c1434cba9c1be3fb3
D34DPUL <77775456+D34DPUL@users.noreply.github.com>
2021-07-28 16:04:53 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 export TZ=Asia/Kolkata #put before last build command
-blissify bliss_liber-userdebug
+blissify -g bliss_liber-userdebug
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 #rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
