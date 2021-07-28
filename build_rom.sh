1af4e8f54dec6933c77a3ecb1ed9e83882bcdc90
Aryan Karan <aryankaran28022004@gmail.com>
2021-07-28 16:43:31 +0530

    Lineage FE for onclite

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b cm-14.1 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/aryankaran/Intex_Cloud_Q11.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/Los-FE/manifest.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/aryankaran/local_manifests.git --depth 1 -b Lineage_FE .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 export TZ=Asia/Kolkata #put before last build command
-brunch Cloud_Q11
+brunch lineage_onclite-user
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
-rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+location=out/target/product/onclite/*onclite*.zip
+echo $location
+curl --upload-file $location https://transfer.sh/$(basename $location)
