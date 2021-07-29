ca16b4bcfd79655650ff4cbda24598a439c9c660
xoo2001 <ekora2001@gmail.com>
2021-07-30 00:00:47 +0700

    fix local

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u http://github.com/Los-FE/manifest.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Infinix-MT6768-LDM/local_manifests.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/Infinix-MT6768-LDM/local_manifests.git --depth 1 -b master .repo/local_manifests  
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
