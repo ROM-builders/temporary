db7f59eb98d0cef8e5524756751ba3319298c421
Rajkale99 <kaleraj.rk@gmail.com>
2021-07-29 15:37:45 +0530

    Update blobs

--- build_rom.sh
+++ build_rom.sh
@@ -9,6 +9,5 @@ export WITH_GAPPS=true
 lunch legion_miatoll-userdebug
 export TZ=Asia/kolkata #put before last build command
 make legion
-
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
