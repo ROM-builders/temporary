66e3c7714321c9f405182381f42ae95ef230ce58
xoo2001 <ekora2001@gmail.com>
2021-07-30 12:15:27 +0700

    fluid-11

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Los-FE/manifest.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/xoo2001/local_manifest.git --depth 1 -b lineage .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Fluid/manifest.git -b fluid-11 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/xoo2001/local_manifest.git --depth 1 -b fluid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_ido-userdebug
+lunch fluid_ido-userdebug
 export TZ=Asia/Jakarta
 mka bacon
 
