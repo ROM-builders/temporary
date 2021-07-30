b1a20feaaa299ca5b2109428f60621fd19b37486
ronin787 <82747624+ronin787@users.noreply.github.com>
2021-07-30 21:53:46 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch evolution_X00TD-userdebug
+lunch evolution_X00TD-eng
 export TZ=Asia/kolkata #put before last build command
 mka evolution
 
