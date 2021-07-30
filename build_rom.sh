7e6e7676be7e2bd8f7af367332ddf792788a88b2
Lokesh Vazirani <68844395+radeon790@users.noreply.github.com>
2021-07-30 10:52:28 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 
 # build rom
-source build/envsetup.sh
+. build/envsetup.sh
 lunch aosp_RMX1831-userdebug
 mka bacon 
 
