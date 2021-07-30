985c45d3921d24a12762775329e8c56d1382dbbd
Eko R.A <82219556+xoo2001@users.noreply.github.com>
2021-07-30 13:16:15 +0700

    fix cpu spesifik

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Project-Fluid/manifes
 git clone https://github.com/xoo2001/local_manifest.git --depth 1 -b fluid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom 
 source build/envsetup.sh
 lunch fluid_ido-userdebug
 export TZ=Asia/Jakarta
