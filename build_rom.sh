fca5d8b25a160386e42a7d29ea1ee5730e1c0efe
zhenyolka <zhenyolka@gmail.com>
2021-07-31 09:47:12 +0300

    Convert to build for pyxis

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/kardebayan/local_manifests.git --depth 1 -b r11 .repo/local_manifests
+git clone https://github.com/zhenyolka/android_manifest.git --depth 1 -b lineage-18.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_CPH1859-userdebug
+lunch lineage_pyxis-userdebug
 mka bacon -j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
