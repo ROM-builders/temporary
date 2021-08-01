db1e69783b0b2a30ee428196aed7082730f3a5eb
Hotorou <37245252+ZunayedDihan@users.noreply.github.com>
2021-08-01 21:48:05 +0600

    derp

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init -u https://github.com/AICP/platform_manifest.git -b r11.1 --depth=1 --no-repo-verify -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Hotorou/local.git --depth 1 -b aicp .repo/local_manifests
+repo init -u git://github.com/DerpFest-11/manifest.git --depth=1 -b 11 --no-repo-verify -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/Hotorou/local.git --depth 1 -b derp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aicp_daisy-user
+lunch derp_daisy-user
 export TZ=Asia/Dhaka #put before last build command
-brunch daisy
+mka derp
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
