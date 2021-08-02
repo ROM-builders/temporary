0cfbac78b4bebd32b1174c72a34646099fea17ed
ItoRenz <86091226+ItoRenz@users.noreply.github.com>
2021-08-02 23:29:06 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 -u git://github.com/NusantaraProject-ROM/android_manifest -b 10 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/ItoRenz/manifest.git --depth 1 -b main .repo/manifest
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_rolex-userdebug
-export TZ=Asia/Dhaka #put before last build command
-mka derp
+lunch nad_rolex-userdebug
+export TZ=Asia/Indonesia #put before last build command
+mka nad
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
