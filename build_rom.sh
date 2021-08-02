0f0a462c317c8b7b3a4f577c903795129ad782c4
xoo2001 <ekora2001@gmail.com>
2021-08-02 15:21:25 +0700

    fix booted

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/NusantaraProject-ROM/android_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Infinix-MT6768-LDM/local_manifests.git --depth 1 -b nad-11 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ProjectSakura/android.git -b 11 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/xoo2001/local_manifest.git --depth 1 -b lineage .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch nad_X682C-userdebug
+lunch lineage_ido-userdebug
 export TZ=Asia/Jakarta
-mka nad
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
