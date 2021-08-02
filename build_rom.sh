762eff92ee05bf86d358e9a9d5ebab23ce8966f3
ItoRenz <86091226+ItoRenz@users.noreply.github.com>
2021-08-02 18:57:28 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/ItoRenz/manifest.git --depth 1 -b main .repo/manifest
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
+lunch lineage_rolex-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
