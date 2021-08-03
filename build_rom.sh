ee6e451ff4629dbbf9ae6cb43188abf0aa152ea0
モリトキ | #BreakTheCode <61047914+Moritokii@users.noreply.github.com>
2021-08-03 12:38:27 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/DotOS/manifest.git -b dot11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Moritokii/local_manifest.git --depth 1 -b dot .repo/local_manifests
+git clone https://github.com/Moritokii/local_manifest.git --depth 1 -b dot-r .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
