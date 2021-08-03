59eda1c51a19cc18a2c6b370bc5b703010204855
Riot-X <abhiragh0@gmail.com>
2021-08-03 08:48:54 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,6 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
+export TARGET_GAPPS_ARCH=arm64
 lunch exthm_olivewood-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
