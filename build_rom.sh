27b3c7fa6b3f51757adf83b28a7d9b43c37835d1
ItoRenz <86091226+ItoRenz@users.noreply.github.com>
2021-08-03 07:15:12 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 -u git://github.com/NusantaraProject-ROM/android_manifest.git -b 10 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/ItoRenz/manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/ItoRenz/manifest.git --depth 1 -b main .repo/manifest
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
