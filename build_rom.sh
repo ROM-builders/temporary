d008d61395c43a5690a3c249e307447c46705c67
RONIN808 <71311970+RONIN808@users.noreply.github.com>
2021-08-04 14:34:07 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch nad_X00T-eng
+lunch nad_X00T-userdebug
 export TZ=Asia/Kolkata #put before last build command
 mka derp
 
