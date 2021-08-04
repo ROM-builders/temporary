dbda5e005273976fcace2c75e72334fa1a2404c5
Rajkale99 <kaleraj.rk@gmail.com>
2021-08-05 03:57:42 +0530

    Fix Fc

--- build_rom.sh
+++ build_rom.sh
@@ -2,6 +2,9 @@
 repo init --depth=1 --no-repo-verify -u git://github.com/Project-LegionOS/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/rajkale99/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+rm -rf packages/apps/LegionSettings && git clone -b 3.12 https://github.com/Project-LegionOS/packages_apps_LegionSettings packages/apps/LegionSettings
+rm -rf frameworks/base && git clone -b 3.12 https://github.com/Project-LegionOS/frameworks_base frameworks/base
+rm -rf vendor/LegionParts && git clone -b 3.12 https://github.com/Project-LegionOS/vendor_LegionParts vendor/LegionParts
 
 # build rom
 source build/envsetup.sh
