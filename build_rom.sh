23a8307c3aef1ad2d50ff1eff76b40864465eefe
ItoRenz <86091226+ItoRenz@users.noreply.github.com>
2021-08-04 13:04:59 +0700

    RessurectionRemix-Riva

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/ProjectSakura/android.git -b 11 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/ResurrectionRemix/platfrom_manifest.git -b Q -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/ItoRenz/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch lineage_rolex-userdebug
-export TZ=Asia/Jakarta #put before last build command
+lunch rr_riva-userdebug
+export TZ=Asia/Jakarta
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
