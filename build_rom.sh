7cc903b8d8c4f0e2b790679dbba74e77384bdebf
Harshhaa Reddy <79190327+harshhaareddy@users.noreply.github.com>
2021-08-05 14:21:24 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_X00TD-userdeb
+lunch lineage_X00TD-userdebug
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
