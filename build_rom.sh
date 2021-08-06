82d0b53a0fdceb3a992f396c8cd61b70e77415a4
xoo2001 <ekora2001@gmail.com>
2021-08-06 08:28:39 +0000

    fix hospot, fix root, fix boot

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Los-FE/manifest.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/xoo2001/local_manifest.git --depth 1 -b los-r2 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Havoc-OS/manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/xoo2001/local_manifest.git --depth 1 -b havoc .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_wt88047x-userdebug
+lunch havoc_wt88047x-userdebug
 export TZ=Asia/Jakarta
 mka bacon
 
