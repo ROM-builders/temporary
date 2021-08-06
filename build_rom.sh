a51eb32be81b8d8fb1c5f192c37f83c5c38ec0e9
Rishawn <70256146+Stealth1226@users.noreply.github.com>
2021-08-06 11:33:25 +0530

    lighthouse h850 august update

--- build_rom.sh
+++ build_rom.sh
@@ -6,10 +6,11 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lighthouse_h850-user
+export SKIP_ABI_CHECKS=true
 mka lighthouse
 
 
 # upload rom
 # If you need to upload json/multiple files too then put like this 'rclone copy out/target/product/mido/*.zip cirrus:mido -P  rclone copy out/target/product/mido/*.zip.json cirrus:mido -P'
-rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P || rclone copy out/target/product/h850/*20210710*zip cirrus:h850 -P
-rclone copy out/target/product/h850/*20210710*json cirrus:h850 -P
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+rclone copy out/target/product/h850/*h850*json cirrus:h850 -P
