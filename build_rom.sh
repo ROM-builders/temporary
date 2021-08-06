642d62c5dd6288b8d0d2170a60a5f0a111158785
xoo2001 <ekora2001@gmail.com>
2021-08-06 23:34:57 +0700

    fix boot, fix root, fix hospot

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Havoc-OS/android_mani
 git clone https://github.com/xoo2001/local_manifest.git --depth 1 -b havoc .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom 
 source build/envsetup.sh
 lunch havoc_wt88047x-userdebug
 export TZ=Asia/Jakarta
