df689444cbc6317c5c81c19555e0f4fe3f1c336a
zhenyolka <zhenyolka@gmail.com>
2021-08-07 09:55:57 +0300

    Fix build error

--- build_rom.sh
+++ build_rom.sh
@@ -2,6 +2,10 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/PotatoProject/manifest -b dumaloo-release -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/zhenyolka/android_manifest.git --depth 1 -b lineage-18.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+rm -rf hardware/qcom-caf/sdm845/display
+rm -rf hardware/qcom-caf/sdm845/media
+git clone https://github.com/KudProject/hardware_qcom_display-caf.git -b lineage-18.1/sdm845 hardware/qcom-caf/sdm845/display
+git clone https://github.com/KudProject/hardware_qcom_media-caf.git -b lineage-18.1/sdm845 hardware/qcom-caf/sdm845/media
 
 # build rom
 source build/envsetup.sh
