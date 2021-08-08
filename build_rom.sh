418368d878bb6da475d7c374afcd79638e734645
Mr.FF <40267376+ferry666@users.noreply.github.com>
2021-08-08 09:53:50 +0700

    Update build_rom.sh
    Initial build hycon #2

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/HyconOS/manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/HyconOS/manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/ferry666/local_manifests.git --depth 1 -b A11 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
