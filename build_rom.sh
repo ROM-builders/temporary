bba4a4ee83cc7756f918f4a41a27ad8f3251bf43
ItoRenz <86091226+ItoRenz@users.noreply.github.com>
2021-08-10 01:14:55 +0700

    Trigger: fix sync error

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_rolex-user
+lunch lineage_rolex-userdebug
 export TZ=Asia/Jakarta
 mka bacon
 
