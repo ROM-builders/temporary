d589ae8e53cd5f0be473a7002cac6aea92a3981f
zhantech <hifzhan41@gmail.com>
2021-08-11 04:07:10 +0700

    Derpfest Mojito

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,12 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/zhantech/local_manifest.git --depth 1 -b derp-mojito .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
+lunch derp_mojito-userdebug
+export KBUILD_BUILD_USER="ZHANtech™"; export KBUILD_BUILD_HOST="batikholic"
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
