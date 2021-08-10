9073d507534f62243e4b86173a4db28d3efee3b3
Pratyay <58354320+popoASM@users.noreply.github.com>
2021-08-10 18:55:59 +0530

    rr

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/AospExtended/manifest.git -b 10.x -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/ResurrectionRemix/platform_manifest.git -b Q -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/10or-E/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_holland1-userdebug
+lunch rr_holland1-userdebug
 export TZ=Asia/Kolkata #put before last build command
 make bacon 
 
