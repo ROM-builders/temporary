815e4fd111bf0d22e58c0c96b50bdfb8c7056682
Razs <73821326+rasenss@users.noreply.github.com>
2021-08-10 18:21:58 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch dot_ginkgo-user
+lunch dotfe_ginkgo-userdebug
 export TZ=Asia/Jakarta #put before last build command
 mka bacon
 
