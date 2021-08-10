8b5cbc0371fe2f6fb85e173c8a77e051941d66a5
ZHANtech™ <hifzhan41@gmail.com>
2021-08-10 15:43:03 +0700

    Fix Branch Error

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/zhantech/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/zhantech/Local-Manifests.git --depth 1 -b 11 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
