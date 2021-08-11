316bf522f6e54e746d0e6fd899af0e599276b421
Hotorou <37245252+ZunayedDihan@users.noreply.github.com>
2021-08-12 00:22:02 +0600

    userdebug bess

--- build_rom.sh
+++ build_rom.sh
@@ -5,8 +5,9 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch aicp_daisy-user
+lunch aicp_daisy-userdebug
 export TZ=Asia/Dhaka #put before last build command
+export SELINUX_IGNORE_NEVERALLOWS=true
 brunch daisy
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
