87fdd7aeacdf2a8bd3498ba2ee4a4a8bc3098339
Razs <73821326+rasenss@users.noreply.github.com>
2021-08-11 22:37:33 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -8,7 +8,7 @@ source build/envsetup.sh
 lunch bliss_ginkgo-userdebug
 blissify -g ginkgo
 export TZ=Asia/Jakarta #put before last build command
-mka bacon
+
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
