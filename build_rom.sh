cd104eafcf542b55ab1a405ec923edd821668f37
Sarthak Roy <sarthakroy2002@gmail.com>
2021-08-11 13:14:03 +0530

    Update the entire audio stack

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Realme-G70-Series/local_manifest -b rmui2 --depth 1 .repo/local_manifests
+git clone https://github.com/sarthakroy2002/local_manifest --depth 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -11,5 +11,5 @@ export SKIP_ABI_CHECKS=true
 export SKIP_API_CHECKS=true
 mka bacon -j$(nproc --all)   
 
-# upload   rom
+# upload  rom
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
