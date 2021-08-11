72f41a96914d3db44305069664f1aa7d3ed5fe11
Razs <73821326+rasenss@users.noreply.github.com>
2021-08-11 12:48:39 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/dotOS-FE/manifest.git -b dot11 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/PixelExperience/manifest.git -b eleven-plus -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/rasenss/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch dotfe_ginkgo-userdebug
+lunch aosp_ginkgo-userdebug
 export TZ=Asia/Jakarta #put before last build command
 mka bacon
 
