3b6d9497ac91b46a4b6ee9d66d8e88c9edd3f111
ZHANtech™ <hifzhan41@gmail.com>
2021-08-11 19:02:26 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,4 @@
 # sync rom
-rm -rf vendor/xiaomi/mojito
 repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/zhantech/local_manifest.git --depth 1 -b derp-mojito .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
