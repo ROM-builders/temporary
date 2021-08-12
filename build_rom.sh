bdd98257d70105fa6a198cbc9fdabde3bb87b3a7
LynzhX <58753821+LynzhX@users.noreply.github.com>
2021-08-12 17:21:56 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch floko_RMX2001-user
+lunch floko_RMX2001-userdebug
 export TZ=Asia/Jakarta #put before last build command
 export ALLOW_MISSING_DEPENDENCIES=true
 mka bacon
