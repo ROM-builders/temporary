1d6ad4cddc5f410cfeb3dc09809aae2b7828f999
LynzhX <88780052+LynzhX@users.noreply.github.com>
2021-08-12 10:11:01 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/FlokoROM/manifesto.git -b 11.0 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/LynzhX/local_manifest.git --depth 1 -b main .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -j32 -c --no-clone-bundle --no-tags
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch floko_RMX2001-userdebug
-export TZ=Asia/Dhaka #put before last build command
+export TZ=Asia/Jakarta #put before last build command
 export ALLOW_MISSING_DEPENDENCIES=true
 mka bacon
 
