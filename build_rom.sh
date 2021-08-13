3532d829eecdc15191d9ad8a94de93663a3d74c0
LynzhX <58753821+LynzhX@users.noreply.github.com>
2021-08-13 10:34:04 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch floko_RMX2001-userdebug
 export TZ=Asia/Jakarta #put before last build command
 export ALLOW_MISSING_DEPENDENCIES=true
 brunch RMX2001 2>&1 | tee ~/log/floko_$(date '+%Y%m%d_%H-%M-%S').log
