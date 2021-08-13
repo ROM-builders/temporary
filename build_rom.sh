c54c9fa6d20d1ca0152e0144df21e38b9348e598
Jegan B <87298700+Jamesgosling2004@users.noreply.github.com>
2021-08-13 17:08:05 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/StyxProject/manifest.git -b R -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Jamesgosling2004/local-Manifests.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/Jamesgosling2004/local_manifesto.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch styx_RMX1831-userdebug
+lunch styx_RMX1821-userdebug
 export ALLOW_MISSING_DEPENDENCIES = true #put before last build command
 m styx-ota
 
