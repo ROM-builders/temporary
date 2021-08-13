67db30f59c0b7800d2d94c6c140677537bb600bf
officialputuid <officialputuid@hack.id>
2021-08-13 18:00:30 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch RMX2001-userdebug
-export TZ=Asia/Makassar #put before last build command
+export TZ=Asia/Makassar
 make -j24 otapackage
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
