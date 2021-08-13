4aec6ab197ac86a3c55d0f38270ba0d2aee07e59
Jegan B <87298700+Jamesgosling2004@users.noreply.github.com>
2021-08-13 15:26:03 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/StyxProject/manifest -b R -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/StyxProject/manifest.git -b R -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/Jamesgosling2004/local-Manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
