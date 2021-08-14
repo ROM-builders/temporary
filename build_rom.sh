e3dc9bb7b68021f0a9e5e2def7bab5c8e770ad76
VERY HAPPY <77049889+ajitlenka30@users.noreply.github.com>
2021-08-14 06:32:25 +0530

    Used new hals

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/CherishOS/android_manifest.git -b test -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/ajitlenka30/local_manifest.git --depth 1 -b 11 .repo/local_manifests
+git clone https://github.com/ajitlenka30/local_manifest.git --depth 1 -b 11.2 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 
