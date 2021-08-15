6f283464321d422943db9a5580f0f46b410dce48
VERY HAPPY <77049889+ajitlenka30@users.noreply.github.com>
2021-08-15 14:33:03 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -8,7 +8,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch bootleg_tissot-userdebug
-mka bacon -j$(nproc --all)
+mka bacon -j8
 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
