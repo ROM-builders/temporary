9175724c7c6d8a346be1d87cc9ac87515388c4ca
ItoRenz <86091226+ItoRenz@users.noreply.github.com>
2021-08-15 13:24:53 +0700

    Initial Build

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/ProjectSakura/android.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/ItoRenz/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/ProjectRadiant/manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/ItoRenz/local_manifest.git --depth 1 -b radiant .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch lineage_rolex-userdebug
+lunch radiant_rolex-userdebug
 export TZ=Asia/Jakarta
 mka bacon
 
