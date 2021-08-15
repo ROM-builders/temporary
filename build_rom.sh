1547a6390817860aa0f7968f6a2eac8cbbe88dc5
Jegan B <87298700+Jamesgosling2004@users.noreply.github.com>
2021-08-15 11:34:46 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/Project-Fluid/manifest.git -b fluid-11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/Jamesgosling2004/local_manifest.git --depth 1 -b main .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync --force-sync --no-tags --no-clone-bundle -j$(nproc --all)
 
 # build rom
 source build/envsetup.sh
