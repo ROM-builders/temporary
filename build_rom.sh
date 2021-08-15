06a9b93c9b3b01b44febf2173e8561b0e006bed1
VERY HAPPY <77049889+ajitlenka30@users.noreply.github.com>
2021-08-15 14:32:35 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/BootleggersROM/manifest.git -b queso -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/ajitlenka30/local_manifest.git --depth 1 -b ten .repo/local_manifests
-repo sync --current-branch --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 
 
