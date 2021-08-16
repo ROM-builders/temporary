1879236f64f13cc377c8230b99809e7b52667596
Jegan B <87298700+Jamesgosling2004@users.noreply.github.com>
2021-08-16 23:21:58 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch legion_RMX1831-userdebug
+lunch legion_RMX1831-user
 export TZ=Asia/Dhaka #put before last build command
 make legion
 
