6e3310fd54bb8f8adeb4c12d652b8eac3c17ea74
Rishawn <70256146+Stealth1226@users.noreply.github.com>
2021-08-16 10:44:54 +0530

    lighthouse h850 august update

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lighthouse_h850-user
-export SKIP_ABI_CHECKS=true
 mka lighthouse
 
 
