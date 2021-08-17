25c1efcdba1111b8ce526c51f0b40ea0f8d0b185
Jegan B <87298700+Jamesgosling2004@users.noreply.github.com>
2021-08-17 09:43:36 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch nad_RMX1831-user
+lunch nad_RMX1831-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka nad
 
