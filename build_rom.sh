7eae70ae6394c2a7394f51737dc105d41b92517a
Orangeskai <42552126+Orangeskai@users.noreply.github.com>
2021-08-17 23:07:10 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 -u git://github.com/DotOS/manifest.git -b dot11 -g default,-
 git clone https://github.com/Orangeskai/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc)
 
-# build rom 
+# build rom
 source build/envsetup.sh
 lunch dot_RMX2001-userdebug
 export TZ=Asia/Jakarta #put before last build comman
