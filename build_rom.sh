3e3896e312b2a25cc7a3141491f16289eb58364d
Orangeskai <42552126+Orangeskai@users.noreply.github.com>
2021-08-18 19:18:12 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 -u https://github.com/PixelPlusUI/manifest -b 11 -g default,
 git clone https://github.com/Orangeskai/local_manifest.git --depth 1 -b ppui .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom 
+# build rom testing again 
 source build/envsetup.sh
 lunch aosp_RMX2001-userdebug
 export TZ=Asia/Jakarta #put before last build comman
