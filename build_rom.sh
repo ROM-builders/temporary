13d489d1decc3e3077635b8f9752c42a7f494aa4
Jegan B <87298700+Jamesgosling2004@users.noreply.github.com>
2021-08-18 16:38:10 +0530

    Nusantara bringuo

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 #sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/NusantaraProject-ROM/android_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Jamesgosling2004/local-Manifests.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/Jamesgosling2004/local-manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch nad_RMX1831-user
+lunch nad_RMX1831-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka nad
 
