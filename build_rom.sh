62c006642260797b97191e593eff7418095ff409
Indra DP <53829264+indraadp@users.noreply.github.com>
2021-08-18 10:55:06 +0700

    Build 2: SuperiorOS Xcalibur juice

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/SuperiorOS/manifest.git
 git clone https://github.com/indraadp/local_manifest --depth 1 -b superior .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom (build 2nd)
 source build/envsetup.sh
 lunch superior_juice-userdebug
 export TZ=Asia/Jakarta #put before last build command
