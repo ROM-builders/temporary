a031c96c6161e84dc2bb0ec7512479d6f88e88c2
Indra DP <53829264+indraadp@users.noreply.github.com>
2021-08-18 19:59:51 +0700

    juice: Build ProjectRadiant

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/SuperiorOS/manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/indraadp/local_manifest --depth 1 -b superior .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ProjectRadiant/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/indraadp/local_manifest --depth 1 -b projectradiant .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch superior_juice-userdebug
+lunch radiant_juice-userdebug
 export TZ=Asia/Jakarta #put before last build command
 mka bacon
 
