7de1b118ea4c6bc483a181a1f3877aeb8f16e4e9
Khalakuzzaman Apon <61594531+Apon77@users.noreply.github.com>
2021-08-19 15:55:01 +0600

    Remove unremoved blissfy tag [skip ci]

--- build_rom.sh
+++ build_rom.sh
@@ -8,7 +8,6 @@ source build/envsetup.sh
 lunch derp_vayu-user
 export TZ=Asia/Dhaka #put before last build command
 mka derp
-blissify -g -c mido
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
