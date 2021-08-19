a38c3ee63d02d78df5f9573d5e16b72ee0bb90d1
Indra DP <53829264+indraadp@users.noreply.github.com>
2021-08-19 16:43:52 +0700

    [TestBuild 2] ProjectRadiant juice

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ProjectRadiant/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/indraadp/local_manifest --depth 1 -b projectradiant .repo/local_manifests
+git clone https://github.com/indraadp/local_manifest.git --depth 1 -b projectradiant .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
