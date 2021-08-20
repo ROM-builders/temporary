b48b2d1d59f535e90417faaeb3f343f8d0ac29b7
Rahul K <rahul.kantrapally@gmail.com>
2021-08-20 16:30:48 +0100

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch evolution-x_vince-userdebug
-export TZ=Asia/Dhaka #put before last build command
+export TZ=South Asia/Dhaka #put before last build command
 mka evolution-x
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
