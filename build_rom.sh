8b73a04686a0d7c03197c0953f1095f5a0ea493c
Rahul K <41164942+rk134@users.noreply.github.com>
2021-08-20 21:40:22 +0100

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch evolution_vince-userdebug
+lunch evolution_$vince-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka evolution
 
