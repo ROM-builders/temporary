068af0e42d25fb0e3ce0866f8397c1534557d382
Rahul K <41164942+rk134@users.noreply.github.com>
2021-08-20 17:15:41 +0100

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/Project-Fluid/manifest.git -b fluid-11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Jamesgosling2004/local_manifest.git --depth 1 -b main .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo init -u https://github.com/Evolution-X/manifest -b elle -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/rk134/local_manifests.git --depth 1 -b master .repo/local_manifests
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune
 
 # build rom
 source build/envsetup.sh
-lunch fluid_RMX1821-userdebug
+lunch evolution-x_vince-userdebug
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
