67d8ee2553f1329859f04eb7c4ed3da7f5c16f43
Rahul K <41164942+rk134@users.noreply.github.com>
2021-08-21 08:55:46 +0100

    Testing out changes on build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=TRUE
-lunch vince-userdebug
+lunch aosp_vince-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
