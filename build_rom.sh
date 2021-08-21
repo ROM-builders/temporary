aa18e320ba4a5e6e9ea610e039d13d509643444c
ItoRenz <86091226+ItoRenz@users.noreply.github.com>
2021-08-22 03:05:58 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/Spark-Rom/manifest.git -b fire -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/ItoRenz/local_manifest.git --depth 1 -b spark1 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/Stellar-OS/android.git -b stellar-S1 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/ItoRenz/local_manifest.git --depth 1 -b stellar .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch spark_rolex-user
+lunch stellar_rolex-userdebug
 export TZ=Asia/Jakarta
-mka spark
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
