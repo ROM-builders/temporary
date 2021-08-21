2982bcc00ca679938aa30ecf94d8dc408f812bd6
Rahul K <41164942+rk134@users.noreply.github.com>
2021-08-21 17:36:54 +0100

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,9 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-export ALLOW_MISSING_DEPENDENCIES=TRUE
 lunch evolution_vince-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
 mka evolution
 
