055755bc81e7fcb5ab86aa678a0c324c2fdc3c91
Naveen <71399635+Naveen-nk1@users.noreply.github.com>
2021-08-23 02:00:46 +0530

    X00TD-StyxProject-Naveen-nk1

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/ResurrectionRemix/platform_manifest.git -b Q -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Naveen-nk1/Local-Manifests.git --depth=1 -b RR .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo init --depth=1 --no-repo-verify -u https://github.com/StyxProject/manifest.git -b R -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/Naveen-nk1/local-manifest.git --depth 1 -b Styx .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -j$(nproc --all) --force-sync --no-tags --no-clone-bundle --prune --optimized-fetch
+
 
 # build rom
 source build/envsetup.sh
-lunch rr_X00T-userdebug
+lunch styx_X00TD-userdebug
 export TZ=Asia/Kolkata #put before last build command
-mka bacon
+m styx-ota
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
