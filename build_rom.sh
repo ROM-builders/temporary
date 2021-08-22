829d58d2b7b57064c53a58933465e2b26638cb98
Jegan B <87298700+Jamesgosling2004@users.noreply.github.com>
2021-08-22 13:45:51 +0530

    LTS Bringup

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/NusantaraProject-ROM/an
 git clone https://github.com/Jamesgosling2004/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build romm
 source build/envsetup.sh
 lunch nad_RMX1831-userdebug
 export TZ=Asia/Dhaka #put before last build command
