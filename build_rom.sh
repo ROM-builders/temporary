c59ea92f0ddbcccf78ad8f051db15d38728ce8aa
Sarthak Roy <sarthakroy2002@gmail.com>
2021-08-25 00:31:41 +0530

    RMX2020 AEE blobs updated
    * test booting

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -
 git clone https://github.com/sarthakroy2002/local_manifest --depth 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build  rom
 source build/envsetup.sh
 lunch lineage_RMX2020-userdebug
 export TZ=Asia/Dhaka #put before last build command
