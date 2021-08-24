39c4832b319d876b90b321f8bbdf6deed204d697
Indra DP <53829264+indraadp@users.noreply.github.com>
2021-08-24 08:09:35 +0700

    [Build 3] PixysOS vanilla juice

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/indraadp/local_manifest.git --depth 1 -b pixys .rep
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+. build/envsetup.sh
 lunch pixys_juice-userdebug
 export TZ=Asia/Jakarta #put before last build command
 make pixys
