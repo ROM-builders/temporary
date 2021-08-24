545d42ae08def749b695f6688d5ea808d2977bd3
Anupam kushwaha <kanupam500@gmail.com>
2021-08-24 23:27:36 +0530

    RMX2030-CrDroid-kanup4m
    CrDroid RMX2030

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-17.1 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/crdroidandroid/android.git -b 11.0 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/kanup4m/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
+lunch rmx2030
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
