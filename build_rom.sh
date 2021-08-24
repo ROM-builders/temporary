589a89e692d6ca3fe853da91eb389ae3770f2bc9
Anupam kushwaha <kanupam500@gmail.com>
2021-08-24 22:21:00 +0530

    RMX2030-Lineage-kanup4m
    Rom Build for RMX2030

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,8 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-17.1 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/kanup4m/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
 
 # build rom
 source build/envsetup.sh
