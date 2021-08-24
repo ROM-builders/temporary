4bf22dd25f9aac6407fbeceafa439f628dad601c
Soo-Hwan Na <69458352+roynatech2544@users.noreply.github.com>
2021-08-24 11:32:53 +0900

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch omni_a20e-eng
-export TZi=Asia/Dhaka #put before last build command
+export TZ=Asia/Dhaka #put before last build command
 mka recoveryimage
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
