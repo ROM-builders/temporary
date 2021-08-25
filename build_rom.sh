a0c71222727393ededf5aee016c256ac4044bc44
aldytos <89200661+basic-general@users.noreply.github.com>
2021-08-25 23:43:17 +0700

    ginkgo-xdroid-CAF-aldytos

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch xdroid_ginkgo-user
+lunch xdroid_ginkgo-userdebug
 export TZ=Asia/Jakarta #put before last build command
 make xd -j$(nproc --all)
 
