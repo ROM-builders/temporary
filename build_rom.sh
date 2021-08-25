69b0eab3f03b2defc78c9e4eba7d23f02445de7b
ImFlynnn <76860156+ImFlynnn@users.noreply.github.com>
2021-08-25 13:42:06 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_m
 git clone https://github.com/ImFlynnn/local_manifest.git --depth 1 -b banana .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc)
 
-# build 
+# build rom
 . build/envsetup.sh
 lunch banana_RMX2001-userdebug
 export TZ=Asia/Jakarta #put before last build command
