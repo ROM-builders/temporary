378dedefe7a3806d2d3fe6f88ab801c8a51887d9
ImFlynnn <76860156+ImFlynnn@users.noreply.github.com>
2021-08-26 20:35:39 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/bananadroid/android_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/ImFlynnn/local_manifest.git --depth 1 -b banana .repo/local_manifests
+git clone https://github.com/ImFlynnn/local_manifest.git --depth 1 -b banana2 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc)
 
 # build rom
