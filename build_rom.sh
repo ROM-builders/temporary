00461ee4379e70f957d5196b77e0bc8eed875ed1
Pratyaksh Bharadwaj <54440925+rockstar5495@users.noreply.github.com>
2021-08-27 09:51:11 +0530

    mido-colt-rockstar5495
    Signed-off-by: rockstar5495 <pratyakshom.05@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/Colt-Enigma/platform_manifest.git -b c11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/rockstar5495/local_manifest.git --depth 1 -b main .repo/local_manifests
-repo sync --no-tags --no-clone-bundle --force-sync -c -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source . build/envsetup.sh
