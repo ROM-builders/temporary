2eaf460deeac4e5c098fadc9b7bb6b981632d6f8
Puneet Gautam <89017901+kryptoniteX@users.noreply.github.com>
2021-08-28 14:59:47 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -8,8 +8,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch aosp_X01BD-userdebug
 export HYCON_BUILD_TYPE=OFFICIAL #put before last build command
-export ALLOW_MISSING_DEPENDENCIES=true
-mka bacon
+mka bacon -jX
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
