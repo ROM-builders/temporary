321c7741713af5ee653562472de18e00785300fd
Soo-Hwan Na <69458352+roynatech2544@users.noreply.github.com>
2021-08-29 08:08:21 +0900

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch evolution_a20-user
+lunch evolution_a20e-user
 export TZ=Asia/Dhaka #put before last build command
 mka evolution
 
