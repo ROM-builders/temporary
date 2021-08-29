1d78eddd4a7688f1944221f453649b1c14ee8b32
Puneet Gautam <89017901+kryptoniteX@users.noreply.github.com>
2021-08-29 17:43:46 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,14 @@
 # sync rom
 
 repo init --depth=1 --no-repo-verify -u git://github.com/HyconOS/manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/kryptoniteX/local_manifest --depth 1 -b main .repo/local_manifest
+git clone https://github.com/kryptoniteX/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
 lunch aosp_X01BD-userdebug
 export HYCON_BUILD_TYPE=OFFICIAL #put before last build command
-export TZ=Asia/Kolkata
+export ALLOW_MISSING_DEPENDENCIES=TRUE
 mka bacon
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
