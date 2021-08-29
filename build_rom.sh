e19943c38cab1e48cf861664cc9bf6b4b4c4b613
Anupam kushwaha <kanupam500@gmail.com>
2021-08-29 19:40:49 +0530

    r5x-DerpFest-kanup4m

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init -u git://github.com/DerpFest-11/manifest.git -b 11
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/kanup4m/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
