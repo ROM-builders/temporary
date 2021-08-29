38cca4522ac31e8082fb9497b9ed928c3681f96a
Puneet Gautam <89017901+kryptoniteX@users.noreply.github.com>
2021-08-30 01:49:30 +0530

    added support for multi devices

--- build_rom.sh
+++ build_rom.sh
@@ -11,4 +11,4 @@ export HYCON_BUILD_TYPE=OFFICIAL #put before last build command
 export ALLOW_MISSING_DEPENDENCIES=TRUE
 mka bacon
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
-rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+rclone copy out/target/product/X01BD/*.zip cirrus:X01BD -P && rclone copy out/target/product/X01BD/*.zip.json cirrus:X01BD -P
