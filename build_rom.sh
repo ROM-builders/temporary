a78df89500c99feb5e64fac392b4108fb3759d48
Puneet Gautam <89017901+kryptoniteX@users.noreply.github.com>
2021-08-30 09:51:43 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -8,6 +8,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 . build/envsetup.sh
 lunch aosp_X01BD-userdebug 
 export ALLOW_MISSING_DEPENDENCIES=TRUE #put before last build command
-mka bacon
+make bacon
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
