d24a223e006e9523e2202ac327ed6d082b0f32ee
Puneet Gautam <89017901+kryptoniteX@users.noreply.github.com>
2021-08-31 02:26:47 +0530

    clean uploads

--- build_rom.sh
+++ build_rom.sh
@@ -10,4 +10,4 @@ lunch revengeos_raphael-userdebug
 export ALLOW_MISSING_DEPENDENCIES=TRUE
 make bacon
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
-rclone copy out/target/product/raphael/*.zip cirrus:raphael -P && rclone copy out/target/product/raphael/*.zip.json cirrus:raphael -P
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
