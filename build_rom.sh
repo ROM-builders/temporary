3a8c4a9248288f950d85c209e986a6ada8259a1b
kanup4m <kanupam500@gmail.com>
2021-08-30 21:41:43 +0530

    Revert "Update Build.sh"
    This reverts commit da8519797c38281226c1cc8e292cb3e5f6ae3983.

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 -u https://github.com/Wave-Project/manifest -b r -g default,-device,-mips,-darwin,-notdefault
 
-git clone https://github.com/kanup4m/local_manifest.git -b main .repo/local_manifests
+git clone https://github.com/kanup4m/local_manifest.git -b wave .repo/local_manifests
 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) # repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
