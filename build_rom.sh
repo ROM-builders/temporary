faba5117295b0a76e9caf5edaa7374f6782378f3
Naveen <71399635+Naveen-nk1@users.noreply.github.com>
2021-08-31 21:42:46 +0530

    X00T-Exthmui-Naveen-nk1

--- build_rom.sh
+++ build_rom.sh
@@ -6,8 +6,9 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch exthm_X00T-userdebug
+export TARGET_GAPPS_ARCH=arm64
 export TZ=Asia/Kolkata #put before last build command
-mka der
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
