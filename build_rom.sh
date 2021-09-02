e6a4fafb888fe4a1f533882da81c0526819764a0
Chikal Ubaidillah <87101173+Badroel07@users.noreply.github.com>
2021-09-02 16:25:12 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -2,10 +2,8 @@
 repo init --depth=1 --no-repo-verify -u git://github.com/Project-LegionOS/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/Badroel07/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-#hal
-rm -rf hardware/qcom-caf/msm8996/display &&  git clone -b lineage-18.1-caf-msm8996 https://github.com/mi-sdm439/android_hardware_qcom_display.git hardware/qcom-caf/msm8996/display && rm -rf hardware/qcom-caf/msm8996/media && 
-git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-18.1-caf-msm8996  hardware/qcom-caf/msm8996/media && rm -rf hardware/qcom-caf/msm8996/audio && git clone https://github.com/LineageOS/android_hardware_qcom_audio --single-branch -b lineage-18.1-caf-msm8996 hardware/qcom-caf/msm8996/audio
-# build rom
+
+#build_rom
 . build/envsetup.sh
 lunch legion_olivewood-userdebug
 export TZ=Asia/Dhaka #put before last build comm
