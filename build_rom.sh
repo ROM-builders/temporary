27a8bac16a41c3012cfca1073cc9a4573a1a1dda
Chikal Ubaidillah <87101173+Badroel07@users.noreply.github.com>
2021-09-02 21:02:17 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/Project-LegionOS/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Badroel07/local_manifest.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/Badroel07/manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 #build_rom
