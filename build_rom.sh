91ae90e4c314838bbd8c75eca382ef8f58e42bdf
Puneet Gautam <89017901+kryptoniteX@users.noreply.github.com>
2021-09-02 20:39:57 +0530

    raphael_AEX_kryptoniteX

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,13 @@
 # sync rom
 
-repo init --depth=1 --no-repo-verify -u git://github.com/HyconOS/manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/kryptoniteX/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://https://github.com/AospExtended/manifest.git -b 11.x default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/kryptoniteX/local_manifest.git --depth 1 -b aex_raphael .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch aosp_X01BD-userdebug
-export HYCON_BUILD_TYPE=OFFICIAL #put before last build command
-export ALLOW_MISSING_DEPENDENCIES=TRUE
-mka bacon
+lunch aosp_raphael-userdebug
+export ALLOW_MISSING_DEPENDENCIES=TRUE #put before last build command
+make bacon
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
-rclone copy out/target/product/X01BD/*.zip cirrus:X01BD -P && rclone copy out/target/product/X01BD/*.zip.json cirrus:X01BD -P
+rclone copy out/target/product/raphael/*.zip cirrus:raphael -P && rclone copy out/target/product/raphael/*.zip.json cirrus:raphael -P
