5c8acbcb867e18e95b95a57cc4360626485449b2
PinCredible <86541065+PinCredible@users.noreply.github.com>
2021-09-04 20:27:13 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,9 +5,23 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
+export PEX_BUILD_TYPE=OFFICIAL
+export org.pex.build_maintainer="PinCredible"
 lunch aosp_tulip-user
 export TZ=Asia/Kolkata #put before last build command
 mka bacon -j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+
+#official stuff
+git clone https://github.com/PixelExtended/OTA
+python3 OTA/support/ota.py
+PinCredible
+tulip
+Redmi Note 6 pro
+google.com
+
+# OTA Zip & Upload
+zip -r OTA.zip OTA
+curl --upload-file OTA.zip
