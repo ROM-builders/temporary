ee3c690aeef7eb110bb5d32ee057133c152e8a6d
PinCredible <86541065+PinCredible@users.noreply.github.com>
2021-09-05 02:58:59 +0530

    Removed Make zip & added rclone to upload

--- build_rom.sh
+++ build_rom.sh
@@ -8,10 +8,11 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 lunch aosp_tulip-user
 export TZ=Asia/Kolkata #put before last build command
 mka bacon -j$(nproc --all)
+
+#OTA Json Generate
 python3 OTA/support/ota.py
-zip -r OTA.zip OTA
-curl --upload-file OTA.zip transfer.sh/OTA.zip
 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+rclone copy OTA/builds/tulip.json cirrus:tulip -P
