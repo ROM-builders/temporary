2d2fbcd63a3cdbe90d514c8e3f210d835f5ede19
kanup4m <kanupam500@gmail.com>
2021-09-04 23:17:48 +0530

    Gapps include

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,6 @@ repo init --depth=1 --no-repo-verify -u git://github.com/ForkLineageOS/android.g
 git clone https://github.com/kanup4m/local_manifest.git -b los .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-
 # build rom
 source build/envsetup.sh
 lunch lineage_r5x-userdebug
