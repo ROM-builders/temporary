dc24c31891a28a83ec5b6fd536e9fc0077786fa6
kanup4m <kanupam500@gmail.com>
2021-09-05 20:27:15 +0530

    Fp fix

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-make install clean
 lunch lineage_r5x-userdebug
 export TZ=Asia/Jakarta
 make bacon
