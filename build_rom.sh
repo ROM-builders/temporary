8b3731ca5897200a7e3256f6f2e8aaedb37f461d
ehtesham01 <ehteshammalik4@gmail.com>
2021-09-06 04:39:16 +0530

    export SELINUX_IGNORE_NEVERALLOWS=true

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_lavender-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true 
 export TZ=Asia/Kolkata #put before last build command
 mka bacon -j$(nproc --all)
 
