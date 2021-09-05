a6a1e3f4a85d251c8b8205ad590539c5ac7e9969
frost <50163698+fr05t77@users.noreply.github.com>
2021-09-05 21:22:57 +0700

    [Build 1] juice: DotOS-FE vanilla

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/Colt-Enigma/platform_manifest.git -b c11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/fr05t77/local_manifest.git --depth 1 -b colt .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/dotOS-FE/manifest.git -b dot11 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/fr05t77/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch colt_juice-userdebug
+lunch dotfe_juice-userdebug
 export TZ=Asia/Jakarta #put before last build command
-mka colt -j$(nproc --all)
+make bacon -j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
