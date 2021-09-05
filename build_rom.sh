1b8efa695660a840508cebd616dd06842549f899
ehtesham01 <ehteshammalik4@gmail.com>
2021-09-06 02:14:44 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch potato_lavender-userdebug
+lunch potato_lavender-user
 export TZ=Asia/Kolkata #put before last build command
 brunch lavender
 
