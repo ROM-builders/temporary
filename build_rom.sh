20ff4fe69776b522fedfd2691aa2ac627a378eb3
PinCredible <86541065+PinCredible@users.noreply.github.com>
2021-09-08 03:14:10 +0530

    Nogapps Build

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/PixelExtended/manifest.git -b ace -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/PinCredible/local-manifest.git --depth 1 -b pex .repo/local_manifests
+git clone https://github.com/PinCredible/local-manifest.git --depth 1 -b nogapps .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -9,10 +9,6 @@ lunch aosp_tulip-user
 export TZ=Asia/Kolkata #put before last build command
 mka bacon -j$(nproc --all)
 
-#OTA Json Generate
-python3 OTA/support/ota.py
-
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-rclone copy OTA/builds/tulip.json cirrus:tulip -P
