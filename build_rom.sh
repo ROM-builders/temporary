4f0891eac552a778d502dc0ae88032ee6877759c
Rabin <60746629+rabin-tech@users.noreply.github.com>
2021-09-07 13:45:00 +0545

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 export TZ=Asia/Kathmandu    #put before last build command
+lunch bliss_tulip-userdebug
 blissify -g tulip
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
