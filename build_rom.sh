c69f07e0e6e9c4fc9f8e39836b326de23c98e3d5
dimas-ady <dimasady123@outlook.co.id>
2021-09-07 21:36:59 +0700

    Fix build

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ReloadedOS/android_manifest.git -b q
- -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/ReloadedOS/android_manifest.git -b q -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/dimas-ady/local-manifest.git --depth 1 -b reloaded-q .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
