025d8f612a8d13f9e80450e8380454bd5c2fcc6c
ABHIXXY <88493880+ABHIXXY@users.noreply.github.com>
2021-09-07 08:13:28 +0000

    mojito havoc ABHIXXY
    build HAVOC for mojito

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/Havoc-OS/android_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/Havoc-OS/android_manifest.git --depth 1 -b eleven .repo/andriod_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
+lunch havoc_mojito-user
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
