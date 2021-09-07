84d609f3b68d013dd42f7fad4d0173e7020cdfdb
Rabin <60746629+rabin-tech@users.noreply.github.com>
2021-09-07 20:12:48 +0545

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-export TZ=Asia/Kathmandu     #put before last build command
+export TZ=Asia/Kathmandu      #put before last build command
 lunch bliss_tulip-userdebug
 blissify -g tulip
 
