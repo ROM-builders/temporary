f1e58dbd87c2abbb0bf9fc60b02259bcbc405e49
dimas-ady <dimasady123@outlook.co.id>
2021-09-08 05:13:11 +0700

    Fix typo in local manifest url

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ReloadedOS/android_manifest.git -b q -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/dimas-ady/local-manifest.git --depth 1 -b reloaded-q .repo/local_manifests
+git clone https://github.com/dimas-ady/local_manifest.git --depth 1 -b reloaded-q .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
