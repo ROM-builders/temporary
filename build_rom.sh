1640b92a29c1acc165e91f7823fbf74f0235aa33
Muhammad <77672824+muhammad23012009@users.noreply.github.com>
2021-09-08 06:31:02 +0000

    Adapt for dotOS

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,17 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/dotOS/manifest.git -b dot11 -g default,-device,-mips,-darwin,-notdefault
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+git clone https://github.com/muhammad23012009/android_device_motorola_hannah -b lineage-18.1 device/motorola/hannah
+git clone https://github.com/LineageOS/android_device_motorola_hannah-common -b lineage-18.1 device/motorola/hannah-common
+git clone https://github.com/lineageos/android_device_motorola_msm8937-common -b lineage-18.1 device/motorola/msm8937-common
+git clone https://github.com/lineageOS/android_kernel_motorola_msm8953 -b lineage-18.1 kernel/motorola/msm8953
+git clone https://github.com/themuppets/proprietary_vendor_motorola vendor/motorola
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
+lunch dot_hannah-userdebug
 export TZ=Asia/Dhaka #put before last build command
-mka derp
+m bacon -j24
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
