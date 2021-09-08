1b35b529e803626d27a7efd50220b7fd8bc1542f
Kakplis <88788478+Kakplis@users.noreply.github.com>
2021-09-09 02:00:42 +0700

    Selinux bacon

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_X00TD-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
 export LINEAGE_VERSION_APPEND_TIME_OF_DAY=true
 export SKIP_ABI_CHECKS=true
 export SKIP_API_CHECKS=true
