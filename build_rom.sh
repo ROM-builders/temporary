88998c3e0318dcc620d33ff89f087e2a5a44a886
dimas-ady <dimasady123@outlook.co.id>
2021-09-08 07:22:03 +0700

    Build LineageOS 17.1

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ReloadedOS/android_manifest.git -b q -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/dimas-ady/local_manifest.git --depth 1 -b reloaded-q .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b q -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/dimas-ady/local_manifest.git --depth 1 -b lineage-17.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch lineage_X00T-userdebug
 export TZ=Asia/Dhaka #put before last build command
-make reloaded
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
