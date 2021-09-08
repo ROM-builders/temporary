c17afa49d65fb5c1fa6f7e4be1f050e6fd0f9770
Kakplis <88788478+Kakplis@users.noreply.github.com>
2021-09-08 18:46:39 +0700

    X00TD-lineage-Kakplis

--- build_rom.sh
+++ build_rom.sh
@@ -5,10 +5,11 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-breakfast X00TD
-export TZ=Asia/Dhaka #put before last build command
-chroot
-brunch X00TD
+lunch lineage_X00TD-userdebug
+export LINEAGE_VERSION_APPEND_TIME_OF_DAY=true
+export SKIP_ABI_CHECKS=true
+export SKIP_API_CHECKS=true
+mka bacon -j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
