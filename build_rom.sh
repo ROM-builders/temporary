220245229432ebb0e116755230eae538eb8dac0d
ehtesham01 <ehteshammalik4@gmail.com>
2021-09-08 06:59:10 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch nad_lavender-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true 
-export TZ=Asia/Kolkata #put before last build command
+export TZ=Asia/Kolkata ##put before last build command
 mka nad -j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
