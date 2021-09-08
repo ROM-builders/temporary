94d3aa61d94c4b4184bbf4333eeab4c8fe63c256
Kakplis <88788478+Kakplis@users.noreply.github.com>
2021-09-08 08:09:09 +0700

    X00TD-lineage-Kakplis

--- build_rom.sh
+++ build_rom.sh
@@ -5,9 +5,10 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage-X00TD-user
+lunch lineage-X00TD-userdebug
 export TZ=Asia/Dhaka #put before last build command
 chroot
 brunch bacon
+
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
