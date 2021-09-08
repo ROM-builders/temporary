dba0c1d305bc75d8bc2ccebab7a7d673feee2945
Kakplis <88788478+Kakplis@users.noreply.github.com>
2021-09-08 08:12:37 +0700

    X00TD-lineage-Kakplis

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage-X00TD-userdebug
+lunch lineage_X00TD-userdebug
 export TZ=Asia/Dhaka #put before last build command
 chroot
 brunch bacon
