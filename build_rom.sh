b027ec9dc2d2d651742e0e476193e99284941bb2
Sarthak Roy <sarthakroy2002@gmail.com>
2021-09-08 09:20:03 +0530

    RUI 2 OSS INITIAL BUILD

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/sarthakroy2002/local_manifest --depth 1 .repo/local_manifests
+git clone https://github.com/sarthakroy2002/local_manifest -b rui2-oss --depth 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
