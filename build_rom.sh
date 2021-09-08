e5f614638bb8decc5f8198ae3bf703d623600139
Kakplis <88788478+Kakplis@users.noreply.github.com>
2021-09-08 23:01:07 +0700

    X00TD-lineage-Kakplis

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_X00TD-userdebug
+lunch lineage_X00TD-user
 export LINEAGE_VERSION_APPEND_TIME_OF_DAY=true
 export SKIP_ABI_CHECKS=true
 export SKIP_API_CHECKS=true
