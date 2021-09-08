cdf7d0e6d212307528e5bcc0d51be18d545bb2b5
gattud <82766617+gattud@users.noreply.github.com>
2021-09-08 17:17:40 +0000

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_pine-user
+lunch lineage_pine-userr
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
