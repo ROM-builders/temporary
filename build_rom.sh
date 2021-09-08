158a222d7ca9bda074f44eee9c795234fec4997d
Kakplis <88788478+Kakplis@users.noreply.github.com>
2021-09-09 01:58:24 +0700

    Make bacon

--- build_rom.sh
+++ build_rom.sh
@@ -9,7 +9,7 @@ lunch lineage_X00TD-userdebug
 export LINEAGE_VERSION_APPEND_TIME_OF_DAY=true
 export SKIP_ABI_CHECKS=true
 export SKIP_API_CHECKS=true
-mka bacon -j$(nproc --all)
+make bacon -j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
