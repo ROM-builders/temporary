21f73aca423b80c8e487c33e663da3986edd9a8f
Dimas Adiyaksa <69880727+dimas-ady@users.noreply.github.com>
2021-09-10 06:06:45 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,17 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-17.1 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/dimas-ady/local_manifest.git --depth 1 -b lineage-17.1 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-15.1 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/dimas-ady/local_manifest.git --depth 1 -b lineage-15.1-microg .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
+cd frameworks/base
+git fetch "https://gerrit.omnirom.org/android_frameworks_base" refs/changes/29/36729/4 && git cherry-pick FETCH_HEAD
+pushd ../../packages/apps/PermissionController # for LineageOS, location may vary for different ROMs
+git fetch "https://gerrit.omnirom.org/android_packages_apps_PackageInstaller" refs/changes/30/36730/3 && git cherry-pick FETCH_HEAD
+wget https://github.com/microg/android_packages_apps_GmsCore/raw/master/patches/android_frameworks_base-O.patch
+patch -p1 < android_frameworks_base-P.patch
 source build/envsetup.sh
-lunch lineage_X00T-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
+lunch lineage-userdebug
 export TZ=Asia/Jakarta #put before last build command
 make bacon
 
