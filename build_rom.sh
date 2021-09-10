4b0a241bbd47e4a0c8c905055d079168a0c7817c
Chikal Ubaidillah <87101173+Badroel07@users.noreply.github.com>
2021-09-10 22:57:11 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/Evolution-X/manifest.git -b elle -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Badroel07/manifest.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b elle -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/Badroel07/manifest --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 #build_rom
