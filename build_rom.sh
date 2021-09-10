b739cf0119cad29560f501b81d17d72ee9e906c1
mangaters12 <89984784+mangaters12@users.noreply.github.com>
2021-09-11 04:54:36 +0700

    test_xdroid
    BuildTest_Xdroid

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,17 @@
 # sync rom
-repo init -u https://github.com/xdroid-CAF/xd_manifest -b eleven
+repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init -u https://github.com/xdroid-CAF/xd_manifest -b eleven default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/xdroid-CAF/xd_manifest.git
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
 # build rom
 source build/envsetup.sh
 lunch derp_lavender-user
-export TZ=Asia/Jakarta #put before last build command
+lunch xdroid_lavender-userdebug
+export TZ=Asia/Dhaka #put before last build command
 mka derp
+make xdroid
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
