a6a41c54bdba9a3443dd1a953ca67447d1b7707b
Jegan B <87298700+Jamesgosling2004@users.noreply.github.com>
2021-09-10 11:04:35 +0530

    Initial Bringup

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/Project-Nova-UI/Manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/Project-Nova-UI/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/Jamesgosling2004/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
