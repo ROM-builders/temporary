ba49fec53d7a94f4caea462ad1e45518932fd746
Mr_Purple_666 <78635632+MrPurple666@users.noreply.github.com>
2021-09-10 19:42:11 -0300

    tulip-rr-MrPurple666

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/ResurrectionRemix/platform_manifest.git -b Q -g default,-device,-mips,-darwin,-notdefault
 
-git clone https://github.com/MrPurple666/local-manifest.git --depth=1 -b main .repo/local_manifests
+git clone https://github.com/MrPurple666/local_manifest.git --depth=1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
