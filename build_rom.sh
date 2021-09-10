13ce481e86788b7b5ae667e2b79c9b96ef4457c3
iAboothahir <65064906+iAboothahir@users.noreply.github.com>
2021-09-10 18:50:38 +0530

    For aex11

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/syberia-project/manifest.git -b 11.0 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Rombuilding-X00TD/local-manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/STRK-ND/local-manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom syberia
 
 source build/envsetup.sh
 
-lunch syberia_X00TD-userdebug
+lunch aosp_X00TD-userdebug
 export TZ=Asia/Dhaka #put before last build command
-mka bacon
+m aex
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
