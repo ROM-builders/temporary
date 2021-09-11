8ff30820dc6c401e5f8f6f3762b154df621eaa4f
Trần Nguyễn Tuấn Anh <43093213+dopaemon@users.noreply.github.com>
2021-09-11 12:14:05 +0700

    LineageOS 17.1 - On7xelte

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init -q --no-repo-verify --depth=1 -u https://github.com/LineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
+repo init -q --no-repo-verify --depth=1 -u https://github.com/LineageOS/android.git -b lineage-17.1 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/halium-exynos/local_manifest.git --depth 1 -b lineage-17.1 .repo/local_manifests
 repo sync -v -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # build rom
 source build/envsetup.sh
-lunch lineage_whyred-userdebug
+lunch lineage_on7xelte-userdebug
 export TZ=Asia/Ho_Chi_Minh #put before last build command
 mka bacon -j$(nproc --all)
 
