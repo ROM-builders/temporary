d39f0d16e51d4fdd7b661fbe24809b41478f1d48
Dimas Adiyaksa <69880727+dimas-ady@users.noreply.github.com>
2021-09-11 10:03:27 +0700

    No trace

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-15.1 --trace -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-15.1 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/dimas-ady/local_manifest.git --depth 1 -b lineage-15.1-microg .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
