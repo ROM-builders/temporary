b3040a9effdae48f6461e61f6bda06ac66daabb2
Dimas Adiyaksa <69880727+dimas-ady@users.noreply.github.com>
2021-09-11 16:59:14 +0700

    Trigger build

--- build_rom.sh
+++ build_rom.sh
@@ -3,6 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git
 git clone https://github.com/dimas-ady/local_manifest.git --depth 1 -b lineage-15.1-microg .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+
 # build rom
 source build/envsetup.sh
 export TZ=Asia/Jakarta #put before last build command
