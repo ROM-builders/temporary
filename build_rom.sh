cea2c789a01a77d3bc08ca3416dc3fd9d680da4f
ZeroToHero <89984784+mangaters12@users.noreply.github.com>
2021-09-11 12:04:32 +0700

    Colt_OS
    COLT ENIGMA OS UN-OFFCIAL-TEST

--- build_rom.sh
+++ build_rom.sh
@@ -1,17 +1,15 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
-repo init -u https://github.com/xdroid-CAF/xd_manifest -b eleven default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/xdroid-CAF/xd_manifest.git
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+repo init --depth=1 --no-repo-verify -u https://github.com/Colt-Enigma/platform_manifest -b c11 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/mangaters12/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_lavender-user
-lunch xdroid_lavender-userdebug
+lunch colt_lavender-userdebug
 export TZ=Asia/Dhaka #put before last build command
-mka derp
-make xdroid
+mka colt
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
