e957b686a6e4f6b32776cd7666bd4539054aca4e
Muhammad <77672824+muhammad23012009@users.noreply.github.com>
2021-09-11 05:54:54 +0000

    revert zip upload

--- build_rom.sh
+++ build_rom.sh
@@ -10,9 +10,4 @@ export TZ=Asia/Dhaka #put before last build command
 mka bacon -j24
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
-up(){
-	curl --upload-file $1 https://transfer.sh/$(basename $1); echo
-	# 14 days, 10 GB limit
-}
-
-up out/target/product/hannah/*.zip
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
