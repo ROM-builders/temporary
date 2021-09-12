9498c8b63a39864372b257d23193df283d539cd3
Dimas Adiyaksa <69880727+dimas-ady@users.noreply.github.com>
2021-09-12 14:58:31 +0700

    Install jdk & use brunch

--- build_rom.sh
+++ build_rom.sh
@@ -3,12 +3,12 @@ repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git
 git clone https://github.com/dimas-ady/local_manifest.git --depth 1 -b lineage-15.1-microg .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+apt install openjdk-8-jdk -y
 
 # build rom
 source build/envsetup.sh
-lunch lineage-userdebug
 export TZ=Asia/Jakarta #put before last build command
-make bacon
+brunch X00TD
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
