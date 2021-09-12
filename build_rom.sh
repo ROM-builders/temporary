917fb7e610cfdd8b62509050f15c74363ca32b57
Naveen <71399635+N4veenNK@users.noreply.github.com>
2021-09-12 20:41:02 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,11 +7,10 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 . build/envsetup.sh
 lunch exthm_X00T-userdebug
 export TZ=Aexportsia/Kolkata #put before last build command
-export TARexportGET_GAPPS_ARCH=arm64
+export TARGET_GAPPS_ARCH=arm64
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
-
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
 
 
