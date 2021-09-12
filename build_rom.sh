bb1ab69dbf8dbb225fddb09efb9dc9e8a0c06528
Dimas Adiyaksa <69880727+dimas-ady@users.noreply.github.com>
2021-09-13 05:34:33 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,9 +3,8 @@ repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git
 git clone https://github.com/dimas-ady/local_manifest.git --depth 1 -b lineage-15.1-microg .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-apt install openjdk-8-jdk -y
-
 # build rom
+apt install openjdk-8-jdk -y
 source build/envsetup.sh
 export TZ=Asia/Jakarta #put before last build command
 brunch X00TD
