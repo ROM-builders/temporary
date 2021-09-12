5d009d513a3544a6afcf3271b00c80d1b47417a3
Naveen <71399635+N4veenNK@users.noreply.github.com>
2021-09-12 07:11:47 +0530

    Update manifest

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/exthmui/android.git -b exthm-11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/N4veenNK/local_manifest.git --depth=1 -b exthmui .repo/local_manifests
+git clone https://github.com/N4veenNK/local_manifests.git --depth=1 -b exthmui .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
