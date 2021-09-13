019abcabd7625ada354d5a031fe401fda2920bb7
kanup4m <kanupam500@gmail.com>
2021-09-13 21:54:27 +0530

    CrDroid Update

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/ForkLineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
+repo init -u git://github.com/crdroidandroid/android.git -b 11.0
 git clone https://github.com/kanup4m/local_manifest.git -b los .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-export TARGET_FLOS=true
 lunch lineage_r5x-userdebug
 export TZ=Asia/Jakarta
-make bacon
-
+brunch r5x
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
