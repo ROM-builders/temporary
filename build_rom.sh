dca4b8a211e699b8e716b1a0abb70695fdf53a3a
Kakplis <88788478+Kakplis@users.noreply.github.com>
2021-09-14 05:14:51 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-16.0 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Kakplis/local_manifest.git --depth 1 -b main .repo/local_manifest
+git clone https://github.com/Kakplis/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j1
 
 # build rom
 source build/envsetup.sh
-breakfast X00TD
+lunch Lineage_X00TD-user
 export TZ=Asia/Dhaka #put before last build command
-brunch X00TD
+mka bacon -jx
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
