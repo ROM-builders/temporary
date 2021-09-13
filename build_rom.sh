ff37bcd94aed5cd08286cce136a6a423da2c8fc4
Rishawn <70256146+Stealth1226@users.noreply.github.com>
2021-09-13 13:41:39 +0530

    lighthouse h850 september update

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 -u git://github.com/lighthouse-os/manifest.git -b raft -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/Stealth1226/local_manifest --depth 1 -b h850 .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
