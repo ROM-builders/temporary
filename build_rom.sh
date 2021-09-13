98eba2c28014ee7e752306f11af59c4f1c4d3230
Chikal Ubaidillah <87101173+Badroel07@users.noreply.github.com>
2021-09-14 04:38:09 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/crdroidandroid/android.git -b 11.0 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Badroel07/manifest --depth 1 -b crdroid .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-LegionOS/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/Badroel07/manifest --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 #build_rom
 . build/envsetup.sh
+lunch legion_olivewood-userdebug
 export TZ=Asia/Dhaka #put before last build comm
-brunch olivewood
+make legion
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
