987e01212ddf982fe23cb39b6afb1775cfff07af
kanup4m <kanupam500@gmail.com>
2021-09-13 22:01:35 +0530

    CrDroid Update

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_r5x-userdebug
 export TZ=Asia/Jakarta
 brunch r5x
 
