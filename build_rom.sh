6853c992288a92bab370104edac6f88e692aab83
Chikal Ubaidillah <87101173+Badroel07@users.noreply.github.com>
2021-09-13 08:45:55 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/crdroidandroid/android.git -b 11.0 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Badroel07/manifest --depth 1 -b main .repo/local_manifests
+git clone https://github.com/Badroel07/manifest --depth 1 -b crdroid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 #build_rom
