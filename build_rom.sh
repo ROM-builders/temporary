f5f3e6de4974ea6e42c4d1ad64714fd1e03be869
Naveen <71399635+N4veenNK@users.noreply.github.com>
2021-09-14 07:36:31 +0530

    X00T-BiancaProject-N4veenNK

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/exthmui/android.git -b exthm-11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/N4veenNK/Local-Manifests.git --depth=1 -b exthm .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/BiancaProject/android.git -b 11 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/N4veenNK/Local-Manifests.git --depth=1 -b bianca .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-. build/envsetup.sh
-lunch exthm_X00T-userdebug
+source build/envsetup.sh
+lunch bianca_X00T-userdebug
 export TZ=Aexportsia/Kolkata #put before last build command
-export TARGET_GAPPS_ARCH=arm64
-mka bacon
+mka dudu
+
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
