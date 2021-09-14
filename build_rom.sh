974e3549f7e6427cf8ffac84e89a6f16e350a836
kanup4m <kanupam500@gmail.com>
2021-09-14 15:24:25 +0530

    Update device tree

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-export TZ=Asia/Jakarta
+export TZ=Asia/Kolkata #put before last build command
 brunch r5x
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
