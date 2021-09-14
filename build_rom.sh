334b8e927e9841af9a2e2d255787f6ceb8eaecb4
Chikal Ubaidillah <87101173+Badroel07@users.noreply.github.com>
2021-09-14 18:54:10 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/ProjectSakura/android.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Badroel07/manifest --depth 1 -b master .repo/local_manifests
+git clone https://github.com/Badroel07/manifest --depth 1 -b project_sakura .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 #build_rom
