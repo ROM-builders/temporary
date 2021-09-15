154888d082629cbf254be211f062d139eaf97026
hsx02 <72127977+hsx02@users.noreply.github.com>
2021-09-15 18:01:25 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch dot_pine-userdebug
-export TZ=Asia/Dhaka #put before last build command
+export SELINUX_IGNORE_NEVERALLOWS=true #put before last build command
 make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
