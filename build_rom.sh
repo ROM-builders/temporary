373d489d4e17dfcb9502d394671cbcfd04128353
Sumon SN <56505303+sumonsn@users.noreply.github.com>
2021-09-15 12:59:44 +0600

    Fixed link

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/ShapeShiftOS/android_manifest.git -b android_11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/SumonSN/Local-Manifests.git --depth 1 -b RMX3171 .repo/local_manifests
+git clone https://github.com/SumonSN/Local_manifest.git --depth 1 -b RMX3171 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 
