302b5a1248999cd890082b1913c6272f178820b1
VERY HAPPY <77049889+ajitlenka30@users.noreply.github.com>
2021-09-15 11:41:06 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/CherishOS/android_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/CherishOS/android_manifest.git -b test -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/ajitlenka30/local_manifest --depth 1 -b 11.2 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch cherish_tissot-userdebug
+lunch cherish_tissot-user
 export TZ=Asia/Dhaka #put before last build command
 mka bacon -j$(nproc --all)
 
