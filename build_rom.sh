5c40d272c9dc6226a0e7a39d1eaca9ec48cc4e40
VERY HAPPY <77049889+ajitlenka30@users.noreply.github.com>
2021-09-15 11:55:06 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch cherish_tissot-user
+lunch cherish_tissot-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon -j$(nproc --all)
 
