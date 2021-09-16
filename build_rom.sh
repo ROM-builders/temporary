2b8405f79f39f155d188b8e1734ae40a92fbf90b
Naveen <71399635+N4veenNK@users.noreply.github.com>
2021-09-16 17:14:57 +0530

    Fix build

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Project-LegionOS/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/Project-LegionOS/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/N4veenNK/Local-Manifests.git --depth 1 -b legion .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
