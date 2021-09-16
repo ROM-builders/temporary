50950e5aa01d018aed82e6aa86f118d6bf9e8a32
wHoEMi <ehteshammalik18998@gmail.com>
2021-09-16 09:02:30 +0530

    build apex vndk under vendor partition

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch conquer_lavender-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
-export TZ=Asia/Kolkata #put before last build command
+export TZ=Asia/Kolkata # put before last build command
 make carthage
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
