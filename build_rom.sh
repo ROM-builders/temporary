44059365f4034072f53ab60fd0032f584123c333
Naveen <71399635+N4veenNK@users.noreply.github.com>
2021-09-16 08:11:02 +0530

    X00TD-LegionOS-N4veenNK

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch legion_X00TD-userdebug
+export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Kolkata #put before last build command
 make legion
 
