33fabb89d9cabbd4a677d47d5b84a2f01fe7dc25
ASHISH11948 <90844148+ASHISH11948@users.noreply.github.com>
2021-09-17 10:57:15 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/Havoc-OS/android_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/ASHISH11948/-local_manifest- --depth 1 -b master .repo/local_manifests
+git clone https://github.com/ASHISH11948/local_manifest --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
 
 # build rom
