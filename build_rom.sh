a09a86327825f8425684fa0a31e82edbbc5aae06
deadline646 <lexaxx14@gmail.com>
2021-09-17 16:02:04 +0400

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/deadline646/local_manifest.git --depth 1 -b LOS .repo/local_manifests # update local_manifest.xml
+repo init --depth=1 --no-repo-verify -u https://github.com/descendant-xi/manifests.git -b eleven-staging -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/deadline646/local_manifest.git --depth 1 -b Descendant .repo/local_manifests # update local_manifest.xml
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # h.
 # build rom
 source build/envsetup.sh
-lunch lineage_juice-userdebug
+lunch descendant_juice-userdebug
 export TZ=Asia/Jakarta
-make bacon
+mka descendant
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
