1d1416a207f0ed8e6d6e5f3a0e6bc0c54fd3a622
deadline646 <lexaxx14@gmail.com>
2021-09-17 19:04:20 +0400

    pine: update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -
 git clone https://github.com/deadline646/local_manifest.git --depth 1 -b LOS-pine .repo/local_manifests # update local_manifest.xml
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# h.
+# h...
 # build rom
 source build/envsetup.sh
 lunch lineage_pine-userdebug
