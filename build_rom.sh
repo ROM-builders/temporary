a45bdea5f784de6f6dff0a5a2c004a209885762e
Sumon SN <56505303+sumonsn@users.noreply.github.com>
2021-09-17 07:56:43 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/ShapeShiftOS/android_manifest.git -b android_11 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://https://github.com/DotOS/manifest.git -b dot11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/SumonSN/Local_manifest.git --depth 1 -b RMX3171 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 
 # build rom
 source build/envsetup.sh
-lunch ssos_RMX3171-userdebug
+lunch dot_RMX3171-userdebug
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
