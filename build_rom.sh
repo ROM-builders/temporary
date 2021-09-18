b2e52dc2d108c9e32b9dc5b819ca4f6a849078dd
deadline646 <lexaxx14@gmail.com>
2021-09-18 17:00:30 +0400

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,9 @@
+# Sync Tree's
 repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/deadline646/local_manifest.git --depth 1 -b LOS .repo/local_manifests # update local_manifest.xml
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+# Build ROM
 source build/envsetup.sh
 lunch lineage_juice-userdebug
 export TZ=Asia/Jakarta
