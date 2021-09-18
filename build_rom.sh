3d19f9bf10a14256531402b4364039b9fc42830e
Subhendu-tech <55965922+Subhendu-tech@users.noreply.github.com>
2021-09-19 00:43:06 +0530

    trying with different trees

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_X01AD-userdebug
+lunch lineage_X01AD-user
 export TZ=Asia/Kolkata #put before last build command
 make lineage
 
