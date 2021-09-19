a4baa3486f4b20ba6df8c8459ec9a9a2532fcbd1
Subhendu-tech <55965922+Subhendu-tech@users.noreply.github.com>
2021-09-19 22:47:49 +0530

    Pixys tryout

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/PixysOS/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/Subhendu-tech/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 #build rom
 source build/envsetup.sh
-lunch aosp_X01AD-user
+lunch pixys_X01AD-user
 export TZ=Asia/Kolkata #put before last build command
 make X01AD
 
