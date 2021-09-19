431362b2b1e210b2890305ee9b488c23568939c1
a9ito <anggit.febriantoro@gmail.com>
2021-09-19 19:59:44 +0700

    ci: build sakura & trigger

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com:xdroid-CAF/xd_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com:ProjectSakura/android.git -b 11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/a9iworks/local_manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch xdroid_miatoll-user
+lunch lineage_miatoll-userdebug
 export TZ=Asia/Jakarta #put before last build command
-make xd -j$(nproc --all)
+mka bacon -j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
