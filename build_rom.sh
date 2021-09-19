ed0819e16527b5620bb3b1f424aa8a60cb849964
Subhendu-tech <55965922+Subhendu-tech@users.noreply.github.com>
2021-09-19 08:22:15 +0530

    Fix cloning

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manif
 git clone https://github.com/Subhendu-tech/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+#build rom
 source build/envsetup.sh
 lunch aosp_X01AD-user
 export TZ=Asia/Kolkata #put before last build command
