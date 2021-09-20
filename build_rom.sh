a099d783b44a02aea3d2293dd045433226a39127
Vishal Rajbhar <71400384+rvvishal@users.noreply.github.com>
2021-09-20 23:27:01 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_a50-userdebug
-export TZ=Asia/Dhaka #put before last build command
+export TZ=Asia/India
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
