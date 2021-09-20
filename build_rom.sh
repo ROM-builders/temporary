e8633d4138985d5ceea2fc30de1f3ca247a4423e
Egor Kostylev <76757246+nekoshirro@users.noreply.github.com>
2021-09-20 10:41:44 +0700

    build Nusantara Project ROM

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,17 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo init -u https://github.com/NusantaraProject-ROM/android_manifest -b 11
+repo sync -c --force-sync --no-tags --no-clone-bundle -j$(nproc --all)
+
+git clone https://github.com/nekoshirro/android_device_xiaomi_miatoll.git device/xiaomi/miatoll --depth 1
+git clone https://github.com/nekoshirro/android_device_xiaomi_sm6250-common.git device/xiaomi/sm6250-common --depth 1
+git clone https://github.com/nekoshirro/android_vendor_xiaomi_miatoll.git vendor/xiaomi/miatoll --depth 1
+git clone https://github.com/nekoshirro/android_vendor_xiaomi_sm6250-common.git vendor/xiaomi/sm6250-common --depth 1
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
-export TZ=Asia/Dhaka #put before last build command
-mka derp
+lunch nad_miatoll-userdebug
+export TZ=Asia/Jakarta #put before last build command
+mka nad
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
