b332facfbe6543aa8f3be41a2f8e72d79a3cba73
officialputuid <officialputuid@hack.id>
2021-09-21 06:03:18 +0800

    temporary: Triggers

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/StyxProject/manifest -b
 git clone https://github.com/officialputuid/RMX2001-StyxProject --depth 1 -b MASTER .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom (vanilla)
 source build/envsetup.sh
 lunch styx_RMX2001-user
 export TZ=Asia/Makassar #put before last build command
