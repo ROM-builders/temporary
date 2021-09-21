64e6b9625f7af505d70ff2f0c38fbed39cdfb900
Adam Jafarov <59512479+lazydev1852@users.noreply.github.com>
2021-09-21 21:07:34 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DotOS/manifest.git -b dot11 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/ProjectRadiant/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
 wget -S https://raw.githubusercontent.com/lazydev1852/LGV30Dot5.2/main/joan_build.xml -O .repo/local_manifests/joan_build.xml
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch dot_joan-userdebug
+lunch radiant_joan-userdebug
 export TZ=Europe/Moscow #put before last build command
 make bacon
 
