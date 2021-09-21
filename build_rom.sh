576c7d70ac37a5d0851ec760d2bf305734d2f7f0
a7imranjha <65493194+a7imranjha@users.noreply.github.com>
2021-09-21 13:48:01 +0500

    olivewood-PE-a7imranjha
    Build PE for olivewood

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/PixelExperience/manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/a7imranjha/local_manifest -depth 1 -b main .repo/local_manifests
+git clone https://github.com/a7imranjha/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
