61f28d556620e21a47f3f23f2bb523407193bead
ASHISH11948 <90844148+ASHISH11948@users.noreply.github.com>
2021-09-21 09:20:26 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,9 +5,9 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-
-export TZ=Asia/Dhaka #put before last build command
-brunch olivewood
+lunch corvus_olivewood-user
+export TZ=Asia/kolkata #put before last build command
+make corvus
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
