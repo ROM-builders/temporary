de483a3ff47bb273eb3bd604482556010e1025dd
a7imranjha <65493194+a7imranjha@users.noreply.github.com>
2021-09-22 00:17:12 +0500

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/PixelExperience/manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/a7imranjha/local_manifest -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
