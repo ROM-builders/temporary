319e0c51f87c2fdb5c3eb7abed2f00e8ef4a2298
Alexander Chycherin <77262770+SevralT@users.noreply.github.com>
2021-09-21 15:31:58 +0300

    build_rom: Build dotOS rom

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/dotOS/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/SevralT/local_manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
+lunch dot_fajita-user
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
