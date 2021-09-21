829aff683b9c9ad7b73503f46cf4aba099fb7e0d
a7imranjha <65493194+a7imranjha@users.noreply.github.com>
2021-09-21 11:42:32 +0500

    Olivewood-PE-a7imranjha
    PE for Olivewood

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/PixelExperience/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/a7imranjha/local_manifest -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
-export TZ=Asia/Dhaka #put before last build command
+lunch aosp_olivewood-user
+export TZ=Asia/Karachi #put before last build command
 mka derp
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
