18fc70abb5c1fbd867e56da3aae0608f0976b71b
Hitesoni :% <72127977+hsx02@users.noreply.github.com>
2021-09-22 16:16:26 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch nad_pine-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
 mka nad 
 
