59c500fbdb06b750f996961e6e4f45409d0989da
a7imranjha <65493194+a7imranjha@users.noreply.github.com>
2021-09-22 18:04:39 +0500

    privet-crdroid-fix

--- build_rom.sh
+++ build_rom.sh
@@ -4,10 +4,8 @@ git clone https://github.com/a7imranjha/local_manifest -b main .repo/local_manif
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
 #
 # build rom
-source build/envsetup.sh
-lunch lineage_olivewood-user
+. build/envsetup.sh
 export TZ=Asia/Karachi #put before last build command
-mka bacon
-
+brunch olivewood
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
