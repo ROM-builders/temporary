c4fb32ad51c700d81902c544cdf5c7efc18e4681
ASHISH11948 <90844148+ASHISH11948@users.noreply.github.com>
2021-09-22 07:05:10 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Corvus-R/android_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/ASHISH11948/local_manifest -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
 
 # build rom
 . build/envsetup.sh
-lunch corvus_olivewood-user
-export TZ=Asia/kolkata #put before last build command
-make corvus
-
+lunch arrow_olivewood-userdebug
+export TZ=Asia/Jakarta #put before last build command 
+m bacon
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
