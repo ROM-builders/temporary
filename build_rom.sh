97b937d9b896a8e56eb25b436c03be4e6bd43558
a7imranjha <65493194+a7imranjha@users.noreply.github.com>
2021-09-22 17:43:36 +0500

    privet-crdroid

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,7 @@
 repo init --depth=1 --no-repo-verify -u git://github.com/crdroidandroid/android.git -b 11.0 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/a7imranjha/local_manifest -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
-
+#
 # build rom
 source build/envsetup.sh
 lunch lineage_olivewood-user
