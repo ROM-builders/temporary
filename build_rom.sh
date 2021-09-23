175be8153e6f904ebd258ee75fa9a4fec6d64050
SumonSN <56505303+SumonSN@users.noreply.github.com>
2021-09-23 09:26:00 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -9,5 +9,5 @@ lunch aosp_RMX3171-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon -j$(nproc --all)
 
-# upload rom  (if you don't need to upload multiple files, then you don't need to edit next line)
+# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
