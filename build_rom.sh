4cba8f72b98a2d7826b9ee76360b791035e1e60f
nit-in <nit-in@users.noreply.github.com>
2021-09-24 02:55:56 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/Micromax-E6746/local_manifests.git --depth 1 -b tes
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom run
-#uadd definition
+#add definition
 source build/envsetup.sh
 lunch lineage_e6746-userdebug
 export TZ=Asia/Kolkata
