2ae1453a1031ef8c9de2aa4396929435b4138d4c
Ravithakral <91236805+Ravithakral@users.noreply.github.com>
2021-09-23 10:44:58 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b elle -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Ravithakral/local_manifest --depth 1 -b master .repo/local_manifests
+# git clone https://github.com/Ravithakral/local_manifest --depth 1 -b master .repo/local_manifests/evolution_manifest.xml
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
