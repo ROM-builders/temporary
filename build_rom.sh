eb0c513c6fc2654e4f9e1bc5dcfb608078c0812e
nit-in <nit_in@live.com>
2021-09-23 21:14:18 +0530

    use updated partition sizes
    Signed-off-by: nit-in <nit_in@live.com>

--- build_rom.sh
+++ build_rom.sh
@@ -4,6 +4,7 @@ git clone https://github.com/Micromax-E6746/local_manifests.git --depth 1 -b tes
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom run
+#use updated parition sizes
 source build/envsetup.sh
 lunch lineage_e6746-userdebug
 export TZ=Asia/Kolkata
