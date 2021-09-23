6d5aa9d1fb39b4c6416954b98d0732a95527343c
SumonSN <56505303+SumonSN@users.noreply.github.com>
2021-09-23 16:25:46 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/PixelExperience/manifest.git -b eleven-plus -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/SumonSN/local_manifest.git --depth 1 -b RMX3171 .repo/local_manifests
+git clone https://github.com/SumonSN/local_manifest --depth 1 -b RMX3171 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
