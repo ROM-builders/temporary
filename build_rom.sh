3b2d92ab19066881db4da7a651b2d3a3eb7956af
nit-in <nit_in@live.com>
2021-09-23 13:21:16 +0530

    use test2 branch
    Signed-off-by: nit-in <nit_in@live.com>

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Micromax-E6746/local_manifests.git --depth 1 -b test1 .repo/local_manifests
+git clone https://github.com/Micromax-E6746/local_manifests.git --depth 1 -b test2 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom run
