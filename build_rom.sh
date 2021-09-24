efb34025214f16f984444bb424acc6dfc45d7135
deadline646 <lexaxx14@gmail.com>
2021-09-24 17:59:03 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_juice-userdebug
 export TZ=Asia/Dhaka #put before last build command
 brunch juice
 
