755963ec579d576d5703f3fae70a7cecd91c36e0
Sanjeev <42514333+sanjeevstunner@users.noreply.github.com>
2021-09-24 06:51:30 +0530

    Updated device name

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch evolution_tulip-userdebug
+lunch evolution_twolip-userdebug
 export TZ=Asia/Kolkata #put before last build command
 mka evolution
 
