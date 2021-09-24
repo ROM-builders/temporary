25b5f71cfdb9678b9b851bb9e853b16f64d1c767
Sanjeev <42514333+sanjeevstunner@users.noreply.github.com>
2021-09-24 11:05:05 +0530

    Revert device name to tulip

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch evolution_twolip-userdebug
+lunch evolution_tulip-userdebug
 export TZ=Asia/Kolkata #put before last build command
 mka evolution
 
