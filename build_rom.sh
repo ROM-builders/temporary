86282f3b12f1dd1a713f5f2036a5dd33ed360baf
STRK-ND <raj15400881@gmail.com>
2021-09-24 13:13:01 +0530

    Removed common tree & fixed build file

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch aosp_X00TD-userdebug
+lunch aosp_X00T-userdebug
 export TZ=Asia/Dhaka #put before last build command
 m aex
 
