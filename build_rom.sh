e72800c685a63f489dc4aaae5f414253e0f89dc9
nit-in <nit-in@users.noreply.github.com>
2021-09-24 14:15:32 +0530

    drop thermals

--- build_rom.sh
+++ build_rom.sh
@@ -5,6 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom run
 #this build should be successful
+#drop thermals
 source build/envsetup.sh
 lunch lineage_e6746-userdebug
 export TZ=Asia/Kolkata
