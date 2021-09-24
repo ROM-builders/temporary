062afb708fbb604fa4fd66c801e68b0061b1da7e
Sanjeev <42514333+sanjeevstunner@users.noreply.github.com>
2021-09-24 17:44:05 +0530

    Updated source git
    Updated correct formatting for the source repository

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git:https://github.com/Evolution-X/manifest.git -b elle -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://Evolution-X/manifest.git -b elle -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/sanjeevstunner/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
