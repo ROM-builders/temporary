84dc4239befe9070af0b22ec239c0ac5dc7965a6
Alexander Chycherin <77262770+SevralT@users.noreply.github.com>
2021-09-24 21:32:30 +0300

    build_rom: Change local_manifest branch name

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/DotOS/manifest.git -b dot11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/SevralT/local_manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/SevralT/local_manifest.git --depth 1 -b dot11 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
