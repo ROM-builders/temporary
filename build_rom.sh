a3c7b163f717af8581fb586fa722d0296ddb1904
deadline646 <lexaxx14@gmail.com>
2021-09-25 12:36:33 +0300

    resync

--- build_rom.sh
+++ build_rom.sh
@@ -5,9 +5,9 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # Build ROM
 source build/envsetup.sh
-lunch rr_juice-userdebug
+lunch rr_citrus-userdebug
 export TZ=Asia/Dhaka #put before last build command
-mka bacon
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
