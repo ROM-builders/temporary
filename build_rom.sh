7c41a92d75ac26410c6fa2ee9116ea64b56be7db
Sanjeev <42514333+sanjeevstunner@users.noreply.github.com>
2021-09-25 16:33:31 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch aosp_tulip-userdebug
+lunch aosp_twolip-userdebug
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
