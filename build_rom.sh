203e004a2d1f0f0a1cd24fed9e23a54fd3cbfcc5
Sanjeev <42514333+sanjeevstunner@users.noreply.github.com>
2021-09-25 13:04:50 +0530

    Update manifest repo

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/PixelExperience/manifest.git -b eleven-plus -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/sanjeevstunner/local_manifest.git --depth 1 -b PE-11 .repo/local_manifests
+git clone https://github.com/sanjeevstunner/Manifest.git --depth 1 -b PE-11 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
