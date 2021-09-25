b2acf6b9cf7567d3098789871b6007fc94126dda
deadline646 <lexaxx14@gmail.com>
2021-09-25 05:17:04 +0400

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/deadline646/local_manifest.git --depth 1 -b LOS-kernel .repo/local_manifests
+git clone https://github.com/deadline646/local_manifest.git --depth 1 -b LOS .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
