5281a92a55bf3bdfdd352b26cd6db4b1ab1ab3cd
deadline646 <lexaxx14@gmail.com>
2021-09-25 10:25:11 +0400

    juice: fix

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
-# sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/hentaiOS/platform_manifest -b Rika -g default,-device,-mips,-darwin,-notdefault
+# Sync ROM
+repo init --depth=1 --no-repo-verify -u https://github.com/hentaiOS/platform_manifest -b Rika -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/deadline646/local_manifest.git --depth 1 -b hentaiOS .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# Build ROM
 source build/envsetup.sh
 lunch hentai_juice-userdebug
 export TZ=Asia/Dhaka #put before last build command
