559e092ad4e4c365d83d941a2dc65d60575a5145
deadline646 <lexaxx14@gmail.com>
2021-09-25 05:21:57 +0400

    add dt2w

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
-# sync rom
+# Sync ROM
 repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/deadline646/local_manifest.git --depth 1 -b LOS-juice .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# Build ROM
 source build/envsetup.sh
 lunch lineage_juice-userdebug
 export TZ=Asia/Dhaka #put before last build command
