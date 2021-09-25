5fbe3697b1eb96a9e7e2d08361f52b13d63cb8ae
deadline646 <lexaxx14@gmail.com>
2021-09-25 10:29:35 +0400

    trigger: resync

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # Sync ROM
-repo init --depth=1 --no-repo-verify -u https://github.com/hentaiOS/platform_manifest -b Rika -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/hentaiOS/platform_manifest -b Rika -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/deadline646/local_manifest.git --depth 1 -b hentaiOS .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
