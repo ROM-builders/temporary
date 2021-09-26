f666856619c4c72c992e8c9eacd9a68caae192e7
yograjsingh-cmd <84258191+yograjsingh-cmd@users.noreply.github.com>
2021-09-26 18:58:43 +0530

    Rippa fixd

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/P-404/platform_manifest -b 11 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/P-404/platform_manifest -b rippa -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/yograjsingh-cmd/local_manifest.git --depth=1 -b 404 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
