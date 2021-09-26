ace56954b047417ce7b9b31c1e48f255fa291a35
deadline646 <lexaxx14@gmail.com>
2021-09-26 09:10:35 +0300

    trigger: inital build RR

--- build_rom.sh
+++ build_rom.sh
@@ -4,11 +4,11 @@ git clone https://github.com/deadline646/local_manifest.git --depth 1 -b RR .rep
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+. build/envsetup.sh
 lunch rr_citrus-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
-export TZ=Asia/Jakarta #put before last build command
 mka bacon -j$(nproc --all)
+export SELINUX_IGNORE_NEVERALLOWS=true
+export TZ=Asia/Jakarta
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
