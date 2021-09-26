ae57c8902bc194c3c554cd5234cc56ec6be62f39
deadline646 <lexaxx14@gmail.com>
2021-09-26 08:59:57 +0300

    trigger: build RR

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # Sync ROM
-repo init --depth=1 --no-repo-verify -u git://github.com/ResurrectionRemix/platform_manifest -b Q -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/deadline646/local_manifest --depth 1 -b RR .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ResurrectionRemix/platform_manifest.git -b Q -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/deadline646/local_manifest.git --depth 1 -b RR .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # Build ROM
 source build/envsetup.sh
 lunch rr_citrus-userdebug
 export TZ=Asia/Dhaka #put before last build command
-make bacon
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
