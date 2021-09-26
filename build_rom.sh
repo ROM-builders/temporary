b5da020377c75550a0ab785959998aca341e8284
yograjsingh-cmd <84258191+yograjsingh-cmd@users.noreply.github.com>
2021-09-26 19:14:08 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/P-404/platform_manifest -b rippa -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/yograjsingh-cmd/local_manifest.git --depth=1 -b 404 .repo/local_manifests
+git clone https://github.com/yograjsingh-cmd/local_manifest.git --depth=1 -b p 404 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch p404_Z01R-userdebug
-export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Kolkata 
 make bacon
 
