bad4fc607ea9d720709e676ac06d290050d47e52
deadline646 <lexaxx14@gmail.com>
2021-09-26 09:06:53 +0300

    citrus: initial RR building setup.

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,14 @@
-# Sync ROM
+# sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ResurrectionRemix/platform_manifest.git -b Q -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/deadline646/local_manifest.git --depth 1 -b RR .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# Build ROM
+# build rom
 source build/envsetup.sh
 lunch rr_citrus-userdebug
-export TZ=Asia/Dhaka #put before last build command
-mka bacon
+export SELINUX_IGNORE_NEVERALLOWS=true
+export TZ=Asia/Jakarta #put before last build command
+mka bacon -j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
