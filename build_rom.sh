95740e15f4a45ea200edcec894cb193101354c3d
ben863 <r.budhiono@gmail.com>
2021-09-27 20:01:53 +0700

    remove some unused commit

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/cbendot/Local-Manifests.git --depth 1 -b main .repo
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-. build/envsetup.sh
+source build/envsetup.sh
 lunch rr_X00TD-userdebug
 export TZ=Asia/Jakarta #put before last build command
 mka bacon
