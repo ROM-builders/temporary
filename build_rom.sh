8fe8f3b7af69941a7d56ef869f4060cf72e1e241
ben863 <r.budhiono@gmail.com>
2021-09-27 22:03:53 +0700

    fixed local-manifest address

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/ResurrectionRemix/platform_manifest.git -b Q -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/cbendot/Local-Manifests.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/cbendot/Local-Manifests.git --depth 1 -b RR .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
