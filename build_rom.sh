3037febd247bafa2a3bac6c82fdabf715c3e817c
deadline646 <lexaxx14@gmail.com>
2021-09-27 21:45:41 +0400

    juice: resync

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
-# sync rom
+# Sync ROM
 repo init --depth=1 --no-repo-verify -u https://github.com/Project-Fluid/manifest.git -b fluid-11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/deadline646/local_manifest.git --depth 1 -b FluidOS .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# Build ROM
 . build/envsetup.sh
 lunch fluid_juice-userdebug
-export TZ=Asia/Dhaka #put before last build command
+export TZ=Asia/Dhaka
 mka bacon
 
-# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+# Upload ROM
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
