24018e927bbc6fe26a9b17fae188e7b67474efa2
deadline646 <lexaxx14@gmail.com>
2021-09-27 14:17:44 +0300

    trigger: edit command

--- build_rom.sh
+++ build_rom.sh
@@ -4,11 +4,11 @@ git clone https://github.com/deadline646/local_manifest.git --depth 1 -b RR .rep
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 #  lbuild rom
-. build/envsetup.sh
+source build/envsetup.sh
 lunch rr_citrus-userdebug
 export KBUILD_BUILD_USER="deadline646"; export KBUILD_BUILD_HOST="root"
 export TZ=Asia/Kolkata
-mka bacon
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
