0629a48367670bda90f2e56876d6fe2320bbe1b4
RishikRastogi <59395879+RishikRastogi@users.noreply.github.com>
2021-09-27 19:58:31 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -2,6 +2,11 @@
 repo init --depth=1 --no-repo-verify -u git://github.com/syberia-project/manifest.git -b 11.0 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/RishikRastogi/Local-Manifests.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+git clone https://github.com/syberia-project/platform_hardware_qcom_media.git --depth=1 -b 11.0-caf-sdm845 hardware/qcom-caf/sdm845/media
+git clone https://github.com/syberia-project/platform_hardware_qcom_audio.git --depth=1 -b 11.0-caf-sdm845 hardware/qcom-caf/sdm845/audio
+git clone https://github.com/syberia-project/platform_hardware_qcom_display.git --depth=1 -b 11.0-caf-sdm845 hardware/qcom-caf/sdm845/display
+rm -rf frameworks/av
+git clone https://github.com/RishikRastogi/platform_frameworks_av.git --depth=1 -b 11.0 frameworks/av
 
 # build rom
 source build/envsetup.sh
