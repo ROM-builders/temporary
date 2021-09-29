3f55baf8c62eeee5e26a78d556aaa3f9880c01c6
Kakplis <88788478+Kakplis@users.noreply.github.com>
2021-09-30 04:01:08 +0700

    Los

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/P-404/platform_manifest -b rippa -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Kakplis/local_manifest.git --depth 1 -b p-404 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-17.1 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/Kakplis/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch p404_X00T-eng
+lunch lineage_X00T-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
