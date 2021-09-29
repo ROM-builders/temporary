b296d51ee81522bda8798b81157b22ce37756967
EmadGr8 <91535171+EmadGr8@users.noreply.github.com>
2021-09-29 17:10:28 +0300

    Derp_Tulip

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/EmadGr8/Local-Manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
+lunch derp_tulip-user
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
