c01ca5aae1e6da921d3e78e148cc006ac632f35a
RishikRastogi <59395879+RishikRastogi@users.noreply.github.com>
2021-09-29 08:33:01 +0530

    Fix repo again

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/CherishOS/android_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/RishikRastogi/local_manifest.git --depth 1 -b cherish .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j1 --fail-fast
 
 # build rom
 source build/envsetup.sh
