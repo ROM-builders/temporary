528bc6a853fa36ccd1634011ff42a188b6c12aad
deadline646 <lexaxx14@gmail.com>
2021-09-29 13:37:16 +0300

    Re-run

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Project-Fluid/manifes
 git clone https://github.com/deadline646/local_manifest.git --depth 1 -b FluidOS .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# Build ROM
+# Build ROMs
 source build/envsetup.sh
 lunch fluid_juice-userdebug
 export TZ=Asia/Kolkata
