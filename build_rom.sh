45428cd5de4b8c4bdc3958d9ca2d0a079e918f4f
EmadGr8 <91535171+EmadGr8@users.noreply.github.com>
2021-09-29 08:47:07 +0300

    twolip los emad

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/Havoc-OS/android_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/EmadGr8/testbuilds/blob/main/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch havoc_twolip-user
+lunch los_twolip-user
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
