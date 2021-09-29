a7d6cc7ba846112e047a1a2c1f6c4875cdcdeade
Jagaddhita Jalu <88934027+jagaddhita@users.noreply.github.com>
2021-09-29 16:56:26 -0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch octavi_ulysse-userdebug
-export TZ=Asia/Jakarta #put before last build command
+export TZ=Asia/Jakarta
 brunch ulysse
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
