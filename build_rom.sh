9cfaeee311610269b75ada00dde3d6d01982c5bc
Kakplis <88788478+Kakplis@users.noreply.github.com>
2021-09-30 03:48:57 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch 404_X00T-userdebug
+lunch p404_X00T-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
