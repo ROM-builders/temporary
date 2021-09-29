45122299a82ee73fdda85a6334dae5f13e079ea2
Atharv <81741009+atharv2951@users.noreply.github.com>
2021-09-29 18:10:38 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init -u git.github.com/ArrowOS/android_manifest.git -b arrow-10.0 -g default,-device,-mips,-darwin,-notdefault
+repo init -u https://github.com/Corvus-Q/android_manifest.git -b 10 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/atharv2951/local_manifest --depth 1 -b main .repo/local_manifests
-repo sync --force-sync --no-clone-bundle -j$(nproc --all)
+repo sync -j$(nproc --all) --force-sync --no-tags --no-clone-bundle
 
 # build rom
 . build/envsetup.sh
-brunch arrow_j7velte
+lunch du_j7veltw-userdebug
 export TZ=Asia/Dhaka #put before last build command
-m bacon
+make corvus
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
