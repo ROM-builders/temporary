0159339077602c54362a7ab19c3a0c3c7ccc4fab
Kakplis <88788478+Kakplis@users.noreply.github.com>
2021-09-29 18:38:53 +0700

    Los-userdebug

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_X00T-user
+lunch lineage_X00T-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
