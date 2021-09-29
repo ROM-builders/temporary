faa19c11545612d7ce18030003b5a81e05afbde8
Kakplis <88788478+Kakplis@users.noreply.github.com>
2021-09-29 19:47:45 +0700

    user

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_X00T-userdebug
+lunch lineage_X00T-user
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
