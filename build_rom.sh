2ccc9c52c3223ec629df0d25244d259eea1bfcb7
Kakplis <88788478+Kakplis@users.noreply.github.com>
2021-09-30 17:02:41 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-17.1 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Kakplis/local_manifest.git --depth 1 -b lineage-17.1 .repo/local_manifests
+git clone https://github.com/Kakplis/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
