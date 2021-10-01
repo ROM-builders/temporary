826ff56c1e6c56c01fb28ec5df0cc40101863249
offsetc666 <90700307+offsetc666@users.noreply.github.com>
2021-10-02 00:02:47 +0400

    trigger: re-run

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Project-Fluid/manifes
 git clone https://github.com/offsetc666/local_manifest.git --depth 1 -b FluidOS .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom2
+# build rom
 . build/envsetup.sh
 lunch fluid_juice-userdebug
 export TZ=Asia/Dhaka #put before last build command
