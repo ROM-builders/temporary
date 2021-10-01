34b534cccd95764bf1dc9b9e706e7845825e48db
Jagaddhita Jalu <88699126+jaguarezz@users.noreply.github.com>
2021-10-01 16:52:59 -0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch revengeos_ulysse-user
-export TZ=Asia/Jakarta # put before last build command
+export TZ=Asia/Jakarta #put before last build command
 make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
