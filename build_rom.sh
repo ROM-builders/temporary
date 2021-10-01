e6d877f4df958b05b7a28aeda746e9c7c8052f79
Quang Anh <60723055+quxngeh@users.noreply.github.com>
2021-10-01 16:14:52 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-#  sync rom
+# sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/ProjectRadiant/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/quxngeh/local-manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
