fe511f87fd66287bae3a43b8a2677564b1bafe96
Jagaddhita Jalu <88934027+jagaddhita@users.noreply.github.com>
2021-09-30 22:52:49 -0700

    Octavi

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/DotOS/manifest.git -b dot11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/jagaddhita/local_manifest.git --depth 1 -b dot11 .repo/local_manifests
+git clone https://github.com/jagaddhita/local_manifest.git --depth 1 -b octavi .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
-lunch dot_ulysse-userdebug
-export TZ=Asia/Dhaka #put before last build command
-make bacon
+. build/envsetup.sh
+lunch octavi_ulysse-userdebug
+export TZ=Asia/Jakarta #put before last build command
+brunch ulysse
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
