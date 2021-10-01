75197ea2a9b72c2ae3b1deb5768b38ce8dc0ca95
EmadGr8 <91535171+EmadGr8@users.noreply.github.com>
2021-10-01 23:26:06 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch aosp_arm64-eng
+lunch havoc_tulip-userdebug
 export TZ=Asia/Dhaka #put before last build command
 brunch
 
