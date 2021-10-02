3a8320f9999e9de01a0d4647ae632be46f8df53d
Rishik Rastogi <59395879+RishikRastogi@users.noreply.github.com>
2021-10-02 20:58:22 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -8,7 +8,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 lunch bliss_Z01R-userdebug
 export SKIP_ABI_CHECKS=true
 export TZ=Asia/Kolkata #put before last build command
-blissify -g bliss_Z01R-userdebug -j$(nproc --all)
+blissify -g -d bliss_Z01R-userdebug -j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
