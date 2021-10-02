afa592ecea0f0574f02d833393646aad247ca6c4
Quang Anh <60723055+quxngeh@users.noreply.github.com>
2021-10-03 06:33:40 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,6 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/ProjectRadiant/manifest
 git clone https://github.com/quxngeh/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+
 # build rom
 source build/envsetup.sh
 lunch radiant_miatoll-userdebug
