8731d6f3579c9a4ac8c3a796b884905a4870125d
pingmado <75196272+ping2109@users.noreply.github.com>
2021-10-03 10:17:26 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/ShapeShiftOS/android_manifest.git -b android_11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/ping2109/local_manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/ping2109/local_manifest.git --depth 1 -b idk .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
