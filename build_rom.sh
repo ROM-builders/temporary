2b496cf9b8e515f2073825483a61a0d131559346
pingmado <75196272+ping2109@users.noreply.github.com>
2021-10-03 12:44:53 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/crdroidandroid/android.git -b 11.0 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/ping2109/local_manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/ping2109/local_manifest.git --depth 1 -b idk .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
