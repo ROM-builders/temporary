0334bdd00ccf45796ac1e2afaea6bbbf567538bd
pingmado <75196272+ping2109@users.noreply.github.com>
2021-10-03 12:43:49 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/crdroidandroid/android.git -b 11.0 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/ping2109/local_manifest.git --depth 1 -b lineage-18.1 .repo/local_manifests
+git clone https://github.com/ping2109/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-# lunch lineage_rova-userdebug
+lunch lineage_rova-userdebug
 export TZ=Asia/HoChiMinh #put before last build command
 brunch rova
 
