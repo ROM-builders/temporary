2e085d2571b685e4283c04b8f760fae3eb9ebd1c
Thoreck <46625454+fajar4561@users.noreply.github.com>
2021-10-04 03:30:41 +0700

    Try to build rom X00TD

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/ResurrectionRemix/platf
 git clone https://github.com/fajar4561/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom (skrip)
 source build/envsetup.sh
 lunch rr_X00TD-userdebug 
 export TZ=Asia/Jakarta #put before last build command
