08427797193db3dafe9ce09558a913dda1e01126
Thoreck <46625454+fajar4561@users.noreply.github.com>
2021-10-04 02:55:53 +0700

    build-X00TD

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom
+# sync rom (Sinkron kode Sumber Rom)
 repo init --depth=1 --no-repo-verify -u git://github.com/ResurrectionRemix/platform_manifest.git -b Q -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/fajar4561/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
