322a44a0995e5a855fd3fadacce6abc53c65719d
iskandarasri03 <71484902+iskandarasri03@users.noreply.github.com>
2021-10-03 16:03:22 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -8,6 +8,6 @@ source build/envsetup.sh
 lunch arrow_dandelion-userdebug
 export TZ=Asia/Dhaka #put before last build command
 m bacon
-export ALLOW_MISSING_DEPENDENCIES := true
+
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
