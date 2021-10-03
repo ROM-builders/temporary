53df8cc9eafff18a886d82e8e40ca2fa01cc1a16
iskandarasri03 <71484902+iskandarasri03@users.noreply.github.com>
2021-10-03 09:57:15 +0800

    Dandelion PixelExperiencePlus iskandarasri03

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/ArrowOS/android_manifest.git -b arrow-11.0 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/PixelExperience/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/iskandarasri03/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch arrow_dandelion-userdebug
+lunch aosp_$device-userdebug
 export TZ=Asia/Dhaka #put before last build command
-m bacon
+mka bacon -j8
 
 
 #upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
