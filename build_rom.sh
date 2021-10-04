1f532e12eff05b89964ea7f9f04c5059e3324b9a
Rishik Rastogi <59395879+RishikRastogi@users.noreply.github.com>
2021-10-04 08:01:09 +0530

    For bliss

--- build_rom.sh
+++ build_rom.sh
@@ -2,16 +2,14 @@
 repo init --depth=1 --no-repo-verify -u git://github.com/BlissRoms/platform_manifest.git -b r -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/RishikRastogi/local_manifest.git --depth 1 -b Bliss .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
-rm -rf device/asus/Z01R
-git clone https://github.com/ayanm09/kernel_z01r.git --depth=1 -b eleven device/asus/Z01R
 
 # build rom
 . build/envsetup.sh
 lunch bliss_Z01R-userdebug
 export SKIP_ABI_CHECKS=true
 export TZ=Asia/Kolkata #put before last build command
-# blissify -g bliss_Z01R-userdebug -j$(nproc --all)
-mka blissify -g -d j$(nproc --all)
+blissify -g bliss_Z01R-userdebug -j$(nproc --all)
+#mka blissify -g -d j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
