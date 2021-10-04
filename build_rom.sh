cab65503768b0ffc035f2604b8b6825965c8c4f8
Rishik Rastogi <59395879+RishikRastogi@users.noreply.github.com>
2021-10-04 08:14:22 +0530

    Fix

--- build_rom.sh
+++ build_rom.sh
@@ -9,7 +9,6 @@ lunch bliss_Z01R-userdebug
 export SKIP_ABI_CHECKS=true
 export TZ=Asia/Kolkata #put before last build command
 blissify -g bliss_Z01R-userdebug -j$(nproc --all)
-#mka blissify -g -d j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
