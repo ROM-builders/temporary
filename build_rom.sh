68aadc2f2f01e9d432f5bd403384e90d070f9711
WalkingDead <lexaxx14@gmail.com>
2021-10-05 13:57:51 +0400

    trigger: first build

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # Sync ROM
-repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/deadline646/local_manifest.git --depth 1 -b LineageOS .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/AOSPMinimal/platform_manifest -b aosp-12 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/deadline646/local_manifest.git --depth 1 -b AOSP .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # Build ROM
 source build/envsetup.sh
-lunch lineage_juice-userdebug
+lunch aosp_juice-userdebug
 export TZ=Asia/Dhaka #put before last build command
-mka bacon
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
