eb862d97308d01077aa76b35458e494d6e4caf12
プッシー <82253361+HoleDroid@users.noreply.github.com>
2021-10-06 06:45:08 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/ResurrectionRemix/platf
 git clone https://github.com/HoleDroid/local_manifests.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build
 source build/envsetup.sh
 lunch rr_X00TD-userdebug 
 export TZ=Asia/Jakarta #put before last build command
