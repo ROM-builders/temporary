69ddba43df050d33a7b74ef00a9702bc05206029
Jagaddhita Jalu <88699126+jaguarezz@users.noreply.github.com>
2021-10-05 14:41:23 +0700

    Fix manifest syncing error

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch colt_ulysse-userdebug
-export TZ=Asia/Jakarta #put before last build command
+export TZ=Asia/Singapore #put_before_last_build_command
 mka colt -j$
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
