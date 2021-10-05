4f008d7da6e4acddb5e4a38553ca6e2593fdeba3
bibarub <73599925+bibarub@users.noreply.github.com>
2021-10-05 22:34:02 +0300

    bruh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/bibarub/platform_manifest.git -b c11 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/Colt-Enigma/platform_manifest.git -b c11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/bibarub/local_manifest.git --depth 1 -b vayucolt .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
