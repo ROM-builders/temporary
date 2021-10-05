f26eaae70a8f92ee134b7be73907a3afa7f64e08
ZilverQueen <89440054+ZilverQueen@users.noreply.github.com>
2021-10-05 23:08:25 +0700

    trigger: initial build aex

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch aosp_ulysse-userdebug
-export TZ=Asia/Jakarta
+export TZ=Asia/Jakarta 
 m aex
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
