e1c6129c2eeeacbaf33f0c37712359aaa4d564f6
fazrul1994 <74166146+fazrul1994@users.noreply.github.com>
2021-10-06 06:50:56 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/fazrul1994/local_manifests.git --depth 1 -b Los-poplar .repo/local_manifests
+git clone https://github.com/fazrul1994/local_manifests.git --depth 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
