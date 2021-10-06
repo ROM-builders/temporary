82ccd4119ee8afb07aeedae9600ab5aa914d914d
yograjsingh-cmd <84258191+yograjsingh-cmd@users.noreply.github.com>
2021-10-06 08:37:28 +0530

    Oct Patch DerpFest Z01R

--- build_rom.sh
+++ build_rom.sh
@@ -7,8 +7,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch derp_Z01R-userdebug
 export TZ=Asia/Kolkata 
-mka vendorimage
+mka derp
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
-#rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
 rclone copy out/target/product/Z01R/vendor.img cirrus:Z01R -P
