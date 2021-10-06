34027df3c7cbc7383fe9b2a7dd408469cc4078a2
iskandarasri03 <71484902+iskandarasri03@users.noreply.github.com>
2021-10-06 11:36:41 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch arrow_dandelion-userdebug
-export TZ=Asia/Dhaka #put before last build command
+export ALLOW_MISSING_DEPENDENCIES=true
+export TZ=Asia/Singapore #put before last build command
 m otapackage
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
