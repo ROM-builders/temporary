7a122ecafa1a773dc195eadd1bc35fe4a09885b7
fazrul1994 <74166146+fazrul1994@users.noreply.github.com>
2021-10-06 07:59:59 +0700

    Fix compilation

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_poplar-userdebug
+lunch lineage_poplar_kddi-userdebug
 export TZ=Asia/Jakarta #put before last build command
 make -j8 bacon
 
