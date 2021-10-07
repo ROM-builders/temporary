76a4104090f1445217bcb33236d4613b728d7f53
ini23 <92011891+ini23@users.noreply.github.com>
2021-10-08 05:49:37 +0700

    test build

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 -u https://github.com/ini23/manifest.git -b dot11 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/ini23/manifest.git -b dot11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/ini23/local_manifest.git --depth 1 -b dotos .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
