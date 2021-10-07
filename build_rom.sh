3d83983dd96ca1f1b123964368131d5d6dcb9f9d
Thoreck <46625454+fajar4561@users.noreply.github.com>
2021-10-08 02:11:49 +0700

    fix failed

--- build_rom.sh
+++ build_rom.sh
@@ -8,7 +8,7 @@ source build/envsetup.sh
 lunch xdroid_X00TD-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
-export TZ=Asia/Jakarta setelan waktu 
+export TZ=Asia/Jakarta setelan waktu (i'll try to ......)
 make xd
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
