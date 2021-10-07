e5c40d30e04c5a9acc59a578ac551fae5ce2939b
Thoreck <46625454+fajar4561@users.noreply.github.com>
2021-10-08 03:25:23 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/xdroid-CAF/xd_manifest 
 git clone https://github.com/fajar4561/local_manifest.git --depth 1 -b xdroid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom (skrip)
+# build
 source build/envsetup.sh
 lunch xdroid_X00TD-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
