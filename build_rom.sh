07c48dc3406c6f11f33e3d2ad82234d71fd1ecc9
ini23 <92011891+ini23@users.noreply.github.com>
2021-10-07 17:33:53 +0700

    dotos unof

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,13 @@
 # sync rom
-repo init -u https//github.com/Octavi-OS/manifest.git -b 11 --depth=1 --no-repo-verify -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Kitsunejasutin/local_manifest.git --depth 1 -b octavi .repo/local_manifests
+repo init -u git://github.com/DotOS/manifest.git -b dot11 --depth=1 --no-repo-verify -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/ini23/local_manifest/blob/main/local_manifest.xml --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-export WITH_GAPPS=true
-lunch octavi_ginkgo-user
+lunch dot_whyred-userdebug
 export TZ=Asia/Dhaka #put before last build command
-mka octavi
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
