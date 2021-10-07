8ab1910334cb41c3c6684628923ff5b28f2b2a19
ini23 <92011891+ini23@users.noreply.github.com>
2021-10-07 20:55:29 +0700

    fix build

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init -u git://github.com/DotOS/manifest.git -b dot11 --depth=1 --no-repo-verify -g default,-device,-mips,-darwin,-notdefault
+repo init -u git://github.com/DotOS/manifest.git -b dot11 --depth=1 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/ini23/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
