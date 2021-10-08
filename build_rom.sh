dc711e3128fe9fd6d1d931904d873a7cd997bd51
iAboothahir <65064906+iAboothahir@users.noreply.github.com>
2021-10-08 20:33:37 +0530

    build lineageos-19.0

--- build_rom.sh
+++ build_rom.sh
@@ -1,17 +1,21 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/STRK-ND/local-manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-19.0 -g default,-device,-mips,-darwin,-notdefault
+#git clone https://github.com/STRK-ND/local-manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom syberia
+# clone tress
+git clone -b lineage-18.1 https://github.com/LineageOS/android_device_asus_sdm660-common.git device/asus/common --depth=1
+git clone -b lineage-18.1 https://github.com/LineageOS/android_device_asus_X00TD.git device/asus/X00TD --depth=1
+git clone -b lineage-18.1 https://github.com/LineageOS/android_kernel_asus_sdm660.git kernel/asus/sdm660 --depth=1
+git clone -b lineage-18.1 https://github.com/TheMuppets/proprietary_vendor_asus.git vendor/asus --depth=1
+
+# build rom lineage
 
 source build/envsetup.sh
-# permission
-chmod +x device/asus/X00TD/audio_patch.sh
 
-lunch aosp_X00TD-userdebug
+lunch lineage_X00TD-userdebug
 export TZ=Asia/Dhaka #put before last build command
-m aex
+brunch X00TD
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
