f35a8fd32149a79492f866f643644e3f61fd0011
Sanjeev <42514333+sanjeevstunner@users.noreply.github.com>
2021-10-08 08:55:27 +0530

    tulip: PE: Added props to suspend charging when battery is full

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/PixelExperience/manifes
 git clone https://github.com/sanjeevstunner/Manifest.git --depth 1 -b PE-11 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-#commit 1
+#commit 2
 
 # build rom
 source build/envsetup.sh
