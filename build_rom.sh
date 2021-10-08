3e947b48c15f95ed212795c71901e512e9f79c5d
Hitesoni :% <72127977+hsx02@users.noreply.github.com>
2021-10-08 20:27:47 +0530

    added gapps flag

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch nad_pine-userdebug
+export USE_GAPPS=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
 mka nad
