30066ffc2826a5e68db7fa285f85e5b760558493
Hitesoni :% <72127977+hsx02@users.noreply.github.com>
2021-10-08 20:35:29 +0530

    build changed to dot gapps

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/NusantaraProject-ROM/android_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/hsx02/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/DotOS/manifest.git -b dot11 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/hsx02/Local-Manifests.git --depth 1 -b dot-11 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-. build/envsetup.sh
-lunch nad_pine-userdebug
-export USE_GAPPS=true
+source build/envsetup.sh
+lunch dot_pine-userdebug
+export WITH_GAPPS=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
-mka nad
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
