ac112cc33ec6a910a0ada6e15df484cc963ff0ca
fazrul1994 <74166146+fazrul1994@users.noreply.github.com>
2021-10-08 10:55:55 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/fazrul1994/local_manifest.git --depth 1 .repo/local
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+. build/envsetup.sh
 lunch lineage_poplar-userdebug
 export TZ=Asia/Jakarta #put before last build commandd
 make -j8 bacon
