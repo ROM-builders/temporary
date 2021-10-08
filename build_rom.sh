f6b6249cc02182a235c90743b725074e95665fb5
Thoreck <46625454+fajar4561@users.noreply.github.com>
2021-10-09 02:40:50 +0700

    bringup nusantara

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,14 @@
 # sync rom (Sinkron kode Sumber Rom)
-repo init --depth=1 --no-repo-verify -u git://github.com/xdroid-CAF/xd_manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/fajar4561/local_manifest.git --depth 1 -b xdroid .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/NusantaraProject-ROM/android_manifest -b 11 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/fajar4561/local_manifest.git --depth 1 -b nd .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build
 source build/envsetup.sh
-lunch xdroid_X00TD-userdebug
+lunch nad_X00TD-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
-export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Jakarta 
-make xd
+mka nad
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
