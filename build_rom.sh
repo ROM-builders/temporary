6dc18fc1cb3bdae144fe42ca3eedc34c56d0b235
Flasho <75789819+flashokillerify@users.noreply.github.com>
2021-10-08 08:40:25 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-19.0
---depth=1 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-19.0 --depth=1 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/flashokillerify/manifest_pine --depth 1 -b A12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
