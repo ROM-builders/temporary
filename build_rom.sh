47068bc31e304e2e67aa2951782b816f32276deb
WalkingDead <lexaxx14@gmail.com>
2021-10-08 14:35:59 +0400

    Re-run

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -
 git clone https://github.com/deadline646/local_manifest.git --depth 1 -b lineage-19.0 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build ro
 . build/envsetup.sh
 lunch lineage_juice-userdebug
 export TZ=Asia/Dhaka
