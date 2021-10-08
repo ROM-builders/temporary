06cb2d2f7ae7f914ddf5a813f30e47d7b8af2afd
Droneship <81514881+Seifood@users.noreply.github.com>
2021-10-09 00:56:39 +0200

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch crdroid_gauguin-userdebug
+lunch lineage_gauguin-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
