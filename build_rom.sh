2ebeefe4f81930d306ba39bdfb00508d2b58ab6d
Droneship <81514881+Seifood@users.noreply.github.com>
2021-10-08 16:07:19 +0200

    Create build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init -u git://github.com/crdroidandroid/android.git -b 11.0 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/Seifood/local_manifest/tree/main --main -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
+lunch crdroid_gauguin-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
