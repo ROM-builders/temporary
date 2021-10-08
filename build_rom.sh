c2ee5bc5ee0c8d42895097531e15ebf6c393ec87
Hitesoni :% <72127977+hsx02@users.noreply.github.com>
2021-10-08 15:39:24 +0530

    Removed selinux ignore

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch dot_pine-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
