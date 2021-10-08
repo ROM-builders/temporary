78dc8ef24ebf6a626bcaaabc5da9d38a67c45f1f
Kendras056 <88943971+Kendras056@users.noreply.github.com>
2021-10-08 15:43:45 +0700

    RR

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/ResurrectionRemix/platf
 git clone https://github.com/Kendras056/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build
+# build rom
 source build/envsetup.sh
 lunch rr_X00TD-userdebug 
 export TZ=Asia/Jakarta #put before last build command
