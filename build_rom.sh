7fa762eec82aeb53cee74a69811965f08ddb2d5e
mohammad iqbal <92011891+ini23@users.noreply.github.com>
2021-10-08 11:45:32 +0700

    test build

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/DotOS/manifest -b dot
 git clone https://github.com/ini23/local_manifest.git --depth 1 -b dotos .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build
+# build rom
 source build/envsetup.sh
 lunch dot_whyred-userdebug
 export TZ=Asia/Jakarta 
