a3ea1c8ca5de81a33db3123c7ba92e1ae92cbbbe
Cliquers <abenkenari@gmail.com>
2021-10-09 15:35:14 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch superior_X00TD-userdebug
+lunch evolution_X00TD-userdebug
 export TZ=Asia/Jakarta #put before last build command
 mka evolution
 
