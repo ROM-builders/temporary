0bc91b4c8b2f1ad57cc2eac194aa0bbae3b420e0
Dev0786s <92157105+Dev0786s@users.noreply.github.com>
2021-10-10 00:42:07 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/Octavi-OS/platform_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/Dev0786s/local-manifest.git --depth 1 -b master .repo/local_manifests
-repo sync -c -f --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 
 # build rom
 source build/envsetup.sh
-lunch octavi $merlin-userdebug
+lunch octavi_Merlin-userdebug
 brunch Merlin
 export TZ=Asia/Dhaka #put before last build command
 mka derp
