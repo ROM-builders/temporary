040ca59fbf86393248b9dcc81d94993f02ace94c
Jagaddhita Jalu <88934027+jagaddhita@users.noreply.github.com>
2021-10-09 13:34:49 +0700

    ArcaneOS vayu

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/ArcaneOS/Arcane_manifest -b R -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/Sander-Sonia/lm.git --depth 1 -b vayu .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
