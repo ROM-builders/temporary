aa715e52cecd0be0d6dcf5e712f71afbabc31bee
Jagaddhita Jalu <88934027+jagaddhita@users.noreply.github.com>
2021-10-09 13:32:46 +0700

    ArcaneOS vayu

--- build_rom.sh
+++ build_rom.sh
@@ -4,10 +4,13 @@ git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
-lunch derp_vayu-user
-export TZ=Asia/Dhaka #put before last build command
-mka derp
+. build/envsetup.sh
+lunch aosp_vayu-userdebug
+export KBUILD_BUILD_USER=jagaddhita
+export KBUILD_BUILD_HOST=not_kemendikbud
+export ALLOW_MISSING_DEPENDENCIES=true
+export TZ=Asia/Jakarta #put before last build command
+mka bacon -j$(nproc --all) 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
