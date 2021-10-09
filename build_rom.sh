d7f42e329ba117da50ae6c5e7b6a6d887f11aaca
Rishik Rastogi <59395879+RishikRastogi@users.noreply.github.com>
2021-10-09 07:21:35 +0530

    For dot os

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/CherishOS/android_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/RishikRastogi/local_manifest.git --depth 1 -b cherish .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/DotOS/manifest.git -b dot11 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/RishikRastogi/local_manifest.git --depth 1 -b dot-os .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # build rom
 source build/envsetup.sh
 lunch cherish_Z01R-userdebug
+export WITH_GAPPS=true
 export SKIP_ABI_CHECKS=true
 export TZ=Asia/Kolkata #put before last build command
 mka bacon -j$(nproc --all)
