508d4874260434a107d50d288b9748ce6896866b
Thoreck <46625454+fajar4561@users.noreply.github.com>
2021-10-10 04:43:26 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,17 +1,15 @@
 # sync rom (Sinkron kode Sumber Rom)
-repo init --depth=1 --no-repo-verify -u git://github.com/NusantaraProject-ROM/android_manifest -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/fajar4561/local_manifest.git --depth 1 -b nd .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-17.1 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/fajar4561/local_manifest.git --depth 1 -b 17.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build
 source build/envsetup.sh
-lunch nad_X00TD-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
-export SKIP_API_CHECKS=true
-export ALLOW_MISSING_DEPENDENCIES=true
-export TZ=Asia/Jakarta 
-mka nad
 
+lunch lineage_X00TD-userdebug
+export TZ=Asia/Jakarta 
+
+mka bacon
 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
