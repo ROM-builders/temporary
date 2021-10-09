d306e0221982ad60d385be7271d7271aeca93491
Kendras056 <88943971+Kendras056@users.noreply.github.com>
2021-10-09 08:54:29 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom (Sinkron kode Sumber Rom)
+# sync
 repo init --depth=1 --no-repo-verify -u git://github.com/xdroid-CAF/xd_manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/Kendras056/local_manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
