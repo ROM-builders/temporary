4c90882b3ecc54a2b5d310663a155499b4ac5120
Dev0786s <92157105+Dev0786s@users.noreply.github.com>
2021-10-09 23:56:09 +0530

    Merlinx octavi  Dev0786s
    🙂

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo init --depth=1 --no-repo-verify -u git://github.com/Octavi-OS/platform_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/Dev0786s/local-manifest.git --depth 1 -b master .repo/local_manifests
+repo sync -c -f --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j8
+
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
+lunch octavi_$device-userdebug
+brunch Merlin
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
