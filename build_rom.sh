58a42514f21a38d352bac290936e15f103a70a13
Pro-xIx <92198946+Pro-xIx@users.noreply.github.com>
2021-10-09 22:06:01 +0530

    Added force sync

--- build_rom.sh
+++ build_rom.sh
@@ -2,6 +2,7 @@
 repo init --depth=1 --no-repo-verify -u git://github.com/NusantaraProject-ROM/android_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/hsx02/Local-Manifests.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -j1 --fail-fast
 
 # build rom
 . build/envsetup.sh
