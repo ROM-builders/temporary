00716f8d95fca33e68f3b7e90c564faa51157a80
Kendras056 <88943971+Kendras056@users.noreply.github.com>
2021-10-09 12:18:28 +0700

    patch 2

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
-# sync
+# sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/xdroid-CAF/xd_manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/Kendras056/local_manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build
+# build rom
 source build/envsetup.sh
 lunch xdroid_X00TD-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
