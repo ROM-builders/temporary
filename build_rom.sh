ad54b35cd8d9f3cb0632262c9264edc982a17429
rehanalt501 <92092421+rehanalt501@users.noreply.github.com>
2021-10-09 13:43:51 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,21 @@
 # sync rom
-repo init --depth=1 --no-repo-verify git://github.com/DotOS/manifest.git -b dot11 -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+
+repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/Octavi-OS/platform_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
+
+git clone https://github.com/DPSLEGEND/local_mainfest.git --depth 1 -b master .repo/local_manifests
+
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
+
 source build/envsetup.sh
-lunch derp_vayu-user
+
+lunch octavi_RMX3171-user
+
 export TZ=Asia/Dhaka #put before last build command
-mka derp
+
+brunch RMX3171
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
