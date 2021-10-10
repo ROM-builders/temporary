55b073e979d8ba692a85022cb0397b843f29f98d
FraSharp <s23265@iisve.it>
2021-10-10 10:30:09 +0200

    j6lte: LineageOS 18.1: still not booting, try to fix by vintf manifest

--- build_rom.sh
+++ build_rom.sh
@@ -9,5 +9,6 @@ lunch lineage_j6lte-eng
 export TZ=Asia/Dhaka #put_beforelastbuildcommand
 mka bacon
 
+# 101021: manifest updates in j6lte device tree
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
