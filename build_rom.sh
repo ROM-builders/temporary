a83f7482065c5a3a6eabf28a2ae8e550b37b6637
Droneship <81514881+Seifood@users.noreply.github.com>
2021-10-10 03:43:14 +0200

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,9 +5,9 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch aosp_gauguin-userdebug
+lunch lineage_gauguin-userdebug
 export TZ=Asia/Dhaka #put before last build command
-m aex | tee log.txt
+brunch lineage_gauguin-userdebug
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
