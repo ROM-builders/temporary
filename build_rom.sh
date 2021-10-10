c82c09ea292e7f8529c82645164e572846faf70f
Dev0786s <92157105+Dev0786s@users.noreply.github.com>
2021-10-10 06:39:47 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,8 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch octavi_Merlin-userdebug
-brunch Merlin
+lunch octavi merlin-userdebug
+brunch merlin
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
