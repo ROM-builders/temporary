ec34a24d85eebf4249c6a308e1877a5ecbce4404
yograjsingh-cmd <84258191+yograjsingh-cmd@users.noreply.github.com>
2021-10-11 12:45:55 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-12.0 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/yograjsingh-cmd/local_manifest.git --depth 1 -b aosp .repo/local_manifests
+git clone https://github.com/yograjsingh-cmd/local_manifest.git --depth 1 -b arrow .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
