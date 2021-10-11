5c55b9c43ba9c1351b5b424cdb9dce20a9925386
FraSharp <s23265@iisve.it>
2021-10-11 11:23:30 +0200

    j6lte: LineageOS 18.1: fix fatal error in manifest

--- build_rom.sh
+++ build_rom.sh
@@ -12,5 +12,6 @@ mka bacon
 # 101021 := manifest updates in j6lte device tree
 # 101021-23:12 := vintf updates, hopefully this time will boot, also switch to userdebug build
 # 101121-08:39 := switch to oss HWC, switch to eng build to debug boot
+# 101121-11:22 := fix fatal error in manifest
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
