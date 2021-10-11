d4d1296b2c3973f9ad543fee537f59ceadde8bac
FraSharp <s23265@iisve.it>
2021-10-11 08:40:49 +0200

    j6lte: LineageOS 18.1: switch to bsp HWC, use eng build

--- build_rom.sh
+++ build_rom.sh
@@ -5,11 +5,12 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_j6lte-userdebug
+lunch lineage_j6lte-eng
 export TZ=Asia/Dhaka
 mka bacon
 
 # 101021 := manifest updates in j6lte device tree
 # 101021-23:12 := vintf updates, hopefully this time will boot, also switch to userdebug build
+# 101121-08:39 := switch to oss HWC, switch to eng build to debug boot
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
