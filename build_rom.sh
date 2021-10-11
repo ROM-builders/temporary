088467ed80c33841ae34b804092b6cdc1f4d1959
Teguh Budi Santoso <86312158+Tiktodz@users.noreply.github.com>
2021-10-11 22:53:18 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/ResurrectionRemix/platform_manifest.git -b Q -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Tiktodz/local_manifests.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/PixelExperience/manifest.git -b ten-plus -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/Tiktodz/local_manifests.git --depth 1 -b ten+ .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch rr_X00TD-userdebug 
+lunch aosp_X00TD-userdebug 
 export TZ=Asia/Jakarta #put before last build command
 mka bacon
 
