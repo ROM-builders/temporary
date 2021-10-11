9dad356cab63ae464093092bf8e8223672a8984e
iAboothahir <65064906+iAboothahir@users.noreply.github.com>
2021-10-11 23:51:32 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -
 git clone https://github.com/iAboothahir/manifest.git --depth 1 -b lineage-17.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 wget https://raw.githubusercontent.com/iAboothahir/20-20/master/patches/001-fix_error1.patch
-mv 001-fix_error1.patch device/Asus/X00TD
+cp 001-fix_error1.patch device/Asus/X00TD
 cd device/Asus/X00TD
 patch -p1 < 001-fix_error1.patch
 cd -
