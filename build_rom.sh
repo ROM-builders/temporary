5cba4022c5f1ed6370e69a928dfdf49cc20ec39b
braindedboi <85474068+braindedboi@users.noreply.github.com>
2021-10-11 17:31:25 +0530

    Hopefully Fix

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelPlusUI-Elle/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/braindedboi/local_manifests.git --depth 1 -b ppui .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
