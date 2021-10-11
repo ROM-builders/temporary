8de90f0ed0d0d2a7bbda38155586139c6187f4ff
iskandarasri03 <71484902+iskandarasri03@users.noreply.github.com>
2021-10-11 22:02:25 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch arrow_dandelion-userdebug
-export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Singapore #put before last build command
 m otapackage
 
