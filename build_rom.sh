565932d6dd16967be3e48426ffe7c1bdae60edf6
bhargav palan <43906329+bhargavpalan@users.noreply.github.com>
2021-10-11 08:04:09 -0700

    cherishOS Vince

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b eleven
+
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
