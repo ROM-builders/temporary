458d14910bbd364f135b5673ed844abeece1ab30
Hitesoni :% <72127977+hsx02@users.noreply.github.com>
2021-10-12 14:50:13 +0530

    switched branch nad > common

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/NusantaraProject-ROM/android_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/hsx02/Local-Manifests.git --depth 1 -b nad .repo/local_manifests
+git clone https://github.com/hsx02/Local-Manifests.git --depth 1 -b common .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
