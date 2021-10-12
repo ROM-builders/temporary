70d4c20376fce08ca1519fdc22b62cefb87030c2
iskandarasri03 <71484902+iskandarasri03@users.noreply.github.com>
2021-10-12 21:31:07 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,10 +1,10 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/ArrowOS/android_manifest.git -b aarrow--g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/ArrowOS/android_manifest.git -b arrow-11.0 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/iskandarasri03/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+. build/envsetup.sh
 lunch arrow_dandelion-userdebug
 export TZ=Asia/Singapore #put before last build command
 m bacon
