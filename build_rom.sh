af43f728dfbda0fe04249221ad35059f42feb7a1
Akash Chaturvedi <76404534+Akash23q2@users.noreply.github.com>
2021-10-12 21:36:30 +0530

    Merlinx-PixysOs-Akash23q2

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
-# sync rom#
-repo init -u https://github.com/PixysOS/manifest -b eleven
+# sync rom
+repo init --depth=1 --no-repo-verify -u https://github.com/PixysOS/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/Akash23q2/local_manifest --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
