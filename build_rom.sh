a5ec63418aebc3e4886a76d525193554e293f650
Teguh Budi Santoso <86312158+Tiktodz@users.noreply.github.com>
2021-10-12 12:30:21 +0700

    xddx

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
-# sync rom
+# sync
 repo init --depth=1 --no-repo-verify -u git://github.com/xdroid-CAF/xd_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/Tiktodz/local_manifests.git --depth 1 -b xdroid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build
 source build/envsetup.sh
 lunch xdroid_X00TD-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
