c84f1634e88ae9fc9c947b43b20358565bdd9773
Git987655 <91123494+Git987655@users.noreply.github.com>
2021-10-12 13:36:24 +0530

    ulysse-dot-Git987655

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/DotOS/manifest.git -b Dot-5.2 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/Git987655/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_ulysse-user
+lunch dot_ulysse-user
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
