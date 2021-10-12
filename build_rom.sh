8a9123c4ae1fd296b67acfd6b81bc60e8956aea6
MasumS <72314809+masumsaief@users.noreply.github.com>
2021-10-13 00:07:58 +0530

    Update build_rom.sh (#2)

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch carbon_X00T-userdebug
-export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Kolkata #put before last build command
 make carbon
 
