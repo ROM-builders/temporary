5df4c12e53d630a5cfb9c6a460adca6294ab0908
Bob <58835837+Apitpr0@users.noreply.github.com>
2021-10-12 09:39:24 +0800

    ulysse - update local manifest to correct branch

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-11.0 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Apitpr0/local_manifests.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/Apitpr0/local_manifests.git --depth 1 -b eleven-ulova .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
