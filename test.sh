06298001e6d14f27ed36aadfece20300e05e7347
Amy <54113654+00p513-dev@users.noreply.github.com>
2021-10-13 22:07:35 +0100

    Attempt to build amyROM sarah nightly

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-init_check=$(grep 'repo init' $CIRRUS_WORKING_DIR/build_rom.sh | grep 'depth=1')
-if [[ $init_check != *default,-device,-mips,-darwin,-notdefault* ]]; then echo Please use --depth=1 and -g default,-device,-mips,-darwin,-notdefault tags in repo init line.; exit 1; fi
+#init_check=$(grep 'repo init' $CIRRUS_WORKING_DIR/build_rom.sh | grep 'depth=1')
+#if [[ $init_check != *default,-mips,-darwin,-notdefault* ]]; then echo Please use --depth=1 and -g default,-mips,-darwin,-notdefault tags in repo init line.; exit 1; fi
 
 clone_check=$(grep 'git clone' $CIRRUS_WORKING_DIR/build_rom.sh | wc -l)
 if [[ $clone_check -gt 1 ]]; then echo Please use local manifest to clone trees and other repositories, we dont allow git clone to clone trees.; exit 1; fi
