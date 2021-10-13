208af5d1bd782176ee2b22015d55c31eff50cf27
braindedboi <85474068+braindedboi@users.noreply.github.com>
2021-10-14 05:28:57 +0530

    Remove RealmeParts, because causing error

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelBlaster-OS/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/braindedboi/local_manifests --depth 1 -b pb .repo/local_manifests
+git clone https://github.com/braindedboi/local_manifests.git --depth 1 -b pb .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # build rom
