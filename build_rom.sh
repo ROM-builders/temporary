e7eb6b6772528b001caf45a55ebc7c7ebbb1140f
頼む <87476667+Ruega@users.noreply.github.com>
2021-10-14 15:14:03 +0700

    long queue

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
-# sync
+# sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/P-404/android_manifest -b rippa -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/Ruega/local_manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build
+# build rom
 source build/envsetup.sh
 lunch p404_X00TD-user
 make bacon
