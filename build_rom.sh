cbaaebd5e3f777881871a7581510d110ad7bd95f
Thoreck <46625454+fajar4561@users.noreply.github.com>
2021-10-14 08:11:17 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom (Sinkron kode Sumber Rom)
-repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-17.1 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/fajar4561/local_manifest.git --depth 1 -b 17.1 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/fajar4561/local_manifest.git --depth 1 -b 18.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build
