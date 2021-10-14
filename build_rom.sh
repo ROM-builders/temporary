84a52630103152966a259e8cfa5e4cd5d2b1acae
Git987655 <91123494+Git987655@users.noreply.github.com>
2021-10-14 09:32:56 +0530

    ulysse-lineage-Git987655

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Git987655/local_manifest --depth 1 -b master .repo/local_manifest
+git clone https://github.com/Git987655/local_manifest --depth 1 -b master .repo/local_manifests
+git clone https://github.com/Git987655/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
