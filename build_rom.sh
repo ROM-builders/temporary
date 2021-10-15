87162866214d4fbcf3aa67a815a9024aa26e65f1
abhigyan16 <65801637+abhigyan16@users.noreply.github.com>
2021-10-15 22:52:36 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
+lunch derp_vayu-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
