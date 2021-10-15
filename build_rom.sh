f41e3a720dbc4704371a9a4c31b4ac5472c50aee
indi121 <91513844+indi121@users.noreply.github.com>
2021-10-15 21:37:29 +0545

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -2,10 +2,11 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/Wave-Project/manifest -b r -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/indi121/local_manifests.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
 # build rom
 source build/envsetup.sh
 lunch wave_whyred-user
-export TZ=Asia/Kathmandu #put before last build command
+export TZ=Asia/Dhaka #put before last build command
 export SKIP_API_CHECKS=true
 brunch whyred
 
