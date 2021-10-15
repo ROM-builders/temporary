44b87e41e5e248284372b0bdbde28d435363d5b1
shield44 <motoecondorxt1022@gmail.com>
2021-10-15 20:24:03 +0530

    build_rom: use repo sync line of main branch

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b eleven-plus -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/shield44/local_manifest.git --depth 1 -b miatoll .repo/local_manifests
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
