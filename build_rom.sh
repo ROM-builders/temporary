2d44afc97c1e1222095895d40acfe966fd536c4e
braindedboi <85474068+braindedboi@users.noreply.github.com>
2021-10-15 12:10:54 +0530

    Switch to userdebug to fix error

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_RMX3171-user
+lunch lineage_RMX3171-userdebug
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
