1b3421125ace0064d7fd43c016c2d0db0fed00a5
DPSLEGEND <82019599+DPSLEGEND@users.noreply.github.com>
2021-10-15 19:06:05 +0530

    Cipher #2 RMX3171

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/CipherOS/android_manifest.git -b twelve -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/DPSLEGEND/local_manifest.git --depth 1 -b main .repo/local_manifests
-repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
