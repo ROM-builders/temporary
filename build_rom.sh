e05713d667968d6a922a28885cd3a548795163c5
horoid <59495159+horoidrom@users.noreply.github.com>
2021-10-15 12:24:10 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_m0q-eng 
+lunch lineage_m01q-eng 
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
