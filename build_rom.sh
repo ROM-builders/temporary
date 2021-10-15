e5f52a5a5fd1bd18474c761b728b2c6ac9186e3c
Aditya Singh <adityasingh1768@gmail.com>
2021-10-15 20:10:29 +0530

    Awaken a12

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b 11 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b 12 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/ad1tyas1ngh/local_manifest.git --depth 1 -b awaken .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch awaken_r2p-user
+lunch awaken_r2p-userdebug
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
