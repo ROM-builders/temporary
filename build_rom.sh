2d803f446659c6b77ddb1952afcf50ea3569a6af
ibratabian17 <71920907+ibratabian17@users.noreply.github.com>
2021-10-15 22:04:31 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ibratabian17/android_manifest -b 10-cleanup -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/ibratabian17/local_manifest.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ibratabian17/android_manifest -b 11 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/ibraaltabian17/local_manifest --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
