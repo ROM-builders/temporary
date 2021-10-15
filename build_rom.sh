023238d87700a8e3f0cbf9f6bb5d0d19ca7adadd
harshaxvarthan <82403923+harshaxvarthan@users.noreply.github.com>
2021-10-15 15:47:25 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom to 
+# sync rom to # 
 repo init --depth=1 --no-repo-verify -u git://github.com/Project-Xtended/manifest.git -b xr -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/harshaxvarthan/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
