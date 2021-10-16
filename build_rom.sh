74d847f1a4becf9df0372585a6f41ecadd31986a
mikairyuu <miva200267@gmail.com>
2021-10-16 18:27:56 +1000

    EvoX monet upbring

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,18 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git@github.com:Evolution-X/manifest.git -b elle -g default,-device,-mips,-darwin,-notdefault
+mkdir -p .repo/kernel/xiaomi
+mkdir -p .repo/device/xiaomi
+git clone https://github.com/mikairyuu/kernel_xiaomi_sm7250 --depth 1 -b skizo .repo/kernel/xiaomi/sm7250
+git clone https://github.com/mikairyuu/android_device_xiaomi_sm7250-common --depth 1 .repo/device/xiaomi/sm7250-common
+git clone https://github.com/mikairyuu/android_device_xiaomi_monet-oss --depth 1 .repo/device/xiaomi/monet
+git clone https://gitlab.com/mikairyuu/android-vendor-xiaomi-sm7250 --depth 1 .repo/vendor/xiaomi
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
-export TZ=Asia/Dhaka #put before last build command
-mka derp
+lunch evolution_monet-user
+export TZ=Asia/Vladivostok #put before last build command
+mka evolution
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
