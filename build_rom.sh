715369d143a86cc5220aaca624d465d381edcab0
braindedboi <85474068+braindedboi@users.noreply.github.com>
2021-10-16 20:15:18 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelBlaster-OS/manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/braindedboi/local_manifests.git --depth 1 -b pb .repo/local_manifests
+git clone https://github.com/braindedboi/local_manifests.git --depth 1 -b blaster .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # build rom
 source build/envsetup.sh
 lunch aosp_RMX3171-userdebug
 export TZ=Asia/Kolkata #put before last build command
-mka bacon
+make bacon -j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
