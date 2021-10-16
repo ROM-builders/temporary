edee8d1c8f47d0c0578aa0afd65a118edb9bea5a
X00T Best Kernels <88646493+xootkama@users.noreply.github.com>
2021-10-16 16:26:45 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,10 +4,7 @@ git clone https://github.com/xootkama/local_manifest.git --depth 1 -b main .repo
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-. build/envsetup.sh
-lunch voltage_RMX1851-userdebug
-export TZ=Asia/Kolkata #put before last build command
-mka voltage
+ls
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
