3de16506f60610c4a120f93aef8f380010bfb222
X00T Best Kernels <88646493+xootkama@users.noreply.github.com>
2021-10-16 06:43:48 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch aosp_RMX1851-userdebug
+lunch voltage_RMX1851-userdebug
 export TZ=Asia/Kolkata #put before last build command
 mka yaap
 
