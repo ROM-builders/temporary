1a204e43e222f80364063152f29745722171b4d2
プッシー <82253361+HoleDroid@users.noreply.github.com>
2021-10-17 00:39:02 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
-# sync rom
+# sync
 repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-17.1 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/HoleDroid/local-manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build
 source build/envsetup.sh
 lunch lineage_X00TD-userdebug
 export TZ=Asia/Jakarta #put before last build command
