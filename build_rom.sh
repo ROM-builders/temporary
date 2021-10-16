ebc3e5d0088487de3e8b78b1652fc7bfadae7c25
Teguh Budi Santoso <86312158+Tiktodz@users.noreply.github.com>
2021-10-16 11:37:50 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,15 @@
-# sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/Corvus-Q/android_manifest.git -b 10 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Tiktodz/local_manifests.git --depth 1 -b main .repo/local_manifests
+# sync
+repo init --depth=1 --no-repo-verify -u git://github.com/xdroid-CAF/xd_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/Tiktodz/local_manifests.git --depth 1 -b xdroid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build
 source build/envsetup.sh
-lunch du_X00TD-eng
+lunch xdroid_X00TD-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Jakarta
-make corvus
+make xd
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
