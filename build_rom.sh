c4b568f8000719a9ca3b5df208c96a1e00c20187
second <82253361+HoleDroid@users.noreply.github.com>
2021-10-17 13:39:21 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
-# sync rom
+# sync
 repo init --depth=1 --no-repo-verify -u git://github.com/Project-Xtended/manifest.git -b xr -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/HoleDroid/local_manifests.git --depth 1 -b x .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build
 source build/envsetup.sh
 lunch xtended_X00T-userdebug
 export TZ=Asia/Jakarta #put before last build command
