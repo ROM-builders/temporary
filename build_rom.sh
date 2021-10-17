2bd4634dc276974a41a8f098ea42f8a447ff0f4f
Teguh Budi Santoso <86312158+Tiktodz@users.noreply.github.com>
2021-10-17 21:52:53 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,11 @@
 # sync
 repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-17.1 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Tiktodz/local_manifests.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/Tiktodz/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build
 source build/envsetup.sh
 lunch lineage_X00TD-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Jakarta #put before last build command
 mka bacon
 
