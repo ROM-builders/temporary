5b6b07a9f34664e0acff0e2acf666b0ae2a2da74
second <82253361+HoleDroid@users.noreply.github.com>
2021-10-18 12:18:59 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/DotOS/manifest.git -b dot11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/HoleDroid/local-manifests.git --depth 1 -b dos .repo/local_manifests
+git clone https://github.com/HoleDroid/local_manifests.git --depth 1 -b dos .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
