dc2c9f03c53d92ede523abd66f1f1dc1b0b935db
Finix-Projetcs <jarbull87@gmail.com>
2021-10-18 23:21:44 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch xd_rosy-userdebug
+lunch xdroid_rosy-userdebug
 export TZ=Asia/Jakarta #put before last build command
 mka xd
 
