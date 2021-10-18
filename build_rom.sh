f40c0b8f16c00b49c9b4c4965f6b4db1664a512f
Debayan Kar <69067988+kardebayan@users.noreply.github.com>
2021-10-18 11:42:51 +0530

    Build userdebug

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch cipher_CPH1859-eng
+lunch cipher_CPH1859-userdebug
 export TZ=Asia/Kolkata #put before last build command
 make bacon -j$(nproc --all)
 
