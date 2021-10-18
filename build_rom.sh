e2263e05d204df31d241177ab56846ec83076f72
idkwhois <92702206+idkwhois@users.noreply.github.com>
2021-10-18 23:32:42 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExtended/manifest -b ace -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Kingler23q2/local_manifest --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Palladium-OS/platform_manifest -b 11 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/idkwhois/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_r5x-userdebug
+lunch palladium_lava-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon 
 
