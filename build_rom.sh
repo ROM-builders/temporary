dac08c00d43cad542355dcd7c9fbf90ed7dd21ca
Teguh Budi Santoso <86312158+Tiktodz@users.noreply.github.com>
2021-10-18 13:47:45 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build
 source build/envsetup.sh
 lunch lineage_X00T-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Jakarta #put before last build command
 mka bacon
 
