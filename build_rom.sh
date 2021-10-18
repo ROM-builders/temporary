020e41abbf1f504aebaad1a6a74f1760553874d2
horoid <59495159+horoidrom@users.noreply.github.com>
2021-10-18 20:58:09 +0530

    Update build_rom.sh
    kn

--- build_rom.sh
+++ build_rom.sh
@@ -2,6 +2,9 @@
 repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-17.1 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/horoidrom/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+git clone https://github.com/horoidrom/vendor_m01q.git --depth 1 vendor/samsung/m01q
+git clone https://github.com/horoidrom/samsung_m01q_device_tree.git --depth 1 device/samsung/m01q
+git clone https://github.com/horoidrom/samsung_common_tree_msm8937.git --depth 1 device/samsung/msm8937-common
 
 # build rom
 source build/envsetup.sh
