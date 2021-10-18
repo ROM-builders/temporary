10d0211b1ae723534bd16a6bdf79080547f985bd
Kingler23q2 <83709148+Kingler23q2@users.noreply.github.com>
2021-10-18 23:30:27 +0530

    merlinx-palladiumos-Akash23q2

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Corvus-R/android_manifest -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Akash23q2/local_manifest --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Palladium-OS/platform_manifest -b 11 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/Akash23q2/local_manifest --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch corvus_lava-userdebug
+lunch palladium_merlinx-userdebug
 export TZ=Asia/Dhaka #put before last build command
 make corvus
 
