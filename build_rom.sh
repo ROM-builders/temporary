54255949a77212c79124ba4fe440b67df3a909c2
ibratabian17 <71920907+ibratabian17@users.noreply.github.com>
2021-10-18 16:12:44 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch nad_A6020-userdebug
+export KBUILD_BUILD_USER=Ibratabian17
+export KBUILD_BUILD_HOST=WibuLu
 export TZ=Asia/Jakarta #put before last build command
 mka nad
 
