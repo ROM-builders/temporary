f805ce96951003c17ec67aea5f0c2e809c2f1ff7
horoid <92655627+horoid@users.noreply.github.com>
2021-10-19 13:21:50 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-17.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/horoidrom/local_manifest.git --depth 1 -b new .repo/local_manifests
+git clone https://github.com/horoid/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 
