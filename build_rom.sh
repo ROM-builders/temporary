b54faa2e8993c9138041333820fad11f27181043
Dev0786s <92157105+Dev0786s@users.noreply.github.com>
2021-10-18 21:45:12 -0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,14 @@
 #sync rom
-repo init --depth=1  -u https://github.com/Kang-OS-R/android_manifest -b eleven -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/ProjectRadiant/manifest -b eleven -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Dev0786s/local-manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
-lunch kangos_merlinx-userdebug
-SHIP_LAWNCHAIR := true
+. build/envsetup.sh
+lunch radiant_merlinx-userdebug
 USE_GAPPS := true
-SHIP_LAWNCHAIR := true
 export TZ=Asia/Dhaka #put before last build comman
-make bacon
+make bacon -jX
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
