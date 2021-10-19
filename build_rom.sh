f5f9dbc603378a2dee38bb59da8e1a53b391c40b
Dev786s <85985141+Dev786s@users.noreply.github.com>
2021-10-20 03:05:06 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch lighthouse_merlinx-user
+lunch lighthouse_merlinx-userdebug
 export TZ=Asia/Dhaka #put before last build comman
 make lighthouse
 
