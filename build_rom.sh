c8a4c9b621ce2f58f76b17717b1f306ad69f7b99
Aryan Arora <34274451+AruAA@users.noreply.github.com>
2021-10-19 11:59:44 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/ProjectRadiant/manifest -b twelve -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/ProjectRadiant/manifest -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/AruAA/local_manifest --depth 1 -b radiant .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
