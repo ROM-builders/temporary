dcb773d9ffdf359d5bea6f7643f1612bfea4010b
Rishawn <70256146+Stealth1226@users.noreply.github.com>
2021-10-20 18:55:21 +0530

    trigger: test ir fix

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 -u git://github.com/lighthouse-os/manifest.git -b raft -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Stealth1226/local_manifest --depth 1 -b h850 .repo/local_manifests
+repo init --depth=1 -u git://github.com/lighthouse-os/manifest.git -b raft -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Stealth1226/local_manifest --depth 1 -b raft-ir .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
