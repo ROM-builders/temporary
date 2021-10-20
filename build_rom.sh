68c6391a52d9f43c5b63ec162a4911acd72f18a3
Dev0786s <92157105+Dev0786s@users.noreply.github.com>
2021-10-21 02:13:53 +0530

    Arrow

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,15 @@
-# sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b 11 -g default,-mips,-darwin,-notdefault
+# sync rom 
+repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-11.0 -g default,-mips,-darwin,-notdefault
+
+ 
 git clone https://github.com/Dev0786s/local-manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
-lunch awaken_merlinx-user
+. build/envsetup.sh
+lunch arrow_merlinx-user
 export TZ=Asia/Dhaka #put before last build command
-make bacon
+m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
