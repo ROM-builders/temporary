91eaabe002f16ee9060a9b5b8c7848afd955a115
Dev786s <85985141+Dev786s@users.noreply.github.com>
2021-10-20 06:58:37 -0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/lighthouse-os/manifest.git -b sailboat -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/lighthouse-os/manifest.git -b raft -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Dev786s/local-manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 . build/envsetup.sh
 lunch lighthouse_merlinx-userdebug
 export TZ=Asia/Dhaka #put before last build comman
-make lighthouse
+mka lighthouse
 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
