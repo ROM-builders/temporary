b07110bbdc90cd678b1aa0a8d4d08543a6e5ebe6
abhigyan16 <65801637+abhigyan16@users.noreply.github.com>
2021-10-20 14:13:47 +0530

    FluidOS test #2

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/abhigyan16/local_manifests --depth 1 -b aicp .repo/
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+. build/envsetup.sh
 lunch fluid_sakura-user
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
