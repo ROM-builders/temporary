4da576d683fd0fd3996b0bb90426aadd89034f1e
WallBreaker2407 <92675483+WallBreaker2407@users.noreply.github.com>
2021-10-21 12:25:15 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/xdroid-CAF/xd_manifest.git -b eleven -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/xdroid-CAF/xd_manifest -b eleven -g default,-mips,-darwin,-notdefault
 git clone https://github.com/WallBreaker2407/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
