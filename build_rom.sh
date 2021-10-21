919144214ef92ef356393d9b707434ff4e70124e
SumonSN <56505303+SumonSN@users.noreply.github.com>
2021-10-21 18:43:59 +0600

    Added logger service

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch cipher_RMX3171-eng
+lunch cipher_RMX3171-userdebug
 export TZ=Asia/Dhaka #put before last build command
 make bacon -j$(nproc --all)
 
