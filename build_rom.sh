f199fb2bae53919ca775fc55882f983131cbec73
AesXily <92341490+aesxily@users.noreply.github.com>
2021-10-22 06:14:53 +0700

    X01BD-xdroid-CAF-aesxily
    change branch local manifests

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/xdroid-CAF/xd_manifest.git -b eleven -g default,-mips,-darwin,-notdefault
-git clone https://github.com/aesxily/local_manifest.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/aesxily/local_manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
