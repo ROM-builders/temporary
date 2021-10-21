612c0ef8b6b11c7355853d685127affc1eaac1f3
Dev0786s <92157105+Dev0786s@users.noreply.github.com>
2021-10-21 03:10:33 -0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -8,7 +8,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch lineage_merlinx-userdebug
-export TZ=Asia/Dhaka #put before last build commanda
+export TZ=Asia/Dhaka #put before last build commandad
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
