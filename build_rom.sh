5a2729d48975de34669394e03fda94633137d5e9
Teguh Budi Santoso <86312158+Tiktodz@users.noreply.github.com>
2021-10-21 08:12:04 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/xdroid-CAF/xd_manifest.git -b eleven -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Tiktodz/local_manifests.git --depth 1 -b xdroid .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git.git -b lineage-17.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Tiktodz/local_manifests.git --depth 1 -b los .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch xdroid_X00TD-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
-export ALLOW_MISSING_DEPENDENCIES=true
+lunch lineage_X00T-userdebug
 export TZ=Asia/Jakarta #put before last build command
-make xd -j$(nproc --all)
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
