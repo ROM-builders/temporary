1a0c9f7f999485179fd294215d0ff42590f1d5d3
ibratabian17 <71920907+ibratabian17@users.noreply.github.com>
2021-10-21 19:35:20 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,13 @@
 # sync rom
 repo init --depth=1 -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ibraaltabian17/local_manifest.git --depth 1 -b lineage18 .repo/local_manifests
+git clone https://github.com/ibraaltabian17/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch lineage_A6020-userdebug
 export LINEAGE_VERSION_APPEND_TIME_OF_DAY=true
+export ALLOW_MISSING_DEPENDENCIES=true
 export SKIP_ABI_CHECKS=true
 export SKIP_API_CHECKS=true
 export TZ=Asia/Jakarta #put before last build command
