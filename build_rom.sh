a95467dc307c2ff919f4f2849ec00180188ce401
Thoreck <46625454+fajar4561@users.noreply.github.com>
2021-10-21 07:41:48 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom (Sinkron kode Sumber Rom)
-repo init --depth=1 --no-repo-verify -u https://github.com/Komodo-OS-Rom/manifest -b 11 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/Komodo-OS-Rom/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/fajar4561/local_manifest.git --depth 1 -b komodo .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 # build
