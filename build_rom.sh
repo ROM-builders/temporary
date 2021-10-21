ddae44dd2ed1d41e67541039d4434f8ff1c8a7a8
ibratabian17 <71920907+ibratabian17@users.noreply.github.com>
2021-10-21 12:24:34 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,8 +6,11 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_A6020-userdebug
+export LINEAGE_VERSION_APPEND_TIME_OF_DAY=true
+export SKIP_ABI_CHECKS=true
+export SKIP_API_CHECKS=true
 export TZ=Asia/Jakarta #put before last build command
-mka bacon
+mka bacon -j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
