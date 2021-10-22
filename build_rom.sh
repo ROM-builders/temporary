75b3051f681340bd1cd321d9ac1eb45c59b2eb6c
ibratabian17 <71920907+ibratabian17@users.noreply.github.com>
2021-10-22 16:45:49 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,9 +7,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch nad_A6020-userdebug
 export KBUILD_BUILD_USER=Ibratabian17
-export KBUILD_BUILD_HOST=WibuLu
 export TZ=Asia/Jakarta #put before last build command
-mka nad
+mka nad -j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
