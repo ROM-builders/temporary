fe07141afef93ae0dd452e0818d9c0d50e6584e0
Dev0786s <92157105+Dev0786s@users.noreply.github.com>
2021-10-22 08:14:27 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -8,6 +8,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch aosp_merlinx-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build commands
 mka bacon 
 
