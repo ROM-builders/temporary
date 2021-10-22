9992a72c3d327b3f252188fdebb7db58d9f8e595
Finix-Projetcs <jarbull87@gmail.com>
2021-10-22 19:38:06 +0700

    Xdroid uptade rev1.7.x

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,15 @@
+rm -rf xdroid-CAF
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Palladium-OS/platform_manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/AnGgIt88/local_manifest.git --depth 1 -b pldm .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-CAF/xd_manifest -b eleven -g default,-mips,-darwin,-notdefault
+git clone https://github.com/AnGgIt88/local_manifest.git --depth 1 -b eleven .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom saiki
+# build rom
 source build/envsetup.sh
-lunch palladium_rosy-userdebug
-export TZ=Asia/Jakarta #put before last build command
-mka palladium
+lunch xdroid_rosy-userdebug
+export TZ=Asia/Jakarta
+export ALLOW_MISSING_DEPENDENCIES=true
+make xd
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
