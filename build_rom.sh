1bc58828e1d244c00ea039f898c199860b7dcd96
rizengt <84280317+rizengt@users.noreply.github.com>
2021-10-22 13:16:05 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/PotatoProject/manifest.git -b dumaloo-release -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/ForkLineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/rizengt/local_manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch posp_vince-eng
+lunch lineage_$device-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
-mka posp
+make bacon -j$(nproc --all)
 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
