0924e19b8634a756c800808eceed5941a667c440
NFS Projects <68986092+AnGgIt86@users.noreply.github.com>
2021-10-22 10:27:24 +0700

    change branch source

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
-# sync rom source
+# sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b spark -g default,-mips,-darwin,-notdefault
 git clone https://github.com/AnGgIt86/local_manifest.git --depth=1 -b eleven .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom source
+# build rom
 source build/envsetup.sh
 lunch spark_rosy-userdebug
 export TZ=Asia/Jakarta #put before last build command
