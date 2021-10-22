0b703d63d0668b85bfb39fa4aa740e6c60367472
Jagaddhita Jalu <88699126+jaguarezz@users.noreply.github.com>
2021-10-23 05:31:48 +0700

    CesiumOS vayu

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ShapeShiftOS/android_manifest.git -b android_12 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/CesiumOS-org/manifest.git -b eleven -g default,-mips,-darwin,-notdefault
 git clone https://github.com/jaguarior/local_manifests.git --depth=1 -b vayu .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-. build/envsetup.sh
-lunch ssos_vayu-eng
+source build/envsetup.sh
+lunch cesium_vayu-eng
 export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Jakarta #put before last build command
