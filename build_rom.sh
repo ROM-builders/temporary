8fa30c9281143c8953927086698499d1513aa032
Dev0786s <92157105+Dev0786s@users.noreply.github.com>
2021-10-22 06:51:06 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch aosp-merlinx_userdebug
+lunch aosp_merlinx-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon 
 
