f6649e371a67bfa87086aade9f0d34a2e6f40511
NFS Projects <68986092+AnGgIt86@users.noreply.github.com>
2021-10-23 04:17:16 +0700

    Fix error

--- build_rom.sh
+++ build_rom.sh
@@ -6,8 +6,9 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch spark_rosy-user
-export TZ=Asia/Jakarta #put before last build command
-mka spark
+export TZ=Asia/Jakarta
+export ALLOW_MISSING_DEPENDENCIES=true
+mka spark -j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
