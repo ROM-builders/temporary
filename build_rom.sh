d247aad852f77e02fa44bb3a31dab0eb0329a3b2
Cliquers <abenkenari@gmail.com>
2021-10-23 17:01:14 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch du_X00TD-userdebug
+lunch du_X00TD-eng
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Jakarta #put before last build command
 make corvus
