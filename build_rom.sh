ce23deeca2571f6f07be64393e411d9a20864dc8
hsx02 <hsx02self@gmail.com>
2021-10-23 12:44:26 +0530

    build: inital PixelExperience

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/ArrowOS/android_manifest -b arrow-12.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/hsx02/Local-Manifests.git --depth 1 -b common .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://https://github.com/PixelExperience-Staging/manifest -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/hsx02/Local-Manifests.git --depth 1 -b pe-12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
 export SELINUX_IGNORE_NEVERALLOWS=true
-lunch arrow_pine-userdebug
+lunch aosp_pine-userdebug
 export TZ=Asia/Dhaka #put before last build commanda
-m bacon
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
