5557e9cc4bc4bc51169d5a9a4847cacc711b13ea
Hitesoni | ×͜× <72127977+hsx02@users.noreply.github.com>
2021-10-23 22:29:23 +0530

    Fixup

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 . build/envsetup.sh
 export SELINUX_IGNORE_NEVERALLOWS=true
 lunch aosp_pine-userdebug
-export TZ=Asia/Dhaka #put before last build commanda
+export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
