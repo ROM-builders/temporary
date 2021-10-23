8052c8fc9614631e97a04810a65d0cc4e3105f8f
Teguh Budi Santoso <86312158+Tiktodz@users.noreply.github.com>
2021-10-23 12:58:08 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
-# sync rom
+# sync
 repo init --depth=1 --no-repo-verify -u git://github.com/Corvus-Q/android_manifest.git -b 10 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Tiktodz/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build
 source build/envsetup.sh
 lunch du_X00TD-userdebug
 export TZ=Asia/Jakarta #put before last build command
