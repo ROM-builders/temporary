7c2837d613899c6e441731784a15df882342ce9a
Hitesoni | ×͜× <72127977+hsx02@users.noreply.github.com>
2021-10-23 13:41:27 +0530

    fix up

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://https://github.com/PixelExperience-Staging/manifest -b twelve -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/PixelExperience-Staging/manifest -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/hsx02/Local-Manifests.git --depth 1 -b pe-12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
