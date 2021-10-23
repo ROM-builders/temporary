d4b34e4bf920350b39bf7f209a13557610808ac6
Dev786s <85985141+Dev786s@users.noreply.github.com>
2021-10-23 18:40:27 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-12.0  -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b eleven-plus -g default,-mips,-darwin,-notdefault
 
 
 git clone https://github.com/Dev786s/local-manifest.git --depth 1 -b main .repo/local_manifests
@@ -7,11 +7,11 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch arrow_merlinx-eng
+lunch aosp_merlinx-user
 export ALLOW_MISSING_DEPENDENCIES=true 
 export BUILD_BROKEN_DUP_RULES=true
 export TZ=Asia/Dhaka #put before last build command
-m otapackage
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
