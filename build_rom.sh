a1fa14c7e6651597ce7a31f76a7c4ca37185b3c3
Dev0786s <92157105+Dev0786s@users.noreply.github.com>
2021-10-23 09:24:47 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 . build/envsetup.sh
 lunch lighthouse_merlinx-eng
 export ALLOW_MISSING_DEPENDENCIES=true 
-export BUILD BROKEN_MISSING_REQUIRED_MODULES=true
+export BUILD BROKEN_MISSING_REQUIRED_MODULES=true 
+export BUILD_BROKEN_DUP_RULES=true
 export TZ=Asia/Dhaka #put before last build command
 make lighthouse
 
