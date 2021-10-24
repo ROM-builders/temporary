6ceece64c02585a3458d267fe8819f9543c3d0b0
rizengt <84280317+rizengt@users.noreply.github.com>
2021-10-24 18:01:32 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -8,7 +8,7 @@ source build/envsetup.sh
 lunch lineage_vince-eng
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
-brunch vince
+make bacon
 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
