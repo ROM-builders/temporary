f3b4557945bb81a1c43bc73ef0c5ff602e3e86b8
hsx02 <hsx02self@gmail.com>
2021-10-24 12:16:22 +0530

    build: added selinux neverallows

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch aosp_pine-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build commanda
 mka bacon
 
