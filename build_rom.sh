4fc1851854ee7c454b6d82b048db2703fb5b22ee
rizengt <84280317+rizengt@users.noreply.github.com>
2021-10-24 17:55:40 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PotatoProject/manifest.git -b dumaloo-release -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/ForkLineageOS/android.git -b lineage 18.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/rizengt/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch potato_vince-user
+lunch lineage_vince-eng
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
 brunch vince
