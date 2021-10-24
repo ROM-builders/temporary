9cbc6350f22c6b3b4a02cd605f8a21cf6216b24e
Finix-Projetcs <jarbull87@gmail.com>
2021-10-24 20:38:45 +0700

    Test build xdroid 11 witch GCC 12

--- build_rom.sh
+++ build_rom.sh
@@ -1,17 +1,17 @@
 # Finix Projects 2021
 # Thanks to @Apon77
-# sync rom
 
-repo init --depth=1 --no-repo-verify -u https://github.com/ArcaneOS/Arcane_manifest -b R -g default,-mips,-darwin,-notdefault
-git clone https://github.com/AnGgIt88/local_manifest.git --depth=1 -b pldm .repo/local_manifests
+# sync rom
+repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-CAF/xd_manifest -b eleven -g default,-mips,-darwin,-notdefault
+git clone https://github.com/AnGgIt88/local_manifest.git --depth=1 -b eleven .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_rosy-userdebug
+lunch xdroid_rosy-userdebug
 export TZ=Asia/Jakarta
 export ALLOW_MISSING_DEPENDENCIES=true
-mka bacon
+make xd
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
