055f22ed358a0957dc78b867d1cc5885ec62e065
Quang Anh <60723055+quxngeh@users.noreply.github.com>
2021-10-25 04:56:04 +0700

    that was not giving styxos

--- build_rom.sh
+++ build_rom.sh
@@ -1,3 +1,4 @@
+# blah blah blah
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/StyxProject/manifest -b R -g default,-mips,-darwin,-notdefault
 git clone https://github.com/quxngeh/local_manifest.git --depth 1 -b main .repo/local_manifests
