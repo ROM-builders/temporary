95c17d306e88dcb1640ec4eaf1b37d6216289454
MiSrA665 <87112984+MiSrA665@users.noreply.github.com>
2021-10-25 19:33:39 +0700

    Test again rr 873

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,8 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ResurrectionRemix/platform_manifest.git -b Q default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/ResurrectionRemix/platform_manifest.git -b Q default,-mips,-darwin,-notdefault
 git clone https://github.com/MiSrA665/ResurrectionRemix_Mido_Unnoficial/commit/8773522df0544a4b4795137d50da0f9ee3820106 --depth 1 -b master .repo/local_manifests
-repo sync --force-sync --no-clone-bundle
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
 
 # build rom
 . build/envsetup.sh
