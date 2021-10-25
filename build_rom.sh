0a419248a2446eab68cd4ba0812d585a6a4208f8
MiSrA665 <87112984+MiSrA665@users.noreply.github.com>
2021-10-25 19:23:42 +0700

    RROS 873 for mido

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/ResurrectionRemix/manifest.git -b ResurrectionRemix-8.7.3 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/MiSrA665/ResurrectionRemix_Mido_Unnoficial/blob/main/.github/workflows/local_manifest.xml --depth 1 -b master .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo init --depth=1 --no-repo-verify -u https://github.com/ResurrectionRemix/platform_manifest.git -b Q default,-mips,-darwin,-notdefault
+git clone https://github.com/MiSrA665/ResurrectionRemix_Mido_Unnoficial/commit/8773522df0544a4b4795137d50da0f9ee3820106 --depth 1 -b master .repo/local_manifests
+repo sync --force-sync --no-clone-bundle
 
 # build rom
-source build/envsetup.sh
-lunch rros_mido-user
+. build/envsetup.sh
+lunch rr_mido-userdebug
 export TZ=Asia/Jakarta #put before last build command
-mka rros
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
