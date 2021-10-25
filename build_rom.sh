7aa7b6ccdcf517ac303b1d173cfe9061d191aa66
MiSrA665 <87112984+MiSrA665@users.noreply.github.com>
2021-10-25 22:39:28 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/ResurrectionRemix/platform_manifest.git -b Q default,-mips,-darwin,-notdefault
-git clone https://github.com/MiSrA665/ResurrectionRemix_Mido_Unnoficial/commit/8773522df0544a4b4795137d50da0f9ee3820106 --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/ProjectRadiant/manifest -b twelve default,-mips,-darwin,-notdefault
+git clone https://github.com/MiSrA665/Project/blob/main/local_manifest.xml --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 
 # build rom
 . build/envsetup.sh
-lunch rr_mido-userdebug
+lunch radiant_$mido-$user
 export TZ=Asia/Jakarta #put before last build command
-mka bacon
+mka bacon -j$
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
