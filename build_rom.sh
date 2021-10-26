535e041d1894d0a4623d53cf10ad3f00a2969fb4
xoo2001 <ekora2001@gmail.com>
2021-10-26 07:23:23 +0700

    hot10: fix prebuilt kernel

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/Los-FE/manifest.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Infinix-MT6768-LDM/local_manifests.git --depth 1 -b los .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/Los-FE/manifest.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Infinix-MT6768-Series/local_manifest.git --depth 1 -b eleven .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
