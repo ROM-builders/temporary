4d78cf5d8ca98d26d325bc87bbfd8e51535544d2
jhonatan Mikael <75139386+jhonnytech90@users.noreply.github.com>
2021-10-27 20:46:16 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -2,24 +2,6 @@
 repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/jhonnytech90/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-cd /home/cirrus/roms/lineage/device
-mkdir -p asus
-cd asus
-git clone https://github.com/jhonnytech90/device_asus_A001D -b main
-mv device_asus_A001D A001D
-
-cd /home/cirrus/roms/lineage/vendor
-mkdir -p asus
-git clone https://github.com/jhonnytech90/vendor_asus_A001D -b main
-mv vendor_asus_A001D A001D
-cd /home/cirrus/roms/lineage/kernel
-mkdir -p asus
-git clone https://github.com/jhonnytech90/kernel_asus_A001D -b main
-mv kernel_asus_A001D A001D
-cd /home/cirrus/roms/lineage/kernel/A001D
-git clone https://github.com/jhonnytech90/kernel_asus_A001D -b main
-mv kernel_asus_A001D msm-3.18
-cd /home/cirrus/roms/lineage
 
 # build rom
 source build/envsetup.sh
