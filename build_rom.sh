0600894560bdd2de7026a29165c070dd3d08c38c
techosofficial <86093157+techosofficial@users.noreply.github.com>
2021-10-27 19:47:33 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/PixelBlaster-OS/manifest -b pixelblaster-OS-3.5 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/PixelBlaster-OS/manifest -b eleven -g default,-mips,-darwin,-notdefault
 git clone https://github.com/techosofficial/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
