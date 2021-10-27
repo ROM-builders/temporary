ac6a8b412a331b913cc126242eafa28881840dac
Raavan dk <91897724+RAAVANDK@users.noreply.github.com>
2021-10-27 22:35:58 +0530

    merlinx pixelextended RAAVANDK
    bhai ab to bn ja

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExtended/manifest -b ace -g default,-mips,-darwin,-notdefault
-git clone -u https://github.com/PixelExtended/manifest -b ace --depth=1 -b master .repo/local_manifests
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+repo init --depth=1 --no-repo-verify -u git://github.com/PixelExtended/manifest -b ace -g default,-mips,-darwin,-notdefault
+git clone https://github.com/RAAVANDK/local-manifest.git --depth 1 -b main .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
