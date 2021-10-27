42dd7aaee9b5e00698a785f3d5ca17056bf7d574
Egor Kostylev <76757246+nekoshirro@users.noreply.github.com>
2021-10-27 13:08:20 +0700

    git: add github identity

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,6 @@
 # sync rom
+git config --global user.name nekoshirro
+git config --global user.email ais.muzakky@gmail.com
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelBlaster-OS/manifest -b eleven -g default,-mips,-darwin,-notdefault
 git clone https://github.com/nekoshirro/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
