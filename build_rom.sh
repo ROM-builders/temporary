48c10e2cf6bc6b2cc38d84455f597838334b5c90
Evgeniy <zhenyolka@gmail.com>
2021-10-27 23:39:10 +0300

    Build POSP with Android 12

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PotatoProject/manifest -b dumaloo-release -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/zhenyolka/android_manifest.git --depth 1 -b lineage-18.1 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PotatoProject/manifest -b frico-release -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/zhenyolka/pyxis-manifest-a12.git --depth 1 -b posp-12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
