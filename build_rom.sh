fcbee797e791e2fac7a2dcca3a78a9f5ab8ac395
mobx <67450638+mobxCode@users.noreply.github.com>
2021-10-27 15:49:33 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,19 @@
+# Copyright (c) 2021, The Linux Foundation. All rights reserved.
+# Thankz to @Apon77
+
+# Date 27-Oct-2021
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DotOS/manifest.git -b dot11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/mobxCode/local_manifest --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/DotOS/manifest.git -b dot11 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/mobxCode/local_manifest.git --depth=1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch dot_juice-userdebug
-export TZ=Asia/Jakarta #put before last build command
-make bacon -j$(nproc --all)
-
+export TZ=Asia/Jakarta
+export ALLOW_MISSING_DEPENDENCIES=true
+export LOCALVERSION=-DotOS
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
