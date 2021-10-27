c3d90ec1d578e2b1929f25043efbb042150630d2
Egor Kostylev <76757246+nekoshirro@users.noreply.github.com>
2021-10-27 13:06:31 +0700

    fix wrong branch

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,7 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/PixelBlaster-OS/manifest -b eleven -g default,-mips,-darwin,-notdefault
-git clone https://github.com/nekoshirro/local_manifest.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelBlaster-OS/manifest -b eleven -g default,-mips,-darwin,-notdefault
+git clone https://github.com/nekoshirro/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j1 --fail-fast
 
 # build rom
