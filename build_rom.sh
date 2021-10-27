ce580b596940ffaacc72bdb9cced2151e8664ebb
mobx <67450638+mobxCode@users.noreply.github.com>
2021-10-27 15:01:11 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch dot-userdebug
+lunch dot_juice-userdebug
 export TZ=Asia/Jakarta #put before last build command
 make dot -j$(nproc --all)
 
