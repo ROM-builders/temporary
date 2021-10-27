921b54d79927de287d24af628ac27343271e9f35
zhenyolka <zhenyolka@gmail.com>
2021-10-27 23:42:43 +0300

    Fix repo init

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PotatoProject/manifest -b frico-release -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/PotatoProject/manifest -b frico-release -g default,-mips,-darwin,-notdefault
 git clone https://github.com/zhenyolka/pyxis-manifest-a12.git --depth 1 -b posp-12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
