f7b07773f54d9118b4b63a51ad917ea70256a085
Nazar Kovalenko <85736144+Jaxer159@users.noreply.github.com>
2021-10-27 13:51:53 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manif
 git clone https://github.com/Jaxer159/manifests --depth 1 -b pe_temporary .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build pe
+# build rom
 . build/envsetup.sh
 lunch aosp_olives-user
 export TZ=Asia/Dhaka #put before last build command
