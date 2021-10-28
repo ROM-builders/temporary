5e98c7d72a2f24e328f0df192e14efd71ea1193b
Gotam919 <82812329+Gotam919@users.noreply.github.com>
2021-10-29 00:03:19 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init —depth=1 --no-repo-verify -u git://github.com/crdroidandroid/android.git -b 11.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Gotam919/Local-Manifest.git --depth 1 -b main .repo/local_manifests
+git clone —depth=1 https://github.com/Gotam919/local_manifest -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
