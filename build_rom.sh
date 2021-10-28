c213da6cd41ed0cb8fc2b8cda4f21c0e2add43bc
Droneship <81514881+Seifood@users.noreply.github.com>
2021-10-28 21:26:31 +0200

    Fix sync error

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelBlaster-OS/manifest -b 12 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelBlaster-Temp/manifest -b 12 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Seifood/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch aosp_gauguin-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
