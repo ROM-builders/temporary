2344b2173ba8ee233d4f5acea042f1d55e0563f2
ibratabian17 <71920907+ibratabian17@users.noreply.github.com>
2021-10-28 08:44:59 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch arrow_A6020-userdebug
+lunch aosip_A6020-userdebug
 export KBUILD_BUILD_USER=Ibratabian17
 export TZ=Asia/Jakarta #put before last build command
 mka bacon -j$(nproc --all)
