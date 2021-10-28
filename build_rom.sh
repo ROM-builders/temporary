645519bd567e1188ef8eb09252a0c9ec77ed7df6
ibratabian17 <71920907+ibratabian17@users.noreply.github.com>
2021-10-29 04:07:06 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/AOSiP/platform_manifest.git -b eleven -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ibraaltabian17/local_manifest.git --depth 1 -b AOSiP .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-11.0 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/ibraaltabian17/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
