6964e4c6944bd35dff332565c81d77a27e12e0f5
Raavan dk <91897724+RAAVANDK@users.noreply.github.com>
2021-10-28 07:25:58 +0530

    merlinx Pixys-OS RAAVANDK
    wen build sir

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/PixelExtended/manifest -b ace -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/PixysOS/manifest -b eleven -g default,-mips,-darwin,-notdefault
 git clone https://github.com/RAAVANDK/local-manifest.git --depth 1 -b main .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync  --force-sync --current-branch --no-tags --no-clone-bundle --optimized-fetch --prune -j$(nproc --all)
 
 # build rom
-source build/envsetup.sh
-lunch derp_vayu-user
+source build/build/envsetup.sh
+lunch pixys_$device-userdebug
+make pixys -j8
 export TZ=Asia/kolkata #put before last build command
 mka derp
 
