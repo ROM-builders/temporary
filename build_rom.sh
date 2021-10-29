61567d2c76e95c4e64fb5751c4589096812cb059
Harikumar <62538135+phoenix-1708@users.noreply.github.com>
2021-10-29 15:41:57 +0530

    Edited build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-11.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/phoenix-1708/local_manifest.git -b arrow-11 --depth 1 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-12.0 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/phoenix-1708/local_manifest.git -b main --depth 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
