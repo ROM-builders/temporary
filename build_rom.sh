040380e4e839840e94893e3e6850df2491df8a2a
MiSrA665 <87112984+MiSrA665@users.noreply.github.com>
2021-10-29 15:49:12 +0700

    Try HentaiOS

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/exthmui/android.git -b exthm-11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/MiSrA665/Local-Manifest.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/hentaiOS/platform_manifest -b Rika -g default,-mips,-darwin,-notdefault
+git clone https://github.com/MiSrA665/Local-Manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-. build/envsetup.sh && lunch exthm_mido-userdebug && mka bacon
+. build/envsetup.sh
+lunch hentai_mido-userdebug
 export TZ=Asia/Jakarta #put before last build command
+make otapackage
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
