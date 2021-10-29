d17f3399a93fcb9ee9f8fcfdb60bdfba064cbb5b
MiSrA665 <87112984+MiSrA665@users.noreply.github.com>
2021-10-29 19:51:22 +0700

    RROS Mido Test

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-19.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/MiSrA665/Local-Manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ResurrectionRemix/platform_manifest.git -b Q -g default,-mips,-darwin,-notdefault
+git clone https://github.com/MiSrA665/Local-Manifest.git --depth ten -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
-lunch lineage_mido-userdebug
+. build/envsetup.sh
+lunch rr_mido-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Jakarta #put before last build command
 mka bacon
 
