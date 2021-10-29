c5bb8c3ecc66479f5b8fffc860230090324e6b2d
MiSrA665 <87112984+MiSrA665@users.noreply.github.com>
2021-10-29 09:01:58 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,17 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/exthmui/android.git -b exthm-11 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/WeebProjekt/platform_manifest -b cheese -g default,-mips,-darwin,-notdefault
 git clone https://github.com/MiSrA665/Local-Manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-. build/envsetup.sh
-lunch exthm_mido-userdebug
-export SKIP_ABI_CHECKS=true
-export SELINUX_IGNORE_NEVERALLOWS=true
-export TEMPORARY_DISABLE_PATH_RESTRICTIONS=true
+source build/envsetup.sh
+lunch weeb_$mido-userdebug
 export TZ=Asia/Jakarta #put before last build command
-mka bacon
-
+make weeb-prod -j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
