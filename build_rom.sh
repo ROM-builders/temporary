27c70e46646657c32a4e4b3938ae038497bc3d7e
Optimaxdi <33113347+zacky93@users.noreply.github.com>
2021-10-29 20:17:25 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ProjectRadiant/manifest -b twelve -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Fluid/manifest.git -b fluid-11 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/zacky93/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch radiant_juice-userdebug
+lunch fluid_juice-userdebug
 export TZ=Asia/Jakarta #put before last build command
 mka bacon
 
