0760c7538c6ccc31956d10e970e58ef42bd309bb
Krishdeokate <85944593+Krishdeokate@users.noreply.github.com>
2021-10-29 20:23:15 +0530

    <whyred>-<ssos>-<Krishdeokate>

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch derp_whyred-user
+lunch ssos_whyred-user
 export TZ=Asia/Dhaka #put before last build command
 mka ssos
 
