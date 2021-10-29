a95cf547e5b1a1eafc61ea3eeba24e18ee6b9e62
MiSrA665 <87112984+MiSrA665@users.noreply.github.com>
2021-10-29 12:50:51 +0700

    Fix Commit again

--- build_rom.sh
+++ build_rom.sh
@@ -4,8 +4,10 @@ git clone https://github.com/MiSrA665/Local-Manifest.git --depth 1 -b master .re
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-. build/envsetup.sh && lunch exthm_mido-userdebug && mka bacon
+. build/envsetup.sh
+lunch exthm_mido-userdebug
 export TZ=Asia/Jakarta #put before last build command
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
