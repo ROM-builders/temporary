528d8da9da2871d0ec91eebcfedcec59d7694913
jhonatan Mikael <75139386+jhonnytech90@users.noreply.github.com>
2021-10-29 02:31:10 -0300

    A001D-lineageOS-JhonatanMikael
    FIX MANIFEST

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_A001D-userdebug
+lunch lineage_A001D-eng
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
