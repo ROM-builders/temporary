c75b0c6c4efa81429878b3c63b8515503d17afce
Naveen <naveen4264nk@gmail.com>
2021-10-29 09:44:45 +0530

    Typo mistakes

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/ProjectRadiant/manifest
 git clone https://github.com/N4veenNK/local-manifest.git --depth 1 -b radiant .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+#  build rom
 . build/envsetup.sh
 lunch radiant_X00TD-userdebug
 export TZ=Asia/Kolkata #put before last build command
