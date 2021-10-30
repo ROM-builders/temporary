be250c38124a02c9981164d195d614497f06b352
Chio <93084100+aberchio@users.noreply.github.com>
2021-10-31 05:44:55 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch vayu-userdebug
+export WITH_GAPPS=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Jakarta #put before last build command
