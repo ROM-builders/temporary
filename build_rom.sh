72f9f7b10b664d3c2ea632db7a472ee1441c1d8d
SriBalaji <86469621+iamthecloverly@users.noreply.github.com>
2021-10-31 00:05:25 +0530

    RMX2001: Build with Gapps

--- build_rom.sh
+++ build_rom.sh
@@ -8,6 +8,11 @@ source build/envsetup.sh
 lunch cipher_RMX2001-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon -j$(nproc --all)
+export CIPHER_GAPPS=true
+export TARGET_FACE_UNLOCK_SUPPORTED=true
+export TARGET_USES_BLUR=true
+export SKIP_ABI_CHECKS=true
+export SKIP_API_CHECKS=true
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
