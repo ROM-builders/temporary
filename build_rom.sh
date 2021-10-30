3845bb598cee41d6d5820aee7e6830bcca45f019
Raavan dk <91897724+RAAVANDK@users.noreply.github.com>
2021-10-30 23:26:54 +0530

    Merlin pixelblaster
    Techos

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,15 @@
 # sync rom 
 repo init --depth=1 --no-repo-verify -u git:///github.com/PixelBlaster-OS/manifest -b eleven -g default,-mips,-darwin,-notdefault
-git clone https://github.com/RAAVANDK/local-manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/Redmi-MT6768/local-manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
 lunch aosp_merlinx-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true 
-export BUILD_BROKEN_DUP_RULES=true 
 
-export TZ=Asia/Dhaka #put before last build command
+
+export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
