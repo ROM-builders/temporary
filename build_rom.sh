9c05b94b9e8c2bfc8bafd75217d0a3d2c7e72e20
Harikumar <62538135+phoenix-1708@users.noreply.github.com>
2021-10-30 10:02:25 +0530

    Edited build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,15 @@
 # sync rom
 
-repo init --depth=1 --no-repo-verify -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-mips,-darwin,-notdefault
-git clone https://github.com/phoenix-1708/local_manifest.git -b aex --depth 1 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-11.0 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/phoenix-1708/local_manifest.git -b arrow-11 --depth 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 
 source build/envsetup.sh
-lunch aosp_sweet-userdebug
+lunch arrow_sweet-userdebug
 export TZ=Asia/Dhaka #put before last build command
-m aex
+m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
