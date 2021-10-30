a804f4e618daf30d088597a2a530251e3378ee9f
RasyidAlKautsar <87112984+MiSrA665@users.noreply.github.com>
2021-10-30 18:56:52 +0700

    Fix Commit

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/WeebProjekt/platform_manifest -b reborn -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/WeebProjekt/platform_manifest -b reborn -g default,-mips,-darwin,-notdefault
 git clone https://github.com/MiSrA665/Local_Manifests.git depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
