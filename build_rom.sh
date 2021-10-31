b9603b13518deb5ed14d66f52e0762302db868f7
Hasin Abrar Haque <70853665+AbrarNoob@users.noreply.github.com>
2021-10-31 15:22:58 +0600

    Initial

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/AbrarNoob/local_manifest.git --depth 1 -b awaken .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b 12 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/AbrarNoob/local_manifest.git --depth 1 -b awa .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
