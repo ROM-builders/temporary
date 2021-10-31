09eea82a48a936f5fc77dfcefe03aa00501c79fc
Alone0316 <70204635+Hunter-commits@users.noreply.github.com>
2021-10-31 17:50:44 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ProjectRadiant/manifest -b eleven -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/ArcaneOS/Arcane_manifest -b R -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Hunter-commits/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch radiant_mido-user
+lunch aosp_mido-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
