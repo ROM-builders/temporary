ad520d641068d7bea411809a5915d2d7932c2d8e
Sahil Ahmed <73250691+sahilhv@users.noreply.github.com>
2021-10-31 11:47:54 +0530

    build.sh: Init fluid 11

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/Project-Fluid/manifest.git -b fluid-11
- -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/Project-Fluid/manifest.git -b fluid-11 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/sahilhv/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
