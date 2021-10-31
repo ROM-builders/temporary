912ebe942592a19ca67393fb49b4ac472bdc0eef
Harikumar <62538135+phoenix-1708@users.noreply.github.com>
2021-10-31 15:51:30 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 
 repo init --depth=1 --no-repo-verify -u https://github.com/Corvus-R/android_manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/phoenix-1708/local_manifest.git -b corvus-11 --depth 1 .repo/local_manifests
+git clone https://github.com/phoenix-1708/local_manifest.git -b corvus-11_test --depth 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
