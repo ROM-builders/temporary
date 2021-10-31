1a9d595eb188c0e3d6e5375e4e8b7c93ed0177ab
Raavan dk <91897724+RAAVANDK@users.noreply.github.com>
2021-10-31 08:12:52 +0530

    Update build_rom.sh
    Merlix pixelblaster

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom 
 repo init --depth=1 --no-repo-verify -u git:///github.com/PixelBlaster-OS/manifest -b eleven -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Redmi-MT6768/local-manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/RAAVANDK/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -10,7 +10,7 @@ export ALLOW_MISSING_DEPENDENCIES=true
 
 
 export TZ=Asia/Kolkata #put before last build command
-mka bacon
+mka bacon -j8
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
