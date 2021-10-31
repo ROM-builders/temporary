73c3c2209b529f5666b4d5f6704403d9ecadc577
indi121 <91513844+indi121@users.noreply.github.com>
2021-11-01 04:39:06 +0545

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync ro
-repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-17.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/indi121/local_manifests.git --depth 1 -b los .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ResurrectionRemix/platform_manifest.git -b Q -g default,-mips,-darwin,-notdefault
+git clone https://github.com/indi121/local_manifests.git --depth 1 -b rr1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_vince-user
+lunch rr_vince-user
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
