bede37143d06393938e5cdc90edf63823077c149
Raavan dk <91897724+RAAVANDK@users.noreply.github.com>
2021-11-01 23:38:35 +0530

    Bhsdk kb bnegi

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,16 @@
 # sync rom 
-repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/RAAVANDK/local_manifest.git --depth 1 -b main .repo/local_manifest
-repo sync
+repo init --depth=1 --no-repo-verify -u https://github.com/PixysOS/manifest -b eleven -g default,-mips,-darwin,-notdefault
+git clone https://github.com/RAAVANDK/Local-Manifest.git --depth 1 -b master .repo/local_manifest
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
+
 . build/envsetup.sh
+lunch pixys_merlinx-userdebug
+make pixys -j8 
 
-
-export TZ=Asia/Kolkata #put before last build command
-mka bacon -j8
+export TZ=Asia/kolkata #put before last build command
+make pixys -j8
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
