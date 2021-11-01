5d44d3172ef55e052b9204648228a2cd548bf43e
Hitesoni | ×͜× <72127977+hsx02@users.noreply.github.com>
2021-11-01 22:03:11 +0530

    Build elixir pine

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/hsx02/manifest.git -b snow  -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/Project-Elixir/manifest.git -b snow  -g default,-mips,-darwin,-notdefault
 git clone https://github.com/hsx02/Local-Manifests.git --depth 1 -b pe-12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
