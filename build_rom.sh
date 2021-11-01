1c87510467fad58e42ae4210b604887347f29e9b
AllMight007 <86153221+AllMight007@users.noreply.github.com>
2021-11-01 18:05:33 +0530

    Build corvus for sweet

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,16 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b elle -g default,-mips,-darwin,-notdefault
-git clone https://github.com/AllMight007/local_manifest.git --depth 1 -b laurel_evo .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo init --depth=1 --no-repo-verify -u https://github.com/Corvus-R/android_manifest.git -b 11 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/AllMight007/local_manifest.git --depth 1 -b corvus11 .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --opthttps://github.com/AllMight007/local_manifest.git --depthimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch evolution_laurel_sprout-userdebug
+lunch corvus_sweet-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
 export SKIP_ABI_CHECKS=true
 export SKIP_API_CHECKS=true
 export TZ=Asia/Dhaka #put before last build command
-mka evolution
+make corvus
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
