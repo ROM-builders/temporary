9106023f1f56a8c495ec2c83e82bd332219e1112
braindedboi <85474068+braindedboi@users.noreply.github.com>
2021-11-02 13:38:40 +0530

    Fix typo

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/CipherOS/android_manife
 git clone https://github.com/braindedboi/local_manifests --depth 1 -b cipher-rui2 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
-# build rom
+#  build rom
 source build/envsetup.sh
 lunch lineage_moon-userdebug
 export TZ=Asia/Kolkata #put before last build command
