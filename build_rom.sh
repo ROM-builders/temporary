836c7e837d2fd3a23dde086d2049f501c03b488c
Raavan dk <91897724+RAAVANDK@users.noreply.github.com>
2021-11-02 17:29:46 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom 
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelBlaster-OS/manifest.git -b eleven -g default,-mips,-darwin,-notdefault
 git clone https://github.com/RAAVANDK/Local_Manifest.git --depth 1 -b RAAVANDK .repo/local_manifest
-repo sync -c --no-clone-bundle --no-tags 
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
 # build rom
 
 . build/envsetup.sh
