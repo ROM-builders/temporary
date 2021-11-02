0d37c0fa6ab89d37964aa0bc48aaee28fca2ab18
Harikumar <62538135+phoenix-1708@users.noreply.github.com>
2021-11-02 20:13:29 +0530

    Build ssos12 for sweet

--- build_rom.sh
+++ build_rom.sh
@@ -1,18 +1,18 @@
 # sync rom
 
-repo init --depth=1 --no-repo-verify -u https://github.com/Havoc-OS/android_manifest.git -b eleven -g default,-mips,-darwin,-notdefault
-git clone https://github.com/schrodingerscatonline/local_manifest.git --depth 1 -b havoc --depth 1 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ShapeShiftOS/android_manifest.git -b android_12 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/phoenix-1708/local_manifest.git --depth 1 -b ssos12 --depth 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 
 source build/envsetup.sh
-lunch havoc_laurel_sprout-userdebug
+lunch ssos_sweet-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
 export SKIP_ABI_CHECKS=true
 export SKIP_API_CHECKS=true
 export TZ=Asia/Dhaka #put before last build command
-mka bacon
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
