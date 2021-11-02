6ebcd2d999c52de8dea70ef605b40f72e3fa6b86
Baba Nirala <89604469+jaaat4u@users.noreply.github.com>
2021-11-02 20:51:34 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init -u https://github.com/Octavi-OS/platform_manifest.git -b 12 --depth=1
-git clone https://github.com/jaaat4u/local_manifest.git --depth=1 -b mainrepo/local_manifests
+git clone https://github.com/jaaat4u/local_manifest.git --depth 1 -b mainrepo/local_manifests
 repo sync -c -f --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j8
 
 # build rom
