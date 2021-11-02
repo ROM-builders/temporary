9d8b636915ac4eb2e6cfb9eccbef50122735ef0d
Raavan dk <91897724+RAAVANDK@users.noreply.github.com>
2021-11-02 06:52:16 +0530

    Merlinx PixelBlaster-OS RAAVANDK

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,15 @@
 # sync rom 
-repo init --depth=1 --no-repo-verify -u https://github.com/PixysOS/manifest -b eleven -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelBlaster-OS/manifest -b eleven -g default,-mips,-darwin,-notdefault
 git clone https://github.com/RAAVANDK/Local-Manifest.git --depth 1 -b master .repo/local_manifest
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
+repo sync -c -j8 force-sync --no-clone-bundle --no-tags
 # build rom
 
 . build/envsetup.sh
-lunch pixys_merlinx-userdebug
-make pixys -j8 
+lunch aosp_merlinx-userdebug
+
 
 export TZ=Asia/kolkata #put before last build command
-make pixys -j8
+make bacon -j8
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
