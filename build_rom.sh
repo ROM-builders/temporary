4971866c78eb4d05d0a65437958ace06491919fd
Bigboideep <93543034+Bigboideep@users.noreply.github.com>
2021-11-02 14:41:39 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/Project-Xtended/manifest.git -b xr -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Xtended/manifest.git -b xr -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Bigboideep/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
