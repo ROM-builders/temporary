d7b976f48bd2cdfe963eebef1a7b7d0b885ccb14
fazrul1994 <74166146+fazrul1994@users.noreply.github.com>
2021-11-03 15:40:03 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u  git://github.com/Evolution-X/manifest -b elle -g default,-mips,-darwin,-notdefault
-git clone https://github.com/fazrul1994/local_manifests.git --depth 1 -b main .repo
+git clone https://github.com/fazrul1994/local_manifests.git --depth 1 .repo/local_manifests/evolution_manifest.xml
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
