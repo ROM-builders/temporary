b6abf8a4d31dfa5a07cf4d01f8bb1bac0950d3f9
Raavan dk <91897724+RAAVANDK@users.noreply.github.com>
2021-11-03 16:08:57 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom 
-repo init --depth=1 --no-repo-verify -u https://github.com/Pixys-OS/manifest.git -b eleven -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/amyROM/android.git -b rosie -g default,-mips,-darwin,-notdefault
 git clone https://github.com/RAAVANDK/Local_Manifest.git --depth 1 -b RAAVANDK .repo/local_manifest
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 # build rom
 
 . build/envsetup.sh
-lunch pixys_merlinx-userdebug
+lunch amy_merlinx-eng
 export TZ=Asia/kolkata 
-make pixys -j8
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
