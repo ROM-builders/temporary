68d1f6d36e95983eaf338681ff28be5e9c146172
horoid <92655627+horoid@users.noreply.github.com>
2021-11-03 08:34:26 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_m01q-eng 
+lunch lineage_m01q-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
