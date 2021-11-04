d05bab56be2d64288296dd2755a88d07a4219f49
Gotam919 <82812329+Gotam919@users.noreply.github.com>
2021-11-04 08:19:19 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch arrow_RMX1941-userdebug
+lunch arrow_aqua-userdebug
 export TZ=Asia/Dhaka #put before last build command
 m bacon
 
