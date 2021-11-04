75deae97d0be797761e20c0149c6b86ccf61bd73
ArkanMuhammad1986 <93345864+ArkanMuhammad1986@users.noreply.github.com>
2021-11-04 23:43:21 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/WeebProjekt/platform_manifest -b reborn
- -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ArkanMuhammad1986/Local_Manifest.git --depth 1 -b WeebProjekt .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b fire -g default,-mips,-darwin,-notdefault
+git clone https://github.com/ArkanMuhammad1986/Local_Manifest.git --depth 1 -b SparkOS .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch weeb_mido-userdebug
+lunch spark_mido-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Jakarta #put before last build command
-make weeb-prod
+mka spark
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
