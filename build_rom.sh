80ba32cd246ba717cf43ecfc253c6266bc37d5c7
ArkanMuhammad1986 <93345864+ArkanMuhammad1986@users.noreply.github.com>
2021-11-04 11:25:31 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch rr_mido-eng
+export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Tokyo #put before last build command
 mka bacon
