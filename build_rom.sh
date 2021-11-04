7779f945eb68e05084fe48f17934874dbb1daf8e
Asif <49162687+nhAsif@users.noreply.github.com>
2021-11-04 16:49:00 +0600

    fix error

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/crdroidandroid/android.git -b 11.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b cr .repo/local_manifest
+git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b cr .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
