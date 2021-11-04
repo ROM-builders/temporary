2d1f2d4a81504e5f06e0af8fac36950ac971df4a
Chio <93084100+aberchio@users.noreply.github.com>
2021-11-04 17:26:35 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -8,7 +8,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 lunch palladium_vayu-user
 export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
-export TZ=Asia/Singapore #put before last build command
+export TZ=Asia/Jakarta #put before last build command
 mka palladium -j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
