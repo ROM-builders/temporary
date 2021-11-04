b1a3dad8ff87154a59bd0a85cb99a3522d903320
Harikumar <62538135+phoenix-1708@users.noreply.github.com>
2021-11-04 21:59:42 +0530

    Build ssos for sweet

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 
 repo init --depth=1 --no-repo-verify -u https://github.com/ShapeShiftOS/android_manifest.git -b android_12 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/phoenix-1708/local_manifest.git --depth 1 -b ssos12 --depth 1 .repo/local_manifests
+git clone https://github.com/phoenix-1708/local_manifest.git --depth=1 -b ssos12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
