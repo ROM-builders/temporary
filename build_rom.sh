de0544655a95affe3f1a8d0724ea3babaed8a87a
ArkanMuhammad1986 <93345864+ArkanMuhammad1986@users.noreply.github.com>
2021-11-04 23:43:54 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/WeebProjekt/platform_manifest -b reborn
- -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/WeebProjekt/platform_manifest -b reborn -g default,-mips,-darwin,-notdefault
 git clone https://github.com/ArkanMuhammad1986/Local_Manifest.git --depth 1 -b WeebProjekt .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
