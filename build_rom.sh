6b479c9ba4e7cb4c3e65b201707d8c98261cea74
indi121 <91513844+indi121@users.noreply.github.com>
2021-11-05 05:27:52 +0545

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/DotOS/manifest.git -b d
 git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b dot .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build ro
 source build/envsetup.sh
 lunch dot_whyred-user
 export TZ=Asia/Dhaka #put before last build command
