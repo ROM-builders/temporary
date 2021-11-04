2412f20ac7ed924941e16e07f1db20ef147c7905
Raavan dk <91897724+RAAVANDK@users.noreply.github.com>
2021-11-04 18:13:33 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom 
-repo init --depth=1 --no-repo-verify -u git://github.com/amyROM/android.git -b rosie -g default,-mips,-darwin,-notdefault
-git clone https://github.com/RAAVANDK/Local-Manifest.git --depth 1 -b RAAVANDK .repo/local_manifest
+repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b eleven -g default,-mips,-darwin,-notdefault
+git clone https://github.com/RAAVANDK/Local-Manifests.git --depth 1 -b RAAVANDK .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 # build rom
 
 . build/envsetup.sh
-lunch amy_merlinx-user
+lunch cherish_merlinx-user
 export TZ=Asia/kolkata 
-make bacon
+make bacon -j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
