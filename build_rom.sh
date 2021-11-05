9c3d0a7669adccf3dac766e21de96eb366fd8449
rizengt <84280317+rizengt@users.noreply.github.com>
2021-11-05 20:51:43 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,15 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ForkLineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/rizengt/local_manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/rizengt/local_manifest.git --depth 1 -b arrow-12.0 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_vince-eng
+lunch lineage_vince-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
 export TARGET_FLOS=true
-mka bacon
+make bacon
 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
