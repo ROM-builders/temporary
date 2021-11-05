6fab8ae3e6b52788752ae4401a9ba0e10d83c068
Sarthak Roy <sarthakroy2002@gmail.com>
2021-11-05 21:22:31 +0530

    Initial build

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/sarthakroy2002/local_manifest -b main --depth 1 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/CipherOS/android_manifest.git -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/sarthakroy2002/local_manifest -b rui2-oss --depth 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_RMX2020-userdebug
+lunch cipher_RMX2020-eng
 export TZ=Asia/Dhaka #put before last build command
 export SKIP_ABI_CHECKS=true
 export SKIP_API_CHECKS=true
