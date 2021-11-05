770befa298db47125872ea06e196b87857f588d5
braindedboi <85474068+braindedboi@users.noreply.github.com>
2021-11-05 08:26:59 +0530

    Typo

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/HyconOS/manifest -b eleven -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/braindedboi/local_manifests --depth 1 -b hycon .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # build rom
