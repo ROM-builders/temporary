bf566e12b917cbdb374e53018402775fdbbc594f
Kernel Hacker <92838621+kernelhacker69@users.noreply.github.com>
2021-11-05 10:40:42 +0530

    no need conflicts

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,5 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ArcaneOS/Arcane_manifest -b R -g default,-mips,-darwin,-notdefault
-
 git clone https://github.com/GhostMaster69-dev/local_manifest.git --depth 1 -b arcane-11 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
