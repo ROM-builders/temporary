9eca43fbb5b3aa54c2f7afc073e5d80e55b1a68e
Sarthak Roy <sarthakroy2002@gmail.com>
2021-11-05 21:27:30 +0530

    Initial Build

--- build_rom.sh
+++ build_rom.sh
@@ -11,7 +11,7 @@ export SKIP_ABI_CHECKS=true
 export SKIP_API_CHECKS=true
 export BUILD_HOSTNAME=sarthakroy2002
 export BUILD_USERNAME=neolit
-mka bacon -j$(nproc --all)   
+mka bacon 
 
 # upload  rom
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
