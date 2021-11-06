32bb3e441a8aee216324449144bab68bdb590e7a
AnGgIt88 <jarbull87@gmail.com>
2021-11-06 20:12:51 +0700

    bringup to AOSP 12
    Signed-off-by: AnGgIt88 <jarbull87@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -2,17 +2,17 @@
 # Thanks to @Apon77
 
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-CAF/xd_manifest -b eleven -g default,-mips,-darwin,-notdefault
-git clone https://github.com/AnGgIt88/local_manifest.git --depth=1 -b eleven .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Elixir/manifest -b snow -g default,-mips,-darwin,-notdefault
+git clone https://github.com/AnGgIt88/local_manifest.git --depth=1 -b twelve .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
-lunch xdroid_rosy-userdebug
+. build/envsetup.sh
+lunch aosp_rosy-userdebug
 export TZ=Asia/Jakarta
 export ALLOW_MISSING_DEPENDENCIES=true
-export LOCALVERSION=-XdroidCAF-rev1.7
-make xd
+export CONFIG_LOCALVERSION="-Elixir"
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
