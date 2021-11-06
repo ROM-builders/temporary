86ef552b9e83ad9f715d36d1fef35f37ae9a098d
mobx <67450638+mobxCode@users.noreply.github.com>
2021-11-06 14:03:08 +0700

    BringUp DotOs

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/DotOS/manifest.git -b
 git clone https://github.com/mobxCode/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build roms
 source build/envsetup.sh
 lunch dot_juice-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
