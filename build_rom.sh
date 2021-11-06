0e52b5e9b72df1e5193039f42d340394b2f938fe
Raavan dk <91897724+RAAVANDK@users.noreply.github.com>
2021-11-06 23:02:41 +0530

    Pixys

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom 
-repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b eleven -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/PixysOS/manifest -b eleven -g default,-mips,-darwin,-notdefault
 git clone https://github.com/RAAVANDK/Local-Manifests.git --depth 1 -b RAAVANDK .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 # build rom
 
 . build/envsetup.sh
-lunch cherish_merlinx-user
+lunch pixys_merlinx-userdebug
 export TZ=Asia/kolkata 
-make bacon -j$(nproc --all)
+make pixys -j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
