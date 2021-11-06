0374afb0ff66166dcd0005b9105b9be1f3fc1ab9
Sarthak Roy <sarthakroy2002@gmail.com>
2021-11-06 10:32:22 +0530

    Initial build
    Y

--- build_rom.sh
+++ build_rom.sh
@@ -6,12 +6,13 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch cipher_RMX2020-eng
-export TZ=Asia/Dhaka #put before last build command
+export TZ=Asia/Dhaka #put before last  build command
 export SKIP_ABI_CHECKS=true
 export SKIP_API_CHECKS=true
 export BUILD_HOSTNAME=sarthakroy2002
 export BUILD_USERNAME=neolit
 mka bacon 
+
 
 # upload  rom
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
