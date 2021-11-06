c1e0dbba8411cf255e929073c9ac42e1886adabf
Gotam919 <82812329+Gotam919@users.noreply.github.com>
2021-11-06 09:29:32 +0500

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-11.0 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Havoc-OS/android_manifest.git -b eleven -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Gotam919/local_manifest.git --depth=1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch arrow_aqua-userdebug
+lunch havoc_aqua-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true 
 export TZ=Asia/Dhaka #put before last build command
 m bacon
