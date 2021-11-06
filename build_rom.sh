6346866aa8f672aa605538e6e6814ed9de4e6c3b
Teguh Budi Santoso <86312158+Tiktodz@users.noreply.github.com>
2021-11-06 13:33:32 +0700

    switch to eas

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
-# sync
+# sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/Spark-Rom/manifest.git -b fire -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Tiktodz/local_manifest.git --depth 1 -b sos .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build
+# build rom
 source build/envsetup.sh
 lunch spark_X00TD-userdebug
 export TZ=Asia/Jakarta #put before last build command
