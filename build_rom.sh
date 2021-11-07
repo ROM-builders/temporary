e240554e4d4c17c94193015ee5da34b79f5a8320
Alone0316 <70204635+Hunter-commits@users.noreply.github.com>
2021-11-07 15:41:56 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelBlaster-OS/manifest -b 12 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelBlaster-Temp/manifest -b 12 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Hunter-commits/local_manifest.git --depth 1 -b PixelBlaster .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
