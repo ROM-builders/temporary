449dbc9b3808fc15666fc955c77bddf2221cd1f8
zhenyolka <zhenyolka@gmail.com>
2021-11-07 17:09:35 +0300

    pyxis: fix sound, encryption, add f2fs support

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PotatoProject/manifest -b frico-release -g default,-mips,-darwin,-notdefault
-git clone https://github.com/zhenyolka/pyxis-manifest-a12.git --depth 1 -b posp-12 .repo/local_manifests
+git clone https://github.com/Pyxis-Community/android_manifest.git --depth 1 -b posp-12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
