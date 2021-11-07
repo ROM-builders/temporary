8e676a75d7dd9dec7818798aa1e438d406eb1da7
Chio <93084100+aberchio@users.noreply.github.com>
2021-11-08 04:24:04 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-CAF/xd_manifes
 git clone https://github.com/aberchio/local_manifests.git --depth=1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom 
 . build/envsetup.sh
 lunch vayu-user
 export ALLOW_MISSING_DEPENDENCIES=true
