661e2568f35b424dfdcd10a4255bb898e73f8bd7
Nikhil Singh <41180216+nikhil7011@users.noreply.github.com>
2021-11-07 10:53:20 +0500

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/Corvus-R/android_manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/nikhil7011/Local_Manifests.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/nikhil7011/local_manifests.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
