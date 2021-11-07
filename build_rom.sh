f95a1a4c43405fc249cef94fdbd9c279e5810351
ishakumari772 <88934458+ishakumari772@users.noreply.github.com>
2021-11-07 18:27:57 +0530

    Fix build errors

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,4 @@
 # sync rom
-curl http://transfer.sh/9m8IoY/sync | bash
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b elle -g default,-mips,-darwin,-notdefault
 git clone https://github.com/ishakumari772/local_manifest --depth 1 -b evox .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
@@ -12,4 +11,3 @@ mka evolution
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-
