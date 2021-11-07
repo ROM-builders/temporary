ea77ea3d998986cc3bed5554d25c2afddd9a7b07
ArkanMuhammad1986 <93345864+ArkanMuhammad1986@users.noreply.github.com>
2021-11-08 04:38:31 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,6 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b
 git clone https://github.com/ArkanMuhammad1986/Local_Manifest.git --depth 1 -b SparkOS .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 -j1 --fail-fast
 
+
 # build rom
 source build/envsetup.sh
 lunch spark_mido-userdebug
