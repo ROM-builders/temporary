369da77606d1cd1b9c36fe6a95d289396071ece4
ibratabian17 <71920907+ibratabian17@users.noreply.github.com>
2021-11-07 07:33:27 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch aosp_A6020-userdebug
+lunch arrow_A6020-userdebug
 export KBUILD_BUILD_USER=Ibratabian17
 export TZ=Asia/Jakarta #put before last build command
 m bacon
