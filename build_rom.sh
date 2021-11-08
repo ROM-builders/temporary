b00c7b0fd2ad8fedadda309d2f18ebd541b4d4a6
Abhiroop Reddy <abhiroopc84@gmail.com>
2021-11-08 12:56:42 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 -u git://github.com/aex-tmp/manifest.git -b 12.x -g default,-mips,-darwin,-notdefault
-git clone https://github.com/me-abhiroop/local_manifest-1.git --depth 1 -b main-12 .repo/local_manifests
+git clone https://github.com/me-abhiroop/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
