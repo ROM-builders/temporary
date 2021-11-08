192da1b26175009e4d394e973215f8e8331cbc84
Chio <93084100+aberchio@users.noreply.github.com>
2021-11-08 08:21:25 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-CAF/xd_manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/aberchio/local_manifests.git --depth=1 -b xd .repo/local_manifests
+git clone https://github.com/aberchio/local_manifests.git --depth=1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom 
