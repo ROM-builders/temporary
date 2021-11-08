5037a087f8d9601883c6d414374af6045102744d
Raavan dk <91897724+RAAVANDK@users.noreply.github.com>
2021-11-08 22:55:18 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch arrow_aqua-userdebug
+lunch arrow_merlinx-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true 
 export TZ=Asia/Kolkata #put before last build command
 m otapackage
