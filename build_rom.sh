3f449d3f1d4d4289eeac0a5527cd81f951b0e4bc
Bigboideep <93543034+Bigboideep@users.noreply.github.com>
2021-11-08 06:53:39 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/Bigboideep/local_manifest.git --depth 1 -b main .re
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+. build/envsetup.sh
 lunch evolution_olivewood-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka evolution
