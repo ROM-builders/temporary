183073a8340852134d563f9291c93be252eca4af
Optimaxdi <33113347+zacky93@users.noreply.github.com>
2021-11-08 15:35:16 +0700

    Build Dot Juice

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch dot_whyred-userdebug
+lunch dot_juice-userdebug
 export TZ=Asia/Jakarta 
 make bacon
 
