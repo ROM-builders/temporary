ffd8a7c0512d53178cff74ffe5323c4218050163
Jaxx067 <93719270+Jaxx067@users.noreply.github.com>
2021-11-09 00:32:24 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch voltage_X00T-userdebug
+lunch voltage_X00TD-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
