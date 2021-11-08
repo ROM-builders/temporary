cc329ea3030feccfb3343f40d3ed0867ebfac2fa
Kendras056 <88943971+Kendras056@users.noreply.github.com>
2021-11-08 18:37:49 +0700

    mka descendant

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/Havoc-OS/android_manifest.git -b eleven -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/Descendant-XI/manifests.git -b eleven-staging -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Kendras056/local_manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) 
 
 # build rom
 source build/envsetup.sh
-lunch havoc_X00TD-userdebug
+lunch descendant_X00TD-userdebug
 export TZ=Asia/Jakarta #put before last build command
-brunch X00TD
-
+mka descendant
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
