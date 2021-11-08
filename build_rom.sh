c70b1d8de582bc6fddd0754f67d1f08d78b20ad6
Kendras056 <88943971+Kendras056@users.noreply.github.com>
2021-11-08 18:47:10 +0700

    test

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/Descendant-XI/manifests.git -b eleven-staging -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/descendant-xi/manifests.git -b eleven-staging -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Kendras056/local_manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) 
 
