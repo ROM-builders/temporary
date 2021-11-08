235172baebe3d64ff3559a1522a4688fc7a99760
Cloud Project <84621067+cloudprject@users.noreply.github.com>
2021-11-08 19:32:14 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init -u https://github.com/PixelExtended/manifest -b ace 
 git clone https://github.com/cloudprject/local_manifests --depth=1 
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync --force-sync --no-tags --no-clone-bundle
 
 # build rom
 source build/envsetup.sh
