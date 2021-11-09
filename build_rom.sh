76bc3e03ea6bb144a75f4f9f8fa258667f566fc3
FyyyyK <87652862+FyyyyK@users.noreply.github.com>
2021-11-09 21:45:26 +0700

    initial zeus

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/Corvus-Q/android_manifest.git -b 10 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/Zeus-OS/android_manifest.git -b 11.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/FyyyyK/local_manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch du_X00TD-userdebug
+lunch zeus_X00TD-userdebug
 export TZ=Asia/Tokyo #put before last build command
-make corvus
+brunch X00TD
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
