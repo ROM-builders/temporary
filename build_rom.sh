4de4e2f005a93f7ec80ae2c4422d90b3807ab9c3
NFS Projects <68986092+AnGgIt86@users.noreply.github.com>
2021-11-09 08:27:59 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # Copyright (c) 2021, The Linux Foundation. All rights reserved.
 # Thankz to @Apon77
 
-# Date 09-Nov-2021 07:40
+# Date 09-Nov-2021 08:27
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ArcaneOS/Arcane_manifest -b R -g default,-mips,-darwin,-notdefault
 git clone https://github.com/AnGgIt86/local_manifest.git --depth=1 -b eleven .repo/local_manifests
