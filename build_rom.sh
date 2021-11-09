7f552f79bf3872d875aec48fef5317b9a966e30b
Thoreck <46625454+fajar4561@users.noreply.github.com>
2021-11-09 12:52:50 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,6 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/CarbonROM/android.git -
 git clone https://github.com/fajar4561/local_manifest.git --depth 1 -b carbon .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+
 # build rom
 source build/envsetup.sh
 lunch carbon_X00TD-userdebug
