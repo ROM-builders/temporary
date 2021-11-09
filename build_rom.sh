38e307bfc15e9cb4866142db6b60f03e84d1c51f
FyyyyK <87652862+FyyyyK@users.noreply.github.com>
2021-11-09 16:31:38 +0700

    initial du

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/Corvus-R/android_manifest.git -b 11 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/Corvus-Q/android_manifest.git -b 10 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/FyyyyK/local_manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch corvus_X00TD-userdebug
-export TZ=Asia/Jakarta #put before last build command
+lunch du_X00TD-userdebug
+export TZ=Asia/Tokyo #put before last build command
 make corvus
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
