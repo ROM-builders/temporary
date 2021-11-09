a58748593b5dd1e4db7edb83c5c3febab163a536
Hasin Abrar Haque <70853665+AbrarNoob@users.noreply.github.com>
2021-11-09 11:58:34 +0600

    Fix

--- build_rom.sh
+++ build_rom.sh
@@ -3,10 +3,10 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/androi
 git clone https://github.com/AbrarNoob/local_manifest.git --depth 1 -b awa .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-
 # build rom
 source build/envsetup.sh
 lunch awaken_lavender-userdebug
+export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
