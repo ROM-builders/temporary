6a0ef1168d1dc8f07e827e023ffbdaebe7798719
AnGgIt86 <jarbull86@gmail.com>
2021-11-10 05:46:31 +0700

    Aex-11
    Signed-off-by: AnGgIt86 <jarbull86@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -1,10 +1,10 @@
 # Copyright (c) 2021, The Linux Foundation. All rights reserved.
 # Thankz to @Apon77
 
-# Date 09-Nov-2021 10:40
+# Date 10-Nov-2021 05:50
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ArcaneOS/Arcane_manifest -b R -g default,-mips,-darwin,-notdefault
-git clone https://github.com/AnGgIt86/local_manifest.git --depth=1 -b eleven .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/AospExtended/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/AnGgIt86/local_manifest.git --depth=1 -b aex-11 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -13,7 +13,7 @@ lunch aosp_rosy-userdebug
 export TZ=Asia/Jakarta
 export ALLOW_MISSING_DEPENDENCIES=true
 export CONFIG_LOCALVERSION="-ArcaneOS"
-mka bacon
+mka aex
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
