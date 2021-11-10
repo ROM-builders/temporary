4bc185552acc029db4bb6971fd9567a0c48168a0
Thoreck <46625454+fajar4561@users.noreply.github.com>
2021-11-10 07:32:49 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,6 +4,7 @@ git clone https://github.com/fajar4561/local_manifest.git --depth 1 -b carbon .r
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 
+
 # build rom
 source build/envsetup.sh
 lunch carbon_X00TD-userdebug
