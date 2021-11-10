370e9d58bdbdf78e353bf36a1242004addb69d71
sajidshahriar <sazidshahriar39@gmail.com>
2021-11-10 18:54:55 +0600

    2nd Build

--- build_rom.sh
+++ build_rom.sh
@@ -3,6 +3,8 @@ repo init --depth=1 --no-repo-verify -u git://github.com/ProjectSakura/android.g
 git clone https://github.com/sajidshahriar72543/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+#2
+
 # build rom
 . build/envsetup.sh
 lunch lineage_beryllium-userdebug
