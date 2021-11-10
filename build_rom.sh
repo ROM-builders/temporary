f09620d2b16e1af84f4f984a1bcb2f7b33c0afb8
fazrul1994 <74166146+fazrul1994@users.noreply.github.com>
2021-11-11 01:48:59 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u  git://github.com/Evolution-X/manifest -b elle -g default,-mips,-darwin,-notdefault
-git clone https://github.com/fazrul1994/local_manifests.git --depth 1 .repo/local_manifests
+git clone https://github.com/fazrul1994/local_manifests.git .repo
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch evolution_poplar-userdebug
+lunch evolution_poplar_kddi-userdebug
 export TZ=Asia/Jakarta #put before last build command
 mka evolution
 
