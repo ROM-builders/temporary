93309b5015f158b0b8589f8c2e8e5f99b96b3bf4
ibratabian17 <71920907+ibratabian17@users.noreply.github.com>
2021-11-12 06:55:30 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b eleven-plus -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ibraaltabian17/local_manifest.git --depth 1 -b aosp .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ProjectHiper-ROM/android_manifest -b 11 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/ibraaltabian17/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch rr_A6020-userdebug
+lunch lineage_berryllium-userdebug
 export KBUILD_BUILD_USER=Ibratabian17
 export TZ=Asia/Jakarta #put before last build command
-mka bacon -j$(nproc --all)
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
