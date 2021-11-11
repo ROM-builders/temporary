bbfd5bb24ad9e215204646d08514899893b23de0
debnationxd <71305844+debnationxd@users.noreply.github.com>
2021-11-11 18:32:08 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch syberia_lavender
+brunch lavender
 #export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Kolkata #put before last build command
 mka syberia
