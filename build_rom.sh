ea2f0646fcb88316a6cfc8cb4c77648306da929b
mobxCode <93519486+mobxprjkt@users.noreply.github.com>
2021-11-11 12:03:10 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelPlusUI/manifest -b eleven -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelPlusUI-Elle/manifest -b eleven -g default,-mips,-darwin,-notdefault
 git clone https://github.com/mobxprjkt/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
