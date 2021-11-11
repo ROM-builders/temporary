581622ded15295c5c08ce5b8b0a6bee7dd980a4e
Zacky Marzuki <33113347+optimax-di@users.noreply.github.com>
2021-11-11 22:10:21 +0700

    Try PixelPlus

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelPlusUI/manifest -b eleven -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelPlusUI-Elle/manifest -b eleven -g default,-mips,-darwin,-notdefault
 git clone https://github.com/optimax-di/Local-Manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch ppui_juice-userdebug
+lunch aosp_juice-userdebug
 export TZ=Asia/Jakarta
 mka bacon
 
