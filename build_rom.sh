e19ce6300cae4b480f435cd754bf3444f0c6c072
WalkingDead <lexaxx14@gmail.com>
2021-11-12 16:41:43 +0300

    citrus: try build

--- build_rom.sh
+++ build_rom.sh
@@ -3,10 +3,10 @@ repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -
 git clone https://github.com/WalkingDead3/manifest.git --depth 1 -b lineage-17.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-#build rom
+# build rom
 source build/envsetup.sh
 lunch lineage_citrus-userdebug
-export KBUILD_BUILD_USER="WalkingDead :3"
+export KBUILD_BUILD_USER="WalkingDead"
 export KBUILD_BUILD_HOST="WalkingCI"
 export TZ=Europe/Samara
 make bacon
