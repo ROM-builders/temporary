d897bbc821a9af61d19f66076db79bc0629d7591
WalkingDead <lexaxx14@gmail.com>
2021-11-12 16:44:36 +0300

    citrus: init build (try)

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch lineage_citrus-userdebug
 export KBUILD_BUILD_USER="WalkingDead"
-export KBUILD_BUILD_HOST="DeadPastor"
+export KBUILD_BUILD_HOST="Dead3Pastor"
 export TZ=Europe/Samara
 make bacon
 
