42ed49a8ef0394f8a9056a854eaa326e489f8cdb
ArkanMuhammad1986 <93345864+ArkanMuhammad1986@users.noreply.github.com>
2021-11-12 09:56:26 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/NusantaraProject-ROM/android_manifest -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ArkanMuhammad1986/Local_Manifest.git --depth 1 -b HyconOS .repo/local_manifests
+git clone https://github.com/ArkanMuhammad1986/Local_Manifest.git --depth 1 -b 11 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
