a07412a02f84eaf1a8c12040b376c0a2d7389ec3
Kneba <abenkenari@gmail.com>
2021-11-12 09:51:20 +0700

    snugyc

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/ForkLineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/cygnus-rom/manifest.git -b caf-11 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Kneba/local_manifests.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_X00TD-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
-export SKIP_API_CHECKS=true
-export TARGET_FLOS=true
-export TZ=Asia/Tokyo #put before last build
-make bacon
+lunch cygnus_X00TD-userdebug
+export TZ=Asia/Jakarta #put before last build
+make cygnus
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
