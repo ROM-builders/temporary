0477a7f927b1fd125423b71ce22e8aad9de6c8ed
WalkingDead <lexaxx14@gmail.com>
2021-11-12 16:36:46 +0300

    citrus: try build

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-17.1 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-17.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/WalkingDead3/manifest.git --depth 1 -b lineage-17.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-#  lbuild rom
+#build rom
 source build/envsetup.sh
 lunch lineage_citrus-userdebug
 export KBUILD_BUILD_USER="WalkingDead :3"
