720e55b9a68135d088b02c441ccf795d8f38556e
VERY HAPPY <77049889+ajitlenka30@users.noreply.github.com>
2021-11-12 15:25:00 +0530

    Corvus os for sweet

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Corvus-R/android_manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ajitlenka30/local_manifest --depth 1 -b 11 .repo/local_manifests
+git clone https://github.com/ajitlenka30/local_manifest --depth 1 -b sweet .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch corvus_tissot-user
+lunch corvus_sweet-user
 export TZ=Asia/Dhaka #put before last build command
 make corvus
 
