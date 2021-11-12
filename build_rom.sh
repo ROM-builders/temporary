ec23cc97fe14659bde23cfee4d509dbc01b710bd
Chio <93084100+aberchio@users.noreply.github.com>
2021-11-12 18:21:02 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/DotOS/manifest.git -b d
 git clone https://github.com/GrooVylle/local_manifests.git --depth=1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom 
 source build/envsetup.sh
 lunch dot_vayu-user
 export  ALLOW_MISSING_DEPENDENCIES=true
