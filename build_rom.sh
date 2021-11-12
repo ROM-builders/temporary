878374436cdc77673662d42304e65fb24367862d
Raavan dk <91897724+RAAVANDK@users.noreply.github.com>
2021-11-12 10:34:17 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b eleven -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/Octavi-OS/platform_manifest.git -b 11 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/RAAVANDK/local-manifests.git --depth=1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
-brunch device-merlinx
+. build/envsetup.sh
+lunch octavi_merlinx-userdebug
+brunch merlinx
 export ALLOW_MISSING_DEPENDENCIES=true
 export BUILD_BROKEN_DUP_RULES=true
 
