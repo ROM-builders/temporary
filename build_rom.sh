0782f2f000fda4d71161355f77c0e706f2102332
fazrul1994 <74166146+fazrul1994@users.noreply.github.com>
2021-11-14 05:31:12 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u  git://github.com/Evolution-X/manifest -b elle -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u  git:https://github.com/Ancient-Lab/manifest -b ten -g default,-mips,-darwin,-notdefault
 git clone https://github.com/fazrul1994/local_manifests.git .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch evolution_poplar_kddi-userdebug
+lunch ancient_poplar_-userdebug
 export TZ=Asia/Jakarta #put before last build command
-mka evolution
+mka bacon -j$(nproc --all)
 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
