285e0a8aaddf185da226b866305f5a6831ce17be
braindedboi <85474068+braindedboi@users.noreply.github.com>
2021-11-13 08:25:11 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PotatoProject/manifest -b frico-release -g default,-mips,-darwin,-notdefault
-git clone https://github.com/braindedboi/local_manifests --depth 1 -b potato .repo/local_manifests
+git clone https://github.com/braindedboi/local_manifests.git --depth 1 -b potato .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # build rom
