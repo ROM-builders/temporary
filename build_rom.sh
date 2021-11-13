e6fd9134122b18ac64ac7176f32a102c3bcfa4ae
Raavan dk <91897724+RAAVANDK@users.noreply.github.com>
2021-11-13 08:50:55 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,17 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/Octavi-OS/platform_manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/RAAVANDK/local-manifests.git --depth=1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/xdroid-CAF/xd_manifest.git -b eleven -g default,-mips,-darwin,-notdefault
+git clone https://github.com/RAAVANDK/local_manifest.git --depth 1 -b xdroid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-. build/envsetup.sh
-lunch octavi_merlinx-userdebug
-brunch merlinx
-export ALLOW_MISSING_DEPENDENCIES=true
-export BUILD_BROKEN_DUP_RULES=true
+source build/envsetup.sh
+lunch xdroid_merlinx-userdebug
+export TZ=Asia/kolkata #put before last build command
+make xd 
 
-export TZ=Asia/Kolkata
-mka bacon
+
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
