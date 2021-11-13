af4938272d3722aef24cef43208cbba272764bd2
Atharv <81741009+atharv2951@users.noreply.github.com>
2021-11-13 15:38:13 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest -b 12 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/DotOS/manifest.git -b dot11 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/atharv2951/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
-. build/envsetup.sh
+# build ro
+source build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
-lunch voltage_pine-userdebug
+export TARGET_SUPPORTS_64_BIT_APPS=true
+lunch dot_PLE-userdebug
 export TZ=Asia/Kolkata #put before last build command
-mka bacon
+maka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
