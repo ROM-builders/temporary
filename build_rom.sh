9a7fd44055c71ca425345cfb7b4cceba25778aad
WalkingDead <lexaxx14@gmail.com>
2021-11-13 08:17:11 +0400

    citrus: new build LOS for Poco M3 (only)

--- build_rom.sh
+++ build_rom.sh
@@ -6,8 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_citrus-userdebug
-export KBUILD_BUILD_USER="WalkingDead"
-export KBUILD_BUILD_HOST="Dead3Pastor"
 export TZ=Europe/Samara
 make bacon
 
