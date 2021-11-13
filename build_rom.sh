70b3205456fbcb4245666c316a8791a90e8c8946
Raavan dk <91897724+RAAVANDK@users.noreply.github.com>
2021-11-13 08:53:07 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch xdroid_merlinx-userdebug
+export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/kolkata #put before last build command
 make xd 
 
