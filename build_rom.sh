600c482e3de553e80fa6035da5dd1edb2677f9b1
indi121 <91513844+indi121@users.noreply.github.com>
2021-11-14 12:52:43 +0545

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_vince-user
+lunch lineage_vince-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
