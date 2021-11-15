29e422a4d5c2d5f1dc50d4225b02c79e7291dc60
braindedboi <85474068+braindedboi@users.noreply.github.com>
2021-11-15 16:11:47 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/StyxProject/manifest -b S -g default,-mips,-darwin,-notdefault
-git clone https://github.com/braindedboi/local_manifests --depth 1 -b styx .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/CipherOS/android_manifest.git -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/DPSLEGEND/local_manifest/blob/cipher/local_manifest.xml --depth 1 -b cipher .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # build rom
 source build/envsetup.sh
-lunch styx_moon-userbeug
+lunch cipher_moon-userdebug
 export TZ=Asia/Kolkata #put before last build command
-m styx-ota
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
