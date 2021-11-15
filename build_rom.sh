51c34fb4b69ab40454fafa0c1c559ef6fea71c84
debnationxd <71305844+debnationxd@users.noreply.github.com>
2021-11-15 22:21:13 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch potato_lavender-userdebug
-export ALLOW_MISSING_DEPENDENCIES=true
+#export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Kolkata #put before last build command
 mka potato
 
