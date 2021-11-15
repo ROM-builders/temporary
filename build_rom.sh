45864c0e1129c01f56f61565b4daf405c39d3aad
Chio <93084100+aberchio@users.noreply.github.com>
2021-11-15 12:15:23 +0700

    aex-tmp

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch vayu-userdebug
-export ALLOW_MISSING_DEPENDENCIES=true
 export WITH_GAPPS=true
 export TZ=Asia/Dhaka #put before last build command
 m aex
