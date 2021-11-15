de3db453973a0e9afee98c4d219bf360be275821
Atharv <81741009+atharv2951@users.noreply.github.com>
2021-11-14 21:15:58 -0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DotOS/manifest.git -b dot11 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-17.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/atharv2951/local_manifest --depth 1 -b main .repo/local_manifests
+rm -rf harware/samsung
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build ro
+# build rom
 source build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
-export TARGET_SUPPORTS_64_BIT_APPS=true
-lunch dot_PLE-userdebug
+lunch lineage_j7velte-userdebug
 export TZ=Asia/Kolkata #put before last build command
-maka bacon
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
