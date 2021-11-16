c8f64b627be80504ef7efe5fbc41402e82780a31
Chio <93084100+aberchio@users.noreply.github.com>
2021-11-16 05:39:55 -0800

    Clone ssos `android_manifest` from gv
    Some stuff is broken with default ssos "android_manifest", so clone it from gv repo with some additional fix

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ShapeShiftOS/android_manifest.git -b android_12 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/GrooVylle/android_manifest.git -b android_12 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/GrooVylle/local_manifests.git --depth=1 -b ssos .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
