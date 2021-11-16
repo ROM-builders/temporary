c61215ec2f49e023b296cee7749b69cc62145869
mobxCode <93519486+mobxprjkt@users.noreply.github.com>
2021-11-16 08:08:15 +0700

    add more applications

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,8 @@ repo init --depth=1 --no-repo-verify -u https://github.com/PixelExtended/manifes
 git clone https://github.com/mobxprjkt/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build roms
+
+# build rom
 source build/envsetup.sh
 lunch aosp_juice-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
@@ -13,3 +14,4 @@ mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+
