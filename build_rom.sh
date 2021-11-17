76ccb2f9be5a84ea61ce77ef93bf67f37a89f6cd
RasyidAlKautsar <93345864+ArkanMuhammad1986@users.noreply.github.com>
2021-11-18 05:02:07 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ArkanMuhammad1986/Local_Manifest.git --depth 1 -b AexV1 .repo/local_manifests
+git clone https://github.com/ArkanMuhammad1986/Local_Manifest.git --depth 1 -b Aex .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
