74bf1fa0dae8673d580988552510880ff7fe5409
braindedboi <85474068+braindedboi@users.noreply.github.com>
2021-11-17 22:20:19 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/CipherOS/android_manifest.git -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/braindedboi/local_manifests --depth 1 -b cipher-rui2 .repo/local_manifests
+git clone https://github.com/braindedboi/local_manifests --depth 1 -b cipher .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # build rom
 source build/envsetup.sh
 lunch cipher_moon-userdebug
 export TZ=Asia/Kolkata #put before last build command
-make bacon
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
