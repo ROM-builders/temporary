a748a41403cf2af3ace28a8341cb276982a87016
Attorelle <74150746+Attorelle@users.noreply.github.com>
2021-11-18 01:02:24 +0300

    gemini-crDroid-Attorelle

--- build_rom.sh
+++ build_rom.sh
@@ -2,6 +2,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/crdroidandroid/android.
 git clone https://github.com/attorelle/local_manifests.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+# Build ROM
 source build/envsetup.sh
 lunch lineage_gemini-userdebug
 export TZ=Europe/Moscow
