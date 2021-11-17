c55b66a51a072ac5b77c46cd7f64eba6ca3fdf74
RasyidAlKautsar <93345864+ArkanMuhammad1986@users.noreply.github.com>
2021-11-18 06:27:43 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch aosp_mido-userdebug
-export TZ=Asia/Tokyo #put before last build command
+export TZ=Asia/Jakarta #put before last build command
 m aex
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
