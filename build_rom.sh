c4aed9fc3d3ab20cc9fb87975207fc91b3e2c1ab
RasyidAlKautsar <93345864+ArkanMuhammad1986@users.noreply.github.com>
2021-11-17 15:40:12 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch aosp_mido-userdebug
-export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Jakarta #put before last build command
 m aex
 
