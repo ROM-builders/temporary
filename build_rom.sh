e17c95d7f3c6da6c9a3b4d4c2f0ea283a2f49fd9
RasyidAlKautsar <93345864+ArkanMuhammad1986@users.noreply.github.com>
2021-11-18 05:11:28 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch aosp_mido-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Tokyo #put before last build command
 m aex
 
