47300aa4fa24ca5b6c9e547a0e951d57ab17c18b
Dea1hwa1cher <71354578+Dea1hwa1cher@users.noreply.github.com>
2021-11-18 01:22:19 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_gemini-user
-export TZ=Asia/Dhaka #put before last build command
+export TZ=Europe/Moscow #put before last build command
 brunch gemini
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
