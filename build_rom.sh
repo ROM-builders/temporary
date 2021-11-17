9b122d812af931108dd11749b72c8ddc63218dee
Aaradhay Vijay <80588995+inferno964@users.noreply.github.com>
2021-11-17 10:42:46 +0530

    source build

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/inferno964/local_manifest.git --depth 1 -b master .
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-. build/envsetup.sh
+source build/envsetup.sh
 lunch styx_lavender-userdebug
 m styx-ota
 
