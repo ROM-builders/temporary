53d723d3c9070e83f44d1ec828491f7e25843875
GhostMaster69-dev <rathore6375@gmail.com>
2021-11-17 21:22:12 +0530

    Remove aosp prefix from product name
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch ssos_vince-userdebug
+lunch vince-userdebug
 export TZ=Asia/Kolkata #put before last build command
 make bacon
 
