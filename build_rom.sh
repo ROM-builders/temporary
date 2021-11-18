b521809cf7cb72d3f9f2b6aa4be0c6b4b1f27815
Dea1hwa1cher <71354578+Dea1hwa1cher@users.noreply.github.com>
2021-11-18 12:55:11 +0300

    ulova crDroid

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/crdroidandroid/android.git -b 11.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Dea1hwa1cher/local_manifest.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/Dannoob/local_manifests.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_gemini-userdebug
+lunch lineage_ulova-userdebug
 export TZ=Europe/Moscow #put before last build command
 mka bacon
 
