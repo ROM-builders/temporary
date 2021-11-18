3db8faf63d04a9075c9326e12dcf970b828e72f8
rehanalt501 <92092421+rehanalt501@users.noreply.github.com>
2021-11-18 14:59:08 +0530

    Fluid 12

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/CipherOS/android_manifest.git -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/rehanalt501/local_manifestt.git --depth 1 -b cipher .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Fluid/manifest.git -b fluid-12 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/rehanalt501/local_manifestt.git --depth 1 -b fluid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch cipher_moon-userdebug
+lunch fluid_moon-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
