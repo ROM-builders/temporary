0f780784eeb398c7336aefd00982b7d1ccc5b569
Aniket Dubey <63295358+itsaniketdubey@users.noreply.github.com>
2021-11-18 12:18:10 +0530

    Fixed typo

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/ShapeShiftOS/android_manifest.git -b android_11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/itsaniketdubey/local-manifest.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/itsaniketdubey/local-manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 #1 
