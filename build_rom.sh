06e489d739501f402b8e44414c0c275e4eeaa618
Chio <93084100+aberchio@users.noreply.github.com>
2021-11-19 05:08:31 +0700

    Nuke `XiaomiParts`

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/ShapeShiftOS/android_
 git clone https://github.com/GrooVylle/local_manifests.git --depth=1 -b ssos .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom 
+# build rom
 source build/envsetup.sh
 lunch vayu-user
 export ALLOW_MISSING_DEPENDENCIES=true
