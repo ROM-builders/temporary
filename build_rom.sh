9284870e5c9a7206812b59a07925cf8a6b1c48ca
Sn0w <dubeyaniket.ad@gmail.com>
2021-11-19 22:33:00 +0530

    Try 1st Build

--- build_rom.sh
+++ build_rom.sh
@@ -3,6 +3,8 @@ repo init --depth=1 --no-repo-verify -u git://github.com/syberia-project/manifes
 git clone https://github.com/Sn0wCone/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+#1
+
 # build rom
 source build/envsetup.sh
 lunch syberia_beryllium-userdebug
