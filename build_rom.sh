b8acc9d01298fc2c5f30c0baa764d49db307327c
Faisalhossainerfan <67220051+Faisalhossainerfan@users.noreply.github.com>
2021-11-19 14:06:53 +0600

    Santoni Pixelexperience Faisalhossainerfan
    Pixelexperience rom for redmi 4x(santoni)

--- build_rom.sh
+++ build_rom.sh
@@ -7,16 +7,16 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # Set up environment
 
-$ . build/envsetup.sh
+. build/envsetup.sh
 
 # Choose a target
 
-$ lunch aosp_$device-userdebug
+lunch aosp_santoni-userdebug
 
 export TZ=Asia/Dhaka #put before last build command
 # Build the code
 
-$ mka bacon
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
