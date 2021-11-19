a3e1ee2a0904264eba0589a14d38ceec6025bc7c
qwertyuiii-code <85434511+qwertyuiii-code@users.noreply.github.com>
2021-11-19 20:11:31 +0300

    certus-KomodoOs-qwertyuui-code
    certus KomodoOs for qwertyuui-code

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
-# sync rom
-repo init -u https://github.com/Komodo-OS-Rom/manifest -b ten
+repo init --depth=1 --no-repo-verify -u https://github.com/Komodo-OS-Rom/manifest -b ten-bak-g default,-mips,-darwin,-notdefault
+git clone https://github.com/qwertyuiii-code/local-manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
- build/envsetup.sh
- lunch komodo_certus-userdebug
- masak komodo -jX
-
+source build/envsetup.sh
+lunch komodo_certus-userdebug
+export TZ=Europe/Moscow
+masak komodo
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
