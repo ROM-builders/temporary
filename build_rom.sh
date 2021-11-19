25f2fc5ad4480f9aada8b211ce4a469881c93dbf
WallBreaker2407 <92675483+WallBreaker2407@users.noreply.github.com>
2021-11-19 22:27:39 +0700

    fix FP

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch nad_X00TD-eng
+lunch nad_X00TD-userdebug
 export TZ=Asia/Jakarta #put before last build command
 mka nad
 
