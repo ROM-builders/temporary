b4c6d0a58b3a6823d30a41d0614e024f688e8cfe
WinSSitaly <20368887+WinSSitaly@users.noreply.github.com>
2021-11-19 20:24:39 +0100

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,9 +6,9 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 # 2021 11 08
 source build/envsetup.sh
-brunch i9105p
+lunch lineage_i9105p-userdebug
 export TZ=Asia/Dhaka #put before last build command
-#mka i9105p
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
