d846c79d03801a1ad8e44fe705353ed7bb89879c
RasyidAlKautsar <93345864+ArkanMuhammad1986@users.noreply.github.com>
2021-11-20 22:33:49 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch nad_mido-userdebug
+lunch nad_mido-user
 export TZ=Asia/Jakarta #put before last build command
 mka nad
 
