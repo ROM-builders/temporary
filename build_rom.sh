eefb2f4ff9da70b714d87c94b1aab2622354afcb
neonmicky <63160537+neonmicky@users.noreply.github.com>
2021-11-20 17:21:02 +0530

    Fix sync

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -
 git clone https://github.com/neonmicky/local_manifest.git --depth 1 -b xr .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+#build rom
 source build/envsetup.sh
 lunch lineage_tissot-userdebug
 export TZ=Asia/Kolkata #put before last build command
