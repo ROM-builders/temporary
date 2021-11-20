6748cc25e64db7d9226e917c9789af1ad599ff00
qwertyuiii-code <85434511+qwertyuiii-code@users.noreply.github.com>
2021-11-20 21:05:52 +0500

    certus-HavocOs-qwertyuui

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
-repo init --depth=1 --no-repo-verify -u https://github.com/Komodo-OS-Rom/manifest -b ten-bak-g default,-mips,-darwin,-notdefault
+   repo init -u https://github.com/Havoc-OS/android_manifest.git -b ten
 git clone https://github.com/qwertyuiii-code/local-manifest.git --depth 1 -b main .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+
 
 # build rom
-source build/envsetup.sh
-lunch komodo_certus-userdebug
+. build/envsetup.sh
 export TZ=Europe/Ekaterinburg
-masak komodo
+brunch
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
