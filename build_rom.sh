0b80e5a7276ef69d2f9381f0650bb064591a519a
Faisalhossainerfan <67220051+Faisalhossainerfan@users.noreply.github.com>
2021-11-21 03:39:51 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/PixelExperience/manifest -b eleven -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/PixelExperience/manifest -b eleven-plus -g default,-mips,-darwin,-notdefault
 
 
 git clone https://github.com/Faisalhossainerfan/local_manifest.git .repo/local_manifests
