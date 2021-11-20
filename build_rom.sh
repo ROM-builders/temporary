4cce26504bf13da477a21da030de92d52205b58d
AnGgIt86 <jarbull86@gmail.com>
2021-11-21 04:26:26 +0700

    fix doze
    Signed-off-by: AnGgIt86 <jarbull86@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # Copyright (c) 2021, The Linux Foundation. All rights reserved.
 # Thankz to @Apon77
 # Date 21-Nov-2021
-
+#
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Havoc-OS/android_manifest.git -b eleven -g default,-mips,-darwin,-notdefault
 git clone https://github.com/AnGgIt86/local_manifest.git --depth=1 -b havoc .repo/local_manifests
