4ab13eefbecd6860f01b7c3cad1858dbf0b0c7f9
zhenyolka <zhenyolka@gmail.com>
2021-11-20 19:03:11 +0300

    pyxis: Sparkify

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PotatoProject/manifest -b frico-release -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Pyxis-Community/android_manifest.git --depth 1 -b posp-12 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/Spark-Rom/manifest.git -b spark -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Pyxis-Community/android_manifest.git --depth 1 -b spark-12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch potato_pyxis-userdebug
-brunch pyxis
+lunch spark_pyxis-userdebug
+mka spark
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
