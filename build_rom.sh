b94b8efab3cace750d01a1828fb7385f7b0303b6
Faisalhossainerfan <67220051+Faisalhossainerfan@users.noreply.github.com>
2021-11-22 03:10:55 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,8 +1,8 @@
 # sync rom
 
-repo init --depth=1 --no-repo-verify -u git://github.com/PixelExperience/manifest.git -b eleven-plus -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest.git -b eleven-plus -g default,-mips,-darwin,-notdefault
 
-git clone https://github.com/Faisalhossainerfan/local_manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/Faisalhossainerfan/local_manifest.git --depth 1 -b eleven-plus .repo/local_manifests
 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
@@ -12,6 +12,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 lunch aosp_santoni-userdebug
 
+export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Dhaka #put before last build command
 
 mka bacon
