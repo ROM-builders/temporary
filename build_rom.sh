08e7317555fa2d9d18f715ed447d91bbbd883224
WallBreaker2407 <92675483+WallBreaker2407@users.noreply.github.com>
2021-11-21 19:29:21 +0700

    fix error

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
-# sync rom
+# sync rom 
 repo init --depth=1 --no-repo-verify -u git://github.com/NusantaraProject-ROM/android_manifest.git -b 11 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/WallBreaker2407/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom 
 source build/envsetup.sh
 lunch nad_X00TD-userdebug
 export TZ=Asia/Jakarta #put before last build command
