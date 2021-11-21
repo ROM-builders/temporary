b6d5358d100e96a15c398602b8151f6b56519e6c
Faisalhossainerfan <67220051+Faisalhossainerfan@users.noreply.github.com>
2021-11-22 03:12:29 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -9,7 +9,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 
 . build/envsetup.sh
-
 lunch aosp_santoni-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Dhaka #put before last build command
