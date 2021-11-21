3de7ca5edce64b85355d53a133ec8f889b9f9e4f
sajidshahriar <sazidshahriar39@gmail.com>
2021-11-21 19:13:24 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/ProjectRadiant/manifest
 git clone https://github.com/Sn0wCone/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-#1
+#01
 
 # build rom
 . build/envsetup.sh
