0aa9c2c7aecb043db08fe4f5ae1fe70bf1074405
RasyidAlKautsar <93345864+ArkanMuhammad1986@users.noreply.github.com>
2021-11-22 05:27:47 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch weeb_mido-userdebug
+export SKIP_ABI_CHECKS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Jakarta #put before last build command
