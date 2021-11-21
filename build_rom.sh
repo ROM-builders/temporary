f985aad6ba5ae84ce563dd52941aa16bcf6c0f34
Kudakenai <76825405+Kudakenai@users.noreply.github.com>
2021-11-21 11:08:55 +0100

    Derpfest here we go

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault 
-git clone https://github.com/Kudakenai/local_manifest.git --depth=1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/Kudakenai/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_miatoll-userdebug
-export TZ=Asia/Dhaka #put before last build command
+lunch derpfest_miatoll-user
+export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
