83ae565994d89c1ca50ad9101922bdf6db05ebcf
Sahil Ahmed <73250691+sahilhv@users.noreply.github.com>
2021-11-21 22:02:04 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,11 @@
 # sync rom
-
-repo init --depth=1 --no-repo-verify -u https://github.com/Havoc-OS/android_manifest.git-b eleven -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Havoc-OS/android_manifest.git -b eleven -g default,-mips,-darwin,-notdefault
 git clone https://github.com/sahilhv/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch havoc_vince-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
