a5698aea560312fb693c5eea5f1a052f41e16abe
Naeem Mansuri <72666318+naeem000@users.noreply.github.com>
2021-11-22 17:32:47 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 12.x
+repo init -u https://github.com/Evolution-X/manifest -b snow Evo
 git clone https://github.com/naeem000/local_manifest.git --depth 1 -b 11 .repo/local_manifests
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
@@ -7,7 +7,7 @@ repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 source build/envsetup.sh
 lunch aosp_jasmine_sprout-userdebug
 export TZ=Asia/Dhaka #put before last build command
-m aex -j6
+mka evolution
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
