350614292231dab000734cdec60df703c9597786
mobx <67450638+mobxCode@users.noreply.github.com>
2021-11-22 11:15:28 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -12,7 +12,7 @@ export TZ=Asia/Jakarta #put before last build command
 mka bacon
 
 
-# Time = 09:30 WIB
+# Time = 11:15 WIB
 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
