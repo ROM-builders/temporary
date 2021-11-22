191178e28500391a7c0a0335ba2b0e1ec1945d0e
RasyidAlKautsar <93345864+ArkanMuhammad1986@users.noreply.github.com>
2021-11-23 06:55:29 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch octavi_mido-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Jakarta #put before last build command
 brunch mido
