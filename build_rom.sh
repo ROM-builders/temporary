40b6d9a815b5740cb8400f803e0a687df8a50eb2
braindedboi <85474068+braindedboi@users.noreply.github.com>
2021-11-22 14:59:40 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/StyxProject/manifest -b S -g default,-mips,-darwin,-notdefault
-git clone https://github.com/braindedboi/local_manifests --depth 1 -b styx .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Octavi-OS/platform_manifest.git -b 12 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/braindedboi/local_manifests --depth 1 -b octavi .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # build rom
 source build/envsetup.sh
-lunch styx_moon-userbeug
+lunch octavi_moon-userdebug
 export TZ=Asia/Kolkata #put before last build command
-m styx-ota
+brunch moon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
