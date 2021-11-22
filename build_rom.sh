b51a3c78f1df390d83523ab30cd14317374c4191
Kendras056 <88943971+Kendras056@users.noreply.github.com>
2021-11-23 04:25:22 +0700

    build luca

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Kendras056/local_manifests.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-17.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Kendras056/local_manifests.git --depth 1 -b los .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) 
 
 # build rom
 source build/envsetup.sh
-lunch aosp_X00TD-userdebug
+lunch lineage_X00TD-userdebug
 export TZ=Asia/Jakarta #put before last build command
-m aex
+mka bacon
 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
