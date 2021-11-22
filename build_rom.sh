75cbd03d75b0f36c8d7f7a4a77a85baf05b561e7
Chio <93084100+aberchio@users.noreply.github.com>
2021-11-22 20:53:09 +0700

    Switch to GApps build

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch superior_vayu-user
+export WITH_GAPPS=true
 export TZ=Asia/Jakarta #put before last build command
 make bacon
 
