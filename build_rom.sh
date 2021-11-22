b32cc0e9c84d9e6f7059683d48d57a2193f5cb55
RasyidAlKautsar <93345864+ArkanMuhammad1986@users.noreply.github.com>
2021-11-22 19:00:26 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/RevengeOS/android_manifest -b r11.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ArkanMuhammad1986/Local_Manifest.git --depth 1 -b aicp .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-LegionOS/manifest.git -b 12 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/ArkanMuhammad1986/Local_Manifest.git --depth 1 -b Legion .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
-lunch revengeos_mido-userdebug
+. build/envsetup.sh
+lunch legion_mido-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
+export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Jakarta #put before last build command
-make bacon
+make legion
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
