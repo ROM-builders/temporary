99a69b1a84f93566f770046cde560091d744fc4c
mobx <67450638+mobxCode@users.noreply.github.com>
2021-11-22 09:29:19 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,18 +1,18 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b eleven -g default,-mips,-darwin,-notdefault
-git clone https://github.com/mobxCode/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b eleven-plus -g default,-mips,-darwin,-notdefault
+git clone https://github.com/mobxCode/local_manifest.git --depth 1 -b pixel .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 
 # build roms
 source build/envsetup.sh
-lunch aospmakece-userdebug
+lunch aosp_juice-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Jakarta #put before last build command
 mka bacon
 
 
-# Time = 19:15 WIB
+# Time = 09:30 WIB
 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
