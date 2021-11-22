c928ab75d5d2021e05347e6773800672cc374599
Tiktodz Kontoel <46625454+fajar4561@users.noreply.github.com>
2021-11-22 08:59:54 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,6 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/NusantaraProject-ROM/an
 git clone https://github.com/fajar4561/local_manifest.git --depth 1 -b nd .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+
 # build rom
 source build/envsetup.sh
 lunch nad_X00TD-userdebug
