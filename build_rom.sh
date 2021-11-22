b7d2b2a0cff6f9081cadafe7f3ae8e1b4fcb223e
qwertyuiii-code <85434511+qwertyuiii-code@users.noreply.github.com>
2021-11-22 16:37:07 +0500

    Certus-Sakura-qwertyuui

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-repo init -depth=1 --no-repo-verify -u https://github.com/Colt-Enigma/platform_manifest -b -g default,-mips,-darwin,-notdefault
+repo init -depth=1 --no-repo-verify -u git://github.com/ProjectSakura/android.git -b 11 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/qwertyuiii-code/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
@@ -6,8 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 export TZ=Europe/Moscow
-lunch colt_certus64-userdebug
-maka colt
+lunch lianege_certus-userdebug
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
