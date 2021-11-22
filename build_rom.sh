03e33420aa145b50ac3f9817f48d4f608d37290b
RasyidAlKautsar <93345864+ArkanMuhammad1986@users.noreply.github.com>
2021-11-22 21:35:35 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/NusantaraProject-ROM/android_manifest -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ArkanMuhammad1986/Local_Manifest.git --depth 1 -b 11 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/exthmui/android.git -b exthm-11 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/ArkanMuhammad1986/Local_Manifest.git --depth 1 -b exthm-11 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch nad_mido-userdebug
+lunch exthm_mido-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Tokyo #put before last build command
-mka nad
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
