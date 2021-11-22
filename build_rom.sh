f32bc26847cf52c0405f7a6ae333c3394b5697f7
Naeem Mansuri <72666318+naeem000@users.noreply.github.com>
2021-11-22 18:23:37 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest.git -b snow -g default,-mips,-darwin,-notdefault
+repo init -u https://github.com/Evolution-X/manifest.git -b snow Evo
 git clone https://github.com/naeem000/local_manifest.git --depth 1 -b 11 .repo/local_manifests
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
