4ef556535a49ea0b534eeec5dbcbdbc331008713
qwertyuiii-code <85434511+qwertyuiii-code@users.noreply.github.com>
2021-11-22 11:13:58 +0500

    Certus64-nusantara-qwertyuui

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-repo init -depth=1 --no-repo-verify -u git://github.com/ProjectSakura/android.git -b 11 -g default,-mips,-darwin,-notdefault
+repo init -depth=1 --no-repo-verify -u https://github.com/NusantaraProject-ROM/android_manifest -b 12 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/qwertyuiii-code/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
@@ -6,8 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 export TZ=Europe/Moscow
-lunch lineage_certus64-userdebug
-mka bacon
+lunch nad_certus64-user
+make nad
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
