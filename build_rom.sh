dee9e6ef556d075032521c33254ce0fda7fccd30
Kendras056 <88943971+Kendras056@users.noreply.github.com>
2021-11-23 07:23:01 +0700

    test build

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-17.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Kendras056/local_manifests.git --depth 1 -b los .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/P-404/android_manifest.git -b rippa -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Kendras056/local_manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_X00T-userdebug
+lunch p404_X00TD-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Jakarta #put before last build command
-mka bacon
-
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
