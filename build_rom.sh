80bc89a2d524b3206b729288cf3f1640582a7074
sajidshahriar <sazidshahriar39@gmail.com>
2021-11-23 19:38:28 +0600

    Try Styx OS

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/ArcaneOS/Arcane_manifest -b R -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/StyxProject/manifest -b R -g default,-mips,-darwin,-notdefault
 git clone https://github.com/sajidshahriar72543/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 #40
 
 # build rom
-. build/envsetup.sh
-lunch aosp_beryllium-userdebug
+source build/envsetup.sh
+lunch styx_beryllium-userdebug
 export TZ=Asia/Dhaka #put before last build command
-mka bacon
+m styx-ota
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
