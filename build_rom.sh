f20c1377e0e25635fc7142137fdc36713e9fbb07
Aniket Dubey <63295358+itsaniketdubey@users.noreply.github.com>
2021-11-23 10:42:17 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/ShapeShiftOS/android_ma
 git clone https://github.com/itsaniketdubey/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-#2
+#3
 
 # build rom
 source build/envsetup.sh
