a6cf2d2699db797ac4a958e61bc92ac41f74e755
Alone0316 <70204635+Hunter-commits@users.noreply.github.com>
2021-11-24 17:06:28 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch mido_mido-userdebug
+lunch aosp_mido-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true 
 
 export TZ=Asia/Kolkata #put before last build command
