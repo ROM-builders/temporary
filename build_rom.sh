8713ad4465b76763aa9542b50f27a92778dcdde0
Chio <93084100+aberchio@users.noreply.github.com>
2021-11-24 18:37:19 +0700

    Switch to `eng` build variant for development purpose

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch superior_vayu-user
+lunch superior_vayu-eng
 export SELINUX_IGNORE_NEVERALLOWS=true
 export WITH_GAPPS=true
 export TZ=Asia/Jakarta #put before last build command
