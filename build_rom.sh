0212c32a8c5c4b89aa169a9d89dc9a6605df80b4
sajidshahriar <sazidshahriar39@gmail.com>
2021-11-25 01:07:09 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/resist15/manifest -b R 
 git clone https://github.com/sajidshahriar72543/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-#42
+#43
 
 # build rom
 source build/envsetup.sh
