d56ff6cedc885e1c6b606d6a2eeaff2781730804
AnGgIt88 <jarbull87@gmail.com>
2021-11-24 16:37:48 +0700

    Update AospExtended 8.7
    Signed-off-by: AnGgIt88 <jarbull87@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # Copyright (c) 2021, The Linux Foundation. All rights reserved.
 # Thankz to @Apon77
+# Date 24-Nov-2021 16:40
 
-# Date 15-Nov-2021
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/AospExtended/manifest.git -b 11.x -g default,-mips,-darwin,-notdefault
 git clone https://github.com/AnGgIt86/local_manifest.git --depth=1 -b Aex-11 .repo/local_manifests
@@ -11,8 +11,9 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 . build/envsetup.sh
 lunch aosp_rosy-userdebug
 export TZ=Asia/Jakarta
-export KBUILD_BUILD_USER=AnGgIt86
-export KBUILD_BUILD_HOST=FinixDev™
+export ALLOW_MISSING_DEPENDENCIES=true
+export BUILD_USERNAME=xiaomi
+export BUILD_HOSTNAME=finix-project
 m aex
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
