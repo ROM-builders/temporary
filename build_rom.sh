f4b4338570507ee0d8c85a9f1364a74bcdf26a33
sajidshahriar <sazidshahriar39@gmail.com>
2021-11-24 13:06:17 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/StyxProject/manifest -b
 git clone https://github.com/sajidshahriar72543/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-#40
+#41
 
 # build rom
 source build/envsetup.sh
