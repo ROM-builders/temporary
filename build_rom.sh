4bf67ef446b26cd2f5510e679f2aac836c7a868f
Gotam919 <82812329+Gotam919@users.noreply.github.com>
2021-11-24 07:17:43 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,10 +1,9 @@
 # sync rom
-repo init --depth=1 -u repo init -u https://github.com/ArrowOS/android_manifest.git -b arrow-12.0
- -g default,-mips,-darwin,-notdefault
+repo init --depth=1 -u repo init -u https://github.com/ArrowOS/android_manifest.git -b arrow-12.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Gotam919/local_manifest.git --depth=1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+ # build rom
 source build/envsetup.sh
 lunch arrow_RMX1941-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true 
@@ -13,3 +12,5 @@ mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+
+
