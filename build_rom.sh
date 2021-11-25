e9685e9f934c50cfbd4b32fa5fcb0b2f759dd2ae
Atharv <81741009+atharv2951@users.noreply.github.com>
2021-11-25 17:30:44 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Komodo-OS-Rom/manifest -b ten -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/ShapeShiftOS/android_manifest.git -b android_10 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/exynos7870shrp/local_manifest --depth=1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch komodo_j7velte-userdebug
+lunch ssos_j7velte-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
+export WITH_GAPPS=true
 export TZ=Asia/Kolkata #put before last build command
-masak komodo
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
