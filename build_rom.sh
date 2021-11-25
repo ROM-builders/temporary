5f4b4a08267292ba6f1b350154912aa24a973e7d
AashiQ <77391250+aashiq-exe@users.noreply.github.com>
2021-11-25 08:47:08 +0530

    RMX1851 evolution X aashiq-exe

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b elle -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b snow   -g default,-mips,-darwin,-notdefault
 git clone https://github.com/aashiq-exe/local_manifest --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
