97025f8372ece4c131133dbf69c63118bdd0a968
dopaemon <polarisdp@gmail.com>
2021-11-26 00:18:45 +0700

    Begonia: CherishOS
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,14 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/rasenss/manifest_local --depth 1 -b Aosp .repo/local_manifests
+git clone -b CherishOS-Begonia-A12 https://github.com/KernelPanic-OpenSource/local_manifest.git --depth 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch cherish_ginkgo-userdebug
+lunch cherish_begonia-userdebug
 export WITH_GMS=true
-export TZ=Asia/Jakarta #put before last build command
-brunch ginkgo
- 
+export TZ=Asia/Ho_Chi_Minh #put before last build command
+mka bacon -j$(nproc --all)
+
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
