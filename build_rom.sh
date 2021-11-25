8e90a839808d814ff86c1c62318bf3509a201477
dopaemon <polarisdp@gmail.com>
2021-11-26 00:22:36 +0700

    use brunch
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -8,7 +8,7 @@ source build/envsetup.sh
 lunch cherish_begonia-userdebug
 export WITH_GMS=true
 export TZ=Asia/Ho_Chi_Minh #put before last build command
-mka bacon -j$(nproc --all)
+brunch begonia
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
