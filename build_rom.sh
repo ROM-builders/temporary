f6dbfdb99b24eb42082f036c8251ed121f37ed33
Mayur <85495935+xenxynon@users.noreply.github.com>
2021-11-26 16:40:32 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,19 +1,23 @@
-
-# Initialize local repository
 repo init -u https://github.com/PixelExperience/manifest -b twelve
 
-# Sync
+
+
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
-# Set up environment
+
+
 $ . build/envsetup.sh
 
-# Choose a target
+
+
 $ lunch aosp_lavender-userdebug
 
-# Build the code
+
+
 $ mka bacon
 
-
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+
+
