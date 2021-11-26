ab38d01e05ab7b4e86ac5549a41fdead9556ebff
Mayur <85495935+xenxynon@users.noreply.github.com>
2021-11-26 14:44:28 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,33 @@
 
-# Initialize local repository
-repo init -u https://github.com/PixelExperience/manifest -b twelve
+git clone https://github.com/akhilnarang/scripts
+cd scripts
+sudo bash setup/android_build_env.sh
+cd ..
 
-# Sync
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+chmod a+x ~/bin/
 
 
-# build rom
-source build/envsetup.sh
+sudo repo init -u https://github.com/PixelExperience/manifest -b eleven-plus
+
+
+sudo repo sync -j$(nproc --all) -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+
+git clone https://github.com/ArrowOS-Devices/android_kernel_xiaomi_sdm660.git -b arrow-11.0 kernel/xiaomi/sdm660
+git clone https://github.com/sergej3/android_device_xiaomi_sdm660-common.git -b arrow-11.0 device/xiaomi/sdm660-common
+git clone https://github.com/sergej3/android_device_xiaomi_lavender.git -b arrow-11.0 device/xiaomi/lavender
+git clone https://github.com/sergej3/android_vendor_xiaomi_lavender.git -b arrow-11.0 vendor/xiaomi/lavender
+git clone https://github.com/ArrowOS-Devices/android_vendor_xiaomi_sdm660-common.git -b arrow-11.0 vendor/xiaomi/sdm660-common/
+
+git clone https://github.com/AOSP-11/hardware_qcom-caf_display_msm8998.git -b 11 hardware/qcom-caf/msm8998-r/display
+git clone https://github.com/AOSP-11/hardware_qcom-caf_audio_msm8998.git -b 11 hardware/qcom-caf/msm8998-r/audio
+git clone https://github.com/AOSP-11/hardware_qcom-caf_media_msm8998.git -b 11 hardware/qcom-caf/msm8998-r/media
+
+
+
+. build/envsetup.sh ccache -M 50G
 lunch aosp_lavender-userdebug
-export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
