d05b680d2fa580229afa470e9852b2e776b064bb
mobx <67450638+mobxCode@users.noreply.github.com>
2021-11-26 14:16:21 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/AospExtended/manifest.git -b 11.x -g default,-mips,-darwin,-notdefault
-git clone https://github.com/mobxCode/local_manifest.git --depth 1 -b aex .repo/local_manifests
+git clone https://github.com/mobxCode/local_manifest.git --depth 1 -b AEX-11.x .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 
@@ -14,7 +14,7 @@ export TZ=Asia/Jakarta #put before last build command
 m aex
 
 
-# Time = 12:47 WIB
+# Time = 14:20 WIB
 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
