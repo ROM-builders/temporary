575063e65d27e7774886dcd6946228a23772fb3a
Atharv Negi <94751339+AtarvNegi2951@users.noreply.github.com>
2021-11-26 22:20:31 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-17.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/AtarvNegi2951/local_manifest --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/elytraOS/manifest.git -b skylight -g default,-mips,-darwin,-notdefault
+git clone https://github.com/exynos7870shrp/local_manifest -b pine-s .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
-lunch lineage_j7velte-userdebug
+. build/envsetup.sh
+lunch elytra_pine-userdebug
 export ALLOW_MISSING_DEPENDENCES=true
 export TZ=Asia/Kolkata #put before last build command
-mka bacon
+brunch pine
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
