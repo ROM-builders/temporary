9a1bb4e6a6ed0bac0dc6a2b78e236de290b76c89
Faisalhossainerfan <67220051+Faisalhossainerfan@users.noreply.github.com>
2021-11-26 21:02:01 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/Project-Awaken/android_manifest.git -b 12 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Faisalhossainerfan/local_manifest-1.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/PixelExperience/manifest.git -b eleven-plus -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Faisalhossainerfan/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
-lunch awaken_santoni-userdebug
+sed -i '/qti-telephony-common.jar/d' vendor/xiaomi/santoni/santoni-vendor.mk
+. build/envsetup.sh
+lunch aosp_santoni-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Dhaka #put before last build command
-make bacon
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
