08cb4a6d3b7d4f03cb8be31905d01546ffb7da00
aslenofarid <92686606+aslenofarid@users.noreply.github.com>
2021-11-26 13:53:01 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-15.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/aslenofarid/Local_Manifests.git --depth 1 -b 15.1 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/aslenofarid/Local_Manifests.git --depth 1 -b 12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_X00TD-userdebug
+lunch aosp_X00TD-userdebug
 export TZ=Asia/Jakarta #put before last build command
 mka bacon
 
