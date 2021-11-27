347a8a087a109c2b280c39aefac6d3786adc0a19
techosmemeui <95066966+techosmemeui@users.noreply.github.com>
2021-11-27 21:39:39 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git:/github.com/RevengeOS/android_manifest.git -b r11.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/techosmemeui/local_manifests --depth 1 -b main .repo/local_manifests
+git clone https://github.com/techosmemeui/local_manifests.git --depth 1 -b eleven .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
