e4f281e7f8f790b4aa4cfb0f54d5e728fed9798b
Sanjeev <42514333+sanjeevstunner@users.noreply.github.com>
2021-11-27 08:48:53 +0530

    vayu: PE

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/PixelExperience/manifest.git -b eleven-plus -g default,-mips,-darwin,-notdefault
-git clone https://github.com/sanjeevstunner/Manifest.git --depth 1 -b twolip_aosp-11 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/PixelExperience/manifest.git -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/sanjeevstunner/Manifest.git --depth 1 -b vayu_aosp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_twolip-userdebug
+lunch aosp_vayu-userdebug
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
