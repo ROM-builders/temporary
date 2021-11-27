156cbea3bc7f5d40755a6579e94098f472647b1e
Mayur <85495935+xenxynon@users.noreply.github.com>
2021-11-27 07:53:34 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 #sync rom 
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest/blob/twelve/default.xml -b 11 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 -u https://github.com/PixelExperience/manifest/blob/twelve/default.xml -b 11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/xenxynon/manifest/blob/main/local_manifest.xml --depth 1 -b 11 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
