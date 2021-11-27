ca8bb9ddbb46a97c1583e2cae9689733424c78ad
second <82253361+HoleDroid@users.noreply.github.com>
2021-11-28 06:18:18 +0700

    Anjay

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DotOS/manifest.git -b dot11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/HoleDroid/local_manifests.git --depth 1 -b dos .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-17.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/HoleDroid/local_manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch dot_X00TD-userdebug
+lunch lineage_X00TD-userdebug
 export TZ=Asia/Jakarta #put before last build command
 make bacon
 
