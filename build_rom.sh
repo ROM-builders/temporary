5d0429283ba1377853a809276bc581458983109b
Jopii <jesusjodarpiernas@gmail.com>
2021-11-28 23:37:24 +0100

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,8 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/PixelExperience/manifest.git -b twelve -g default,-mips,-darwin,-notdefault
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 git clone git://github.com/Jopii/local_manifest.git --depth 1 -b main .repo/local_manifests
-
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
