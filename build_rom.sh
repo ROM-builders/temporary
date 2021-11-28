924c1929e922d309f0bc34363153f8f2e93280b4
sajidshahriar <sazidshahriar39@gmail.com>
2021-11-28 22:14:23 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,12 +3,13 @@ repo init --depth=1 --no-repo-verify -u git://github.com/ArcaneOS/Arcane_manifes
 git clone https://github.com/sajidshahriar72543/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-#49
+#50
 
 # build rom
 . build/envsetup.sh
 lunch aosp_beryllium-userdebug
 export TZ=Asia/Dhaka #put before last build command
+export BUILD_HOSTNAME=6e6f6f62
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
