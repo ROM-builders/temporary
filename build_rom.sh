a7c41b26f7eb923c8137119fd2f5e80edd7be14a
Lopster <68187820+Abhishek001konni@users.noreply.github.com>
2021-11-28 23:38:16 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,11 +5,9 @@ repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
 # build rom
 source build/envsetup.sh
-sudo mkdir /mnt/ccache
+lunch aosp_miatoll-userdebug
+m aex -j$(nproc --all) | tee log.txt
 
-lunch
-
-mka derp
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
