1571507b4ecf793e5d54506b0b7f68e16f8d88a6
Lopster <68187820+Abhishek001konni@users.noreply.github.com>
2021-11-28 23:39:28 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init -u git://github.com/aex-tmp/manifest.git -b 12.x
-https://github.com/Abhishek001konni/local_manifest --depth 1 -b main 
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+repo init --depth=1 --no-repo-verify -u git://github.com/aex-tmp/manifest.git -b 12.x -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Abhishek001konni/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch aosp_miatoll-userdebug
-m aex -j$(nproc --all) | tee log.txt
-
+export TZ=Asia/Dhaka #put before last build command
+m aex
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
