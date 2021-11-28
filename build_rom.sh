16577da22f6da235013554ed58f31b092f1a9c3f
Rishawn <70256146+Stealth1226@users.noreply.github.com>
2021-11-28 10:19:10 +0530

    trigger: initial rs988 S build

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lighthouse_h850-eng
+lunch lighthouse_rs988-eng
 make lighthouse
 
 
