1bb4d1f4e2decfe407ae054602fc8e5ed84b2148
GhostMaster69-dev <rathore6375@gmail.com>
2021-11-29 17:22:25 +0530

    ShapeshiftOS Android 11
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,12 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ShapeShiftOS/android_manifest.git -b android_11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/GhostMaster69-dev/local_manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/GhostMaster69-dev/local_manifest.git --depth 1 -b ssos-11-new .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch vince-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Kolkata #put before last build command
 make bacon
 
