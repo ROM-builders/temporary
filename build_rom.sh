da58613444b22945e0b77afff56e15e590cf224c
Naeem Mansuri <72666318+naeem000@users.noreply.github.com>
2021-11-29 14:55:14 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,12 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b elle -g default,-mips,-darwin,-notdefault
-git clone https://github.com/naeem000/local_manifest/blob/11/local_manifest.xml --depth 1 -b 11 .repo/local_manifests
+git clone https://github.com/naeem000/local_manifest.git --depth 1 -b 11 .repo/local_manifests
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
 # build rom
 source build/envsetup.sh
 lunch evolution_jasmine_sprout-userdebug
+export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Dhaka #put before last build command
 mka evolution
 
