3ce121590938aacbd28eb6d65c99c5672d958b2a
Kudakenai <76825405+Kudakenai@users.noreply.github.com>
2021-11-29 13:57:22 +0100

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-aosp_miatoll-userdebug
+lunch aosp_miatoll-userdebug
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
