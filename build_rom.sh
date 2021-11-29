76154e29cf9d6882609ba49905f7617a28e9ba24
xenxynon <85495935+xenxynon@users.noreply.github.com>
2021-11-29 06:11:14 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
-#sync rom 
-repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-12/manifest.git -b 12 -g default,-mips,-darwin,-notdefault
+#sync rom  
+repo init --depth=1 repo -u https://github.com/Project-Awaken/android_manifest -b 12 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/xenxynon/manifest/blob/main/local_manifest.xml --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
