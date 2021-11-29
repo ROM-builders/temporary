2cb0d14b03edf33cd62b689ee1aae2e30e436f9e
xenxynon <85495935+xenxynon@users.noreply.github.com>
2021-11-29 05:54:40 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 #sync rom 
 repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-12/manifest.git -b 12 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/xenxynon/manifest/blob/main/manifest.xml --depth 1 -b master .repo/local_manifests
+git clone https://github.com/xenxynon/manifest/blob/main/local_manifest.xml --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
