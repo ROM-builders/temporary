c1cea59dd6befb9d516c3642b34122cfd05ac8ff
xenxynon <85495935+xenxynon@users.noreply.github.com>
2021-11-29 15:27:22 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 #sync rom  
 repo init --depth=1 -u https://github.com/Project-Awaken/android_manifest -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/xenxynon/manifest/blob/main/local_manifest.xml --depth 1 -b master .repo/local_manifests
+git clone https://github.com/xenxynon/manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
