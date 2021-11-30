29e41a38a1f0673d15b9222f5e789ac52b95e071
badboy45y <92835014+badboy45y@users.noreply.github.com>
2021-12-01 05:02:04 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/ForkLineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/indi121/local_manifests.git --depth 1 -b flos .repo/local_manifests
+git clone https://github.com/badboy45y/local_manifests.git --depth 1 -b flos .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
