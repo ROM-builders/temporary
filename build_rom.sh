43e6bd194cb148d414dd2ba5b98c9e0b7a3126eb
badboy45y <92835014+badboy45y@users.noreply.github.com>
2021-12-01 05:00:37 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,6 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch lineage_vince-user
 export TARGET_FLOS=true
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
