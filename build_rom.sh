f1b8ac1d19a6bf4e30501a939fe1ba361b75cbe2
xenxynon <85495935+xenxynon@users.noreply.github.com>
2021-12-01 11:52:08 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 #sync rom  
-repo init --depth=1 -u https://github.com/Project-Awaken/android_manifest -b 11 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/xenxynon/manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch awaken_lavender-userdebug
 export TZ=Asia/Kolkata #put before last build command
-make bacon
+mka derp
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
