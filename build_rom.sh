ed521b3c2fc10e575804fc7745ac686269248d37
Akhil M S <51810638+AkhilmsAchu@users.noreply.github.com>
2021-12-01 09:07:45 +0530

    Revert "Update build_rom.sh"
    This reverts commit 402e8c35f48fffad8098e0f7eda47ce28d7e1224.

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/ProtonAOSP/android_manifest -b rvc -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/Havoc-OS/android_manifest.git -b eleven -g default,-mips,-darwin,-notdefault
 git clone https://github.com/AkhilmsAchu/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh 
-lunch aosp_tulip-userdebug
+lunch havoc_tulip-userdebug
 brunch tulip
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
