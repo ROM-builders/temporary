572f364e2a6d626268dba7ef954f005fae3e30d1
Sachin9822 <70788804+Sachin9822@users.noreply.github.com>
2021-12-02 20:30:31 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_jfltexx-userdebug
+lunch lineage_jfltexx_userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
