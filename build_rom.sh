7c2d7b2bd2532fe07d52babcde7a15c4b5b26fef
Sachin9822 <70788804+Sachin9822@users.noreply.github.com>
2021-12-02 18:42:16 +0530

    samsung-lineage-Sachin9822

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-brunch lineage_jfltexx-userdebug
+brunch lineage_jfltexx_userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
