092f9c384d27f0cdead627d5691766f067a0762d
RasyidAlKautsar <93345864+ArkanMuhammad1986@users.noreply.github.com>
2021-12-02 07:00:56 +0700

    Rebase All

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch ssos_mido-eng
+lunch ssos_mido-userdebug
 export TZ=Asia/Jakarta #put before last build command
 make bacon
 
