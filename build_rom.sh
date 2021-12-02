038c06bc9ca8d24285bc5f06b118d6689929b2cc
RasyidAlKautsar <93345864+ArkanMuhammad1986@users.noreply.github.com>
2021-12-03 06:15:35 +0700

    mido: Add Maintainer String

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 . build/envsetup.sh
 lunch cherish_mido-userdebug
 export SKIP_ABI_CHECKS=true
-export TZ=Asia/Dhaka #put before last build command
+export TZ=Asia/Jakarta #put before last build command
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
