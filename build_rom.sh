6a5979bf30204369f43cb1edeb7919869a34ea9a
CarlJohnson65 <87112984+CarlJohnson65@users.noreply.github.com>
2021-12-03 06:28:02 +0700

    fuck source legion

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch octavi_mido-userdebug
+lunch mido-userdebug
 export SKIP_ABI_CHECKS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
