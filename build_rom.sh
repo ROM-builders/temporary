241eeb3d62af6def8287dadbba85faa8f713d125
Carl Johnson <87112984+CarlJohnson65@users.noreply.github.com>
2021-12-03 15:49:17 +0700

    build with permissive

--- build_rom.sh
+++ build_rom.sh
@@ -7,6 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 . build/envsetup.sh
 lunch octavi_mido-userdebug
 export SKIP_ABI_CHECKS=true
+export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
