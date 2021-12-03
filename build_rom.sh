1b9cb9085ff80b9004c8634be3c35741d750a93f
Sachin9822 <70788804+Sachin9822@users.noreply.github.com>
2021-12-03 13:01:32 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://android.googlesource.com/platform/manifest -b master -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-17.0 -g default,-mips,-darwin,-notdefault
 
 git clone https://github.com/Sachin9822/local_manifest.git --depth 1 -b main .repo/local_manifests
 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-. build/envsetup.sh
-lunch aosp_marlin-userdebug
+source build/envsetup.sh
+breakfast tissot
+croot
+brunch tissot
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
