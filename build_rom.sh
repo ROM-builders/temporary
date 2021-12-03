f74503edb30a78320486c347fc22b5953aabfeed
Aniket Dubey <63295358+itsaniketdubey@users.noreply.github.com>
2021-12-03 11:00:57 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/ForkLineageOS/android.g
 git clone https://github.com/itsaniketdubey/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-#11
+#12
 
 # build rom
 source build/envsetup.sh
