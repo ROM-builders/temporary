375396e3a59ce98f94b4eb66324d1736fc8a8838
sajidshahriar <sazidshahriar39@gmail.com>
2021-12-03 12:03:22 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,18 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/ArcaneOS/Arcane_manifest -b R -g default,-mips,-darwin,-notdefault
-git clone https://github.com/sajidshahriar72543/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/ForkLineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/sajidshahriar72543/local_manifest.git --depth 1 -b flos .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 #51
 
 # build rom
-. build/envsetup.sh
-lunch aosp_beryllium-userdebug
+source build/envsetup.sh
+lunch lineage_beryllium-userdebug
 export TZ=Asia/Dhaka #put before last build command
 export BUILD_HOSTNAME=6e6f6f62
-mka bacon
+export TARGET_FLOS=true
+export WITH_GMS=true
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
