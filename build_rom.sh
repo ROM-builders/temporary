229adba9b67ba31fdc8824684a3d373aeec3e194
xenxynon <85495935+xenxynon@users.noreply.github.com>
2021-12-03 21:18:00 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 #sync rom  
-repo init --depth=1 https://github.com/aex-tmp/manifest.git -b 12.x -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/aex-tmp/manifest.git -b 12.x -g default,-mips,-darwin,-notdefault
 git clone https://github.com/xenxynon/manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
