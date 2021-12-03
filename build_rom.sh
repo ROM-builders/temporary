38d20f58197c28ee511007d26c8bed484eeaa556
Carl Johnson <87112984+CarlJohnson65@users.noreply.github.com>
2021-12-03 10:13:12 +0700

    Fix enforcing no boot

--- build_rom.sh
+++ build_rom.sh
@@ -8,7 +8,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 lunch octavi_mido-userdebug
 export SKIP_ABI_CHECKS=true
 export ALLOW_MISSING_DEPENDENCIES=true
-export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
