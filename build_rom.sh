571ab2a68f3466eef8979062d9298728bb804234
sajidshahriar <sazidshahriar39@gmail.com>
2021-12-03 13:54:30 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/ForkLineageOS/android.g
 git clone https://github.com/sajidshahriar72543/local_manifest.git --depth 1 -b flos .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-#51
+#52
 
 # build rom
 source build/envsetup.sh
