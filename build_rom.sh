0ec94743fef7935f94716e98e3c68b013009e380
Sachin9822 <70788804+Sachin9822@users.noreply.github.com>
2021-12-03 11:41:54 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-breakfast tissot
 lunch tissot
 export TZ=Asia/Dhaka #put before last build command
 mka derp
