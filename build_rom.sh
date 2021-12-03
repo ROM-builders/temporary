85b7ac3cc99a0d3894a7e7d8da24d34ffdfd5519
badboy45y <92835014+badboy45y@users.noreply.github.com>
2021-12-03 14:29:59 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 . build/envsetup.sh
 lunch aosp_vince-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
-export TZ=Asia/Dhaka #put before last build command
+export TZ=Asia/Dhaka #put before las build command
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
