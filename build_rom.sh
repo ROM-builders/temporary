847b659fc49826f32dc4503c939182d6f9b1dc07
iamnabilzaman <94600437+iamnabilzaman@users.noreply.github.com>
2021-12-04 14:00:39 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,19 @@
 # sync rom
-repo init --depth=1 -g default,-mips,-darwin,-notdefault  git://github.com/yaap/manifest.git -b eleven
-git clone https://github.com/iamnabilzaman/local-mainfest --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/yaap/manifest.git -b eleven -g default,-mips,-darwin,-notdefault
+git clone https://github.com/iamnabilzaman/local-mainfest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
+#2
 
 # build rom
 source build/envsetup.sh
-lunch yaap_santoni-user
+lunch derp_vayu-user
+. build/envsetup.sh
+lunch aosp_beryllium-userdebug
 export TZ=Asia/Dhaka #put before last build command
-mka yaap
+mka derp
+export BUILD_HOSTNAME=6e6f6f62
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
