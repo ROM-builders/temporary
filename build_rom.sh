01ef5701b4229c6fe3b1e271de5def5cc8182f89
iamnabilzaman <94600437+iamnabilzaman@users.noreply.github.com>
2021-12-05 00:44:05 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,3 @@
-# sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/yaap/manifest.git -b eleven -g default,-mips,-darwin,-notdefault
 git clone https://github.com/iamnabilzaman/local-mainfest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
