34f01a22f7fcf11637f4d01092638d2412423432
iamnabilzaman <94600437+iamnabilzaman@users.noreply.github.com>
2021-12-04 14:05:31 +0600

    yaap santoni iamnabilzaman

--- build_rom.sh
+++ build_rom.sh
@@ -3,17 +3,11 @@ repo init --depth=1 --no-repo-verify -u git://github.com/yaap/manifest.git -b el
 git clone https://github.com/iamnabilzaman/local-mainfest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-#2
-
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
-. build/envsetup.sh
-lunch aosp_beryllium-userdebug
+lunch yaap_device-userdebug
 export TZ=Asia/Dhaka #put before last build command
-mka derp
-export BUILD_HOSTNAME=6e6f6f62
-mka bacon
+make yaap
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
