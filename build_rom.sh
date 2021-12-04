b26638ded58a5ce57ba41bb68bb7a49f9be4c980
Jall <95171334+Razorito@users.noreply.github.com>
2021-12-04 20:01:19 +0700

    Fix manifest syncing error

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch vayu-user
+export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Jakarta #put before last build command
 mka bacon
 
