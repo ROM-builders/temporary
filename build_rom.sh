abd77bba148236cfee496f14b3919f7cf9d63cd3
Akira <76020594+AkiraNoSushi@users.noreply.github.com>
2021-12-04 22:14:38 -0400

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch arrow_pine-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=UTC #put before last build command
 m bacon
 
