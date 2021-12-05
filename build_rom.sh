ed9c5c816f4bb48d8b0d76a66efd9237173220c9
Faisalhossainerfan <67220051+Faisalhossainerfan@users.noreply.github.com>
2021-12-05 11:51:10 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch exthm_santoni-userdebug
+export EXTHM_GAPPS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
