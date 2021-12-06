5c819c400008117706fc73a5ae4ef948b89d37f2
Kendras056 <88943971+Kendras056@users.noreply.github.com>
2021-12-06 10:42:28 +0700

    fix smart charging maybe

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch amy_X00TD-userdebug
-export BUILD_USERNAME=tiktodz
+export BUILD_USERNAME=venom
 export BUILD_HOSTNAME=android-build
 export TZ=Asia/Tokyo
 mka bacon
