c605158afdf0aa168acae7ede37fcd6a085cfd97
Kendras056 <88943971+Kendras056@users.noreply.github.com>
2021-12-06 16:43:38 +0700

    Regenerate

--- build_rom.sh
+++ build_rom.sh
@@ -8,7 +8,7 @@ source build/envsetup.sh
 lunch amy_X00TD-userdebug
 export BUILD_USERNAME=venom
 export BUILD_HOSTNAME=android-build
-export TZ=Asia/Tokyo
+export TZ=Asia/Jakarta
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
