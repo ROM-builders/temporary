0ad64c5cbe10b09667b4d92e405a78022e69c485
Alone0316 <rutujamandlik06@gmail.com>
2021-12-06 10:55:04 +0000

    Fixed selinux neverallow

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch cipher_mido-userdebug
-SELINUX_IGNORE_NEVERALLOWS=true
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Kolkata #put before last build command
 make bacon
 
