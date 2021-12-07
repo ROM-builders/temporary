17d1d59b4552cf6ad79ec9bed5c5d802c1fb5ca4
Kendras056 <88943971+Kendras056@users.noreply.github.com>
2021-12-07 16:01:57 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -8,7 +8,7 @@ source build/envsetup.sh
 lunch derp_X00TD-userdebug
 export BUILD_USERNAME=venom
 export BUILD_HOSTNAME=android-build
-export TZ=Asia/Jakarta
+export TZ=Asia/Tokyo
 mka derp
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
