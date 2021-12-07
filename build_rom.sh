f4c2b95db1af268fb123574ffb8ed73b19a1f66a
qwertyuiii-code <85434511+qwertyuiii-code@users.noreply.github.com>
2021-12-07 18:53:22 +0500

    Build bootlegRom
    Build bootlegRom for certus

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,13 @@
-repo init -depth=1 --no-repo-verify -u git://github.com/crdroidandroid/android.git -b 11.0 -g default,-mips,-darwin,-notdefault
+repo init -depth=1 --no-repo-verify -u https://github.com/BootleggersROM/manifest.git -b queso -g default,-mips,-darwin,-notdefault
 git clone https://github.com/qwertyuiii-code/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 
 # build rom
-. build/envsetup.sh
-brunch certus
+source build/envsetup.sh
+lunch bootleg_certus-userdebug
+mka bacon
+
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
