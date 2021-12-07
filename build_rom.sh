1414eb89cb90cb216bc117bfecddd7cd54e336ba
Jall <95171334+Razorito@users.noreply.github.com>
2021-12-08 04:52:37 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest 
 git clone https://github.com/Razorito/manifest.git --depth=1 -b evo12-vayu .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom 
 . build/envsetup.sh
 lunch vayu-user
 export ALLOW_MISSING_DEPENDENCIES=true
