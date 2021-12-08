21da26ca604f699f72a3cf4d299309900f6082a4
Kendras056 <88943971+Kendras056@users.noreply.github.com>
2021-12-08 09:18:27 +0700

    awaken bringup

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/Project-Awaken/android_manifest.git -b 11 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Kendras056/local_manifests.git --depth 1 -b derp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) 
 
 # build rom
 source build/envsetup.sh
-lunch derp_X00TD-userdebug
+lunch awaken_X00TD-userdebug
 export BUILD_USERNAME=venom
 export BUILD_HOSTNAME=android-build
 export TZ=Asia/Tokyo
-mka derp
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
