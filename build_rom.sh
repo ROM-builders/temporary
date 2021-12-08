647c5ef36026e3e3deb5dfd92b9d70e59bc773a2
jaichiranjeeva <jaichiranjeevadadi@gmail.com>
2021-12-08 18:30:41 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/unknownyard/manifest.git -b t -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/unknownyard/manifest.git -b test -g default,-mips,-darwin,-notdefault
 git clone https://github.com/jaichiranjeeva/localmanifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
