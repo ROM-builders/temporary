bbe6eee8b4bf74940613c1e6d8b97b830c82459e
RasyidAlKautsar <93345864+RasyidAlKautsar@users.noreply.github.com>
2021-12-09 06:31:26 +0700

    fix unable sync full

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b 12 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/RasyidAlKautsar/local_manifest.git --depth 1 -b Kuntull .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 -j1 --fail-fast
 
 # build rom
 source build/envsetup.sh
