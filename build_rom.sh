36ea2e80b284a0f489af6791d9736bf0e0f385f1
RasyidAlKautsar <93345864+RasyidAlKautsar@users.noreply.github.com>
2021-12-09 05:19:36 +0700

    fix sync

--- build_rom.sh
+++ build_rom.sh
@@ -9,7 +9,7 @@ lunch awaken_mido-userdebug
 export SKIP_ABI_CHECKS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
-export TZ=Asia/Dhaka #put before last build command
+export TZ=Asia/Jakarta #put before last build command
 make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
