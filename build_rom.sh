08ff7f18722b3f6ebcaa1274a25f236279220efe
Aditya OP <95341296+badboypt@users.noreply.github.com>
2021-12-09 09:59:22 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch aosp_vince-userdebug
-export ALLOW_MISSING_DEPENDENCIES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build comman
 make bacon
