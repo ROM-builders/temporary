ef4cf25aff63dff7fe255e210c7d0a5f62fbe6ae
Faisalhossainerfan <67220051+Faisalhossainerfan@users.noreply.github.com>
2021-12-10 00:00:37 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/Project-Fluid/manifest.git -b fluid-12 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/Project-Awaken/android_manifest.git -b 12 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Faisalhossainerfan/local_manifest-1.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch fluid_santoni-userdebug
+lunch awaken_santoni-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Dhaka #put before last build command
-mka bacon
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
