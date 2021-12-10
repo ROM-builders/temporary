36fbb7ba22c66623560f5a7a178c5ed200f6f4f9
zєг๏ <77091137+khirendra03@users.noreply.github.com>
2021-12-10 23:10:39 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/VoltageOS/manifest.git -b 12 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/khirendra03/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/Spark-Rom/manifest.git -b 12 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/khirendra03/local_manifest.git --depth 1 -b spark .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
