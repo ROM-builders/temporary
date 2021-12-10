94586146ba00d79cb4ca09e799d72844bacd98c8
Achmad Arsyil Wiradinata <dekukamikix@gmail.com>
2021-12-10 20:41:18 +0700

    surya-dotos6.0-dekukamikix

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-12/manifest.git -b 12 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/DotOS/manifest.git -b dot12 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/dekukamikix/local_manifests.git --depth 1 -b surya .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
