6274557a50e59e72e1ec6c142ff8c333e86c4560
Rishawn <70256146+Stealth1226@users.noreply.github.com>
2021-12-10 20:00:00 +0530

    build userdebug

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 export WITH_GAPPS=true
-lunch lighthouse_X2-eng
+lunch lighthouse_X2-userdebug
 make lighthouse
 
 
