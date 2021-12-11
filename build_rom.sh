735191ff9811c18608ee6102b74da55b7ca673db
FyyyyK <87652862+FyyyyK@users.noreply.github.com>
2021-12-11 10:05:27 +0700

    initial corvus

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,16 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/SuperiorOS/manifest.git -b eleven -g default,-mips,-darwin,-notdefault
-git clone https://github.com/FyyyyK/local_manifests.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/Corvus-R/android_manifest.git -b 11 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/FyyyyK/local_manifests.git --depth 1 -b 11 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch superior_X00TD-userdebug
-export TZ=Asia/Jakarta #put before last build command
-make bacon
-
+lunch corvus_X00TD-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
+export SKIP_ABI_CHECKS=true
+export RAVEN_LAIR=Unofficial
+export TZ=Asia/Bangkok #put before last build command
+make corvus
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
