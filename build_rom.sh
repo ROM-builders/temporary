5df3c361d07d78f9c2bffce62284a251c67821e6
CYB3R GOD <39610904+vickycena1@users.noreply.github.com>
2021-12-11 13:27:35 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,17 +1,18 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/PixelExtended/manifest.git -b snow -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/ArrowOS/android_manifes.git -b arrow-12.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/vickycena1/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_juice-userdebug
+lunch arrow_juice-userdebug
 export BUILD_USER=cyb3rgod0
 export BUILD_HOST=fsociety
 export BUILD_USERNAME=cyb3rgod0
 export BUILD_HOSTNAME=fsociety
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
+m otapackage
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
