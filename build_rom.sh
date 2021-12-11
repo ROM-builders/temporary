13fe505d4fd6c15041816979d1906e40bd2b13cd
Karan Parashar <whyredfire@gmail.com>
2021-12-11 11:50:14 +0530

    fixup error

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest.git -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/PixelOS-Devices/local_manifests.git --depth 1 -b twelve-ci .repo/local_manifests
+git clone https://github.com/PixelOS-Devices/local_manifests.git --depth 1 -b twelve-plain .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom 
