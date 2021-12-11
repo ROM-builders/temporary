63d2dcb00d1a5acd2d3665f4ad3bb4db5c5177e3
FyyyyK <87652862+FyyyyK@users.noreply.github.com>
2021-12-11 11:55:23 +0700

    display

--- build_rom.sh
+++ build_rom.sh
@@ -6,13 +6,9 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch corvus_X00TD-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
-export ALLOW_MISSING_DEPENDENCIES=true
-export SKIP_ABI_CHECKS=true
 export RAVEN_LAIR=Unofficial
 export TZ=Asia/Bangkok #put before last build command
 make corvus
-
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
