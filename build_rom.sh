a40581c51b183b3336202312425254daec7452a2
HeroBuxx <herobuxx@conqueros.co>
2021-12-12 05:43:08 +0000

    bluecross: AospExtended
    Signed-off-by: HeroBuxx <herobuxx@conqueros.co>

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,36 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/bluecrossdev/manifest -b 12.x -g default,-mips,-darwin,-notdefault
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
-source build/envsetup.sh
-lunch derp_vayu-user
-export TZ=Asia/Dhaka #put before last build command
-mka derp
+# Clone device source
+git clone https://github.com/bluecrossdev/device_google_crosshatch -b 12.x device/google/crosshatch
+git clone https://github.com/bluecrossdev/device_google_crosshatch-sepolicy device/google/crosshatch-sepolicy
+git clone https://github.com/bluecrossdev/vendor_google vendor/gpx --depth=11
+mkdir vendor/google
+mv vendor/gpx/* vendor/google/
+git clone https://github.com/HotDogfinba11/kernel_google_msm-4.9 --depth=1 kernel/google/bluecross
+rm -rf device/aosp/sepolicy
+git clone https://github.com/herobuxx/device_custom_sepolicy device/aosp/sepolicy
+git clone https://github.com/ProtonAOSP/android_packages_apps_ElmyraService packages/apps/ElmyraService -b sc
 
-# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+# Set timezone
+export TZ=Asia/Jakarta
+
+# Extra build flags
+export WITH_GAPPS=true
+
+# Build for Pixel 3
+source build/envsetup.sh
+lunch aosp_blueline-user
+mka aex
+
+# Upload for Pixel 3
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+
+# Build for Pixel 3 XL
+source build/envsetup.sh
+lunch aosp_crosshatch-user
+mka aex
+
+# Upload for Pixel 3 XL
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
