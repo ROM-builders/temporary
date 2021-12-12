84f91f9f0086613559dc2c1b84fe735201c5f9b1
FyyyyK <87652862+FyyyyK@users.noreply.github.com>
2021-12-12 22:46:19 +0700

    Los dol

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/Corvus-R/android_manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/FyyyyK/local_manifests.git --depth 1 -b 11 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-17.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/FyyyyK/local_manifests.git --depth 1 -b 10 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch corvus_X00TD-userdebug
+lunch lineage_X00TD-eng
 export TZ=Asia/Bangkok #put before last build command
-make corvus
+mka lineage
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
