4f7ed3a56ad6fb6a26aa881978acf6f1ec1cd3db
aslenofarid <92686606+aslenofarid@users.noreply.github.com>
2021-12-13 05:54:29 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_X00TD-userdebug
+lunch lineage_X00TD-eng
 export TZ=Asia/Jakarata #put before last build command
 mka bacon
 
