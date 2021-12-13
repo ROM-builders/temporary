161748011cd40dfab7cb1394da72c7ae7bbc41ce
Carl Johnson <87112984+CarlJohnson65@users.noreply.github.com>
2021-12-13 21:37:39 +0700

    elixir time

--- build_rom.sh
+++ build_rom.sh
@@ -1,17 +1,16 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b spark -g default,-mips,-darwin,-notdefault
-git clone https://github.com/CarlJohnson65/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Elixir/manifest -b snow -g default,-mips,-darwin,-notdefault
+git clone https://github.com/CarlJohnson65/local_manifest.git --depth 1 -b elixir .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 -j1 --fail-fast
 
 # build rom
-source build/envsetup.sh
-lunch spark_mido-userdebug
+. build/envsetup.sh
+lunch aosp_mido-userdebug
 export RELAX_USES_LIBRARY_CHECK=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 export SKIP_ABI_CHECKS=true
 export TZ=Asia/Dhaka #put before last build command
-mka spark
-
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
