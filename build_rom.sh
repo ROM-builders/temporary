f20ca5e69df5cb9558a40bc612b2268635181e78
FyyyyK <87652862+FyyyyK@users.noreply.github.com>
2021-12-13 11:35:17 +0700

    Dancok

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_X00TD-ueer
+lunch lineage_X00TD-user
 export TZ=Asia/Bangkok #put before last build command
 mka bacon
 
