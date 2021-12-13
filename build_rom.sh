1cc03be3a76afda2a0e63fbf9079f282c32d04ab
FSOC137Y <39610904+vickycena1@users.noreply.github.com>
2021-12-14 03:45:51 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,17 +1,17 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/Corvus-R/android_manifest.git -b 11 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/PixelExperience/manifest.git -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/vickycena1/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch corvus_juice-userdebug
+lunch aosp_juice-userdebug
 export BUILD_USER=cyb3god0
 export BUILD_HOST=fsociety
 export BUILD_USERNAME=cyb3god0
 export BUILD_HOSTNAME=fsociety
 export TZ=Asia/Kolkata #put before last build command
-make corvus
+mka bacon
 #new
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
