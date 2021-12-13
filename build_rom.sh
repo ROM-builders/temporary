5ce75b67204dc7d8f542c1ccddff2d87d2fd28d6
atharv705 <84908033+atharv705@users.noreply.github.com>
2021-12-13 18:03:54 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch voltage_pine-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
-export TZ=Asia/Dhaka #put before last build command
+export TZ=Asia/Kolkata #put before last build command
 brunch pine
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
