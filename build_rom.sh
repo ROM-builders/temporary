0f6170029c77ed00f01438f3d31c7b79d6dca6e9
FyyyyK <87652862+FyyyyK@users.noreply.github.com>
2021-12-13 13:21:18 +0700

    English

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_X00TD-user
+lunch lineage_X00TD-eng
 export TZ=Asia/Bangkok #put before last build command
 mka bacon
 
