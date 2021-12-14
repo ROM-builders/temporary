5a2463a606861a0ad9a5cf8b43c7ad4b4e752e94
Faisalhossainerfan <67220051+Faisalhossainerfan@users.noreply.github.com>
2021-12-15 02:45:08 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/yaap/manifest.git -b eleven -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Faisalhossainerfan/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/ProtonAOSP/android_manifest.git -b sc -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Faisalhossainerfan/local_manifest-1.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch yaap_santoni-userdebug
+lunch aosp_santoni-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Dhaka #put before last build command
-make yaap
+m
 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
