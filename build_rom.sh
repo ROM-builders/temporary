53eec0182e82dfe31023e148f1c1279c825e1138
hsx02 <hsx02self@gmail.com>
2021-12-15 02:44:33 +0530

    manifest: make unofficial spark

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/PixelExtended/manifest.git -b snow -g default,-mips,-darwin,-notdefault
-git clone https://github.com/yesidev/Local-Manifests.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/Spark-Rom/manifest.git -b spark -g default,-mips,-darwin,-notdefault
+git clone https://github.com/yesidev/Local-Manifests.git --depth 1 -b spark .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-. build/envsetup.sh
-lunch aosp_pine-user
+. build/env*
+lunch spark_pine-user
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
