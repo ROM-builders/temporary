e4a16b949fcd6656997a2b60af2b5526d3741edc
Thoreck <46625454+fajar4561@users.noreply.github.com>
2021-12-14 08:44:37 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/NusantaraProject-ROM/android_manifest.git -b 11 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/fajar4561/local_manifest.git --depth 1 -b nd .repo/local_manifests
-repo forall -vc "git reset --hard"
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
