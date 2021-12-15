209ead8b6699e107c2a2fb8c5140bd04f02b270f
iaiaian-droid <96176772+iaiaian-droid@users.noreply.github.com>
2021-12-15 19:58:23 +0800

    pine-linage-iaiaian-droid

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b master -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/iaiaian-droid/Local-Manifests.git --depth 1 -b pixel .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
