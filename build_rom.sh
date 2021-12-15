9f045710db42225c67eb844a294c8d2a2100a2e3
iaiaian-droid <96176772+iaiaian-droid@users.noreply.github.com>
2021-12-15 18:43:34 +0800

    adapted to build Lineage OS 18.1
    adapted to build Lineage OS 18.1 using vytska pixel branch

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/NusantaraProject-ROM/android_manifest -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/vytska69/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/iaiaian-droid/Local-Manifests.git --depth 1 -b pixel .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
