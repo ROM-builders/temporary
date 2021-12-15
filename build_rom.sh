04b797c0c2682c4ad37ddad68116fabcd58a9b54
iaiaian-droid <96176772+iaiaian-droid@users.noreply.github.com>
2021-12-15 20:20:20 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b master -g default,-device,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b master -g default,-mips,-darwin,-notdefault
 git clone https://github.com/iaiaian-droid/Local-Manifests.git --depth 1 -b lineage .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
