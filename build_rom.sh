d7fd93025a1b02e666a2c9e209805127dc9ce5b5
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2021-12-16 23:01:29 +0700

    Duh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/AospExtended/manifest.git -b 12.x -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/aex-tmp/manifest.git -b 12.x -g default,-mips,-darwin,-notdefault
 git clone https://github.com/NFS86/local_manifest --depth 1 -b aex-12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
