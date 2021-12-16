1028791af621f8b01404497d93ddbe65473a80ce
Tuan Anh <60842977+log1cs@users.noreply.github.com>
2021-12-16 08:27:01 +0700

    update lighthouse

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/hentaiOS/platform_manifest -b Rika -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/log1cs/local-manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/lighthouse-os/manifest.git -b sailboat -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/log1cs/judyln_local-manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-. build/envsetup.sh
-lunch hentai_judyln-userdebug
+source build/envsetup.sh
+lunch lighthouse_judyln-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/HoChiMinh #put before last build command
-mka otapackage
+make lighthouse
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
