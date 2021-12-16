ad849dbabb0f0e3671ebd2dbdd7813c5c0da1839
NFS86 <jarbull86@gmail.com>
2021-12-16 22:54:33 +0700

    test AEX 12
    Signed-off-by: NFS86 <jarbull86@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ArcaneOS/Arcane_manifest -b R -g default,-mips,-darwin,-notdefault
-git clone https://github.com/NFS86/local_manifest --depth 1 -b eleven .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/AospExtended/manifest.git -b 12.x -g default,-mips,-darwin,-notdefault
+git clone https://github.com/NFS86/local_manifest --depth 1 -b aex-12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom 
@@ -11,7 +11,7 @@ export BUILD_USER=finix
 export BUILD_HOST=rosy
 export BUILD_USERNAME=finix
 export BUILD_HOSTNAME=rosy
-mka bacon
+m aex
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
