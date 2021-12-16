cb7472ca13899c3e2d022dcaaf7ba20c055ada16
Aditya OP <95341296+badboypt@users.noreply.github.com>
2021-12-16 05:36:26 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/yaap/manifest.git -b eleven -g default,-mips,-darwin,-notdefault
-git clone https://github.com/badboypt/local_manifest.git --depth 1 -b yaap .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/yaap/manifest.git -b twelve-g default,-mips,-darwin,-notdefault
+git clone https://github.com/badboypt/local_manifest.git --depth 1 -b a12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
 lunch yaap_vince-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
-export TZ=Asia/Dhaka #put before last build comman
+export TZ=Asia/Dhaka #put before last build command
 make yaap
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
