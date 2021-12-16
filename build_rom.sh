deb4fb85b99a18c519ff0a44d1a5e0aeeb4661e5
bdbdcom <96200526+bdbdcom@users.noreply.github.com>
2021-12-16 20:08:33 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Octavi-OS/platform_manifest.git -b 12 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExtended/manifest -b snow -g default,-mips,-darwin,-notdefault
 git clone https://github.com/bdbdcom/local_manifests.git --depth 1 -b backup .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 # build rom 
 source build/envsetup.sh
-lunch Octavi_mido-user
+lunch aosp_mido-user
 export TZ=Asia/Dhaka #put before last build command
-make bacon
+mka bacon -jX
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
