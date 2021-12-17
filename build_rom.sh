40d5ed9515a4fde386f3dc99f0c4998bff862fc0
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2021-12-17 08:24:45 +0700

    Coeg

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/aex-tmp/manifest.git -b
 git clone https://github.com/NFS86/local_manifest --depth 1 -b aex-12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom 
 source build/envsetup.sh
 lunch aosp_rosy-userdebug
 export TZ=Asia/Jakarta
