ce4b62d7b4b841395d13862ba947e915c89b8e69
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2021-12-17 13:44:57 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,17 +1,17 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/aex-tmp/manifest.git -b 12.x -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b spark -g default,-mips,-darwin,-notdefault
 git clone https://github.com/NFS86/local_manifest --depth 1 -b aex-12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom 
 source build/envsetup.sh
-lunch aosp_rosy-userdebug
+lunch spark_rosy-userdebug
 export TZ=Asia/Jakarta
 export BUILD_USER=finix
 export BUILD_HOST=rosy
 export BUILD_USERNAME=finix
 export BUILD_HOSTNAME=rosy
-m aex
+mka spark
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
