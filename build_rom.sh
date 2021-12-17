cb2d22ebe3a9114a1fd778d842624515ea6b7d3d
ibratabian17 <71920907+ibratabian17@users.noreply.github.com>
2021-12-17 07:43:30 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -14,6 +14,6 @@ KBUILD_BUILD_USER="Ibratabian17"
 KBUILD_BUILD_HOST="Hiper"
 brunch A6020
 
-#13
+#14 Update Sourcw
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
