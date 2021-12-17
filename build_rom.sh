d429260c62e1fb44679f26469c79a67e34ecf5d6
Lynx <94979060+lynnnnzx@users.noreply.github.com>
2021-12-18 03:57:05 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,11 +3,11 @@ repo init --depth=1 --no-repo-verify -u git://github.com/Project-LegionOS/manife
 git clone https://github.com/lynnnnzx/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom 
+# build rom
 source build/envsetup.sh
 lunch legion_juice-userdebug
 export TZ=Asia/Jakarta
-make legion 
+make legion
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
