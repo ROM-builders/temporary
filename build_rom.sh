7d515d9f1c54c1cb736f91d6f54d7c9337e3421c
bdbdcom <96200526+bdbdcom@users.noreply.github.com>
2021-12-17 15:18:15 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,7 @@
 
 repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b 12 -g default,-mips,-darwin,-notdefault
 
-git clone https://github.com/bdbdcom/local_manifest.git --depth 1 -b awaken .repo/local_manifests
+git clone https://github.com/bdbdcom/local_manifest.git --depth 1 -b main .repo/local_manifests
 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
