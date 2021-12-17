eea297b50273460e7bfd7da8aef58f162e1db707
bdbdcom <96200526+bdbdcom@users.noreply.github.com>
2021-12-17 15:08:35 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,8 +1,8 @@
 # sync rom
 
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExtended/manifest -b snow -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b 12 -g default,-mips,-darwin,-notdefault
 
-git clone https://github.com/bdbdcom/local_manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/bdbdcom/local_manifest.git --depth 1 -b awaken .repo/local_manifests
 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
