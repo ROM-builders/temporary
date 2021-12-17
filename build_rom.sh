f71822c0e6dce7fba21db57d1c80cfe06346de89
yesidev <96131314+yesidev@users.noreply.github.com>
2021-12-17 08:57:28 +0530

    Switch to userdebug

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch spark_pine-user
+lunch spark_pine-userdebug
+export WITH_GAPPS=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
