287035ced10cb8ef1d214f6167c72e3239d296dc
Aditya OP <95341296+Aknx77@users.noreply.github.com>
2021-12-18 12:45:57 +0530

    bliss rom
    Changed rom superior to bliss rom

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/SuperiorOS/manifest.git -b eleven -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Aknx77/local_manifest.git --depth 1 -b superior .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/BlissRoms/platform_manifest.git -b arcadia -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Aknx77/local_manifest.git --depth 1 -b bliss .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch superior_vince-userdebug
+blissify options vince
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
-mka bacon
+blissify vince
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
