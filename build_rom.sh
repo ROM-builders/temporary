70f1fc518dd96a1790f6ae2dab44eb2b9ff119c4
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2021-12-18 20:09:20 +0700

    Fix sepolicy

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b
 git clone https://github.com/NFS86/local_manifest --depth 1 -b aex-12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom 
+# build rom
 source build/envsetup.sh
 lunch spark_rosy-userdebug
 export TZ=Asia/Jakarta
