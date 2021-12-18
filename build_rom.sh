323f8682c97c9ff16dd0cb43aa9d74f56eadd5ca
RasyidAlKautsar <arkan1986muhammad@gmail.com>
2021-12-19 04:35:52 +0700

    update

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ArkanMuhammad1986/local_manifest.git --depth 1 -b ch .repo/local_manifests
+git clone https://github.com/RasyidAlKautsar/local_manifest.git --depth 1 -b cherish-12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -9,7 +9,6 @@ lunch cherish_mido-userdebug
 export SKIP_ABI_CHECKS=true
 export TZ=Asia/Jakarta #put before last build command
 mka bacon
-
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
