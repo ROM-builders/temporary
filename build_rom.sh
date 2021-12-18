8e3781bf3d9a307f3eb5b3c5d5a4e2340f41a6f7
HitchTrailBlazer <75736765+TelkomFlexi@users.noreply.github.com>
2021-12-18 12:30:14 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/NusantaraProject-ROM/android_manifest -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/TelkomFlexi/local-manifest.git --depth 1 -b nad .repo/local_manifests
+git clone https://github.com/TelkomFlexi/local-manifest.git --depth 1 -b ginkgo .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
