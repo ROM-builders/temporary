902074d5874db1f28bab33b481e3834ddfc306b6
Jake <92788616+iaiaian1@users.noreply.github.com>
2021-12-18 12:42:09 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage 18.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/iaiaian-droid/local_manifest.git --depth 1 -b lineage .repo/local_manifest
+repo init --depth=1 --no-repo-verify -u git://github.com/PotatoProject/manifest.git -b frico-release -g default,-mips,-darwin,-notdefault
+git clone https://github.com/iaiaian1/local-manifests.git --depth 1 -b POSP-12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-Lunch lineage_pine-userdebug
+lunch potato_pine-userdebug
 export TZ=Asia/Dhaka #put before last build command
-Mka bacon
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
