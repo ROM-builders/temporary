32c7a43621a1f92745e293496940a854499fd4cd
Lynx <94979060+lynnnnzx@users.noreply.github.com>
2021-12-19 17:29:21 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/NusantaraProject-ROM/android_manifest -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/lynnnnzx/local_manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/lynnnnzx/local_manifest.git --depth 1 -b nad-juice .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch nad_ysl-userdebug
+lunch nad_juice-userdebug
 export TZ=Asia/Jakarta
 mka nad
 
