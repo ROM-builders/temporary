b4ba6bea1b9e7daa35dc42b4d39b2324f7ad9875
HitchTrailBlazer <75736765+TelkomFlexi@users.noreply.github.com>
2021-12-19 15:37:17 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,10 +6,10 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch nad_ginkgo-userdebug
-export ALLOW_MISSING_DEPENDENCIES=true
 export BUILD_USER=hitch
 export BUILD_HOST=blazer
 export TZ=Asia/Jakarta
+export ALLOW_MISSING_DEPENDENCIES=true
 mka nad
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
