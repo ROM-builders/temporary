0a36128f21e24493a99c2d32f1d8f91da48b4d94
HitchTrailBlazer <75736765+TelkomFlexi@users.noreply.github.com>
2021-12-20 17:13:34 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch nad_ginkgo-userdebug
 export BUILD_USER=hitch
-export BUILD_HOST=hitch
+export BUILD_HOST=blazer
 export TZ=Asia/Jakarta
 export ALLOW_MISSING_DEPENDENCIES=true
 mka nad
