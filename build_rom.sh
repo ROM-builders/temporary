651b9af3fc1f0f56faea2373902e6e2dd486503d
Oke aku kerumah tante sekarang <96351877+K-fear@users.noreply.github.com>
2021-12-21 01:55:23 +0700

    Sue

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,16 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/Octavi-OS/platform-manifest.git -b 12 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/K-fear/Local-Manifests.git --depth 1 -b octavi-mido .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
-export TZ=Asia/Dhaka #put before last build command
+lunch octavi_mido-userdebug
+export TZ=Asia/Jakarta
+export BUILD_USER=cirrus
+export BUILD_HOST=mido
+export BUILD_USERNAME=cirrus
+export BUILD_HOSTNAME=mido
 mka derp
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
