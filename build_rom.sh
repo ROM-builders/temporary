0448a7e2e03a50a4b62393af0352126410b435b9
ibratabian17 <71920907+ibratabian17@users.noreply.github.com>
2021-12-21 05:27:26 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_berryllium-userdebug
+lunch lineage_beryllium-userdebug
 export KBUILD_BUILD_USER=Ibratabian17
 export TZ=Asia/Jakarta #put before last build command
 mka bacon
