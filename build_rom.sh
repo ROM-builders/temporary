0052348a20b935a1b3727178a17928f1d42eca49
iamnabilzaman <94600437+iamnabilzaman@users.noreply.github.com>
2021-12-21 12:31:57 +0600

    santoni Octavi-S iamnabilzaman

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Octavi-OS/platform_manifest.git -b 12 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/iamnabilzaman/Local-Manifests12.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Octavi-OS/platform_manifest.git -b 12  -g default,-mips,-darwin,-notdefault
+git clone https://github.com/iamnabilzaman/local_manifest12.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
