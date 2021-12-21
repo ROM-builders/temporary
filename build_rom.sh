84de14959881330cf295faf66bb7725369abde61
HitchTrailBlazer <75736765+TelkomFlexi@users.noreply.github.com>
2021-12-22 00:39:18 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,17 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/NusantaraProject-ROM/android_manifest.git -b 11 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Komodo-OS/manifest -b 12 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/TelkomFlexi/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j12
 
 # build rom
 source build/envsetup.sh
-lunch nad_ginkgo-userdebug
+lunch komodo_ginkgo-userdebug
 export BUILD_USER=hitch
 export BUILD_HOST=blazer
 export TZ=Asia/Jakarta
 export ALLOW_MISSING_DEPENDENCIES=true
-mka nad
+export SELINUX_IGNORE_NEVERALLOWS=true
+mka komodo -jX
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
