473bd3900335dfdce53ff775c4c7f75796e38a59
ＺＥＵＳ <80810363+OFFICIAL-ZEUS@users.noreply.github.com>
2021-12-22 21:46:25 +0530

    Octavi [ 12 ]

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,6 @@ repo init --depth=1 --no-repo-verify -u git://github.com/Octavi-OS/platform_mani
 git clone https://github.com/OFFICIAL-ZEUS/Manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-
 # build rom
 . build/envsetup.sh
 lunch octavi_tulip-userdebug
