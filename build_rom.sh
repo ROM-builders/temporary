39cfd1b4befc3b551a4aef35cde6449bd19a8ca2
Lynx <94979060+lynnnnzx@users.noreply.github.com>
2021-12-22 11:56:39 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/exthmui/android.git -b exthm-11 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/SuperiorOS/manifest.git -b eleven -g default,-mips,-darwin,-notdefault
 git clone https://github.com/lynnnnzx/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch exthm_juice-userdebug
+lunch superior_juice-userdebug
 export TZ=Asia/Jakarta
 mka bacon
 
