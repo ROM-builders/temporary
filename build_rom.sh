21c5004e19f7b89e6939839772026423cad79191
Jagaddhita Jalu <96040596+jaltod@users.noreply.github.com>
2021-12-23 09:03:33 +0700

    vayu: Setup new name for TARGET_PREBUILT_KERNEL

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.g
 git clone https://github.com/jaltod/manifest.git --depth=1 -b simba .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom 
+# build rom
 . build/envsetup.sh
 lunch superior_vayu-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
