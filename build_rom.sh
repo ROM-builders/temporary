b0bd3ac3d1ce2d065efe00852aeb09e7ebf70948
FONZ <40863317+FONZACUS@users.noreply.github.com>
2021-12-23 18:53:35 +0700

    reinit

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch nad_olives-user
-#export TZ=Asia/Jakarta #put before last build command
 make nad
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
