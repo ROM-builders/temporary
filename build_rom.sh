5f01e85eb6dc8c65703c3ac24b47f1c9a884e7d5
HitchTrailBlazer <75736765+TelkomFlexi@users.noreply.github.com>
2021-12-23 08:42:55 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch cherish_ginkgo-userdebug
+export BUILD_USERNAME=rama
+export BUILD_HOSTNAME=ramadhani
 export TZ=Asia/Jakarta
 export ALLOW_MISSING_DEPENDENCIES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
