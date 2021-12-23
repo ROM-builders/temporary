793b0202b2f232df2e42f2cbc47db09dd99967df
imannig <89372281+imannig@users.noreply.github.com>
2021-12-23 10:34:19 +0700

    Change branch

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/aex-tmp/manifest.git -b 12.x -g default,-mips,-darwin,-notdefault
-git clone https://github.com/imannig/local_manifest.git --depth 1 -b aex-12 .repo/local_manifests
+git clone https://github.com/imannig/local_manifest.git --depth 1 -b aex12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
