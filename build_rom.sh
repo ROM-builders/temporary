af9fae7005cbf9ef064c44e296ac666d06994c2f
Oke aku kerumah tante sekarang <96351877+K-fear@users.noreply.github.com>
2021-12-24 09:55:04 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/Project-Xtended/manifest.git -b xs -g default,-mips,-darwin,-notdefault
 git clone https://github.com/ArkanMuhammad1986/Local-Manifests.git --depth 1 -b xtended .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
