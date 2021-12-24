dcaea9696d5da00f505314d9c2e7895639b341d8
HitchTrailBlazer <75736765+TelkomFlexi@users.noreply.github.com>
2021-12-25 00:39:31 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/TelkomFlexi/local_manifest --depth 1 -b main .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j12
 
 # build rom
 source build/envsetup.sh
 lunch cherish_ginkgo-userdebug
-export BUILD_USERNAME=MrRamZ
-export BUILD_HOSTNAME=MrRamZ
+export BUILD_USERNAME=MrRamZZ
+export BUILD_HOSTNAME=MrRamZZ
 export TZ=Asia/Jakarta
 export ALLOW_MISSING_DEPENDENCIES=true
 mka cherish
