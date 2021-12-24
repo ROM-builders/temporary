16f37fc27a9c76dabea990739f6d1c4d442fc770
Sanjeev <42514333+sanjeevstunner@users.noreply.github.com>
2021-12-24 15:16:45 +0530

    vayu: PE: Rebase kernel to Litten kernel

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/PixelExperience/manifes
 git clone https://github.com/sanjeevstunner/Manifest.git --depth 1 -b vayu_aosp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom 
 source build/envsetup.sh
 lunch aosp_vayu-userdebug
 export TZ=Asia/Kolkata #put before last build command
