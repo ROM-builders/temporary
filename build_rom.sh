27ed44357a9bf0d2d2e16dcacb7a569f2b2699fa
Oke aku kerumah tante sekarang <96351877+K-fear@users.noreply.github.com>
2021-12-24 13:37:19 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
  # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Project-Xtended/manifest.git -b xs -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ArkanMuhammad1986/Local-Manifests.git --depth 1 -b xtended .repo/local_manifests
+git clone https://github.com/K-fear/Local-Manifests.git --depth 1 -b xtended-mido .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -8,7 +8,7 @@ source build/envsetup.sh
 lunch xtended_mido-userdebug
 export TZ=Asia/Jakarta #put before last build command
 make xtended
-
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
