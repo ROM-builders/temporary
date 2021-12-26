318e7c8cfe91676574206e4175ba822d67ee6f2e
Teguh Budi Santoso <86312158+Tiktodz@users.noreply.github.com>
2021-12-26 16:14:26 +0700

    plus edition

--- build_rom.sh
+++ build_rom.sh
@@ -8,7 +8,7 @@ source build/envsetup.sh
 lunch aosp_X00TD-userdebug
 export BUILD_USERNAME=tiktodz
 export BUILD_HOSTNAME=android-build
-export TZ=Asia/Jakarta
+export TZ=Asia/Bangkok
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
