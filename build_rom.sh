5864c0d4f486861fa73af1f4e9416c8aabc19098
NouBoi <96467255+NouBoi@users.noreply.github.com>
2021-12-26 21:14:58 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,18 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Corvus-R/android_manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/NouBoi/local_manifests --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
+git clone  --depth 1 https://github.com/NouBoi/PLE_local_manifest -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch corvus_pine-userdebug
+lunch limeage_PLE-userdebug
 export TZ=Asia/Kolkata #put before last build command
-make corvus
+export ALLOW_MISSING_DEPENDENCIES=true
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+export SELINUX_IGNORE_NEVERALLOWS=true
+
+# Build
+brunch PLE
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
