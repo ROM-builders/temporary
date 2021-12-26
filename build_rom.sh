c4455cc7307c8e65cb5d274744155153f87bf33a
HitchTrailBlazer <75736765+TelkomFlexi@users.noreply.github.com>
2021-12-27 06:10:15 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,8 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch cherish_ginkgo-userdebug
-export BUILD_USERNAME=hitch
-export BUILD_HOSTNAME=blazer
+export BUILD_USERNAME=Hitch
+export BUILD_HOSTNAME=TrailBlazer
 export TZ=Asia/Jakarta
 export ALLOW_MISSING_DEPENDENCIES=true
 brunch ginkgo
