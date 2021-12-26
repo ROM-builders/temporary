8968d8222ad75dc212643d2ab3ebd661dc218fe6
Risa Witama <60031111+risawitama@users.noreply.github.com>
2021-12-27 06:17:12 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,13 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Komodo-OS/manifest -b 12 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/risawitama/local_manifests.git --depth 1 -b main-1 .repo/local_manifests
+git clone https://github.com/risawitama/local_manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch komodo_onclite-userdebug
 export TZ=Asia/Jakarta #put before last build command
+export SELINUX_IGNORE_NEVERALLOWS=true
 mka komodo
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
