62677bde2a2f8685b08ee6829dc0465a1d278c40
Sanjeev <42514333+sanjeevstunner@users.noreply.github.com>
2021-12-27 18:20:18 +0530

    vayu: PE: eng build

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch aosp_vayu-userdebug
+lunch aosp_vayu-eng
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
