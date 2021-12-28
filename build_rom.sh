d43dc14c948646cee0273056cffe15216663f0e4
Aditya OP <95341296+Aknx77@users.noreply.github.com>
2021-12-28 06:51:02 +0530

    vince:Fusion os
    Fusion os 12

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch fuse_vince-eng
+export WITH_GAPPS := true
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command  
 make fuse-prod
