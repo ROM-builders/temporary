5839086056d2c411beb0839cf44c737103822e78
Jake <92788616+iaiaian1@users.noreply.github.com>
2021-12-28 08:37:27 +0800

    added gapps for easy testing

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch octavi_pine-userdebug
+#Remove gapps, this is only for testing
+export WITH_GAPPS=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
