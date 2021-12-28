032e7b02b9c05ffb8553485433735c9202e7a0cc
Lynx <94979060+lynnnnzx@users.noreply.github.com>
2021-12-28 16:17:38 +0700

    Test Build FusionOS Twelve

--- build_rom.sh
+++ build_rom.sh
@@ -7,6 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch fuse_juice-userdebug
 export BUILD_USERNAME=lynx
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Jakarta
 make fuse-prod
 
