0309b4b59fb8f89ecabd89a63aa2e3bd7bc7cf12
HitchTrailBlazer <75736765+TelkomFlexi@users.noreply.github.com>
2021-12-28 10:48:38 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,10 +6,11 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch cherish_ginkgo-userdebug
-export BUILD_USERNAME=Supra Getar
-export BUILD_HOSTNAME=Supra Getar
+export BUILD_USERNAME=Zebbbs
+export BUILD_HOSTNAME=Zebbbs
 export TZ=Asia/Jakarta
 export ALLOW_MISSING_DEPENDENCIES=true
 brunch ginkgo
+
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
