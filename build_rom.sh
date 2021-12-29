a2d1a2d94975b24bd94babd9374c1200dae81d76
wanderlusttt <58342756+wanderlusttt@users.noreply.github.com>
2021-12-29 23:20:29 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/ForkLineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-17.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/wanderlusttt/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch lineage_X00TD-userdebug
-export TARGET_FLOS=true
-export WITH_GMS=true
+#export TARGET_FLOS=true
+#export WITH_GMS=true
 export TZ=Asia/Kolkata #put before last build command
 make bacon
 
