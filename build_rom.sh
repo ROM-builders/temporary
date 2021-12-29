6e2dd861d1875fd07c348b7454ac492df9ccef7e
NouBoi <96467255+NouBoi@users.noreply.github.com>
2021-12-29 09:02:17 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,18 +1,18 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
-git clone  --depth 1 https://github.com/NouBoi/PLE_local_manifest -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b twelve -g default,-mips,-darwin,-notdefault
+git clone  --depth 1 https://github.com/nokia-msm8937/PLE-local-manifest -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_PLE-userdebug
+lunch aosp_PLE-userdebug
 export TZ=Asia/Dehradun #put before last build command
 export ALLOW_MISSING_DEPENDENCIES=true
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 
 # Build
-brunch PLE
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
