c34e2c9cb4119552387119fccf15ebed16f0f229
Kendras056 <88943971+Kendras056@users.noreply.github.com>
2021-12-29 14:30:07 +0700

    regenerate

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/Fusion-OS/android_manif
 git clone https://github.com/Kendras056/local_manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
-# build roms
+# build rom
 source build/envsetup.sh
 lunch fuse_X00TD-userdebug
 export BUILD_USERNAME=venom
