c9a31c705f115795067e63d43b4033ec5fdb1203
aslenofarid <92686606+aslenofarid@users.noreply.github.com>
2021-12-29 20:02:42 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-19.0 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/P-404/platform_manifest -b rippa -g default,-mips,-darwin,-notdefault
 git clone https://github.com/aslenofarid/local_manifest.git --depth 1 -b 19 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_X00T-userdebug
+lunch p404_X00T-userdebug
 export TZ=Asia/Jakarta #put before last build command
 mka bacon
 
