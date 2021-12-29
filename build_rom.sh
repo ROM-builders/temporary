1edf1b3d579cf94dfa18b8e06139df4698b16a49
NouBoi <96467255+NouBoi@users.noreply.github.com>
2021-12-29 17:09:48 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch aosp_PLE-userdebug
 export TARGET_GAPPS_ARCH=arm64
-export TZ=Asia/Dehradun #put before last build command
+export TZ=Asia/Kolkata #put before last build command
 export ALLOW_MISSING_DEPENDENCIES=true
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
