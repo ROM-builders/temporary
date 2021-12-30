560b96f27f9aa58f2d85fbc210d0571ae1ca7d2e
Lynx <94979060+lynnnnzx@users.noreply.github.com>
2021-12-31 01:08:11 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,17 +1,17 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Project-Elixir/manifest.git -b snow -g default,-mips,-darwin,-notdefault
-git clone https://github.com/lynnnnzx/local_manifest.git --depth 1 -b twelve-elixir .repo/local_manifests
+git clone https://github.com/lynnnnzx/local_manifest.git --depth 1 -b twelve-evox .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch aosp_juice-userdebug
+lunch evolution_juice-user
 export BUILD_USERNAME=lynx
 export KBUILD_BUILD_USER=LynZx
 export KBUILD_BUILD_HOST=Cirrus-CI
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Jakarta
-mka bacon
+mka evolution
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
