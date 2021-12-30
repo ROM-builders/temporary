5ee1d8a53c414ac19334414417870df531d37202
ibratabian17 <71920907+ibratabian17@users.noreply.github.com>
2021-12-30 07:34:02 +0700

    Build Lineage

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -
 git clone https://github.com/ibraaltabian17/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom arroew
+# build rom Lineage
 source build/envsetup.sh
 lunch lineage_A6020-userdebug
 BUILD_USER="Ibratabian17"
@@ -14,6 +14,5 @@ export TZ=Asia/Jakarta #put before last build command
 croot
 brunch A6020
 
-#14 Update Sourcw
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
