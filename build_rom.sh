d7f9513bdbe1fcf4f1dfc67095fa150bc9279c99
Lynx <94979060+lynnnnzx@users.noreply.github.com>
2021-12-30 22:57:01 +0700

    Initial Project Elixir

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Fusion-OS/android_manifest.git -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/lynnnnzx/local_manifest.git --depth 1 -b twelve-fusion .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Elixir/manifest.git -b snow -g default,-mips,-darwin,-notdefault
+git clone https://github.com/lynnnnzx/local_manifest.git --depth 1 -b twelve-elixir .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
-lunch fuse_juice-userdebug
+. build/envsetup.sh
+lunch aosp_juice-userdebug
 export BUILD_USERNAME=lynx
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Jakarta
-make fuse-prod
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
