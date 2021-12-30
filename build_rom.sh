47bd887c6345c47b13421ae435746be6fbf4c554
c3eru <96016081+c3eru@users.noreply.github.com>
2021-12-30 22:09:20 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,17 +1,17 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/DotOS/manifest.git -b dot11 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b spark -g default,-mips,-darwin,-notdefault
 git clone https://github.com/c3eru/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 
 # build roms
 source build/envsetup.sh
-lunch dot_juice-userdebug
+lunch spark_juice-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Jakarta
 export BUILD_USERNAME=mobxprjkt
 export BUILD_HOSTNAME=c3eru
-make bacon
+mka bacon
  
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
