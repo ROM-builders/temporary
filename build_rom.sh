c6817d0df28a4e5ad1a9b9f1089f892af29f0ff8
wanderlusttt <sbharathraju3@gmail.com>
2021-12-30 12:51:33 +0000

    AOSPA for X00TD

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/ForkLineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/wanderlusttt/local_manifest.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/wanderlusttt/manifest.git -b ruby -g default,-mips,-darwin,-notdefault
+git clone https://github.com/wanderlusttt/local_manifest.git --depth 1 -b ruby .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
-lunch lineage_X00TD-userdebug
-export TARGET_FLOS=true
+#source build/envsetup.sh
+./rom-build.sh X00TD -t userdebug
 export TZ=Asia/Kolkata #put before last build command
-make bacon
+#make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
