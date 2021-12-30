3ab1073a184b7110a993ad7495a04c8901764452
ibratabian17 <71920907+ibratabian17@users.noreply.github.com>
2021-12-30 07:32:35 +0700

    Remove Export
    * Export strings is deprecated

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,19 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b eleven-plus -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ibraaltabian17/local_manifest.git --depth 1 -b aosp .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/ibraaltabian17/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom arroew
 source build/envsetup.sh
-lunch rr_A6020-userdebug
-export KBUILD_BUILD_USER=Ibratabian17
+lunch lineage_A6020-userdebug
+BUILD_USER="Ibratabian17"
+BUILD_HOST="EmerlSyc"
+BUILD_USERNAME="Ibratabian17"
+BUILD_HOSTNAME="EmerlSyc"
 export TZ=Asia/Jakarta #put before last build command
-mka bacon -j$(nproc --all)
+croot
+brunch A6020
 
+#14 Update Sourcw
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
