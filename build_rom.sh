3211a8493beb35a524f6bbaaf6cc562eae0b238c
mobx <67450638+mobxCode@users.noreply.github.com>
2021-12-31 15:15:49 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -13,7 +13,7 @@ export BUILD_USERNAME=mobxprjkt
 export BUILD_HOSTNAME=RANDOMBUILD
 mka bacon
  
-# TIME = 13:13 WIB
+# TIME = 15:15 WIB
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
