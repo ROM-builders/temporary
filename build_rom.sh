4b11fd97a135c85c60b39a98660498b4f9a0057e
mobx <67450638+mobxCode@users.noreply.github.com>
2021-12-31 08:43:40 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,22 +1,19 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExtended/manifest -b ace -g default,-mips,-darwin,-notdefault
-git clone https://github.com/mobxCode/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b spark -g default,-mips,-darwin,-notdefault
+git clone https://github.com/c3eru/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 
 # build roms
 source build/envsetup.sh
-lunch aosp_juice-userdebug
-#export SKIP_ABI_CHECKS=true
-export BUILD_USERNAME=mobxprojekt
-export BUILD_HOSTNAME=RANDOMBUILD
+lunch spark_juice-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
-export TZ=Asia/Jakarta #put before last build command
+export TZ=Asia/Jakarta
+export BUILD_USERNAME=mobxprjkt
+export BUILD_HOSTNAME=RANDOMBUILD
 mka bacon
-
-
-# Time = 18:47 WIB
-
+ 
+# TIME = 8:45 WIB
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
