adda1d38c34db318e683c2bce450951c21904f6d
zєг๏ <77091137+khirendra03@users.noreply.github.com>
2021-12-31 14:04:43 +0530

    Initial commit for ppui
    Finding a good rom to build in ci

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/Spark-Rom/manifest.git -b spark -g default,-mips,-darwin,-notdefault
-git clone https://github.com/khirendra03/local_manifest.git --depth 1 -b spark .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/PixelPlusUI-SnowCone/manifest.git -b snowcone -g default,-mips,-darwin,-notdefault
+git clone https://github.com/khirendra03/local_manifest.git --depth 1 -b test .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch spark_X01BD-userdebug
+lunch aosp_X01BD-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon 
 
