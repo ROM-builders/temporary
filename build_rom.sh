e20d3fd04fd90c7f4ff7ccd2becec8da4393f52d
iaiaian1 <yapjakebrian1@gmail.com>
2021-12-31 19:02:48 +0800

    Awaken OS build.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage 18.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/iaiaian-droid/local_manifest.git --depth 1 -b lineage .repo/local_manifest
+repo init --depth=1 --no-repo-verify -u git://github.com/Project-Awaken/android_manifest.git -b 12 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/iaiaian1/local-manifests.git --depth 1 -b Awaken-12.0 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-Lunch lineage_pine-userdebug
+lunch awaken_pine-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
-Mka bacon
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
