beda3d829b27ba8323196f74e1637c9aed48d989
wanderlusttt <58342756+wanderlusttt@users.noreply.github.com>
2021-12-31 15:31:34 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,10 +4,10 @@ git clone https://github.com/wanderlusttt/local_manifest.git --depth 1 -b ruby .
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-#source build/envsetup.sh
+# source build/envsetup.sh
 export TZ=Asia/Kolkata #put before last build command
 ./rom-build.sh X00TD -t userdebug
-#mka derp
+# mka derp
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
