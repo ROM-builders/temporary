e82caeb7967b29ce4b923c2763dd3a5fa11e42fb
TianWalkzzMiku0 <77526226+kentanglu@users.noreply.github.com>
2021-12-31 07:48:45 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/projectarcana-aosp/manifest -b 12.x -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Asyanx/local_manifests --depth 1 -b main .repo/local_manifests
+git clone https://github.com/Asyanx/local_manifests -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
