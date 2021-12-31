d56cffe9747ea46aad2c00ea64b6ba7fc0597d6d
HitchTrailBlazer <75736765+TelkomFlexi@users.noreply.github.com>
2021-12-31 20:07:33 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch komodo_ginkgo-userdebug
 export BUILD_USERNAME=rama
-export BUILD_HOSTNAME=rama
+export BUILD_HOSTNAME=newyear
 export TZ=Asia/Jakarta
 export ALLOW_MISSING_DEPENDENCIES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
