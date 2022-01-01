9ae4d7eb0bb881425b73ab2514151b5a3f9e53a0
jhonatan Mikael <75139386+jhonnytech90@users.noreply.github.com>
2022-01-01 15:25:54 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,8 +6,10 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch aosp_A001D-userdebug
+export TZ=Asia/Dhaka # put before last build command
 export ALLOW_MISSING_DEPENDENCIES=true
-export TZ=America/Sao_Paulo
+export SKIP_ABI_CHECKS=true
+export SKIP_API_CHECKS=true
 mka bacon
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
