5f8b5b66605967ce7349039f1a6d06e02c2019fb
Himanish Mandrekar <mandrekarh25@gmail.com>
2022-01-01 18:25:35 +0530

    laurel_sprout-ConquerOS-HimanishM25

--- build_rom.sh
+++ build_rom.sh
@@ -3,6 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/conquerOS/manifest.git 
 git clone https://github.com/HimanishM25/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+
 # build rom
 source build/envsetup.sh
 lunch conquer_laurel_sprout
