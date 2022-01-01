e62c4c17e82313c4f6c0363d29a34ab91595c087
TianWalkzzMiku0 <77526226+kentanglu@users.noreply.github.com>
2022-01-01 10:02:15 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,15 @@
-# sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/projectarcana-aosp/manifest -b 12.x -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Asyanx/local_manifests -b main .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j1 --fail-fast
+# sync rom.
+repo init --depth=1 --no-repo-verify -u git://github.com/projectarcana-aosp/manifest.git -b 12.x -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/Asyanx/local_manifests.git --depth 1 -b main .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
 # build rom
 source build/envsetup.sh
+export SELINUX_IGNORE_NEVERALLOWS=true
+export WITH_GAPPS=true
 lunch aosp_merlinx-userdebug
-export TZ=Asia/Dhaka #put before last build command
-make bacon
+export TZ=Asia/Jakarta #put before last build command
+make bacon 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
