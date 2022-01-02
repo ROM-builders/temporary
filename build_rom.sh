f72f39a51fcc79843560fdbc4f36daecb6b73916
HitchTrailBlazer <75736765+TelkomFlexi@users.noreply.github.com>
2022-01-02 21:30:59 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -10,7 +10,6 @@ export BUILD_USERNAME=ramaa
 export BUILD_HOSTNAME=ramaa
 export TZ=Asia/Jakarta
 export ALLOW_MISSING_DEPENDENCIES=true
-export SELINUX_IGNORE_NEVERALLOWS=true
 mka komodo
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
