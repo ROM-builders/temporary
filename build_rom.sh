99b3df0628937d9320746c20ce178e3ffa046e24
ShahzebQureshi <55016172+ShahzebQureshi@users.noreply.github.com>
2022-01-02 19:37:51 +0500

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -10,7 +10,7 @@ export BUILD_USER=ShahzebQureshi
 export BUILD_HOST=cirrus-ci
 export BUILD_USERNAME=ShahzebQureshi
 export BUILD_HOSTNAME=cirrus-ci
-lunch evolution_joan-user
+lunch evolution_joan-userdebug
 export TZ=Asia/Karachi #put before last build command
 mka bacon
 
