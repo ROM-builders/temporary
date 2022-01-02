3438c8b6bddd5e02eb6b415df32f9e4a8561135d
jhonatan Mikael <75139386+jhonnytech90@users.noreply.github.com>
2022-01-02 14:47:40 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch aosp_A001D-userdebug
-export TZ=Asia/Dhaka # put before last build command
+export TZ=Asia/Dhaka
 export ALLOW_MISSING_DEPENDENCIES=true
 export SKIP_ABI_CHECKS=true
 export SKIP_API_CHECKS=true
