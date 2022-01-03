b8fea3d65f157f57e49079822539f29096d53f63
mobx <67450638+mobxCode@users.noreply.github.com>
2022-01-04 00:38:47 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,19 +1,20 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/PixelPlusUI-SnowCone/manifest.git -b snowcone -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/syberia-project/manifest.git -b 12.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/mobxCode/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 
 # build roms
 source build/envsetup.sh
-lunch aosp_juice-userdebug
+lunch syberia_juice-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Jakarta
 export BUILD_USERNAME=mobxprjkt
 export BUILD_HOSTNAME=RANDOMBUILD
 mka bacon
  
-# TIME = 08:07 WIB
+# TIME = 00:39 WIB
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
