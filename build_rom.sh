4150e042759bdde6a0b340ccde1275de265dce4f
jhonatan Mikael <75139386+jhonnytech90@users.noreply.github.com>
2022-01-03 10:52:47 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-11.0 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/CAF-Extended/manifest.git -b 11.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/jhonnytech90/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
 source build/envsetup.sh
-lunch aosp_A001D-userdebug
-export TZ=Asia/Dhaka
-export ALLOW_MISSING_DEPENDENCIES=true
-export SKIP_ABI_CHECKS=true
-export SKIP_API_CHECKS=true
-mka bacon
+lunch cafex_lavender-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
+export TZ=Asia/Kolkata # put before last build command
+export KBUILD_BUILD_USER=wHoEMi
+export KBUILD_BUILD_HOST=ehtesham
+m cafex
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
