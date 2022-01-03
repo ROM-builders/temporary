62fd8bcf036e17af6e8882c59de58ce65d5be84a
rahul <41164942+rk134@users.noreply.github.com>
2022-01-03 17:35:26 +0000

    vince: build release build, final crDroid build for now.

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_vince-eng
+lunch lineage_vince-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
 make bacon
