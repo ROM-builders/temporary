3812677087a9eeb3dae2d5367e6624369ce91c8a
sndaire <96482879+sndaire@users.noreply.github.com>
2022-01-03 16:58:53 +0300

    miatoll-cherish-sndaire

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DotOS/manifest.git -b dot11 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b twelve -g default,-mips,-darwin,-notdefault
+mv .repo/local_manifests /tmp/
 git clone https://github.com/sndaire/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch dot_miatoll-user
+ls device/xiaomi/miatoll
+lunch cherish_miatoll-userdebug
 export TZ=Africa/Nairobi #put before last build command
-mka dot
+mka cherish
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
