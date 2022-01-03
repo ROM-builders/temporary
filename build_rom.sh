ad05384338dc7e9f84f658487a75c81a229df6a8
Hemker1469 <97006255+Hemker1469@users.noreply.github.com>
2022-01-03 09:12:28 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/iaiaian1/local-manifests.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/Hemker1469/local-manifests.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
