bec6c99df27e5a194ecf85234cd6c63ddc04d10a
Himanish Mandrekar <mandrekarh25@gmail.com>
2022-01-05 04:47:21 +0530

    build aospa

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/PotatoProject/manifest.git -b frico-release -g default,-mips,-darwin,-notdefault
-git clone https://github.com/iaiaian1/local-manifests.git --depth 1 -b POSP-12.0 .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo init -u https://github.com/AOSPA/manifest -b sapphire
+git clone https://github.com/HimanishM25/local_manifest.git --depth 1 -b master .repo/local_manifests
+repo sync -c --no-clone-bundle --current-branch --no-tags 
+
 
 # build rom
-source build/envsetup.sh
-lunch potato_pine-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
-export TZ=Asia/Dhaka #put before last build command
-brunch pine
+export TZ=Asia/India #put before last build command
+./rom-build.sh laurel_sprout
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
