58d67969ce5f876e23ef0c789a6df11e6bedabaf
Jake <92788616+iaiaian1@users.noreply.github.com>
2022-01-05 07:21:06 +0800

    export ALLOW_MISSING_DEPENDENCIES=true

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,9 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch bliss_pine-userdebug
+#FOR TESTING
+export ALLOW_MISSING_DEPENDENCIES=true 
+#FOR TESTING
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
 blissify -v pine
