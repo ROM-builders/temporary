09dce6dc17b45c6a40fe0c628dbde86408b9c3d6
laleeroy <gabwielcastwo@gmail.com>
2022-01-04 19:57:27 +0800

    forgot to change lunch codes

--- build_rom.sh
+++ build_rom.sh
@@ -5,9 +5,9 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch corvus_alioth-userdebug
+lunch aosp_mi8937-userdebug
 export TZ=Asia/Manila #put before last build command
-make corvus
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
