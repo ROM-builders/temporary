83186deade196330c4172e391518ea6ac3cfeeae
Lynx <94979060+lynnnnzx@users.noreply.github.com>
2022-01-04 23:22:42 +0700

    Initial SSOS Twelve

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/exthmui/android.git -b exthm-11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/lynnnnzx/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ShapeShiftOS/android_manifest.git -b android_12 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/lynnnnzx/local_manifest.git --depth 1 -b twelve-ssos .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-. build/envsetup.sh
-lunch exthm_juice-userdebug
+source build/envsetup.sh
+lunch ssos_juice-userdebug
 export BUILD_USERNAME=lynx
+export KBUILD_BUILD_USER=LynZx
 export TZ=Asia/Jakarta
 mka bacon
 
