e338431fc0ae3f58ebac9ba14cb3b1414e74518b
HitchTrailBlazer <75736765+TelkomFlexi@users.noreply.github.com>
2022-01-04 12:27:26 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch komodo_ginkgo-userdebug
 export BUILD_USERNAME=Ramadhani
-export BUILD_HOSTNAME=Ramadhani
+export BUILD_HOSTNAME=Komodo
 export TZ=Asia/Jakarta
 export ALLOW_MISSING_DEPENDENCIES=true
 mka komodo
