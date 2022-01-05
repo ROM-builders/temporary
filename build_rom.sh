3f4fe22ae4928475fd76ef2952c6920099d656f3
laleeroy <gabwielcastwo@gmail.com>
2022-01-06 07:14:36 +0800

    userdebug again

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch aosp_mi8937-user
+lunch aosp_mi8937-userdebug
 export TZ=Asia/Manila #put before last build command
 mka bacon
 
