bc5017bc8dc3ec33f7b95bb5f8b8313f5992cacb
iamnabilzaman <94600437+iamnabilzaman@users.noreply.github.com>
2022-01-06 04:00:23 +0600

    santoni LineageOS19 iamnabilzaman

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-19.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/iamnabilzaman/local_manifest12.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/iamnabilzaman/local_manifest12 -b los19 --depth 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
