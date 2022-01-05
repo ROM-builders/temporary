0a1b361f8949e20139e32aa5405735556b69cfdd
Anon1020 <97034713+Anon1020@users.noreply.github.com>
2022-01-05 08:35:38 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,8 +1,10 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Anon1020/local-manifest.xml.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/Anon1020/local-manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+
++
 # build rom
 source build/envsetup.sh
 lunch lineage_vince-userdebug
