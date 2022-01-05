71435afd3bdf96682ed22e2d379e73fb499708ac
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2022-01-05 15:26:54 +0700

    rosy: initial build `OctaviOS` 12

--- build_rom.sh
+++ build_rom.sh
@@ -1,17 +1,19 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/NFS-projects/local_manifest --depth 1 -b PE-12 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Octavi-OS/platform_manifest.git -b 12 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/NFS-projects/local_manifest --depth 1 -b oct-12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_rosy-userdebug
+lunch octavi_rosy-userdebug
 export TZ=Asia/Jakarta #put before last build command
 export KBUILD_USER=rosy
 export KBUILD_HOST=user
 export BUILD_USERNAME=rosy
 export BUILD_HOSTNAME=user
-mka bacon
+export SELINUX_IGNORE_NEVERALLOWS=true
+export WITH_GAPPS=true
+brunch rosy
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
