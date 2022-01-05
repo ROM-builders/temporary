20cad4cf20bf94e312272ccb97b44df0209e032c
Hitesoni | ×͜× <72127977+hsx02@users.noreply.github.com>
2022-01-05 19:47:43 +0530

    Userdebug with gapps

--- build_rom.sh
+++ build_rom.sh
@@ -5,8 +5,10 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch aosp_pine-user
+lunch aosp_pine-userdebug
 export WITH_GAPPS=true
+export SELINUX_IGNORE_NEVERALLOWS=true
+
 export TZ=Asia/Dhaka #put before last build command
 m aex
 
