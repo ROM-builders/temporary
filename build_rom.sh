6e6d292764fdff583de8e0a3a0674248ed9ed1e5
bhargav palan <43906329+bhargavpalan@users.noreply.github.com>
2022-01-05 15:59:13 +0530

    fix missing radio configs

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,6 @@ repo init --depth=1 --no-repo-verify -ugit://github.com/ForkLineageOS/android.gi
 git clone https://github.com/bhargavpalan/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-
 # build rom
 source build/envsetup.sh
 lunch cherish_vince-eng
