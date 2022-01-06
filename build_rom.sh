486279de97ed305339f1bc653bc6132aea9e0612
Hafidz Muzakky <95564478+tumbal-prjkt@users.noreply.github.com>
2022-01-06 14:37:40 +0700

    change to modified manifest

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Komodo-OS/manifest -b 12 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/tumbal-prjkt/manifest -b 12 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/tumbal-prjkt/shit_repo.git --depth 1 -b komodo .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
