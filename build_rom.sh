4b0e412f8d8ac6b80375b218c63dcbd87fb70a95
Teguh Budi Santoso <86312158+Tiktodz@users.noreply.github.com>
2022-01-06 10:39:38 +0700

    Update blobs with kernel upstream

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
-# sync rom
+# sync roms
 repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Tiktodz/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build roms
 source build/envsetup.sh
 lunch lineage_X00TD-userdebug
 export BUILD_USERNAME=tiktod
