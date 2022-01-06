485b156f4742512805cf4eaf6ce96d5a43f53d22
Teguh Budi Santoso <86312158+Tiktodz@users.noreply.github.com>
2022-01-06 16:44:31 +0700

    Introduce ZenParts - Etc.
    Change-Id: I82ba2b789e1f20ee5640e88897cbe66f824efa29

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
-# sync roms
+# sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Tiktodz/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build roms
+# build rom
 source build/envsetup.sh
 lunch lineage_X00TD-userdebug
 export BUILD_USERNAME=tiktod
