9ac70d9b4f5b76e53e37b67c629315faef276e31
jhonatan Mikael <75139386+jhonnytech90@users.noreply.github.com>
2022-01-06 11:31:13 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 . build/envsetup.sh
 lunch arrow_A001D-userdebug
-export TZ=Asia/Dhaka
+export TZ=America/Sao_Paulo
 m A001D
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
