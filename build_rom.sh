ba5014280c8ecca13a89739876d9f4403549e5e8
Lynx <94979060+lynnnnzx@users.noreply.github.com>
2022-01-06 23:18:11 +0700

    Initial CherishOS Twelve Juice

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ShapeShiftOS/android_manifest.git -b android_12 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/lynnnnzx/local_manifest.git --depth 1 -b twelve-ssos .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/lynnnnzx/local_manifest.git --depth 1 -b twelve-cherish .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
-lunch ssos_juice-userdebug
+. build/envsetup.sh
+lunch cherish_juice-userdebug
 export BUILD_USERNAME=lynx
 export KBUILD_BUILD_USER=LynZx
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Jakarta
 mka bacon
 
