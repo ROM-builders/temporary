7204af0027cee75a29515fadb6bb6d3ea0846c43
iamnabilzaman <94600437+iamnabilzaman@users.noreply.github.com>
2022-01-07 15:11:17 +0600

    santoni StatiXOS iamnabilzaman

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-19.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/iamnabilzaman/local_manifest12 -b los19 --depth 1 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/StatiXOS/android_manifest.git -b sc -g default,-mips,-darwin,-notdefault
+git clone https://github.com/iamnabilzaman/local_manifest12 -b statix-12 --depth 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_santoni-userdebug
+lunch statix_santoni-user
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
