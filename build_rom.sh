0cac012cedb3074bd6a4399d630f2b89643dff68
Lynx <94979060+lynnnnzx@users.noreply.github.com>
2022-01-08 04:39:30 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ShapeShiftOS/android_manifest.git -b android_12 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/lynnnnzx/local_manifest.git --depth 1 -b twelve-ssos .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-Pixelish/manifest.git -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/lynnnnzx/local_manifest.git --depth 1 -b twelve-aosp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
-lunch ssos_juice-userdebug
+. build/envsetup.sh
+lunch aosp_juice-userdebug
 export BUILD_USERNAME=lynx
 export KBUILD_BUILD_USER=LynZx
 export TZ=Asia/Jakarta
