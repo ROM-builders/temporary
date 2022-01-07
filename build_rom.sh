25aef4e686a0a4233fd0ab93f353a96f6ec220ac
Jake <92788616+iaiaian1@users.noreply.github.com>
2022-01-07 17:21:10 +0800

    OLIVES BUILD SH!

--- build_rom.sh
+++ build_rom.sh
@@ -1,17 +1,17 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/BlissRoms/platform_manifest.git -b arcadia -g default,-mips,-darwin,-notdefault
-git clone https://github.com/iaiaian1/local-manifests.git --depth 1 -b Bliss-12.0 .repo/local_manifests
+git clone https://github.com/iaiaian1/local-manifests.git --depth 1 -b OLIVES-MASTER .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch bliss_pine-userdebug
+lunch bliss_olives-userdebug
 #FOR TESTING
 #export ALLOW_MISSING_DEPENDENCIES=true 
 #FOR TESTING
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
-blissify -v pine
+blissify -v olives
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
