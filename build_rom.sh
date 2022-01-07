5c9ec5498c57d122c65880f264ddab0105314823
Sundram <63789329+Yasundram@users.noreply.github.com>
2022-01-07 09:47:47 +0530

    Userdebug build

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch cherish_RMX1941-eng
+lunch cherish_RMX1941-userdebug
 export CHERISH_MAINTAINER=Sundram™
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
