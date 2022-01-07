8bebc124d25280127e2c021dd1baadc5c59acdd1
jhonatan Mikael <75139386+jhonnytech90@users.noreply.github.com>
2022-01-07 03:13:14 -0300

    HavocOS-A001D-JhonatanMikael

--- build_rom.sh
+++ build_rom.sh
@@ -1,10 +1,10 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-11.0 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Havoc-OS/android_manifest.git -b eleven -g default,-mips,-darwin,-notdefault
 git clone https://github.com/jhonnytech90/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 . build/envsetup.sh
-lunch arrow_A001D-userdebug
+lunch havoc_A001D-userdebug
 export TZ=America/Sao_Paulo
 m bacon
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
