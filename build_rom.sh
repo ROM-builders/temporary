0615546aca0e266e9bf9c8a3f04afb00a0ce2dae
marat2509-hub <93652988+marat2509-hub@users.noreply.github.com>
2022-01-07 22:04:55 +0300

    lava-cherishos-marat2509

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/CherishOS/android_manifest.git -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://gitlab.com/marat2509/local_manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
+lunch cherish_lava-user
+export SELINUX_IGNORE_NEVERALLOWS_ON_USER=true
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
