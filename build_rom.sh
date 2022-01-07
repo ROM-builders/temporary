e6bfeb1ce2cf01f15e74fefbd069fdb49e69515d
João Guilherme <36928653+Guilherme2041@users.noreply.github.com>
2022-01-07 18:07:25 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://codeaurora.org/platform/manifest.git -b release -m LA.UM.9.6.2.c25-01800-89xx.0.xml --repo-url=git://codeaurora.org/tools/repo.git --repo-branch=caf-stable -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://codeaurora.org/platform/manifest.git -b release -m LA.UM.9.6.2.c25-01800-89xx.0.xml -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Guilherme2041/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
