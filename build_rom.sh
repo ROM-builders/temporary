555282308212c7a9a5f5d4af1d9cf12dd73f2ad4
iamnabilzaman <94600437+iamnabilzaman@users.noreply.github.com>
2022-01-08 22:05:06 +0600

    santoni AOSPK-12 iamnabilzaman

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/conquerOS/manifest.git -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/iamnabilzaman/local_manifest12 -b conquer-12 --depth 1 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/AOSPK/manifest -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/iamnabilzaman/local_manifest12 -b aospk-12 --depth 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch conquer_santoni-userdebug
+lunch aosp_santoni-userdebug
 export TZ=Asia/Dhaka #put before last build command
-make carthage
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
