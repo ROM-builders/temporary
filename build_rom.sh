1a7f960186e3db76b2a91da7f4a0c33c2a2f3954
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2022-01-08 22:29:10 +0700

    Change build witch Clang

--- build_rom.sh
+++ build_rom.sh
@@ -11,5 +11,5 @@ export BUILD_USERNAME=rosy
 export BUILD_HOSTNAME=user
 make bacon
 
-# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+# upload rom
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
