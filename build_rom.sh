2bc93faf006039368e8d933b25dbbd7f929a880e
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2022-01-08 15:26:19 +0700

    Add custom username and hostname
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -7,6 +7,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch evolution_ginkgo-userdebug
 export TZ=Asia/Jakarta #put before last build command
+export BUILD_USERNAME=fiqri
+export BUILD_HOSTNAME=cirrusci
 mka evolution
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
