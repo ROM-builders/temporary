adff87be710c504726da2a1b36c6cf370be90c37
GhostMaster69-dev <rathore6375@gmail.com>
2022-01-08 16:56:43 +0530

    Build OctaviOS
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -6,9 +6,9 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch vince-userdebug
-export KBUILD_BUILD_USER="octavi"
+export KBUILD_BUILD_USER="android"
 export KBUILD_BUILD_HOST="localhost"
-export SELINUX_IGNORE_NEVERALLOWS=true
+#export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
