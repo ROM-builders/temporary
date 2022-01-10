e17b7fc27fb40b531a17a35b9d8d813c51d349d6
Sayooj Soman <80192443+sayooxx@users.noreply.github.com>
2022-01-10 08:33:28 +0530

    sweet-corvus-sayooxx

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch corvus_sweet-userdebug
 export TZ=Asia/Dhaka #put before last build command
-make corvus
+make corvu
 
 
 export 
