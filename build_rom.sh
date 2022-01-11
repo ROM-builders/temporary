80e687d9c13340a3bc39effee58619518e2de699
HitchTrailBlazer <75736765+TelkomFlexi@users.noreply.github.com>
2022-01-11 19:33:25 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch cherish_ginkgo-userdebug
-export BUILD_USERNAME=Zipp
+export BUILD_USERNAME=ZephyrusROG
 export BUILD_HOSTNAME=ZephyrusROG
 export TZ=Asia/Jakarta
 export ALLOW_MISSING_DEPENDENCIES=true
