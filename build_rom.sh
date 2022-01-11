d0fbb44826434d336126b6474e7bad52c916db9b
Sayooj Soman <80192443+sayooxx@users.noreply.github.com>
2022-01-11 18:48:33 +0530

    sweet-komodo-sayooxx

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch komodo_sweet-userdebug
-
 export TZ=Asia/Dhaka #put before last build command
 mka komodo
 
