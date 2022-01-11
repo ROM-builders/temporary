0a1b520a0c37f30df666f63c13fdd0052da67db3
Jake <92788616+iaiaian1@users.noreply.github.com>
2022-01-11 13:04:53 +0800

    https

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/ArrowOS/android_manifest.git -b arrow-12.0 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-12.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/iaiaian1/local-manifests.git --depth 1 -b Arrow-12.0 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
