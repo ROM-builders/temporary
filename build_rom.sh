5f9b7c51c73f5de93ba9acda41a56c1fd8477d6f
ＭＯＣＨＡ <80810363+OFFICIAL-MOCHA@users.noreply.github.com>
2022-01-11 21:50:04 +0530

    Octavi os A12

--- build_rom.sh
+++ build_rom.sh
@@ -3,6 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/Octavi-OS/platform_mani
 git clone https://github.com/OFFICIAL-MOCHA/Manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+
 # build rom
 . build/envsetup.sh
 lunch octavi_tulip-userdebug
