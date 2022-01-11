a6cf9c3b18700c7c633f9812d3df3fbbdb0a0c60
Sayooj Soman <80192443+sayooxx@users.noreply.github.com>
2022-01-11 10:42:32 +0530

    sweet-komodo-sayooxx

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/IronOSProject/android_manifest.git -b twelve -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/Komodo-OS/manifest -b 12 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/sayooxx/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch iron_sweet-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
+lunch komodo_sweet-userdebug
+
 export TZ=Asia/Dhaka #put before last build command
-mka bacon
+mka komodo
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
