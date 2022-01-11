5e7f8a03e1321923e892f52adf42e9605f911bcb
lazr <93397127+lazrdev@users.noreply.github.com>
2022-01-11 08:20:13 +0100

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/ProtonAOSP/android_manifest -b 11 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/ProtonAOSP/android_manifest.git -b sc -g default,-mips,-darwin,-notdefault
 git clone https://github.com/lazrdev/local_manifest --depth 1 -b proton_12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
