5c8db1321fcff4a106556143c343794b5d85cb36
nohaxrobot <97295772+nohaxrobot@users.noreply.github.com>
2022-01-11 13:39:58 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom
+# sync rom.
 repo init --depth=1 --no-repo-verify -u https://github.com/Project-Fluid/manifest.git -b fluid-12 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/flashokillerify/Local-Manifests/blob/ --depth 1 -b fluid-12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
