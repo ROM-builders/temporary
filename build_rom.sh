e44abcb12b0061e6bc82702d8e07ea62a54de83a
jhonatan Mikael <75139386+jhonnytech90@users.noreply.github.com>
2022-01-11 02:31:57 -0300

    A001D-CAFEX-JHONATANMIIKAEL

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch cafex_A001D-userdebug
-export TZ=America/Sao_Paulo
+export TZ=America/Sao_Paulo #upgrade
 m cafex
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
