64cf01ea9f43988cad23406ebc42313f4a73b4a3
Kneba <abenkenary3@gmail.com>
2022-01-12 17:53:59 +0700

    permissive build

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
-# sync rom
+# sync roms
 repo init --depth=1 --no-repo-verify -u https://github.com/CipherOS/android_manifest -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Tiktodz/local_manifest.git --depth 1 -b nad .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build roms
 source build/envsetup.sh
 lunch cipher_X00TD-userdebug
 export TZ=Asia/Bangkok #put before last build command
