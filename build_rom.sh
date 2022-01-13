6ef0ef2bb32a2cc8b7d482628f151af0e8f1bf8b
iDrinkCoffee <80449624+huhugh221@users.noreply.github.com>
2022-01-13 07:09:35 +0000

    Make RevengeOS for merlinx

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/CipherOS/android_manifest.git -b twelve -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/RevengeOS/android_manifest.git -b r11.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/huhugh221/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch cipher_a10-userdebug
+lunch revengeos_merlinx-userdebug
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
