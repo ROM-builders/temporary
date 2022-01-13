ce12bc15637cf5aca9677bc3851300d3a8168b6e
NouBoi <96467255+NouBoi@users.noreply.github.com>
2022-01-13 08:10:05 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone  --depth 1 https://github.com/nokia-msm8937/PLE-local-manifest -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/LessAosp/manifest -b twelve -g default,-mips,-darwin,-notdefault
+git clone  --depth 1 https://github.com/NouBoi/local_manifests/blob/main/local_manifests.xml -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_PLE-userdebug
+lunch aosp_pine-userdebug
 export TARGET_GAPPS_ARCH=arm64
 export TZ=Asia/Kolkata #put before last build command
 export ALLOW_MISSING_DEPENDENCIES=true
@@ -13,7 +13,7 @@ export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 
 # Build
-mka bacon
+maka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
