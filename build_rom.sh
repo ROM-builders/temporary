3a3c99f7f1b8b6fed4760f9f4ba714677a754811
Lynx <94979060+lynnnnzx@users.noreply.github.com>
2022-01-14 20:44:47 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/lynnnnzx/local_manifest.git --depth 1 -b twelve-ppu
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-. build/envsetup.sh
+. build/envsetup.sh 
 lunch aosp_juice-userdebug
 export BUILD_USERNAME=lynx
 export KBUILD_BUILD_USER=LynZx
