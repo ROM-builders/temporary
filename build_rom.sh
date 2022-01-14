90c09f2fa8d1ee1244dc0275dcf07a16d80064b8
nohaxrobot <97295772+nohaxrobot@users.noreply.github.com>
2022-01-14 15:19:48 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Project-Elixir/manifest -b snow -g default,-mips,-darwin,-notdefault
-git clone https://github.com/dlwlrma123/local_manifest --depth 1 -b SDM439 .repo/local_manifests
+git clone https://github.com/vytska69/Local-Manifests --depth 1 -b elixir .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom...
 source build/envsetup.sh
-lunch aosp_vayu-user
+lunch aosp_pine-user
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
