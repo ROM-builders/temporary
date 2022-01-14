1010cb745e1336ee20f6ffd3128149f440e36311
aslenofarid <92686606+aslenofarid@users.noreply.github.com>
2022-01-15 02:05:00 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,9 +5,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-breakfast X00TD
+lunch lineage_X00TD-userdebug
 export TZ=Asia/Jakarta #put before last build command
-chroot
 brunch X00TD
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
