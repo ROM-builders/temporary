3100da04dcc2847dfdf03fef27ee36be95cb077b
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2022-01-15 06:43:23 +0700

    Settings issue fix test

--- build_rom.sh
+++ build_rom.sh
@@ -12,5 +12,5 @@ export BUILD_HOSTNAME=user
 export WITH_GAPPS := true
 make bacon
 
-# upload rom
+# upload rom 
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
