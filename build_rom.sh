605938ca4b9d85ec830988ed3170e587cf03ebda
HitchTrailBlazer <75736765+TelkomFlexi@users.noreply.github.com>
2022-01-14 20:28:26 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,16 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b twelve -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Xtended/manifest.git -b wip -g default,-mips,-darwin,-notdefault
 git clone https://github.com/TelkomFlexi/local_manifest --depth 1 -b Gatau .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j12
 
 # build rom
 source build/envsetup.sh
-lunch cherish_ginkgo-userdebug
+lunch xtended_ginkgo-userdebug
 export BUILD_USERNAME=Zephyrus
 export BUILD_HOSTNAME=Zephyrus
 export TZ=Asia/Jakarta
 export ALLOW_MISSING_DEPENDENCIES=true
-brunch ginkgo
+make xtended
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
