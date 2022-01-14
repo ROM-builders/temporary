5f792b6e15b5df4eee9f4c89769e530c4bcbd20e
jhonatan Mikael <75139386+jhonnytech90@users.noreply.github.com>
2022-01-14 11:45:45 -0300

    A001D-EVOLUTIONX-JHONATANMIKAEL

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch evolution_A001D-userdebug
-export TZ=America/Sao_Paulo #up
+export TZ=America/Sao_Paulo #update
 mka evolution
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
