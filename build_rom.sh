b23c0c350ac3d611f41822d91b8341fea30512ec
HitchTrailBlazer <75736765+TelkomFlexi@users.noreply.github.com>
2022-01-15 20:25:02 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,16 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Project-Xtended/manifest.git -b wip -g default,-mips,-darwin,-notdefault
-git clone https://github.com/TelkomFlexi/local_manifest --depth 1 -b Gatau .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Octavi-OS/platform_manifest.git -b 12 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/TelkomFlexi/manifest_locals --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j12
 
 # build rom
 source build/envsetup.sh
-lunch xtended_ginkgo-userdebug
+lunch octavi_ginkgo-userdebug
 export BUILD_USERNAME=Nezha 哪吒
-export BUILD_HOSTNAME=Ram ×͜×
+export BUILD_HOSTNAME=RamJR×͜×
 export TZ=Asia/Jakarta
 export ALLOW_MISSING_DEPENDENCIES=true
-make xtended
+brunch ginkgo
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
