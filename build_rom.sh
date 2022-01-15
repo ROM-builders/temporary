10f91e9a59f00cb50c69a8cd326546d630c6060e
Krazy-Goel <88996044+Krazy-Goel@users.noreply.github.com>
2022-01-15 08:48:41 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch corvus_olives-userdebug
+lunch corvus_olive-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true 
 
 export TZ=Asia/Dhaka #put before last build command
