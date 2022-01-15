b2abc41918ce98955f0fd6ab1711df56b9eeec01
iamshubh69 <97435707+iamshubh69@users.noreply.github.com>
2022-01-15 13:00:34 +0530

    ArrowOS-moon-TEST-shubh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/CipherOS/android_manifest.git -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/iamshubh69/local_manifest.git --depth 1 -b cipher-temp .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/ArrowOS/android_manifest.git -b arrow-12.0 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/iamshubh69/local_manifest.git --depth 1 -b arrow-temp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch cipher_moon-userdebug
+lunch arrow_moon-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
