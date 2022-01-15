5b6baa0df9e37ef13b977c73aaf59a9d2eb8cceb
NouBoi <96467255+NouBoi@users.noreply.github.com>
2022-01-15 20:46:32 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,19 +1,18 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LessAosp/manifest -b twelve -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest -b 12 -g default,-mips,-darwin,-notdefault
 git clone  --depth 1 https://github.com/NouBoi/local_manifests -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_pine-userdebug
-export TARGET_GAPPS_ARCH=arm64
+lunch voltage_selene-userdebug
 export TZ=Asia/Kolkata #put before last build command
 export ALLOW_MISSING_DEPENDENCIES=true
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
-export SELINUX_IGNORE_NEVERALLOWS=true
+export WITH_GAPPS=true
 
 # Build
-maka bacon
+brunch selene
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
