0a6c270dd367b8f6f94b128dd9c50c32628a98f4
Nabil Zaman <94600437+iamnabilzaman@users.noreply.github.com>
2022-01-17 01:12:04 +0600

    santoni ConquerOS iamnabilzaman

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/conquerOS/manifest.git -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/iamnabilzaman/local_manifest12.git --depth 1 -b conquer-12_hals_fixed .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
