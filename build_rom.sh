0fd4873f307465e6670b3c2467a9f88b110b2c07
Jake <92788616+iaiaian1@users.noreply.github.com>
2022-01-16 10:44:43 +0800

    use https

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/projectarcana-aosp/manifest -b 12.x -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/projectarcana-aosp/manifest -b 12.x -g default,-mips,-darwin,-notdefault
 git clone https://github.com/iaiaian1/local-manifests.git --depth 1 -b OLIVES-arcane-12.0 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
