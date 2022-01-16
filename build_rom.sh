16fa8d62afb6dd8cb99b35f93e4507b0232d3d2f
Raiden Ishigami <contact.raidenishi69@gmail.com>
2022-01-16 10:55:35 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init -u https://github.com/Evolution-X/manifest -b elle
+repo init --depth=1 -u https://github.com/PixelPlusUI-SnowCone/manifest -b snowcone
 git clone https://github.com/raidenkkj/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
 # build rom
 . build/envsetup.sh
-lunch evolution_A001D-userdebug
+lunch aosp_A001D-userdebug
 export TZ=America/Sao_Paulo
-mka evolution
+mka bacon -jX
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
