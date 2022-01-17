64843b9aa1735fc8ebadab771a9424890ed1c5aa
NouBoi <96467255+NouBoi@users.noreply.github.com>
2022-01-17 17:51:59 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Octavi-OS/platform_manifest.git -b 12  -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/projectarcana-aosp/manifest -b 12.x  -g default,-mips,-darwin,-notdefault
 git clone  --depth 1 https://github.com/NouBoi/local_manifests -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch octavi_selene-userdebug
+lunch aosp_selene-userdebug
 export TZ=Asia/Kolkata #put before last build command
 export ALLOW_MISSING_DEPENDENCIES=true
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
