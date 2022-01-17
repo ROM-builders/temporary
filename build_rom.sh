890842bec6f8481571a6ae93ff63fdd4bcf72129
Hitesoni | ×͜× <72127977+hsx02@users.noreply.github.com>
2022-01-17 17:35:05 +0530

    January build update

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/Spark-Rom/manifest.git -b spark -g default,-mips,-darwin,-notdefault
-git clone https://github.com/hsx02/Local-Manifests.git --depth 1 -b spark-12.repo/local_manifests
+git clone https://github.com/hsx02/Local-Manifests.git --depth 1 -b spark-12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
