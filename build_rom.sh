64b1b33b01e40d3a8a299b27a4827d5320ddbf54
Mutlu Öztürk <49791081+mutluemir08@users.noreply.github.com>
2022-01-18 01:30:35 +0300

    Derp-GM8_sprout-mutluemir08
    Build Derp for GM8_sprout

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/mutluemir08/local_manifest -b main .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j4
 
 # build rom
 source build/envsetup.sh
