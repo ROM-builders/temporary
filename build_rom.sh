ff735e0433d5df7ed862de73b152ac81b280f619
Raiden Ishigami <contact.raidenishi69@gmail.com>
2022-01-18 11:01:11 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,12 @@
 # sync rom
-repo init --depth=1 -u https://github.com/PixelPlusUI-SnowCone/manifest -b snowcone
+repo init --depth=1 -u git://github.com/ConquerOS/manifest.git -b eleven
 git clone https://github.com/raidenkkj/local_manifest.git --depth 1 -b main .repo/local_manifests
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 # build rom
 . build/envsetup.sh
-lunch aosp_A001D-userdebug
+lunch conquer_A001D-userdebug
 export TZ=America/Sao_Paulo
-mka bacon -jX
+make carthage -j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
