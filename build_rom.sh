480a0a7fa936ba656d3a939f03bb8c09d6eacad5
Jake <92788616+iaiaian1@users.noreply.github.com>
2022-01-18 11:04:52 +0800

    https

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/Project-Awaken/android_manifest.git -b 12 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest.git -b 12 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/iaiaian1/local-manifests.git --depth 1 -b Awaken-12.0 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
