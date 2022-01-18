8f767d3f521d41109c4ccf80ea518098a25b49d9
Zkyz7 <67148823+Zkyz7@users.noreply.github.com>
2022-01-18 14:06:18 +0800

    Vayu-EvolutionX-Zkyz7

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/Evolution-X/manifest.git -b elle -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/Evolution-X/manifest.git -b snow -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Zkyz7/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 wget -q  https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/master/clang-r437112b.tar.gz -O "clang-r437112b.tar.gz"
 tar -xf clang-r437112b.tar.gz -C prebuilts/clang/host/linux-x86/clang-r437112b
-echo "zkyz7@shogun" > kernel/xiaomi/vayu/.builderdata
+echo "elizabethangelalorenza@gengkapak" > kernel/xiaomi/vayu/.builderdata
 source build/envsetup.sh
 lunch evolution_vayu-userdebug
 export TZ=Asia/Dhaka #put before last build command
