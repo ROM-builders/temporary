4bc469307875441bc34f6e0e1ffb9fbdc03f97c5
Mayuralt <95099373+Mayuralt@users.noreply.github.com>
2022-01-18 16:17:40 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest.git -b 12 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Mayuralt/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_lavender-userdebug
+lunch awaken_lavender-userdebug
 export TZ=Asia/Kolkata #put before last build command
 make bacon
 
