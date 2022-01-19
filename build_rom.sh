545f0494edb7ad15b04fc44db8e20d6594534618
Krxyzn <72651636+Krxyzn@users.noreply.github.com>
2022-01-19 11:20:50 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/Komodo-OS/manifest -b 12 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u repo init -u git:///github.com/Komodo-OS/manifest -b 12 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Krxyzn/local-manifest-.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
