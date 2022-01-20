1aae6f566d4301f2eb00b6d7143ec5f5c642d742
Soni Dharma Ardhana <58514579+01soni247@users.noreply.github.com>
2022-01-20 22:54:59 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init -u https://github.com/ArrowOS/android_manifest.git -b arrow-12.0
+repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-12.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/01soni247/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
