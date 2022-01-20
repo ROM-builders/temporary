a78e069e9f41bcaa22bb98ae81c8b4a0d81a1a22
Jake <92788616+iaiaian1@users.noreply.github.com>
2022-01-20 16:37:20 +0800

    use https to comply for latest change

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/PotatoProject/manifest.git -b frico-release -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/PotatoProject/manifest.git -b frico-release -g default,-mips,-darwin,-notdefault
 git clone https://github.com/iaiaian1/local-manifests.git --depth 1 -b POSP-12.0 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
