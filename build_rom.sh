316b348f3310353287a91ab942b66ac5c6da8261
Krxyzn <72651636+Krxyzn@users.noreply.github.com>
2022-01-21 10:05:59 +0700

    Fix

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Octavi-OS/platform_manifest.git -b 12 -g default,-mips,-darwin,-notdefault
+
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 git clone https://github.com/Krxyzn/local-manifest-.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
