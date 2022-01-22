9f7665e674e833d4502546cd0180728751cb189f
Ashutosh Tripathi <pushpatripathi1111@gmail.com>
2022-01-22 10:06:51 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b eleven-plus -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/RevengeOS/android_manifest -b r11.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Jaxer159/manifests --depth 1 -b pe+_11_temporary_juice .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
