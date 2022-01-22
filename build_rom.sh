a4c2452c2cad71aa830aaf410dc4b13df0c1af66
Ashutosh Tripathi <pushpatripathi1111@gmail.com>
2022-01-23 01:06:01 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u  https://github.com/LineageOS/android.git -b lineage-19.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/stupid-kid-af/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Fusion-OS/android_manifest.git -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/lynnnnzx/local_manifest.git --depth 1 -b twelve-fusion .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
