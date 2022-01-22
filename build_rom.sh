24b3ab427aa1e49a1226dcb7ebc133320a8a5fdf
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2022-01-22 23:43:14 +0700

    Adapt for building Miku UI
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/syberia-project/manifest.git -b 12.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/fiqri19102002/local_manifest.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Mushroom/platform_manifest.git -b snow -g default,-mips,-darwin,-notdefault
+git clone https://github.com/fiqri19102002/local_manifest.git --depth 1 -b miku .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch syberia_ginkgo-userdebug
+lunch miku_ginkgo-userdebug
 export TZ=Asia/Jakarta #put before last build command
 export BUILD_USERNAME=fiqri
 export BUILD_HOSTNAME=cirrusci
-make syberia
+make diva
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
