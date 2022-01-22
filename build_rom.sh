f828b9f5e80929df0172f2094168ae060212e301
Eshaan <80750851+NotEshaanX06@users.noreply.github.com>
2022-01-23 01:40:18 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch cipher_oilve-userdebug
+lunch cipher_oilves-userdebug
 export TZ=Asia/Delhi #put before last build command
 make bacon
 
