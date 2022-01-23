fa1ad52ba325c4436b1495073d3bef18fc401729
Sushrut Gupta <guptasushrut@gmail.com>
2022-01-23 20:27:33 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/Evolution-X/manifest.git -b snow -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Sushrut1101-ROMs/local_manifests.git --depth=1 .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j24
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync
 
 # build rom
 source build/envsetup.sh
@@ -14,10 +14,10 @@ mka evolution
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
 
-#cd out/target/product/m20lte
-#OUTPUT="evolution*m20lte*.zip"
-#FILENAME=$(ls $OUTPUT)
-#curl -sL https://git.io/file-transfer | bash
-#./transfer wet $FILENAME
-#cd -
-#exit 0
+cd out/target/product/m20lte
+OUTPUT="evolution*m20lte*.zip"
+FILENAME=$(ls $OUTPUT)
+curl -sL https://git.io/file-transfer | bash
+./transfer wet $FILENAME
+cd -
+exit 0
