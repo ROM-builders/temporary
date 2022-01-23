ca813bea8fedc19b99c3cf95ebce0bc08066e508
Sushrut Gupta <guptasushrut@gmail.com>
2022-01-23 20:02:18 +0530

    m20lte

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,23 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo init --depth=1 --no-repo-verify -u git://github.com/Evolution-X/manifest.git -b snow -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Sushrut1101-ROMs/local_manifests.git --depth 1 -b m20lte .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j24
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
-export TZ=Asia/Dhaka #put before last build command
-mka derp
+export SKIP_ABI_CHECKS=true
+export ALLOW_MISSING_DEPENDENCIES=true # Sorry to use this, but it being used to skip lineage specific errors (for hardware_samsung repo)
+lunch evolution_m20lte-userdebug
+export TZ=Asia/Kolkata #put before last build command
+mka -j24 evolution
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+
+cd out/target/product/m20lte
+OUTPUT="evolution*m20lte*.zip"
+FILENAME=$(ls $OUTPUT)
+curl -sL https://git.io/file-transfer | sudo bash
+./transfer wet $FILENAME
+cd -
+exit 0
