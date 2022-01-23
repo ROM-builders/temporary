38520f36463fd995ee5797eeaccedfc87656aa8e
Sushrut Gupta <guptasushrut@gmail.com>
2022-01-23 20:22:19 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -14,10 +14,10 @@ mka evolution
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
 
-cd out/target/product/m20lte
-OUTPUT="evolution*m20lte*.zip"
-FILENAME=$(ls $OUTPUT)
-curl -sL https://git.io/file-transfer | bash
-./transfer wet $FILENAME
-cd -
+#cd out/target/product/m20lte
+#OUTPUT="evolution*m20lte*.zip"
+#FILENAME=$(ls $OUTPUT)
+#curl -sL https://git.io/file-transfer | bash
+#./transfer wet $FILENAME
+#cd -
 #exit 0
