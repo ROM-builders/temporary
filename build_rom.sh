d52ae39d43030df5b8a3a7d012ae1328bf553731
Ashutosh Tripathi <pushpatripathi1111@gmail.com>
2022-01-23 23:07:20 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom
+# synnc rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b snow -g default,-mips,-darwin,-notdefault
 git clone https://github.com/stupid-kid-af/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
