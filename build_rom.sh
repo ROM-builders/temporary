7c40ef1bf2be01a900038202f7791c0e0a6d28de
Ashutosh Tripathi <pushpatripathi1111@gmail.com>
2022-01-23 23:00:52 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u  https://github.com/CipherOS/android_manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/stupid-kid-af/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u  https://github.com/ForkLineageOS/android -b lineage-18.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Dullest-Pants/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch cipher_juice-userdebug
+lunch lineage_juice-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka evolution
 
