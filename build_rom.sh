89009949cae9006e945417140f12f1903fb1ecaa
Krxyzn <72651636+Krxyzn@users.noreply.github.com>
2022-01-24 15:15:52 +0700

    Santoni: Build Nusantara Project

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Octavi-OS/platform_manifest.git -b 12 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/NusantaraProject-ROM/android_manifest -b 12 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Krxyzn/local-manifest-.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch octavi_santoni-userdebug
-export OFFICIAL_BUILD=true
+lunch nad_santoni-userdebug
 export TZ=Asia/Jakarta #put before last build command
 mka bacon
 
