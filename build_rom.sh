b6a9864a8938ac745ce3a6260bd6803b6b2c1708
laleeroy <gabwielcastwo@gmail.com>
2022-01-25 00:26:45 +0800

    Revert "build with gapps"
    This reverts commit ab4ea24745006ad1cbab4b468e8566d5621feabb.

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch aosp_ulysse-userdebug
-export WITH_GAPPS=true
 export TZ=Asia/Manila #put before last build command
 make bacon
 
