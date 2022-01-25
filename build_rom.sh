5f587ae39959b9857865afea2334021071256b9d
Abijith K <79492151+mrastrange@users.noreply.github.com>
2022-01-25 11:37:08 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init -u https://github.com/Octavi-OS/platform_manifest.git -b 11 --depth=1
+repo init -u https://github.com/Octavi-OS/platform_manifest.git -b 11 --depth=1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/mrastrange/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c -f --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j8
 # build rom
