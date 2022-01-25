9ab0b1b2aa4ede591da04c4f2903242b45eda316
Sayooj Soman <80192443+sayooxx@users.noreply.github.com>
2022-01-25 09:47:58 +0530

    Create build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/sayooxx/local_manifest.git --depth 1 -b CIpherOs .r
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+source build/envsetup.s
 lunch elytra_sweet-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export WITH_GAPPS=true
