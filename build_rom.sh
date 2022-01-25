814a658ade8df525066553b0eb70ae6261ff8d86
Abijith K <79492151+mrastrange@users.noreply.github.com>
2022-01-25 17:19:23 +0530

    Octavi

--- build_rom.sh
+++ build_rom.sh
@@ -2,6 +2,7 @@
 repo init -u https://github.com/Octavi-OS/platform_manifest.git -b 11 --depth=1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/mrastrange/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
 # build rom
 source build/envsetup.sh
 lunch octavi_RMX1941-userdebug
