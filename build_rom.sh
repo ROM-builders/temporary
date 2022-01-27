8d142cbea2fec714a9a097239055d6b71657ac83
Ansh <89060758+EvilAnsh@users.noreply.github.com>
2022-01-27 08:26:07 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch xtended_santoni-user
+lunch legion_santoni-user
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
