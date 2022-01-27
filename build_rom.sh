f32d7ceef1533fa58d214f45f156d7426f3b57e8
Atharv Negi <94751339+AtarvNegi2951@users.noreply.github.com>
2022-01-27 18:20:01 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest -b 12 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u repo init --depth=1 --no-repo-verify -u https://github.com/conquerOS/manifest.git -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Atharv2951-Roms/Local-Manifests -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch voltage_pine-userdebug
+lunch conquer_selene-userdebug
 export ALLOW_MISSING_DEPENDENCES=true
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Mumbai #put before last build command
-brunch pine
+make carthage
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
