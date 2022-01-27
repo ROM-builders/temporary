68539391bdd2f7f4a9c9597d49d6f113ecf4223c
swervin Musluktansuicenogullari <94082908+sweervin@users.noreply.github.com>
2022-01-27 05:59:43 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/sweervin/local_manifest.git --depth 1 -b main .repo
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+. build/envsetup.sh
 lunch aosp_laurel_sprout-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
