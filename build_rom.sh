0618e75df0175ec25819fb7fdd17f7ff9161674f
Aditya Nayak <AdityaN.0974@gmail.com>
2022-01-28 15:00:19 +0530

    Build Gapps

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch cipher_vince-userdebug
+export CIPHER_GAPPS=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
 make bacon
