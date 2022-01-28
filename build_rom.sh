7bc1a2c511f3be212cba06446089c445985bf50e
swervin Musluktansuicenogullari <94082908+sweervin@users.noreply.github.com>
2022-01-28 11:07:10 +0300

    qssi.prop changes, dropping some gapps

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 . build/envsetup.sh
 lunch aosp_laurel_sprout-userdebug
 export SKIP_ABI_CHECKS=true
-export TZ=Asia/Dhaka #put before last build command
+export TZ=Asia/Dhaka
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
