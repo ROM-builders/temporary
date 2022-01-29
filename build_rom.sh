96f3664ef94a20db0fb8cbc6c3db3ff40a4428ab
SubAsH <88982581+subash2001@users.noreply.github.com>
2022-01-29 08:07:35 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init -u https://github.com/PixelExperience/manifest -b eleven-plus 
+repo init -u https://github.com/PixelExperience/manifest -b eleven-plus -g default,-mips,-darwin,-notdefault
 git clone https://github.com/subash2001/local_manifests.git --depth 1 -b RMX1821-PE .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
