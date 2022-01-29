fcf5215cac88a7ae6e17139de4f7d1ee9c3ed49d
頼む <87476667+Ruega@users.noreply.github.com>
2022-01-30 01:59:52 +0700

    Fix manifest conflicts

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_X00TD-userdebug
-export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Bangkok #put before last build command
 brunch X00TD
 
