995327155e8896504b7306b839fb5b77accb49eb
wanderlusttt <58342756+wanderlusttt@users.noreply.github.com>
2022-01-29 23:20:22 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
-repo init --depth=1 --no-clone-bundle --current-branch --no-tags --no-repo-verify -u git://github.com/AOSPA/manifest.git -b sapphire -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/AOSPA/manifest.git -b sapphire -g default,-mips,-darwin,-notdefault
 git clone https://github.com/wanderlusttt/local_manifest.git --depth=1 -b sapphire .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c --no-clone-bundle --no-tags --current-branch --prune --force-sync -j8
 
 # build rom
 #source build/envsetup.sh
