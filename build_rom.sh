04ddc0c011794df5463472ab8bbcb2199649c44a
Hyper <98272700+Hyperizer69@users.noreply.github.com>
2022-01-29 17:09:30 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/The-RAVEN-OS/manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/xootkama/local_manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/Hyperizer69/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
