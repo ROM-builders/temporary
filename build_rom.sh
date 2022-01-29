be6a1ee2270d47fce07782c050a8f0daa60ae1ba
Aditya Nayak <AdityaN.0974@gmail.com>
2022-01-29 18:06:09 +0530

    Build Gapps

--- build_rom.sh
+++ build_rom.sh
@@ -8,7 +8,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 lunch cipher_vince-userdebug
 export CIPHER_GAPPS=true
 export SELINUX_IGNORE_NEVERALLOWS=true
-export TZ=Asia/Dhaka #put before last build command
+export TZ=Asia/Dhaka #put before last build command 
 make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
