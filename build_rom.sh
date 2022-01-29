da3d096205d42f4bb2c2b928de057228c2a0bf1e
leo <87917944+leole0leoleo@users.noreply.github.com>
2022-01-29 10:38:10 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch derp_guamp-user
+lunch lineage_guamp-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
