df078fe0748d76dcda49a671550e87123fcdaf8e
zєг๏ <77091137+khirendra03@users.noreply.github.com>
2022-01-30 21:21:57 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/khirendra03/local_manifest.git --depth 1 -b cherish .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest.git -b 12 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/khirendra03/local_manifest.git --depth 1 -b voltage .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch cherish_X01BD-userdebug
+lunch voltage_X01BD-userdebug
 export TZ=Asia/Dhaka #put before last build command
 brunch X01BD
 
