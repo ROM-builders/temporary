bca453f99d66dba9afc00d384bcd3d0003ea0972
MOCHA <80810363+official-mocha@users.noreply.github.com>
2022-01-30 13:49:43 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,8 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/Octavi-OS/platform_manifest.git -b 12 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/OFFICIAL-MOCHA/Manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/official-mocha/local_manifest.git --depth 1 -b octavi .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
 
 # build rom
 . build/envsetup.sh
