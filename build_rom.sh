73b909fd7798f6a06e4436a8dab3e70bd9218fa0
頼む <87476667+Ruega@users.noreply.github.com>
2022-01-30 15:13:11 +0700

    change branch

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
-# sync rom
+# sync roms
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android -b lineage-17.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Ruega/local_manifests.git --depth 1 -b 10 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build roms
 source build/envsetup.sh
 lunch lineage_X00TD-userdebug
 export TZ=Asia/Bangkok #put before last build command
