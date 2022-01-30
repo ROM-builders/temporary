da474b10b2f7753adf6408920ae2f930ab2741a2
Sushrut Gupta <guptasushrut@gmail.com>
2022-01-30 18:23:47 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,9 +7,11 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch evolution_m20lte-userdebug
 export TZ=Asia/Kolkata #put before last build command
-export ALLOW_MISSING_DEPENDENCIES=true #Sorry to use, but it is a workaroud to fix errors related to lineage dependencies that isn't needed for AOSP.
+export ALLOW_MISSING_DEPENDENCIES=true #  Sorry to use, but it is a workaroud to fix errors related to lineage dependencies that isn't needed for AOSP.
 export SKIP_ABI_CHECKS=true
-mka evolution
+export JVAL="-j24"
+export TARGET=evolution
+mka ${JVAL} ${TARGET}
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
