dc161d3e611cbf82ea877df394f2ce6c2bb9a040
Lynx <94979060+lynnnnzx@users.noreply.github.com>
2022-02-01 03:57:50 +0700

    Test Build LineageOS Fan Edition

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ProjectStreak/platform_manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/lynnnnzx/local_manifest.git --depth 1 -b twelve-streak .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Los-FE/manifest.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/lynnnnzx/local_manifest.git --depth 1 -b lineage-18.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-. build/envsetup.sh
-lunch streak_juice-userdebug
+source build/envsetup.sh
+lunch lineage_juice-userdebug
 export BUILD_USERNAME=lynx
-export SELINUX_IGNORE_NEVERALLOWS=true
+export BUILD_HOSTNAME=serber-memek
 export TZ=Asia/Jakarta
-mka bacon
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
