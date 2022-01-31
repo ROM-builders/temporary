2e7277e435deea4690959f56f843063a5994380b
Light Yagami <markrayon5@gmail.com>
2022-01-31 21:10:49 +0530

    Project Sakura A70q

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ProjectSakura/android.git -b 11 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/lightyagami/local_manifest.git --depth 1 -b main .repo/local_manifests
-repo sync --current-branch --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune -j `nproc`
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
