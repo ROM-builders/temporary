fd0dd936dfb3943501307a2e83b11ea4597d82a9
MOCHA <80810363+official-mocha@users.noreply.github.com>
2022-01-31 14:39:01 +0530

    Octavi : error fix

--- build_rom.sh
+++ build_rom.sh
@@ -3,6 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/Octavi-OS/platform_mani
 git clone https://github.com/official-mocha/local_manifest.git --depth 1 -b octavi .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+
 # build rom
 . build/envsetup.sh
 lunch octavi_tulip-userdebug
