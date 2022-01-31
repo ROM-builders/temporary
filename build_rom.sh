545852e23d15c187eb69d8c0a2c9475333e9e4b8
Raiden Ishigami <contact.raidenishi69@gmail.com>
2022-01-31 01:59:24 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch arrow_A001D-userdebug
+lunch lineage_A001D-userdebug
 export TZ=America/Sao_Paulo
 export TARGET_FLOS=true
 make bacon
