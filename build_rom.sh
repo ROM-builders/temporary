e29bde6908f2a88eb5decc91f5ebf80f1abd662f
Azure-007 <64735082+Azure-007@users.noreply.github.com>
2022-02-01 11:47:21 +0530

    Raphael_Project-Arcana

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/CipherOS/android_manifest.git -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Azure-007/local_manifest.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/projectarcana-aosp/manifest.git -b 12.x -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Azure-007/local_manifest.git --depth 1 -b aosp-12.0 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch cipher_raphael-userdebug
+lunch aosp_raphael-userdebug
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
