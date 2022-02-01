2fb9d7dc341fd10aa8505dde34ac7479a9f8d1dd
ganssjr <76063071+ganssjr@users.noreply.github.com>
2022-02-02 00:42:19 +0700

    Rolex-aicp-11
    Hehey

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/AICP/platform_manifest.git -b r11.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/ganssjr/local_manifests.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
