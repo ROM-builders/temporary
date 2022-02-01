32616de692790348b84fed785ae8e06a62192db9
lazr <93397127+lazrdev@users.noreply.github.com>
2022-02-01 07:01:03 +0100

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u ttps://github.com/PotatoProject/manifest.git -b frico-release -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/PotatoProject/manifest.git -b frico-release -g default,-mips,-darwin,-notdefault
 git clone https://github.com/lazrdev/local_manifest.git --depth 1 -b potato-12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
