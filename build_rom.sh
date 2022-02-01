856e32393fae0895f3851056ef45865430cb3b09
Anyinglulu <63843048+anyinglulu1@users.noreply.github.com>
2022-02-01 18:50:03 +0700

    Forget to edit $device

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch evolution_$device-userdebug
+lunch evolution_mido-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka evolution
 
