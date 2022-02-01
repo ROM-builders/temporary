e0c83556469f299174b4269e4a1c01a27608462b
Shubh <97435707+iamshubh69@users.noreply.github.com>
2022-02-01 17:41:15 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest.git -b eleven -g default,-mips,-darwin,-notdefault
-git clone https://github.com/iamshubh69/local_manifests.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/iamshubh69/local_manifests.git --depth 1 -b PE .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_RMX3171-userdebug
+lunch aosp_moon-userdebug
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
