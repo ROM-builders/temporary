9efcfe6c4f0627cf994d0c5ec855643fe305969b
dld_xda <61960986+dangi-del@users.noreply.github.com>
2022-02-01 16:02:57 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-19.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/dangi-del/Local-Manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
@@ -8,7 +8,7 @@ source build/envsetup.sh
 lunch Lineage_mido-useruserdebug
 export WITH_GAPPS=true
 export SELINUX_IGNORE_NEVERALLOWS=true
-export TZ=Asia/Rajasthan #put before last build command ./rom-build.sh mido
+export TZ=Asia/Rajasthan #put before last build command
 mka Lineage
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
