196670ca9fc487ddcd83f51040fbf5c2cecf03da
Shubh <97435707+iamshubh69@users.noreply.github.com>
2022-02-02 14:22:56 +0530

    Fixed typo

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch xtended_moom-userdebug
+lunch xtended_moon-userdebug
 export TZ=Asia/Kolkata #put before last build command
 make xtended
 
