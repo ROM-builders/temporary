71d64db69330953d8a19800af5deae01cd114d35
NouBoi <96467255+NouBoi@users.noreply.github.com>
2022-02-02 09:18:17 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch spark_selene-userdebug
 export WITH_GAPPS=true
-export TZ=Asia/Mumbai #put before last build command
+export TZ=Asia/Kolkata #put before last build command
 export SELINUX_IGNORE_NEVERALLOWS=true
 
 # Build
