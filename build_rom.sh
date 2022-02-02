2dea10129b9c1475ae6c9b179867294023a07a84
ZenkaBestia <z.mail.6600@gmail.com>
2022-02-02 21:15:23 +0800

    LMI-Arcana-ZenkaBestia

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/PixelOS-Pixelish/manifest.git -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ZenkaBestia/local_manifests.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/projectarcana-aosp/manifest.git -b 12.x -g default,-mips,-darwin,-notdefault
+git clone https://github.com/ZenkaBestia/local_manifests.git --depth 1 -b 12-arcana .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
