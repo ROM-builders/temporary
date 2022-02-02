b114b2f392ee8a42a3e1522ab6f1a5c4a52480d1
Ashwani02c2 <96495714+Ashwani02c2@users.noreply.github.com>
2022-02-02 15:58:20 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-brunch j7elte-user
+lunch crdroid_j7elte-user
+brunch j7elte
 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
