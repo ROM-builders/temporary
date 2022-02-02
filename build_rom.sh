24a53a25487be033bca0ff8220823dde30d008d2
Alone0316 <rutujamandlik06@gmail.com>
2022-02-02 17:16:25 +0000

    retry

--- build_rom.sh
+++ build_rom.sh
@@ -5,6 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
+lunch aospa_mido-userdebug
 export TZ=Asia/Dhaka #put before last build command
 ./rom-build.sh mido
 
