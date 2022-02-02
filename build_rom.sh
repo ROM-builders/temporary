76ae5a6a8cc0e6e59d1a18948eb051296731f8ec
MOCHA <80810363+official-mocha@users.noreply.github.com>
2022-02-01 19:30:49 -1100

    PixelExtended

--- build_rom.sh
+++ build_rom.sh
@@ -1,8 +1,7 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/projectarcana-aosp/manifest -b 12.x -g default,-mips,-darwin,-notdefault
-git clone https://github.com/OFFICIAL-ZEUS/Manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/PixelExtended/manifest -b snow -g default,-mips,-darwin,-notdefault
+git clone https://github.com/official-mocha/local_manifest.git --depth 1 -b pex .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
 
 # build rom
 source build/envsetup.sh
