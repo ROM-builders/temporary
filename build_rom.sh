d4588503ecf74970ee5322c9ef50b7c2a187b8be
Zero_0R <80145720+Rahat87942R@users.noreply.github.com>
2022-02-03 13:11:13 +0600

    J7elte-Crdroid-Zero_0
    Building Crdroid for test

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch xtended_j7elte-userdebug
 export TZ=Asia/Dhaka
 brunch j7elte
 
