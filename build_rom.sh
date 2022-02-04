bfdd9c9e3cabc0e1bdfea3df045df93dcb6f2f06
Suraj G Meharwade <66472830+Suru-web@users.noreply.github.com>
2022-02-04 19:43:05 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/Suru-web/local_manifest.git --depth 1 -b master .re
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+. build/envsetup.sh
 lunch aosp_$denniz-userdebug 
 mka bacon
 
