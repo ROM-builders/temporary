e1242295e00f6a6791dd1a26bc43903f653e411e
Aghora7 <ukikieran@gmail.com>
2022-02-04 15:58:45 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch aosp_olivewood-userdebug
+lunch aosp_beryllium-eng
 export TZ=Asia/Dhaka #put before last build command
 make bacon -j24
 
