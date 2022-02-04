03fa897198be5af019729ae6ba4ac7fc98772ff1
NandoSiburian <99009854+NandoSiburian@users.noreply.github.com>
2022-02-05 00:08:26 +0700

    gemini-nusantara-NandoSiburian
    Build Nusantara for Gemini

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/nusantara/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/NandoSiburian/nando/blob/a3afc6278b9b54011293620c3e61b996bc133ce9/local_manifest --depth 1 -b master .repo/local_manifests
+git clone https://github.com/NandoSiburian/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
