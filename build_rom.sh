8ad1b0cba5dc879082b1a610219d96634e87e674
MadmoudRMX2020 <98421074+MadmoudRMX2020@users.noreply.github.com>
2022-02-05 13:47:51 +0200

    RMX2020_CrystalOS_MadmoudRMX2020

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init -u https://github.com/CrystalOS-Temp/manifest.git -b A12 --depth=1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/CrystalOS-Temp/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/MadmoudRMX2020/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
