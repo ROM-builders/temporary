6ee567c51ca40fe2e642111d05e229f3c4889dd6
Jomar <94120477+Neutralxs@users.noreply.github.com>
2022-02-05 23:04:15 +0800

    Create build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Neutralxs/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/ArrowOS/android_manifest.git -b arrow-11.0 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Neutralxs/local_manifest --depth 1 -b arrow-11-UI1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom [7]
 source build/envsetup.sh
-lunch lineage_RMX2185-userdebug
-export TZ=Asia/Manila #put before last build command
-make bacon
+lunch arrow_RMX2185-userdebug
+export TZ=Asia/Dhaka #put before last build command
+m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
