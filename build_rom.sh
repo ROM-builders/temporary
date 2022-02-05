47ee2b8a87c50924efdfbd6b9781c7c00f83df82
Ashwani02c2 <96495714+Ashwani02c2@users.noreply.github.com>
2022-02-05 08:44:19 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/Ashwani02c2/local_manifest.git --depth 1 -b main .r
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-. build/envsetup.sh
+.source build/envsetup.sh
 lunch lineage_j7elte-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
