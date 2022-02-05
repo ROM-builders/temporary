6a2c31e871a2082b4541285606501b1e73e65a68
Ashwani02c2 <96495714+Ashwani02c2@users.noreply.github.com>
2022-02-05 09:01:04 +0530

    Create build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,10 +4,10 @@ git clone https://github.com/Ashwani02c2/local_manifest.git --depth 1 -b main .r
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-.source build/envsetup.sh
+. source build/envsetup.sh
 lunch lineage_j7elte-userdebug
 export TZ=Asia/Dhaka #put before last build command
-mka bacon
+make bacon-j8
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
