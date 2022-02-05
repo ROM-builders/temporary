37ad272257c45416b7b460a7cbbfbceac8e7cc66
RasyidAlKautsar <rasyid2007alkautsar@gmail.com>
2022-02-06 06:27:25 +0700

    test new dt

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch spark_mido-userdebug
+lunch spark_mido-user
 export TZ=Asia/Jakarta #put before last build command
 mka bacon
 
