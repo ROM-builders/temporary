bb74a65ff3df06ebba0b6e772108a9fc561df176
Anyinglulu <63843048+anyinglulu1@users.noreply.github.com>
2022-02-05 12:07:56 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 export WITH_GAPPS=false
 lunch xtended_mido-user
-export TZ=Asia/Jakarta #put before last build command
+export TZ=Asia/Dhaka #put before last build command
 make xtended
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
