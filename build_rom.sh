2722b6c57d8b5721a8bc57c6f3e823a83b717280
Hakan <hknhzl@gmail.com>
2022-02-05 19:32:09 +0300

    tissot-CherishOS-twelve-lafactorial

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/CherishOS/android_manif
 git clone https://github.com/Lafactorial/local-manifest.git --depth 1 -b 12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom 
+# build rom
 source build/envsetup.sh
 lunch cherish_tissot-userdebug
 export TZ=Asia/Dhaka #put before last build command
