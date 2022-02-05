4b02ebebad3d170503835867c2f45ee70ad8aafb
Anyinglulu <63843048+anyinglulu1@users.noreply.github.com>
2022-02-05 20:13:06 +0700

    User to userdebug

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 export WITH_GAPPS=false
-lunch xtended_mido-user
+lunch xtended_mido-userdebug
 export TZ=Asia/Jakarta #put before last build command
 make xtended
 
