2f92e7fa51dfacf4bd05e0945fd2d7b1463796e8
MadmoudRMX2020 <98421074+MadmoudRMX2020@users.noreply.github.com>
2022-02-05 16:56:57 +0200

    RMX2020-ResurrectionRemix-MadmoudRMX2020

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/CrystalOS-Temp/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 -u https://github.com/ResurrectionRemix/platform_manifest.git -b Q -g default,-mips,-darwin,-notdefault
 git clone https://github.com/MadmoudRMX2020/local_manifest.git --depth 1 -b main .repo/local_manifests
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+repo sync --force-sync --no-clone-bundle
 
 # build rom
 . build/envsetup.sh
+lunch rr_$RMX2020-userdebug
 export TZ=Asia/Dhaka #put before last build command
-Crystalize crystal_RMX2020-userdebug
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
