2b05cd9b6fb5cb35129847c3b663e8bbeccd710f
lazr <93397127+lazrdev@users.noreply.github.com>
2022-02-06 10:54:16 +0100

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ProjectRadiant/manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/lazrdev/local_manifest.git --depth 1 -b potato-12 .repo/local_manifests
+git clone https://github.com/lazrdev/local_manifest.git --depth 1 -b radiant-12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
