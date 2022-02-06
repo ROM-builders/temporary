99708e4187038a5203af84d72f7cee914d28c25e
Mikaaaa29 <94102101+Dhanzu28@users.noreply.github.com>
2022-02-06 19:19:59 +0700

    PixelReloaded Caf X00TD
    A

--- build_rom.sh
+++ build_rom.sh
@@ -5,11 +5,11 @@ repo sync -j$( nproc --all ) --force-sync -c --no-clone-bundle --no-tags --optim
 
 # build rom
 source build/envsetup.sh
-lunch aosp_X00TD-userdebug
+breakfast X00TD
 export BUILD_USERNAME=mikaaaa29
 export BUILD_HOSTNAME=android_build
 export TZ=Asia/Bangkok #put before last build command
-mka bacon
+brunch X00TD
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
