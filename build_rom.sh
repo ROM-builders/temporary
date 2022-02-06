fc3007fd372336688a33982a6d69433ad88bdcec
Atharv Negi <94751339+AtarvNegi2951@users.noreply.github.com>
2022-02-06 18:57:56 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch streak_vayu-user
+lunch streak_selene-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
