b67212eca04bb6f33b991809fd51e4df3064ff3d
dld_xda <61960986+dangi-del@users.noreply.github.com>
2022-02-06 12:50:01 +0530

    mido-PE-Twelve By DLD
    Try PE BUild

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/dangi-del/local_manifest.git --depth 1 -b cherish-12 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/dangi-del/local_manifest.git --depth 1 -b .repo/local_manifest
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-. build/envsetup.sh
-lunch Cherish_mido-userdebug
+source build/envsetup.sh
+lunch aosp_mido-userdebug
 export TZ=Asia/india #put before last build command
-mka aex
+mka mido
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
