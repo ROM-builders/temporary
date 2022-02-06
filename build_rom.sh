ce1a99ac6b45fb76477042484eccb5bbfa581960
Anyinglulu <63843048+anyinglulu1@users.noreply.github.com>
2022-02-07 06:04:14 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunchxtended_mido-userdebug
+lunch xtended_mido-userdebug
 export TZ=Asia/Jakarta #put before last build command
 make xtended
 
