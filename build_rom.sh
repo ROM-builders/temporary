2e0d8e021bf55b4407fc6f884952f3418eeda012
Mikaaaa29 <94102101+Dhanzu28@users.noreply.github.com>
2022-02-06 18:49:16 +0700

    PixelReloaded Caf X00TD
    PR10 Rebuild By Mikaaaa29

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/PixelExperience/manifest -b eleven-plus -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Tiktodz/local_manifest.git --depth 1 -b pe .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo init --depth=1 --no-repo-verify -u https://github.com/Swamoy14/android_manifest.git -b PR10 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Dhanzu28/local_manifest.git --depth 1 -b PR10 .repo/local_manifests
+repo sync -j$( nproc --all ) --force-sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch aosp_X00TD-userdebug
-export BUILD_USERNAME=tiktodz
+export BUILD_USERNAME=mikaaaa29
 export BUILD_HOSTNAME=android-build
 export TZ=Asia/Bangkok
 mka bacon
