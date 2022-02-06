425b8909ec2e613ffbf621b0bd8b8223d8512572
Suraj G Meharwade <66472830+Suru-web@users.noreply.github.com>
2022-02-06 17:26:12 +0530

    Changed master to main

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init -u https://github.com/PixelExperience/manifest -b twelve
-git clone https://github.com/Suru-web/local_manifest.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/Suru-web/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
