477e761ebd127a91feb289020ea86897a2d4cdd7
Jomar <94120477+Neutralxs@users.noreply.github.com>
2022-02-07 10:37:12 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,11 @@
 # sync rom
-repo init -u https://github.com/NusantaraProject-ROM/android_manifest -b 11 -g default,-mips,-darwin,-notdefault
+repo init -u https://github.com/CherishOS/android_manifest.git -b eleven -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Neutralxs/local_manifest --depth 1 .repo/local_manifests
-repo sync -c --force-sync --no-tags --no-clone-bundle -j$(nproc --all) -j8
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags -j8
 
 # build rom [7]
 . build/envsetup.sh
-  lunch nad_<RMX2185>-buildtype
-  mka nad
+ brunch RMX2185
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
