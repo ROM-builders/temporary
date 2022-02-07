96d612ae922e10ed09a572af0373ce39fe8ddeee
NouBoi <96467255+NouBoi@users.noreply.github.com>
2022-02-07 18:36:07 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/xdroidsp/xd_manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone --depth 1 https://github.com/AtarvNegi2951/local_manifest -b main .repo/local_manifests
+git clone --depth=1 https://github.com/AtarvNegi2951/local_manifest -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
