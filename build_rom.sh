dab1046fc4d1310d12e9593c8e6e1e66110cacbd
Gofaraway71 <88510377+Gofaraway71@users.noreply.github.com>
2022-02-07 08:25:46 +0100

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,9 @@
-# sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
-# build rom
-source build/envsetup.sh
-lunch derp_vayu-user
-export TZ=Asia/Dhaka #put before last build command
-mka derp
-
+# sync romrepo init -u https://github.com/BlissRoms/platform_manifest.git -b arcadia -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Gofaraway71/local_manifest.git --depth 1 -b master 
+.repo/local_manifestsrepo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
+# build romsource 
+build/envsetup.shlunch lineage-RM6785-userdebug 
+export TZ=Asia/Dhaka 
+#put before last build commandblissify -gapps RM6785 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
