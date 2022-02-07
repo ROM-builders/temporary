8abff17b6d887df756c5387b2007b0de8df60c81
Ashwani02c2 <96495714+Ashwani02c2@users.noreply.github.com>
2022-02-08 00:56:23 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch nad_j7elte-userdebug
+rm -rf hardware/ril/libril
 export TZ=Asia/Dhaka #put before last build command
 mka nad
 
