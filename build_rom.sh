f816b8829802ad217f090f1f048bc211aeaaa749
NouBoi <96467255+NouBoi@users.noreply.github.com>
2022-02-07 18:30:20 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch xd_selene-userdebug
 export WITH_GAPPS=true
-export TZ=Asia/Kolkata #put before last build command
+export TZ=Asia/Dehradun #put before last build command
 
 # Build
 mka xd
