3e77b95691a4938db013bf7603e4c0bbc1f049f2
Atharv Negi <94751339+AtarvNegi2951@users.noreply.github.com>
2022-02-07 18:47:14 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,6 @@
-# sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/xdroidsp/xd_manifest -b twelve -g default,-mips,-darwin,-notdefault
+# sync manifest
+
+repo init -u https://github.com/xdroid-oss/xd_manifest -b twelve --no-repo-verify -g default,-mips,-darwin,-notdefault
 git clone https://github.com/AtarvNegi2951/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
