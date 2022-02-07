08896c5690a99272e428003ccb9d0434ef4a03c7
Jeremy Pérez <jerespn@gmail.com>
2022-02-06 21:03:28 -0400

    Intial build for garden Resurrection Remix
    Build Resurrection Remix for garden eRRe eRRe

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/crdroidandroid/android.git -b 12.0 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/ResurrectionRemix/platform_manifest.git -b Q -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Jeremito1/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch lineage_garden-userdebug
+lunch rr_garden-userdebug
 export TZ=Asia/Kuala Lumpur #put before last build command
 m bacon
 
