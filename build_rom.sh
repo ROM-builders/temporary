ce561572896e22c188df90a7e5a8fa0b3d2400c7
Mayur Narzary <flynryder427@gmail.com>
2022-02-07 10:58:54 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/StyxProject/manifest -b S -g default,-mips,-darwin,-notdefault
-git clone https://github.com/xenynon/manifest.git --depth 1 -b styx .repo/local_manifests
+git clone https://github.com/xenxynon/manifest.git --depth 1 -b styx .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
