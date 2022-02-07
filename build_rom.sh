a95fc7fe0ea6bad22ed9b9dc2a34e24b392aae93
Onkar singh <80300768+onkarsinghsahil@users.noreply.github.com>
2022-02-07 21:42:27 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch aosp_mido-userdebug
+lunch aosp_mido-onkar
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
