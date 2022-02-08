381ee2c1032a3410718741cd5fd1269535bf95f5
Atharv <81741009+atharv1103@users.noreply.github.com>
2022-02-08 11:15:04 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest -b 12 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Atharv2951-Roms/Local-Manifests/blob --depth=1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/AospExtended/manifest.git -b 12.x -g default,-mips,-darwin,-notdefault
+git clone https://github.com/AtarvNegi2951/local_manifest --depth=1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch voltage_pine-userdebug
-export ALLOW_MISSING_DEPENDENCIES=true
+lunch aosp_selene-userdebug
 export TZ=Asia/Kolkata #put before last build command
-brunch pine
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
