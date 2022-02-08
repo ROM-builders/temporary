c4646cc6d211274586ea0c4263cfa83ab03bb22d
Hitesoni | ×͜× <72127977+hsx02@users.noreply.github.com>
2022-02-08 16:12:43 +0530

    Last failed, testing unified trees

--- build_rom.sh
+++ build_rom.sh
@@ -7,6 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch spark_mi439-userdebug
 export WITH_GAPPS=true
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
