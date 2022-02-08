90ec72d8ddaa026bc682c600b8489700cdba15e3
BASSTANISHK9 <99045632+BASSTANISHK9@users.noreply.github.com>
2022-02-08 10:33:50 +0530

    Superior Os A11 J7elte
    Build SuperiorOs for j7elte BASSTANISHK9

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/SuperiorOS/manifest/tree/eleven -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/BASSTANISHK9/local_manifest --depth 1 -b master .repo/local_manifests
+git clone https://github.com/BASSTANISHK9/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
