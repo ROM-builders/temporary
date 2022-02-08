9f0f57ba6514c30c2cd346640b467b2869660a28
Atharv Negi <94751339+AtarvNegi2951@users.noreply.github.com>
2022-02-08 09:04:15 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,17 @@
-# sync manifest
-repo init --depth=1 --no-repo-verify -u git://github.com/xdroid-oss/xd_manifest -b twelve default,-mips,-darwin,-notdefault
+# sync
+
+repo init --depth=1 --no-repo-verify -u https://github.com/AospExtended/manifest.git -b 12.x -g default,-mips,-darwin,-notdefault
 git clone https://github.com/AtarvNegi2951/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch xd_selene-userdebug
+lunch aosp_selene-userdebug
+export WITH_GAPPS=true
 export TZ=Asia/Kolkata #put before last build command
 
 # Bacon
-mka xd
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
