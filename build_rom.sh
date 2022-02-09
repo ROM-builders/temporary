2c66aea6a6669651395dcc64aa4b337d1e701850
pingmado <75196272+ping2109@users.noreply.github.com>
2022-02-09 18:27:16 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_i9100-user
+lunch lineage_i9100-eng
 export SKIP_ABI_CHECKS=true
 export ALLOW_MISSING_DEPEDENCIES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
