c80f2be7c1dbeb6fab8564f9695745b7e073be77
kqeez69420x <99300566+kqeez@users.noreply.github.com>
2022-02-09 19:14:51 +0800

    fixed locl_manifest

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.g
 git clone https://github.com/kqeez/local_manifest --depth 1 -b rmx2020 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom-
 source build/envsetup.sh
 lunch superior_RMX2020-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
