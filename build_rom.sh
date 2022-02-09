9485c0576f9bc6c4b1446cfd44c0d09e0f8e6a5e
Julival Bittencourt <53379692+julival25@users.noreply.github.com>
2022-02-09 09:21:11 -0300

    Tulip: Build SparkOS With GAPPS

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build romm
 source build/envsetup.sh
 export SELINUX_IGNORE_NEVERALLOWS=true
+export WITH_GAPPS=true
 lunch spark_tulip-userdebug
 export TZ=America/Sao_Paulo #put before last build command
 mka bacon -j$(nproc --all)
