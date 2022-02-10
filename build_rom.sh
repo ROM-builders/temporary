210744477d38c387fa72c2de1e8c0fb7ae93b9a1
Mayur Narzary <flynryder427@gmail.com>
2022-02-10 20:40:45 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/StagOS/manifest -b s12 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/StagOS/manifest.git -b s12 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/xenxynon/manifest.git --depth=1 -b stag .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
