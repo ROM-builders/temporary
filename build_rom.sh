3e7628605d743f054bfe024057248c99204e1ea9
aminuddinihsan20 <47616229+aminuddinihsan20@users.noreply.github.com>
2022-02-10 17:50:34 +0700

    Surya-xdroid-oss
    Build Surya xdroid oss

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch aosp_surya-userdebug
+lunch xdroid_surya-userdebug
 export TZ=Asia/jakarta #put before last build command
 make xd 
 
