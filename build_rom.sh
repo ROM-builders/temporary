af413ab3950c0e17e12228e3b9ebd59c64754c30
Krishdangi <87589323+Krishdangi@users.noreply.github.com>
2022-02-10 19:51:29 +0530

    olivewood: Bring-UP Aosp extended

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/lighthouse-os/manifest.git -b sailboat -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Krishdangi/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/AospExtended/manifest.git -b 12.x -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Krishdangi/local_manifest.git --depth 1 -b krish .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lighthouse_olives-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
+lunch aosp_device_olivewood-userdebug
 export TZ=Asia/Dhaka #put before last build command
-make lighthouse
+m aex
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
