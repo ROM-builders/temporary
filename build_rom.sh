b388200dcb4716f4e3259a074e2ca2051f011ad5
Atharv Negi <94751339+atharvnegi@users.noreply.github.com>
2022-02-10 23:58:38 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync
 
 repo init --depth=1 --no-repo-verify -u https://github.com/AospExtended/manifest.git -b 12.x -g default,-mips,-darwin,-notdefault
-git clone https://github.com/AtarvNegi2951/local_manifest --depth 1 -b main .repo/local_manifests
+git clone https://github.com/atharvnegi/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
