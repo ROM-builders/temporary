6f4da3799d7612face8e206e203e4fdd5590eeda
Daman <82019599+DPSLEGEND@users.noreply.github.com>
2022-02-10 18:14:54 +0530

    Initial build

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/AospExtended/manifest.git -b 12.x -g default,-mips,-darwin,-notdefault
-git clone https://github.com/DPSLEGEND/local_manifest --depth 1 -b AEX .repo/local_manifests
+git clone https://github.com/DPSLEGEND/local_manifest --depth 1 -b AEX-moon .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_RMX2020-userdebug
+lunch aosp_moon-userdebug
 export TZ=Asia/Dhaka #put before last build command
 m aex
 
