1c8fda2d870981fa5af194806f43c8edaf48139f
Fandy Ahmad <99375985+BadClients@users.noreply.github.com>
2022-02-10 21:20:32 +0800

    rosy-xdroidOSS-BadClients
    Build xdroidOSS for Rosy

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/xdroid-oss/xd_manifest -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/BadClients/local_manifest.git --depth 1 -b main .repo/local_manifests
-repo sync -c -j8 --force-sync --no-clone-bundle --no-tags
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
