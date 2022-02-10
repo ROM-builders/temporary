0477104403d2485e14cff58325c466e6921eed04
Hyper <98272700+Hyperizer69@users.noreply.github.com>
2022-02-10 19:10:48 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch fuse_RMX1851-userdebug
+lunch aosp_RMX1851-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Kolkata #put before last build command
 make fuse-prod 
