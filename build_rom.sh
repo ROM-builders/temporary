86edd1d8ef9ff5c775e226aa33e1026608f5c00e
Fandy Ahmad <99375985+BadClients@users.noreply.github.com>
2022-02-10 21:14:39 +0800

    rosy-xdroidOSS-BadClients
    Build xdroidOSS for Rosy

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/xdroid-oss/xd_manifest -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/BadClients/local_manifest.git --depth 1 -b main .repo/local_manifests
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+repo sync -c -j8 --force-sync --no-clone-bundle --no-tags
 
 # build rom
 source build/envsetup.sh
 lunch xdroid_rosy-userdebug
-make xd
+make xd -j8
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
