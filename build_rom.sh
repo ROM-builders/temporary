ffdcb262ecbcf823c793bfb3233a151e903752e8
Jeremy Pérez <jerespn@gmail.com>
2022-02-10 00:40:30 -0400

    Superior for garden

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/SuperiorOS/manifest.git -b eleven -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Jeremito1/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch lineage_garden-userdebug
+lunch superior_garden-userdebug
 export TZ=Asia/Kuala Lumpur #put before last build command
 m bacon
 
