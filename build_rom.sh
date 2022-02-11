548da6caac8fc6ae0b66a25f9f6636303652eecf
GhostMaster69-dev <rathore6375@gmail.com>
2022-02-11 17:06:15 +0530

    Build OctaviOS userdebug
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -5,10 +5,10 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch vince-user
+lunch vince-userdebug
 #export WITH_GAPPS=true
 #export TARGET_GAPPS_ARCH=arm64
-#export SELINUX_IGNORE_NEVERALLOWS=true
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
