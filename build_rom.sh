ffc7ac378ef7166b873883cf7643ab8031f9192b
Hyper <98272700+Hyperizer69@users.noreply.github.com>
2022-02-11 06:52:05 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/The-RAVEN-OS/manifest -b twelve -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://Fusion-OS/android_manifest  -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Hyperizer69/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch aosp_RMX1851-userdebug
+lunch fuse_RMX1851-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Kolkata #put before last build command
 make fuse-prod 
