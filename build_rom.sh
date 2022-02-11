505573abb4b9fa491b432496a6e9b03ca90603b6
Suraj G Meharwade <66472830+Suru-web@users.noreply.github.com>
2022-02-11 14:28:04 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,9 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch aosp_denniz-userdebug 
+export SELINUX_IGNORE_NEVERALLOWS=true
+export ALLOW_MISSING_DEPENDENCIES=true
+export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
