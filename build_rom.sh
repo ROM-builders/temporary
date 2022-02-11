1acedff3c1e48077d83e8af451041404d727f317
Angel Castillo <49908914+acastillorobles77@users.noreply.github.com>
2022-02-11 17:23:03 -0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-blissify options vayu
+lunch bliss_vayu
 export TZ=Asia/Kolkata #put before last build command
 blissify -g vayu
 
