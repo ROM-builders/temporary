fe605d0d65c0d041b9af3903f1306bd8429061ee
rahul <rahul-k@bigdi.cc>
2022-02-12 14:23:20 +0000

    vince: Feb patch

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/crdroidandroid/android.
 git clone https://github.com/rk134/local_manifests.git --depth 1 -b crd .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom 
 source build/envsetup.sh
 lunch lineage_vince-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
