81d694b812e1a6b763e93500d28ff44a9536c94c
FSOC137Y <39610904+vickycena1@users.noreply.github.com>
2022-02-11 21:31:13 -0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,17 +1,17 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/ArrowOS/android_manifest -b arrow-12.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/vickycena1/local_manifest.git --depth 1 -b main .repo/local_manifest
+repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-CAF/xd_manifest -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/vickycena1/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
-lunch arrow_juice-userdebug
+. build/envsetup.sh
+lunch xdroid_juice-userdebug
 export BUILD_USERNAME=cyb3rgod0
 export BUILD_HOSTNAME=fsociety
 export KBUILD_BUILD_USER=cyb3rgod0
 export KBUILD_BUILD_HOST=fsociety
 export TZ=Asia/Kolkata #put before last build command
-m bacon
+make xd
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
