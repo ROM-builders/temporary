ef1d243ed81e793badb93223548556080b8274d5
vytautas <vytautas.duchas@gmail.com>
2022-02-13 19:52:38 +0200

    initial unified build

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/Evolution-X/manifest.git -b snow -g default,-mips,-darwin,-notdefault
-git clone https://github.com/hsx02/Local-Manifests.git --depth 1 -b aex-11 .repo/local_manifests
+git clone https://github.com/hsx02/Local-Manifests.git --depth 1 -b evox-12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
