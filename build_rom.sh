302ee8353a27bf0829ec9bd23d8c24920a8db164
S4MUR411 <59146493+S4MUR411@users.noreply.github.com>
2022-02-13 15:25:38 +0600

    forklos-laurel_sprout-S4MUR411
    fork los for laurel_sprout

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b eleven-plus -b lineage-18.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/S4MUR411/local_manifest.git --depth 1 -b master .repo/local_manifests
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+repo init --depth=1 --no-repo-verify -u git://github.com/ForkLineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/S4MUR411/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
 
 # build rom
-. build/envsetup.sh
-lunch aosp_$device-userdebug
-mka bacon -jX
+source build/envsetup.sh
+lunch lineage_laurel_sprout-userdebug
+make bacon -j$(nproc --all)
+export TZ=Asia/Dhaka
 
-# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+# upload rom
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
