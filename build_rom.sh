bafb2786ae71d3de1e35951bc1fa043301871407
NexusHacker <96295487+NexusHacker@users.noreply.github.com>
2022-02-13 15:28:16 +0530

    RMX1851 ravenos Nexushacker

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
 repo init -u https://github.com/The-RAVEN-OS/manifest -b twelve
 git clone https://github.com/NexusHacker/local_manifests.git --depth 1 -b master .repo/local_manifests
-repo sync -c -j8(nproc --all) --force-sync --no-clone-bundle --no-tags
+repo sync -c -jx(nproc --all) --force-sync --no-clone-bundle --no-tags
 
 # build rom
 . build/envsetup.sh
 lunch aosp_RMX1851-userdebug
 export TZ=Asia/Dhaka #put before last build command
-mka bacon
+mka bacon -j8
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
