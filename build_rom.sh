3a1ad4bb5dfba43822a1ce5e3d0ee028e4a35191
Julival Bittencourt <53379692+julival25@users.noreply.github.com>
2022-02-13 14:34:49 +0000

    Build Spark Tulip

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
-#sync rom
+# sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/Spark-Rom/manifest.git -b spark -g default,-mips,-darwin,-notdefault
 git clone https://github.com/julival25/local_manifest.git --depth 1 -b spark .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build romm
+# build rom
 source build/envsetup.sh
-export SELINUX_IGNORE_NEVERALLOWS=true
-export WITH_GAPPS=true
 lunch spark_tulip-userdebug
+export WITH_GAPPS=true
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=America/Sao_Paulo #put before last build command
 mka bacon
 
