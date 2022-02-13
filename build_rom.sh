37baaf84f4072e7df64b994822d470045a2d5789
Shubh <97435707+iamshubh69@users.noreply.github.com>
2022-02-13 22:31:01 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_vayu-user
+lunch lineage_moon-userdebug
 SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPEDENCIES=true
 export SKIP_ABI_CHECKS=true
