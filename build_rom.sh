b73fe7d5bc178203281eea2e639a80de491ff3b2
vytautas <vytautas.duchas@gmail.com>
2022-02-13 19:50:27 +0200

    initial build for mi439 (pine+olives)

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch evolution_pine-userdebug
+lunch evolution_mi439-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
 mka evolution
