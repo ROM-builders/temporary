67d4c9b7647e557278ad0a5491c37fad99235564
Mayur Narzary <flynryder427@gmail.com>
2022-02-13 14:12:22 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://git.stag-os.org/StagOS/manifest.git -g default,-mips,-darwin,-notdefault
-git clone https://github.com/xenxynon/manifest.git --depth=1 -b stag .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.git -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/xenxynon/manifest.git --depth=1 -b sup .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch stag_lavender-userdebug
+lunch superior_lavender-userdebug
+export BUILD_WITH_GAPPS=true
 export TZ=Asia/Kolkata #put before last build command
-make stag
+make bacon -j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
