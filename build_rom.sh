d557157f84163587d4014df6e7a6fd41fd331d39
sajinprakas <93714378+sajinprakas@users.noreply.github.com>
2022-02-13 17:30:37 +0530

    final build

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/projectarcana-aosp/ma
 git clone https://github.com/Azure-007/local_manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom final build
 source build/envsetup.sh
 lunch aosp_raphael-userdebug 
 export SELINUX_IGNORE_NEVERALLOWS=true
