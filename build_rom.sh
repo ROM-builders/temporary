f17374305c39ba5afeb56765ba5957c707747ec8
NexusHacker <96295487+NexusHacker@users.noreply.github.com>
2022-02-13 15:14:39 +0530

    RMX1851 ravenos Nexushacker

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init -u https://github.com/The-RAVEN-OS/manifest -b twelve -g default,-mips,-darwin,-notdefault
+repo init -u https://github.com/The-RAVEN-OS/manifest -b twelve
 git clone https://github.com/NexusHacker/local_manifests.git --depth 1 -b master .repo/local_manifests
 repo sync -c -j8(nproc --all) --force-sync --no-clone-bundle --no-tags
 
