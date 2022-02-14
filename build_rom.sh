c5bfcdecc9710c1925e920d6aafcc59835557534
GhostMaster69-dev <rathore6375@gmail.com>
2022-02-14 13:02:21 +0530

    Project Radiant user build
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -5,8 +5,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch vince-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
+lunch vince-user
+#export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
