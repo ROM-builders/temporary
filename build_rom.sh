43a9092f849f40816b852a48cf8917d682ec1f85
mikairyuu <miva200267@gmail.com>
2022-02-15 06:13:00 +1000

    Upbring radiant

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom 
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest.git -b snow -g default,-mips,-darwin,-notdefault
-git clone https://github.com/mikairyuu/local_manifest.git --depth 1 -b snow-wip .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ProjectRadiant/manifest -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/mikairyuu/local_manifest.git --depth 1 -b radiant .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch evolution_monet-userdebug
+lunch radiant_monet-userdebug
 export TZ=Asia/Vladivostok #put before last build command
 mka evolution
 
