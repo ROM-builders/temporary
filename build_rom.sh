929b917ca53a2f55be52a9fe5103278e2aba7a89
BagasSep21 <91189529+BagasSep21@users.noreply.github.com>
2022-02-14 23:41:44 +0700

    Test Build

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/CipherOS/android_mani
 git clone https://github.com/lynnnnzx/local_manifest.git --depth 1 -b cipher .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom 
 . build/envsetup.sh
 lunch cipher_juice-userdebug
 export BUILD_USERNAME=lynx
