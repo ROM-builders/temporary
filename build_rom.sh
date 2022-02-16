4fb704ea218e0ce74593ae5910b669bf043e8663
Azure-007 <64735082+Azure-007@users.noreply.github.com>
2022-02-16 18:42:46 +0530

    Fixed Sync Error

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,13 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/projectarcana-aosp/manifest -b 12.x -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Azure-007/local_manifests.git --depth 1 -b arcana-wip .repo/local_manifests
+git clone https://github.com/Azure-007/local_manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch aosp_raphael-userdebug 
 export SELINUX_IGNORE_NEVERALLOWS=true
+export WITH_GAPPS=true
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
