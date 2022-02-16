cbf28bc8e785b3d480c47e8d40c320e1ab962ca8
Krazy-Goel <88996044+Krazy-Goel@users.noreply.github.com>
2022-02-16 12:54:02 +0530

    Fixed few bugs

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch komodo_$olives-userdebug
+lunch komodo_olives-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true 
 export WITH_GAPPS=true
 export TZ=Asia/Dhaka #put before last build command
