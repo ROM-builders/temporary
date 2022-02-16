78ff40a1156c7c931f232b8491db2273ad3aa67f
Nabil Zaman <94600437+iamnabilzaman@users.noreply.github.com>
2022-02-17 01:59:05 +0600

    santoni xdroid iamnabilzaman

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-oss/xd_manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/iamnabilzaman/local_manifest12.git -b xd --depth 1 .repo/local_manifests
+git clone https://github.com/iamnabilzaman/local_manifest12.git -b xdroid --depth 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -8,9 +8,8 @@ source build/envsetup.sh
 lunch xdroid_santoni-userdebug
 export BUILD_USERNAME=nabil
 export BUILD_HOSTNAME=project-entire
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
 export TZ=Asia/Dhaka #put before last build command
-make xd
+make xd 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
