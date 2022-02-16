b85b5cc04b1ba97e463d4d4d8056774290a873dd
Azure-007 <64735082+Azure-007@users.noreply.github.com>
2022-02-16 11:01:15 +0530

    Change kernel

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/projectarcana-aosp/manifest -b 12.x -g default,-mips,-darwin,-notdefault
-git clone https://github.com/XCode219/local_manifests.git --depth 1 -b aosp-12.0 .repo/local_manifests
+git clone https://github.com/Azure-007/local_manifests.git --depth 1 -b arcana-wip .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
