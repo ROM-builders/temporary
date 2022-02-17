7a5d040133f9f4b33ba4be34c42ab12566abbc8a
Akshat <79436731+akshat0011@users.noreply.github.com>
2022-02-17 02:00:48 -0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b eleven -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/Corvus-R/android_manifest.git -b 11 -b eleven -g default,-mips,-darwin,-notdefault
 git clone https://github.com/akshat942/local_manifest/blob/caf-11/local_manifest.xml --depth 1 -b caf-11 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
+./device/redmi/rosemary/unpack-vendor.sh
 source build/envsetup.sh
-lunch aosp_rosemary-userdebug
+lunch corvus_rosemary-userdebug
 export TZ=Asia/Dhaka #put before last build command
-mka bacon
+mka corvus
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
