5c8bd1465a8aa7f9db24d86733adb0c1396db28d
Julival Bittencourt <53379692+julival25@users.noreply.github.com>
2022-02-17 20:33:35 -0300

    Revert "Fix Sync added fail-fast" This reverts commit dc808ac

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/Spark-Rom/manifest.git -b spark -g default,-mips,-darwin,-notdefault
 git clone https://github.com/julival25/local_manifest.git --depth 1 -b spark .repo/local_manifests
-repo sync -c -j1 --fail-fast --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
