b235cc229d6f63a489787feb695a1f0bc5bf59d6
Daman <daman.dogee.02@gmail.com>
2022-02-17 21:55:20 +0530

    Initial build

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-12.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/DPSLEGEND/local_manifest.git --depth 1 -b arrow-temp .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 12.0 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/DPSLEGEND/local_manifest.git --depth 1 -b crdroid-c3 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch arrow_moon-userdebug
+lunch lineage_RMX2020-userdebug
 export TZ=Asia/Dhaka #put before last build command
-m bacon
+brunch RMX2020
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+  
