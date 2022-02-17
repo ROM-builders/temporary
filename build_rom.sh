d4a77c507efd4f3e8678930a18a8ce34a56fcd57
Sarthak Roy <sarthakroy2002@gmail.com>
2022-02-17 23:26:30 +0530

    Trigger CipherOS Official Build 12-02 Gapps

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/CipherOS/android_manifest.git -b twelve -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/CipherOS/android_manifest.git -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/sarthakroy2002/local_manifest -b cipher --depth 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
@@ -11,8 +11,11 @@ export SKIP_ABI_CHECKS=true
 export SKIP_API_CHECKS=true
 export BUILD_HOSTNAME=sarthakroy2002
 export BUILD_USERNAME=neolit
+export CIPHER_GAPPS=true
 mka bacon 
 
+# get buildate for OTA pushing
+bash vendor/cipher/build/tools/ota.sh
 
 # upload rom
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
