887feecd624513915fb5ef88d22abaf3a423f75f
Monu <56600996+monu70152@users.noreply.github.com>
2022-02-17 17:55:20 +0530

    a new way with some help of pro

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
-repo init -u https://github.com/projectarcana-aosp/manifest -b 12.x
-git clone https://github.com/monu70152/local_manifest.git -b master
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+repo init --depth=1 --no-repo-verify -u https://github.com/projectarcana-aosp/manifest.git -b 12.x -g default,-mips,-darwin,-notdefault
+git clone https://github.com/monu70152/local_manifest.git --depth 1 -b master .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -jX
 
 # build rom
 source build/envsetup.sh
