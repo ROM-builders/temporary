a8872ac8bcee7272a1d049067360d295f88afcca
Shubh <97435707+iamshubh69@users.noreply.github.com>
2022-02-17 19:22:12 +0530

    Fix

--- build_rom.sh
+++ build_rom.sh
@@ -6,9 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_moon-userdebug
-SELINUX_IGNORE_NEVERALLOWS=true
-export ALLOW_MISSING_DEPEDENCIES=true
-export SKIP_ABI_CHECKS=true
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
