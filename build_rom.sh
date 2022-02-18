395d707d7141bf86daa78c1d92216cbad82fb39e
jhonatan Mikael <75139386+jhonnytech90@users.noreply.github.com>
2022-02-18 15:04:01 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch dot_A001D-userdebug
-export TZ=America/Sao_Paulo #up
+export TZ=America/Sao_Paulo #agoravaicaraio
 make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
