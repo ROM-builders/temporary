0ca65621af849751c7034ea26ff764eeb58bd664
BagasSep21 <91189529+BagasSep21@users.noreply.github.com>
2022-02-19 04:15:58 +0700

    Test Error's

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/syberia-project/manif
 git clone https://github.com/lynnnnzx/local_manifest.git --depth 1 -b syberia .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom 
 source build/envsetup.sh
 lunch syberia_juice-userdebug
 export BUILD_USERNAME=lynx
