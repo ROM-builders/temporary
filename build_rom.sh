b04b9e266fae0dece39ee71be86039ae2f7a782c
Monu <56600996+monu70152@users.noreply.github.com>
2022-02-19 06:06:14 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/projectarcana-aosp/manifest.git -b 12.x -g default,-mips,-darwin,-notdefault
 git clone https://github.com/monu70152/local_manifest.git --depth 1 -b master .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j1
 
 # build rom
 source build/envsetup.sh
