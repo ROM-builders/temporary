2a25b3b4717b4275896f3a80c3725c4a8b0d08b8
supflash <99805745+supflash@users.noreply.github.com>
2022-02-19 13:03:03 +0400

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/Project-Elixir/manifest.git -b snow -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/supflash/manifest.git -b snow -g default,-mips,-darwin,-notdefault
 git clone https://github.com/supflash/local_manifest.git --depth 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
