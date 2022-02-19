224137c423f3ada916c199815bcecc2df51c97c7
RioChanY <100027207+RioChanY@users.noreply.github.com>
2022-02-19 21:24:16 +0530

    Updated

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch havoc_mojito-user
-export TZ=Asia/Banghok #put before last build command
+export TZ=Asia/singapur #put before last build command
 mka havoc
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
