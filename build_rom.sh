2d11b0080f45064f70e3cd6832474bad80b77da1
BagasSep21 <91189529+BagasSep21@users.noreply.github.com>
2022-02-19 10:25:57 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,16 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/CipherOS/android_manifest.git -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/lynnnnzx/local_manifest.git --depth 1 -b cipher .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/exthmui/android.git -b exthm-11 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/lynnnnzx/local_manifest.git --depth 1 -b exthmui .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch cipher_juice-userdebug
+lunch exthm_juice-userdebug
 export BUILD_USERNAME=lynx
-export BUILD_HOSTNAME=cirrus-ci
-export SELINUX_IGNORE_NEVERALLOWS=true
+export BUILD_HOSTNAME=serber-memek
+export PLATFORM_SECURITY_PATCH=2022-02-05
 export TZ=Asia/Jakarta
-make bacon
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
