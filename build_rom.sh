ccf49b4cc980cfa0910cf8c9b032fa40164d11e6
Ashish <97603141+JaswalAshish@users.noreply.github.com>
2022-02-19 20:32:50 +0530

    Olivewood: done some fixes

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch evolution_olivewood-userdebug
+lunch evolution_olivewood-eng
 export TZ=Asia/Dhaka #put before last build command
 mka evolution
 
