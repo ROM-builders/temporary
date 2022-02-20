858423a1dca93827a7e15db096a55fce17efd1e9
RioChanY <100027207+RioChanY@users.noreply.github.com>
2022-02-20 07:30:54 +0530

    Fix -1

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/crdroidandroid/android.git -b 11.0 -g  default,-mips,-darwin,-notdefault
 git clone https://github.com/RiochanY/local_manifest.git depth 1 -b 11.0 --depth=1 .repo/local_manifests
-
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
 
 # build rom
