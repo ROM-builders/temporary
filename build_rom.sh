eab5a4d6f8b9fcf5499d1532f97a40c80c707dca
Anky <ankit8080dhanare@gmail.com>
2022-02-21 00:55:04 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch awwaken_phoenix-userdebug
+lunch awaken_phoenix-userdebug
  #put before last build command
 make bacon -j$(nproc --all)
 
