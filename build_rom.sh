92ec082bff06cff4d1cac8729ab10fc4785d1c12
RioChanY <100027207+RioChanY@users.noreply.github.com>
2022-02-20 07:27:08 +0530

    Updated

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/ProtonAOSP/android_manifest -b sc -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ProtonAOSP/android_manifest depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/crdroidandroid/android.git -b 11.0 -g  default,-mips,-darwin,-notdefault
+git clone https://github.com/RiochanY/local_manifest.git depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
