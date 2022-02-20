44177aeaf9b680bbba8fd10f31f7c98714f57833
BASSTANISHK9 <99045632+BASSTANISHK9@users.noreply.github.com>
2022-02-20 17:24:30 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/Evolution-X/manifest -b elle default,-mips,-darwin,-notdefault
 git clone https://github.com/BASSTANISHK9/local_manifest.git --depth 1 -b main.repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sy can -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
