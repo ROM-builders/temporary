cf5ae5046a4555600837a196b536f1fd817265f7
moonitime <97696888+moonitime@users.noreply.github.com>
2022-02-21 23:49:16 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/PixelAirOS/Manifest -b twelve -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git:/github.com/ArrowOS/android_manifest.git -b arrow-12.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/moonitime/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_lavender-userdebug
+lunch arrow_lavender-userdebug
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
