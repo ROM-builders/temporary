7d3f5b95623ef83543e5a5b47ef3f3a7d8a4a758
Ansh <89060758+EvilAnsh@users.noreply.github.com>
2022-02-21 18:33:42 +0530

    Change - jX to - j8

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/projectarcana-aosp/manifest -b 12.x -g default,-mips,-darwin,-notdefault
 git clone https://github.com/EvilAnsh/local-manifest.git --depth 1 -b main .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -jX 
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
 
 # build rom
 source build/envsetup.sh
