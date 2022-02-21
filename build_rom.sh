ea51b41c481557325a58b542139e24501cd329e8
DenastySS <67180787+DenastySS@users.noreply.github.com>
2022-02-21 23:41:06 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,8 +5,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch xdroid_taimen-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
+lunch xdroid_taimen-eng
+#export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Jakarta #put before last build command
 make xd
 
