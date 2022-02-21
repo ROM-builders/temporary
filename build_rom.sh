076b6679f371c1ca91ca100944bb09106aad11a8
vigneshd2362 <100097546+vigneshd2362@users.noreply.github.com>
2022-02-21 17:27:47 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -8,7 +8,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 lunch arrow_juice-userdebug
 export BUILD_USERNAME=cyb3rgod0
 export BUILD_HOSTNAME=fsociety
-export TZ=Asia/Kolkata #put before last build commandz
+export TZ=Asia/Kolkata
 m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
