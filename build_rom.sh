8975feaeac254345f351d366191be0124afc0a92
Anantgoel0123456 <88424558+Anantgoel0123456@users.noreply.github.com>
2022-02-21 20:57:37 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch aosp_alioth-user
 lunch aosp_alioth-userdebug
 export WITH_GAPPS=true
 export TZ=Asia/Manila #put before last build command
