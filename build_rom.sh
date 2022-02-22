a5dced8c3b973c05d5be153834068574349bf51c
Prashant <93838092+ImPrashantt@users.noreply.github.com>
2022-02-22 21:41:16 +0530

    Feb build

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch aosp_lavender-user
+export PEX_BUILD_TYPE=OFFICIAL
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
