7d7b2cfb4a487589bd5ed4e358a480dd88b37f17
Hitesoni | ×͜× <72127977+hsx02@users.noreply.github.com>
2022-02-22 09:56:59 +0530

    Test build

--- build_rom.sh
+++ build_rom.sh
@@ -8,7 +8,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 lunch aosp_pine-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
-export PEX_BUILD_TYPE=OFFICIAL
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
