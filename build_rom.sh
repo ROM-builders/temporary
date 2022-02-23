4599795582deb358f349444763dffeb539546566
Julival Bittencourt <53379692+julival25@users.noreply.github.com>
2022-02-23 20:15:05 -0300

    Initial Build SparkOS - Polaris

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/Spark-Rom/manifest.git -b spark -g default,-mips,-darwin,-notdefault
-git clone https://github.com/julival25/local_manifest.git --depth 1 -b spark .repo/local_manifests
+git clone https://github.com/julival25/local_manifest.git --depth 1 -b spark-polaris .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch spark_tulip-userdebug
+lunch spark_polaris-userdebug
 export WITH_GAPPS=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=America/Sao_Paulo #put before last build command
