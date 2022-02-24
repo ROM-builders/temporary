3b19dac4c230c806fcec925e8900ee307b2d6ae0
Monu 2 <99852733+monuohlyan@users.noreply.github.com>
2022-02-24 22:52:08 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom 
 . build/envsetup.sh
-lunch xtended_device-userdebug
+lunch xtended_X00TD-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Kolkata #put before last build command
 make xtended
