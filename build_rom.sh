6035b114148d284e47fa2b6e4e8b66b256ffe6a9
Monu 2 <99852733+monuohlyan@users.noreply.github.com>
2022-02-24 22:49:19 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ProjectRadiant/manifest.git -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/monuohlyan/local_manifest.git --depth 1 -b radiant .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Xtended/manifest.git -b xs -g default,-mips,-darwin,-notdefault
+git clone https://github.com/monuohlyan/local_manifest.git --depth 1 -b xtended .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom 
 . build/envsetup.sh
-lunch radiant_X00TD-userdebug
+lunch xtended_device-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Kolkata #put before last build command
-mka bacon
+make xtended
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
