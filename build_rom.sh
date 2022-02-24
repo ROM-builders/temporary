07e996cb2818d1aed400e146ecf7729f7bdaa2d7
Ansh <89060758+EvilAnsh@users.noreply.github.com>
2022-02-24 11:22:35 +0530

    Blissify

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/projectarcana-aosp/manifest -b 12.x -g default,-mips,-darwin,-notdefault
-git clone https://github.com/EvilAnsh/local-manifest.git --depth 1 -b main .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
+repo init --depth=1 --no-repo-verify -u https://github.com/BlissRoms/platform_manifest.git -b arcadia -g default,-mips,-darwin,-notdefault
+git clone https://github.com/EvilAnsh/Local-Manifest.git --depth 1 -b bliss .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_santoni-userdebug
-export TZ=Asia/Mumbai #put before last build command
-make bacon 
+lunch bliss_santoni-userdebug
+export TZ=Asia/Dhaka #put before last build command
+blissify -v santoni
 
-# upload rom (if your don't need to upload multiple files, then you don't need to edit next line)
+# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
