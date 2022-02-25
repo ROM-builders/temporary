b43d0c676c5c6ced3e67949d34f144b403fcceba
w1zzyl <96798575+w1zzyl@users.noreply.github.com>
2022-02-25 23:06:33 +0100

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,12 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/Project-Elixir/manifest.git -b snow -g default,-mips,-darwin,-notdefault
-git clone https://github.com/kuda07/local_manifest.git --depth 1 -b vayu-elixir .repo/local_manifests
+git clone https://github.com/w1zzyl/local_manifest.git --depth 1 -b vayu-elixir .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
 lunch aosp_vayu-userdebug
+export BUILD_USERNAME=w1zzyl
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
