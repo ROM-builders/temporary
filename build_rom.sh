fc40d62fd4d05671e2da128cf991d741013cf938
Hitesoni | ×͜× <72127977+hsx02@users.noreply.github.com>
2022-02-25 10:39:15 +0530

    Fixed dependencies

--- build_rom.sh
+++ build_rom.sh
@@ -7,6 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 . build/envsetup.sh
 lunch aosp_pine-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
+export PEX_BUILD_TYPE=OFFICIAL
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
