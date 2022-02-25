2464f7eb725aef14b2b40731647cc1f3e1fde399
Mayur <flynryder427@gmail.com>
2022-02-25 07:50:18 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest -b 12 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/xenxynon/manifest.git --depth=1 -b volt .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u -u https://github.com/PotatoProject/manifest -b frico-release -g default,-mips,-darwin,-notdefault
+git clone https://github.com/xenxynon/manifest.git --depth=1 -b potato .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch voltage_lavender-userdebug
+lunch potato_lavender-userdebug
 export TZ=Asia/Kolkata #put before last build command
-export GAPPS_BUILD=true
-make bacon
+export WITH_GMS=true
+brunch lavender
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
