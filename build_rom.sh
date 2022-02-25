9d84f1aa0e227298019c5278a64e4fb0ee3d92ed
Ritvik <99066143+Ritvik6969@users.noreply.github.com>
2022-02-25 14:27:45 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/ConquerOs-5.1/manifest.git -b 12 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/ConquerOS-5.1/manifest.git -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Ritvik6969/Local-Manifests.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
