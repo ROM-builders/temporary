4dccce55fcbee03d4a374f362e263cb7c6c2db07
Monu 2 <99852733+monuohlyan@users.noreply.github.com>
2022-02-25 19:21:47 +0530

    done some fixes

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 . build/envsetup.sh
 lunch xtended_X00TD-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
-export TZ=Asia/Kolkata #put before last build command
+export RELAX_USES_LIBRARY_CHECK=true
+export TZ=Asia/Kolkata
 make xtended
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
