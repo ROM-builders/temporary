d43e622d3d7baa93f8983c92077f98f92cc59fc0
ariffjenong <arifbuditantodablekk@gmail.com>
2022-02-25 09:04:13 +0700

    fix typo

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/ArrowOs/android_manifest.git -b arrow-12.0 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/ArrowOS/android_manifest.git -b arrow-12.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/ariffjenong/local_manifest.git --depth 1 -b Arrow-12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
