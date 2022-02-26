795ed2eefb5db624e8d7844394b68dca9a5fdc52
RasyidAlKautsar <rasyid2007alkautsar@gmail.com>
2022-02-27 04:56:26 +0700

    Fix FP #4

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch cherish_mido-userdebug
+export BUILD_HOSTNAME=RasyidAlKautsar
 export TZ=Asia/Jakarta #put before last build command
 mka bacon
 
