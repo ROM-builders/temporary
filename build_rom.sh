170c5c6cd6bc3c84063140e8b55339c8ce699889
Nabil Zaman <94600437+iamnabilzaman@users.noreply.github.com>
2022-02-26 13:39:10 +0600

    santoni OctaviOS iamnabilzaman

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch octavi_santoni-userdebug
+export SKIP_ABI_CHECKS=true
 export BUILD_USERNAME=nabil
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
