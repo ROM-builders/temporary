1faca4ebb0bee87cc6f98412a7e88285e5d3cd5e
Zahran Ansaria <89372281+imannig@users.noreply.github.com>
2022-02-26 16:50:35 +0700

    change branch local manifest

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Project-Elixir/manifest -b snow -g default,-mips,-darwin,-notdefault
-git clone https://github.com/imannig/local_manifest --depth 1 -b twelve-elixir .repo/local_manifests
+git clone https://github.com/imannig/local_manifest --depth 1 -b twelve-elixir-1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
