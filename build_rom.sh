a0fd07db29c6fec805e539ab463e303f6730fc7a
zhenyolka <zhenyolka@gmail.com>
2022-02-26 12:16:30 +0300

    pyxis: Skip ABI checks

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch octavi_pyxis-userdebug
+export SKIP_ABI_CHECKS=true
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
