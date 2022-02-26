7f743465347f1be4896bb6814660ac9843dbf338
Mayur <flynryder427@gmail.com>
2022-02-26 07:44:22 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,6 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch potato_lavender-userdebug
 export TZ=Asia/Kolkata #put before last build command
+export ALLOW_MISSING_DEPENDENCIES=true
 export WITH_GMS=true
 brunch lavender
 
