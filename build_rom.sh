73301323affc17f468dbbeb6f8ba28985fd48aff
Raiden Ishigami <contact.raidenishi69@gmail.com>
2022-02-27 20:18:11 -0300

    Add ALLOW_MISSING_DEPENDENCIES=true

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch arrow_A001D-userdebug
+export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=America/Sao_Paulo
 m bacon
 
