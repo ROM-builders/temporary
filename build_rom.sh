532f6bac7a9e8383f8097f5370b273f59ace134f
Coptan⁹⁹ <omarcoptan9@gmail.com>
2022-02-27 07:56:37 +0200

    Lavender-PixelOS-OmarAlCoptan

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-Pixelish/manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/OmarAlCoptan/local_manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/OmarAlCoptan/local_manifest.git --depth 1 -b PixelOS .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch aosp_lavender-userdebug
-export TZ=Asia/Dhaka #put before last build command
+export TZ=Africa/Egypt #put before last build command
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
