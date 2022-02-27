1f91aa8af847079c8451117a466041b10496f291
iamrehan07 <92092421+iamrehan07@users.noreply.github.com>
2022-02-27 09:16:01 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android -b 12.0 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/iamrehan07/android?organization=iamrehan07&organization=iamrehan07 -b 12.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/iamrehan07/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
