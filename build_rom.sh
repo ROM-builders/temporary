95ff64c5419ce3767665e15b2388c9c3b75c962a
Ayush Kumar Behera <100088098+ayushkumar35@users.noreply.github.com>
2022-03-01 01:49:36 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init -u git://github.com/omnirom/android.git -b android-12.0 -g default,-mips,-darwin,-notdefault
+repo init -u https://github.com/StyxProject/manifest -b S --depth=1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/ayushkumar35/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch omni_lavender-user
+lunch styx_lavender-user
 export TZ=Asia/Kolkata #put before last build command
-mka lavender
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
