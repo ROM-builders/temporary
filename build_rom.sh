1504d97b1ef20c0b66cdb98986184be4d9e981a5
sajinprakas <93714378+sajinprakas@users.noreply.github.com>
2022-02-28 22:09:13 +0530

    DotOS Initial Build

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/projectarcana-aosp/manifest -b 12.x -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/DotOS/manifest.git -b dot12 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/XCode219/local_manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom final build #
 source build/envsetup.sh
-lunch aosp_raphael-userdebug
+lunch dot_raphael-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
 make bacon
