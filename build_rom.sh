3973bb653c43ba20e615dd953fcc70ce4fc7619f
sajinprakas <93714378+sajinprakas@users.noreply.github.com>
2022-03-01 02:32:03 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/DotOS/manifest.git -b dot12 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/DotOS/manifest.git -b dot12 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/XCode219/local_manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
