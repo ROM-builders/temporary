b64aa40e7376c452b1d822bfbd0279ff6f1bcad2
Snowman2409 <93831265+Snowman2409@users.noreply.github.com>
2022-03-01 11:10:56 +0530

    Lavender-pixel experience-snwoman2409

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u -u https://github.com/PotatoProject/manifest -b frico-release -g default,-mips,-darwin,-notdefault
-git clone https://github.com/xenxynon/manifest.git --depth=1 -b potato .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo init --depth=1 --no-repo-verify -u -u https://github.com/PixelExperience/manifest -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Snowman2409/local-manifest --depth=1 -b potato .repo/local_manifests
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags--optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch potato_lavender-user
+$ lunch aosp_$device-userdebug
 export TZ=Asia/Kolkata #put before last build command
 export WITH_GMS=true
 brunch lavender
