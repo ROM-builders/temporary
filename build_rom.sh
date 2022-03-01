c263aed64b8e41639e76123337179863850e3fbc
vigneshd2362 <100097546+vigneshd2362@users.noreply.github.com>
2022-02-28 19:45:35 -0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/projectarcana-aosp/manifest.git -b 12.x -g default,-mips,-darwin,-notdefault
-git clone https://github.com/vickycena1/local_manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/vigneshd2362/local_manifest.git --depth 1 -b arcana .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
