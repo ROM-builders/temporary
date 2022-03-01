67c9c99e4f7fb269c0f1f8e0995edb134ebaf156
Ansh <89060758+EvilAnsh@users.noreply.github.com>
2022-03-01 10:17:28 +0530

    export ALLOW_MISSING_DEPENDENCIES=true

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch aosp_santoni-userdebug
+export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Mumbai #put before last build command
 make bacon 
 
