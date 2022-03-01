527e32f08a6aa4aaa5fbba524c3fbd5435354b19
Yillié <yilliee@protonmail.com>
2022-03-01 23:25:00 +0500

    build_rom : Amend script for OctaviOS GSI

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,17 @@
-# sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo init --depth=1 --no-repo-verify -u https://github.com/Octavi-OS/platform_manifest.git -b 12 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Octavi-OS-GSI/treble_manifest.git --depth 1 -b 12 .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || exit 2
+
+cd device/phh/treble && bash generate.sh octavi && cd ../../../
 
 # build rom
-source build/envsetup.sh
-lunch derp_vayu-user
-export TZ=Asia/Dhaka #put before last build command
-mka derp
+. build/envsetup.sh
+lunch treble_arm64_bgN-userdebug
+export TZ=Asia/Karachi # Put before last build command
+mka systemimage
+
+mv out/target/product/phhgsi_arm64_ab/system.img out/target/product/phhgsi_arm64_ab/OctaviOS-v3.4-arm64_bgN-$(TZ='Asia/Karachi' date "+%Y%m%d").img
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
-rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+rclone copy out/target/product/phhgsi*/OctaviOS*.img cirrus:OctaviOS-GSI -P
+
