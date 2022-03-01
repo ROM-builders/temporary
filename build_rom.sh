e48fe3ae9137f48f8ce4c6f9a95fbf931d6e1160
ayushkumar35 <ayushbehera077@gmail.com>
2022-03-01 13:58:12 +0000

    fluid

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init -u https://github.com/StyxProject/manifest -b S --depth=1 -g default,-mips,-darwin,-notdefault
+repo init -u https://github.com/Project-Fluid/manifest.git -b fluid-12 --depth=1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/ayushkumar35/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch styx_lavender-user
+lunch fluid_lavender-userdebug
 export TZ=Asia/Kolkata #put before last build command
-m styx-ota
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
