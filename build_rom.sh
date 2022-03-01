d56579d761a05b47e27805ce6671731f47ced289
Arif Munandar <90164420+arifmndr17@users.noreply.github.com>
2022-03-01 18:55:19 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init -u https://github.com/projectarcana-aosp/manifest -b 12.x -g default,-mips,-darwin,-notdefault
-git clone https://github.com/arifmndr17/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/arifmndr17/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
