e956eb916e757019aa2dc6ddcbdb3bae8437b503
vigneshd2362 <100097546+vigneshd2362@users.noreply.github.com>
2022-02-28 19:57:58 -0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -10,8 +10,8 @@ export BUILD_USERNAME=vicky
 export BUILD_HOSTNAME=cyb3rgod0
 export KBUILD_BUILD_USER=vicky
 export KBUILD_BUILD_HOST=cyb3rgod0
-export TZ=Asia/Kolkata #put before last build commandz
-make bacon -jX
+export TZ=Asia/Kolkata #put before last build command
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
