b046752c02c04f42f13790de309bfb0849c3a946
Ayush Kumar Behera <100088098+ayushkumar35@users.noreply.github.com>
2022-03-01 19:50:17 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init -u https://github.com/Project-Fluid/manifest.git -b fluid-12 --depth=1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ayushkumar35/local_manifest.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/ayushkumar35/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
