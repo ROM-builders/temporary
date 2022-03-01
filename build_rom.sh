6b58af9a94402e2f41a24e0f575909de3f5783b5
Asif <49162687+nhAsif@users.noreply.github.com>
2022-03-01 21:06:37 +0600

    rosy: update kernel & dt

--- build_rom.sh
+++ build_rom.sh
@@ -6,8 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_rosy-userdebug
-export BUILD_USERNAME=nhAsif
-export BUILD_HOSTNAME=rosy
 export TZ=Asia/Dhaka
 brunch rosy
 
