8c8ed357829955473224cbc76953a88f7591c393
Julival Bittencourt <53379692+julival25@users.noreply.github.com>
2022-03-01 12:18:57 -0300

    Tulip:Syberia Project | Initial build |

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,15 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/syberia-project/manifest.git -b 12.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/iaiaian1/local-manifests.git --depth 1 -b Syberia-12.0 .repo/local_manifests
+git clone https://github.com/julival25/local&manifest.git --depth 1 -b syberia .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 #17
 
 # build rom
 source build/envsetup.sh
-lunch syberia_pine-userdebug
-export TZ=Asia/Dhaka #put before last build command
+lunch syberia_tulip-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
+export TZ=America/Sao_Paulo #put before last build command
 make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
