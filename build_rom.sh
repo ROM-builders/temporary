d095d1984a997ec02005443469a5301f662a032f
Yillié <yilliee@protonmail.com>
2022-03-02 07:31:14 +0500

    build_rom : Try to upload file to wetransfer instead of changing the rclone command

--- build_rom.sh
+++ build_rom.sh
@@ -8,6 +8,8 @@ lunch treble_arm64_bgN-userdebug
 export TZ=Asia/Karachi # Put before last build command
 mka systemimage
 
-# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
-rclone copy out/target/product/phhgsi*/system.img cirrus:OctaviOS-GSI -P
+curl -sL https://git.io/file-transfer | sh
+./transfer wet out/target/product/phhgsi*/system.img
 
+# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
