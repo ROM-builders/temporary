e882a3b96aeb517f6e777e43f8055561bd0d8590
hsx02 <hsx02self@gmail.com>
2022-03-02 08:19:12 +0530

    initial mi439 unified build

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/PixelExtended/manifest.git -b snow  -g default,-mips,-darwin,-notdefault
-git clone https://github.com/hsx02/Local-Manifests.git --depth 1 -b pex-12 .repo/local_manifests
+git clone https://github.com/hsx02/Local-Manifests.git --depth 1 -b pexmi439 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch aosp_pine-userdebug
+lunch aosp_mi439-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export PEX_BUILD_TYPE=OFFICIAL
 export TZ=Asia/Dhaka #put before last build command
@@ -15,3 +15,4 @@ mka bacon
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
 
 
+jp
