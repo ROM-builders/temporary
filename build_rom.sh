a9457fb3248920b5a1788a7107fe8f3e4387692f
ariffjenong <arifbuditantodablekk@gmail.com>
2022-03-02 08:54:04 +0700

    Build vanilla and test derfelot new kernel

--- build_rom.sh
+++ build_rom.sh
@@ -9,6 +9,6 @@ lunch nad_maple_dsds-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Jakarta #put before last build command
 make nad
-
+ 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
