993039b030c37a90b85f3fd8aa2ce1588b37c997
aayush-kumar-bm-ai <aayush.kumar@brightmoney.co>
2022-03-03 19:55:45 +0530

    Update local_manifest branch
    Signed-off-by: aayush-kumar-bm-ai <aayush.kumar@brightmoney.co>

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ArcaneOS/Arcane_manifest.git -b R -g default,-mips,-darwin,-notdefault
-git clone https://github.com/felapr1804/local_manifest.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/felapr1804/local_manifest.git --depth 1 -b raphael .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
