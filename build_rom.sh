6d90d7a543fe0c1e429fc36db6cc493766c87c3c
Akbarfr <akbareevnxx@gmail.com>
2022-03-05 03:13:58 +0700

    lighthouse-12.0

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 12.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/weslahmales/local_manifest.git --depth 1 -b crdroidandroid-12.0 .repo/local_manifests
+repo init -u https://github.com/lighthouse-os/manifest.git -b sailboat -g default,-mips,-darwin,-notdefault
+git clone https://github.com/weslahmales/local_manifest.git --depth 1 -b lighthouse-12.0 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_ulysse-userdebug
+lunch aosp_ulysse-userdebug
 export TZ=Asia/Jakarta  #put before last build command
-brunch ulysse
+make lighthouse
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
