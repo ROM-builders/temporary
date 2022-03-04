32129cb3236758f477ee92f05a7e4cddf57c2b49
WallBreaker2407 <92675483+WallBreaker2407@users.noreply.github.com>
2022-03-04 14:21:55 +0700

    initial build

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch lineage_X00TD-userdebug
 export TZ=Asia/Jakarta #put before last build command
-export BUILD_USERNAME=GeForce-RTX
+export BUILD_USERNAME=RTX
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
