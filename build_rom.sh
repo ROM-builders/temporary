5b43836afa0aca03ab7172e02bf9b874ccf6db0d
defaulter123 <58555570+defaulter123@users.noreply.github.com>
2022-03-04 15:47:01 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/ShapeShiftOS/android_manifest.git -b android_12 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/defaulter123/local_manifesst.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/defaulter123/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
