5115914b63d4ec0fbeafb155e40276ebb17155ed
Saran Thongplong <68327355+lolipuru@users.noreply.github.com>
2022-03-05 15:37:30 -0800

    prebuilt with lawnchair on gapps

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 export WITH_GAPPS=true
+export PREBUILT_LAWNCHAIR=true
 lunch aosp_perseus-userdebug
 export TZ=Asia/Bangkok #put before last build command
 make bacon
