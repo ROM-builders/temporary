ffd72a4eca610d1feb35fa08775637067d039ca7
T.B.S <86312158+Tiktodz@users.noreply.github.com>
2022-03-05 16:12:27 +0700

    top global fix 1.id
    lawack:D

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch lineage_X00T-userdebug
 export BUILD_USERNAME=tiktod
-export SELINUX_IGNORE_NEVERALLOWS=true
+export SKIP_ABI_CHECKS=true
 export TZ=Asia/Bangkok #put before last build command
 mka bacon
 
