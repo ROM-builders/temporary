98f8ba107b9f44b6e5f86c0511dfd42b7f4b740f
RasyidAlKautsar <rasyid2007alkautsar@gmail.com>
2022-03-05 16:36:07 +0700

    Source Fixed!

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 . build/envsetup.sh
 lunch cherish_mido-userdebug
 export BUILD_HOSTNAME=RasyidAlKautsar
-export CHERISH_VANILLA=true
+export BUILD_USER=CherishOS
 export TZ=Asia/Jakarta #put before last build command
 mka bacon
 
