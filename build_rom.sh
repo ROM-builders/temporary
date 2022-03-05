50efc7ad59f4c6b7d218094588cdca9c709ecee3
Jomar <94120477+Neutralxs@users.noreply.github.com>
2022-03-05 22:30:46 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init -u git://github.com/DerpFest-11/manifest.git -b 11
+repo init -u https://github.com/ShapeShiftOS/android_manifest -b android_12
 git clone https://github.com/Neutralxs/local_manifest --depth 1 .repo/local_manifests
-repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
+repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune
 
 # build rom [7]
 . build/envsetup.sh
-lunch derp_RMX2185-user
-mka derp
+lunch ssos_RMX2185-userdebug
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
