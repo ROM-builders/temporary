608dedcf0ed32f821d58dd04c32d3983b7af02e2
RasyidAlKautsar <rasyid2007alkautsar@gmail.com>
2022-03-07 04:06:37 +0700

    test with new stuff

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 . build/envsetup.sh
 lunch cherish_mido-user
 export BUILD_HOSTNAME=RasyidAlKautsar
-export BUILD_USER=RasyidAlKautsar
+export BUILD_USERNAME=RasyidAlKautsar
 export TZ=Asia/Jakarta #put before last build command
 mka bacon
 
