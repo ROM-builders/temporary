3ecdddabacf499e249edef0d954feed678ecaa8a
T.B.S <86312158+Tiktodz@users.noreply.github.com>
2022-03-06 10:03:51 +0700

    regen build

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
-# sync roms
+# sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-Pixelish/manifest -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Tiktodz/local_manifest.git --depth 1 -b xr .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build roms
+# build rom
 source build/envsetup.sh
 lunch aosp_X00TD-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
