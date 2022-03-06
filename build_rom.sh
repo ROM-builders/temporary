a69da18716fb6d2323a76c259e74b9697a4c0127
neonmicky <63160537+neonmicky@users.noreply.github.com>
2022-03-06 10:10:06 +0530

    Trigger Build

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/androi
 git clone https://github.com/neonmicky/local_manifest.git --depth 1 -b 12.0 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# buildrom
+# build rom
 source build/envsetup.sh
 lunch lineage_tissot-userdebug
 export TZ=Asia/Kolkata #put before last build command
