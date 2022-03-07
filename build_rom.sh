ae897857eba410fcd417c00c50121c706e3b21fd
Abhi2354 <100251020+Abhi2354@users.noreply.github.com>
2022-03-07 16:01:24 +0530

    fixed errors : olives cherish

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch cherish_olives-userdebug
+export RELAX_USES_LIBRARY_CHECK=true
 export TZ=Asia/Dhaka #put before last build command
 brunch olives
 
