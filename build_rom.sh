f1e9661073859627a630102cc1327c4bf7a19f6c
Anky <ankit8080dhanare@gmail.com>
2022-03-07 16:13:26 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch xdroid_phoenix-userdebug
-
 make xd -j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
