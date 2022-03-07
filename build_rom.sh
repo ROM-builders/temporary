12b4d596fd174c2e33659176680fd5eb6ba4730a
Ashish <97603141+JaswalAshish@users.noreply.github.com>
2022-03-07 12:32:56 +0530

    Fixed some errors again

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest.git -b spark -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b spark -g default,-mips,-darwin,-notdefault
 git clone https://github.com/JaswalAshish/local_manifest.git --depth 1 -b spark-mi439 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
