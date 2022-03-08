1e24a8c93cb9977f97e5be0e33ec83a7240c589a
Karan2354 <99638921+karan2354@users.noreply.github.com>
2022-03-08 06:41:52 +0530

    mi439 cherish vanilla

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/karan2354/local_manifest.git --depth 1 -b cherish .repo/local_manifests
+git clone https://github.com/karan2354/local_manifest.git --depth 1 -b cherish-12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
