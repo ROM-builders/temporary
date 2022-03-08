324ef7f9e27fc6d25e719d42f4c8a0d70f37ca4b
Salmonromdev45 <100950252+Salmonromdev45@users.noreply.github.com>
2022-03-08 08:02:31 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch evolution_$klteduos-userdebug
+lunch evolution_klteduos-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka evolution
 
