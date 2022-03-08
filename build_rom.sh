6b4b4c9f0dd46700cfb09f4e260ee10b3a712f4c
Anky <ankit8080dhanare@gmail.com>
2022-03-09 00:36:29 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,7 @@
 repo init --depth=1 --no-repo-verify -u git://github.com/xdroid-oss/xd_manifest -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Anky-AD/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-rm -rf hardware/xia
+
 # build rom and lunch
 source build/envsetup.sh
 lunch xdroid_phoenix-userdebug
