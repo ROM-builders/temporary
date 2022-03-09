da3ed5f94dce7cc5457ef4c2d7624b8de2b2f3a6
Matheus Souza <33396386+matheucomth@users.noreply.github.com>
2022-03-09 00:54:28 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch aosp_tulip-user
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=America/Sao_Paulo #update localmanifest
 mka bacon
 
