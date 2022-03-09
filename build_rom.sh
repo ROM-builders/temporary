1280cccad43511d5b78fd6b60263eebb8503654f
Karan2354 <99638921+karan2354@users.noreply.github.com>
2022-03-09 09:27:41 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lighthouse_mi439-userdebug
+lunch lighthouse_mi439-userdebug 
 export TZ=Asia/Dhaka #put before last build command
 make lighthouse
 
