5a2a9e61d9303529e16161cb597bb80ca58b1df8
Ritvik <99066143+Ritvik6969@users.noreply.github.com>
2022-03-10 14:49:23 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExtended/manifest -b snow -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Ritvik6969/local_manifest.git --depth 1 -b EvolutionX .repo/local_manifests
+git clone https://github.com/Ritvik6969/local_manifest.git --depth 1 -b Pixel-Extended .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
