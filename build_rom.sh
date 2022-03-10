b65250bc34144ee5dcdb379adc4199a7ee6ed3e4
Anant Ahir <99502637+anantinfinity@users.noreply.github.com>
2022-03-11 01:38:19 +0530

    xd-anant-beryl

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/ArrowOS/android_manifest.git -b arrow-12.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/anantinfinity/local_manifest.git --depth 1 -b arrow .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/xdroid-oss/xd_manifest.git -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/anantinfinity/local_manifest.git --depth 1 -b xd .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch arrow_beryllium-userdebug
+lunch xdroid_beryllium-userdebug
 export TZ=Asia/Kolkata #put before last build command
 export BUILD_HOSTNAME=anant78
-m bacon
+make xd
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
