97326629781537ab261ee0b0caa4cc23c4338a9b
w1zzyl <96798575+w1zzyl@users.noreply.github.com>
2022-03-10 17:14:56 +0100

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/Project-Elixir/manifest.git -b snow -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/PixysOS/manifest -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/w1zzyl/local_manifest.git --depth 1 -b vayu-elixir .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch aosp_vayu-userdebug
-export BUILD_USERNAME=w1zzyl
+lunch pixys_vayu-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
-export ALLOW_MISSING_DEPENDENCIES=true
-export TZ=Asia/Dhaka #put before last build command
-mka bacon
+export TZ=Europe/Madrid #put before last build command
+make pixys
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
