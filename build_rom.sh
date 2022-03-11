70667a2da666726f351304e09eae4991e7ddfa1a
K <82253361+HoleDroid@users.noreply.github.com>
2022-03-11 20:18:17 +0700

    initial build los

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_X00T-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Tokyo #put before last build command
 mka bacon
 
