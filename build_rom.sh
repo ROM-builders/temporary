ab5cb5fc67c408831dca662ad1bfd2dac26134e7
T.B.S <86312158+Tiktodz@users.noreply.github.com>
2022-03-11 09:57:30 +0700

    test build

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
-# sync roms
+# sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Tiktodz/local_manifest.git --depth 1 -b cherish .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build roms
+# build rom
 source build/envsetup.sh
 lunch cherish_X00TD-userdebug
 export RELAX_USES_LIBRARY_CHECK=true
