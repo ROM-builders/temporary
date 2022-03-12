77f7d38d77d9e06d9e82f4328aa99fefe39ed2a4
Anky <ankit8080dhanare@gmail.com>
2022-03-12 20:22:24 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com:AOSP-Krypton/manifest.git -b A12 default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/AOSP-Krypton/manifest.git -b A12 default,-mips,-darwin,-notdefault
 git clone https://github.com/Anky-AD/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
