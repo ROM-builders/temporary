7068b62a61ea1cb86884b8d6e619ef9e78977d54
Aryan Karan <aryankaran28022004@gmail.com>
2022-03-12 09:10:07 +0530

    Havoc-OS for mi8937

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Havoc-OS/android_manifest -b eleven -g default,-mips,-darwin,-notdefault
-git clone https://github.com/aryan-karan/local_manifests --depth 1 -b havoc-rolex .repo/local_manifests
+git clone https://github.com/aryankaran/local_manifests-1 --depth 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 export TZ=Asia/Kolkata
-brunch havoc_rolex-user
+brunch lineage_mi8937-user
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
