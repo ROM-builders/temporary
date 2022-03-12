ef836fec80101945a329f904263e1221489cf578
Snowman2409 <93831265+Snowman2409@users.noreply.github.com>
2022-03-12 10:34:33 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u -u https://github.com/PixelExperience/manifest -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Snowman2409/local-manifest --depth=1 -b main .repo/local_manifests
-
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 # build rom
 source build/envsetup.sh
