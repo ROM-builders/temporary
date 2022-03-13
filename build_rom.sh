ac5b91287e5d17892f3a1f0c51f1e79a7c6f7088
Surabhil Ssk <42171987+surabhilssk@users.noreply.github.com>
2022-03-13 21:16:15 +0530

    vince-awakenOS-surabhilssk
    Build awakenOS for Redmi 5 Plus(vince)

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/Project-Awaken/android_manifest -b 12 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/surabhilssk/local_manifest --depth 1 -b master .repo/local_manifests
-repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
+repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune
 
 # build rom
 source build/envsetup.sh
 lunch awaken_vince-userdebug
 export TZ=Asia/Dhaka #put before last build command
-make bacon -j$(nproc --all)
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
