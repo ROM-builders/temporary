e0b725d13bd5441b7c625051760f33ae3fb244c8
Surabhil Ssk <42171987+surabhilssk@users.noreply.github.com>
2022-03-13 21:32:53 +0530

    vince-awakenOS-surabhilssk

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/Project-Awaken/android_manifest -b 12 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/surabhilssk/local_manifest --depth 1 -b master .repo/local_manifests
-repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
