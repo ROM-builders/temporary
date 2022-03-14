48bca53cae6c03aa3e6b640eaeb924298613f0ae
NRanjan-17 <97696537+NRanjan-17@users.noreply.github.com>
2022-03-14 18:50:20 +0530

    mido-Evolution X snow-NRanjan-17

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,15 @@
 # sync rom
-repo init -u https://github.com/PixelExperience/manifest -b twelve
-git clone https://github.com/NRanjan-17/Android-Building.git --depth 1 -b main .repo/local_manifests
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b snow -g default,-mips,-darwin,-notdefault
+git clone https://github.com/RasyidAlKautsar/local_manifest.git --depth 1 -b snow .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
-lunch aosp_mido-user
-export TZ=Asia/Delhi #put before last build command
-mka bacon -jX
+. build/envsetup.sh
+lunch evolution_mido-userdebug
+export SKIP_ABI_CHECKS=true
+export TZ=Asia/Dhaka #put before last build command
+mka evolution
 
-# upload rom
+
+# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
