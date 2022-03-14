af00f4072277cdde3e5dc012df3f842dc081ad02
Surabhil Ssk <42171987+surabhilssk@users.noreply.github.com>
2022-03-14 21:14:14 +0530

    vince-awakenOS-surabhilssk
    Build awakenOS for Redmi 5 Plus(vince)

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/Project-Awaken/android_manifest -b 12 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/surabhilssk/local_manifest/tree/awaken-12 --depth 1 -b master .repo/local_manifests
+git clone https://github.com/surabhilssk/local_manifest/tree/awaken-12 --depth 1 -b awaken-12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch awaken_<devicecodename>-userdebug
+lunch awaken_vince-userdebug
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
