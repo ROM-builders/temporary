948c8d7eef2ec759051b550786a9ed6baa17d1e7
AzurE-007 <100161001+AzurE-007@users.noreply.github.com>
2022-03-14 13:51:55 +0530

    Fix Manifest

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ProjectRadiant/manifest -b twelve-L -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/AzurE-007/manifest -b twelve-L -g default,-mips,-darwin,-notdefault
 git clone https://github.com/AzurE-007/local_manifests.git --depth 1 -b radiant .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
