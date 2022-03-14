67c2cd831d09e682c4d35df3e543a157322e4f7d
NRanjan-17 <97696537+NRanjan-17@users.noreply.github.com>
2022-03-14 18:32:46 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init -u https://github.com/Evolution-X/manifest -b snow
+repo init -u https://github.com/PixelExperience/manifest -b twelve
 git clone https://github.com/NRanjan-17/Android-Building.git --depth 1 -b main .repo/local_manifests
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
 # build rom
 source build/envsetup.sh
-lunch evolution_mido-user
+lunch aosp_mido-user
 export TZ=Asia/Delhi #put before last build command
-mka evolution
+mka bacon -jX
 
 # upload rom
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
