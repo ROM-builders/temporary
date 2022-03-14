fafa499cc227255bf6d888571470295067a5bec5
FSOC137Y <39610904+vickycena1@users.noreply.github.com>
2022-03-14 11:28:13 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,7 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/projectarcana-aosp/manifest -b 12.x -g default,-mips,-darwin,-notdefault
 git clone https://github.com/vickycena1/local_manifest.git --depth 1 -b arcana .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
+rm -rf packages/apps/bluetooth
 # build rom
 source build/envsetup.sh
 lunch aosp_juice-userdebug
