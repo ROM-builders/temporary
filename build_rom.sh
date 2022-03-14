dcb63ce1ac7c6941482912ce6a59baf35c3b16e0
Abhi2354 <100251020+Abhi2354@users.noreply.github.com>
2022-03-14 14:47:19 +0530

    spark os test build

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch spark_olives-userdebug 
+lunch spark_olives-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
