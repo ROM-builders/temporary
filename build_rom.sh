74ac9ccd9bded71f3778a91d554575bd149b09f8
Ritvik <99066143+Ritvik6969@users.noreply.github.com>
2022-03-16 00:21:11 +0530

    Xdroid_Aosp-Vince-Ritvik6969

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch xdroid_vince-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
-export WITH_GAPPS=true
 export TZ=Asia/Kolkata #put before last build command
 make xd
 
