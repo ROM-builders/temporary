1dd6458a4cca5a6590ad059237d145e9d3c46466
Mayur ◢◤ <flynryder427@gmail.com>
2022-03-15 11:55:47 +0530

    Wrong branch

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-CAF/xd_manifest -b twelve -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-CAF/xd_manifest -b eleven -g default,-mips,-darwin,-notdefault
 git clone https://github.com/xenxynon/manifest.git --depth=1 -b xd .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
