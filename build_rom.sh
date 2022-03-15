14828868ad1b39a33f1dd0d621ffd45790695cec
Ansh <singhansh64321@gmail.com>
2022-03-15 19:20:04 +0530

    Build Userdebug

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch aosp_santoni-user
+lunch aosp_santoni-userdebug 
 export TZ=Asia/Mumbai #put before last build command
 make bacon 
 
