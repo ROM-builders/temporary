fc2d2da7ebb42bf663507b6a00f56ad2b3f517bf
Azis Maulana <71966811+RAYDENFLY@users.noreply.github.com>
2022-03-16 02:30:24 +0700

    vince: Fluid

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/WeebProjekt/platform_manifest -b reborn -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/Project-Fluid/manifest.git -b fluid-12 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Yurika-Projects/Local-Manifests.git --depth 1 -b master .repo/local_manifests
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+repo sync --force-sync --no-tags --no-clone-bundle -j$(nproc --all)
 
 # build rom
 source build/envsetup.sh
-lunch weeb_vince-userdebug
+lunch fluid_device-userdebug
 export TZ=Asia/Tokyo #put before last build command
-make weeb-prod -j$(nproc --all)
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
