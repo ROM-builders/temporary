ce433bdc214c3c5bad9cfc6d9614fe7a98572550
zainullahk <72628804+zainullahk@users.noreply.github.com>
2022-03-15 18:22:29 +0400

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/zainullahk/manifest-pixelos -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/PixelOS-Pixelish/manifest.git --depth 1 -b vayu_pixelos .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/PixelExperience/manifest.git -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/sanjeevstunner/Manifest.git --depth 1 -b vayu_aosp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom 
 source build/envsetup.sh
 lunch aosp_vayu-user
 export TZ=Asia/Kolkata #put before last build command
