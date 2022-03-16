6acd113d2000353c6e1e3c234b7885913d9b4e31
Azis Maulana <71966811+RAYDENFLY@users.noreply.github.com>
2022-03-16 16:15:36 +0700

    vince-Fluid-Yurika-Projects

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/Project-Fluid/manifest.git -b fluid-12 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Yurika-Projects/Local-Manifests.git --depth 1 -b fluid-12 .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+git clone https://github.com/Yurika-Projects/local-manifests.git --depth 1 -b fluid-12 .repo/local_manifests
+repo sync --force-sync --no-tags --no-clone-bundle -j$(nproc --all)
 
 # build rom
 source build/envsetup.sh
