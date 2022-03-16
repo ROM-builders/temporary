eb153370cbf01432c3d0c5d14b7b1bef3aaf58ce
Karan2354 <99638921+karan2354@users.noreply.github.com>
2022-03-16 17:38:46 +0530

    skip abi checks

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom 
 source build/envsetup.sh
 lunch octavi_mi439-userdebug
+export SKIP_ABI_CHECKS=true
 export TZ=Asia/Dhaka #put before last build command
 brunch mi439
 
