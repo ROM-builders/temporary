63ee5cc305625edfac83b1bd21a1b09fe7f8e1c3
Coptan⁹⁹ <omarcoptan9@gmail.com>
2022-03-16 11:23:28 +0200

    Force Sync😑

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,6 @@ repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-Pixelish/mani
 git clone https://github.com/OmarAlCoptan/local_manifest.git --depth 1 -b PixelOS .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-
 # build rom
 source build/envsetup.sh
 lunch aosp_lavender-userdebug
