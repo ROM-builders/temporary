65044e05909ce86d5cefc8f2ec92e29f4ff86ee0
NRanjan-17 <97696537+NRanjan-17@users.noreply.github.com>
2022-03-16 19:07:17 +0530

    Havoc os initial build

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/NRanjan-17/Android-Building.git --depth 1 -b main .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo init --depth=1 --no-repo-verify -u git://github.com/Havoc-OS/android_manifest.git -b twelve -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/NRanjan-17/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
 # build rom
 source build/envsetup.sh
 lunch aosp_mido-userdebug
 export SKIP_ABI_CHECKS=true
 export TZ=Asia/Delhi #put before last build command
-mka bacon
+brunch
 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
