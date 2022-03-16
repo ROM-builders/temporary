e196b92680a13e199ff5169dffb492439b8df722
NRanjan-17 <97696537+NRanjan-17@users.noreply.github.com>
2022-03-16 19:47:15 +0530

    Havoc OS Initial Build

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
-repo init -u https://github.com/Havoc-OS/android_manifest.git -b twelve
+repo init --depth=1 --no-repo-verify -u https://github.com/Havoc-OS/android_manifest.git -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/NRanjan-17/local_manifest.git --depth 1 -b main .repo/local_manifests
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
