e056bc5bddfd88cd63ad63e7e6add2739a14c4a5
Coptan⁹⁹ <omarcoptan9@gmail.com>
2022-03-16 21:01:42 +0200

    Please Build!😑

--- build_rom.sh
+++ build_rom.sh
@@ -8,7 +8,6 @@ source build/envsetup.sh
 lunch aosp_lavender-userdebug
 export TZ=Africa/Egypt #put before last build command
 mka bacon
- 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
