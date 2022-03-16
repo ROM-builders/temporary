793d3345c058ca6236e529e5d81cb53fb306a641
aminuddinihsan20 <47616229+aminuddinihsan20@users.noreply.github.com>
2022-03-16 21:27:21 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExtended/manifest -b snow -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b 12 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/aminuddinihsan20/local_manifests --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_surya-userdebug
+lunch awaken_surya-userdebug
 export TZ=Asia/Bandung #put before last build command
 mka bacon
 
