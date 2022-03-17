066464c3532cd247860d986d5c16ccce5d498cbd
moonitime <97696888+moonitime@users.noreply.github.com>
2022-03-18 02:18:20 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/Evolution-X/manifest -b twelve -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/Evolution-X/manifest -b snow -g default,-mips,-darwin,-notdefault
 git clone https://github.com/moonitime/local_manifest.git --depth 1 -b main .repo/local_manifests
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
