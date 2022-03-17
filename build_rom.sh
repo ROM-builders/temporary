aebd08526559b98408c40cb10e11ad9fe26edd06
T.B.S <86312158+Tiktodz@users.noreply.github.com>
2022-03-18 06:39:34 +0700

    trigger builld

--- build_rom.sh
+++ build_rom.sh
@@ -7,6 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch lineage_X00T-userdebug
 export BUILD_USERNAME=tiktod
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Tokyo #put before last build command
 mka bacon
 
