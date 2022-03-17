bfd99d3dae7d96f578c7342ebe5c15f8169711c5
Surabhil Ssk <42171987+surabhilssk@users.noreply.github.com>
2022-03-17 18:01:32 +0530

    changed git to https

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/Project-Awaken/android_manifest -b 12 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b 12 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/surabhilssk/local_manifest/tree/awaken-12 --depth 1 -b awaken-12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
