0539d8a89853072564d96f627b4ffc15ac8f68ec
Matheus Souza <33396386+matheucomth@users.noreply.github.com>
2022-03-17 11:20:01 -0300

    Create build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch xdroid_tulip-userdebug
+lunch aosp_tulip-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export BUILD_BROKEN_DUP_RULES=true
 export TZ=America/Sao_Paulo #update localmanifest
