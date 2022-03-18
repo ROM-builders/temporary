c941580db7b2c2addc24087accb2571d9c0cc644
Onkar singh <80300768+onkarsinghsahil@users.noreply.github.com>
2022-03-18 22:30:25 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,10 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch aosp_mido-userdebug
+export WITH_GAPPS=false
+export SKIP_ABI_CHECKS=true
+export BUILD_USERNAME=Onkar
+export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Dhaka #put before last build command
 mka aex
 
