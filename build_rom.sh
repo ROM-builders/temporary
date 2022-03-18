bc605bd0a8fb78b0f1ce769a7dfa6231d5032fe2
Onkar singh <80300768+onkarsinghsahil@users.noreply.github.com>
2022-03-18 13:31:27 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/xdroid-CAF/xd_manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/onkarsinghsahil/local-manifest.git --depth 1 -b Xdroid .repo/local_manifest
+repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b snow -g default,-mips,-darwin,-notdefault
+git clone https://github.com/onkarsinghsahil/local-manifest.git --depth 1 -b evolution-x .repo/local_manifest
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
-lunch xdroid_mido-userdebug
+. build/envsetup.sh
+lunch evolution_mido-userdebug
+export SKIP_ABI_CHECKS=true
 export TZ=Asia/Dhaka #put before last build command
-mka xdroid
+mka evolution
+
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
