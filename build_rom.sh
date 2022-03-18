d7bc41614e14d0b8a89796a85753df468ae7dd06
NRanjan-17 <97696537+NRanjan-17@users.noreply.github.com>
2022-03-18 07:15:52 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch evolution_mido-userdebug
-export SKIP_ABI_CHECKS=true
 export TZ=Asia/Delhi #put before last build command
 mka evolution 
 
