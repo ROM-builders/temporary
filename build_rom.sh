432b1d60b4bce2ba3e67561432c8f4aabd08cbad
NRanjan-17 <97696537+NRanjan-17@users.noreply.github.com>
2022-03-19 14:50:12 +0530

    Evolution x snow mido

--- build_rom.sh
+++ build_rom.sh
@@ -8,6 +8,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 lunch evolution_mido-userdebug
 export TZ=Asia/Delhi #put before last build command
 BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
+BUILD_BROKEN_DUP_RULES := true
 mka evolution
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
