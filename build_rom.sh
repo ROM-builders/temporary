68a30fc02e26dbd059b8728f357f24a6687f512c
Karan2354 <99638921+karan2354@users.noreply.github.com>
2022-03-20 04:02:06 +0530

    vanilla build

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch streak_mi439-userdebug
+export TARGET_BUILD_GAPPS=false
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
