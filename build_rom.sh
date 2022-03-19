b51f38b2241c77da79630fbe84af67c1feebc950
Azis Maulana <71966811+RAYDENFLY@users.noreply.github.com>
2022-03-20 02:21:42 +0700

    vince-NusantaraProject-ROM-RAYDENFLY

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/exthmui/android.git -b exthm-11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Yurika-Projects/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/NusantaraProject-ROM/android_manifest.git -b 12 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/RAYDENFLY/Local-Manifests.git --depth 1 -b nad .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch exthm_vince-userdebug
+lunch nad_vince-buildtype
 export TZ=Asia/Tokyo #put before last build command
-mka bacon
+make nad
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
