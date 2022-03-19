71e2dae6acde30b1523f828e3d1704fe292dd54a
GhostMaster69-dev <rathore6375@gmail.com>
2022-03-19 17:03:56 +0530

    Build PixelExtended
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b twelve -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExtended/manifest -b snow -g default,-mips,-darwin,-notdefault
 git clone https://github.com/GhostMaster69-dev/local_manifest.git --depth 1 -b pixel-12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
