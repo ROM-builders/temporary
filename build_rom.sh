781b0123d07552eb53fbe5e9fd4889733aac4af9
DarkMine <mr_white_mr_white@hotmail.com>
2022-03-19 16:46:20 +0300

    tissot-crdroidandroid-12-DarkMine

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/DarkMine/local_manifest.git --depth 1 -b 12.0 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 12.0 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/DarkMine/local_manifest.git --depth 1 -b cr .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build  rom
 source build/envsetup.sh
-lunch cherish_tissot-userdebug
+lunch lineage_tissot-userdebug
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
