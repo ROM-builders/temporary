26e45ee400b83af28fcaf53d8d28b7ac8e7ad390
Subhendu-droid <55965922+Subhendu-droid@users.noreply.github.com>
2022-03-20 19:02:02 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/Project-Xtended/manifest.git -b xs -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/PixelExperience/manifest -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Subhendu-droid/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build romm
+# build rom
 source build/envsetup.sh
-lunch xtended_X01AD-userdebug
-export TZ=Asia/Kolkata #put before last build command
+lunch aosp_X01AD-user
+export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
