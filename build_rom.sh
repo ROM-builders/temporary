f04743c18c9ccccf87e90d965da6de594199a5b2
motoonepower <101825685+motoonepower@users.noreply.github.com>
2022-03-20 12:30:37 +0530

    aex

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/motoonepower/local-manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/AospExtended/manifest -b 12.x -g default,-mips,-darwin,-notdefault
+git clone https://github.com/motoonepower/local-manifest.git --depth 1 -b aex .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
