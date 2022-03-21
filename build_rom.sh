c87633708de4ddd574ec2c04ac85f6b17f323e5c
Pere <96798575+w1zzyl@users.noreply.github.com>
2022-03-21 16:44:46 +0100

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,17 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-Pixelish/manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/w1zzyl/local_manifest.git --depth 1 -b vayu-pixelos .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/w1zzylvayustuff/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
 lunch aosp_vayu-userdebug
+export KBUILD_BUILD_USER=w1zz
+export KBUILD_BUILD_HOST=w1zzyl
+export BUILD_USERNAME=w1zz
+export BUILD_HOSTNAME=w1zzyl
 export TZ=Europe/Madrid #put before last build command
-make bacon
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
