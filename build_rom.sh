ed1023525167a78ca665f2ce7b0602b667f58112
Onkar singh <80300768+onkarsinghsahil@users.noreply.github.com>
2022-03-21 23:43:44 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch aosp_mido-userdebug
 export BUILD_USERNAME=onkarsinghsahil
-export ALLOW_MISSING_DEPENDENCIES=true
+
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
