e0924f8d9facbe6cd814e98606ca166ea0bbcfcb
NRanjan-17 <97696537+NRanjan-17@users.noreply.github.com>
2022-03-21 23:30:43 +0530

    Pixel Experience 12.1 Initial Build Redmi Note 4

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch aosp_mido-userdebug
+BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
 export TZ=Asia/Delhi #put before last build command
 mka bacon
 
