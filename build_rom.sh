0580a2df9f654ea2061fae2b5ab41496bc2ef7db
motoonepower <101825685+motoonepower@users.noreply.github.com>
2022-03-22 20:33:07 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manif
 git clone https://github.com/motoonepower/local-manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build roms
+# build rom
 source build/envsetup.sh
 lunch aosp_chef-userdebug
 export TZ=Asia/Dhaka #put before last build command
