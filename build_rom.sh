69a8735df150488ca8c5f1b1dbe85f8853091d1f
NRanjan-17 <97696537+NRanjan-17@users.noreply.github.com>
2022-03-22 12:33:15 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch aosp_mido-userdebug
+lunch xiaomi_mido-userdebug
 BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
 export TZ=Asia/Delhi #put before last build command
 mka bacon
