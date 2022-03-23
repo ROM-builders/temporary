fe912a5e959d8fd2d8406b643984a84238a14e7e
laleeroy <gabwielcastwo@gmail.com>
2022-03-23 09:15:34 +0800

    User build
    Signed-off-by: laleeroy <gabwielcastwo@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/projectarcana-aosp/manifest -b 12.x -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/projectarcana-aosp/manifest -b 12.x -g default,-mips,-darwin,-notdefault
 git clone https://github.com/laleeroy/local_manifest.git --depth 1 -b ulysse .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_ulysse-userdebug
+lunch aosp_ulysse-user
 export TZ=Asia/Manila #put before last build command
 make bacon
 
