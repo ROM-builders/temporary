8eac29bbdf14e57cbb6e4692aa081d693ea69c0e
alifianazhar <83335389+alifianazhar@users.noreply.github.com>
2022-03-23 07:39:30 +0700

    nos

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,12 @@
 # sync rom
-repo init -u https://gitlab.com/CalyxOS/platform_manifest.git -b android10
+repo init -u https://github.com/NusantaraProject-ROM/android_manifest -b 10
 git clone https://github.com/alifianazhar/local_manifest.git --depth 1 -b main .repo/local_manifests
-repo sync -j8
+repo sync -c --force-sync --no-tags --no-clone-bundle
 
 # build rom
-source build/envsetup.sh
-lunch calyx_riva-user
-export TZ=Asia/Dhaka #put before last build command
-m
+.build/envsetup.sh
+lunch nad_riva-buildtype
+mka nad -j
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
