81059cd92d84e05506ed70d15e8133492b313d15
MadmoudRMX2020 <98421074+MadmoudRMX2020@users.noreply.github.com>
2022-03-23 11:37:30 +0200

    RMX2020: ShapeShiftOS.

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/ShapeShiftOS/android_manifest.git -b android_12 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/MadmoudRMX2020/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-export ALLOW_MISSING_DEPENDENCIES=true
-export SELINUX_IGNORE_NEVERALLOWS=true
 source build/envsetup.sh
+lunch ssos_RMX2020-eng
 export TZ=Asia/Dhaka #put before last build command
-brunch lineage_RMX2020-eng
+make bacon
 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
