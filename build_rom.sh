914692e2aaa66dc97d31be8c1719f1ed0bc75323
alifianazhar <83335389+alifianazhar@users.noreply.github.com>
2022-03-23 07:52:06 +0700

    riva-calyxos-alifianazhar

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,13 @@
 # sync rom
-repo init -u https://github.com/NusantaraProject-ROM/android_manifest -b 10
-git clone https://github.com/alifianazhar/local_manifest.git --depth 1 -b main .repo/local_manifests
-repo sync -c --force-sync --no-tags --no-clone-bundle
+repo init --depth=1 --no-repo-verify -uhttps://gitlab.com/CalyxOS/platform_manifest.git -b android10 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/alifianazhar/Local-Manifests.git --depth 1 -b main .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-.build/envsetup.sh
-lunch nad_riva-buildtype
-mka nad -j
+source build/envsetup.sh
+lunch calyx_riva-user
+export TZ=Asia/Dhaka #put before last build command
+mka calyx
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
