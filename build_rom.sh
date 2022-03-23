1fb4ffd43fa8eab3f5fc5d75600977c2c6fe3741
Rishawn <70256146+Stealth1226@users.noreply.github.com>
2022-03-23 14:26:23 +0530

    switch to https for repo init

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 -u git://github.com/lighthouse-os/manifest.git -b sailboat_L1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 -u https://github.com/lighthouse-os/manifest.git -b sailboat_L1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Stealth1226/local_manifest --depth 1 -b sailboat-miatoll .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
