379cb3844ff311ef68ac71e5c0a2d1c698a225b9
alifianazhar <83335389+alifianazhar@users.noreply.github.com>
2022-03-23 13:02:37 +0700

    graphene

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://gitlab.com/CalyxOS/platform_manifest -b android12 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/GrapheneOS/platform_manifest -b 12.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/alifianazhar/Local-Manifests --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch calyx_riva-user
-export TZ=Asia/Dhaka #put before last build command
-mka calyx
+lunch graphene_riva-user
+export TZ=Asia/Jakarta #put before last build command
+mka graphene
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
