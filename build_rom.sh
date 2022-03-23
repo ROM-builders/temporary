06d2f73d07d802d13613eaaa17a7929b34c31549
ariffjenong <arifbuditantodablekk@gmail.com>
2022-03-23 18:43:43 +0700

    retry

--- build_rom.sh
+++ build_rom.sh
@@ -7,6 +7,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch lineage_maple_dsds-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
+export BUILD_HOSTNAME=ArifJeNong
+export BUILD_USERNAME=ArifJeNong
 export TZ=Asia/Jakarta #put before last build command
 make bacon
 
