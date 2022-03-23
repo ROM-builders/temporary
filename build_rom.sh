359829119be78f78639278dc6ab3c922e535129f
Prashant <93838092+ImPrashantt@users.noreply.github.com>
2022-03-23 19:59:55 +0530

    skip abi checks

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch octavi_lavender-userdebug
-export SELINUX_IGNORE_NEVERALLOWS := true
+export SELINUX_IGNORE_NEVERALLOWS=true
+export SKIP_ABI_CHECKS=true
 export TZ=Asia/Dhaka #put before last build command
 brunch lavender
 
