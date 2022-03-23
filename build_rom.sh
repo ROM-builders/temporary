6689a769e549b1a504a49979c7f02bddc7b98451
alifianazhar <83335389+alifianazhar@users.noreply.github.com>
2022-03-23 07:14:46 +0700

    riva-CalyxOS-alifianazhar
    ganti ke gitlab

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/CalyxOS/platform_manifest.git -b android10 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://gitlab.com/CalyxOS/platform_manifest.git -b android10 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/alifianazhar/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
