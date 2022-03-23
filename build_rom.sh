65765631be800ed1690eeed3d23b64299f129f81
Abhi2354 <100251020+Abhi2354@users.noreply.github.com>
2022-03-23 21:53:13 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/AOSPK/manifest -b twe
 git clone https://github.com/Abhi2354/local_manifest.git --depth 1 -b aospk .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom 
 source build/envsetup.sh
 lunch aosp_olives-userdebug
 export TZ=Asia/Dhaka #put before last build command
