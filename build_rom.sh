d6077282d48bd4d8bb9fb41037422255110936d9
NRanjan-17 <97696537+NRanjan-17@users.noreply.github.com>
2022-03-24 19:25:05 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,8 +7,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch arrow_mido-userdebug
 export ARROW_GAPPS=true
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
-export BUILD_BROKEN_DUP_RULES=true
 export TZ=Asia/Delhi #put before last build command
 m bacon
 
