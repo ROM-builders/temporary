3a9d4a3488ced8ccc292ad2db331472428fc5d6e
Aryan Karan <aryankaran28022004@gmail.com>
2022-03-24 19:58:38 +0530

    Test build

--- build_rom.sh
+++ build_rom.sh
@@ -10,7 +10,7 @@ export KBUILD_BUILD_USER=aryan
 export KBUILD_BUILD_HOST=aryan-HP-G7-notebook
 export BUILD_USERNAME=aryan
 export BUILD_HOSTNAME=aryan-HP-G7-notebook-PC
-brunch havoc_rolex-user
+brunch havoc_rolex-eng
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
