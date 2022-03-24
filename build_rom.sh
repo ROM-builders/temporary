9125fe5ae0608bff3c7a3a05e9eb42f61a8deb22
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2022-03-24 20:54:48 +0530

    Opps Mybad

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ForkLineageOS -b lineage-19.0 -g default,-mips,-darwin,-notdefault
-git clone rushiranpise/local_manifest --depth 1 -b los .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ForkLineageOS/android.git -b lineage-19.0 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/rushiranpise/local_manifest --depth 1 -b los .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
