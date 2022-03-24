4536a7bba49fb136eb509d6b2dc3936735bbbd6d
S4MUR411 <59146493+S4MUR411@users.noreply.github.com>
2022-03-25 00:22:50 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch aosp_laurel_sprout-userdebug
+lunch lineage_laurel_sprout-userdebug
 export SKIP_ABI_CHECKS=true
 export TZ=Asia/Dhaka
 mka bacon
