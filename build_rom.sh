6f70ace36653c17cb6132123ed62d4b0aa6cc62a
Monu <56600996+monu70152@users.noreply.github.com>
2022-03-25 14:52:01 +0530

    Revert "update build_rom.sh"
    This reverts commit 88ac281347ced18f5c9c624531e9f475f1865e3f.

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,16 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b snow -g default,-mips,-darwin,-notdefault
-git clone https://github.com/monu70152/local_manifest.git --depth 1 -b evo .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/projectarcana-aosp/manifest.git -b 12.x -g default,-mips,-darwin,-notdefault
+git clone https://github.com/monu70152/local_manifest.git --depth 1 -b arcana .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 #23
 # build rom 
 source build/envsetup.sh
-lunch evolution_X00TD-userdebug
+lunch aosp_X00TD-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Kolkata #put before last build commad
-mka evolution
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
