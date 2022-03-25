2f7f0784cc926b6845faca29e0ad3e4164418036
WolfAURman <93985232+WolfAURman@users.noreply.github.com>
2022-03-26 04:58:19 +0600

    lava-crdroid-WolfAURman
    build crdroid for lava

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify repo init -u git://github.com/LineageOS/android.git -b lineage-19.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/WolfAURman/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify repo init -u git://github.com/crdroidandroid/android.git -b 11.0 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/WolfAURman/local_manifest.git --depth 1 -b crdroid7.x-mt6768 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
