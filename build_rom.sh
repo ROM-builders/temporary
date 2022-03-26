a95e1e839ca432284578a10e149ec80f1be9b35b
NRanjan-17 <97696537+NRanjan-17@users.noreply.github.com>
2022-03-26 07:46:08 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch cherish_mido-userdebug
+export WITH_GAPPS=true
 export TZ=Asia/Delhi #put before last build command
 brunch mido
 
