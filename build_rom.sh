e23b4aea2fed1fd7a0d6125911e79cc1d012b332
Onkar singh <80300768+onkarsinghsahil@users.noreply.github.com>
2022-03-26 16:04:39 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/onkarsinghsahil/local-manifest.git --depth 1 -b main .repo/local_manifest
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/onkarsinghsahil/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_mido-userdebug
+lunch lineage_mido-userdebug
 export BUILD_USERNAME=onkarsinghsahil
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
