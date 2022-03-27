1f8bf909dbdf93af148b03822299d164c0a22fbc
Kendras056 <88943971+Kendras056@users.noreply.github.com>
2022-03-27 13:39:08 +0700

    initial blaze

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest
 git clone https://github.com/Kendras056/local_manifests.git --depth 1 -b 12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build roms
 source build/envsetup.sh
 lunch blaze_X00TD-userdebug
 export TZ=Europe/Moscow #put before last build command
