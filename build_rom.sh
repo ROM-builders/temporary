0444741bffa5da362af1c26482c495af574e172b
Dev786s <85985141+Dev786s@users.noreply.github.com>
2022-03-26 23:47:26 -0700

    FIX ROM SYNC

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ProjectSakura/android.git -b 12 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Dev786s/local-manifest.git --depth 1 -b main .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync --current-branch --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune -j `nproc`
 
 # build rom
 source build/envsetup.sh
