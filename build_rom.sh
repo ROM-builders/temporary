24f8d8acbf73ed9be80c42b7ed7aca7068253b03
NRanjan-17 <97696537+NRanjan-17@users.noreply.github.com>
2022-03-27 05:59:44 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b twelve-one -g default,-mips,-darwin,-notdefault
 git clone https://github.com/NRanjan-17/local_manifest.git --depth 1 -b Cherish-OS-12.1 .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 -j1 --fail-fast
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
