6ac07f9b830ca08d7f75de8dd87acb048d0ae0be
Onkar singh <80300768+onkarsinghsahil@users.noreply.github.com>
2022-03-27 14:57:42 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/onkarsinghsahil/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixysOS/manifest -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/onkarsinghsahil/local_manifest.git --depth 1 -b pix .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_mido-userdebug
+lunch pixys_mido-userdebug
 export BUILD_USERNAME=onkarsinghsahil
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
