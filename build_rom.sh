6bae933190e0efc1de5a15e82bc440fead1a7430
Monu <56600996+monu70152@users.noreply.github.com>
2022-03-27 07:08:32 +0000

    removed gappa command from here.         Signed off by: Monu <sunil9802506051@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -8,7 +8,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch aosp_X00TD-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
-export WITH_GAPPS=true
 export TZ=Asia/Kolkata #put before last build commad
 make bacon
 
