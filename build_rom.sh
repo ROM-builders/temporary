2d79eef49e62c8c7ba373bd0f7d66e117eaeee8f
CLHEX <100755123+clhexftw@users.noreply.github.com>
2022-03-28 21:02:08 +0530

    vayu: let the build be vanilla

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch lighthouse_vayu-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
-export WITH_GAPPS=true
 export TZ=Asia/Dhaka #put before last build command
 make lighthouse
   
