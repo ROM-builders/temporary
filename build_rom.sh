555372ef42dade29ed2fd37390741665f8b44305
Coptan⁹⁹ <omarcoptan9@gmail.com>
2022-03-28 02:43:03 +0200

    PE lavender

--- build_rom.sh
+++ build_rom.sh
@@ -4,6 +4,7 @@ git clone https://github.com/OmarAlCoptan/local_manifest.git --depth 1 -b pe .re
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 
+
 # build rom
 source build/envsetup.sh
 lunch aosp_lavender-userdebug
