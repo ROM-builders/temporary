4979f92b95bc2ab66156cccd5db277aff218a07a
Dev786s <85985141+Dev786s@users.noreply.github.com>
2022-03-29 00:58:48 +0530

    12.1

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Dev786s/manifest.git -b snow -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Dev786s/local-manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
-lunch aosp_merlinx-userdebug
+. build/envsetup.sh
+lunch blaze_merlinx-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
- mka bacon 
+ brunch merlix
 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
