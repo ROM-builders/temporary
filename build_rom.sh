b79e2050d5803ae6c9ae5576b990055e8a4b0cd8
MadmoudRMX2020 <98421074+MadmoudRMX2020@users.noreply.github.com>
2022-03-28 10:08:20 +0200

    Ginkgo: remove $ from the lunch command

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch aosp_$ginkgo-userdebug
+lunch aosp_ginkgo-eng
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
