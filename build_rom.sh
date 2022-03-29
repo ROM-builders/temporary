c0e7243237fd1ebda065abd063ba4d205dfc7dba
Mayur ◢◤ <flynryder427@gmail.com>
2022-03-29 13:35:06 +0530

    Fix lunch codes

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh 
-lunch evolution_lavender-user
+lunch evolution_RMX2061-userdebug
 export TZ=Asia/Kolkata #put before last build command
 mka evolution
 
