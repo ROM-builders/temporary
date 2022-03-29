b8acd7b27f9677244c13a1f1a16bd525d3442113
Anoosragh <83896147+ok-ano0s@users.noreply.github.com>
2022-03-29 17:03:06 +0530

    RMX1941: Add logger

--- build_rom.sh
+++ build_rom.sh
@@ -12,5 +12,8 @@ export BUILD_USERNAME=ok-ano0s
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
+# get buildate for OTA pushing
+bash vendor/cipher/build/tools/ota.sh
+
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
