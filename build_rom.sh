05c6e31a10ebeb5ad7c3a83c39e67226ecce656e
Dev786s <85985141+Dev786s@users.noreply.github.com>
2022-03-30 23:09:55 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,7 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelPlusUI-SnowCone/manifest -b snowcone -g default,-mips,-darwin,-notdefault
 
 
-git clone https://github.com/Dev786s/local-manifest.git --depth 1 -b -b Android12 .repo/local_manifests
+git clone https://github.com/Dev786s/local-manifest.git --depth 1 -b Android12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
