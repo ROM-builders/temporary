c3c75c6f2156f29b096217b10f26c5d23b597070
Ashish <97603141+JaswalAshish@users.noreply.github.com>
2022-03-30 21:27:51 +0530

    fixed manifest

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest -b 12.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/JaswalAshish/local_manifest.git --depth 1 -b pb .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
