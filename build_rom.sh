028c88ac8d7a2308130cf7cfe56445d1061af857
Nabil Zaman <94600437+iamnabilzaman@users.noreply.github.com>
2022-03-30 10:30:14 +0600

    santoni AOSPA iamnabilzaman

--- build_rom.sh
+++ build_rom.sh
@@ -8,7 +8,7 @@ source build/envsetup.sh
 lunch aospa_santoni-userdebug
 export BUILD_USERNAME=nabil
 export TZ=Asia/Dhaka #put before last build command
-./rom-build.sh santoni
+./rom-build.sh santoni -t userdebug
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
