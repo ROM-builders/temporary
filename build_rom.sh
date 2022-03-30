b34146c40aec84dbfcd8da5a4a6c0ac8b6caad9d
Leonid <62807998+leonidofsparta@users.noreply.github.com>
2022-03-30 22:32:34 +0200

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init -u https://github.com/Octavi-OS/platform_manifest.git --depth=1 --no-repo-verify -g default,-mips,-darwin,-notdefault
+repo init -u https://github.com/Octavi-OS/platform_manifest.git -b 12 --depth=1 --no-repo-verify -g default,-mips,-darwin,-notdefault
 git clone https://github.com/leonidofsparta/local_manifest.git --depth 1 -b master .repo/local_manifest
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
