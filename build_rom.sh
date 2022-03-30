6e01c7d26c6428ad18459621da17a0dfd217f315
Onkar singh <80300768+onkarsinghsahil@users.noreply.github.com>
2022-03-30 18:42:35 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixysOS/manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/onkarsinghsahil/local-manifest.git --depth 1 -b pix .repo/local_manifest
+git clone https://github.com/onkarsinghsahil/local-manifest.git --depth 1 -b pix .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
