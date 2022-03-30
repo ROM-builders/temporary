ca71d433bb94861e48a0b5e96d21a607dbab284a
Md Abu Talha <85061045+roku-senpai@users.noreply.github.com>
2022-03-30 19:57:31 +0600

    use https

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/PixelOS-Pixelish/manifest.git -b twelve -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-Pixelish/manifest.git -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/rokusenpaislab/local_manifest_violet.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
