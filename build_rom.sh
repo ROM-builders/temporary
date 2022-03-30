87c5c1931cd71b3deb5622cc8ca197171dced186
ariffjenong <arifbuditantodablekk@gmail.com>
2022-03-30 16:08:04 +0700

    update common tree

--- build_rom.sh
+++ build_rom.sh
@@ -8,8 +8,9 @@ source build/envsetup.sh
 lunch cherish_maple_dsds-userdebug
 export BUILD_HOSTNAME=ArifJeNong
 export BUILD_USERNAME=ArifJeNong
+#export CHERISH_VANILLA=true
 export TZ=Asia/Jakarta #put before last build command
-mka bacon
+brunch maple_dsds
  
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
