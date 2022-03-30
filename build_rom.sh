b50951ad0da3d754e1a3f9be9a1e75dc1bfa813e
T.B.S <86312158+Tiktodz@users.noreply.github.com>
2022-03-30 12:16:25 +0700

    unshishufied manifest

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
-# sync rom
+# sync roms
 repo init --depth=1 --no-repo-verify -u https://github.com/Bootleggers-BrokenLab/manifest.git -b sambun -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Tiktodz/local_manifest.git --depth 1 -b sambun .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build roms
 source build/envsetup.sh
 lunch bootleg_X00TD-userdebug
 export TZ=Asia/Tokyo #put before last build command
