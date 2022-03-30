53ddabbb8cc3a51523c207b9a853cbb46503ca67
greengreen2212 <101193930+greengreen2212@users.noreply.github.com>
2022-03-30 13:15:52 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/greengreen2212/local_manifest.git --depth 1 -b pix .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android -b lineage-18.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/greengreen2212/local_manifest.git --depth 1 -b lin1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_beryllium-userdebug
+lunch lineage_beryllium-userdebug
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
