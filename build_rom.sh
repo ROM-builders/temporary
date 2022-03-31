11733fd66f3a79245422da9deeb7fdf84b62cd2f
Estatè <86308945+TheEstate@users.noreply.github.com>
2022-03-31 02:12:54 +0200

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch arrow_mi439-userdebug
+lunch arrow_olives-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
 m bacon
