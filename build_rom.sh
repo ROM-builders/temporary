f7561e5ef7f45bdfb6e45d660f1eee9678582b98
zaidenx <101551137+zaidenx@users.noreply.github.com>
2022-04-01 12:24:15 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/zaidenx/local_manifest.git --depth 1 -b Arrow .repo
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+. build/envsetup.sh
 lunch arrow_RMX1821-user
 export TZ=Asia/Dhaka #put before last build command
 m bacon
