b8a6f84184872c116e2e682287aec20b85fc071c
Nerdhannn <97657256+nerdhannn@users.noreply.github.com>
2022-04-01 19:17:33 +0700

    X01BD-ProtonAOSP-nerdhannn

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 -u git://github.com/ProtonAOSP-SDMxx0/android_manifest -b sc-v2 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com//ProtonAOSP-SDMxx0/android_manifest.git -b sc-v2 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/ProtonAOSP-SDMxx0/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
