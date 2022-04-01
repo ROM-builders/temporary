59ae56c7623bd354fe9711fe3d3213982c679868
Onkar singh <80300768+onkarsinghsahil@users.noreply.github.com>
2022-04-01 13:05:51 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch xdroid_mido-userdebug
-
 export BUILD_USERNAME=Onkar
 export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Dhaka #put before last build command
