c1add6230ec84e92cd0ba829bd24cef1c054fa2a
Sayan Sarkar <97832667+SKORPION29@users.noreply.github.com>
2022-04-01 23:17:55 +0530

    miatoll-lighthouse-SKORPION29

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,13 @@
 # sync rom
-repo init -u https://github.com/BlissRoms/platform_manifest.git -b arcadia
+repo init --depth=1 -u https://github.com/lighthouse-os/manifest.git -b sailboat_L1;
 git clone https://github.com/SKORPION29/local_manifest --depth 1 -b main
-repo sync -c --force-sync --no-tags --no-clone-bundle -j$(nproc --all) --optimized-fetch --prune
+repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all);
 
 # build rom
 source build/envsetup.sh
-export TZ=Asia/Dhaka  #put before last build command
-blissify bliss_miatoll-userdebug
+lunch lighthouse_miatoll-userdebug
+export TZ=Asia/Dhaka #put before last build command
+make lighthouse
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
