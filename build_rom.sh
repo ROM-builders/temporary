2beb4b2d09f2e68f899e526b82ea7592eca3a257
ibratabian17 <71920907+ibratabian17@users.noreply.github.com>
2022-04-02 13:27:37 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,18 +1,20 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ibraaltabian17/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ProtonAOSP/android_manifest -b sc-v2 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/ibraaltabian17/local_manifest.git --depth 1 -b proton .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom arroew
 source build/envsetup.sh
-lunch lineage_A6020-userdebug
+lunch aosp_beryllium-userdebug
 BUILD_USER="Ibratabian17"
-BUILD_HOST="EmerlSyc"
+BUILD_HOST="Prjktla"
 BUILD_USERNAME="Ibratabian17"
-BUILD_HOSTNAME="EmerlSyc"
+BUILD_HOSTNAME="Prjktla"
 export TZ=Asia/Jakarta #put before last build command
 croot
-brunch A6020
+export BUILD_USER="Ibratabian17"
+export BUILD_HOST="Prjktla"
+mka bacon
 
 #14 Update Sourcw
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
