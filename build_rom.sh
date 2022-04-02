3f4d70d964511d3632764677a3cc2073ab5fa401
Sayan Sarkar <97832667+SKORPION29@users.noreply.github.com>
2022-04-03 00:27:44 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 -u https://github.com/lighthouse-os/manifest.git -b sailboat_L1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/SKORPION29/local_manifest --depth 1 -b main .repo/local_manifests
+git clone https://github.com/SKORPION29/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
