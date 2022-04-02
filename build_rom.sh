9ee19cf1c8c01cd4a8aff905616c172b3576fd55
adrian-8901 <adrianlam230@gmail.com>
2022-04-02 01:20:18 +0000

    Lets build arrow

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,14 @@
-# sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+# sync romrepo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-11.0
+git clone https://github.com/Adi-sdm/device_realme_RMX1941 device/realme/RMX1941 && git clone https://github.com/Realme-C2/android_vendor_realme_RMX1941 -b eleven vendor/realme/RMX1941
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
+wget https://github.com/Realme-C2/patches/raw/main/mt6765.sh && chmod +x mt6765.sh && ./mt6765.sh
+rm -rf packages/apps/FMRadio
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
+lunch arrow_RMX1941-userdebug
 export TZ=Asia/Dhaka #put before last build command
-mka derp
+m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
