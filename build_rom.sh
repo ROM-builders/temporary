b3fed41a4e7f2093a325f96815366ba58e77f6ad
Estatè <86308945+TheEstate@users.noreply.github.com>
2022-04-02 11:49:14 +0200

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/lighthouse-os/manifest.git -b sailboat_L1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/TheEstate/local_manifest.git --depth 1 -b sailboat_L1 .repo/local_manifests
-repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all);
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
