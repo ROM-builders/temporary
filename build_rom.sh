3f3a69624e373a3382ed63b8ba24b85921133a62
Shivan Singh <shivan.0972@gmail.com>
2022-04-02 23:36:08 +0530

    Ignore neverallows

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,9 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_mido-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
+export ALLOW_MISSING_DEPENDECIES=true
+export SKIP_ABI_CHECKS=true
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
