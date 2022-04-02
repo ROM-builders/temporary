4462b1cc5164625d4c977f2ce1ba0993ff857073
ibratabian17 <71920907+ibratabian17@users.noreply.github.com>
2022-04-02 18:37:42 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manif
 git clone https://github.com/ibraaltabian17/local_manifest.git --depth 1 -b proton .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom arow
+# build rom arow beryllium
 source build/envsetup.sh
 lunch arrow_beryllium-userdebug
 BUILD_USER="Ibratabian17"
