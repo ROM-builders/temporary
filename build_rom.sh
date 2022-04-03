5881abd59800c75226323cb58d5f343bde080c9a
Coptan⁹⁹ <omarcoptan9@gmail.com>
2022-04-04 00:17:41 +0200

    OctaviOS

--- build_rom.sh
+++ build_rom.sh
@@ -7,8 +7,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch octavi_lavender-userdebug
-export WITH_GAPPS=true
-export SKIP_ABI_CHECKS=true
 export TZ=Africa/Egypt #put before last build command
 brunch lavender
 
