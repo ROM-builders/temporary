880dd3707f1298e109ab7036ff08a0b6db63a714
A50-Victor <80767544+victor4cris@users.noreply.github.com>
2022-04-02 23:55:07 -0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,16 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/victor4cris/local_manifest --depth 1 -b crDroid .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Corvus-R/android_manifest.git -b 12-test -g default,-mips,-darwin,-notdefault
+git clone https://github.com/victor4cris/local_manifest --depth 1 -b corvus .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch lineage_a505fn-userdebug
+lunch corvus_a505fn-userdebug
 export KBUILD_BUILD_USER=v1ct0r
 export KBUILD_BUILD_HOST=v1ct0r
 export BUILD_USERNAME=v1ct0r
 export TZ=Asia/Kolkata #put before last build command
-brunch a505fn
+make corvus
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
