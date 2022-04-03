1365c648d045c4114cda7b3264892356014d353b
sonic011gamer <58566108+sonic011gamer@users.noreply.github.com>
2022-04-03 12:47:01 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/aospneon/local_manifest.git --depth 1 -b lineage-19.1 .repo/local_manifests
+git clone https://github.com/sonic011gamer/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build  rom
 source build/envsetup.sh
-lunch lineage_tissot-userdebug
+lunch talkman-userdebug
 export TZ=Asia/Kolkata #put before last build command
 m bacon
 
