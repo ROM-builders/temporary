0640d29d00e5ab95d3fa7c645d1b2bcc41547adb
Nishant <82361039+Ultron12bot@users.noreply.github.com>
2022-04-04 18:06:37 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/Komodo-OS-Rom/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/Komodo-OS-Rom/manifest -b 11 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Ultron12bot/local_manifiest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
