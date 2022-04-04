9a0e8b7b8a2e1ab48700deed9dae271fff10a514
Nishant <82361039+Ultron12bot@users.noreply.github.com>
2022-04-04 18:02:19 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/BaikalOS/manifest.git -b r11.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/Komodo-OS-Rom/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Ultron12bot/local_manifiest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_X01AD-user
+lunch komodo_X01AD-userdebug
 export TZ=Asia/Kolkata #put before last build command
-mka clob
+masak komodo -jX
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
