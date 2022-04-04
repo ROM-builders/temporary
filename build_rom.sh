3a88f6e65f6a942a56ea2efd305c195e271945ed
laleeroy <gabwielcastwo@gmail.com>
2022-04-05 03:22:55 +0800

    Retry
    Signed-off-by: laleeroy <gabwielcastwo@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch xdroid_alioth-user
+export KBUILD_BUILD_VERSION=EUT-V69
 export TZ=Asia/Manila #put before last build command
 make xd
 
