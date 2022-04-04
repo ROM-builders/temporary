510c094177b2f1be7e33099248dda78d21a0b6e6
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2022-04-04 08:01:24 +0530

    fix manifest url

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelPlusUI-SnowCone -b snowcone -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelPlusUI-SnowCone/manifest -b snowcone -g default,-mips,-darwin,-notdefault
 git clone https://github.com/rushiranpise/local_manifest.git --depth 1 -b ppui .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
