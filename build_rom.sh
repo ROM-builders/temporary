42fe639d78f06d488023479595d0857a8a4c7248
ariffjenong <arifbuditantodablekk@gmail.com>
2022-04-05 09:47:28 +0700

    maple: update device tree

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,16 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b twelve-one -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ariffjenong/local_manifest.git --depth 1 -b maple_cherish .repo/local_manifests
+git clone https://github.com/ariffjenong/local_manifest.git --depth 1 -b cherish-12.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch cherish_maple-userdebug
-export BUILD_HOSTNAME=ArifJeNong
+lunch cherish_maple_dsds-userdebug
+export BUILD_HOSTNAME=ubuntu
 export BUILD_USERNAME=ArifJeNong
-export CHERISH_VANILLA=true
+#export CHERISH_VANILLA=true
 export TZ=Asia/Jakarta #put before last build command
-brunch maple
+brunch maple_dsds
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
