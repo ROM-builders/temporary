e1e7e5277db210a9ee5db69b65d88a06d3bce5b5
Nishant <82361039+Ultron12bot@users.noreply.github.com>
2022-04-06 14:01:46 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch blaze_X01AD-userdebug
+lunch X01AD-userdebug
 export TZ=Asia/Kolkata #put before last build command
 brunch X01AD
 
