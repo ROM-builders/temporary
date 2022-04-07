2b1cb63779ba8610d0b7af5075129610ec1873e5
AbhiRagh <abhiragh0@gmail.com>
2022-04-07 20:23:23 +0530

    Trigger Awaken 12.1 Build

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/Project-Awaken/android_manifest.git -b 12 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Th30utlaw/local_manifest.git --depth 1 -b awaken .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/Project-Awaken/android_manifest -b 12.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Th30utlaw/local_manifest.git --depth 1 -b awaken-test .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch awaken_olives-userdebug
+lunch awaken_mi439-userdebug
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
