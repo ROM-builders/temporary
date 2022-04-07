cb5092bd523cec1f3f82602ed38160f6c3e847da
amier_san09 <73158213+MhmmdAmier@users.noreply.github.com>
2022-04-07 14:51:43 +0700

    Fix error

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lighthouse_ulysse-userdebug
-export TZ=Asia/Jakarta #put before last build command
+export TZ=Asia/Jakarta
 make lighthouse
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
