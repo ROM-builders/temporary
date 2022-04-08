64c71316afc32e3a8ef77c95f4eede04a0c87e31
neonmicky <63160537+neonmicky@users.noreply.github.com>
2022-04-08 20:05:06 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 export SELINUX_IGNORE_NEVERALLOWS=true
 lunch lineage_tissot-userdebug
-export TZ=Asia/Dhaka #put before last build command
+export TZ=Asia/Kolkata #put before last build command
 m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
