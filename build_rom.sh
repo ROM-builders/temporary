ca9f804597e4ee0ba73592c0ccc5ad2c6e14fc6b
Kreio <39789245+kreioroscape@users.noreply.github.com>
2022-04-09 21:14:23 +0800

    done!

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init -u https://github.com/Project-Awaken/android_manifest.git -b 12.1
-git clone 
-repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest.git -b r -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/kreioroscape/local_manifest.git --depth 1 -b 11.0 .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
-export TZ=Asia/Dhaka #put before last build command
-mka derp
+lunch awaken-ginkgo-userdebug
+export TZ=Asia/Jakarta #put before last build command
+awaken -g ginkgo 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
