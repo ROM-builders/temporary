4c981fc38f46c61d84cb136d75af51a2b6936d6d
amier_san09 <73158213+MhmmdAmier@users.noreply.github.com>
2022-04-09 14:55:44 +0700

    Fix: try again

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch blaze_ulysse-userdebug
-export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Jakarta 
 brunch ulysse
 
