f1432c601f15ca1f17568897cc295c1af7e3fba5
cyborgdc2000 <93866768+cyborgdc2000@users.noreply.github.com>
2022-04-09 16:10:50 +0530

    RMX1931-ProjectArcana

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/projectarcana-aosp/manifest -b 12.x -g default,-mips,-darwin,-notdefault
-git clone https://github.com/cyborgdc2000/local_manifest.git --depth 1 -b RMX1931 .repo/local_manifests
+git clone https://github.com/cyborgdc2000/local_manifest.git --depth 1 -b twelve .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
