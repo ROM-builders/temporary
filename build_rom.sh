c101bc22374c555d27912c2f70f91a1e0c48ae55
jeno1712 <43789899+jeno1712@users.noreply.github.com>
2022-04-09 11:54:23 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -2,9 +2,9 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-12.1 -g default,-mips,-darwin,-notdefault  
 git clone https://github.com/jeno1712/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-rm -rf hardware/qcom-caf/msm8996
 
 #build rom
+rm -rf hardware/qcom-caf/msm8996-R
 source build/envsetup.sh
 lunch arrow_X01AD-userdebug
 export TZ=Asia/Kolkata #put before last build command
