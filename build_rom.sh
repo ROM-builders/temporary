9952ed906eb7af9700c059e5c46e42d6fc618480
jeno1712 <43789899+jeno1712@users.noreply.github.com>
2022-04-09 11:32:48 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -2,6 +2,10 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-12.1 -g default,-mips,-darwin,-notdefault  
 git clone https://github.com/jeno1712/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+rm -rf hardware/qcom-caf/msm8996
+git clone https://github.com/LineageOS/android_hardware_qcom_display.git -b lineage-19.0-caf-msm8996 hardware/qcom-caf/msm8996/display
+git clone https://github.com/LineageOS/android_hardware_qcom_audio.git -b lineage-19.0-caf-msm8996 hardware/qcom-caf/msm8996/audio
+git clone https://github.com/LineageOS/android_hardware_qcom_media.git -b lineage-19.0-caf-msm8996 hardware/qcom-caf/msm8996/media
 
 #build rom
 source build/envsetup.sh
