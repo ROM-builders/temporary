b402503c6200cb6ef6e5d317e6671f6bda9203be
Rylee <100063196+Anushi06@users.noreply.github.com>
2022-04-10 10:23:35 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -13,7 +13,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 
 source build/envsetup.sh
-
+ 
 lunch aosp_ulysse-user
 
 export TZ=Asia/Manila #put before last build command
