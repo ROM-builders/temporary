3179aabe958884e697535655c73b5fb1597be94f
DunggVN <65992011+dunggvn@users.noreply.github.com>
2022-04-10 14:46:10 +0700

    Ready

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,6 @@
 # sync rom
 repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 12.x
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 git clone https://github.com/anht3889/android_device_lge_sm8150-common -b aosp device/lge/sm8150-common
 git clone https://github.com/anht3889/android_device_lge_flash-common -b aosp device/lge/flash-common
 git clone https://github.com/anht3889/android_device_lge_flashlmdd -b aosp device/lge/flashlmdd
@@ -11,7 +12,7 @@ git clone https://github.com/anht3889/android_hardware_lge -b aosp hardware/lge
 # build rom
 source build/envsetup.sh
 lunch aosp_flashlmdd-userdebug
-m aex -j$(nproc --all) | tee log.txt
+m aex -j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
