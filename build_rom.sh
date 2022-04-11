b33e2a1ceb0819eb9eaa2be2eb08054806132335
Hafidz Muzakky <95564478+aleksiby@users.noreply.github.com>
2022-04-11 17:24:17 +0700

    Build dotOS 11

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/projectarcana-aosp/manifest -b 12.x -g default,-mips,-darwin,-notdefault
-git clone https://github.com/aleksiby/manifest.git --depth 1 -b arcana-12 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/DotOS/manifest.git -b dot11 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/aleksiby/manifest.git --depth 1 -b dot-11 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_joyeuse-userdebug
+lunch dot_joyeuse-userdebug
 export TZ=Asia/Jakarta #put before last build command
 make bacon
 
