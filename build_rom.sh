6a8fbf3968e27b274bdeade7cfba850e41841008
ariffjenong <arifbuditantodablekk@gmail.com>
2022-04-11 09:15:23 +0700

    maple: update CherishOS 3.6

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch cherish_maple-userdebug
+lunch cherish_maple-user
 export BUILD_HOSTNAME=znxt
 export BUILD_USERNAME=znxt
 #export CHERISH_VANILLA=true
