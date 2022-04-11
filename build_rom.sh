3d96e983ecbeb59877d118f94a8fe27a9093850a
cyborgdc2000 <93866768+cyborgdc2000@users.noreply.github.com>
2022-04-12 01:04:00 +0530

    Fix fingerprint override error

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/androi
 git clone https://github.com/cyborgdc2000/local_manifest.git --depth 1 -b twelve .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom 
+# build rom
 source build/envsetup.sh
 lunch awaken_RMX1931-userdebug
 export TZ=Asia/Kolkata #put before last build command
