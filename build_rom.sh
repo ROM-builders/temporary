1e9d5e66b3376c9a2c94ac032e1d9d08f59ec0a1
cyborgdc2000 <93866768+cyborgdc2000@users.noreply.github.com>
2022-04-12 12:06:48 +0530

    Build Arcana RMX1931

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b 12.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/projectarcana-aosp/manifest -b 12.x -g default,-mips,-darwin,-notdefault
 git clone https://github.com/cyborgdc2000/local_manifest.git --depth 1 -b twelve .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch awaken_RMX1931-userdebug
+lunch aosp_RMX1931-userdebug
 export TZ=Asia/Kolkata #put before last build command
 make bacon
 
