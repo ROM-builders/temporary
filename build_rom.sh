1b7dd5cea0e14a638f29080826be1fecfd995444
Marc Gading <62163236+Cramx25@users.noreply.github.com>
2022-04-12 15:26:43 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_judyln-userdebug
+export BUILD_USERNAME=Cramx25
 export TZ=Asia/Manila #put before last build command
 mka bacon
 
