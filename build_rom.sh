fd3582a7c509784a4f534b2b8a1c93242f92342b
Lynx <100063196+Lynx041@users.noreply.github.com>
2022-04-13 00:35:51 +0700

    build arcana for ulysse

--- build_rom.sh
+++ build_rom.sh
@@ -1,48 +1,15 @@
-ulysse-projectarcana-aosp-12.x Started
-
-https://cirrus-ci.com/build/5632956524920832
-
 # sync rom
 
-repo init --depth=1 --no-repo-verify -u https://github.com/Lynx041/android_device_xiaomi_ulysse-1 -b 12.x -g default,-mips,-darwin,-notdefault
-
-git clone https://github.com/laleeroy/local_manifest.git --depth 1 -b ulysse .repo/local_manifests
-
+repo init --depth=1 --no-repo-verify -u https://github.com/projectarcana-aosp/manifest -b 12.x -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Lynx041/local_manifest.git --depth 1 -b ulysse .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 
 source build/envsetup.sh
-
 lunch aosp_ulysse-user
-
 export TZ=Asia/Manila #put before last build command
-
 make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
-
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
- 
