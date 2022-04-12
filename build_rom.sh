19b3fc96e332f3d58e77ee5dce40935139b42f8d
Lynx <100063196+Lynx041@users.noreply.github.com>
2022-04-12 14:04:32 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ https://cirrus-ci.com/build/5632956524920832
 
 repo init --depth=1 --no-repo-verify -u https://github.com/projectarcana-aosp/manifest -b 12.x -g default,-mips,-darwin,-notdefault
 
-git clone https://github.com/Anushi07/local_manifest/tree/main --depth 1 -b ulysse .repo/local_manifests
+git clone https://github.com/Lynx041/android_device_xiaomi_ulysse-1 --depth 1 -b ulysse .repo/local_manifests
 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
@@ -23,5 +23,27 @@ make bacon
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
  
  
