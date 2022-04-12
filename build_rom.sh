4ebc68c2fb335c9a5c0f4f7c08c2e52a142585f0
Marc Gading <62163236+Cramx25@users.noreply.github.com>
2022-04-12 14:42:20 +0800

    for building los

--- build_rom.sh
+++ build_rom.sh
@@ -5,8 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch
-13
+lunch lineage_judyln-userdebug
 export TZ=Asia/Manila #put before last build command
 mka bacon
 
