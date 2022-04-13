ff22d5ef4498fc84b49ab6ae200b1cccdebce3f9
Julival Bittencourt <53379692+julival25@users.noreply.github.com>
2022-04-13 17:36:13 -0300

    changed branche and build type to userdebug

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/Spark-Rom/manifest.git -b sparkL -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/Spark-Rom/manifest.git -b spark -g default,-mips,-darwin,-notdefault
 git clone https://github.com/julival25/local_manifest.git --depth 1 -b spark .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch spark_tulip-user
+lunch spark_tulip-userdebug
 export WITH_GAPPS=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=America/Sao_Paulo #put before last build command
