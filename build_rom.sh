aae1a31634efe29436b0f3f0912ccab62f9dd936
Marc Gading <62163236+Cramx25@users.noreply.github.com>
2022-04-14 00:00:29 +0800

    Awts

--- build_rom.sh
+++ build_rom.sh
@@ -11,7 +11,7 @@ export KBUILD_BUILD_HOST=Cramx25
 export BUILD_HOSTNAME=Cramx25
 export BUILD_USERNAME=Cramx25
 export TZ=Asia/Manila #put before last build command
-mka bacon
+brunch judyln
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
