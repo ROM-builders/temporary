0b90e8e7f6eab4329753bf0e3b3bdeb66eaae0dc
D34DPUL <77775456+D34DPUL@users.noreply.github.com>
2022-04-14 01:37:09 +0530

    Trigger build

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom
+# sync rom 
 repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/D34DPUL/local_manifest_awaken.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
