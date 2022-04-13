d24a619cad71441364f01dc558982b0469a7c92a
Jeremy Pérez <jerespn@gmail.com>
2022-04-13 15:07:34 -0400

    hope boots this time.... garden los19.1

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Jeremito1/local_manifest.git --depth 1 -b los-19.1 .repo/local_manifests
+git clone https://github.com/Jeremito1/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch lineage_garden-userdebug
-export TZ=Asia/Kuala Lumpur #put before last build command
+export TZ=Asia/Kolkata #put before last build command
 m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
