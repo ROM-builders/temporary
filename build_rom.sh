97aa00b7a88e1bbbb1cf292d9f0380f8de34c92b
alternoegraha <37102951+alternoegraha@users.noreply.github.com>
2022-04-14 16:55:35 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest.
 git clone https://github.com/alternoegraha/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom [1]
 . build/envsetup.sh
 lunch evolution_garden-userdebug
 export TZ=Asia/Jakarta #put before last build command
