be551e415f6fccfc8dee2b9486dc3a5e414beeba
Pratyay Borborah <102370202+popoA3M@users.noreply.github.com>
2022-04-14 23:34:13 +0530

    E-projectarcana-aosp-popoA3M

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/popoA3M-World/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/projectarcana-aosp/manifest -b 12.x -g default,-mips,-darwin,-notdefault
+git clone https://github.com/popoA3M-World/local_manifest.git --depth 1 -b 12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_E-userdebug
+lunch aosp_E-userdebug
 export TZ=Asia/Kolkata #put before last build command
 make bacon
 
