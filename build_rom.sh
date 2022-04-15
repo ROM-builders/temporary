ecf7765a86136fcbeaa36ff912ff21053a5791fa
DunggVN <65992011+dunggvn@users.noreply.github.com>
2022-04-15 21:09:32 +0700

    Fix - Bootleggers 6.7

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Bootleggers-BrokenLab/manifest.git -b sambun -g default,-mips,-darwin,-notdefault
 git clone https://github.com/dunggvn/local_manifest.git --depth 1 -b aosp .repo/local_manifests
+git clone https://github.com/Bootleggers-BrokenLab/libcore.git --depth 1 -b sambun
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
