b6b1ae1d8d8826f73342cb579bba3a13cc2b9bc8
alternoegraha <37102951+alternoegraha@users.noreply.github.com>
2022-04-15 08:12:58 +0700

    try again

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/aosp-mirror/platform_manifest.git -b android12L-release -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/aosp-mirror/platform_manifest.git -b android-12.1.0_r4 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/alternoegraha/local_manifest.git --depth 1 -b vanilla_aosp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
