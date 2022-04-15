af73f2b5b6395f6504a538d476f218570231110e
anyinglulu1 <seanchendana@gmail.com>
2022-04-15 14:12:23 +0700

    hmm

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/anyinglulu1/local_manifest --depth 1 -b lineage .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/projectarcana-aosp/manifest -b 12.x -g default,-mips,-darwin,-notdefault
+git clone https://github.com/anyinglulu1/local_manifest --depth 1 -b Arcana-Whyred .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_whyred-userdebug
+lunch aosp_whyred-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export SKIP_ABI_CHECKS=true
 export TZ=Asia/Dhaka #put before last build command
