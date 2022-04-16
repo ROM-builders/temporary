52bdeafabd19c24b1e35266e27a8293477893081
Rishawn <70256146+Stealth1226@users.noreply.github.com>
2022-04-16 15:43:20 +0530

    trigger: fix flash issue

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-#export WITH_GAPPS=true
+export WITH_GAPPS=true
 lunch lighthouse_miatoll-userdebug
 make lighthouse
 
