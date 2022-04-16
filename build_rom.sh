9ab1f3f57501792d04f02ec9cd5a919b80fdb50d
Nishant <82361039+Ultron12bot@users.noreply.github.com>
2022-04-16 10:13:54 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/AospExtended/manifest.git -b 12.x -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/AospExtended/manifest.git -b 12.x -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Ultron12bot/local_manifiest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
