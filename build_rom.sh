bf81d8e2f631504a1fe2f20dd99f2a7ba799d70b
Rishawn <70256146+Stealth1226@users.noreply.github.com>
2022-04-16 20:22:07 +0530

    fix sync issue

--- build_rom.sh
+++ build_rom.sh
@@ -14,3 +14,4 @@ make lighthouse
 # If you need to upload json/multiple files too then put like this 'rclone copy out/target/product/mido/*.zip cirrus:mido -P  rclone copy out/target/product/mido/*.zip.json cirrus:mido -P'
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
 rclone copy out/target/product/miatoll/*miatoll*json cirrus:miatoll -P
+
