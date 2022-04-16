214183b2e1e4e97c3c9ac573e7ec7d19cd80c4b5
Krtonia(TAxHELIOS) <96814551+Krtonia@users.noreply.github.com>
2022-04-16 23:12:07 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch blaze_beryllium-userdebug
-export TZ=Asia/Dhaka #put before last build command
+expor TZ=Asia/Dhaka #put before last build command
 make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
