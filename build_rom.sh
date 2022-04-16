f5fd73eb909ff7cf37253660fc58d1f4fedd6454
Anyinglulu <63843048+anyinglulu1@users.noreply.github.com>
2022-04-16 13:35:32 +0700

    Fixed

--- build_rom.sh
+++ build_rom.sh
@@ -6,8 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch aosp_whyred-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
-export SKIP_ABI_CHECKS=true
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
