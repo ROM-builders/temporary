e1615fdd0056da141c27d83cb24ed5309ccae914
Anyinglulu <63843048+anyinglulu1@users.noreply.github.com>
2022-04-17 09:05:30 +0700

    Rerun

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch aosp_whyred-userdebug
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES= true 
+export ALLOW_MISSING_DEPENDECIES= true
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
