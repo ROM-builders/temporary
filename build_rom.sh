66e302d452e53d3b7893f2e34f83acb659403963
Ashish <97603141+JaswalAshish@users.noreply.github.com>
2022-04-19 05:10:00 +0530

    New update for havoc

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Havoc-OS/android_manifest -b eleven -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Havoc-OS/android_manifest.git -b eleven -g default,-mips,-darwin,-notdefault
 git clone https://github.com/JaswalAshish/local_manifest.git --depth 1 -b havoc .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
