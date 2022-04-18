91e942d86f5465082d51c93a77cb2baef2cdbce0
Shivam1143 <103647875+Shivam1143@users.noreply.github.com>
2022-04-18 08:58:32 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch aosp_X01AD-userdebug
+lunch aosp_X01AD-eng
 mka bootimage
 export TZ=Asia/Kolkata #put before last build command
 m aex
