e3f292d32d099b4032bca768d048099f04f27f4d
Angel Castillo <49908914+acastillorobles77@users.noreply.github.com>
2022-04-17 19:55:00 -0500

    vayu-BlissRoms-acastillorobles77

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch vayu
+lunch vayu-userdebug
 blissify -g vayu
 export TZ=Asia/Dhaka #put before last build command
 
