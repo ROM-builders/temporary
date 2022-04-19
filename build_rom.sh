9772b43b2d283a6471e185d59d4fcfcd28328599
greengreen2212 <101193930+greengreen2212@users.noreply.github.com>
2022-04-19 02:29:11 +0000

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch awaken_beryllium-user
+export WITH_GAPPS=true
+export TARGET_GAPPS_ARCH=arm64
 export TZ=Asia/Kolkata #put before last build command
 make bacon
 
