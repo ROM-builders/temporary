5fd051b2b7a169a9626db33cd331c5029a29cb6d
68orkun <83245917+68orkun@users.noreply.github.com>
2022-04-19 20:46:11 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b 12.1 -g default,-mips,-darwin,-notdefault
+repo init -u https://github.com/ProjectBlaze/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/68orkun/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch awaken_lava-userdebug
+lunch blaze_lava-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export ALLOW_MISSING_DEPENDENCIES=true
-export AWAKEN_MAINTAINER=orkunergun
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
