7bac614882c01bfdeefeb561ac0acc4b664cc48a
Lynx <100063196+Lynx041@users.noreply.github.com>
2022-04-19 14:05:10 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch aosp_ulysse-user
+lunch aosp_ulysse-user 
 export TZ=Asia/Manila
 make bacon
 
