5004cd2558d808cb36ee9771a47de5f3759fe4f8
Nguyen Cong Vinh <80946737+vn-ncvinh@users.noreply.github.com>
2022-04-19 22:41:51 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,18 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/j0ok34n/local_manifests.git --depth 1 -b blaze .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b 12.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/j0ok34n/local_manifests.git --depth 1 -b aosp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+sed -i 's/aosp/awaken/g' device/lge/flashlmdd/awaken_flashlmdd.mk
+sed -i 's/aosp/awaken/g' device/lge/flashlmdd/AndroidProducts.mk
+mv device/lge/flashlmdd/aosp_flashlmdd.mk device/lge/flashlmdd/awaken_flashlmdd.mk
 
 # build rom
-. build/envsetup.sh
-lunch blaze_flashlmdd-userdebug
+source build/envsetup.sh
+lunch awaken_flashlmdd-userdebug
 export WITH_GAPPS=true
 export TARGET_GAPPS_ARCH=arm64
 export TZ=Asia/Ho_Chi_Minh #put before last build command
-brunch flashlmdd
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
