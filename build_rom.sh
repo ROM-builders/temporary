60e51b50097e0181f8fcf1b3cf612f2afe308cd4
Anyinglulu <63843048+anyinglulu1@users.noreply.github.com>
2022-04-20 10:51:23 +0000

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,9 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch aosp_whyred-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES= true 
-export ALLOW_MISSING_DEPENDECIES= true
+export SELINUX_IGNORE_NEVERALLOWS= true
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES= true
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
