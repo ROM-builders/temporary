d377ef9ec53c66c34c0dfff531f9db837537c48a
pingmado <75196272+ping2109@users.noreply.github.com>
2022-04-20 16:24:12 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b twelve-one -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ping2109/local_manifest.git -b cherish-spes .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+git clone https://github.com/ping2109/local_manifest.git -b cherish-spes .repo/local_manifests
 
 # build rom
 source build/envsetup.sh
