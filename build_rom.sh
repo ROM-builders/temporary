d95387b7b84f71bf55d6ca6046a5d63dfa5b2983
romgharti <romgharti@gmail.com>
2022-04-20 20:32:59 +0545

    mojito: AEX: Vanilla build

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch aosp_mojito-user
 export ALLOW_MISSING_DEPENDENCIES=true
-export WITH_GAPPS=true
+# export WITH_GAPPS=true
 export TZ=Asia/Dhaka #put before last build command
 m aex
 
