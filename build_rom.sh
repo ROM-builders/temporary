e3ed51912618611f70b69614b6822407e35749a3
SupFlashVr <104121241+SupFlashVr@users.noreply.github.com>
2022-04-21 10:09:26 +0400

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-Pixelish/mani
 git clone https://github.com/SupFlashVr/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom 
 source build/envsetup.sh
 lunch pixel_lava-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
