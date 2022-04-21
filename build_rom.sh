beec22cf946c46afa8f90b96f95618fd96e5645e
SupFlashVr <104121241+SupFlashVr@users.noreply.github.com>
2022-04-21 08:57:21 +0400

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -9,6 +9,8 @@ lunch derp_vayu-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export ALLOW_MISSING_DEPENDENCIES=true
+export WITH_GAPPS=true
+export TARGET_GAPPS_ARCH=arm64
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
