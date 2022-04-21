88f7d60a1f2e8028ad9cb142b082f48c0641517d
Nabil Zaman <94600437+iamnabilzaman@users.noreply.github.com>
2022-04-21 19:53:51 +0600

    m20lte CorvusOS iamnabilzaman

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch corvus_m20lte-userdebug
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
 export BUILD_USERNAME=nabil
 export TZ=Asia/Dhaka #put before last build command
 make corvus
