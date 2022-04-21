48bb10e5c29d3b3ceb1e5e5ff31a812da9113bc3
NRanjan-17 <97696537+NRanjan-17@users.noreply.github.com>
2022-04-22 05:16:50 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch pixys_mido-userdebug
 export TZ=Asia/Delhi #put before last build command
-export BUILD_MISSING_DEPENDENCIES=true
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
