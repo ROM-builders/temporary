a1bc8712da7ada39b9dd70ae4e8b312c4113ef52
Sayan Sarkar <97832667+SKORPION29@users.noreply.github.com>
2022-04-22 12:22:32 +0530

    remove devicesettings clone from build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,6 @@
 # sync rom
 repo init --depth=1 -u https://github.com/lighthouse-os/manifest.git -b sailboat_L1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/SKORPION29/local_manifest.git --depth 1 -b main .repo/local_manifests
-git clone https://github.com/LineageOS/android_packages_resources_devicesettings -b lineage-19.1 packages/resources/devicesettings
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
