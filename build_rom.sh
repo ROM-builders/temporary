77d3f7eab4a39d37d1b1efe2d4b0eb7cc0e3b2f4
makhk <30686963+makhk@users.noreply.github.com>
2022-04-22 17:47:46 +0530

    Test build Flos19.1

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,16 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ForkLineageOS/android -b lineage-19.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/hsx02/Local-Manifests.git --depth 1 -b flos-19.1 .repo/local_manifests
+git clone https://github.com/makhk/local_manifest.git --depth 1 -b flos .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_mi439-userdebug
-export TARGET_FLOS=true
-export SELINUX_IGNORE_NEVERALLOWS=true
-export TZ=Asia/Jakarta #put before last build command
-brunch mi439
+lunch lineage_sweet-user
+export WITH_GMS=false
+export RELAX_USES_LIBRARY_CHECK=true
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+export TZ=Asia/Dhaka #put before last build command
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
