470556f5f706f22fdc54755f085ea593b6f75641
NRanjan-17 <97696537+NRanjan-17@users.noreply.github.com>
2022-04-22 19:05:59 +0530

    removed rules

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch pixys_mido-userdebug
 export TZ=Asia/Delhi #put before last build command
-export BUILD_BROKEN_DUP_RULES=true
 make pixys
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
