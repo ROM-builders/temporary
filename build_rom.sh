d49a66c0068ed095660f5a4220e4267696b5c1ac
ritvik-ch <102162683+ritvik-ch@users.noreply.github.com>
2022-04-22 14:08:11 +0530

    again

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/ritvik-ch/local_manifest.git --depth 1 -b lighthous
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+. build/envsetup.sh
 lunch lighthouse_vince-user
 export TZ=Asia/Kolkata
 make lighthouse
