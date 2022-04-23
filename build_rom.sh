dd569adc6579fd55705c22566d210977e10b3765
romgharti <romgharti@gmail.com>
2022-04-24 05:23:43 +0545

    mojito: user build

--- build_rom.sh
+++ build_rom.sh
@@ -5,9 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch pixys_mojito-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
-export ALLOW_MISSING_DEPENDENCIES=true
+lunch pixys_mojito-user
 # export WITH_GAPPS=true
 export TZ=Asia/Dhaka #put before last build command
 make pixys
