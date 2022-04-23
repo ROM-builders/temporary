1c2cf261d2ae829ecbb09598f510ff516551e1c3
iDrinkCoffee <80449624+huhugh221@users.noreply.github.com>
2022-04-23 09:42:51 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ProjectRadiant/manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/huhugh221/local_manifest.git --depth 1 -b master .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b snow -g default,-mips,-darwin,-notdefault
+git clone https://github.com/huhugh221/local_manifest.git --depth 1 -b main .repo/local_manifests
 
 # build rom
 source build/envsetup.sh
-lunch radiant_merlinx-userdebug
-export TZ=Asia/Dhaka #put before last build command
-make bacon
+lunch evolution_phoenix-userdebug
+export TZ=Asia/Jakarta #put before last build command
+mka evolution
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
