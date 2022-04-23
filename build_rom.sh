77c982c971d761b3cbc08e12c8e06315978ed32f
romgharti <romgharti@gmail.com>
2022-04-23 14:10:55 +0545

    mojito: hal fix

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch styx_mojito-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
-export styx_variant := ok
+export styx_variant= true 
 export TZ=Asia/Dhaka #put before last build command
 m styx-ota
 
