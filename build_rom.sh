e14f24a4eebe6ec6eca8ceca1aa67afe2e5eb0c4
OrangeHorizonst <deswarashandika10@gmail.com>
2022-04-24 09:00:45 +0800

    surya: lunch fixup

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch surya_lineage-userdebug
+lunch lineage_surya-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
