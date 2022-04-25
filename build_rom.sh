915c9a433ee345e9dd60f8b0edf35433474c43f8
SunnyM <sunnyteja201@gmail.com>
2022-04-25 21:04:39 +0530

    r5x : blaze init
    <r5x>-<blaze>-<JaswantTeja>

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/SwiftOS-DROID/android_manifest.git -b 12 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/JaswantTeja/local_manifest.git --depth 1 -b Swift .repo/local_manifest
-repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all);
+repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/JaswantTeja/local_manifest.git --depth 1 -b blaze .repo/local_manifest
+repo sync -c -j$(nproc --all) --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune
 
 # build rom
 . build/envsetup.sh
-lunch swift_r5x-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
+lunch blaze_r5x-userdebug
+SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Kolkata #put before last build command
-mka swift
+brunch r5x
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
