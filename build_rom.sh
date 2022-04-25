c6c2fdde6de44b937906d9e62d90851a5f0935e7
Wayne <99174654+ugly-saifu@users.noreply.github.com>
2022-04-25 22:47:52 +0530

    X01AD-YAAP-ugly-saifu

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/projectarcana-aosp/manifest -b 12.x -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/yaap/manifest.git -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/ugly-saifu/local_manifest.git -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
@@ -9,9 +9,9 @@ export USE_CCACHE=1
 ccache -M 50G
 export CONFIG_STATE_NOTIFIER=y
 export SELINUX_IGNORE_NEVERALLOWS=true
-lunch aosp_X01AD-userdebug
+lunch yaap_X01AD-userdebug
 export TZ=Asia/Dhaka #put before last build command
-make bacon
+m yaap
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
