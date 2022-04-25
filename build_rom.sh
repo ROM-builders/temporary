1c9e30c83d672cffbc1c1f5142a79b13e9f2ab1b
SunnyM <sunnyteja201@gmail.com>
2022-04-25 10:10:16 +0530

    Update build_rom.sh
    <r5x>-<awaken>-<JaswantTeja>

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b 12.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/Project-Awaken/android_manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/JaswantTeja/local_manifest.git --depth 1 -b master .repo/local_manifest
- repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
+repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
 
 # build rom
 source build/envsetup.sh
