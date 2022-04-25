e5d9f0f0b531c412339ca3c189885259a37fe6d1
Nishant <82361039+Ultron12bot@users.noreply.github.com>
2022-04-25 08:51:51 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch havoc _X01AD-userdebug
+lunch havoc_X01AD-userdebug
 export TZ=Asia/Kolkata #put before last build command
 brunch X01AD
 
