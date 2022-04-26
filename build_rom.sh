9b60327622333b1ca0562974cac367445e3274bb
T.B.S <86312158+Tiktodz@users.noreply.github.com>
2022-04-27 01:31:29 +0700

    trigger build - saur

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
-# sync roms
+# sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/yaap/manifest.git -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Tiktodz/local_manifest.git --depth 1 -b yaap .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build roms
+# build rom
 source build/envsetup.sh
 lunch yaap_X00TD-user
 export BUILD_USERNAME=teguh
