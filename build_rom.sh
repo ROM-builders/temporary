9044b791f04c63d59be24111e2cf5a8fa8010a8e
SunnyM <sunnyteja201@gmail.com>
2022-04-26 16:58:20 +0530

    Init Awaken
    r5x-Project-Awaken-12.1-JaswantTeja

--- build_rom.sh
+++ build_rom.sh
@@ -6,8 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch awaken_r5x-user
-export SELINUX_IGNORE_NEVERALLOWS=true
-export WITH_GAPPS=true
 export TZ=Asia/NewDelhi #put before last build command
 make bacon
 
