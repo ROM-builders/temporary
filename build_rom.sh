cb9f16fd9c9eebb996f6cdd1da5fdef2074762ad
Wayne <99174654+ugly-saifu@users.noreply.github.com>
2022-04-26 09:51:58 +0530

    X0A1D-CORVUS

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/yaap/manifest.git -b twelve -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/Corvus-R/android_manifest.git -b 12-test -g default,-mips,-darwin,-notdefault
 git clone https://github.com/ugly-saifu/local_manifest.git -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
