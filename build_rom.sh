6b6000116fadb3e02b6c55dba26d8c0520d85d9d
SunnyM <sunnyteja201@gmail.com>
2022-04-27 04:34:19 +0530

    Init Awaken

--- build_rom.sh
+++ build_rom.sh
@@ -1,10 +1,10 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ProjectStreak/platform_manifest -b twelve.one -g default,-mips,-darwin,-notdefault
-git clone https://github.com/JaswantTeja/local_manifest.git --depth 1 -b streak .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ProjectAwaken/android_manifest -b 12.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/JaswantTeja/local_manifest.git --depth 1 -b awaken .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-. build/envsetup.sh
+source build/envsetup.sh
 lunch streak_r5x-userdebug
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
