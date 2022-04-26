5bd519c23826afec2ae63fca3b69cc44d1ed093c
SunnyM <sunnyteja201@gmail.com>
2022-04-26 17:00:14 +0530

    Init Streak XD
    r5x-Project-Awaken-12.1-JaswantTeja

--- build_rom.sh
+++ build_rom.sh
@@ -6,8 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch streak_r5x-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
-export WITH_GAPPS=true
 export TZ=Asia/NewDelhi #put before last build command
 mka bacon
 
