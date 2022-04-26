5c7a63fe5bb81f6ae9e423f15eb37a0fe53887b1
SunnyM <sunnyteja201@gmail.com>
2022-04-26 17:25:36 +0530

    r5x : awaken 12.1

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,9 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch awaken_r5x-user
+export SELINUX_IGNORE_NEVERALLOWS=true
+export WITH_GAPPS=true
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export TZ=Asia/NewDelhi #put before last build command
 make bacon
 
