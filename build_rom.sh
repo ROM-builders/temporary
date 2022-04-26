2aa6d90a1760ab85c2c1645c8c3c725b7979529d
Ashish <97603141+JaswalAshish@users.noreply.github.com>
2022-04-26 08:25:07 +0530

    Fixed some issues again

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch evolution_mi439-eng
+lunch evolution_mi439-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka evolution
 
