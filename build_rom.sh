0b1c31325c3ef712ce8f4b1bce9cbf4f7c274ccc
Wayne <99174654+ugly-saifu@users.noreply.github.com>
2022-04-26 09:50:13 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -9,9 +9,9 @@ export USE_CCACHE=1
 ccache -M 50G
 export CONFIG_STATE_NOTIFIER=y
 export SELINUX_IGNORE_NEVERALLOWS=true
-lunch yaap_X01AD-userdebug
+lunch corvus_X01AD-userdebug
 export TZ=Asia/Dhaka #put before last build command
-m yaap
+make corvus
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
