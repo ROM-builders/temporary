d0385986696efc2d96214d0e4abeed70af9bd7e6
ritvik-ch <102162683+ritvik-ch@users.noreply.github.com>
2022-04-27 22:33:54 +0530

    Fixed error

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/STRK-ND/local-manifest.git --depth 1 -b arrow-12.x .repo/local_manifests
+git clone https://github.com/ritvik-ch/local-manifest.git --depth 1 -b arrow-12.x .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) 
 
 # build rom
