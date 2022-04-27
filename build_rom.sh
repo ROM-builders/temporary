9b814707d4fca27bb4741c5e282a2ecc2f07c771
Tkpointz <89636439+Tkpointz@users.noreply.github.com>
2022-04-27 11:05:21 -0500

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,9 +5,9 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch potato_ares-user
+lunch dot_ares-user
 export TZ=Asia/Dhaka #put before last build command
-brunch ares
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
