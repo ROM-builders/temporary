488eb46d694dd6323c893e1cc72f079e9611625d
T.B.S <86312158+Tiktodz@users.noreply.github.com>
2022-04-28 03:38:12 +0700

    permissive

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
-# sync rom
+# sync roms
 repo init --depth=1 --no-repo-verify -u https://github.com/yaap/manifest.git -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Tiktodz/local_manifest.git --depth 1 -b yaap .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build roms
 source build/envsetup.sh
 lunch yaap_X00TD-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
