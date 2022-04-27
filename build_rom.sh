d067d2f15021184bd6b2ee66dcea844f8f937f7b
SunnyM <sunnyteja201@gmail.com>
2022-04-27 14:07:53 +0530

    r5x : awaken error fixed

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/JaswantTeja/local_manifest.git --depth 1 -b awaken .repo/local_manifests
+git clone https://github.com/JaswantTeja/local_manifest.git --depth 1 -b awaken .repo/local_manifest
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
