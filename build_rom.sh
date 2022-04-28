86ce8afbb1710fc2c1e92a687469812f2e726b22
Matheus Souza <33396386+matheucomth@users.noreply.github.com>
2022-04-28 20:57:18 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch cherish_tulip-user
+lunch cherish_tulip-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export RELAX_USES_LIBRARY_CHECK=true
 export TZ=Asia/Dhaka #put before last build command
