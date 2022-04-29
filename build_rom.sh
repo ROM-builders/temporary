c70ac4f0f48c513cf326e45e59dbaa4e497f6636
Kendras056 <88943971+Kendras056@users.noreply.github.com>
2022-04-29 16:43:06 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch octavi_X00TD-userdebug
+export BUILD_USERNAME=siren
 export TZ=Europe/Brussels #put before last build command
 mka bacon
 
