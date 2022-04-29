ce6d3be04445faa7980d13bcfb3119711124ad86
A50-Victor <80767544+victor4cris@users.noreply.github.com>
2022-04-29 11:45:56 +0300

    Start Trigger Beyod1LTE PPUI 4.4 BASED 12.1

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b linegae-19.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/victor4cris/local_manifest --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelPlusUI/manifest -b sv2 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/victor4cris/local_manifest --depth 1 -b S10 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
 export TZ=Asia/Kolkata #put before last build command
-brunch a505fn
+lunch aosp_beyond1lte-userdebug
+make bacon -j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
