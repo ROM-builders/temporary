84df740ee3feb8e163de50188f3022004b6628aa
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2022-04-30 00:19:46 +0530

    fix chop chop

--- build_rom.sh
+++ build_rom.sh
@@ -13,7 +13,7 @@ export BUILD_HOSTNAME=rushiranpise
 export SELINUX_IGNORE_NEVERALLOWS=true
 export WITH_GAPPS=true
 export TZ=Asia/Kolkata #put before last build command
-m otapackage
+m aex
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 # rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
