bc99a21b09c0cccbfc97e8337631070b721177ba
Nguyen Cong Vinh <80946737+vn-ncvinh@users.noreply.github.com>
2022-04-29 18:13:13 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/j0ok34n/local_manifests.git --depth 1 -b awaken .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.git -b twelvedotone -g default,-mips,-darwin,-notdefault
+git clone https://github.com/j0ok34n/local_manifests.git --depth 1 -b superior .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j4
 
 # build rom
-source build/envsetup.sh
-lunch awaken_flashlmdd-userdebug
+. build/envsetup.sh
+lunch superior_flashlmdd-userdebug
 export WITH_GAPPS=true
 export TARGET_GAPPS_ARCH=arm64
 export TZ=Asia/Ho_Chi_Minh #put before last build command
-make bacon
+m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
