ac02aa8428be823ea08da786731d019c4d8e13ca
w1zzyl <96798575+w1zzyl@users.noreply.github.com>
2022-04-29 10:16:50 +0200

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch evolution_onclite-user
+lunch evolution_onclite-userdebug
 export TZ=Europe/Madrid #put before last build command
 mka evolution
 
