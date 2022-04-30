e05eb5d9e3dd6e1940904277fdc421d799b82c7e
Hitesoni | ×͜× <72127977+hsx02@users.noreply.github.com>
2022-05-01 01:55:08 +0530

    Fix last failed

---
+++ build_rom.sh
@@ -0,0 +1,15 @@
+# sync rom
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/hsx02/Local-Manifests.git --depth 1 -b awkn .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
+# build rom
+source build/envsetup.sh
+lunch awaken_mi439-userdebug
+export TZ=Asia/Dhaka #put before last build command
+make bacon
+
+# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+
+
