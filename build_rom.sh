9b5a2db8d71be33a09b2c1cd371ddefbae4af680
Jake <yapjakebrian1@gmail.com>
2022-04-30 12:21:46 +0800

    Add neverallow flag

--- build_rom.sh
+++ build_rom.sh
@@ -8,7 +8,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch aosp_mi439-userdebug
-# export SELINUX_IGNORE_NEVERALLOWS=true
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
