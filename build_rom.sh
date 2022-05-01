bff7ec39dca787e0c5af05bf383ba9f5533a04b7
Mika Haluku <104668518+mika1zumi@users.noreply.github.com>
2022-05-01 08:45:13 +0700

    Nad_X00T 4.19
    Yess yessssss

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,15 @@
 # sync roms
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android -b lineage-17.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Tiktodz/local_manifest.git --depth 1 -b 10 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/NusantaraProject-ROM/android_manifest -b 12 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/mika1zumi/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build roms
 source build/envsetup.sh
-lunch lineage_X00T-userdebug
-export BUILD_USERNAME=tiktod
+lunch nad_X00T-userdebug
+export BUILD_USERNAME=mika1zumi
+export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Tokyo #put before last build command
-mka bacon
+make nad
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
