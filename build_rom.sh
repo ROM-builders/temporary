9202b133817c2e1876e075eb4cf2c8d91c2aaac8
Nabil Zaman <94600437+iamnabilzaman@users.noreply.github.com>
2022-05-01 16:16:50 +0600

    m20lte ArrowOS iamnabilzaman

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Corvus-R/android_manifest.git -b 12-test -g default,-mips,-darwin,-notdefault
-git clone https://github.com/iamnabilzaman/local_manifest_m20lte --depth 1 -b android-12.0 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-12.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/iamnabilzaman/local_manifest_m20lte --depth 1 -b android-12.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch corvus_m20lte-userdebug
+lunch arrow_m20lte-userdebug
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
 export BUILD_USERNAME=nabil
 export TZ=Asia/Dhaka #put before last build command
-make corvus
+m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
