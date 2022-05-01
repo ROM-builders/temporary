bbd35ad13692175993ac21812dff88253c32511d
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2022-05-01 10:47:29 +0530

    update blocksize

--- build_rom.sh
+++ build_rom.sh
@@ -13,10 +13,10 @@ export BUILD_HOSTNAME=rushiranpise
 export SELINUX_IGNORE_NEVERALLOWS=true
 export WITH_GAPPS=true
 export TZ=Asia/Kolkata #put before last build command
-m aex
+m otapackage
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 # rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
 rclone copy out/target/product/chef/*.zip cirrus:chef/aex -P
 
-# 1
+# 3
