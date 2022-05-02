629e7ebac978cde8645a4f6da96653897f66d6ef
STRK-NDatus5 <raj15400881@gmail.com>
2022-05-02 18:56:11 +0530

    CorvFy

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/STRK-ND/local-manifest.git --depth 1 -b arrow .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) 
+repo init --depth=1 --no-repo-verify -u https://github.com/Corvus-R/android_manifest.git -b 12-test -g default,-mips,-darwin,-notdefault
+git clone https://github.com/STRK-ND/local-manifest.git --depth 1 -b corvus .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-. build/envsetup.sh
-lunch arrow_X00TD-userdebug
-export ARROW_GAPPS=true
+source build/envsetup.sh
+lunch corvus_X00TD-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export SKIP_ABI_CHECKS=true
-export TZ=Asia/Dhaka #put before last build command
-m bacon
+export USE_GAPPS=true
+make corvus
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
