0cfc5179d39f1deed91256227babf2f8cadddf48
nohaxrobot <97295772+nohaxrobot@users.noreply.github.com>
2022-05-03 10:40:56 +0530

    fic

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/aex-tmp/manifest.git -b 12.1.x -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/aex-tmp/manifest.git -b 12.1.x -g default,-mips,-darwin,-notdefault
 git clone https://github.com/nohaxrobot/Local-Manifests --depth 1 -b elixir repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
