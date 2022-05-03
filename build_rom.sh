95e03a6a15aeeec62d723560deab1c624168a7a0
Kneba <abenkenary3@gmail.com>
2022-05-03 15:51:59 +0700

    fix manifests

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch fluid_X00TD-userdebug
-export TZ=Asia/Shanghai #put before last build command
+export TZ=Asia/Shanghai #put this before last build command
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
