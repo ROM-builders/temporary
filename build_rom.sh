b05693adf27fb3f5e08e3429bb57df84a6475746
Wayne <99174654+ugly-saifu@users.noreply.github.com>
2022-05-03 09:42:00 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/ProjectRadiant/manifest -b twelve -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/projectarcana-aosp/manifest -b 12.x -g default,-mips,-darwin,-notdefault
 git clone https://github.com/ugly-saifu/local_manifest.git -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch radiant_X01AD-userdebug
+lunch aosp_X01AD-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
