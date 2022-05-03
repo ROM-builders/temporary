4465d6b10b492e82399bd68f5949d92031bc7bfe
Neion <78258079+aanneion@users.noreply.github.com>
2022-05-03 22:50:58 +0600

    inti spes

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-Pixelish/manifest -b twelve -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b 12.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/aanneion/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_r5x-userdebug
+lunch awaken_spes-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
