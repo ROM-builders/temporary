b79c5ad1499f6ebdb2df653296b395a21e10dd0e
ritvik-ch <102162683+ritvik-ch@users.noreply.github.com>
2022-05-04 14:13:29 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch conquer_vince-user
+lunch conquer_vince-userdebug
 export TZ=Asia/Kolkata #time
 make carthage
 
