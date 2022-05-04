81c5a3f2519bbda76b1569006deb4bf1f8e72668
SunnyM <sunnyteja201@gmail.com>
2022-05-04 13:36:12 +0530

    RMX2030

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/JaswantTeja/r5x-Tree-Manifest.git -b awaken .repo/local_manifests
+git clone https://github.com/JaswantTeja/r5x-Tree-Manifest.git -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
