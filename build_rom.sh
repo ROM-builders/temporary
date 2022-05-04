d31fe2903d1b340977d708f3083bfedaa4060eba
ritvik-ch <102162683+ritvik-ch@users.noreply.github.com>
2022-05-05 01:11:46 +0530

    Los

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch lineage_vince-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true 
-export TZ=Asia/Kolkata #put before last build command
+export TZ=Asia/Kolkata 
 brunch vince
 
 
