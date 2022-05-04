2b5f7044d80f8a89596e18ac29d0b8872e3bff87
ritvik-ch <102162683+ritvik-ch@users.noreply.github.com>
2022-05-04 22:03:24 +0530

    Initial Build

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,14 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest -b 12l -g default,-mips,-darwin,-notdefault
-git clone https://github.com/AbrarNoob/local_manifest.git --depth 1 -b voltage .repo/local_manifests
+git clone https://github.com/ritvik-ch/local_manifest.git --depth 1 -b voltage-12.x .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 
 # build rom
 source build/envsetup.sh
-lunch voltage_alioth-userdebug
-export TZ=Asia/Dhaka #put before last build command
-make bacon
+lunch voltage_vince-user
+export TZ=Asia/Kolkata #put before last build command
+brunch vince
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
