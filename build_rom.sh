99911a889f7d4557118a623df0a3e4396cf083a3
JaswantTeja <sunnyteja201@gmail.com>
2022-05-05 21:02:33 +0530

    Realmeme Triggger

--- build_rom.sh
+++ build_rom.sh
@@ -5,8 +5,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch awaken_r5x-userdebug
-export TZ=Asia/Kolkata #put before last build command
+lunch awaken_r5x-user
+export TZ=Asia/Dhaka #put before last build command
 make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
