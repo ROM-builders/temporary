91e3b6806e6b10828600ee24ac121c496f9f11ed
RasyidAlKautsar <rasyid2007alkautsar@gmail.com>
2022-05-06 11:25:33 +0700

    try fix stuck in Mi Logo

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b snow -g default,-mips,-darwin,-notdefault
-git clone https://github.com/RasyidAlKautsar/local_manifest.git --depth 1 -b snow .repo/local_manifests
+git clone https://github.com/RasyidAlKautsar/local_manifest.git --depth 1 -b awaken .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -9,7 +9,6 @@ lunch evolution_mido-userdebug
 export SKIP_ABI_CHECKS=true
 export TZ=Asia/Dhaka #put before last build command
 mka evolution
-
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
