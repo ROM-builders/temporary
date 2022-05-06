cfaeb5b91179b942402be07d718ca1500a8965a1
susandahal <susandahal53@gmail.com>
2022-05-06 15:19:50 +0545

    Build userdebug

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch pixys_surya-user
+lunch pixys_surya-userdebug
 #export BUILD_WITH_GAPPS=true
 export TZ=Asia/Kathmandu #put before last build command
 mka pixys
