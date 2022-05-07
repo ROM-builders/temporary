980e55d95f63a5147b6822e38ee3b1ecc36fe9d4
Lynx <100063196+Lynx041@users.noreply.github.com>
2022-05-07 23:01:01 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,8 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
  
 # build rom
 . build/envsetup.sh
-lunch blaze_ulysse-eng
-export SELINUX_IGNORE_NEVERALLOWS=true
+lunch blaze_ulysse-userdebug
 export TZ=Asia/Manila #put before last build command
 brunch ulysse
  
