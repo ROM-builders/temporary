7aae125fba3140bb4dce9a2cc2c066ce0082fafc
ritvik-ch <102162683+ritvik-ch@users.noreply.github.com>
2022-05-07 22:18:45 +0530

    Fixed Zram

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch voltage_vince-user
-export WITH_GAPPS=true
 export TZ=Asia/Kolkata
 brunch vince
 
