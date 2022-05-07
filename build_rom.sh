e6904288e63b49a3c856e96c54c7496d5d1b2ee1
AtoZ <63843048+anyinglulu1@users.noreply.github.com>
2022-05-07 14:46:45 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/anyinglulu1/local_manifest --depth 1 -b lineage .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android -b lineage-19.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/anyinglulu1/local_manifest.git --depth 1 -b aex .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch lineage_whyred-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
-export SKIP_ABI_CHECKS=true
+export SELINUX_IGNORE_NEVERALLOWS= true
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
