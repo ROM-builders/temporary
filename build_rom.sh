84f6022b6630fdb83f80f764e5cbe0644b48474d
lazar <93397127+lazrdev@users.noreply.github.com>
2022-05-07 12:23:35 +0200

    let's update to 12.1

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PotatoProject/manifest.git -b frico_mr1-release -g default,-mips,-darwin,-notdefault
-git clone https://github.com/lazrdev/local_manifest.git --depth 1 -b potato-12.1 .repo/local_manifests
+git clone https://github.com/lazrdev/local_manifest.git --depth 1 -b potato-12-mi439 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
