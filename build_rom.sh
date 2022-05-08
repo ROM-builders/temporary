a88b82343f79dc04b3b918cb7fa6a0bb82e60770
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2022-05-08 08:42:18 +0530

    some build flags

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,13 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_chef-userdebug
-export TZ=Asia/Dhaka #put before last build command
+export TARGET_FLOS=true
+export WITH_GMS=false
+export KBUILD_BUILD_USER=rushiranpise
+export KBUILD_BUILD_HOST=rushiranpise
+export BUILD_USERNAME=rushiranpise
+export BUILD_HOSTNAME=rushiranpise
+export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
