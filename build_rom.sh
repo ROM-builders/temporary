5e37aa9919dc479de6de25f0ad79146bc4952acd
RasyidAlKautsar <rasyid2007alkautsar@gmail.com>
2022-05-08 12:29:32 +0700

    try again

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b twelve-one -g default,-mips,-darwin,-notdefault
 git clone https://github.com/RasyidAlKautsar/local_manifest.git --depth 1 -b cherish-12 .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 -j1 --fail-fast
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
