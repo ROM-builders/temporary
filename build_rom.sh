af1b9b108dec9f15a4aadb66098e07f7e58791ba
rahul <rahul-k@bigdi.cc>
2022-05-09 15:54:41 +0100

    vince: build user variant with gapps

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch aosp_vince-userdebug
+lunch aosp_vince-user
+export WITH_GAPPS=true
 export TZ=Asia/Dhaka #put before last build command
 m aex
 
