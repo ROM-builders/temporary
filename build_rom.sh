af42f9c0b9458658d71a5be6c0e35bd7841607e9
Lynx <100063196+Lynx041@users.noreply.github.com>
2022-05-10 00:19:42 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,17 +1,46 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
+
+repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
+
 git clone https://github.com/Lynx041/local_manifest.git --depth 1 -b ulysse .repo/local_manifests
+
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
  
+
 # build rom
+
 . build/envsetup.sh
+
 lunch blaze_ulysse-userdebug
+
+#export WITH_GAPPS=true
+
 export SELINUX_IGNORE_NEVERALLOWS=true
-export TZ=Asia/Manila #put before last build command
+
+export TZ=Asia/Dhaka #put before last build command
+
 brunch ulysse
+
  
+
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+
+
+
+
+ 
+
+
+
+
+
+
+ 
+
+
 
 
 
