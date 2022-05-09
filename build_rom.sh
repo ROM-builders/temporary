07c0dbab3d88a28febef01f07c8deee25994f67e
Shridhan Varadkar <shridhan98@gmail.com>
2022-05-09 09:54:08 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,10 +1,12 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest.git -b 12l -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Shreedhan003/Local_Manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/ImPrashantt/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
+lunch voltage_lavender-userdebug
+export TZ=Asia/Dhaka #put before last build command
 brunch lavender
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
