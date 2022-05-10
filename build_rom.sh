fe3f95ce0ba0d711a17277a0411f3dfa82f129e5
swapnil vicky <74442290+SwapnilVicky@users.noreply.github.com>
2022-05-10 21:16:13 +0530

    fix bootloop

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch bliss_gram-userdebug
+lunch bliss_gram-eng
 export TZ=Asia/Dhaka #put before last build command
 blissify -v gram
 
