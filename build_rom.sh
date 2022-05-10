bb94e1490fb9e5a0959d1829e0dd87f123aa9001
A50-Victor <80767544+victor4cris@users.noreply.github.com>
2022-05-10 08:53:49 +0300

    Pixel OS

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelPlusUI-SnowCone/manifest -b snowcone-12.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-Pixelish/manifest -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/victor4cris/local_manifest --depth 1 -b S10 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
 export TZ=Asia/Kolkata #put before last build command
-lunch aosp_beyond2lte-userdebug
+lunch aosp_beyond1lte-userdebug
 make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
