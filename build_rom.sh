243da9b35693eacdca5f5f603c7be190431ad2b5
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2022-05-11 09:01:17 +0530

    fix nightlight

--- build_rom.sh
+++ build_rom.sh
@@ -14,7 +14,7 @@ export CUSTOM_BUILD_TYPE=OFFICIAL
 export PPUI_MAINTAINER=rushiranpise
 export IS_PHONE=true
 export TZ=Asia/Kolkata #put before last build command
-mka otapackage
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 # rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
