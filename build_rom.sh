bea3540aef0288a29accd28fa03c77a84366aadd
Stealth1226 <rishawniyer06@gmail.com>
2022-05-11 20:30:11 +0530

    init aospa

--- build_rom.sh
+++ build_rom.sh
@@ -1,17 +1,15 @@
 # sync rom
-repo init --depth=1 -u https://github.com/lighthouse-os/manifest.git -b sailboat_L1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Stealth1226/local_manifest --depth 1 -b sailboat-miatoll .repo/local_manifests
+repo init --depth=1 -u https://github.com/AOSPA/manifest.git -b sapphire -g default,-mips,-darwin,-notdefault
+git clone https://github.com/pa-stelt/local_manifest --depth 1 -b sapphire .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-export WITH_GAPPS=true
-lunch lighthouse_miatoll-userdebug
-make lighthouse
+lunch aospa_miatoll-userdebug
+./rom-build.sh miatoll -t userdebug
 
 
 # upload rom
 # If you need to upload json/multiple files too then put like this 'rclone copy out/target/product/mido/*.zip cirrus:mido -P  rclone copy out/target/product/mido/*.zip.json cirrus:mido -P'
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-rclone copy out/target/product/miatoll/*miatoll*json cirrus:miatoll -P
 
