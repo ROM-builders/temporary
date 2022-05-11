c87ff17dff98146a752951c5ddd24b2b3a700b9f
GhostMaster69-dev <rathore6375@gmail.com>
2022-05-11 20:28:13 +0530

    Fork-Krypton
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/FORK-Krypton/manifest.git -b A12 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Fork-Krypton/manifest.git -b A12 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/GhostMaster69-dev/local_manifest.git --depth 1 -b krypton-12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
