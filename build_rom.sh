d0edb8503756356ae543dc14f55fcc0ec23d0b8b
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2022-05-12 17:11:31 +0530

    chef-DotOS-dot12.1-rushiranpise

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,19 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/DotOS/manifest.git -b dot12.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/rushiranpise/local_manifest.git --depth 1 -b dot .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom 1
 source build/envsetup.sh
-lunch derp_vayu-user
+lunch dot_chef-userdebug
+export KBUILD_BUILD_USER=rushiranpise
+export KBUILD_BUILD_HOST=rushiranpise
+export BUILD_USERNAME=rushiranpise
+export BUILD_HOSTNAME=rushiranpise
 export TZ=Asia/Dhaka #put before last build command
-mka derp
+mka otapackage
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
-rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+# rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+rclone copy out/target/product/chef/*.zip cirrus:chef/dot -P
+rclone copy out/target/product/chef/boot.img cirrus:chef/dot -P
