1df5a10f2e6524ff074bccac9fff06b1f00daaf9
w1zzyl <96798575+w1zzyl@users.noreply.github.com>
2022-05-12 13:48:41 +0200

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,7 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b twelve-one -g default,-mips,-darwin,-notdefault
 git clone https://github.com/w1zzylvayustuff/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
- 
+
 # build rom
 source build/envsetup.sh
 lunch cherish_vayu-user
