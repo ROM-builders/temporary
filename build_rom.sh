6960ca5b12965ae9a6e042c7e31d23e4f51eb9ca
RasyidAlKautsar <rasyid2007alkautsar@gmail.com>
2022-05-12 18:41:16 +0700

    test [skip ci]

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch spark_mido-user
-export SPARK_BUILD_TYPE=MONTHLY
+export SPARK_BUILD_TYPE=OFFICIAL
 export BUILD_HOSTNAME=RasyidAlKautsar
 export BUILD_USERNAME=RasyidAlKautsar
 export TZ=Asia/Jakarta #put before last build command
@@ -14,5 +14,3 @@ mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-
-
