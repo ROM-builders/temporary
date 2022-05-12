acbfb3da3f0def08080553fa61f72ff17d8306d1
Abhiram <103818282+Azure-Helper@users.noreply.github.com>
2022-05-12 19:34:22 +0530

    Retry

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest -b sap
 git clone https://github.com/Azure-Helper/local_manifests.git --depth 1 -b old .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom [16]
+# build rom [17]
 source build/envsetup.sh
 lunch aospa_raphael-eng
 export ALLOW_MISSING_DEPENDENCIES=true
