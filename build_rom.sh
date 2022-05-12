93a4ad313f4c2b1b617e9cc222db62882f2e31ca
w1zzyl <96798575+w1zzyl@users.noreply.github.com>
2022-05-12 17:42:48 +0200

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b twelve-one -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/ForkLineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/w1zzylvayustuff/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
  
 # build rom
 source build/envsetup.sh
-lunch cherish_vayu-user
+lunch lineage_vayu-user
 export KBUILD_BUILD_USER=w1zzyl
 export KBUILD_BUILD_HOST=Ryzen
 export BUILD_USERNAME=w1zzyl
