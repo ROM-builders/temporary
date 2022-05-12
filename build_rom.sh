b4081f11cb82cb8e8d1b8e0b5aa0a4d2cb13dbf9
A50-Victor <80767544+victor4cris@users.noreply.github.com>
2022-05-12 17:36:27 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,19 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 export TZ=Asia/Kolkata #put before last build command
-lunch aosp_beyond0lte-userdebug
-make bacon
 lunch aosp_beyond1lte-userdebug
-make bacon
-lunch aosp_beyond2lte-userdebug
-make bacon
-lunch aosp_beyondx-userdebug
-make bacon
-lunch aosp_d1-userdebug
-make bacon
-lunch aosp_d2s-userdebug
-make bacon
-lunch aosp_d2x-userdebug
 make bacon
 
 
