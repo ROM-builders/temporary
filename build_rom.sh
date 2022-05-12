0e01cd80bac9cea0c88963595183057eebc8bc98
clhex <100755123+clhexftw@users.noreply.github.com>
2022-05-12 12:05:54 +0530

    vayu: initial voltage build

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,13 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest -b 12l -g default,-mips,-darwin,-notdefault
-git clone https://github.com/AbrarNoob/local_manifest.git --depth 1 -b voltage .repo/local_manifests
+git clone https://github.com/clhexvayulab/local_manifest.git --depth 1 -b vayu_voltage .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 
 # build rom
 source build/envsetup.sh
-lunch voltage_alioth-userdebug
+lunch voltage_vayu-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
