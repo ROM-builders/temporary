da3d22368bb2321bc1bb3d564bc00806e01f845b
rahul <rahul-k@bigdi.cc>
2022-05-12 20:16:54 +0100

    vince: User build with may patch

--- build_rom.sh
+++ build_rom.sh
@@ -5,8 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom 
 source build/envsetup.sh
-lunch lineage_vince-eng
-export SELINUX_IGNORE_NEVERALLOWS=true
+lunch lineage_vince-user
 export TZ=Asia/Dhaka #put before last build command
 brunch vince
 
