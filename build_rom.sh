46d7edf4970a1c6557594fa92cbf6238b4366051
swapnil vicky <74442290+SwapnilVicky@users.noreply.github.com>
2022-05-12 18:15:51 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/BlissRoms/platform_ma
 git clone https://github.com/SwapnilVicky/local_manifest.git --depth 1 -b 12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom#
+# build rom
 . build/envsetup.sh
 lunch bliss_gram-userdebug
 export TZ=Asia/Dhaka #put before last build command
