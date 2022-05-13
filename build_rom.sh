c61a435519deed4bd306cc158725b825eea69c79
Matheus Souza <33396386+matheucomth@users.noreply.github.com>
2022-05-13 19:34:11 -0300

    set ALLOW_MISSING_DEPENDENCIES to true

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
+lunch aosp_tulip-userdebug
+export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
