4c2631afa870c1ad04288155952382b8e245a3cc
clhex <100755123+clhexftw@users.noreply.github.com>
2022-05-13 17:01:56 +0530

    vayu: may patch test

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lighthouse_vayu-user
+lunch lighthouse_vayu-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
 make lighthouse
      
