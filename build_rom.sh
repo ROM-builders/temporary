0c528b7ec1371eb9de38426c16368e83f14c8071
Fr0ztyy43 <82140208+Fr0ztyy43@users.noreply.github.com>
2022-05-14 13:01:09 +0200

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/AospExtended/manifest.git -b 12.1.x -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Fr0ztyy43/local_manifest1/blob/main/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
