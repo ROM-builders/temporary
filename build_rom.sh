8ed57f2253f7821feb2752aa153ad5ea6f04ec0c
Matheus Souza <33396386+matheucomth@users.noreply.github.com>
2022-05-14 11:11:01 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.git -b twelvedotone -g default,-mips,-darwin,-notdefault
-git clone https://github.com/matheucomth/local_manifest --depth 1 -b radiant .repo/local_manifests
+git clone https://github.com/matheucomth/local_manifest.git --depth 1 -b radiant .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
