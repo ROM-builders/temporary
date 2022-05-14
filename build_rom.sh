d86e92c0f16a9df6d6ea53c08ada3888f7016ed2
matheucomth <matheussouzas65@outlook.com>
2022-05-14 00:15:08 -0300

    set SELINUX_IGNORE_NEVERALLOWS to true

--- build_rom.sh
+++ build_rom.sh
@@ -5,6 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
+export SELINUX_IGNORE_NEVERALLOWS=true
 lunch superior_tulip-userdebug
 export TZ=Asia/Dhaka #put before last build command
 m bacon
