07fbda27d7c4f27413cb98c8082b8cefd97d3b8f
Mayur <flynryder427@gmail.com>
2022-05-14 16:25:09 +0530

    build userdebug

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch p404_lavender-eng
+lunch p404_lavender-userdebug
 export WITH_GMS=true
 export TZ=Asia/Kolkata #put before last build command
 make bacon
