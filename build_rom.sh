6f6d191d21f0de8f112bb389de7b52a2a261521b
Shridhan Varadkar <shridhan98@gmail.com>
2022-05-14 16:01:19 +0530

    aicp repo changes

--- build_rom.sh
+++ build_rom.sh
@@ -1,10 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/AICP/platform_manifest.git -b r11.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/AICP/platform_manifest.git -b r11.1 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/Shreedhan003/local_manifest.git --depth 1 -b aicp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
+lunch aicp_lavender-userdebug
 export TZ=Asia/Delhi #put before last build command
 brunch lavender
 
