84de059c0414b4fe5e1f8ae6fc34b81dad217225
Shridhan Varadkar <shridhan98@gmail.com>
2022-05-14 10:48:57 +0000

    los lavender

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/AICP/platform_manifest.git -b r11.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Shreedhan003/local_manifest.git --depth 1 -b aicp .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-17.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Shreedhan003/local_manifest.git --depth 1 -b LOS .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-. build/envsetup.sh
-lunch aicp_lavender-userdebug
-export TZ=Asia/Delhi #put before last build command
-brunch lavender
+source build/envsetup.sh
+lunch lineage_lavender-userdebug
+export TZ=Asia/kolkata #put before last build command
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
