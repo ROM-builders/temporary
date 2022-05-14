a2f4d321d118c045ad1b361206b5e12c63a5a53a
SunnyM <104504298+youngstill@users.noreply.github.com>
2022-05-14 20:02:18 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b spark -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/ProjectStreak/platform_manifest -b twelve.one -g default,-mips,-darwin,-notdefault
 git clone https://github.com/youngstill/r5x-Tree-Manifest.git -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
-lunch spark_r5x-userdebug
+. build/envsetup.sh
+lunch streak_r5x-userdebug
 export TZ=Asia/Kolkata #put before last build command
-mka spark
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
