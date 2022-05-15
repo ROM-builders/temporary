fd28f7d4e249bed99c55fb3670f33cbb5838adf8
Matheus Souza <33396386+matheucomth@users.noreply.github.com>
2022-05-15 00:42:46 -0300

    build syberia project

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.git -b twelvedotone -g default,-mips,-darwin,-notdefault
-git clone https://github.com/matheucomth/local_manifest.git --depth 1 -b radiant .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/syberia-project/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/matheucomth/local_manifest.git --depth 1 -b syberia .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -8,9 +8,9 @@ source build/envsetup.sh
 export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export RELAX_USES_LIBRARY_CHECK=true
-lunch superior_tulip-userdebug
+lunch syberia_tulip-userdebug
 export TZ=Asia/Dhaka #put before last build command
-m bacon
+brunch tulip
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
