7931a19e32a27f9817a8ae35fa2d5b392678991e
Abhiram <103818282+Azure-Helper@users.noreply.github.com>
2022-05-15 23:46:21 +0530

    Fix manifest

--- build_rom.sh
+++ build_rom.sh
@@ -5,9 +5,9 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom [28]
 source build/envsetup.sh
-lunch aospa_raphael-userdebug
+lunch aospa_raphael-eng
 export TZ=Asia/Dhaka #put before last build command
-./rom-build.sh raphael -t userdebug
+./rom-build.sh raphael -t eng
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
