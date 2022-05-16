6d10eb2d03bc828668a4aa408eecf9247262459f
Subham-XD <101913389+Subham-XD@users.noreply.github.com>
2022-05-16 18:16:27 +0530

    Asus Max -PE

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/PixelExperience/manifest -b twelve -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b twelve-plus -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Subham-XD/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_X01AD-user
+lunch aosp_X01AD-userdebug
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
