aad8babf6c4158585e02609ea64b8df38d687cdb
Mayur <flynryder427@gmail.com>
2022-05-16 15:35:13 +0530

    Pe lavender

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/xenstuff/manifest --depth=1 -b pe3 .repo/local_manifests
+git clone https://github.com/XenStuff/manifest --depth=1 -b pe3 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -9,7 +9,5 @@ lunch aosp_lavender-user
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
-# Dummy Parts to trigger builds
-# 5tg build
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
