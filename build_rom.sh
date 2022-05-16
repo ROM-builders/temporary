14e3b9c85fd1e81a68555723a84a389874cc10f9
Abhiram <103818282+Azure-Helper@users.noreply.github.com>
2022-05-17 04:47:39 +0530

    Build userdebug

--- build_rom.sh
+++ build_rom.sh
@@ -3,11 +3,11 @@ repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest -b sap
 git clone https://github.com/Azure-Helper/local_manifests.git --depth 1 -b pa .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom [33]
+# build rom [34]
 source build/envsetup.sh
-lunch aospa_raphael-eng
+lunch aospa_raphael-userdebug
 export TZ=Asia/Dhaka #put before last build command
-./rom-build.sh raphael -t eng -v beta
+./rom-build.sh raphael -t userdebug
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
