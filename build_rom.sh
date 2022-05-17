8966df7fb5ba9c7b8bdf8380db516477c92ded94
Sayan Sarkar <97832667+SKORPION29@users.noreply.github.com>
2022-05-17 21:02:59 +0530

    Build ProjectBlaze

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,14 @@
 # sync rom
-repo init --depth=1 -u https://github.com/lighthouse-os/manifest.git -b sailboat_L1 -g default,-mips,-darwin,-notdefault
+repo init -u https://github.com/ProjectBlaze/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/SKORPION29/local_manifest.git --depth 1 -b main .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c -j$(nproc --all) --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune
 
 # build rom
 source build/envsetup.sh
-lunch lighthouse_miatoll-userdebug
+lunch blaze_miatoll-userdebug
+export WITH_GAPPS=true
 export TZ=Asia/Dhaka #put before last build command
-make lighthouse
+brunch miatoll
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
