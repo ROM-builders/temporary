0bddb41f49b699eed5d480661b6d0f8d77562258
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2022-05-17 10:20:53 +0530

    change trees

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/DotOS/manifest.git -b dot12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/rushiranpise/local_manifest.git --depth 1 -b dot .repo/local_manifests
+git clone https://github.com/rushiranpise/local_manifest.git --depth 1 -b dotlos .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom 1
@@ -10,8 +10,9 @@ export KBUILD_BUILD_USER=rushiranpise
 export KBUILD_BUILD_HOST=rushiranpise
 export BUILD_USERNAME=rushiranpise
 export BUILD_HOSTNAME=rushiranpise
+export WITH_GAPPS=false
 export TZ=Asia/Dhaka #put before last build command
-mka otapackage
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 # rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
