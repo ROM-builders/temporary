efacefb670c4a070a04d2272074fb6296de0e35b
Kendras056 <88943971+Kendras056@users.noreply.github.com>
2022-05-17 14:01:39 +0700

    X00TD-syberia-project-12.1-Kendras056

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Kendras056/local_manifests.git --depth 1 -b eleven .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/syberia-project/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Kendras056/local_manifests.git --depth 1 -b 12.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-breakfast lineage_X00TD-userdebug
+lunch syberia_X00TD-userdebug
 export BUILD_USERNAME=morbius
 export TZ=Europe/Brussels #put before last build command
-croot
-brunch X00TD
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
