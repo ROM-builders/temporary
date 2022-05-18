4752eec0c8614d30714ab1b75ab990229455925c
Prashant <93838092+ImPrashantt@users.noreply.github.com>
2022-05-18 07:59:09 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch lineage_lavender-userdebug
+
 export TZ=Asia/Dhaka #put before last build command
 brunch lavender
 
