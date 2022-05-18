3777ba2c2367d225ffe6048c80962efeb0689b62
Prashant <93838092+ImPrashantt@users.noreply.github.com>
2022-05-18 08:01:24 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-
+export WITH_GMS=true
 export TZ=Asia/Dhaka #put before last build command
 brunch lavender
 
