6b0882fbb7e6dc2cae5a5d9bd307388857c8d06a
Hitesoni | ×͜× <72127977+hsx02@users.noreply.github.com>
2022-05-18 18:56:55 +0530

    Test build

--- build_rom.sh
+++ build_rom.sh
@@ -10,7 +10,8 @@ export TARGET_FLOS=true
 export WITH_GMS=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Jakarta #put before last build command
-brunch Mi439
+M
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
