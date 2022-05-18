7521bf2ed09574ee97e3e015f78f9c48ef4602a6
SupFlashS <104255518+SupFlashS@users.noreply.github.com>
2022-05-18 17:35:39 +0400

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/SupFlashS/local_manifest.git --depth 1 .repo/local_
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom 
-. build/envsetup.sh
+source build/envsetup.sh
 lunch arrow_lava-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export WITH_GAPPS=true
