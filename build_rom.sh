8799d7f3ab30e9b2b3a40da3300867c585bb6db5
takumi021 <omrameshpatel@gmail.com>
2022-05-19 11:43:20 +0000

    Clone Sepolicy repos from pe

--- build_rom.sh
+++ build_rom.sh
@@ -2,6 +2,10 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/CipherOS/android_manifest.git -b twelve-L -g default,-mips,-darwin,-notdefault
 git clone https://github.com/takumi021/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+rm -rf device/qcom/sepolicy
+rm -rf device/qcom/sepolicy_vndr
+git clone https://github.com/PixelExperience/device_qcom_sepolicy device/qcom/sepolicy
+git clone https://github.com/PixelExperience/device_qcom_sepolicy_vndr device/qcom/sepolicy_vndr
 
 # build rom
 . build/envsetup.sh
