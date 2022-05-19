061b99ba9310a413e39209d2090a6f6edc4b35b6
ritvik-ch <102162683+ritvik-ch@users.noreply.github.com>
2022-05-19 11:28:22 +0530

    Userdebug

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch voltage_vince-user
+lunch voltage_vince-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Kolkata #tz
 make bacon
 
