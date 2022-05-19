dc710273ab13a269aff1b10a1174e706648e0a6e
Kendras056 <88943971+Kendras056@users.noreply.github.com>
2022-05-19 18:18:41 +0700

    user build

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch syberia_X00TD-userdebug
+lunch syberia_X00TD-user
 export BUILD_USERNAME=morbius
 export TZ=Europe/Moscow #put before last build command
 mka bacon
