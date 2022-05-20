57414ba44c78f2f3adbd981f17c0dcf815ed1e34
ItsAdityan <nayakaditya0530@gmail.com>
2022-05-20 07:28:27 +0530

    Build Blaze

--- build_rom.sh
+++ build_rom.sh
@@ -4,13 +4,12 @@ git clone https://github.com/ItsAdityan/local_manifest.git --depth 1 -b blaze .r
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-. build/envsetup.sh
-lunch blaze_vince-userdebug
+source build/envsetup.sh
 #export WITH_GAPPS=true
 #export TARGET_GAPPS_ARCH=arm64
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Kolkata #put before last build command
-brunch vince
+brunch blaze_vince-userdebug
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
