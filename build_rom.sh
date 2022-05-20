cec4e06af10b14cf233368d95a90d04494b701bb
Ayan <79016495+AYAN98542@users.noreply.github.com>
2022-05-20 12:06:34 +0530

    juice-yaap-twelve-AYAN98542

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/yaap/manifest.git -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ayan98542/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/AYAN98542/Local-Manifests.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
