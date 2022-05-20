407cc4796bd14c8605ccc6fb53288dc048030f9c
Mayur <flynryder427@gmail.com>
2022-05-20 15:56:56 +0530

    Try to fix ximi parts

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/P-404/android_manifest -b shinka -g default,-mips,-darwin,-notdefault
-git clone https://github.com/XenStuff/manifest --depth=11 -b 404 .repo/local_manifests
+git clone https://github.com/xenxynon-lab-404 --depth=1 -b 404 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -10,6 +10,6 @@ export WITH_GMS=true
 export TZ=Asia/Kolkata #put before last build command
 make bacon
 
-# 12
+# 13
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
