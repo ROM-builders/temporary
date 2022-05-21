66075dfa52bf54bba8f4271be9e1e6f7ddb10a35
vytautas <vytautas.duchas@gmail.com>
2022-05-21 19:14:44 +0300

    hope sync normal now

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom
+  # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 12.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/vytska69/Local-Manifests.git --depth 1 -b crd8-mithorium .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
@@ -13,6 +13,7 @@ make bacon
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
 rclone copy out/target/product/mi439/mi439.json cirrus:mi439 -P
+
 
 
 
