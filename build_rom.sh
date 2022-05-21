f6132f3ee5b7922fb968dd78552f8d280b5df848
Suraj Kumar <105637531+imsurajxd@users.noreply.github.com>
2022-05-22 01:07:34 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init -u https://github.com/PixelExperience/manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/imsurajxd/local_manifest --depth=1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b twelve-plus -g default,-mips,-darwin,-notdefault
+git clone https://github.com/imsurajxd/local_manifest --depth=1 -b pe3 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
