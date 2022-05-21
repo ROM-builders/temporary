37e1dcc5b5ef04a49348b4f1546d09b3da3f63f0
Ritvik <102162683+ritvik-ch@users.noreply.github.com>
2022-05-21 13:51:23 +0530

    Ship SkiaGL

--- build_rom.sh
+++ build_rom.sh
@@ -5,8 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch voltage_vince-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
+lunch voltage_vince-user
 export TZ=Asia/Kolkata #tz
 make bacon
 
