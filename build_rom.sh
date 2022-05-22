9dfe22a9206853ea9e6ea7521b7ff8ee3ea59a82
Nischay Sanjay <nischay.sanjay23@gmail.com>
2022-05-23 00:04:04 +0530

    Fix aospa manifest syncing

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/AOSPA/manifest -b sapphire -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest -b sapphire -g default,-mips,-darwin,-notdefault
 git clone https://github.com/IceBreaker2451/local_manifest --depth 1 -b sapphire .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
