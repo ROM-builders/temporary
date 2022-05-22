ac74c600496a12b98e07064b2d359b613ca5d53c
T.B.S <ewprjkt@proton.me>
2022-05-22 11:31:24 +0700

    final surabaya

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
-# sync rom
+# sync roms
 repo init --depth=1 --no-repo-verify -u https://github.com/NusantaraProject-ROM/android_manifest -b 12 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Tiktodz/local_manifest.git --depth 1 -b nad .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build roms
 source build/envsetup.sh
 lunch nad_X00TD-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
