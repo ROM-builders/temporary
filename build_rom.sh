6fae58b6f91f2062275fecf33712227a7fe62ea0
Anantgoel0123456 <88424558+Anantgoel0123456@users.noreply.github.com>
2022-05-22 22:16:43 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/projectarcana-aosp/manifest -b 12.x -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Anantgoel0123456/local_manifest.git --depth 1 -b alioth .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Corvus-R/android_manifest.git -b 12-test -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Anantgoel0123456/local_manifest.git --depth 1 -b Corvus .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_alioth-userdebug
-export WITH_GAPPS=true
-export TZ=Asia/Manila #put before last build command
-make bacon
+lunch corvus_mido-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true 
+export TZ=Asia/Dhaka #put before last build command
+make corvus
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
