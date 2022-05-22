2bba63573b370b068693894c9fc106f8b89bacdb
vytautas <vytautas.duchas@gmail.com>
2022-05-22 14:03:41 +0300

    fix splash loop

--- build_rom.sh
+++ build_rom.sh
@@ -14,3 +14,4 @@ make bacon
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
 rclone copy out/target/product/Mi439/Mi439.json cirrus:mi439 -P
 
+
