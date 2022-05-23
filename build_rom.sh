9249f9dcb6ad71b98b6146af5ac2f8667f558998
Suraj Kumar <105637531+imsurajxd@users.noreply.github.com>
2022-05-23 21:33:41 +0530

    Update Device Code

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source=. build/envsetup.sh
-lunch xdroid_$devicecodename-userdebug
+lunch xdroid_lavender-userdebug
 export TZ=Asia/Kolkata #put before last build command
 make xd -j$(nproc --all)
 
