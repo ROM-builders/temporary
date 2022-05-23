d1405021c72c0f739ceff7b4db47ebeda0f777ab
the projects <84352313+theorchidproject@users.noreply.github.com>
2022-05-23 22:50:25 +0100

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom
+# sync rom fixed
 repo init --depth=1 --no-repo-verify -u git://LineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/theorchidproject/local_manifest.git --depth 1 -b test1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
