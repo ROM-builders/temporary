b6a87f924963e0ca656432c8cc1fbd0607c2e840
Ritvik <102162683+ritvik-ch@users.noreply.github.com>
2022-05-24 14:28:58 +0530

    Initial Build

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,13 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PotatoProject/manifest.git -b frico_mr1-release -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Kneba/local_manifests.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/ritvik-ch/local_manifest.git --depth 1 -b Posp-12.x .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch potato_X00TD-userdebug
-export BUILD_USERNAME=kneba
-export SELINUX_IGNORE_NEVERALLOWS=true
-export TZ=Europe/Moscow #put before last build command
-brunch X00TD
+lunch potato_vince-user
+export TZ=Asia/Jakarta #put before last build command
+brunch vince
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
