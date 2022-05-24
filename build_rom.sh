a14db87fc1e840c9413019f91026e6878614979f
Mayur <flynryder427@gmail.com>
2022-05-24 12:46:58 +0530

    Pixel rom again

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b twelve-plus -g default,-mips,-darwin,-notdefault
-git clone https://github.com/XenStuff/manifest --depth=1 -b pe .repo/local_manifests
+git clone https://github.com/Snowman2409/local-manifest --depth=1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_lavender-userdebug
+lunch aosp_lavender-user
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
