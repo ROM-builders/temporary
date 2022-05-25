85b07162e9d91a370dda951730d44105e6dc0282
T.B.S <ewprjkt@proton.me>
2022-05-25 14:27:05 +0700

    trigger built

--- build_rom.sh
+++ build_rom.sh
@@ -6,8 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch corvus_X00TD-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
-export SKIP_ABI_CHECKS=true
 export TZ=Asia/Shanghai #put before last build command
 make corvus
 
