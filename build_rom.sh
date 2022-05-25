c21ad0bcd077d1eafe08be2fbce04c9a54685cf1
67 <94082908+sweervin@users.noreply.github.com>
2022-05-25 20:32:10 +0300

    Update kernel and vendor modules

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manif
 git clone https://github.com/sweervin/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+
 . build/envsetup.sh
 export ARROW_GAPPS=true
 lunch arrow_laurel_sprout-userdebug
