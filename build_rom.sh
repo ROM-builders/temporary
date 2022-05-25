e9f41c428480e7f6ae59c1a8f6ee249240e54344
Ritvik <102162683+ritvik-ch@users.noreply.github.com>
2022-05-25 12:12:03 +0530

    Fix brunch

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/ritvik-ch/local_manifest.git --depth 1 -b Posp-12.x
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+. build/envsetup.sh
 lunch potato_vince-user
 export TZ=Asia/Jakarta #put before last build command
 brunch Vince
