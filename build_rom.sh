41a8fa0dbc7813951ac6456ff1acd01c33afae9b
Shridhan Varadkar <shridhan98@gmail.com>
2022-05-25 19:59:37 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch havoc_lavender-userdebug
-export TZ=Asia/Dhaka #put before last build command
+export TZ=Asia/Delhi #put before last build command
 brunch lavender
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
