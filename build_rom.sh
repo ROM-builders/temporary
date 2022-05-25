9652a9ec36b83c02b6eadfcc33fe74f9912dc811
orkunergun <104510596+orkunergun@users.noreply.github.com>
2022-05-25 20:54:51 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/r9lab/local_manifest.git --depth 1 -b arrow .repo/local_manifests
+git clone https://gitlab.com/ArrowOS_lava/local_manifest.git --depth 1 -b A12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
