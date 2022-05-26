0811c7153262e7700485910da349fc1a4eb7cb2a
Sayan Sarkar <97832667+SKORPION29@users.noreply.github.com>
2022-05-27 02:37:36 +0530

    Initial build

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest -b 12l -g default,-mips,-darwin,-notdefault
 git clone https://github.com/SKORPION29/local_manifest.git --depth 1 -b voltage .repo/local_manifests
-repo sync -c -j8 --force-sync --no-clone-bundle --no-tags
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom 
 source build/envsetup.sh
