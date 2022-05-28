6b7c527ee4d675e5e5ca9fab9ba0c549d2563ba1
Shridhan Varadkar <shridhan98@gmail.com>
2022-05-28 12:47:41 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest.git -b 12l -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Shreedhan003/local_manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/Shreedhan003/local_manifest.git --depth 1 -b voltage-official .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
