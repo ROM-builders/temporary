a351630a986a6d98871c3accf8367771c97eff4d
YogRaJSinGh <84258191+yograjfire18@users.noreply.github.com>
2022-05-28 18:53:10 +0530

    PE Z01R

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch aosp_Z01R-user
+lunch aosp_Z01R-userdebug
 export TZ=Asia/Kolkata 
 mka bacon
 
