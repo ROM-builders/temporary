069283f05d5fc0f8656bfd8b25fc0a06a3661372
Coptan⁹⁹ <omarcoptan9@gmail.com>
2022-05-29 00:46:04 +0200

    Pixys

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/PixysOS/manifest -b t
 git clone https://github.com/OmarAlCoptan/local_manifest.git --depth 1 -b pixys .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
    
-# build rom
+# build rom 
 source build/envsetup.sh
 lunch pixys_lavender-userdebug
 export TZ=Africa/Egypt #put before last build command
