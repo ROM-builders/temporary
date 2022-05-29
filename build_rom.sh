73fc7f1fca00a178a7672ad3a33b051b26571078
Pranshu Goyal <78131793+datnerdguy@users.noreply.github.com>
2022-05-29 20:00:01 +0530

    eng build

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch p404_whyred-userdebug
+lunch p404_whyred-eng
 export TZ=Asia/Dhaka #put before last build command
 export WITH_GAPPS=true #oof i am a fucking nuuu
 make bacon
