d5e726a409f46fd5676e77fcbc1a3fb5a098ea05
Julival Bittencourt <53379692+julival25@users.noreply.github.com>
2022-05-29 11:24:59 -0300

    Update branch

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/Spark-Rom/manifest.git -b spark -g default,-mips,-darwin,-notdefault
-git clone https://github.com/julival25/local_manifest.git --depth 1 -b spark-polaris .repo/local_manifests
+git clone https://github.com/julival25/local_manifest.git --depth 1 -b spark .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
