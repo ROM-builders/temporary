54ad397c5818985d47ef1248babeb6c8f2991bec
Ritvik <102162683+ritvik-ch@users.noreply.github.com>
2022-05-29 21:57:34 +0530

    Initial Dot

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,14 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/DotOS/manifest.git -b dot12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/yograjsingh-cmd/local_manifest.git --depth 1 -b dotos .repo/local_manifests
+git clone https://github.com/ritvik-ch/local_manifest.git --depth 1 -b Dot-12.x .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch dot_Z01R-userdebug
+lunch dot_vince-userdebug
 export TZ=Asia/Kolkata 
 make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-#rclone copy out/target/product/Z01R/vendor.img cirrus:Z01R -P
+#rclone copy out/target/product/vince/vendor.img cirrus:Z01R -P
