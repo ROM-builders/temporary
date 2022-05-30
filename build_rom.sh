4d17aac63ff2ef0dec7ef33ce8faa8142f46eb88
Shubham Pardhi <83544346+thousandecibles@users.noreply.github.com>
2022-05-31 02:37:35 +0530

    lisa-PixelExperience-twelve-plus-thousandecibles

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom 
+# sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/PixelExperience/manifest -b twelve-plus -g default,-mips,-darwin,-notdefault
 git clone https://github.com/thousandecibles/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j
