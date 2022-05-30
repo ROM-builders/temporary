0eb9b8908a470f3f4180591df9246dcc300b2430
Shubham Pardhi <83544346+thousandecibles@users.noreply.github.com>
2022-05-31 01:27:34 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/PixelExperience/manifest -b twelve-plus -g default,-mips,-darwin,-notdefault
-git clone https://github.com/thousandecibles/local_manifest/tree/main.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/thousandecibles/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c -$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
 # build rom
