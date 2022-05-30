9e6f2c4e4007aa4ad3aba64cad9d98b552f16ccd
clhex <100755123+clhexftw@users.noreply.github.com>
2022-05-30 20:36:57 +0530

    initial build

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,14 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/P-404/android_manifest -b shinka -g default,-mips,-darwin,-notdefault
-git clone https://github.com/xenxynon-lab-404/manifest --depth=1 -b 404 .repo/local_manifests
+git clone https://github.com/clhexx00tlab/local_manifest.git --depth=1 -b p404 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch p404_lavender-userdebug
-export WITH_GAPPS=true
+lunch p404_X00T-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
+export ALLOW_MISSING_DEPENDENCIES=true
+#export WITH_GAPPS=true
 export TZ=Asia/Kolkata #put before last build command
 make bacon
 
