5de5b783d80130b0a4b1252a9d4f54fd62fad23a
clhex <100755123+clhexftw@users.noreply.github.com>
2022-05-30 04:22:24 +0000

    build gapps

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch aosp_X00TD-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
-#export WITH_GAPPS=true
+export WITH_GAPPS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Dhaka #put before last build command
 m aex
