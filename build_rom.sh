ff39edd2aaef9730f2a8eb5ad4ea53dcf04c3956
zacky46 <103025109+zacky46@users.noreply.github.com>
2022-05-31 16:22:58 +0700

    whyred-Voltage-12l-zacky46

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch voltage_whyred-user
+#lunch voltage_whyred-user
 export TZ=Asia/Dhaka #put before last build command
 brunch whyred
 
