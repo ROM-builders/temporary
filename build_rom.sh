40e53fd7aa7023e30e7119e703020573db2d1ad3
NP-XD <103098781+Tester9958@users.noreply.github.com>
2022-05-31 21:25:39 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch aosp_X01AD-userdebug
-export WITH_GAPPS=true
 export TZ=Asia/Kolkata #put before last build command
 m aex
 
