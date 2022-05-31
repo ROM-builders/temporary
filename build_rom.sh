19f80cee1d398c420bdba8fb4d894f25339002f3
Iris <100063196+Iris0411@users.noreply.github.com>
2022-06-01 03:37:02 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Lynx041/local_manifest.git --depth 1 -b ulysse .repo/local_manifests
+git clone https://github.com/Iris0411/local_manifest.git --depth 1 -b ulysse .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
  
 # build rom
