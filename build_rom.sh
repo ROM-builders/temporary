b9b6f88f9a86938261a8710792c075659a949e69
zacky46 <103025109+zacky46@users.noreply.github.com>
2022-05-31 16:47:59 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch voltage_whyred-user
+lunch voltage_whyred-userdebug
 export TZ=Asia/Dhaka #put before last build command
 brunch whyred
 
