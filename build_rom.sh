69a7cd25fc303a64fc635e49946038606f809fa0
Mika Haluku <104668518+mika1zumi@users.noreply.github.com>
2022-06-01 05:10:19 +0700

    Y

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch spark_X00T-userdebug
+export BUILD_USERNAME=axololtl
 export TZ=Asia/Makassar #put before last build command
 mka bacon
 
