39969b187a5251257247ee97e80efe9e7f9ca0f9
67 <94082908+sweervin@users.noreply.github.com>
2022-06-01 08:35:15 +0300

    Switch to jemalloc memory allocator

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,11 @@
-
 repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest -b arrow-12.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/sweervin/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
 
 . build/envsetup.sh
 export ARROW_GAPPS=true
 lunch arrow_laurel_sprout-userdebug
 export TZ=Asia/Dhaka
 m bacon
-
 
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
