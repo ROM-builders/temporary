453e1ccd7233bb27870935a55e896de60b3abf93
Iris <100063196+Iris0411@users.noreply.github.com>
2022-06-01 22:06:17 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
  
 # build rom
 . build/envsetup.sh
-lunch blaze_ulysse-userdebug
+lunch b
 export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Manila #put before last build command
 brunch ulysse
