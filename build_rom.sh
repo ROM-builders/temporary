11ddf812a0f1197b59ad30b3bd6d81e48de9ab61
Iris <100063196+Iris0411@users.noreply.github.com>
2022-06-01 22:10:30 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,12 +5,13 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
  
 # build rom
 . build/envsetup.sh
-lunch b
+
 export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Manila #put before last build command
 brunch ulysse
  
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+ 
  
  
