69a96891c25e73ebcb63d9247207ff9d4368cbfb
Hitesoni | ×͜× <72127977+hsx02@users.noreply.github.com>
2022-06-01 08:20:00 +0530

    Vanilla build

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch spark_spes-user
-export WITH_GAPPS=true
 export TZ=Asia/Shanghai #put before last build command
 mka bacon
 
