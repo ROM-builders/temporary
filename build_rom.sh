59ff72194a0cc66c316b393173290053c80b8ad6
Siimsek <62520983+siimsek@users.noreply.github.com>
2022-06-01 19:46:48 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch voltage_ginkgo-user
 export TZ=Asia/Kolkata 
 brunch ginkgo
 
