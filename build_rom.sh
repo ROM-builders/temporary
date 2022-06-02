88e6724a59125c10669adc5f86eb79291dfed132
Nabil Zaman <94600437+iamnabilzaman@users.noreply.github.com>
2022-06-02 22:33:16 +0600

    m20lte Fork-Lineage iamnabilzaman

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ForkLineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefaul
+repo init --depth=1 --no-repo-verify -u https://github.com/ForkLineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/iamnabilzaman/local_manifest_m20lte --depth 1 -b lineage-19.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
