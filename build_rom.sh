dd10981cae23fb19473a51b4098530ef5e72aaaa
Ivan K <attoscape@gmail.com>
2022-06-03 22:53:44 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/hentaiOS/platform_manifest.git -b SaltyIceCream -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/hentaiOS/platform_manifest -b SaltyIceCream -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Attorelle/local_manifest/ --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
