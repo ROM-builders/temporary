2924d017d1ebdb6c1ebb09f3ffebdf94ffa1a040
T.B.S <ewprjkt@proton.me>
2022-06-04 00:41:59 +0700

    return trees

--- build_rom.sh
+++ build_rom.sh
@@ -6,8 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build roms
 source build/envsetup.sh
 lunch lineage_X00T-userdebug
-export BUILD_USERNAME=tiktod
-export TZ=Asia/Tokyo #put before last build command
+export BUILD_USERNAME=github
+export TZ=Asia/Jayapura #put before last build command
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
