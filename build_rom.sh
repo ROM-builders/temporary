b36268475d7907c283f128f2c4b177ce515228e8
Alone0316 <rutujamandlik06@gmail.com>
2022-06-03 05:13:00 +0000

    retry

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch aospa_mido-userdebug
 export TZ=Asia/Dhaka #put before last build command
-./rom-build.sh mido -t userdebug 
+./rom-build.sh mido -t userdebug
 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
