948e2e7817cd22b9cc4c48c5d3e58b77e0a525ec
Ritvik <102162683+ritvik-ch@users.noreply.github.com>
2022-06-04 01:51:11 +0530

    Drop neverallows

--- build_rom.sh
+++ build_rom.sh
@@ -3,6 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest -b
 git clone https://github.com/ritvik-ch/local_manifest.git --depth 1 -b voltage-12.x .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+
 # build rom
 source build/envsetup.sh
 export SELINUX_IGNORE_NEVERALLOWS=true 
@@ -11,4 +12,4 @@ brunch voltage_vince-userdebug
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-rclone copy vendor/ota/vince.json cirrus:vince -P
+#rclone copy vendor/ota/vince.json cirrus:vince -P
