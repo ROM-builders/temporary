8e8b6c06b6b9a6769feb0c07950ebad58dfefe22
Ritvik <102162683+ritvik-ch@users.noreply.github.com>
2022-06-03 10:21:27 +0530

    Fix sync

--- build_rom.sh
+++ build_rom.sh
@@ -3,6 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest
 git clone https://github.com/ritvik-ch/local_manifest.git --depth 1 -b Blaze-12.x .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+
 # build rom
 source build/envsetup.sh
 #export WITH_GAPPS=true
