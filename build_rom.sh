631f0670e05a6ac41c853eb9e17b0d2074102175
T.B.S <ewprjkt@proton.me>
2022-06-04 01:43:08 +0700

    regen queue

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
-# sync roms
+# sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android -b lineage-17.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Tiktodz/local_manifest.git --depth 1 -b 10 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build roms
+# build rom
 source build/envsetup.sh
 lunch lineage_X00T-userdebug
 export BUILD_USERNAME=github
