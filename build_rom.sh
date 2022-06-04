ddb46e5dc9d937f4af9f28ea071f9c76310decd1
NFS86 <jarbull86@gmail.com>
2022-06-04 09:51:27 +0000

    initial build and test Aosp-Extended [rosy]
    Signed-off-by: NFS86 <jarbull86@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,16 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/AospExtended/manifest.git -b 12.1.x -g default,-mips,-darwin,-notdefault
 git clone https://github.com/NFS-projects/local_manifest.git --depth 1 -b 12.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
+lunch aosp_rosy-userdebug
 export TZ=Asia/Jakarta
-export USE_NFSparts=true
+export USE_NFSparts=false
 export NFS_BUILD_VARIANT=normal
 export NFS_ADD_GCAMGO=true
-brunch blaze_rosy-userdebug
+m aex
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
