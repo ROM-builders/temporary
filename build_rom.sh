ad5049e8290d3074822e1705347a9f9989f33b45
Orkun Ergün <104510596+orkunergun@users.noreply.github.com>
2022-06-04 20:20:30 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,22 +1,15 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/r9lab/local_manifest.git --depth 1 -b awaken .repo/local_manifests
+git clone https://github.com/r9lab/local_manifest.git --depth 1 -b awaken-m .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch awaken_lava-userdebug
+lunch awaken_merlinx-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
-export KBUILD_BUILD_USER=orkun
-export KBUILD_BUILD_HOST=orkun
-export BUILD_USERNAME=orkun
-export BUILD_HOSTNAME=orkun
-export WITH_GAPPS=true
-export TARGET_GAPPS_ARCH=arm64
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-rclone copy out/target/product/lava/*.zip cirrus:lava/awaken -P
+rclone copy out/target/product/merlinx/*.zip cirrus:merlinx/awaken -P
