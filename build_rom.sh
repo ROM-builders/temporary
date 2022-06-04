5f26a175456cb0c625259a279da01f36af4751fb
Salih Pelit <91677076+msalihpelit@users.noreply.github.com>
2022-06-04 18:09:04 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch cherish_ginkgo-userdebug
+lunch cherish_willow-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
