63abfce2451a8c007be7aa0ee5e3fd7f0cd4238f
amritoj2 <93515519+amritoj2@users.noreply.github.com>
2022-06-05 00:31:11 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init -u https://github.com/yaap/manifest.git -b twelve
-git clone https://github.com/amritoj2/-local_manifest.git --depth 1 -b main .repo/local_manifests
-repo sync -j$(nproc --all) --no-tags --no-clone-bundle --current-branch
+repo init -u https://github.com/PixelPlusUI-SnowCone/manifest -b snowcone-12.1
+git clone https://github.com/amritoj2/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
 # build rom
-source build/envsetup.sh
-lunch yaap_r5x-user
+. build/envsetup.sh
+lunch aosp_$r5x-userdebug
 export TZ=Asia/Dhaka #put before last build command
-m yaap
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
