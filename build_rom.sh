ffda08e3fa8ab832878f15f0ec3192ba9bdf0628
amritoj2 <93515519+amritoj2@users.noreply.github.com>
2022-06-05 01:36:50 +0600

    r5x-pixelplusui-snowcone-12.1-amritoj2

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init -u https://github.com/PixelPlusUI-SnowCone/manifest -b snowcone-12.1
 git clone https://github.com/amritoj2/local_manifest.git --depth 1 -b main .repo/local_manifests
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
