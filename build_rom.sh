fa08a9072e9ff8db5001a16e1c67cec8cc460b5f
ucoboy <68426327+ucoboy@users.noreply.github.com>
2022-06-05 17:11:59 +0530

    Initial build

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch ssos_vayu-userdebug
+lunch ssos_tissot-userdebug
 export TZ=Asia/Kolkata #put before last build command
 make bacon
 
