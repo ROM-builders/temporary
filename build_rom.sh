07210378b146445dbcbda5e7deb55d743120caf2
T.B.S <ewprjkt@proton.me>
2022-06-05 12:37:58 +0700

    ellev

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,14 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Tiktodz/local_manifest.git --depth 1 -b 18 .repo/local_manifests
+git clone https://github.com/Tiktodz/local_manifest.git --depth 1 -b rr .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-breakfast lineage_X00TD-userdebug
-export BUILD_USERNAME=tiktod
+lunch lineage_X00TD-userdebug
+export BUILD_USERNAME=wizard
 export TZ=Asia/Shanghai #put before last build command
-croot
-brunch X00TD
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
