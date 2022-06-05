0f27dd530901bcb3f432b30f28d1efc75de86fb9
Ankit Savani <70018840+AnkitSavani111@users.noreply.github.com>
2022-06-05 21:00:16 +0530

    Drop libperfmgr

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b snow -g default,-mips,-darwin,-notdefault
-git clone https://github.com/AnkitSavani111/local_manifest.git --depth 1 -b Drop_libperfmgr .repo/local_manifests
+git clone https://github.com/AnkitSavani111/local_manifest.git --depth 1 -b EvolutionX .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -12,4 +12,3 @@ make evolution
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-
