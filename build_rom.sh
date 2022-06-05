89d827dbeaae466137524ef8f74c7533c853a3dd
amritoj2 <93515519+amritoj2@users.noreply.github.com>
2022-06-05 11:28:23 +0600

    r5x-pixelplusui-snowcone-12.1-amritoj2

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch aosp_$r5x-userdebug
+lunch aosp_r5x-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
