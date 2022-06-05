cb86bacae06ca802e0ad05ab8476faa87b1c985d
Suraj Kumar <106401203+ImSurajx@users.noreply.github.com>
2022-06-05 16:05:50 +0530

    Let's Try To Build CrDroid

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/imsurajx/local_manifest --depth 1 -b main-rice .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 12.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/imsurajx/local_manifest --depth 1 -b main-crdroid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
