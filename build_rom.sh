1053d956e6269236052c51ec4679e7ede2cc1b2d
Mika Haluku <104668518+mika1zumi@users.noreply.github.com>
2022-06-05 10:42:36 +0700

    To Unofficial Lmfao

--- build_rom.sh
+++ build_rom.sh
@@ -8,7 +8,7 @@ source build/envsetup.sh
 lunch evolution_X00TD-userdebug
 export TARGET_USES_MINI_GAPPS := true
 export TARGET_INCLUDE_LIVE_WALLPAPERS := false
-export TZ=Asia/Jakarta #put before last build command
+export TZ=Asia/Tokyo #put before last build command
 mka evolution
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
