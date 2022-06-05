749d894d7897701d32a8a245c077f4347e56bc66
xenxynon <flynryder427@gmail.com>
2022-06-05 12:35:47 +0530

    ArrowOS

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/P-404/android_manifest -b shinka -g default,-mips,-darwin,-notdefault
-git clone https://github.com/xenxynon-lab-404/manifest --depth=1 -b 404 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest -b arrow-12.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/xenstuff/manifest  --depth=1 -b arrow .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch p404_lavender-userdebug
-export WITH_GAPPS=true
+lunch arrow_lavender-user
+export ARROW_GAPPS=true
 export TZ=Asia/Kolkata #put before last build command
-make bacon
+m bacon
 
-# 15
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
