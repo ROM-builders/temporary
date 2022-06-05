2443a3d2b937576e011aabdf7bfc8d723d8933f7
Archsploit <archsploit@protonmail.com>
2022-06-05 08:15:08 +0530

    Lets try user build

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,12 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 export TZ=Asia/Dhaka #put before last build command
-brunch RMX1821
+export KBUILD_BUILD_USER=archsploit
+export KBUILD_BUILD_HOST=archsploit
+export BUILD_USERNAME=archsploit
+export BUILD_HOSTNAME=archsploit
+lunch lineage_RMX1821-user
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
