9253eef6975495c853269123e5875861beaa6a11
Suraj Kumar <106401203+ImSurajx@users.noreply.github.com>
2022-06-06 18:23:35 +0530

    Let's Try To Build Lineage Os Gapps Version

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/imsurajx/local_manifest --depth 1 -b main-crdroid .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/imsurajx/local_manifest --depth 1 -b main-lineage .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-. build/envsetup.sh
-export With_GAPPS=true
+source build/envsetup.sh
+breakfast lavender
+./extract-files.sh
+croot
 export TZ=Asia/Kolkata #put before last build command
 brunch lavender
 
