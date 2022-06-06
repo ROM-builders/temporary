3d890dbbe816b8b28d6a832d862f1829a57adc72
Ram Badan Pandey <rambpandey238@gmail.com>
2022-06-06 05:54:34 -0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/ProjectBlaze/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/TechnoStartOfficial/local_manifest --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/AmogOS-Rom/manifest.git -b sus -g default,-mips,-darwin,-notdefault
+git clone https://github.com/TechnoStartOfficial/onclite_local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch blaze_surya-userdebug
+lunch aosp_onclite-userdebug
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
