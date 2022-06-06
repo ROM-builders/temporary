03c181ed6f8d1066a7d5a5647e938f8c91f6a29b
mikairyuu <miva200267@gmail.com>
2022-06-06 15:21:57 +1000

    Test proximity fixes

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom 
+# sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest.git -b snow -g default,-mips,-darwin,-notdefault
 git clone https://github.com/mikairyuu/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
@@ -7,9 +7,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch evolution_monet-user
 export TZ=Asia/Vladivostok #put before last build command
-mka recoveryimage
+mka evolution
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
-rclone copy out/target/product/monet/recovery.img cirrus:monet -P
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
 rclone copy out/target/product/monet/*.zip.json cirrus:monet -P
