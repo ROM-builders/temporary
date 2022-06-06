76a96526b2d692663fba8317939da0cc8893d78f
JaswantTeja <sunnyteja201@gmail.com>
2022-06-06 05:58:14 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch corvus_device-userdebug
+lunch corvus_r5x-userdebug
 export TZ=Asia/Kolkata #put before last build command
 make corvus
 
