316cea148b6bab21b76a3ead500acc554a4cde31
Suraj Kumar <106401203+ImSurajx@users.noreply.github.com>
2022-06-07 21:58:25 +0530

    Let's Try To Build Project Arcana

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/yaap/manifest.git -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/imsurajx/local_manifest --depth 1 -b main-yaap .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/projectarcana-aosp/manifest -b 12.x -g default,-mips,-darwin,-notdefault
+git clone https://github.com/imsurajx/local_manifest --depth 1 -b main-aosp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-export TARGET_BUILD_GAPPS=true)
-lunch yaap_lavender-user
+export WITH_GAPPS=true
+lunch aosp_lavender-userdebug
 export TZ=Asia/Kolkata #put before last build command
-m yaap
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
