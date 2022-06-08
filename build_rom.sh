08ff40da2fcb1e61a40bfe544ab4fdb589e21706
NamCap25 <94762272+NamCap25@users.noreply.github.com>
2022-06-08 09:42:11 +0100

    Fixed repo init command

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 -u https://github.com/PixelPlusUI-SnowCone/manifest -b snowcone-12.1
+repo init --depth=1 -u https://github.com/PixelPlusUI-SnowCone/manifest -b snowcone-12.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/NamCap25/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
