fd27de16217c011650d2f81835dba17c2e140756
Grom-exe <103995190+Grom-exe@users.noreply.github.com>
2022-06-08 17:20:43 +0200

    lava-Corvus-R-12-test
    Build CorvusOS for lava

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init -u https://github.com/Corvus-R/android_manifest.git -b 12-test -g default, -mips, -darwin, -notdefault
-git clone https://github.com/Grom-exe/local_manifest/local_manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/Grom-exe/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
