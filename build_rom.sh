bcec3709654fabfd84cfcbdf5c2335d11a58d9ae
SumitOP787 <82710581+SumitOP787@users.noreply.github.com>
2022-06-08 18:40:55 +0530

    RMX3360-VoltageOS-12l-SumitOP787

--- build_rom.sh
+++ build_rom.sh
@@ -4,9 +4,9 @@ git clone https://github.com/SumitOP787/local_manifest.git --depth 1 -b main .re
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
-export TZ=Asia/Delhi
-brunch voltage_RMX3360-userdebug
+. build/envsetup.sh
+export TZ=Asia/Dhaka #put before last build command
+brunch RMX3360
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
