342501db01fc72b73817e8f75d618198a848f2ad
SUMIT <72486118+S8M1T@users.noreply.github.com>
2022-06-08 12:13:35 +0530

    RMX3360-VoltageOS-12l-S8M1T

--- build_rom.sh
+++ build_rom.sh
@@ -4,10 +4,9 @@ git clone https://github.com/S8M1T/local_manifest.git --depth 1 -b main .repo/lo
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
-lunch voltage_RMX3360-userdebug
+. build/envsetup.sh
 export TZ=Asia/Dhaka #put before last build command
-make bacon
+brunch RMX3360
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
