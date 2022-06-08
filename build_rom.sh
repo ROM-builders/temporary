fc562475bec518eb270073f1e59a0d517572e082
MukulSharma06175 <sharmamukul06175@gmail.com>
2022-06-08 18:57:07 +0530

    Lunch 2 time

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch corvus_lavender-userdebug
+lunch corvus_lavender-userdebug
 export TZ=Asia/Dhaka #put before last build command
 make corvus
 
