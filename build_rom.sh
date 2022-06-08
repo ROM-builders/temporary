d23b9bfd71e14ea1f5b12fe5690800009f44bc55
Orkun Ergün <104510596+orkunergun@users.noreply.github.com>
2022-06-08 12:37:48 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,14 +3,6 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest 
 git clone https://github.com/r9lab/local_manifest.git --depth 1 -b evo .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# patch ims
-rm -rf frameworks/opt/telephony 
-git clone https://github.com/CipherOS/android_frameworks_opt_telephony -b twelve-L frameworks/opt/telephony 
-rm -rf packages/modules/Wifi
-git clone https://github.com/CipherOS/android_packages_modules_Wifi -b twelve-L packages/modules/Wifi
-rm -rf frameworks/opt/net/ims
-git clone https://github.com/CipherOS/android_frameworks_opt_net_ims -b twelve-L frameworks/opt/net/ims
-
 # build rom
 source build/envsetup.sh
 lunch evolution_lava-userdebug
