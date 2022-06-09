1afacecc980dea11a8cc8e172a490138d9988278
MukulSharma06175 <sharmamukul06175@gmail.com>
2022-06-09 11:29:25 +0530

    fixed dt issue

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,7 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/Corvus-R/android_manifest.git -b 12-test -g default,-mips,-darwin,-notdefault
 git clone https://github.com/MukulSharma06175/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-       
+        
 # build rom
 . build/envsetup.sh
 lunch corvus_lavender-userdebug
