cbaa49800b9c8fd282a8da3c3adc177dcf66ef15
T.B.S <ewprjkt@proton.me>
2022-06-09 21:14:43 +0700

    Trigger: build

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_X00T-userdebug
-export BUILD_USERNAME=tiktod
 export TZ=Asia/Jayapura #put before last build command
 mka bacon
 
