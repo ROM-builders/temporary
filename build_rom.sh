7e199073b11cddbdf47afbbc9bb1955523ff8393
Ritvik <102162683+ritvik-ch@users.noreply.github.com>
2022-06-10 14:31:28 +0530

    Fix bootloop

--- build_rom.sh
+++ build_rom.sh
@@ -3,6 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest -b
 git clone https://github.com/ritvik-ch/local_manifest.git --depth 1 -b voltage-12.x .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+
 # build rom
 source build/envsetup.sh
 export KBUILD_BUILD_USER=Ritvik
