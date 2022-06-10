46b0a47066551cd96daeadee491ea10d18afca5a
PrajwalGoli <92887845+PrajwalGoli@users.noreply.github.com>
2022-06-10 23:02:42 +0530

    lavender-superiorOS-12.1-PrrajwalG

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
+repo init -u https://github.com/SuperiorOS/manifest.git -b twelvedotone -g default,-mips,-darwin,-notdefault
 git clone  https://github.com/PrajwalGoli/Hello-world --depth 1 -b main .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
 # build rom
-source build/envsetup.sh
-lunch blaze_lavender-userdebug
-export WITH_GMS=true
-export TZ=Asia/Dhaka #put before last build command 
-brunch lavender
+  . build/envsetup.sh
+  lunch superior_lavender-userdebug
+  m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
