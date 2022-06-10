4db60d31e8cc44f2e770d61edc7a9ed1be4037bb
ben863 <r.budhiono@gmail.com>
2022-06-11 03:49:37 +0700

    build: remove unecessary this no effect for kernel user && host in crdroid
    Change-Id: I283591eeee2391c4864edd0ab53c5914eaefaafb
    Signed-off-by: ben863 <r.budhiono@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -6,8 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_X00TD-userdebug
-export KBUILD_BUILD_USER=ben863
-export KBUILD_BUILD_HOST=cirrus-ci.org
 export BUILD_USERNAME=ben863
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Jakarta #put before last build command
