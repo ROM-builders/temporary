512cf5bea7017de4c79f687407bad2a94b7778b4
Matheus Souza <33396386+matheucomth@users.noreply.github.com>
2022-06-10 10:18:57 -0300

    merge to patch ASB June

--- build_rom.sh
+++ build_rom.sh
@@ -4,6 +4,7 @@ git clone https://github.com/matheucomth/local_manifest.git --depth 1 -b PixelOS
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build roms
+. build/envsetup.sh
 lunch aosp_tulip-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
