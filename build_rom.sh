0c1930f7c1553a95a564519357463789bd2b2567
JaswantTeja <sunnyteja201@gmail.com>
2022-06-10 22:04:22 +0530

    r5x : Blaze Gapps
    * Its My First Bulid After Getting Into The Team☺️

--- build_rom.sh
+++ build_rom.sh
@@ -5,6 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
+export USE_GAPPS := true
 lunch blaze_r5x-userdebug
 export TZ=Asia/Kolkata #put before last build command
 
