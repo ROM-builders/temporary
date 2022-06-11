944750f263bcf68fde4c5fcdf23f6cc6198487a8
amritoj2 <93515519+amritoj2@users.noreply.github.com>
2022-06-11 09:52:35 +0600

    Build crdroid android
    Build pls

--- build_rom.sh
+++ build_rom.sh
@@ -3,6 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/androi
 git clone https://github.com/amritoj2/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+
 # build rom
 . build/envsetup.sh
 lunch lineage_r5x-userdebug
