a2d7ffc28e709e1d819f348ccaf449699c4f9e8a
SKetU <96108085+SKetU-l@users.noreply.github.com>
2022-06-11 21:00:28 +0530

    Final build @SKetU-l

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,14 @@
 # sync rom
-repo init -u https://github.com/Spark-Rom/manifest -b spark
-https://github.com/Spark-Rom/manifest
-repo sync --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
+repo init -u https://github.com/syberia-project/manifest.git -b 12.1
+https://github.com/syberia-project/manifest.git -b 12.1
+repo sync -c --force-sync --no-tags --no-clone-bundle -j$(nproc --all) --optimized-fetch --prune
 
 # build rom
-. build/env*
-lunch spark_<ASUS_X00TD>-userdebug
-mka bacon -j$(nproc --all)
+cd <ASUS_X00TD>
+<X00TD>
+git add -A
+git commit -m "commit message"
+git push ssh://<SKetU-l>@gerrit.syberiaos.com:29418/syberia-X00TD/<X00TD> HEAD:refs/for/12.0
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
