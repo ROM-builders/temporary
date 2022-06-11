79e4e7609f69b126ec795319fee9736baab6d514
Hitesoni | ×͜× <72127977+hsx02@users.noreply.github.com>
2022-06-11 21:03:53 +0530

    Test build

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,14 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/OmarAlCoptan/local_manifest.git --depth 1 -b blaze .repo/local_manifests
+git clone https://github.com/hsx02/Local-Manifests.git --depth 1 -b blaze-spes .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
    
 # build rom 
 source build/envsetup.sh
-#export WITH_GAPPS=true
+lunch blaze_spes-userdebug
+export WITH_GAPPS=true
 export TZ=Africa/Egypt #put before last build command
-brunch lavender         
+brunch spes         
  
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
