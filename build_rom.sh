c4c8d8324e8f7cdbff2e43c932ee511e0fcfa72f
Suraj Kumar <106401203+ImSurajx@users.noreply.github.com>
2022-06-12 15:00:24 +0530

    Back To Pe

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b twelve-plus -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ImSurajx/local_manifest --depth 1 -b main-plus .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/ImSurajx/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
