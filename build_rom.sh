5725bb23b69a78baeb8a211ad0bf636b951f2808
Ritvik <102162683+ritvik-ch@users.noreply.github.com>
2022-06-13 22:15:05 +0530

    Initial Build

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-oss/xd_manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/datnerdguy/local_manifest --depth 1 -b xdroid .repo/local_manifests
+git clone https://github.com/ritvik-ch/local_manifest --depth 1 -b xdroid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch xdroid_whyred-user
+lunch xdroid_vince-userdebug
 export TZ=Asia/Kolkata #put before last build command ###
 mka xd
 
