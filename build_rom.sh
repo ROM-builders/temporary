c103979df469f89485aab7615c3a15cac8662362
JaswantTeja <sunnyteja201@gmail.com>
2022-06-13 22:35:43 +0530

    r5x : Fusion Errors Fixxed

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-export WITH_GAPPS :=true
+export WITH_GAPPS := true
 lunch fuse_r5x-userdebug
 export TZ=Asia/kolkata #put before last build command
 make fuse-prod
