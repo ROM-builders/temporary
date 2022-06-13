d84830b584ed598cb177df38782a6ce90cc2ad53
Coptan⁹⁹ <omarcoptan9@gmail.com>
2022-06-13 19:55:59 +0200

    Pixys

--- build_rom.sh
+++ build_rom.sh
@@ -5,6 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
     
 # build rom 
 source build/envsetup.sh
+lunch pixys_lavender-userdebug
 export BUILD_WITH_GAPPS=true
 export TZ=Africa/Egypt #put before last build command
 make pixys     
