8c02402276bbe407314984d92d384aae5a571b9b
Grom-exe <103995190+Grom-exe@users.noreply.github.com>
2022-06-14 18:46:03 +0200

    Spark-Rom-spark-Grom-exe
    Build SparkOS for lava

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Corvus-R/android_manifest -b 12-test -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Grom-exe/local_manifest --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b spark -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Grom-exe/local_manifest --depth 1 -b SparkOS-mt6768PHQ .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch corvus_lava-userdebug
+lunch spark_lava-userdebug
 export TZ=Asia/Dhaka #put before last build command
-mka bacon
+mka bacon -j
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
