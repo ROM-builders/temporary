27e6821b033fb166840ab2fe96fc89c6fa037a4c
T.B.S <ewprjkt@proton.me>
2022-06-14 13:25:00 +0700

    Added custom stuff

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_X00T-userdebug
+export BUILD_USERNAME=Tiktod
 export TZ=Asia/Jayapura #put before last build command
 mka bacon
 
