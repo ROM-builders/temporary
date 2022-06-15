2205292bf29abd9ffd86850eae07a75814fd410e
Grom-exe <103995190+Grom-exe@users.noreply.github.com>
2022-06-15 14:32:22 +0200

    lava-ShapeShiftOS-android_12-Grom-exe
    Build Ssos for lava

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ShapeShiftOS/android_manifest.git -b android_12 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/ShapeShiftOS/android_manifest -b android_12 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Grom-exe/local_manifest --depth 1 -b Ssos-mt6768PHQ .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
