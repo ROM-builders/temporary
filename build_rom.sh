b3e44dfd378436726633573b967185a36d0ff1f2
Salih Pelit <91677076+msalihpelit@users.noreply.github.com>
2022-06-15 19:24:30 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Corvus-R/android_manifest.git -b 12-test -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/AICP/platform_manifest -b s12.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/msalihpelit/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch corvus_ginkgo-userdebug
+lunch aicp_ginkgo-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
