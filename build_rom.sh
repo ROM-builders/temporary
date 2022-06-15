1b7658b45db20ba998b4b0aa78c402f85387c95e
prango71 <101040779+prango71@users.noreply.github.com>
2022-06-15 09:01:04 +0530

    sync sources

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch xdroid_whyred-user
-export TZ=Asia/Kolkata #put before last buil com
+export TZ=Asia/Kolkata #put before last buil co
 mka xd
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
