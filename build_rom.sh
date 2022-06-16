b3e772e4f059c83b725877b2174336c3c9c45ad5
Shridhan Varadkar <shridhan98@gmail.com>
2022-06-16 13:03:50 +0530

    New Device tree

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest.git -b 12l -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Shreedha003/local_manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/Shreedha003/local_manifest.git --depth 1 -b new-trees .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -10,4 +10,4 @@ brunch lavender
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-rclone copy vendor/ota/lavender.json cirrus:lavender -P
+# rclone copy vendor/ota/lavender.json cirrus:lavender -P
