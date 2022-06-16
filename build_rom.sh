abb257073d674f0636ac49ea49704e75140c0c1e
pingmado <75196272+ping2109@users.noreply.github.com>
2022-06-16 15:16:06 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch awaken_spes-userdebug
+lunch awaken_spes-eng
 export TZ=Asia/Ho_Chi_Minh
 make bacon
 
