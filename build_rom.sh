b1339b8ffb81a04d628d16c2620c465e089abd32
tedomi2705 <hongducthbk123@gmail.com>
2022-06-17 16:20:34 +0700

    build

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b snow -g default,-mips,-darwin,-notdefault
-git clone https://github.com/tedomi2705/local_manifest.git --depth=1 -b snow .repo/local_manifests
+git clone https://github.com/tedomi2705/local_manifest.git --depth=1 -b unsigned .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 
@@ -14,4 +14,3 @@ mka evolution
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-rclone copy out/target/product/tulip/*.zip.json cirrus:tulip -P
