588bc4f3d533e6e45a0dbc0193b6a08f9fb5e853
Snowman2409 <93831265+Snowman2409@users.noreply.github.com>
2022-06-17 15:58:42 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,11 +4,11 @@ git clone https://github.com/Snowman2409/local-manifest/blob/main/one.xml --dept
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 # build rom
 source build/envsetup.sh
-lunch aosp_lavender-userdebug
+lunch aosp_lavender-user
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
 export SELINUX_IGNORE_NEVERALLOWS=true
 
-m bacon 
+mka bacon 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
