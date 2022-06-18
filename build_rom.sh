c540b4542aa00e7cf33db7cab6e18fde65999bae
khirendra03 <khirendrapatel3@gmail.com>
2022-06-18 07:56:23 +0530

    X01BD-VoltageOS-12l-khirendra03

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,6 @@ repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest.gi
 git clone https://github.com/khirendra03/local_manifest.git --depth 1 -b voltage .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-
 # build rom
 source build/envsetup.sh
 export TZ=Asia/Delhi #put before last build command
@@ -11,4 +10,4 @@ brunch voltage_X01BD-userdebug
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-
+rclone copy vendor/ota/X01BD.json cirrus:X01BD -P
