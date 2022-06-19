581140950907f030fdb6909e97d773c5d13f3a8d
itzorr <103074348+itzorr@users.noreply.github.com>
2022-06-19 13:42:39 +0300

    laurel_sprout-Spark-Rom-spark-itzorr

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/itzorr/local_manifest.git --depth 1 -b main .repo/l
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/env*
+source build/envsetup.sh
 lunch spark_laurel_sprout-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
