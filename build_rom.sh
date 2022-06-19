d52459d41ee6c54f610b909d8f6dc21952a115a0
Excraze <krtonia@gmail.com>
2022-06-19 12:59:00 +0530

    Beryllium-SuperiorOS-excraze

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest -b twelvedotone -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.git -b twelvedotone -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Krtonia/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lunch superior_beryllium-userdebug
+lunch superior_beryllium-userdebug
 export TZ=Asia/Dhaka #put before last build command
 m bacon
 
