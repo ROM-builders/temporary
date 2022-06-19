898ab2ebc106cce086eefe9d451343071400f1e0
Onkar singh <onkarsinghsahil@gmail.com>
2022-06-19 16:34:40 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 11.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/HemanthJabalpuri/local_manifest --depth 1 -b crdroid-11-UI1 .repo/local_manifests
+git clone https://github.com/onkarsinghsahil/local_manifest --depth 1 -b XD .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom [4]
 source build/envsetup.sh
-lunch lineage_RMX2185-userdebug
+lunch lineage_mido-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
