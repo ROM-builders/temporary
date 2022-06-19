33f44f8175774f0083e2b4deaf320da5bcd5741d
clhex <100755123+clhexftw@users.noreply.github.com>
2022-06-19 10:09:34 +0530

    initial build

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,12 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-Pixelish/manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/sanjeevstunner/Manifest.git --depth 1 -b vayu_pixelos .repo/local_manifests
+git clone https://github.com/clhexftw/local_manifest.git --depth 1 -b pixelos .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_vayu-user
+lunch aosp_vayu-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
