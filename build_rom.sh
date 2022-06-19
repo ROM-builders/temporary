fcc766cf2cff05bfba60de0d0e8ec0cebc7548b0
GhostMaster69-dev <rathore6375@gmail.com>
2022-06-19 17:17:33 +0530

    Superior vanilla

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch superior_vince
-export BUILD_WITH_GAPPS=true
+#export BUILD_WITH_GAPPS=true
 export TZ=Asia/Kolkata #put before last build command
 m bacon
 
