8c6b472ccd133f7668c342700df341323dc6ed43
Kneba <abenkenary3@gmail.com>
2022-06-20 22:41:18 +0700

    Failing repos

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync roms
+# sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/syberia-project/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Tiktodz/local_manifest.git --depth 1 -b syb .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
