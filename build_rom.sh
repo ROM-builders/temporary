8d59d08a48e7b375880f513eadca411387c6402d
vivekroy <50535325+bugroy111@users.noreply.github.com>
2022-06-22 15:46:29 +0530

    karate-LineageOS-lineage-18.1-bugroy111

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -
 git clone https://github.com/bugroy111/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom - bring back K6
 source build/envsetup.sh
 #lunch derp_vayu-user
 export TZ=Asia/Dhaka #put before last build command
