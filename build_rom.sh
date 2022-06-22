960ae7f6731302b11a080615147974cdf2ddaf70
Amritorock <101019800+Amritorock@users.noreply.github.com>
2022-06-22 09:02:06 +0600

    Rebuild crdroidandroid
    Let's build again crdroidandroid

--- build_rom.sh
+++ build_rom.sh
@@ -4,9 +4,10 @@ git clone https://github.com/Amritorock/local_manifest.git --depth 1 -b main .re
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-. build/envsetup.sh
+source build/envsetup.sh
+lunch lineage_r5x-user
 export TZ=Asia/Dhaka #put before last build command
-brunch r5x
+m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
