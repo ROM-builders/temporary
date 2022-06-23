c2f7c4efe3a8f463c49c62a236a5e7335e2fee91
itzorr <103074348+itzorr@users.noreply.github.com>
2022-06-23 16:27:26 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b spark -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/itzorr/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch spark_laurel_sprout-userdebug
+lunch aosp_laurel_sprout-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
