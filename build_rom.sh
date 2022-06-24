55838c94cb69a6c828997016b327c1085092b1f2
Amrito <93515519+amritoj2@users.noreply.github.com>
2022-06-24 22:32:56 +0600

    r5x-ShapeShiftOS-android_12-amritoj2
    Let's fix pr label

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,6 @@ repo init --depth=1 --no-repo-verify -u https://github.com/ShapeShiftOS/android_
 git clone https://github.com/amritoj2/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-
 # build rom
 source build/envsetup.sh
 lunch ssos_r5x-user
