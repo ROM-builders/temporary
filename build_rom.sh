7b7a98684407a148488e0021169c7672fe4a0a09
SUMIT <72486118+S8M1T@users.noreply.github.com>
2022-06-25 17:30:27 +0530

    RMX3031-ProjectBlaze-12.1-S8M1T

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,17 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/S8M1T/local_manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/S8M1T/local_manifest.git --depth 1 -b RMX3031 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 export WITH_GAPPS=true
-lunch blaze_RMX3031-userdebug
-export TZ=Asia/Kolkata #put before last build command
-brunch RMX3031
+export KBUILD_BUILD_USER=S8M1T
+export KBUILD_BUILD_HOST=S8M1T
+export BUILD_USERNAME=S8M1T
+export BUILD_HOSTNAME=S8M1T
+export TZ=Asia/Kolkata #put before last build command 
+brunch blaze_RMX3031-userdebug
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
