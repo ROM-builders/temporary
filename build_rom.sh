c709b67d06216e7d2413ec3f846002d095e94e2e
Orkun Ergün <104510596+orkunergun@users.noreply.github.com>
2022-06-25 17:46:07 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init -u https://github.com/PixelExperience/manifest -b twelve
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b twelve-plus -g default,-mips,-darwin,-notdefault
 git clone https://gitlab.com/R9Lab/Manifest.git --depth 1 -b PE-12.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
