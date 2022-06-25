d4065c13cf75c742762ffdfb4f3c462d13814608
Muhamed Lafeer <92130586+L4FeeR@users.noreply.github.com>
2022-06-25 17:50:37 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/L4FeeR/local_manifest.git --depth=1 -b main .repo/l
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # noob build.
-# build rom..
+# build rom.
 source build/envsetup.sh
 lunch evolution_tulip-user
 export BUILD_USERNAME=L4FeeR
