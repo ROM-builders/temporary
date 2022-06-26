aa793c96eb53623527f8f4fe2d15ac54270e0f81
Amrito <93515519+amritoj2@users.noreply.github.com>
2022-06-26 08:55:30 +0600

    r5x-Spark-Rom-spark-amritoj2
    Fixed error let's rebuild

--- build_rom.sh
+++ build_rom.sh
@@ -4,6 +4,7 @@ git clone https://github.com/amritoj2/local_manifest.git --depth 1 -b main .repo
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 
+
 # build rom
 source build/envsetup.sh
 lunch spark_r5x-user
