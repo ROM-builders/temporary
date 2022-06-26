00dbb0d4ce67c4100a88d513ff8cbd9a600ff5b5
Orkun Ergün <104510596+orkunergun@users.noreply.github.com>
2022-06-26 21:51:12 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b snow -g default,-mips,-darwin,-notdefault
-git clone https://github.com/MT6768Lab/local_manifest.git --depth 1 -b EvoX-12.1-lava .repo/local_manifests
+git clone https://gitlab.com/R9Lab/Manifest.git --depth 1 -b EvoX-12.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -11,3 +11,4 @@ mka evolution
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+rclone copy out/target/product/lava/*.zip.json cirrus:lava -P
