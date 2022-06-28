1fa35e1912273191a674ac04c66dbca41e42d4d6
AhmedKazim-Dev <88555521+AhmedKazim-Dev@users.noreply.github.com>
2022-06-29 04:32:17 +0500

    miatoll-AospExtended-12.1.x-AhmedKazim-Dev

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/AospExtended/manifest
 git clone https://github.com/AhmedKazim-Dev/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom 
+# build rom
 source build/envsetup.sh
 export CURRENT_BUILD_TYPE=gapps
 lunch aosp_miatoll-userdebug
