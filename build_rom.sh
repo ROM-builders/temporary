1db4053d09f3e36681fb26b34c89903aa4d032fb
SUMIT <72486118+S8M1T@users.noreply.github.com>
2022-06-28 15:46:38 +0530

    RMX3031-VoltageOS-12l-S8M1T

--- build_rom.sh
+++ build_rom.sh
@@ -3,11 +3,10 @@ repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest.gi
 git clone https://github.com/S8M1T/local_manifest.git --depth 1 -b RMX3031 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-
 # build rom
 . build/envsetup.sh
 export TZ=Asia/Delhi #put before last build command
-brunch voltage_RMX3031-eng
+brunch RMX3031
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
