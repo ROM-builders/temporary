57f3f9c1e938283813e2b4bc3b6097a46c9be883
Nischay Sanjay <nischay.sanjay23@gmail.com>
2022-06-29 00:24:54 +0530

    Switch back to 4.4 trees

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch aospa_tulip-userdebug
 export TZ=Asia/Kolkata #put before last build command
 ./rom-build.sh tulip
 
