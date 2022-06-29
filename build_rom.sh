13d946772801c478a0fbb2c6fb91811a1573b01d
Rahul <82636553+rahulkhatri137@users.noreply.github.com>
2022-06-29 20:51:47 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/CipherOS/android_mani
 git clone https://github.com/rahulkhatri137/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom 3
 . build/envsetup.sh
 lunch cipher_CPH1859-userdebug
 export TZ=Asia/Kolkata #put before last build command
