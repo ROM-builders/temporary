c63b70e80987c0491a372879e4ac2cbead6f1d81
Grom-exe <103995190+Grom-exe@users.noreply.github.com>
2022-06-30 00:24:26 +0200

    lava-VoltageOS-12l-Grom-exe
    Build VoltageOS for lava

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest.git -b 12l -g default,-mips,-darwin,-notdefault
-git clone https://gitlab.com/R9Lab/Manifest.git --depth 1 -b VoltageOS-12.1 .repo/local_manifests
+git clone https://github.com/Grom-exe/local_manifest --depth 1 -b VoltageOS12l-mt6768PHQ .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -11,3 +11,4 @@ brunch lava
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+
