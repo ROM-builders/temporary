abee7958d80002e429029c868ca3f3afa7fe68e8
hmm9912 <101258400+hmm9912@users.noreply.github.com>
2022-06-29 08:29:42 +0530

    cph1859-cipheros-twelve-L-parkerpet

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/parkerpet/local_manifest --depth 1 -b main .repo/lo
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+. build/envsetup.sh
 lunch cipher_CPH1859-userdebug
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
