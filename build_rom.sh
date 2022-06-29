d1ca5d72b9609ec08056bbafca475943fddf79b5
romgharti <romgharti@gmail.com>
2022-06-29 18:44:16 +0545

    mojito: SuperiorOS Bringup

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,16 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/AospExtended/manifest.git -b 12.1.x -g default,-mips,-darwin,-notdefault
-git clone https://github.com/romgharti/local_manifest.git --depth 1 -b aex-12.0 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.git -b twelvedotone -g default,-mips,-darwin,-notdefault
+git clone https://github.com/romgharti/local_manifest.git --depth 1 -b superior .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_mojito-user
-# export WITH_GAPPS=true
+lunch superior_mojito-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
+export ALLOW_MISSING_DEPENDENCIES=true
+# export BUILD_WITH_GAPPS=true
 export TZ=Asia/Dhaka #put before last build command
-m aex
+m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
