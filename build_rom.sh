68cc2c30a0f121a22985bff9556178ee9f43df25
Mayur <flynryder427@gmail.com>
2022-06-30 09:00:30 +0530

    release build

--- build_rom.sh
+++ build_rom.sh
@@ -6,8 +6,9 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 export TZ=Asia/Kolkata #put before last build command
-launch lavender user -cjbg -i lavender_release_incremental -o lavender --build-both-targets
-#
+launch lavender user -gjc -i lavender_release_incremental -o lavender --build-both-targets
+#7
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-rclone copy o*/t*/p*/l*/*json cirruslavender -P
+rclone copy lavender/*zip cirrus:lavender -P
+rclone copy OTA/*/*json cirrus:lavender -P
