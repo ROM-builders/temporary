340c00451383e00b1a225e762c4ef18d0e211b2d
Orkun Ergün <104510596+orkunergun@users.noreply.github.com>
2022-06-30 21:08:47 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -9,7 +9,7 @@ lunch fuse_lava-userdebug
 export BUILD_HOSTNAME=orkunergun
 export TARGET_KERNEL_CLANG_VERSION=proton
 export TZ=Asia/Dhaka #put before last build command
-make fuse
+make fuse-prod
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
