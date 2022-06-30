5fea46808d64e439e94859b0543c23ddddbff374
wanderlusttt <58342756+wanderlusttt@users.noreply.github.com>
2022-06-30 22:46:27 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/androi
 git clone https://github.com/wanderlusttt/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom - crDroidSettings > Customizations
+# build rom - retry crDroidSettings > Customizations
 source build/envsetup.sh
 export TZ=Asia/Dhaka #put before last build command
 brunch X00TD
