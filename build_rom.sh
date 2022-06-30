c92ec9196191f4ed4df98ac82b1fe00265372783
Rahul <82636553+rahulkhatri137@users.noreply.github.com>
2022-06-30 19:08:09 +0530

    CPH1859-xdroid-oss-twelve-rk137

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/CipherOS/android_manifest.git -b twelve-L -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-oss/xd_manifest -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/rahulkhatri137/local_manifest --depth 1 -b o .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom 5
+# build rom
 . build/envsetup.sh
-lunch cipher_CPH1859-userdebug
+lunch xdroid_CPH1859-userdebug
 export TZ=Asia/Kolkata #put before last build command
-mka bacon
+make xd
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
