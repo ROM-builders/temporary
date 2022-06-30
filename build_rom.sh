468a4e489df2ab13fc4b8efcb2684ace8cef73b7
Suraj Kumar <106401203+ImSurajx@users.noreply.github.com>
2022-06-30 20:35:06 +0530

    Say Hi To Pixel Experience

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/StagOS/manifest.git -b s12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ImSurajx/manifest --depth 1 -b StagOs .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b twelve -b s12.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/ImSurajx/manifest --depth 1 -b PixelExperience .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
-lunch stag_lavender-userdebug
+. build/envsetup.sh
+lunch aosp_lavender-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Kolkata #put before last build command
-make stag
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
