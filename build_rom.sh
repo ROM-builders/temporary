603b6a382ca3bb84a01c9c032e59214c08f61eca
Hasin Abrar Haque <70853665+AbrarNoob@users.noreply.github.com>
2022-07-02 02:07:43 +0600

    Test changes

--- build_rom.sh
+++ build_rom.sh
@@ -3,10 +3,9 @@ repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest
 git clone https://github.com/AbrarNoob/local_manifest --depth 1 -b blaze .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-
-
 # build rom
 source build/envsetup.sh
+export WITH_GAPPS=true
 lunch blaze_alioth-userdebug
 export TZ=Asia/Dhaka #put before last build command
 brunch alioth
