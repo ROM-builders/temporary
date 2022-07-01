7755b586ddabd9964eeaa85071e069df75412e6f
SUMIT <72486118+S8M1T@users.noreply.github.com>
2022-07-01 11:19:27 +0530

    RMX3031-ArrowOS-arrow-12.1-S8M1T

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
   
 # build rom
 . build/envsetup.sh
-lunch arrow_RMX3360-eng
+lunch arrow_RMX3301-eng
 export ARROW_GAPPS=true
 export TZ=Asia/Kolkata #put before last build command
 m bacon
