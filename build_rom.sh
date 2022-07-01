fbd9ed886195e26f8e7ac3d18f0bad7440350f3f
clhex <100755123+clhexftw@users.noreply.github.com>
2022-07-01 10:54:49 +0530

    switch back to qti powerhal

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,7 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-12.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/clhexftw/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-  
+   
 # build rom
 . build/envsetup.sh
 lunch arrow_vayu-userdebug
