b25fe2ce4c1cb08d5d9bf515b7ac07c4b37da378
SUMIT <72486118+S8M1T@users.noreply.github.com>
2022-07-01 11:02:10 +0530

    RMX3031-ArrowOS-arrow-12.1-S8M1T

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest.git -b 12l -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-12.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/S8M1T/local_manifest.git --depth 1 -b RMX3031 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
+  
 # build rom
 . build/envsetup.sh
-export TZ=Asia/Delhi #put before last build command
-brunch RMX3031
-
+lunch arrow_RMX3360-eng
+export ARROW_GAPPS=true
+export TZ=Asia/Kolkata #put before last build command
+m bacon
+    
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+ 
