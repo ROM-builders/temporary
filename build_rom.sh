4bdfc86aa969fbec6c5c4039cdbf061265049967
clhex <100755123+clhexftw@users.noreply.github.com>
2022-07-01 21:28:58 +0530

    switch to eng

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
   
 # build rom
 . build/envsetup.sh
-lunch arrow_X00TD-userdebug
+lunch arrow_X00TD-eng
 export SELINUX_IGNORE_NEVERALLOWS=true
 export ARROW_GAPPS=true
 export TZ=Asia/Kolkata #put before last build command
