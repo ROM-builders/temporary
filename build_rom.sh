303e22c88271ae13bad4fab4cee21774ca33ae66
ahmetakif616 <77838245+ahmetakif616@users.noreply.github.com>
2022-07-01 16:13:02 +0300

    olive-dotos6.2-ahm3tak1f616

--- build_rom.sh
+++ build_rom.sh
@@ -4,12 +4,10 @@ git clone https://github.com/ahmetakif616/local_manifest.git --depth 1 -b main .
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-# Initialize the environment with the envsetup.sh script:
 source build/envsetup.sh
-# lunch your device (codename)
-lunch dot_olive-userdebug
-# start compilation for your device
-make bacon
+lunch dotos_olive-user
+export TZ=Asia/Dhaka #put before last build command
+mka derp
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
