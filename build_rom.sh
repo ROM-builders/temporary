b1b633ebe9c6158d30e22d091507cb70a68db708
zhenyolka <zhenyolka@gmail.com>
2022-07-01 16:16:55 +0300

    Run launch twice to fix build

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch corvus_pyxis-userdebug
+lunch corvus_pyxis-userdebug && lunch corvus_pyxis-userdebug
 export SKIP_ABI_CHECKS=true
 make corvus
 
