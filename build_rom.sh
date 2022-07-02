30207fc6c3c68d979b5253d764260254c7b2e251
NP-XD <103098781+Tester9958@users.noreply.github.com>
2022-07-02 09:14:14 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch evolution_X01AD-userdebug
+export TARGET_USES_MINI_GAPPS=true
 export TZ=Asia/Kolkata #put before last build command
 mka evolution
 
