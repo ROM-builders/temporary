2a2a0f21d5805316a0b91396886a57fd5c18f28e
Gromexd <93418301+Gromexd@users.noreply.github.com>
2022-07-03 00:40:23 +0200

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 export SKIP_ABI_CHECKS=true
+lunch corvus_device-userdebug
 make corvus
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
