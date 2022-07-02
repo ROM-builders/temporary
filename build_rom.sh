205cc4055405375cbe69b9635e6a85a96a115adc
Gromexd <93418301+Gromexd@users.noreply.github.com>
2022-07-03 00:38:53 +0200

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch corvus_lava-userdebug
 export SKIP_ABI_CHECKS=true
 make corvus
 
