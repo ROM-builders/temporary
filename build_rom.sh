7e3ab15aef4130d390c1ce61fde99d84470b1972
Gromexd <93418301+Gromexd@users.noreply.github.com>
2022-07-03 00:20:44 +0200

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Corvus-R/android_manifest.git -b 12-test -g default,-mips,-darwin,-notdefault
+repo init -u https://github.com/Corvus-R/android_manifest.git -b 12-test -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Grom-exe/local-manifest.git --depth 1 -b lutisnus .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
