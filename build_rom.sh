ed98d7a75551078c1dc31bd471653c5d552d0c5d
zRyu <36316857+ryuKizuha@users.noreply.github.com>
2022-07-03 05:32:52 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ryuKizuha/android_manifest-cipher.git -b twelve-L -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/CipherOS/android_manifest.git -b twelve-L -g default,-mips,-darwin,-notdefault
 git clone https://github.com/ryuKizuha/local_manifests.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
