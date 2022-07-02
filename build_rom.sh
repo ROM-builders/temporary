5bc0a954b7c421403024106c2ae327b5316ff396
VitorGSC <102420586+VitorGSC@users.noreply.github.com>
2022-07-02 13:03:43 -0300

    Drop hacksource

--- build_rom.sh
+++ build_rom.sh
@@ -3,26 +3,6 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest.
 git clone https://github.com/VitorGSC/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# fix source for old moto devices
-
-cd frameworks/native
-git fetch https://github.com/phhusson/platform_frameworks_native android-12.0.0_r16-phh
-git cherry-pick 11160ca79ca44375af895f70af14bb2af909aa77
-git cherry-pick 40b43f648327b3fc13a18f0f28da54b34db11c79
-cd ../..
-cd system/netd
-git fetch https://github.com/phhusson/platform_system_netd android-12.0.0_r15-phh
-git cherry-pick 5f6bfe7390eafc659b36996398deb670436fc9df
-cd ..
-cd bpf
-git fetch https://github.com/phhusson/platform_system_bpf android-12.0.0_r15-phh
-git cherry-pick 2f0ac4a3596fc20c94828a01534fd77d12ec20dd
-cd ../..
-cd external/selinux
-git fetch https://github.com/phhusson/platform_external_selinux android-12.0.0_r16-phh android-12.0.0_r26-phh android-12.0.0_r28-phh
-git cherry-pick 010b772593639c9fdb4392ac976d5f3da4ea5e57
-cd ../..
-
 # build rom
 source build/envsetup.sh
 lunch evolution_ali-userdebug
