d0d0336815c035076555ada31b8e344aaf92157d
AhmedKazim-Dev <88555521+AhmedKazim-Dev@users.noreply.github.com>
2022-07-03 04:12:54 +0500

    miatoll-SuperiorOS-twelvedotone-AhmedKazim-DEV

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/AospExtended/manifest.git -b 12.1.x -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.git -b twelvedotone -g default,-mips,-darwin,-notdefault
 git clone https://github.com/AhmedKazim-Dev/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+. build/envsetup.sh
 export CURRENT_BUILD_TYPE=gapps
-lunch aosp_miatoll-userdebug
+lunch superior_miatoll-userdebug
 export CURRENT_BUILD_TYPE=gapps
-m aex
+m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
