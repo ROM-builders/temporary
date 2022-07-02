7ceae75102d6bad2734cd32bed77c496ab1a6b3a
Grom-exe <103995190+Grom-exe@users.noreply.github.com>
2022-07-03 00:07:14 +0200

    lava-Corvus-R-12-test
    Build CorvusOS for lava

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest.git -b 12l -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Grom-exe/local_manifest.git --depth 1 -b VoltageOS12l-mt6768PHQ .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Corvus-R/android_manifest.git -b 12-test -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Grom-exe/local-manifest.git --depth 1 -b lutisnus .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-export TZ=Asia/Dhaka #put before last build command
-brunch voltage_lava-eng
+lunch corvus_lava-userdebug
+lunch corvus_lava-userdebug
+make corvus
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
