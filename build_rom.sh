88cdc6036e86bcccf9854be9b51aed1602b8fdcc
zRyu <36316857+ryuKizuha@users.noreply.github.com>
2022-07-03 04:08:09 +0700

    change sync

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/CipherOS/android_manifest.git -b twelve-L -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ryuKizuha/local_manifests.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+git clone https://github.com/ryuKizuha/local_manifests.git --depth 1 -b master .repo/local_manifests
 
 # build rom
 . build/envsetup.sh
