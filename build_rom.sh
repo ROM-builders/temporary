3c820d199a1e2198f9eb0250e4dae50dd8d83388
zRyu <36316857+ryuKizuha@users.noreply.github.com>
2022-07-03 04:09:55 +0700

    delete vendor source

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,15 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/CipherOS/android_manifest.git -b twelve-L -g default,-mips,-darwin,-notdefault
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+rm -rf vendor/qcom/opensource/commonsys/system/bt
+rm -rf vendor/qcom/opensource/commonsys/bluetooth_ex
+rm -rf vendor/qcom/opensource/commonsys/packages/apps/Bluetooth
 git clone https://github.com/ryuKizuha/local_manifests.git --depth 1 -b master .repo/local_manifests
 
 # build rom
 . build/envsetup.sh
 lunch cipher_begonia-userdebug
-export TZ=Asia/Jakarta #put before last build command
+export TZ=Asia/Jakarta
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
