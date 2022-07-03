d96cc9f28010d4fa3935dae39c8494337ba253f4
Prashant <93838092+Prashant-1695@users.noreply.github.com>
2022-07-03 08:14:53 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,6 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
+export BUILD_WITH_GAPPS=true
 lunch superior_lavender-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
