a1035db19b1051e08cc34e68538b5767a60e0140
ＭＯＣＨＡ <80810363+official-mocha@users.noreply.github.com>
2022-07-04 23:57:07 +0530

    fix some minor issue in build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch superior_tulip-userdebug
 export BUILD_WITH_GAPPS=true
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export TZ=Asia/Kolkata #put before last build command
 m bacon
 
