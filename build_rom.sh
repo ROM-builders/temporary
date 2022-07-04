df28bdd7ceae22cdb04a56e408311401ef3a99fc
ＭＯＣＨＡ <80810363+official-mocha@users.noreply.github.com>
2022-07-04 17:17:14 +0530

    Fixing error

--- build_rom.sh
+++ build_rom.sh
@@ -3,6 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b
 git clone https://github.com/official-mocha/local_manifest.git --depth 1 -b spark .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+
 # build rom
 . build/envsetup.sh
 lunch spark_tulip-userdebug
