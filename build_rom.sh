329afa3f080179cf6d7b14d499dc2d92795ed058
zRyu <36316857+ryuKizuha@users.noreply.github.com>
2022-07-04 10:05:43 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,13 +4,14 @@ git clone https://github.com/ryuKizuha/local_manifest.git --depth 1 -b main .rep
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
+make clean
 . build/envsetup.sh
 lunch corvus_begonia-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
 export SKIP_ABI_CHECKS=true
 export BUILD_USERNAME=ryuKizuha
 export TZ=Asia/Jakarta 
-make begonia
+make corvus
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
