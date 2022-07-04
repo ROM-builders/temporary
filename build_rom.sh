75adedcb445e7945ae23becb84172fb655440cfe
zRyu <36316857+ryuKizuha@users.noreply.github.com>
2022-07-04 08:48:25 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -9,8 +9,8 @@ lunch corvus_begonia-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
 export SKIP_ABI_CHECKS=true
 export BUILD_USERNAME=ryuKizuha
-export TZ=Asia/Jakarta
-m corvus
+export TZ=Asia/Jakarta 
+make begonia
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
