62cc8d918ffdda5c5b05bef0cc4a781347729ac5
walternewtz <77871609+walternewtz@users.noreply.github.com>
2022-07-04 06:06:06 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,6 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch lineage_beryllium-user
 export TZ=Asia/Delhi #put before last build command
+export ALLOW_MISSING_DEPENDENCIES=true
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
