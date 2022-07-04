3a7db03a11fc2e3c1f9feaa63c104bc11244bde3
Mayur <flynryder427@gmail.com>
2022-07-04 11:46:28 +0530

    Banja

--- build_rom.sh
+++ build_rom.sh
@@ -4,8 +4,10 @@ git clone https://github.com/xenxynon-AOSPA/manifest --depth 1 -b master .repo/l
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
-./rom-build.sh lavender -t userdebug -s keys
+. build/envsetup.sh
+lunch aospa_lavender-user
+export TZ=Asia/Kolkata # put before last build command
+./rom-build.sh lavender -t userdebug -v beta
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
