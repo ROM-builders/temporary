f2789dba7caede56848d060a69d54bfc8cccded7
Mayur <flynryder427@gmail.com>
2022-07-04 12:36:47 +0530

    Manifest fixes

--- build_rom.sh
+++ build_rom.sh
@@ -8,6 +8,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 lunch aospa_lavender-user
 export TZ=Asia/Kolkata # put before last build command
 ./rom-build.sh lavender -t userdebug -v beta
-
+#2
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
