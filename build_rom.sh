ef6cdfb9762b586ac2d077d12188a997a5cd6aa5
himanshu0218 <70850303+himanshu0218@users.noreply.github.com>
2022-07-04 13:08:19 +0530

    Flos Alioth (fix sepolicy, revert some changes)

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ForkLineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/himanshu0218/local_manifest.git --depth 1 -b alioth .repo/local_manifests
+git clone https://github.com/himanshu0218/local_manifest.git --depth 1 -b alioth_lineage .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
