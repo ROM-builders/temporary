aaf5db8e9fd06a1110a64567893dde0217e25ec5
ＭＯＣＨＡ <80810363+official-mocha@users.noreply.github.com>
2022-07-05 08:22:39 +0530

    Fixing error

--- build_rom.sh
+++ build_rom.sh
@@ -3,12 +3,10 @@ repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.g
 git clone https://github.com/official-mocha/local_manifest.git --depth 1 -b superior .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-
 # build rom
 source build/envsetup.sh
 lunch superior_tulip-userdebug
 export BUILD_WITH_GAPPS=true
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export TZ=Asia/Kolkata #put before last build command
 m bacon
 
