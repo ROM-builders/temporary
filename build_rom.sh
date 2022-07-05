808f665d973c2cfde3ddaa4967c385f587616312
Haikal Luthfianino Balukia <51993230+chikafujiwara45@users.noreply.github.com>
2022-07-05 16:46:45 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/ConquerOS/manifest.git -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/chikafujiwara45/Local-Manifests.git --depth 1 -b conquer .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/xdroid-oss/xd_manifest -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/chikafujiwara45/Local-Manifests.git --depth 1 -b xdroid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch conquer_X00TD-userdebug
+lunch xdroid_X00TD-userdebug
 export TZ=Asia/Jakarta
-mka carthage
+export BUILD_USERNAME=haikalluthfianinobalukia
+mka xd
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
