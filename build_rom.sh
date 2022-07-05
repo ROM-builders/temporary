086b249384a0e29345c91abdd63f23355f12fddc
walternewtz <77871609+walternewtz@users.noreply.github.com>
2022-07-05 06:07:23 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/androi
 git clone https://github.com/walternewtz/local_manifest.git --depth 1 -b crdroid12.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom. 
+# build rom
 source build/envsetup.sh
 lunch lineage_beryllium-user
 export TZ=Asia/Delhi #put before last build command
