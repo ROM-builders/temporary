2c8e760019cb957ba5f5688ceb25716d7f4ef943
aleksiby <95564478+aleksiby@users.noreply.github.com>
2022-07-05 10:39:49 +0700

    Make Project Blaze 12.1
    Signed-off-by: aleksiby <95564478+aleksiby@users.noreply.github.com>

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelPlusUI-SnowCone/manifest -b snowcone-12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/aleksiby/joyeuse-manifests.git --depth 1 -b ppui .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/aleksiby/joyeuse-manifests.git --depth 1 -b blaze .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_joyeuse-userdebug
+lunch blaze_joyeuse-userdebug
 export TZ=Asia/Jakarta #put before last build command
-mka bacon
+brunch joyeuse
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
