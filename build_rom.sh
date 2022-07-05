c60959006ef2dab75f0e3652f6d085442609ab63
clhex <100755123+clhexftw@users.noreply.github.com>
2022-07-05 21:04:56 +0530

    fixup clone link

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/P-404/android_manifest -b shinka -g default,-mips,-darwin,-notdefault
-git clone https://github.com/clhex-p404lab/local_manifest --depth=1 -b main .repo/local_manifests
+git clone https://github.com/p404-vayu/local_manifest --depth=1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
