f04280b3ea45aefce2b4033f5a467da907503ff5
clhex <100755123+clhexftw@users.noreply.github.com>
2022-07-05 20:58:50 +0530

    switch to eng build

--- build_rom.sh
+++ build_rom.sh
@@ -5,13 +5,13 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch p404_vayu-userdebug
+lunch p404_vayu-eng
 export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 #export WITH_GAPPS=true
 export TZ=Asia/Kolkata #put before last build command
 make bacon
   
-# 17
+# 18
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
