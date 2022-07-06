38050b31e341822ed02a57a068587df9796b520b
walternewtz <77871609+walternewtz@users.noreply.github.com>
2022-07-06 07:43:09 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 12.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/walternewtz/local_manifest.git --depth 1 -b crdroid12.1 .repo/local_manifests
-rm -rf hardware/qcom-caf/sdm845
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch lineage_beryllium-user
+export SKIP_ABI_CHECKS=true
 export TZ=Asia/Delhi #put before last build command
 make bacon
 
