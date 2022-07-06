abb704eedeaa5ffb0c85481b184aeb57f69e2d6e
Angel Castillo <49908914+acastillorobles77@users.noreply.github.com>
2022-07-05 20:33:11 -0500

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-CAF/xd_manifest -b twelve -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-oss/xd_manifest.git -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/acastillorobles77/local_manifest.git --depth 1 -b xdroid-12.x-vayu .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
