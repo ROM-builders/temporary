d3af0ad196b217a75f29bbdd880f9da546ce63de
ＭＯＣＨＡ <80810363+official-mocha@users.noreply.github.com>
2022-07-06 09:10:21 +0530

    fixing error

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch superior_tulip-userdebug
-export BUILD_WITH_GAPPS=true
 export TZ=Asia/Kolkata #put before last build command
 m bacon
 
