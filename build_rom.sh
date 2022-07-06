9fe1a9c5ba5fbf920cc817b84e23b8455e535daa
Onkar singh <onkarsinghsahil@gmail.com>
2022-07-06 12:19:29 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Fusion-OS/android_manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/P-Salik/local_manifest --depth 1 -b fusionOS .repo/local_manifests
+git clone https://github.com/onkarsinghsahil/local_manifest --depth 1 -b fuse .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch fuse_RMX1941-userdebug
+lunch fuse_mido-userdebug
 export TZ=Asia/Kolkata #put before last build command
 make fuse-prod
 
