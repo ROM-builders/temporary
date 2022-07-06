968e78ab5da6b6a7f2d61eea3716a59c304e9733
Shivam1143 <103647875+Shivam1143@users.noreply.github.com>
2022-07-06 22:04:12 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b snow -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Ultron12bot/local_manifest.git --depth 1 -b EvoX .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/DotOS/manifest.git -b dot12.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Ultron12bot/local_manifest.git --depth 1 -b DOS .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch evolution_X01AD-userdebug
-export TARGET_USES_MINI_GAPPS=true
+lunch dot_X01AD-userdebug
 export TZ=Asia/Dhaka #put before last build command
-mka evolution
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
