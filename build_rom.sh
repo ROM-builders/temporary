e2c4ded5d46c000c3749f3b7753b27b44d95157e
Mayur <flynryder427@gmail.com>
2022-07-07 10:19:53 +0530

    Debloat some apps

--- build_rom.sh
+++ build_rom.sh
@@ -9,7 +9,7 @@ lunch aospa_lavender-user
 export TARGET_FLATTEN_APEX=true
 export TZ=Asia/Kolkata # put before last build command
 ./rom-build.sh lavender -t userdebug -s keys
-#3
+#4
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
 rclone copy *aospa**2022*zip cirrus:lavender -P
