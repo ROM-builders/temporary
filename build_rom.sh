880c167f045f89d549f972315b43211b210a0ba8
walternewtz <77871609+walternewtz@users.noreply.github.com>
2022-07-07 15:34:46 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -9,7 +9,8 @@ lunch lineage_beryllium-user
 export SKIP_ABI_CHECKS=true
 export SKIP_BOOTJAR_CHECKS=true
 export TZ=Asia/Delhi #put before last build command
-make clean && make bacon
+make clean 
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
