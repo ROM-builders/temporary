661cfd72518230baa010f85b4beb45c3a23b2776
Rahul Kantrapally <rahul-k@bigdi.cc>
2022-07-07 15:37:33 +0100

    fix sync issues

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/AICP/platform_manifest.git -b s12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/rk134/local_manifests.git --depth 1 -b aicp.repo/local_manifests
+git clone https://github.com/rk134/local_manifests.git --depth 1 -b aicp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
