bb09081cb175e7abbba12f42d072e94c833494d9
zRyu <36316857+ryuKizuha@users.noreply.github.com>
2022-07-07 19:00:59 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Corvus-R/android_manifest -b 12-test -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ryuKizuha/local_manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-oss/xd_manifest -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/ryuKizuha/local_manifest.git --depth 1 -b xd .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch corvus_begonia-userdebug
-lunch corvus_begonia-userdebug
+lunch xdroid_begonia-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
+export BUILD_USERNAME=ryuKizuha
 export TZ=Asia/Jakarta
-make corvus
-
+make xd
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
