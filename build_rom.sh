cfacdd23404892b16ffdde54acaf04ebbb2062e4
Abhiram <103818282+Azure-Helper@users.noreply.github.com>
2022-07-07 19:50:12 +0530

    userdebug

--- build_rom.sh
+++ build_rom.sh
@@ -5,13 +5,13 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build from [2]
 source build/envsetup.sh
-lunch aospa_raphael-eng
+lunch aospa_raphael-userdebug
 export KBUILD_BUILD_USER=azure
 export KBUILD_BUILD_HOST=azure
 export BUILD_USERNAME=azure
 export BUILD_HOSTNAME=azure
 export TZ=Asia/Delhi #put before last build command
-./rom-build.sh raphael -t eng -v beta
+./rom-build.sh raphael -t userdebug -v beta
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
