437a96dbe732f5c6ba9ccdbd6a99e98ec378844c
Matheus Souza <33396386+matheucomth@users.noreply.github.com>
2022-07-06 22:52:00 -0300

    July patch

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-Pixelish/manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/matheucomth/local_manifest.git --depth 1 -b PixelOS .repo/local_manifests
+git clone https://github.com/matheucomth/local_manifest.git --depth 1 -b PixelOS-4.4 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
