15dd824ad72711362f27c483debbbae2afb09f13
Haikal Luthfianino Balukia <51993230+chikafujiwara45@users.noreply.github.com>
2022-07-08 13:52:06 +0700

    Sepolicy Fix

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch xdroid_X00TD-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Jakarta
 export BUILD_USERNAME=Chizuru+
 mka xd
