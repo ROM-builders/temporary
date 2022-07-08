f939f73bbf837e7180a3e4dcd9a4086bcdc1e7c1
Haikal Luthfianino Balukia <51993230+chikafujiwara45@users.noreply.github.com>
2022-07-08 17:22:19 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -8,7 +8,7 @@ source build/envsetup.sh
 lunch xdroid_X00TD-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Jakarta
-export BUILD_USERNAME=Chizuru+
+export BUILD_USERNAME=chizuru
 mka xd
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
