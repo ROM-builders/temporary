112b052c54a39bfd9db376f99ff052acc4466930
wanderlusttt <58342756+wanderlusttt@users.noreply.github.com>
2022-07-08 07:54:41 +0530

    Build with proper ANT+

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git
 git clone https://github.com/wanderlusttt/local_manifest.git --depth 1 -b lineage-18.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom - Build with AOSP clang
+# build rom - Build with proper ANT+
 source build/envsetup.sh
 export TZ=Asia/Dhaka #put before last build command
 export LINEAGE_VERSION_APPEND_TIME_OF_DAY=true
