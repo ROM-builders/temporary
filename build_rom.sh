9af0aa14bdab675cb6f3dbcb9c8e96a61de2d400
JaswantTeja <sunnyteja201@gmail.com>
2022-07-08 07:58:07 +0530

    Let's Goo

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/JaswantTeja/local_manifest.git --depth 1 -b awaken .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Havoc-OS/android_manifest.git -b eleven -g default,-mips,-darwin,-notdefault
+git clone https://github.com/JaswantTeja/local_manifest.git --depth 1 -b havoc-1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
-export USE_GAPPS=true
-lunch awaken_r5x-userdebug
+. build/envsetup.sh
+lunch havoc_r5x-userdebug
 export TZ=Asia/Dhaka #put before last build command
-make bacon
+m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
