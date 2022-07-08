d30b48463a95bcf33d9214e2f4fa562dc19dc52a
khirendra03 <khirendrapatel3@gmail.com>
2022-07-08 08:45:11 +0530

    X01BD: july update

--- build_rom.sh
+++ build_rom.sh
@@ -3,6 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest.gi
 git clone https://github.com/khirendra03/local_manifest.git --depth 1 -b voltage .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+
 # build rom
 source build/envsetup.sh
 export TZ=Asia/Delhi #put before last build command
