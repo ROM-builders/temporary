fa05caa2d8ff7476b7b8826fb620059919f475c5
Snowman2409 <93831265+Snowman2409@users.noreply.github.com>
2022-07-09 18:44:13 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,10 +1,10 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExtended/manifest -b snow -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.git -b twelvedotone -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Snowman2409/local-manifest --depth=1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 # build rom
 source build/envsetup.sh
-lunch aosp_lavender-user
+lunch superior_lavender-userdebug
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
 export SELINUX_IGNORE_NEVERALLOWS=true
 
