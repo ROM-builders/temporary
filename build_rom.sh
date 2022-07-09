0c79cb2f7cf0b169075495c15cc92109670ceb58
Amritorock <101019800+Amritorock@users.noreply.github.com>
2022-07-09 16:27:45 +0600

    Hopefully error fixed

--- build_rom.sh
+++ build_rom.sh
@@ -4,6 +4,7 @@ git clone https://github.com/Amritorock/local_manifest.git --depth 1 -b main .re
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 
+
 # build rom
 . build/envsetup.sh
 lunch aosp_r5x-userdebug
