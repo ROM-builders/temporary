06abeb01f4f774e7754e03548720801074ebc7a0
Dragon-1237 <89452912+Dragon-1237@users.noreply.github.com>
2022-07-09 22:04:09 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch corvus_mido-userdebug
+lunch corvus_lavender-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true 
 export WITH_GAPPS=true
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
