67cc224efd46dd3a363fb40f7861d1f56c937139
romgharti <romgharti@gmail.com>
2022-07-09 13:36:19 +0545

    mojito: FluidOS bringup

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Project-Fluid/manifest -b fluid12.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Fluid/manifest -b fluid-12.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/romgharti/local_manifest.git --depth 1 -b fluid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
