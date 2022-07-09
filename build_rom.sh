62f884785e1eca4291369fe1baf9456c79a94b18
aslenofarid <92686606+aslenofarid@users.noreply.github.com>
2022-07-09 18:54:01 +0700

    Initial build

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,15 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ResurrectionRemix/platform_manifest.git -b Q -g default,-mips,-darwin,-notdefault
-git clone https://github.com/aslenofarid/local_manifest --depth 1 -b RR-Q .repo/local_manifests
+git clone https://github.com/aslenofarid/local_manifest --depth 1 -b ResurrectionRemix-Q .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch rr_X00TD-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
 export BUILD_USERNAME=asleno
 export TZ=Asia/Jakarta #put before last build command
 mka bacon
-
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
