0425f834d599b5349e4acf71575a0583286ae4ce
Moonitime: Ultra Romantic <97696888+moonitime@users.noreply.github.com>
2022-07-09 07:07:38 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-Pixelish/manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/moonitime/local_manifest.git --depth 1 -b pe .repo/local_manifests
+git clone https://github.com/Mooni-s-Lab/manifest.git --depth 1 -b pos .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
