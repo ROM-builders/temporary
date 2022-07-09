5a64a7578dc5768ab7610754d76dd03ad0d65eb0
NRanjan-17 <nalinishranjan05@gmail.com>
2022-07-09 06:35:36 +0530

    Its time for official blaze

--- build_rom.sh
+++ build_rom.sh
@@ -1,17 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b twelve-plus -g default,-mips,-darwin,-notdefault
-git clone https://github.com/NRanjan-17/local_manifest.git --depth 1 -b Pixel-Experience-12 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/NRanjan-17/local_manifest.git --depth 1 -b Blaze-12.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_mido-userdebug
-export KBUILD_BUILD_USER=hyper
-export KBUILD_BUILD_HOST=17
-export BUILD_USERNAME=hyper
-export BUILD_HOSTNAME=17
+lunch blaze_mido-userdebug
 export TZ=Asia/Delhi #put before last build command
-mka bacon
+brunch mido
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
