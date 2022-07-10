b6be3bd95f03d10c58bc84c53aa77799c2e15ee7
Suraj Kumar <surajkumar18jun2004@gmail.com>
2022-07-11 00:57:17 +0530

    Switch Device Tree Branch

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b twelve-plus -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ImSurajx/manifest --depth 1 -b pe-test .repo/local_manifests
+git clone https://github.com/ImSurajx/manifest --depth 1 -b PixelExperiencePlus .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
