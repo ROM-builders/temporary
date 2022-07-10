6ea2117c42ec4168d26b7391e298a8dbf306d018
Kannon <99554811+kann-on@users.noreply.github.com>
2022-07-10 19:59:48 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest.git -b snow -g default,-mips,-darwin,-notdefault
-git clone https://github.com/VitorGSC/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/kann-on/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch evolution_ali-userdebug
+lunch blaze_river-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka evolution
 
