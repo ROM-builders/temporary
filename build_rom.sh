883d12904de21acd1ce487d7ce60925002e3e9a5
clhex <100755123+clhexftw@users.noreply.github.com>
2022-07-10 15:01:22 +0530

    try fixing sepolicy

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch p404_vayu-userdebug
-#export SELINUX_IGNORE_NEVERALLOWS=true
+export SELINUX_IGNORE_NEVERALLOWS=true
 #export ALLOW_MISSING_DEPENDENCIES=true
 export WITH_GAPPS=true
 export TZ=Asia/Kolkata #put before last build command
