eba625151f0e1b48f102f28deebb8c3d3ffff79e
Matheus Souza <33396386+matheucomth@users.noreply.github.com>
2022-07-09 23:53:04 -0300

    PixelExperience July patch

--- build_rom.sh
+++ build_rom.sh
@@ -1,10 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b twelve-plus -g default,-mips,-darwin,-notdefault
+git clone https://github.com/matheucomth/local_manifest.git --depth 1 -b PixelOS .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+. build/envsetup.sh
+lunch aosp_tulip-userdebug
 lunch derp_vayu-user
 export TZ=Asia/Dhaka #put before last build command
 mka derp
