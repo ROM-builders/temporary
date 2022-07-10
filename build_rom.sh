13fc6d3b534513d78f660b36b5226d2ffd21cb1a
Dragon-1237 <89452912+Dragon-1237@users.noreply.github.com>
2022-07-10 16:01:38 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch corvus_lavender-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true 
+export SELINUX_IGNORE_NEVERALLOWS=true
 export WITH_GAPPS=true
 export TZ=Asia/Dhaka #put before last build command
 make corvus
