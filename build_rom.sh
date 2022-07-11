a6b16f8809ed2a993b8dae43d0c8d7d9caeab33e
Kannon <99554811+kann-on@users.noreply.github.com>
2022-07-11 11:53:18 -0300

    river ProjectBlaze kann-on
    build ProjectBlaze for river

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/kann-on/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo sync -c -j8 --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune 
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
+lunch blaze_river-userdebug
 export TZ=Asia/Dhaka #put before last build command
-mka derp
+brunch river
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
