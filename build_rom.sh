aa556a860378a9dde9aefcdbe942a0353d642c41
Shridhan Varadkar <shridhan98@gmail.com>
2022-07-11 10:16:23 +0530

    surya: Trigger

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest.git -b 12l -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Shreedhan003/local_manifest.git --depth 1 -b test .repo/local_manifests
+git clone https://github.com/Shreedhan003/local_manifest.git --depth 1 -b suryavos .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
 export TZ=Asia/Delhi ##put before last build command
-brunch lavender
+brunch surya
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-# rclone copy vendor/ota/lavender.json cirrus:lavender -P
+rclone copy vendor/ota/lavender.json cirrus:lavender -P
