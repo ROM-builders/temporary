84fa8ab405b426309e7255d2bca8942c21bc0d8f
DragonRider188 <109064859+DragonRider188@users.noreply.github.com>
2022-07-11 15:04:17 +0600

    Let's build awaken os for r5x

--- build_rom.sh
+++ build_rom.sh
@@ -3,6 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/androi
 git clone https://github.com/DragonRider188/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+
 # build rom
 source build/envsetup.sh
 lunch awaken_r5x-userdebug
