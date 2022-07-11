559b33442f7c8fbe44ef525607d45d0833d01ec6
67 <94082908+sweervin@users.noreply.github.com>
2022-07-11 16:38:22 +0300

    July ASB

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest -b arrow-12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/sweervin/local_manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/sweervin/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 . build/envsetup.sh
