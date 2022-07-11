debd0eec314bb9280c3f43f6979bec5c351a1fc8
NRanjan-17 <nalinishranjan05@gmail.com>
2022-07-11 15:20:59 +0530

    mido: user build

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch aosp_mido-userdebug
+lunch aosp_mido-user
 export KBUILD_BUILD_USER=hyper
 export KBUILD_BUILD_HOST=17
 export BUILD_USERNAME=hyper
