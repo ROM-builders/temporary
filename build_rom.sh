50f2ba84765ec4a74f6d7da92530b33206f8c09d
Sager Chowdary <104504298+SagerChowdary@users.noreply.github.com>
2022-07-12 16:49:32 +0530

    Less Gii

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/JaswantTeja/local_manifest.git --depth 1 -b blaze .repo/local_manifests
+git clone https://github.com/SagerChowdary/local_manifest.git --depth 1 -b blaze .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
