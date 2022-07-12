352019b6dfed6afa8fb0476c95514f91572983a3
Mayur <flynryder427@gmail.com>
2022-07-12 08:52:43 +0530

    July update

--- build_rom.sh
+++ build_rom.sh
@@ -5,13 +5,13 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_lavender-userdebug
+lunch lineage_lavender-user
 export TARGET_FLOS=true
 export WITH_GMS=true
 export BUILD_BROKEN_DUP_SYSPROP=true
 export TZ=Asia/Kolkata #put before last build command
 make bacon
 
-# 6
+# 7
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
