7f13621615c6bab6114aef61ac361eec442dabfa
Prashant <93838092+Prashant-1695@users.noreply.github.com>
2022-07-12 11:53:39 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch komodo_lavender-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
 mka komodo
 
