5ff116db3b5b526c316e4e0c6496afb766d67ce3
Angel Castillo <49908914+acastillorobles77@users.noreply.github.com>
2022-07-13 17:31:57 -0500

    Test

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Havoc-OS/android_manifest.git -b eleven -g default,-mips,-darwin,-notdefault
-git clone https://github.com/acastillorobles77/local_manifest.git --depth 1 -b havoc-11.0-vayu .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Havoc-OS/android_manifest.git -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/acastillorobles77/local_manifest.git --depth 1 -b havoc-12.x-vayu .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
-export TZ=Asia/Dhaka #put before last build command
+. build/envsetup.sh
+export TZ=America/Mexico_City #put before last build command
 brunch
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
