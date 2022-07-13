20357e3eaa614b4aef18f24c6c21e3ed27982fd4
VitorGSC <102420586+VitorGSC@users.noreply.github.com>
2022-07-13 04:57:12 -0300

    Test update

--- build_rom.sh
+++ build_rom.sh
@@ -5,6 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
+source vendorsetup.sh
 lunch evolution_ali-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka evolution
