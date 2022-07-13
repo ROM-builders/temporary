574642872a72bcaaa7daa1a54e7a2ccadab67395
Rahul <82636553+rahulkhatri137@users.noreply.github.com>
2022-07-13 20:12:56 +0530

    Allow Missing

--- build_rom.sh
+++ build_rom.sh
@@ -5,6 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
+export ALLOW_MISSING_DEPENDENCIES=true
 lunch cipher_CPH1859-userdebug
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
