77981e59919ae31254626ccb912596dff00c534b
Lambada10 <62511588+Lambada10@users.noreply.github.com>
2022-07-13 09:13:26 +0200

    Build Vanilla

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-export WITH_GAPPS=true
+# export WITH_GAPPS=true
 export ARROW_GAPPS=true # TODO: nuke that check
 export TZ=Europe/Warsaw #put before last build command
 brunch vayu
