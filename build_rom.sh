6caa92469929a92a8048fdd2c6aa7c24d668bd34
Christopher Keith Irvine <Mizdrake7@gmail.com>
2022-07-13 15:03:15 +0530

    Update build_rom.sh
    * Define lunch choice
    Signed-off-by: Christopher <mizdrake7@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -5,6 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
+lunch voltage_r5x-userdebug
 export TZ=Asia/Dhaka #put before last build command
 brunch r5x
 
