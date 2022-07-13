ed9a1589937569631c200d442819b58e63cd99c7
Nischay Sanjay <nischay.sanjay23@gmail.com>
2022-07-13 15:05:32 +0530

    Clone sepolicy

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch p404_tulip-userdebug
+lunch p404_tulip-eng
 export TZ=Asia/Kolkata ##put before last build command
 make bacon
 
