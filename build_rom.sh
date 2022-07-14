4af9059fa30b281abf53136c6fbe21ce4777ac68
Mr-Prince0 <109238814+Mr-Prince0@users.noreply.github.com>
2022-07-14 11:01:44 +0530

    RMX1801-Corvus-R-12-test-Mr-Prince0
    Build

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 -u https://github.com/Corvus-R/android_manifest.git -b 12-test
-git clone https://github.com/Mr-Prince0/corvus-manifest-.git --depth 1 -b main .repo/corvus_manifests
+repo init --depth=1 -u https://github.com/Corvus-R/android_manifest.git -b 12-test -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Mr-Prince0/local-manifest-.git --depth 1 -b main .repo/local_manifests
 repo sync -j$(nproc --all) --force-sync --no-tags --no-clone-bundle
 
 # build rom
 build/envsetup.sh
 lunch corvus_device-userdebug
-export TZ=Asia/Dhaka #put before last build command
+export TZ=Asia/Kolkata #put before last build command
 make corvus
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
