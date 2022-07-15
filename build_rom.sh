1d09143a438d6dcb5233a844ece845c34a673178
T.B.S <ewprjkt@proton.me>
2022-07-15 21:46:06 +0700

    Turu

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch syberia_X00TD-userdebug
-export TZ=Asia/Shanghai #put before last build command
+export SELINUX_IGNORE_NEVERALLOWS=true
+export TZ=Asia/Makassar #put before last build command
 make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
