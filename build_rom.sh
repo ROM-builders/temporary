b8eb1f299ce2ab2bb098321b1322f1869e47ba51
Matheus Souza <33396386+matheucomth@users.noreply.github.com>
2022-07-14 22:04:41 -0300

    Fix loop

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_man
 git clone https://github.com/matheucomth/local_manifest.git --depth 1 -b cherish-twelve .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build roms
+# build ro
 . build/envsetup.sh
 lunch cherish_tulip-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
