6400fdeb46ae3eba2f144f946d142ccdb7056cfe
Mr-rouz <109348355+Mr-rouz@users.noreply.github.com>
2022-07-15 21:14:51 +0700

    land-crDroid-Mr-rouz

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/crdroid/android.git -b crdroidandroid-12.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/crdroid/android.git -b crDroid -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Mr-rouz/local_manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch crdroid_land-userdebug
+lunch crDroid_land-userdebug
 export TZ=Asia/Dhaka #put before last build command
-mka crdroid
+mka crDroid
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
