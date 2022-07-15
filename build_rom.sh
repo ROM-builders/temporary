89089044c4ead455d2c4912cd678c27fc4aba56d
Haikal Luthfianino Balukia <51993230+megumin775@users.noreply.github.com>
2022-07-15 19:12:31 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,14 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Komodo-OS/manifest -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/chikafujiwara45/Local-Manifests.git --depth 1 -b komodo-12.1 .repo/local_manifests
+git clone https://github.com/megumin775/Local-Manifests.git --depth 1 -b komodo-12.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build
 source build/envsetup.sh
 lunch komodo_X00TD-userdebug
 export TZ=Asia/Jakarta
-export BUILD_USER=Takami
-export BUILD_USERNAME=Chika
+export BUILD_USER=chunchunmaru
+export BUILD_USERNAME=megumin
 export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export RELAX_USES_LIBRARY_CHECK=true
