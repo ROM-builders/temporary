2038d7c118a6dc43efa6aa79fcc67116d41c651b
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2022-07-15 20:28:31 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 . build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
-lunch aosp_evert-userdebug
+lunch aosp_evert
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
