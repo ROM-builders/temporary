0751b767481c624ef8f3e1c5a945ef02f587c7fd
P Salik <78614601+P-Salik@users.noreply.github.com>
2022-07-16 08:26:40 +0530

    Fix error

--- build_rom.sh
+++ build_rom.sh
@@ -14,3 +14,4 @@ rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1
 rclone copy out/target/product/RMX1941/recovery.img cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
 
 
+
