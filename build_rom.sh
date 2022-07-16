ba89c8e3e2095f2bfa1ba536d55c3e4e08cb9838
Mr-Prince0 <109238814+Mr-Prince0@users.noreply.github.com>
2022-07-17 00:42:41 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch dot_RMX1801-userdebug
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
