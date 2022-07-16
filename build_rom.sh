d72c5f6271126fca88175897b16222c3f0084c79
ISHAN DEY <99247751+IshanDeyA12@users.noreply.github.com>
2022-07-16 07:21:56 +0000

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/CherishOS-Temp/android_manifest.git -b twelve-one -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest -b twelve-one -g default,-mips,-darwin,-notdefault
 git clone https://github.com/IshanDeyA12/local_manifest --depth 1 -b cherish .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
