31dbdaa88f756e55672263dab1be370eaee48cc4
romgharti <romgharti@gmail.com>
2022-07-16 18:08:46 +0545

    mojito: fixing

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch aosp_mojito-userdebug
+lunch p404_mojito-userdebug
 #export SELINUX_IGNORE_NEVERALLOWS=true
 #export ALLOW_MISSING_DEPENDENCIES=true
 # export WITH_GAPPS=true
