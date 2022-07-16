d22250e894c54b0d964401dad9de075a5aa22eee
iskandarasri03 <71484902+iskandarasri03@users.noreply.github.com>
2022-07-17 00:54:31 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/CarbonROM/android.git -b cr-9.0 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/PixelOS-Pixelish/manifes t-b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/iskandarasri03/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch carbon_garden-userdebug
+lunch aosp_fog-userdebug
 export TZ=Asia/Kuala Lumpur #put before last build command
-make carbon
+mka bacon 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
