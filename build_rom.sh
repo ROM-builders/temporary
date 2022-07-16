87716500bde0d6ae6cb853f3375663bbc715ca20
Haikal Luthfianino Balukia <51993230+megumin775@users.noreply.github.com>
2022-07-17 02:24:27 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Fusion-OS/android_man
 git clone https://github.com/megumin775/Local-Manifests.git --depth 1 -b komodo-12.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build 
+# build rom
 . build/envsetup.sh
 lunch fuse_X00TD-user
 export TZ=Asia/Jakarta
