f4f209cabc88b64576d3d68361120b0ea7e67751
sajidshahriar <sazidshahriar39@gmail.com>
2022-07-16 08:48:09 +0600

    Bery: FusionOS

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/Fusion-O
 git clone https://github.com/sajidshahriar72543/local_manifest.git --depth 1 -b fuse .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-#16
+#17
 
 # build rom
 source build/envsetup.sh
