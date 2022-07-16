6a77084368e88e6e1afdccc83b4575ea4cc2d4bc
SUMIT <72486118+S8M1T@users.noreply.github.com>
2022-07-16 19:07:52 +0530

    RMX3360-VoltageOS-12l-S8M1T

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,7 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest -b 12l -g default,-mips,-darwin,-notdefault
 git clone https://github.com/S8M1T/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
+ 
 # build rom
 . build/envsetup.sh
 export TZ=Asia/Delhi #put before last build command
