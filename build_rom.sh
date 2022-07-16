5c7e1ae74131aa85ba16bfef68b9ddc2eccd924b
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2022-07-16 00:04:55 -0300

    evert-Fusion-OS-twelve-Aflaungos
    Build Fusion OS for evert

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,17 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Aflaungos/local_manifest --depth 1 -b Project-Blaze .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/Fusion-OS/android_manifest -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Aflaungos/local_manifest.git --depth 1 -b fuse .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+#17
+
 # build rom
-. build/envsetup.sh
-export ALLOW_MISSING_DEPENDENCIES=true
-export SELINUX_IGNORE_NEVERALLOWS=true
-lunch aosp_evert-userdebug
-export TZ=Asia/Kolkata #put before last build command
-mka bacon
+source build/envsetup.sh
+lunch fuse_evert-userdebug
+export TZ=Asia/Dhaka
+export KBUILD_BUILD_USER=PaperBoy
+export BUILD_HOSTNAME=PaperBoy #put before last build command
+make fuse-prod
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
