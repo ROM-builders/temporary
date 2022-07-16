da4d714e9de172c0fedfcda456a254094d0d95ef
ISHAN DEY <99247751+IshanDeyA12@users.noreply.github.com>
2022-07-16 07:15:46 +0000

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/IshanDeyA12/local_manifest --depth 1 -b awaken .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/CherishOS-Temp/android_manifest.git -b twelve-one -g default,-mips,-darwin,-notdefault
+git clone https://github.com/IshanDeyA12/local_manifest --depth 1 -b cherish .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch awaken_alioth-userdebug
+lunch cherish_alioth-userdebug
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
