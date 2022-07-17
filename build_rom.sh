65b9370147a0120e4955ea128292f6eef5e09e6c
Mr-Prince0 <109238814+Mr-Prince0@users.noreply.github.com>
2022-07-17 16:50:22 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/DotOS/manifest.git -b dot12.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Mr-Prince0/local-manifest-.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-. build/envsetup.sh
+source build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
-lunch blaze_RMX1801-userdebug
-
+export SELINUX_IGNORE_NEVERALLOWS=true
+lunch dot_RMX1801-userdebug
 export TZ=Asia/Dhaka #put before last build command
-brunch RMX1801
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
