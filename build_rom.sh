99c0253d3d4965ba090ebe64eae1f3b784b728e3
Anandhasayanan <anandhasayanan007jb@gmail.com>
2022-07-17 06:24:34 +0530

    Build Flos gapps

--- build_rom.sh
+++ build_rom.sh
@@ -3,11 +3,11 @@ repo init --depth=1 --no-repo-verify -u https://github.com/ForkLineageOS/android
 git clone https://github.com/anandhan07/local_manifest.git --depth 1 -b lineage-19.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom [6]
+# build rom [7]
 source build/envsetup.sh
 lunch lineage_vince-user
 export TARGET_FLOS=true
-export WITH_GMS=false
+export WITH_GMS=true
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
