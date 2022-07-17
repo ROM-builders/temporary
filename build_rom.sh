b7faf2a170626e33f104924dfeadfa630e7e3697
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2022-07-17 12:12:22 -0300

    evert-ProjectBlaze-12.1-Aflaungos

--- build_rom.sh
+++ build_rom.sh
@@ -9,6 +9,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch blaze_evert-userdebug
 export TZ=Asia/Dhaka
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
 export KBUILD_BUILD_USER=PaperBoy
 export BUILD_HOSTNAME=PaperBoy #put before last build command
 make bacon
