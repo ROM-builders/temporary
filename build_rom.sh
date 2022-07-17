417d5e9232f6bbdd62f7d2d00b095443d00616cd
aslenofarid <92686606+aslenofarid@users.noreply.github.com>
2022-07-17 15:25:12 +0700

    Hmmm

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-17.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/aslenofarid/local_manifest.git --depth 1 -b lineage-17.1 .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j1
+repo sync
 
 # build rom
 source build/envsetup.sh
@@ -9,7 +9,6 @@ breakfast X00TD
 export BUILD_USERNAME=aslenofarid
 export TZ=Asia/Jakarta #put before last build command
 brunch X00TD
-
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
