39b8ab5b87cc32e6bd75b2e1cdeb099188c02301
Amrito Karmokar <101019800+Amritorock@users.noreply.github.com>
2022-07-17 08:54:38 +0600

    Hope fixed sync

--- build_rom.sh
+++ build_rom.sh
@@ -3,6 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/androi
 git clone https://github.com/Amritorock/local_manifest.git --depth 1 -b LineageOS .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+
 # build rom
 . build/envsetup.sh
 export WITH_GMS=true
