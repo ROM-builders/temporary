219a73f322c0de92dddd2f0dcb717a70696020aa
Meghthedev <96943033+Meghthedev@users.noreply.github.com>
2022-07-17 18:47:23 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,9 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 12.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Meghthedev/local_manifest .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-git clone https://github.com/realme-mt6771-devs/android_device_oppo_CPH1859 device/oppo/CPH1859
-git clone https://github.com/realme-mt6771-devs/android_device_realme_mt6771-common device/realme/mt6771-common
-git clone https://github.com/realme-mt6771-devs/android_kernel_realme_mt6771 kernel/realme/mt6771
-git clone https://github.com/realme-mt6771-devs/android_vendor_oppo_CPH1859 vendor/oppo/CPH1859
-rm -rf device/mediatek/sepolicy_vndr
-git clone https://github.com/CorvusRom-Devices/device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr
-
-# build rom
+ # build rom
 source build/envsetup.sh
 lunch lineage_CPH1859-userdebug
 export TZ=Asia/Kolkata #put before last build command
