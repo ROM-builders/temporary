384169c83477a1d39b9698811cea6635f86fef7c
Raiden Ishigami <contact.raidenishi69@gmail.com>
2022-07-18 18:30:29 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_A001d-userdebug
+lunch lineage_A001D-userdebug
 export TZ=America/Sao_Paulo
 mka bacon
 
