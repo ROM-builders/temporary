d3367d2920d9a0b4cc53710e1ac44464b7b31491
Silentshot7 <62003211+Silentshot7@users.noreply.github.com>
2022-07-18 10:42:35 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify u https://github.com/VoltageOS/manifest -b 12l -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Silentshot7/local_manifest.git --depth 1 -b voltage .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Havoc-OS/android_manifest.git -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Silentshot7/local_manifest.git --depth 1 -b havoc .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch voltage_mido-userdebug
+lunch aosp_mido-userdebug
 export SKIP_ABI_CHECKS=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
