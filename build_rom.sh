f7c7b275cd38d02f774c337bed78e1eb8d8bbc86
Haikal Luthfianino Balukia <51993230+megumin775@users.noreply.github.com>
2022-07-18 21:58:57 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,18 +1,19 @@
-# sync
-repo init --depth=1 --no-repo-verify -u https://github.com/Fusion-OS/android_manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/megumin775/Local-Manifests.git --depth 1 -b komodo-12.1 .repo/local_manifests
+# sync rom
+repo init --depth=1 --no-repo-verify -u https://github.com/Komodo-OS/manifest -b 12.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/megumin775/Local-Manifests.git --depth 1 -b komodo-fixed .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build 
+# build rom
 . build/envsetup.sh
-lunch fuse_X00TD-user
+lunch komodo_X00TD-userdebug
 export TZ=Asia/Jakarta
-export BUILD_USER=chunchunmaru
-export BUILD_USERNAME=megumin
+export BUILD_USER=hklknz
+export BUILD_HOST=cloud
+export BUILD_USERNAME=honoka
 export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export RELAX_USES_LIBRARY_CHECK=true
-mka fuse-prod
+mka komodo
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
