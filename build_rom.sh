f7f7dc0143c379a5691fc5de557e1cda90d84077
Raiden Ishigami <contact.raidenishi69@gmail.com>
2022-07-18 18:19:54 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,8 +5,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_a001d-userdebug
-export TZ=Asia/Dhaka #put before last build command
+lunch lineage_A001d-userdebug
+export TZ=America/Sao_Paulo
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
