fd31651ab9bb1b3b113e58a67cb8278f79e92272
Raiden Ishigami <contact.raidenishi69@gmail.com>
2022-07-19 18:36:32 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,10 +4,10 @@ git clone https://github.com/raidenkkj/local_manifest --depth 1 -b master .repo/
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-. build/envsetup.sh
+source build/envsetup.sh
 lunch lineage_A001D-userdebug
 export TZ=America/Sao_Paulo
-brunch A001D
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
