ca1e99dc92f587d3f2d592bac3669e2330c4a1e0
Haikal Luthfianino Balukia <51993230+megumin775@users.noreply.github.com>
2022-07-19 10:19:07 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Fusion-OS/android_manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/megumin775/Local-Manifests.git --depth 1 -b fusion .repo/local_manifests
+git clone https://github.com/megumin775/Local-Manifests.git --depth 1 -b Fusion-OS .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
