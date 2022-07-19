53b499062b18a09eb5c85bf1c109b795c51394b6
Amrito Karmokar <101019800+Amritorock@users.noreply.github.com>
2022-07-19 13:44:59 +0600

    Let's build crdroid gapps for r5x

--- build_rom.sh
+++ build_rom.sh
@@ -5,8 +5,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-export WITH_GMS=true
 lunch lineage_r5x-userdebug
+export WITH_GMS=true
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
