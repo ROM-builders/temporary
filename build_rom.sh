125d439ef03da776f33bf5a2e3524fa7d9268339
Amrito Karmokar <101019800+Amritorock@users.noreply.github.com>
2022-07-19 07:09:27 +0600

    Let's build spark again

--- build_rom.sh
+++ build_rom.sh
@@ -3,8 +3,6 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b
 git clone https://github.com/Amritorock/local_manifest.git --depth 1 -b spark .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-
-
 # build rom
 . build/env*
 lunch spark_r5x-userdebug
