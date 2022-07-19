80fdceeda3bd848f3324d5672de1cd11b2114d10
LittleChest <81231195+LittleChest@users.noreply.github.com>
2022-07-19 21:25:13 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 -u https://github.com/PixelExperience/manifest.git -b twelve-plus -g default,-mips,-darwin,-notdefault
-git clone https://github.com/maluueu/local_manifest.git --depth 1 -b vangogh .repo/local_manifests
+git clone https://github.com/LittleChest/local_manifest.git --depth 1 -b vangogh .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
