3d7caa24d460a1ba1213e005738122abfa309dea
OmarAlCoptan <omarcoptan9@gmail.com>
2022-07-21 00:40:57 +0200

    Test build

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,7 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/OmarAlCoptan/local_manifest.git --depth 1 -b blaze .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-   
+    
 # build rom 
 source build/envsetup.sh
 #export WITH_GAPPS=true
