cbdd5a033f351c2cdd7ef8fd6cb1d6a14ae4b0f4
Haikal Luthfianino Balukia <51993230+megumin775@users.noreply.github.com>
2022-07-20 18:35:46 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,19 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Komodo-OS/manifest -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/megumin775/Local-Manifests.git --depth 1 -b komodo-fixed .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-Pixelish/manifest -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/megumin775/Local-Manifests.git --depth 1 -b PixelOS .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch komodo_X00TD-userdebug
+lunch aosp_X00TD-userdebug
 export TZ=Asia/Jakarta
 export BUILD_USER=hklknz
 export BUILD_HOST=cloud
 export BUILD_USERNAME=honoka
-mka komodo
+export SELINUX_IGNORE_NEVERALLOWS=true
+export ALLOW_MISSING_DEPENDENCIES=true
+export RELAX_USES_LIBRARY_CHECK=true
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
