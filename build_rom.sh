ea06f23b79bceeeb4afc2f22158bc62d5e11a57c
Rahul <82636553+rahulkhatri137@users.noreply.github.com>
2022-07-20 06:45:24 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-oss/xd_manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/rahulkhatri137/local_manifest --depth 1 -b s .repo/local_manifests
+git clone https://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@github.com/rahulkhatri137/mani --depth 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom 2
 . build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
 lunch xdroid_CPH1859-user
