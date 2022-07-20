35a07d6976d8229dc82789899b652a017f3ea09e
aslenofarid <92686606+aslenofarid@users.noreply.github.com>
2022-07-20 09:20:35 +0700

    Hmm

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,16 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-17.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/aslenofarid/local_manifest.git --depth 1 -b lineage-17.1 .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j1
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j4
 
 # build rom
+make clobber
 source build/envsetup.sh
 breakfast X00TD
 export BUILD_USERNAME=aslenofarid
 export TZ=Asia/Jakarta #put before last build command
 croot
-make bacon -j$(nproc--all)
+bruch X00TD
 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
