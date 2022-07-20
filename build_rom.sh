55b9b247abb302c089e4f840adfe2e8914a8c35d
Tanjiro-12 <109660954+Tanjiro-12@users.noreply.github.com>
2022-07-20 20:39:11 +0530

    RMX1801-VoltageOS-Tanjiro-12

--- build_rom.sh
+++ build_rom.sh
@@ -7,6 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 . build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
+export WITH_GAPPS=true
 export TZ=Asia/Dhaka #put before last build command
 brunch RMX1801
 
