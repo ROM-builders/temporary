a80394665ab7ba70a3257fe30c804033b393efb0
Onkar singh <onkarsinghsahil@gmail.com>
2022-07-20 10:07:41 +0530

    Build userdebug

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch xdroid_mido-eng
+lunch xdroid_mido-userdebug
 export TZ=Asia/Dhaka #put before last build command
 make xd
 
