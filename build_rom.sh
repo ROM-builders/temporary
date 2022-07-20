44db0284e25e310e4546e5f17b41a6e02bca45b8
aslenofarid <92686606+aslenofarid@users.noreply.github.com>
2022-07-20 18:47:44 +0700

    Okey

--- build_rom.sh
+++ build_rom.sh
@@ -5,12 +5,10 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-breakfast X00TD
+lunch lineage_X00TD-userdebug
 export BUILD_USERNAME=aslenofarid
 export TZ=Asia/Jakarta #put before last build command
-croot
-bruch X00TD
-
+brunch X00TD
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
