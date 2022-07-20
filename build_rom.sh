02fae9282781fd9b8216164124f85ec2a1d1888e
Krish-Na12 <109458465+Krish-Na12@users.noreply.github.com>
2022-07-20 08:05:38 +0530

    RMX1801-ProjectBlaze-Krish-Na12

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 . build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
-lunch blaze_RMX1801-userdebug
+#lunch blaze_RMX1801-userdebug
 export TZ=Asia/Dhaka #put before last build command
 brunch RMX1801
 
