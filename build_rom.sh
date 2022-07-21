af8479b36777ac42f0af571888ff9b3cb9dc91ef
itzorr <103074348+itzorr@users.noreply.github.com>
2022-07-21 09:16:37 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/StagOS/manifest.git -b s12.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/itzorr/manifest.git -b s12.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/itzorr/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
