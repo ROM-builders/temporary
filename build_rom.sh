d10958a98727d6f531798ef51b2a5a41cdd6b0df
LittleChest <81231195+LittleChest@users.noreply.github.com>
2022-07-21 21:10:11 +0800

    my name🤗

--- build_rom.sh
+++ build_rom.sh
@@ -7,6 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch aosp_vangogh-user
 export TZ=Asia/Shanghai #put before last build command
+export BUILD_USERNAME=littlechest
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
