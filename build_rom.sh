9e88234a968cb2aca7fa97ae58cc844179e7db8d
Mayur <flynryder427@gmail.com>
2022-07-21 15:43:58 +0530

    ಠ益ಠ dont fail dammit

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-oss/xd_manifest -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/XenStuff/manifest --depth 1 -b xdroid .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
