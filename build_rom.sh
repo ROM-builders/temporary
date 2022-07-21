22ba9418933e04eded1cd3374ebe02a589861709
Christopher Keith Irvine <Mizdrake7@gmail.com>
2022-07-21 13:29:36 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,6 @@ repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_man
 git clone https://github.com/mizdrake7/local_manifest.git --depth 1 -b cherish .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-
 # build rom
 . build/envsetup.sh
 export CHERISH_VANILLA=true
