6a939e72211ce175b4bec4881272595414231a5e
Don boruza <109259258+donboruza@users.noreply.github.com>
2022-07-22 04:44:43 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch lineage_begonia-userdebug
-export TZ=Asia/Dhaka #put before last build command
+export TZ=Asia/Jakarta
 brunch begonia
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
