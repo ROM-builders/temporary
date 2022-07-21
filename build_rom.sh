7034e42b55740d98d06d56f7cae8f3a20683ba4e
Christopher Keith Irvine <Mizdrake7@gmail.com>
2022-07-21 23:24:10 +0600

    NeverAllows true

--- build_rom.sh
+++ build_rom.sh
@@ -5,6 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Kolkata #put before last build command
 brunch r5x
 
