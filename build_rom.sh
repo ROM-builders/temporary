7b1ef4f90edd8e04673449c5ab4e28e00ceec7c7
arkmark99 <109340333+arkmark99@users.noreply.github.com>
2022-07-22 13:51:15 +0530

    ?

--- build_rom.sh
+++ build_rom.sh
@@ -3,8 +3,6 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Project-Fluid/manifes
 git clone https://github.com/arkmark99/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# new
-
 # build rom
 source build/envsetup.sh
 lunch fluid_CPH1859-userdebug 
