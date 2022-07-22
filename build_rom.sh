b642c8444003f7f453e7505ab74686dcf318c03f
ChiwaCime <51258794+ChiwaCime@users.noreply.github.com>
2022-07-22 17:33:25 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,14 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ChiwaCime/local_manifest.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/ChiwaCime/local_manifest.git --depth 1 -b monet .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
 lunch lineage_monet-user
 export SKIP_ABI_CHECKS=true
-export TZ=Asia/Dhaka #put before last build command
-mka monet
+export TZ=Asia/Chongqing #put before last build command
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
