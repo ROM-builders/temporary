233cf8254e74a6fbc29160bd86014fa3c3fe6e46
Sri19944 <89214859+Sri19944@users.noreply.github.com>
2022-07-22 22:16:25 +0530

    Mi439-evolution-Sri19944

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch evolution_mi439-user
+lunch evolution_mi439-userdebug
 export TZ=Asia/Delhi #put before last build command
 mka evolution
 
