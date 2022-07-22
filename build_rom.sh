6133ae00302600423c412159870f53954e90abb7
Haikal Luthfianino Balukia <51993230+megumin775@users.noreply.github.com>
2022-07-22 07:35:24 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-Pixelish/mani
 git clone https://github.com/megumin775/Local-Manifests.git --depth 1 -b PixelOS .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build
 . build/envsetup.sh
 lunch aosp_X00TD-userdebug
 export TZ=Asia/Jakarta
