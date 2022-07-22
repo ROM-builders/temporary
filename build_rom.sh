a9283422114de8a695309e75e8ebc4562bf3ea8a
Christopher Keith Irvine <Mizdrake7@gmail.com>
2022-07-22 13:27:10 +0600

    Hope sync error fixed

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/mizdrake7/local_manifest.git --depth 1 -b cherish .
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-. build/envsetup.sh
+source build/envsetup.sh
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Kolkata #put before last build command
 brunch r5x
