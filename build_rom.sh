a3d66a7ab8af3e4b3dfed7cd0402939b2b9defc4
wHoEMi <ehteshammalik18998@gmail.com>
2022-07-24 00:24:36 +0530

    updated some blobs

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch aospa_lavender-user
-export TZ=Asia/Kolkata #put before last build command
+export TZ=Asia/Kolkata # put before last build command
 ./rom-build.sh lavender -t user -v beta -s keys
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
