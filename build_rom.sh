67751e48ea363d8ab4a73fe9c962fd2e2c66a33d
Amrito Karmokar <101019800+Amritorock@users.noreply.github.com>
2022-07-23 15:25:03 +0600

    Let's build voltage

--- build_rom.sh
+++ build_rom.sh
@@ -3,6 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest -b
 git clone https://github.com/Amritorock/local_manifest.git --depth 1 -b voltage .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+
 # build rom
 source build/envsetup.sh
 lunch voltage_r5x-userdebug
