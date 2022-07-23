2e540cc03914887d3bcff9dae0b647671d188ba0
Matheus Souza <33396386+matheucomth@users.noreply.github.com>
2022-07-23 10:28:25 -0300

    eng build

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch superior_tulip-userdebug
+lunch superior_tulip-eng
 export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export RELAX_USES_LIBRARY_CHECK=true
