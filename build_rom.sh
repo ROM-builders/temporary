403623918907a1684e6f22f3004d279281e77df9
Rahul Singh Bhadoriya <94305290+StockAndroid@users.noreply.github.com>
2022-07-23 21:34:23 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,16 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Corvus-R/android_manifest.git -b 12-test -g default,-mips,-darwin,-notdefault
 git clone https://github.com/StockAndroid/local_manifest --depth 1 -b main .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -j$(nproc --all) --force-sync --no-tags --no-clone-bundle
 
-# build rom
-source build/envsetup.sh
-lunch lineage_miatoll-userdebug
-export TZ=Asia/Kolkata #put before last build command
-make bacon
+# Set up environment
+. build/envsetup.sh
+
+# Choose a target
+lunch corvus_device-userdebug
+
+# Build the ROM
+make corvus
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
