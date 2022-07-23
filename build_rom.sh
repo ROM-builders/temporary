8c1b54ea84a17688330b2350694a29bc8c8322ce
İsmail <84140360+Headache01@users.noreply.github.com>
2022-07-23 15:13:49 +0000

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -10,3 +10,4 @@ brunch havoc_GM8_sprou-userdebug
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+
