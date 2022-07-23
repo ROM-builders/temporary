8e551cd38199f5c80e799a1600d1051a198bdda7
PowerDamp <103279826+PowerDamp@users.noreply.github.com>
2022-07-23 18:49:19 +0530

    beryllium-PixelExperience-twelve-plus-power

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b twelve-one -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest.git -b twelve-plus -g default,-mips,-darwin,-notdefault
 git clone https://github.com/PowerDamp/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch cherish_beryllium-user
+lunch aosp_beryllium-user
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
