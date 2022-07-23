b359b2a01c9ed2f01c4819e0ecded60e16dfd839
Christopher Keith Irvine <Mizdrake7@gmail.com>
2022-07-23 07:30:07 +0600

    Let's build cherish

--- build_rom.sh
+++ build_rom.sh
@@ -9,7 +9,7 @@ source build/envsetup.sh
 lunch cherish_r5x-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Kolkata #put before last build command
-brunch r5x
+make bacon 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
