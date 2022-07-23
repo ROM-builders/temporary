9670e753bd3a5f1b07ae28ff854bd49c7c895a4c
Don boruza <109259258+donboruza@users.noreply.github.com>
2022-07-23 22:59:14 +0700

    increase zram to 4Gb

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch lineage_begonia-userdebug
+export BUILD_USERNAME=don.boruza
 export TZ=Asia/Jakarta
 mka bacon
 
