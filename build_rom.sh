44a448387b8fe7cd3bf458d25f702d7d87c1a50a
Prashant <93838092+Prashant-1695@users.noreply.github.com>
2022-07-24 19:40:04 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/P-404/android_manifest.git -b shinka -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Prashant-1695/local_manifest.git --depth 1 -b shinka .repo/local_manifests
+git clone https://github.com/Prashant-1695/local_manifest.git --depth 1 -b kd .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
