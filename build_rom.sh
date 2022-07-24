2481385b320e9a0a5b521d6dc30534ddaf2e71ce
Progcker <83493179+Progcker@users.noreply.github.com>
2022-07-24 15:22:29 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch ssos_CPH1859-userdebug
+export TZ=Asia/Kolkata #put before last build
 make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
