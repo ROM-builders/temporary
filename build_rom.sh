61175c605db7d0464c67db745b4b5d87fe889c16
Sumit Mohanty <72486118+S8M1T@users.noreply.github.com>
2022-07-24 15:35:16 +0530

    RMX3360-ForkLineageOS-lineage-19.1-S8M1T

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,7 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/ForkLineageOS/android -b lineage-19.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/S8M1T/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
- 
+
 # build rom
 . build/envsetup.sh
 lunch lineage_RMX3360-userdebug
