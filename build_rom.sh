0a8f0c9795e269000ea6676f9f4dd529c4369ca6
Progcker <83493179+Progcker@users.noreply.github.com>
2022-07-24 23:56:13 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,10 +5,9 @@ git clone https://github.com/Progcker/local_manifest --depth 1 -b main .repo/loc
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
-lunch ssos_CPH1859-userdebug
-export TZ=Asia/Kolkata #put before last build
-make bacon
+source build/envsetup.sh;
+lunch potato_CPH1859-userdebug;
+brunch CPH1859;
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
