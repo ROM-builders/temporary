f193a5a35db834c9b424f7001ac0c05459e30f74
Meghthedev <96943033+Meghthedev@users.noreply.github.com>
2022-07-24 20:22:55 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Project-Fluid/manifes
 git clone https://github.com/Meghthedev/local_manifest .repo/local_manifests -b Pixys
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
- # build rom
+ #  build rom
 source build/envsetup.sh
 lunch fluid_CPH1859-userdebug
 export TZ=Asia/Kolkata #put before last build command
