7cf3580c5dcca054d00d2a18199b288616eb86bf
Christopher Keith Irvine <Mizdrake7@gmail.com>
2022-07-24 06:18:43 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch voltage_r5x-userdebug
-export TZ=Asia/Kolkata #put before last build command
+export TZ=Asia/Delhi #put before last build command
 brunch r5x
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
