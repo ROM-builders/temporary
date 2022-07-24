ad4f5f357196ddd78441383519f2f367a5c4f387
Mayur <flynryder427@gmail.com>
2022-07-24 11:56:44 +0530

    Relez build

--- build_rom.sh
+++ build_rom.sh
@@ -8,8 +8,8 @@ source build/envsetup.sh
 lunch p404_lavender-user
 export WITH_GAPPS=true
 export TZ=Asia/Kolkata #put before last build command
-. rom-build.sh
+./build.sh bacon
 
-# 34
+# 35
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
