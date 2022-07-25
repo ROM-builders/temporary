48f4060cf66f32528204da87abda1cdcdbeb3e1e
Christopher Keith Irvine <Mizdrake7@gmail.com>
2022-07-26 04:45:26 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b test -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b twelve-one -g default,-mips,-darwin,-notdefault
 git clone https://github.com/mizdrake7/local_manifest.git --depth 1 -b cherish .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
