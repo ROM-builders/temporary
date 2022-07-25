a86e63c748333ec38585f56f899a209515edd193
Progcker <83493179+Progcker@users.noreply.github.com>
2022-07-25 19:26:30 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,14 @@
 # sync rom
-
 repo init --depth=1 --no-repo-verify -u https://github.com/PotatoProject/manifest -b frico_mr1-release -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Progcker/local_manifest --depth 1 -b main .repo/local_manifests
-
+git clone https://github.com/Progcker/local_manifest.git --depth 1 -b potato .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-
-export TZ=Asia/Kolkata #put before last build command
-brunch potato_CPH1859-userdebug
+lunch potato_CPH1859-userdebug
+export WITH_GMS=true
+export TZ=Asia/Dhaka #put before last build command
+brunch CPH1859
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
