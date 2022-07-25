6f128e94e28a01f3108b800a4bb701cf05c4dbf5
Hasin Abrar Haque <70853665+AbrarNoob@users.noreply.github.com>
2022-07-25 17:41:51 +0600

    2.7.2

--- build_rom.sh
+++ build_rom.sh
@@ -3,6 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Project-Fluid/manifes
 git clone https://github.com/AbrarNoob/local_manifest --depth 1 -b fluid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+
 # build rom
 . build/envsetup.sh
 lunch fluid_alioth-userdebug
