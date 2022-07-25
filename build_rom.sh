5c0f2541f9352a0fd95cea02dd1eeb9c818089d0
ChiwaCime <51258794+ChiwaCime@users.noreply.github.com>
2022-07-25 08:26:01 +0800

    Update build_rom.sh
    Trigger

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 . build/envsetup.sh
 lunch lineage_monet-userdebug
 export SKIP_ABI_CHECKS=true
-export TZ=Asia/Chongqing #put before last build command
+export TZ=Asia/Chongqing #put before last build command 
 make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
