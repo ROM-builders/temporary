e614d6f1c8b8b5564a51e764e7df11679c022b30
İsmail <84140360+Headache01@users.noreply.github.com>
2022-07-25 12:52:01 +0000

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,8 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Havoc-OS/android_manifest.git -b eleven -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Headache01/local_manifest.git --depth 1 -b havoc-11 .repo/local_manifests
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 # build rom
 source build/envsetup.sh
 export TZ=Asia/Dhaka #put before last build command
