c754eb1dd434797edc8524788d5e43cef5ee77b8
T.B.S <ewprjkt@proton.me>
2022-07-25 22:41:09 +0700

    Turu

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/Tiktodz/local_manifest.git --depth 1 -b fluid .repo
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+. build/envsetup.sh
 lunch fluid_X00TD-userdebug
 export TZ=Asia/Makassar #put before last build command
 mka bacon
