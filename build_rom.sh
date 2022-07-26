38c3a0ce5864d55ecefaded99439b889e99af923
Progcker <83493179+Progcker@users.noreply.github.com>
2022-07-27 01:47:26 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch p404_$devicecodename-userdebug
+lunch p404_CPH1859-userdebug
 #export SELINUX_IGNORE_NEVERALLOWS=true
 #export TZ=Asia/Kolkata #put before last build command
 make bacon
