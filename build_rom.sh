8d4098fd38ed300dc9b4434042cf9fd076067412
Dooms-v <109235336+Dooms-v@users.noreply.github.com>
2022-07-26 21:16:25 +0530

    beryllium-Crdroid-12.1-Dooms

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest.git -b 12l -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 12.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Dooms-v/local-manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch voltage_beryllium-userdebug
+lunch lineage_beryllium-user
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
