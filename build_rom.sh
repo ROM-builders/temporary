7d058afe7e00a3ed70a95b1193dc46cff199220d
Progcker <83493179+Progcker@users.noreply.github.com>
2022-07-27 02:13:58 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -8,7 +8,6 @@ source build/envsetup.sh
 lunch p404_CPH1859-userdebug
 export WITH_GAPPS=true
 export P404_BUILDTYPE=GAPPS
-#export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Kolkata #put before last build command
 make bacon
 
