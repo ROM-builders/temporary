03179f68722b5d3d139708503f41d0e0570a3700
Don boruza <109259258+donboruza@users.noreply.github.com>
2022-07-27 04:23:45 +0700

    build: fix missing icon IME button

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b
 git clone https://github.com/donboruza/local_manifests.git --depth 1 -b spark .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build roms
+# build rom
 source build/envsetup.sh
 lunch spark_begonia-userdebug
 export BUILD_USERNAME=don.boruza
