da54b0517fbfce82d2f81212850faf73e9f7d23c
Progcker <83493179+Progcker@users.noreply.github.com>
2022-07-26 09:18:46 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -13,8 +13,7 @@ export BUILD_WITH_GAPPS=true
 #export TARGET_SUPPORTS_GOOGLE_RECORDER=true
 export TZ=Asia/Kolkata #put before last build command
 m bacon
-export TZ=Asia/Kolkata #put before last build command
-m bacon
+
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
