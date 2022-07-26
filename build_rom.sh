65aeff7526f2b405fefd14ffc36810134abe1175
Leinier Cruz Salfran <leiniercs@gmail.com>
2022-07-26 13:02:26 +0000

    Fixes

--- build_rom.sh
+++ build_rom.sh
@@ -7,6 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch aosp_garden-user
 export TZ=UTC #put before last build command
+export SELINUX_IGNORE_NEVERALLOWS=false
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
