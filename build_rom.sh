a8ec9d39b1967712db32f3309a268716ca100b00
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2022-07-26 09:33:03 -0300

    evert-crdroidandroid-12.1-Aflaungos

--- build_rom.sh
+++ build_rom.sh
@@ -7,11 +7,11 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_evert-userdebug
+
 export TZ=Asia/Dhaka
 export KBUILD_BUILD_USER=PaperBoy
 export BUILD_HOSTNAME=PaperBoy #put before last build command
-m bacon
+breakfast evert
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
