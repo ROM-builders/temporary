1762e990937499f548673a48d9443fac92d6ecb1
Don boruza <109259258+donboruza@users.noreply.github.com>
2022-07-27 16:04:38 +0700

    build: Make selinux permissive

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Project-Fluid/manifes
 git clone https://github.com/donboruza/local_manifests.git --depth 1 -b fluid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build roms
+# build rom
 . build/envsetup.sh
 lunch fluid_begonia-userdebug
 export BUILD_USERNAME=don.boruza
