40e2edf490ea6d199522715f7be275a713825d7d
Dooms-v <109235336+Dooms-v@users.noreply.github.com>
2022-07-27 23:54:46 +0530

    beryllium--BlissRoms-arcadia-next-Dooms

--- build_rom.sh
+++ build_rom.sh
@@ -5,6 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
+lunch bliss_beryllium-user
 export TZ=Asia/Dhaka #put before last build command
 blissify -v beryllium
 
