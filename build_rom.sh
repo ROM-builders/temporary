a8c73fb75fd723bdfdf50e8d49d28f0f95954bc4
Amrito Karmokar <101019800+Amritorock@users.noreply.github.com>
2022-07-27 23:46:07 +0600

    Hope now rom booting fixed

--- build_rom.sh
+++ build_rom.sh
@@ -3,6 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manif
 git clone https://github.com/Amritorock/local_manifest.git --depth 1 -b Pe+ .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+
 # build rom
 . build/envsetup.sh
 lunch aosp_r5x-userdebug
