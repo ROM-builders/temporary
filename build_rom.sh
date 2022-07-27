69791df2e48e7db30ac86a0e003307a54bdf2d29
İsmail <84140360+Headache01@users.noreply.github.com>
2022-07-27 14:15:28 +0000

    GM8_sprout-ResurrectionRemix-Q-Headache01

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Havoc-OS/android_manifest.git -b eleven -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Headache01/local_manifest.git --depth 1 -b havoc-11 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ResurrectionRemix/platform_manifest.git -b Q -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Headache01/local_manifest.git --depth 1 -b rr-q .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 export TZ=Asia/Dhaka #put before last build command
-brunch havoc_GM8_sprout-userdebug
+lunch rr_GM8_sprout-userdebug
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
