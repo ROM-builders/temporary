5bd425b8ffcb1eab9bcc440ce2afa420936a7731
Christopher Keith Irvine <Mizdrake7@gmail.com>
2022-07-28 00:56:45 +0600

    Let's build fluid os for r5x

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b twelve-one -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Fluid/manifest.git -b fluid-12.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/mizdrake7/local_manifest.git --depth 1 -b fluid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
