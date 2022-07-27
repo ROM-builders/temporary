cdcb34c30fa8a418b4537e03fc589665077df406
VioletBlue90 <107307040+VioletBlue90@users.noreply.github.com>
2022-07-27 21:07:04 +0700

    build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/projectarcana-aosp -b 11 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS -b 11 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/VioletBlue90/local_manifest --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch projectarcana_mido-user
+lunch lineage_mido-user
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
