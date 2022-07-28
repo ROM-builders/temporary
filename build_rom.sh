82cb2a4091b2c63fef19ff479e26627ae4cbc0d7
Matheus Souza <33396386+matheucomth@users.noreply.github.com>
2022-07-27 23:17:44 -0300

    build userdebug

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch cherish_tulip-eng
+lunch cherish_tulip-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export RELAX_USES_LIBRARY_CHECK=true
