62e25c0d0ecfcc82a7ffeb5d454d9798415f7d6b
qsharp501 <65484455+qsharp501@users.noreply.github.com>
2022-07-28 10:08:47 +0700

    alioth-fluid-userdebug-qsharp501

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,8 @@
-# sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest -b twelve-one -g default,-mips,-darwin,-notdefault
-git clone https://github.com/qsharp501/local_manifest --depth 1 -b cherish .repo/local_manifests
+@@ -3,7 +3,6 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Project-Fluid/manifes
+git clone https://github.com/AbrarNoob/local_manifest --depth 1 -b fluid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
-source build/envsetup.sh
-lunch cherish_alioth-userdebug
-export TZ=Asia/Dhaka #put before last build command
-make bacon
 
-# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
-rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+# build rom
+. build/envsetup.sh
+lunch fluid_alioth-userdebug
