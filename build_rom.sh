44a6089bfdaf734a0adcb1ee8c123e1916b50ef9
qsharp501 <65484455+qsharp501@users.noreply.github.com>
2022-07-28 10:30:10 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-@@ -3,7 +3,6 @@ repo init --depth=1 and -g default,-mips,-darwin,-notdefault https://github.com/Project-Fluid/manifest
+@@ -3,7 +3,6 @@ repo init --depth=1 and -g default,-mips,-darwin,-notdefault https://github.com/Project-Fluid/manifest.git
 git clone https://github.com/qsharp501/local_manifest --depth 1 -b fluid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
@@ -6,3 +6,18 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch fluid_alioth-userdebug
+
+
+# sync rom
+repo init --depth=1 --no-repo-verify -u git://github.com/Project-Fluid/manifest.git -b fluid-12.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/qsharp501/local_manifest.git --depth 1 -b master .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
+# build rom
+source build/envsetup.sh
+lunch fluid-alioth-userdebug
+export TZ=Asia/Dhaka #put before last build command
+mka bacon
+
+# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
