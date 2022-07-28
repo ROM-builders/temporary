a0dcee4eb8b51fd73e8e798e014dfbad2ae04ef0
T.B.S <ewprjkt@proton.me>
2022-07-28 14:52:39 +0700

    selinux ignore

--- build_rom.sh
+++ build_rom.sh
@@ -7,6 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch lineage_X00TD-userdebug
 export BUILD_USERNAME=stress
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Makassar #put before last build command
 mka bacon
 
