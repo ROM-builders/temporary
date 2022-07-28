e3a46eedbd21976e432d083af163952350bb9c0b
OmarAlCoptan <omarcoptan9@gmail.com>
2022-07-28 02:41:48 +0200

    Test

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom
+I# sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/OmarAlCoptan/local_manifest.git --depth 1 -b blaze .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
@@ -18,4 +18,5 @@ rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1
  
   
   
+ 
  
