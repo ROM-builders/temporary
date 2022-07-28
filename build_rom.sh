3fa39f8f607d887dcb44727776b27cf01ee0ab96
dtyghc <100001963+dtyghc@users.noreply.github.com>
2022-07-28 19:21:51 +0530

    fixed a mistake

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch dot_veux-userdebug
+lunch dot_olives-userdebug
 export WITH_GAPPS=true
 export TZ=Asia/Dhaka #put before last build command
 make bacon
