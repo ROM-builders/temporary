8b1dac81c4173fc3bfc49ef887d16a705a5b78bd
Progcker <83493179+Progcker@users.noreply.github.com>
2022-07-28 19:25:33 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Project-Elixir/manifest -b snow -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/PotatoProject/manifest -b frico_mr1-release -g default,-mips,-darwin,-notdefault
 git clone https://github.com/progcker/local_manifest --depth 1 -b Elixir .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-. build/envsetup.sh
-lunch aosp_CPH1859-userdebug
+source build/envsetup.sh
+lunch potato_CPH1859-userdebug
 export TZ=Asia/Delhi #put before last build command
 mka bacon
 
