79c032d5f211445c067941e010926dbc947ec9b7
T.B.S <ewprjkt@proton.me>
2022-07-28 10:19:26 +0700

    Regenerate

--- build_rom.sh
+++ build_rom.sh
@@ -1,10 +1,10 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android -b lineage-18.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Tiktodz/local_manifest.git --depth 1 -b 18 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-. build/envsetup.sh
+source build/envsetup.sh
 lunch lineage_X00TD-userdebug
 export BUILD_USERNAME=stress
 export TZ=Asia/Makassar #put before last build command
