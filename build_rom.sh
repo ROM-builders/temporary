af0e63eaf1194305054fb5918743f50bb963797f
Progcker <83493179+Progcker@users.noreply.github.com>
2022-07-28 08:21:02 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Meghthedev/local_manifest --depth 1 -b arrow .repo/local_manifests
+git clone https://github.com/progcker/local_manifest --depth 1 -b arrow .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
