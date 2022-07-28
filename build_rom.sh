adbf51cde54b18d87e8638b28349df2ec9c67894
qsharp501 <65484455+qsharp501@users.noreply.github.com>
2022-07-28 10:19:04 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-@@ -3,7 +3,6 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Project-Fluid/manifes
+@@ -3,7 +3,6 @@ repo init --depth=1 and -g default,-mips,-darwin,-notdefault https://github.com/Project-Fluid/manifes
 git clone https://github.com/qsharp501/local_manifest --depth 1 -b fluid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
