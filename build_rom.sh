e743070c6465d0ad5a20f1e977febb479cd29f23
Progcker <83493179+Progcker@users.noreply.github.com>
2022-07-28 08:11:35 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,10 +4,11 @@ git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
-lunch derp_vayu-user
-export TZ=Asia/Dhaka #put before last build command
-mka derp
+. build/envsetup.sh
+export ARROW_GAPPS=true
+lunch arrow_CPH1859-userdebug
+export TZ=Asia/Delhi #put before last build command
+m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
