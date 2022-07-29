b05d7437d91bc1091113307989c2fca66f2dc0b3
Christopher Keith Irvine <Mizdrake7@gmail.com>
2022-07-30 00:39:26 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -11,6 +11,7 @@ export KBUILD_BUILD_USER=MAdMiZ
 export KBUILD_BUILD_HOST=MAdMiZ
 export BUILD_USERNAME=MAdMiZ
 export BUILD_HOSTNAME=MAdMiZ
+export RELAX_USES_LIBRARY_CHECK=true
 export TZ=Asia/Kolkata #put before last build command
 make bacon
 
