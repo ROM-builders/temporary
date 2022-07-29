791ba118714d5976f49344e5112040da319f0dfe
Don boruza <109259258+donboruza@users.noreply.github.com>
2022-07-30 03:54:24 +0700

    build: try without depth1

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Corvus-R/android_manifest.git -b 12-test -g default,-mips,-darwin,-notdefault
-git clone https://github.com/donboruza/local_manifests.git --depth 1 -b corvus .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+git clone https://github.com/donboruza/local_manifests.git! -b corvus .repo/local_manifests
+repo sync -j8 --force-sync --no-tags --no-clone-bundle
 
 # build roms
 source build/envsetup.sh
