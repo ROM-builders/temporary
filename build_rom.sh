83ada0443bb92cddc4490785b64c37b5e9be8165
Dooms-v <109235336+Dooms-v@users.noreply.github.com>
2022-07-29 21:29:47 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch bliss_beryllium-user
+lunch bliss_beryllium-userdebug
 export TZ=Asia/Dhaka #put before last build command
 blissify -g beryllium
 
