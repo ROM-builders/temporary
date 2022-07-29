bf080fff72c94bcaca294992cca4d8d4aed28e8a
Don boruza <109259258+donboruza@users.noreply.github.com>
2022-07-30 06:04:03 +0700

    build: fix innocent !

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Corvus-R/android_manifest.git -b 12-test -g default,-mips,-darwin,-notdefault
-git clone https://github.com/donboruza/local_manifests.git! -b corvus .repo/local_manifests
+git clone https://github.com/donboruza/local_manifests.git -b corvus .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build roms
