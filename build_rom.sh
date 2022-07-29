44c303db8283e8555b804403a796041a9aface44
Hemanth <hemanthjabalpuri@gmail.com>
2022-07-29 08:34:12 +0530

    Havoc-OS v4.19

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Havoc-OS/android_mani
 git clone https://github.com/HemanthJabalpuri/local_manifest --depth 1 -b havoc-11-UI1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom [5]
+# build rom [6]
 source build/envsetup.sh
 lunch havoc_RMX2185-userdebug
 export TZ=Asia/Dhaka #put before last build command
