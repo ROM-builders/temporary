3c6aba2cf8465ef63b9cfcec6c529d9691d30665
Don boruza <109259258+donboruza@users.noreply.github.com>
2022-07-30 09:06:48 +0700

    build: Nuke manual clone

--- build_rom.sh
+++ build_rom.sh
@@ -1,10 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Corvus-R/android_manifest.git -b 12-test -g default,-mips,-darwin,-notdefault
-git clone --depth=1 https://github.com/donboruza/device_redmi_note8pro -b corvus device/redmi/begonia
-git clone --depth=1 https://github.com/donboruza/vendor_redmi_note8pro -b twelve vendor/redmi/begonia
-git clone --depth=1 https://github.com/donboruza/android_kernel_xiaomi_mt6785 -b 12.1 kernel/xiaomi/mt6785
-git clone --depth=1 https://github.com/donboruza/android_device_mediatek_sepolicy -b 12.1 device/mediatek/sepolicy
-git clone --depth=1 https://github.com/donboruza/android_vendor_redmi_begonia-ims -b 12.0 vendor/redmi/begonia-ims
+git clone https://github.com/donboruza/local_manifests.git --depth 1 -b corvus .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build roms
