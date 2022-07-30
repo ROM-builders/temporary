5cd8a234f58078e6a0b90e3cae166c49ee9c050c
Mika Haluku <104668518+mika1zumi@users.noreply.github.com>
2022-07-30 07:23:04 +0700

    X00T-Evolution-X-snow-mika1zumi

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,17 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/mika1zumi/local_manifest.git --depth 1 -b EvoX-Snow .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
-export TZ=Asia/Dhaka #put before last build command
-mka derp
+lunch evolution_X00T-userdebug
+export TARGET_FACE_UNLOCK_SUPPORTED := true
+export WITH_GAPPS := true
+export TARGET_USES_MINI_GAPPS := true
+export TARGET_INCLUDE_LIVE_WALLPAPERS := false
+export TZ=Asia/Tokyo #put before last build command
+mka evolution
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
