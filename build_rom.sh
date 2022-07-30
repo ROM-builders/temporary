4e27204c34ade625f0ffe2daf97a661266b925ef
Progcker <83493179+Progcker@users.noreply.github.com>
2022-07-30 12:21:23 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -8,7 +8,7 @@ source build/envsetup.sh
 lunch potato_CPH1859-userdebug
 export WITH_GMS=true
 export TZ=Asia/Delhi #put before last build command
-brunch CPH1859
+make potato
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
