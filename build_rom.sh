d259d0b0ef7f40187314a1b9a9ec353c1e52ced5
Progcker <83493179+Progcker@users.noreply.github.com>
2022-07-30 08:03:30 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PotatoProject/manifest -b frico_mr1-release -g default,-mips,-darwin,-notdefault
-git clone https://github.com/progcker/local_manifest --depth 1 -b spark .repo/local_manifests
+git clone https://github.com/progcker/local_manifest --depth 1 -b potato .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
