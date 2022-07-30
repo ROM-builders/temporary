3230fb84ba839ba810993648ca6cdf203ead364b
Progcker <83493179+Progcker@users.noreply.github.com>
2022-07-30 07:59:39 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b spark -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/PotatoProject/manifest -b frico_mr1-release -g default,-mips,-darwin,-notdefault
 git clone https://github.com/progcker/local_manifest --depth 1 -b spark .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch spark_CPH1859-userdebug
+lunch potato_CPH1859-userdebug
 export TZ=Asia/Delhi #put before last build command
 mka bacon
 
