03c999967cf91bbad5afac5f88754d96218743f4
Progcker <83493179+Progcker@users.noreply.github.com>
2022-07-30 18:01:32 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,8 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-SELINUX_IGNORE_NEVERALLOWS=true
-export TZ=Asia/Delhi #put before last build command
+export TZ=Asia/Kolkata #put before last build command
 brunch potato_CPH1859-userdebug
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
