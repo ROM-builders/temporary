9d0418edad7844fbdd21edb2d404862bb4683027
Progcker <83493179+Progcker@users.noreply.github.com>
2022-07-30 23:13:05 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,6 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
+
 export TZ=Asia/Delhi #put before last build command
 brunch potato_CPH1859-userdebug
 
