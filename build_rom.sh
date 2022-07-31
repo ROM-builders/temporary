21f7276f3def2a1da3650fb8314ffafe0e959767
Meghthedev <96943033+Meghthedev@users.noreply.github.com>
2022-07-31 12:20:50 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Meghthedev/local_manifest .repo/local_manifests -b Pixys
+repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-12/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Meghthedev/manifest_m30s -b main .repo/local_manifests 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
  #  build rom
 source build/envsetup.sh
-lunch arrow_CPH1859-userdebug
+lunch derp_m307f-userdebug
 export TZ=Asia/Kolkata #put before last build command
-mka bacon
+mka derp
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
