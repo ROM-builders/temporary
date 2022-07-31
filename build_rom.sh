fb9b9d9a59cd3d4f8119c8f9e3111098bce79c9a
HoangLong <106237833+VR-HoangLong@users.noreply.github.com>
2022-07-31 15:33:48 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android -b lineage-18.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/VR-HoangLong/Local-Manifests --depth 1 -b master .repo/local_manifests
+git clone https://github.com/VR-HoangLong/Local-Manifests .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
