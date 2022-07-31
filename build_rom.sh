054e52940244b21fa18d964169e49d8a4559f209
Progcker <83493179+Progcker@users.noreply.github.com>
2022-07-31 18:41:39 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,8 +6,9 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 #build rom
 . build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
+export SELINUX_IGNORE_NEVERALLOWS=true
 lunch blaze_CPH1859-userdebug
-export TZ=Asia/Kolkata
+export TZ=Asia/Dhaka #put before last 
 make bacon
 
 
