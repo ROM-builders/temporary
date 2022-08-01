25983feac0d0d70fe8cd594f95dd9f20a16ccbab
Leinier Cruz Salfran <44554001+leiniercs@users.noreply.github.com>
2022-08-01 01:05:30 -0400

    crDroid-12.1-garden

--- build_rom.sh
+++ build_rom.sh
@@ -5,9 +5,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_garden-user
 export TZ=UTC #put before last build command
-m bacon
+brunch lineage_garden-user
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
