034f3d8631fa3118fa5763c4563c6b5cfa42cf0a
vivekroy <50535325+bugroy111@users.noreply.github.com>
2022-08-01 09:00:28 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,11 +3,11 @@ repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git
 git clone https://github.com/bugroy111/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom - Build for karate - July update
+# build rom - Build for karatep - July update
 source build/envsetup.sh
 #set time zone
 export TZ=Asia/Kolkata #put before last build command
-brunch karate
+brunch karatep
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
