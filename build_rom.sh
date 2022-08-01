1cd014a9fa8e9692cba1096a2ac678c64c99e4d5
Amrito Karmokar <101019800+Amritorock@users.noreply.github.com>
2022-08-02 01:28:09 +0600

    Let's build Ricedroid

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ForkLineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Amritorock/local_manifest.git --depth 1 -b Losfork .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Amritorock/local_manifest.git --depth 1 -b RiceDroid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch lineage_r5x-userdebug
-export WITH_GMS=true
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
