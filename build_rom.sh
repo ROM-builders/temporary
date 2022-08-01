f64742a2e8d4273edffb0bd723046d0351e8a52f
Ansh <singhansh64321@gmail.com>
2022-08-01 20:22:17 +0530

    Build v3.9

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch cherish_santoni-userdebug
+lunch cherish_santoni-user
 export TZ=Asia/Mumbai #put before last build command
 mka bacon
 
