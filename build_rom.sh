412d84b9890e42d8808a887f8cd290e32414520c
HoangLong <106237833+VR-HoangLong@users.noreply.github.com>
2022-08-02 22:52:11 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android -b lineage-19.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/VR-HoangLong/localmanifest --depth 1 -b lineage .repo/local_manifests
+git clone https://github.com/VR-HoangLong/localmanifest --depth 1 -b lineage-19.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_kltekdi-user
+lunch lineage_kltekdi-userdebug
 export TZ=Asia/Ho_Chi_Minh #put before last build command
 make bacon
 
