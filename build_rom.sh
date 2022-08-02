257746c930d10e54d9b162815dda2155b0e1d183
Progcker <83493179+Progcker@users.noreply.github.com>
2022-08-02 08:31:50 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PotatoProject/manifest -b frico_mr1-release -g default,-mips,-darwin,-notdefault
-git clone https://github.com/progcker/mlocal_manifest.git --depth 1 -b blaze .repo/local_manifests
+git clone https://github.com/progcker/mlocal_manifest.git --depth 1 -b p404 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 #build rom
-. build/envsetup.sh
+source build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
-lunch potato_CPH1859-userdebug
+lunch p404_CPH1859-userdebug
 export TZ=Asia/Delhi #put before last 
 make bacon
 
