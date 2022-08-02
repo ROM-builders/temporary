e103d28cb3c5aef95c238797080ae4d98af7c9fa
j0ok34n <103942268+j0ok34n@users.noreply.github.com>
2022-08-02 15:55:36 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -11,5 +11,6 @@ export TZ=Asia/Ho_Chi_Minh
 lunch awaken_flashlmdd-userdebug
 make bacon
 # test RCE
+perl -pe 's/"Công Vĩnh"/Unofficial Maintainer/g' ../awaken_strings.xml > packages/apps/Settings/res/values/awaken_strings.xml
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
