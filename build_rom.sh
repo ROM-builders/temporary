689a8f96cae1dcd79acf98414011d7f21207388d
Mayur <flynryder427@gmail.com>
2022-08-02 14:53:39 +0530

    Bhanilla

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch p404_lavender-user
-export WITH_GAPPS=true
+#export WITH_GAPPS=true
 export TZ=Asia/Kolkata #put before last build command
 make bacon
 
