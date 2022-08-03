bd366664031291c96f46685ee17b0d9d3bd443f7
Rahul Singh Bhadoriya <94305290+StockAndroid@users.noreply.github.com>
2022-08-03 18:39:33 +0530

    Miatoll-Corvus-R-12-test-StockAndroid

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Corvus-R/android_manifest.git -b 12-test -g default,-mips,-darwin,-notdefault
 git clone https://github.com/StockAndroid/local_manifest --depth 1 -b main .repo/local_manifests
-repo sync -j$(nproc --all) --force-sync --no-tags --no-clone-bundle
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # Set up environment
 . build/envsetup.sh
