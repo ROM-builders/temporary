3e40d6896d289b07fdc6a8d8c49b2890767cec00
NFSDev™ <95971776+NFS86@users.noreply.github.com>
2022-08-03 19:16:59 +0700

    Test Kernel

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/NFS-projects/local_manifest --depth 1 -b 12.1 .repo/local_manifests
+git clone https://github.com/NFS-project/local_manifest --depth 1 -b 12.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -14,4 +14,3 @@ brunch lineage_rosy-userdebug
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-
