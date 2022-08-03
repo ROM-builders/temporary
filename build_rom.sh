063db9bb3615e872ef331c2b445bee4474ca02c1
Dragon-1237 <89452912+Dragon-1237@users.noreply.github.com>
2022-08-03 20:09:57 +0530

    making XD

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom 
 source build/envsetup.sh
-lunch dot_xdroid-userdebug
+lunch xdroid_veux-userdebug
 export WITH_GAPPS=true
 export TZ=Asia/Dhaka #put before last build command
 make xd
