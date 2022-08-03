2677151419e1767d95778cb76a93702fe711c99f
Dooms-v <109235336+Dooms-v@users.noreply.github.com>
2022-08-03 22:14:00 +0530

    beryllium-Bootleggers-BrokenLab-sambunimbo-Dooms

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Havoc-OS/android_manifest.git -b eleven -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Dooms-v/local-manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Bootleggers-BrokenLab/manifest.git -b sambunimbo -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Dooms-v/local-manifest.git --depth 1 -b 12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch havoc_beryllium-user
+lunch bootleg_beryllium-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
