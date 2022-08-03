b696f9230d45a3989f52237233f8f4daf6752216
Meghthedev <96943033+Meghthedev@users.noreply.github.com>
2022-08-03 22:50:21 +0530

    A37!

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Meghthedev/manifest_m30s -b main .repo/local_manifests 
+repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 11.0 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Meghthedev/manifest_A37 -b main .repo/local_manifests 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
  #  build rom
 source build/envsetup.sh
+lunch lineage_A37-userdebug
 export TZ=Asia/Kolkata #put before last build command
-brunch m307f
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
