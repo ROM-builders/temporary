8c56b8db30bf6f15a9885b61ea2301d7e7bfd636
Shahid Parvez <shahidparvez.official@gmail.com>
2022-08-04 21:59:03 +0600

    rosemary-PixelExperience-mrsnailo

--- build_rom.sh
+++ build_rom.sh
@@ -5,8 +5,11 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
+
 lunch aosp_rosemary-userdebug
+
 export TZ=Asia/Dhaka #put before last build command
+
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
