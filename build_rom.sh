ada23eb0e6bbd2df4d55e32b2360f5ac71f40243
Kneba <abenkenary3@gmail.com>
2022-08-04 11:05:15 +0700

    Autis sync

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch potato_X00TD-userdebug
+breakfast X00TD
 export TZ=Asia/Jakarta #put before last build command
 brunch X00TD
 
