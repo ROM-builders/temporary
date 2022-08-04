9a68efda25a4b719dbb21fc1a4210d4bf196e5a4
romgharti <romgharti@gmail.com>
2022-08-05 04:44:26 +0545

    Again fixing

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch kscope_mojito-userdebug
+export ALLOW_MISSING_DEPENDENCIES=true
+export SELINUX_IGNORE_NEVERALLOWS=true
 # export WITH_GAPPS=true
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
