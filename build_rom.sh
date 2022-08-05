15f3750c59b9f9b3f8333e71c8e9445e371d9c98
Shahid Parvez <shahidparvez.official@gmail.com>
2022-08-05 20:07:04 +0600

    rosemary-PixelExperience-eleven-mrsnailo

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/PixelExperience/manifest.git -b eleven -g default,-mips,-darwin,-notdefault
-https://github.com/mrsnailo/local_manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/mrsnailo/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
