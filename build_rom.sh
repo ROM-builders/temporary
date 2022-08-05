842436bf1427f20fb457d97230c9b6de17789c66
qweewqi <102307285+qweewqi@users.noreply.github.com>
2022-08-05 09:24:07 +0300

    Fix6

--- build_rom.sh
+++ build_rom.sh
@@ -9,9 +9,9 @@ source build/envsetup.sh
 apply-waydroid-patches
 lunch lineage_waydroid_arm64-userdebug
 export TZ=Asia/Dhaka #put before last build command
-# build/envsetup.sh this before build!
-make systemimage --jobs 8
-make vendorimage --jobs 8
+
+make systemimage
+make vendorimage
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
