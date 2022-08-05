1472ff38ce06a8291dc9835110ef59f0627cd238
T.B.S <ewprjkt@proton.me>
2022-08-05 22:38:38 +0700

    build: selinux ignore

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch lineage_X00TD-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Makassar #put before last build command
 brunch X00TD
 
