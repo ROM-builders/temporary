010fcd10d78794a9a4f5b46fd68fd87362dec267
Shahid Parvez <shahidparvez.official@gmail.com>
2022-08-05 19:46:24 +0600

    rosemary-PixelExperience-eleven-mrsnailo

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://PixelExperience/manifest.git -b eleven -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/PixelExperience/manifest.git -b eleven -g default,-mips,-darwin,-notdefault
 https://github.com/mrsnailo/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-
 lunch aosp_rosemary-userdebug
-
 export TZ=Asia/Dhaka #put before last build command
-
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
