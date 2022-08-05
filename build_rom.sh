6bd7c377ebb0531fbf687d2e3975fc00f5cdc429
Amrito Karmokar <101019800+Amritorock@users.noreply.github.com>
2022-08-05 19:30:10 +0600

    Let's build kaleidoscope

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch kscope_r5x-userdebug
-# export WITH_GAPPS=true
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
