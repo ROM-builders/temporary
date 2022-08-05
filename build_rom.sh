3455c6f60ff2c3cb79aa520ef72a662c719b786a
qweewqi <102307285+qweewqi@users.noreply.github.com>
2022-08-05 08:56:53 +0300

    Fix3

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git@github.com:LineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault ## copied from /Havoc-OS
+repo init --depth=1 --no-repo-verify -u git@github.com:LineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault ## copied from / /Havoc-OS
 wget -O - https://raw.githubusercontent.com/waydroid/android_vendor_waydroid/lineage-18.1/manifest_scripts/generate-manifest.sh | bash
 echo '<?xml version="1.0" encoding="UTF-8"?><manifest><!-- Remove replaced Projects --><remove-project name="platform/external/libdrm" /><remove-project name="platform/external/mesa3d" /><!--  <remove-project name="platform/hardware/intel/common/libva" />--><remove-project name="LineageOS/android_packages_apps_SetupWizard" /><remove-project name="LineageOS/android_external_chromium-webview_patches" /><remove-project name="LineageOS/android_external_chromium-webview_prebuilt_arm"/><remove-project name="LineageOS/android_external_chromium-webview_prebuilt_arm64"/><remove-project name="LineageOS/android_external_chromium-webview_prebuilt_x86"/></manifest>' > .repo/local_manifests/01-removes.xml
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
