9736f5906c3c7580d543ad95c00dd146ae8f2587
Progcker <83493179+Progcker@users.noreply.github.com>
2022-08-06 05:34:14 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,13 +5,12 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 #build rom
 . build/envsetup.sh
-#export WITH_GAPPS=true
+export WITH_GAPPS=true
 #export ALLOW_MISSING_DEPENDENCIES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 lunch blaze_CPH1859-userdebug
 export TZ=Asia/Kolkata #put before last 
 make bacon
-
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
