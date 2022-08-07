d3d6c5092f4c61f3df8df61cfd640f49b69cf75b
LittleChest <81231195+LittleChest@users.noreply.github.com>
2022-08-07 10:11:04 +0800

    upload (copy)

--- build_rom.sh
+++ build_rom.sh
@@ -7,9 +7,9 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch aosp_vangogh-user
 export TZ=Asia/Shanghai
-export BUILD_USERNAME=LittleChest
-export BUILD_HOST=Cirrus-CI
 mka bacon
 
-# upload rom
+# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+rclone copy out/target/product/vangogh/*.zip.json cirrus:vangogh -P
 rclone copy out/target/product/vangogh/*.zip cirrus:vangogh -P
