65f7eed1c7a2c5bb921c4f828d0e666cd205e97a
Onkar singh <onkarsinghsahil@gmail.com>
2022-08-07 07:30:26 +0530

    eng build

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch kscope_mido-user
+lunch kscope_mido-eng
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
