0ad295d614bb132a35f1fc8cadeea4c3b9af1ae4
wHoEMi <ehteshammalik18998@gmail.com>
2022-08-09 02:21:41 +0530

    fix fingerprint wake-up issue

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch arrow_lavender-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
+export ARROW_GAPPS=true
 export TZ=Asia/Kolkata # put before last build command
 m bacon
 
