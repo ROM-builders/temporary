3dfbf29e0406ac31c9aa436e9fcc2cefb574fc40
Dooms-v <109235336+Dooms-v@users.noreply.github.com>
2022-08-08 23:54:12 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch evolution_beryllium-user
+lunch evolution_beryllium-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka evolution
 
