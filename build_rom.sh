e111d8235c0818565d7268cd0d91cf8c2be6af8b
ZualoliconVN <94312960+ZualoliconVN@users.noreply.github.com>
2022-08-09 12:59:14 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b twelve-one -g default,-mips,-darwin,-notdefault
-git clone https://github.com/mizdrake7/local_manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/ZualoliconVN/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
