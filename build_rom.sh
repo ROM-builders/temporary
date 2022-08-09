421b8a9c14f13ed186b430b6503e64ac29e38b0d
Crypt045 <110026608+Crypt045@users.noreply.github.com>
2022-08-09 20:29:54 +0800

    r5x-Voltage-Crypt045

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init -u https://github.com/VoltageOS/manifest -b 12l
+git clone https://github.com/Crypt045/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
+lunch voltage-r5x_userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
