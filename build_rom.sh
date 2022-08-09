c9fb6af39613329e10fe4aa675becad3fe627d2c
Orkun Ergün <68orkun@proton.me>
2022-08-09 09:52:51 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://gitlab.com/R9Lab/Manifest.git --depth 1 -b ProjectBlaze-12.1 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b twelve-one -g default,-mips,-darwin,-notdefault
+git clone https://gitlab.com/R9Lab/Manifest.git --depth 1 -b CherishOS-12.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch blaze_lava-userdebug
-export WITH_GAPPS=true
+lunch cherish_lava-userdebug
+export CHERISH_VANILLA=true
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
