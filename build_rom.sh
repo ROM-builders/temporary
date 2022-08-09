4a6caf4f906a9de5d9ce4b899a828c1d54e3c7c4
ZualoliconVN <94312960+ZualoliconVN@users.noreply.github.com>
2022-08-09 13:25:19 +0700

    r5x-CherishOS-ZualoliconVN
    Build CherishOS 3.9.5 for r5x

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,22 @@
-# sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b twelve-one -g default,-mips,-darwin,-notdefault
+git clone https://github.com/ZualoliconVN/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
-export TZ=Asia/Dhaka #put before last build command
-mka derp
+lunch cherish_r5x-userdebug
+export CHERISH_VANILLA=true
+export SELINUX_IGNORE_NEVERALLOWS=true
+export KBUILD_BUILD_USER=ZualoliconVN
+export KBUILD_BUILD_HOST=ZualoliconVN
+export BUILD_USERNAME=ZualoliconVN
+export BUILD_HOSTNAME=ZualoliconVN
+export RELAX_USES_LIBRARY_CHECK=true
+export SKIP_ABI_CHECKS=true
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+export USE_DEXOPT=true
+export TZ=Asia/Kolkata #put before last build command
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
