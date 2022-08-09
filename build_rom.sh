82292e34acab130676ccdf04fcca97d93db33949
Crypt045 <110026608+Crypt045@users.noreply.github.com>
2022-08-09 19:07:23 +0800

    r5x-Voltage-Crypt045

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init -u https://github.com/Corvus-R/android_manifest.git -b 12-test
+repo init -u https://github.com/VoltageOS/manifest -b 12l
 git clone https://github.com/Crypt045/local_manifest/tree/main --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch corvus-r5x_userdebug
+lunch voltage-r5x_userdebug
 export TZ=Asia/Dhaka #put before last build command
-mka corvus
+mka voltage
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
