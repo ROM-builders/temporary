9aabf4c876f1a4a4c3fbcd60b1a5d31a3e9e3dbf
Ashish Jaswal <98325176+JaswalAshish48@users.noreply.github.com>
2022-08-09 05:50:48 +0530

    Some more fixes

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest.git -b snow -g default,-mips,-darwin,-notdefault
-git clone https://github.com/JaswalAshish/local_manifest.git --depth 1 -b ashish .repo/local_manifests
+git clone https://github.com/JaswalAshish48/local_manifest.git --depth 1 -b snow .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
