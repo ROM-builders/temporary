7126980a1f7124104ec22cb56317fbab4fb461fa
Progcker <83493179+Progcker@users.noreply.github.com>
2022-08-09 22:03:38 +0530

    last rom

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 #build rom
 . build/envsetup.sh
 export WITH_GAPPS=true
-#export ALLOW_MISSING_DEPENDENCIES=true
+export ALLOW_MISSING_DEPENDENCIES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 lunch blaze_CPH1859-userdebug
 export TZ=Asia/Kolkata #put before last 
