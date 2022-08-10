093f4105c248f0993dc5d563bba09e7c50486aef
Om Patel <89215836+takumi021@users.noreply.github.com>
2022-08-11 01:52:57 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/takumi021/local_manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/takumi021/local_manifest.git --depth 1 -b bear .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
 export TZ=Asia/Delhi #put before last build command
-lunch arrow_r5x-userdebug
+lunch arrow_bear-userdebug
 m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
