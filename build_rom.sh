3a0f0a34aca27637bfc11e37152ad63886c28a76
Don boruza <109259258+donboruza@users.noreply.github.com>
2022-08-10 14:28:16 +0700

    build: cleanup tree

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b 
 git clone https://github.com/donboruza/local_manifests.git --depth 1 -b rice .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build roms
+# build rom
 . build/envsetup.sh
 lunch lineage_begonia-userdebug
 export BUILD_USERNAME=don.boruza
