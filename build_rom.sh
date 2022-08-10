e1e947f0cf1a478bd8376b5ca8f0536a6f3e823c
ZualoliconVN <94312960+ZualoliconVN@users.noreply.github.com>
2022-08-10 07:53:49 +0700

    r5x-CherishOS-twelve-one-ZualoliconVN

--- build_rom.sh
+++ build_rom.sh
@@ -2,6 +2,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/ZualoliconVN/android_
 git clone https://github.com/ZualoliconVN/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+
 # build rom
 source build/envsetup.sh
 lunch cherish_r5x-userdebug
