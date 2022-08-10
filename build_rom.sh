c90659b1d7ff8fac84c82dae94cba457ba3b782d
iDrinkCoffee <80449624+huhugh221@users.noreply.github.com>
2022-08-10 22:14:52 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch fluid_merlin-userdebug
+lunch fluid_merlinx-userdebug
 export TZ=Europe/Bucharest #put before last build command
 mka bacon
 
