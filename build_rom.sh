5b3bd1ec37e19feee8639dd280b9557ab30c5a4f
Back Space <dikiawan9@gmail.com>
2022-08-12 02:03:53 +0700

    Conquer Is Here !!

---
+++ build_rom.sh
@@ -0,0 +1,15 @@
+# sync rom
+repo init --depth=1 -u git://github.com/conquerOS/manifest.git -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/dikiawan-9/manifest.git --depth 1 -b conquer .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
+# build rom
+source build/envsetup.sh
+lunch conquer_lavender-userdebug
+export TZ=Asia/Jakarta
+export BUILD_USERNAME=dikiawan-9
+export SELINUX_IGNORE_NEVERALLOWS=true
+make carthage
+
+# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
