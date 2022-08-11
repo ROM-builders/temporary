9c15181e45699d48b093be5381f3777538a86ee6
Aditya Serasha Wahyudi <110929734+advandigital@users.noreply.github.com>
2022-08-11 13:00:21 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 . build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
-export TZ=Asia/Makassar #put before last build command
+export TZ=Asia/Jakarta #put before last build command
 brunch RMX1801
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
