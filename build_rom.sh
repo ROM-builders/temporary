f396d7881ae7e8e738b9fe38197e1e6a835849fa
Firman Ari <106390999+GCapsper@users.noreply.github.com>
2022-08-11 23:46:17 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,14 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ImPrashantt/local_manifest.git --depth 1 -b crdroid .repo/local_manifests
+git clone https://github.com/GCapsper/Local-Manifest.git --depth 1 -b Build .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-. build/envsetup.sh
-export WITH_GMS=true
-export TZ=Asia/Dhaka #put before last build command
-brunch lavender
+source build/envsetup.sh
+export BUILD_USERNAME=dixiese
+export SELINUX_IGNORE_NEVERALLOWS=true
+export TZ=Asia/Jakarta #put before last build command
+brunch whyred
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-
-# 4
