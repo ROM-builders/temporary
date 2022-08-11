cb9cafdda5e20b7611b0cd5bd9aa64ad7f1b3b31
pingmado <75196272+ping2109@users.noreply.github.com>
2022-08-11 11:52:13 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,16 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/CherishOS/android_manifest.git -b twelve-one -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ping2109/local_manifest.git --depth 1 -b cherish-a50 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/Evolution-X/manifest.git -b snow -g default,-mips,-darwin,-notdefault
+git clone https://github.com/ping2109/local_manifest.git --depth 1 -b evox-a50 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
+rm -rf hardware/samsung
+git clone https://github.com/Galaxya50/android_hardware_samsung -b android-12.0 ./hardware/samsung
 . build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Ho_Chi_Minh #put before last build command
-brunch a505f
+lunch evolution_a505f-userdebug
+mka evolution
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
