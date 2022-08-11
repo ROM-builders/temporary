c537366841a36ff2e2b03f8ee65c457b607d25ac
Back Space <dikiawan9@gmail.com>
2022-08-12 02:45:41 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 -u git://github.com/conquerOS/manifest.git -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/dikiawan-9/manifest.git --depth 1 -b conquer .repo/local_manifest
+git clone https://github.com/dikiawan-9/manifest.git --depth 1 -b conquer .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
