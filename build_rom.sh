379925d4eda8ff61b013129782c2303ab683d12b
Prashant <93838092+Prashant-1695@users.noreply.github.com>
2022-08-11 21:27:40 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch fusion_lavender-user
+lunch fuse_lavender-user
 export TZ=Asia/Dhaka #put before last build command
 . fuse*sh
 
