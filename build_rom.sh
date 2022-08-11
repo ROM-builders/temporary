37e92c7f3ca617b4c01c792ea58179b7a4ae0f3d
Back Space <dikiawan9@gmail.com>
2022-08-12 03:12:17 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -9,7 +9,7 @@ lunch conquer_lavender-userdebug
 export TZ=Asia/Jakarta
 export BUILD_USERNAME=dikiawan-9
 export SELINUX_IGNORE_NEVERALLOWS=true
-make carthage j8
+branch carthage j8
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
