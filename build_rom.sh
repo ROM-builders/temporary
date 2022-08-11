de44772e34a919478d8b7f7edfccf2f9997f5114
dtyghc <100001963+dtyghc@users.noreply.github.com>
2022-08-11 20:31:33 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/DotOS/manifest.git -b dot12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/dtyghc/local_manifest.git --depth 1 -b dotos .repo/local_manifests
+git clone https://github.com/dtyghc/local_manifest.git --depth=1 -b dotos .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom 
+# build rom
 source build/envsetup.sh
 lunch dot_olives-userdebug
 export WITH_GAPPS=true
