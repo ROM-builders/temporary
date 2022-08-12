8a4db8e86440a93de56bad19a20f11406a9044a5
Sayan Sarkar <97832667+SKORPION29@users.noreply.github.com>
2022-08-12 21:06:34 +0530

    Build vanilla

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom 
 source build/envsetup.sh
-export WITH_GAPPS=true
+#export WITH_GAPPS=true
 export TZ=Asia/Kolkata #put before last build command
 brunch blaze_miatoll-userdebug
 
