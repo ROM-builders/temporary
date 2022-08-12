bc103e3ec796cbc05778109c8a6cfdaa18581b99
Pratyay Borborah <102370202+popoA3M@users.noreply.github.com>
2022-08-12 13:02:35 +0530

    Build with gapps

--- build_rom.sh
+++ build_rom.sh
@@ -7,6 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch spark_miatoll-userdebug
 export TZ=Asia/Kolkata #put before last build command
+export WITH_GAPPS=true
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
