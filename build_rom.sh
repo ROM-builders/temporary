148000b4f55f663138379af7478b53a3cba31365
swapnil vicky <74442290+SwapnilVicky@users.noreply.github.com>
2022-08-13 16:26:56 +0530

    go specific grameshwaram

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/SwapnilVicky/local_manifest.git --depth 1 -b 12L .repo/local_manifests
+git clone https://github.com/SwapnilVicky/local_manifest.git --depth 1 -b gram .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom#
 source build/envsetup.sh
-lunch lineage_miatoll-userdebug
+lunch lineage_gram-userdebug
 export TZ=Asia/Kolkata #put before last build command
 make bacon
 
