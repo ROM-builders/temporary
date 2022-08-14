21dc4b72680aebee28cfe495befa3c8b49041270
Progcker <83493179+Progcker@users.noreply.github.com>
2022-08-14 19:14:29 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/progcker/mlocal_manifest.git --depth 1 -b pixelos .
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 #build rom
-source build/envsetup.sh
+. build/envsetup.sh
 lunch aosp_CPH1859-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
