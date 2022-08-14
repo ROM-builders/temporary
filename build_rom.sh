9fb7fed0baa074ffc1ad76d1ca6469309524b1fa
strongreasons <109900777+strongreasons@users.noreply.github.com>
2022-08-15 01:52:10 +0700

    X00TD-AICP-s12.1-strongreasons

--- build_rom.sh
+++ build_rom.sh
@@ -6,9 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch aicp_X00TD-userdebug
-export BUILD_BROKEN_DUP_RULES=true
 export TZ=Asia/Makassar #put before last build command
-time brunch X00TD
+brunch X00TD
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
