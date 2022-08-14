a4d69d8f339bacde2f1690c75568fb9d223d0925
strongreasons <109900777+strongreasons@users.noreply.github.com>
2022-08-15 01:49:09 +0700

    X00TD-AICP-s12.1-strongreasons

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ProtonAOSP/android_manifest -b sc -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/AICP/platform_manifest.git -b s12.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/strongreasons/local_manifest.git --depth 1 -b sc .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch proton_X00TD-userdebug
+lunch aicp_X00TD-userdebug
+export BUILD_BROKEN_DUP_RULES=true
 export TZ=Asia/Makassar #put before last build command
-m
+time brunch X00TD
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
