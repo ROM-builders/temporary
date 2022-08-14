556752e7115b014bc1c554324994848ab84e2414
strongreasons <109900777+strongreasons@users.noreply.github.com>
2022-08-15 01:30:02 +0700

    X00TD-ProtonAOSP-sc-strongreasons

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Komodo-OS/manifest -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/strongreasons/local_manifest.git --depth 1 -b komodo .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ProtonAOSP/android_manifest -b sc -g default,-mips,-darwin,-notdefault
+git clone https://github.com/strongreasons/local_manifest.git --depth 1 -b sc .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch komodo_X00TD-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
+lunch proton_X00TD-userdebug
 export TZ=Asia/Makassar #put before last build command
-mka komodo
+m
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
