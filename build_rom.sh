025dc098f950a66c894ce2e0bbf4af4ca3c8098a
Raiden Ishigami <contact.raidenishi69@gmail.com>
2022-08-14 01:04:08 -0300

    #3

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/Havoc-OS/android_manife
 git clone https://github.com/raidenkkj/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build roms
+# build romss
 source build/envsetup.sh
 lunch havoc_A001D-userdebug
 export TZ=America/Sao_Paulo
