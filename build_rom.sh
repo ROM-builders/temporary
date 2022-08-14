5d5f6a8a2c2a7c61322b319cd32ce9bbbafcdd69
Bob <58835837+Apitpr0@users.noreply.github.com>
2022-08-15 07:40:32 +0800

    update for test build of derpfest

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-11.0 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/Apitpr0/local_manifests.git --depth 1 -b eleven-ulova .repo/local_manifests
+repo init -u https://github.com/DerpFest-12/manifest.git -b 12.1 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/Apitpr0/local_manifests.git --depth 1 -b master .repo/local_manifests/derp_mi8937.xml
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch arrow_ulysse-userdebug
+lunch derp_device-debug
 export TZ=Asia/Kuala_Lumpur #put before last build command
-m bacon
+mka derp 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
