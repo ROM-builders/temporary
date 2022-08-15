efdb78cf8f5fdb02bdb0e35fb69269d6bc543dc8
Hakan <hknhzl@gmail.com>
2022-08-15 10:59:14 +0300

    Not with gapps

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch derp_tissot-userdebug
-#export BUILD_WITH_GAPPS=true
 export TZ=Asia/Kathmandu #put before last build command
 mka derp
 
