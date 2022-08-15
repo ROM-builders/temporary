34381e617482be9e723c0c4d4bfbe9bedca4d03d
yudi <111181040+serentod@users.noreply.github.com>
2022-08-15 22:38:26 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,9 +5,9 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-export ALLOW_MISSING_DEPENDENCIES=true
+lunch lineage_RMX1801-userdebug
 export TZ=Asia/Jakarta #put before last build command
-brunch RMX1801
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
