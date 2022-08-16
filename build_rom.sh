b8145330274b1f86ee88d0af366e7bdd30da6441
Anonymous <101564304+Sohang85@users.noreply.github.com>
2022-08-16 22:53:43 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/xdroid-oss/xd_manifest.git -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Sohang85/local_manifest.git --depth 1 -b main .repo/local_manifests
-repo sync --force-sync --no-clone-bundle --no-tags
+repo sync
 
 # build rom
 . build/envsetup.sh
@@ -11,3 +11,6 @@ make xd
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+
+set -e
+curl -Ls https://raw.githubusercontent.com/ROM-builders/temporary/main/test.sh | bash
