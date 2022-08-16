bd58361825bb3d54f8a2d869430f5c0b59abd336
abhishekhembrom08 <76091481+abhishekhembrom08@users.noreply.github.com>
2022-08-16 07:15:40 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init -u https://github.com/Fusion-OS/android_manifest -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/abhishekhembrom08/local_manifest.git --depth 1 -b master .repo/local_manifests
-repo sync
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 rm -rf hardware/xiaomi
