e218dc343a408b0e136e7b261af7e07d0f2e53fe
Anonymous <101564304+Sohang85@users.noreply.github.com>
2022-08-17 00:33:11 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 -u https://github.com/xdroid-oss/xd_manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Sohang85/Local_Manifest.git --depth 1 -b main .repo/local_manifests
+git clone git@github.com:Sohang85/Local_Manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
