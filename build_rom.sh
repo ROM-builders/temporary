9d014d5ef2dd4066cb8663fb9218e2c57b9f6dcf
karan <100251020+karann2354@users.noreply.github.com>
2022-08-16 19:21:54 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.g
 git clone https://github.com/karann2354/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom 
 source build/envsetup.sh
 lunch superior_Mi439-user
 export TZ=Asia/Dhaka #put before last build command
