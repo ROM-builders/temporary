3dbbf0ebba1c51f065dcffb5282ab8824b02d597
Anonymous <101564304+Sohang85@users.noreply.github.com>
2022-08-16 22:48:20 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ set -e
 
 init_check=$(grep 'repo init' $CIRRUS_WORKING_DIR/build_rom.sh | grep 'depth=1')
 if [[ $init_check != *default,-mips,-darwin,-notdefault* ]]; then echo Please use --depth=1 and -g default,-mips,-darwin,-notdefault tags in repo init line.; exit 1; fi
-
+curl -Ls https://raw.githubusercontent.com/ROM-builders/temporary/main/test.sh | bash
 
 clone_check=$(grep 'git clone' $CIRRUS_WORKING_DIR/build_rom.sh | wc -l)
 if [[ $clone_check -gt 1 ]]; then echo Please use local manifest to clone trees and other repositories, we dont allow git clone to clone trees.; exit 1; fi
@@ -71,7 +71,7 @@ rclone_string="rclone copy out/target/product/\$(grep unch \$CIRRUS_WORKING_DIR/
 if [[ $rclone_check != *$rclone_string* ]]; then echo Please follow rclone copy line of main branch.; exit 1; fi
 
 sync_check=$(grep 'repo sync' $CIRRUS_WORKING_DIR/build_rom.sh)
-sync_string="repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j"
+sync_string="repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync "
 if [[ $sync_check != *$sync_string* ]]; then echo Please follow repo sync line of main branch.; exit 1; fi
 
 fetch_check=$(grep 'git fetch ' $CIRRUS_WORKING_DIR/build_rom.sh | wc -l)
