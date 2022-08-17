32fa3ad613c122fd804b4da5debe242be918ae8e
newuserbtw <90239625+newuserbtw@users.noreply.github.com>
2022-08-17 08:53:06 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+set -e
+set -x
+
 # sync rom
 repo init -u https://github.com/CipherOS/android_manifest.git -b twelve-L
 git clone --depth=1 https://github.com/newuserbtw/local_manifest/blob/main/local_manifest.xml -b main .repo/local_manifest
@@ -10,3 +15,4 @@ mka bacon -j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+#sorry guys
