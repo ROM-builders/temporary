b2031f33b287b277da9b0272a79cfa6aa9e4ba43
Raiden Ishigami <contact.raidenishi69@gmail.com>
2022-08-16 23:21:06 -0300

    build_rom.sh: fix all things

--- build_rom.sh
+++ build_rom.sh
@@ -1,18 +1,14 @@
-#!/bin/bash
-
-set -e
-set -x
-
 # sync rom
-repo init -u https://github.com/CipherOS/android_manifest.git -b twelve-L
-git clone --depth=1 https://github.com/newuserbtw/local_manifest/blob/main/local_manifest.xml -b main .repo/local_manifest
-repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
+repo init --depth=1 --no-repo-verify -u https://github.com/CipherOS/android_manifest.git -b twelve-L -g default,-mips,-darwin,-notdefault
+git clone https://github.com/newuserbtw/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
 # build rom
-source build/envsetup.sh
-lunch lineage-Mi439-userdebug
+. build/envsetup.sh
+lunch cipher_Mi439-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
-mka bacon -j$(nproc --all)
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-#sorry guys
