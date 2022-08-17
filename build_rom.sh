ad53de53492f152f6a757dfcd3489137497dfd5a
ben863 <r.budhiono@gmail.com>
2022-08-17 11:31:17 +0700

    initial build
    Change-Id: I3ea031d4e77086c4cd019a66d058597023fa8d32

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Havoc-OS/android_manifest.git -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/cbendot/Local-Manifests.git --depth 1 -b havoc .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/cbendot/Local-Manifests.git --depth 1 -b 19.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch havoc_X00TD-userdebug
+lunch lineage_X00TD-userdebug
 export BUILD_USERNAME=ben863
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Jakarta #put before last build command
@@ -13,5 +13,3 @@ make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-
-# 86
