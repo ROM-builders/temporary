65932bbd743a945d829b6e0b471d584f428c7e71
Anonymous <101564304+Sohang85@users.noreply.github.com>
2022-08-18 00:09:53 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync
 # build rom
 . build/envsetup.sh
 lunch xdroid_whyred-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
 make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
