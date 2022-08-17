d137cc94ddaad22675245b95f3ca87b81bc12f98
Anonymous <101564304+Sohang85@users.noreply.github.com>
2022-08-18 00:22:34 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,10 +6,10 @@ git clone https://github.com/Sohang85/Local_Manifest.git --depth 1 -b main .repo
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync
 
 # build rom
-. build/envsetup.sh
-lunch xdroid_whyred-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
-make xd
+source build/envsetup.sh
+lunch xdroid_whyred-user
+export TZ=Asia/Kolkata #put before last build command ###
+mka xd
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
