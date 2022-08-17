9581d8788be2450c46e3682febbdfa90473cfef3
Nishant <82361039+Ultron12bot@users.noreply.github.com>
2022-08-17 21:38:42 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b elle -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Ultron12bot/local_manifest.git --depth 1 -b Evo-A11 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b snow -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Ultron12bot/local_manifest.git --depth 1 -b EvoX .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
