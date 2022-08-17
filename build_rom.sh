d1e4ae243d1a722a60d7d9cefe17369030a0f767
ben863 <r.budhiono@gmail.com>
2022-08-17 11:38:37 +0700

    fix device codename
    Change-Id: I3563008ed8c797bcb4d132fce857cecf21db928f

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_X00TD-userdebug
+lunch lineage_ginkgo-userdebug
 export BUILD_USERNAME=ben863
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Jakarta #put before last build command
