f1173c18a3b12323078c40f320ab8f61d84db8da
abhishekhembrom08 <76091481+abhishekhembrom08@users.noreply.github.com>
2022-08-18 19:23:32 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/CherishOS/android_manifest.git -b eleven -g default,-mips,-darwin,-notdefault
-git clone https://github.com/abhishekhembrom08/manifest_local.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/abhishekhembrom08/manifest_local.git --depth 1 -b main .repo/local_manifests.xml
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
