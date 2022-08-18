c1c6541fb132fc62c71c1fd078b5537cb1b62aca
Lite <97386083+Lite009@users.noreply.github.com>
2022-08-18 23:35:49 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Project-Fluid/manifest.git -b fluid-12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Lite009/local_manifests.git --depth 1 -b fluid-12 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b twelve-plus -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Lite009/local_manifests.git --depth 1 -b 12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch fluid_vince-userdebug
+lunch aosp_vince-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
-export WITH_GAPPS=true
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
