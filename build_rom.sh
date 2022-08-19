0f9a7db242096fbe450d141ecf4d90970db44fb9
abhishekhembrom08 <76091481+abhishekhembrom08@users.noreply.github.com>
2022-08-19 14:32:45 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,16 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/CarbonROM/android.git -b cr-9.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/abhishekhembrom08/manifest_local.git --depth 1 -b main .repo/local_manifests.xml
+git clone https://github.com/abhishekhembrom08/manifest_local.git --depth 1 -b main .repo/local_manifest
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+. build/envsetup.sh
 lunch carbon_RMX1805-userdebug
+export USE_CCACHE=1 && ccache -M 50G
+export CONFIG_STATE_NOTIFIER=y
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-
-
