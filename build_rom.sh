19b4ce846da403a9648bcb5de225de8cea4a7441
Fr0ztyy43 <82140208+Fr0ztyy43@users.noreply.github.com>
2022-08-19 15:03:46 +0200

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git
 git clone https://github.com/Fr0ztyy43/local_manifests.git --depth 1 -b los18.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build ro
+# build rom
 source build/envsetup.sh
 lunch lineage_begonia-userdebug
 export TZ=Asia/Dhaka #put before last build command
