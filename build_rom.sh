6d8370609e611ec4ce7e47566092a0f14c9d6a9a
elt49 <111217052+elt49@users.noreply.github.com>
2022-08-19 22:56:47 +0530

    losex

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/elt49/local_manifest --depth 1 -b main .repo/local_manifests
+git clone https://github.com/elt49/local_manifest --depth 1 -b losex .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
    
 # build rom
@@ -8,6 +8,6 @@ source build/envsetup.sh
 lunch lineage_vayu-userdebug
 export TZ=Asia/Dhaka #put before last build command
 m bacon 
-     
+      
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
