fc6fbd30bf7691cc4a34c35c5271cb8a0248ddbc
Prashant <93838092+Prashant-1695@users.noreply.github.com>
2022-08-19 09:13:48 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,10 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-12.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-13.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/ImPrashantt/local_manifest.git --depth 1 -b arrow .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-export ARROW_GAPPS=true
 lunch arrow_lavender-userdebug
 export TZ=Asia/Dhaka #put before last build command
 m bacon
