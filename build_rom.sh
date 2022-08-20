6dcd0e3063f3398b0be0a7bc86c4546710617c86
Ankit Savani <70018840+AnkitSavani111@users.noreply.github.com>
2022-08-21 05:17:48 +0530

    Build Evo(Try User build #2)

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch evolution_vince-user
-export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
 make evolution
 
