00e0db1c2c0a25143f813208a3d927974bc8d9ab
Hansraj <106469343+Hansraj76@users.noreply.github.com>
2022-08-20 09:53:29 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,8 +5,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-mka evolution
+lunch evolution_RMX1851-userdebug
 export TZ=Asia/Dhaka #put before last build command
-
+mka evolution
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
