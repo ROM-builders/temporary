c5ecfddca880443358ab4e0dfd07d639982dd98f
abhishekhembrom08 <76091481+abhishekhembrom08@users.noreply.github.com>
2022-08-20 21:01:25 +0530

    Update build_rom.sh
    script

--- build_rom.sh
+++ build_rom.sh
@@ -1,10 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/CarbonROM/android.git -b cr-9.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/abhishekhembrom08/local_manifest.git --depth 1 -b realme .repo/local_manifest
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
+export USE_CCACHE=1
+export ccache -M 50G
 export CONFIG_STATE_NOTIFIER=y
 export SELINUX_IGNORE_NEVERALLOWS=true
 ALLOW_MISSING_DEPENDENCIES=true
