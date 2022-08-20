c8f1077252fb8b5172620614f95f600c560fc5de
Orkun Ergün <68orkun@proton.me>
2022-08-20 14:43:45 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-Pixelish/manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://gitlab.com/R9Lab/Manifest.git --depth 1 -b PixelOS-12.1 .repo/local_manifests
+git clone https://gitlab.com/RN9Lab/Manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_lava-userdebug
+lunch aosp_merlinx-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon 
 
