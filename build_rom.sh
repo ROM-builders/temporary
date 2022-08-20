b946f1d338cc551ba3f5dc983b998c4ada80ada0
Fr0ztyy43 <82140208+Fr0ztyy43@users.noreply.github.com>
2022-08-20 08:10:58 +0200

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,12 +3,12 @@ repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git
 git clone https://github.com/Fr0ztyy43/local_manifests.git --depth 1 -b los18.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build ro
+# build rom
 source build/envsetup.sh
 lunch lineage_begonia-userdebug
 export TZ=Asia/Dhaka #put before last build command
-export BUILD_USERNAME=Fr0ztyy43
-mka bacon
+
+m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
