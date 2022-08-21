93d401efc1adafd322d9853083a18d55038e66b3
Sensei205 <71149028+Sensei205@users.noreply.github.com>
2022-08-21 21:54:53 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,10 +4,10 @@ git clone https://github.com/Sensei205/local_manifest --depth 1 -b cr-11 .repo/l
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-. build/envsetup.sh
-brunch lineage_onclite-userdebug
+source build/envsetup.sh
+lunch lineage_onclite-userdebug
 export TZ=Asia/Dhaka #put before last build command
-m bacon
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
