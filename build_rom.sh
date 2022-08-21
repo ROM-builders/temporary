f04b247687c7abdd95e492438b9958970a255a5f
Prashant <93838092+Prashant-1695@users.noreply.github.com>
2022-08-21 08:27:38 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch arrow_lavender-userdebug
+export ARROW_GAPPS=true
 export TZ=Asia/Dhaka #put before last build command
 m bacon
 
