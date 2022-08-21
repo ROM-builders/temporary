ecdafd2ea8d5c095fbf81ba31f2fb0a4f2259d5a
Progcker <83493179+Progcker@users.noreply.github.com>
2022-08-22 01:01:26 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-Pixelish/manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Progcker/mlocal_manifest --depth 1 -b pix .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ForkLineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Progcker/mlocal_manifest --depth 1 -b derp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-. build/envsetup.sh
-export SELINUX_IGNORE_NEVERALLOWS=true
-lunch aosp_CPH1859-userdebug
-export TZ=Asia/Kolkata #put before last build command
-mka bacon
+source build/envsetup.sh
+lunch lineage_CPH1859-userdebug
+export TZ=Asia/Kolkata #put before last build 
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
