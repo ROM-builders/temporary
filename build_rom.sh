c63104ebded627bb7c56ee7d768b2da5ac288a37
Sensei205 <71149028+Sensei205@users.noreply.github.com>
2022-08-21 21:53:04 +0300

    onclite-crdroidandroid-11.0-Sensei205

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 11.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Sensei205/local_manifest.git --depth 1 -b cr-11 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android -b 11.0 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Sensei205/local_manifest --depth 1 -b cr-11 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
-lunch lineage_garden-userdebug
-export TZ=Asia/Jakarta #put before last build command
+. build/envsetup.sh
+brunch lineage_onclite-userdebug
+export TZ=Asia/Dhaka #put before last build command
 m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
