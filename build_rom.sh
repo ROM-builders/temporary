8a3e056e9ee7efe64553da90c2454ce62c495db4
Sensei205 <71149028+Sensei205@users.noreply.github.com>
2022-08-21 21:42:02 +0300

    onclite-crdroidandroid-11.0-Sensei205
    Test

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 11.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Sensei205/local_manifest.git --depth=1 -b cr-11 .repo/local_manifests
+git clone https://github.com/Sensei205/local_manifest.git --depth 1 -b cr-11 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_onclite-userdebug
-export TZ=Asia/Dhaka #put before last build command
-make bacon
+lunch lineage_garden-userdebug
+export TZ=Asia/Jakarta #put before last build command
+m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
