e67d05710a96d09022b46a246c296cbcebf1f799
susandahal <susandahal53@gmail.com>
2022-08-22 21:28:34 +0545

    Fix mistake

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-oss/xd_manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone  --depth 1 https://github.com/susandahal/local_manifest -b xdroid .repo/local_manifests
+git clone https://github.com/susandahal/local_manifest --depth 1 -b xdroid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
