7669d9b9f9d98a6a33df283c201727c059baccad
Fr0ztyy43 <82140208+Fr0ztyy43@users.noreply.github.com>
2022-08-22 14:38:46 +0200

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/ArrowOS/android_manifest.git -b arrow-12.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Fr0ztyy43/local_manifest/blob/main/local_manifest --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/BlissRoms/platform_manifest.git -b arcadia-next -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Fr0ztyy43/local_manifests.git --depth 1 -b bliss .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch arrow_begonia-userdebug
+lunch bliss_begonia-userdebug
 export TZ=Asia/Dhaka #put before last build command
-m bacon
+blissify -v
+
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
