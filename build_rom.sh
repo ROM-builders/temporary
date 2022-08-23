72a622742abdaf55bd5fb4806b7a01d53d13919c
strongreasons <109900777+strongreasons@users.noreply.github.com>
2022-08-24 02:43:17 +0700

    X00TD-LineageOS-lineage-17.1-strongrasons

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/AICP/platform_manifest.git -b s12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/strongreasons/local_manifest.git --depth 1 -b sc .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-17.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/strongreasons/local_manifest.git --depth 1 -b q .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aicp_X00TD-userdebug
+lunch lineage_X00TD-userdebug
 export TZ=Asia/Makassar #put before last build command
 mka bacon
 
