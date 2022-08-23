039c44093cd6416651ff300770080632a99dbbb1
Sri19944 <89214859+Sri19944@users.noreply.github.com>
2022-08-23 22:35:00 +0530

    Mi439-DerpFest-12-12.1-Sri19944

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch derp_Mi439-userdebug
-export TZ=Asia/Dhaka #put before last build command
+export TZ=Asia/Delhi #put before last build command
 mka derp
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
