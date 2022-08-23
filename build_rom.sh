7487a419413f6ec02852145a79035350acd0188e
kiritami <111682109+kiritami@users.noreply.github.com>
2022-08-23 14:56:52 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,9 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/kiritami/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+git clone https://github.com/kiritami/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo init -m manifest.xml
+repo sync
 
 # build rom
 . build/envsetup.sh
