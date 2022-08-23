ed1195a239fa5b7e7833772b8210cfae104f219e
Sri19944 <89214859+Sri19944@users.noreply.github.com>
2022-08-23 16:36:51 +0530

    Mi439-DerpFest-12-12.1-Sri19944

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch derp_Mi439-user
+lunch derp_Mi439-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
