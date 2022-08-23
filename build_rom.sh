c5d0c423f351e36fd9dbf2435122e478f586b665
Sensei205 <71149028+Sensei205@users.noreply.github.com>
2022-08-23 03:35:50 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,5 @@
 # sync rom
+sudo apt-get install libssl-dev
 repo init --depth=1 --no-repo-verify -u https://github.com/ResurrectionRemix/platform_manifest.git -b Q -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Sensei205/local_manifest --depth 1 -b rr-10 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
