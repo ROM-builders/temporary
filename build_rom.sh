8868e9967dafe3c1fdd39bb83d69881e0f588752
Lite <97386083+Lite009@users.noreply.github.com>
2022-08-23 16:49:45 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b twelve-plus -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Lite009/local_manifests.git --depth 1 -b 12 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Havoc-OS/android_manifest.git -b eleven -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Lite009/local_manifests.git --depth 1 -b 11 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch aosp_vince-userdebug
+lunch havoc_vince-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
+export WITH_GAPPS=true
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
