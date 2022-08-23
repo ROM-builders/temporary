f46e127a6c74a22a82c4d7025dfb7027170e4e74
Orkun Ergün <68orkun@proton.me>
2022-08-23 10:01:53 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,10 +1,10 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-Pixelish/manifest -b twelve -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExtended/manifest -b snow -g default,-mips,-darwin,-notdefault
 git clone https://gitlab.com/R9Lab/Manifest.git --depth 1 -b PEX-12.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+. build/envsetup.sh
 lunch aosp_lava-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
