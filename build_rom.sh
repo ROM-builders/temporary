949363bf91abdf2a524806f6f6ad30706ec0d229
Darknius Lighntess <darknius21@gmail.com>
2022-08-24 12:19:14 -0700

    juice-AICP-12.1-darknius20

--- build_rom.sh
+++ build_rom.sh
@@ -1,10 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/darknius20/local_manifest --depth 1 -b aicp12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-# DT/VT/KT/TC
-git clone --depth=1 https://github.com/darknius20/device_xiaomi_juice --single-branch -b aicp12.1 device/xiaomi/juice
-git clone --depth=1 https://github.com/darknius20/android_vendor_xiaomi_juice --single-branch -b 11.0 vendor/xiaomi/juice
-git clone --depth=1 https://github.com/darknius20/kernel_poco_citrus --single-branch -b main kernel/xiaomi/juice
 
 # build rom
 source build/envsetup.sh
@@ -13,6 +10,8 @@ export ALLOW_MISSING_DEPENDENCIES=true
 export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
 export BUILD_BROKEN_DUP_RULES=true
 export TZ=Asia/Jakarta #put before last build command
+export BUILD_USERNAME="darknius"
+export BUILD_HOSTNAME="darx-labs"
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
