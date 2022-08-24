11c9dbe4488f357908a7b971f164209ae93e628b
Arjun Gowda <arjuntsgowda@gmail.com>
2022-08-24 06:35:17 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_beryllium-userdebug
+lunch lineage_beryllium-user
 export TZ=Asia/Delhi #put before last build command
 make bacon
 
