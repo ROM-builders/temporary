0a733e99d016428930293f3b728af0eb2ff961c3
newuserbtw <90239625+newuserbtw@users.noreply.github.com>
2022-08-24 14:43:12 +0700

    e

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,14 @@
 
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/CipherOS/android_manifest.git -b twelve-L -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/yaap/manifest.git -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/newuserbtw/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
 # build rom
 . build/envsetup.sh
-lunch cipher_Mi439-userdebug
+lunch yapp_Mi439-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
-make bacon
+m yapp
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
