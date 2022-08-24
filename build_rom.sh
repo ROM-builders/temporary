86b83f8ad3ee37a672f151cbc3cba629ab8e11e3
crazyads69 <111816472+crazyads69@users.noreply.github.com>
2022-08-24 15:43:27 +0700

    a10s-LineageOS-lineage-17.1-crazyads69
    Build LineageOS for a10s

--- build_rom.sh
+++ build_rom.sh
@@ -5,11 +5,12 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
+make clean && make clobber
 lunch lineage_a10s-userdebug
 export TZ=Asia/Ho_Chi_Minh #put before last build command
 export WITH_SU=false
 export WITH_GMS=false
-mka bacon
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
