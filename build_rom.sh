6f74d67f73b4186b500b177be2a5d4d775578bdb
crazyads69 <111816472+crazyads69@users.noreply.github.com>
2022-08-24 15:47:46 +0700

    a10s-LineageOS-lineage-17.1-crazyads69
    Build LineageOS for a10s

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-make clobber
 lunch lineage_a10s-userdebug
 export TZ=Asia/Ho_Chi_Minh #put before last build command
 export WITH_SU=false
