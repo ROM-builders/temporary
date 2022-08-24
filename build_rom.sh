a9e66f9f4b7ee204ca6ed9599648d693025c6f68
vytautas <vytautas.duchas@gmail.com>
2022-08-25 00:01:33 +0300

    GApps build

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch cherish_vayu-userdebug
-export USE_LAWNCHAIR =true
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true 
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
