be3adbd6b9a8ec50ed1e71051c60a982299555e3
crazyads69 <111816472+crazyads69@users.noreply.github.com>
2022-08-25 15:05:00 +0700

    Fix error

--- build_rom.sh
+++ build_rom.sh
@@ -9,7 +9,7 @@ lunch lineage_a10s-userdebug
 export TZ=Asia/Ho_Chi_Minh #put before last build command
 export WITH_SU=false
 export WITH_GMS=false
-make bacon
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
