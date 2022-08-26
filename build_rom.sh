d2cb55cd7b68fa479520fc37e72d1f39640325f2
ShahzebQureshi <55016172+ShahzebQureshi@users.noreply.github.com>
2022-08-26 09:41:20 +0500

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -uhttps://github.com/Project-Fluid/manifest -b fluid-12.1  -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Fusion-OS/android_manifest -b twelve  -g default,-mips,-darwin,-notdefault
 git clone https://github.com/ShahzebQureshi/local_manifest --depth 1 -b fluid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
@@ -11,9 +11,9 @@ export BUILD_USER=ShahzebQureshi
 export BUILD_HOST=cirrus-ci
 export BUILD_USERNAME=ShahzebQureshi
 export BUILD_HOSTNAME=cirrus-ci
-lunch fluid_joan-userdebug
+lunch fuse_joan-userdebug
 export TZ=Asia/Karachi #put before last build command
-mka bacon
+make fuse-prod
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
