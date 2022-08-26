9c0125992ad79c98dd5217c199ad2b0d38528d6e
Yasuda Minori <80877391+s0umyaz@users.noreply.github.com>
2022-08-26 10:24:53 +0530

    begonia-DotOS-dot12.1-s0umyaz

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom
+# sync dotos rom
 repo init --depth=1 --no-repo-verify -u https://github.com/DotOS/manifest.git -b dot12.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/s0umyaz/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
