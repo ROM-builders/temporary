84c4886e369b2f103597c6504c6964a8eac05e45
newuserbtw <90239625+newuserbtw@users.noreply.github.com>
2022-08-26 12:32:07 +0700

    n

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/yaap/manifest.git -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/newuserbtw/local_manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/newuserbtw/local_manifest.git --depth 1 -b main .repo
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 # build rom
 . build/envsetup.sh
