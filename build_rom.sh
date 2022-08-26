23f282498736c7fb63e454e5e2a5aaaac5e61e6e
Fr0ztyy43 <82140208+Fr0ztyy43@users.noreply.github.com>
2022-08-26 13:34:57 +0200

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android -b 
 git clone https://github.com/Fr0ztyy43/local_manifests.git --depth 1 -b los17.1_a3 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build romm
 source build/envsetup.sh
 lunch lineage_a3lte-userdebug
 export TZ=Asia/Dhaka #put before last build command
