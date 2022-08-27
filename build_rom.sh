6d59229aafca6d3f32f9cd549f2a2ea8127264aa
67 <94082908+sweervin@users.noreply.github.com>
2022-08-27 14:56:11 +0300

    Lineage 18.1 test with updated vendor blobs

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,10 @@
-repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest -b arrow-12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/sweervin/local_manifest --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android -b lineage-18.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/sweervin/local_manifest --depth 1 -b 18.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 . build/envsetup.sh
-export ARROW_GAPPS=true
-lunch arrow_laurel_sprout-userdebug
+lunch lineage_laurel_sprout-user
 export TZ=Asia/Dhaka
 m bacon
-
-##FuckAOSP
 
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
