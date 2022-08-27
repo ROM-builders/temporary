5b689c0b3d83431f1be7eef4489f8d26c193519e
manukdroid <111986320+manukdroid@users.noreply.github.com>
2022-08-27 13:04:56 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ResurrectionRemix/platform_manifest -b Q -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/manukdroid/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch rr_RMX1801-userdebug
+lunch lineage_RMX1801-userdebug
 export TZ=Asia/Kolkata #put before last build command
 make bacon
 
