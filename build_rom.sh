03db0cd34525387a793b28d372b7026d867631ad
Orkun Ergün <68orkun@proton.me>
2022-08-27 16:00:58 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b spark -g default,-mips,-darwin,-notdefault
-git clone https://gitlab.com/R9Lab/Manifest.git --depth 1 -b SparkOS-12.1 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Fluid/manifest.git -b fluid-12.1 -g default,-mips,-darwin,-notdefault
+git clone https://gitlab.com/R9Lab/Manifest.git --depth 1 -b Fluid-12.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
-lunch spark_lava-userdebug
-export WITH_GAPPS=true
+. build/envsetup.sh
+lunch fluid_lava-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
