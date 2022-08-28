b84e9a49411942a099718caa659d9ec31bc5d22f
Sumit Mohanty <72486118+S8M1T@users.noreply.github.com>
2022-08-28 17:32:01 +0530

    Another one!

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,7 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/S8M1T/local_manifest.git --depth 1 -b RMX3360 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
+ 
 # build rom
 . build/envsetup.sh
 export KBUILD_BUILD_USER=S8M1T
