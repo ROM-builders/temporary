ee043d126e0922cf994380a91fd19028fe25cad3
Sensei205 <71149028+Sensei205@users.noreply.github.com>
2022-08-28 03:36:09 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ConquerOS/manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Sense205/local_manifest.git --depth=1 -b conquer .repo/local_manifests
+git clone https://github.com/Sensei205/local_manifest.git --depth=1 -b conquer .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
