249ea43b183a7476e899fc820c484eb55a1677eb
67 <94082908+sweervin@users.noreply.github.com>
2022-08-28 18:57:13 +0300

    Import Xiaomi HAL definitions

--- build_rom.sh
+++ build_rom.sh
@@ -5,6 +5,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 . build/envsetup.sh
 lunch lineage_laurel_sprout-user
 export TZ=Asia/Dhaka
-m bacon
+m otapackage
 
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
