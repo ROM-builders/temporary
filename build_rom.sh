9d007f40dbc430124192635ea54707814238bb8a
Christopher Keith Irvine <Mizdrake7@gmail.com>
2022-08-30 01:55:26 +0530

    r5x-SuperiorOS-twelvedotone

--- build_rom.sh
+++ build_rom.sh
@@ -1,10 +1,10 @@
-repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b twelve-one -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.git -b twelvedotone -g default,-mips,-darwin,-notdefault
 git clone https://github.com/mizdrake7/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch cherish_r5x-userdebug
+lunch superior_r5x-userdebug
 export CHERISH_VANILLA=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 export KBUILD_BUILD_USER=MAdMiZ
@@ -16,7 +16,7 @@ export SKIP_ABI_CHECKS=true
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export USE_DEXOPT=true
 export TZ=Asia/Kolkata #put before last build command
-make bacon
+m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
