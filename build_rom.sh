2fd53845e15b3889676638d15dcff112e4b994ff
SKetU <96108085+SKetU-l@users.noreply.github.com>
2022-08-29 12:50:52 +0530

    X00TD-crDroid-crdroid-12.1-SKetU-l

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch crDroid_X00TD-userdebug
+lunch lineage_X00TD-userdebug
 export TZ=Asia/Kolkata #put before last build command
 make bacon
 
