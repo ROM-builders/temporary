92d756a399ee44185542ed5770fa7e03d265b830
Progcker <83493179+Progcker@users.noreply.github.com>
2022-08-29 11:29:47 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,18 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/progcker/mlocal_manifest.git --depth 1 -b blaze .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.git -b twelvedotone -g default,-mips,-darwin,-notdefault
+git clone https://github.com/progcker/mlocal_manifest.git --depth 1 -b sup .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-#build rom
+# build rom
 source build/envsetup.sh
-export WITH_GAPPS=true
+lunch superior_CPH1859-userdebug
+export BUILD_WITH_GAPPS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
-lunch blaze_CPH1859-userdebug
-export TZ=Asia/Kolkata #put before last 
-mka bacon
+export TARGET_SUPPORTS_NEXT_GEN_ASSISTANT=true
+export TARGET_SUPPORTS_GOOGLE_RECORDER=true
+export TZ=Asia/Kolkata #put before last build command
+m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
