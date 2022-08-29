a2f7da7a6051502231b680e7945d6b624a3ba363
Mika Haluku <104668518+mika1zumi@users.noreply.github.com>
2022-08-29 15:20:20 +0700

    X00TD

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-12.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/mika1zumi/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch arrow_X00TD-userdebug
+lunch blaze_X00TD-userdebug
+export WITH_GAPPS := true
+export TARGET_INCLUDE_LIVE_WALLPAPERS := false
 export TZ=Asia/Jakarta #put before last build command
-m bacon
+brunch X00TD
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
