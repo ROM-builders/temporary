8a739cbcff00f456fe2599de96b11be9ead39280
Uday Bindal <72090728+UdayBindal2312@users.noreply.github.com>
2022-08-29 18:14:13 +0530

    miatoll-ricedroid-udaybindal2312

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b twelve -g default,-mips,-darwin,-notdefault
+repo init -u https://github.com/RiceDroid/android -b twelve
 git clone https://github.com/UdayBindal2312/local_manifest.git --depth 1 -b main .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # build rom
 source build/envsetup.sh
