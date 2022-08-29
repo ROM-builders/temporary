f9271d63435563ab0d8f565de1727ef0cd8ea266
Orkun Ergün <68orkun@proton.me>
2022-08-29 10:30:17 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
  . build/envsetup.sh
-lunch lineage_lava-userdebug
+lunch lineage_merlinx-userdebug
 export TARGET_KERNEL_CLANG_VERSION=proton
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
