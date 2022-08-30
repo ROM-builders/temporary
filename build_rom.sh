bc16dc0e8dd0f725d94d9e835b150df81e6567b5
Sumit Mohanty <72486118+S8M1T@users.noreply.github.com>
2022-08-30 16:50:12 +0530

    RMX3031-RiceDroid-twelve-S8M1T

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,16 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/S8M1T/local_manifest.git --depth 1 -b RMX3031 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
-lunch derp_vayu-user
-export TZ=Asia/Dhaka #put before last build command
-mka derp
+. build/envsetup.sh
+export KBUILD_BUILD_USER=S8M1T
+export KBUILD_BUILD_HOST=S8M1T
+export BUILD_USERNAME=S8M1T
+export BUILD_HOSTNAME=S8M1T
+export TZ=Asia/Kolkata #put before last build command
+brunch RMX3031
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
