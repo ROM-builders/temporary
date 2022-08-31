67cd70c5dffd4ade4402b9eccd34d70082eac2bd
Sepehr <111783439+tracefs@users.noreply.github.com>
2022-09-01 01:33:42 +0430

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # #sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/xdroid-oss/xd_manifest -b thirteen
+repo init --depth=1 --no-repo-verify -u git://github.com/xdroid-oss/xd_manifest -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/tracefs/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
