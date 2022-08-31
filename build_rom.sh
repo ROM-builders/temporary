784526e3598b75f9acddf529ef65645272863259
Eyüp DURAK <dkey5@pm.me>
2022-08-31 21:02:00 +0300

    universal7580: add s5neolte

--- build_rom.sh
+++ build_rom.sh
@@ -5,11 +5,11 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-breakfast j7elte
+breakfast s5neolte
 export TZ=Europe/Istanbul #put before last build command
 croot
-brunch j7elte
-zip out/target/product/j7elte/lineage-18.1-20220831-recovery.img.zip out/target/product/j7elte/recovery.img
+brunch s5neolte
+zip out/target/product/s5neolte/lineage-18.1-20220831-recovery.img.zip out/target/product/s5neolte/recovery.img
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
