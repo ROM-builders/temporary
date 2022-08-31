5a9862212ba6cf37a204e14837ee200680e74779
Sepehr <111783439+tracefs@users.noreply.github.com>
2022-09-01 01:35:41 +0430

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # #sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/xdroid-oss/xd_manifest -b thirteen -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-oss/xd_manifest -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/tracefs/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch xd_lava-userdebug
+lunch xdroid_lava-userdebug
 export TZ=Asia/Tehran #put before last build command
 mka xd
 
