bbe32b2332e782352c669fb974e03cb4395ccfee
Eyüp DURAK <dkey5@pm.me>
2022-08-31 20:32:28 +0300

    recovery: fix issue
    Please use local manifest to clone trees and other repositories, we dont allow git clone to clone trees.

--- build_rom.sh
+++ build_rom.sh
@@ -12,6 +12,5 @@ brunch j7elte
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 sha256sum out/target/product/j7elte/*
-git clone https://github.com/exynos7580-dev/lineage_OTA.git
-bash lineage_OTA/scripts/recovery-j7elte.sh
+bash .repo/local_manifests/recovery-j7elte.sh
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
