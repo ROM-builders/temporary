1b3ce58c020dac0b417343a0f066f51c5bf7f214
Eyüp DURAK <dkey5@pm.me>
2022-09-01 13:36:16 +0300

    j7elte: upload recovery image

--- build_rom.sh
+++ build_rom.sh
@@ -9,7 +9,9 @@ breakfast j7elte
 export TZ=Europe/Istanbul #put before last build command
 croot
 brunch j7elte
-zip out/target/product/j7elte/lineage-18.1-20220901-recovery.img.zip out/target/product/j7elte/recovery.img
+mv out/target/product/j7elte/recovery.img  out/target/product/j7elte/lineage-18.1-20220901-recovery.img 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+sha256sum out/target/product/j7elte/*
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*recovery.img cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
