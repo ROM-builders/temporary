f186b4ca620c1ae450a6c7a60556a55f29c2c756
iTZ_UDAY2312 <72090728+UdayBindal2312@users.noreply.github.com>
2022-09-01 06:30:22 +0530

    miatoll-LineageOS-lineage-19.1-udaybindal2312

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/UdayBindal2312/local_manifest.git --depth=1 -b main .repo/local_manifests
+git clone https://github.com/UdayBindal2312/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # building rom
@@ -9,5 +9,5 @@ lunch lineage_miatoll-userdebug
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
-# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+# upload rom
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
