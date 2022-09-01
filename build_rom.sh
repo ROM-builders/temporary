0380726badb999a07793cd3bba5fad13592980d3
Eyüp DURAK <dkey5@pm.me>
2022-09-01 09:23:11 +0300

    s5neolte: delete Updater app

--- build_rom.sh
+++ build_rom.sh
@@ -9,7 +9,7 @@ breakfast s5neolte
 export TZ=Europe/Istanbul #put before last build command
 croot
 brunch s5neolte
-zip out/target/product/s5neolte/lineage-18.1-20220831-recovery.img.zip out/target/product/s5neolte/recovery.img
+zip out/target/product/s5neolte/lineage-18.1-20220901-recovery.img.zip out/target/product/s5neolte/recovery.img
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
