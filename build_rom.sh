df2655fdc5a66bb14702da081c6ccc19e8d946f2
iTZ_UDAY2312 <72090728+UdayBindal2312@users.noreply.github.com>
2022-09-01 16:07:24 +0530

    switching to cr12

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,7 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 12.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/UdayBindal2312/local_manifest.git -b cr-12L --depth 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
-# build rommm
+# build rom
 source build/envsetup.sh
 lunch lineage_miatoll-userdebug
 export TZ=Asia/Kolkata #put before last build command
