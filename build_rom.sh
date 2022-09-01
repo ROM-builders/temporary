20e4ac507fb9c5e8dc39b11469988fac6dc1eb1f
iTZ_UDAY2312 <72090728+UdayBindal2312@users.noreply.github.com>
2022-09-01 06:36:48 +0530

    miatoll-LineageOS-lineage-19.1-udaybindal2312

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/UdayBindal2312/local_manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/UdayBindal2312/local_manifest.git -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # building rom
 source build/envsetup.sh
 lunch lineage_miatoll-userdebug
-export TZ=Asia/Kolkata #put before last build command
-mka bacon
+export TZ=Asia/Kolkata
+make bacon
 
 # upload rom
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
