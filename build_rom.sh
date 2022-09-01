4ccfe524fc537707727600bf058c7449234daf7e
Sensei205 <71149028+Sensei205@users.noreply.github.com>
2022-09-01 23:34:43 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,3 @@
- (14 sloc)  932 Bytes
-
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/BlissRoms/platform_manifest.git -b arcadia-next -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Sensei205/local_manifest.git --depth 1 -b bliss .repo/local_manifests
@@ -8,10 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch bliss_onclite-userdebug
-export ALLOW_MISSING_DEPENDENCIES=true
-export RELAX_USES_LIBRARY_CHECK=true
 export TZ=Asia/Dhaka #put before last build command
-blissify onclite
+blissify -v onclite
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
