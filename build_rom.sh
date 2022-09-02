6ad4c73b2b59d691a5d0831bf7d4882c471039c8
Eyüp DURAK <dkey5@pm.me>
2022-09-02 17:07:23 +0300

    j7elte: crdroid build

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/dkey5/local_manifests.git --depth 1 -b j7elte_lineage-18.1 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 11.0 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/dkey5/local_manifests.git --depth 1 -b crdroid-j7elte .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
-breakfast j7elte
+. build/envsetup.sh
 export TZ=Europe/Istanbul #put before last build command
-croot
 brunch j7elte
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
-zip out/target/product/j7elte/lineage-18.1-20220902-recovery.zip out/target/product/j7elte/recovery.img
-rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/lineage-18.1-20220902-UNOFFICIAL-j7elte.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/lineage-18.1-20220902-recovery.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
