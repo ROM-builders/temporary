4041e41a0c425115fe1054fa269a9421d987300d
iTZ_UDAY2312 <72090728+UdayBindal2312@users.noreply.github.com>
2022-09-02 19:20:56 +0530

    rice build.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/UdayBindal2312/local_manifest.git -b cr12 --depth 1 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/UdayBindal2312/local_manifest.git --depth 1 -b RiceDroid-12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
