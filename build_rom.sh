761bc7fc8f8347b2e726027ea732785e8f14dca6
T.B.S <ewprjkt@proton.me>
2022-09-03 11:41:55 +0700

    make it vanilla

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_X00TD-userdebug
-export WITH_GMS=true
 export TZ=Asia/Shanghai #put before last build command
 make bacon
 
