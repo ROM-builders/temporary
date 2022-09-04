c80eb68ce3955f165204675fb1ba11bb4d83f326
newuserbtw <90239625+newuserbtw@users.noreply.github.com>
2022-09-04 20:45:52 +0700

    s

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,15 @@
 
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-12/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Komodo-OS/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/newuserbtw/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 #please fucking sync 
 # build rom
 . build/envsetup.sh
-lunch derp_Mi439-userdebug
+lunch komodo_Mi439-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
-mka derp
+mka komodo -jX
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
