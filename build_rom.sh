73152f866358303427e66a4c85f822240ab910f6
ItzHecker404 <112565007+ItzHecker404@users.noreply.github.com>
2022-09-04 08:02:19 +0530

    Arcana 12L fix

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/projectarcana-aosp/manifest -b 12.x -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ItzHecker404/local_manifest.git -b main  --depth 1 .repo/local_manifests
+git clone https://github.com/ItzHecker404/local_manifest.git -b Arcana-12L --depth 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom 
