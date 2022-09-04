cf90ecbc7b67021ff8033fa82bc781090dda50ad
Dinesh <dinesh2742004@gmail.com>
2022-09-04 15:05:22 +0530

    santoni-Spark-Rom-spark-DineshMSDian

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-unch spark_santoni-user
+lunch spark_santoni-user
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
