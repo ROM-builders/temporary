a015968daf8aecfdeffa6104a37592e7d6d7afea
Crypt045 <110026608+Crypt045@users.noreply.github.com>
2022-09-04 10:13:59 +0800

    Lineage-r5x-Crypt045

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init -u https://github.com/Evolution-X/manifest -b tiramisu
+repo init -u https://github.com/LineageOS/android.git -b lineage-19.1
 git clone https://github.com/Crypt045/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
 # build rom
 source build/envsetup.sh
-lunch evolution_r5x-user
+lunch lineage-r5x_userdebug
 export TZ=Asia/Dhaka #put before last build command
-mka evolutionx
+mka lineage
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
