d74c317f5edf8356a0117413a7933e2871f6bd8f
minhtricute69 <42957778+minhtricute69@users.noreply.github.com>
2022-09-04 14:44:39 +0700

    a10s-LineageOS-lineage-17.1-minhtricute69
    Build LineageOS for a10s

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-breakfast a10s
+lunch lineage_a10s-userdebug
 export TZ=Asia/Ho_Chi_Minh #put before last build command
 export WITH_SU=false
 export WITH_GMS=false
