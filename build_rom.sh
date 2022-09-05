6f39f7459a4fd2869b07a3f0017814f5886ed20b
Sensei205 <71149028+Sensei205@users.noreply.github.com>
2022-09-05 13:53:03 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,8 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch bootleg_onclite-user
-export SELINUX_IGNORE_NEVERALLOWS := true
-export ALLOW_MISSING_DEPENDENCIES := true
+export ALLOW_MISSING_DEPENDENCIES := true 
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
