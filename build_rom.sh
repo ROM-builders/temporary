bc78c9de1560f26887af5cfb173594e55572f05f
sajidshahriar72543 <sazidshahriar39@gmail.com>
2022-09-05 20:52:27 +0600

    whyred: Arrow13

--- build_rom.sh
+++ build_rom.sh
@@ -1,18 +1,17 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/Fusion-OS/android_manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/sajidshahriar72543/local_manifest.git --depth 1 -b fuse .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/ArrowOS/android_manifest.git -b arrow-13.0 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/sajidshahriar72543/local_manifest.git --depth 1 -b arrow-whyred .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-#17
+#1
 
 # build rom
-source build/envsetup.sh
-lunch fuse_beryllium-userdebug
+. build/envsetup.sh
+lunch arrow_whyred-userdebug
 export TZ=Asia/Dhaka
 export KBUILD_BUILD_USER=PaperBoy
 export BUILD_HOSTNAME=PaperBoy #put before last build command
-make fuse-prod
+m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-
