0f3c9fccfb6c2fa55fffbc4bfb8893362bd54de1
Abhishek Hembrom <76091481+abhishekhembrom08@users.noreply.github.com>
2022-09-05 17:29:06 +0530

    Update build_rom.sh
    stuck dotos

--- build_rom.sh
+++ build_rom.sh
@@ -23,6 +23,3 @@ rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1
 #fck_Alone0316_madarchod_nahi_sudhrega
 #HavocBhiMadarchodNikla
 #LagtaHaiDotOSviMadarChodNiklega
-
-
-
