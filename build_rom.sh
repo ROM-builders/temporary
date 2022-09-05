33767aaee02e9f8a2c8433588d2821f028457008
Abhishek Hembrom <76091481+abhishekhembrom08@users.noreply.github.com>
2022-09-05 09:48:38 +0530

    Update build_rom.sh
    dotos fr ginkayy with A12.1

--- build_rom.sh
+++ build_rom.sh
@@ -1,21 +1,26 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/AICP/platform_manifest -b s12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/abhishekhembrom08/manifest_local.git --depth 1 -b rova .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/DotOS/manifest -b dot12.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/abhishekhembrom08/manifest_local.git --depth 1 -b dotosGinkgo .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 #export SELINUX_IGNORE_NEVERALLOWS=true
 #export ALLOW_MISSING_DEPENDENCIES=true
-lunch aicp_rova-userdebug
+lunch dot_ginkgo-userdebug
+export KBUILD_BUILD_USER=xyz_abhishek
+export KBUILD_BUILD_HOST=xyz_abhishek
+export BUILD_USERNAME=xyz_abhishek
+export BUILD_HOSTNAME=xyz_abhishek
 export TZ=Asia/Kolkata #put before last build command
-mka bacon
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/recovery.img cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
 
 
-#fck_Alone0316_madarchod_nahi_sudhrega
+#fck_@Alone0316_madarchod_nahi_sudhrega
 #HavocBhiMadarchodNikla
 
 
