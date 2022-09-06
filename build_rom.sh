28d36f9922222ee122d7c8707e80fa125da331e7
JuanTamadski <76487328+JuanTamadski@users.noreply.github.com>
2022-09-07 07:52:29 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch blaze_Z01R-userdebug
+lunch blaze_ZE620KL-userdebug
 export TZ=Asia/Kolkata 
 brunch Z01R
 
