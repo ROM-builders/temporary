e6d1d0feaa3197398eefd32eee5a0f7e043f2759
JuanTamadski <76487328+JuanTamadski@users.noreply.github.com>
2022-09-06 21:04:59 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/ProjectBlaze/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/JuanTamadski/local_manifest.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/yograjsingh-cmd/local_manifest.git --depth 1 -b blaze .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch blaze_Z01R_userdebug
-export TZ=Asia/Dhaka #put before last build command
+lunch blaze_Z01R-userdebug
+export TZ=Asia/Kolkata 
 brunch Z01R
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
