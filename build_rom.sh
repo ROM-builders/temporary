b4022f6aeb4f3fffb6eae80a746e18c1aed976b7
redznn <103375250+redznn@users.noreply.github.com>
2022-09-06 10:49:35 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init -u https://github.com/PixelExperience/manifest -b twelve-plus
+repo init -u https://github.com/ArrowOS/android_manifest.git -b arrow-12.1
 git clone https://github.com/redznn/local_manifest.git
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch PixelExperience_m12-eng
+lunch Arrow_m12-eng
 export TZ=Asia/Dhaka #put before last build command
-mka pixelexperience
+mka arrow
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
