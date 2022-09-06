4c79fb9e076505b02840d1d32eb20dc2bd47eb92
Matheus Souza <33396386+matheucomth@users.noreply.github.com>
2022-09-06 19:56:51 -0300

    switch branch of manifest

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,13 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/ArrowOS/android_manifest.git -b arrow-13.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/matheucomth/local_manifest --depth 1 -b arrow-13 .repo/local_manifests
+git clone https://github.com/matheucomth/local_manifest.git --depth 1 -b arrow .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build roms
+# build rom
 . build/envsetup.sh
 lunch arrow_tulip-userdebug
 export TZ=Asia/Dhaka
 export KBUILD_BUILD_USER=PaperBoy
-export BUILD_HOSTNAME=PaperBoy #put before last build command
 m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
