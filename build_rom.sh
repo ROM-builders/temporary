cbf96abb28f9d67e323b0f1a1e5b39046c058f2f
hsx02 <hsx02self@gmail.com>
2022-09-07 11:41:42 +0530

    adapt for redmi note 11

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,22 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://github.com/NRanjan-17/local_manifest.git --depth 1 -b Evolution-X-tiramisu .repo/local_manifests
+git clone https://github.com/hsx02/Local-Manifests.git --depth 1 -b evo13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch evolution_mido-userdebug
-export KBUILD_BUILD_USER=HyperPower
-export KBUILD_BUILD_HOST=17
-export BUILD_USERNAME=HyperPower
-export BUILD_HOSTNAME=17
+lunch evolution_spes-userdebug
 export TZ=Asia/Delhi #put before last build command
+export SELINUX_IGNORE_NEVERALLOWS=true
+export TARGET_BOOT_ANIMATION_RES=1080
+export TARGET_FACE_UNLOCK_SUPPORTED=true
+export TARGET_SUPPORTS_QUICK_TAP=false
+export TARGET_INCLUDE_LIVE_WALLPAPERS=false
+export TARGET_SUPPORTS_GOOGLE_RECORDER=false
+export TARGET_SUPPORTS_CALL_RECORDING=true
+export TARGET_SUPPORTS_NEXT_GEN_ASSISTANT=true
+export TARGET_INCLUDE_STOCK_ARCORE=false
+export TARGET_ENABLE_BLUR=true
 mka evolution
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
