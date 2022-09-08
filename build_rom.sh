97fbb321ad5b2d3c4f488a418bac79165963d054
Christopher Keith Irvine <Mizdrake7@gmail.com>
2022-09-08 15:03:48 +0530

    r5x-VoltageOS-12-mizdrake7

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,10 @@
-repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 12.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest -b 12l -g default,-mips,-darwin,-notdefault
 git clone https://github.com/mizdrake7/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-
 # build rom
 source build/envsetup.sh
-lunch lineage_r5x-userdebug
+lunch voltage_r5x-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export KBUILD_BUILD_USER=MAdMiZ
 export KBUILD_BUILD_HOST=MAdMiZ
