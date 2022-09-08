42fab4eb371450a95c6d214eb3abd089df1aedfd
iTZ_UDAY2312 <72090728+UdayBindal2312@users.noreply.github.com>
2022-09-08 18:37:27 +0530

    roll back to sairam trees

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,14 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/UdayBindal2312/local_manifest.git -b RiceDroid-12 --depth 1 .repo/local_manifests
+git clone https://github.com/UdayBindal2312/local_manifest.git -b RiceDroid-12L --depth 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch lineage_miatoll-userdebug
-export TZ=Asia/Kolkata #put before last build command
+#put before last build command
+export TZ=Asia/Kolkata 
 mka bacon
 
-# upload rom#
+# upload rom
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
