fa11d49c1e6e42ca0d8dd7d2966da3997843c276
newuserbtw <90239625+newuserbtw@users.noreply.github.com>
2022-09-08 19:42:52 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,7 @@
 # sync rom 
 repo init -u https://github.com/Project-Kaleidoscope/android_manifest.git -b sunflowerleaf -g default,-mips,-darwin,-notdefault 
-git clone https://github.com/newuserbtw/local_manifest.git --depth 1 -b main .repo/local_manifests repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
+git clone https://github.com/newuserbtw/local_manifest.git --depth 1 -b main .repo/local_manifests 
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
 #please fucking sync 
 # build rom 
 . build/envsetup.sh 
