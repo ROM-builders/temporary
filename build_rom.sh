397f6106d06162f24950c7745d2527121fb7972c
newuserbtw <90239625+newuserbtw@users.noreply.github.com>
2022-09-09 20:25:21 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom 
-repo init --depth=1 --no-repo-verify -u https://github.com/CipherOS/android_manifest.git -b twelve-L -b twelve-L -g default,-mips,-darwin,-notdefault 
+repo init --depth=1 --no-repo-verify -u https://github.com/CipherOS/android_manifest.git -b twelve-L -g default,-mips,-darwin,-notdefault 
 git clone https://github.com/newuserbtw/local_manifest.git --depth 1 -b main .repo/local_manifests 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 # please fucking sync 
