b85fc2268b2bca1b9050f16a943427d89353f09c
Ninja Pika <76650191+ninjapika@users.noreply.github.com>
2022-09-09 12:47:41 +0530

    certus-SparkOs-ninjapika

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom
+# sync rom 
 repo init -u https://github.com/Spark-Rom/manifest -b spark
 git clone https://github.com/ninjapika/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
