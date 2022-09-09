724991d0653ca852347c113a190c2c19da26eb61
iTZ_UDAY2312 <72090728+UdayBindal2312@users.noreply.github.com>
2022-09-09 17:45:17 +0530

    evoX initialize

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/UdayBindal2312/local_manifest.git -b RiceDroid-12L --depth 1 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
+git clone https://github.com/UdayBindal2312/local_manifest.git -b Evo-13 --depth 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_miatoll-userdebug
-#put before last build command
-export TZ=Asia/Kolkata 
-mka bacon
-
-# upload room
+lunch evolution_miatoll-userdebug
+export TZ=Asia/Kolkata #put before last build command
+breakfast && mka bacon
+# upload rom
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
