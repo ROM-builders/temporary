c0f555f968b2b20727c8ae8d5a2c610cb2f936b0
iTZ_UDAY2312 <72090728+UdayBindal2312@users.noreply.github.com>
2022-09-09 06:11:47 +0530

    sync issues

--- build_rom.sh
+++ build_rom.sh
@@ -10,5 +10,5 @@ lunch lineage_miatoll-userdebug
 export TZ=Asia/Kolkata 
 mka bacon
 
-# upload rom
+# upload room
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
