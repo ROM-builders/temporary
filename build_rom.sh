459a194d9d618cb3c43791efb3bafa54cf1f3df0
JuanTamadski <76487328+JuanTamadski@users.noreply.github.com>
2022-09-10 08:56:11 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -uhttps://github.com/ProjectBlaze/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/AospExtended/manifest.git -b 12.1.x -g default,-mips,-darwin,-notdefault
 git clone https://github.com/yograjsingh-cmd/local_manifest.git --depth 1 -b blaze .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch blaze_Z01R-userdebug
+lunch aosp_Z01R-userdebug
 export TZ=Asia/Kolkata 
 mka bacon
 
