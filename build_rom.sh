eba0948cf00f37f6332ba2bde6ae544c8ba1540d
Nguyen Cong Vinh <admin@vnncv.io>
2022-09-11 07:08:25 +0700

    kebab-Project-Awaken-12.1-j0ok34n

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,16 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/j0ok34n/local_manifests.git --depth 1 -b kebab-aosp .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b 12.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/j0ok34n/local_manifests.git --depth 1 -b kebab-awaken .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-. build/envsetup.sh
-lunch aosp_kebab-userdebug
-mka bacon
+source build/envsetup.sh
+cp packages/apps/Settings/res/values/awaken_strings.xml ../awaken_strings.xml
+perl -pe 's/Unofficial Maintainer/"Công Vĩnh"/g' ../awaken_strings.xml > packages/apps/Settings/res/values/awaken_strings.xml
+export TZ=Asia/Ho_Chi_Minh
+lunch awaken_kebab-userdebug
+make bacon
+perl -pe 's/"Công Vĩnh"/Unofficial Maintainer/g' ../awaken_strings.xml > packages/apps/Settings/res/values/awaken_strings.xml
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
