e088758e17c877bb6ae9c07c50edf1ab30219e60
Raiden Ishigami <contact.raidenishi69@gmail.com>
2022-09-11 05:28:56 -0300

    Attempt #4

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Corvus-AOSP/android_m
 git clone https://github.com/raidenkkj/local_manifest.git --depth 1 -b corvus-mithorium .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom 
+# build rom
 . build/envsetup.sh
 lunch corvus_Mi439-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
