707ae2c919cf06fa960ccd501a3e5bd1eabaa0c3
iTZ_UDAY2312 <72090728+UdayBindal2312@users.noreply.github.com>
2022-09-11 13:34:39 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manif
 git clone https://github.com/UdayBindal2312/local_manifest.git -b arrow --depth 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom#
 source build/envsetup.sh
 lunch arrow_miatoll-userdebug
 export TZ=Asia/Kolkata # Add before last build command
