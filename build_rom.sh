a1941f52089c96aff3a029374f2ab0a8cc8b963f
SKetU <96108085+SKetU-l@users.noreply.github.com>
2022-09-11 19:47:01 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/SKetU-l/local_manifest --depth 1 -b blaze .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
 
 # build rom
 source build/envsetup.sh
