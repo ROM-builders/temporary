9e0119c70def5b1639b2c4aff658c25d66abfda5
Anant Ahir <99502637+anantinfinity@users.noreply.github.com>
2022-09-12 22:20:48 +0530

    beryllium-xdroid-oss-thirteen

--- build_rom.sh
+++ build_rom.sh
@@ -3,9 +3,12 @@ repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-oss/xd_manifes
 git clone https://github.com/anantinfinity/local_manifest.git --depth 1 -b 13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+#1
+
 # build rom
 source build/envsetup.sh
 lunch xdroid_beryllium-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Kolkata #put before last build command
 make xd
 
