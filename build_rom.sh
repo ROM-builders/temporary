dfbec9513e332937e475b95de9d8aafd67b9d4aa
karann <100251020+karann2354@users.noreply.github.com>
2022-09-12 20:31:56 +0530

    Mi439-crdroidandroid-11.0-karann2354

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/androi
 git clone https://github.com/karann2354/local_manifest.git --depth 1 -b crd .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom 
+# build rom
 source build/envsetup.sh
 export TZ=Asia/Dhaka #put before last build command
 brunch Mi439
