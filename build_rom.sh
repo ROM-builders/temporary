c80057f4d14f339e6cd3eb2ed8de6a614beefdc5
iTZ_UDAY2312 <72090728+iTZ-UDAY2312@users.noreply.github.com>
2022-09-12 18:05:36 +0530

    fix pr

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync sources
 repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/iTZ-UDAY2312/ROM_Manifest -b rice --depth 1 .repo/local_manifests
+git clone https://github.com/iTZ-UDAY2312/ROM_Manifest.git -b rice --depth 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # Build rom
