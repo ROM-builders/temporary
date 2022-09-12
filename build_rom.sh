2fa0cbab8fb375951b9ef68dff0c29971bea4499
Rahul Singh <94305290+ThakurRahulSingh@users.noreply.github.com>
2022-09-12 18:19:02 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/ThakurRahulSingh/local_manifest.git --depth 1 -b ma
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # Set up environment
- build/envsetup.sh
+. build/envsetup.sh
 lunch arrow_miatoll-userdebug
 export TZ=Asia/Dhaka #put before last build command
 # Build the ROM
