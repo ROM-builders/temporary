a33cc54e22ea7f3ea13d4a54648e3c5c52601226
iTZ_UDAY2312 <72090728+iTZ-UDAY2312@users.noreply.github.com>
2022-09-12 17:54:46 +0530

    rice bringup

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync sources
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/UdayBindal2312/ROM_Manifest --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/UdayBindal2312/ROM_Manifest -b rice --depth 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # Build rom
