c758f7dfac1856ca46c114a0565aa4028ddf7388
Jash <jashvakharia@gmail.com>
2022-09-14 03:18:50 +0530

    fix branch name

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/DotOS/manifest.git -b dot12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/jash69/local-manifests.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/jash69/local-manifests.git --depth 1 -b main .repo/local_manifests          #fix branch name
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
