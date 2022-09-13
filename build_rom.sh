162f0d104402e54e14ed59892998f17a7fd950fe
Uxtrom <101785747+Uxtrom@users.noreply.github.com>
2022-09-13 21:45:19 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Ricedroid/manifest.git -b twele -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Ricedroid/manifest.git -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Uxtrom/local_manifests.git --depth 1 -b rice-12.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
