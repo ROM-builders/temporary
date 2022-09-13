c73c593746f639fbed95463f3fb71b14b998bdd1
Soyab Khan <56043965+soyabkhanmalek@users.noreply.github.com>
2022-09-13 18:36:58 +0530

    r5x-projectlighthouse-soyabkhanmalek

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/lighthouse-os/manifest.git -b sailboat_L1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/soyabkhanmalek/Local-Manifests.git --depth 1 -b main .repo/local_manifests
-repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all);
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 # build rom
 source build/envsetup.sh
 lunch lighthouse_r5x-user
