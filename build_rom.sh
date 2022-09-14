a369b6e43329626ce3d80ffa62d621fe3a8fc9ca
Sayan Sarkar <97832667+SKORPION29@users.noreply.github.com>
2022-09-14 22:13:35 +0530

    Rebuild

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest -b
 git clone https://github.com/SKORPION29/local_manifest.git --depth 1 -b voltage .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom 
 source build/envsetup.sh
 export TZ=Asia/Kolkata #put before last build command
 brunch voltage_miatoll-userdebug
