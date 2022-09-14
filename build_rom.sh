25bd36b0fd3f11fd53d4af043ef4f440762247fd
iTZ_UDAY2312 <72090728+iTZUDAY2312@users.noreply.github.com>
2022-09-14 06:52:32 +0530

    fix armv8-2a

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 12.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android -b 12.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/iTZUDAY2312/android_manifest --depth 1 -b CrDroid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch lineage_miatoll-userdebug
-export TZ=Asia/Dhaka #put before last build command
+export TZ=Asia/Kolkata # Put before last build command
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
