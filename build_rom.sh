68f2a673e9346ded5d6e76d9db48cda0b5d30926
NRanjan-17 <nalinishranjan05@gmail.com>
2022-09-14 18:14:33 +0530

    mido: Initial PixelExperience thirteen

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/NRanjan-17/local_manifest.git --depth 1 -b Pixel-Experience-12 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b thirteen -g default,-mips,-darwin,-notdefault
+git clone https://github.com/NRanjan-17/local_manifest.git --depth 1 -b Pixel-Experience-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
