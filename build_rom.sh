dbc9b05f668d294c39abb11ed5509ba637a57608
acupofcups <90239625+newuserbtw@users.noreply.github.com>
2022-09-14 08:37:38 +0700

    lazrdev's manifest cuz error also his looks scary af
    thx

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,7 @@
 # sync rom 
 repo init --depth=1 --no-repo-verify -u https://github.com/CipherOS/android_manifest.git -b twelve-L -g default,-mips,-darwin,-notdefault 
 git clone https://github.com/newuserbtw/local_manifest.git --depth 1 -b main .repo/local_manifests 
+#thx for manifest lazr
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 # please fucking sync 
 # build rom. do it please i want my streak of failed builds to end
