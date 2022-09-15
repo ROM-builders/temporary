f74cd0c0eaddc615141798021897ec88fdaa56f6
JuanTamadski <76487328+JuanTamadski@users.noreply.github.com>
2022-09-16 04:44:18 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b twelve -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android.git -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/JuanTamadski/local_manifest.git --depth 1 -b rice .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
