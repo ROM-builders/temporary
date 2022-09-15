1344c74c64471fd750846bcc76ea560169823adc
iTZ_UDAY2312 <72090728+iTZUDAY2312@users.noreply.github.com>
2022-09-15 19:17:44 +0530

    lunch as cherish

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_miatoll-userdebug
+lunch cherish_miatoll-userdebug
 export TZ=Asia/Kolkata # Put before last build command
 mka bacon
 
