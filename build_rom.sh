1f2711abaeed27da94d89e9c4ff3af1146845ddf
Raiden Ishigami <contact.raidenishi69@gmail.com>
2022-09-15 14:28:47 -0300

    Attempt #4

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 export SELINUX_IGNORE_NEVERALLOWS=true
+export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Dhaka #put before last build command
 brunch blaze_Mi439-userdebug
 
