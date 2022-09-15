553038e0426040b605b00d6c7d78e5cb1aa7a10d
Hitesoni | ×͜× <72127977+hsx02@users.noreply.github.com>
2022-09-15 09:04:56 +0530

    Fix bootloop

--- build_rom.sh
+++ build_rom.sh
@@ -9,6 +9,7 @@ lunch evolution_spes-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export TZ=Asia/Delhi #put before last build command
+cd roms
 mka evolution
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
