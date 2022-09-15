15a1d076fb681f5b5e0b5885d1ef270ecd08927e
JuanTamadski <76487328+JuanTamadski@users.noreply.github.com>
2022-09-15 14:40:51 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync romhttps://github.com/AICP/platform_manifest.git -b s12.1
+# sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/AICP/platform_manifest.git -b s12.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/JuanTamadski/local_manifest.git --depth 1 -b test .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
