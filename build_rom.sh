dae931702eeb93d5d932f80f035d146c74d3b592
Raiden Ishigami <contact.raidenishi69@gmail.com>
2022-09-15 03:47:35 -0300

    Attempt #2

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest
 git clone https://github.com/raidenkkj/local_manifest.git --depth 1 -b blaze-mithorium .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom 
+# build rom
 . build/envsetup.sh
 lunch blaze_Mi439-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
