de37038480523efa98842bf506229b9d9266de9d
Goku <100507815+god-goku@users.noreply.github.com>
2022-09-15 20:18:25 +0530

    Lets try cherish a13

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/god-goku/local-manifest.git --depth 1 -b veux .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest -b tiramisu -g default,-mips,-darwin,-notdefault
+git clone https://github.com/god-goku/local-manifest.git --depth 1 -b cherry .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-#9
+
 # build rom
 . build/envsetup.sh
+lunch cherish_veux-userdebug
 export TZ=Asia/Dhaka #put before last build command
 brunch veux
 
