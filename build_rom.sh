d7928135c89b253a8f2906d6cbd6e8e732591639
anantNOOB <113432669+anantNOOB@users.noreply.github.com>
2022-09-16 10:22:59 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch xdroid_beryllium-userdebug
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
 export TZ=Asia/Kolkata #put before last build command
 make xd
 
