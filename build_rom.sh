9b3a5bcd50c54c0361d8d86b34a13409c3611c71
Anandhasayanan <anandhasayanan007jb@gmail.com>
2022-09-16 18:53:11 +0530

    Overlay fixes

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest -b sapphire -g default,-mips,-darwin,-notdefault
-git clone https://github.com/anandhan07/local_manifest.git --depth 1 -b sapphire .repo/local_manifests
+git clone https://github.com/anandhan07/local_manifest.git --depth 1 -b AOSPA-sapphire .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom [6]
 source build/envsetup.sh
 lunch aospa_vince-userdebug
 export TZ=Asia/Dhaka #put before last build command
