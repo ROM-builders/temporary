f2cbf33873bc05d9a561b29f55496d5f43dc0be7
JaswantMamidisetti <111034654+JaswantMamidisetti@users.noreply.github.com>
2022-09-17 13:58:12 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,6 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build roms
 . build/envsetup.sh
+export WITH_GAPPS=true
 export TZ=Asia/Kolkata #put before the last build command
 brunch r5x
 
