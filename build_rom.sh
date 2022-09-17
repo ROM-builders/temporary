f09272bdbb95816a38070f07cf84cffdaccb83ad
JaswantMamidisetti <111034654+JaswantMamidisetti@users.noreply.github.com>
2022-09-17 07:18:27 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build roms
 . build/envsetup.sh
-lunch blaze_r5x-userdebug
 export TZ=Asia/Kolkata #put before the last build command
 brunch r5x
 
