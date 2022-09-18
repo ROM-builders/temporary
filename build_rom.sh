851d32e62d4c99797aa62c438236a34a3c246ba7
swapnil vicky <74442290+SwapnilVicky@users.noreply.github.com>
2022-09-18 18:57:22 +0530

    initial build

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-oss/xd_manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/SwapnilVicky/local_manifest.git --depth 1 -b 13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
