dd3a61cfe0bfb589147ead721cdd28a9a9a05502
nerd.zenss_ <97657256+NerdZ3ns@users.noreply.github.com>
2022-09-18 13:08:08 +0700

    X01BD-xdroid-oss-thirteen-NerdZ3ns

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-oss/xd_manifest -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/NerdZ3ns/local_manifest.git --depth 1 -b master .repo/local_manifestsrepo
-sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+git clone https://github.com/NerdZ3ns/local_manifest.git --depth 1 -b master .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom 4.19
 source build/envsetup.sh
