6c573c32174c9d95b799910973b1f2d51dc4a7ec
nerd.zenss_ <97657256+NerdZ3ns@users.noreply.github.com>
2022-09-18 13:07:29 +0700

    X01BD-xdroid-oss-thirteen-NerdZ3ns

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-oss/xd_manifest -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/NerdZ3ns/local_manifest.git --depth 1 -b master .repo/local_manifestsrepo 
-sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
+git clone https://github.com/NerdZ3ns/local_manifest.git --depth 1 -b master .repo/local_manifestsrepo
+sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom 4.19
 source build/envsetup.sh
@@ -11,7 +11,7 @@ export BUILD_HOSTNAME=android-build
 export KBUILD_BUILD_USER=nobody
 export KBUILD_BUILD_HOST=android-build
 export TZ=Asia/Jakarta
-make xd 
+make xd
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
