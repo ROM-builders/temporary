5d9e2fa22779cd84e7040d47f27048155ff10d7f
boedhack99 <boedhack@gmail.com>
2022-09-20 06:19:53 +0700

    trigger: fog ( redmi 10c ): Try to fix callui resolution

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/ArrowOS/android_manifest.git -b arrow-13.0 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-13.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/boedhack99/local_manifest.git --depth 1 -b fog-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
