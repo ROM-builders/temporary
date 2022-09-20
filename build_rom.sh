f86ac248735d44c8a04b1e7aad2faf40cdd929af
Hitesoni | ×͜× <72127977+hsx02@users.noreply.github.com>
2022-09-20 14:01:43 +0530

    Fix trees

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom
+## sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
 git clone https://github.com/hsx02/Local-Manifests.git --depth 1 -b cherish .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
