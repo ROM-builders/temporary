e5dc68707897a6a3ea654f4f5537dbe76e62bf1a
Hasin Abrar Haque <70853665+AbrarNoob@users.noreply.github.com>
2022-09-20 23:50:13 +0600

    Again

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,8 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
 git clone https://github.com/AbrarNoob/local_manifest --depth 1 -b evox .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
- 
+
+
 # build rom
 source build/envsetup.sh
 lunch evolution_alioth-userdebug
