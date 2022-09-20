3965473c271a70143d8753318953789e99a074e1
NFS86 <jarbull86@gmail.com>
2022-09-20 08:03:26 +0000

    rosy initial build ArrowOS 13.0

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,21 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-13.0 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/NFS-project/local_manifest --depth 1 -b rosy-arrow-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
-export TZ=Asia/Dhaka #put before last build command
-mka derp
+lunch arrow_rosy-userdebug
+export TZ=Asia/Jakarta
+export KBUILD_BUILD_USER=rosy
+export KBUILD_BUILD_HOST=nfsproject
+export BUILD_USERNAME=rosy
+export BUILD_HOSTNAME=nfsproject
+export use_GrapheneCamera=true
+export USE_NFSparts=true
+export LINUX_COMPILE_BY=NFS86
+export LINUX_COMPILE_HOST=NFS-project
+m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
