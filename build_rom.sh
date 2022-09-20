a3216bb096d1e79ab4438e808bc84ec19a74dbd3
Raiden Ishigami <contact.raidenishi69@gmail.com>
2022-09-20 19:46:04 -0300

    buildrom: setup flos | attempt #1

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,16 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/Havoc-OS/android_manifest.git -b eleven -g default,-mips,-darwin,-notdefault
-git clone https://github.com/raidenkkj/local_manifest.git --depth 1 -b a001d-havoc .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ForkLineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/raidenkkj/local_manifest.git --depth 1 -b flos-mithorium .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch havoc_A001D-userdebug
-export TZ=America/Sao_Paulo
-export ALLOW_MISSING_DEPENDENCIES=true
-m bacon
+lunch lineage_Mi439-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
+export TZ=Asia/Dhaka #put before last build command
+export TARGET_FLOS=true
+export WITH_GMS=false
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
