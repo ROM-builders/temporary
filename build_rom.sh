50572b5cece6f2428c00e4fbe410a818e4bace24
RasyidKautsar <rasyid2007alkautsar@gmail.com>
2022-09-22 08:13:19 +0700

    fix xiaomi pret

--- build_rom.sh
+++ build_rom.sh
@@ -2,6 +2,7 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Prvtcmt/lcl_mnfst.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
 # build rom
 . build/envsetup.sh
 lunch cherish_mido-user
