1e6f62529527c4cb6b830c7511a89f5737023631
Akash Kakkar <akash.galaxy07@gmail.com>
2022-09-22 15:38:28 +0530

    munch-Evolution-X-tiramisu-akash07k

--- build_rom.sh
+++ build_rom.sh
@@ -1,8 +1,6 @@
-# initialize the repo
-repo init --depth=1 --no-repo-verify -u git://github.com/Evolution-X/manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
-# clone manifest
-git clone https://github.com/akash07k/local_manifests.git --depth 1 -b EvolutionX .repo/local_manifests
 # sync rom
+repo init --depth=1 --no-repo-verify -u git://github.com/Evolution-X/manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
+git clone https://github.com/akash07k/local_manifests.git --depth 1 -b EvolutionX .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # build rom
