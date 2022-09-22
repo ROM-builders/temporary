009fae59c6e5b054c63372939bc5e10eab2f9177
ahmedhridoy371 <40537458+ahmedhridoy371@users.noreply.github.com>
2022-09-22 11:40:41 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch evolution_lavender-user
+export WITH_GAPPS=TRUE
 export TZ=Asia/Dhaka #put before last build command
 mka evolution
 
