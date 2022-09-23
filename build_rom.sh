0d503b9d2017ee898bc5a15b6ef35630e6be6d40
themaltuant <110547541+themaltuant@users.noreply.github.com>
2022-09-23 15:08:09 +0530

    x

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,7 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/themaltuant/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
+ 
 # build rom
 source build/envsetup.sh
 lunch awaken_vayu-user
