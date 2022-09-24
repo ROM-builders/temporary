e65ba49ea4dbddeb73174088146796f2ae15a16d
Nishant <82361039+Nishant9947@users.noreply.github.com>
2022-09-24 09:20:07 +0530

    Try again

--- build_rom.sh
+++ build_rom.sh
@@ -8,6 +8,7 @@ source build/envsetup.sh
 lunch evolution_X01AD-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TARGET_USES_MINI_GAPPS=true
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export TZ=Asia/Dhaka #put before last build command
 mka evolution
 
