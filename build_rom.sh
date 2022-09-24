576bda1839b1358909ce915ff386ca87f0a562d4
JuanTamadski <76487328+JuanTamadski@users.noreply.github.com>
2022-09-24 08:25:52 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/AICP/platform_manifest.git -b s12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/yograjsingh-cmd/local_manifest.git --depth 1 -b Derp-12 .repo/local_manifests
+git clone https://github.com/JuanTamadski/local_manifest.git --depth 1 -b a505x .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aicp_Z01R-user
+lunch aicp_Z01R-a505x
 export TZ=Asia/Kolkata 
 mka bacon
 
