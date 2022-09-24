f49046a62684ee73a8ee8995763843a90d5342c3
Nishant <82361039+Nishant9947@users.noreply.github.com>
2022-09-24 06:34:59 +0530

    Go tiramisu

--- build_rom.sh
+++ build_rom.sh
@@ -8,7 +8,7 @@ source build/envsetup.sh
 lunch evolution_X01AD-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TARGET_USES_MINI_GAPPS=true
-export TZ=Asia/Dhaka #put  before last build command
+export TZ=Asia/Dhaka #put before last build command
 mka evolution
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
