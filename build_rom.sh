5337449530f45f300bae8245d691d0a7f34bb66d
iTZ_UDAY2312 <72090728+iTZUDAY2312@users.noreply.github.com>
2022-09-24 07:49:45 +0530

    certus update v5

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/iTZUDAY2312/android_manifest --depth 1 -b CrDroid .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/iTZUDAY2312/android_manifest --depth 1 -b certus .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_miatoll-userdebug
+lunch lineage_certus-userdebug
 export TZ=Asia/Kolkata # Put before last build command
 mka bacon
 
