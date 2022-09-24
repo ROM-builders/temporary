d46409f1292b3687272d5cfff513674af8cb2745
boedhack99 <boedhack@gmail.com>
2022-09-24 10:16:01 +0700

    build: Switch to A13 branch (2)

--- build_rom.sh
+++ build_rom.sh
@@ -11,4 +11,3 @@ mka evolution
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-
