c032b2fbf83ea09ad72ffa7b62d6990049389db8
Soyab Khan <56043965+soyabkhanmalek@users.noreply.github.com>
2022-09-26 00:25:39 +0530

    j1xlte-LineageOS-lineage-17.1-soyabkhanmalek

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
-# sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/lighthouse-os/manifest.git -b sailboat_L1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/soyabkhanmalek/local_manifest.git --depth 1 -b main .repo/local_manifests
+# sync rom j1xlte-LineageOS-lineage-17.1-soyabkhanmalek
+repo init --depth=1 --no-repo-verify - git://github.com/wecouldcalliteven/android.git -b lineage-17.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/soyabkhanmalek/local_manifest.git --depth 1 -b j1xlte .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lighthouse_mojito-user
+lunch lineage_j1xlte-userdebug
 export TZ=Asia/Dhaka #put before last build command
-make lighthouse
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
