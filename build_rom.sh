dca1391445d293a4757d405ac186d827028c09a6
dtyghc <100001963+dtyghc@users.noreply.github.com>
2022-09-26 03:52:19 +0530

    Trying Again

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest 
 git clone https://github.com/dtyghc/local_manifest.git --depth=1 -b evo-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom 
 source build/envsetup.sh
 lunch evolution_Mi439-userdebug
 export WITH_GAPPS=true
