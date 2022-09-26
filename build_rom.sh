cb4c89bf7278c94a4099ba85c5f32cff99c21480
themaltuant <110547541+themaltuant@users.noreply.github.com>
2022-09-26 21:08:07 +0530

    user build

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch p404_vayu-userdebug
+lunch p404_vayu-user
 export WITH_GAPPS=true
 export TZ=Asia/Kolkata #put before last build command
 make bacon
