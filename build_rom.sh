1dfde004ae18e8260d99e93b61c9f1e9da684308
Futar0 <104120854+Futar0@users.noreply.github.com>
2022-09-26 14:20:06 +0800

    lava-LineageOS-lineage-19.1-futar0
    LineageOs Build For Lava

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -
 
 git clone https://gitlab.com/R9Lab/Manifest.git --depth 1 -b LineageOS-12.1 .repo/local_manifests
 
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
