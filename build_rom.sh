c89f7e2e613f781557ba7d0c69d8f517571c46e5
Futar0 <104120854+Futar0@users.noreply.github.com>
2022-09-28 12:27:05 +0800

    lava-PixelExperience-twelve-plus-Futar0
    Pixel Experience For Lava

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/crdroidandroid/android.git -b 12.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/PixelExperience/manifest -b twelve-plus -g default,-mips,-darwin,-notdefault 
 
-git clone https://gitlab.com/R9Lab/Manifest.git --depth 1 -b LineageOS-12.1 .repo/local_manifests
+git clone https://gitlab.com/R9Lab/Manifest.git --depth 1 -b 12.1 .repo/local_manifests
 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
@@ -9,7 +9,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 source build/envsetup.sh
 
-lunch lineage_lava-userdebug
+lunch aosp_lava-userdebug
 
 export TZ=Asia/Dhaka #put before last build command
 
