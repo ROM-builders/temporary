ed12c64dd249f55121016ff2434b9d2b825ff8ca
Raiden Ishigami <contact.raidenishi69@gmail.com>
2022-09-28 11:02:22 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # 9
 # build rom
 . build/envsetup.sh
-export BUILD_HOSTNAME=RaidenIshigami
 export BUILD_USERNAME=RaidenIshigami
 export TARGET_USES_MINI_GAPPS=true
 export SELINUX_IGNORE_NEVERALLOWS=true
