bfb6f44ae2d957f6ab19ae2ef3341acca347b0d5
ZHANtech™ <hifzhan41@gmail.com>
2022-09-30 05:32:51 +0700

    Change RR Repo

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ResurrectionRemix/platform_manifest.git -b Q -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/ResurrectionRemix-Revived/platform_manifest.git -b Q -g default,-mips,-darwin,-notdefault
 git clone https://github.com/zhantech/local_manifest.git --depth 1 -b rr-tissot .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
