6aab93f297189435b578d31720c53716efec608a
xack21 <86214526+xack21@users.noreply.github.com>
2022-09-29 18:55:33 +0530

    land-CherishOS-tiramisu-xack

--- build_rom.sh
+++ build_rom.sh
@@ -7,6 +7,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch cherish_land-userdebug
 export WITH_GMS := false
+export
+ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
