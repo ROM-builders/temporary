b7a318510b05cb5f0cc3530ac6f0da61a3392539
Anandhasayanan <anandhasayanan007jb@gmail.com>
2022-09-29 19:58:37 +0530

    Build vanilla final

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ForkLineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/anandhan07/local_manifest.git --depth 1 -b fork.lineage-19.1-wip .repo/local_manifests
+git clone https://github.com/anandhan07/local_manifest.git --depth 1 -b lineage-19.1-fork .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom [10]
+# build rom [11]
 source build/envsetup.sh
 lunch lineage_vince-user
 export TARGET_FLOS=true
