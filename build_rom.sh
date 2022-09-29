04313b192785fb76dc668154c736423be744a652
xack21 <86214526+xack21@users.noreply.github.com>
2022-09-29 14:41:17 +0530

    land- CherishOS-tiramisu-xack

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
 git clone https://github.com/xack21/local_manifest-1.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_land-userdebug
+lunch cherish_land-userdebug
+export WITH_GMS := false
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
