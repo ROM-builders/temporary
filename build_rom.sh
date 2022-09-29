7ed6b90632587bba25079291d58f95e7c2d1bd24
Rahul Singh <94305290+ThakurRahulSingh@users.noreply.github.com>
2022-09-30 05:16:16 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PotatoProject/manifest -b frico_mr1-release -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ThakurRahulSingh/local_manifest.git --depth 1 -b posp .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExtended/manifest -b snow -g default,-mips,-darwin,-notdefault
+git clone https://github.com/ThakurRahulSingh/local_manifest.git --depth 1 -b pex .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # Set up environment
 . build/envsetup.sh
-lunch potato_miatoll-userdebug
+lunch aosp_miatoll-userdebug
 export TZ=Asia/Dhaka #put before last build command
-m otapackage
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
