7f66f4aaa4aab48a6b3972b42c824a63f050712d
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2022-09-30 18:49:54 +0530

    cherish 13

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest -b twelve-one -g default,-mips,-darwin,-notdefault
-git clone https://github.com/rushiranpise/local_manifest.git --depth 1 -b cherish .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest -b tiramisu -g default,-mips,-darwin,-notdefault
+git clone https://github.com/rushiranpise/local_manifest.git --depth 1 -b cherish13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -18,4 +18,4 @@ mka bacon
 rclone copy out/target/product/chef/*.zip cirrus:chef/cherish -P
 rclone copy out/target/product/chef/boot.img cirrus:chef/cherish -P
 
-# 7
+# 1
