33af7f90d5d18842ee090818ac21635c64f58ac4
ahmedhridoy371 <40537458+ahmedhridoy371@users.noreply.github.com>
2022-09-30 13:06:54 +0600

    After Cleaning Trying Again To Build Spark

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/Spark-Rom/manifest -b spark -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ahmedhridoy371/local_manifest --depth 1 -b spark .repo/local_manifests
+git clone https://github.com/ahmedhridoy371/local_manifest.git --depth 1 -b spark-A12.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
