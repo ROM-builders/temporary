379f418cb8ca6f20f992226d7f2a111823269da1
Orkun Ergün <68orkun@proton.me>
2022-10-01 21:17:04 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,13 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://gitlab.com/R9Lab/Manifest.git --depth 1 -b CherishOS-13 .repo/local_manifests
+git clone https://gitlab.com/RN9Lab/Manifest.git --depth 1 -b CherishOS-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch cherish_lava-userdebug
+lunch cherish_merlinx-userdebug
 export WITH_GMS=true
+export TARGET_USES_MINI_GAPPS=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
