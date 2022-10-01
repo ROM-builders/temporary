9ecaa14a0cf4a18d53c05b8a941fdf917c0094b8
Futar0 <104120854+Futar0@users.noreply.github.com>
2022-10-02 00:18:33 +0800

    lava-Project-Fluid-fluid-12.1-Futar0

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ForkLineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
-git clone https://gitlab.com/R9Lab/Manifest.git --depth 1 -b LineageOS-12.1 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Fluid/manifest.git -b fluid-12.1 -g default,-mips,-darwin,-notdefault
+git clone https://gitlab.com/R9Lab/Manifest.git --depth 1 -b Fluid-12.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # build rom
 source build/envsetup.sh
-lunch lineage_lava-userdebug
+lunch fluid_lava-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
