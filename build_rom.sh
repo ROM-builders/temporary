bd086606aa0f82d3e5d8b70bb5f6b69e6999efc2
Futar0 <104120854+Futar0@users.noreply.github.com>
2022-10-01 08:40:24 +0800

    lava-ForkLineageOS-lineage-19.1-Futar0
    Laba laba

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 11.0 -g default,-mips,-darwin,-notdefault
-git clone https://gitlab.com/R9Lab/Manifest.git --depth 1 -b crDroid-11 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ForkLineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
+git clone https://gitlab.com/R9Lab/Manifest.git --depth 1 -b LineageOS-12.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # build rom
