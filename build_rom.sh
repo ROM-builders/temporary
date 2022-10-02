716684ac109de259621b7e26d8fa4a6bc12fbca8
ahmedhridoy371 <40537458+ahmedhridoy371@users.noreply.github.com>
2022-10-02 20:08:41 +0600

    Building LineageOs

--- build_rom.sh
+++ build_rom.sh
@@ -1,18 +1,18 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/ForkLineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/LineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/ahmedhridoy371/local_manifest --depth 1 -b fork .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch lineage_lavender-userdebug
-export TARGET_FLOS=true
-export WITH_GMS=true
+
+
 export ALLOW_MISSING_DEPENDENCIES=true
 export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
 export BUILD_BROKEN_DUP_RULES=true
 export TZ=Asia/Jakarta #put before last build command
-make  bacon
+breakfast lavender
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
