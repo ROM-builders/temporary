66b2ee7b961ce49cdd39a20a369f3f07852f5882
Orkun Ergün <68orkun@proton.me>
2022-10-02 22:13:29 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,6 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch cherish_lava-userdebug
 export WITH_GMS=true
+export TARGET_USES_MINI_GAPPS=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
