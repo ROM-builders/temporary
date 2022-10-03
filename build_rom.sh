f6b2ade1f472196be964a5f8ce41f2c6f80aeb60
JuanTamadski <76487328+JuanTamadski@users.noreply.github.com>
2022-10-04 05:03:37 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b spark -g default,-mips,-darwin,-notdefault
-git clone https://github.com/JuanTamadski/local_manifest.git --depth 1 -b spark .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/JuanTamadski/local_manifest.git --depth 1 -b lineage-s .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch spark_Z01R-user
+lunch lineage_Z01R-user
 export TZ=Asia/Kolkata 
 export SELINUX_IGNORE_NEVERALLOWS=true
 mka bacon
