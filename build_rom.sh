cf5dc6a288e22283c61025471c3afb16c2980659
Spector <33796688+Spector0@users.noreply.github.com>
2022-10-03 14:42:46 +0530

    Use lunch instead

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-breakfast lineage_oscar-user
+lunch lineage_oscar-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
