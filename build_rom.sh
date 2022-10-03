cf02904272094a136ecc6f5459810e1e2792660c
anandhanajay <68692677+anandhanajay@users.noreply.github.com>
2022-10-03 23:36:22 +0530

    Miatoll-PE+-anandhanajay
    First build

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/PixelExperience/manifest -b eleven-plus -g default,-mips,-darwin,-notdefault
 git clone https://github.com/anandhanajay/local-manifest.git --depth 1 -b main .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
 # build rom
 . build/envsetup.sh
