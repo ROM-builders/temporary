79ccbc122218689743feb79d7a31717f477481fc
Spector <33796688+Spector0@users.noreply.github.com>
2022-10-03 11:47:45 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init -u https://github.com/LineageOS/android.git -b lineage-19.1 || true
+repo init -u https://github.com/LineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Spector0/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_oscar-user
+breakfast lineage_oscar-user
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
