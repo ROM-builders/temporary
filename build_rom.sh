9967a77507bf689ef079d7ff9500dcf59c023552
anandhanajay <68692677+anandhanajay@users.noreply.github.com>
2022-10-04 07:41:05 +0530

    miatoll-RiceDroid-twelve-anandhanajay
    My first rom ever

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,54 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/anandhanajay/local-manifest.git --depth 1 -b main .repo/local-manifest
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # build rom
-source build/envsetup.sh
+. build/envsetup.sh
 lunch derp_vayu-user
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+brunch "miatoll"
+# maintainer flag (one word only/no spacing)
+RICE_MAINTAINER := anandhanajay
+
+# chipset flag (one word only/no spacing)
+RICE_CHIPSET := sm6250
+
+# gapps build flag, if not defined build type is vanilla
+WITH_GMS := false
+
+# Sushi Bootanimation (only 720/1080p/1440 supported. if not defined, bootanimation is google bootanimation)
+SUSHI_BOOTANIMATION := 1080
+
+# Graphene Camera
+TARGET_BUILD_GRAPHENEOS_CAMERA := true
+
+# disable/enable blur support, default is false
+TARGET_ENABLE_BLUR := true
+
+# UDFPS ICONS/ANIMATIONS
+TARGET_HAS_UDFPS := true
+
+# Quick tap feature
+TARGET_SUPPORTS_QUICK_TAP := false
+
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Memory properties
+# Tune these properties according to device perfconfigstore.xml(if available)/device capabilities.
+# These props are overridable
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.fw.bg_apps_limit?=48 \
+    persist.sys.fw.use_trim_settings?=true \
+    persist.sys.fw.empty_app_percent?=50 \
+    persist.sys.fw.trim_empty_percent?=100 \
+    persist.sys.fw.trim_cache_percent?=100 \
+    persist.sys.fw.trim_enable_memory?=2147483648 \
+    persist.sys.fw.bservice_age?=120000 \
+    persist.sys.fw.bservice_limit?=6 \
+    persist.sys.fw.bservice_enable?=true 
+    
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
