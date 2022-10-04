bf3b1b57afa6960f50b28ac1f48b94cd4801b45e
zaidannn7 <zaidanmovic0512@gmail.com>
2022-10-05 04:10:07 +0700

    juice: fix up

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/lighthouse-os/manifest.git -b sailboat_L1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/zaidannn7/manifest.git -b sailboat_L1 -g default,-mips,-darwin,-notdefault
 git https://github.com/zaidannn7/local_manifest --depth 1 -b potato-test .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
