2cb6613eace1cefa3ca07602cb0412b5d10c2b8c
Raiden Ishigami <contact.raidenishi69@gmail.com>
2022-10-04 20:43:37 -0300

    Use minigapps

--- build_rom.sh
+++ build_rom.sh
@@ -5,6 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
+export TARGET_USES_MINI_GAPPS=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Dhaka
