3ff6fa31b5cec060c68ef99f9476967d95d2bff6
Matheus Souza <33396386+matheucomth@users.noreply.github.com>
2022-10-05 12:58:15 -0300

    fix props

--- build_rom.sh
+++ build_rom.sh
@@ -3,8 +3,8 @@ repo init --depth=1 --no-repo-verify -u ttps://github.com/PixelOS-Pixelish/manif
 git clone  https://github.com/matheucomth/local_manifest.git --depth 1 -b PixelOS .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
-. build/envsetup.sh
+# build roms
+. build/envssetup.sh
 lunch aosp_tulip-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
