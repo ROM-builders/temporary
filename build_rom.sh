68b5c6bab49eeb159f74bfab88c5003ca9635fd8
JuanTamadski <76487328+JuanTamadski@users.noreply.github.com>
2022-10-05 15:46:58 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch spark_Z01R-user
+lunch spark_Z01R-userdebug
 export TZ=Asia/Kolkata 
 export SELINUX_IGNORE_NEVERALLOWS=true
 mka bacon
