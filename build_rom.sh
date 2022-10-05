06fc9bca6251cd9063e98189c66623b7a75acceb
susandahal <susandahal53@gmail.com>
2022-10-05 20:15:41 +0545

    XdroidOSS thirteen

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-oss/xd_manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/susandahal/local_manifest --depth 1 -b xdroid .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-oss/xd_manifest -b thirteen -g default,-mips,-darwin,-notdefault
+git clone https://github.com/susandahal/local_manifest --depth 1 -b xdroid13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch xdroid_surya-userdebug
+lunch xdroid_surya-eng
 #export TZ=Asia/Dhaka #put before last build command
 mka xd
 
