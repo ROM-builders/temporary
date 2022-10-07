f2cfa4e82bb87107f5e8fd6d9e6e928b87b72f2c
hsx02 <hsx02self@gmail.com>
2022-10-07 09:55:59 +0530

    initial build

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/RiceDroid/android -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/hsx02/Local-Manifests.git --depth 1 -b rcd13 .repo/local_manifests
+git clone https://github.com/hsx02/Local-Manifests.git --depth 1 -b rice133 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
