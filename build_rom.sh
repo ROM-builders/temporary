5fc5ba49a177fd4001a2dad5275d605d42e6f398
Furkan Çakmak <87311803+furkancakmak34x@users.noreply.github.com>
2022-10-07 16:10:48 +0300

    taimen_pixelexperience_furkancakmak34x
    test

--- build_rom.sh
+++ build_rom.sh
@@ -9,5 +9,5 @@ lunch aosp_taimen-userdebug
 export TZ=Europe/Istanbul
 mka bacon -j8
 
-# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+# upload rom (if you don't need to upload multiple files, then you don't need to edit next line.)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
