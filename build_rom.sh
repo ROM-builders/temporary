81c5ad84e18f7f49e396e2a94c3a9290923cba2c
Matheus Souza <33396386+matheucomth@users.noreply.github.com>
2022-10-07 19:12:52 -0300

    twolip: rebase tree

--- build_rom.sh
+++ build_rom.sh
@@ -3,9 +3,9 @@ repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manif
 git clone https://github.com/matheucomth/local_manifest.git --depth 1 -b PixelOS .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build roms
+# build rom
 . build/envsetup.sh
-lunch aosp_tulip-userdebug
+lunch aosp_twolip-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export RELAX_USES_LIBRARY_CHECK=true
