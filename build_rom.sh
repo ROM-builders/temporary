cc11e2902d8d66ccff02580f687e9481ac2e770e
prango71 <101040779+prango71@users.noreply.github.com>
2022-10-08 19:40:10 +0530

    Initial xd 13

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,7 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-oss/xd_manifest -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/datnerdguy/local_manifest --depth 1 -b xdroid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
+#
 # build rom
 source build/envsetup.sh
 export KBUILD_BUILD_USER=datnerdguy
