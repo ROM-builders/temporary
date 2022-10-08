e8371ae15cc638584c3d0724c05f39d4c8bb1fe2
Anandhasayanan <anandhasayanan007jb@gmail.com>
2022-10-08 15:14:46 +0530

    Fix kernel error

--- build_rom.sh
+++ build_rom.sh
@@ -3,12 +3,12 @@ repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b 
 git clone https://github.com/anandhan07/local_manifest.git --depth 1 -b ricedroid-13.0 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom [3]
+# build rom [4]
 . build/envsetup.sh
 lunch lineage_vince-user
 export WITH_GMS=true
 export TZ=Asia/Dhaka #put before last build command
-m bacon
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
