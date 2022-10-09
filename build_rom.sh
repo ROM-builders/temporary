8bbcdeecb4dc1625cf13f872615f5fc0ff82b968
ZualoliconVN <94312960+ZualoliconVN@users.noreply.github.com>
2022-10-09 20:16:44 +0700

    Griffin-ResurrectionRemix-Q-ZualoliconVN

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ResurrectionRemix/platform_manifest.git -b Q -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ZualoliconVN/local_manifest --depth 1 -b ProjectSakura .repo/local_manifests
+git clone https://github.com/ZualoliconVN/local_manifest --depth 1 -b Griffin-ResurrectionRemix-Q-ZualoliconVN .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -8,7 +8,6 @@ source build/envsetup.sh
 lunch rr_griffin-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
-
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
