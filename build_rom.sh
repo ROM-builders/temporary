ff8987ded836f5ed4bc4d3a12c2d9e3a37e30451
AllMight007 <86153221+AllMight007@users.noreply.github.com>
2022-10-09 11:10:34 +0530

    sweet-Spark-Rom-AllMight007

--- build_rom.sh
+++ build_rom.sh
@@ -1,27 +1,12 @@
 # sync rom
-#repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-#git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
-#repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b pyro -g default,-mips,-darwin,-notdefault
 git clone https://github.com/phoenix-1708/local_manifest.git --depth=1 -b spark .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-
-
 # build rom
-lunch spark_sweet-userdebug
 
-
-export ALLOW_MISSING_DEPENDENCIES=true
-
-export SKIP_ABI_CHECKS=true
-
-export SKIP_API_CHECKS=true
-
-export TZ=Asia/Dhaka #put before last build command
-
-mka bacon
+source build/envsetup.sh
 lunch spark_sweet-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
 export SKIP_ABI_CHECKS=true
