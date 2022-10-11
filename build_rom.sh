4499a6c50776eb31c62100a62176279d299cda8f
Nishant <82361039+Nishant9947@users.noreply.github.com>
2022-10-11 06:56:03 +0530

    Fixed duplicate

--- build_rom.sh
+++ build_rom.sh
@@ -8,7 +8,7 @@ source build/envsetup.sh
 lunch evolution_X01AD-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
-export TZ=Asia/Kolkata #put before last build command
+export TZ=Asia/Kolkata #put  before last build command
 mka evolution
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
