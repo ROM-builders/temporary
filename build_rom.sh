05174878b980d437d85dda91ddcae5fe8de42095
Futar0 <104120854+Futar0@users.noreply.github.com>
2022-10-11 20:32:28 +0800

    lava-Bootleggers-BrokenLab-sambunimbo-Futar0

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 11.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Futar0/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Bootleggers-BrokenLab/manifest.git -b sambunimbo -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Futar0/local_manifest.git --depth 1 -b bootleggers .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # build rom
 source build/envsetup.sh
-lunch lineage_lava-userdebug
+lunch bootleg_lava-userdebug
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
