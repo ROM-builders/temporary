8fc9b17619966492e5d544b274bb2edf082f15ee
Abhishek Hembrom <76091481+abhishekhembrom08@users.noreply.github.com>
2022-10-11 15:31:24 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
-# sync rom
+# sync rom 
 repo init --depth=1 --no-repo-verify -u https://github.com/CipherOS/android_manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/abhishekhembrom08/manifest_local.git --depth 1 -b blazeGinkgo .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom 
 source build/envsetup.sh
 lunch cipher_ginkgo-userdebug
 export TZ=Asia/Kolkata #put before last build command
