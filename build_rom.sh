25d1558bcd78d6c318e3205d4e9d537b8ec99ea5
Futar0 <104120854+Futar0@users.noreply.github.com>
2022-10-12 04:47:04 +0800

    lava-Havoc-OS-eleven-Futar0

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Bootleggers-BrokenLab/manifest.git -b sambunimbo -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Futar0/local_manifest.git --depth 1 -b bootleggers .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Havoc-OS/android_manifest.git -b eleven -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Futar0/local_manifest.git --depth 1 -b havoc .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # build rom
 source build/envsetup.sh
-lunch bootleg_lava-userdebug
+lunch havoc_lava-userdebug
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
