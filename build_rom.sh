df42ecad913c3b659e0eef3927f425470ffcdaaf
ZualoliconVN <94312960+ZualoliconVN@users.noreply.github.com>
2022-10-12 22:32:24 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/IDN-Labs/platform_manifest.git -b Q -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/ResurrectionRemix-Revived/platform_manifest.git -b Q -g default,-mips,-darwin,-notdefault
 git clone https://github.com/ZualoliconVN/local_manifest --depth 1 -b griffin-ResurrectionRemix-Q-ZualoliconVN .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
