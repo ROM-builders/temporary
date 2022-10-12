f2066d68724090aeb59bf2f564d7c1fc93f7efda
tejassinggh <109585789+tejassinggh@users.noreply.github.com>
2022-10-12 23:30:18 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/tejassinggh/Local_Manifest.git --depth 1 -b awaken_ginkgo .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/tejassinggh/Local_Manifest.git --depth 1 -b blaze_ginkgo .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch awaken_ginkgo-userdebug
+lunch blaze_ginkgo-userdebug
 export TZ=Asia/Delhi #put before last build command
 make bacon
 
