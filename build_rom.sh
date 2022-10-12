51169002f4bcdb0288a508594e07026ddaa7f291
tejassinggh <109585789+tejassinggh@users.noreply.github.com>
2022-10-12 10:51:59 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/tejassinggh/LocalManifest --depth 1 -b awaken_ginkgo .repo/local_manifests
+git clone https://github.com/tejassinggh/Local_Manifest.git --depth 1 -b awaken_ginkgo .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
