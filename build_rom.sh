4084df4b51c80987b1938b17d73cb4f708e833a1
GhostMaster69-dev <rathore6375@gmail.com>
2022-10-12 13:26:42 +0530

    Build Gapps with fixes

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-#export ARROW_GAPPS=true
+export ARROW_GAPPS=true
 lunch arrow_vince-user
 export TZ=Asia/Kolkata #put before last build command
 m bacon
