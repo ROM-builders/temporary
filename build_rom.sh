bf717f6ebbe7fa708b0aafdc7d0cc3f9fbb5378a
Abhishek Hembrom <76091481+abhishekhembrom08@users.noreply.github.com>
2022-10-12 08:39:07 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
-# sync rom 
+# sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b triton -g default,-mips,-darwin,-notdefault
 git clone https://github.com/abhishekhembrom08/manifest_local.git --depth 1 -b blazeGinkgo .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom 
+# build rom
 source build/envsetup.sh
 lunch awaken_ginkgo-userdebug
 export TZ=Asia/Kolkata #put before last build command
