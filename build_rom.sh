d98c8a0ad33c0469a9367d364b22c65e94a25cef
Julival Bittencourt <53379692+bittencourtjulival@users.noreply.github.com>
2022-10-13 11:20:11 -0300

    Bringup Rice13

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,15 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/MiDoNaSR545/mainfest_personal -b los --depth 1 -b master .repo/local_manifests
+git clone https://github.com/bittencourtjulival/local_manifest -b 13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-export TZ=Asia/Dhaka #put before last build command
-brunch dream2lte
+export TZ=America/Sao_Paulo #put before last build command
+export WITH_GMS=true
+brunch polaris
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+
+
