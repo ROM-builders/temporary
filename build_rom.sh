7a31752e3ab8070015969959deab5364fbf7cf05
Abhishek Hembrom <76091481+abhishekhembrom08@users.noreply.github.com>
2022-10-13 07:00:08 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom 
-repo init --depth=1 --no-repo-verify -u https://github.com/CipherOS/android_manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b triton -g default,-mips,-darwin,-notdefault
 git clone https://github.com/abhishekhembrom08/manifest_local.git --depth 1 -b blazeGinkgo .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch cipher_ginkgo-userdebug
+lunch awaken_ginkgo-userdebug
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
