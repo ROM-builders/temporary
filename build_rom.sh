9152f0b46a33d115dc9409aa8bd73a7451fdcb47
Goku <100507815+god-goku@users.noreply.github.com>
2022-10-15 01:34:06 +0530

    Updated source

--- build_rom.sh
+++ build_rom.sh
@@ -3,13 +3,14 @@ repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_man
 git clone https://github.com/god-goku/local-manifest.git --depth 1 -b cherry .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-#40
+#41
 # build rom
 . build/envsetup.sh
 export TZ=Asia/Dhaka #put before last build command
 export BUILD_USER=Monu
 export BUILD_HOST=cloud
 export BUILD_USERNAME=Monu
+export BUILD_HOSTNAME=cloud
 brunch veux
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
