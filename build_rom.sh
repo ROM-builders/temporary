136daf834568999d57f118a12ad9ee9d27bc0d6c
Christopher Keith Irvine <Mizdrake7@gmail.com>
2022-10-14 16:25:18 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -15,7 +15,6 @@ export SKIP_ABI_CHECKS=true
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export USE_DEXOPT=true
 export CHERISH_VANILLA := true
-
 export TZ=Asia/Kolkata #put before last build command
 brunch r5x
 
