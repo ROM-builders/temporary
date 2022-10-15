d92285cb606181ea52fd7bff369648ed5066b926
Matheus Souza <33396386+matheucomth@users.noreply.github.com>
2022-10-15 18:41:24 -0300

    Bump to kernel 4.19

--- build_rom.sh
+++ build_rom.sh
@@ -3,13 +3,15 @@ repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manif
 git clone https://github.com/matheucomth/local_manifest.git --depth 1 -b PixelOS .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build roms
+# build rom
 . build/envsetup.sh
 lunch aosp_tulip-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export RELAX_USES_LIBRARY_CHECK=true
-export TZ=Asia/Dhaka #put before last build command
+export TZ=Asia/Dhaka 
+
+#put before last build command
 make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
