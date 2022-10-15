49b80ce0b1bdffd1a959aa9459a8f5e55cd0064f
Julival Bittencourt <53379692+bittencourtjulival@users.noreply.github.com>
2022-10-15 13:08:29 -0300

    Polaris: build riceDroid userdebug

--- build_rom.sh
+++ build_rom.sh
@@ -5,6 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
+lunch lineage_tissot-userdebug
 export TZ=America/Sao_Paulo #put before last build command
 export ALLOW_MISSING_DEPENDENCIES=true
 export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
