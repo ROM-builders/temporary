b3f58fdd43c239ad0a31a404671ecec57dc83b55
xack21 <86214526+xack21@users.noreply.github.com>
2022-10-15 12:44:02 +0530

    Mi8937-CherishOS-tiramisu-xack21

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/xack21/local_manifest.git --depth 1 -b master .repo
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+. build/envsetup.sh
 export TZ=Asia/Dhaka #put before last build command
 brunch Mi8937
 
