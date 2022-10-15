0bc1e7c5ae0f3b128e871ca802f9f3ce3eb92234
baconpeedit <109585789+baconpeedit@users.noreply.github.com>
2022-10-15 23:09:34 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/tejassinggh/Local_Manifest.git --depth 1 -b lineage_ginkgo .repo/local_manifests
+git clone https://github.com/baconpeedit/Local_Manifest.git --depth 1 -b lineage_ginkgo .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
