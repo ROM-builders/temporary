001709247becbfcd0f5c82adff9d47b196a58591
xack21 <86214526+xack21@users.noreply.github.com>
2022-10-16 12:09:32 +0530

    Mi8937-CherishOS-tiramisu-xack21

--- build_rom.sh
+++ build_rom.sh
@@ -6,8 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 export TZ=Asia/Dhaka #put before last build command
-lunch cherish_Mi8937-userdebug
-mka bacon
+brunch Mi8937
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
