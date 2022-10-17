ad7ccca89a43d74cf15285d2bd39b988bbfd4b19
Abhishek Hembrom <76091481+abhishekhembrom08@users.noreply.github.com>
2022-10-17 07:09:51 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom
+# sync rom 
 repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-13.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/abhishekhembrom08/manifest_local.git --depth 1 -b rova-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
