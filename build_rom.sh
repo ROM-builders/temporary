cb2ede460725f86b40a4c35399674bc358f31e39
Abhi <78593493+yash1375@users.noreply.github.com>
2022-10-17 19:03:15 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/xdroid-oss/xd_manifest -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/yash1375/local_manifest.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/yash1375/local_manifest.git --depth 1 -b a13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
