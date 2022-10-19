6741c8d6001752dd88adefc5ff43fe9d13c9467d
NightlyHorny <80430167+NightlyHorny@users.noreply.github.com>
2022-10-18 22:34:43 -0500

    Update

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ForkLineageOS/android.git -g default,-mips,-darwin,-notdefault
 git clone https://github.com/kitchen-at-night/local_manifest.git --depth 1 .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j
 
 # build rom
 source build/envsetup.sh
