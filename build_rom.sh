57c2328e2a6c2bc79a09ce3ccde2cb7be9ad6a6d
ArcG <115998816+Arctic-G@users.noreply.github.com>
2022-10-19 15:15:22 +0000

    Make it vanilla, baby!

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch pixys_spes-userdebug
-export WITH_GMS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
 export BUILD_BROKEN_DUP_RULES=true
