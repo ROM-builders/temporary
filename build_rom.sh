86d32ab6f90c05837017c162ce9a33d3221b96a2
Abhishek Hembrom <76091481+abhishekhembrom08@users.noreply.github.com>
2022-10-19 17:05:01 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 #export SELINUX_IGNORE_NEVERALLOWS=true
-export ALLOW_MISSING_DEPENDENCIES=true
+#export ALLOW_MISSING_DEPENDENCIES=true
 lunch arrow_rova-userdebug
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
