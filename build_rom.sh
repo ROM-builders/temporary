0aff47e3f35c95c4236d83e52cbe7a270f381ab1
Heck234 <110883156+Heck234@users.noreply.github.com>
2022-10-20 12:05:14 +0000

    r5x-RiceDroid-thirteen-Heck234
    build ricedroid for r5x

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u http://github.com/https://RiceDroid/Android -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Heck234/local_manifest --depth 1 -b RiceDroid .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetcr5x-RiceDroid-thirteen-Heck234h --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
