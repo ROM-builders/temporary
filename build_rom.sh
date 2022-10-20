b986e7d74ebbd7717a2e9d42dcd3fa71cbbcad67
nayganteng <115395427+nayganteng@users.noreply.github.com>
2022-10-21 04:39:29 +0700

    build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
  
 # build rom 
 source build/envsetup.sh  
-lunch blaze_rmx2185-userdebug
+lunch blaze_RMX2185-userdebug
 export TZ=Asia/Jakarta #put before last build command    
 mka bacon 
  
