6dd5213196580f0fcccfa6b029d28c392a748093
ArcG <115998816+Arctic-G@users.noreply.github.com>
2022-10-20 12:50:40 +0000

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,11 +6,12 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch pixys_spes-userdebug
+#export BUILD_WITH_GAPPS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
 export BUILD_BROKEN_DUP_RULES=true
 export TZ=Asia/Ho_Chi_Minh #put before last build command
-make pixys
+mka pixys
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
