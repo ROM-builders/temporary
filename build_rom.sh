b4d589282ee25733a3783bb8ba38d4834755fb68
ArcG <115998816+Arctic-G@users.noreply.github.com>
2022-10-21 05:49:56 +0000

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,17 +1,16 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixysOS/manifest.git -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Arctic-G/local-manifest.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/Arctic-G/Local-Manifests.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch pixys_spes-userdebug
-#export BUILD_WITH_GAPPS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
 export BUILD_BROKEN_DUP_RULES=true
 export TZ=Asia/Ho_Chi_Minh #put before last build command
-mka pixys
+make pixys
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
