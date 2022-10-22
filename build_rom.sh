52c834e895d481d83efdfac0218369f9dd441826
Rishawn <70256146+Stealth1226@users.noreply.github.com>
2022-10-22 22:26:25 +0530

    initial topaz build

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 -u https://github.com/AOSPA/manifest.git -b sapphire -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pa-stelt/local_manifest --depth 1 -b sapphire .repo/local_manifests
+repo init --depth=1 -u https://github.com/AOSPA/manifest.git -b topaz -g default,-mips,-darwin,-notdefault
+git clone https://github.com/pa-stelt/local_manifest --depth 1 -b topaz .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
