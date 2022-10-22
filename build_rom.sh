0c7fc09ca2b2cde5a4f0f3a6723e18e878bbdfa9
Rahif M <rahifmanjatha372@gmail.com>
2022-10-22 14:37:52 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,8 @@ git clone https://gitlab.com/the-muppets/proprietary_vendor_xiaomi -b lineage-18
 # build rom
 source build/envsetup.sh
 export TZ=Asia/Dhaka #put before last build command
-brunch beryllium
+lunch lineage_beryllium-user
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
