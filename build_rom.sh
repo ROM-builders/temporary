59e5abb9e55a9e14ed375305b32f5dd07912ad2d
Don boruza <109259258+donboruza@users.noreply.github.com>
2022-10-23 01:43:55 +0700

    build: fix wrong source rom

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b spark -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/donboruza/local_manifests.git --depth 1 -b sweet .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
