9ef16533d69884ea0ac93df964d2244895cb3b02
zaidannn7 <zaidanmovic0512@gmail.com>
2022-10-22 12:49:45 +0700

    Initial Ricedroid Build (some fixes)

--- build_rom.sh
+++ build_rom.sh
@@ -12,8 +12,8 @@ export BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES=true
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
 export BUILD_BROKEN_DUP_RULES=true
-brunch chime
 export TZ=Asia/Jakarta #put before last build command
+brunch chime
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
