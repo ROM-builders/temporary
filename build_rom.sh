864e18d171898a369e0dbdc8f1da70ede99b8736
ahmedhridoy371 <40537458+ahmedhridoy371@users.noreply.github.com>
2022-10-23 12:03:27 +0600

    server Error

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/ForkLineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/ForkLineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/ahmedhridoy371/local_manifest --depth 1 -b fork .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
