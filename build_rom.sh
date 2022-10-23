8277568e22afe3c9a84194c72e2ddb877f1f6ac7
Orkun Ergün <68orkun@proton.me>
2022-10-23 21:30:04 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch lineage_lava-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
-export TZ=Asia/Dhaka #put before last build command
+export TZ=Asia/Dhaka
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
