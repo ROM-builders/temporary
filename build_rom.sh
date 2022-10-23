324fe7277bd23389a0bd9b0caf87e5d1dc4db3e1
zaidanprjkt <72336864+zaidannn7@users.noreply.github.com>
2022-10-23 15:49:32 +0700

    juice: fix derp

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/CarbonROM/android.git -b cr-9.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/zaidannn7/local_manifestgit --depth 1 -b carbon .repo/local_manifests
+git clone https://github.com/zaidannn7/local_manifest.git --depth 1 -b carbon .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
