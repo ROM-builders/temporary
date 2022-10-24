9c8d0e051a132b87569beed20e9ef39d66c214eb
baconpeedit <109585789+baconpeedit@users.noreply.github.com>
2022-10-25 01:20:02 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/baconpeedit/Local_Manifest.git --depth 1 -b blazeginkgo .repo/local_manifests
+git clone https://github.com/baconpeedit/Local_Manifest.git --depth 1 -b blaze13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
