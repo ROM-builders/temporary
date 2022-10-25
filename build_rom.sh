1f46c50a2580b4d9b90528d67b479e2598c56ced
Matheus Souza <33396386+matheucomth@users.noreply.github.com>
2022-10-25 16:56:22 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
 git clone https://github.com/theAugust99/local_manifest.git --depth 1 -b lineage-20.0 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch  evolution_nairo-userdebug
+lunch evolution_nairo-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka evolution
 
