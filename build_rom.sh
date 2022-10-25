e76f6d0358858caee97c93ed4d378f71b656e11c
swapnil vicky <74442290+SwapnilVicky@users.noreply.github.com>
2022-10-25 20:07:20 +0530

    switch to sailboat kernel

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/BlissRoms/platform_manifest.git -b arcadia-next -g default,-mips,-darwin,-notdefault
-git clone https://github.com/SwapnilVicky/local_manifest.git --depth 1 -b bliss .repo/local_manifests
+git clone https://github.com/SwapnilVicky/local_manifest.git --depth 1 -b arrow .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
