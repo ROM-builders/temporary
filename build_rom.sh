d968c1756d0f90428e3a62a9bafcaa48d27ecb42
Arafattex <69374795+Arafattex@users.noreply.github.com>
2022-10-26 09:59:02 +0600

    For pex

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExtended/manifest -b trece -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Arafattex/local_manifest.git --depth 1 -b Lancelot_pixel .repo/local_manifests
+git clone https://github.com/Arafattex/local_manifest.git --depth 1 -b Lancelot_pex .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
