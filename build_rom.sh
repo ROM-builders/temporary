5cf4f1522fcccae023768cb8ae5df42819bd5b09
ZualoliconVN <94312960+ZualoliconVN@users.noreply.github.com>
2022-10-27 12:32:07 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ZualoliconVN/local_manifest.git --depth 1 -b cherish .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/darknius09/platform_manifest.git -b Q -g default,-mips,-darwin,-notdefault
+git clone https://github.com/ZualoliconVN/local_manifest.git --depth 1 -b rr .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -15,7 +15,8 @@ export SKIP_ABI_CHECKS=true
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export USE_DEXOPT=true
 export TZ=Asia/Kolkata #put before last build command
-brunch r5x
+lunch rr_griffin-userdebug
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
