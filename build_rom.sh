dfa5b49c504ffe723af676f35a5a31bc678e0d11
ZualoliconVN <94312960+ZualoliconVN@users.noreply.github.com>
2022-10-27 23:44:21 +0700

    r5x-CherishOS-tiramisu-ZualoliconVN

--- build_rom.sh
+++ build_rom.sh
@@ -1,8 +1,7 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ResurrectionRemix/platform_manifest -b Q -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ZualoliconVN/local_manifest.git --depth 1 -b rr .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
+git clone https://github.com/ZualoliconVN/local_manifest.git --depth 1 -b cherish .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
 
 # build rom
 source build/envsetup.sh
@@ -13,11 +12,9 @@ export BUILD_USERNAME=ZualoliconVN
 export BUILD_HOSTNAME=ZualoliconVN
 export RELAX_USES_LIBRARY_CHECK=true
 export SKIP_ABI_CHECKS=true
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export USE_DEXOPT=true
 export TZ=Asia/Kolkata #put before last build command
-lunch rr_griffin-userdebug
-mka bacon
+brunch r5x
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
