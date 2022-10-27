ff0f089536845c3e8422f41e2fc1b9ab29a956f2
ZualoliconVN <94312960+ZualoliconVN@users.noreply.github.com>
2022-10-27 13:05:24 +0700

    griffin-ResurrectionRemix-Q-ZualoliconVN

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/darknius09/platform_manifest.git -b Q -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/darknius09/rr_manifest -b Q -g default,-mips,-darwin,-notdefault
 git clone https://github.com/ZualoliconVN/local_manifest.git --depth 1 -b rr .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
