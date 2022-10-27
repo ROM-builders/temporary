8f0355720b6d056c5767700278ebe1db5472f53c
Mozzaru <100791931+Mozzaru@users.noreply.github.com>
2022-10-28 05:43:35 +0700

    Derpfest 12.1
    Buildd

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-12/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Mozzaru/local_manifest.git --depth 1 -b master .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-12/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Mozzaru/local_manifest.git --depth 1 -b Derpfest-12.1 .repo/local_manifests
+repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
 
 # build rom
 source build/envsetup.sh
 lunch derp_markw-user
-export TZ=Asia/Dhaka #put before last build command
+export TZ=Asia/ID #put before last build command
 mka derp
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
