8fc9770b5a39cbb532f421040071a173272ea8a1
Tanmsyffa <sltnmsyffaa@gmail.com>
2022-10-27 19:19:33 +0700

    Use our xd_manifest.

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-oss/xd_manifest -b thirteen -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Tansmsyffa/xd_manifest -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Tanmsyffa/local_manifest.git --depth 1 -b xd-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
