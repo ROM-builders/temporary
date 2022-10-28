e584d10e2bb64d8fcbf9f8ff617b64d4472ef5a6
rianza <116883839+rianza@users.noreply.github.com>
2022-10-28 19:43:43 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage-markw-user
+lunch lineage_markw-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
