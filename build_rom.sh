57aedec11300337b7de9519dd09dc2bbe9d2ccea
ahmedhridoy371 <40537458+ahmedhridoy371@users.noreply.github.com>
2022-10-29 01:40:56 +0600

    trying again

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/RiceDroid/android -b thirteen -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/ahmedhridoy371/local_manifest --depth 1 -b rice13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
