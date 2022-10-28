d62ec8f9c58b1335e72d1f4151ce46e80c2f0220
Abhishek Hembrom <76091481+abhishekhembrom08@users.noreply.github.com>
2022-10-28 05:32:55 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch awaken_ginkgo-userdebug
+export TARGET_KERNEL_CLANG_VERSION=proton
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
