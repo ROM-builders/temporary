1080982b25904229ceec16e617ad87a887a63d24
Mozzaru <100791931+Mozzaru@users.noreply.github.com>
2022-10-28 07:03:04 +0700

    Ricedroid 12.1
    Flezzz

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,16 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-12/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Mozzaru/local_manifest.git --depth 1 -b Derpfest-12.1 .repo/local_manifests
-repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
+repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Mozzaru/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_markw-user
+lunch lineage-markw-userdebug
+export BUILD_USERNAME=flezz.   
+
+export BUILD_HOSTNAME=prototype
 export TZ=Asia/ID #put before last build command
-mka derp
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
