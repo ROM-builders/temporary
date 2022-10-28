218adc5b63a13197bdc3841707ece3a2d3d3bd70
Rahul Gorai <rahulgorai0206@gmail.com>
2022-10-28 13:18:13 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-#lunch cherish_beryllium-user
+lunch cherish_beryllium-userdebug
 export TZ=Asia/Kolkata #put before last build command
 brunch beryllium
 
