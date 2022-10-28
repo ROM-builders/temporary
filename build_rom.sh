676659c2d13e47cde61bcdeef7c65984fe255e26
AndVer2 <72114405+AndVer2@users.noreply.github.com>
2022-10-28 07:50:00 +0100

    m10lte-LineageOS-lineage-18.1-AndVer2

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
-# sync rom
+# sync roms
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android -b lineage-18.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/AndVer2/android_manifest_samsung_m10lte.git --depth 1 -b lineage-18.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build roms
 source build/envsetup.sh
 lunch lineage_m10lte-userdebug
 export TZ=Europe/London #put before last build command
