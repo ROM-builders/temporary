a36e2bbc75309aec2e8a7d62dd1d15d55718fcb5
Mozzaru <100791931+Mozzaru@users.noreply.github.com>
2022-10-28 17:19:08 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,12 @@
 # sync rom 
-repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b twelve -g default,-mips,-darwin,-notdefault 
-git clone https://github.com/Mozzaru/local_manifest.git --depth 1 -b main .repo/local_manifests 
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Mozzaru/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
  
 # build rom 
 source build/envsetup.sh 
-lunch lineage_msrkw-userdebug 
-export BUILD_USERNAME=zax
-
-export BUILD_HOSTNAME=prototype
-
+lunch lineage_markw-userdebug 
 export TZ=Asia/Jakarta 
 mka bacon  
  
