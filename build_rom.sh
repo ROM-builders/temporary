61b8b43be93e194c202d88f5ef0323a99af25e32
Mozzaru <100791931+Mozzaru@users.noreply.github.com>
2022-10-29 07:19:10 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,15 @@
 # sync rom 
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-13.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Mozzaru/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
  
 # build rom 
-source build/envsetup.sh 
-lunch lineage_markw-userdebug 
+source build/envsetup.sh
+export ARROW_GAPPS=true
+lunch arrow_arkw-userdebug 
 export TZ=Asia/Jakarta 
-mka bacon  
+m bacon  
  
   
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
