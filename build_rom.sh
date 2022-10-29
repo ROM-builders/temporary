555f9f5113182f1260c73026be0d9325f82569e0
Christopher Keith Irvine <Mizdrake7@gmail.com>
2022-10-30 03:29:13 +0530

    Now want gaypps

--- build_rom.sh
+++ build_rom.sh
@@ -15,7 +15,6 @@ export SKIP_ABI_CHECKS=true
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export USE_DEXOPT=true
 export TZ=Asia/Kolkata #put before last build command
-export CHERISH_VANILLA=true
 brunch r5x
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
