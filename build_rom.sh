4eda17cebf9b616113604167d9469e08a2004f53
Anandhasayanan <anandhasayanan007jb@gmail.com>
2022-10-29 06:55:17 +0530

    Revert changes for now

--- build_rom.sh
+++ build_rom.sh
@@ -3,10 +3,11 @@ repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b 
 git clone https://github.com/anandhan07/local_manifest.git --depth 1 -b ricedroid-13.0 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom [26]
+# build rom [27]
 . build/envsetup.sh
 lunch lineage_vince-user
 export WITH_GMS=false
+export SKIP_ABI_CHECKS=true
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
