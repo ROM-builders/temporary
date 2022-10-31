c50f69a6b9bdab61795f3108a8e38c66d599ab6c
Arafattex <69374795+Arafattex@users.noreply.github.com>
2022-10-31 09:03:26 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,8 +1,6 @@
 # sync rom 
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault
 git -C .repo/manifests fetch --unshallow
-git -C .repo/manifests revert 670c209ba91d2f5ec0dab84ed2669cde71d35997
-git -C .repo/manifests revert 031f9849ea87ac1d784de96a2b8cd4322082e2f1
 git clone https://github.com/Arafattex/local_manifest.git --depth 1 -b Lancelot_los .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
