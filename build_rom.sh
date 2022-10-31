45b7d96f1476a4c03a133466579aef12f76e3327
baconpeedit <tejassingh649@gmail.com>
2022-10-31 19:50:39 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/tejassinggh/Local_Manifest.git --depth 1 -b awaken_ginkgo .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b thirteen -g default,-mips,-darwin,-notdefault
+git clone https://github.com/baconpeedit/Local_Manifest.git --depth 1 -b rice13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch awaken_ginkgo-userdebug
+lunch lineage_ginkgo-userdebug
 export TZ=Asia/Delhi #put before last build command
 make bacon
 
