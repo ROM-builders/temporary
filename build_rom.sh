07a67d524f06eb998bf55a4474bc4484283a8148
Arafattex <69374795+Arafattex@users.noreply.github.com>
2022-10-31 12:08:31 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom 
+# sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Arafattex/local_manifest.git --depth 1 -b Lancelot_blaze .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
