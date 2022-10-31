1497dbb0824a248b84b6f153bda1563ff83f350a
Arafattex <69374795+Arafattex@users.noreply.github.com>
2022-10-31 15:19:30 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
-#lunch blaze_lava-userdebug
 export TZ=Asia/Dhaka #put before last build command
 brunch lava
 
