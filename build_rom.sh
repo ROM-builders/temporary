493068cc610cdef17948a88c53f1510751a0bb0f
Sachin9822 <70788804+Sachin9822@users.noreply.github.com>
2022-11-01 19:13:56 +0530

    Test

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/PixelExperience/manifest -b thirteen -g default,-mips,-darwin,-notdefault 
-git clone https://github.com/Sachin9822/local_manifest.git --depth 1 -b pixel .repo/local_manifests 
+repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 13 -g default,-mips,-darwin,-notdefault 
+git clone https://github.com/Sachin9822/local_manifest.git --depth 1 -b blaze .repo/local_manifests 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
  
-# build rom  
-source build/envsetup.sh  
-lunch aosp_tissot-eng
+# build rom   
+source build/envsetup.sh   
+lunch blaze_tissot-userdebug
 export TZ=Asia/Dhaka #put before last build command   
 mka bacon   
  
