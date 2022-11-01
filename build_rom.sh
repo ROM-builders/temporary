be6bd93c51d8a65ac873cad26d01270b810d9655
JuanTamadski <76487328+JuanTamadski@users.noreply.github.com>
2022-11-01 09:40:47 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PotatoProject/manifest -b frico_mr1-release -g default,-mips,-darwin,-notdefault
-git clone https://github.com/JuanTamadski/local_manifest.git --depth 1 -b posp .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Bootleggers-BrokenLab/manifest.git -b tirimbino -g default,-mips,-darwin,-notdefault
+git clone https://github.com/JuanTamadski/local_manifest.git --depth 1 -b btl .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch potato_Z01R-userdebug
+lunch bootleg_Z01R-userdebug
 export TZ=Asia/Kolkata 
 mka bacon
 
