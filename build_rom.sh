11361bcd39436e1938be7f88eb6abb76b4429a8b
Arafattex <69374795+Arafattex@users.noreply.github.com>
2022-11-01 20:38:20 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,13 @@
 # sync rom 
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault
-git -C .repo/manifests fetch --unshallow
-git -C .repo/manifests reset --hard 5646a37199c90d887663b8af6bcafeebad854862
-git clone https://github.com/Arafattex/local_manifest.git --depth 1 -b Lancelot_los .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PotatoProject/manifest -b gnocchi-release -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Arafattex/local_manifest.git --depth 1 -b Lancelot_potato .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
-lunch lineage_lava-userdebug
 export TZ=Asia/Dhaka #put before last build command
-mka bacon
+brunch lava
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
