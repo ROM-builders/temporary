792eb1ec11e88043ba992e5fb30551bccd571bf9
Wayne <99174654+ugly-saifu@users.noreply.github.com>
2022-11-01 12:15:08 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/DotOS/manifest.git -b dot12.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/ugly-saifu/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch dot_X01AD-userdebug
+lunch cherish_X01AD-userdebug
 export TZ=Asia/Dhaka #put before last build command
-make bacon
+brunch X01AD
 export ALLOW_MISSING_DEPENDENCIES=true
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
