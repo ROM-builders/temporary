37144f85d9d9e45f07b6b3ca2037ccd7f21f6604
Dooms-v <109235336+Dooms-v@users.noreply.github.com>
2022-11-03 21:25:47 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/syberia-project/manifest.git -b 13.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Dooms-v/local-manifest.git --depth 1 -b rice-13 .repo/local_manifests
+git clone https://github.com/Dooms-v/local-manifest.git --depth 1 -b syberia-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
