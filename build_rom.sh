8f9e6b96dd2cfd5699b21301faa049729761dcef
qwertyuiii-code <85434511+qwertyuiii-code@users.noreply.github.com>
2022-11-03 21:45:09 +0300

    а

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ git clone https://github.com/qwertyuiii-code/local_manifest.git --depth 1 -b mai
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 
-# build rom
+# build  rom
 . build/envsetup.sh
 export TZ=Asia/Yekaterinburg
 lunch cherish_chime-userdebug
