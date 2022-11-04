c63fff627eb43b7d7b24c11947e647b6497918dd
Sachin752002 <103373100+Sachin752002@users.noreply.github.com>
2022-11-04 15:09:05 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 export SELINUX_IGNORE_NEVERALLOWS=true
+export BUILD_BROKEN_DUP_RULES=true
 brunch X01AD
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
