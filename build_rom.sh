a641ce5cfb9151535fab795811ce9555a6da4ddd
criperm19 <96837536+CreeperAmigo19@users.noreply.github.com>
2022-11-04 09:05:31 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch lineage_A001D-userdebug
+lunch carbon_A001D-userdebug
 export TZ=America/Sao_Paulo
 export SELINUX_IGNORE_NEVERALLOWS=true
 mka bacon
