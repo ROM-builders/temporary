9b65220ea2536ee9879112fd38e87e03d9f33367
xack21 <86214526+xack21@users.noreply.github.com>
2022-11-04 19:55:32 +0530

    land-PixelExperience-thirteen-xack

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b thirteen -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/xack21/manifest -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/xack21/local_manifest-1.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
