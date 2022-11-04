44fe72e28062e2b2fc92664ce0f3b93a3a9852c6
nayproject <115395427+nayganteng@users.noreply.github.com>
2022-11-05 03:16:40 +0700

    build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-12/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/nayganteng/local_manifest --depth 1 -b main .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
 
 # build rom
 source build/envsetup.sh
-lunch derpfest_RMX2185-userdebug
+lunch derp_RMX2185-userdebug
 export TZ=Asia/Jakarta #put before last build command
 mka derp
 
