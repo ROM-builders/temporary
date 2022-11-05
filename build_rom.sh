b197bba3ba9ddd935b9138e3881f59f8d8881151
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2022-11-05 09:01:52 +0530

    its rice time

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/rushiranpise/local_manifest --depth 1 -b rice .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b thirteen -g default,-mips,-darwin,-notdefault
+git clone https://github.com/rushiranpise/local_manifest --depth 1 -b los13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -8,16 +8,20 @@ source build/envsetup.sh
 lunch lineage_chef-userdebug
 export RICE_MAINTAINER=rushi24
 export RICE_CHIPSET=sdm660
-export WITH_GMS=false
+export WITH_GMS=true
+export TARGET_CORE_GMS=true
+export TARGET_OPTOUT_GOOGLE_TELEPHONY=false
 export RICE_OFFICIAL=true
+export RICE_PACKAGE_TYPE=PIXEL
 export SUSHI_BOOTANIMATION=1080
-export TARGET_BUILD_GRAPHENEOS_CAMERA=true
+export TARGET_BUILD_APERTURE_CAMERA=true
 export TARGET_ENABLE_BLUR=true
 export TARGET_SUPPORTS_QUICK_TAP=true
 export TARGET_FACE_UNLOCK_SUPPORTED=true
+export TARGET_KERNEL_OPTIONAL_LD=true
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-#2
+#1
