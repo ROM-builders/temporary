db87cee33449a9250128b7fa40ed49571fda8f20
Arafattex <69374795+Arafattex@users.noreply.github.com>
2022-11-05 17:23:41 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
-# sync rom 
+# sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Bootleggers-BrokenLab/manifest.git -b tirimbino -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Arafattex/local_manifest.git --depth 1 -b Lancelot_bootleg .repo/local_manifests
+git clone https://github.com/Arafattex/local_manifest.git --depth 1 -b Lancelot_bootleg2 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
