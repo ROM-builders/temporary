f73ee5840c35f7dafe3ff46f023ea9dc0d748bcf
Futar0 <104120854+Futar0@users.noreply.github.com>
2022-11-05 22:19:25 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,6 +5,9 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
+export SELINUX_IGNORE_NEVERALLOWS=true
+export ALLOW_MISSING_DEPENDENCIES=true
+export SKIP_ABI_CHECKS=true
 export TZ=Asia/Dhaka #put before last build command
 brunch lava
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
