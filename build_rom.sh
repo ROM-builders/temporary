69c5f8cd829ffae96479bea60382ccb3b890b02a
rianza <116883839+rianza@users.noreply.github.com>
2022-11-06 15:12:14 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/rianza/local_manifet.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage-markw-user
+lunch lineage_markw-user
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
