bed470b6d7332fc3aa38256d1a9f0d780ce01bdb
youssefnone <113144168+youssefnone@users.noreply.github.com>
2022-11-06 09:08:14 +0100

    m10lte-LineageOS-lineage-17.1-youssefnone

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
-# sync roms
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android -b lineage-19.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/AndVer2/android_manifest_samsung_m10lte.git --depth 1 -b lineage-19.1 .repo/local_manifests
+# sync rom
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android -b lineage-17.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/AndVer9/android_manifest_samsung_m10lte --depth 1 -b lineage-17.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
