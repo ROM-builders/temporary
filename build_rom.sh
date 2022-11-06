e19d4bc17be6812f123a2c7bbdf2e461002c384c
themaltuant <110547541+themaltuant@users.noreply.github.com>
2022-11-06 07:11:04 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,8 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch p404_vayu-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
+lunch p404_vayu-user
 export WITH_GAPPS=true
 export TZ=Asia/Kolkata #put before last build command
 make bacon
