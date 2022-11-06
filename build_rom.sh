3b139ae12589d7e9c0ef37e97c331f145c1d1814
baconpeedit <tejassingh649@gmail.com>
2022-11-07 00:20:49 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch arrow_ginkgo-userdebug
+export ARROW_GAPPS=true
 export TZ=Asia/Delhi #put before last build command
 m bacon
 
