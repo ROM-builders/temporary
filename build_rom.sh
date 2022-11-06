15f4e1059f8c2d073d5d3b810c4ba083bf5f1a81
swapnil vicky <74442290+SwapnilVicky@users.noreply.github.com>
2022-11-06 11:43:25 +0530

    initial release

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 12.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 13.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/SwapnilVicky/local_manifest.git --depth 1 -b cr .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom#
+# build rom
 . build/envsetup.sh
 export TZ=Asia/Kolkata #put before last build command
 brunch miatoll
