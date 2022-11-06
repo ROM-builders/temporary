73040df6e73211378f29475da1b214cebf403de5
weswolf <31709861+paulowesll@users.noreply.github.com>
2022-11-06 16:55:05 -0300

    fix typo

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android -b lineage-19.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/paulowesll/local_manifest --depth 1 -b m52xq .repo/local_manifest
+git clone https://github.com/paulowesll/local_manifest --depth 1 -b m52xq .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build roms
