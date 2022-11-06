0cc3bb964b998d8bc4a3fe70eac0c62fdc48dbcb
AndVer2 <72114405+AndVer2@users.noreply.github.com>
2022-11-06 14:22:36 +0100

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_m10lte-eng
+lunch lineage_m10lte-userdebug
 export TZ=Europe/London #put before last build command
 mka bacon
 
