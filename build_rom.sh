8d6d6d1c547cdcc525cbc03988d14e3ada732ff0
Abhiram <103818282+Azure-Helper@users.noreply.github.com>
2022-11-07 14:03:28 +0530

    Test

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest -b sap
 git clone https://github.com/back-up-git/local_manifests.git --depth 1 -b sapphire .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom [3]
+# build rom
 source build/envsetup.sh
 lunch aospa_raphael-userdebug
 export KBUILD_BUILD_USER=azure
@@ -11,7 +11,7 @@ export KBUILD_BUILD_HOST=azure
 export BUILD_USERNAME=azure
 export BUILD_HOSTNAME=azure
 export TZ=Asia/Kolkata # put before last build command
-./rom-build.sh raphael -t userdebug -v beta
+./rom-build.sh raphael -t userdebug -v alpha
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
