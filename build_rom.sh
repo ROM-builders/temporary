b5b315a2463d39f7842438160c8d8977b64bd319
JuanTamadski <76487328+JuanTamadski@users.noreply.github.com>
2022-11-07 12:07:53 +0800

    added neverallow

--- build_rom.sh
+++ build_rom.sh
@@ -7,6 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch bootleg_Z01R-userdebug
 export TZ=Asia/Kolkata 
+export SELINUX_IGNORE_NEVERALLOWS=true
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
