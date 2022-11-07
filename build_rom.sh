c16dffefda33b1e84184a23fe918b2d096589d7b
swapnil vicky <74442290+SwapnilVicky@users.noreply.github.com>
2022-11-07 12:06:49 +0530

    fix error :)

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/androi
 git clone https://github.com/SwapnilVicky/local_manifest.git --depth 1 -b cr .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom#
+# build rom
 . build/envsetup.sh
 export TZ=Asia/Kolkata #put before last build command
 brunch miatoll
