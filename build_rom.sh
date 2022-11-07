8f07f208e2a5c34169c067c12789802401550d45
weswolf <31709861+paulowesll@users.noreply.github.com>
2022-11-07 10:09:57 -0300

    try again

--- build_rom.sh
+++ build_rom.sh
@@ -3,16 +3,12 @@ repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android -b 
 git clone https://github.com/paulowesll/local_manifest --depth 1 -b m52xq .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build roms
-. build/envsetup.sh
-lunch lineage_m52xq-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
+# build rom
+source build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
-export RELAX_USES_LIBRARY_CHECK=true
-export TZ=Asia/Dhaka
-
-#put before last build command
-brunch m52xq
+lunch lineage_m52xq-userdebug
+export TZ=Asia/Dhaka #put before last build command
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
