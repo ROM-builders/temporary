34d8c9c216131e0da367ff6d89a5ddf0213bc819
Amrito Karmokar <amritokarmokar5i@gmail.com>
2022-11-07 06:41:52 +0600

    Let's go

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom 
 source build/envsetup.sh
 lunch rr_r5x-userdebug
+export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
