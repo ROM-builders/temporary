6be1f2dac4c09659574b25d8c13b9f1f96c68a65
ahmedhridoy371 <40537458+ahmedhridoy371@users.noreply.github.com>
2022-11-08 10:22:51 +0600

    Trigger Crdroid

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,13 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 13.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ahmedhridoy371/local_manifest --depth 1 -b cr13 .repo/local_manifests
+git clone https://github.com/ahmedhridoy371/local_manifest --depth 1 -b crdroidandroid-13.0 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch lineage_lavender-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
+export SELINUX_IGNORE_NEVERALLOWS=true
 export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
 export BUILD_BROKEN_DUP_RULES=true
 export TZ=Asia/Jakarta #put before last build command
