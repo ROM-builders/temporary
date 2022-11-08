765f6f378de3eabf525e1ba9130163451d5918bb
Shridhan Varadkar <shridhan98@gmail.com>
2022-11-08 20:45:24 +0530

    A13 initial builld

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest.git -b 12l -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Shreedhan003/local_manifest.git --depth 1 -b test .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
