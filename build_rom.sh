180d5ad87c673c766a2c7b8f52eee59c48dc50f3
NightlyHorny <80430167+NightlyHorny@users.noreply.github.com>
2022-11-08 23:38:16 -0600

    Update

--- build_rom.sh
+++ build_rom.sh
@@ -1,17 +1,16 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/kitchen-at-night/manifets_los.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/strwbrry143/android -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/kitchen-at-night/local_manifest.git --depth 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 source build/envsetup.sh
-lunch lineage_surya-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
 export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
 export BUILD_BROKEN_DUP_RULES=true
-export WITH_GMS=true
 export TZ=Asia/Jakarta #put before last build command
-make bacon
+brunch surya
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
