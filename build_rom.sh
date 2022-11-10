b4a539a60c75a4d4f28a1044151bebed76198a00
Sachin9822 <70788804+Sachin9822@users.noreply.github.com>
2022-11-10 08:53:16 +0530

    lineage 20 build

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 13 -g default,-mips,-darwin,-notdefault 
-git clone https://github.com/Sachin9822/local_manifest.git --depth 1 -b blaze .repo/local_manifests 
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault 
+git clone https://github.com/Sachin9822/local_manifest.git --depth 1 -b lineage .repo/local_manifests 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
  
 # build rom   
 source build/envsetup.sh   
-lunch blaze_tissot-userdebug
+lunch lineage_tissot-userdebug
 export TZ=Asia/Dhaka #put before last build command   
 mka bacon   
 
