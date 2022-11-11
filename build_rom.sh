106555b22ccddb0a55f86604e1000292e69c4ad9
xioyo <68426327+xioyo@users.noreply.github.com>
2022-11-11 15:05:43 +0530

    Initial build

--- build_rom.sh
+++ build_rom.sh
@@ -1,17 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/P-404/android_manifest -b tokui -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ucoboy/local_manifest.git --depth=1 -b 404 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b thirteen-plus -g default,-mips,-darwin,-notdefault
+git clone https://github.com/xioyo/local_manifest.git --depth=1 -b pe13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch p404_sunfish-userdebug
-export WITH_GAPPS=true
-export SELINUX_IGNORE_NEVERALLOWS=true
-export ALLOW_MISSING_DEPENDENCIES=true
-export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
+lunch aosp_sunfish-userdebug
 export TZ=Asia/Kolkata #put before last build command
-make bacon
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
