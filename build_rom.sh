aa03f9c25d4d271e38fc92830d601456e3df34ab
desvhu-333 <116807723+desvhu-333@users.noreply.github.com>
2022-11-11 17:11:21 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,6 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch syberia_mido-userdebug
 export TZ=Asia/Kolkata #put before last build command
+export KERNEL_SUPPORTS_LLVM_TOOLS := true
 mka syberia
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
