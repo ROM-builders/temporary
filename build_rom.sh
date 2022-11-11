961898f792e0f20632931ad1584f63f7eb615c5d
Asif <49162687+nhAsif@users.noreply.github.com>
2022-11-11 14:07:24 +0600

    fix error

--- build_rom.sh
+++ build_rom.sh
@@ -6,10 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch aosp_alioth-user
-export KBUILD_BUILD_USER=nhAsif
-export KBUILD_BUILD_HOST=pricexd
-export BUILD_USERNAME=nhAsif
-export BUILD_HOSTNAME=pricexd
 export TZ=Asia/Dhaka
 mka bacon
 
