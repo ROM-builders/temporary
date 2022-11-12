56116ad1301547ac70d3a317b2ef1a70ba93d51f
xioyo <68426327+xioyo@users.noreply.github.com>
2022-11-12 18:55:13 +0530

    Try again

--- build_rom.sh
+++ build_rom.sh
@@ -6,9 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_sunfish-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
-export ALLOW_MISSING_DEPENDENCIES=true
-export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
 export TZ=Asia/Kolkata #put before last build command
 m bacon
 
