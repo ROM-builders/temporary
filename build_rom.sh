aa631808aa6b1cb1f369baaa81533dbd78823c2e
Goku <100507815+god-goku@users.noreply.github.com>
2022-11-12 17:15:20 +0530

    #82

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://github.com/god-goku/local-manifest.git --depth 1 -b ch13 .repo/local_manifests
+git clone https://github.com/god-goku/local-manifest.git --depth 1 -b cherry .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-#81
+#82
 # build rom
 . build/envsetup.sh
 export TZ=Asia/Dhaka #put before last build command
