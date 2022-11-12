080226a56051a4f500ccc95d4c186ce24a6f8793
Yoni Asleno Farid <92686606+aslenofarid@users.noreply.github.com>
2022-11-12 18:54:09 +0700

    Ok fix

--- build_rom.sh
+++ build_rom.sh
@@ -3,13 +3,12 @@ repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git
 git clone https://github.com/aslenofarid/local_manifest.git --depth 1 -b lineage-19.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build roms
 source build/envsetup.sh
 lunch lineage_X00TD-userdebug
 export BUILD_USERNAME=aslenofarid
 export TZ=Asia/Jakarta #put before last build command
 brunch X00TD
-
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
