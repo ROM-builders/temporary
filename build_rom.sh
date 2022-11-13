0768eadbf6a8c9328a60ad63f38bb22178e86e43
Amrito Karmokar <amritokarmokar5i@gmail.com>
2022-11-13 22:20:10 +0600

    Vanilla

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_r5x-userdebug
+export WITH_GMS=false
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
