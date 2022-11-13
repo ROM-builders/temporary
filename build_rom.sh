2b2ca15104443a56563af71704c7b395e210dd7d
Antony Braiano <78004742+Antony-Braiano@users.noreply.github.com>
2022-11-13 16:19:04 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,6 @@ repo init  --no-repo-verify -u https://github.com/RiceDroid/android -b thirteen 
 git clone https://github.com/raidenkkj/local_manifest.git --depth 1 -b ricedroid-spes .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-#TeSt1
 
 # build rom
 . build/envsetup.sh
