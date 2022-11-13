f62f821f8772d7ef1fc221bfbb5ad7df5e321046
zaidannn7 <72336864+zaidannn7@users.noreply.github.com>
2022-11-13 14:01:58 +0700

    juice: change dt potato

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,25 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PotatoProject/manifest -b frico_mr1-release -g default,-mips,-darwin,-notdefault
-git clone https://github.com/zaidannn7/local_manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/zaidannn7/local_manifest.git --depth 1 -b potato .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom 
 source build/envsetup.sh
+export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
+export RELAX_USES_LIBRARY_CHECK=true
+export BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES=true
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+export BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE=true
+export BUILD_BROKEN_VERIFY_USES_LIBRARIES=true
 export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
 export BUILD_BROKEN_DUP_RULES=true
-export SELINUX_IGNORE_NEVERALLOWS=true
-export BUILD_USERNAME=zaidan
-export BUILD_HOSTNAME=ytta-labs
+export BUILD_USERNAME=zaidannn7
+export BUILD_HOSTNAME=zdnx-server
+export KBUILD_BUILD_NAME=zaidannn7
+export KBUILD_BUILD_HOST=zdnx-server
+export BUILD_BROKEN_CLANG_ASFLAGS=true
+export BUILD_BROKEN_CLANG_CFLAGS=true
 export TZ=Asia/Jakarta #put before last build command
 brunch potato_juice-userdebug
 
