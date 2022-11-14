0cf033bdc7d1cfbaf88f60abfd644c8a87be578f
Arafattex <69374795+Arafattex@users.noreply.github.com>
2022-11-14 16:39:50 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom.  
+# sync rom.
 repo init --depth=1 --no-repo-verify -u https://github.com/ForkLineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Arafattex/local_manifest.git --depth 1 -b Lancelot_lospro .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
