99ea24eecead657194d93683ef036194f2a36fb7
enweazudaniel <daniel.enweazu@student.aul.edu.ng>
2022-11-14 08:24:42 +0100

    spes-superioros-enweazudaniel

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 export SUPERIOR_GAPPS=minimal
 lunch superior_spes-userdebug
-export TZ=Asia/Dhaka #put before last build command
+export TZ=Asia/Dhaka #put before last buildcommand
 m bacon -j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
