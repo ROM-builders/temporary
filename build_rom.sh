dab5f7dc18e1465d15b6d5098a43d357e4a9fc4b
danzox <93202212+danzox@users.noreply.github.com>
2022-11-14 11:08:18 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/danzox/local_manifest.git --depth 1 -b Lineage .repo/local_manifests
 repo sync -c --no--bundle -no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 export TZ=Asia/Jakarta #put before last build command
-lunch aosp_Mi8937-userdebug
+lunch lineage_Mi8937-userdebug
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
