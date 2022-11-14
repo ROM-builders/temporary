86bb629334dd446c5564b946db29954ae2bfd41f
enweazudaniel <daniel.enweazu@student.aul.edu.ng>
2022-11-14 08:28:24 +0100

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,8 +7,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 export SUPERIOR_GAPPS=minimal
 lunch superior_spes-userdebug
-export TZ=Asia/Dhaka #put before last buildcommand
-m bacon -j$(nproc --all)
+export TZ=Asia/Dhaka #put before last build command
+m bacon -j8
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
