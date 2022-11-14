abf06a20bfc78dc4f28e7855531b8da811d9ec16
GhostMaster69-dev <rathore6375@gmail.com>
2022-11-14 20:48:26 +0530

    Build Evolution X

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://github.com/GhostMaster69-dev/local_manifest.git --depth 1 -b devolution-tiramisu .repo/local_manifests
+git clone https://github.com/GhostMaster69-dev/local_manifest.git --depth 1 -b evolution-x-tiramisu .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
