645b72735446ba6ba4132f1042000f6e2847d3d8
Lite <97386083+Lite009@users.noreply.github.com>
2022-11-15 07:02:29 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom 
 . build/envsetup.sh
 lunch awaken_vince-user
+export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
