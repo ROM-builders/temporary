5c98c0b825e5ea211b15b1f4bc242d65c4707086
Arafattex <69374795+Arafattex@users.noreply.github.com>
2022-11-15 06:09:15 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/Arafattex/local_manifest.git --depth 1 -b Lancelot_
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom 
-source build/envsetup.sh
+source build/envsetup.sh 
 export ALLOW_MISSING_DEPENDENCIES=true
 export TARGET_FLOS=true
 lunch lineage_lava-userdebug
