e2ab6092568953af906eb8d44df429433a9e2e44
Arafattex <69374795+Arafattex@users.noreply.github.com>
2022-11-15 13:57:02 +0600

    lava-ForkLineageOS-lineage-19.1-Arafattex

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,15 @@
-# sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+# sync rom.  
+repo init --depth=1 --no-repo-verify -u https://github.com/ForkLineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Arafattex/local_manifest.git --depth 1 -b Lancelot_lospro .repo/local_manifests
+repo sync 
 
-# build rom
-source build/envsetup.sh
-lunch derp_vayu-user
+# build rom 
+source build/envsetup.sh 
+export ALLOW_MISSING_DEPENDENCIES=true
+export TARGET_FLOS=true
+lunch lineage_lava-userdebug
 export TZ=Asia/Dhaka #put before last build command
-mka derp
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
