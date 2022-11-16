1815224368d672cd708e745382dd1c58dc713adf
Angel Castillo <acastillorobles77@gmail.com>
2022-11-16 14:17:33 -0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/BlissRoms/platform_manifest.git -b arcadia-next -g default,-mips,-darwin,-notdefault
-git clone https://github.com/acastillorobles77/local_manifest.git --depth 1 -b bliss-12.x-vayu .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/BlissRoms/platform_manifest.git -b typhoon -g default,-mips,-darwin,-notdefault
+git clone https://github.com/acastillorobles77/local_manifest.git --depth 1 -b bliss-13.0-vayu .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
