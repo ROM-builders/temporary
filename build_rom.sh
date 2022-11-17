8b6efba3ef3779c1b8474ec8c8670dba27f1bdd4
Alec Chan <88945170+alecchangod@users.noreply.github.com>
2022-11-17 18:10:32 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,14 @@
-# sync rom
+# sync rom 
 repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 13.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/alecchangod/local_manifest.git --depth 1 -b cr .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
-# build rom 
+ 
+# build rom  
 . build/envsetup.sh
 lunch lineage_monet-userdebug
 export TZ=Asia/Hong_Kong #put before last build command 
-make bacon
-
-# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+make bacon 
+ 
+# upload rom (if you don't need to upload multiple files, then you don't need to edit next line) 
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+ 
