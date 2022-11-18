74045c12db4fbd8a0417009e691c9a8d1fa25d62
Asif <49162687+nhAsif@users.noreply.github.com>
2022-11-18 19:02:47 +0600

    Trigger build

--- build_rom.sh
+++ build_rom.sh
@@ -5,12 +5,9 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch evolution_alioth-userdebug
+lunch evolution_alioth-user
 export TZ=Asia/Dhaka
-export BUILD_USERNAME=alioth
-export BUILD_HOSTNAME=userdebug
 mka evolution
-
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
