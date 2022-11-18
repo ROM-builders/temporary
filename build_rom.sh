eeee212030842bc1c11a2b1d3e836c9a043f8e27
adarsh956 <114992695+adarsh956@users.noreply.github.com>
2022-11-18 11:17:00 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch derp_dandelion-user
+lunch derp_garden-user
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
