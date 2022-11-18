a57c6f647b674777b207de46eca0859fc9236edd
Antony Braiano <78004742+Antony-Braiano@users.noreply.github.com>
2022-11-18 00:53:54 -0300

    SuperiorOS bringup

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Antony-Braiano/local_manifest.git --depth 1 -b ricedroid-spes .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 
 # build rom
 . build/envsetup.sh
-lunch lineage_spes-userdebug
+lunch superior_spes-userdebug
 export TZ=Asia/Dhaka #put before last build command
 make
 
