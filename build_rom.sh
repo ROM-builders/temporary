b55c66280e31a0c4c1899804260bd8be3ddc93d4
Christopher Keith Irvine <Mizdrake7@gmail.com>
2022-11-19 23:43:46 +0530

    r5x-CherishOS-tiramisu-mizdrake7

--- build_rom.sh
+++ build_rom.sh
@@ -14,10 +14,9 @@ export SKIP_ABI_CHECKS=true
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export USE_DEXOPT=true
 export SELINUX_IGNORE_NEVERALLOWS=true
-export CHERISH_VANILLA=true
+export CHERISH_VANILLA=false
 export TZ=Asia/Kolkata #put before last build command
 brunch r5x
-
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
