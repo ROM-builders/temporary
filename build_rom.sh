1d9349b7df528369148801e81b8038391e9bdcf6
Alec Chan <88945170+alecchangod@users.noreply.github.com>
2022-11-19 12:56:50 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,14 @@
-# sync rom  
+# sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-oss/xd_manifest -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/alecchangod/local_manifest.git --depth 1 -b xd .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
  
-# build rom 
-. build/envsetup.sh 
-lunch xdroid_monet-userdebug 
-export TZ=Asia/Hong_Kong #put before last build command  
-make xd 
-#16 
+# build rom
+. build/envsetup.sh
+lunch xdroid_monet-userdebug
+export TZ=Asia/Hong_Kong #put before last build command
+make xd
+#16
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+ 
