888211aec8e80fcff8fe65ebb2a6208e3c2b99ce
khirendra03 <khirendrapatel3@gmail.com>
2022-11-20 18:53:18 +0530

    4.19 initial build
    Signed-off-by: khirendra03 <khirendrapatel3@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/khirendra03/local_manifest.git --depth 1 -b voltage .repo/local_manifests
+git clone https://github.com/khirendra03/local_manifest.git --depth 1 -b 13-4.19 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -10,5 +10,5 @@ brunch voltage_X01BD-userdebug
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-rclone copy vendor/ota/X01BD.json cirrus:X01BD -P
+#rclone copy vendor/ota/X01BD.json cirrus:X01BD -P
 
