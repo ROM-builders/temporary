6443f46ef27f9ba68ea357a2a49758faed77290d
zaidannn7 <72336864+zaidannn7@users.noreply.github.com>
2022-11-20 12:41:41 +0700

    Initial P404 Build !

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Fork-Krypton/manifest.git -b A12 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/zaidannn7/local_manifest --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/P-404/platform_manifest -b shinka -g default,-mips,-darwin,-notdefault
+git clone https://github.com/zaidannn7/local_manifest --depth 1 -b p404 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -18,8 +18,10 @@ export BUILD_USERNAME=zaidannn7
 export BUILD_HOSTNAME=zdnx-labs
 export KBUILD_BUILD_NAME=zaidannn7
 export KBUILD_BUILD_HOST=zdnx-labs
+export WITH_GAPPS=false
 export TZ=Asia/Jakarta
-launch juice userdebug
+lunch p404_juice-userdebug
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
