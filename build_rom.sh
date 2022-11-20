3a7992788471c3d8d173e37a8b8bb2ffbda050fe
Indierock00 <52739181+Indierock00@users.noreply.github.com>
2022-11-20 16:33:28 +0100

    superior-13

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Indierock00/local_manifest.git --depth 1 -b lineage-20.0 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Indierock00/local_manifest.git --depth 1 -b superior-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_markw-userdebug
+lunch superior_markw-userdebug
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
