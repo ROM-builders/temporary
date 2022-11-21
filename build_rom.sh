5c21ecf034f8a1be998f432cb92dd1005ab28fb1
criperm19 <96837536+CreeperAmigo19@users.noreply.github.com>
2022-11-21 18:22:40 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 11.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/CreeperAmigo19/local-manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/CreeperAmigo19/local-manifest.git --depth 1 -b patch-1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
