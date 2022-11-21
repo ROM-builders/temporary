ed4f14c6cd9f166aa7b51b1cd34b24f0fe339f4e
ellenxdf <76019171+ellenxdf@users.noreply.github.com>
2022-11-22 01:23:59 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/PixelExperience/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/dafhaaji/local_manifest --depth 1 -b main .repo/local_manifests
+git clone https://github.com/ellenxdf/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
