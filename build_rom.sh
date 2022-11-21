92a09455c1939a316483dfda5325d9121850605f
Christopher Keith Irvine <Mizdrake7@gmail.com>
2022-11-21 14:39:23 +0530

    r5x-CherishOS-tiramisu-mizdrake7

--- build_rom.sh
+++ build_rom.sh
@@ -18,10 +18,9 @@ export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
 export BUILD_BROKEN_VERIFY_USES_LIBRARIES=true
 export BUILD_BROKEN_DUP_RULES=true
 export BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE=true
-export CHERISH_VANILLA=true
+export CHERISH_VANILLA=false
 export TZ=Asia/Kolkata #put before last build command
 brunch r5x
-
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
