d2a6f1105e3f3b4d3a686d6bb84114a219719519
A³ Project Repositories <112053455+aepranata@users.noreply.github.com>
2022-11-22 17:28:14 +0700

    Fix branch

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/aepranata/local_manifests.git --depth 1 -b ricedroid-13 .repo/local_manifests
+git clone https://github.com/aepranata/local_manifests.git --depth 1 -b ricedroid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
