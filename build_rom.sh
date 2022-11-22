5e15d2ba42634370522ccc3005bf82e1a094d495
criperm19 <96837536+CreeperAmigo19@users.noreply.github.com>
2022-11-21 21:54:59 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,10 +5,9 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-brunch A001D
 export TZ=America/Sao_Paulo
 export SELINUX_IGNORE_NEVERALLOWS=true
-mka
+brunch A001D
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
