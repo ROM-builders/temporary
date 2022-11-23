e3640674c2544bbd91228d374974479b2bf52de9
Nazar Kovalenko <85736144+Jaxer159@users.noreply.github.com>
2022-11-23 23:43:10 +0200

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom
+# sync arrow
 repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-13.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Jaxer159/manifests --depth 1 -b arrow_13_temporary_chime .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
