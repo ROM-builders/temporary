225fc237e1fb065b162a8f9ad3be8f60351b7c6f
A³ Project Repositories <112053455+aepranata@users.noreply.github.com>
2022-11-24 07:10:12 +0700

    #6

--- build_rom.sh
+++ build_rom.sh
@@ -3,16 +3,16 @@ repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b 
 git clone https://github.com/aepranata/local_manifests.git --depth 1 -b ricedroid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build romu⁴
+# build rom
 source build/envsetup.sh
 lunch lineage_rosy-userdebug
-export KBUILD_BUILD_USER=ibnuridwan
+export KBUILD_BUILD_USER=aepranata
 export KBUILD_BUILD_HOST=a3project
-export BUILD_USERNAME=ibnuridwan
+export BUILD_USERNAME=aepranata
 export BUILD_HOSTNAME=a3project
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Jakarta #put before last build command
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
-rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.s 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
