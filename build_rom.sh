bcee62161af56f66e5a5749a87f4efa13b9f623c
EmadGr8 <91535171+EmadGr8@users.noreply.github.com>
2022-11-24 17:36:32 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 breakfast aosp_tulip-userdebug
 export TZ=Asia/Dhaka 
-lunch tulip
+brunch tulip
 
 # upload rom
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
