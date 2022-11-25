60a2d23da58033e52c7612b628368d4a03fe764c
Sandeep saxena <112549093+San4255@users.noreply.github.com>
2022-11-25 19:05:01 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/San4255/local-manifest.git --depth 1 -b main .repo/
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+. build/envsetup.sh
 lunch spark_phoenix-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
