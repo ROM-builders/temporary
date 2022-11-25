041d199c93028203e1315a5083fa9b357ff816f6
Yoni Asleno Farid <92686606+aslenofarid@users.noreply.github.com>
2022-11-25 16:11:50 +0700

    amp feedback

--- build_rom.sh
+++ build_rom.sh
@@ -5,10 +5,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-breakfast X00TD
 export BUILD_USERNAME=aslenofarid
 export TZ=Asia/Jakarta #put before last build command
-chroot
 brunch X00TD
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
