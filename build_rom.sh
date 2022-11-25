2cfd05cedd2a7d0d1d8a4edf4371cf27c0f053d9
Sandeep saxena <112549093+San4255@users.noreply.github.com>
2022-11-25 17:28:21 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,10 +5,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-export TARGET_KERNEL_CLANG_VERSION=proton
-export BUILD_USERNAME=Risan
-export TZ=Asia/Kolkata 
 lunch spark_phoenix-userdebug
+export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
