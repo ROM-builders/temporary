8ab329e90f6a39ec064c81eb90896f90ae71356b
Adalat222 <118736057+Adalat222@users.noreply.github.com>
2022-11-26 10:48:38 +0530

    gauguin-Evolution-X-tiramisu-Adalat222

--- build_rom.sh
+++ build_rom.sh
@@ -7,6 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 export TZ=Asia/Dhaka #put before last build command
 lunch evolution_gauguin-user
+#build code
 mka evolution
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
