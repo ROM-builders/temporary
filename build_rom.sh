906505e09112727e5b437b6d848265ddb61284f6
NRanjan-17 <nalinishranjan05@gmail.com>
2022-11-26 14:51:47 +0530

    mido: Initial PixysOS thirteen

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixysOS/manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/NRanjan-17/local_manifest.git --depth 1 -b Pixys-12.1 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixysOS/manifest -b thirteen -g default,-mips,-darwin,-notdefault
+git clone https://github.com/NRanjan-17/local_manifest.git --depth 1 -b Pixys-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
