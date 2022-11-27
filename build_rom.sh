9f6a414c20603a3253dd2047a1a5c179af09447f
Atul9977 <104277401+Atul9977@users.noreply.github.com>
2022-11-27 12:46:03 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,16 +5,9 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-export KBUILD_BUILD_USER=Adarsh
-export KBUILD_BUILD_HOST=Adarsh
-export BUILD_USERNAME=Adarsh
-export BUILD_HOSTNAME=Adarsh
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
-export USE_DEXOPT=true
-export SELINUX_IGNORE_NEVERALLOWS=true
-export CHERISH_VANILLA=true
-export TZ=Asia/Kolkata #put before last build command
-brunch garden
+lunch cherish_garden-userdebug
+export TZ=Asia/Dhaka #put before last build command
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
