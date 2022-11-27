61d99810f278643e8774b5b122c7096e3d2f4a8c
Hafidz Muzakky <ais.muzakky@gmail.com>
2022-11-27 11:20:19 +0700

    Timezone Jakarta

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch xdroid_joyeuse-userdebug
-export TZ=Asia/Jakarta #put before last build command
+export TZ="/usr/share/zoneinfo/Asia/Jakarta" #put before last build command
 make xd
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
