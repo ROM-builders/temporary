f6451dbc4ed8fe4c7499328b222eafdc5ee74a72
Alec Chan <88945170+alecchangod@users.noreply.github.com>
2022-11-27 21:27:45 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-oss/xd_manifest -b thirteen -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-oss/xhttps://github.com/Project-Xtended/manifest.git -b xt -g default,-mips,-darwin,-notdefault
 git clone https://github.com/alecchangod/local_manifest.git --depth 1 -b xtended .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
  
