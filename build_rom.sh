687be0e8724cfc89521c1357f26791f992cd0f03
Anandhasayanan <anandhasayanan007jb@gmail.com>
2022-11-27 10:45:20 +0530

    8.5 Release GApps

--- build_rom.sh
+++ build_rom.sh
@@ -3,10 +3,10 @@ repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b 
 git clone https://github.com/anandhan07/local_manifest.git --depth 1 -b ricedroid-13.0 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom [51]
+# build rom [52]
 . build/envsetup.sh
 lunch lineage_vince-user
-export WITH_GMS=false
+export WITH_GMS=true
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
