e8728e549f791fa8fc3c6033ff5cc20c38f41051
Atul9977 <104277401+Atul9977@users.noreply.github.com>
2022-11-27 09:34:56 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch cherish_dandelion-user
+lunch cherish_garden-user
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
