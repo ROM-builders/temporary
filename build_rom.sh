49ff7c6b8997722cffc9140b3e34b306b0ecf076
EmadGr8 <91535171+EmadGr8@users.noreply.github.com>
2022-11-28 07:41:14 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/EmadGr8/local_manifest.git --depth 1 -b LOS-19.1 .r
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+. build/envsetup.sh
 lunch lineage_twolip-user
 export TZ=Asia/Dhaka
 make bacon
