dbe67b8fe0c5d2321785e2c6c54f9351c9468e4c
romgharti <romgharti@gmail.com>
2022-11-28 11:46:36 +0545

    Initial

--- build_rom.sh
+++ build_rom.sh
@@ -1,17 +1,17 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Corvus-AOSP/android_manifest.git -b 13 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/romgharti/local_manifest.git --depth 1 -b corvus-13.0 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Xtended/manifest.git -b xt -g default,-mips,-darwin,-notdefault
+git clone https://github.com/romgharti/local_manifest.git --depth 1 -b xt .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch corvus_mojito-user
+lunch xtended_mojito-user
 #export ALLOW_MISSING_DEPENDENCIES=true
 #export SELINUX_IGNORE_NEVERALLOWS=true
-export CORVUS_MAINTAINER=Rom_Gharti
-export USE_GAPPS=true
+export XTENDED_BUILD_MAINTAINER=Rom_Gharti
+export WITH_GAPPS=true
 export TZ=Asia/Dhaka #put before last build command
-make corvus
+make xtended
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
