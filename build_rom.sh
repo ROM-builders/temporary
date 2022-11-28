65352d7755cff599bf634fb9616972c5eaa3af12
Sachin9822 <70788804+Sachin9822@users.noreply.github.com>
2022-11-28 18:21:24 +0530

    retry 9

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,7 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault 
 git clone https://github.com/Sachin9822/local_manifest.git --depth 1 -b lineage .repo/local_manifests 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-  
+   
 # build rom    
 source build/envsetup.sh 
 lunch lineage_tissot-userdebug  
