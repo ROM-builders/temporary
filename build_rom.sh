0104c2ea67c6b99036341882a9e9bc1be8337c29
rahmanaagung <116900824+rahmanaagung@users.noreply.github.com>
2022-11-30 21:07:06 +0700

    wayne-xd13-rahmanaagung
    build xd13

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-CAF/xd_manifest.git -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/rahmanaagung/Local-Manifest.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/rahmanaagung/Local-Manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
