3cdbca0518463f0ee57f4a51447e195086d32999
ChiwaCime <51258794+ChiwaCime@users.noreply.github.com>
2022-11-30 09:50:43 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 13.0 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
 git clone https://github.com/ChiwaCime/local_manifest.git --depth 1 -b vangogh .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch lineage_vangogh-userdebug 
-export SKIP_ABI_CHECKS=true
+lunch evolution_vangogh-userdebug 
 export TZ=Asia/Chongqing #put before last build command 
 make bacon
 
