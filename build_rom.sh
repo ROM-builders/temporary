b6597d65348ccbc062e76f9bcdec5a8808c82cf3
rahmanaagung <116900824+rahmanaagung@users.noreply.github.com>
2022-11-30 19:59:36 +0700

    wayne-xd-rahmanaagung
    hamd

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch xdroid_$wayne-userdebug
+lunch xdroid_wayne-userdebug
 make xd -j$(nproc --all)
 
 
