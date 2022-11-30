7b8ebcc65156e26f9ce6e276e749687bda2ab6fc
rahmanaagung <116900824+rahmanaagung@users.noreply.github.com>
2022-11-30 20:09:16 +0700

    wayne
    wayne

--- build_rom.sh
+++ build_rom.sh
@@ -9,8 +9,8 @@ lunch xdroid_$wayne-userdebug
 make xd -j$(nproc --all)
 
 
-export TZ=Asia/Dhaka #put before last build command
-mka derp
+#put before last build command
+
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
