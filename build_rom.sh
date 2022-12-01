39d43c9e14f86c7a1aec98852dbfeda915665546
Atul9977 <104277401+Atul9977@users.noreply.github.com>
2022-12-02 00:46:20 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/StagOS/manifest.git -b s12.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/AospExtended/manifest.git -b 12.1.x -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Atul9977/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch stag_garden-userdebug
+lunch aosp_garden-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export TZ=Asia/Kolkata #put before last build command
-make stag
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
