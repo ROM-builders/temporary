2b8797bcd51e56f10b74aabd247e4961bc9eb26d
Alec Chan <88945170+alecchangod@users.noreply.github.com>
2022-12-01 20:19:34 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,11 +4,11 @@ git clone https://github.com/alecchangod/local_manifest.git --depth 1 -b cr .rep
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
  
 # build rom  
-. build/envsetup.sh
+source build/envsetup.sh
 lunch lineage_monet-userdebug
 export TZ=Asia/Hong_Kong #put before last build command 
 make bacon 
-# 2
+# 3
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line) 
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
  
