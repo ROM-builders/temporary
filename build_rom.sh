117e36cf129e0400659fee31ee691a61b1a06c07
romgharti <romgharti@gmail.com>
2022-12-01 15:50:25 +0545

    DT change

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch xtended_mojito-user
+lunch xtended_mojito-userdebug
 #export ALLOW_MISSING_DEPENDENCIES=true
 #export SELINUX_IGNORE_NEVERALLOWS=true
 export XTENDED_BUILD_MAINTAINER=Rom_Gharti
