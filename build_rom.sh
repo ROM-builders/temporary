6a9acd726670414c49509fcc98b13f4776a61280
xack21 <86214526+xack21@users.noreply.github.com>
2022-12-01 21:36:42 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-13.0 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/xack21/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch arrow_Mi8937-userdebug
+lunch superior_Mi8937-userdebug
 m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
