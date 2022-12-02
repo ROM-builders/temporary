fb4e859dde35ae3c763630be313bd74f3167fdeb
rahmanaagung <116900824+rahmanaagung@users.noreply.github.com>
2022-12-02 13:25:42 +0700

    wayne-RiceDroid-twelve-rahmanaagung
    build wayne

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,6 @@
 # Thanks to @Apon77
 
+
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/rahmanaagung/local_manifest.git --depth=1 -b main .repo/local_manifests
