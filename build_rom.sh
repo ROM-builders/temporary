39164c48e228ee9bd7390821f423ee6deca49cb6
rahmanaagung <116900824+rahmanaagung@users.noreply.github.com>
2022-12-02 12:23:55 +0700

    Wayne-XDroid-Twelve-rahmanaagung
    build for wayne

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,31 @@
+# Thanks to @Apon77
+
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-CAF/xd_manifest.git -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/rahmanaagung/Local-Manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/rahmanaagung/local_manifest.git --depth=1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch xdroid_wayne-userdebug
-export TZ=Asia/Jakarta #put before last build command
+export TZ=Asia/Jakarta
+export BUILD_USER=rahmanaagung
+export BUILD_HOST=wayje
+export BUILD_USERNAME=rahmanaagung
+export BUILD_HOSTNAME=wayne
 make xd
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+
+
+
+
+
+
+
+
+
+
+
+
