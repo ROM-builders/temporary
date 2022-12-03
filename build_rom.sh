ea8229182073cbee1293feffa9599acb20bdf6c3
xack21 <86214526+xack21@users.noreply.github.com>
2022-12-03 21:47:48 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch superior_Mi8937-userdebug
+export ALLOW_MISSING_DEPENDENCIES=true
 m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
