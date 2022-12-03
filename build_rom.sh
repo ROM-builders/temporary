99d8c15e3fd477898c3fc67bd9f0d9289a6fee41
Vimal Pandey <118910290+Vimal227@users.noreply.github.com>
2022-12-03 20:30:37 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch ricedroid_garden-user
+brunch ricedroid_garden-user
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
