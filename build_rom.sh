3d5387189b478460b3f0cd85625ce41f57080140
Hasin Abrar Haque <70853665+AbrarNoob@users.noreply.github.com>
2022-12-04 00:33:20 +0600

    Initial build

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,13 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Shreedhan003/local_manifest.git --depth 1 -b test .repo/local_manifests
+git clone https://github.com/AbrarNoob/local_manifest --depth 1 -b vos .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-export KBUILD_BUILD_USER=xtra_sharif
-export KBUILD_BUILD_HOST=xtra_sharif
-export BUILD_USERNAME=xtra_sharif
-export BUILD_HOSTNAME=xtra_sharif
+lunch voltage_alioth-userdebug
 export TZ=Asia/Delhi #put before last build command
-brunch lavender
+brunch alioth
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
