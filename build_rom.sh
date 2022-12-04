50fd092630eab5eb6f4dd795e224e92c9040b78a
Sergeant Kakashi <111608769+sergeantkakashi@users.noreply.github.com>
2022-12-04 21:23:43 +0100

    a505fn-crdroidandroid-sergeantkakashi

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/Havoc-OS/android_manifest.git -b eleven -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/crdroidandroid/android.git -b 13.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/sergeantkakashi/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch havoc_j7y17lte-user
+lunch aosp_a505fn-user
 export TZ=Asia/Dhaka #put before last build command
 mka bacon 
 
