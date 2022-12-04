6468d83da2aeb7f3742075ae66c3c0f953e2c9be
rahmanaagung <116900824+rahmanaagung@users.noreply.github.com>
2022-12-04 11:45:18 +0700

    wayne-AOSPK-twelve-rahmanaagung
    build wayne

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/AOSPK/manifest.git -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/rahmanaagung/local_manifest.git --depth 1 -b AOSPK .repo/local_manifests
+git clone https://github.com/rahmanaagung/local_manifest.git --depth 1 -b AOSPK12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
