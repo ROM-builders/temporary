608458154f81b0a8357eeebfe785695dd934e8e9
Ezaa <64605130+RooGhz720@users.noreply.github.com>
2022-12-04 22:29:28 +0700

    Update build_rom.sh
    build conquer sweet

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-AOSP/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/conquerOS/manifest.git -b tithonia -g default,-mips,-darwin,-notdefault
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_sweet-user
+lunch conquer_sweet-user
 export TZ=Asia/Jakarta #put before last build command
-mka derp
+mka carthage
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
