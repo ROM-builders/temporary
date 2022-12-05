7cf87a88d2ef8bd6626ea63a74168eaa34ab985b
Sergeant Kakashi <111608769+sergeantkakashi@users.noreply.github.com>
2022-12-05 15:56:58 +0100

    rosemary-PixelOS-Pixelish-twelve-sergeantkakashi

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/crdroidandroid/android -b 13.0 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/PixelOS-AOSP/manifest.git -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/sergeantkakashi/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_a505fn-user
+lunch aosp_rosemary-user
 export TZ=Asia/Dhaka #put before last build command
 mka bacon 
 
