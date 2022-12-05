b7bd63b1dadf5d88dcf736af6de467036ad370a1
Sergeant Kakashi <111608769+sergeantkakashi@users.noreply.github.com>
2022-12-05 19:53:34 +0100

    rosemary-crdroidandroid-13.0-sergeantkakashi

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/crdroidandroid/android.git -b 12.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/crdroidandroid/android.git -b 13.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/sergeantkakashi/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_rosemary-user
+lunch lineage_rosemary-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon 
 
