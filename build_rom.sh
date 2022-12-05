369216ad5307e6354a46351ac5675c17238a66d3
ChiwaCime <51258794+ChiwaCime@users.noreply.github.com>
2022-12-05 09:36:12 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b thirteen-plus -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ChiwaCime/local_manifest.git --depth 1 -b vangogh .repo/local_manifests
+git clone https://github.com/ChiwaCime/local_manifest.git --depth 1 -b dummy .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
