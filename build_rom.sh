e2a5718a4212da9b6bbe9cca6d1412ab73915fdd
DESTROYER3264 <118124438+DESTROYER3264@users.noreply.github.com>
2022-12-05 15:04:14 +0530

    Fix moto cam

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android.git
 git clone https://github.com/DESTROYER3264/local_manifest.git --depth 1 -b rice .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-#  build rom
+# build rom
 source build/envsetup.sh
 lunch lineage_liber-userdebug
 export TZ=Asia/Kolkata #put before last build command
