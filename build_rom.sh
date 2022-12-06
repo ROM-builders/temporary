3faa3230b0c14a79e8db9324acf934c5d92a2a65
CodeChas3r <69832543+CodeChas3r@users.noreply.github.com>
2022-12-06 11:35:52 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/CodeChas3r/local_manifest.git --depth 1 -b lineage-19.1 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u -u https://github.com/Corvus-AOSP/android_manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/CodeChas3r/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_rosemary-userdebug
+lunch corvus_rosemary-userdebug
 export TZ=Asia/Jakarta #put before last build command
 make bacon
 
