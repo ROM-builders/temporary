72992f285d95fd9d8aee34d9ed6b7b130a1c1b2a
Arafattex <69374795+Arafattex@users.noreply.github.com>
2022-12-06 14:36:59 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch corvus_lava-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
-# export USE_PROTON=true
+export USE_PROTON=true
 export TZ=Asia/Dhaka #put before last build command
 make corvus
 
