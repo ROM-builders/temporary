5f0c623a61d54a6edf551076ef137d4c147a64cf
EmadGr8 <91535171+EmadGr8@users.noreply.github.com>
2022-12-06 19:27:42 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/EmadGr8/local_manifest.git --depth 1 -b Arrow-12.1-sensei .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-13.0 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/EmadGr8/local_manifest.git --depth 1 -b Arrow-13.0 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch arrow_tulip-user
+lunch aosp_tulip-user
 export TZ=Asia/Dhaka
 m bacon
 
