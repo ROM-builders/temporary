b69298657a6a545e343368f404899bd9f3f0687f
Alec Chan <88945170+alecchangod@users.noreply.github.com>
2022-12-06 21:27:44 +0800

    build with new fingerprint

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch xtended_monet-user
-export WITH_GAPPS=true
+# export WITH_GAPPS=true
 export TZ=Asia/Hong_Kong #put before last build command
 make xtended
 # 15
