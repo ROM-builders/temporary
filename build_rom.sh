4b3188d8071ee65fda229393719dfc2e7541a4fb
Aman Setia <13214574+Amanse@users.noreply.github.com>
2022-12-07 19:29:52 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b 13 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Amanse/local_manifest.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Amanse/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch aosp_RMX3461
-export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
