6dc1f0517fbf67c223f0108f5a54801b27a7ebdb
z3zens <78209717+z3zens@users.noreply.github.com>
2022-12-07 16:42:22 +0700

    X01BD-xdroid-oss-thirteen-z3zens

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-oss/xd_manifes
 git clone https://github.com/z3zens/local_manifest --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom xd13
+# build rom xd13 
 source build/envsetup.sh
 lunch xdroid_X01BD-userdebug
 export BUILD_USERNAME=nobody
