bb3d8f6eb3377c04a6a0b76fe64d4461b883c92f
Sachin752002 <103373100+Sachin752002@users.noreply.github.com>
2022-12-07 15:33:48 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExtended/manifest -b trece -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Sachin752002/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b pyro -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Sachin752002/local_manifest.git --depth 1 -b Spark .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_X01AD-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
+lunch spark_X01AD-userdebug
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
