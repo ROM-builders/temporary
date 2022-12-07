3075dcc0cea2ec4bbbdf8dbf6f73081a9201b15c
Kneba <abenkenary3@gmail.com>
2022-12-07 14:30:26 +0700

    dancok build

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android.git -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Kneba/local_manifests.git --depth 1 -b rice .repo/local_manifests
+git clone https://github.com/Kneba/local_manifests.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
