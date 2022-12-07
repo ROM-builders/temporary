7ee2fb542c466c9e8e2bfa53a301e43dde1b0325
Et3rnity <73420351+Et3rnity101@users.noreply.github.com>
2022-12-07 13:49:52 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Corvus-AOSP/android_manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Et3rnity101/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch corvus_rosemary-userdebug
+lunch lineage_rosemary-userdebug
 export TZ=Asia/Jakarta #put before last build command
 make bacon
 
