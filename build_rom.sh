86423fa4c67d35e81a608a538a96261c9a02410f
Orkun Ergün <68orkun@proton.me>
2022-12-08 19:13:48 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,18 +1,18 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://gitlab.com/R9Lab/Manifest.git --depth 1 -b EvoX-13 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExtended/manifest -b trece -g default,-mips,-darwin,-notdefault
+git clone https://gitlab.com/R9Lab/Manifest.git --depth 1 -b PEX-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch evolution_lava-userdebug
+lunch aosp_lava-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export KBUILD_BUILD_USER=Orkun
 export KBUILD_BUILD_HOST=Cirrus 
 export BUILD_USERNAME=Orkun
 export BUILD_HOSTNAME=Cirrus
 export TZ=Europe/Istanbul #put before last build command
-mka evolution
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
