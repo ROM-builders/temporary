58b632294e0f62f9b3e59ffa310418d8e83bd5da
Om Patel <89215836+takumi021@users.noreply.github.com>
2022-12-09 10:22:01 +0530

    r5x: Start a13

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-export TZ=Asia/Delhi #put before last build command
+export TZ=Asia/Delhi #put before build command
 lunch arrow_r5x-userdebug
 m bacon
 
