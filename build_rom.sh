408d205b163306e95bddbbec40471632fe86d7d1
EmadGr8 <91535171+EmadGr8@users.noreply.github.com>
2022-12-09 10:19:18 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/EmadGr8/local_manifest.git --depth 1 -b EvoX .repo/local_manifests
+git clone https://github.com/EmadGr8/local_manifest.git --depth 1 -b Evo-LOS19 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
