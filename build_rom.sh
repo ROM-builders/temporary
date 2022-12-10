51a3c4230324345e8b78bc955358f8a1cb75ea7e
EmadGr8 <91535171+EmadGr8@users.noreply.github.com>
2022-12-10 21:41:07 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/EmadGr8/local_manifest.git --depth 1 -b EvoLOS19 .r
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+. build/envsetup.sh
 breakfast tulip
 export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
