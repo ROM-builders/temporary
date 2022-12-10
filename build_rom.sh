69e3c82d2eb4e438e533210e87a72b4e1215d100
EmadGr8 <91535171+EmadGr8@users.noreply.github.com>
2022-12-10 21:47:28 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-breakfast evolution_tulip_userdebug
+breakfast evolution_tulip-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export RELAX_USES_LIBRARY_CHECK=true
