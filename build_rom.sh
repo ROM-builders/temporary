e63fe666ac97f0abd5e0d34388383409862e4319
GhostMaster69-dev <rathore6375@gmail.com>
2022-12-10 18:51:04 +0530

    Test Changes

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 #export ARROW_GAPPS=true
-export TARGET_DISABLE_POSTRENDER_CLEANUP=true
 lunch arrow_vince-user
 export TZ=Asia/Kolkata #put before last build command
 m bacon
