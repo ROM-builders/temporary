6c4f962157f3ae6601d3d0aa91d3225c8b779bf2
EmadGr8 <91535171+EmadGr8@users.noreply.github.com>
2022-12-10 21:45:27 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,13 +4,13 @@ git clone https://github.com/EmadGr8/local_manifest.git --depth 1 -b EvoLOS19 .r
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-. build/envsetup.sh
-breakfast tulip
+source build/envsetup.sh
+breakfast evolution_tulip_userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export RELAX_USES_LIBRARY_CHECK=true
 export TZ=Asia/Dhaka
-make bacon
+brunch tulip
 
 # upload rom
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
