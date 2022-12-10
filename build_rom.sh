d5acdae7069de01829f4cd4a0b5bb5e9e693c8fb
Sachin752002 <103373100+Sachin752002@users.noreply.github.com>
2022-12-10 13:44:00 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b pyro -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Sachin752002/local_manifest.git --depth 1 -b Spark .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExtended/manifest -b trece -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Sachin752002/local_manifest.git --depth 1 -b PexT .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch spark_X01AD-userdebug
+lunch aosp_X01AD-userdebug
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 mka bacon
 
