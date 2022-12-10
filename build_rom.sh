957b91a1fd23d71bf717cd8a906debc578380b9c
galang8664 <95033122+galang8664@users.noreply.github.com>
2022-12-10 16:14:52 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,12 @@
 # sync rom
 repo init -u https://github.com/crdroidandroid/android.git -b 13.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/galang8664/local_manifest.git --depth 1 -b crd13 .repo/local_manifests
+git clone https://github.com/galang8664/local_manifest.git --depth 1 -b crd-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
 lunch lineage_lava-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
