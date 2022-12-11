bc7f2fa4d007ef56d7105182d1d25a1d8ffabcec
EmadGr8 <91535171+EmadGr8@users.noreply.github.com>
2022-12-11 15:25:41 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,12 +5,12 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-breakfast evolution_tulip-userdebug
+breakfast lineage_twolip-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export RELAX_USES_LIBRARY_CHECK=true
 export TZ=Asia/Dhaka
-brunch tulip
+brunch twolip
 
 # upload rom
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
