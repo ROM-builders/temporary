ce6f360ddee9f1deedf29bd13aec26c33fd16ef8
Alec Chan <88945170+alecchangod@users.noreply.github.com>
2022-12-12 20:04:53 +0800

    Test vendor

--- build_rom.sh
+++ build_rom.sh
@@ -9,7 +9,7 @@ lunch xtended_vangogh-user
 # export WITH_GAPPS=true
 export TZ=Asia/Hong_Kong #put before last build command
 make xtended
-# 3
+# 4
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
 
