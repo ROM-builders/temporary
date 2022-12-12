69ca2b8c790ada2677e6db193782dd9934c477e0
Des3rve <119830528+Des3rve@users.noreply.github.com>
2022-12-12 18:23:34 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 11.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Des3rve/local_manifest.git --depth 1 -b crdroid .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Des3rve/local_manifest.git --depth 1 -b ricedroid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -8,8 +8,8 @@ source build/envsetup.sh
 lunch lineage_rosemary-userdebug
 export BUILD_USERNAME=Des3rve
 export BUILD_HOSTNAME=cirrus
-export TZ=Asia/Jakarta #put before last build command
-make bacon
+export TZ=Asia/Dhaka #put before last build command
+brunch rosemary
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
