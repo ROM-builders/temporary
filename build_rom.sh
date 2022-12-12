029d8fe0a12729ad0a4c3c0d891572a5482d1033
xack21 <86214526+xack21@users.noreply.github.com>
2022-12-12 21:56:12 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch arrow_fog-userdebug
+lunch 
 m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
