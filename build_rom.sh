24d2e65d04f5935b98b064c46a036b036ed5f082
EmadGr8 <91535171+EmadGr8@users.noreply.github.com>
2022-12-13 19:15:23 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/EmadGr8/local_manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/EmadGr8/local_manifest.git --depth 1 -b Z3LOS .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_twolip-user
+lunch lineage_z3dual-user
 export TZ=Asia/Dhaka
 make bacon
 
