e2af8ad0cbd57b0ed411fcee6be002ff2a4b8c20
Nem1x <90353607+Nem1xx@users.noreply.github.com>
2022-12-13 19:40:06 +0100

    mistakes have been made

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 11.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Nem1xx/local_manifests.git -b crdroid-11.0 --depth 1 -b lineage-18.1 .repo/local_manifests
+git clone https://github.com/Nem1xx/local_manifests.git -b crdroid-11.0 --depth 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
