809c1dadb4bb2f9ab819f605c61c41fc9fb90ff0
Atul9977 <104277401+Atul9977@users.noreply.github.com>
2022-12-14 00:01:11 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -11,7 +11,7 @@ lunch arrow_garden-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export TZ=Asia/Kolkata #put before last build command
-m bacon
+m otapackage
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
