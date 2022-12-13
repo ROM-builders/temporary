35810e5314bae2631ac4ef2667e5edafe2c0f921
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2022-12-13 01:46:47 -0300

    m23xq-ProjectBlaze-12.1-Aflaungos

--- build_rom.sh
+++ build_rom.sh
@@ -7,11 +7,9 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-export TZ=Asia/Dhaka
-export KBUILD_BUILD_USER=PaperBoy
-export BUILD_HOSTNAME=PaperBoy #put before last build command
+export TZ=Asia/Dhaka #put before last build command
 lunch blaze_m23xq-userdebug
-brunch m23xq
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
