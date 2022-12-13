6f944a767742d29cae5d04fbfbeb2cfe650495b5
Yoni Asleno Farid <92686606+aslenofarid@users.noreply.github.com>
2022-12-13 15:43:56 +0700

    Typo

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-19.1 11 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/aslenofarid/local_manifest.git --depth 1 -b lineage-19.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build roms
 source build/envsetup.sh
 lunch lineage_X00TD-userdebug
 export BUILD_USERNAME=aslenofarid
