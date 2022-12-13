e6fc2114b44da2d87effa06c4037c0b586fb6b4f
Prasad Haardy <114338429+PrasadHaardy@users.noreply.github.com>
2022-12-13 08:13:31 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/CipherOS/android_mani
 git clone https://github.com/JaswantTeja/local_manifest.git --depth=1 -b cipher .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom 
 source build/envsetup.sh
 lunch cipher_r5x-userdebug
 export TZ=Asia/Kolkata #put before last build command
