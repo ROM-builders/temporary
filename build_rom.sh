b3535fc88fc3a27e07b2f69cef0bcceb0cc76c32
Raiden Ishigami <contact.raidenishi69@gmail.com>
2022-12-14 04:19:44 -0300

    Attempt #1 cherish-OS

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 13.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/raidenkkj/local_manifest.git --depth 1 -b crd9-mithorium .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
+git clone https://github.com/raidenkkj/local_manifest.git --depth 1 -b cherish13-mithorium .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom 5
+# build rom 0
 . build/envsetup.sh
-lunch lineage_Mi439-userdebug
+export CHERISH_VANILLA=true
 export TZ=Asia/Dhaka #put before last build command
-make bacon
+brunch Mi439
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
