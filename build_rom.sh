fbdfb9ed1c96bd9850184e0991eb041704f13235
Kneba <abenkenary3@gmail.com>
2022-12-15 00:36:15 +0700

    reflash

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch lineage_X00TD-userdebug
-export TZ=Asia/Dhaka #put before last build command
+export TZ=Asia/Shanghai #put before last build command
 brunch X00TD
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
