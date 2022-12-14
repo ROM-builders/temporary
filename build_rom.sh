b1ea59bbdb86a49321f89d439d08e05d7890a5a7
Ǥђ๏ຮ₮⌁Ⲙครtє࿐ <rathore6375@gmail.com>
2022-12-14 21:52:56 +0530

    Build Arrow

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 #export ARROW_GAPPS=true
 lunch arrow_vince-user
-export TZ=Asia/Kolkata #put before last build command
+export TZ=Asia/Kolkata
 m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
