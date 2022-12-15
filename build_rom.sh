6e68c5ca23857f19d4bf735534a3e085c4195f2f
Wasik Ahmmed Fahm <62275326+OmegaBeam69@users.noreply.github.com>
2022-12-15 12:42:53 +0600

    raphael-Spark-Rom-pyro-OmegaBeam69

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Bootleggers-BrokenLab/manifest.git -b tirimbino -g default,-mips,-darwin,-notdefault
-git clone https://github.com/WasikFahim69/local_manifest.git --depth 1 -b bootleg .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com//Spark-Rom/manifest.git -b pyro -g default,-mips,-darwin,-notdefault
+git clone https://github.com/OmegaBeam69/local_manifest.git --depth 1 -b spark .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch bootleg_raphael-userdebug
+lunch spark_raphael-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
