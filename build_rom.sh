842a2ecb929001adc2e2436d63b678f9bfa43946
alternoegraha <37102951+alternoegraha@users.noreply.github.com>
2022-12-17 05:54:43 +0700

    Kaleidoscope!

--- build_rom.sh
+++ build_rom.sh
@@ -1,17 +1,17 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/alternoegraha/local_manifest.git --depth 1 -b lineage19_vndk30 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Kaleidoscope/android_manifest.git -b sunflowerleaf -g default,-mips,-darwin,-notdefault
+git clone https://github.com/alternoegraha/local_manifest.git --depth 1 -b kscope_fog .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 export KBUILD_BUILD_USER=alternoegraha
-export KBUILD_BUILD_HOST=cringe
+export KBUILD_BUILD_HOST=cirrus
 export BUILD_USERNAME=alternoegraha
-export BUILD_HOSTNAME=cringe
+export BUILD_HOSTNAME=cirrus
 . build/envsetup.sh
-lunch lineage_garden-userdebug
+lunch kscope_fog-userdebug
 export TZ=Asia/Jakarta #put before last build command
-m bacon
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
