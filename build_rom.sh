165c09f7ddd9361226caf4aec5f0837c11cbbfb1
Nem1x <90353607+Nem1xx@users.noreply.github.com>
2022-12-16 18:51:40 +0100

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom
+# sync crdroid
 repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 11.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Nem1xx/local_manifests.git -b crdroid-11.0 --depth 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
