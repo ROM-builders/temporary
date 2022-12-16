662cdea8b9fc177569d5089ceb224c487d345b8e
Atharv Negi <94751339+atharvnegi@users.noreply.github.com>
2022-12-16 22:16:56 +0530

    m11q-LineageOs-18.1-atharv2951

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/atharvnegi/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_selene-userdebug
+lunch lineage_m11q-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
