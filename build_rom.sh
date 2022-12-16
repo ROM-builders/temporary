d8a92c0f132de56a424400a3584e9c5b48ffbaed
Rinto Khan <114152118+rinto02@users.noreply.github.com>
2022-12-16 14:05:03 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/Evolution-X/manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://github.com/rinto02/local_manifest --depth 1 -b main .repo/local_manifests
+git clone https://github.com/rinto02/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 # build rom
 source build/envsetup.sh
