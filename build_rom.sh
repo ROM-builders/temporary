73bbc24b5a22f6307974f1b1d1821d25b2d7e664
RadeonPS <88305032+RadeonPS@users.noreply.github.com>
2022-12-18 12:32:02 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/PixelExperience/manifest.git -b twelve-plus -g default,-mips,-darwin,-notdefault
-git clone https://github.com/RadeonPS/local_manifest.git --depth 1 -b beryllium_pe12 .repo/local_manifests
+#git clone https://github.com/RadeonPS/local_manifest.git --depth 1 -b beryllium_pe12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch device_xiaomi_beryllium
+lunch aosp_beryllium-userdebug
 export TZ=Asia/Mumbai #put before last build command
 mka bacon 
 
