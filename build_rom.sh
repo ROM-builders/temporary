30a8a306a3c21bfae70d9bcf64e9ea8f0df4254b
AlexandruSemina(semialex16) <96374040+alexandrusemina@users.noreply.github.com>
2022-12-18 03:29:44 -0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/Spark-Rom/manifest -b pyro -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest.git -b pyro -g default,-mips,-darwin,-notdefault
 git clone https://github.com/alexandrusemina/local_manifest.git .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
