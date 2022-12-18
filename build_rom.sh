84195c37e3dbe8317ee2c2290ba9de96aa4650df
AlexandruSemina(semialex16) <96374040+alexandrusemina@users.noreply.github.com>
2022-12-18 09:27:36 +0200

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/Spark-Rom/manifest -b pyro -g default,-mips,-darwin,-notdefault
-git clone https://github.com/alexandrusemina/local_manifest.git .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+git clone https://github.com/alexandrusemina/device_xiaomi_veux device/xiaomi/veux
 
 # build rom
 source build/envsetup.sh
