27cf4f488154386e43e257aa732263c99cf497e9
ThunderBolt <93515519+ironHeart6@users.noreply.github.com>
2022-12-20 00:04:30 +0600

    Build pe

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b thirteen-plus -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Amritorock/local_manifest --depth 1 -b Pe .repo/local_manifests
+git clone https://github.com/takumi021/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
