2de2e97abe3eab5deff3f994e78cb74ec03fcf92
S ∆ U R ∆ V <90666710+SauRavRwT@users.noreply.github.com>
2022-12-19 22:19:25 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init -u https://github.com/Project-Xtended/manifest.git -b xt -g default,-mips,-darwin,-notdefault
-git clone https://github.com/SauRavRwT/Local-Manifest.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/SauRavRwT/Local-Manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
