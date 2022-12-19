c123b92837e862c867023c29f6c7d4668b1ebb56
Atharv Negi <94751339+atharvnegi@users.noreply.github.com>
2022-12-19 19:49:57 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_m11q-userdebug
-export TZ=Asia/Delhi #put before last build command
+export TZ=Asia/Dehradun #put before last build command
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
