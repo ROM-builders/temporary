0dfa8008cc285dff7e48425b7683c55f182d9361
Haikal Luthfianino Balukia <51993230+hklknz@users.noreply.github.com>
2022-12-19 12:22:01 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync
+# sync muehehehe
 repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-oss/xd_manifest -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/hklknz/Local-Manifests.git --depth 1 -b tissot .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
