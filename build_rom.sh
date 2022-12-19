3c1624235906ae94dda37aee3c1d5c9643a8020e
Sourav <thefallnn@gmail.com>
2022-12-19 20:38:58 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch  superior_veux-userdebug
+lunch superior_veux-userdebug
 export TZ=Asia/Kolkata #put before last build command
 m bacon
 
