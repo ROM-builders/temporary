3f21c898bc0a9e9efcac8e06290eacc356f20b67
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2022-12-19 01:20:10 -0300

    m23xq-ArrowOS-arrow-12.1-Aflaungos

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,16 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u repo init -u repo init -u https://github.com/LineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-12.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Aflaungos/local_manifest.git --depth 1 -b m23xq-LineageOS-19.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 #17
 
-# build rom
-source build/envsetup.sh
+. build/envsetup.sh
+lunch arrow_m23xq-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
-export TZ=Asia/Dhaka #put before last build command
-lunch lineage_m23xq-userdebug
-make bacon
+export ARROW_GAPPS=true
+export TZ=Asia/Kolkata #put before last build command
+m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
