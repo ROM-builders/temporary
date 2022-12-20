6641087202ad2a82a6d16a16a7762adf52745685
Arafattex <69374795+Arafattex@users.noreply.github.com>
2022-12-20 06:23:25 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom 
+# sync rom  
 repo init --depth=1 --no-repo-verify -u https://github.com/Corvus-AOSP/android_manifest.git -b 13 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Arafattex/local_manifest.git --depth 1 -b Lancelot_corvus .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
