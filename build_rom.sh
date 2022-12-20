e34e99ea59e3c35795e470835172e509adb282b9
EmadGr8 <91535171+EmadGr8@users.noreply.github.com>
2022-12-20 22:53:40 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch aosp_tulip-user
+lunch arrow_tulip-user
 export TZ=Asia/Dhaka
 m bacon
 
