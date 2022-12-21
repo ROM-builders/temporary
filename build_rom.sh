ed832732e9ccf9e5b1b64c05dafe40c552db5d93
Ryofry <106879576+Ryofry@users.noreply.github.com>
2022-12-21 11:46:03 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Ryofry/local_manifest.git --depth 1 -b surya-TheStretchh-DT-Ryofry .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/conquerOS/manifest.git -b tithonia -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Ryofry/local_manifest.git --depth 1 -b surya-rd-stuffs-dt-ryofry .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_surya-userdebug
-export TZ=Asia/Dhaka #put before last build command
-mka bacon
+lunch conquer_surya-userdebug
+export TZ=Asia/Jakarta #put before last build command
+mka carthage
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
