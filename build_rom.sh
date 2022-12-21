17a232bdbe7b7f81bf12e57c19792ba1836f926b
Lora <121148299+nullprjkt@users.noreply.github.com>
2022-12-21 18:11:29 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,8 +1,7 @@
 # THIS BUILD IS MEANT FOR PERSONAL USE. Anything I included here are all properly credited.
-# Attempt 2.
+# 3rd attempt.
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-oss/xd_manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
-# (DT made by hsx02.. I will make my own soon.)
 git clone https://github.com/nullprjkt/local_manifest.git --depth 1 -b xd-spes .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
