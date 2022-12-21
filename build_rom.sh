8f354f7392f47c2e9db964f18e3ebe075fafbd25
Sachin9822 <70788804+Sachin9822@users.noreply.github.com>
2022-12-21 06:57:42 +0530

    init build

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/CipherOS/android_manifest.git -b thirteen -g default,-mips,-darwin,-notdefault 
-git clone https://github.com/Sachin9822/local_manifest.git --depth 1 -b cipher-13 .repo/local_manifests  
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b thirteen-plus -g default,-mips,-darwin,-notdefault 
+git clone https://github.com/Sachin9822/local_manifest.git --depth 1 -b pixel .repo/local_manifests  
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
      
 # build rom     
