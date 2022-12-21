1eb0602b8099f0daac37986596ae12874a22882f
Tejas Singh <tejassingh649@gmail.com>
2022-12-21 12:30:10 +0530

    Trigger Build

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch xtended_ginkgo-userdebug
 export WITH_GAPPS=true
-export TZ=Asia/Delhi #put before last build command
+export TZ=Asia/Delhi #put before last build command 
 make xtended
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
