1c14b6cad0803fd088deda4442d546a99e8b96cd
j0ok34n <103942268+j0ok34n@users.noreply.github.com>
2022-12-22 16:31:32 +0700

    miatoll-Project-Awaken-triton-j0ok34n

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/j0ok34n/local_manifests.git --depth 1 -b awaken-local .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b triton -g default,-mips,-darwin,-notdefault
+git clone https://github.com/j0ok34n/local_manifestw.git --depth 1 -b awaken-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
