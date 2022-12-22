b642f4985d27027916574e999d86765450e9ae56
Fr0ztyy43 <82140208+Fr0ztyy43@users.noreply.github.com>
2022-12-22 19:16:30 +0100

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/androi
 git clone https://github.com/Fr0ztyy43/local_manifests.git --depth 1 -b cr11 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build romff
+# build romf
 source build/envsetup.sh
 lunch lineage_begonia-userdebug
 export TZ=Asia/Dhaka #put before last build command
