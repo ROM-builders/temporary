1a6a31e02999717925894ae08122f227355e6adb
Notkeret <jerespn@gmail.com>
2022-12-23 09:46:09 -0400

    Cdroid for fog

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Corvus-AOSP/android_manifest.git -b 13 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Notkerd69/local_manifest.git --depth 1 -b cobus-quetejodan .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 13.0 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Notkerd69/local_manifest.git --depth 1 -b los-20 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -9,11 +9,11 @@ export KBUILD_BUILD_HOST=cirrushet
 export BUILD_USERNAME=Notkerd69
 export BUILD_HOSTNAME=cirrushet
 source build/envsetup.sh
-lunch corvus_fog-userdebug
+lunch lineage_fog-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=America/Caracas #put before last build command
-make corvus
+brunch fog
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
