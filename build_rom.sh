0ea3fbde4a31df12e6b17e32cc4fea40acc0eadd
EmadGr8 <91535171+EmadGr8@users.noreply.github.com>
2022-12-23 05:25:52 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,8 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_twolip-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
+lunch lineage_twolip-user
 export TZ=Asia/Dhaka
 make bacon
 
