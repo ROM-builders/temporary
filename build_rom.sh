9dac55f4f8e9ce0d1cd9ed304495517323f0c79b
Anandhasayanan <anandhasayanan007jb@gmail.com>
2022-12-23 18:35:53 +0530

    Fixes

--- build_rom.sh
+++ build_rom.sh
@@ -3,9 +3,9 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b
 git clone https://github.com/anandhan07/local_manifest.git --depth 1 -b spark-13.0 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom [30]
+# build rom [31]
 . build/envsetup.sh
-lunch spark_vince-user
+lunch spark_vince-userdebug
 export WITH_GAPPS=true
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
