77b5860378f998c6818c818bfda90ce776263724
Sachin9822 <70788804+Sachin9822@users.noreply.github.com>
2022-12-24 09:05:10 +0530

    retry 3

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
      
 # build rom     
 source build/envsetup.sh   
-lunch cipher_tissot-userdebug   
+lunch voltage_tissot-userdebug   
 export TZ=Asia/Dhaka #put before last build command   
 mka bacon  
   
