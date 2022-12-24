f6a290310713583537cc2f971af6e2e343f00a51
Ryofry <106879576+Ryofry@users.noreply.github.com>
2022-12-24 18:05:13 +0700

    surya

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch arrow_surya-userdebug
 export ARROW_GAPPS=false
-export TZ=Asia/Jakarta #put before last build command
+export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
