a4b851db4d16cc8759e5b0419637c1a5fe936b5f
SKetU <sketu@donot.care>
2022-12-24 12:22:06 +0530

    Superior time

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/azberto/local_manifest --depth 1 -b superior .repo/local_manifests
+git clone https://github.com/SKetU-l/local_manifest-1 --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
   
