cd18c6fe4377bad766c239e0af556a51bb7fc4ee
Ryofry <106879576+Ryofry@users.noreply.github.com>
2022-12-24 17:59:22 +0700

    surya-arrow-13.0-ryofry

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch arrow_surya-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
 export ARROW_GAPPS=false
 export TZ=Asia/Jakarta #put before last build command
 mka bacon
