eed1397d6c4a0225a047aacf2dac90724e6ebb1a
Ryofry <106879576+Ryofry@users.noreply.github.com>
2022-12-24 18:36:55 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch arrow_surya-userdebug
+lunch arrow_surya-user
 export ARROW_GAPPS=false
 export TZ=Asia/Jakarta #put before last build command
 mka bacon
