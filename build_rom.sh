3b63a5247a50ef6dec3a890cb4a70797239b6744
alternoegraha <37102951+alternoegraha@users.noreply.github.com>
2022-12-25 09:41:59 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/alternoegraha/local_manifest.git --depth 1 -b lineage19_vndk30 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-13.0 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/alternoegraha/local_manifest.git --depth 1 -b arrow_fog .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_garden-userdebug
+lunch arrow_fog-userdebug
 export KBUILD_BUILD_USER=alternoegraha
 export KBUILD_BUILD_HOST=cirrus
 export BUILD_USERNAME=alternoegraha
