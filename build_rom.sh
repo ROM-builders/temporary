1754c5be43dc4bbec1c25124b7ba7befe2dcaa4b
Adalat222 <118736057+Adalat222@users.noreply.github.com>
2022-12-25 10:44:49 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 #export TZ=Asia/Dhaka #put before last build command
-lunch evolution_gauguin-user
+lunch evolution_gauguin-userdebug
 #build code
 mka evolution
 
