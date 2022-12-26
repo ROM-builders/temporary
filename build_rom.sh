6788014fe5cccc6565592e95ca9aa8eb5f0952c6
Emman <95723794+emmanueeeeeel@users.noreply.github.com>
2022-12-26 20:45:21 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init -u https://github.com/ForkLineageOS/android.git -b lineage-19.1
+repo init --depth=1 -u https://github.com/catalyst-android/android.git -b 13
 git clone https://github.com/emmanueeeeeel/local_manifest --depth 1 -b master .repo/local_manifest
-repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
+repo sync --current-branch --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune -j `nproc`
 
 # build rom
-source build/envsetup.sh
-lunch lineage_lava-userdebug
+. build/envsetup.sh
+lunch catalyst_lava-userdebug
 export TZ=Asia/Dhaka #put before last build command
-make bacon
+m bacon -j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
