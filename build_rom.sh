166971c0cafc8c65ba1f89c7f2c64175eb6dce67
Atul977 <121159372+Atul977@users.noreply.github.com>
2022-12-26 14:00:48 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 export KBUILD_BUILD_USER=Atul
 export BUILD_USERNAME=Atul
 source build/envsetup.sh
-lunch aosp_garden-userdebug
+lunch aicp_garden-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export TZ=Asia/Dhaka #put before last build command
