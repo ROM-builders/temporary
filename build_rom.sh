c31b23f33a785e774f24475bbce451786246d839
Emman <95723794+emmanueeeeeel@users.noreply.github.com>
2022-12-27 05:05:54 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
 repo init --depth=1 -u https://github.com/catalyst-android/android.git -b 13 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/emmanueeeeeel/local_manifest --depth 1 -b master .repo/local_manifest
-repo sync --current-branch --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune `nproc`
+repo sync --current-branch --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune -j `nproc`
 
 # build rom
 . build/envsetup.sh
 lunch catalyst_lava-userdebug
 export TZ=Asia/Dhaka #put before last build command
-m bacon -j$(nproc --all)
+m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
