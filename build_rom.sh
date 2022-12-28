359a828d178d93c45b251452fd2a986a54d78fca
boedhack99 <boedhack@gmail.com>
2022-12-28 10:24:37 +0700

    build: Move BT profiles to vendor props

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-i# sync rom
+ii# sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-13.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/boedhack99/local_manifest.git --depth 1 -b arrow-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
@@ -11,3 +11,4 @@ mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+
