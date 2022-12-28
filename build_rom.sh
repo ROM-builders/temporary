06d43454ddc180d1002260c6bc7800fc18da1292
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2022-12-28 20:35:09 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,6 @@
 # sync rom
-repo init -u https://github.com/LineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
+repo init -u https://github.com/PixelExperience/manifest -b thirteen
+ -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Spector0/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
