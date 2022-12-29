337ce4cb7cc62e1cf46c5b689a2a66abd9c319d1
Wendi Wardani <86230878+dankelno@users.noreply.github.com>
2022-12-30 03:20:16 +0700

    X01AD-Xdroid-dankelno

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/Xdroid/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-CAF/xd_manifest -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/dankelno/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
