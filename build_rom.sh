8576c1631d17b652613db3022b56cfcfa1ca4e19
Wendi Wardani <86230878+dankelno@users.noreply.github.com>
2022-12-30 06:58:24 +0700

    X01AD-NusantaraProject-ROM-Dankelno

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/NusantaraProject-ROM/android_manifest -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/dankelno/local_manifest --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/NusantaraProject-ROM/android_manifest.git -b 11 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/dankelno/local_manifest.git --depth 1 -b Nusa-11.0 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch dankelno_nusantara-userdebug
+lunch X01AD_nusantara-userdebug
 export TZ=Asia/Jakarta #put before last build command
 mka derp
 
