ab30ef42566b4178654dc95db367ead5d697c2d2
Wendi Wardani <86230878+Wrdn28@users.noreply.github.com>
2022-12-30 16:43:40 +0700

    X01AD-Crdroidandroid-11.0-Wrdn28

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/xdroid-CAF/xd_manifest -b eleven -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/crdroidandroid/android.git -b 11.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Wrdn28/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch X01AD_Xdroid-userdebug
+lunch crdroid_x01ad-userdebug
 export TZ=Asia/Jakarta #put before last build command
 mka derp
 
