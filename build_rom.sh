4ed392e9a746d905293e440933ce3d9a2357a5dd
EmadGr8 <91535171+EmadGr8@users.noreply.github.com>
2022-12-30 20:30:19 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,9 +5,9 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_z3dual-user
+breakfast lineage_z3dual-user
 export TZ=Asia/Dhaka
-make bacon
+brunch z3dual
 
 # upload rom
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
