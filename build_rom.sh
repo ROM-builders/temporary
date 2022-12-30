a0939e6154379d1924c01e9ad25b7d532a69f050
EmadGr8 <91535171+EmadGr8@users.noreply.github.com>
2022-12-30 10:33:47 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -2,6 +2,7 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/EmadGr8/local_manifest.git --depth 1 -b Z3LOS .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+rm -rf packages/apps/FMRadio
 
 # build rom
 source build/envsetup.sh
