e6ba4e87f894011cacc24a4df3303611fbf1dd88
Wendi Wardani <86230878+Wrdn28@users.noreply.github.com>
2022-12-30 14:31:56 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch X01AD_crandroid-userdebug
+lunch crdroid_x01ad-userdebug
 export TZ=Asia/Jakarta #put before last build command
 mka derp
 
