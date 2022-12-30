f630a1aa926397d145d299ab503a89f95e4982c4
Adalat222 <adalatkumar2580@gmail.com>
2022-12-30 06:16:28 +0530

    test

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/Adalat222/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch derp_vayu-userdebug
+lunch derp_gauguin-userdebug
 export TZ=Asia/Delhi #put before last build command
 mka derp
 
