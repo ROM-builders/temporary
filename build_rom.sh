41171331aec821f0141d52317fbdb19a0340d018
Wendi Wardani <86230878+Wrdn28@users.noreply.github.com>
2022-12-30 17:07:03 +0700

    X01AD-Crdroidandroid-Wrdn28

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/crdroidandroid/android.
 git clone https://github.com/Wrdn28/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build romm
 source build/envsetup.sh
 lunch lineage_X01AD-userdebug
 export TZ=Asia/Jakarta #put before last build command
