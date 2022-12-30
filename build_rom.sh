98b4a1edb78d9a8b49e5615919c7e01fd87f1a20
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2022-12-30 14:49:09 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init -u https://github.com/PixelExperience/manifest -b thirteen
  -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Spector0/local_manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/phhgsi/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
