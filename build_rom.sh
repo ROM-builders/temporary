cb9cca1fd29143bdd42995890ea8e43e046739b0
Alec Chan <88945170+alecchangod@users.noreply.github.com>
2023-01-01 03:31:29 +0800

    aospa

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Project-Xtended/manifest.git -b xt -g default,-mips,-darwin,-notdefault
-git clone https://github.com/alecchangod/local_manifest.git --depth 1 -b xtended .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest -b topaz -g default,-mips,-darwin,-notdefault
+git clone https://github.com/alecchangod/local_manifest.git --depth 1 -b aospa .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
  
 # build rom
 source build/envsetup.sh
-lunch xtended_monet-user
+lunch aospa_monet-user
 # export WITH_GAPPS=true
 export TZ=Asia/Hong_Kong #put before last build command
-make xtended
-# 22
+./rom-build.sh monet -c -t user -v release
+# 1
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
 
