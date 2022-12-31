214a41bca8fe4a02ae50437306a23bd4764d39e0
Muhammed Salih Pelit <91677076+msalihpelit@users.noreply.github.com>
2022-12-31 10:10:01 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch spark_ginkgo-user
+lunch spark_ginkgo-userdebug
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
