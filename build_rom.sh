f2499622b4f58121733a2be410f6bfc50423a37e
alternoegraha <37102951+alternoegraha@users.noreply.github.com>
2023-01-02 10:39:38 +0700

    try 404

--- build_rom.sh
+++ build_rom.sh
@@ -1,17 +1,17 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/alternoegraha/local_manifest.git --depth 1 -b lineage19_vndk30 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/P-404/android_manifest.git -b tokui -g default,-mips,-darwin,-notdefault
+git clone https://github.com/alternoegraha/local_manifest.git --depth 1 -b p404 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_garden-userdebug
+lunch p404_fog-userdebug
 export KBUILD_BUILD_USER=alternoegraha
 export KBUILD_BUILD_HOST=cirrus
 export BUILD_USERNAME=alternoegraha
 export BUILD_HOSTNAME=cirrus
 export TZ=Asia/Jakarta #put before last build command
-m bacon
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
