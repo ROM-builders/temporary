0dcbc6facc34b20c22df0dbcf31387baf3676689
S ∆ U R ∆ V <90666710+SauRavRwT@users.noreply.github.com>
2023-01-03 15:33:36 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/SauRavRwT/Local-Manifest.git --depth 1 -b blaze .re
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 #7
-
+#8
 
 # build rom
 source build/envsetup.sh
