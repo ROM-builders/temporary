e094be40c4eee3f3d79b5c856e0f3d71f70a6c51
Nothing x Dev <85100277+NothingxDev@users.noreply.github.com>
2023-01-03 22:07:18 +0530

    Spacewar-PixelExperience-thirteen-NothingxDev

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/PixelExperience/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/NothingxDev/local_manifest.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b thirteen -g default,-mips,-darwin,-notdefault
+git clone https://github.com/NothingxDev/local_manifest --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
