c12a928d27dab12eff483dd7fd722479de2e7531
Jony <108582874+SimpleJony@users.noreply.github.com>
2023-01-05 14:10:50 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manif
 git clone https://github.com/SimpleJony/local_manifest --depth=1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom 
+# build rom
 . build/envsetup.sh
 lunch arrow_phoenix-userdebug
 export TZ=Asia/Shanghai #put before last build command
