1b52e8f4daaf20496948e9e7e503995513e8e3b7
EmadGr8 <91535171+EmadGr8@users.noreply.github.com>
2023-01-06 22:09:56 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-13.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/EmadGr8/local_manifest.git --depth 1 -b Arrowfixed .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Elixir/manifest -b Tiramisu -g default,-mips,-darwin,-notdefault
+git clone https://github.com/EmadGr8/local_manifest.git --depth 1 -b aosp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch arrow_tulip-user
+lunch aosp_tulip-user
 export TZ=Asia/Dhaka
-m bacon
+mka bacon
 
 # upload rom
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
