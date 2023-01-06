f9be9b16e02b29541394ae8df208ae862f4e0822
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2023-01-06 12:06:33 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest.git -b thirteen-plus -g default,-mips,-darwin,-notdefault
-git clone https://github.com/phhgsi/local_manifest.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/rombuilder/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_oscar-userdebug
+lunch aosp_holi-userdebug
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
