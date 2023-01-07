eea35205b766490e20fa6dd5a50b65f6db530031
Rinto Khan <114152118+rinto02@users.noreply.github.com>
2023-01-07 08:40:01 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,6 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch derp_RMX2020-userdebug
 export SELINUX_IGNORE_NEVERALLOW=true
+export USE_CUSTOM_CLANG=true
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
