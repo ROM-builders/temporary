b9552f876f77df8f47207fa677338e30ce2fd2c4
Haikal Luthfianino Balukia <51993230+hklknz@users.noreply.github.com>
2023-01-08 03:52:50 +0700

    tissot-xdroid-oss-twelve-hklknz

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync roms
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-AOSP/manifest.git -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/hklknz/Local-Manifests.git --depth 1 -b tissot-pos .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-oss/xd_manifest -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/hklknz/Local-Manifests.git --depth 1 -b tissot-xd .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build roms
 source build/envsetup.sh
-lunch aosp_tissot-userdebug
-export TZ=Asia/Tokyo #put before last build command
+lunch xdroid_tissot-userdebug
+export TZ=Asia/Tokyo
 export BUILD_BROKEN_DUP_RULES=true
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES=true
@@ -18,7 +18,7 @@ export BUILD_USERNAME=Honoka
 export BUILD_HOSTNAME=Aquors
 export KBUILD_BUILD_USER=Honoka
 export KBUILD_BUILD_HOST=Aquors
-mka bacon
+mka xd
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
