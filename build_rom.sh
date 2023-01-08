68fadc154bf47a87b10e8cdcbf678538c5eb6396
root <don.boruza@gmail.com>
2023-01-08 17:20:58 +0000

    build: fix fastbootpkg build tasks missing separator

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/CipherOS/android_mani
 git clone https://github.com/donboruza/local_manifests.git --depth 1 -b cipher-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom 
 source build/envsetup.sh
 lunch cipher_sweet-userdebug
 export BUILD_USERNAME=don.boruza
