61ec6b15c2e2ae853f9328a92d68a1a264f1c22d
snnbyyds <87487115+snnbyyds@users.noreply.github.com>
2023-01-08 21:22:40 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,6 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 bash ./fun/blobs.sh
+croot
 lunch aosp_fajita-user
 export TZ=Asia/Shanghai #put before last build command
 mka bacon
