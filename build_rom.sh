a8573c9d75999f2b0f62aae2413353f834116e23
kitw4y <91876095+kitw4y@users.noreply.github.com>
2023-01-08 19:56:07 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://github.com/kitw4y/local_manifest.git --depth 1 -b evox-r .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/kitw4y/local_manifest.git --depth 1 -b derp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 repo sync -j1 --fail-fast
 
 # build rom 
 source build/envsetup.sh
-lunch evolution_lancelot-userdebug
+lunch derp_lancelot-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
-export TZ=Asia/Kolkata #put before last build command
-mka evolution
+export TZ=Asia/Jakarta #put before last build command
+mka derp
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
