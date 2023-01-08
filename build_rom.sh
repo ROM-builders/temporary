34a7be690e8a507db7d0c5746483e951aa6c25ed
Dinesh <dinesh2742004@gmail.com>
2023-01-08 17:04:38 +0530

    santoni-Spark-Rom-pyro-DineshMSDian
    Retry

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch spark_santoni-userdebug
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export TZ=Asia/Chennai #put before last build command
 mka bacon
 
