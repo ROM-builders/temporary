f7393817e8c8d98dca91eb034dbb60ddf4be44e3
snnbyyds <87487115+snnbyyds@users.noreply.github.com>
2023-01-10 07:10:02 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,9 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 bash ./fun/blobs.sh
+
 croot
+
 lunch arrow_fajita-user
 export KBUILD_BUILD_USER=snnbyyds
 export BUILD_USERNAME=snnbyyds
