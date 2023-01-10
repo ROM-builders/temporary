f21937092f28d0b23bc734bf3b983237ee2d0286
Dinesh <dinesh2742004@gmail.com>
2023-01-10 18:16:40 +0530

    santoni-PixelExperience-thirteen-plus-DineshMSDian

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b pyro -g default,-mips,-darwin,-notdefault 
-git clone https://github.com/DineshMSDian/local_manifest --depth 1 -b Spark .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b thirteen-plus -g default,-mips,-darwin,-notdefault 
+git clone https://github.com/DineshMSDian/local_manifest --depth 1 -b aosp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch spark_santoni-userdebug
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+lunch aosp_santoni-userdebug
+#export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export TZ=Asia/Chennai #put before last build command
 mka bacon
 
