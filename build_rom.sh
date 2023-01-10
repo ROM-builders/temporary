4e65c3a5e5b76ac1b3e2bcd10d30d431c16ecf4d
Indierock00 <52739181+Indierock00@users.noreply.github.com>
2023-01-10 11:26:53 +0100

    update ricedroid oss link

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android.git -b thirteen -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroidoss/android.git -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Indierock00/local_manifest.git --depth 1 -b ricedroid-t .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
