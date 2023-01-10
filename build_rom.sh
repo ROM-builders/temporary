62586bc401a0dd400b3128b533893c3d40767641
Traveler <83869085+Travelerext@users.noreply.github.com>
2023-01-10 08:28:58 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,6 @@ repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-oss/xd_manifes
 git clone https://github.com/Travelerext/local_manifest.git --depth 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-
 # build rom
 source build/envsetup.sh
 lunch xdroid_clover-userdebug
