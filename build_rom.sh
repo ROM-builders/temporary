3fe6bc3e455e62865db9c6dd8b357bcf22674e8f
EmadGr8 <91535171+EmadGr8@users.noreply.github.com>
2023-01-10 22:09:53 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,8 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch arrow_tulip-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
+lunch arrow_tulip-user
 export TZ=Asia/Dhaka
 m bacon
 
