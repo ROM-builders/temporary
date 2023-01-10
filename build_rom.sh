a04947df87dca52b319e3253750f9d311aeb3ff1
Dinesh <dinesh2742004@gmail.com>
2023-01-10 18:17:58 +0530

    santoni-PixelExperience-thirteen-plus-DineshMSDian

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch aosp_santoni-userdebug
-#export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export TZ=Asia/Chennai #put before last build command
 mka bacon
 
