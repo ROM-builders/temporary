66d0925274b5c0a6afbf9378ce185b04445e1177
Mozzaru <100791931+Mozzaru@users.noreply.github.com>
2023-01-10 14:12:10 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,14 +3,12 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest 
 git clone https://github.com/Mozzaru/local_manifest.git --depth 1 -b Evolution-X-tiramisu .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
- 
 # build rom 
 source build/envsetup.sh 
 lunch evolution_markw-userdebug 
 export TZ=Asia/Jakarta 
 mka evolution 
  
-  
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
