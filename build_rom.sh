9e6ea93b6a458e26dfde3f6da5400a97995d41e7
Hafidz Muzakky <ais.muzakky@gmail.com>
2023-01-10 15:36:26 +0700

    Build for cheetah
    Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch aosp_joyeuse-userdebug
+lunch aosp_cheetah-userdebug
 export TZ=Asia/Jakarta #put before last build command
 mka bacon
 
