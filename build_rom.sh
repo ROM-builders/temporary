90c561d3e3dd62f83cb006ad8e12de5154d0483c
snnbyyds <87487115+snnbyyds@users.noreply.github.com>
2023-01-11 08:47:35 +0800

    58j58

--- build_rom.sh
+++ build_rom.sh
@@ -5,9 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-
 croot
-
 lunch arrow_fajita-user
 export KBUILD_BUILD_USER=snnbyyds
 export BUILD_USERNAME=snnbyyds
