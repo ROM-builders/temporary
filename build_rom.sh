4ce522a45505245af3531b9a878b7b1c87cf4d32
ZualoliconVN <94312960+ZualoliconVN@users.noreply.github.com>
2023-01-11 20:56:18 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-export SELINUX_IGNORE_NEVERALLOWS=true
 export KBUILD_BUILD_USER=ZualoliconVN
 export KBUILD_BUILD_HOST=ZualoliconAndroidLab
 export BUILD_USERNAME=ZualoliconVN
@@ -17,7 +16,6 @@ export USE_DEXOPT=true
 export CHERISH_VANILLA=true
 export TZ=Asia/Kolkata #put before last build command
 brunch vayu
-
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
