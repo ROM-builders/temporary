0f756e89fb9834b16620cc4e814d057b8431a636
Alec Chan <88945170+alecchangod@users.noreply.github.com>
2023-01-12 22:57:14 +0800

    .

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest -b topaz -g default,-mips,-darwin,-notdefault -j1
+repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest -b topaz -g default,-mips,-darwin,-notdefault 
 git clone https://github.com/alecchangod/local_manifest.git --depth 1 -b aospa .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j1
  
 # build rom
 source build/envsetup.sh
@@ -9,7 +9,7 @@ lunch aospa_monet-user
 # export WITH_GAPPS=true
 export TZ=Asia/Hong_Kong #put before last build command
 ./rom-build.sh monet -c -t user -v release
-# 11
+# 12
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
 
