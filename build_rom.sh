a498f150f5a527cd194fe3135e39fb16c50b41f4
EmadGr8 <91535171+EmadGr8@users.noreply.github.com>
2023-01-13 11:03:17 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,12 @@
-# sync rom 
+# sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b triton -g default,-mips,-darwin,-notdefault
 git clone https://github.com/EmadGr8/local_manifest.git --depth 1 -b awaken .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch awaken_tulip-user
+lunch awaken_tulip-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka
 make bacon
 
