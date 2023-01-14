eb8a99060513c82777a715e7102dc01ec328dccc
BhashaRana <122691639+BhashaRana@users.noreply.github.com>
2023-01-14 23:08:09 +0530

    Fix f*

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,3 @@
-
-/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P# sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b twelve-plus -g default,-mips,-darwin,-notdefault
 git clone https://github.com/BhashaRana/local_manifest --depth 1 -b m52xq .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
