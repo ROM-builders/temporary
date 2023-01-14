a43067e96993e0e7b4ea81c6b7860d0914176954
janakniraula <112856726+janakniraula@users.noreply.github.com>
2023-01-14 17:57:00 +0545

    Mi439-Evolution-X-tiramisu-janakniraula

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/janakniraula/local_manifests.git --depth 1 -b linea
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+. build/envsetup.sh
 lunch evolution_Mi439-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka evolution
