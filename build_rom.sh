213a14bd285e202d271fe4705b27c3c1ffe1df3d
Hirokixd <doodle280204@gmail.com>
2023-01-14 11:16:35 +0000

    X01AD-xdroid-CAF-Hirokixd

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-oss/xd_manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Hirokixd/local_manifest.git --depth 1 -b Crdroid-12.1 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-CAF/xd_manifest.git -b eleven -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Hirokixd/local_manifest.git --depth 1 -b xdcaf-11 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch xdroid_X01AD-userdebug
-export BUILD_USERNAME=Hirokixd
 export TZ=Asia/Jakarta #put before last build command
 make xd
 
