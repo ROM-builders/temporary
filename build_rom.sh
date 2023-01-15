3f30eb155e3ab928117f84bb278dc2bc37ccb2f4
kitw4y <91876095+kitw4y@users.noreply.github.com>
2023-01-16 05:46:43 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -2,13 +2,11 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
 git clone https://github.com/kitw4y/local_manifest.git --depth 1 -b evox-r .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-repo sync -j1 --fail-fast
 
 # build rom 
 source build/envsetup.sh
 lunch evolution_lancelot-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
-export TZ=Asia/Kolkata #put before last build command
+export TZ=Asia/Jakarta #put before last build command
 mka evolution
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
