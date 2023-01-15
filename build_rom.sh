91c594296c3bc0691c6fd64a69cdce52644e0502
BhashaRana <122691639+BhashaRana@users.noreply.github.com>
2023-01-15 15:42:59 +0530

    Plox work

--- build_rom.sh
+++ build_rom.sh
@@ -1,3 +1,4 @@
+
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b twelve-plus -g default,-mips,-darwin,-notdefault
 git clone https://github.com/BhashaRana/local_manifest --depth 1 -b m52xq .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
@@ -8,6 +9,8 @@ export ALLOW_MISSING_DEPENDENCIES=true
 lunch aosp_m52xq-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
+
+
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
