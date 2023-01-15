7c380f481f131106d4a13dbfbab80d0f3570c11d
root <don.boruza@gmail.com>
2023-01-15 12:49:22 +0000

    build: bring ricedroid back

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b thirteen -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/ricedroidOSS/android -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/donboruza/local_manifests.git --depth 1 -b sweet-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom 
+# build rom
 source build/envsetup.sh
 lunch lineage_sweet-userdebug
 export BUILD_USERNAME=don.boruza
