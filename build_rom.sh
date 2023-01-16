fe8c4be8ef90c03b27de20c74848ac54583c66ce
Hitesoni | ×͜× <72127977+hsx02@users.noreply.github.com>
2023-01-17 00:02:11 +0530

    Fix clamg Failed

--- build_rom.sh
+++ build_rom.sh
@@ -2,15 +2,16 @@ repo init --depth=1 --no-repo-verify -u https://github.com/ricedroidOSS/android 
 git clone https://github.com/hsx02/Local-Manifests.git --depth 1 -b rc-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
-source build/envsetup.sh
-lunch lineage_spes-userdebug
+# build rom [01]
+. build/envsetup.sh
+lunch lineage_vince-user
 export ALLOW_MISSING_DEPENDENCIES=true
 export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
 export BUILD_BROKEN_DUP_RULES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
-export TZ=Asia/Jakarta #put before last build command
-brunch  spes
+export WITH_GMS=true
+export TZ=Asia/Dhaka #put before last build command
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
