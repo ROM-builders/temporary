c3179cd4d095e13f7812296c276abbcaf1463dc6
deafaf <122148266+Monster7773@users.noreply.github.com>
2023-01-16 22:36:33 +0530

    lava.pixel-exprience-thirteen.Monster7773
    Build Pixel Experience for lava

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,19 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/PixelExperience/manifest -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Monster7773/local_manifest --depth 1 -b main .repo/local_manifests
+git clone https://github.com/Monster7773/local_manifest_2 --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch aosp_lava-userdebug
-export TZ=Asia/Delhi #put before last build command
-mka bacon -j$(nproc --all)
+export BUILD_USERNAME=deadaf
+
+export KBUILD_BUILD_NAME=deadaf
+
+export BUILD_HOSTNAME=already_dead_af
+
+export TZ=Asia/Delhi#put before last build command
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
