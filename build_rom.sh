395458a4e467776cc440a567a4ec69b87917427b
deafaf <122148266+Monster7773@users.noreply.github.com>
2023-01-17 14:30:19 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,7 @@
 
  repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b thirteen -g default,-mips,-darwin,-notdefault 
 
- git clone https://github.com/Arafattex/local_manifest.git --depth 1 -b Lancelot_pixel .repo/local_manifests 
+ git clone https://github.com/Monster7773/local_manifest --depth 1 -b Lancelot_pixel .repo/local_manifests 
 
  repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
 
