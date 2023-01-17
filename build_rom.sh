a2dcc75542005c2e23cbcc5985fd373d9047ca82
Notkeret <jerespn@gmail.com>
2023-01-17 15:30:29 -0400

    fog: Crdroid initial build

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 13.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Notkerd69/local_manifest.git --depth 1 -b test-s44 .repo/local_manifests
+git clone https://github.com/Notkerd69/local_manifest.git --depth 1 -b los-20 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -11,6 +11,7 @@ export BUILD_HOSTNAME=cirrushet
 source build/envsetup.sh
 lunch lineage_fog-userdebug
 export TZ=America/Caracas #put before last build command
+export ALLOW_MISSING_DEPENDENCIES=true
 brunch fog
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
