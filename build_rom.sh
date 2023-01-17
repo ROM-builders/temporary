9082e50d8834e650dec562b5100ddc9009c4c3e3
Alec Chan <88945170+alecchangod@users.noreply.github.com>
2023-01-17 13:01:11 +0800

    J8

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest -b topaz -g default,-mips,-darwin,-notdefault 
 git clone https://github.com/alecchangod/local_manifest.git --depth 1 -b aospa .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j1
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
  
 # build rom
 source build/envsetup.sh
@@ -12,6 +12,7 @@ export TZ=Asia/Hong_Kong #put before last build command
 # 13
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+
 
 
 
