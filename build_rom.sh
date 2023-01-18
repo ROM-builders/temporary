a27f18daf3c27d510a931f61e30b4817c0f882d8
Haikal Luthfianino Balukia <51993230+hklknz@users.noreply.github.com>
2023-01-18 15:59:12 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync Rom
+# sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-12/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/hklknz/Local-Manifests.git --depth 1 -b tissot-derpfest .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
