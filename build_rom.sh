c9da973d8727b374ad7ea00db4e8a3cd4bad13f3
z3zens <78209717+z3zens@users.noreply.github.com>
2023-01-18 10:03:06 +0700

    X01BD-ricedroidOSS-thirteen-z3zens

--- build_rom.sh
+++ build_rom.sh
@@ -11,7 +11,7 @@ export BUILD_HOSTNAME=android-build
 export KBUILD_BUILD_USER=nobody
 export KBUILD_BUILD_HOST=android-build
 export TZ=Asia/Jakarta
-brunch X01BD
+mka bacon
 
 # upload rom
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
