2b123865b4d9e859fa41182029067667b70fec4b
z3zens <78209717+z3zens@users.noreply.github.com>
2023-01-18 10:05:38 +0700

    X01BD-ricedroidOSS-thirteen-z3zens

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ricedroidOSS -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/z3zens/local_manifest.git --depth 1 -b thirteen .repo/local_manifests
+git clone https://github.com/z3zens/local_manifest.git --depth 1 -b rice .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom ricedroidOSS
@@ -11,7 +11,7 @@ export BUILD_HOSTNAME=android-build
 export KBUILD_BUILD_USER=nobody
 export KBUILD_BUILD_HOST=android-build
 export TZ=Asia/Jakarta
-mka bacon
+brunch X01BD
 
 # upload rom
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
