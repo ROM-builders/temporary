9edbbe4c99b4c6e22c1f6c8b2693a33bb8dcab86
kitw4y <91876095+kitw4y@users.noreply.github.com>
2023-01-18 10:02:39 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest 
 git clone https://github.com/kitw4y/local_manifest.git --depth 1 -b evox-r .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom 
+# build rom 1
 source build/envsetup.sh
 lunch evolution_lancelot-userdebug
 export TZ=Asia/Jakarta #put before last build command
