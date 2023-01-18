db2cae49aa67396d9ded2050caf5fd59fe01ad39
Haikal Luthfianino Balukia <51993230+hklknz@users.noreply.github.com>
2023-01-18 13:50:02 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-12/manifest.
 git clone https://github.com/hklknz/Local-Manifests.git --depth 1 -b tissot-derpfest .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build roms
 source build/envsetup.sh
 lunch derp_tissot-userdebug
 export TZ=Asia/Tokyo
