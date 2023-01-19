4971ce7bb27035026823b5ae4c0d063b73748b62
deafaf <122148266+Monster7773@users.noreply.github.com>
2023-01-19 17:08:28 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,7 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Monster7773/local_manifest_3 --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
+repo j1 --fail-fast
 # build rom
 source build/envsetup.sh
 lunch aosp_lava-userdebug
