760bca2884ec8e2609d0cc6e0482caa91d707055
Mozzaru <100791931+Mozzaru@users.noreply.github.com>
2023-01-19 18:09:26 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch arrow_markw-user
+lunch arrow_markw-userdebug
 export TZ=Asia/Bandung #put before last build command
 mka bacon
 
