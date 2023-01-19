7327581e5c5f5b74affce7702e6098444e2bfaac
Futar0 <104120854+Futar0@users.noreply.github.com>
2023-01-19 14:02:26 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Futar0/local_manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/Futar0/local_manifest.git --depth 1 -b a13-lance .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_spes-userdebug
+lunch lineage_lancelot-userdebug
 export TZ=Asia/Dhaka #put before last build command
 export TARGET_BOOT_ANIMATION_RES=1080
 export SELINUX_IGNORE_NEVERALLOWS=true
