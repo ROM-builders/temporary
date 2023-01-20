25fbae9dd0be597088399eb2602023e8ade8bf7d
Raiden Ishigami <contact.raidenishi69@gmail.com>
2023-01-20 11:55:30 -0300

    Revert "A001D-CRDROID #1"
    This reverts commit ad82fe0c09f6a2e5cc85c08d2aac7956de663ecd.

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 11.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/raidenkkj/local_manifest.git --depth 1 -b a001d-crdroid .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
+git clone https://github.com/raidenkkj/local_manifest.git --depth 1 -b cherish13-mithorium .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom 1
+# build rom
 . build/envsetup.sh
-lunch lineage_A001D-userdebug
-export TZ=America/Sao_Paulo
-export SELINUX_IGNORE_NEVERALLOWS=true
-mka bacon
+export CHERISH_VANILLA=true
+export TZ=Asia/Dhaka #put before last build command
+brunch Mi439
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
