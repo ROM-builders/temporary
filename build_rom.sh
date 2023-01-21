7855f51e667b03b3ca7e2549c90a37a40ae2e064
boedhack99 <boedhack@gmail.com>
2023-01-21 21:41:04 +0700

    build: Initial LineageOS mojito

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/boedhack99/local_manifest.git --depth 1 -b los-20 .repo/local_manifests
+git clone https://github.com/boedhack99/local_manifest.git --depth 1 -b los-mojito .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_fog-userdebug
+lunch lineage_mojito-userdebug
 export TZ=Asia/Bangkok #put before last build command
 export ALLOW_MISSING_DEPENDENCIES=true
 mka bacon
