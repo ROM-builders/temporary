d5d69223b898865c44d8726a0d06cafbcbd948e9
NRanjan-17 <nalinishranjan05@gmail.com>
2023-01-22 00:52:09 +0530

    mido: Test Chnages

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixysOS/manifest -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/NRanjan-17/local_manifest.git --depth 1 -b PixysOS-13 .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
