f67cdbbb2221c41be1a4523e6bbbc885daa89619
Droneship <81514881+Seifood@users.noreply.github.com>
2023-01-22 09:31:49 +0200

    Trying sync

--- test.sh
+++ test.sh
@@ -71,7 +71,7 @@ rclone_string="rclone copy out/target/product/\$(grep unch \$CIRRUS_WORKING_DIR/
 if [[ $rclone_check != *$rclone_string* ]]; then echo Please follow rclone copy line of main branch.; exit 1; fi
 
 sync_check=$(grep 'repo sync' $CIRRUS_WORKING_DIR/build_rom.sh)
-sync_string="repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j"
+sync_string="repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8"
 if [[ $sync_check != *$sync_string* ]]; then echo Please follow repo sync line of main branch.; exit 1; fi
 
 fetch_check=$(grep 'git fetch ' $CIRRUS_WORKING_DIR/build_rom.sh | wc -l)
