3e45610cbf65f2d71f6cbecab42bdd2106d4a649
Zakaaa31 <amrizakaria999@gmail.com>
2023-01-22 15:52:02 +0700

    Include dependencies

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,12 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/CarbonROM/android.git -b cr-9.0 -g default,-mips,-darwin,-notdefault
-https://github.com/Zakaaa31/local_manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/Zakaaa31/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch carbon_lava-userdebug
+export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Jakarta #put before last build command
 make carbon
 
