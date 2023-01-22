407fe19ca9681f613139a255ec9adcbd6cb4ec0a
Emanosi <105166741+emanosi@users.noreply.github.com>
2023-01-22 23:18:46 +0700

    lava-PixelOS-13.0-emanosi

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init -u https://github.com/NusantaraProject-ROM/android_manifest -b 12
+repo init -u https://github.com/PixelOS-AOSP/manifest.git -b thirteen
 git clone https://github.com/emanosi/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 
 # build rom
-  . build/envsetup.sh
-  lunch nad_<lava>-gapps
-  make nad
+. build/envsetup.sh
+lunch aosp_lava-gapps
+  make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
