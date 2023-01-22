48606b59b209fc142a0f27918d3b7bb3cc701b64
mrxzzet <110835705+mrxzzet@users.noreply.github.com>
2023-01-22 15:14:55 +0300

    Mi439-LineageOS-lineage-20.0-mrxzzet

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-20.0 -b 11 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/mrxzzet/local_manifests.git --depth 1 -b master local_manifests/lineage-20.0.xml
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
+lunch Mi439
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
