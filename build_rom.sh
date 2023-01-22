9660763408976f329a9e82600efc33d4fb64916f
Emanosi <105166741+emanosi@users.noreply.github.com>
2023-01-23 01:17:05 +0700

    lancelot-PixelOS-AOSP-emanosi

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch aosp_lava-gapps
+lunch aosp_lancelot-userdebug
+export TZ=Asia/Dhaka #put before last build command
   make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
