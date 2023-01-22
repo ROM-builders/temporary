cc562bc6bc59d8823b34f9cd7e45992b8f812846
Rinto Khan <114152118+rinto02@users.noreply.github.com>
2023-01-22 12:54:38 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,17 +1,16 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Project-Xtended/manifest.git -b xt -g default,-mips,-darwin,-notdefault
-git clone https://github.com/rinto02/local_manifest.git --depth 1 -b xt .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
+git clone https://github.com/rinto02/local_manifest.git --depth 1 -b cherish .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch xtended_RMX2020-userdebug
 export KBUILD_BUILD_USER=Rinto
 export KBUILD_BUILD_HOST=Rinto
 export BUILD_USERNAME=Rinto
 export BUILD_HOSTNAME=Rinto
 export TZ=Asia/Dhaka #put before last build command
-mka xtended
+brunch RMX2020
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
