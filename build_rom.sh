7f196eee31ab53943a4265dcf28f0072703eb36c
mrxzzet <110835705+mrxzzet@users.noreply.github.com>
2023-01-22 20:51:33 +0300

    Mi439-LineageOS-lineage-20.0-mrxzzet

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,16 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-20.0 -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/mrxzzet/local_manifests.git --depth 1 -b master local_manifests/lineage-20.0.xml
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault
+mkdir mkdir .repo/local_manifests
+git clone https://github.com/mrxzzet/local_manifests/blob/lineage-20.0/lineage-20.0.xml .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 gh repo clone mrxzzet/mi-thorium_patches
 git apply mrxzzet/mi-thorium_patches
 
 # build rom
 source build/envsetup.sh
-lunch Mi439
+lunch Mi439-userdebug
 export TZ=Asia/Dhaka #put before last build command
-mka derp
+make Mu439
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
