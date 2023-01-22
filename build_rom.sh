40ccc26b1423e323ea4e478190dd082ff1ce31e2
Aether <120278927+aether192@users.noreply.github.com>
2023-01-23 00:10:05 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init -u https://github.com/CherishOS/android_manifest.git -b tiramisu
+repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
 https://github.com/aether192/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
