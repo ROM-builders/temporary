dfe5a051c9fdce91f989f253c52ccf3f42fca035
Mozzaru <100791931+Mozzaru@users.noreply.github.com>
2023-01-22 12:09:00 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b pyro -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Mozzaru/local-manifests.git --depth 1 -b spark-13 .repo/local_manifests
+git clone https://github.com/Mozzaru/local_manifest.git --depth 1 -b spark-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom 2
+# build rom 3
 source build/envsetup.sh
 lunch spark_markw-userdebug
 export TZ=Asia/Jakarta #put before last build command
