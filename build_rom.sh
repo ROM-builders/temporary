ed1d1d6242c832b15dd8f26bf7e9d2f4a932f590
criperm19 <96837536+criperm19@users.noreply.github.com>
2023-01-23 00:00:43 -0300

    leu deu
    HAHAHAHAJA

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom
+# sync ro
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-17.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/CreeperAmigo19/local-manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
