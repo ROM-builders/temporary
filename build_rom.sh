284d15bcca9db39c49c35dcd94f415bd26832a16
Hyperlink <119830528+hyperlink12@users.noreply.github.com>
2023-01-24 05:21:05 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch derp_rosemary-userdebug
+export BUILD_USERNAME=hyperlink12
+export BUILD_HOSTNAME=hyperlink12
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
