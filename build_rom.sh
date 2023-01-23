8e8bd7fd7d9bbd5592761619b3c5ab6297e7184f
snnbyyds <87487115+snnbyyds@users.noreply.github.com>
2023-01-23 08:20:15 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -12,9 +12,10 @@ lunch aosp_fajita-user
 export KBUILD_BUILD_USER=snnbyyds
 export KBUILD_BUILD_HOST=uotan
 export BUILD_USERNAME=snnbyyds
-export BUILD_HOSTNAME=uotan
+export BUILD_HOSTNAME=test
 export TZ=Asia/Shanghai #put before last build command
 mka bacon
+
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
