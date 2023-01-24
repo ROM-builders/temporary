8815504f7e9cd4cd27fb76b2ca5cdb6cc6af172d
JV007xp <114124246+JV007xp@users.noreply.github.com>
2023-01-24 10:14:18 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
- brunch A001D-userdebug
+    brunch
 export TZ=America/Sao_Paulo
 export ALLOW_MISSING_DEPENDENCIES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
