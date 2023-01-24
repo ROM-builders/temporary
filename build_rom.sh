7eee7c89d1f17699f984aa4a2a155595f813906c
khirendra03 <khirendrapatel3@gmail.com>
2023-01-24 09:52:44 +0530

    pixelos bringup
    Signed-off-by: khirendra03 <khirendrapatel3@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/khirendra03/local_manifest.git --depth 1 -b lineage .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-AOSP/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
+git clone https://github.com/khirendra03/local_manifest.git --depth 1 -b aosp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 export TZ=Asia/Delhi #put before last build command
-lunch lineage_X01BD-userdebug
+lunch aosp_X01BD-userdebug
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
