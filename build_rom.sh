986ce122b59dcf881806114fad10343004b66a47
Haikal Luthfianino Balukia <51993230+hklknz@users.noreply.github.com>
2023-01-24 08:11:20 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-12/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/hklknz/Local-Manifests.git --depth 1 -b tissot-derpfest .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/hklknz/Local-Manifests.git --depth 1 -b tissot-bn .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build roms
 source build/envsetup.sh
-lunch derp_tissot-userdebug
+lunch banana_tissot-userdebug
 export TZ=Asia/Tokyo
 export BUILD_BROKEN_DUP_RULES=true
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
@@ -18,7 +18,7 @@ export BUILD_USERNAME=Honoka
 export BUILD_HOSTNAME=Aquors
 export KBUILD_BUILD_USER=Honoka
 export KBUILD_BUILD_HOST=Aquors
-mka derp
+m banana
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
