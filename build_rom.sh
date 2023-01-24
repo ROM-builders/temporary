0c51f84fdce5c0b593110957cc8e4f16ff3b005b
mrxzzet <110835705+mrxzzet@users.noreply.github.com>
2023-01-24 20:36:25 +0300

    fix

--- build_rom.sh
+++ build_rom.sh
@@ -4,9 +4,9 @@ git clone https://github.com/Nem1xx/local_manifests.git -b crdroid-11.0 --depth 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-. build/envsetup.sh
+build/envsetup.sh
 export TZ=Asia/Dhaka #put before last build command
 brunch Mi439
 
-# pls upload this time fr upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
