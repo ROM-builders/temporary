32cf4d4aed63d41273b1e54ff6280c36d107d565
Rinto Khan <114152118+rinto02@users.noreply.github.com>
2023-01-24 18:24:27 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,23 +5,15 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
+lunch lineage_RMX2020-userdebug
 export KBUILD_BUILD_USER=Rinto
 export KBUILD_BUILD_HOST=Rinto
 export BUILD_USERNAME=Rinto
 export BUILD_HOSTNAME=Rinto
-export RELAX_USES_LIBRARY_CHECK=true
 export SKIP_ABI_CHECKS=true
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
-export USE_DEXOPT=true
 export SELINUX_IGNORE_NEVERALLOWS=true
-export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
-export BUILD_BROKEN_VERIFY_USES_LIBRARIES=true
-export BUILD_BROKEN_DUP_RULES=true
-export BUILD_BROKEN_CLANG_ASFLAGS=true
-export BUILD_BROKEN_CLANG_CFLAGS=true
-export BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE=true
 export TZ=Asia/Dhaka #put before last build command
-brunch RMX2020
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
