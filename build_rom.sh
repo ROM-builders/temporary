4cd23a4c1c4b4b294050401878f18550d18d89f7
Atharv Negi <94751339+atharvnegi@users.noreply.github.com>
2023-01-24 15:38:03 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b thirteen -g default,-mips,-darwin,-notdefault 
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Elixir/manifest -b Tiramisu -g default,-mips,-darwin,-notdefault 
 git clone https://github.com/atharvnegi/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
