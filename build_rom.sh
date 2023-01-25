a04a7956913b67fb1954957aee88305cdba4a7e6
aetherAF <123431511+aetherAF@users.noreply.github.com>
2023-01-25 22:31:58 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/aether192/local_manifests --depth 1 -b main .repo/l
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.shh
+source build/envsetup.sh
 lunch bootleg_lancelot-user
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
