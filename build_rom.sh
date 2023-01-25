313b3327c2e097f8ee3aa8a8911f7ac2480fb915
aetherAF <123431511+aetherAF@users.noreply.github.com>
2023-01-25 17:12:09 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b pyro -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Bootleggers-BrokenLab/manifest.git -b tirimbino -g default,-mips,-darwin,-notdefault
 git clone https://github.com/aether192/local_manifests --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch spark_lancelot-user
+lunch bootleg_lancelot-user
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
