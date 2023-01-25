5f71928c1bb27ee454b0bd32aa90ee4bcf30d86f
DESTROYER3264 <118124438+DESTROYER3264@users.noreply.github.com>
2023-01-25 14:04:26 +0530

    retry

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-#  sync rom
+# sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ricedroidOSS/android -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/DESTROYER3264/local_manifest.git --depth 1 -b rice .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
