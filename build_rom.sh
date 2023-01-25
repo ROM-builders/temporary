15b0d415d3350f1c50b197086ee19d13bc47ef03
mrxzzet <110835705+mrxzzet@users.noreply.github.com>
2023-01-25 16:07:34 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/mrxzzet/local_manifests.git -b crdroid-11.0 --depth
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-build/envsetup.sh
+. build/envsetup.sh
 export TZ=Asia/Dhaka #put before last build command
 brunch Mi439
 
