854dc856e4dd3def5880708e94c2d56353608eb4
GTA <54992611+mountain47@users.noreply.github.com>
2023-01-26 02:02:22 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/PixysOS/manifest -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/mountain47/local_manifest.git --depth 1 -b main .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync --current-branch 
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync --current-branch -j8
 
 # build rom
 . build/envsetup.sh
