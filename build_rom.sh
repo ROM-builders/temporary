b7f5e96ab30a2e65f5256a26d10e482889254be5
GTA <54992611+mountain47@users.noreply.github.com>
2023-01-26 01:52:08 +0530

    moon-pixysOS-twelve-mountain47
    hope this build will work

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
-repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 12.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/PixysOS/manifest -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/mountain47/local_manifest.git --depth 1 -b main .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync --current-branch -j8
 
 # build rom
 . build/envsetup.sh
-brunch moon
-lunch lineage_moon-userdebug
+lunch pixys_moon_userdebug
+make pixys -j8
 export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export RELAX_USES_LIBRARY_CHECK=true
