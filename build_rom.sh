7fd1ebf4721769b7c036a7c5a7affec1b7843627
GTA <54992611+mountain47@users.noreply.github.com>
2023-01-26 01:59:22 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/PixysOS/manifest -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/mountain47/local_manifest.git --depth 1 -b main .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync --current-branch 
+repo sync #-c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync --current-branch 
 
 # build rom
-source build/envsetup.sh
+. build/envsetup.sh
 lunch pixys_moon_userdebug
 make pixys -j8
 export SELINUX_IGNORE_NEVERALLOWS=true
