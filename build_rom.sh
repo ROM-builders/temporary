6833ed64600928114cff9c176d4deed4a60cecfc
z3zens <78209717+z3zens@users.noreply.github.com>
2023-01-26 14:03:27 +0700

    X01BD-xdroid-oss-twelve-z3zens

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-oss/xd_manifest -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/NerdZ3ns/local_manifest.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/z3zens/local_manifest.git --depth 1 -b xd12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom 4.19 test rebase 17
+# build rom
 source build/envsetup.sh
 lunch xdroid_X01BD-userdebug
 export BUILD_USERNAME=nobody
