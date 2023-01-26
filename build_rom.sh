c41eda6cc5148c4113153c2635f91fc7936b260a
Emanosi <105166741+emanosi@users.noreply.github.com>
2023-01-26 14:23:04 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch corvus_lancelot-user
+export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Jakarta #put before last build command
 make corvus
 
