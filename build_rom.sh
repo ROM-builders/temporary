478fba1a79e8f0b171e9f05757bf1907feb31ffd
AbrarNoobAlt <123289074+AbrarNoobAlt@users.noreply.github.com>
2023-01-26 13:40:01 +0600

    Init vos

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 13 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/AbrarNoob/local_manifest --depth 1 -b banana .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/AbrarNoob/local_manifest --depth 1 -b vos .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-export WITH_GAPPS=true
-export BUILD_CORE_GAPPS=true
-lunch banana_alioth-userdebug
-export TZ=Asia/Dhaka #put before last build command
-m banana
+lunch voltage_alioth-userdebug
+export TZ=Asia/Delhi #put before last build command
+brunch alioth
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+# rclone copy vendor/ota/lavender.json cirrus:lavender -P
