b05758177079b83bec7bedb7c5c29939a9538dc6
JV007xp <114124246+JV007xp@users.noreply.github.com>
2023-01-27 16:54:19 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Havoc-OS/android_manifest.git -b eleven -g default,-mips,-darwin,-notdefault
-git clone https://github.com/JV007xp/local_manifest.git --depth 1 -b main .repo/local_manifests
+git clone de https://github.com/JV007xp/android_device_asus_A001D --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
