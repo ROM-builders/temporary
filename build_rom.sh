e339956d7bb0ad407ece40a2ffe2fa40f05fb2f1
darshan <43618707+mevadadarshan19@users.noreply.github.com>
2023-01-28 00:22:29 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/PalyrimOS/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/Project-Flexo/manifest -b 13.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/mevadadarshan19/local_manifest.git --depth 1 -b elixir .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_X01BD-userdebug
+lunch aosp_X01BD-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
