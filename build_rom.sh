453a5dc760a71240140cb6067fb6b130a6bc0dd3
ᴍᴏᴄʜᴀ <118044457+xyz-mocha@users.noreply.github.com>
2023-01-27 23:22:21 +0530

    fixing

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 13.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/xyz-mocha/local_manifest --depth 1 -b crdroid .repo/local_manifests
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
