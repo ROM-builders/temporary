c938e789a3a4dd20121a400d25ab1ae3a74a37f8
CJAlfa1995 <123786653+CJAlfa1995@users.noreply.github.com>
2023-01-28 14:39:08 -0600

    Nio-Spark-CJAlfa1995

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch aosp_nio-user
+lunch pyro_nio-user
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
