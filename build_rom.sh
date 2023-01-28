3f4532e25012e416b7b7059056af2fd28cdbb135
Zakamayo <123351573+Zakamayo@users.noreply.github.com>
2023-01-28 11:37:33 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -8,7 +8,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 lunch xdroid_lava-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
-export TZ=Asia/Jakarta #put before last build command
+export TZ=Asia/Tokyo #put before last build command
 make xd
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
