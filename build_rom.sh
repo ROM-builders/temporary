c12f84a35bd2dd6cbe03f673bc83b63642e4e7a1
Rinto Khan <114152118+rinto02@users.noreply.github.com>
2023-01-28 10:14:08 +0600

    Build Vanilla

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-export WITH_GAPPS=true
+export WITH_GAPPS=false
 export KBUILD_BUILD_USER=Rinto
 export KBUILD_BUILD_HOST=Rinto
 export BUILD_USERNAME=Rinto
