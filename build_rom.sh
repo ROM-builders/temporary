63eddb2924dd797e1cefa6a000b98b2041ab30b6
Zakamayo <123351573+Zakamayo@users.noreply.github.com>
2023-01-28 08:14:55 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,6 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 . build/envsetup.sh
 lunch xdroid_lava-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export TZ=Asia/Jakarta #put before last build command
 make xd
 
