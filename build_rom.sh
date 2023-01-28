226744832b9e327f26f5b8fade307351c54cbaeb
abousgamer <114771325+abousgamer@users.noreply.github.com>
2023-01-28 18:15:48 +0100

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_m10lte-user
+lunch lineage_m10lte-eng
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
