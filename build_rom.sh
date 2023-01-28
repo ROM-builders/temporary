1c07375403d01525d5a8c85fcf1535f06172cc69
Rinto Khan <114152118+rinto02@users.noreply.github.com>
2023-01-28 06:14:35 +0600

    RMX2020-ricedroidOSS-thirteen-rinto02

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Project-Xtended/manifest.git -b xt -g default,-mips,-darwin,-notdefault
-git clone https://github.com/rinto02/local_manifest.git --depth 1 -b xt .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ricedroidOSS/android -b thirteen -g default,-mips,-darwin,-notdefault
+git clone https://github.com/rinto02/local_manifest.git --depth 1 -b ricedroid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-export WITH_GAPPS=true
+export WITH_GMS=true
 export KBUILD_BUILD_USER=Rinto
 export KBUILD_BUILD_HOST=Rinto
 export BUILD_USERNAME=Rinto
@@ -13,9 +13,9 @@ export BUILD_HOSTNAME=Rinto
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
 export ALLOW_MISSING_DEPENDENCIES=true
 export LOCAL_MODULE_TAGS=userdebug
-lunch xtended_RMX2020-userdebug
+lunch lineage_RMX2020-userdebug
 export TZ=Asia/Dhaka #put before last build command
-make xtended
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
