0983d91f238aabcb92f2b2630f56cc17ba185a6d
HDzungx <118291317+hdzungx@users.noreply.github.com>
2023-01-28 19:07:34 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 export TZ=Asia/Dhaka #put before last build command
-export SELINUX_IGNORE_NEVERALLOWS=true
 export WITH_GMS=true
 brunch munch
 
