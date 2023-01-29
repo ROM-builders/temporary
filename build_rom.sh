9235efd6f86549ac1078b9eca81a910525d69efd
Rinto Khan <114152118+rinto02@users.noreply.github.com>
2023-01-30 05:21:20 +0600

    Retry uploading

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/ricedroidOSS/android 
 git clone https://github.com/rinto02/local_manifest.git --depth 1 -b ricedroid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom 
+# build rom
 source build/envsetup.sh
 export KBUILD_BUILD_USER=Rinto
 export KBUILD_BUILD_HOST=Rinto
