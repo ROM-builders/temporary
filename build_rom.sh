773382257a1cc513c9c5e65bb8b9cdc5c07158cb
GTA <54992611+mountain47@users.noreply.github.com>
2023-01-29 19:40:21 +0530

    moon-CherishOs-twelve-one-mountain47

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 repo init --depth=1 -u https://github.com/CherishOS/android_manifest.git -b twelve-one -g default,-mips,-darwin,-notdefault
 #localmanifest
-git clone https://github.com/mountain47/local_manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/mountain47/local_manifest.git --depth 1 -b cherish .repo/local_manifests
 
 # Sync
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
@@ -10,9 +10,9 @@ source build/envsetup.sh
 brunch moon
 
 
-#export SELINUX_IGNORE_NEVERALLOWS=true
-#export ALLOW_MISSING_DEPENDENCIES=true
-#export RELAX_USES_LIBRARY_CHECK=true
+export SELINUX_IGNORE_NEVERALLOWS=true
+export ALLOW_MISSING_DEPENDENCIES=true
+export RELAX_USES_LIBRARY_CHECK=true
 export TZ=Asia/Dhaka 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
