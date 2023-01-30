c42feb17112119a35caf18a09346a1778f2eef20
Akshat Singh <91002031+Legendleo90@users.noreply.github.com>
2023-01-30 22:12:55 +0530

    beryllium-PixelExperience-thirteen-plus-Legendleo90

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch aosp_$device-userdebug
+lunch aosp_beryllium-user
 export BUILD_USERNAME=Leo
 export BUILD_HOSTNAME=ArchX
 export TZ=Asia/Kolkata #put before last build command
