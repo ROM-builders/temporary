30039d1ff00923a3859eb2b2b57ad9c65ce249c0
Zakamayo <123351573+Zakamayo@users.noreply.github.com>
2023-01-31 05:57:12 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -9,7 +9,7 @@ lunch xdroid_lancelot-userdebug
 export TARGET_BOOT_ANIMATION_RES=1080
 export SELINUX_IGNORE_NEVERALLOWS=true
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
-export TZ=Asia/Jakarta #put before last build command
+export TZ=Asia/Tokyo #put before last build command
 make xd
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
