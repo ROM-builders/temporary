4d66ee4dc444bd1c1f641af517592cd46392a158
S ∆ U R ∆ V <90666710+SauRavRwT@users.noreply.github.com>
2023-01-30 06:59:07 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,6 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Project-Xtended/manif
 git clone https://github.com/SauRavRwT/Local-Manifest.git --depth 1 -b xt .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+#Xt - 4
 # build rom
 source build/envsetup.sh
 lunch xtended_veux-user
