330cbcda5dbbc4770863b9a5b86932c343f492ca
Christopher Keith Irvine <Mizdrake7@gmail.com>
2023-01-30 19:34:05 +0530

    derp

--- build_rom.sh
+++ build_rom.sh
@@ -1,22 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://github.com/mizdrake7/local_manifest.git --depth 1 -b cherish .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+git clone ttps://github.com/SimpleJony/local_manifest --depth=1 -b derp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-export SELINUX_IGNORE_NEVERALLOWS=true
-export KBUILD_BUILD_USER=MAdMiZ
-export KBUILD_BUILD_HOST=MAdMiZ
-export BUILD_USERNAME=MAdMiZ
-export BUILD_HOSTNAME=MAdMiZ
-export RELAX_USES_LIBRARY_CHECK=true
-export SKIP_ABI_CHECKS=true
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
-export USE_DEXOPT=true
-export CHERISH_VANILLA=true
-export TZ=Asia/Kolkata #put before last build command
-brunch r5x
+lunch derp_r5x-userdebug
+export TZ=Asia/Shanghai #put before last build command
+mka derp
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
