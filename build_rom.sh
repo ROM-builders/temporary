d9ff52ea513c74a89482949d499044c2e596826b
Asif <49162687+nhAsif@users.noreply.github.com>
2023-01-31 22:29:42 +0600

    fix error

--- build_rom.sh
+++ build_rom.sh
@@ -3,13 +3,12 @@ repo init --depth=1 --no-repo-verify -u https://github.com/ricedroidOSS/android 
 git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b rice .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom [13]
+# build rom [15]
 . build/envsetup.sh
 lunch lineage_alioth-user
 export WITH_GMS=true
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
-
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
