a59bda740527d3fb0a36d806b9253ae29395b001
Mozzaru <100791931+Mozzaru@users.noreply.github.com>
2023-02-01 03:34:47 +0700

    Markw

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
-# sync rom
+J# sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Mozzaru/local_manifest.git --depth 1 -b evolution-t .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom          
+# build rom (1)          
 source build/envsetup.sh
 lunch evolution_markw-userdebug
 export KBUILD_BUILD_USER=Van.
