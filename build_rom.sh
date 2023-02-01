db51ccc81c02ebf6183bffa9800198729cd4d962
Wendi Wardani <86230878+Wrdn28@users.noreply.github.com>
2023-02-02 00:22:58 +0700

    X01AD-xdroid-oss-thirteen-Wrdn28

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch xdroid_X01AD-userdebug
+export ALLOW_MISSING_DEPEDENCIES=true
 export TZ=Asia/Jakarta #put before last build command
 make xd
 
