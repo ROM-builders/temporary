9ebabfdd742298b5297fef605652362f6615275a
Haikal Luthfianino Balukia <51993230+hklknz@users.noreply.github.com>
2023-02-02 06:33:47 +0700

    I FORGOR

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-oss/xd_manifes
 git clone https://github.com/hklknz/Local-Manifests.git --depth 1 -b mido-xd12L .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build roms
+# build rom
 source build/envsetup.sh
 lunch xdroid_mido-userdebug
 export TZ=Asia/Tokyo 
