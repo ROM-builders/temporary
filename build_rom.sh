ed1591d20c50d26e11494481a08b90413b2dda7f
Mozzaru <100791931+Mozzaru@users.noreply.github.com>
2023-02-01 10:18:22 +0700

    Test
    Tes

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-J# sync rom
+# sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Mozzaru/local_manifest.git --depth 1 -b blaze-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
