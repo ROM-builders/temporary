355f44b3373d36d9b98fdd677b6535cc0f9fa178
Rinto Khan <114152118+rinto02@users.noreply.github.com>
2023-02-01 07:47:33 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -16,7 +16,7 @@ export ALLOW_MISSING_DEPENDENCIES=true
 export LOCAL_MODULE_TAGS=userdebug
 lunch blaze_RMX2020-userdebug
 export TZ=Asia/Dhaka #put before last build command
-mka blaze 
+mka bacon
  
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
