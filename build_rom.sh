3e29823143536638e09d7585d9e45dbb7372e1f5
GTA <54992611+mountain47@users.noreply.github.com>
2023-02-01 21:38:01 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,34 +1,12 @@
-<?xml version="1.0" encoding="UTF-8"?>  
+repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.git -b eleven -g default,-mips,-darwin,-notdefault
+git clone https://github.com/mountain47/local_manifest.git --depth 1 -b 11 .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-<manifest>
-        
-        <!-- fetching sources -->      
+# build rom
+source build/envsetup.sh
+lunch superior_<devicecodename>-userdebug
+mka bacon
+export TZ=Asia/Dhaka #put before last build command
 
-        <remote name="Rinto02"
-                fetch="https://github.com/rinto02" />
-
-         <remote name="r450784d"
-                fetch="https://gitlab.com/rintokhan" />
-
-         <remote name="mountain47"
-                fetch="https://github.com/mountain47" />
- 
-        <remote name="Realme-G70-Series"
-                fetch="https://github.com/Realme-G70-Series" />
-
-
-
-      <!-- device trees -->
-
-     <project path="device/realme/RMX2193" name="android_device_realme_RMX2193" remote="mountain47" revision="11" />
-     <project path="vendor/realme/RMX2193" name="vendor_realme_RMX2193" remote="mountain47" revision="11" />
-      <project path="kernel/realme/RMX2193" name="kernel_realme_RMX2193" remote="mountain47" revision="Q" clone-depth="1" />
-      <project path="packages/apps/RealmeDirac" name="android_packages_apps_RealmeDirac" remote="Realme-G70-Series" revision="master" clone-depth="1" />
-
-        
-      <!-- clang -->
-
-        <project path="prebuilts/clang/host/linux-x86/clang-proton" name="proton-clang" remote="Rinto02" revision="main" clone-depth="1" />
-        <project path="prebuilts/clang/host/linux-x86/clang-r450784d" name="clang-r450784d" remote="r450784d" revision="master" clone-depth="1" />
- 
-</manifest>
+# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
