8fd341992b308fec6b5b04dc318a73d968cbd199
Rinto Khan <114152118+rinto02@users.noreply.github.com>
2023-02-01 06:46:27 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,6 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
+export WITH_GAPPS=true
 export KBUILD_BUILD_USER=Rinto
 export KBUILD_BUILD_HOST=Rinto
 export BUILD_USERNAME=Rinto
@@ -15,7 +16,7 @@ export ALLOW_MISSING_DEPENDENCIES=true
 export LOCAL_MODULE_TAGS=userdebug
 lunch blaze_RMX2020-userdebug
 export TZ=Asia/Dhaka #put before last build command
-make bacon
+mka blaze
  
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
