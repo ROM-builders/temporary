1634d50632d4cba78c47861336c364cd16391b13
Christopher Keith Irvine <Mizdrake7@gmail.com>
2023-02-02 13:53:21 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,28 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
-git clone ttps://github.com/SimpleJony/local_manifest --depth=1 -b derp .repo/local_manifests
+git clone https://github.com/mizdrake7/local_manifest --depth=1 -b derp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch derp_r5x-userdebug
-export TZ=Asia/Shanghai #put before last build command
+export KBUILD_BUILD_USER=MAdMiZ
+export KBUILD_BUILD_HOST=MAdMiZ
+export BUILD_USERNAME=MAdMiZ
+export BUILD_HOSTNAME=MAdMiZ
+export RELAX_USES_LIBRARY_CHECK=true
+export SKIP_ABI_CHECKS=true
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+export USE_DEXOPT=true
+export SELINUX_IGNORE_NEVERALLOWS=true
+export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
+export BUILD_BROKEN_VERIFY_USES_LIBRARIES=true
+export BUILD_BROKEN_DUP_RULES=true
+export BUILD_BROKEN_CLANG_ASFLAGS=true
+export BUILD_BROKEN_CLANG_CFLAGS=true
+export BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE=true
+export RELAX_USES_LIBRARY_CHECK=true
+export TZ=Asia/Kolkata #put before last build command
 mka derp
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
