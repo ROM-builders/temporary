543fa212c8d26d26e97f570415bdf70cee5e3e8f
S ∆ U R ∆ V <90666710+SauRavRwT@users.noreply.github.com>
2023-02-03 19:41:52 +0530

    Update build_rom.sh
    3rd try

--- build_rom.sh
+++ build_rom.sh
@@ -4,6 +4,7 @@ git clone https://github.com/SauRavRwT/Local-Manifest.git --depth 1 -b blaze .re
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 #2
+#3
 # build rom
 source build/envsetup.sh
 lunch blaze_veux-user
