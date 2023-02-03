5d052c55cca65261a6fdf95891db36029c5166b2
GTA <54992611+mountain47@users.noreply.github.com>
2023-02-04 02:15:55 +0530

    RMX2193-clCrDroid-11.0-mountain47
    build crdroid

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-repo init --depth=1 --no-repo-verify -u https://github.com/PixysOS/manifest -b eleven -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 11.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/mountain47/local_manifest.git --depth 1 -b 11 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
