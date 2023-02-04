74fd0d6ead9508cd5e91ed85c681f414e76727a2
S ∆ U R ∆ V <90666710+SauRavRwT@users.noreply.github.com>
2023-02-04 09:12:55 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,13 +3,11 @@ repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest
 git clone https://github.com/SauRavRwT/Local-Manifest.git --depth 1 -b blaze .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-#2
-#3
 # build rom
 source build/envsetup.sh
 lunch blaze_veux-user
 export TZ=Asia/Dhaka #put before last build command
-mka veux
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
