73ef21199867fe1ffdeb9f1e5f9264241ac1959e
ᴍᴏᴄʜᴀ <118044457+xyz-mocha@users.noreply.github.com>
2023-02-04 06:52:24 +0530

    banana

--- build_rom.sh
+++ build_rom.sh
@@ -1,8 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 13 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/xyz-mocha/local_manifest --depth 1 -b banana .repo/local_manifests
+git clone https://github.com/xyz-mocha/local_manifest --depth 1 -b bananaDroid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
 
 # build rom
 . build/envsetup.sh
