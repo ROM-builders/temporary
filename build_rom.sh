2a635d098353d1610afa35fd84910bb5b2e16136
darshan <43618707+mevadadarshan19@users.noreply.github.com>
2023-02-05 11:50:21 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/BootleggersROM/manifest.git -b tirimbino -g default,-mips,-darwin,-notdefault
 git clone https://github.com/mevadadarshan19/local_manifest.git --depth 1 -b BTLG .repo/local_manifests
-repo sync --current-branch --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune -j8(nproc --all)
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
