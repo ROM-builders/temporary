bce9bf48ebc7f27330752269bf32faf4749b85e8
GTA <54992611+mountain47@users.noreply.github.com>
2023-02-06 04:04:08 +0530

    RMX2193-PixelOS-mountain47
    Build for Realme narzo 20/30a

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 export TZ=Asia/Dhaka #put before last build command
-lunch aosp_moon-user
+lunch aosp_rmx2193-user
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
