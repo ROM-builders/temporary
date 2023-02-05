d3a81d08ea5c613cafcc7f964c5f3d19bc99bacd
GTA <54992611+mountain47@users.noreply.github.com>
2023-02-06 03:03:19 +0530

    moon-yaap-mountain47

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 export TZ=Asia/Dhaka #put before last build command
-lunch yaap_device-user
+lunch yaap_moon-user
 m yaap
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
