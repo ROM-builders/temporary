c463cb1264865dfda4da5d2f0d194237ae39127b
sanjayzx <73420351+sanjayzx@users.noreply.github.com>
2023-02-07 17:00:25 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch bootleg_rosemary-userdebug
+export WITH_GAPPS=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export TZ=Asia/Jakarta #put before last build command
