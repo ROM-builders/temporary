a0685cf279737d5156eba8688f58a52eb0f05557
Indierock00 <52739181+Indierock00@users.noreply.github.com>
2023-02-07 22:56:38 +0100

    syberia-13.0 raphael

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Indierock00/local_manifest.git --depth 1 -b superior-13 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/syberia-project/manifest.git -b 13.0 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Indierock00/local_manifest.git --depth 1 -b Syb-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch superior_markw-userdebug
+lunch syberia_raphael-userdebug
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
