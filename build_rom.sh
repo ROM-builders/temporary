c152af6df9412accec79c2cc19441c88186fb4c0
paryal58 <92784787+paryal58@users.noreply.github.com>
2023-02-08 07:23:57 -0500

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://github.com/paryal58/local_manifest.git --depth 1 -b old .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b pyro -g default,-mips,-darwin,-notdefault
+git clone https://github.com/paryal58/local_manifest.git --depth 1 -b spar .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch cherish_instantnoodle-userdebug
-export TZ=Asia/Tokyo #put before last build command
+lunch spark_instantnoodle-userdebug
+export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
