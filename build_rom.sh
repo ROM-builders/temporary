2b80d164338c2fc2a91d8e51a185316dd7d0b679
z3zens <78209717+z3zens@users.noreply.github.com>
2023-02-08 09:27:37 +0700

    X01BD-ricedroidOSS-thirteen-z3zens

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ricedroidOSS/android -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/z3zens/local_manifest.git --depth 1 -b thirteen .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 --fail-fast
 
 # build rom ricedroidOSS try boot changes
 source build/envsetup.sh
