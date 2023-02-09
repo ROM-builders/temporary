3086b2abf475477b4ee5eb9de8c1eccd2a76cdd4
ThunderBolt <93515519+ironHeart6@users.noreply.github.com>
2023-02-09 22:33:43 +0600

    Let's build ricedroid

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b thirteen-plus -g default,-mips,-darwin,-notdefault
-git clone https://github.com/takumi021/local_manifest --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ricedroidOSS/android -b thirteen -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Amritorock/local_manifest --depth 1 -b rice .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_r5x-userdebug
+lunch lineage_r5x-userdebug
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
