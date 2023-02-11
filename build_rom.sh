a9a30d22577a14f81eb98ea3df5bc781090d9c88
JV007xp <114124246+JV007xp@users.noreply.github.com>
2023-02-11 12:08:08 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch carbon_A001D-userdebug
+lunch havoc_A001D-userdebug
 export TZ=America/Sao_Paulo
 export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
