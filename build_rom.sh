2096e800dd58b3c9ead1033d16d5ddd27d41daa9
exer <98556122+ekkusa@users.noreply.github.com>
2023-02-13 08:36:46 -0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,8 +5,10 @@ repo sync -c --no-clone-bundle --force-remove-dirty --optimized-fetch --prune --
 # build rom
 source build/envsetup.sh
 lunch lineage_akari_kddi-defconfig
-export TZ=Asia/Dhaka #put before last build command
-mka bacon
+export BUILD_USERNAME=ekkusa
+export BUILD_HOSTNAME=miyo
+export TZ=Asia/Singapore
+mka bacon -j16
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
