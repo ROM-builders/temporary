88ee1b727e61a0441b25a5bdf5db014445735eea
Dooms <vmbuildforme@gmail.com>
2023-02-13 10:03:10 +0530

    blaze 2.5

--- build_rom.sh
+++ build_rom.sh
@@ -3,6 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest
 git clone https://github.com/Krtonia/local_manifest.git --depth 1 -b blaze .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+
 # build rom
 source build/envsetup.sh
 lunch blaze_beryllium-user
