8d7ab997a06c1ca3df970e4cc6751974efc24b48
Mozzaru <100791931+Mozzaru@users.noreply.github.com>
2023-02-13 10:52:16 +0700

    Gass

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/P-404/android_manifes
 git clone https://github.com/Mozzaru/local_manifest.git --depth 1 -b p404-T .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom    
+# build rom     
 source build/envsetup.sh
 lunch p404_markw-userdebug
 export KBUILD_BUILD_USER=Van.
