9e9eafb3fe2a1421838856ca24c831eaf65aeebe
GTA <54992611+mountain47@users.noreply.github.com>
2023-02-13 10:40:07 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/Fusion-OS/android_manifest -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/mountain47/local_manifest.git --depth 1 -b a12.1 .repo/local_manifests
-repo sync --current-branch --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c -q --current-branch --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune -j$(nproc --all)
 
 # Initialize the environment with the envsetup.sh script:
 source build/envsetup.sh
