047464d9d764505668e635a92b36defdd1ff2c59
CodeChas3r <69832543+CodeChas3r@users.noreply.github.com>
2023-02-13 09:34:14 +0800

    Let's get derpppp it

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,10 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch derp_rosemary-userdebug
+export BUILD_USERNAME=CodeChas3r
+export BUILD_HOSTNAME=userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
