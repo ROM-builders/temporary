9d6908d556d896ed67a58d532ee3a6f4eb8b6d7d
paryal58 <92784787+paryal58@users.noreply.github.com>
2023-02-13 07:45:08 -0500

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch evolution_instantnoodle-userdebug
+lunch evolution_instantnoodle-user
 export TZ=America/Los_Angeles #put before last build command
 mka evolution
 
