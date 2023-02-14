19a0a8d9d3eef22426b9ffe071fa39f038375358
exer <dayesofficial@gmail.com>
2023-02-14 07:01:07 +0000

    build_rom.sh: change build username

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --force-remove-dirty --optimized-fetch --prune --
 # build rom
 source build/envsetup.sh
 lunch lineage_akari_kddi-defconfig
-export BUILD_USERNAME=ekkusa
+export BUILD_USERNAME=exer
 export BUILD_HOSTNAME=miyo
 export TZ=Asia/Singapore
 mka bacon -j16
