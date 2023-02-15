22e2a70df0bc1f2630cd8eb58699601ec41356d8
dld_xda <61960986+dangi-del@users.noreply.github.com>
2023-02-15 11:44:24 +0530

    Alpha-Droid_Mido By DLD
    Mido_Alpha_Droid

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
-# sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/AlphaDroid-Project/manifest -b alpha-13 default,-mips,-darwin,-notdefault
+git clone https://github.com/dangi-del/local-manifest.git --depth 1 -b main .repo/local_manifest
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
-export TZ=Asia/Dhaka #put before last build command
+lunch mido-AlphaDroid-13-dangi
+export TZ=Asia/india #put before last build command
+WITH_GAPPS:=true
 mka derp
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
