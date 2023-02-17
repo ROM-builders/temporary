6007543b50ed4e497ed4ab8ad4470570c023c725
Ansh <singhansh64321@gmail.com>
2023-02-17 15:22:11 +0530

    Build v10.2

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android.git -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/EvilAnsh/local-manifest.git --depth 1 -b rice .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ricedroidOSS/android -b thirteen -g default,-mips,-darwin,-notdefault
+git clone https://github.com/EvilAnsh/Local-Manifest.git --depth 1 -b rice .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch lineage_RM6785-user
+export WITH_GMS=true
 export TZ=Asia/Mumbai #put before last build command
 make bacon
 
-# upload rom (if your don't need to upload multiple files, then you don't need to edit next line)
+# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
