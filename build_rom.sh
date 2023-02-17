f704312232deab712a58ce6544fbacfedddce6e5
SKetU-l <sketu@donot.care>
2023-02-17 23:07:24 +0530

    Default sync

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ricedroidOSS/android -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/SKetU-l/local_manifest.git --depth 1 -b main .repo/local_manifests
-repo sync -c --no-clone-bundle --force-remove-dirty --optimized-fetch --prune --force-sync -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
