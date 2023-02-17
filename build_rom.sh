f4e9a8264e6cb7af547e955ff8c87dc64f735ff2
Jax Lee <sd369888@outlook.com>
2023-02-17 22:03:58 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -8,6 +8,7 @@ export SELINUX_IGNORE_NEVERALLOWS=true
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 source build/envsetup.sh
 lunch aosp_evergo-userdebug
+
 export TZ=Asia/Shanghai #put before last build command
 mka bacon
 
