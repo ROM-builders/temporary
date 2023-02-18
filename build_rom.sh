2ecc4b4ab441de4babcc05d3fd74349fd8b798fe
Haikal Luthfianino Balukia <51993230+hklknz@users.noreply.github.com>
2023-02-18 20:00:49 +0700

    Testing CoreGApps

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom
+# sync roms
 repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 13 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/hklknz/Local-Manifests.git --depth 1 -b tissot-bananadroidCoreGApps .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
