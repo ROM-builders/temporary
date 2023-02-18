594a1ba5ee4f88816b01ca2a226a7afaded53fe1
snnbyyds <87487115+snnbyyds@users.noreply.github.com>
2023-02-18 08:52:25 +0800

    feb test

--- build_rom.sh
+++ build_rom.sh
@@ -12,6 +12,7 @@ export KBUILD_BUILD_USER=snnbyyds
 export KBUILD_BUILD_HOST=uotan
 export BUILD_USERNAME=snnbyyds
 export BUILD_HOSTNAME=uotan
+
 export TZ=Asia/Shanghai #put before last build command
 mka bacon
 
