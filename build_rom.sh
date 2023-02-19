a9fbe7d36d61de74a11f9e5f96ae52b5de65eef5
Fr0ztyy43 <82140208+Fr0ztyy43@users.noreply.github.com>
2023-02-19 09:07:46 +0100

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,19 +1,19 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/StagOS/manifest.git -b t13 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Fr0ztyy43/local_manifests.git --depth 1 -b corvus .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-17.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Fr0ztyy43/local_manifests.git --depth 1 -b los17.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rommmm
 source build/envsetup.sh
-lunch stag_begonia-userdebug
+lunch a3lte_begonia-userdebug
 
 export KBUILD_BUILD_USER=Fr0ztyy43 
 export KBUILD_BUILD_HOST=Fr0ztyy43
 export BUILD_USERNAME=Fr0ztyy43 
 export BUILD_HOSTNAME=Fr0ztyy43 
 
-export TZ=Asia/Dhaka #put before last build command
-make stag
+export TZ=Europe/Warsaw #put before last build command
+brunch a3lte
 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
