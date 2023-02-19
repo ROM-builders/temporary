b4d6c49939a16ce830e634324ea4b41c7615acd2
Amrito Karmokar <amritokarmokar5i@gmail.com>
2023-02-19 19:11:42 +0600

    Trigger

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch lineage_r5x-userdebug 
 export TARGET_DISABLE_EPPE=true
-export SELINUX_IGNORE_NEVERALLOWS=true
+export SELINUX_IGNORE_NEVERALLOWS=true 
 export TZ=Asia/Dhaka #put before last build command
 make bacon 
 
