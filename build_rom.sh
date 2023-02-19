da833cdbc1b97677242e427e2760dadc103f6aef
Mudabbirul Islam <71183979+MudabbirulSaad@users.noreply.github.com>
2023-02-19 19:32:40 +0600

    dumpling-PixelExperience-13-mudabbirulsaad

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b thirteen-plus -g default,-mips,-darwin,-notdefault
-git clone https://github.com/MudabbirulSaad/local_manifests --depth 1 -b pe .repo/local_manifests
+git clone https://github.com/MudabbirulSaad/local_manifests --depth 1 -b pe-dumpling .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
