791ceb4505aa655afcdf0011a4a8f0106e74b82b
deafaf <122148266+deadaf5@users.noreply.github.com>
2023-02-19 09:25:55 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,15 +5,14 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch derp_lancelot-user
+lunch spark_lancelot-userdebug
 
-export KBUILD_BUILD_USER=deadaf
-export KBUILD_BUILD_HOST=deadaf
-export BUILD_USERNAME=deadaf
-export BUILD_HOSTNAME=deadaf
-export TZ=Asia/Kolkata #put before last build command
+export TZ=asia/Dhaka #put before last build command
+export TARGET_BOOT_ANIMATION_RES=1080
+export SELINUX_IGNORE_NEVERALLOWS=true
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 
-mka derp
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
