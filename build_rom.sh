29d6ee178f52daef1301c40d328e8303f1f8f9d5
Tejas Singh <tejassingh649@gmail.com>
2023-02-20 05:51:35 +0530

    Trigger new build

--- build_rom.sh
+++ build_rom.sh
@@ -4,15 +4,14 @@ git clone https://github.com/baconpeedit/Local_Manifest.git --depth 1 -b xtended
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+. b*/e*
 lunch xtended_ginkgo-userdebug
 export BUILD_USERNAME=Tejas
 export KBUILD_BUILD_USER=tejas
 export KBUILD_BUILD_HOST=I_Am_Charsi
 export BUILD_HOSTNAME=I_Am_Charsi
-export WITH_GAPPS=true
 export TZ=Asia/Kolkata
-make xtended
+mka xtended
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
