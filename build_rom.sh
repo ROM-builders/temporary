b365d531f2cf26481fd020d890f165a8b0e47246
Futar0 <104120854+Futar0@users.noreply.github.com>
2023-02-20 15:29:23 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/BootleggersROM/manifest.git -b tirimbino -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Futar0/local_manifest.git --depth 1 -b Bootleg .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Futar0/local_manifest.git --depth 1 -b Certus .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch bootleg_lancelot-userdebug
+lunch bootleg_certus-userdebug
 export TZ=asia/Dhaka #put before last build command
-export TARGET_BOOT_ANIMATION_RES=1080
+export TARGET_BOOT_ANIMATION_RES=720
 export SELINUX_IGNORE_NEVERALLOWS=true
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 mka bacon
