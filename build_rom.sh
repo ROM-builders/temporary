11bf5327c70776239b76691ea23c37ec6afcffa6
Maxim <110835705+mrxzzet@users.noreply.github.com>
2023-02-20 10:59:23 +0300

    update 2

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,18 @@ repo init -u https://github.com/PixysOS/manifest -b thirteen
 git clone https://github.com/mrxzzet/local_manifests-1 -b lineage-caf .repo/local_manifests
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
-# build rom
+# applying patches
+git clone https://github.com/Nem1xx/mi-thorium_patches patches
+cd patches/common/system/core
+git apply 0001-liblp-Allow-to-flash-on-bigger-block-device.patch
+cd ../../../
+cd patches/lineage-20.x/vendor/lineage
+git apply 0001-Split-msm8937-from-UM_3_18_FAMILY-and-fix-it.patch
+cd ../../../
+rm -rf vendor/qcom/opensource/data-ipa-cfg-mgr
+git clone https://github.com/janakniraula/android_vendor_qcom_opensource_data-ipa-cfg-mgr -b arrow-13.0 data-ipa-cfg-mgr
+
+# build
 . build/envsetup.sh
 lunch pixys_Mi439-userdebug
 export TZ=Asia/Dhaka #put before last build command
