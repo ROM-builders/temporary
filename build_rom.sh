cbc549f4b2489ae27bbea9bd25f8e16e9598a948
CodeChas3r <69832543+CodeChas3r@users.noreply.github.com>
2023-02-21 09:09:48 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 13.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/CodeChas3r/local_manifest.git --depth 1 -b crdroidandroid-13 .repo/local_manifests
+git clone https://github.com/CodeChas3r/local_manifest.git --depth 1 -b crdroid-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -8,7 +8,6 @@ source build/envsetup.sh
 export BUILD_USERNAME=CodeChas3r
 export BUILD_HOSTNAME=CodeChas3r
 export SELINUX_IGNORE_NEVERALLOWS=true
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export TZ=Asia/Dhaka #put before last build command
 brunch lineage_rosemary-userdebug
 
