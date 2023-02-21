39ea497a374cb56caf638c406daa1e1a775210cf
Ansh <singhansh64321@gmail.com>
2023-02-21 13:09:31 +0530

    Build CherishOS v4.5

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch cherish_RM6785-user
+lunch cherish_RM6785-userdebug
 export TZ=Asia/Mumbai #put before last build command
 make bacon
 
