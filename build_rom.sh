bee608a09de136637a941c14476e6e6acee788f2
ᴍᴏᴄʜᴀ <118044457+xyz-mocha@users.noreply.github.com>
2023-02-21 19:05:07 +0530

    Fixed

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch p404_raphael-userdebug
+lunch p404_tulip-userdebug
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
