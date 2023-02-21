cc6cf6a5505121996541363389d4ea992788d4cb
snnbyyds <snnbyyds@gmail.com>
2023-02-21 17:37:00 +0800

    fuck clang

--- build_rom.sh
+++ build_rom.sh
@@ -5,6 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
+
 croot
 lunch aosp_fajita-user
 export KBUILD_BUILD_USER=snnbyyds
