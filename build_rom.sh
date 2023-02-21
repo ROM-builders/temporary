1238d0ccf5a851719644efc54b30616a6b36acb0
deafaf <122148266+deadaf5@users.noreply.github.com>
2023-02-21 20:44:57 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -10,8 +10,8 @@ lunch spark_lancelot-userdebug
 export TZ=asia/Dhaka #put before last build command
 export TARGET_BOOT_ANIMATION_RES=1080
 export SELINUX_IGNORE_NEVERALLOWS=true
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
+
+
 
 mka bacon
 
