7686a1407c4dc7aa798c470500141768e75aef0b
Mozzaru <100791931+Mozzaru@users.noreply.github.com>
2023-02-22 16:37:15 +0700

    Fix

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/PixysOS/manifest -b t
 git clone https://github.com/Mozzaru/local_manifest.git --depth 1 -b pixy-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom         
+# build rom          
 source build/envsetup.sh
 lunch pixys_markw-userdebug
 export TZ=Asia/Jakarta #put before last build command
