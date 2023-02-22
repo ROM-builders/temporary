6fca8b3428a12e9e3617708995424c21b66e9caa
Maxim <110835705+mrxzzet@users.noreply.github.com>
2023-02-22 17:47:57 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,14 +3,6 @@ repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest -b top
 git clone https://github.com/mrxzzet/local_manifests --depth=1 -b lineage-kernel .repo/local_manifests
 repo sync --no-clone-bundle --current-branch --no-tags -j8
 
-git clone https://github.com/mrxzzet/mi-thorium_patches patches
-cd patches/common/system/core
-git apply 0001-liblp-Allow-to-flash-on-bigger-block-device.patch
-cd ../../../
-cd lineage-20.x/vendor/lineage
-git apply 0001-Split-msm8937-from-UM_3_18_FAMILY-and-fix-it.patch
-cd ../../../../
-
 # build rom
 . build/envsetup.sh
 lunch aospa_Mi439-userdebug
@@ -19,4 +11,4 @@ mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-#end
+
