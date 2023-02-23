7c3f365c053572c63361bda5871653e9cfec5fe6
Anandhan <anandhasayanan007jb@gmail.com>
2023-02-23 15:54:18 +0530

    Build vanilla

--- build_rom.sh
+++ build_rom.sh
@@ -3,10 +3,10 @@ repo init --depth=1 --no-repo-verify -u https://github.com/ricedroidOSS/android 
 git clone https://github.com/anandhan07/local_manifest.git --depth 1 -b ricedroid-13.0 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom [08]
+# build rom [09]
 . build/envsetup.sh
-lunch lineage_vince-eng
-export WITH_GMS=true
+lunch lineage_vince-user
+export WITH_GMS=false
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
