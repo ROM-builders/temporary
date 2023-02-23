57be01d8b222e5ef37db66e68f931f60ad23ba10
Lite <97386083+Lite009@users.noreply.github.com>
2023-02-24 00:00:25 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch aosp_vince-userdebug
+lunch aosp_vince-user
 export TZ=Asia/Kolkata #put before last build command
 make bacon
 
