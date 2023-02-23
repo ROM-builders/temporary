6b7044823d5653aa79bd0c5a1e43aa125ef0d336
ahmedhridoy371 <40537458+ahmedhridoy371@users.noreply.github.com>
2023-02-23 23:29:19 +0600

    trigger gapps build

--- build_rom.sh
+++ build_rom.sh
@@ -10,7 +10,7 @@ export ALLOW_MISSING_DEPENDENCIES=true
 export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
 export BUILD_BROKEN_DUP_RULES=true
 export TZ=Asia/Jakarta #put before last build command
-blissify -v lavender
+blissify -g lavender
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
