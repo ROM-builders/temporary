b38d29d03fb9cbb96bde75520b9b934578e03437
JV007xp <114124246+JV007xp@users.noreply.github.com>
2023-02-24 16:14:04 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,20 +1,18 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/CarbonROM/android.git -b cr-9.0 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/ArcaneOS/Arcane_manifest -b R -g default,-mips,-darwin,-notdefault
 git clone --https://github.com/JV007xp/local-manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch carbon_A001D-userdebug
+lunch aosp_A001D-userdebug
 export TZ=America/Sao_Paulo
-export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
-export TARGET_BOOT_ANIMATION_RES=1080
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
-export BUILD_BROKEN_DEPFILE=true
 
 
-mke carbon
+
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
