49d79fca7a273a1ed6e45d0d73d5b4924e98f243
Mozzaru <100791931+Mozzaru@users.noreply.github.com>
2023-02-24 17:13:52 +0700

    Markw

--- build_rom.sh
+++ build_rom.sh
@@ -8,7 +8,7 @@ source build/envsetup.sh
 export WITH_GAPPS=true
 lunch pixys_markw-eng
 export TZ=Asia/Jakarta #put before last build command
-make pixys
+make pixys 
 
 # upload rom (if your don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
