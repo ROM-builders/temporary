efde47f9167e363aca7fb5d9ca1b15bcbe6fdf33
Jax Lee <sd369888@outlook.com>
2023-02-24 20:13:29 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
 git clone https://github.com/weaponmasterjax/local_manifest.git --depth 1 -b spark-13 .repo/local_manifests
-rm -rf .repo/projects/hardware/qcom-caf/sm8250/audio.git hardware/qcom-caf/sm8250/audio
-rm -rf .repo/projects/hardware/qcom-caf/sm8250/display.git hardware/qcom-caf/sm8250/display
-rm -rf .repo/projects/hardware/qcom-caf/sm8250/media.git hardware/qcom-caf/sm8250/media
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
