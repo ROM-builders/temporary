70d6d67693407461da0be80da69a86114de71e50
Omansh Krishn <96343317+omansh-krishn@users.noreply.github.com>
2023-02-24 14:15:21 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,7 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
 git clone https://github.com/omansh-krishn/local_manifest -b evox .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-# @omansh_krishn
+#  @omansh_krishn
 
 # build rom
 source build/envsetup.sh
