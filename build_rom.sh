63e2ba1a957a4ca9eae21128fa91aed32314c4b3
Haikal Luthfianino Balukia <51993230+hklknz@users.noreply.github.com>
2023-02-25 23:17:16 +0700

    Ahhhh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync roms
-repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/hklknz/android_manifest.git -b 13 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/hklknz/Local-Manifests.git --depth 1 -b tissot-bananadroidCoreGApps .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
@@ -8,10 +8,10 @@ source build/envsetup.sh
 lunch banana_tissot-userdebug
 export TZ=Asia/Tokyo #put before last build command
 m banana
-export KBUILD_BUILD_USER=Honoka
-export KBUILD_BUILD_HOST=HonkCI
-export BUILD_USERNAME=Honoka
-export BUILD_HOSTNAME=HonkCI
+export KBUILD_BUILD_USER=hklknz
+export KBUILD_BUILD_HOST=Roselia-CI
+export BUILD_USERNAME=hklknz
+export BUILD_HOSTNAME=Roselia-CI
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
