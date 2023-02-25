579c79254c3576dee83bd86c03ad7dc778214681
Mozzaru <100791931+Mozzaru@users.noreply.github.com>
2023-02-25 15:23:42 +0700

    Tetes

--- build_rom.sh
+++ build_rom.sh
@@ -10,6 +10,7 @@ export KBUILD_BUILD_USER=Van.
 export KBUILD_BUILD_HOST=Van.
 export BUILD_USERNAME=Van.
 export BUILD_HOSTNAME=Van.
+export WITH_GAPPS=false
 export TZ=Asia/Jakarta #put before last build command
 mka bacon 
 
