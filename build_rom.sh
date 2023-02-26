b7e38f960ede4cf2895dc89f222d6d98530784aa
Omansh Krishn <96343317+omansh-krishn@users.noreply.github.com>
2023-02-26 19:12:47 +0530

    santoni-PixelExperience-thirteen-plus-omansh-krishn

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,23 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b thirteen-plus -g default,-mips,-darwin,-notdefault
+git clone https://github.com/omansh-krishn/local_manifest -b pe .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
+#   @omansh_krishn
+#updated local manifest
 # build rom
+#retrying again, weird sync error
 source build/envsetup.sh
-lunch derp_vayu-user
-export TZ=Asia/Dhaka #put before last build command
-mka derp
+export ALLOW_MISSING_DEPENDENCIES=true
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+#export SELINUX_IGNORE_NEVERALLOWS=true
+#export BUILD_USERNAME=OmanshKrishn
+#export BUILD_HOSTNAME=Arch
+#export KBUILD_BUILD_USER=OmanshKrishn
+#export KBUILD_BUILD_HOST=Arch
+export TZ=Asia/Kolkata # put before last build command
+lunch evolution_santoni-userdebug
+mka evolution
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+#rclone copy out/target/product/santoni/recovery.img cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
