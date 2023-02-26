e1eb069afab8d84a5880c6831770ff1ac9622220
Omansh Krishn <96343317+omansh-krishn@users.noreply.github.com>
2023-02-26 13:39:00 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -9,7 +9,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
-export SELINUX_IGNORE_NEVERALLOWS=true
+#export SELINUX_IGNORE_NEVERALLOWS=true
 #export BUILD_USERNAME=OmanshKrishn
 #export BUILD_HOSTNAME=Arch
 #export KBUILD_BUILD_USER=OmanshKrishn
