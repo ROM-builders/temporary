e458e4976dc6643fd934ffdaf29ddc2f27ccf690
deafaf <122148266+deadaf5@users.noreply.github.com>
2023-02-27 23:04:28 +0530

    Add aosp

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExtended/manifest.git -b trece -g default,-mips,-darwin,-notdefault
 git clone https://github.com/deadaf5/local_manifest.git --depth 1 -b blaze .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch blaze_lancelot-userdebug
+lunch aosp_lancelot-userdebug
 export TZ=asia/Dhaka #put before last build command
 export TARGET_BOOT_ANIMATION_RES=1080
 export SELINUX_IGNORE_NEVERALLOWS=true
