db4381dd1d7454a18c0d07ee4e9fd47a527c0c75
darshan <43618707+mevadadarshan19@users.noreply.github.com>
2023-02-27 23:35:28 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Project-Xtended/manifest.git -b xt -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Mevadadarshan1997/local_manifest.git --depth 1 -b xtended .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/mevadadarshan19/local_manifest.git --depth 1 -b nerd13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch xtended_X01BD-userdebug
+lunch derp_X01BD-userdebug
 
 export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Dhaka #put before last build command
-make xtended
+mka derp
 
 
 
