1e1365be73ceb7cd5432cd996c6112e00856094e
xenxynon <flynryder427@gmail.com>
2023-02-27 20:28:46 +0530

    User build

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest -b sapphire -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest -b topaz -g default,-mips,-darwin,-notdefault
 git clone https://github.com/AOSPA-lavender/manifest --depth 1 -b w .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch aospa_lavender-user
+lunch aospa_lavender-userdebug
 export TARGET_FLATTEN_APEX=true
 export TZ=Asia/Kolkata # put before last build command
-./rom-build.sh lavender -t userdebug -s keys
+./rom-build.sh lavender -t user -v beta -s keys
 #4
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
