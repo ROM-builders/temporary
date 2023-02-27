17813b5f768fffc5d7d6fc25a4a29527a5888bd3
exer <98556122+ekkusa@users.noreply.github.com>
2023-02-26 16:45:47 -0800

    fix

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ricedroidOSS/android -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Miiyo/local-manifest.git --depth 1 -b master .repo/local_manifests
-repo sync -c --no-clone-bundle --force-remove-dirty --optimized-fetch --prune --force-sync -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
