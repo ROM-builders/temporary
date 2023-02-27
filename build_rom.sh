3bec992d1a7b0610ac9321fc66592ba78823afc8
exer <98556122+ekkusa@users.noreply.github.com>
2023-02-27 15:44:59 +0800

    Support llvm tools for rice

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_akari_kddi-user
+export KERNEL_SUPPORTS_LLVM_TOOLS=true
 export BUILD_USERNAME=exer
 export BUILD_HOSTNAME=miyo
 export TZ=Asia/Singapore #put before last build command
