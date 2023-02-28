c6dbb8e3388226b90577c235507da14b2e84de9e
exer <98556122+ekkusa@users.noreply.github.com>
2023-02-28 11:30:27 -0800

    build_rom.sh: Switch local-manifest to rice-tm

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ricedroidOSS/android -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Miiyo/local-manifest.git --depth 1 -b thirteen .repo/local_manifests
+git clone https://github.com/Miiyo/local-manifest.git --depth 1 -b rice-tm .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build roms
