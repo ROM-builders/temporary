d8675c265b6a3489208c2f65cc79d84ad6ebe540
exer <98556122+ekkusa@users.noreply.github.com>
2023-02-28 22:00:31 +0800

    Test again

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/ricedroidOSS/android 
 git clone https://github.com/Miiyo/local-manifest.git --depth 1 -b thirteen .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build roms
 source build/envsetup.sh
 export KERNEL_SUPPORTS_LLVM_TOOLS=true
 export BUILD_USERNAME=exer
