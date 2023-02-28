049830aed94c20d13ec64620cd5a2cd906dfad72
Tejas Singh <tejassingh649@gmail.com>
2023-02-28 12:04:57 +0530

    Sync

--- build_rom.sh
+++ build_rom.sh
@@ -12,5 +12,5 @@ export TZ=Asia/Kolkata
 mka evolution
 cat out/target/product/ginkgo/evolution_gink*.zip.json
 
-# upload rom 
+# upload rom
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
