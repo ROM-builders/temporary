325e3bce9fc2b01dcf0bc96d8b3c9c2b44c6476a
exer <98556122+ekkusa@users.noreply.github.com>
2023-03-01 21:29:17 -0800

    trigger build

--- build_rom.sh
+++ build_rom.sh
@@ -9,7 +9,7 @@ lunch aospa_akari_kddi-userdebug
 export BUILD_USERNAME=exer
 export BUILD_HOSTNAME=miyo
 export TZ=Asia/Singapore #put before last build command
-./rom-build.sh akari_kddi -c -t userdebug -v beta
+./rom-build.sh akari_kddi -c -t userdebug
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
