2489e56da73882c254f749fa83638c328a478609
exer <98556122+ekkusa@users.noreply.github.com>
2023-03-01 19:13:20 -0800

    akari-AOSPA-topaz-ekkusa

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ricedroidOSS/android -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Miiyo/local-manifest.git --depth 1 -b rice-tm .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest -b topaz -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Miiyo/local-manifest.git --depth 1 -b aospa-topaz .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build roms
 source build/envsetup.sh
-export KERNEL_SUPPORTS_LLVM_TOOLS=true
+lunch aospa_akari_kddi-userdebug
 export BUILD_USERNAME=exer
 export BUILD_HOSTNAME=miyo
 export TZ=Asia/Singapore #put before last build command
-brunch lineage_akari_kddi-user
+./rom-build.sh akari_kddi -c -t userdebug -v beta
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
