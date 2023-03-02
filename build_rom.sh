338ddf8082856fb19ed5935b46c7e9b85293b6ae
xenxynon <flynryder427@gmail.com>
2023-03-03 05:03:49 +0530

    Kys

--- build_rom.sh
+++ build_rom.sh
@@ -8,8 +8,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 lunch aospa_lavender-userdebug
 export TARGET_FLATTEN_APEX=true
 export TZ=Asia/Kolkata # put before last build command
-./rom-build.sh lavender -t user -v beta -s keys
-#4
+./rom-build.sh lavender -t user -v beta -s keys -c
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
 rclone copy *aospa**2023*zip cirrus:lavender -P
