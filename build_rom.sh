6273c11950b136694f2034f4d162a0ba76cb2ea3
xenxynon <flynryder427@gmail.com>
2023-03-02 18:49:34 +0530

    Again

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest -b topaz -g default,-mips,-darwin,-notdefault
-git clone https://github.com/AOSPA-lavender/manifest --depth 1 -b w .repo/local_manifests
+git clone https://github.com/AOSPA-lavender/manifest --depth 1 -b kys .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
