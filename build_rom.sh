37ccaa98c223d274ada8b140ef7343ec47cc27f1
ThunderBolt <93515519+ironHeart6@users.noreply.github.com>
2023-03-03 15:47:10 +0600

    Trigger build

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Amritorock/local_manifest --depth 1 -b awaken .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 12.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Amritorock/local_manifest --depth 1 -b cr .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom 
 source build/envsetup.sh
-lunch awaken_r5x-userdebug 
+lunch lineage_r5x-userdebug 
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
