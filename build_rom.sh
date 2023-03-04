e4d2272857b753b868e0fe12def49825b628b378
Omansh Krishn <96343317+omansh-krishn@users.noreply.github.com>
2023-03-04 12:53:50 +0530

    santoni-bananadroid-13-omansh-krishn

--- build_rom.sh
+++ build_rom.sh
@@ -1,24 +1,25 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://github.com/omansh-krishn/local_manifest -b evox .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/omansh-krishn/local_manifest.git -b banana .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # @omansh_krishn
 
-# build  rom
+# build rom
 
 source build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
-export PATH=$PATH:$rom_name/prebuilts/clang/host/linux-x86/clang-r450784d/bin
+#export TARGET_KERNEL_CLANG_VERSION := r450784d
+#export PATH=$PATH:$rom_name/prebuilts/clang/host/linux-x86/clang-r450784d/bin
 #export SELINUX_IGNORE_NEVERALLOWS=true
 #export BUILD_USERNAME=OmanshKrishn
 #export BUILD_HOSTNAME=Arch
 #export KBUILD_BUILD_USER=OmanshKrishn
 #export KBUILD_BUILD_HOST=Arch
 export TZ=Asia/Kolkata # put before last build command
-lunch evolution_santoni-userdebug
-mka evolution
+lunch banana_santoni-userdebug
+m banana
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 #rclone copy out/target/product/santoni/recovery.img cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
