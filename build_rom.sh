7d72ff6ca1e0e2e11a5e45d7d241cad134b6222b
Om Patel <89215836+takumi021@users.noreply.github.com>
2023-03-05 02:33:54 +0530

    Fix display hal clone

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 export TZ=Asia/Delhi #put before build comman
-lunch arrow_r5x-userdebug
+lunch arrow_r5x-userdebug #
 m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
