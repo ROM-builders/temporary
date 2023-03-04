502cbced1ec668c1df2371a7217c68af3b7dd758
darshan <43618707+mevadadarshan19@users.noreply.github.com>
2023-03-04 22:21:39 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/mevadadarshan19/local_manifest.git --depth 1 nerd13 .repo/local_manifests
+git clone https://github.com/Mevadadarshan1997/local_manifest.git --depth 1 CR .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch lineage_X01BD-userdebug
 
-
+export ALLOW_MISSING_DEPENDENCIES=true
 
 export TZ=Asia/Dhaka #put before last build command
 make bacon
