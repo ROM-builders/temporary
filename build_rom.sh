fee49ac586e63a6ce2be3954e725c874f4cad6be
mitmac29 <118977337+mitmac29@users.noreply.github.com>
2023-03-05 21:46:08 +0530

    lavender-CarbonROM-cr-9.0-mitmac29
    lavender-CarbonROM-cr-9.0-mitmac29

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/mitmac29/local_manifest.git --depth 1 -b main .repo
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-. build/envsetup.sh
+source build/envsetup.sh
 lunch carbon_lavender-userdebug
 export TZ=Asia/Dhaka #put before last build command
 make carbon
