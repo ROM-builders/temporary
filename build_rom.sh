68cb024aabe0fd940e3682b928691988acddacc2
Om Patel <89215836+takumi021@users.noreply.github.com>
2023-03-06 17:32:59 +0530

    Fix Display Hal clone

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b triton -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/takumi021/android_manifest -b triton -g default,-mips,-darwin,-notdefault
 git clone https://github.com/takumi021/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
 lunch awaken_r5x-userdebug
-export TZ=Asia/Kolkata #
+export TZ=Asia/Kolkata 
 make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
