49d8a3cfa77648ae6c45e39c56e48dc5bdef8b53
Carckv2 <119148632+Carckv2@users.noreply.github.com>
2023-03-06 12:04:55 -0500

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/bananadroid/android_manifest.git -b 13 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/youssefnone/android_manifest_samsung_j8y18lte --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git: //github.com/CarbonROM/android.git -b cr-11.0 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Carckv2/android_manifest_samsung_j8y18lte.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
