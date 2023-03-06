f0841c9c0ccfbed2886fc98e989a065564def17c
AaaSyanxxxx07 <76146196+Asyanx@users.noreply.github.com>
2023-03-06 17:26:09 +0700

    build: fog: Initial build lineage-20

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Asyanx/local_manifest --depth 1 -b derp .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Asyanx/local_manifest --depth 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom DerpFest-AOSP
 source build/envsetup.sh
-lunch derp_fog-userdebug
+lunch lineage_fog-userdebug
 export BUILD_USERNAME=nobody
 export BUILD_HOSTNAME=android-build
 export KBUILD_BUILD_USER=nobody
