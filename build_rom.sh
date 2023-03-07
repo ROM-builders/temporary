307a4982e09c617998e416fd3b1ac21dfa6bdaec
JV007xp <114124246+JV007xp@users.noreply.github.com>
2023-03-07 15:07:27 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,16 +5,13 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch carbon_A001D-userdebug
+lunch lineage_A001D-userdebug
 export TZ=America/Sao_Paulo
 export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
-export TARGET_BOOT_ANIMATION_RES=1080
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
-export BUILD_BROKEN_DEPFILE=true
+OWS=true
+mka bacon
 
-
-mke carbon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
