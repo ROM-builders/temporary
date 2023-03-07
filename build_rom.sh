2547fcf3cbcd8c1a192a0fe179fab6c25ab1b325
Jax Lee <sd369888@outlook.com>
2023-03-08 00:10:12 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch evolution_everpal-eng
+lunch evolution_everpal-userdebug
 export TZ=Asia/Shanghai #put before last build command
 export WITH_GAPPS=true
 mka evolution
