dd10df70c362e491f3a5fd82c2d1264d9dcfbb59
Stepan <127230512+NikodGithub@users.noreply.github.com>
2023-03-07 21:07:12 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,17 @@
 # Sync
-repo init -u https://github.com/ArrowOS/android_manifest.git -b arrow-11.0
-git clone https://github.com/NikodGithub/local_manifest.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-11.0 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/NikodGithub/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # Build
 source build/envsetup.sh
+
 lunch arrow_lava-userdebug
+
 export TZ=Asia/Dhaka
-m otapackage
 export BUILD_USERNAME=Nikod
+
+m otapackage
 
 # Upload
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
