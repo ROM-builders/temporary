a08dcf26a0ccc16718ca1eefb44158e8809aaf14
Tejas Singh <tejassingh649@gmail.com>
2023-03-08 10:56:14 +0530

    Trigger Build

--- build_rom.sh
+++ build_rom.sh
@@ -4,13 +4,13 @@ git clone https://github.com/baconpeedit/Local_Manifest.git --depth 1 -b evoluti
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+. build/envsetup.sh
 lunch evolution_ginkgo-userdebug
 export BUILD_USERNAME=Tejas
 export BUILD_HOSTNAME=I_Am_Charsi
 export TZ=Asia/Kolkata
 mka evolution
-cat out/target/product/ginkgo/evolution_gink*.zip.json
+cat o*/t*/p*/ginkgo/*.zip.json
 
 # upload rom
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
