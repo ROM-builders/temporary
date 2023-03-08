2c59722954a9f3235168233baa9bc47c4c321d47
Arafattex <69374795+Arafattex@users.noreply.github.com>
2023-03-08 07:43:26 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom
+# sync rom 
 repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android -b 13.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Arafattex/local_manifest.git --depth 1 -b Lancelot_crdroida13r .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
