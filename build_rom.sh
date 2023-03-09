142834b06c90133ef8e0b94cdc744842ec89b390
Omansh Krishn <96343317+omansh-krishn@users.noreply.github.com>
2023-03-09 20:01:06 +0530

    rebuild

--- build_rom.sh
+++ build_rom.sh
@@ -17,7 +17,7 @@ export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export TZ=Asia/Kolkata # put before last build command
 lunch aosp_santoni-userdebug
 mka bacon
-
+#rebuild
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 #rclone copy out/target/product/santoni/recovery.img cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
