c0e442b2eb197da0bfb9ac3950180432ee4a76b2
Nikita Savchenko <83372618+9QS@users.noreply.github.com>
2023-03-09 11:41:36 +0300

    whyred-AOSPA-topaz-9QS

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 -u https://github.com/AOSPA/manifest -b topaz -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest -b topaz -g default,-mips,-darwin,-notdefault
 git clone https://github.com/9QS/local_manifest-whyred.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
