2912f50030b00f5caec231fe0d7030e5fec00078
Futar0 <104120854+Futar0@users.noreply.github.com>
2023-03-10 06:16:47 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Project-Fluid/manifest.git -b fluid-12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Futar0/local_manifest.git --depth 1 -b fluid-certus .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b spark -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Futar0/local_manifest.git --depth 1 -b spark-certus .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch fluid_certus-userdebug
+lunch spark_certus-userdebug
 export TZ=asia/Dhaka #put before last build command
 export TARGET_BOOT_ANIMATION_RES=720
 export SELINUX_IGNORE_NEVERALLOWS=true
