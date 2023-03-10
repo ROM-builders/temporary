9e4673859d8c1a3e7ed2075fdd4fd555ba8bac06
Futar0 <104120854+Futar0@users.noreply.github.com>
2023-03-10 10:41:56 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/AICP/platform_manifest.git -b s12.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Futar0/local_manifest.git --depth 1 -b spark-certus .repo/local_manifests
-repo sync --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
