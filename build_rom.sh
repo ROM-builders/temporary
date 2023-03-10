0e92b5d0c4bb91e87bc64a71d9db42ba4211edb5
HDzungx <118291317+hdzungx@users.noreply.github.com>
2023-03-11 00:59:36 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/CherishOS/android_manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://github.com/HDzungx-Space/local_manifests.git --depth 1 -b test .repo/local_manifests
+git clone https://github.com/HDzungx-Space/local_manifests.git --depth 1 -b cherish-nexus-KP .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
