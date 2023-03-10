71f1cd27edec9d2fb742480994c3197f89c5e3d3
Nikita Savchenko <83372618+9QS@users.noreply.github.com>
2023-03-10 12:50:17 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,8 @@
 #  sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest -b topaz -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/9QS/manifest -b topaz -g default,-mips,-darwin,-notdefault
 git clone https://github.com/9QS/local_manifest-whyred.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom AOSPA topaz
 # build rom AOSPA topaz 
 source build/envsetup.sh
 lunch aospa_whyred-eng
