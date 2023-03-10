773dc3954983dea1df142199432ca85cf6b09d18
Killerpac <45239423+Killerpac@users.noreply.github.com>
2023-03-11 00:11:05 +0530

    fix

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b thirteen 
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Killerpac/local_manifests --depth 1 -b aosp-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
