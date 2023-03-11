ed740b3ed11a29b7f35e200dc2b5d578b0bdbf0d
Jax Lee <sd369888@outlook.com>
2023-03-11 22:50:05 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,7 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
 git clone https://github.com/weaponmasterjax/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
 # build rom
 source build/envsetup.sh
 lunch evolution_everpal-userdebug
@@ -12,3 +12,4 @@ mka evolution
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+#P.S:I know u guys are working hard to support the servers but plz make repo sync reliable so we wont waste a whole 4 hours' wait
