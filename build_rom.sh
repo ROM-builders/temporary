b134ef2a3562084b0e0c701fc2e13dd23371e3b3
Soo-Hwan Na <69458352+roynatech2544@users.noreply.github.com>
2023-03-11 16:10:41 +0900

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git
 git clone https://github.com/A51-Development/local_manifests --depth 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-git --git-dir prebuilts/clang/host/linux-x86/ reset --hard
+git --git-dir prebuilts/clang/host/linux-x86/.git reset --hard
 # build r
 source build/envsetup.sh
 lunch lineage_a51-user
