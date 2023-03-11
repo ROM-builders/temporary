2f04825d7080ea9d80ccf7ed88c608b37d6382ab
nhAsif <najmulhasan3609@gmail.com>
2023-03-11 08:55:16 +0600

    initial build

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ricedroidOSS/android -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b rice-jasmine .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
+git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b cherish-jasmine .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom [3]
 . build/envsetup.sh
-lunch lineage_jasmine_sprout-userdebug
+lunch cherish_jasmine_sprout-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
