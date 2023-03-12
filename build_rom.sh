e80ba50e6fe319091956176ef8192791d3d5b508
Asyanx <76146196+Asyanx@users.noreply.github.com>
2023-03-13 04:49:30 +0700

    build : pex

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/PixelExtended/manifes
 git clone https://github.com/Asyanx/local_manifest.git --depth 1 -b Los-20 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom pex
 source build/envsetup.sh
 lunch aosp_fog-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
