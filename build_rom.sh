ba2fb4d32fef58de09a8267bf83811a66ded9175
z3zens <78209717+z3zens@users.noreply.github.com>
2023-03-13 12:26:39 +0700

    X01BD-P-404-tokui-z3zens

--- build_rom.sh
+++ build_rom.sh
@@ -1,17 +1,17 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest -b topaz -g default,-mips,-darwin,-notdefault
-git clone https://github.com/z3zens/local_manifest.git --depth 1 -b topaz .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/P-404/android_manifest -b tokui -g default,-mips,-darwin,-notdefault
+git clone https://github.com/P-404-SDM660/X01BD_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom AOSPA topaz
 source build/envsetup.sh
-lunch aospa_X01BD-userdebug
+lunch p404_X01BD-userdebug
 export BUILD_USERNAME=nobody
 export BUILD_HOSTNAME=android-build
 export KBUILD_BUILD_USER=nobody
 export KBUILD_BUILD_HOST=android-build
 export TZ=Asia/Jakarta
-./rom-build.sh X01BD -c -t userdebug -v beta
+make bacon
 
 # upload rom
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
