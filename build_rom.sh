3c79bdb296817ad752efaa62e3e788bebe814d8c
snnbyyds <87487115+snnbyyds@users.noreply.github.com>
2023-03-14 22:46:38 +0800

    Retry

--- build_rom.sh
+++ build_rom.sh
@@ -5,11 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-
-
 croot
-
-
 lunch aosp_fajita-user
 export KBUILD_BUILD_USER=snnbyyds
 export KBUILD_BUILD_HOST=uotan
