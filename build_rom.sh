5e93e7890cca184dcf9d7941f3f3d5eeadef2ddf
Omansh Krishn <96343317+omansh-krishn@users.noreply.github.com>
2023-03-15 05:21:56 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,6 @@ git clone https://github.com/omansh-krishn/local_manifest -b evox-test .repo/loc
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # @omansh_krishn
-
 # build rom
 
 source build/envsetup.sh
