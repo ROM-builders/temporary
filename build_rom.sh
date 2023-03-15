06b7312ab180fd7a1be9e20ef31de2e19acd74f7
IdkAnythin07 <avranil2007rt@gmail.com>
2023-03-15 11:57:13 +0530

    initial

--- build_rom.sh
+++ build_rom.sh
@@ -5,6 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
+lunch bliss_RMX1941-userdebug
 export TZ=Asia/Kolkata #put before last build command
 blissify -v RMX1941
 
