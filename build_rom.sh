ef5cb99e44c4b1054586bb0b14fa9b3ae635e3f0
IdkAnythin07 <94067465+IdkAnythin07@users.noreply.github.com>
2023-03-15 22:39:29 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/BlissRoms/platform_ma
 git clone https://github.com/IdkAnythin07/local_manifest --depth 1 -b bliss .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rum
 source build/envsetup.sh
 lunch bliss_RMX1941-userdebug
 export TZ=Asia/Kolkata #put before last build command
