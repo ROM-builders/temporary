ecfef1f92eadcba4844d84b7a6a0b57524265a92
Ansh <singhansh64321@gmail.com>
2023-03-15 15:27:00 +0530

    RM6785-ricedroidOSS-thirteen-EvilAnsh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_RM6785-userdebug
+lunch lineage_RM6785-user
 export WITH_GMS=true
 export TZ=Asia/Mumbai #put before last build command
 make bacon
