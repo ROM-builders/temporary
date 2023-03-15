db7faf6d53224f51020387251fc3ff469a39a788
iTZ_UDAY2312 <72090728+iTZUDAY2312@users.noreply.github.com>
2023-03-15 18:01:35 +0530

    build: Los 19.1 a12L bringup

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android -b 12.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android -b lineage-19.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/iTZUDAY2312/android_manifest --depth 1 -b curtana .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
