c8832e5bffb4dec07e11eaf0652a5fbdffe18cf7
Hakan <hknhzl@gmail.com>
2023-03-15 07:14:31 +0300

    Trigger build

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/ricedroidOSS/android 
 git clone https://github.com/Lafactorial/local_manifest --depth 1 -b rice .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom 
 source build/envsetup.sh
 lunch lineage_tissot-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
