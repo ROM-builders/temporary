bd08aa950acc1c75e6ed2e105632549747c6dd6c
Jax Lee <sd369888@outlook.com>
2023-03-16 23:25:13 +0800

    Update build_rom.sh
    add gapps and see if src is ok

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 export WITH_GAPPS=true
+export CHERISH_VANILLA=false
 export TZ=Asia/Shanghai #put before last build command
 brunch everpal
 
