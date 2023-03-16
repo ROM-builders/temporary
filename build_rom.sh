0c306fdc0b1e93243db0e425941b978be7ca8686
IdkAnythin07 <94067465+IdkAnythin07@users.noreply.github.com>
2023-03-17 00:36:27 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rum
 source build/envsetup.sh
 lunch bliss_RMX1941-userdebug
-export TZ=Asia/Kolkata #put before last build command
+export TZ=Asia/Kolkata #put b4 last build command
 blissify -v RMX1941
 
 # upload rum (if you don't need to upload multiple files, then you don't need to edit next line)
