ed0faced16ae3409c80ab1d0affb1f84dc5c4d80
Python Py <89606297+pythonpy1997@users.noreply.github.com>
2023-03-16 10:45:04 +0530

    Adding user build and prebuild kernel

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch aosp_RM6785-userdebug
+lunch aosp_RM6785-user
+export PREBUILT_KERNEL=true
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
