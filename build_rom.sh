ef1dd19d688a7f2c6790c63e1a8e3353fd04d194
mozzaru <rianyanuar52@gmail.com>
2023-03-17 07:28:31 +0700

    adapt bkaze

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/mozzaru/local_manifest.git --depth 1 -b blaze-13 .r
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+. build/envsetup.sh
 lunch blaze_markw-userdebug
 export TZ=Asia/Dhaka #put before last build command
 brunch markw
