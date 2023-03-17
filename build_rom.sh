57c68b2b9d224da86967e549f83ce2d90ada0c5f
ThunderBolt <93515519+ironHeart6@users.noreply.github.com>
2023-03-18 01:28:35 +0600

    Let's boot it

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,7 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/syberia-project/manifest.git -b 13.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Amritorock/local_manifest --depth 1 -b syb .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
- 
+
 # build rom
 source build/envsetup.sh
 lunch syberia_r5x-userdebug 
