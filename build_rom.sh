1dcfc28d6bddf9003ff5a3332939e5ab856f25a5
exer <98556122+ekkusa@users.noreply.github.com>
2023-03-16 17:56:40 -0700

    build_rom.sh: Init Bootleg Tirimbino

--- build_rom.sh
+++ build_rom.sh
@@ -4,12 +4,13 @@ git clone https://github.com/Miiyo/local-manifest.git --depth 1 -b aospa-topaz .
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build roms
-lunch aospa_akari-userdebug
+lunch bootleg_akari-userdebug
 source build/envsetup.sh
+export WITH_GAPPS=true
 export BUILD_USERNAME=exer
 export BUILD_HOSTNAME=miyo
 export TZ=Asia/Singapore #put before last build command
-./rom-build.sh akari -c -t userdebug -v beta
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
