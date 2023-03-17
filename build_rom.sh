d732125b68f1f4663299e1b84bacebd038d3bb57
Moji <68327458+mojienjoyment@users.noreply.github.com>
2023-03-17 18:51:52 +0330

    cancro-LineageOS-lineage-18.1-mojienjoyment
    Build LineageOS for Cancro

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch lineage_cancro-userdebug
-export TZ=Asia/Tehran #put before last build command
+export TZ=Asia/Tehran #putt before last build command
 m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
