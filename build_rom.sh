3f37e86736d1f36d7c2742eedc23b45117dfd882
Omansh Krishn <96343317+omansh-krishn@users.noreply.github.com>
2023-03-17 09:36:58 +0530

    santoni-ProjectBlaze-13-omansh-krishn

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ricedroidOSS/android -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/omansh-krishn/local_manifest --depth 1 -b ricedroid .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/omansh-krishn/local_manifest --depth 1 -b blaze13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 export ALLOW_MISSING_DEPENDENCIES=true
 source build/envsetup.sh
+lunch blaze_santoni-userdebug
 export TZ=Asia/Kolkata #put before last build command
 brunch santoni
-#added webview manually
+
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
