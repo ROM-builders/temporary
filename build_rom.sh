3cb61470afc4646151f01dbc11c1c910a3996523
Omansh Krishn <96343317+omansh-krishn@users.noreply.github.com>
2023-03-17 13:37:55 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,6 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 
+
 export ALLOW_MISSING_DEPENDENCIES=true
 source build/envsetup.sh
 lunch blaze_santoni-userdebug
