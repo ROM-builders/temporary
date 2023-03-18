68ab6a6455af4a579f95e4e0d9e6d3b690d92fde
darshan <43618707+mevadadarshan19@users.noreply.github.com>
2023-03-18 22:00:37 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -11,7 +11,7 @@ export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
    
-    
+     
      
  
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
