39002b2c4268a1ccbf98bb5ad3b0066cc5eaa9fc
Omansh Krishn <96343317+omansh-krishn@users.noreply.github.com>
2023-03-18 14:11:56 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 
 export ALLOW_MISSING_DEPENDENCIES=true
-source . build/envsetup.sh
+source build/envsetup.sh
 lunch blaze_santoni-userdebug
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
