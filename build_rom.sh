d389e8afe539cd96d511302b787b2e95d4f59b2e
IdkAnythin07 <94067465+IdkAnythin07@users.noreply.github.com>
2023-03-18 23:29:08 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync ru
+# sync rum
 repo init --depth=1 --no-repo-verify -u https://github.com/BlissRoms/platform_manifest -b typhoon -g default,-mips,-darwin,-notdefault
 git clone https://github.com/IdkAnythin07/local_manifest --depth 1 -b bliss .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch bliss_RMX1941-userdebug
 export TZ=Asia/Kolkata #put b4 lst build command
-blissify -v RMX1941
+blissify -g RMX1941
 
 # upload rum (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
