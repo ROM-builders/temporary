399635656642a702999a75b524d61332ad0cd822
Đł Ɽ₳łⱫɆⱠ ㋡ <122148266+acex69@users.noreply.github.com>
2023-03-18 15:28:06 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ricedroidOSS/android.git -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/acex69/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/acex69/local_manifest.git --depth 1 -b Blaze .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_lancelot-eng
+lunch blaze_lancelot-userdebug
 export TZ=Asia/Dhaka #put before last build command
 export SELINUX_IGNORE_NEVERALLOWS=true
 mka bacon
