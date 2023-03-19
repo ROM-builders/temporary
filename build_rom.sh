fe82542d230984ebec8ac0799e41953e9b3da399
Cherry <99527152+blueseaxy@users.noreply.github.com>
2023-03-19 08:18:33 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init -u ssh://git@github.com/NusantaraProject-ROM/android_manifest.git -b 12.1
+repo init -u https://github.com/Havoc-OS/android_manifest.git -b twelve
 git clone https://github.com/blueseaxy/local_manifest.git .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch nad_garden-userdebug
+lunch havoc_garden-userdebug
 export TZ=Asia/Jakarta #put before last build command
-mka nad 
+mka havoc 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
