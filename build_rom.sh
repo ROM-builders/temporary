360f50fa241cfb17ed4370240d11cca6d3dd9df2
Cherry <99527152+blueseaxy@users.noreply.github.com>
2023-03-19 09:10:50 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom
+# sync rom #
 repo init --depth=1 --no-repo-verify -u https://github.com/Havoc-OS/android_manifest.git -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/blueseaxy/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
