b9f7d5a10506b220ac22610c5d3632734f963e80
JodOrWot <111036482+athizz2005@users.noreply.github.com>
2023-03-20 14:29:02 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/ricedroidOSS/android 
 git clone https://github.com/athizz2005/local_manifest --depth 1 -b 13 .repo/local_manifests 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
  
-# build rom 
+# build rom #
 source build/envsetup.sh 
 lunch lineage_violet-userdebug 
 export TZ=Asia/Dhaka #put before last build command 
