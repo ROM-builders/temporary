5349a5a82e9c142cbd809621ac2c5caf51d8ebcd
mozzaru <rianyanuar52@gmail.com>
2023-03-20 14:35:03 +0700

    Spark: Test Changes

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch spark_markw-eng
 export WITH_GAPPS=false
-export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
