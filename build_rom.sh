0b0dab7658a50220d426942bc62bc9c6d0923251
trying122 <112693784+trying122@users.noreply.github.com>
2023-03-20 19:44:24 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/crdroidandr
 git clone https://github.com/trying122/local_manifest --depth 1 -b face .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
   
-# build rom
+# build rom 
 source build/envsetup.sh
 lunch lineage_beryllium-userdebug
 export BUILD_USERNAME=trying122
