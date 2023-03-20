dd8f769774fa599ddf6ed89bcc7dd20a9a0851d9
Asyanx <76146196+Asyanx@users.noreply.github.com>
2023-03-20 17:27:25 +0700

    build: try again

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom
+# sync rom pex
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExtended/manifest.git -b trece -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Asyanx/local_manifest.git --depth 1 -b aosp-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
