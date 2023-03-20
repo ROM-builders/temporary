20fea3a647cccef3246c244f2740d7bc1d35f796
sd369888 <127422397+sd369888@users.noreply.github.com>
2023-03-20 23:09:05 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,8 +7,10 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch spark_everpal-userdebug
 export WITH_GAPPS=true
+export BUILD_USERNAME=Weaponmasterjax
+export BUILD_HOSTNAME=amdyes
 export TZ=Asia/Brunei #put before last build command
-mka evolution
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
