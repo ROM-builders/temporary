dea7348bc195c427b84840e6cebf10fb2b771b1c
Asyanx <76146196+Asyanx@users.noreply.github.com>
2023-03-21 06:38:51 +0700

    fog-PixelExtended-trece-asyanx

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
-# sync rom pex
+# sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExtended/manifest.git -b trece -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Asyanx/local_manifest.git --depth 1 -b aosp-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom pex
+# build rom
 source build/envsetup.sh
 lunch aosp_fog-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
