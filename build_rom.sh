f0c5824559932d98b80e89d1580fad7f550f0dbd
exer <98556122+ekkusa@users.noreply.github.com>
2023-03-21 00:23:55 -0700

    build_rom.sh: akari-AOSPA-topaz-ekkusa

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,16 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/BootleggersROM/manifest.git -b tirimbino -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Miiyo/local-manifest.git --depth 1 -b tirimbino .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest -b topaz -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Miiyo/local-manifest.git --depth 1 -b aospa-topaz .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build roms
-source build/envsetup.sh
-lunch bootleg_akari-userdebug
-export WITH_GAPPS=true
-export BUILD_USERNAME=exer
-export BUILD_HOSTNAME=miyo
-export TZ=Asia/Singapore #put before last build command
-mka bacon
+lunch aospa_akari-userdebug
+export BUILD_USERNAME=nobody
+export BUILD_HOSTNAME=android-build
+export KBUILD_BUILD_USER=nobody
+export KBUILD_BUILD_HOST=android-build
+export TZ=Asia/Singapore
+./rom-build.sh akari -c -t userdebug -v beta
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
