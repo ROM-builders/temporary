41d9444d2106b49a5baf3a63074b14ae3834ed80
Amrito Karmokar <amritokarmokar5i@gmail.com>
2023-03-21 09:40:10 +0600

    Fix

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/Amritorock/local_manifest --depth 1 -b syb .repo/lo
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom 
-source build/envsetup.sh 
+source build/envsetup.sh
 lunch syberia_r5x-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
