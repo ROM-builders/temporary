f2ee96e51ccb44d0b643c4c7d5adfee69db1c738
Karthik <karthik1896@outlook.com>
2023-03-21 11:37:18 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rommyk
+# sync rommykk
 repo init --depth=1 --no-repo-verify -u https://github.com/P-404/android_manifest -b tokui -g default,-mips,-darwin,-notdefault
 git clone https://github.com/karthik1896/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
