db64c5000488e419f63ac3c9d0ae18289da00733
trying122 <112693784+trying122@users.noreply.github.com>
2023-03-21 10:09:01 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/crdroidandroid/android -b 13.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/trying122/local_manifest --depth 1 -b face .repo/local_manifests
+repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/LineageOS/android -b lineage-20.0 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/trying122/local_manifest --depth 1 -b lin1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
  
 # build rom 
