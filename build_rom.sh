bc80619e53289a1902d303e374adf1038ccf989e
Aayush Kumar <110930480+Aayushk24@users.noreply.github.com>
2023-03-22 17:02:33 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init -u https://github.com/PixelExtended/manifest -b trece --depth=1 --no-repo-verify
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExtended/manifest -b trece -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Aayushk24/Manifest --depth 1 -b akmain .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
