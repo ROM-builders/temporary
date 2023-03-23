bb99295cff01eadbdf720804fcdec4f64e4a48c1
Ansh <singhansh64321@gmail.com>
2023-03-23 11:15:28 +0530

    CherishOS: Build v4.6

--- build_rom.sh
+++ build_rom.sh
@@ -6,9 +6,9 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch cherish_RM6785-user
-export CHERISH_VANILLA=true
+export CHERISH_VANILLA=false
 export TZ=Asia/Mumbai #put before last build command
 make bacon
 
-# upload rom (if your don't need to upload multiple files, then you don't need to edit next line)
+# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
