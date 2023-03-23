c66b4ab341edad960cfddca05ad708bc2d166cf3
Jax Lee <sd369888@outlook.com>
2023-03-23 22:50:59 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -10,6 +10,7 @@ export TZ=Asia/Shanghai #put before last build command
 lunch spark_everpal-userdebug
 mka bacon
 
+
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
 #P.S:I know u guys are working hard to support the servers and i wont cause troubles anymore TwT
