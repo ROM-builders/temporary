c2195fc0fb3a6d18dd7ec99af715eaad068e26cb
Jax Lee <sd369888@outlook.com>
2023-03-24 22:09:28 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,6 +4,8 @@ git clone https://github.com/weaponmasterjax/local_manifest.git --depth 1 -b rip
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
+export SELINUX_IGNORE_NEVERALLOWS=true
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 source build/envsetup.sh
 export TZ=Asia/Shanghai #put before last build command
 lunch awaken_everpal-userdebug
