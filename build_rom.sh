3a549f714ec0581860f7566c6ce4fd70ad6fabf9
Prettysir <114632521+Prettysir@users.noreply.github.com>
2023-03-24 18:28:13 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify repo -u https://github.com/DerpFest-12/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/Prettysir/local_manifest/blob/decommonized/local_manifest.xml --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
+lunch derp_X00QD-user
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
