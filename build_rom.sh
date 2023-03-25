8ae6c7f0a8e07a3c997c001c6b15141e6d99a227
Vestia Zaka <126310783+Zaka531@users.noreply.github.com>
2023-03-25 12:19:46 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,10 +5,10 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_A37-userdebug
+breakfast lineage_A37-userdebug
 export TARGET_BOOT_ANIMATION_RES=720
 export TZ=Asia/Dhaka #put before last build command
-mka lineage
+brunch A37
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
