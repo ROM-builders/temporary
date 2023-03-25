f68acb73ad80863aa111b0994636336d2e16cea6
d4fun <126586537+d4fun@users.noreply.github.com>
2023-03-25 14:35:29 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 -u https://github.com/Corvus-AOSP/android_manifest.git -b 13
-git clone https://github.com/d4fun/local_manifests.git --depth 1 -b master .repo/local_manifests
-repo sync -j$(nproc --all) --force-sync --no-tags --no-clone-bundle
+repo init --depth=1 -u https://github.com/PixelPlusUI/manifest -b tiramisu
+git clone https://github.com/d4fun/local_manifests.git --depth 1 -b tiramisu .repo/local_manifests
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
 # build rom
 source build/envsetup.sh
-lunch corvus_viva-userdebug
+lunch aosp_viva-userdebug
 export TZ=Asia/Jakarta #put before last build command
-make corvus
+mka bacon -jX
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
