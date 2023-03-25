81d276ffb9aa3eb2d86c6cceb2fe71f3186c3619
Asyanx <76146196+Asyanx@users.noreply.github.com>
2023-03-25 16:20:16 +0700

    fog: fixup

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom
+# sync rom pixys
 repo init --depth=1 --no-repo-verify -u https://github.com/PixysOS/manifest.git -b thirteen-v1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Asyanx/local_manifest.git --depth 1 -b pixys-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
