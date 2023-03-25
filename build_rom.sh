8ae51ee2e36d4008d92fb46345ffe9ce86fb203b
d4fun <126586537+d4fun@users.noreply.github.com>
2023-03-25 14:44:39 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 -u https://github.com/PixelPlusUI/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
 git clone https://github.com/d4fun/local_manifests.git --depth 1 -b tiramisu .repo/local_manifests
-repo sync -c -j8 --force-sync --no-clone-bundle --no-tags
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
