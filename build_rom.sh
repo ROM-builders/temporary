88abff204aad84b60f581693435ffaa4c433f53a
Prettysir <114632521+Prettysir@users.noreply.github.com>
2023-03-24 23:06:35 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,8 @@
 # sync rom
-repo init --depth=1 --no-repo-verify repo -u https://github.com/DerpFest-12/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify repo -u https://github.com/PixysOS/manifest -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Prettysir/local_manifest/blob/decommonized/local_manifest.xml --depth 1 -b master .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c -j8 --force-sync --no-clone-bundle --no-tags
+
 
 # build rom
 source build/envsetup.sh
