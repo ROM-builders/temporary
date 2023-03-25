f51a0e321bea5fafa6aaf194787166b54fc82869
eartinity24 <116898350+eartinity24@users.noreply.github.com>
2023-03-25 10:04:52 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/PixelExperience/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/eartinity24/local_manifest.git --depth 1 -b Pe .repo/local_manifests
+git clone https://github.com/eartinity24/local_manifest.git --depth 1 -b thirteen .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
