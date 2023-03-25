73c6d2d73735d4342494ceb3909c69d5739d83b7
Prettysir <114632521+Prettysir@users.noreply.github.com>
2023-03-25 00:23:39 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ricedroidOSS/android -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Prettysir/local_manifest.git -b rice .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/Project-Xtended/manifest.git -b xt -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Prettysir/Local-Manifest.git --depth 1 -b xt .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_x00qd-user
-export BUILD_USERNAME=Cold
-export BUILD_HOSTNAME=colddz
-export TZ=Asia/Kolkata #put before last build command
-mka bacon
+lunch xtended_x00qd-user
+export TZ=Asia/Dhaka #put before last build command
+make xtended  
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
