b85e1212cd1fc6bad85ad0880d158ddded682ba6
PrathamWasTaken <perrygamingoffical@gmail.com>
2023-03-26 15:24:37 +0530

    RM6785-ArrowOS-13-PrathamWasTaken
    Build ArrowOS for RM6785

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,16 @@
-# sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+#!/bin/bash
+
+# Sync repositories. Clone sources for compilation.
+repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS-MiPa-Edition/android_manifest.git -b arrow-13.0 -g default,-mips,-darwin,-notdefault
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+git clone --depth 1 -b arrow-13 https://github.com/PrathamWasTaken/device_realme_RM6785 device/realme/RM6785
 
-# build rom
-source build/envsetup.sh
-lunch derp_vayu-user
-export TZ=Asia/Dhaka #put before last build command
-mka derp
+# Compile the ROM. Build using sources.
+. build/envsetup.sh
+lunch arrow_RM6785-user
+export TZ=Asia/Kolkata
+export ARROW_GAPPS=true
+mka bacon -j"$(nproc --all)"
 
-# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+# Upload the ROM. Make it available for download.
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
