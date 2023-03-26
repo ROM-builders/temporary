fe7fc3fe24eb2613e3480229d9db7691ca8ef2f7
mozzaru <100791931+mozzaru@users.noreply.github.com>
2023-03-26 16:20:56 +0700

    Test

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,9 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch spark_markw-eng
+export WITH_GAPPS=true
+export SELINUX_IGNORE_NEVERALLOWS=true
+export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
