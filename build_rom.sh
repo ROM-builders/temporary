6a48e989ad523bbd6a39a637b3d3a6b02f7409b6
Rahul Kantrapally <rahul-k@bigdi.cc>
2023-03-26 23:12:03 +0100

    lunaa: Initial topaz CI build

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,13 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest.git -b topaz -g default,-mips,-darwin,-notdefault
-git clone https://github.com/XenStuff/manifest.git --depth 1 -b aospa .repo/local_manifests
+git clone https://github.com/rk134/local_manifest.git --depth 1 -b aospa .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aospa_phone1-userdebug
+lunch aospa_lunaa-eng
 export TZ=Asia/Dhaka #put before last build command
-./r* phone1 -v beta -s certs -t user -z
+./r* lunaa -v alpha -n 1 -t eng
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-rclone copy *aospa**2023*zip cirrus:lavender -P
