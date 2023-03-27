8a4c896082611ffa6f1b2da6edfb65b0b32ff64e
IdkAnythin07 <94067465+IdkAnythin07@users.noreply.github.com>
2023-03-27 19:15:30 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/IdkAnythin07/local_manifest --depth 1 -b Pixel_exp .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ricedroidOSS/android -b thirteen -g default,-mips,-darwin,-notdefault
+git clone https://github.com/IdkAnythin07/local_manifest --depth 1 -b rice .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
