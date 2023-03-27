e7d862cbd1dcb958393051ea45598488f3aa4e45
GhostMaster69-dev <rathore6375@gmail.com>
2023-03-28 01:54:20 +0530

    octavi: Test changes

--- build_rom.sh
+++ build_rom.sh
@@ -6,8 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch octavi_vince-user
-export USE_GAPPS=true
-export WITH_GAPPS=true
+#export USE_GAPPS=true
+#export WITH_GAPPS=true
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
