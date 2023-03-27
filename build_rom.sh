dc8e2b80be5cc3b8de1f4820b5073a62dc2f888d
alternoegraha <37102951+alternoegraha@users.noreply.github.com>
2023-03-27 13:54:35 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest.git -b thirteen-plus -g default,-mips,-darwin,-notdefault
-git clone https://github.com/alternoegraha/local_manifest.git --depth 1 -b pixel2 .repo/local_manifests
+git clone https://github.com/alternoegraha/local_manifest.git --depth 1 -b pixelexperience .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build roms
