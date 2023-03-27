ced314e49a8ce3359445c3364e8d3382fa453e9e
Omansh Krishn <96343317+omansh-krishn@users.noreply.github.com>
2023-03-27 05:37:36 +0530

    updated local manifest

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,5 @@
 # sync rom
+
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest.git -b thirteen-plus -g default,-mips,-darwin,-notdefault
 git clone https://github.com/omansh-krishn/local_manifests --depth 1 -b pixelexperience-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
