131f03556876ef88a91cb770b70dfbb1b1779e77
Akshat Singh <91002031+Legendleo90@users.noreply.github.com>
2023-03-27 08:54:38 +0530

    Add Gapps

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch spark_beryllium-userdebug
+export WITH_GAPPS=true
 export BUILD_USERNAME=Akshat
 export BUILD_HOSTNAME=leopubglover
 export TZ=Asia/Kolkata #put before last build command
