4424bdfd7aa60c4bfff16a1e095a21ecd55fda0c
Mudabbirul Islam <71183979+MudabbirulSaad@users.noreply.github.com>
2023-03-28 12:30:48 +0600

    update branch

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://github.com/MudabbirulSaad/local_manifests --depth 1 -b pe-dumpling .repo/local_manifests
+git clone https://github.com/MudabbirulSaad/local_manifests --depth 1 -b evox-dumpling .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
