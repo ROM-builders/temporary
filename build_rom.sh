ed6fa5597c6779e35d48d965cd0eb5b715b0b59b
Asif <49162687+nhAsif@users.noreply.github.com>
2023-03-28 14:41:40 +0600

    test kernelsu

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_man
 git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b cherish-alioth .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom [5]
+# build rom [6]
 . build/envsetup.sh
 lunch cherish_alioth-user
 export TZ=Asia/Dhaka #put before last build command
