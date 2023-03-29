4f3d49bf574c20504aeef06cc90700c5759ced19
MiDoNaSR545 <57400341+MiDoNaSR545@users.noreply.github.com>
2023-03-30 00:18:29 +0200

    March update

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/CherishOS/android_manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone  https://github.com/MiDoNaSR545/mainfest_personal --depth 1 -b dreamlte .repo/local_manifests
+git clone  https://github.com/MiDoNaSR545/mainfest_personal --depth 1 -b dreamltee .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
