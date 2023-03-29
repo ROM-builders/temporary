69029502a90b45baaac5ac324a26eed67af68716
Killerpac <45239423+Killerpac@users.noreply.github.com>
2023-03-29 22:25:52 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch aosp_nashc-userdebug
-export TZ=Asia/Kolkata #work 
+export TZ=Asia/Kolkata 
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
