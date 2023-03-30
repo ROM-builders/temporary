c45bd9ad8abbfcf644864fd2e5fb140166998c93
K R I S H N A <116506067+pure-soul-kk@users.noreply.github.com>
2023-03-30 16:06:01 +0530

    Sweet: changed rom repository link

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/PixelExtended/manifest/manifest.git -b trece -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/PixelExtended/manifest.git -b trece -g default,-mips,-darwin,-notdefault
 git clone https://github.com/pure-soul-kk/local_manifest.git --depth 1 -b pex .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
