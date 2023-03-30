79db11c6bb741c4583dec7fa77a870c0a9051bcb
Sensei-Developer <111665604+Sensei-Developer@users.noreply.github.com>
2023-03-31 00:29:36 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,3 +1,4 @@
+
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Sensei-Developer/local_manifest.git --depth 1 -b lineage-20.0_lavender .repo/local_manifests
@@ -11,6 +12,7 @@ brunch lavender
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+
 
 
 
