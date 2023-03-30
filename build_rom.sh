1160738c47a520967e65e222bc009fd8e7cc706c
IMpoSIbbLE990 <73650475+wgduwjej@users.noreply.github.com>
2023-03-30 07:56:47 +0700

    PIXEL-EXPERIENCE
    PE Build for Realme C11

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,14 @@
 # sync rom
-repo init -u https://github.com/Octavi-OS/platform_manifest.git -b 12 g default,-
+repo init -u https://github.com/PixelExperience/manifest -b eleven -g default,-
 mips,-darwin,-notdefault
 git clone https://github.com/wgduwjej/manifest.git --depth 1 -b master .repo/local_manifest
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch octavi_RMX2185-userdebug
+lunch aosp_RMX2185-userdebug
 export TZ=Asia/Dhaka #put before last build command
-brunch RMX2185
+mka bacon -j
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
