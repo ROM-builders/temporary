873ea64e80e5f840e65870adad445ad0f51dcc41
Python Py <89606297+pythonpy1997@users.noreply.github.com>
2023-03-30 12:10:23 +0530

    Build command changed [ octavi ]
    For Octavi

--- build_rom.sh
+++ build_rom.sh
@@ -5,10 +5,10 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch octavi_RM6785-user
+lunch octavi_RM6785-userdebug
 export PREBUILT_KERNEL=true
 export TZ=Asia/Kolkata #put before last build command
-mka bacon
+brunch RM6785
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
