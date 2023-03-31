75a3ced902c4b1c75ca81f73aca77dfd43dce6eb
IdkAnythin07 <94067465+IdkAnythin07@users.noreply.github.com>
2023-03-31 20:41:46 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,14 @@
-# sync rom
+# sync rum
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
 git clone https://github.com/IdkAnythin07/local_manifest --depth 1 -b evox .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rum
 source build/envsetup.sh
 lunch evolution_RMX1941-userdebug
 export USERNAME=IdkAnythin07
 export TZ=Asia/Kolkata #put before last build command
 mka evolution
 
-# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+# upload rum (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
