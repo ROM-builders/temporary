3f21aacbac2020974241601906e05b599debbda5
SurvivalHorror <33040871+SurvivalHorror@users.noreply.github.com>
2023-03-31 22:20:10 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 11.0  -g default,-mips,-darwin,-notdefault
 git clone https://github.com/SurvivalHorror/local_manifest.git --depth 1 -b main .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags  --optimized-fetch --force-sync --prune -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
