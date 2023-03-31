dea438205ade1eae97f982be99b13cf91c974b85
SurvivalHorror <33040871+SurvivalHorror@users.noreply.github.com>
2023-03-31 21:53:24 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,15 @@
 # sync rom
 # repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-repo init --depth=1 -u https://github.com/crdroidandroid/android.git -b 11.0 --no-repo-verify -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 11.0  -g default,-mips,-darwin,-notdefault
 # git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
 git clone https://github.com/SurvivalHorror/local_manifest.git --depth 1 -b main .repo/local_manifests
-repo sync --force-sync --current-branch --no-tags --no-clone-bundle --optimized-fetch --prune -j$(nproc --all)
+# repo sync --force-sync --current-branch --no-tags --no-clone-bundle --optimized-fetch --prune -j$(nproc --all)
+repo sync -c --no-clone-bundle --no-tags  --optimized-fetch --force-sync --prune -j$(nproc --all)
 
 # build rom
 source build/envsetup.sh
-# lunch derp_vayu-user
 breakfast cheryl
 # export TZ=Asia/Dhaka #put before last build command
-# mka derp
 brunch cheryl
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
