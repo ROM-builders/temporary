839faa986b5963c3af75b91091a6305222f98a69
nhAsif <najmulhasan3609@gmail.com>
2023-03-31 09:18:05 +0600

    initial build

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b cherish-jasmine .repo/local_manifests
+git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b cherish-wayne .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom [9]
 . build/envsetup.sh
-lunch cherish_jasmine_sprout-userdebug
+lunch cherish_wayne-user
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
