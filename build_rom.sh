3813eb19474535dc27843b026ebd5d9b60b45166
SurvivalHorror <33040871+SurvivalHorror@users.noreply.github.com>
2023-03-31 20:13:43 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@
 repo init --depth=1 -u git://github.com/crdroidandroid/android.git -b 11.0 --no-repo-verify -g default,-mips,-darwin,-notdefault
 # git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
 git clone https://github.com/SurvivalHorror/manifest_cheryl_crdroid_tmp.git --depth 1 -b main .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync --force-sync --current-branch --no-tags --no-clone-bundle --optimized-fetch --prune -j$(nproc --all)
 
 
 # build rom
@@ -12,7 +12,7 @@ source build/envsetup.sh
 breakfast cheryl
 # export TZ=Asia/Dhaka #put before last build command
 # mka derp
-mka systemimage
+brunch cheryl
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
