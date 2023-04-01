b972197c5cd619e808401a87ca2ec659a3e6e4ba
Joxquin <68979987+Joxquin@users.noreply.github.com>
2023-03-31 19:27:47 -0500

    Fixes

--- build_rom.sh
+++ build_rom.sh
@@ -1,8 +1,13 @@
 # sync rom
-repo init -u https://github.com/PixysOS/manifest -b thirteen
-repo sync -c -j8 --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo init --depth=1 --no-repo-verify -u https://github.com/PixysOS/manifest -b thirteen -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Joxquin/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+. build/envsetup.sh
 lunch pixys_fog-userdebug
+export TZ=Asia/Dhaka #put before last build command
 make pixys
+
+# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
