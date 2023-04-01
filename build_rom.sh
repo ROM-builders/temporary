e3451e017d6bc1af2bce14d74a54e5e6177bf4db
Python Py <89606297+pythonpy1997@users.noreply.github.com>
2023-04-01 08:08:44 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,10 +4,10 @@ git clone https://github.com/pythonpy1997/local_manifests.git --depth 1 -b thirt
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
-lunch octavi_RM6785-user
+. build/envsetup.sh
+lunch octavi_RM6785-userdebug
 export TZ=Asia/Kolkata #put before last build command
-mka octavi 
+m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
