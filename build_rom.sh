797b433b3bc9af81a65153af14c6027c273e1430
Ansh <singhansh64321@gmail.com>
2023-04-02 12:05:11 +0530

    OctaviOS: Build v4.2

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://github.com/EvilAnsh/Local-Manifest.git --depth 1 -b cva .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Octavi-Staging/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
+git clone https://github.com/pythonpy1997/local_manifests.git --depth 1 -b thirteen-oct .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch cherish_RM6785-user
-export CHERISH_VANILLA=false
+lunch octavi_RM6785-userdebug
 export TZ=Asia/Mumbai #put before last build command
-make bacon
+brunch RM6785
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
