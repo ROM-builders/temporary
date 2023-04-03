47fdc14f2984c49129adcdc2a96d13862044bfe3
Sajid Hossain Chowdhury <127777393+Sajid445@users.noreply.github.com>
2023-04-03 13:15:28 +0530

    Troika-lineage-Sajid445
    Lineage-20

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git@github.com:LineageOS/android.git -b lineage-20.0 --git-lfs
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs
 git clone https://github.com/Sajid445/local_manifest.git .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
