1b1bd8f4667a6868b03068c3ee99657b5de1a18a
ayeneo <129434536+ayeneo@users.noreply.github.com>
2023-04-03 18:17:49 +0300

    Delete ci directory

--- ci/ccache_stats.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-if [[ $ax613_roms != *" $rom_name "* ]]; then exit 0; fi
-export CCACHE_DIR=~/ccache/$rom_name/$device
-ccache -s
--- ci/remove.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -e
-if [[ $ax613_roms != *" $rom_name "* ]]; then exit 0; fi
-rm -rf ~/roms/$rom_name/out/target/product/$device
