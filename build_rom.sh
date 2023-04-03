156bdc9b19acfdc03a2834fad12ded0e506bda41
emmnuel69 <121844716+emmnuel69@users.noreply.github.com>
2023-04-03 18:25:15 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,15 @@
 # sync rom
-repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 13 default,-mips,-darwin,-notdefault
+repo init -u https://github.com/Project-Awaken/android_manifest -b triton --git-lfs default,-mips,-darwin,-notdefault
 git clone https://github.com/Akariiiiiiiiiiiii/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_lancelot-user
+lunch awaken_lancelot-user
 export BUILD_USERNAME=Emmnuel
 export BUILD_HOSTNAME=Emmnuel
 export TZ=Asia/Manila #put before last build command
-mka derp
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
