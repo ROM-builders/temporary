f7d8516915785625b64728a397e0a0b9c33bd681
darshan <43618707+mevadadarshan19@users.noreply.github.com>
2023-04-04 12:18:07 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -10,7 +10,7 @@ lunch bliss_X01BD-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Dhaka #put before last build command
 blissify X01BD
-
+ 
  
   
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
