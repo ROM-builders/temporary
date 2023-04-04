0f2f0b019e1345a0ca4177fe72016e318c32ef61
Alone0316 <rutujamandlik06@gmail.com>
2023-04-04 05:22:32 +0000

    userdebug

--- build_rom.sh
+++ build_rom.sh
@@ -5,9 +5,9 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch aospa_mido-eng
+lunch aospa_mido-userdebug
 export TZ=Asia/Dhaka #put before last build command
-./rom-build.sh mido -t eng
+./rom-build.sh mido -t userdebug
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
