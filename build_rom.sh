314fdaa4d105d07caf4631bf35fff2b60b1ec883
Hakan <hknhzl@gmail.com>
2023-04-04 16:57:09 +0300

    Trigger build

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_man
 git clone https://github.com/Lafactorial/local_manifest --depth 1 -b Cherish .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom 
+# build rom
 source build/envsetup.sh
 lunch cherish_tissot-userdebug
 export TZ=Asia/Dhaka #put before last build command
