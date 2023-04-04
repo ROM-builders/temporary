c75393b5952583605b1ca6c35dde12838335cc12
ayeneo <129434536+ayeneo@users.noreply.github.com>
2023-04-04 03:02:46 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b thirteen -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/ricedroidOSS -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/MiDoNaSR545/mainfest_personal -b los --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
