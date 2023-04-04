70488f77821419bd4c4be38b88c6eb5556605d33
Sajid Hossain Chowdhury <127777393+Sajid445@users.noreply.github.com>
2023-04-04 06:57:53 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs
-git clone https://github.com/Sajid445/local_manifest.git -b main LineageOs .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Sajid445/local_manifest.git --depth 1 -b main LineageOs .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
