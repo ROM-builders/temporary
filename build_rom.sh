2299c4f5c94fa870b3b588c8eb16fce5f644409b
Karthik <karthik1896@outlook.com>
2023-04-05 14:47:06 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rommykk
-repo init --depth=1 --no-repo-verify -u https://github.com/P-404/android_manifest -b tokui -g default,-mips,-darwin,-notdefault
-git clone https://github.com/karthik1896/local_manifest.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS-staging/manifest.git -b qpr2 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/karthik1896/local_manifest.git --depth 1 -b vos .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch p404_X00T-userdebug
+lunch voltage_X00T-userdebug
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
