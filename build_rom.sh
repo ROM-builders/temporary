3c6b6c3e0cfc84869e220055e3fcb0c8653866c8
Asif <49162687+nhAsif@users.noreply.github.com>
2023-04-05 21:26:44 +0600

    retry

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.g
 git clone https://github.com/levi3609/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build roms
+# build roms [1]
 source build/envsetup.sh
 lunch superior_alioth-user
 export TZ=Asia/Dhaka 
