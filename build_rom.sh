6837e3b8dddec715ae1d90676563beb57121941a
Python Py <89606297+pythonpy1997@users.noreply.github.com>
2023-04-07 03:19:19 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_RMX2001-userdebug
-export PREBUILT_KERNEL=true
 export TZ=Asia/Kolkata #put before last build command
 m bacon 
 
