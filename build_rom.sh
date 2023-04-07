3ec531f92fa9af287900d866db403a30f25cb3e1
SKetU <sketu@donot.care>
2023-04-07 09:03:24 +0530

    Awaken try for RMX1801

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/SKetU-l/local_manifest.git --depth 1 -b rice-r2p .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest.git -b triton -g default,-mips,-darwin,-notdefault
+git clone https://github.com/SKetU-l/local_manifest-1 --depth 1 -b awaken-r2p .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom #4
-source build/envsetup.sh
+  
+# build rom
+. build/envsetup.sh
+lunch awaken_RMX1801-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Kolkata #put before last build command
-brunch RMX1801
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
