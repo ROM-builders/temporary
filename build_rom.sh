ce0698226a3b877e5cf2cb4f69ff16bd4c24e5c9
snnbyyds <87487115+snnbyyds@users.noreply.github.com>
2023-04-07 21:42:33 +0800

    kernel test 2

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-
 croot
 lunch aosp_enchilada-userdebug
 export KBUILD_BUILD_USER=snnbyyds
