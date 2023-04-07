abbdbe95a16b8fd4c0644cf384650701a5b2163b
Daniel Arya <86230878+Wrdn28@users.noreply.github.com>
2023-04-07 20:38:07 +0700

    X01AD-BlissRoms-typhoon-Wrdn28

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/syberia-project/manifest.git -b 13.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Wrdn28/local_manifest.git --depth 1 -b syberia-13 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/BlissRoms/platform_manifest.git -b typhoon -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Wrdn28/local_manifest.git --depth 1 -b Bliss-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch syberia_X01AD-userdebug
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
-export ALLOW_MISSING_DEPEDENCIES=true
-export SELINUX_IGNORE_NEVERALLOWS=true
+lunch bliss_X01AD-userdebug
 export TZ=Asia/Jakarta #put before last build command
-mka syberia
+blissify -v X01AD
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
