53e628e4d3ee873a28afafbf105ea9ca1584b740
Jax Lee <sd369888@outlook.com>
2023-04-08 00:59:29 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://github.com/weaponmasterjax/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExtended/manifest -b trece --depth 1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/weaponmasterjax/local_manifest.git --depth 1 -b pex .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 export TZ=Asia/Shanghai #put before last build command
-lunch evolution_everpal-userdebug
+export BUILD_USERNAME=weaponmasterjax
+export BUILD_HOSTNAME=jaxone
+lunch aosp_everpal-userdebug
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
