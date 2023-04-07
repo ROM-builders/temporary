e5d5cf70d436803e93c83815d867f48110ab3269
ShahzebQureshi <55016172+ShahzebQureshi@users.noreply.github.com>
2023-04-07 06:28:28 +0500

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -11,7 +11,7 @@ export BUILD_USERNAME=ShahzebQureshi
 export BUILD_HOSTNAME=cirrus-ci
 lunch blaze_joan-userdebug
 export TZ=Asia/Karachi #put before last build command
-brunch joan
+m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
