6ec4e34d6b31c0d9d013e1370d46cc293865d95a
Debjyoti220 <116152719+BreathLGD@users.noreply.github.com>
2023-04-08 13:53:13 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest.git -b thirteen --depth=1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/BreathLGD/local_manifest --depth 1 
+git clone https://github.com/BreathLGD/local_manifest --depth 1 -b main .repo/local_menifest
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
