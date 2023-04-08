67f84589d82d898e78d449c2168ae173a56d01b4
snnbyyds <87487115+snnbyyds@users.noreply.github.com>
2023-04-08 18:17:59 +0800

    kernel test 3

--- build_rom.sh
+++ build_rom.sh
@@ -12,7 +12,6 @@ export KBUILD_BUILD_HOST=uotan
 export BUILD_USERNAME=snnbyyds
 export BUILD_HOSTNAME=uotan
 export TZ=Asia/Shanghai #put before last build command
-
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
