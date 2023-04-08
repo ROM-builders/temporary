3f0878d63f6eb10fd3db327c5b99794d3b52a758
pawar-mahesh <pawar.mahesh2345@gmail.com>
2023-04-09 00:10:55 +0530

    Add hostname
    Signed-off-by: pawar-mahesh <pawar.mahesh2345@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch aosp_alioth-user
+export BUILD_USERNAME=mahi
+export BUILD_HOSTNAME=mahi
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
