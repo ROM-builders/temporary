2b6cc9efc22d325f550cf5be5f9570913667084e
skittels15 <50242333+skittels15@users.noreply.github.com>
2023-04-08 23:46:55 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/camellia-roms/android_manifest -b 12 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/camellia-roms/local_manifest.git --depth 1 -b banana .repo/local_manifests
+git clone https://github.com/skittels15/local_manifest --depth 1 -b banana .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
