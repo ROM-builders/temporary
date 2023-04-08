292eae9b23d55f86655ecebac164a025ee0b1db3
rianza <116883839+rianza@users.noreply.github.com>
2023-04-09 05:45:04 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://github.com/rianza/Local_Manifest.git --depth 1 -b evolution-13 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/BootleggersROM/manifest.git -b tirimbino -g default,-mips,-darwin,-notdefault
+git clone https://github.com/rianza/Local_Manifest.git --depth 1 -b bootleg-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom                  
 source build/envsetup.sh
-lunch evolution_markw-userdebug
+lunch bootleg_markw-userdebug
 export TZ=Asia/Jakarta #put before last build command
-mka evolution
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
