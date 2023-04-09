a4d10f143a5730fc245faa7afe128978b910fd44
root <don.boruza@gmail.com>
2023-04-09 16:00:19 +0000

    build: run again

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/androi
 git clone https://github.com/donboruza/local_manifests.git --depth 1 -b crdroid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom 
+# build rom
 source build/envsetup.sh
 lunch lineage_dreamlte-userdebug
 export BUILD_USERNAME=don.boruza
