94068c13e86ecf52a62f93c60ecd38a5b8a74b91
GhostMaster69-dev <rathore6375@gmail.com>
2023-04-09 06:29:22 +0530

    Syberia Gapps
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch syberia_vince-userdebug
+export WITH_GMS=true
 mka syberia
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
