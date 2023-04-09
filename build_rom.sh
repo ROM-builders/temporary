62b7d466f0a7b0d6015b98f20e8f568af3582e7a
Đł Ɽ₳łⱫɆⱠ ㋡ <abhinav.115260@gmail.com>
2023-04-09 07:42:23 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-13.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/acex69/local_manifest.git --depth 1 -b arrow .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExtended/manifest.git -b trece -g default,-mips,-darwin,-notdefault
+git clone https://github.com/acex69/local_manifest.git --depth 1 -b aosp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch arrow_lancelot-userdebug
+lunch aosp_lancelot-userdebug
 export BUILD_USERNAME=@raizel_69
 
 export BUILD_HOSTNAME=@raizel69X
