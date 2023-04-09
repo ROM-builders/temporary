878c359f2d8074d9a2e3446102ec512b3653628e
Sachin9822 <70788804+Sachin9822@users.noreply.github.com>
2023-04-09 06:48:31 +0530

    Retry 18

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom     
 source build/envsetup.sh  
 lunch blaze_tissot-userdebug
-export ALLOW_MISSING_DEPENDENCIES=true
+export ALLOW_MISSING_DEPENDENCIES=tr ue
 export TZ=Asia/Dhaka #put before last build command    
 mka bacon 
   
