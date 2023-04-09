b39d4e813a3f0338703a6ef946255261eaafa056
Dooms <vmbuildforme@gmail.com>
2023-04-09 23:20:47 +0530

    initial bootleggers

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Bootleggers-BrokenLab/manifest.git -b tirimbino -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/BootleggersROM/manifest.git -b tirimbino -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Krtonia/local_manifest.git --depth 1 -b boot .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
