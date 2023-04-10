e78523bc9fca26a041c9bb87b80c2465af9888dc
Omansh Krishn <96343317+omansh-krishn@users.noreply.github.com>
2023-04-10 20:55:36 +0530

    last try

--- build_rom.sh
+++ build_rom.sh
@@ -3,9 +3,11 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest.git -b thirteen-plus -g default,-mips,-darwin,-notdefault
 git clone https://github.com/omansh-krishn/local_manifest --depth 1 -b pixelexperience-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+# build rom
 
+source build/envsetup.sh
 export TARGET_GAPPS_ARCH=arm64
-export TARGET_KERNEL_CLANG_VERSION=clang-r445002
+export TARGET_KERNEL_CLANG_VERSION=trb_clang
 export TARGET_KERNEL_CLANG_PATH=$(pwd)/prebuilts/clang/host/linux-x86/${TARGET_KERNEL_CLANG_VERSION}
 export TARGET_KERNEL_CROSS_COMPILE_PREFIX=${TARGET_KERNEL_CLANG_PATH}/bin/aarch64-linux-gnu-
 export TARGET_KERNEL_CROSS_COMPILE_ARM32_PREFIX=${TARGET_KERNEL_CLANG_PATH}/bin/arm-linux-gnueabi-
@@ -16,8 +18,7 @@ export BUILD_HOSTNAME=ubuntu
 export KBUILD_BUILD_USER=OmanshKrishn
 export KBUILD_BUILD_HOST=ubuntu
 export ALLOW_MISSING_DEPENDENCIES=true
-# build rom
-source build/envsetup.sh
+
 lunch aosp_santoni-userdebug
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
