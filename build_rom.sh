475e31f8c7bafe9a8054bfaa0e4aadb3ef2ab061
IdkAnythin07 <94067465+IdkAnythin07@users.noreply.github.com>
2023-04-10 13:43:44 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,36 +1,28 @@
-# sync rom
-repo init --depth=1 --no-repo-verify -u https://://github.com/SuperiorOS/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
+# sync romu
+repo init --depth=1 --no-repo-verify -u https://://github.com/SuperiorOS/manifest -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/IdkAnythin07/local_manifest.git --depth 1 -b superior .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rum
 source build/envsetup.sh
 lunch superior_RMX1941-userdebug
 
 export BUILD_USER=IdkAnythin07
-
 export BUILD_USERNAME=IdkAnythin07
-
 export KBUILD_BUILD_USER=IdkAnythin07
-
 export KBUILD_BUILD_USERNAME=IdkAnythin07
-
 export BUILD_HOST=cirrus-ci
-
 export BUILD_HOSTNAME=cirrus-ci
 
 export SELINUX_IGNORE_NEVERALLOWS=true
-
 export ALLOW_MISSING_DEPENDENCIES=true
-
 export RELAX_USES_LIBRARY_CHECK=true
 
 export SUPERIOR_GAPPS=true
 
 export TZ=Asia/Kolkata #put before last build command
-
 mka bacon
 
 
-# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+# upload rum (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
