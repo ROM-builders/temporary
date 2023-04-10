5963ee652517a728236e4bb3026a08ce8e9f0a68
stuepz <129644009+stuepz@users.noreply.github.com>
2023-04-10 20:53:00 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_viva-user
+export WITH_GAPPS=true 
+export DEVICE_MAINTAINERS=Rom\ Bdr.\ stuepz
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
