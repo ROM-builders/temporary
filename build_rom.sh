c8ba1e29d11d1a71d22661cc51f33bb93e81026c
Daniel Arya <86230878+Wrdn28@users.noreply.github.com>
2023-04-10 16:40:58 +0700

    X01AD-SuperiorOS-thirteen-Wrdn28

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/BlissRoms/platform_manifest.git -b typhoon -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Wrdn28/local_manifest.git --depth 1 -b Bliss-13 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Wrdn28/local_manifest.git --depth 1 -b superior-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch bliss_X01AD-userdebug
+lunch superior_X01AD-userdebug
 export TZ=Asia/Jakarta #put before last build command
-blissify -v X01AD
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
