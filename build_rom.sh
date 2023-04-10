702b2f58f4cfcef08b153be05380a1fdcb9b2525
Akshat Singh <91002031+Legendleo90@users.noreply.github.com>
2023-04-10 08:25:39 +0530

    Pex Bringup

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-AOSP/manifest -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Legendleo90/local_manifest.git -b pos .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExtended/manifest -b trece -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Legendleo90/local_manifest.git -b pex .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
