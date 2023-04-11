e648f09c60ede37b506266676fe3b02c1052816a
GhostMaster69-dev <rathore6375@gmail.com>
2023-04-11 09:05:49 +0530

    Test octavi

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch octavi_vince-user
+lunch octavi_vince-userdebug
 #export USE_GAPPS=true
 #export WITH_GAPPS=true
 mka bacon
