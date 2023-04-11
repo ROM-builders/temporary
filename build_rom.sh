1e8e63c486382e74f2e2542b5fe1039b059b6c33
Wahyu Oktariansyah <123318464+Whot1966@users.noreply.github.com>
2023-04-12 06:01:20 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch bootleg_X00TD-user
 export BUILD_USERNAME=wo1966
-export TZ=Asia/Jakarta #put before last build command
+export TZ=Asia/Jakarta
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
