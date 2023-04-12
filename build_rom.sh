23748b16f9f9eda531eb9145d2df18e2d5458e73
Angel Castillo <acastillorobles77@gmail.com>
2023-04-12 10:07:07 -0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch rr_Insignia_Delta3-userdebug
+lunch lineage_Insignia_Delta3-userdebug
 export TZ=America/Mexico_City #put before last build command
 mka bacon
 
