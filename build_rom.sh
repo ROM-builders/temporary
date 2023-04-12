0578e09353b28a26788d2ad3af6e8529a0f422f2
Jax Leo <sd369888@outlook.com>
2023-04-12 23:44:22 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,10 +7,9 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 export TZ=Asia/Chongqing #put before last build command
 export BUILD_USERNAME=weaponmasterjax
-export BUILD_HOSTNAME=jaxone
+export BUILD_HOSTNAME=jax
 lunch aosp_everpal-userdebug
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-
