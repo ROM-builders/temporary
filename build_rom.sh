be7d7b2aaae447d0a06123a9b140639dbb3d5145
snnbyyds <snnbyyds@gmail.com>
2023-04-13 12:20:06 +0800

    rs

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 croot
 lunch aosp_enchilada-userdebug
-
 export KBUILD_BUILD_USER=snnbyyds
 export KBUILD_BUILD_HOST=uotan
 export BUILD_USERNAME=snnbyyds
