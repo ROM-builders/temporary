3916ac6fe6ab9bc8f0af473162443894727810fc
mozzaru <100791931+mozzaru@users.noreply.github.com>
2023-04-13 23:31:24 +0700

    Patch  April

--- build_rom.sh
+++ build_rom.sh
@@ -3,9 +3,10 @@ repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifes
 git clone https://github.com/mozzaru/local_manifest.git --depth 1 -b derpfest-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom  
+# build rom   
 source build/envsetup.sh
 lunch derp_markw-userdebug
+export KBUILD_BUILD_USER=biasa
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
