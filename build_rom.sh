7a446e137491eaca13e2948d20ee2569dd3a854d
ahmedhridoy371 <40537458+ahmedhridoy371@users.noreply.github.com>
2023-04-13 22:08:53 +0600

    testing

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/ahmedhridoy371/local_manifest --depth 1 -b SuperiorOS-thirteen .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j1 --fail-fast
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 --fail-fast
 
 # build rom
 source build/envsetup.sh
