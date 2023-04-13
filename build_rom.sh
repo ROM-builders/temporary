72a5b93a9c3ffae1f7f5381e5382cab1187412a0
K R I S H N A <krishnakripa34567@gmail.com>
2023-04-13 11:51:07 +0530

    temporary: updated branch name and added export build user&hostname

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,13 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExtended/manifest.git -b trece -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pure-soul-kk/local_manifest.git --depth=1 -b pex .repo/local_manifests
+git clone https://github.com/pure-soul-kk/local_manifest.git --depth=1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch aosp_sweet-user
+export BUILD_USERNAME=K R I S H N A
+export BUILD_HOSTNAME=pure-soul-kk
 export TZ=Asia/Kolkata #put before last build command
 mka bacon 
 
