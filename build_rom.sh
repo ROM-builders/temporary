f5241e181bb5555a831ff4d1770199451d70f7d6
Ronald Campos <71366260+Ronald826@users.noreply.github.com>
2023-04-14 20:21:32 +0800

    Xiaomi Selene LineageOS Ronald826

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch selene-user
+lunch lineage_selene-user
 export TZ=Asia/manila #put before last build command
 mka lineage
 
