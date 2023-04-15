068629be7110ea45605082e39d9ba4de11550657
mozzaru <100791931+mozzaru@users.noreply.github.com>
2023-04-15 21:06:13 +0700

    Trigger build

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom       
 source build/envsetup.sh
 lunch derp_markw-userdebug
-export KBUILD_BUILD_USER=biasa
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
