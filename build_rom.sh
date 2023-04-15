6f5aebbb9155c15ec65a58f6cac3c8ab58908548
Zia Uddin Zisan <ziauddinzisan2001@gmail.com>
2023-04-16 01:40:48 +0600

    alioth: Init Bootleggers

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Xeesan/local_manifest --depth=1 -b vos .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/BootleggersROM/manifest.git -b tirimbino -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Xeesan/local_manifest --depth=1 -b btlg .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
+lunch bootleg_alioth-userdebug
 export TZ=Asia/Dhaka #put before last build command
-brunch voltage_alioth-userdebug
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
