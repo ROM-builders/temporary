e96ae169e229100f6547238753f5c80151cdbd7f
android <124842392+andriirosemary@users.noreply.github.com>
2023-04-16 13:07:28 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/CherishOS/android_manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://github.com/andriirosemary/Local-Manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/CodeChas3r/local_manifest.git --depth 1 -b CherishOS-tiramisu-CodeChas3r1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
