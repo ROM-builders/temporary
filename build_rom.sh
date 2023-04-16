0068de1cf05f09236da14662318c03896d0dc6fa
android <124842392+andriirosemary@users.noreply.github.com>
2023-04-16 10:21:24 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest.git -b 12.1-g default,-mips,-darwin,-notdefault
-git clone https://github.com/andriirosemary/local_manifest.git -b voltage .repo/local_manifest
+repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b twelve-one -g default,-mips,-darwin,-notdefault
+git clone https://github.com/andriirosemary/Local_Manifest.git --depth 1 -b cherish .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch voltage_rosemary-userdebug
+lunch cherish_rosemary-userdebug
+export CHERISH_VANILLA=false
 export TZ=Asia/Dhaka #put before last build command
-mka derp
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
