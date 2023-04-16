af1790f1154e8080b24f7444e834f377d828b37f
Đł Ɽ₳łⱫɆⱠ ㋡ <abhinav.115260@gmail.com>
2023-04-16 15:13:21 +0530

    POS: Trigger build

--- build_rom.sh
+++ build_rom.sh
@@ -1,20 +1,13 @@
-
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExtended/manifest.git -b trece -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-AOSP/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/acex69/local_manifest.git --depth 1 -b aosp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
 # build rom
 source build/envsetup.sh
 lunch aosp_lancelot-userdebug
 export TZ=Asia/Delhi #put before last build command
 export BUILD_USERNAME=@raizel_69
 export BUILD_HOSTNAME=@raizel69X
-
-
-mka bacon
-
-   
-
+make bacon
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
