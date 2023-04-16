cd9d202db3e03feae48cc6636e1ba83de412a536
android <124842392+andriirosemary@users.noreply.github.com>
2023-04-16 04:30:42 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,8 +5,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch voltage_rosemary-user
-export TZ=Asia/Dhaka #put before last build command
+lunch voltage_rosemary-userdebug
+export TZ=Dhaka #put before last build command
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
