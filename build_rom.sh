6dfbc83348c941e726b3a3e5beb25461990997ba
android <124842392+andriirosemary@users.noreply.github.com>
2023-04-16 13:44:04 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,17 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/CherishOS/android_manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://github.com/CodeChas3r/local_manifest.git --depth 1 -b CherishOS-tiramisu-CodeChas3r1 .repo/local_manifests
+git clone https://github.com/andriirosemary/Local_Manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch cherish_rosemary-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+export BUILD_USERNAME=andriirosemary 
+export BUILD_HOSTNAME=andriirosemary
 export TZ=Asia/Dhaka #put before last build command
-mka cherish
+mka bakon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
