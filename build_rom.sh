6d8f29f01d4d827cec3e9ec59fac12cb91c7a7dd
Salih <91677076+DeliUstaTR@users.noreply.github.com>
2023-04-16 08:41:19 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/AlrightAndroid-Brokenlab/platform_manifest -b Q -g default,-mips,-darwin,-notdefault
-git clone https://github.com/msalihpelit/Local_Manifest.git --depth 1 -b default .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/Spark-Rom/manifest -b pyro -g default,-mips,-darwin,-notdefault
+git clone https://github.com/msalihpelit/Local_Manifest.git --depth 1 -b pyro .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch alright_ginkgo-userdebug
+lunch spark_ginkgo-userdebug
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
