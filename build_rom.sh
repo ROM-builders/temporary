3dd43c1d69bf59eed024b8a946dc62bfa7294ec1
android <124842392+andriirosemary@users.noreply.github.com>
2023-04-17 19:14:41 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/RisingTechOSS/android_manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/andriirosemary/Local_Manifest.git --depth 1 -b rosemary-coltos-tiramisu .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/syberia-project/manifest.git -b 13.0 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/andriirosemary/Local_Manifest.git --depth 1 -b syberia .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 export TZ=Asia/Dhaka #put before last build command
-lunch colt_rosemary-userdebug
-make colt 
+lunch syberia_rosemary-userdebug
+make syberia
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
