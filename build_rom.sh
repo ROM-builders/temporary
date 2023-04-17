91bd8ccd322909c21847b7836b7e79d822fa4530
unsatifsed27 <unsatifsederror@gmail.com>
2023-04-17 12:53:05 +0700

    sweet-bananadroid-13-UnsatifsedError

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,18 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/UnsatifsedError/local_manifests.git --depth 1 -b banana .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
-export TZ=Asia/Dhaka #put before last build command
-mka derp
+lunch banana_sweet-userdebug
+export TZ=Asia/Jakarta #put before last build command
+export TARGET_SUPPORTS_64_BIT_APPS=true
+export KBUILD_BUILD_USER=unsatifsed
+export KBUILD_BUILD_HOST=gedang
+export BUILD_USERNAME=unsatifsed
+export BUILD_HOSTNAME=gedang
+m banana
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
