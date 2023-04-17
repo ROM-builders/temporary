2ae7aa63c39f213717095a3e59d2aef658fe3e1a
Naim <88440061+FollowNaim@users.noreply.github.com>
2023-04-18 03:37:21 +0600

    Spes | ArrowOS-13.1 | FollowNaim
    build arrowos for spes

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom
+# sync rom 
 repo init --depth=1 --no-repo-verify -u git://github.com/ArroeOS/android_mainfest.git -b arrow-13.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/FollowNaim/Local_Manifest.git --depth 1 -b main .repo/local_manifest
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
