ffb4f179529d74ad66b11cb81c9dd15834e0ac5c
android <124842392+andriirosemary@users.noreply.github.com>
2023-04-17 10:44:55 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/RisingTechOSS/android_manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/andriirosemary/Local_Manifest.git --depth 1 -b cherish1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
