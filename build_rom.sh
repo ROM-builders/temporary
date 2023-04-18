7a0dc23a563daa657d05be4636310e813bbac16e
xenxynon <flynryder427@gmail.com>
2023-04-18 09:08:27 +0530

    New beeld

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest.git -b topaz -g default,-mips,-darwin,-notdefault
-git clone https://github.com/XenStuff/manifest.git --depth 1 -b aospa .repo/local_manifests
+git clone https://github.com/AOSPA-Lavender/manifest.git --depth 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -11,4 +11,4 @@ export TZ=Asia/Dhaka #put before last build command
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-rclone copy *aospa**2023*zip cirrus:lavender -P
+rclone copy *aospa*2023*image*zip cirrus:phone1 -P
