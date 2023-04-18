b70c1b32df83bae1565b518942733a40d12a8d3c
ayeneo <129434536+ayeneo@users.noreply.github.com>
2023-04-18 05:43:13 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ricedroidOSS/android -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ayeneo/mainfest_personal --depth 1 -b rice .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/CherishOS/android_manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
+git clone  https://github.com/ayeneo/mainfest_personal --depth 1 -b dreaml .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_dreamlte-userdebugexport
-export ALLOW_MISSING_DEPENDENCIES=true
-export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
-export BUILD_BROKEN_DUP_RULES=true
 export TZ=Asia/Dhaka #put before last build command
-mka bacon
+brunch dreamlte
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
