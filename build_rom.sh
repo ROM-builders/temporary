cc9d761f46b1cc81dbbe1acb89714115301c415e
darshan <43618707+mevadadarshan19@users.noreply.github.com>
2023-04-18 21:33:03 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -10,7 +10,7 @@ lunch xtended_X01BD-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Dhaka #put before last build command
 make xtended
-
+ 
  
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
