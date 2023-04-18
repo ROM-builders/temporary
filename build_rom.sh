95efe7f045b6f4e91108fd47ebdc12e749ae9bcd
askairt <askairt@gmail.com>
2023-04-18 11:04:07 +0700

    selene-LineageOS-lineage-20.0-askairt

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
-repo init -u https://github.com/LineageOS/android.git -b lineage-20.0 depth=1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/askairt/local_manifest.git .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j6
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs -g default,-mips,-darwin,-notdefault
+git clone https://github.com/askairt/local_manifest.git --depth 1 .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 #build rom
 source build/envsetup.sh
