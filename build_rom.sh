a5532d3d031c4bf28f1da32e17926f24d2ccf06c
unsatifsed27 <unsatifsederror@gmail.com>
2023-04-18 19:50:48 +0700

    manifest: fix repo sync

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 13 -g default,-mips,-darwin,-notdefault --git-lfs
 git clone https://github.com/UnsatifsedError/local_manifests.git --depth 1 -b banana .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
