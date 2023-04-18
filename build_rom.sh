1448271cd4272e24f7025d075fc645f7d324b3c7
stuepz <129644009+stuepz@users.noreply.github.com>
2023-04-18 22:54:16 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init -u https://github.com/CipherOS/android_manifest.git -b thirteen --depth=1 -g default,-mips,-darwin,-notdefault
+repo init -u https://github.com/CipherOS/android_manifest.git -b twelve --depth=1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/stuepz/local_manifest.git --depth 1 -b 13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch cipher_viva-userdebug
 export TZ=Asia/Dhaka #put before last build command
-mka bacon
+mka bacon -j8
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
