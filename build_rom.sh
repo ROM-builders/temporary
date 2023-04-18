501d8ca686f329aaf9cade663c3ec0aa037432ae
Han <66839648+Neko059@users.noreply.github.com>
2023-04-18 21:44:23 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/Neko059/local_manifest.git --depth 1 -b master .rep
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+source build/envsetup.sh 
 lunch aosp_fog-userdebug
 export TZ=Asia/Jakarta #put before last build command
 export ALLOW_MISSING_DEPENDENCIES=true
