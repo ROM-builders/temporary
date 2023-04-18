6b1f35c7f9df43b25294f88007b3f5e46d222444
ayeneo <129434536+ayeneo@users.noreply.github.com>
2023-04-18 18:21:21 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/CherishOS/android_manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone  https://github.com/ayeneo/mainfest_personal --depth 1 -b main .repo/local_manifests
+git clone https://github.com/ayeneo/local_manifests --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
