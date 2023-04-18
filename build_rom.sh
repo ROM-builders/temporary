aa98c3da6b2465802277cf9787abe5d348823784
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2023-04-18 12:48:24 +0000

    X01BD-Evo-ManitnjG
    try

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,17 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ManitnjG/local_manifest.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/ManitnjG/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch evolution_X01BD-userdebug
+
+export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Delhi #put before last build command
 mka evolution
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+
+
