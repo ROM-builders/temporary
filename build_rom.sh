310f014e15056d173081c1eedad83e03d3d55d27
GhostMaster69-dev <rathore6375@gmail.com>
2023-04-18 21:46:32 +0530

    octavi April patch
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,23 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Octavi-Staging/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/GhostMaster69-dev/local_manifest.git --depth 1 -b thirteen .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
+if ! [ -d device/xiaomi/vince ]; then
+    git clone --depth=1 -b 13 https://github.com/OctaviOS-Devices/device_xiaomi_vince.git device/xiaomi/vince
+else
+    rm -rf device/xiaomi/vince
+    git clone --depth=1 -b 13 https://github.com/OctaviOS-Devices/device_xiaomi_vince.git device/xiaomi/vince
+fi
+
+if ! [ -d prebuilts/clang/host/linux-x86/clang-r450784d ]; then
+    git clone --depth=1 -b master https://gitlab.com/Amritorock/clang-r450784d.git prebuilts/clang/host/linux-x86/clang-r450784d
+fi
 
 # build rom
 source build/envsetup.sh
-lunch octavi_vince-user
 #export USE_GAPPS=true
 #export WITH_GAPPS=true
+lunch octavi_vince-user
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
