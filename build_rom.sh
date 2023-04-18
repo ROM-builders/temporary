90946c91cacbe89893552f4dc0b7c3d8c3111833
Alec Chan <88945170+alecchangod@users.noreply.github.com>
2023-04-18 16:58:59 +0800

    Build with miui cam

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Project-Xtended/manifest.git -b xt -g default,-mips,-darwin,-notdefault
-git clone https://github.com/alecchangod/local_manifest.git --depth 1 -b xtended .repo/local_manifests
+git clone https://github.com/alecchangod/local_manifest.git --depth 1 -b anx-xtended .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
  
 # build rom
@@ -9,7 +9,7 @@ lunch xtended_monet-user
 # export WITH_GAPPS=true
 export TZ=Asia/Hong_Kong #put before last build command
 make xtended
-# 32
+# 33
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
 
