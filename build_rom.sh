ad9f819648de03fa53c272f450de0d31e2e92570
Joxquin <68979987+Joxquin@users.noreply.github.com>
2023-04-19 18:44:02 -0500

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixysOS/manifest -b thirteen -g default,-mips,-darwin,-notdefault
+repo init -u https://github.com/elytraOS/manifest.git -b tesseract
 git clone https://github.com/Joxquin/local_manifest.git --depth 1 -b main .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync --no-clone-bundle --current-branch --no-tags --force-sync -j$(nproc --all)
 
 # build rom
 . build/envsetup.sh
-lunch pixys_fog-userdebug
+lunch elytra_fog-userdebug
 export TZ=Asia/Dhaka #put before last build command
-make pixys
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
