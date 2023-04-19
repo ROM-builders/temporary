851e158f47c852ac9498a67ebb8f7460dd9de243
android <124842392+andriirosemary@users.noreply.github.com>
2023-04-19 11:44:14 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,6 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
+export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Dhaka #put before last build command
 lunch colt_rosemary-userdebug
 make colt 
