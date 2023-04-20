673fb2249eb694271fb7774a6aff0116d476cdfa
Han <66839648+Neko059@users.noreply.github.com>
2023-04-20 20:51:33 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom pixel
 source build/envsetup.sh
 lunch aosp_fog-userdebug
-export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Jakarta #put before last build command
 mka bacon
 
