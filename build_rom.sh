5f71d51b1089e2bf4ff6ccf01dbbe42e46c33638
IdkAnythin07 <94067465+IdkAnythin07@users.noreply.github.com>
2023-04-20 20:52:22 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
-# sync rom
+# sync rum
 repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest -b 13 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Idkanythin07/local_manifest --depth 1 -b derp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rum
 
 source build/envsetup.sh
 
@@ -33,5 +33,5 @@ export TZ=Asia/Kolkata #put before last build command
 
 mka derp
 
-# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+# upload rum (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
