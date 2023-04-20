37889ab84814bcfe9c187f4529c91288ab5b6917
Python Py <89606297+pythonpy1997@users.noreply.github.com>
2023-04-20 20:32:09 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pythonpy1997/local_manifests.git --depth 1 -b thirteen-vol .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/StagOS/manifest.git -b t13 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/pythonpy1997/local_manifests.git --depth 1 -b t13-stagos .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch voltage_RM6785-user
+lunch stag_RM6785-userdebug
+export WITH_GAPPS=true
 export TZ=Asia/Kolkata #put before last build command
-m bacon
+make stag
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
