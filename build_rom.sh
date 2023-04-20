cdaa4418d38aa3c136a89eab275ad967fdf212fa
Lalit <123730669+archero32@users.noreply.github.com>
2023-04-20 06:35:31 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init -u https://github.com/bananadroid/android_manifest.git -b 13 --git-lfs
+repo init --depth=1 --no-repo-verify -u git://github.com/bananadroid/android_manifest.git -b 13 --git-lfs -g default,-mips,-darwin,-notdefault
 git clone https://github.com/archero32/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
