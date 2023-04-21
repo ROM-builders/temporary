dcb5d18f3c80e23f863b10cc7efe44eadc77ca80
ahmedhridoy371 <40537458+ahmedhridoy371@users.noreply.github.com>
2023-04-21 11:57:33 +0600

    trigger build

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch octavi_lavender-userdebug
+export USE_GAPPS=true
+export WITH_GAPPS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
 export BUILD_BROKEN_DUP_RULES=true
@@ -14,3 +16,4 @@ brunch lavender
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+g
