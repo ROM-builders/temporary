7c90b19929c76b07499d43642a621ba1a4dae59d
Alec Chan <88945170+alecchangod@users.noreply.github.com>
2023-04-22 04:34:28 +0800

    initial vangogh

--- build_rom.sh
+++ build_rom.sh
@@ -1,17 +1,27 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Project-Xtended/manifest.git -b xt -g default,-mips,-darwin,-notdefault
-git clone https://github.com/alecchangod/local_manifest.git --depth 1 -b anx-xtended .repo/local_manifests
+git clone https://github.com/alecchangod/local_manifest.git --depth 1 -b van-anx-xtended .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
  
 # build rom
 source build/envsetup.sh
-lunch xtended_monet-user
+lunch xtended_vangogh-eng
 # export WITH_GAPPS=true
+#export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Hong_Kong #put before last build command
 make xtended
-# 33
+# 29
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+
+
+
+
+
+
+
+
+
 
 
 
