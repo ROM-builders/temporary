db5fdd8c2853775bd00ec88cc95d75a20f4063b3
TheWinner02 <124472504+TheWinner02@users.noreply.github.com>
2023-04-22 21:15:33 +0200

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init -u https://github.com/Octavi-Staging/manifest.git -b thirteen --depth=1 and -g default,-mips,-darwin,-notdefault
-git clone https://github.com/TheWinner02/local_manifest.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Octavi-Staging/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
+git clone https://github.com/TheWinner02/local_manifest.git --depth 1 -b main.repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch octavi_a40-userdebug
-export TZ=Asia/Dhaka #put before last build command
+export TZ=Asia/Jakarta #put before last build command
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
