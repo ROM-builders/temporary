3194725586a3e8a6ac4eb2c1f09075326f7c07e8
Matheus Souza <33396386+matheucomth@users.noreply.github.com>
2023-04-22 00:20:40 -0300

    Rebase tree

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch awaken_tulip-userdebug
+lunch awaken_twolip-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export RELAX_USES_LIBRARY_CHECK=true
