ea1e0dbfd2c5b0aa2b3abbd4a7ae4c4fbbaf036d
SKetU <sketu@donot.care>
2023-04-22 16:01:09 +0530

    1st try of 4.19 bootlogers

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,12 @@
-# sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest.git -b triton -g default,-mips,-darwin,-notdefault
-git clone https://github.com/SKetU-l/local_manifest --depth 1 -b awaken-r2p .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/BootleggersROM/manifest.git -b tirimbino -g default,-mips,-darwin,-notdefault
+git clone https://github.com/SKetU-l/local_manifest --depth=1 -b btlg .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-  
 # build rom
-. build/envsetup.sh
-lunch awaken_RMX1801-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
-export TZ=Asia/Kolkata #put before last build command
-make bacon
+source build/envsetup.sh
+lunch bootleg_X00TD-userdebug
+export TZ=Asia/Dhaka #put before last build command
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
