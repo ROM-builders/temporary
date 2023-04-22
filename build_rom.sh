211c4b77345e8a7aae7f494a91cd0d3f5b56d3a8
TheWinner02 <124472504+TheWinner02@users.noreply.github.com>
2023-04-22 21:42:45 +0200

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-oss/xd_manifes
 git clone https://github.com/TheWinner02/local_manifest.git --depth 1 -b main.repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom.
+# build rom
 source build/envsetup.sh
 lunch xdroid_a40-userdebug
 export TZ=Asia/Jakarta #put before last build command
