ed987cce13712483e4eee9c02a6362799f2a35af
rianza <116883839+rianza@users.noreply.github.com>
2023-04-23 09:25:21 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest 
 git clone https://github.com/rianza/Local_Manifest.git --depth 1 -b evolution-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom                        
+# build rom                         
 source build/envsetup.sh
 lunch evolution_markw-userdebug
 export TZ=Asia/Jakarta #put before last build command
