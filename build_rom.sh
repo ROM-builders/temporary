1f9ec99e476b5c11ff163b1a0426e1cd9fa3fb54
Haikal Luthfianino Balukia <51993230+hklknz@users.noreply.github.com>
2023-04-23 08:12:17 +0700

    Trigger Build

--- build_rom.sh
+++ build_rom.sh
@@ -13,7 +13,6 @@ export KBUILD_BUILD_HOST=Roselia-CI
 export BUILD_USERNAME=hklknz
 export BUILD_HOSTNAME=Roselia-CI
 cat o*/t*/p*/tissot/*.zip.json
-rm -rf hardware/lineage/compat
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
