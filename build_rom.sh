bb89ce9041a451e6e198802aa00858180e9dec83
Han <66839648+Neko059@users.noreply.github.com>
2023-04-24 01:25:05 +0700

    fix typo ( local_manifest not local-manifest )

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b thirteen-plus -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Neko059/local-manifest.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/Neko059/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom pixel part 5
+# build rom pixel part 6
 source build/envsetup.sh
 lunch aosp_fog-userdebug
 export TZ=Asia/Jakarta #put before last build command
