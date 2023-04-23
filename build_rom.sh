9aba48ad7d7bda5d57acbc3b6699cc923f524f61
Jax Leo <sd369888@outlook.com>
2023-04-23 22:51:32 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExtended/manifest -b trece --depth 1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/weaponmasterjax/local_manifest.git --depth 1 -b pex .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu --depth 1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/weaponmasterjax/local_manifest.git --depth 1 -b Evox .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -8,8 +8,8 @@ source build/envsetup.sh
 export TZ=Asia/Chongqing #put before last build command
 export BUILD_USERNAME=weaponmasterjax
 export BUILD_HOSTNAME=jax
-lunch aosp_everpal-userdebug
-mka bacon
+lunch evolution_everpal-userdebug
+mka evolution
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
