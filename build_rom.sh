e6d546631529d536d6601029c84cfdc44020ce35
essaNAJJAR <51336462+essaNAJJAR@users.noreply.github.com>
2023-04-23 16:17:55 +0300

    sweet-evolutionX-tiramisu-essanj

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom
+## sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/Evolution-X/manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
 git clone https://github.com/essaNAJJAR/Local_Manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
