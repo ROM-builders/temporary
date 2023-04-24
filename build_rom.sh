2fad6d3e2a4b2503842ce95b5c16854261141c63
Shwetank Rai <60915576+ishugrapher@users.noreply.github.com>
2023-04-24 12:14:51 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init -u https://github.com/crdroidandroid/android.git -b 13.0 --git-lfs
-git clone https://github.com/ishugrapher/local_manifest.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/ishugrapher/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync
 
 # build rom
