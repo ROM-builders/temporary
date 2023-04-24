db12faf2a1a732dc50c1d5fba29eb9e952893c55
Han <66839648+Neko059@users.noreply.github.com>
2023-04-24 19:35:56 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom pixel part 6
 source build/envsetup.sh
 lunch aosp_fog-userdebug
+export BUILD_USERNAME=Zxyn
+export KBUILD_BUILD_USER=Zxyn
 export TZ=Asia/Jakarta #put before last build command
 mka bacon
 
