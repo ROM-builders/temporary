17e900194b7bf598d89b66fd4d46ab6c9141ff3e
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2023-04-24 20:39:45 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,18 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/BlissRoms/platform_manifest.git -b typhoon -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Mevadadarshan1997/local_manifest.git --depth 1 -b bliss .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/bananadroid/android_manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/ManitnjG/local_manifest.git --depth 1 -b banana .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
-lunch bliss_X01BD-userdebug
 
+. build/envsetup.sh
+lunch banana_<devicecodename>-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Dhaka #put before last build command
-blissify X01BD
+m banana
+
+
+
  
 
   
