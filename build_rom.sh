0b2b45fcaa3fefd7455d3cb859df1f208d10d470
igniteinm <111630059+igniteinm@users.noreply.github.com>
2023-04-24 12:50:10 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/trying122/local_manifest.git --depth 1 -b vos .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/igniteinm/local_manifest.git --depth 1 -b fu .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
- 
-# build rom
+
+# build rom #4
 source build/envsetup.sh
-lunch voltage_beryllium-userdebug
-export TZ=Asia/Kolkata #put before last build command
-make bacon
+lunch lineage_beryllium-user
+export TZ=Asia/Delhi #put before last build command
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
