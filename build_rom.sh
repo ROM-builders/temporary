d4cc0eaf7af80861299ef6c611192ef28a0a56e1
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2023-04-25 01:48:48 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Octavi-Staging/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ManitnjG/local_manifest.git --depth 1 -b octavi .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/RisingTechOSS/android.git -b thirteen-g default,-mips,-darwin,-notdefault
+git clone https://github.com/ManitnjG/local_manifest.git --depth 1 -b rising .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch octavi_X01BD-userdebug
+lunch rising_X01BD-userdebug
 
 export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Dhaka #put before last build command
