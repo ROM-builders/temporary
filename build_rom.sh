ec3cd7b484064d086db9f658422cb8ef55c9d3db
TheWinner02 <124472504+TheWinner02@users.noreply.github.com>
2023-04-24 10:55:14 +0200

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-oss/xd_manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Octavi-Staging/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/TheWinner02/local_manifest.git --depth 1 -b main.repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch xdroid_a40-userdebug
+lunch octavi_a40-userdebug
 export TZ=Asia/Jakarta #put before last build command
 make bacon
 
