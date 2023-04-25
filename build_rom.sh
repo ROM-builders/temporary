a1ddb0906832fe83de75a82ed4873b22ed9a3751
Abhishek Hembrom <76091481+abhishekfire08@users.noreply.github.com>
2023-04-25 22:06:34 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,10 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom 
 source build/envsetup.sh
 lunch stag_ginkgo-userdebug
-export BUILD_USERNAME=xyz_abhishek
-export BUILD_HOSTNAME=xyz_abhishek
-#export ALLOW_MISSING_DEPENDENCIES=true
-#export TARGET_KERNEL_CLANG_VERSION=proton
 export TZ=Asia/Kolkata #put before last build command
 make stag
 
