d9388d06bf68759d15eff968bda1dc51890d4940
android <124842392+andriirosemary@users.noreply.github.com>
2023-04-25 17:05:20 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,10 +5,11 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-export ALLOW_MISSING_DEPENDENCIES=true
+lunch arrow_rosemary-userdebug
+export BUILD_USERNAME=andrii
+export BUILD_HOSTNAME=andrii
 export TZ=Asia/Dhaka #put before last build command
-lunch colt_rosemary-userdebug
-make colt 
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
