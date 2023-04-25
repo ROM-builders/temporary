8960aa7bbc7509f34167d16b000c6a4b8bc5eb82
rianza <116883839+rianza@users.noreply.github.com>
2023-04-26 05:44:27 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom                          
 source build/envsetup.sh
 lunch evolution_markw-userdebug
+export WITH_GAPPS := false
+export USE_GAPPS := false
 export TZ=Asia/Jakarta #put before last build command
 mka evolution
 
