e3500d8c50f7e076819fa7446fefb7e5258ce80d
eartinity24 <116898350+eartinity24@users.noreply.github.com>
2023-04-26 18:16:13 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/eartinity24/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest.git -b triton -g default,-mips,-darwin,-notdefault
+git clone https://github.com/pritiranjan24/local_manifest.git --depth 1 -b Triton .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage-bearmonets-userdebug
-export TZ=Asia/Bhubaneshwar #put before last build command
+lunch awaken_r5x-userdebug
+export TZ=Asia/Kolkata #put before last build command
 make bacon 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
