ffbc7a8ec398d7c72a11185e1040d6bc55809138
Nukxx <122034421+Nukxx@users.noreply.github.com>
2023-04-27 14:17:50 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom 
+# sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/yaap/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/nukxx/local_manifest.git --depth 1 -b Yaap .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
