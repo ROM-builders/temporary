40e29f93981378c2d47bdcca8c93538e5f5e1daa
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2023-04-28 00:47:15 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/BlissRoms/platform_manifest.git -b typhoon -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Mevadadarshan1997/local_manifest.git --depth 1 -b bliss .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-13.1  -b -g default,-mips,-darwin,-notdefault
+git clone https://github.com/ManitnjG/local_manifest.git --depth 1 -b bliss .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch bliss_X01BD-userdebug
+lunch arrow_X01BD-userdebug
 
 export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Dhaka #put before last build command
-blissify X01BD
- 
+m bacon
 
   
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
