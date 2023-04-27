4ad41ac316535b066af4c37aeea25ef4f92613c3
Tejas Singh <tejas@m.ms.evolution-x.org>
2023-04-27 11:54:47 +0530

    Trigger Build

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,14 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://github.com/baconpeedit/Local_Manifest.git --depth 1 -b evolution .repo/local_manifests
+git clone https://github.com/Charsi-Tejas/Local_Manifest.git --depth 1 -b evolution .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
 lunch evolution_ginkgo-userdebug
-export BUILD_USERNAME=Tejas
-export BUILD_HOSTNAME=I_Am_Charsi
 export TZ=Asia/Kolkata
 mka evolution
-cat o*/t*/p*/ginkgo/*.zip.json
+cat out/target/product/ginkgo/*.zip.json
 
 # upload rom
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
