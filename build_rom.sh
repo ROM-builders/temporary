fc17a7f1bf3ded9a0b5bd8b2b056983f3492b257
Abhishek Hembrom <76091481+abhishekfire08@users.noreply.github.com>
2023-04-28 08:24:23 +0530

    Customizzed sync not working
    using default values

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b pyro -g default,-mips,-darwin,-notdefault
 git clone https://github.com/abhishekfire08/manifest_local.git --depth 1 -b SparkOS .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j24
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom 
 source build/envsetup.sh
