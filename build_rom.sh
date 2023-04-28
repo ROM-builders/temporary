ac5f529ae06a24fd43d66d418549bc23e5ac55bb
android <124842392+andriirosemary@users.noreply.github.com>
2023-04-28 20:03:03 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/RisingTechOSS/android_manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/andriirosemary/Local_Manifest.git --depth 1 -b rosemary-coltos-tiramisu .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Fusion-OS/android_manifest -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/andriirosemary/Local_Manifest.git --depth 1 -b fusion .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch arrow_rosemary-userdebug
+lunch fuse_rosemary-userdebug
 export BUILD_USERNAME=andrii
 export BUILD_HOSTNAME=andrii
 export TZ=Asia/Dhaka #put before last build command
-mka arrow
+make fuse-prod -j
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
