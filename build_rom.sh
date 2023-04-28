1d9125cd4ecd3147666537938698911ba8c97b2c
Python Py <89606297+pythonpy1997@users.noreply.github.com>
2023-04-28 16:01:00 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch yaap_RM6785-userdebug
+lunch yaap_RM6785-user
+export PREBUILT_KERNEL=true
 export TZ=Asia/Kolkata #put before last build command
 m yaap
 
