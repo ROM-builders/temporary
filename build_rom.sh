d1a6fe5381e54ba4657a7163eac494defb6a10ce
Alec Chan <88945170+alecchangod@users.noreply.github.com>
2023-04-30 15:01:13 +0800

    Retry

--- build_rom.sh
+++ build_rom.sh
@@ -7,6 +7,10 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch xtended_haydn-userdebug
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+export BUILD_USER=alecchangod
+export BUILD_USERNAME=alecchangod
+export KBUILD_BUILD_USER=alecchangod
+export KBUILD_BUILD_USERNAME=alecchangod
 # export WITH_GAPPS=true
 export TZ=Asia/Hong_Kong #put before last build command
 make xtended
