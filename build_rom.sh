686f31169b411be034d40d665a4a286c4a9882c4
Rohit Tiwari <130814475+RT1648@users.noreply.github.com>
2023-05-01 17:18:25 +0530

    Fleur-Derpfest-13-RT1648

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch derp_fleur-userdebug
+lunch derp_fleur-user
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
