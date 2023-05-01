a93aeccf01af653d7d6a90d892f024f30a7b4a0b
Isaiah Rafael <lqlp0011@gmail.com>
2023-05-02 05:42:13 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u github.com/tequilaOS/platform_manifest.git -b tortilla -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u github.com/aospa/manifest.git -b topaz -g default,-mips,-darwin,-notdefault
 git clone https://github.com/kreiorogit/local_manifest.git --depth 1 -b Aosp .repo/local_manifest
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch tequilaOS_sweet-userdebug
+lunch aospa_sweet-userdebug
 export TZ=Asia/Jakarta #put before last build command
 make bacon -j$(nproc --all)
 
