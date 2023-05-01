466a7c8874ac275172a343df4d1d5150ae0708ea
YogRaJSinGh <84258191+yograjfire18@users.noreply.github.com>
2023-05-01 17:06:36 +0530

    build bootleg for spartan

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/yograjfire18/local_manifest.git --depth 1 -b derp-spartan .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/BootleggersROM/manifest.git -b tirimbino -g default,-mips,-darwin,-notdefault
+git clone https://github.com/yograjfire18/local_manifest.git --depth 1 -b bootleg-spartan .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_spartan-userdebug
+lunch bootleg_spartan-userdebug
 export TZ=Asia/Kolkata 
-mka derp
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
 rclone copy out/target/product/spartan/recovery.img cirrus:spartan -P
-
