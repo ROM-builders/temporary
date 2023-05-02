8ad64a4d385832b6f98906760ce215ec7c563a86
dudu2504 <91299298+dudu2504@users.noreply.github.com>
2023-05-02 16:45:43 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -8,7 +8,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 almoço derp_vayu-user
 export TZ=Asia/Dhaka # coloque antes do último comando de compilação
-mka derp 
+mka derp  
 # upload rom (se você não precisa fazer upload de vários arquivos, não precisa editar a próxima linha)
 rclone copy out/target/product/ $( grep unch $CIRRUS_WORKING_DIR /build_rom.sh -m 1 | cut -d '  ' -f 2 | cut -d _ -f 2 | cut -d - -f 1 ) / * . zip cirrus: $( grep unch $CIRRUS_WORKING_DIR /build_rom.sh -m 1 | corte -d '  ' -f 2 | corte -d _ -f 2 | corte -d - -f 1 ) -P
 
