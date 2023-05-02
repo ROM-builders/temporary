e53e3bdd4f9868b8dea84ef937e71d4b794dae5c
Hakan <hknhzl@gmail.com>
2023-05-02 08:28:36 +0300

    Trigger build

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest.
 git clone https://github.com/lafactorial/local_manifest.git --depth 1 -b EVO .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom 
 source build/envsetup.sh
 lunch evolution_tissot-userdebug
 export TZ=Asia/Kolkata #put before last build command
