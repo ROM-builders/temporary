1ffe9f1c8fa150548a644a95d6a01c51122b64b6
Arijit Saha <107979673+Arijit78@users.noreply.github.com>
2023-05-02 13:31:28 +0600

    whyred-CherishOS-tiramisu-Arijit78

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
 # build rom
 source build/envsetup.sh
-lunch cherish_whyred-user
+lunch cherish_whyred-userdebug
 export TZ=Asia/Dhaka #put before last build command
 brunch whyred
 
