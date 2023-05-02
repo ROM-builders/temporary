03a99c3cd1f0db3e1964b51d317b178b7be6b31f
dudu2504 <91299298+dudu2504@users.noreply.github.com>
2023-05-02 16:40:45 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,15 @@
-# sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+
+# ROM de sincronização
+repo init -u https://github.com/omnirom/android.git -b android-13.0 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/dudu2504/Vayu/blob/d2265ae6a9cd01210a9d94f967506d00eaaafe4b/local_manifest.xml --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+#rom de compilação
 source build/envsetup.sh
-lunch derp_vayu-user
-export TZ=Asia/Dhaka #put before last build command
+almoço derp_vayu-user
+export TZ=Asia/Dhaka # coloque antes do último comando de compilação
 mka derp
 
-# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
-rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+# upload rom (se você não precisa fazer upload de vários arquivos, não precisa editar a próxima linha)
+rclone copy out/target/product/ $( grep unch $CIRRUS_WORKING_DIR /build_rom.sh -m 1 | cut -d '  ' -f 2 | cut -d _ -f 2 | cut -d - -f 1 ) / * . zip cirrus: $( grep unch $CIRRUS_WORKING_DIR /build_rom.sh -m 1 | corte -d '  ' -f 2 | corte -d _ -f 2 | corte -d - -f 1 ) -P
+
