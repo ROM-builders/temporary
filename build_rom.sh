cb8164b23b58158c5f4914a19be265dc4491cefb
Ctps123 <79877353+Ctps123@users.noreply.github.com>
2023-05-02 22:05:12 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/syberia-project/manifest.git -b 13.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Ctps123/Local-manifests.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/Ctps123/Local-manifest.git --depth 1 -b Master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
