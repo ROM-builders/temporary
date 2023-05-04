a7088cbcc7e9abaacf08ed6ed317113968649df1
Nukxx <122034421+Nukxx@users.noreply.github.com>
2023-05-03 21:45:41 -0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/yaap/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/nukxx/local_manifest.git --depth 1 -b Yaap .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
+git clone https://github.com/nukxx/local_manifest.git --depth 1 -b CherishOS-tiramisu .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch yaap_rosemary-userdebug
+lunch cherish_rosemary-userdebug 
 export TZ=Asia/Dhaka #put before last build command
-m yaap
+mka bacon
 
 # Build count [2] 
 
