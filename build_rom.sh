8f563c75100bfded17a3b5b86065b6abfa945fe6
IdkAnythin07 <94067465+IdkAnythin07@users.noreply.github.com>
2023-05-05 15:53:12 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
-# sync rom
+# sync rum
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/IdkAnythin07/local_manifest.git --depth 1 -b Pixel_exp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rum
 source build/envsetup.sh
 lunch aosp_RMX1941-userdebug
 
@@ -19,5 +19,5 @@ export SKIP_API_CHECKS=true
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
-# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+# upload rum (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
