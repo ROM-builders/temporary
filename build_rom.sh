07583d8e16a145e2360c30781556432bfe6da54a
Paras Dhiman <55368020+dhimanparas20@users.noreply.github.com>
2023-05-07 17:24:56 +0530

    spartan-banana-dhimanparas20
    Build BananaDroid for RMX3371

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo init -u https://github.com/bananadroid/android_manifest.git -b 13 --git-lfs
+git clone https://github.com/dhimanparas20/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
+lunch banana_spartan-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
