3cc9a81a1a435c4447f645d9784b9bafbcde56b9
Tejas Singh <tejas@m.ms.evolution-x.org>
2023-05-09 18:18:45 +0530

    fix sync

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/Testing-Stuffs/Local_Manifest.git --depth 1 -b boot
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+. build/envsetup.sh
 lunch bootleg_ginkgo-userdebug
 export TZ=Asia/Kolkata
 mka bootleg
