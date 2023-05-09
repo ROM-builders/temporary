0555be0eb25560fde642524fa9d7db8ac77754ff
Tejas Singh <tejas@m.ms.evolution-x.org>
2023-05-09 12:59:29 +0530

    Trigger Build

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/BootleggersROM/manifest.git -b tirimbino -g default,-mips,-darwin,-notdefault
-git clone https://github.com/TestingStuffs/Local_Manifest.git --depth 1 -b bootleg .repo/local_manifests
+git clone https://github.com/Testing-Stuffs/Local_Manifest.git --depth 1 -b bootleg .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
