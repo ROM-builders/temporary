f4ed31d212e202d4269e4b60313a0e5a7b7dc5f1
AbrarNoobAlt <123289074+AbrarNoobAlt@users.noreply.github.com>
2023-05-10 21:15:54 +0600

    Again due to upload issue

--- build_rom.sh
+++ build_rom.sh
@@ -4,6 +4,7 @@ git clone https://github.com/AbrarNoob/local_manifest --depth 1 -b banana .repo/
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 
+
 # build rom
 . build/envsetup.sh
 lunch banana_alioth-userdebug
