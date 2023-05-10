a65d3e5b6ee3b275a96b77da3249c6937e018996
Simhadri07 <125692190+Simhadri07@users.noreply.github.com>
2023-05-10 21:53:22 +0530

    rmx2061-pixelos-simhadri07

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/RisingTechOSS/android -b thirteen -g default,-mips,-darwin,-notdefault
+repo init -u https://github.com/PixelOS-AOSP/manifest.git -b thirteen
 git clone https://github.com/Simhadri07/local_manifest.git --depth 1 -b master .repo/local_manifest
-repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
+repo sync
 
 # build rom
-source build/envsetup.sh
-lunch lineage_device-userdebug/user
+source . build/envsetup.sh
+lunch android_rmx2061-unoffficial
 export TZ=Asia/Kolkata #put before last build command
-mka bacon -jX
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
