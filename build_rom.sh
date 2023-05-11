d31d27589bfb8832f0eb613a94690fc48bac101c
Rohit Tiwari <130814475+RT1648@users.noreply.github.com>
2023-05-11 13:25:53 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init -u https://github.com/PixelExperience/manifest -b thirteen
+git clone https://github.com/RT1648/local_manifestt.git -b main
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
