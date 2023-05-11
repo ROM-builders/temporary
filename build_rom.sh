b298e48246a4ba70cb12f682852aae21d6d5a592
Prettysir <114632521+Prettysir@users.noreply.github.com>
2023-05-11 13:12:34 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest -b tiramisu -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/ArrowOS/android_manifest.git -b arrow-12.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Prettysir/local_manifest.git -b cherish .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch cherish_X00QD-user
-export CHERISH_VANILLA=false
-export BUILD_USERNAME=Akshat
-export BUILD_HOSTNAME=coldziin
-export TZ=Asia/Kolkata #put before last build command
-mka bacon
+lunch arrow_m23xq-user
+m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
