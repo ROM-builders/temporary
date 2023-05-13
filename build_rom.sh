344eade59f124f7f7a59bd646eb994bfde26eab8
CodeChas3r <119830528+CodeChas3r1@users.noreply.github.com>
2023-05-13 11:37:19 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/CodeChas3r1/local_manifest.git --depth 1 -b derp13 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/CodeChas3r1/local_manifest.git --depth 1 -b derp12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
