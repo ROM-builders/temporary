a9d7e0a34848bc9492490d3f3a87dc6bdbf2d4a8
Eritsuu <133488662+Eritsuu@users.noreply.github.com>
2023-05-14 06:54:51 +0700

    Update

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://github.com/vytska69/Local-Manifests.git --depth 1 -b evox-mi439 .repo/local_manifests
+git clone https://github.com/Eritsuu/Local-Manifests.git --depth 1 -b evox-mi439 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
