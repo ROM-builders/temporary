9e1e82e5189d9905f17b0143031236fd12a5c0f6
Zahran Ansaria <zahranansaria@gmail.com>
2023-05-17 10:09:34 +0700

    temp: don't use more depth and add ".git" on end of repo link

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixysOS/manifest -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/imannig/local_manifests --depth=1 -b pixys .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixysOS/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
+git clone https://github.com/imannig/local_manifests --depth 1 -b pixys .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
