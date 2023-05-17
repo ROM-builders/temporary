a187dedaec0dd6f727be1dafe6bc3f6174e008b7
IdkAnythin07 <94067465+IdkAnythin07@users.noreply.github.com>
2023-05-17 20:28:48 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,23 @@
 # sync rum
-repo init --depth=1 --no-repo-verify -u https://github.com/BlissRoms/platform_manifest -b typhoon -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/BlissRoms/platform_manifest -b typhoon-qpr2 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/IdkAnythin07/local_manifest --depth 1 -b bliss .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rum
 source build/envsetup.sh
 lunch bliss_RMX1941-userdebug
+
+export BUILD_USER=IdkAnythin07
+export BUILD_USERNAME=IdkAnythin07
+export KBUILD_BUILD_USER=IdkAnythin07
+export KBUILD_BUILD_USERNAME=IdkAnythin07
+export BUILD_HOST=cirrus-ci
+export BUILD_HOSTNAME=cirrus-ci
+export SKIP_ABI_CHECKS=true
+export SKIP_API_CHECKS=true
+
 export TZ=Asia/Kolkata #put b4 lst build command
+
 blissify -g RMX1941
 
 # upload rum (if you don't need to upload multiple files, then you don't need to edit next line)
