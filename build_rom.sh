414fdd6b654e0dbb1b07c89572981721df15ef81
Priya Ranjan Samal <116898350+eartinity24@users.noreply.github.com>
2023-05-18 16:42:24 +0530

    Done build

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-AOSP/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/eartinity24/local_manifest.git --depth 1 -b Pos .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest.git -b thirteen-plus -g default,-mips,-darwin,-notdefault
+git clone https://github.com/eartinity24/local_manifest.git --depth 1 -b Pe/plus .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
