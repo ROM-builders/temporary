29ab9e66cfdebad211cfa4b10cf1ea48f083d412
Priya Ranjan Samal <116898350+eartinity24@users.noreply.github.com>
2023-05-18 06:30:59 +0530

    Done build

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch aosp_r5x-userdebug
-BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
 export TZ=Asia/Kolkata #put before last build command
 make bacon
 
