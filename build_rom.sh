73526956d9ffa6ceeed1bc50a40c845571d95824
IdkAnythin07 <94067465+IdkAnythin07@users.noreply.github.com>
2023-05-19 02:18:19 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rum (18th try)
+# sync rum (19th try)
 repo init --depth=1 --no-repo-verify -u https://github.com/BlissRoms/platform_manifest -b typhoon-qpr2 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/IdkAnythin07/local_manifest --depth 1 -b bliss .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
