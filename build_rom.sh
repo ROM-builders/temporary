66717b9f2812620c01a8cc6708abc5d718df6c89
Matheus Souza <33396386+matheucomth@users.noreply.github.com>
2023-05-18 20:38:54 -0300

    fixing my noob bringup

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.g
 git clone https://github.com/matheucomth/local_manifest.git --depth 1 -b superior .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build roms
+# build rom
 . build/envsetup.sh
 lunch superior_tulip-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
