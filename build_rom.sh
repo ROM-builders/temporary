eab400680e58d66cb5dce8be6efd3703b797d4c2
Tejas Singh <tejas@m.ms.evolution-x.org>
2023-05-19 22:39:49 +0530

    Trigger 7.9.1

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,14 @@
-# sync rom
+# sync
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Testing-Stuffs/Local_Manifest.git --depth 1 -b evolution .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build
+export TZ=Asia/Kolkata
 . build/envsetup.sh
 lunch evolution_ginkgo-userdebug
-export TZ=Asia/Kolkata
 mka evolution
 cat */*/*/ginkgo/*.zip.json
 
-# upload rom
+# upload
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
