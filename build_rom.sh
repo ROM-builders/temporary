916edf1ac22b571d08b72ce7bdd73deefad181c1
Emmanuel <130129564+emmnueel@users.noreply.github.com>
2023-05-20 08:11:32 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch cherish_lancelot-userdebug
+lunch aosp_lancelot-userdebug
 export BUILD_USERNAME=Emmanuel
 export BUILD_HOSTNAME=CI
 export TZ=Asia/Manila
