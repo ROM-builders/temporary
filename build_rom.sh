7df3fe48553ced0027272027acfbb217911e83f7
Shravankumar <shravankumarsayz@gmail.com>
2023-05-22 14:52:32 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,10 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo init -u https://github.com/Spark-Rom/manifest -b pyro
+repo sync --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
+lunch spark_RMX1901-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
