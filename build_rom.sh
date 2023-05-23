cb887ad5daa95cafe307b88240be7043b2078839
Shravankumar <shravankumarsayz@gmail.com>
2023-05-23 09:24:27 +0530

    RMX1901-Spark OS-pyro-shravansayz

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
-
-repo init -u https://github.com/Octavi-Staging/manifest.git -b thirteen -g default,-mips.-darwin.-notdefault
+#sync rom
+repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b pyro -g default,-mips,-darwin,-notdefault
 git clone https://github.com/shravansayz/local_manifest.git -b main .repo/local_manifests
 repo sync -c -f --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j8
 
 # build rom
 source build/envsetup.sh
-lunch octavi_RMX1901-userdebug
+lunch spark_RMX1901-userdebug
 export TZ=Asia/Dhaka #put before last build command
-mka bacon -j$(nproc --all)
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
