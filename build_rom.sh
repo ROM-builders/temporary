75bc1c51fde04d98c276a6fb7aa6daaa64e8e5d4
Aditya Eka Pranata <aepranata@gmail.com>
2023-05-24 14:25:23 +0700

    build: Remove SElinux neverallow

--- build_rom.sh
+++ build_rom.sh
@@ -6,9 +6,9 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_rosy-userdebug
-export BUILD_USERNAME=@builder
-export BUILD_HOSTNAME=@atiga-pc
-export SELINUX_IGNORE_NEVERALLOWS=true
+export BUILD_USERNAME=aepranata
+export BUILD_HOSTNAME=a3-builder
+#export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Jakarta #put before last build command
 make bacon
 
