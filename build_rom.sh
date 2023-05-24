060f4d7558f5f19a50cbaf081871b32d82b0a273
XloaderHell969 <132766529+XloaderHell969@users.noreply.github.com>
2023-05-24 14:11:57 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/Miku-UI/manifesto.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-AOSP/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/XloaderHell969/local_manifest-.git --depth 1 -b master .repo/local_manifest-
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
