da45b2b2e06bae78df19e52b599599c74a737e91
DESTROYER32 <44976858+DESTROYER-32@users.noreply.github.com>
2023-05-24 11:14:30 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch voltage_liber-userdebug
 export TZ=Asia/Kolkata #put before last build command
 brunch voltage_liber-userdebug
 
