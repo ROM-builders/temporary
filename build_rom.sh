bdbe0cd9cac3e30a6a72c802fe73c299e10e06a1
XloaderHell969 <132766529+XloaderHell969@users.noreply.github.com>
2023-05-24 14:23:38 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch derp_RMX2185-user
+lunch derp_rmx2185-user
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
