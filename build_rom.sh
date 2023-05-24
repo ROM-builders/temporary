6048811bff2091309d78dcf7a911f5713b9f575e
Tejas Singh <tejas@m.ms.evolution-x.org>
2023-05-24 12:39:26 +0530

    NFC Fix Attempt

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
-# sync rom
+# sync
 repo init --depth=1 --no-repo-verify -u https://github.com/Project-Xtended/manifest.git -b xt -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Testing-Stuffs/Local_Manifest.git --depth 1 -b xtended .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build
+export TZ=Asia/Kolkata
 . build/envsetup.sh
 lunch xtended_ginkgo-userdebug
-export TZ=Asia/Kolkata
 mka xtended
 
-# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+# upload (useless anyway as of now)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
