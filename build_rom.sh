10f0bfcecd81eaebac1fd0d492adc35b7a9bfbc9
IJoseph23 <121319594+IJoseph23@users.noreply.github.com>
2023-05-26 11:49:16 +0200

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,6 +4,7 @@ git clone https://github.com/IJoseph23/local_manifest.git --depth 1 -b main .rep
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
+
 source build/envsetup.sh
 lunch lineage_gta4xl-user
 export TZ=Asia/Dhaka #put before last build command
