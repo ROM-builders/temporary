67d830cd9a177df7ca5b2ff90edec4645d10e2db
Siddharth Tiwari <95074087+llSiddharthll@users.noreply.github.com>
2023-05-28 13:29:59 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -4,6 +4,7 @@ git clone https://github.com/llSiddharthll/local_manifest.git --depth 1 -b main 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
+sh /device/xiaomi/fleur/prebuilts/unpack-vendor.sh
 source build/envsetup.sh
 lunch aosp_fleur-eng
 export TZ=Asia/Dhaka #put before last build command
