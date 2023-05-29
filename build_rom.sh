ea3f7c702c27603f7d05e566a47d04d7cceb37f5
Aqua <127223292+Burhanverse@users.noreply.github.com>
2023-05-29 19:29:39 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch pixelos_lancelot-userdebug
+lunch aosp_lancelot-user
 export TZ=Asia/Kolkata #put before last build command
 make bacon
 
