a555b78cb0e5b2c37db4f00514d918c3479ce5eb
Aqua <127223292+Burhanverse@users.noreply.github.com>
2023-05-29 23:35:32 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch aosp_lancelot-user
+lunch aosp_lancelot-userdebug
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
