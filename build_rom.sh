7d7d1cd36beaebd0f97ed08be50ae529ce11f1ba
strongreasons <109900777+strongreasons@users.noreply.github.com>
2023-05-31 04:21:34 +0700

    X00TD-LineageOS-lineage-17.1-strongreasons

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom
+# sync romo
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android -b lineage-17.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/strongreasons/local_manifest.git --depth 1 -b 17 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
