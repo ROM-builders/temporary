6a68a89f2eaea8c251075b262e6b60515c1e3313
Lite <97386083+Lite009@users.noreply.github.com>
2023-05-30 08:58:29 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/androi
 git clone https://github.com/Lite009/local_manifests.git --depth 1 -b Crdroid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom 
 source build/envsetup.sh
 export WITH_GAPPS=true
 export TZ=Asia/Kolkata #put before last build command
