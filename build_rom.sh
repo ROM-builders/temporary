b33f68aa9905e50bba3a791bc949bcadea3d4fd5
Itu <131593996+WAR-DEPT@users.noreply.github.com>
2023-05-31 15:39:47 +0530

    miatoll-SuperiorOS-thirteen-WAR-DEPT

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/PixelOS-AOSP/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/SuperiorOS/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/WAR-DEPT/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
-lunch aosp_miatoll-user
+. build/envsetup.sh
+lunch superior_miatoll-userdebug
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
