fda798d782cd3f54729a65d10d22e2618a991cf0
Fr0ztyy43 <82140208+Fr0ztyy43@users.noreply.github.com>
2023-05-31 08:58:12 +0200

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Fr0ztyy43/local_manifests --depth 1 -b los20 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/StagOS/manifest.git -b t13 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Fr0ztyy43/local_manifests --depth 1 -b stag .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build romakb
 source build/envsetup.sh
 
 
-lunch lineage_begonia-userdebug
+lunch stag_begonia-userdebug
 
 export KBUILD_BUILD_USER=Fr0ztyy43 
 export KBUILD_BUILD_HOST=Noob
