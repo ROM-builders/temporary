655fa69796174ed92998a496ebf6e857a63f9bff
Zhyper0 <132120605+Zhyper0@users.noreply.github.com>
2023-06-01 08:46:22 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,11 +5,11 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_certus-userdebug
+
 export TZ=asia/Dhaka #put before last build command
 export TARGET_BOOT_ANIMATION_RES=720
 export SELINUX_IGNORE_NEVERALLOWS=true
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
-make bacon
+brunch certus
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
