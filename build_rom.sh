07c8b9f9a780a66e294e5254aff716da8145a241
Hemanth <hemanthjabalpuri@gmail.com>
2023-06-01 22:41:48 +0530

    Build with latest changes from los

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git
 git clone https://github.com/HemanthJabalpuri/local_manifest --depth 1 -b rhode .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom (1)
+# build rom (2)
 source build/envsetup.sh
 lunch lineage_rhode-userdebug
 export TZ=Asia/Kolkata #put before last build command
