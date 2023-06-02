3b88fcffe0678e263735e98e34f89825f1efd127
@ozip.cf <41739417+ozipoetra@users.noreply.github.com>
2023-06-02 08:22:57 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch lineage_merlinx-user
-export TZ=Asia/Jakarta #put before last build command
+export TZ=Asia/Jakarta
 export RISING_CHIPSET=MT6769Z
 export RISING_MAINTAINER=ozipoetra
 export RISING_PACKAGE_TYPE=PIXEL
@@ -19,7 +19,7 @@ export WITH_GMS=true
 export TARGET_CORE_GMS=false
 export TARGET_USE_GOOGLE_TELEPHONY=true
 export TARGET_CORE_GMS_EXTRAS=true
-m bacon
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
