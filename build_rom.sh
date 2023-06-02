4e2cb197dfa89fdc1f94d44d754cc8e29918d7f1
Aqua <127223292+Burhanverse@users.noreply.github.com>
2023-06-02 09:47:00 +0530

    DerpMode ON

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch drep_lancelot-userdebug
+lunch derp_lancelot-userdebug
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
