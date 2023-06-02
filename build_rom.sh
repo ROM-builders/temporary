b0fb2538951d8480fa022f4197bb3a2d52bfd966
ozipoetra <ozipoetra120@gmail.com>
2023-06-02 07:53:41 +0700

    test

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,13 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/RisingTechOSS/android -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/ozipoetra/local_manifest.git --depth 1 -b main .repo/local_manifests
-repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
 
 # build rom
 source build/envsetup.sh
 lunch lineage_merlinx-user
 export TZ=Asia/Jakarta #put before last build command
-# rising flag
 export RISING_CHIPSET=MT6769Z
 export RISING_MAINTAINER=ozipoetra
 export RISING_PACKAGE_TYPE=PIXEL
@@ -16,7 +15,6 @@ export TARGET_BUILD_APERTURE_CAMERA=true
 export TARGET_ENABLE_BLUR=false
 export TARGET_HAS_UDFPS=true
 export TARGET_USE_PIXEL_FINGERPRINT=false
-# gms flag
 export WITH_GMS=true
 export TARGET_CORE_GMS=false
 export TARGET_USE_GOOGLE_TELEPHONY=true
