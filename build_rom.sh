886e7df8aadf40b9f17b084b8927dc1620939f50
Andy Prasetya <54360298+nullptr03@users.noreply.github.com>
2023-06-03 14:33:21 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
-repo init -u https://github.com/bananadroid/android_manifest.git -b 13 --git-lfs
+repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 13 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/nullptr03/local_manifest.git --depth 1 -b master .repo/local_manifests
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
