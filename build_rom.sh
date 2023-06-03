eb80636107166a69b518e67ab5f660d59346a4e1
Andy Prasetya <54360298+nullptr03@users.noreply.github.com>
2023-06-03 12:16:34 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,10 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo init -u https://github.com/bananadroid/android_manifest.git -b 13 --git-lfs
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
+lunch banana_<merlinx>-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
