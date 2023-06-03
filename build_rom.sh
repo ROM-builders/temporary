f553b02415e9aefd93c27a4e5b32ae4eb0690ff6
Andy Prasetya <54360298+nullptr03@users.noreply.github.com>
2023-06-03 14:26:25 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+repo init -u https://github.com/bananadroid/android_manifest.git -b 13 --git-lfs
 git clone https://github.com/nullptr03/local_manifest.git --depth 1 -b master .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune
 
 # build rom
 source build/envsetup.sh
