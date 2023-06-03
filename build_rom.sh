e45a065d4910f75e773e3f7bfbf0c5ff02f0f875
YudhoPatrianto <124352333+YudhoPatrianto@users.noreply.github.com>
2023-06-03 13:41:04 +0700

    selene: Welcome To XDroidOSS

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,5 @@
 # sync rom
+curl -Ls https://raw.githubusercontent.com/ROM-builders/temporary/main/ci/test.sh | bash
 repo init -u https://github.com/xdroidOSS-Pixel/manifest -b thirteen  default,-mips,-darwin,-notdefault
 git clone https://github.com/YudhoPatrianto/local-manifest.git -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
