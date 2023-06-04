6b715e1c88b6dda7fc99490e45e76349698b9901
Andy Prasetya <54360298+nullptr03@users.noreply.github.com>
2023-06-04 08:57:27 +0800

    CherishOS

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b 13 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/nullptr03/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_merlinx-userdebug
+lunch cherish_merlinx-userdebug
 export BUILD_USERNAME = @nullptr03.
 export BUILD_HOSTNAME = @nullptr03.
 export KBUILD_BUILD_NAME = @nullptr03.
