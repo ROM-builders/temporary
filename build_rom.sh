8aad42b7e029ba61cb1129557d9f4de43c7e3f90
priyanshu jangid <97881238+thepriyanshujangid@users.noreply.github.com>
2023-06-05 21:57:44 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/bananadroid/android_manifest.git -b 13 --git-lfs -g default,-mips,-darwin,-notdefault
-git clone https://github.com/thepriyanshujangid/local_menifest.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/thepriyanshujangid/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 
