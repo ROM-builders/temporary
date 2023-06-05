b6d3c7ad6bc7bc027daa4e4e294ad386628df72d
z3zens <78209717+z3zens@users.noreply.github.com>
2023-06-05 15:11:46 +0700

    X01BD-AOSPA-topaz-z3zens

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest -b top
 git clone https://github.com/z3zens/local_manifest.git --depth 1 -b topaz .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom AOSPA topaz beta 
+# build rom AOSPA topaz beta
 source build/envsetup.sh
 lunch aospa_X01BD-userdebug
 export BUILD_USERNAME=nobody
