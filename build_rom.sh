97d7be57cdd8b214444f3304be101b6d5cc41b54
mitsu00 <127077483+mitsu00@users.noreply.github.com>
2023-06-05 19:23:19 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -11,7 +11,7 @@ mka bacon
 
 # upload rom (if you on't need to upload multiple files, then you don't need to edit next line)
 ls /out/target/product/
-curl -X POST -H "Content-Type:multipart/form-data" -F chat_id=965784022 -F document=@"out/target/product/merlinx/*.zip" "https://api.telegram.org/bot5241366125:AAGmyJDczdhciMypsKxWafJH6w0LfvSzYpE/sendDocument"
+# curl -X POST -H "Content-Type:multipart/form-data" -F chat_id=965784022 -F document=@"out/target/product/merlinx/*.zip" "https://api.telegram.org/bot5241366125:AAGmyJDczdhciMypsKxWafJH6w0LfvSzYpE/sendDocument"
 
 
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
