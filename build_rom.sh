a4e32fa0a63735ec802f747238325264ff06a6b6
ozipoetra <ozipoetra120@gmail.com>
2023-06-05 13:59:46 +0700

    test

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android -b lineage-20.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ozipoetra/local-manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/ozipoetra/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -10,4 +10,9 @@ export TZ=Asia/Jakarta #put before last build command
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+telegram_upload() {
+  curl -X POST -H "Content-Type:multipart/form-data" -F chat_id=965784022 -F document=@"$1" "https://api.telegram.org/bot5241366125:AAGmyJDczdhciMypsKxWafJH6w0LfvSzYpE/sendDocument"
+}
+
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+telegram_upload "out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip"
--- ci/notify.sh
+++ ci/notify.sh
@@ -8,7 +8,7 @@ grep _jasmine_sprout $CIRRUS_WORKING_DIR/build_rom.sh > /dev/null && device=jasm
 grep _laurel_sprout $CIRRUS_WORKING_DIR/build_rom.sh > /dev/null && device=laurel_sprout
 grep _GM8_sprout $CIRRUS_WORKING_DIR/build_rom.sh > /dev/null && device=GM8_sprout
 grep _maple_dsds $CIRRUS_WORKING_DIR/build_rom.sh > /dev/null && device=maple_dsds
-your_telegram_id=@ROM_builders_junk
+your_telegram_id=965784022
 show=$(cat $CIRRUS_WORKING_DIR/build_rom.sh)
 curl -s "https://api.telegram.org/bot${bot_api}/sendmessage" -d "text=<code>$device-$rom_name</code> Started
 https://cirrus-ci.com/build/$CIRRUS_BUILD_ID $show" -d "chat_id=${your_telegram_id}" -d "parse_mode=HTML"
