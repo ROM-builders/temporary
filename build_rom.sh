733de94fc5eb5171784b9f094170f314f2193a19
strongreasons <109900777+strongreasons@users.noreply.github.com>
2023-06-05 14:25:19 +0700

    X00TD-AOSPA-topaz-strongreasons

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android -b lineage-17.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/strongreasons/local_manifest.git --depth 1 -b 17 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest -b topaz -g default,-mips,-darwin,-notdefault
+git clone https://github.com/strongreasons/local_manifest.git --depth 1 -b topaz .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build roms
+# build rom
 source build/envsetup.sh
-lunch lineage_X00TD-userdebug
-export BUILD_USERNAME=stress
-export BUILD_USER=stress
-export KBUILD_BUILD_USER=stress
-export TZ=Asia/Jakarta #put before last build command
-mka bacon
+lunch aospa_X00TD-userdebug
+export TZ=Asia/Jakarta # put before last build command
+./rom-build.sh X00TD -t userdebug
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
