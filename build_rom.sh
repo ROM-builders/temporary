e243561a0be0816b316844b73c1c8879529a0b73
arslan1400 <132056853+arslan1400@users.noreply.github.com>
2023-06-06 06:33:16 -0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom
+# sync roms
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android -b lineage-20.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/arslan1400/local_manifest.git --depth 1 -b a52sxq .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
