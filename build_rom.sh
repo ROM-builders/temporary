c197fc6f7429fea16a8b86cca901742b38720e2b
Andy Prasetya <54360298+nullptr03@users.noreply.github.com>
2023-06-07 19:26:32 +0800

    BootLeggers

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch cherish_merlinx-userdebug
+lunch bootleg_merlinx-userdebug
 export BUILD_USERNAME=Andy
 export BUILD_HOSTNAME=Andy
 export TZ=Asia/Makassar
