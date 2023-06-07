f7a3d936b21cc6a4f6fc65ee7fbd21af290bf6f8
Andy Prasetya <54360298+nullptr03@users.noreply.github.com>
2023-06-08 00:22:50 +0800

    DerpFest done sur

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/BootleggersROM/manifest.git -b tirimbino -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Burhanverse/local_manifest.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Burhanverse/local_manifest.git --depth 1 -b derpfest .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch bootleg_lancelot-userdebug
+lunch derp_lancelot-userdebug
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
