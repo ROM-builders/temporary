00f794a2daeaeebccd6f7c88001acb9ce88d8d99
Expert Jatt <135785987+EXPERT-JATT@users.noreply.github.com>
2023-06-08 13:58:58 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/CherishOS/android.git -b 13.0 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/crdroidandriod/android.git -b 13.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/EXPERT-JATT/local_manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
