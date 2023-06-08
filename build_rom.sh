c70081d1c307f7fbdb649a8732286f904bee2f9f
Shravan55555 <134358009+Shravan55555@users.noreply.github.com>
2023-06-08 12:47:32 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,22 +6,12 @@ repo sync -c -f --force-sync --optimized-fetch --no-tags --no-clone-bundle --pru
 # build rom
 source build/envsetup.sh
 lunch spark_RMX1901-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
-export ALLOW_MISSING_DEPENDENCIES=true
-export RELAX_USES_LIBRARY_CHECK=true
-export BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES=true
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
-export BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE=true
-export BUILD_BROKEN_VERIFY_USES_LIBRARIES=true
-export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
-export BUILD_BROKEN_DUP_RULES=true
-export SKIP_ABI_CHECKS=true
-export BUILD_USERNAME=SK.
-export BUILD_HOSTNAME=SK.
-export KBUILD_BUILD_NAME=SK.
-export KBUILD_BUILD_HOST=SK.
-export TZ=Asia/Kolkata #put before last build command
+export BUILD_USERNAME=shrav1
+
+export TZ=Asia/Kolkata
 mka bacon
+export TZ=Asia/Kolkata #put before last build command
+
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
