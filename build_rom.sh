2fd14c3f164b6dba474989b4574b52232235a89e
Di Raizel ㋡ <abhinav.115260@gmail.com>
2023-06-09 00:58:54 +0530

    Rebuild with misc improvement

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
- # sync Rom    
+ # sync Rom     
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
 git clone https://github.com/acex69/local_manifest.git --depth 1 -b evox .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
