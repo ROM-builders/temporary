763aff9d463d98181d25e631ad70559729186758
Expert Jatt <135785987+EXPERT-JATT@users.noreply.github.com>
2023-06-08 11:05:09 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch Cherish_tulip-user
+lunch Lineage_tulip-user
 export TZ=Asia/Dhaka #put before last build command
 m bacon
 
