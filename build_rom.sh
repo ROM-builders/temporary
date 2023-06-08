67294f58d1155386b469a955ac919139f7e7fe80
Expert Jatt <135785987+EXPERT-JATT@users.noreply.github.com>
2023-06-08 10:02:54 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch DerpFest_tulip-user
+lunch derp_tulip-user
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
