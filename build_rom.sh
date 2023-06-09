5fc878f07a5223cef8d8f7de4f5ddf10e4f95b82
Akshat Singh <akshatleo9456@gmail.com>
2023-06-09 08:08:37 +0530

    remove hardware/xiaomi

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/alphadroid-project/ma
 git clone https://github.com/Legendleo90/local_manifest.git -b alpha .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom#
 source build/envsetup.sh
 lunch lineage_beryllium-user
 export BUILD_USERNAME=Akshat
