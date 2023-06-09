b045716a8f567ed27028d2c2a7b24b4ce7de3c2e
Arafattex <69374795+Arafattex@users.noreply.github.com>
2023-06-09 11:03:45 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -8,7 +8,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 lunch lineage_lancelot-user
 export BUILD_USERNAME=shas45558
 export TZ=Asia/Kolkata #put before last build command
-export WITH_GMS=true
 m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
