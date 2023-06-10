b4d6016e175c7175869862ee1912dadf6884d854
Naim <88440061+FollowNaim@users.noreply.github.com>
2023-06-10 23:32:40 +0600

    Los | spes | FollowNaim

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom 
-repo init --depth=1 --no-repo-verify -u git://github.com/ArrowOS/android_manifest -b arrow-13.1 -g default,-mips,-darwin,-notdefault
+repo init -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs
 git clone https://github.com/FollowNaim/Local_Manifest --depth 1 -b main .repo/local_manifest
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
+lunch los_spes-user
 export TZ=Asia/Dhaka #put before last build command
-mka derp
+mka spes
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
