0f67870f7194a74311ba9aaf64a2efc515b86bb4
K R I S H N A <krishnakripa34567@gmail.com>
2023-06-11 12:49:41 +0530

    temporary: init Alphadroid

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pure-soul-kk/local_manifest.git --depth=1 -b cherish .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/alphadroid-project/manifest -b alpha-13 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/pure-soul-kk/local_manifest.git --depth=1 -b alpha .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch cherish_PL2-userdebug
+lunch lineage_PL2-userdebug
 export BUILD_USERNAME=K R I S H N A
 export BUILD_HOSTNAME=pure-soul-kk
 export TZ=Asia/Kolkata #put before last build command
-mka bacon 
+make bacon 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
