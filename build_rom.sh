7bec6ee764a6749252cb8e78fdeb4635a7cad6da
Sundram <101039005+xyz-sundram@users.noreply.github.com>
2023-06-11 08:59:51 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 13.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/xyz-sundram/local_manifests.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
+git clone https://github.com/xyz-sundram/local_manifests.git --depth 1 -b Cherish-A13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build roms
 . build/envsetup.sh
-lunch lineage_twolip-user
+lunch cherish_twolip-user
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
