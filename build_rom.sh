f1ec34b23e323196ef3c64361b6aa563e9ad41c0
roynatech2544 <whiteshell2544@naver.com>
2023-06-12 11:18:04 +0900

    m

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b thirteen-plus -g default,-mips,-darwin,-notdefault
-git clone https://github.com/roynatech2544/local_manifests --depth 1 .repo/local_manifests -b dreamlte
+repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest -b 13 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/roynatech2544/local_manifests --depth 1 .repo/local_manifests -b dreamlte-derp
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build r
 source build/envsetup.sh
-lunch aosp_dream2lte-user
+lunch derp_dream2lte-user
 export TZ=Asia/Seoul #put before last build command
-mka bacon
+mka derp
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
