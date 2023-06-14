35056c0e90885cb6250d9f2f8ae06ea77eff7dac
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2023-06-14 09:40:21 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,17 +3,15 @@ repo init -u https://github.com/alphadroid-project/manifest -b alpha-13 --git-lf
 git clone https://github.com/ManitnjG/local_manifest.git --depth 1 -b al .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+export ALLOW_MISSING_DEPENDENCIES=true
+
 # build rom
 
 source build/envsetup.sh
-
 lunch lineage_X01BD-userdebug
-
+export TZ=Asia/Dhaka #put before last build command
 make bacon
 
-export ALLOW_MISSING_DEPENDENCIES=true
-export TZ=Asia/Dhaka #put before last build command
-
-  
+ 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
