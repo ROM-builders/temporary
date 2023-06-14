b500df0ec57b9142309521222ee48b2cfbf56eff
mitsu00 <127077483+mitsu00@users.noreply.github.com>
2023-06-14 17:03:45 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/alphadroid-project/manifest -b alpha-13 --git-lfs -g default,-mips,-darwin,-notdefault
-git clone https://github.com/mitsu00/local_manifest.git --depth 1 -b alpha .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 13 --git-lfs -g default,-mips,-darwin,-notdefault
+git clone https://github.com/mitsu00/local_manifest.git --depth 1 -b banana-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch lineage_merlinx-user
-export BUILD_USERNAME=mitsu00
-export BUILD_HOSTNAME=ozip.my.id
+lunch banana_merlinx-user
+export BUILD_USERNAME=ozip
+export BUILD_HOSTNAME=ozip
 export TZ=Asia/Jakarta #putt before last build command
-mka bacon
+mka banana
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
