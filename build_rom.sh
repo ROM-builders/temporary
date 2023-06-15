dc770daa6ad8bf775851ae093756f4c4fd529ea7
SKetU <sketu@donot.care>
2023-06-15 16:36:25 +0530

    Try --git-lfs

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b triton -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b triton --git-lfs -g default,-mips,-darwin,-notdefault
 git clone https://github.com/SKetU-l/local_manifest --depth=1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
