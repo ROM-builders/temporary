3b9522e05665fd97a2c50ecdad01d99de18fe109
K R I S H N A <krishnakripa34567@gmail.com>
2023-06-15 14:56:54 +0530

    sweet: alphadroid

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/alphadroid-project/manifest -b alpha-13 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pure-soul-kk/local_manifest.git --depth=1 -b alpha .repo/local_manifests
+git clone https://github.com/pure-soul-kk/local_manifest.git --depth=1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_PL2-userdebug
+lunch lineage_sweet-userdebug
 export BUILD_USERNAME=K R I S H N A
 export BUILD_HOSTNAME=pure-soul-kk
 export TZ=Asia/Kolkata #put before last build command
