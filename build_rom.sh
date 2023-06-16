1b5f756d9da19a603edc71886100f2bee6de1344
S∆UR∆V R∆W∆T <90666710+SauRavRwT@users.noreply.github.com>
2023-06-16 08:38:13 +0530

    2nd try removed duplicate value

--- build_rom.sh
+++ build_rom.sh
@@ -3,6 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Project-Xtended/manif
 git clone https://github.com/SauRavRwt/local-manifest.git --depth=1 -b xt .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+#2
 # build rom
 source build/envsetup.sh
 lunch xtended_redwood-userdebug
