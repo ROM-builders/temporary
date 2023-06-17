34f4d7c56788c44ad0d791373403d3dd67e22962
Adalat222 <kumaradalat2325@gmail.com>
2023-06-17 19:22:12 +0530

    gauguin-Evolution-X-tiramisu-TheTester87

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
 git clone https://github.com/TheTester87/local_manifests --depth 1 -b master .repo/local_manifests
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
