a4f0277e1021d1bab78a4ae68e02b44d7385efd3
Rishik-AKA-TPSRISHIKop <rishikrvr@gmail.com>
2023-06-18 11:26:45 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,14 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/PixelPlusUI/manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Rishik-AKA-TPSRISHIKop/local_manifest.git --depth 1 -b master .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+git clone https://github.com/SamsungExynos9810/local_manifests-v2 .repo/local_manifests
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
 # build rom
 source build/envsetup.sh
 lunch aosp_star2lte-userdebug
 export TZ=Asia/Dhaka 
 #put before last build command
-mka bacon -j8
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
