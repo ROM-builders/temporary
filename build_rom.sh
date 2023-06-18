fc275e486947681ae9fe5b55998ba50284f43f52
Aditya Serasha Wahyudi <126944411+goyangasek@users.noreply.github.com>
2023-06-18 17:59:58 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/PixelExperience/manifest -b twelve-plus -g default,-mips,-darwin,-notdefault
-git clone https://github.com/goyangasek/local_manifests.git --depth 1 -b vanilla .repo/local_manifests
+git clone https://github.com/goyangasek/local_manifests.git --depth 1 -b kontol .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
