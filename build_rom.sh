d2d21b91324001793b41c864dcc53e92dcef06e0
Paweł "PawiX25" Wyrębak <81538700+PawiX25@users.noreply.github.com>
2023-06-18 18:10:08 +0200

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init -u https://github.com/BootleggersROM/manifest.git -b tirimbino -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/BootleggersROM/manifest.git -b tirimbino -g default,-mips,-darwin,-notdefault
 git clone https://github.com/RenoirPixelExperienceTrees/local_manifests.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
