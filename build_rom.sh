e272c3ab522c6e7775271ceaaecdb1c011e47092
mozzaru <rianyanuar52@gmail.com>
2023-06-19 07:09:26 +0700

    test

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom 
 source build/envsetup.sh
-lunch blaze_markw-eng
+lunch blaze_markw-userdebug
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
