1541667b7a0b4a994032166e9bcc78beb1b445e7
Sundram <101039005+xyz-sundram@users.noreply.github.com>
2023-06-20 12:17:49 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/androi
 git clone https://github.com/xyz-sundram/local_manifests.git --depth 1 -b crd .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build roms
+# build rom
 . build/envsetup.sh
 lunch lineage_tulip-userdebug
 export WITH_GAPPS=true
