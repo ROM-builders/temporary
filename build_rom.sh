0b34b3ee6a685b1c8a86891e984f2491b5a4ceca
dany <adamzulkey@gmail.com>
2023-06-21 11:00:38 +0800

    lisa: PixysOS 2

--- build_rom.sh
+++ build_rom.sh
@@ -11,7 +11,7 @@ export KBUILD_BUILD_HOST=Jiu
 export BUILD_USERNAME=danyscape
 export BUILD_HOSTNAME=Jiu
 export TZ=Asia/Kuala_Lumpur #put before last build command
-make pixys
+make pixys 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
