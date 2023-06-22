2d4d61c91b2954c0a2148eddf028e82a1319df74
JV007xp <114124246+JV007xp@users.noreply.github.com>
2023-06-22 20:01:55 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,11 +5,10 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch carbon_A001D-userdebug
+lunch banana_A001D-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=America/Sao_Paulo
-
-make carbon
+m banana
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
