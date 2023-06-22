9074d9fb6f9b70c35bc249c0068a9795c0d0db15
JV007xp <114124246+JV007xp@users.noreply.github.com>
2023-06-22 20:00:38 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/CarbonROM/android.git -b cr-9.0 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 12 -g default,-mips,-darwin,-notdefault
 git clone --https://github.com/JV007xp/local-manifest.git --depth 1 -b Carbon .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
