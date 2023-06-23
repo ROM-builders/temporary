149ca6875510879335b3878fd2a7ded106ef3e66
Muichiro Tokito <132120605+Muichiro09@users.noreply.github.com>
2023-06-23 08:09:53 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 11.0 --git-lfs default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/CarbonROM/android.git -b cr-9.0 --git-lfs default,-mips,-darwin,-notdefault
 git clone https://github.com/Muichiro09/local_manifest.git --depth 1 -b eleven-surya .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_surya-userdebug
+lunch carbon_surya-userdebug
 export TZ=asia/Dhaka #put before last build command
 export SELINUX_IGNORE_NEVERALLOWS=true
-make bacon
+make carbon
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
