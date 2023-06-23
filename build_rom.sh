2e3eacc13e8c8745338a19418732e55c814766d8
nxzhuu47 <112096541+nxzhuu47@users.noreply.github.com>
2023-06-23 11:10:41 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Realme-C2/android_device_realme_RMX1941.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/nxzhuu47/c2_local_manifests.git --depth 1 -b master .repo/c2_local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
+lunch derp_rmx1941-user
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
