f9196c0cc4c86285693923131328ffc3befbda4e
Andy Prasetya <54360298+nullptr03@users.noreply.github.com>
2023-06-24 12:19:16 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,10 +6,10 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch blaze_merlinx-userdebug
-export KBUILD_USERNAME=Andy
-export KBUILD_HOSTNAME=Andy
 export BUILD_USERNAME=Andy
 export BUILD_HOSTNAME=Andy
+export KBUILD_USERNAME=Andy
+export KBUILD_HOSTNAME=Andy
 export TZ=Asia/Makassar
 mka bacon
 
