b0f913404a88546d23069a4180cc8eaf67f91e80
Sundram <101039005+xyz-sundram@users.noreply.github.com>
2023-06-26 08:13:20 +0530

    crdroidify

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 11.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/xyz-sundram/local_manifests.git --depth 1 -b crdroid-test .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 13.0 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/xyz-sundram/local_manifests.git --depth 1 -b crdroid13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch lineage_tulip-userdebug
-export WITH_GMS=true
-export SELINUX_IGNORE_NEVERALLOWS=true
+lunch lineage_twolip-user
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
