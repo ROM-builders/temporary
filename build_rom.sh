2e6bc20ce40d309428881c031703ebb27090f70f
crackedmodmenu <121314959+crackedmodmenu@users.noreply.github.com>
2023-06-27 09:25:52 +0200

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init -u https://github.com/LineageOS/android.git -b lineage-19.1 --git-lfs
+git clone https://raw.githubusercontent.com/Redmi-MT6768/local_manifests/master/twelve.xml --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
+lunch lineage_merlin-user
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
