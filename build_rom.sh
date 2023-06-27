1394b11892951a45478555c7e612cfb98e2e137e
fukiame <fukiame@proton.me>
2023-06-27 14:56:35 +0700

    tri do dai

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch arrow_selene-user
+lunch arrow_selene-userdebug
 export TZ=Asia/Ho_Chi_Minh # put before last build command
 m otapackage
 
