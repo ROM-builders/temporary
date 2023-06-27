0e0a8c606a190bc8ef3265b22eba6578cce3b91f
crackedmodmenu <121314959+crackedmodmenu@users.noreply.github.com>
2023-06-27 10:48:57 +0200

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,13 @@
-# sync Rom    
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest.git -b thirteen-plus -g default,-mips,-darwin,-notdefault
+ # sync Rom      
+repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
 git clone https://github.com/acex69/local_manifest.git --depth 1 -b merlinx .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-# build rom             
-source build/envsetup.sh 
-lunch aosp_merlinx-userdebug   
-export TZ=Asia/Delhi  
-make bacon    
+# build rom                 
+source build/envsetup.sh   
+lunch evolution_merlinx-userdebug 
+export TZ=Asia/Delhi    
+export BUILD_USERNAME=@raizel_69 
+export BUILD_HOSTNAME=@acex69 
+mka evolution 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line) 
-rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 
