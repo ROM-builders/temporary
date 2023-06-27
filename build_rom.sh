d8e99e66f3341c9c5149785b6a989068e6e6f658
crackedmodmenu <121314959+crackedmodmenu@users.noreply.github.com>
2023-06-27 10:40:18 +0200

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,11 @@
-# sync rom
-repo init -u https://github.com/LineageOS/android.git -b lineage-19.1 --git-lfs
-git clone https://raw.githubusercontent.com/Redmi-MT6768/local_manifests/master/twelve.xml --depth 1 -b master .repo/local_manifests/local_manifests.xml
+# sync Rom    
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest.git -b thirteen-plus -g default,-mips,-darwin,-notdefault
+git clone https://github.com/acex69/local_manifest.git --depth 1 -b merlinx .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
-# build rom
-source build/envsetup.sh
-lunch lineage_merlin-user
-export TZ=Asia/Dhaka #put before last build command
-mka bacon
-
-# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
-rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+# build rom             
+source build/envsetup.sh 
+lunch aosp_merlinx-userdebug   
+export TZ=Asia/Delhi  
+make bacon    
+# upload rom (if you don't need to upload multiple files, then you don't need to edit next line) 
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut
