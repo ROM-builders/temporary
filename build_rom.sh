7613beb13c47f252cdcd3826644dad9975dd2903
Andy Prasetya <54360298+nullptr03@users.noreply.github.com>
2023-06-28 10:14:59 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/syberia-project/manifest.git -b 13.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/nullptr03/local_manifest.git --depth 1 -b syberia .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b pyro -g default,-mips,-darwin,-notdefault
+git clone https://github.com/nullptr03/local_manifest.git --depth 1 -b spark_mstn .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch syberia_merlinx-userdebug
+lunch spark_moonstone-userdebug
 export BUILD_USERNAME=Andy
 export BUILD_HOSTNAME=Andy
+export KBUILD_USERNAME=Andy
+export KBUILD_HOSTNAME=Andy
 export TZ=Asia/Makassar
 mka bacon
 
