0ce62317b779d2aac8f2f31c2cd11f61e79801af
Omansh Krishn <96343317+omansh-krishn@users.noreply.github.com>
2023-06-29 19:00:43 +0530

    final changes
    zyc_clang
    user build
    less clutter in build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,25 +1,20 @@
 # sync rom
 
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest.git -b thirteen-plus -g default,-mips,-darwin,-notdefault
-git clone https://codeberg.org/omansh-krishn/local_manifest.git --depth 1 -b pixelexperience-13-t .repo/local_manifests
+git clone https://codeberg.org/omansh-krishn/local_manifest.git --depth 1 -b pixelexperience-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 # build rom
 
 source build/envsetup.sh
-lunch aosp_santoni-userdebug
+lunch aosp_santoni-user
 export TARGET_GAPPS_ARCH=arm64
-export TARGET_KERNEL_CLANG_VERSION=clang-r383902b1
-export TARGET_KERNEL_CLANG_PATH=$(pwd)/prebuilts/clang/host/linux-x86/${TARGET_KERNEL_CLANG_VERSION}
-export TARGET_KERNEL_CROSS_COMPILE_PREFIX=${TARGET_KERNEL_CLANG_PATH}/bin/aarch64-linux-gnu-
-export TARGET_KERNEL_CROSS_COMPILE_ARM32_PREFIX=${TARGET_KERNEL_CLANG_PATH}/bin/arm-linux-gnueabi-
-export TARGET_KERNEL_LLVM_BINUTILS=false
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export BUILD_USERNAME=OmanshKrishn
 export BUILD_HOSTNAME=debian
 export KBUILD_BUILD_USER=OmanshKrishn
 export KBUILD_BUILD_HOST=debian
 export ALLOW_MISSING_DEPENDENCIES=true
-export TZ=Asia/Kolkata #put before last build command
+export TZ=Asia/Kolkata
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
