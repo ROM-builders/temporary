178df37b542e0e7ab521d2add18d30e7df7f7274
Omansh Krishn <96343317+omansh-krishn@users.noreply.github.com>
2023-07-01 16:50:37 +0530

    check

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,6 @@ repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manif
 git clone https://codeberg.org/omansh-krishn/local_manifest.git --depth 1 -b pixelexperience-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 # build rom
-
 source build/envsetup.sh
 lunch aosp_santoni-user
 export TARGET_GAPPS_ARCH=arm64
