0112a2646e67f0b0058dc2b1e3a96ee12b1a3f00
cruden <98847288+ZewOn31@users.noreply.github.com>
2023-07-02 11:13:02 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init -u https://github.com/CherishOS/android_manifest.git -b tiramisu
+repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
 git clone https://github.com/ZewOn31/local_manifest.git --depth 1 -b master .repo/local_manifests
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
 lunch cherish_veux-userdebug
-export TZ=Europe/Istanbul #put before last build command
+export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
