c995b7ce0baf6001bc6316a6bf097e65270ff20f
alecchan02 <132892224+alecchan02@users.noreply.github.com>
2023-07-02 09:08:24 +0800

    Final build

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch xtended_monet-eng
+lunch xtended_monet-userdebug
 export TZ=Asia/Dhaka #put before last build command
 make xtended
 # 3
