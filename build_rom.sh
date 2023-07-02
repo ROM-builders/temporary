9687759e30a6823001652b938afb831dce98d61b
Tanmsyffa <sltnmsyffaa@gmail.com>
2023-07-02 16:58:28 +0700

    fog: Fix incorrect lunch line

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch fog_yaap-userdebug
+lunch yaap_fog-userdebug
 export TZ=Asia/Dhaka #put before last build command
 export TARGET_BUILD_GAPPS=true
 m yaap
