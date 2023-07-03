bd6bb581b5da7c16af389648a991f322fc3449a1
Enprytna <e.endipriyatna@gmail.com>
2023-07-04 02:19:53 +0700

    Initial DerpFest-AOSP for ginkgo

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/Enprytna/local_manifests.git --depth 1 -b DerpFest-
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+. build/envsetup.sh
 lunch derp_ginkgo-userdebug
 export TZ=Asia/Jakarta #put before last build command
 mka derp
