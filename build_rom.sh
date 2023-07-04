29c1c96d238a51374218c190efeb2d13e8634df5
YARA <122050843+Yara0202@users.noreply.github.com>
2023-07-03 23:32:02 -0700

    X01AD-DerpFest-12-12.1-Yara0202

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest -b 12.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-12/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Yara0202/local_manifest.git --depth 1 -b derp-12.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch derp_X01AD-userdebug
-export TZ=Asia/Jakarta #put before last build command
+export TZ=asia/Jakarta #put before last build command
 mka derp
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
