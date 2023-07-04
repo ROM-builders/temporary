03702d2ac01bab37cb58b7a7ba15f0f94d603bb0
Salih <91677076+DeliUstaTR@users.noreply.github.com>
2023-07-04 10:27:20 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch cherish_ginkgo-userdebug
+ALLOW_MISSING_DEPENDENCIES=true
 export CHERISH_VANILLA=true
 export TZ=Asia/Dhaka #put before last build command
 make bacon
