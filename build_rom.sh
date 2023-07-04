d8ed1812084bb687e84898357bb1047077ebb42a
Burhan Siddiquee <127223292+Burhanverse@users.noreply.github.com>
2023-07-04 12:59:31 +0530

    Test after nuked radio

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom test4
 source build/envsetup.sh
 lunch blaze_lancelot-userdebug
+
+export ALLOW_MISSING_DEPENDENCIES=true
 export BUILD_USERNAME=burhan 
 export BUILD_HOSTNAME=burhan 
 export TZ=Asia/Kolkata #put before last build command
