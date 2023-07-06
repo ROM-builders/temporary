9a67aa5ca82b76545ed7e6d2e43262de959ac635
l u n a i r e <103412573+Seraphine3@users.noreply.github.com>
2023-07-06 15:29:54 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Seraphine3/sdm845-manifests.git --depth 1 -b cherish .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 -j1 --fail-fast
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j1 --fail-fast
 
 # build rom
 source build/envsetup.sh
