ed02f32cd7b4e3c292ac4cd52dc4fce5717bf13c
Priya Ranjan Samal <138547877+Priyaranjansamal@users.noreply.github.com>
2023-07-07 23:02:18 +0530

    Test #2

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_anifest.git -b triton -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest.git -b triton -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Priyaranjansamal/local_manifest.git --depth 1 -b awaken .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
