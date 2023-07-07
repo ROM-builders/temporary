be61ee94f163d14edda90541bbfe65d413a0e027
Just your love <76810719+itsmeNaraa@users.noreply.github.com>
2023-07-07 10:00:02 +0800

    Alpha

--- build_rom.sh
+++ build_rom.sh
@@ -1,18 +1,17 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://github.com/itsmeNaraa/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/alphadroid-project/manifest -b alpha-13 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/nullptr03/local_manifest.git --depth 1 -b alpha_mstn .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch evolution_moonstone-userdebug
-export KBUILD_BUILD_USER=narachan
-export KBUILD_BUILD_HOST=narachan 
-export BUILD_USERNAME=narachan
-export BUILD_HOSTNAME=narachan
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
-export TZ=Asia/Jakarta #put before last build command
-mka evolution
+lunch lineage_moonstone-userdebug
+export BUILD_USERNAME=Andy
+export BUILD_HOSTNAME=Andy
+export KBUILD_USERNAME=Andy
+export KBUILD_HOSTNAME=Andy
+export TZ=Asia/Makassar
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
