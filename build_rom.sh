f4d871e1552ea4ba1e4edcc368f4231808107d27
emorafsan <138754767+emorafsan@users.noreply.github.com>
2023-07-08 10:06:03 +0600

    initial build

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch banana_mojito-userdebug
-export TZ=Asia/Jakarta #put before last build command
+export TZ=Asia/Dhaka #put before last build command
 m banana
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
