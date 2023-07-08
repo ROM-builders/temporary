4a026b9ec80c12c3ae2db9f25c3427d926f78b4e
alecchan02 <132892224+alecchan02@users.noreply.github.com>
2023-07-09 01:26:11 +0800

    remove duplicated

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch xtended_haydn-userdebug
+lunch xtended_haydn-eng
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export BUILD_USER=alecchangod
 export BUILD_USERNAME=alecchangod
