300b4508e233d4a2217774cac6fa1383b84110f1
Rishik-AKA-TPSRISHIKop <rishikrvr@gmail.com>
2023-07-08 13:05:53 +0530

    star2lte-SuperiorOS-AOSP-thirteen-Rishik-AKA-TPSRISHIKop

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 -u https://github.com/PixelOS-AOSP/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
+repo init --depth=1 -u https://github.com/SuperiorOS/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Rishik-AKA-TPSRISHIKop/local_manifest -b main .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --optimized-fetch
 
 # build rom
 source build/envsetup.sh
-lunch aosp_star2lte-userdebug
+lunch superior_star2lte-userdebug
 export TZ=Asia/Dhaka 
 # put before last build command
 mka bacon
