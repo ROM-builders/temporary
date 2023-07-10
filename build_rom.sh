1f1ae35734cbbabb3a092a8f550182ca1179248c
Koushik Dey <kousickdey3@gmail.com>
2023-07-11 00:35:44 +0530

    OctaviOS Bringup
    Signed-off-by: Koushik Dey <kousickdey3@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Koushikdey2003/local_manifest.git --depth 1 -b Evolution-X .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Octavi-Staging/android_manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Koushikdey2003/local_manifest.git --depth 1 -b octavi .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch evolution_RMX2020-user
+lunch octavi_RMX2020-user
 export TZ=Asia/Kolkata #put before last build command
-mka evolution
+brunch RMX2020
 cat */*/*/RMX2020/*.zip.json
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
