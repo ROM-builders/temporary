1545a4e3c39cba602734a0650ccb630f3863ad76
Pranav <77088825+pranavjha21@users.noreply.github.com>
2023-07-11 13:25:04 +0530

    Ok

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest.git -b triton -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Priyaranjansamal/local_manifest.git --depth 1 -b awaken .repo/local_manifests
+git clone https://github.com/eartinity/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
