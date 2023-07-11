11891055b8c0f51b1f377888e15bc30198a854d2
TheTester87 <136893589+TheTester87@users.noreply.github.com>
2023-07-11 16:20:45 +0530

    gauguin-Evolution-X-tiramisu-Niyush-04

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,7 @@
 repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
 git clone https://github.com/TheTester87/local_manifests --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
+#Hello Dev
 # build rom
 source build/envsetup.sh
 lunch evolution_gauguin-userdebug
