c1b089708dcde88aba0a7c20f408094dcd9fb193
Krishna Sonawane <68546246+mrkrishnasonawane@users.noreply.github.com>
2023-07-11 09:58:36 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -14,3 +14,4 @@ cat */*/*/RMX2020/*.zip.json
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
 rclone copy out/target/product/RMX2020/RMX2020.json cirrus:RMX2020 -P
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/recovery.img cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+ 
