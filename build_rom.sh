692f76dfe46d1177beb786394b2348f78e71cb84
z3zens <78209717+z3zens@users.noreply.github.com>
2023-07-12 00:36:12 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,17 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest.git -b triton -g default,-mips,-darwin,-notdefault
-git clone https://github.com/z3zens/local_manifest.git --depth 1 -b triton-awaken .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/yaap/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
+git clone https://github.com/z3zens/local_manifest.git --depth 1 -b yaap .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom 
 source build/envsetup.sh
-lunch awaken_X01BD-userdebug
+lunch yaap_X01BD-userdebug
 export BUILD_USERNAME=nobody
 export BUILD_HOSTNAME=android-build
 export KBUILD_BUILD_USER=nobody
 export KBUILD_BUILD_HOST=android-build
 export TZ=Asia/Jakarta #put before last build command 
-make bacon
-
-# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
-rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+m yaap
