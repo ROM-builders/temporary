6913004fa8ec4d50f3725a84e1a4f84271906233
Ahmed gamal <87333939+AhmedGamal09@users.noreply.github.com>
2023-07-11 07:44:54 +0300

    xtended: test fix

--- build_rom.sh
+++ build_rom.sh
@@ -3,9 +3,14 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Project-Xtended/manif
 git clone https://github.com/AhmedGamal09/local_manifest.git --depth 1 -b xtended .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom 17
+# build rom 18
 source build/envsetup.sh
 lunch xtended_mojito-userdebug
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+export BUILD_USER=Ahmed_Gamal
+export BUILD_USERNAME=Ahmed_Gamal
+export KBUILD_BUILD_USER=Ahmed_Gamal
+export KBUILD_BUILD_USERNAME=Ahmed_Gamal
 export TZ=Asia/Dhaka #put before last build command
 make xtended
 
