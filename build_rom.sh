e23aabe8ac06bbb646d3c7c661d713b5c6eff6e1
z3zens <78209717+z3zens@users.noreply.github.com>
2023-07-12 20:57:13 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,4 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/yaap/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest.git -b triton -g default,-mips,-darwin,-notdefault
 git clone https://github.com/z3zens/local_manifest.git --depth 1 -b yaap .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
-# build rom 
-source build/envsetup.sh
-lunch yaap_X01BD-userdebug
-export BUILD_USERNAME=nobody
-export BUILD_HOSTNAME=android-build
-export KBUILD_BUILD_USER=nobody
-export KBUILD_BUILD_HOST=android-build
-export TZ=Asia/Jakarta #put before last build command 
-m yaap
