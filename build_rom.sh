83ef5ead63b8c89c191d1a9a3134425396bd7370
Andy | アンディ <54360298+nullptr03@users.noreply.github.com>
2023-07-13 15:25:28 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,17 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-AOSP/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/nullptr03/local_manifest.git --depth 1 -b pisol .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b thirteen -g default,-mips,-darwin,-notdefault
+git clone https://github.com/nullptr03/local_manifest.git --depth 1 -b pe .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch bootleg_merlinx-userdebug
+lunch aosp_merlinx-userdebug
 export BUILD_USERNAME=Andy
 export BUILD_HOSTNAME=Andy
+export KBUILD_USERNAME=Andy
+export KBUILD_HOSTNAME=Andy
 export TZ=Asia/Makassar
-export WITH_GAPPS=2
-export SELINUX_IGNORE_NEVERALLOWS=true
+export WITH_GAPPS=false
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
