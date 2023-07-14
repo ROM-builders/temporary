203a5baa4d7bb56f7b669a58b23acea3f62b6abc
Rom Bdr. Gharti <romgharti@gmail.com>
2023-07-14 13:33:09 +0545

    superior os

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/BootleggersROM/manifest.git -b tirimbino -g default,-mips,-darwin,-notdefault
-git clone https://github.com/romgharti/local_manifest.git --depth 1 -b btlg-13.0 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
+git clone https://github.com/romgharti/local_manifest.git --depth 1 -b superior-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch bootleg_mojito-user
-touch device/xiaomi/mojito/Boardconfig.mk
-export WITH_GAPPS=true
-export DEVICE_MAINTAINERS=Rom\ Bdr.\ Gharti
+lunch superior_mojito-user
+# export WITH_GAPPS=true
+# export DEVICE_MAINTAINERS=Rom\ Bdr.\ Gharti
 export TZ=Asia/Dhaka #put before last build command
-mka bacon
+m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
