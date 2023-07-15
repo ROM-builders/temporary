01c30178054bc9f96d38167c0518c17c25f1803c
Pradip Maurya <116277265+Niyush-04@users.noreply.github.com>
2023-07-15 13:38:35 +0530

    Fixed

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
-# sync evo
+# sync
 repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
 git clone https://github.com/TheTester87/local_manifests --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build evo
+# build
 source build/envsetup.sh
 lunch evolution_gauguin-userdebug
 export TZ=Asia/Kolkata #put before last build command
