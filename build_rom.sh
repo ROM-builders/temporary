0922f948b119340e49b5d4c482afbd7b62d9b092
شَکِیْب | Shakib <52566642+Shakib-BD@users.noreply.github.com>
2023-07-15 09:00:36 +0600

    reworked build_rom.sh for octavi-thirteen

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 13 --git-lfs -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Octavi-Staging/manifest.git -b thirteen --git-lfs -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Shakib-BD/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch lineage_merlinx-user
+lunch octavi_merlinx-user
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
