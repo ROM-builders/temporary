6b1c9f5d6a16467b3f9526147826ee8e6d6b63d6
Burhan Siddiquee <127223292+Burhanverse@users.noreply.github.com>
2023-07-15 22:00:51 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom gay dependencies 
 source build/envsetup.sh
 lunch blaze_lancelot-userdebug
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+
 export BUILD_USERNAME=burhan 
 export BUILD_HOSTNAME=burhan 
 export TZ=Asia/Kolkata #put before last build command
