08be1a73ddec94a14ab965bb30c5ef6b12ea819c
dogpoopy <103020425+dogpoopy@users.noreply.github.com>
2023-07-16 13:13:27 +0800

    build: Allow missing dependencies

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,9 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch evolution_vayu-userdebug
-export TZ=Asia/Dhaka #put before last build command
+
+export ALLOW_MISSING_DEPENDENCIES=true
+export TZ=Asia/Manila #put before last build command
 mka evolution
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
