57771f96b9d7307cb9041e9448ade3d9a09d3c21
dinonomous <133356394+dinonomous@users.noreply.github.com>
2023-07-18 09:49:38 +0530

    yogurt-LineageOS-lineage-20.0-dinonomous

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_yogurt-user
-export TZ=Asia/Dhaka #put before last build command
+export TZ=Asia/Dhaka #put before last build command.
 mka lineage
 cat */*/*/yogurt/*.zip.json
 
