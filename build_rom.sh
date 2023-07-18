aa6a4cfac39e270764d84c10873fbebeb40ff047
YARA <122050843+Yara0202@users.noreply.github.com>
2023-07-18 19:50:39 +0700

    X01AD-SuperiorOS-twelvedotone-Yara0202

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.git -b twelvedotone -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.git -b twelvedotone -g default,-mips,-darwin,-notdefault --git-lfs
 git clone https://github.com/Yara0202/local_manifest.git --depth 1 -b superior-12.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
