2b6ea6c31f8c2bc143a5a2c6892aeb399bf9558d
Dooms <vmbuildforme@gmail.com>
2023-07-19 17:35:27 +0530

    fix settings crash

--- build_rom.sh
+++ build_rom.sh
@@ -3,6 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/androi
 git clone https://github.com/Krtonia/local_manifest.git --depth 1 -b awaken .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+
 # build rom
 source build/envsetup.sh
 lunch awaken_beryllium-user
