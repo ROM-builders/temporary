b256d1594bdcd657e75b4d1c414fbe20823cec7a
شَکِیْب | Shakib <52566642+Shakib-BD@users.noreply.github.com>
2023-07-19 12:20:37 +0600

    Update build_rom.sh for change local_manifest directory

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 13 --git-lfs -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Shakib-BD/local_manifest.git --depth 1 -b banana .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Shakib-BD/local_manifest.git --depth 1 -b banana-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
-. build/envsetup.sh
-lunch banana_merlinx-user
-export ALLOW_MISSING_DEPENDENCIES=true
-export BUILD_USERNAME=Shakib
-export BUILD_HOSTNAME=Shakib
-export TZ=Asia/Dhaka #putt before last build command
-mka banana
+# build rom 2
+source build/envsetup.sh
+lunch banana_mojito-userdebug
+export TZ=Asia/Dhaka #put before last build command
+m banana
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
