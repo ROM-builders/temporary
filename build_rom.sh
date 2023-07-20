46e96a70bdd7abb23e5faba280569882c73489f6
xenxynon <flynryder427@gmail.com>
2023-07-20 23:35:24 +0530

    start awaken again

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/androi
 git clone https://github.com/SKetU-l/local_manifest --depth=1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom #2
+# build rom #3
 source build/envsetup.sh
 lunch awaken_X00TD-userdebug
 export TZ=Asia/Kolkata #put before last build command
