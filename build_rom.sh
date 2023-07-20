fa11c6926c27b686d34e5d8e51020b23826361a6
Knotzy - 01 <70868798+angelomds42@users.noreply.github.com>
2023-07-20 01:55:59 -0300

    Devon Arrow os fix

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init -u https://github.com/ArrowOS/android_manifest.git -b arrow-13.1 -g default,-mips,-darwin,-notdefault
+repo init -u https://github.com/ArrowOS/android_manifest.git -b arrow-13.1 
 git clone https://github.com/angelomds42/local_manifest --depth 1 -b arrow .repo/local_manifests
 repo sync --no-tags -c -j$(nproc --all) --force-sync --no-clone-bundle
 
