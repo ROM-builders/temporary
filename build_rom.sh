ecd4ce61aca4fe5e849868174da8aa0eed95741a
dogpoopy <qtqtron@gmail.com>
2023-07-21 04:37:52 +0800

    build: script: set kernel name before compiling

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,8 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
 git clone https://github.com/dogpoopy/local_manifest.git --depth 1 -b rom-builder .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
+# set kernel name
+sed -i "s/CONFIG_LOCALVERSION=.*/CONFIG_LOCALVERSION="-coup-de-grace"/g" $(shell pwd)/kernel/xiaomi/vayu/arch/arm64/configs/vayu_user_defconfig
 # build rom
 source build/envsetup.sh
 lunch evolution_vayu-user
