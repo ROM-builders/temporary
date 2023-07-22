0ced4130c5290f6d67393f4433ae2b18b71312a2
nosebs <78350356+nosebs@users.noreply.github.com>
2023-07-22 22:28:55 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 repo init --depth=1 --no-repo-verify -u git://https://github.com/Miku-UI/manifesto.git -b TDA -g default,-mips,-darwin,-notdefault
-git clone https://github.com/dandelion64-Archives/local_manifests.git --depth 1 -b lineage-20 .repo/local_manifests
+git clone https://github.com/nosebs/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
