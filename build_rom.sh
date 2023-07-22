2e3a925fc3218de29750d40983819c04695dfb92
nosebs <78350356+nosebs@users.noreply.github.com>
2023-07-22 22:16:31 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,1 +1,9 @@
-curl -sL https://raw.githubusercontent.com/nosebs/treble_build_miku/TDA/build.sh | bash
+repo init --depth=1 --no-repo-verify -u git://https://github.com/Miku-UI/manifesto.git -b TDA -g default,-mips,-darwin,-notdefault
+git clone https://github.com/dandelion64-Archives/local_manifests.git --depth 1 -b lineage-20 .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
+# build rom
+source build/envsetup.sh
+lunch miku_blossom-user
+export TZ=Asia/Dhaka #put before last build command
+make diva
