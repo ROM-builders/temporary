eb4a20503316a5b5f22b5554fc73e28c3e0ea023
Amit152505 <134253885+Amit152505@users.noreply.github.com>
2023-07-22 22:57:25 +0530

    lisa-crDroid-Amit152505

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,11 @@
 # sync rom
-repo init -u https://github.com/crdroidandroid/android.git -b 13.0 --git-lfs
+repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 13.0 --git-lfs -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Amit152505/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch lineage_lisa-userdebug
-export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
