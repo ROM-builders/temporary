95cfe1d551f16cec69550c67e2a290815cbd9d84
Knotzy - 01 <70868798+angelomds42@users.noreply.github.com>
2023-07-22 21:36:54 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-13.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/angelomds42/local_manifest --depth 1 -b arrow .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-AOSP/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
+git clone https://github.com/angelomds42/local_manifest --depth 1 -b pixelos .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch arrow_devon-userdebug
-m bacon
+lunch aosp_devon-userdebug
+export TZ=Asia/Dhaka #put before last build command
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
