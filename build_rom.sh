8ce25b717f269ce965b5eac282a873e40bb9a57f
sumon <82557471+mdsumon2001@users.noreply.github.com>
2023-07-23 11:15:21 +0600

    Update build_rom.sh changed lunch to userdebug

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 export TZ=Asia/Dhaka #put before last build command
-lunch lineage_RMX2185-user
+lunch lineage_RMX2185-userdebug
 make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
