aabe5cc504ec6be4d5055d8107d0d3a541a3c4ac
Rishik-AKA-TPSRISHIKop <rishikrvr@gmail.com>
2023-07-24 23:24:42 +0530

    star2lte-SuperiorOS-AOSP-thirteen-xxrishikcooTR

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 -u https://github.com/SuperiorOS/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Rishik-AKA-TPSRISHIKop/local_manifest -b main .repo/local_manifests
+git clone https://github.com/Rishik-AKA-TPSRISHIKop/local_manifests-V2.git -b main .repo/local_manifests
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --optimized-fetch
 
 # build rom
@@ -9,7 +9,7 @@ lunch superior_star2lte-userdebug
 export TZ=Asia/Dhaka 
 # put before last build command
 mka bacon
-# update manifest 
+
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
 # Nothing
