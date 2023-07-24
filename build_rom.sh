72e68c52be37d17f33da3eca1779a3ed56dae479
شَکِیْب | Shakib <52566642+Shakib-BD@users.noreply.github.com>
2023-07-24 12:45:56 +0600

    merlinx: add support for arrow 13.1

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 13 --git-lfs -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Shakib-BD/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-13.1 --git-lfs -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Shakib-BD/local_manifest.git --depth 1 -b arrow-13.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch lineage_merlinx-user
+lunch arrow_merlinx-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
