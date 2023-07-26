894dea85e7cd3c9517af516d47e3b2ca675abe7e
RifatHossaN <104590425+RifatHossaN@users.noreply.github.com>
2023-07-27 05:15:59 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,8 +1,8 @@
 # build count = 5 
  
 # sync rom 
-repo init --depth=1 --no-repo-verify -u https://github.com/p-404/android_manifest -b tokui -g default,-mips,-darwin,-notdefault
-git clone https://github.com/rifathossan/local_manifest.git --depth 1 -b p404-tokui .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/P-404/android_manifest -b tokui -g default,-mips,-darwin,-notdefault
+git clone https://github.com/RifatHossan/local_manifest.git --depth 1 -b p404-tokui .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
