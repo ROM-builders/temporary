4deb36ea19aa135e73409083bc2a624193abb781
cbendot <r.budhiono@gmail.com>
2023-07-26 04:58:46 +0000

    Initial X00TD

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 11.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/cbendot/Local-Manifests.git --depth 1 -b cr11 .repo/local_manifests
+git clone https://github.com/mkik-fds/Local-Manifests.git --depth 1 -b cr11 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
