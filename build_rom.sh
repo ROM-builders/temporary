efa898f1b1eab1368bdf964de17f1e4cab5cbb2c
Rishik-AKA-TPSRISHIKop <rishikrvr@gmail.com>
2023-07-27 07:33:25 +0530

    star2lte-SuperiorOS-thirteen-xxrishikcooTR

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 -u https://github.com/SuperiorOS/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Rishik-AKA-TPSRISHIKop/local_manifests-V2.git -b main .repo/local_manifests
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --optimized-fetch
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
