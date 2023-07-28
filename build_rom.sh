aaba9e26474470f49aca0be91900c0ac3b9b8073
swapnil vicky <74442290+SwapnilVicky@users.noreply.github.com>
2023-07-28 14:13:55 +0530

    retry with --git-lfs

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/BlissRoms/platform_manifest.git -b typhoon -g default,-mips,-darwin,-notdefault --git-lfs
+repo init --depth=1 --git-lfs --no-repo-verify -u https://github.com/BlissRoms/platform_manifest.git -b typhoon -g default,-mips,-darwin,-notdefault 
 git clone https://github.com/SwapnilVicky/local_manifest.git --depth 1 -b typhoon .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
