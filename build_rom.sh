98e7f3620336599e98f8b1ba030aed6991411a97
Fleurmiel <136577141+Fleurmiel@users.noreply.github.com>
2023-07-30 12:20:40 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,11 +5,11 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch xtended_fleur-userdebug
+lunch lineage_fleur-userdebug
+export BUILD_HOSTNAME=Chas
+export BUILD_USERNAME=CodeChas3r
 export TZ=Asia/Jakarta #put before last build command
-export ALLOW_MISSING_DEPENDENCIES=ALLOW
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
-mka bacon
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
