95ee2a4a608d79cae918f063c7cfd9866b8ab7c2
Matheus Souza <33396386+matheucomth@users.noreply.github.com>
2023-07-30 19:08:23 -0300

    Initial crdroid 13 for onclite

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync roms
-repo init --depth=1 --no-repo-verify -u https://github.com/alphadroid-project/manifest -b alpha-13 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 13.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/matheucomth/local_manifest.git --depth 1 -b alpha .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
