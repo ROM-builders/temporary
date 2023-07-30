1e2c235d8c48edeba1a3d8712c1c04b6c1a6a235
Amrito Karmokar <amritokarmokar5i@gmail.com>
2023-07-30 11:10:28 +0600

    Test latest blobs

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Amritorock/local_manifest.git --depth 1 -b Pe .repo/local_manifests
+git clone https://github.com/Amritorock/local_manifest.git --depth 1 -b pos .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom 
 source build/envsetup.sh
 lunch aosp_r5x-userdebug 
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export TZ=Asia/Dhaka #put before last build command
 make bacon 
 
