21bcfa4347e6deb4bb79b1e3a5eb734d6fad4807
omanshkrishn <138091069+omanshkrishn@users.noreply.github.com>
2023-07-30 18:15:40 +0530

    trigger build
    it might fail

--- build_rom.sh
+++ build_rom.sh
@@ -3,6 +3,7 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/xdroid-oss/xd_manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://codeberg.org/omansh-krishn/local_manifest.git --depth 1 -b xdroid-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
 # build rom
 source build/envsetup.sh
 lunch xdroid_santoni-user
