20ce35bb088681363c9f96cf22b136ae3e67a71e
Abhinav Kumar <122148266+acex69@users.noreply.github.com>
2023-07-30 21:53:01 +0530

    Retry arrow

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,7 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-13.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/acex69/local_manifest --depth 1 -b arrow .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
+ 
 # build rom
 . build/envsetup.sh
 lunch arrow_lancelot-userdebug
