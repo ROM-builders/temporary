ba66389ff797e37ced1f6bb00e9d041c0fb3ba2c
Links234 <139690836+ERLinks@users.noreply.github.com>
2023-08-01 00:30:40 +0300

    sweet-PixelExperience-thirteen-plus-ERLinks

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/ERLinks/local_manifest.git --depth 1 -b main .repo/
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+. build/envsetup.sh
 lunch aosp_sweet-user
 mka bacon
 
