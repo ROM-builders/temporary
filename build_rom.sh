bd9d6adc193b299804eac9cc89470e57ef6141f0
emorafsan <138754767+emorafsan@users.noreply.github.com>
2023-08-01 05:23:37 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,12 +5,11 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch derp_merlinx-user
-export SELINUX_IGNORE_NEVERALLOWS=true
-export BUILD_USERNAME=emorafsan
-export BUILD_HOSTNAME=emorafsan
+lunch derp_merlinx-userdebug
+export BUILD_USERNAME=Andy
+export BUILD_HOSTNAME=Andy
 export TZ=Asia/Dhaka
-mka derp
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
