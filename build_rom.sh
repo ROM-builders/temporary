d778c59f5059245aa25c12dd608f91aeb336a72a
Links234 <139690836+ERLinks@users.noreply.github.com>
2023-07-31 23:20:00 +0300

    sweet-PixelExperience-ERLinks
    Build PixelExperience for sweet

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,12 @@
 # sync rom
-repo init -u https://github.com/PixelExperience/manifest -b thirteen-plus
+repo init --depth=1 --no-repo-verify -u git://github.com/PixelExperience/manifest.git -b thirteen-plus -g default,-mips,-darwin,-notdefault 
 git clone https://github.com/ERLinks/local_manifest.git --depth 1 -b master .repo/local_manifests
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
 lunch aosp_sweet-user
+export TZ=Asia/Dhaka #put before last build command
 mka bacon -j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
