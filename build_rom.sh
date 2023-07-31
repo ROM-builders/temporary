01aa9f2abe594ba587f18d50fbeecff63dc4b857
Dark angels <95138156+darkangels15@users.noreply.github.com>
2023-07-31 19:54:50 +0700

    Jasmine_sprout-RisingOs-13-Darkangelss

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,19 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/RisingTechOSS/android -b thirteen -g default,-mips,-darwin,-notdefault
+git clone https://github.com/darkangels15/Local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
-export TZ=Asia/Dhaka #put before last build command
-mka derp
+brunch jasmine_sprout-userdebug
+lunch lineage_jasmine_sprout-userdebug
+WITH_GMS=true
+RISING_CHIPSET=sdm660
+RISING_MAINTAINER=dArkAngelS
+TARGET_ENABLE_BLUR=true
+RISING_PACKAGE_TYPE=AOSP
+export TZ=Asia/Jakarta #put before last build command
+mka bacon -jX
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
