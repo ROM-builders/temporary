e09a951102156a474b24fd59f783aabfd4b7f819
Dinesh <dinesh2742004@gmail.com>
2023-08-01 20:15:10 +0530

    santoni-alphadroid-project-alpha-13-DineshMSDian

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b pyro -g default,-mips,-darwin,-notdefault 
-git clone https://github.com/DineshMSDian/local_manifest --depth 1 -b Spark .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/alphadroid-project/manifest -b alpha-13 -g default,-mips,-darwin,-notdefault 
+git clone https://github.com/DineshMSDian/local_manifest --depth 1 -b AlphaDroid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -12,8 +12,8 @@ export BUILD_HOSTNAME=DB-lab
 export KBUILD_BUILD_USER=DineshMSDian
 export KBUILD_BUILD_HOST=DB-lab
 export TZ=Asia/Chennai #put before last build command
-lunch spark_santoni-userdebug
-mka bacon
+lunch lineage_santoni-userdebug
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line )
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
