a002b1e18f80f8a0c25f366bd775bdd882f1783a
Sundram <101039005+xyz-sundram@users.noreply.github.com>
2023-08-01 10:40:13 +0530

    try again

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 12.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 12.1 -g default,-mips,-darwin,-notdefault --git-lfs
 git clone https://github.com/xyz-sundram/local_manifest.git --depth 1 -b SM-M30S .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
