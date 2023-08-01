0359bbbbba0536e95e36eee4405a9d58b97d010d
Le Hong Duc <hongducthbk123@gmail.com>
2023-08-01 16:43:39 +0700

    build broken dup rule

--- build_rom.sh
+++ build_rom.sh
@@ -4,10 +4,12 @@ git clone https://github.com/tedomi2705/local_manifest.git --depth=1 -b tiramisu
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
+export BUILD_BROKEN_DUP_RULES := true
 source build/envsetup.sh
 lunch evolution_tulip-user
 export TZ=Asia/Dhaka #put before last build command
 mka evolution
+cat */*/*/tulip/*.zip.json
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
