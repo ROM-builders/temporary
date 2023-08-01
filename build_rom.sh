f877a29640a2e7b09e999bc225d216ed254fca4b
nosebs <78350356+nosebs@users.noreply.github.com>
2023-08-01 14:53:27 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync romлl
 repo init --depth=1 --no-repo-verify -u https://github.com/alphadroid-project/manifest -b alpha-13 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/nosebs/local_manifest --depth 1 -b main .repo/local_manifests
+git clone https://github.com/dandelion64-Archives/local_manifests --depth 1 -b lineage-20 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
