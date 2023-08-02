abc849f5f83e0e04336298f036e941df8ad4d197
Tejas Singh <tejas@m.ms.evolution-x.org>
2023-08-03 00:51:29 +0530

    back to 20

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-17.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Testing-Stuffs/Local_Manifest.git --depth 1 -b los-17.1 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Testing-Stuffs/Local_Manifest.git --depth 1 -b lineage .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build
