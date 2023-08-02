8b2b4155a2196ba40ece87063e13b1f525328f37
ShenRn <129452476+shenprjkt@users.noreply.github.com>
2023-08-02 07:20:14 +0700

    Vince-MikuUI-ShenRn
    Build MikuUI for vince

--- build_rom.sh
+++ build_rom.sh
@@ -9,7 +9,7 @@ lunch miku_vince-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Jakarta #put before last build command
-mka derp
+mka miku
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
