0ac5f26a329d9bf6244a15bb0d8b926ae460ed5d
ShenRn <129452476+shenprjkt@users.noreply.github.com>
2023-08-02 00:20:03 -0700

    vince-PixelExperience-thirteen-plus-shen

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,8 @@
+# sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest.git -b thirteen-plus -g default,-mips,-darwin,-notdefault
 git clone https://github.com/shenprjkt/local_manifest.git --depth 1 -b pixelexperience-13-t .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
 # build rom
 source build/envsetup.sh
 lunch aosp_vince-user
