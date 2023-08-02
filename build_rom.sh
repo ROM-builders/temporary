95c63f2ac664af830e7ba57e2a15764864f04832
ShenRn <129452476+shenprjkt@users.noreply.github.com>
2023-08-03 01:19:18 +0700

    Fix Sync

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b thirteen-plus -g default,-mips,-darwin,-notdefault
-git clone https://github.com/shenprjkt/local_manifest.git --depth 1 -b pixelexperience-13.0-t .repo/local_manifests
+git clone https://github.com/shenprjkt/local_manifest.git --depth 1 -b pixelexperience-13-t .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom [61]
