ae3a3991cdb3c86c8ff2c8c85cfaf0978e84168f
ajaysinghsati <125855251+ajaysinghsati@users.noreply.github.com>
2023-08-02 11:38:38 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,16 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b thirteen -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs -g default,-mips,-darwin,-notdefault
 git clone https://github.com/ajaysinghsati/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_holi-userdebug
+lunch lineage_CPH2381-userdebug
+export KBUILD_BUILD_USER=ajaysingh
+export KBUILD_BUILD_HOST=ajayphhgsi
+export BUILD_USERNAME=ajaysingh
+export BUILD_HOSTNAME=ajayphhgsi
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
