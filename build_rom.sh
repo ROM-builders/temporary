fe01eb64d3eefb9b20ed9124c400daf48205b3ee
L1ghtzin <lightzin021@gmail.com>
2023-08-02 15:57:38 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_channel-user
-export TZ=America/Sao_Paulo #put before last build command
+export TZ=America/Sao_Paulo
 m bacon 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
