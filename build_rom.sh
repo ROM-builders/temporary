851a1b7a41d0639a8a2b660d8403d8dfdc3d5b04
ShenRn <129452476+shenprjkt@users.noreply.github.com>
2023-08-02 15:23:31 +0700

    Fix sync

--- build_rom.sh
+++ build_rom.sh
@@ -1,22 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest.git -b thirteen-plus -g default,-mips,-darwin,-notdefault
-git clone https://github.com/shenprjkt/local_manifest.git --depth 1 -b pixelexperience-13-t .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b thirteen-plus -g default,-mips,-darwin,-notdefault
+git clone https://github.com/shenprjkt/local_manifest.git --depth 1 -b pixelexperience-13.0-t .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom [61]
 source build/envsetup.sh
 lunch aosp_vince-user
-
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
-export ALLOW_MISSING_DEPENDENCIES=true
-export CUSTOM_BUILD_TYPE=UNOFFICIAL-
-export BUILD_USERNAME=Hoshino
-export BUILD_HOSTNAME=pc
-export KBUILD_BUILD_USER=Hoshino
-export KBUILD_BUILD_HOST=pc
-export TZ=Asia/Kolkata
-
+export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
-rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
