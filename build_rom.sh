08a721f5ec963b17dafcf1de9b835c1ea2079565
omanshkrishn <138091069+omanshkrishn@users.noreply.github.com>
2023-08-02 12:14:17 +0530

    test dt changes

--- build_rom.sh
+++ build_rom.sh
@@ -1,18 +1,19 @@
 # sync rom
 
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest.git -b thirteen-plus -g default,-mips,-darwin,-notdefault
-git clone https://codeberg.org/omansh-krishn/local_manifest.git --depth 1 -b pixelexperience-13 .repo/local_manifests
+git clone https://codeberg.org/omansh-krishn/local_manifest.git --depth 1 -b pixelexperience-13-t .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 # build rom
 source build/envsetup.sh
 lunch aosp_santoni-user
 
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+export ALLOW_MISSING_DEPENDENCIES=true
+export CUSTOM_BUILD_TYPE=UNOFFICIAL-OmanshKrishn
 export BUILD_USERNAME=OmanshKrishn
 export BUILD_HOSTNAME=pc
 export KBUILD_BUILD_USER=OmanshKrishn
 export KBUILD_BUILD_HOST=pc
-export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Kolkata
 
 mka bacon
