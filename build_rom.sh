ef5bead609ada77e1084018b96efa78eb12a152e
Salih <91677076+DeliUstaTR@users.noreply.github.com>
2023-08-03 22:35:45 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Project-Xtended/manifest.git -b xt -g default,-mips,-darwin,-notdefault
-git clone https://github.com/DeliUstaTR/local-manifest.git --depth 1 -b main .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo init --depth=1 --no-repo-verify -u https://github.com/ReloadedOS/manifest -b t -g default,-mips,-darwin,-notdefault
+git clone https://github.com/DeliUstaTR/local-manifest --depth 1 -b reloaded .repo/local_manifests
+repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune --retry-fetches=5 -j$(nproc --all)
 
 # build rom
 source build/envsetup.sh
-lunch xtended_ginkgo-userdebug
+lunch ginkgo-userdebug
 export TZ=Asia/Dhaka #put before last build command
-make xtended
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
