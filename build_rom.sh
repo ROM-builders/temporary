dd83b4b3b8dcea988ceb753cfe0f2e86df495519
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2023-08-03 16:33:08 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 
 # sync rom 
-repo init --depth=1 --no-repo-verify -u github.com/LineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/sounddrill31/local_manifests.git --depth 1 -b addison .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u github.com/LineageOS/android.git -b lineage-18.1 --git-lfs -g default,-mips,-darwin,-notdefault
+git clone https://github.com/sounddrill31/local_manifests.git --depth 1 -b addison-18.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
