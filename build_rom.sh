f7c65c0e4174ff4514d9ee2ecef8bbb9af2fe275
Abhinav Kumar <122148266+acex69@users.noreply.github.com>
2023-08-03 12:35:15 +0530

    PE Trigger build

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync Rom    
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest.git -b thirteen-plus -g default,-mips,-darwin,-notdefault
 git clone https://github.com/acex69/local_manifest.git --depth 1 -b temp .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
 # build rom                
 source build/envsetup.sh  
 lunch aosp_lancelot-userdebug  
