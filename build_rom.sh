94dd59df947a599e56f0089c7418a2cb35270ce2
Salih <91677076+DeliUstaTR@users.noreply.github.com>
2023-08-04 11:02:16 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ReloadedOS/manifest -b t -g default,-mips,-darwin,-notdefault
 git clone https://github.com/DeliUstaTR/local-manifest --depth 1 -b reloaded .repo/local_manifests
-repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune --retry-fetches=5 -j$(nproc --all)
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
