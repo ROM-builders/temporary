88d058c5a86fa43502c4a399184a1ea38baf6863
Dinesh <dinesh2742004@gmail.com>
2023-08-04 19:03:55 +0530

    santoni-alphadroid-project-alpha-13-DineshMSDian

--- build_rom.sh
+++ build_rom.sh
@@ -6,10 +6,10 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_santoni-userdebug
-export BUILD_HOSTNAME=Azure-DB
-export BUILD_USERNAME=Azure-DB
+export BUILD_HOSTNAME=Azure
+export BUILD_USERNAME=Azure
 export TZ=Asia/Chennai #put before last build command
 make bacon
 
-# upload rom (if you don't need to upload multiple files, then you don't need to edit next line )
+# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
