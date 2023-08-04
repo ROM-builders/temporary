3cf900e29da80e189e040caa1cac4d28a652cbd2
ShenRn <129452476+shenprjkt@users.noreply.github.com>
2023-08-04 21:04:36 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Havoc-OS/android_manifest.git -b eleven -g default,-mips,-darwin,-notdefault
-git clone https://github.com/shenprjkt/local_manifest.git --depth 1 -b Havoc-Kenzo .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Havoc-OS/android_manifest.git -b thirteen --git-lfs -g default,-mips,-darwin,-notdefault
+git clone https://github.com/shenprjkt/local_manifest.git --depth 1 -b Havoc .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch havoc_kenzo-userdebug
+lunch havoc_vince-userdebug
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
