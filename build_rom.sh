e89a949b485c101d4cda93b5e1ca8b014aa33cca
ShenRn <129452476+shenprjkt@users.noreply.github.com>
2023-08-04 14:02:36 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Havoc-OS-Revived/android_manifest.git -b eleven --git-lfs -g default,-mips,-darwin,-notdefault
-git clone https://github.com/shenprjkt/local_manifest.git --depth 1 -b Havoc .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-AOSP/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
+git clone https://github.com/shenprjkt/local_manifest.git --depth 1 -b PixelOS .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom [61]
 source build/envsetup.sh
-lunch havoc_vince-userdebug
+lunch aosp_vince-userdebug
+export BUILD_USERNAME=ShenRn
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
