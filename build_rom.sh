a75d1868a616a136960f8141848ca3f4081df3d5
ShenRn <129452476+shenprjkt@users.noreply.github.com>
2023-08-04 21:26:50 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,12 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/shenprjkt/local_manifest.git --depth 1 -b pixelexperiance-13-t .repo/local_manifests
+git clone https://github.com/anandhan07/local_manifest.git --depth 1 -b pixelexperience-13.0 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom [62]
 source build/envsetup.sh
 lunch aosp_vince-user
+export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
