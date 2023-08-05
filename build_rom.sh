fe76fd5257b78234d9d8356259604c59749ef973
Skynet <67794738+TasinAyon@users.noreply.github.com>
2023-08-05 11:37:15 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,18 +1,15 @@
 # sync rom
-#repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs -g default,-mips,-darwin,-notdefault
 repo init --depth=1 --no-repo-verify -u https://github.com/BlissRoms/platform_manifest.git -b typhoon -g default,-mips,-darwin,-notdefault
 git clone https://github.com/AyOn9914/local_manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-#lunch lineage_begonia-userdebug
 lunch bliss_begonia-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export RELAX_USES_LIBRARY_CHECK=true
 export TZ=Asia/Dhaka #put before last build command
-#mka bacon
 blissify -v begonia
 
 # upload rom (if you dont need to upload multiple files, then you don't need to edit next line)
