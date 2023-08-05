7796675da0cf6171f0ce659562c2e5b0cd40b4ce
Rabikishan Rauniyar <141325011+rabikishan000@users.noreply.github.com>
2023-08-05 11:00:03 +0545

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,13 +5,14 @@ repo sync -c --force-sync --no-tags --no-clone-bundle -j$(nproc --all) --optimiz
 
 
 # build rom
-. build/envsetup.sh
-lunch syberia_fleur-userdebug 
-mka syberia
 rm -rf platform/system/sepolicy/vendor/hal_light_default.te
 rm -rf platform/system/sepolicy/vendor/rild.te
 rm -rf system/sepolicy/vendor/hal_light_default.te
 rm -rf system/sepolicy/vendor/rild.te
+. build/envsetup.sh
+lunch syberia_fleur-userdebug 
+mka syberia
+
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
