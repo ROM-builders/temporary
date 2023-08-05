188b804e3e2d2f27e3694c3a4ec90065bc14d8f6
Rabikishan Rauniyar <141325011+rabikishan000@users.noreply.github.com>
2023-08-05 08:42:09 +0545

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,6 @@
 # sync rom
 repo init -u https://github.com/syberia-project/manifest.git -b 13.0
+git clone https://github.com/rabikishan000/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --force-sync --no-tags --no-clone-bundle -j$(nproc --all) --optimized-fetch --prune
 
 # build rom
