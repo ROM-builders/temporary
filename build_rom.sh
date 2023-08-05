d6d5491fd2e6f95b36c765e20e3180b4327139ed
ShenRn <129452476+shenprjkt@users.noreply.github.com>
2023-08-05 10:39:59 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b thirteen-plus -g default,-mips,-darwin,-notdefault
-git clone https://github.com/shenprjkt/local_manifest.git --depth 1 -b pixelexperience-13-t .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/BootleggersROM/manifest.git -b tirimbino -g default,-mips,-darwin,-notdefault
+git clone https://github.com/shenprjkt/local_manifest.git --depth 1 -b bootleg .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_vince-user
+lunch bootleg_vince-user
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
