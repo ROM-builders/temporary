32718edb18148b5ef10abfa591419a42c19d5a95
noobprjkt <94937447+bronya30@users.noreply.github.com>
2023-08-05 18:24:25 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/alphadroid-project/ma
 git clone https://github.com/bronya30/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom [61]
+# build rom [62]
 source build/envsetup.sh
 # export TARGET_WITH_GAPPS=false
 lunch lineage_rosy-userdebug
