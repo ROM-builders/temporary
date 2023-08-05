591559c17a9df52ba49f763dfdf16a68439d1f06
nabiraja <62164079+nobiraja@users.noreply.github.com>
2023-08-05 13:32:25 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs -g default,-mips,-darwin,-notdefault
-git clone https://github.com/nobiraja/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b thirteen -g default,-mips,-darwin,-notdefault
+git clone https://github.com/nobiraja/local_manifest.git --depth 1 -b pixel-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_rasgulla-userdebug
+lunch aosp_rasgulla-userdebug
 export KBUILD_BUILD_USER=nobiraja
 export KBUILD_BUILD_HOST=nobiraja
 export BUILD_USERNAME=nobiraja
