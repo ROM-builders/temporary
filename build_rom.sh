ed04ebc20b7be6c7cfb705958ad15e27fd279b31
Matheus Souza <33396386+matheucomth@users.noreply.github.com>
2023-08-05 07:03:52 -0300

    Fix gcam blobs

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/alphadroid-project/ma
 git clone https://github.com/matheucomth/local_manifest.git --depth 1 -b alpha .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build roms
+# build rom
 . build/envsetup.sh
 lunch lineage_onclite-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
