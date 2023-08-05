5dd8c5d03384cfba7d2b3f4f8f3f5d29c56f596e
Skynet <67794738+TasinAyon@users.noreply.github.com>
2023-08-05 10:54:47 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,12 +7,14 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_begonia-userdebug
+#lunch lineage_begonia-userdebug
+lunch bliss_begonia-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export RELAX_USES_LIBRARY_CHECK=true
 export TZ=Asia/Dhaka #put before last build command
-mka bacon
+#mka bacon
+blissify -v begonia
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
