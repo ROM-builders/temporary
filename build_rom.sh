d633eb4e9c829a957fb9bec0e23136adc8678b53
omanshkrishn <138091069+omanshkrishn@users.noreply.github.com>
2023-08-07 05:08:30 +0530

    inital commit

--- build_rom.sh
+++ build_rom.sh
@@ -1,26 +1,22 @@
 # sync rom
 
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://codeberg.org/omansh-krishn/local_manifest.git --depth 1 -b evox-mi8937 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/RisingTechOSS/android.git -b thirteen -g default,-mips,-darwin,-notdefault
+git clone https://codeberg.org/omansh-krishn/local_manifest.git --depth 1 -b RisingOS-Mi8937 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-# fixed syntax of local_manifest
 # build rom
-
 source build/envsetup.sh
+lunch lineage_Mi8937-user
 
-lunch evolution_Mi8937-user
-export WITH_GMS=true
-export TARGET_KERNEL_CLANG_VERSION=zyc_clang
-export TARGET_KERNEL_CLANG_PATH=$(pwd)/prebuilts/clang/host/linux-x86/${TARGET_KERNEL_CLANG_VERSION}
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export ALLOW_MISSING_DEPENDENCIES=true
+export WITH_GMS=flase
 export BUILD_USERNAME=OmanshKrishn
 export BUILD_HOSTNAME=pc
 export KBUILD_BUILD_USER=OmanshKrishn
 export KBUILD_BUILD_HOST=pc
 export TZ=Asia/Kolkata
 
-m evolution
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
