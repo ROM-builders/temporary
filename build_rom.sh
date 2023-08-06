00cf5c415d8b843f4496cfd1488938265373a26f
dogpoopy <103020425+dogpoopy@users.noreply.github.com>
2023-08-06 09:24:45 +0800

    update revision

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest.git -b thirteen-plus -g default,-mips,-darwin,-notdefault
-git clone https://github.com/dogpoopy/local_manifest.git --depth 1 -b PE+ .repo/local_manifests
+git clone https://github.com/dogpoopy/local_manifest.git --depth 1 -b PE .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
