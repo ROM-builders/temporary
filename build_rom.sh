01706982425ba02efc220673a9a8821e40509116
Abhiram <103818282+Azure-Helper@users.noreply.github.com>
2023-08-07 00:10:22 +0530

    !fixup

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest -b topaz -g default,-mips,-darwin,-notdefault
-git clone https://github.com/back-up-git/local_manifests.git --depth 1 -b topaz-test .repo/local_manifests
+git clone https://github.com/back-up-git/local_manifests.git --depth 1 -b topaz .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom [58]
