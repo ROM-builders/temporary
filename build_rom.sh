36244fc9646eb07961012c0521a82e576b7b0fd6
Shashank patil <9945shashank@gmail.com>
2023-08-07 17:47:21 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Octavi-Staging/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/itsshashanksp/local_manifest.git --depth 1 -b octavi .repo/local_manifests
+git clone https://github.com/itsshashanksp/local_manifest.git -b octavi .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
