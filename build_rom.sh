e64fded01463597eaf1045f18d3b3d3741a08df9
Skynet <67794738+TasinAyon@users.noreply.github.com>
2023-08-07 18:52:30 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/BlissRoms/platform_manifest.git -b typhoon --git-lfs -g default,-mips,-darwin,-notdefault
-git clone https://github.com/AyOn9914/local_manifests.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs -g default,-mips,-darwin,-notdefault
+git clone https://github.com/AyOn9914/android_.repo_local_manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
