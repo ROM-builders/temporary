fb0d8d7b97bf33a341ffdf3fe0b53bafc0091242
Muichiro Tokito <132120605+Muichiro09@users.noreply.github.com>
2023-08-07 12:59:38 +0800

    voltage

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/alphadroid-project/manifest -b alpha-13 --git-lfs default,-mips,-darwin,-notdefault
-git clone https://github.com/Muichiro09/local_manifest.git --depth 1 -b alpha-13 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest.git -b 13 --git-lfs default,-mips,-darwin,-notdefault
+git clone https://github.com/Muichiro09/local_manifest.git --depth 1 -b voltage-surya .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_surya-userdebug
+lunch voltage_surya-userdebug
 export TZ=asia/Dhaka #put before last build command
 make bacon
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
