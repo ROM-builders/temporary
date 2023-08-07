fafc0093c8704e5b34c5fcf43e5c227f25ef10be
nosebs <78350356+nosebs@users.noreply.github.com>
2023-08-07 11:40:00 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/SuperiorOS/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/nosebs/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --force-sync -j8
 
