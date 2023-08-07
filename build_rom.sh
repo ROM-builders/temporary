2e79c30c953dd780bb55121a5f8a850ea7182657
nosebs <78350356+nosebs@users.noreply.github.com>
2023-08-07 11:34:52 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/SuperiorOS/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/nosebs/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-export KBUILD_BUILD_USER=nosebs
-export KBUILD_BUILD_HOST=obed
-export BUILD_USERNAME=nosebs
-export BUILD_HOSTNAME=obed
-export TZ=Europe/Moscow #put before last build command
 lunch superior_blossom-userdebug
+export TZ=Europe/Moscow #put before last build command
 m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
