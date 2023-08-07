0b8d65b5532955c06ef07f8799c76b1a05b90292
Skynet <67794738+TasinAyon@users.noreply.github.com>
2023-08-07 10:51:29 +0600

    begonia-LineageOS-lineage-20.0-ayon7

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/BlissRoms/platform_manifest.git -b typhoon -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/BlissRoms/platform_manifest.git -b typhoon --git-lfs -g default,-mips,-darwin,-notdefault
 git clone https://github.com/AyOn9914/local_manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
