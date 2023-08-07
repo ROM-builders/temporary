ab0f45482ffd64fca8a31d545bf0ad255dc21650
nosebs <78350356+nosebs@users.noreply.github.com>
2023-08-07 11:27:55 +0600

    superior

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,17 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/AICP/platform_manifest.git -b t13.0 --git-lfs -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/nosebs/local_manifest --depth 1 -b main .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-export TZ=Asia/Dhaka #put before last build command
-brunch blossom eng
+export KBUILD_BUILD_USER=nosebs
+export KBUILD_BUILD_HOST=obed
+export BUILD_USERNAME=nosebs
+export BUILD_HOSTNAME=obed
+export TZ=Europe/Moscow #put before last build command
+lunch superior_blossom-userdebug
+m bacon -j8
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
