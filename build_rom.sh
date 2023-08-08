dc945a87cae5b7673a5e52f75265710324d751a6
CODEWITHBG <118347130+Bikesh-Kumar-Gupta@users.noreply.github.com>
2023-08-08 21:56:03 +0545

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 
 # sync rom
 repo init -u https://github.com/syberia-project/manifest.git -b 13.0
-git clone https://github.com/rabikishan000/local_manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/Bikesh-Kumar-Gupta/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
