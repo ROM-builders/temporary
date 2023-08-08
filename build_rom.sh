25246643ce6bc0df40edeaeeefcbff538660b7d0
Abhinav Kumar <122148266+acex69@users.noreply.github.com>
2023-08-08 21:34:28 +0530

    risingOs: Test first build

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ricedroidOSS/android -b thirteen -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/RisingTechOSS/android -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/acex69/local_manifest --depth 1 -b rising .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
