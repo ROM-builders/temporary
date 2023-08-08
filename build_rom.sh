aeedbaf0fdec5ea5553622f01961dda1e7a798a6
rams <135526213+ramshell688@users.noreply.github.com>
2023-08-08 23:31:52 +0700

    camellia-SuperiorOS-ramshell688
    Build SuperiorOS for Camellia

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync
-repo init --depth=1 --no-repo-verify -u https://github.com/Havoc-OS/android_manifest.git -b thirteen --git-lfs -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/ramshell688/Builders.git --depth 1 -b master .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c -j8 --force-sync --no-clone-bundle --no-tags
 
 # build rom
 source build/envsetup.sh
 lunch lineage_camellia-userdebug
-export TZ=Asia/Dhaka #put before last build command
-brunch camellia
+export TZ=Asia/Jakarta #put before last build command
+m bacon -j8
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
