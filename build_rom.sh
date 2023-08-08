f3c6cc6b34d97d8b03b702b121a67f6eae241d6e
rams <135526213+ramshell688@users.noreply.github.com>
2023-08-09 00:00:51 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync
 repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/ramshell688/Builders.git --depth 1 -b master .repo/local_manifests
-repo sync -c -j8 --force-sync --no-clone-bundle --no-tags
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
