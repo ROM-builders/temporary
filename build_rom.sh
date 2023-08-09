95cf331c71006059164cb663dd3deefcc4309b78
Skynet <67794738+TasinAyon@users.noreply.github.com>
2023-08-09 10:58:14 +0600

    begonia-BlissRoms-typhoon-ayon9

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/BlissRoms/platform_manifest.git -b typhoon --git-lfs -g default,-mips,-darwin,-notdefault
-git clone https://github.com/AyOn9914/local_manifests.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/TasinAyon/android_.repo_local_manifests --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
