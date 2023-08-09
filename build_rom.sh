900160549f1a9ff052dfb89f76077a841d3a9d19
Wasik Ahmmed Fahm <62275326+OmegaBeam69@users.noreply.github.com>
2023-08-09 12:18:53 +0600

    Initial Build

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/OmegaBeam69/local_manifest.git --depth 1 -b derp .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u  https://github.com/AICP/platform_manifest.git -b t13.0 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/OmegaBeam69/local_manifest.git --depth 1 -b aicp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom |9|
+# build rom |1|
 source build/envsetup.sh
-lunch derp_raphael-userdebug
+lunch aicp_raphael-userdebug
 export TZ=Asia/Dhaka #put before last build command
-mka derp
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
