a905183984bc56cc4bd4b4759f5a3013ac088206
ghostmaster-reborn <92838621+ghostmaster-reborn@users.noreply.github.com>
2023-08-09 19:16:09 +0000

    initial derp

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/AOSP-Silicon/local_manifest.git --depth 1 -b derpfest-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
-export TZ=Asia/Dhaka #put before last build command
+lunch derp_vince-user
 mka derp
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
