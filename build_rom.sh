03aeb81c5729986fe798c03b418ed9f982b0734e
SpendyYT <104786041+SpendyYT@users.noreply.github.com>
2023-08-09 20:00:26 +0300

    rosemary-Evolution-X-tiramisu-SpendyYT

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-
 lunch evolution_rosemary-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
