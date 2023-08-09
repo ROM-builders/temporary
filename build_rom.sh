8a99f9c49953465b4fb059be367665851affe578
CODEWITHBG <118347130+Bikesh-Kumar-Gupta@users.noreply.github.com>
2023-08-09 11:46:28 +0545

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/syberia-project/manifest.git -b 13.0 -g default, -device, -mips, -darwin, -notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/syberia-project/manifest.git -b 13.0 
 git clone https://github.com/Bikesh-Kumar-Gupta/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
