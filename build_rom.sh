40ee0540b699f275a93312aa217137f82f874ff4
K R I S H N A <krishnakripa34567@gmail.com>
2023-08-09 18:39:25 +0530

    build plox

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Octavi-Staging/manife
 git clone https://github.com/pure-soul-kk/local_manifest --depth 1 -b octa .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom 
 source build/envsetup.sh
 lunch octavi_sweet-userdebug
 export TZ=Asia/Kolkata #put before last build command
