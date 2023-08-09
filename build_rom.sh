6f9576e065156eaf0cbae7aad0749b5245efa5cd
Muichiro Tokito <132120605+Muichiro09@users.noreply.github.com>
2023-08-09 19:10:12 +0800

    superior 13

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest.git -b 13 --git-lfs default,-mips,-darwin,-notdefault
-git clone https://github.com/Muichiro09/local_manifest.git --depth 1 -b voltage-surya .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.git -b thirteen --git-lfs default,-mips,-darwin,-notdefault
+git clone https://github.com/Muichiro09/local_manifest.git --depth 1 -b superior-surya .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch voltage_surya-userdebug
+lunch superior_surya-userdebug
 export TZ=asia/Dhaka #put before last build command
-make bacon
+m bacon
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
