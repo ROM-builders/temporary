6584f5434e9fbf7095c80848a93966f37324e687
Sundram <101039005+xyz-sundram@users.noreply.github.com>
2023-08-10 19:03:11 +0530

    Aug Patch

--- build_rom.sh
+++ build_rom.sh
@@ -5,8 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build roms
 . build/envsetup.sh
-lunch cherish_twolip-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
+lunch cherish_twolip-user
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
