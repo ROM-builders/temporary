2ff16285a876270ab0a6581de35c8e37a63150e2
Tejas Singh <tejas@m.ms.evolution-x.org>
2023-08-10 15:06:01 +0530

    Trigger

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
-# sync rom
+# sync
 repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/levi3609/local_manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/Testing-Stuffs/Local_Manifest.git --depth 1 -b sup .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build roms [2]
-source build/envsetup.sh
-lunch superior_alioth-user
-export TZ=Asia/Dhaka 
-m bacon
+# build
+export TZ=Asia/Kolkata
+. build/envsetup.sh
+lunch superior_ginkgo-user
+mka bacon
 
-# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+# upload
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
