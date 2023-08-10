9203473fa05c216684ebc5e12d9af4e230585ca6
khirendra <khirendrapatel3@gmail.com>
2023-08-10 12:18:05 +0530

    X01BD: print vendor/voltafe/ota file [skip ci]
    Signed-off-by: khirendra <khirendrapatel3@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -10,4 +10,4 @@ brunch voltage_X01BD-userdebug
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-rclone copy vendor/ota/X01BD.json cirrus:X01BD -P
+cat vendor/ota/X01BD.json
