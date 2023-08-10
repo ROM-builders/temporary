d350caaae5ee3dbbe7bfd2e479c7895b9ea8f15b
crackedmodmenu <121314959+crackedmodmenu@users.noreply.github.com>
2023-08-10 23:31:20 +0900

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,10 @@
  # sync Rom      
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android -b lineage-19.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest -b tiramisu --git-lfs -g default,-mips,-darwin,-notdefault
 git clone https://github.com/crackedmodmenu/local_manifest.git --depth 1 -b main .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j30 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -q -j8
 # build rom                 
 source build/envsetup.sh   
-lunch lineage_merlinx-userdebug 
+brunch cherish_merlinx-userdebug 
 export TZ=Asia/Delhi 
-mka evolution 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
