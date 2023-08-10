447dfc06817ce8c0c1d3ee0e52e99d7020e104de
Amrito Karmokar <amritokarmokar5i@gmail.com>
2023-08-10 20:02:19 +0600

    Ok

--- build_rom.sh
+++ build_rom.sh
@@ -7,6 +7,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch aospa_r5x-userdebug
 export TZ=Asia/Dhaka #put before last build command
+export TARGET_BOARD_PLATFORM=trinket
+export BOARD_USES_QCOM_HARDWARE=true
 ./rom-build.sh r5x -t userdebug
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
