aa1fa8f30f0b9cd26b305406e5e070f74161e9bf
Wasik Ahmmed Fahm <62275326+OmegaBeam69@users.noreply.github.com>
2023-08-10 16:36:39 +0600

    Checking DT2W

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_man
 git clone https://github.com/OmegaBeam69/local_manifest.git --depth 1 -b cherish .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom [27]
+# build rom [28]
 source build/envsetup.sh
 lunch cherish_raphael-userdebug
 export CHERISH_VANILLA=true
