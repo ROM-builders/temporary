3faa71bf1e4d327b4de6ba5b5907686ae52a9694
SpendyYT <104786041+SpendyYT@users.noreply.github.com>
2023-08-11 18:27:09 +0300

    rosemary-PixelOS-AOSP-thirteen-SpendyYT

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-AOSP/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
+git clone https://github.com/SpendyYT/local_manifest.git --depth 1 -b pixelos .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
+lunch aosp_rosemary-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
+export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
