eefeef56675b1c085d59edff605fe320a821139d
dogpoopy <qtqtron@gmail.com>
2023-08-11 13:20:49 +0800

    aight, back to buildable kernel source

--- build_rom.sh
+++ build_rom.sh
@@ -11,8 +11,8 @@ export ALLOW_MISSING_DEPENDENCIES=true
 # builder variables
 export BUILD_USERNAME=dogpoopy
 export BUILD_HOSTNAME=linux
-export USER=dogpoopy
-export HOSTNAME=linux
+export KBUILD_BUILD_USER=dogpoopy
+export KBUILD_BUILD_HOST=linux
 # timezone
 export TZ=Asia/Manila
 m evolution
