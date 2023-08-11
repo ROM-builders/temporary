d9f4ce8672a8a87f5d86000848b1f261e8218e57
nabiraja <62164079+nobiraja@users.noreply.github.com>
2023-08-12 00:11:05 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,14 +5,14 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch aosp_rasgulla-userdebug
+lunch aosp_CPH2381-userdebug
 export KBUILD_BUILD_USER=nobiraja
 export KBUILD_BUILD_HOST=nobiraja
 export BUILD_USERNAME=nobiraja
 export BUILD_HOSTNAME=nobiraja
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export TZ=Asia/Dhaka #put before last build command
-make bacon
+make updatepackage
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
