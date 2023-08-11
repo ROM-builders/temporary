f9366ab46957e0b2219cbcfb2888ac046923a668
EmadGr8 <91535171+EmadGr8@users.noreply.github.com>
2023-08-11 03:23:52 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom
+# sync rom 
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/EmadGr8/local_manifest.git --depth 1 -b LOS-20 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
