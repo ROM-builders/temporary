d9980e3937148882473181717b511bcab8fd086a
Hitesoni | ×͜× <72127977+hsx02@users.noreply.github.com>
2023-08-14 13:55:32 +0530

    Ruse for __

--- build_rom.sh
+++ build_rom.sh
@@ -1,30 +1,12 @@
-### sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://github.com/hsx02/Local-Manifests.git --depth 1 -b evo-14 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/RisingTechOSS/android -b thirteen --git-lfs -g default,-mips,-darwin,-notdefault
+git clone https://github.com/hsx02/Local-Manifests.git --depth 1 -b rise .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rommmmmm
-source build/envsetup.sh
-lunch evolution_spes-userdebug
-
-export TZ=Asia/Delhi #put before last build command
-mka evolution
+# build roms
+. build/envsetup.sh
+lunch lineage_spes-userdebug
+export TZ=Asia/Dhaka #put before last build command
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
