e8782b937b70fe70a96c59868218cf3a5c970955
AbrarNoobAlt <123289074+AbrarNoobAlt@users.noreply.github.com>
2023-08-14 16:55:57 +0600

    Init derp

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch derp_alioth-userdebug
+lunch derp_alioth-user
 export TZ=Asia/Jakarta
 mka derp
 
