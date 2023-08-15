1d63c422292a8531385ce833d2ef4ba845f10295
Fr0ztyy43 <82140208+Fr0ztyy43@users.noreply.github.com>
2023-08-15 16:01:13 +0200

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -13,7 +13,7 @@ export KBUILD_BUILD_HOST=Noob
 export BUILD_USERNAME=Fr0ztyy43 
 export BUILD_HOSTNAME=Noob
 export TZ=Asia/Dhaka #put before last build command
-make bacon
+make evolution
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
