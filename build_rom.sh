7659a91d243b3e64236884e4fdb14d0dc88cb2e9
strongreasons <109900777+strongreasons@users.noreply.github.com>
2023-08-15 09:25:30 +0700

    Fix sepolicy

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch blaze_X00TD-user
+lunch blaze_X00TD-userdebug
 export TZ=Asia/Jakarta #put before last build command
 make bacon
 
