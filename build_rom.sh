a239bf8e6a5ae63e12236f9b35f61b0113724762
rian <136869463+rssxda@users.noreply.github.com>
2023-08-15 20:45:35 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,16 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-13.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/angelomds42/local_manifest --depth 1 -b arrow .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/rssxda/manifest -b thirteen-plus -g default,-mips,-darwin,-notdefault
+git clone https://github.com/rssxda/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch arrow_devon-userdebug
+lunch aosp_rhode-userdebug
+m vendorbootimage
 m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+rclone copy out/target/product/rhode/boot.img
+rclone copy out/target/product/rhode/dtbo.img
+rclone copy out/target/product/rhode/vendor_boot.img
