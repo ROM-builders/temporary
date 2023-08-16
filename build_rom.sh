ab3f5407eddee62f15cee5a4089ab5aa1440a3d7
Soo-Hwan Na <whiteshell2544@naver.com>
2023-08-16 20:21:39 +0900

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,9 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest -b 13 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/roynatech2544/local_manifests --depth 1 .repo/local_manifests -b dreamlte-derp
+repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Lenovo-SM6225-Fork/local_manifests --depth 1 .repo/local_manifests -b evox-13
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build r
 source build/envsetup.sh
-lunch derp_dream2lte-user
-export TZ=Asia/Seoul #put before last build command
-mka derp
-
-# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
-rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+lunch evolution_tb128fu-user
+mka evolution
