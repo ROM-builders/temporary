63b0fa1a380be29cde6f3c4b52846c6e04d28399
omanshkrishn <138091069+omanshkrishn@users.noreply.github.com>
2023-08-16 19:16:30 +0530

    RisingOS Bringup

--- build_rom.sh
+++ build_rom.sh
@@ -1,17 +1,17 @@
 # sync rom
 
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest.git -b thirteen-plus -g default,-mips,-darwin,-notdefault
-git clone https://codeberg.org/omansh-krishn/local_manifest.git --depth 1 -b pixelexperience-13-t .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/RisingTechOSS/android -b thirteen --git-lfs -g default,-mips,-darwin,-notdefault
+git clone https://codeberg.org/omansh-krishn/local_manifest.git --depth 1 -b risingos-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 
 source build/envsetup.sh
-lunch aosp_santoni-user
+lunch lineage_santoni-user
 
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export ALLOW_MISSING_DEPENDENCIES=true
-export CUSTOM_BUILD_TYPE=UNOFFICIAL-OmanshKrishn
+export RISING_PACKAGE_TYPE=VANILLA-AOSP-OmanshKrishn
 export BUILD_USERNAME=OmanshKrishn
 export BUILD_HOSTNAME=pc
 export KBUILD_BUILD_USER=OmanshKrishn
