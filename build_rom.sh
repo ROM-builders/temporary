ba103245a1d8ef3ed113dc2e6a1e6bd26053cd35
rian <136869463+rssxda@users.noreply.github.com>
2023-08-15 21:09:12 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init -u https://github.com/rssxda/manifest -b thirteen-plus -g default,-mips,-darwin,-notdefault --depth=1 --no-repo-verify
+repo init -u https://github.com/PixelOS-AOSP/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault --depth=1 --no-repo-verify
 git clone https://github.com/rssxda/local_manifest -b main .repo/local_manifests  --depth=1
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
