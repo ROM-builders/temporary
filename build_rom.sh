70fc91dee0c9cb96844d3a14c0d81492484f6333
Deepak Jangir <37282143+Deepak5310@users.noreply.github.com>
2023-08-17 21:30:38 +0530

    Revert "Update 1"

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/androi
 git clone https://github.com/deedev-projects/local_manifests.git --depth 1 -b lineage-20.0 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build roms
+# build rom
 . build/envsetup.sh
 lunch lineage_spes-user
 export TZ=Asia/Kolkata #put before last build command
