9312227a3ff7e6f2ac7a94590079d5c67b6ae936
شَکِیْب | Shakib <52566642+Shakib-BD@users.noreply.github.com>
2023-08-17 08:04:40 +0600

    merlinx: vanilla r_vendor

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/alphadroid-project/manifest -b alpha-13 --git-lfs -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Shakib-BD/local_manifest.git --depth 1 -b alpha-13-s .repo/local_manifests
+git clone https://github.com/Shakib-BD/local_manifest.git --depth 1 -b alpha-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
