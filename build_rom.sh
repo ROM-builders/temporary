0146dfd049316cf32105869326f9249f03c8d95b
nosebs <78350356+nosebs@users.noreply.github.com>
2023-08-17 13:59:27 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -10,7 +10,7 @@ export KBUILD_BUILD_HOST=blossom
 export BUILD_USERNAME=nosebs
 export BUILD_HOSTNAME=blossom
 export TZ=Europe/Moscow #put before last build command
-brunch blossom eng
+brunch blossom userdebug
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
