d96a5313a04018ec4ba107ee31974d3976ea1578
Soyab Khan <56043965+soyabkhanmalek@users.noreply.github.com>
2023-08-17 22:49:07 +0530

    yunluo-PixelExperience-thirteen-soyabkhanmalek

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,20 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/soyabkhanmalek/local_manifest.git --depth 1 -b yunluo .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b thirteen -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Farmsun/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
-lunch lineage_yunluo-userdebug
-export TZ=Asia/Dhaka #put before last build command
+. build/envsetup.sh
+lunch aosp_yunluo-userdebug
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+export ALLOW_MISSING_DEPENDENCIES=true
+export CUSTOM_BUILD_TYPE=UNOFFICIAL-SoyabKhan
+export BUILD_USERNAME=SoyabKhan
+export BUILD_HOSTNAME=pc
+export KBUILD_BUILD_USER=SoyabKhan
+export KBUILD_BUILD_HOST=pc
+export TZ=Asia/Kolkata
+
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
