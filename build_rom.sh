b14f207bbcad4c4ff438091e6c648aa7b4204110
Akshat Singh <akshatleo9456@gmail.com>
2023-08-17 06:46:07 +0530

    remove non existent project

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/RisingTechOSS/android -b thirteen --git-lfs -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/RisingTechOSS/android -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Legendleo90/local_manifest.git -b rise .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
