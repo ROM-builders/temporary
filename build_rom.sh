86874fa7175453233bb7ab5f6867c53276fb700f
sumon <82557471+mdsumon2001@users.noreply.github.com>
2023-08-17 06:33:54 +0600

    initial crdroid-12.1

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 12.1 -g default,-mips,-darwin,-notdefault --git-lfs
-git clone https://github.com/mdsumon2001/local_manifest.git --depth 1 -b crdroid-12.1 .repo/local_manifests
+git clone https://github.com/mdsumon2001/local_manifest.git --depth 1 -b rmx2185-crdroid-12.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch lineage_RMX2185-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
-export TZ=Asia/Dhaka ##put before last build command
+export TZ=Asia/Dhaka #put before last build command
 m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
