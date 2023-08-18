8467a109362d7cd9c182eadff7007911780c08d6
newestzdn <127198882+newestzdn@users.noreply.github.com>
2023-08-18 13:41:39 +0700

    evox: august update!

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://github.com/zaidannn7/local_manifest --depth 1 -b evox .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu --git-lfs -g default,-mips,-darwin,-notdefault
+git clone https://github.com/zaidanprjkt/local_manifest --depth 1 -b evox .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
