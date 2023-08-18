521a22cd42e48d7961ba4c4e8c8cdfc78f9b2965
Soyab Khan <56043965+soyabkhanmalek@users.noreply.github.com>
2023-08-18 19:32:11 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Farmsun/local_manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/soyabkhanmalek/local_manifest.git --depth 1 -b yunluo .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
