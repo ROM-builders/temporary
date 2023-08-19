d956604604fc4433301a85670bc18fee89c95b1d
YudhoPatrianto <124352333+YudhoPatrianto@users.noreply.github.com>
2023-08-19 12:46:54 +0700

    selene: build: changes branch local manifest,export blur,export UDPFS, change make bacon to brunch

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,16 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git@github.com:CherishOS/android_manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://github.com/YudhoPatrianto/local_manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/YudhoPatrianto/local_manifest.git --depth 1 -b beta .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+build/envsetup.sh
+export TARGET_USES_BLUR=false
+export TARGET_USE_BLUR=false
+export EXTRA_UDFPS_ANIMATIONS=true
 lunch cherish_selene-userdebug
 export TZ=Asia/Dhaka #put before last build command
-make bacon -j 8
+brunch selene
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
