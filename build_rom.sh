7a436ac32bbe8abf8779b02696c86072fdd7f4eb
Itu <131593996+WAR-DEPT@users.noreply.github.com>
2023-08-19 07:54:35 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_miatoll-userdebug
+lunch lineage_miatoll-eng
 export TZ=Asia/Dhaka #put before last build command
 make bacon 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
