b0e004546482066a265bb25237b390203d79b9e6
Yoni Asleno Farid <92686606+aslenofarid@users.noreply.github.com>
2023-08-20 01:57:03 +0700

    Tree miss

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git
 git clone https://github.com/aslenofarid/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build roms
 source build/envsetup.sh
 breakfast X00TD
 export BUILD_HOSTNAME=android-build
