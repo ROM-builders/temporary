fcfea4eae739c0d874fc298bfa08afc7c849dd6c
mozzaru <100791931+mozzaru@users.noreply.github.com>
2023-08-21 21:14:48 +0700

    retry

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/Project-
 git clone https://github.com/mozzaru/local_manifest.git --depth 1 -b awaken-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom            
+# build rom             
 source build/envsetup.sh
 lunch awaken_markw-userdebug
 export TZ=Asia/Dhaka #put before last build command
