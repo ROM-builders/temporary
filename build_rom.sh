9f972ddabacacac5907bf437aa1a3f148b4a27d9
omanshkrishn <138091069+omanshkrishn@users.noreply.github.com>
2023-08-21 21:10:14 +0530

    build with disabled blur & PixelLauncher

--- build_rom.sh
+++ build_rom.sh
@@ -10,7 +10,7 @@ lunch lineage_santoni-user
 
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export ALLOW_MISSING_DEPENDENCIES=true
-export RISING_PACKAGE_TYPE=VANILLA-AOSP-OmanshKrishn
+export RISING_PACKAGE_TYPE=VANILLA-AOSP-WITHPL-OmanshKrishn
 export TARGET_ENABLE_BLUR=false
 export BUILD_USERNAME=OmanshKrishn
 export BUILD_HOSTNAME=pc
