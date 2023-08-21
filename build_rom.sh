0b54ec888fc2031808f963a6d391da38bd5db192
noobprjkt <94937447+bronya30@users.noreply.github.com>
2023-08-21 11:51:38 +0700

    retrofit

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/RisingTechOSS/android
 git clone https://github.com/bronya30/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom [64]
+# build rom [65]
 source build/envsetup.sh
 lunch lineage_rosy-userdebug
 make bacon
