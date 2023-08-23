af38af53b455cc85e26321a0a7ad12329b47b022
Abhinav Kumar (raizel) <122148266+acex88@users.noreply.github.com>
2023-08-23 14:57:25 +0530

    build arrow

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom 
 repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-13.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/acex69/local_manifest --depth 1 -b arrow .repo/local_manifests
+git clone https://github.com/acex88/local_manifest --depth 1 -b arrow .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
  
 # build rom
 . build/envsetup.sh
 lunch arrow_lancelot-userdebug
 export TZ=Asia/kolkata #put before last build command
-m bacon
+m bacon 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
