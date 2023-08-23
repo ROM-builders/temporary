2fea565cb1ee0dd186eb794676d45dead182a6eb
odinson-123 <82627875+odinson-123@users.noreply.github.com>
2023-08-23 07:15:26 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -9,8 +9,9 @@ lunch aosp_lancelot-userdebug
 export TZ=Asia/Dhaka #put before last build command
 export BUILD_USERNAME=ASCE  
 export BUILD_HOSTNAME=@ascex_x  
-make bacon   
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+make bacon   
+
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
