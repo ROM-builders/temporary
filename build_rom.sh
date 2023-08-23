573f7e735cc6f1b60f22b19e1a425c0f75b00fe1
Yoni Asleno Farid <92686606+aslenofarid@users.noreply.github.com>
2023-08-23 16:56:49 +0700

    Try again

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/aslenofarid/local_manifest --depth 1 -b wip .repo/local_manifests
+git clone https://github.com/aslenofarid/local_manifest --depth 1 -b lineage-20 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
