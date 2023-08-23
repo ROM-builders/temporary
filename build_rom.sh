4deb9b7682909a956eef6e1e6a969cefd72f0381
شَکِیْب | Shakib <52566642+Shakib-BD@users.noreply.github.com>
2023-08-23 10:33:06 +0600

    Merlinx: test p-404

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,17 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/P-404/android_manifest -b tokui -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Shakib-BD/local_manifest --depth=1 -b p-404 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
+      
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
+lunch p404_merlinx-user
+export WITH_GAPPS=false
 export TZ=Asia/Dhaka #put before last build command
-mka derp
-
+make bacon
+                  
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+ 
+  
+ 
