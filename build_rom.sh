d62cd9259e9b54f97e7c6b54b3e2e9544bc7c28b
Wasik Ahmmed Fahm <62275326+OmegaBeam69@users.noreply.github.com>
2023-08-25 03:48:29 +0600

    New branch

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b pyro -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b pyro-next -g default,-mips,-darwin,-notdefault
 git clone https://github.com/OmegaBeam69/local_manifest.git --depth 1 -b spark .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
