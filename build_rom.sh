4152d44ae66708e4891d6fd77df07bbe18e0c9b5
Christopher Keith Irvine <Mizdrake7@gmail.com>
2023-08-24 13:00:59 +0530

    status bar icons alarm manager

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,6 @@ git clone https://github.com/mizdrake7/local_manifest.git --depth 1 -b voltage .
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-lunch voltage_r5x-eng
 source build/envsetup.sh
 export KBUILD_BUILD_USER=MAdMiZ
 export KBUILD_BUILD_HOST=MAdMiZ
@@ -23,7 +22,7 @@ export BUILD_BROKEN_CLANG_CFLAGS=true
 export BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE=true
 export RELAX_USES_LIBRARY_CHECK=true
 export TZ=Asia/Kolkata #put before last build command
-make bacon
+brunch r5x
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
