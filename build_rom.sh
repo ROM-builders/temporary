da5ed55fe72e288114c83594c1dcf49c815b3d3f
ghostmaster-reborn <ghostmaster-reborn@users.noreply.github.com>
2023-08-24 08:59:01 +0530

    Arrow august release

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch arrow_vince-user
+export BUILD_HOSTNAME="codespace"
+export BUILD_USERNAME="ghostmaster69-dev"
 m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
