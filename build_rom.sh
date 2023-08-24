56232bfc3805a4f7104b032e75909c4b0f859b90
GunaisHere <133765333+GunaisHere@users.noreply.github.com>
2023-08-25 03:24:17 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch syberia-user
+lunch miatoll-syberia-user
 export TZ=Asia/Dhaka #put before last build command
 mka miatoll-syberiaos-13.0-GunaisHere
 
