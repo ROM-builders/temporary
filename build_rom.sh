17f9ab1b58401e3129d1e972654231566f23105b
deadmeatXX <142789093+deadmeatXX@users.noreply.github.com>
2023-08-25 09:57:04 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch aosp_lancelot-debug
+lunch aosp_lancelot-userdebug
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
