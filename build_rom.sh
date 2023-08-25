954648900e1e268e58ea686ee8c355f18a490143
dinesh <133356394+dinonomous@users.noreply.github.com>
2023-08-25 09:00:54 +0530

    yogurt-Evolution-X-tiramisu-dinonomous
    .

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/CherishOS/android_manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
+repo init -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
 git clone https://github.com/dinonomous/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch cherish_yogurt-user
+lunch evolution_yogurt-user
 export TZ=Asia/Dhaka #put before last build command
 m bacon
 
