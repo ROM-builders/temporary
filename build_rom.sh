0319cac06f4297cfdd2edd66e88e2ed24455acd9
newestzdn <127198882+newestzdn@users.noreply.github.com>
2023-08-26 07:28:21 +0700

    rising: Trying to fix webview issue #2

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/RisingTechOSS/android -b thirteen  -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/RisingTechOSS/android --git-lfs -b thirteen  -g default,-mips,-darwin,-notdefault
 git clone https://github.com/zaidannn7/local_manifest --depth 1 -b rising --git-lfs .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
@@ -25,6 +25,7 @@ mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+
 
 
 
