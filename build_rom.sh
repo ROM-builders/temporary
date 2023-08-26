7a03e8a538feab9c6fe3f2e4d336596a1a42bb14
swapnil vicky <74442290+SwapnilVicky@users.noreply.github.com>
2023-08-26 12:02:35 +0530

    Fixup August patch

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault --git-lfs
-git clone https://github.com/los-miatoll/local_manifest.git --depth 1 -b lineage-20.0 .repo/local_manifests
+git clone https://github.com/SwapnilVicky/local_manifest --depth 1 -b 13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom#
 source build/envsetup.sh
-lunch lineage_miatoll-userdebug
+lunch lineage_miatoll-user
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
