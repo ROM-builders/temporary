ba6ce3867ee8ce925f6a616f81b73d6cb7398b8b
Kristoforus Ananta Putra Magallan <maga64613@gmail.com>
2023-08-27 14:56:32 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,18 +1,18 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest -b arrow-13.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/CEKIKOFGAMERS/local_manifest.git --depth 1 -b lavender .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 -g --git-lfs default,-mips,-darwin,-notdefault
+git clone https://github.com/CEKIKOFGAMERS/local_manifest.git --depth 1 -b LineageOS .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch arrow_lavender-userdebug
+lunch lineage_stone-userdebug
 export BUILD_USERNAME=Kristo
 export BUILD_HOSTNAME=Kristo
 export KBUILD_USERNAME=Kristo
 export KBUILD_HOSTNAME=Kristo
 export TZ=Asia/Jakarta
 export ALLOW_MISSING_DEPENDENCIES=true
-m bacon
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
