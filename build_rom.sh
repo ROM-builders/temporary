2a37f84d611effea4705bf24411088ba171001b7
Sundram <101039005+xyz-sundram@users.noreply.github.com>
2023-08-28 05:11:57 +0530

    test trees #3

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build roms
 . build/envsetup.sh
 lunch lineage_twolip-user
+export TARGET_DISABLE_EPPE=true
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
