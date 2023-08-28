ed154864f8062fd64cc0230db485729c1aeb00f9
Rinto Khan <138510229+Rinto02@users.noreply.github.com>
2023-08-28 06:35:42 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -9,6 +9,6 @@ lunch lineage_RMX2020-user
 export TZ=Asia/Dhaka #put before last build command
 m bacon
 cat out/target/product/RMX2020/RMX2020.json
-
+ 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
