0f7e0334aed17b6a0fa2562e168ec0a2b79abff7
darshan <43618707+mevadadarshan19@users.noreply.github.com>
2023-08-28 19:21:26 +0530

    Update build_rom.sh [skip ci]

--- build_rom.sh
+++ build_rom.sh
@@ -5,8 +5,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-export TZ=Asia/Delhi #put before last build command
 lunch lineage_X01BD-userdebug
+export TZ=Asia/Delhi #put before last build command
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
