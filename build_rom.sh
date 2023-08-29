3debe4b11c4c08b570e07c5d33b12e5710a65c3b
Nobodyayushhere <101562024+Nobodyayushhere@users.noreply.github.com>
2023-08-29 09:52:45 +0530

    replaced with lineage18.1-RMX1821

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init -u https://github.com/LineageOS/android.git -b lineage-18.1 --git-lfs
-git clone git@github.com:Nobodyayushhere/local_manifest.git
-repo sync
+repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/LineageOS/android.git -b lineage-18.1 --git-lfs -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Nobodyayushhere/local_manifest.git --depth 1 -b master .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_RMX1821-userdebug
+lunch lineage_RMX1821-user
 export TZ=Asia/Dhaka #put before last build command
 mbacon lineage
 
