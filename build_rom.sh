f71c0a4569dcfee794be65f720263664cb281a9b
Farmsun <129572861+Farmsun@users.noreply.github.com>
2023-08-29 11:40:41 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b thirteen -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExtended/manifest -b thunder -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Farmsun/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
