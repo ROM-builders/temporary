492503b8071d2701a819ee8cc5beee53b5357817
Nobodyayushhere <101562024+Nobodyayushhere@users.noreply.github.com>
2023-08-29 10:07:50 +0530

    improvement

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/LineageOS/android.git -b lineage-18.1 --git-lfs -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Nobodyayushhere/local_manifest.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/Nobodyayushhere/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
