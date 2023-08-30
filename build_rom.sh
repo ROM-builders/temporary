be04275009b6d8ce8bfc000a81c9c6b295cce07a
GunaisHere <133765333+GunaisHere@users.noreply.github.com>
2023-08-31 01:02:04 +0530

    Miatoll-SyberiaProject-13.0-GunaisHere
    Release Version

--- build_rom.sh
+++ build_rom.sh
@@ -3,10 +3,10 @@ git clone https://github.com/GunaisHere/local_manifests.git --depth 1 -b 13 .rep
 repo sync -c --force-sync --no-tags --no-clone-bundle -j$(nproc --all) --optimized-fetch --prune
 # build rom
 source build/envsetup.sh
-lunch syberia-project_miatoll-user
+lunch miatoll-release
 export BUILD_USERNAME=GL_Founder
 export TZ=Asia/Dhaka #put before last build command
-brunch projectsyberia
+m projectsyberia
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
