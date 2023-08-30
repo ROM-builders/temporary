84d98a121fcbdcae67bb95e86ed305da44d5d32f
Samuel C. B <141483244+SamCme@users.noreply.github.com>
2023-08-30 13:54:25 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,17 +1,17 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LeafOS-Project/android -b leaf-2.0 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest -b topaz -g default,-mips,-darwin,-notdefault
 git clone https://github.com/SamCme/local_manifest-fleur --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch leaf_fleur-userdebug
+lunch lineage_fleur-userdebug
 export KBUILD_BUILD_USER=eltxd
 export KBUILD_BUILD_HOST=eltxd
 export BUILD_USERNAME=eltxd
 export BUILD_HOSTNAME=eltxd
 export TZ=Asia/Jakarta #put before last build command
-mka leaf
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
