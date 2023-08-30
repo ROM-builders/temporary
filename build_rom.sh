c1a7e1355eaf41abd8240050580f70ae5edc6b6d
vigneshvicky <107059364+vigneshvicky2@users.noreply.github.com>
2023-08-30 10:19:11 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -g default,-mips,-darwin,-notdefault
 git clone https://github.com/vignesvicky2/local_manifest.git --depth 1 -b main .repo/local_manifests --depth 1 -b master .repo/local_manifests
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 # build rom
 source build/envsetup.sh
 lunch pixel_yogurt-user
