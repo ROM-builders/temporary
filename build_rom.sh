bbafe6dec40f5a6d63e531fa7c27a926c49f7315
Nobodyayushhere <101562024+Nobodyayushhere@users.noreply.github.com>
2023-08-31 00:51:54 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_RMX1821-user
+lunch lineage_RMX1821-userdebug
 export TZ=Asia/Dhaka #put before last build command
 m bacon
 
