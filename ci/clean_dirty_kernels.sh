67cce0fdf15b9ff8418097115e4d519f8ecfbbc3
Sanjeev <42514333+sanjeevstunner@users.noreply.github.com>
2023-08-30 17:34:32 +0530

    Update with logic for more complex file structure
    We don't need this level of checks, but its good to have

--- ci/clean_dirty_kernels.sh
+++ ci/clean_dirty_kernels.sh
@@ -1,21 +1,25 @@
+#!/bin/bash
+
 # Define the base directory where you want to start scanning
 BASE_DIR="kernel"
 
-# Function to check if the KernelSU directory is present and remove the folder
-remove_if_kernelsu() {
+# Function to check if the KernelSU directory is present and remove subdirectories
+remove_subdirs_if_kernelsu() {
   local dir="$1"
-  
-  # Check if KernelSU directory exists in the given folder
-  if [ -d "$dir/KernelSU" ]; then
-    echo "Removing $dir because KernelSU directory is present."
-    rm -rf -v "$dir"
+
+  # Check if KernelSU directory exists at any level in the given folder
+  if find "$dir" -type d -name "KernelSU" | grep -q .; then
+    echo "Removing subdirectories of $dir because KernelSU directory is present."
+    find "$dir" -mindepth 1 -maxdepth 1 -type d -exec rm -rf -v {} \;
   fi
 }
 
-# Loop through all subdirectories under kernel/*/*
-for dir in "$BASE_DIR"/*/*; do
+# Loop through all subdirectories under kernel
+for dir in "$BASE_DIR"/*; do
   # Check if the current path is a directory
   if [ -d "$dir" ]; then
-    remove_if_kernelsu "$dir"
+    remove_subdirs_if_kernelsu "$dir"
   fi
 done
+
+echo "Dirty kernel trees with KernelSU residues cleaned. Job complete."
