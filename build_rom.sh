0b947c41f29fff355d313d0e116bb5771ed2d9cb
Sanjeev <42514333+sanjeevstunner@users.noreply.github.com>
2023-08-30 06:49:07 +0530

    vayu: Arrow

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault --git-lfs
-git clone https://github.com/sanjeevstunner/Manifest.git --depth 1 -b vayu_lineage .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-13.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/sanjeevstunner/Manifest.git --depth 1 -b vayu_arrow .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # Fix partition sizes 
 # build rom
 source build/envsetup.sh
-lunch lineage_vayu-userdebug
+lunch arrow_vayu-userdebug
+export WITH_GAPPS=false
 # Builder Variables
 export KBUILD_BUILD_USER=chandler
 export KBUILD_BUILD_HOST=bing
@@ -14,7 +15,7 @@ export BUILD_USERNAME=chandler
 export BUILD_HOSTNAME=bing
 # Time Zone
 export TZ=Asia/Kolkata #put before last build command
-make bacon
+m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
