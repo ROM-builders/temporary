6248c4bca45688edc4dc70040dec3462f19f4101
GunaisHere <133765333+GunaisHere@users.noreply.github.com>
2023-08-30 13:30:04 +0530

    Updated commands

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,14 @@
 
-repo init --depth=1 --no-repo-verify -u git://github.com/syberia-project/manifest.git -b 13.0 -g default,-mips,-darwin,-notdefault
+repo init -u https://github.com/syberia-project/manifest.git -b 13.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/GunaisHere/local_manifests.git --depth 1 -b 13 .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
+repo sync -c --force-sync --no-tags --no-clone-bundle -j$(nproc --all) --optimized-fetch --prune
 # build rom
 source build/envsetup.sh
-lunch syberia-project_miatoll-user
+lunch syberia-project_miatoll-userdebug
 export BUILD_USERNAME=GL_Founder
 export BUILD_HOSTNAME=GunaisHere
 export TZ=Asia/Dhaka #put before last build command
-m projectsyberia
+make projectsyberia
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
