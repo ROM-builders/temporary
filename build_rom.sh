6a69761fe855e1c08dcbc03f9a15f2c0ed46338d
Rinto Khan <138510229+Rinto02@users.noreply.github.com>
2023-08-31 01:59:54 +0600

    Make gapps

--- build_rom.sh
+++ build_rom.sh
@@ -5,6 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
+export WITH_GMS=true
 export KBUILD_BUILD_USER=rinto
 export KBUILD_BUILD_HOST=rinto
 export BUILD_USERNAME=rinto
