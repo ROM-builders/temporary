2bb6049b0748bdea78fd47275eb0b920e5d404f9
GunaisHere <133765333+GunaisHere@users.noreply.github.com>
2023-08-31 01:38:16 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 repo init --depth=1 --git-lfs --no-repo-verify -u git://github.com/syberia-project/manifest.git -b 13.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/GunaisHere/local_manifests.git --depth 1 -b main .repo/local_manifests
-repo sync -c --force-sync --no-tags --no-clone-bundle -j$(nproc --all) --optimized-fetch --prune
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 # build rom
 source build/envsetup.sh
 lunch miatoll-release
