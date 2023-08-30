76301a0d650456a47c3db633f26e1936008e8202
vigneshvicky <107059364+vigneshvicky2@users.noreply.github.com>
2023-08-30 11:07:12 +0530

    yogurt-drepfest-tiramisu-vigneshvicky2

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 13 
-git clone https://github.com/vignesvicky2/local_manifest.git --depth 1 -b main .repo/local_manifests --depth 1 -b master .repo/local_manifests
+git clone https://github.com/vigneshvicky2/local_manifest.git --depth 1 -b main .repo/local_manifests --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 # build rom1
 source build/envsetup.sh
