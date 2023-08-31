fd53b1e8b2bee741a23d20dc9fcfe61650af6b13
Nobodyayushhere <101562024+Nobodyayushhere@users.noreply.github.com>
2023-08-31 21:54:25 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -2,6 +2,9 @@
 repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/LineageOS/android.git -b lineage-18.1 --git-lfs -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Nobodyayushhere/local_manifest.git --depth 1 -b main .repo/local_manifests
 git clone cd external/selinux && curl -o 0001-Revert-libsepol-Make-an-unknown-permission-an-error-.patch -L https://github.com/Maanush2004/patches/external/selinux/0001-Revert-libsepol-Make-an-unknown-permission-an-error-.patch && git am 0001-Revert-libsepol-Make-an-unknown-permission-an-error-.patch && cd ../..
+
+git clone cd build/make && curl -o 0001-RMX1821-build-Add-option-to-append-vbmeta-image-to-b.patch -L https://github.com/RMX1821-devs/patches/raw/lineage-20.0/build/make/0001-RMX1821-build-Add-option-to-append-vbmeta-image-to-b.patch && git am 0001-RMX1821-build-Add-option-to-append-vbmeta-image-to-b.patch && cd ../..
+
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
