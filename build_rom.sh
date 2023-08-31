cd64f33916f982115570251a4762f9ec8870e1d7
Kristoforus Ananta Putra Magallan <maga64613@gmail.com>
2023-08-31 20:47:51 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 -g --git-lfs default,-mips,-darwin,-notdefault
-git clone https://github.com/itsmeNaraa/local_manifest.git --depth 1 -b LineageOS .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-19.0 -g --git-lfs default,-mips,-darwin,-notdefault
+git clone https://github.com/itsmeNaraa/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_stone-userdebug
+lunch lineage_moonstone-userdebug
 export BUILD_USERNAME=Kristo
 export BUILD_HOSTNAME=Kristo
 export KBUILD_USERNAME=Kristo
