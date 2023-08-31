09004b9271c5719e5bfcade617e9c0c7011ff2f6
Kristoforus Ananta Putra Magallan <maga64613@gmail.com>
2023-08-31 20:48:51 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-19.0 -g --git-lfs default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-19.1 -g --git-lfs default,-mips,-darwin,-notdefault
 git clone https://github.com/itsmeNaraa/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
