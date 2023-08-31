1cf49bd0473463a02edfddff03f0ce0604ce51a7
Angel Castillo <acastillorobles77@gmail.com>
2023-08-30 19:44:36 -0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/acastillorobles77/local_manifest.git --depth 1 -b lineage-Insignia_Delta3 .repo/local_manifests
+git clone https://github.com/acastillorobles77/local_manifest.git --depth 1 -b lineage-18.1-GA7133 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_Insignia_Delta3-userdebug
+lunch lineage_GA7133-userdebug
 export TZ=America/Mexico_City #put before last build command
 make bacon
 
