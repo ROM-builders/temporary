bc576a9ac46f88d3e2aa13b6201134dd324a575d
anuragnn4823 <143748020+anuragnn4823@users.noreply.github.com>
2023-09-01 02:37:41 -0700

    selene-LineageOS-lineage-20.0-topworstbuildbotever

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs -g default,-mips,-darwin,-notdefault
-git clone https://github.com/anuragnn4823/local_manifest.git --depth 1 -b main .repo/local_manifest
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/anuragnn4823/Local-Manifest.git --depth 1 -b main .repo/local_manifest
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch lineage_selene-userdebug
-export TZ=Asia/Dhaka
+export TZ=Asia/Dhaka #put before last build command
 make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
