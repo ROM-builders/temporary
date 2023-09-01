9b4fb1da0260c40e408bb514299c9a4483a7f54b
nosebs <78350356+nosebs@users.noreply.github.com>
2023-09-01 15:45:42 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/yaap/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/nosebs/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
@@ -9,9 +9,9 @@ export KBUILD_BUILD_USER=nosebs
 export KBUILD_BUILD_HOST=blossom
 export BUILD_USERNAME=nosebs
 export BUILD_HOSTNAME=blossom
-lunch evolution_blossom-eng
+lunch yaap_blossom-userdebug
 export TZ=Europe/Moscow #put beforem last build command
-m evolution
+mka yaap
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
