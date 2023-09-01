99656a11995c6a629ee94b4e4221cf8cf958c5f5
anuragnn4823 <143748020+anuragnn4823@users.noreply.github.com>
2023-08-31 21:43:40 -0700

    selene-lineageos20-anuragnn4823

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/anuragnn4823/local_manifest --depth 1 -b main/.repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
