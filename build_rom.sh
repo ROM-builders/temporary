7720f09ea6814a7ed5a4e761e58f351bc3eb0997
Matheus Souza <33396386+matheucomth@users.noreply.github.com>
2023-08-31 21:07:43 -0300

    try again

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync roms
-repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest -b arrow-13.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-13.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/matheucomth/local_manifest.git --depth 1 -b arrow .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
