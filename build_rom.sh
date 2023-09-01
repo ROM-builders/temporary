af225092b82bd646cef6ccac8627330cb4574474
Amrito Karmokar <93515519+Amritokarmokar@users.noreply.github.com>
2023-09-01 21:06:18 +0600

    Set BUILD_BROKEN_USES_BUILD_COPY_HEADERS to true

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom 
 source build/envsetup.sh
 lunch aospa_r5x-userdebug
+export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
 export TZ=Asia/Dhaka #put before last build command
 ./rom-build.sh r5x -t userdebug
 
