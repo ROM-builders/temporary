1476dbbe1363eedf586d07844d5832a2980db0fc
Sundram <101039005+xyz-sundram@users.noreply.github.com>
2023-09-02 20:47:39 +0530

    try pe

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 13.0 -g default,-mips,-darwin,-notdefault --git-lfs
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest.git -b thirteen-plus -g default,-mips,-darwin,-notdefault
 git clone https://github.com/xyz-sundram/local_manifests.git --depth 1 -b lineage-20 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build roms
 . build/envsetup.sh
-lunch lineage_tulip-userdebug
+lunch aosp_tulip-userdebug
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
