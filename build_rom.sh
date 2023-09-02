20b9eea082ece4330516c9f1a2b592e36296cc00
neogen6 <120629653+neogen6@users.noreply.github.com>
2023-09-02 14:01:49 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-AOSP/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Mi-Thorium/local_manifests.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
