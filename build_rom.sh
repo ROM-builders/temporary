8bfe387a9e663f9cc99fdec0ed9c8efe8013b30a
Amrito Karmokar <93515519+Amritokarmokar@users.noreply.github.com>
2023-09-02 17:08:42 +0600

    Fix duplicate prop

--- build_rom.sh
+++ build_rom.sh
@@ -8,7 +8,7 @@ source build/envsetup.sh
 lunch aospa_r5x-userdebug
 export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
 export TZ=Asia/Dhaka #put before last build command
-./rom-build.sh r5x -t userdebug 
+./rom-build.sh r5x -t userdebug
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
