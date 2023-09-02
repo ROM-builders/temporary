ad0662a6a26ce7e7161d1a525cb41013817a3486
Koushik Dey <kousickdey3@gmail.com>
2023-09-02 23:53:12 +0530

    Trigger Build

--- build_rom.sh
+++ build_rom.sh
@@ -8,5 +8,5 @@ source build/envsetup.sh
 export TZ=Asia/Dhaka #put before last build command
 brunch RMX2020 user
 
-# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+# upload rom - (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
