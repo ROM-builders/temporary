28c7de5da142cf874528a821c9426122167df3c9
Koushik Dey <kousickdey3@gmail.com>
2023-09-03 16:25:03 +0530

    trigger build #2

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Havoc-OS/android_mani
 git clone https://github.com/Koushikdey2003/local_manifest.git --depth 1 -b Havoc-OS-thirteen .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom #2
 source build/envsetup.sh
 export TZ=Asia/Dhaka #put before last build command
 brunch RMX2020 user
