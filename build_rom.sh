53341f308462a450564b671eec1d685072a75430
Samuel C. B <141483244+eltxd@users.noreply.github.com>
2023-09-03 08:39:12 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest -b topaz -g default,-mips,-darwin,-notdefault
-git clone https://github.com/SamCme/local_manifest-fleur --depth 1 -b main .repo/local_manifests
+git clone https://github.com/eltxd/local_manifest-fleur --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
