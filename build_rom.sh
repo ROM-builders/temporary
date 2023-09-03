80bb187f568f2b02fd117d1bc44d27ef460179f2
snnbyyds <87487115+snnbyyds@users.noreply.github.com>
2023-09-03 10:08:19 +0800

    try

--- build_rom.sh
+++ build_rom.sh
@@ -5,6 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
+
 croot
 export CUSTOM_BUILD_TYPE=OFFICIAL
 lunch aosp_fajita-userdebug
