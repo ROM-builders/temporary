a5d016b673f561e035da745c5683a4fea7057118
Isaque <50801963+isaquetp@users.noreply.github.com>
2023-09-03 15:08:59 -0300

    moba-lineage-isaquetp

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git
 git clone https://github.com/isaquetp/local_manifest --depth 1 -b master
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build ron
 source build/envsetup.sh
 lunch lineage_moba-eng
 export TZ=America/Sao_Paulo #put before last build command
