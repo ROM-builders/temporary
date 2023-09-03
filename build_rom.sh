941b50fd313faacc9f89515baa1275d6b3204886
Akshat Singh <akshatleo9456@gmail.com>
2023-09-03 20:59:43 +0530

    Paranoid-ify

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,12 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest -b topaz -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Xeesan/local_manifest --depth=1 -b topaz .repo/local_manifests
+git clone https://github.com/Legendleo90/local_manifests --depth=1 -b topaz .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aospa_alioth-userdebug
 export TZ=Asia/Dhaka #put before last build command
-./rom-build.sh alioth -t userdebug
+./rom-build.sh beryllium -t user
  
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
