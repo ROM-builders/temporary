011bb0035ddc761579fcd6eb08ac2cc54d19c51b
TheZaidSultan <143351449+TheZaidSultan@users.noreply.github.com>
2023-09-03 10:28:32 +0530

    fajita-projectawaken-triton-TheZaidSultan

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch awaken_fajita-userdebug
+lunch awaken_fajita-user
 export TZ=Asia/Kolkata #put before last build command
 make bacon
 
