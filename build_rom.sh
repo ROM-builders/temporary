69286d69a804448bb8a0a4bbea13be6fd5086fa8
شَکِیْب | Shakib <52566642+Shakib-BD@users.noreply.github.com>
2023-09-04 16:07:44 +0600

    Havoc Thirteen Test

--- build_rom.sh
+++ build_rom.sh
@@ -3,11 +3,11 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Havoc-OS/android_mani
 git clone https://github.com/Shakib-BD/local_manifest.git --depth 1 -b havoc-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-## build rom 
+# build rom #2
 source build/envsetup.sh
-lunch lineage_merlinx-userdebug
+lunch lineage_merlinx-user
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
-# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+# upload rom - (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
