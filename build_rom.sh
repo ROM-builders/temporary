28f03babe440cb2ac2b9c947d1be3b9e4c3e2e7d
fukiame <fukiame@proton.me>
2023-09-04 20:01:01 +0700

    Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "h""""""""""""""""""""
    This reverts commit 511982f65faf8093098cd68bd9be635e6f29ec07.

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch derp_selene-userdebug
-export TZ=Asia/Ho_Chi_Minh #put before last build command
+export TZ=Asia/Ho_Chi_Minh # put before last build command
 mka derp
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
