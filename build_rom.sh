bf7e7dd31e3bd19c0760a1dc1210f12a0e4012c6
newestzdn <127198882+newestzdn@users.noreply.github.com>
2023-09-04 23:42:36 +0700

    rising: v1.4 elysium release!

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/RisingTechOSS/android --git-lfs -b thirteen  -g default,-mips,-darwin,-notdefault
-git clone https://github.com/zaidannn7/local_manifest --depth 1 -b rising --git-lfs .repo/local_manifests
+git clone https://github.com/zaidannn7/local_manifest --depth 1 -b rising  .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
