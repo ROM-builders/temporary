69ec8152c77e6bb4ba4696be9ee2136ebecd9c3a
Samuel C. B <141483244+eltxd@users.noreply.github.com>
2023-09-05 19:50:12 +0700

    Fix some

--- build_rom.sh
+++ build_rom.sh
@@ -10,8 +10,9 @@ export KBUILD_BUILD_USER=eltxd
 export KBUILD_BUILD_HOST=eltxd
 export BUILD_USERNAME=eltxd
 export BUILD_HOSTNAME=eltxd
+export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Jakarta #put before last build command
-mka bacon
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
