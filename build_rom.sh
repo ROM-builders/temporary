5e6e3df34120d76a933e3934ff094b2a8a1f36b6
neogen6 <120629653+neogen6@users.noreply.github.com>
2023-09-05 15:18:38 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/AICP/platform_manifest.git -b t13.0 --git-lfs -g default,-mips,-darwin,-notdefault
 git clone https://github.com/neogen6/local_manifests.git --depth 1 -b lineage-20.0 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
@@ -10,6 +10,5 @@ export TZ=Asia/Kolkata #put before last build command
 mka systemimage
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
-zip out/target/product/MiThoriumSSI/MiThoriumSSI-LineageOS.zip out/target/product/MiThoriumSSI/*.img
-mv out/target/product/MiThoriumSSI/MiThoriumSSI-LineageOS.zip out/target/product/MiThoriumSSI/$(printf '%s_' $(grep -lZ 'MiThoriumSSI' out/target/product/MiThoriumSSI/*.img) | sed 's/_$/.zip/')
+zip out/target/product/MiThoriumSSI/MiThoriumSSI-AICP.zip out/target/product/MiThoriumSSI/*.img
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
