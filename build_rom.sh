5a5bde834e237964c0e119b48770b61520aab145
xenxynon <flynryder427@gmail.com>
2023-09-06 08:55:01 +0530

    Release build

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch aospa_phone1-userdebug
 export TZ=Asia/Dhaka #put before last build command
-./r* phone1 -v beta -s certs -t user -z
+./r* phone1 -v release -s certs -t user -z
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
