5de98878fa760d346f1f8a606049d70355e775d4
Miracle <127039117+Miraclev1@users.noreply.github.com>
2023-09-06 07:54:50 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git@github.com:NusantaraProject-ROM/android_manifest.git -b nad-10-mtk -g default,-mips,-darwin,-notdefault
-git clone https://github.com/IMYdev/local_manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/Miraclev1/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 # build rom
 source build/envsetup.sh
