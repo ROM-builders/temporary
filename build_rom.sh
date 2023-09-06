9bbea3f7edfa6bd4f231d44ee2cab0711b1b47fa
Idkjjjj <87739469+Idkjjjj@users.noreply.github.com>
2023-09-06 21:41:24 +0530

    vayu-AOSPA-topaz-Idkjjjj
    baller

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/Idkjjjj/local_manifest/blob/main/local_manifest.xml -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/AOSPA/manifest -b topaz -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Idkjjjj/local_manifest/blob/main/local_manifest.xml --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
