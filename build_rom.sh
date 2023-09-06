414b9c8d8daea52ebc6e773818f57d5e43f0169e
darshan <43618707+mevadadarshan19@users.noreply.github.com>
2023-09-06 12:51:50 +0530

    X01BD-ProjectBlaze-13-mevadadarshan19

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_X01BD-userdebug
+lunch blaze_X01BD-userdebug
 export TZ=Asia/Delhi #put before last build command
 make bacon
 
