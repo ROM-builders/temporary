f96ec00f3a27ac6a49243a503bdf30c676dc6050
Rinto Khan <138510229+Rinto02@users.noreply.github.com>
2023-09-07 06:56:32 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -10,7 +10,7 @@ export KBUILD_BUILD_HOST=rinto
 export BUILD_USERNAME=rinto
 export BUILD_HOSTNAME=rinto
 export TZ=Asia/Dhaka #put before last build command
-brunch RMX2020 user 
+brunch RMX2020 user
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
