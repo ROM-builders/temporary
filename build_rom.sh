aca025ddeb48c73d770c1db8a49c525b88fbe6ba
Kopicetheprojects <92585304+Kopicetheprojects@users.noreply.github.com>
2023-09-07 14:08:34 +0700

    Crdroid-11-mido-kopicetheprojects
    Build gathel asu

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init -u https://github.com/nitrogen-project/android_manifest.git -b 11 -g default,-mips,-darwin,-notdefault
+repo init -u https://github.com/crdroidandroid/android.git -b 11.0 --git -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Kopicetheprojects/Local-Manifests.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch nitrogen_mido-userdebug
-make -j$(nproc --all) otapackage
+lunch crdroid_mido-userdebug
+mk bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
