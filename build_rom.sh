3381e3b6f7293fc71815c3e2bde402963291f1e6
Yoni Asleno Farid <92686606+aslenofarid@users.noreply.github.com>
2023-09-07 13:09:46 +0700

    Initial

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest -b topaz -g default,-mips,-darwin,-notdefault
-git clone https://github.com/strongreasons/local_manifest.git --depth 1 -b topaz .repo/local_manifests
+git clone https://github.com/aslenofarid/local_manifest.git --depth 1 -b topaz .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
