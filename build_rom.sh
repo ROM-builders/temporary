4e7c57f10d3e6ddd1b318e81d7f2b385a8b3ae24
Kopicetheprojects <92585304+Kopicetheprojects@users.noreply.github.com>
2023-09-07 13:48:34 +0700

    NitrogenOS-11-Mido-Kopicetheprojects
    Build NitrogenOs for mido

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
+repo init -u https://github.com/nitrogen-project/android_manifest.git -b 11 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Kopicetheprojects/Local-Manifests.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_mido-user
-export TZ=Asia/Jakarta #put before last build command
-mka derp
+lunch nitrogen_mido-user
+make -j$(nproc --all) otapackage
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
