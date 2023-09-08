04fa51708a91fe59ded3b5fc829880ed76404ee8
Rares6567 <86649625+Rares6567@users.noreply.github.com>
2023-09-08 17:42:58 +0100

    ruby-crDroid-Rares6567

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-AOSP/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/crdroidandroid/android.git -b 13.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Rares6567/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
