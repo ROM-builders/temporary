e29cfb4860dd0438c7c1f71caf8786cda204d3f0
Rares6567 <86649625+Rares6567@users.noreply.github.com>
2023-09-08 17:47:01 +0100

    ruby-crDroid-Rares6567

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/crdroidandroid/android.git -b 13.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Rares6567/local_manifest.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/Rares6567/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
