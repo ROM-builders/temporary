3affe1d1f309923b25882f2b11ef626a1ec12fcf
dogpoopy <qtqtron@gmail.com>
2023-09-09 00:37:06 +0800

    fix unsupported sepolicy format

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest -b topaz -g default,-mips,-darwin,-notdefault
 git clone https://github.com/dogpoopy/android_build_manifest.git --depth 1 -b AOSPA .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j4
 
 # build rom
 source build/envsetup.sh
