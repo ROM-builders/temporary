d5055aaac27f0fc445e494fd5826630f5b9b67e6
Tejas Singh <tejassingh649@rediffmail.com>
2023-09-08 22:53:03 +0530

    fix sync

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Testing-Stuffs/Local_Manifest.git --depth 1 -b evolution .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # build
 export TZ=Asia/Kolkata
