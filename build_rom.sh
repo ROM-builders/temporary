3cd09c67be40ac52446010725b95f2acdce15369
Rsyd <Rasyid2007AlKautsar@outlook.co.id>
2023-09-09 04:20:41 +0700

    Build User

--- build_rom.sh
+++ build_rom.sh
@@ -5,12 +5,12 @@ git clone https://github.com/Rsyd1997/local_manifest.git --depth 1 -b master .re
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+. build/envsetup.sh
 lunch lineage_beryllium-user
 export BUILD_HOSTNAME=Fountain-CI
 export BUILD_USERNAME=Rsyd1997
 export TZ=Asia/Jakarta #put before last build command
-brunch beryllium
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
