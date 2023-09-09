7422c06e5793fee30fb42ed6ebd32efb153663ea
Hakan <hknhzl@gmail.com>
2023-09-09 16:44:56 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/RisingTechOSS/android
 git clone https://github.com/Lafactorial/local_manifest.git --depth 1 -b rising .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j4
 
-# build rom
+# build rom 
 source build/envsetup.sh
 lunch lineage_tissot-userdebug
 export TZ=Asia/Dhaka #put before last build command
