f6a5a9f20baea4adfa7fb46b4bfe062885b86978
Abhinav Kumar (raizel) <122148266+acex88@users.noreply.github.com>
2023-09-09 08:30:49 +0530

    trigger build

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,17 @@
-# sync rom 
- repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 13.0 -g default,-mips,-darwin,-notdefault 
- git clone https://github.com/acex88/local_manifest.git --depth 1 -b crdroid .repo/local_manifests 
- repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
-  
- # build rom 
- source build/envsetup.sh 
- lunch lineage_lancelot-userdebug 
- export KBUILD_BUILD_USER=acex88 
- export BUILD_USERNAME=raizel
- export BUILD_USER=raizel 
- export TZ=Asia/Jakarta #put before last build command 
- m bacon 
-  
- # upload rom (if you don't need to upload multiple files, then you don't need to edit next line) 
- rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+# sync rom
+repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 13.0 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/acex88/local_manifest.git --depth 1 -b crdroid .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
+# build rom
+. build/envsetup.sh
+lunch lineage_lancelot-userdebug
+export BUILD_USERNAME=raizel
+export BUILD_HOSTNAME=acex88
+export KBUILD_BUILD_USER=raizel
+export KBUILD_BUILD_HOST=acex88
+export TZ=Asia/Delhi #put before last build command
+m bacon
+
+# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
