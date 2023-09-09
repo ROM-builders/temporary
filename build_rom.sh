38e0ba4a5b1737ad0a1ee45162f466ba79e7badc
Asce-XOX <143834552+Asce-XOX@users.noreply.github.com>
2023-09-09 19:07:48 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch derp_lancelot-userdebug
+lunch derp_lancelot-eng
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
