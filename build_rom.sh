39a5ec522c6bde1bd561e5ace4d31fe8d0fe1eb5
Rsyd <Rasyid2007AlKautsar@outlook.co.id>
2023-09-09 18:27:23 +0700

    fix bootlooped

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,3 @@
-
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Rsyd1997/local_manifest.git --depth 1 -b master .repo/local_manifests
@@ -10,7 +9,7 @@ lunch lineage_beryllium-user
 export BUILD_HOSTNAME=Fountain-CI
 export BUILD_USERNAME=Rsyd1997
 export TZ=Asia/Jakarta #put before last build command
-brunch beryllium
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
