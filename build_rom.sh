0d3b85959efdb81a3151155971b6ff81d688dc5e
Rsyd <Rasyid2007AlKautsar@outlook.co.id>
2023-09-09 10:29:58 +0700

    fix

--- build_rom.sh
+++ build_rom.sh
@@ -10,7 +10,7 @@ lunch lineage_beryllium-user
 export BUILD_HOSTNAME=Fountain-CI
 export BUILD_USERNAME=Rsyd1997
 export TZ=Asia/Jakarta #put before last build command
-make bacon
+brunch beryllium
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
