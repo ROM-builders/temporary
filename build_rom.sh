eca524d27ed23f0e85a9d17eaa8fe1894430fe91
Hakan <hknhzl@gmail.com>
2023-09-09 16:48:44 +0300

    Trigger build

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/RisingTechOSS/android -b thirteen --git-lfs -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Lafactorial/local_manifest.git --depth 1 -b rising .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j4
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom 
+# build rom
 source build/envsetup.sh
 lunch lineage_tissot-userdebug
 export TZ=Asia/Dhaka #put before last build command
