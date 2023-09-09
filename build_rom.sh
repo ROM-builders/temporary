36f119a8443c599fc9a0a9fbfb2a09bce1e647eb
ihklknz <133083216+ihklknz@users.noreply.github.com>
2023-09-09 12:24:31 +0700

    Nahhh i'ms sorry forgooooot

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom
+# sync roms
 repo init --depth=1 --no-repo-verify -u https://github.com/RisingTechOSS/android -b thirteen --git-lfs -g default,-mips,-darwin,-notdefault
 git clone https://github.com/ihklknz/Local-Manifests.git --depth 1 -b risingOSS-beryllium .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
