d0fdbdb60a82a7198b50b660fbb4bead031a9e89
Sanjeev <42514333+sanjeevstunner@users.noreply.github.com>
2023-09-10 10:02:27 +0530

    vayu:PE+

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manif
 git clone https://github.com/sanjeevstunner/Manifest.git --depth 1 -b vayu_aosp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j4
 
-# Try gapps from Pixel OS again
+# Try gapps from Pixel OS again.
 # build rom
 source build/envsetup.sh
 lunch aosp_vayu-user
