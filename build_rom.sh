20c6a521b9c92523347151fcf063cb095a900617
Guidix <81136142+GuidixX@users.noreply.github.com>
2023-09-10 12:14:53 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom 
 repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android_build.git -b 11.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/GuidixX/local_manifest.git --depth 1 -b lineage-18.1 .repo/local_manifests
+git clone https://github.com/GuidixX/local_manifest.git --depth 1 -b lineage-20 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_olives-userdebug
+lunch lineage_spes-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
