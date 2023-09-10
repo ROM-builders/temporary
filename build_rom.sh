37911baf4a782ea4dd745255ade952fe937b3d26
darshan <43618707+mevadadarshan19@users.noreply.github.com>
2023-09-10 10:35:03 +0530

    Update build_rom.sh [skip ci]

--- build_rom.sh
+++ build_rom.sh
@@ -9,4 +9,5 @@ export TZ=Asia/Delhi #put before last build command
 brunch voltage_X01BD-userdebug
  
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+cat vendor/ota/X01BD.json
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
