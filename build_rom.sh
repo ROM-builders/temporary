58a05934b4e51752dd096ce5d8fddeb374964895
dogpoopy <qtqtron@gmail.com>
2023-09-10 16:12:06 +0800

    bring back miui cam

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch evolution_vayu-userdebug
+lunch evolution_vayu-user
 # builder variables
 export BUILD_USERNAME=dogpoopy
 export BUILD_HOSTNAME=linux
