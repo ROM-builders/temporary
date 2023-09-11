a325e0cddc8d4aef425432979d1669956b0482e6
RifatHossaN <104590425+RifatHossaN@users.noreply.github.com>
2023-09-11 19:51:47 +0600

    rosemary-SuperiorOS-thirteen-RifatHossan

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/rifathossan/local_manifest.git --depth 1 -b superior-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
+ 
 # build rom
 source build/envsetup.sh
 lunch superior_rosemary-user
