8f98ca1ccf368ce3d9bc5082bc9d8a1406ae1144
Hassan Karim <144518310+Hassan-kareem@users.noreply.github.com>
2023-09-11 12:23:02 +0530

    SM-A500XX-Lineage-18.1-Hassan-kareem
    Build LineageOS for SM-A500XX

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_a5ultexx-userdebug
+lunch lineage_SM-A500XX-userdebug
 export TZ=Asia/Jharkhand #put before last build command
 mka bacon
 
