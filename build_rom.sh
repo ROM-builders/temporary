e7c6e184bd14e67ee9bc768c5046fdadf5dae442
Guidix <81136142+GuidixX@users.noreply.github.com>
2023-09-11 18:56:31 +0300

    spes-CrDroidAndroid-11.0-GuidixX

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/CrDroidAndroid/manifest.git -b 11.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/GuidixX/Local-Manifests.git --depth 1 -b lineage-20 .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j4
 
 # build rom
 source build/envsetup.sh
