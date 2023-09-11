d2502ace5cd9eded92d9dec855da1a810ceeae6e
Abhinav Kumar (raizel) <122148266+acex88@users.noreply.github.com>
2023-09-11 10:46:41 +0530

    Trigger Build and give out json also

--- build_rom.sh
+++ build_rom.sh
@@ -13,6 +13,6 @@ export KBUILD_BUILD_USER=raizel
 export KBUILD_BUILD_HOST=acex88
 export TZ=Asia/Delhi #put before last build command
 m bacon
-
+cat out/target/product/lancelot/lancelot.json
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
