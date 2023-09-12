e5893ba3de351703991ed2f8405bad1b5171b38b
Shravankumar <shravankumarsayz@gmail.com>
2023-09-12 10:17:32 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_RMX1901-userdebug
+lunch octavi_RMX1901-userdebug
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Kolkata #put before last build command
