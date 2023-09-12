5c897f21f85d40a6e0c8585089eef227bfa67f43
Amrito Karmokar <93515519+Amritokarmokar@users.noreply.github.com>
2023-09-12 18:17:36 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,11 +5,11 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom 
 source build/envsetup.sh
-lunch aospa_r5x-eng 
+lunch aospa_r5x-userdebug
 export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
-./rom-build.sh r5x -t eng
+./rom-build.sh r5x -t userdebug
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
