f5ea66078f44de64d528daf5b3b760ad4bc33abd
Amrito Karmokar <93515519+Amritokarmokar@users.noreply.github.com>
2023-09-12 10:48:48 +0600

    Trigger again

--- build_rom.sh
+++ build_rom.sh
@@ -9,7 +9,7 @@ lunch aospa_r5x-eng
 export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
-./rom-build.sh r5x -t eng
+./rom-build.sh r5x -t eng 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
