082ed951a02c4f22978f34524f7963e6496c665d
israfilbd <144210295+israfilbd@users.noreply.github.com>
2023-09-12 09:57:52 +0600

    RMX1901-Evolution-X-tiramisu-israfilbd

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-## sync rom
+# sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
 git clone https://github.com/ij-israfil/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j4
