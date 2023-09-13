a1d1a76750988b153940a283e49759fb310d72e1
neogen6 <120629653+neogen6@users.noreply.github.com>
2023-09-13 19:06:37 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/sounddrill31/local_manifests.git --depth 1 -b generic-20.0 .repo/local_manifests
+git clone https://github.com/sounddrill31/treble_manifest.git --depth 1 -b lineage-20.0 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
