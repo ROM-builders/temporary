cd7b930582a2ec5e4b9bbee83863dfe55b7dfd44
zaid2sultan <100853058+zaid2sultan@users.noreply.github.com>
2023-09-13 12:35:01 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-AOSP/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Stable-Stuffs/manifest.git --depth 1 -b Pe-13 .repo/local_manifests
+git clone https://github.com/zaid2sultan/local_manifests.git --depth 1 -b awaken13-lav .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
