29b1c31209833ba1fd95ace76b6b77dafa6e95fc
Rsyd <Rasyid2007AlKautsar@outlook.co.id>
2023-09-13 12:16:14 +0700

    fix branch manifest

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Rsyd1997/local_manifest.git --depth 1 -b 13 .repo/local_manifests
+git clone https://github.com/Rsyd1997/local_manifest.git --depth 1 -b thirteen .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
