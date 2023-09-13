6ee70768eaa05e2f6ffacb8d1513253f54f19aba
zaid2sultan <100853058+zaid2sultan@users.noreply.github.com>
2023-09-13 15:38:10 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-AOSP/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b triton --git-lfs -g default,-mips,-darwin,-notdefault
 git clone https://github.com/zaid2sultan/local_manifests.git --depth 1 -b awaken13-lav .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-. build/envsetup.sh
-lunch aosp_lavender-userdebug
+source build/envsetup.sh
+lunch awaken_lavender-userdebug
 export TZ=Asia/Kolkata #put before last build command
 make bacon
 
