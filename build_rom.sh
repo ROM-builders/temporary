7af38f934f19532d1cbf28f3aa9371660ff565e3
ahmedhridoy371 <40537458+ahmedhridoy371@users.noreply.github.com>
2023-09-14 14:51:00 +0600

    trigger build

--- build_rom.sh
+++ build_rom.sh
@@ -1,17 +1,19 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ResurrectionRemix/platform_manifest.git -b Q -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ahmedhridoy371/local_manifest --depth 1 -b sd660 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ResurrectionRemix/platform_manifest.git -b Q --git-lfs -g default,-mips,-darwin,-notdefault
+git clone https://github.com/ahmedhridoy371/local_manifest --depth 1 -b rr2 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch rr_lavender-userdebug
 export GAPPS=false
+export GAPPS=""
+export TARGET_HAS_FOD=false
 export ALLOW_MISSING_DEPENDENCIES=true
 export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
 export BUILD_BROKEN_DUP_RULES=true
 export TZ=Asia/Jakarta #put before last build command
-make  bacon
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
