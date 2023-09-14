b8879fc2564289d1da8bbff75d0220becaa8da6d
Ritikk11 <144951533+Ritikk11@users.noreply.github.com>
2023-09-14 18:59:21 +0530

    Lavender-RisingOs-Ritikk11

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/RisingTechOSS/android.git -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Ritikk11/local_manifest.git --depth 1 -b main .repo/local_manifest
+git clone https://github.com/Ritikk11/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
