31fa27aa20bfd14e9290ce2986d3da438187fcd5
lightnest57 <122135694+lightnest57@users.noreply.github.com>
2023-09-15 00:05:31 +0700

    Gas

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ResurrectionRemix/platform_manifest -b Q -g default,-mips,-darwin,-notdefault
-git clone https://github.com/limestonetwo/local_manifest --depth 1 -b main .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync --force-remove-dirty -j8
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-17.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/limestonetwo/local_manifest --depth 1 -b lineage .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch rr_finix-userdebug
+lunch lineage_finix-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
-export GAPPS=true
-export TZ=Asia/Jakarta #put before last build command
+export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
