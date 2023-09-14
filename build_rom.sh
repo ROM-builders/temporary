f1ec77043a9f128ba781b41b72c8144aa99d1443
Ritikk11 <144951533+Ritikk11@users.noreply.github.com>
2023-09-14 19:07:50 +0530

    Lavender-RisingOs-Ritikk11

--- build_rom.sh
+++ build_rom.sh
@@ -7,8 +7,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 brunch "device_lavender" userdebug
 lunch lineage_lavender-userdebug
-mka bacon
-lunch lineage_lavender-userdebug
 export RISING_CHIPSET=SDM660
 export RISING_MAINTAINER=Ritikk11
 export TARGET_ENABLE_BLUR := true
@@ -16,7 +14,8 @@ export TARGET_BUILD_APERTURE_CAMERA := true
 export TARGET_USE_PIXEL_FINGERPRINT := true
 export TARGET_HAS_UDFPS := true
 export TZ=Asia/Dhaka
-make updatepackage
+mka bacon
+
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
