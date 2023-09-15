b7f2bb779cc7087909f1340fdf090870e8344037
neogen6 <120629653+neogen6@users.noreply.github.com>
2023-09-15 20:46:06 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch treble_gsi-user
+lunch treble_gsi-userdebug
 export TZ=Asia/Kolkata #put before last build command
 mka derp
 
