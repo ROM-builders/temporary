e345247b1a74c73fc6a60f93a850e2758ce28e2c
RifatHossaN <104590425+RifatHossaN@users.noreply.github.com>
2023-09-15 22:30:56 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # rosemary-SuperiorOS-thirteen-RifatHossan
-# sync rom
+# sync rom 
 repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/rifathossan/local_manifest.git --depth 1 -b superior-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
