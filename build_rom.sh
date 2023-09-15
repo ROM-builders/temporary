38d0c69fc352e263e452ecc174fff2efec8a8406
lightnest57 <122135694+lightnest57@users.noreply.github.com>
2023-09-15 14:44:03 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ResurrectionRemix/platform_manifest -b Q -g default,-mips,-darwin,-notdefault
-git clone https://github.com/limestonetwo/local_manifest --depth 1 -b main .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync --force-remove-dirty -j8
+git clone https://github.com/limestonetwo/local_manifest --depth 1 -b lineage .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
