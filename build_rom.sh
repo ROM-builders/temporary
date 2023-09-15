7c539a3fce70f9d4591a1bebb5c7465d09be638f
Soo-Hwan Na <whiteshell2544@naver.com>
2023-09-15 10:59:09 +0900

    try to use jemalloc

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Lenovo-SM6225-Fork/local_manifests --depth 1 .repo/local_manifests -b evox-13
+git clone https://github.com/Lenovo-SM6225-Fork/local_manifests --depth 1 .repo/local_manifests -b evox-13-jem
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
