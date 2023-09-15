541eed7bfe6f88cbcf160c0d47974d651969c679
Shravankumar <shravankumarsayz@gmail.com>
2023-09-15 12:54:26 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b pyro-next -g default,-mips,-darwin,-notdefault
-git clone https://github.com/shravansayz/local_manifest.git --depth 1 -b spark .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/RisingTechOSS/android -b thirteen --git-lfs -g default,-mips,-darwin,-notdefault
+git clone https://github.com/shravansayz/local_manifest.git --depth 1 -b rise .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j4
 
 # build rom
 source build/envsetup.sh
-lunch spark_RMX1901-userdebug
+lunch lineage_RMX1901-userdebug
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
