fd861877e4bcf2e31b9d3dbd4f1040a1f3745f04
Amrito Karmokar <93515519+Amritokarmokar@users.noreply.github.com>
2023-09-15 14:50:49 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,13 @@
-# sync rom 
-repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest -b topaz -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Amritorock/local_manifest.git --depth 1 -b topaz .repo/local_manifests
+# sync rom
+repo init --depth=1 --no-repo-verify -u https://github.com/syberia-project/manifest.git -b 13.0 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Amritorock/local_manifest --depth 1 -b syb .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom 
-source build/envsetup.sh
-lunch aospa_r5x-userdebug
-export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
-export SELINUX_IGNORE_NEVERALLOWS=true
+source build/envsetup.sh 
+lunch syberia_r5x-userdebug 
 export TZ=Asia/Dhaka #put before last build command
-./rom-build.sh r5x -t userdebug 
+mka bacon 
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
