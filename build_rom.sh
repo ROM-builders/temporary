fe73c46edbfcb4a58455cc186cb87cb20abe535c
ahmedhridoy371 <40537458+ahmedhridoy371@users.noreply.github.com>
2023-09-17 00:19:10 +0600

    Trying again

--- build_rom.sh
+++ build_rom.sh
@@ -12,9 +12,8 @@ export TARGET_HAS_FOD=false
 export ALLOW_MISSING_DEPENDENCIES=true
 export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
 export BUILD_BROKEN_DUP_RULES=true
-export TZ=Asia/Jakarta #put before last build command
-mka bacon
+export TZ=Asia/Jakarta
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
- 
