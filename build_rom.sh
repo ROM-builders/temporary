16f26240260a1c963a18b60d542ff7fb6ac60d2c
zaid2sultan <100853058+zaid2sultan@users.noreply.github.com>
2023-09-17 02:05:48 +0530

    Lavender Awaken zaid2sultan

--- build_rom.sh
+++ build_rom.sh
@@ -6,8 +6,10 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch awaken_lavender-userdebug
+export BUILD_USERNAME=Zaid
+export BUILD_HOSTNAME=zaid2sultan
 export TZ=Asia/Kolkata #put before last build command
-mka derp
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
