b3dbfdb2f2501c0aef6f0625a4d50b50f2601d81
wancsara <145196976+wancsara@users.noreply.github.com>
2023-09-16 17:18:41 +0700

    beryllium-CherishOS-tiramisu-wanscara

--- build_rom.sh
+++ build_rom.sh
@@ -9,7 +9,7 @@ lunch cherish_beryllium-user
 export BUILD_HOSTNAME=Fountain-build
 export BUILD_USERNAME=Rsyd58
 export TZ=Asia/Jakarta #put before last build command
-mka bacon
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
