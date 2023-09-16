d64bdce1e8a2100055e8fbee0ad5536f7c8acf20
dogpoopy <qtqtron@gmail.com>
2023-09-16 17:09:17 +0800

    fixing sepolicy issue

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest -b topaz -g default,-mips,-darwin,-notdefault
-git clone https://github.com/dogpoopy/android_build_manifest.git --depth 1 -b AOSPA .repo/local_manifests
+git clone https://github.com/RecreationRealm/android_build_manifest.git --depth 1 -b AOSPA .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j4
 
 # build rom
