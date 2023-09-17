aaf5382ca47ce27e3b45d8e7698bc6cdbe72afb0
Amrito Karmokar <93515519+Amritokarmokar@users.noreply.github.com>
2023-09-17 13:27:33 +0600

    Initial a13

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/syberia-project/manifest.git -b 13.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Amritorock/local_manifest --depth 1 -b syb .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/BlissRoms/platform_manifest.git -b typhoon -g --git-lfs default,-mips,-darwin,-notdefault 
+git clone https://github.com/Amritorock/local_manifest --depth 1 -b bliss .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom 
-source build/envsetup.sh 
-lunch syberia_r5x-userdebug 
+# build rom#
+. build/envsetup.sh
+lunch bliss_r5x-userdebug
 export TZ=Asia/Dhaka #put before last build command
-mka bacon 
+blissify -v r5x
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
