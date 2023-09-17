e930a504f0db3caa516071a77b1986ea1392e68b
JUSTAPRO69 <72558197+bhavya1245073@users.noreply.github.com>
2023-09-17 20:53:50 +0530

    lancelot-bananadroid-13-bhavya1245073

--- build_rom.sh
+++ build_rom.sh
@@ -9,7 +9,7 @@ lunch banana_lancelot-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export TZ=Asia/Dhaka #put before last build command
-mka bacon
+mka banana
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
