79e1a1f24817238fd68f58042b477b1c9f7731c1
GXC2356 <108477326+GXC2356@users.noreply.github.com>
2023-09-17 04:05:51 +0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 -u https://github.com/ReloadedOS/manifest -b t -g default,-mips,-darwin,-notdefault
+repo init --depth=1 -u https://github.com/ReloadedOS/manifest.git -b t -g default,-mips,-darwin,-notdefault
 git clone https://github.com/GXC2356/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
