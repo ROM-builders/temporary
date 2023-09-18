6e9d3f53481dd3963a0fdd516192bd1c3336e6a2
Abhinav Kumar <122148266+acex88@users.noreply.github.com>
2023-09-18 10:56:58 +0530

    trigger

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,14 @@
 # sync rom 
- repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android -b 13.0 -g default,-mips,-darwin,-notdefault 
- git clone https://github.com/acex88/local_manifests.git -b sky-pixel .repo/local_manifests 
+ repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 13.0 -g default,-mips,-darwin,-notdefault 
+ git clone https://github.com/acex88/local_manifest.git --depth 1 -b sky-pixel .repo/local_manifests 
  repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
+  
   
  # build rom 
  source build/envsetup.sh 
- lunch lineage_sky-eng
- export TZ=Asia/Kolkata #put before last build command 
+ lunch lineage_sky-user 
+ export TARGET_DISABLE_EPPE := true 
+ export TZ=Asia/Dhaka #put before last build command 
  mka bacon 
   
  # upload rom (if you don't need to upload multiple files, then you don't need to edit next line) 
