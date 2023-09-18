61843c942269d98a6dc0c3e0e3801b4e6756af99
Abhinav Kumar <122148266+acex88@users.noreply.github.com>
2023-09-18 06:24:34 +0530

    trigger

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,3 @@
-# sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 13.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/acex88/local_manifest.git --depth 1 -b sky-pixel .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
@@ -13,6 +12,6 @@ export KBUILD_BUILD_USER=raizel
 export KBUILD_BUILD_HOST=acex88
 export TZ=Asia/Delhi #put before last build command
 m bacon
-cat out/target/product/lancelot/lancelot.json
+cat out/target/product/sky/sky.json
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
