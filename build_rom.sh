41da884d5dadf6e9c50e67a28b5bd11ba79c4425
swapnil vicky <74442290+SwapnilVicky@users.noreply.github.com>
2023-09-18 08:36:06 +0530

    build pixel thermal

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom#
 . build/envsetup.sh
-lunch bliss_miatoll-user
+lunch bliss_miatoll-userdebug
 export TZ=Asia/Dhaka #put before last build command
 blissify -v miatoll
 
