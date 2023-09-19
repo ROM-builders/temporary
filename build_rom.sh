e789e813265f9c33480f477f2848eed657d41107
krishnaspeace <134842461+krishnaspeace@users.noreply.github.com>
2023-09-19 13:10:58 +0530

    ysl: try to fixing errors

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
 git clone https://github.com/krishnaspeace/local_manifests.git --depth 1 -b ysl .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
