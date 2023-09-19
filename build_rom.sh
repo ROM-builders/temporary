82ad023ec5155dfa0fddccce9f949ab1132b0307
JUSTAPRO69 <72558197+bhavya1245073@users.noreply.github.com>
2023-09-19 10:04:47 +0530

    beryllium-CherishOS-tiramisu-bhavya1245073

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch cherish_beryllium-user
+export BUILD_HOSTNAME=Fountain-build
+export BUILD_USERNAME=aidiot69
 export ALLOW_MISSING_DEPENDENCIES=true
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export TZ=Asia/Dhaka #put before last build command
