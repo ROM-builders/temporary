a0488970be2ea2345a983c1b3d0c0fd214e888ac
JUSTAPRO69 <72558197+bhavya1245073@users.noreply.github.com>
2023-09-19 09:59:22 +0530

    beryllium-CherishOS-tiramisu-bhavya1245073

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b tiramisu --git-lfs -g default,-mips,-darwin,-notdefault
 git clone https://github.com/bhavya1245073/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_beryllium-userdebug
+lunch cherish_beryllium-user
 export ALLOW_MISSING_DEPENDENCIES=true
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export TZ=Asia/Dhaka #put before last build command
