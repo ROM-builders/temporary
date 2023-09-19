6ba428a8c9123dbb1d22de705b5c5793eb9ae6e0
Yoni Asleno Farid <92686606+aslenofarid@users.noreply.github.com>
2023-09-19 07:56:17 +0700

    Lah?

--- build_rom.sh
+++ build_rom.sh
@@ -1,9 +1,9 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-17.1 -g default,-mips,-darwin,-notdefault
-git clone --recursive https://github.com/aslenofarid/local_manifest.git --depth 1 -b lineage-17.1 .repo/local_manifests
+git clone https://github.com/aslenofarid/local_manifest.git --depth 1 -b lineage-17.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build roms
+# build rom
 source build/envsetup.sh
 lunch lineage_X00TD-userdebug
 export BUILD_HOSTNAME=android-build
