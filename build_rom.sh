ac18ec22f6f9ab343e18f130f4b442beefdec5fb
Jax Leo <sd369888@outlook.com>
2023-09-19 12:24:32 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -9,7 +9,7 @@ export TZ=Asia/Chungking #put before last build command
 export BUILD_USERNAME=weaponmasterjax
 export BUILD_HOSTNAME=jax
 lunch evolution_everpal-userdebug
-mka bacon
+mka evolution
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
