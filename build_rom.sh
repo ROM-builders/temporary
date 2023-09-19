4441258a7cef2a882c73bda8f74719d7c0235850
Abhinav Kumar <122148266+acex88@users.noreply.github.com>
2023-09-20 05:13:07 +0530

    Trigger

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/androi
 git clone https://github.com/acex88/local_manifest.git --depth 1 -b crdroid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
   
-# build rom
+# build rom 
 . build/envsetup.sh
 lunch lineage_lancelot-userdebug 
 export BUILD_USERNAME=raizel
