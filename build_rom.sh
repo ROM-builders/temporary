5f29a071c6a25639d0ff8467c6a98a0602949201
Hakan <hknhzl@gmail.com>
2023-09-22 15:43:10 +0300

    Trigger build

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/Lafactorial/local_manifest.git --depth 1 -b alpha .
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+source build/envsetup.sh 
 lunch lineage_tissot-userdebug
 export TZ=Asia/Dhaka #put before last build command
 m bacon
