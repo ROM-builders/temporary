8b662afaf4b67b65f372c8fcad2db5265afe0851
noobprjkt <94937447+bronya30@users.noreply.github.com>
2023-09-22 08:34:43 -0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom [65]
 source build/envsetup.sh
-lunch lineage_certus64-userdebug
+lunch lineage_certus-userdebug
 make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
