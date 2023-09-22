7c30592fe7d76a7c755b46882118af7276c25040
Akshat Singh <akshatleo9456@gmail.com>
2023-09-22 12:55:21 +0530

    Initial Pixys

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixysOS/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Legendleo90/local_manifest.git --depth 1 -b pixys .repo/local_manifests
+git clone https://github.com/Legendleo90/local_manifests.git --depth 1 -b pixys-ginkgo .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch pixys_beryllium-user
 export BUILD_USERNAME=Akshat
-export BUILD_HOSTNAME=leopubglover
+export BUILD_HOSTNAME=Legendleo90
 export TZ=Asia/Dhaka #put before last build command
 make pixys
 
