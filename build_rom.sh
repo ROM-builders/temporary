38702c21b6975d42c1befd2e0cef63acdd2d008d
swapnil vicky <74442290+SwapnilVicky@users.noreply.github.com>
2023-09-22 15:00:02 +0530

    fix sync again

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_miatoll-user
+lunch lineage_miatoll-userdebug
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
