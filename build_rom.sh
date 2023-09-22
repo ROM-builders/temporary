7cf3e79b66b9790e99d5128245a7fd1da2a4482d
noobprjkt <94937447+bronya30@users.noreply.github.com>
2023-09-22 08:12:48 -0700

    test

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/RisingTechOSS/android -b thirteen --git-lfs -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/alphadroid-project/manifest -b alpha-13 --git-lfs -g default,-mips,-darwin,-notdefault
 git clone https://github.com/bronya30/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom [65]
 source build/envsetup.sh
-lunch lineage_rosy-userdebug
+lunch lineage_certus64-userdebug
 make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
