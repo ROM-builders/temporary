f8020f724c95e900ffc13ee8ad6eff589eb057c3
Ij Israfil <86546896+ij-israfil@users.noreply.github.com>
2023-09-22 22:29:20 +0600

    RMX1901-PixelOS-AOSP-thirteen-IJ

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ij-israfil/local_manifest.git --depth 1 -b evox .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/PixelOS-AOSP/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
+git clone https://github.com/ij-israfil/local_manifest.git --depth 1 -b pixelos .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j4
 
 # build rom
 source build/envsetup.sh
-lunch evolution_RMX1901-userdebug
+lunch aosp_RMX1901-userdebug
 export TZ=Asia/Dhaka #put before last build command#
-mka evolution
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
