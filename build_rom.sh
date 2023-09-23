6edfbb467f1a00d7ec0373f7f9daba474d4fa6ee
Anandhan <anandhasayanan007jb@gmail.com>
2023-09-23 20:39:11 +0530

    Some fixes

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b
 git clone https://github.com/anandhan07/local_manifest.git --depth 1 -b spark-13.0 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom [01]
+# build rom [02]
 . build/envsetup.sh
 lunch spark_vince-user
 export WITH_GAPPS=true
