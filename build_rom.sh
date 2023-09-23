5548539e61ab3d25857c9c277577d593a9c66056
Abhinav Kumar <122148266+acex88@users.noreply.github.com>
2023-09-23 07:48:53 +0530

    Trigger

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom 
  repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 13.0 -g default,-mips,-darwin,-notdefault 
- git clone https://github.com/acex88/local_manifest.git --depth 1 -b crdroid-merlinx.repo/local_manifests 
+ git clone https://github.com/acex88/local_manifest.git --depth 1 -b crdroid-merlinx .repo/local_manifests 
  repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
   
  # build rom 
  source build/envsetup.sh 
- lunch lineage_lancelot-userdebug
+ lunch lineage_merlinx-userdebug
  export TZ=Asia/Dhaka #put before last build command 
  mka bacon
   
