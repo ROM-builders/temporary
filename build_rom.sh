c17ac9e4849275e50d1e80d0e48a5ef42c685c9f
WAR-DEPT <131593996+WAR-DEPT@users.noreply.github.com>
2023-09-23 13:44:03 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_miatoll-userdebug
+export BUILD_USERNAME=itu
+export BUILD_HOSTNAME=itu|가혹한
 export TZ=Asia/Dhaka #put before last build command
 m bacon
 
