eb204b89a04845d49a3d2b42b4115518542a9069
Abhinav Kumar <122148266+acex88@users.noreply.github.com>
2023-09-23 11:06:08 +0530

    Trigger build

--- build_rom.sh
+++ build_rom.sh
@@ -1,18 +1,13 @@
 # sync rom 
- repo init --depth=1 --no-repo-verify -u https://github.com/PixysOS/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault 
- git clone https://github.com/acex88/local_manifest.git --depth 1 -b pixys .repo/local_manifests 
+ repo init --depth=1 --no-repo-verify -u https://github.com/PixysOS/manifest -b thirteen -g default,-mips,-darwin,-notdefault 
+ git clone https://github.com/acex88/local_manifest --depth 1 -b pixys .repo/local_manifests 
  repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 
   
  # build rom  
- . build/envsetup.sh  
- lunch pixys_lancelot-userdebug  
- export BUILD_USERNAME=raizel 
- export BUILD_HOSTNAME=acex88      
- export TARGET_DISABLE_EPPE=true   
- export KBUILD_BUILD_USER=raizel     
- export KBUILD_BUILD_HOST=acex88 
- export TZ=Asia/Delhi #put before last build command 
- m bacon 
- cat out/target/product/lancelot/lancelot.json 
+ source build/envsetup.sh 
+ lunch pixys_lancelot-userdebug 
+ export TZ=Asia/Kolkata #put before last build command 
+ make pixys 
+  
  # upload rom (if you don't need to upload multiple files, then you don't need to edit next line) 
  rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
