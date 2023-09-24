346f9b6101445ef7cfc1ca87eebd8a5c313ed1ce
noobprjkt <94937447+bronya30@users.noreply.github.com>
2023-09-24 06:50:17 -0700

    test

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/alphadroid-project/ma
 git clone https://github.com/bronya30/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom [65]
+# build rom [61]
 source build/envsetup.sh
 lunch lineage_certus-userdebug
 make bacon
