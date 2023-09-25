2014e4bc7dff486cc81ba775fe86fa3b80162576
Priya Ranjan Samal <140955751+eartinity@users.noreply.github.com>
2023-09-25 06:10:33 +0530

    Hm

--- build_rom.sh
+++ build_rom.sh
@@ -7,6 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch aosp_r5x-userdebug
 export TZ=Asia/Kolkata #put before last build command
+export BUILD_BROKEN_MISSING_RRQUIRED_MODULES=true
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
