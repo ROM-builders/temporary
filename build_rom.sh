1d0917f93e5a547cfed96ffec9e4f440caa4e7e4
dogpoopy <qtqtron@gmail.com>
2023-09-26 14:50:35 +0800

    eng build

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 ## build rom
 source build/envsetup.sh
-lunch evolution_vayu-user
+lunch evolution_vayu-eng
 # builder variables
 export BUILD_USERNAME=dogpoopy
 export BUILD_HOSTNAME=linux
