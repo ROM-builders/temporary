6dc460c39f4417792c078bb8923bd31fb28c852d
Yoni Asleno Farid <92686606+aslenofarid@users.noreply.github.com>
2023-09-28 01:02:21 +0700

    Test

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest -b top
 git clone https://github.com/aslenofarid/local_manifest.git --depth 1 -b topaz .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build roms
 source build/envsetup.sh
 lunch aospa_X00TD-userdebug
 export BUILD_HOSTNAME=android-build
