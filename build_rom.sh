db11b9d90b415ae18802a5e4b84ddc96b5a9fd4a
Amrito Karmokar <93515519+Amritokarmokar@users.noreply.github.com>
2023-09-30 01:41:20 +0600

    Gapps

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch arrow_r5x-userdebug
+export ARROW_GAPPS=true
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
