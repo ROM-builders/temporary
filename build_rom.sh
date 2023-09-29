46986c40c7a8e5091c1a79ea7774d628748f6d6f
Aji <79923098+NOTURFRIENDBTW@users.noreply.github.com>
2023-09-29 17:27:37 +0700

    ysl crdroid NOTURFRIENDBTW
    Build crdroid for ysl

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 13.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/NOTURFRIENDBTW/local_manifests.git --depth 1 -b ysl .repo/local_manifests
+git clone https://github.com/NOTURFRIENDBTW/local_manifests --depth 1 -b ysl .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
