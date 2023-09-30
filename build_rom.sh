77826e74777314ce3140296e17ceffc8bc5b79ae
ahmedhridoy371 <40537458+ahmedhridoy371@users.noreply.github.com>
2023-09-30 12:26:46 +0600

    trigger build

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/BlissRoms/platform_manifest.git -b typhoon -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ahmedhridoy371/local_manifest --depth 1 -b BlissRoms-typhoon .repo/local_manifests
+git clone https://github.com/ahmedhridoy371/local_manifest --depth 1 -b 19 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
