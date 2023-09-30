b31ce74a62d9a22cde124abd80bdf4d1491f9172
Asce-XOX <143834552+Asce-XOX@users.noreply.github.com>
2023-09-30 16:06:16 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b thirteen-plus -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Asce-XOX/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Project-Awaken/android_manifest -b triton -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Asce-XOX/local_manifest.git --depth 1 -b awaken .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch aosp_lancelot-userdebug
+lunch awaken_lancelot-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
