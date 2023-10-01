c83b779dc7a897b9e33161a09af69e6d11776fd4
ahmedhridoy371 <40537458+ahmedhridoy371@users.noreply.github.com>
2023-10-01 19:43:53 +0600

    try again

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b 20 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/ahmedhridoy371/local_manifest --depth 1 -b lineage .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
