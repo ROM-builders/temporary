bd190065f4f3c1673175f5e197b10e81723bba52
zaid2sultan <100853058+zaid2sultan@users.noreply.github.com>
2023-10-01 17:40:48 +0530

    Lavender PEX zaid2sultan

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-AOSP/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/zaid2sultan/local_manifests.git --depth 1 -b pixelos13-lav .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExtended/manifest -b thunder -g default,-mips,-darwin,-notdefault
+git clone https://github.com/zaid2sultan/local_manifests.git --depth 1 -b pex13-lav .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -9,7 +9,7 @@ lunch aosp_lavender-userdebug
 export BUILD_USERNAME=Zaid
 export BUILD_HOSTNAME=zaid2sultan
 export TZ=Asia/Kolkata #put before last build command
-make bacon
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
