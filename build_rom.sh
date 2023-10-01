bc501c4b75e51db41398bb87b0493953d36bc9d7
Knotzy - 01 <70868798+angelomds42@users.noreply.github.com>
2023-09-30 23:52:22 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,11 +3,23 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest 
 git clone https://github.com/angelomds42/local_manifest --depth 1 -b evox-rhode .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+#   build  rom  
 source build/envsetup.sh
 lunch evolution_rhode-userdebug
 export TZ=Asia/Kolkata #put before last build command
-m evolution
+export RELAX_USES_LIBRARY_CHECK=true
+export SKIP_ABI_CHECKS=true
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+export SELINUX_IGNORE_NEVERALLOWS=true
+export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
+export BUILD_BROKEN_VERIFY_USES_LIBRARIES=true
+export BUILD_BROKEN_DUP_RULES=true
+export BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE=true
+export RELAX_USES_LIBRARY_CHECK=true
+export SKIP_ABI_CHECKS=true
+export ALLOW_MISSING_DEPENDENCIES=true
+mka evolution
+
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
