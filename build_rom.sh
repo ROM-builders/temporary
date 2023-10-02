a2523cf180b15a7f1181657290a4bf051da89d8e
Joy <141945444+j0YCE-B@users.noreply.github.com>
2023-10-02 16:08:42 +0530

    Mi439-PixelExperience-j0YCE-B

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/j0YCE-B/local_manifest.git --depth 1 -b main .repo/
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source . build/envsetup.sh
+source build/envsetup.sh
 lunch aosp_mi439-user
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
