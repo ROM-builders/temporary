e980ce22adf5bbd3d861ccc8a4294ef8881d3902
newestzdn <127198882+newestzdn@users.noreply.github.com>
2023-10-02 09:42:03 +0700

    Initial CarbonROM 9.0 build!

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 11.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/zaidannn7/local_manifest --depth 1 -b cr .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/CarbonROM/android -b cr-9.0 --git-lfs -g default,-mips,-darwin,-notdefault
+git clone https://github.com/zaidannn7/local_manifest --depth 1 -b carbonnew .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_juice-userdebug
+lunch carbon_juice-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export RELAX_USES_LIBRARY_CHECK=true
@@ -21,10 +21,7 @@ export BUILD_HOSTNAME=scaledzdn.
 export KBUILD_BUILD_NAME=scaledzdn.
 export KBUILD_BUILD_HOST=scaledzdn.
 export TZ=Asia/Jakarta #put before last build command
-mka bacon
-
-
-
+make carbon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
