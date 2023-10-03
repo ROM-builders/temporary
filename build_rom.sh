c3d28fef5f46d7e5aa647ec1fd774251960b4cf9
anuragnn <121393902+anuragnn@users.noreply.github.com>
2023-10-03 17:26:15 +0545

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b thirteen-plus -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs -g default,-mips,-darwin,-notdefault
 git clone https://github.com/anuragnn/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j4
 
 # build rom
 source build/envsetup.sh
-lunch aosp_selene-userdebug
+lunch lineage_selene-eng
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
