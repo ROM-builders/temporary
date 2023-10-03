37d5da141be8eac3b82bc64f5d99a483673eafe0
ahmedhridoy371 <40537458+ahmedhridoy371@users.noreply.github.com>
2023-10-03 07:21:29 +0600

    trigger again

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/RisingTechOSS/android -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ahmedhridoy371/local_manifest --depth 1 -b 20 .repo/local_manifests
+git clone https://github.com/ahmedhridoy371/local_manifest --depth 1 -b rise-1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
