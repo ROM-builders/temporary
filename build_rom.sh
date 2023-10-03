621241eded0da517fdb62cbda5e7041dd8beaf47
Briant Audiera <54709835+briant24@users.noreply.github.com>
2023-10-03 08:51:41 +0700

    kenzo-ArrowOS-11-briant24
    Custom Rom for Xiaomi Redmi Note 3

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,14 @@
 # sync rom
 repo init -u https://github.com/ArrowOS/android_manifest.git -b arrow-11.0
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
-git clone https://github.com/briant24/local_manifest.git --depth 1 -b master
+git clone https://github.com/briant24/local_manifest.git --depth 1 -b master .repo/local_manifest
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-. build/envsetup.sh
+source build/envsetup.sh
 lunch arrow_kenzo-userdebug
 export TZ=Asia/Dhaka #put before last build command
+export BUILD_USERNAME=briant24
+export BUILD_HOSTNAME=CirrusCi
 m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
