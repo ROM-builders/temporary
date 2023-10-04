970db90569910e560aec2bd430be33d9ceb73eb0
Misakiayasaka <147002710+Misakiayasaka@users.noreply.github.com>
2023-10-05 06:12:29 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/exthmui-legacy/android.git -b exthm-10 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/exthmui/android.git -b exthm-10 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Misakiayasaka/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch exthm_RMX2185-user
+lunch exthm_RMX2185-userdebug
 export TZ=Asia/Bangkok #put before last build command
+breakfast RMX2185
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
