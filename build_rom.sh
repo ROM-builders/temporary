9c3d428c851abf10f55478d089cf7a31cfe6d5d0
ahmedhridoy371 <40537458+ahmedhridoy371@users.noreply.github.com>
2023-10-04 19:36:33 +0600

    trigger oct build

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ahmedhridoy371/local_manifest --depth 1 -b test .repo/local_manifests
+git clone https://github.com/ahmedhridoy371/local_manifest --depth 1 -b DerpFest-AOSP-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
