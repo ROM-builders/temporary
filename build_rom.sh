c92291a773524c4960c9f3b8e9723769c5f88912
Akshat Singh <akshatleo9456@gmail.com>
2023-10-04 20:59:05 +0530

    Gapps build

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch cherish_beryllium-user
-export CHERISH_VANILLA=true
+export CHERISH_VANILLA=false
 export BUILD_USERNAME=Akshat
 export BUILD_HOSTNAME=Legendleo90
 export TZ=Asia/Kolkata #put before last build command
