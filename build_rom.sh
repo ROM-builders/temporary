71810047c0246b2b6869a58edf504a52e6ff02b5
dreAd <68554241+dreAd1811@users.noreply.github.com>
2023-10-05 20:54:52 +0530

    moba: switch to lineage

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest.git -b eleven -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/dreAd1811/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
@@ -10,7 +10,7 @@ chmod 777 kernel/lenovo/moba/build.sh
 # Run this script to fix broken symlnink errors 
 bash kernel/lenovo/moba/build.sh
 # Build Variant - eng
-lunch aosp_moba-eng
+lunch lineage_moba-eng
 
 # Start Building!
 mka bacon
