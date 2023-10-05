e930277b792dfd381da7d8c2e8420b361bc49ec9
Nikhil <Naruto242@proton.me>
2023-10-05 15:44:27 +0530

    Updated Rom Source link

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://PixelExperience/manifest.git -b thirteen-plus -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://PixelExperience/manifest.git -b thirteen-plus -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Naruto-242/local_manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
