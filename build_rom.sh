77ad2533ef57d5f8f1f111b44ccd6fbaf53d1170
Gio <86983840+giosaja96@users.noreply.github.com>
2023-10-05 22:43:52 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/AICP/platform_manifes
 git clone https://github.com/giosaja96/local_manifest.git --depth 1 -b aicp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom 2
+# build rom 3
 source build/envsetup.sh
 lunch aicp_mojito-userdebug
 export TZ=Asia/Jakarta #put before last build command
