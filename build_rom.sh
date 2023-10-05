30a6d30a84aa40e52f35f8e7ba55548de16d3d24
Hassan Karim <144518310+Hassan-kareem@users.noreply.github.com>
2023-10-05 11:56:25 +0530

    Mido-LineageOS-lineage-20.0-Hassan-kareem

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-17.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Hassan-kareem/Local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_a5ltedd-userdebug
-export TZ=Asia/Jharkhand #put before last build command
+lunch lineage_mido-userdebug
+export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
