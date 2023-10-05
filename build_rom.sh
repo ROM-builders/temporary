400c7e7f6b6fad1a9d9d535e05ffdd072665391d
Misakiayasaka <147002710+Misakiayasaka@users.noreply.github.com>
2023-10-05 02:06:09 +0000

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init -u https://github.com/exthmui/android.git -b exthm-10 --depth=1 --repo-url=https://mirrors.bfsu.edu.cn/git/git-repo
+repo init -u https://github.com/exthmui/android.git -b exthm-10 --depth=1 --repo-url=https://mirrors.bfsu.edu.cn/git/git-repo -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Misakiayasaka/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
