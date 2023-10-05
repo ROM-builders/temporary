74bd1dcf3c14de4cc00aaa03647db2d2ecac60c0
Misakiayasaka <147002710+Misakiayasaka@users.noreply.github.com>
2023-10-05 11:06:04 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,13 @@
 # sync rom
-repo init -u https://github.com/exthmui/android.git -b exthm-10 --depth=1 -g default,-mips,-darwin,-notdefault --repo-url=https://mirrors.bfsu.edu.cn/git/git-repo
-git clone https://github.com/Misakiayasaka/local_manifest --depth=1 -b main .repo/local_manifest
+repo init --depth=1 --no-repo-verify -u git://github.com/CipherOS/android_manifest.git -b eleven -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/Misakiayasaka/local_manifests --depth=1 -b main .repo/local_manifest
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch exthm_RMX2185-userdebug
+lunch lineage_RMX2185-eng
 export TZ=Asia/Bangkok #put before last build command
-breakfast RMX2185
-mka bacon
+mka bacon -j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
