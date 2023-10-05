b2fd88b5805454fb5e07c31123f0da0929f153db
dreAd <68554241+dreAd1811@users.noreply.github.com>
2023-10-05 19:31:13 +0530

    build_rom: PE A11 for moba

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,21 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+# PE  A11 Repo
+repo init --depth=1 --no-repo-verify -u git://github.com/PixelExperience/manifest -b 11 -g default,-mips,-darwin,-notdefault
+# My local Manifests
+git clone https://github.com/dreAd1811/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
-export TZ=Asia/Dhaka #put before last build command
-mka derp
+# Give permission to create smylinks
+chmod 777 kernel/lenovo/moba/build.sh
+# Run this script to fix broken symlnink errors 
+bash kernel/lenovo/moba/build.sh
+# Build Variant - eng
+lunch aosp_moba-eng
+
+# Start Building!
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
