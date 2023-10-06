64eaea21fd249b295e33566ae72a73f83fcbae56
ahmedhridoy371 <40537458+ahmedhridoy371@users.noreply.github.com>
2023-10-07 04:19:02 +0600

    trigger again

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ahmedhridoy371/local_manifest --depth 1 -b DerpFest-AOSP-13 .repo/local_manifests
+git clone https://github.com/ahmedhridoy371/local_manifest --depth 1 -b derp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -14,3 +14,4 @@ mka derp
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+ 
