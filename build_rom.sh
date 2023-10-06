5eb35c5ab06977a4e6ca96baf78187ab1b7edb26
Hakan <hknhzl@gmail.com>
2023-10-06 08:08:50 +0300

    Trigger build

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/alphadroid-project/ma
 git clone https://github.com/Lafactorial/local_manifest.git --depth 1 -b alpha .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom 
 source build/envsetup.sh 
 lunch lineage_tissot-userdebug
 export TZ=Asia/Dhaka #put before last build command
