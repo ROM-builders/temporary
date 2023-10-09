fdc6f32bb098b5eb9afb6995b6a165aceb258b0d
ahmedhridoy371 <40537458+ahmedhridoy371@users.noreply.github.com>
2023-10-09 19:34:03 +0600

    Trigger Build

--- build_rom.sh
+++ build_rom.sh
@@ -9,9 +9,10 @@ lunch derp_lavender-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
 export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
 export BUILD_BROKEN_DUP_RULES=true
+export TARGET_USES_BLUR=false
+export WITH_GMS=false
 export TZ=Asia/Jakarta #put before last build command
 mka derp
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-
