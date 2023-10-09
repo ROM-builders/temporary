4774a4c9e0efa85a5f1146b259f65472d0445ba2
Endi Priyatna <65471980+Enprytna@users.noreply.github.com>
2023-10-09 12:37:01 +0700

    Fix build make

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch derp_ginkgo-user
+lunch derp_ginkgo-userdebug
 export TZ=Asia/Jakarta
 mka derp
 
