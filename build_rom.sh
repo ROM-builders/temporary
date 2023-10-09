336869808de0fa107ad0afbbb6e93c08a2208db6
sdalt <142908089+sdalt@users.noreply.github.com>
2023-10-09 20:26:49 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -10,5 +10,6 @@ export TZ=Asia/Kolkata #put before last build command
 mka systemimage
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
-zip out/target/product/arm64_bgN/ArrowOS-arrow-13.1-$(date +%Y%m%d)-SoundDrillGSI.zip  out/target/product/*/system.img -j
+cp out/target/product/*/system.img . 
+zip out/target/product/arm64_bgN/ArrowOS-arrow-13.1-$(date +%Y%m%d)-SoundDrillGSI.zip system.img  
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
