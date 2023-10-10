67c43cf3927595bf1acc2e951ab322491d50fdd5
omanshkrishn <138091069+omanshkrishn@users.noreply.github.com>
2023-10-10 08:38:47 +0530

    retrigger

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/alphadroid-project/ma
 git clone https://codeberg.org/omansh-krishn/local_manifest.git --depth 1 -b alpha-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-#build rom
+# build rom
 
 source build/envsetup.sh
 lunch lineage_santoni-user
@@ -21,7 +21,7 @@ export KBUILD_BUILD_USER=OmanshKrishn
 export KBUILD_BUILD_HOST=pc
 export TZ=Asia/Kolkata
 
-make bacon
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
