d89d59110ae030c54adc8d53b5b67aca87aefe6d
Ij Israfil <86546896+ij-israfil@users.noreply.github.com>
2023-10-10 17:39:29 +0600

    RMX1901-SuperiorOS-thirteen-Israfil

--- build_rom.sh
+++ build_rom.sh
@@ -10,7 +10,8 @@ export KBUILD_BUILD_USER=Israfil
 export KBUILD_BUILD_HOST=linux
 export BUILD_USERNAME=Israfil
 export BUILD_HOSTNAME=linux
-export TZ=Asia/Dhaka #put before last build command#
+export TZ=Asia/Dhaka #put before last build command
+m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
