cdb95d261078d027209ef41aea24c3e76e56cb4c
Matheus Souza <33396386+matheucomth@users.noreply.github.com>
2023-10-10 11:28:50 -0300

    sep/oct update

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync roms
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b thirteen-plus -g default,-mips,-darwin,-notdefault
-git clone https://github.com/matheucomth/local_manifest.git --depth 1 -b PixelOS .repo/local_manifests
+git clone https://github.com/matheucomth/local_manifest.git --depth 1 -b PE .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build roms
