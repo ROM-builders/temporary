0f45931d609a5122c7004552ac57b95408b2dc68
Rakib Hossain <101888982+iamrh1819@users.noreply.github.com>
2023-10-11 17:07:52 +0600

    a10-ProjectBlaze-14-iamrh1819
    Build blaze for samsung a10

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/ProjectBlaze/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/ProjectBlaze/manifest -b 14 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/iamrh1819/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
