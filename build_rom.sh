6cc1bd0a67528072aa7d3b9cbec4d0d1581d1bf7
Abhishek Shukla <91975183+abhis699@users.noreply.github.com>
2023-10-12 20:07:16 +0530

    Initial A14 test

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b udc -g default,-mips,-darwin,-notdefault
 git clone https://github.com/abhis699/local_manifest.git --depth 1 -b evolution-x-rmx3371 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
