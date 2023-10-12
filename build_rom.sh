462e38ce4d8e3a0ab18b7c485a6354d666b31752
lopiswe <147716817+lopiswe@users.noreply.github.com>
2023-10-12 18:48:54 +0700

    lancelot-CipherOS-thirteen-lopiswe

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/CipherOS/android_mani
 git clone https://github.com/kitw4y/local_manifest.git --depth 1 -b cipher14 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom 
 source build/envsetup.sh
 lunch cipher_lancelot-userdebug
 export TZ=Asia/Dhaka #put before last build command
