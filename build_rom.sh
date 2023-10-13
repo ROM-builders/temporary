07d042085cc82b928a41ee1b81064225a2486861
YUDI <112261445+bdjmfoe@users.noreply.github.com>
2023-10-13 14:40:00 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ResurrectionRemix/platform_manifest -b Q -g default,-mips,-darwin,-notdefault
 git clone https://github.com/bdjmfoe/local_manifests --depth 1 -b rr .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j2
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j4
 
 # build rom 
 . build/envsetup.sh
