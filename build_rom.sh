c9075a889598a70168704367a30f86b68715f6f6
Manojrauniyar65 <139572254+Manojrauniyar65@users.noreply.github.com>
2023-10-13 19:19:25 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,15 @@
 
 ## sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/AICP/platform_manifest.git -b t13.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/rabikishan000/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.git -b thirteen-g default,-mips,-darwin,-notdefault
+git clone https://github.com/RT1648/local_manifest-fleurprebuilt.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch AICP_fleur-userdebug 
+lunch superior_fleur-user
+rm -rf system/sepolicy/vendor/hal_fingerprint_default.te && rm -rf system/sepolicy/vendor/hal_health_default.te && rm -rf system/sepolicy/vendor/rild.te && rm -rf system/sepolicy/vendor/hal_light_default.te && rm -rf system/sepolicy/vendor/mediacodec.te
 export TZ=Asia/Dhaka #put before last build command
-mka AICP
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
