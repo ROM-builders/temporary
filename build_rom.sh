bec282628a6e34ee925f93ba2708b00e96c6b4eb
ahmedhridoy371 <40537458+ahmedhridoy371@users.noreply.github.com>
2023-10-13 11:46:01 +0600

    server error

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,14 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ahmedhridoy371/local_manifest --depth 1 -b ch .repo/local_manifests
+git clone https://github.com/ahmedhridoy371/local_manifest.git -b ch .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch cherish_lavender-userdebug
-export ALLOW_MISSING_DEPENDENCIES=true
-export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
-export BUILD_BROKEN_DUP_RULES=true
-export TZ=Asia/Jakarta #put before last build command
-brunch lavender
+export CHERISH_VANILLA=true
+export TZ=Asia/Kolkata #put before last build command
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
