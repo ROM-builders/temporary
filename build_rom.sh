e86d86fc7cec604dad56ede4ba3ea912ef4c2366
Rakib Hossain <101888982+iamrh1819@users.noreply.github.com>
2023-10-13 09:18:58 +0600

    a10-DerpFest-AOSP-13-iamrh1819

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/AICP/platform_manifest.git -b t13.0 --git-lfs -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-AOSP/manifest.git -b 14 --git-lfs -g default,-mips,-darwin,-notdefault
 git clone https://https://github.com/iamrh1819/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
-lunch aicp_a10-eng
-make bacon
+. build/envsetup.sh
+lunch derp_$a10-user
+mka derp
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
