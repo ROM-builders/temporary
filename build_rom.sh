84b2ded7043dff5c399ae3aae00094bf67d6a748
igniteinm <111630059+igniteinm@users.noreply.github.com>
2023-10-14 07:10:41 +0530

    Final fix

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/igniteinm/local_manifest.git --depth 1 -b van .repo
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+source build/envsetup.sh 
 lunch lineage_beryllium-user
 export TZ=Asia/Delhi #put before last build command
 mka bacon
