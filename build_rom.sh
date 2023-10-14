2137b021c6f62e8f3e997f772bf50ed34db62619
Manojrauniyar65 <139572254+Manojrauniyar65@users.noreply.github.com>
2023-10-14 13:12:01 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 
 ## sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Manojrauniyar65/manifest.git -b tiramisu -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Manojrauniyar65/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
 git clone https://github.com/RT1648/local_manifest-fleurprebuilt.git --depth 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
