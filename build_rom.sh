0eaa5b457f7fb612e789ecd20affa3c63fc68898
Manojrauniyar65 <139572254+Manojrauniyar65@users.noreply.github.com>
2023-10-15 00:37:34 +0530

    Some fixes

--- build_rom.sh
+++ build_rom.sh
@@ -8,7 +8,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch evolution_fleur-user
 source device/xiaomi/fleur/prebuilts/*
-export TZ=Asia/Dhaka #put before last build command
+export TZ=Asia/Dhaka #put before last build command 
 mka evolution
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
