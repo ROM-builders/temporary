34047bf88e16c9e4ffbdedacff6f7299452ab71e
Rakib Hossain <101888982+iamrh1819@users.noreply.github.com>
2023-10-15 13:04:15 +0600

    a10-ProjectBlaze-13-iamrh1819

--- build_rom.sh
+++ build_rom.sh
@@ -8,9 +8,6 @@ repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --optimiz
 . build/envsetup.sh
 lunch blaze_a10-userdebug
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true 
-export ALLOW_MISSING_DEPENDENCIES=true
-export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
-export BUILD_BROKEN_DUP_RULES=true 
 make bacon
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
