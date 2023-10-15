3071367e5749b5e58f31bd8e60da78b62fc9102f
Rakib Hossain <101888982+iamrh1819@users.noreply.github.com>
2023-10-15 08:28:18 +0600

    a10-ProjectBlaze-13-iamrh1819

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,16 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.git -b thirteen --git-lfs -g default,-mips,-darwin,-notdefault
-git clone https://github.com/iamrh1819/local_manifest.git --depth 1 -b SuperiorOS-thirteen .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/iamrh1819/local_manifest.git --depth 1 -b ProjectBlaze-13 .repo/local_manifests
 #repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune
 
 # build rom
 . build/envsetup.sh
-lunch superior_a10-userdebug
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES= true 
+lunch blaze_a10-userdebug
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true 
 export ALLOW_MISSING_DEPENDENCIES=true
 export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
 export BUILD_BROKEN_DUP_RULES=true 
-m bacon
+make bacon
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
