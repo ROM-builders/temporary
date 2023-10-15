19857fffc20c50c61912963aeacf670097401012
newestzdn <127198882+newestzdn@users.noreply.github.com>
2023-10-15 21:19:49 +0700

    test

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_lime-userdebug
+lunch lineage_lime-user
 export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export RELAX_USES_LIBRARY_CHECK=true
@@ -22,6 +22,7 @@ export KBUILD_BUILD_NAME=scaledzdn.
 export KBUILD_BUILD_HOST=scaledzdn.
 export TZ=Asia/Jakarta #put before last build command
 mka bacon
+
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
