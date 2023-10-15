e878ef2bec5c3f4e55855f51436c5fe0327ecbde
Gio <86983840+giosaja96@users.noreply.github.com>
2023-10-15 11:25:51 +0700

    build bliss

--- build_rom.sh
+++ build_rom.sh
@@ -1,17 +1,17 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/AICP/platform_manifest.git -b t13.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/giosaja96/local_manifest.git --depth 1 -b aicp .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/BlissRoms/platform_manifest.git -b typhoon -g default,-mips,-darwin,-notdefault
+git clone https://github.com/giosaja96/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build aicp 12
+# build bliss
 source build/envsetup.sh
 lunch aicp_mojito-userdebug
 export TZ=Asia/Jakarta #put before last build command
-export KBUILD_BUILD_USER=gioaprilino
-export KBUILD_BUILD_HOST=aicp
-export BUILD_USERNAME=gioaprilino
-export BUILD_HOSTNAME=aicp
-mka bacon
+export KBUILD_BUILD_USER=dioparminggo
+export KBUILD_BUILD_HOST=bliss
+export BUILD_USERNAME=dioparminggo
+export BUILD_HOSTNAME=bliss
+bllisify -g mojito
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
