aa84fef3e8315b8aaf0759b941e08b653216cb04
Rakib Hossain <101888982+iamrh1819@users.noreply.github.com>
2023-10-15 06:20:45 +0600

    a10-SuperiorOS-thirteen-iamrh1819

--- build_rom.sh
+++ build_rom.sh
@@ -11,6 +11,6 @@ export BUILD_BROKEN_MISSING_REQUIRED_MODULES= true
 export ALLOW_MISSING_DEPENDENCIES=true
 export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
 export BUILD_BROKEN_DUP_RULES=true 
-m bacon -j$(nproc --all)
+m bacon
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
