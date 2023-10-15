9d6c492629ae3cddf98e7c5259a108c5076c720b
igniteinm <111630059+igniteinm@users.noreply.github.com>
2023-10-15 08:11:20 +0530

    Fix sync

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs -g default,-mips,-darwin,-notdefault
 git clone https://github.com/igniteinm/local_manifest.git --depth 1 -b van .repo/local_manifests
-repo sync --force-sync -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom 
 source build/envsetup.sh 
