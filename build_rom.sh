85f0112d1e11d2439a0c8ab568919fdd5e0dad87
Suraj Kumar <surajkumar18jun2004@gmail.com>
2023-10-16 10:51:42 +0530

    Oct Patch

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom
+# sync rom 
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Stable-Stuffs/manifest --depth 1 -b Pe-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
