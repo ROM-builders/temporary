d9bb2356e49cb35f1c79dcfbd638f7c01a9d7890
Rakib Hossain <101888982+iamrh1819@users.noreply.github.com>
2023-10-16 13:01:33 +0600

    a10-alphadroid-project-alpha-13-iamrh1819

--- build_rom.sh
+++ build_rom.sh
@@ -9,7 +9,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 lunch lineage_a10-user_vanilla
 export BUILD_USERNAME=Rakib
 export BUILD_HOSTNAME=iamrh1819
-export TZ=Asia/Jakarta
+export TZ=Asia/Dhaka
 make bacon
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
