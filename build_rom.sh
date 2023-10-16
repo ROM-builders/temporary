37c4d1a623b2a2a44eeb8ff5be555c96e9892f3a
Rakib Hossain <101888982+iamrh1819@users.noreply.github.com>
2023-10-16 12:54:15 +0600

    a10-alphadroid-project-alpha-13-iamrh1819

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/iamrh1819/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/alphadroid-project/manifest -b alpha-13 --git-lfs -g default,-mips,-darwin,-notdefault
+git clone https://github.com/iamrh1819/local_manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 #repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune
 
 # build rom
 . build/envsetup.sh
-lunch blaze_a10-userdebug
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+lunch lineage_a10-user_vanilla
 export BUILD_USERNAME=Rakib
 export BUILD_HOSTNAME=iamrh1819
-export TZ=Asia/Dhaka
+export TZ=Asia/Jakarta
 make bacon
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
