7db259ee77b8059ed7a11499dc3acbeadf467a7c
Hassan Karim <144518310+Hassan-kareem@users.noreply.github.com>
2023-10-18 16:03:29 +0530

    a5lte-LineageOS-lineage-18.1-hassan-kareem

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-17.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-18.1 --git-lfs -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Hassan-kareem/Local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_a5ultexx-userdebug
+lunch lineage_a5lte-userdebug
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
