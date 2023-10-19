e2e37083c14898275fd804a4e853d629d8a05af4
Hassan Karim <144518310+Hassan-kareem@users.noreply.github.com>
2023-10-19 09:50:16 +0530

    mido-LineageOS-lineage-20.0-hassan-kareem

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-18.1 --git-lfs -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Hassan-kareem/Local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_a5lte-userdebug
+lunch lineage_mido-userdebug
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
