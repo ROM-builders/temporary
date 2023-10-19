31ed02c100eabe637333143ee317ba8065052f88
Rakib Hossain <101888982+iamrh1819@users.noreply.github.com>
2023-10-19 22:25:21 +0600

    a10-alphadroid-project-alpha-13-iamrh1819

--- build_rom.sh
+++ build_rom.sh
@@ -1,8 +1,8 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/alphadroid-project/manifest -b alpha-13 --git-lfs -g default,-mips,-darwin,-notdefault
-git clone https://github.com/iamrh1819/local_manifest.git --depth 1 -b main .repo/local_manifests
-#repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune
+git clone https://github.com/iamrh1819/local_manifest --depth 1 -b main .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+#repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune
 
 # build rom
 . build/envsetup.sh
