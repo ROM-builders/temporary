12707ee295e5153d468ca2e6cbfe52445fc5f323
Manojrauniyar65 <139572254+Manojrauniyar65@users.noreply.github.com>
2023-10-20 00:43:48 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch evolution_X01BD-user
+lunch evolution_X01BD-userdebug
 export TZ=Asia/Dhaka ##put before last build command
 mka evolution
 
