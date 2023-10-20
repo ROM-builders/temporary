a8c01a04e38e09127356489d3b698c099d1743b2
rianza <rianyanuar53@gmail.com>
2023-10-20 13:37:17 +0700

    build havoc-13

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/RisingTechOSS/android -b thirteen --git-lfs -g default,-mips,-darwin,-notdefault
-git clone https://github.com/rianza/Local_Manifest.git --depth 1 -b rising-13 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Havoc-OS/android_manifest.git -b thirteen --git-lfs -g default,-mips,-darwin,-notdefault
+git clone https://github.com/rianza/Local_Manifest.git --depth 1 -b havoc-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom                                          
