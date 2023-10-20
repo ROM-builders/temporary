fde94a77640f0ff2c6c1ce2a802ed7a1ea63dfd0
Hassan Karim <144518310+Hassan-kareem@users.noreply.github.com>
2023-10-20 16:47:58 +0530

    mido-PixelExperience-thirteen-plus-hassan-kareem

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,10 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch mido_aosp_userdebug
+export BUILD_BROKEN_DUP_RULES=true
+export BUILD_BROKEN_PHONY_TARGETS=true
+export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
+export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
