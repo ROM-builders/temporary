4bea0b4937b7b7aa7ff26d3faaf4697d4bb4d926
Prashant <93838092+Prashant-1695@users.noreply.github.com>
2023-10-20 11:58:32 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,6 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 . build/envsetup.sh
 lunch lineage_lavender-userdebug
 export SELINUX_IGNORE_NEVERALLOWS=true
+export WITH_GMS=true
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
