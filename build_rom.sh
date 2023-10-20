aa86b3504725dda58e83e52c6d036c3ca8285489
Hassan Karim <144518310+Hassan-kareem@users.noreply.github.com>
2023-10-20 09:13:43 +0530

    mido-LineageOS-lineage-16.0-hassan-kareem

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-16.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Hassan-kareem/Local_manifests.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/Hassan-kareem/Local_Manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
