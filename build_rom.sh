4c37add5f38471eb543bfbddce96104bef3b088d
Ritikk11 <144951533+Ritikk11@users.noreply.github.com>
2023-10-21 15:02:03 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,7 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Ritikk11/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
+ 
 # build rom
 source build/envsetup.sh
 lunch superior_lavender-userdebug
