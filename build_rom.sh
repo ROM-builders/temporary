d8a2a4b2f9353c36a506701db4c481a2cc02ffee
Freedom <63868370+Lewewe@users.noreply.github.com>
2023-10-21 20:27:30 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b udc -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/Evolution-X/manifest -b udc -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Arijit78/android_device_xiaomi_whyred_419/manifest.xml --depth 1 -b evo-14 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
