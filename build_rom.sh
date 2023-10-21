8dbc35d7c0285718922b676bd325769b67ff39d9
Ritikk11 <144951533+Ritikk11@users.noreply.github.com>
2023-10-21 20:05:07 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -7,6 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch superior_lavender-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
+export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
