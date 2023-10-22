3f3eff4b23419e3b05d03e5d511bbb15422a877b
mozzaru <rianyanuar52@gmail.com>
2023-10-22 21:52:45 +0700

    test

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/mozzaru/local_manifest.git --depth 1 -b arrow-13.1 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+. build/envsetup.sh
 lunch arrow_markw-userdebug
 export TZ=Asia/Dhaka #put before last build command
 m bacon
