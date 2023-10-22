a2fc3521d5324ec5f2eb260e6805eeac25c01556
Vignesh <122905219+vignesh-arch@users.noreply.github.com>
2023-10-22 08:00:57 +0530

    spes-DerpFest-AOSP-13

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-AOSP/manifest.git -b 14 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-AOSP/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/vignesh-arch/RomBuilding.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
