59ddb2f2ff212e98f6aa86936bc66178cee27947
alecchan02 <132892224+alecchan02@users.noreply.github.com>
2023-10-22 20:53:36 +0800

    Fix dt2w

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-13.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/alecchangod/local_manifest.git --depth 1 -b arrow .repo/local_manifests
+git clone https://github.com/alecchangod/local_manifest.git --depth 1 -b arrow-1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -8,19 +8,7 @@ source build/envsetup.sh
 lunch arrow_monet-eng
 export TZ=Asia/Dhaka #put before last build command
 make bacon
-# 5
+# 7
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-
-
-
-
-
-
-
-
-
-
-
-
 
