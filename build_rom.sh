441c4366bd5c6e0d34a2e1ac700aa6dc926caee4
mozzaru <rianyanuar52@gmail.com>
2023-10-22 21:58:42 +0700

    test

--- build_rom.sh
+++ build_rom.sh
@@ -4,10 +4,10 @@ git clone https://github.com/mozzaru/local_manifest.git --depth 1 -b arrow-13.1 
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-. build/envsetup.sh
+source build/envsetup.sh
 lunch arrow_markw-userdebug
 export TZ=Asia/Dhaka #put before last build command
-m bacon
-
+make bacon
+# 7
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
