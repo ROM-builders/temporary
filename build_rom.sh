4549eccb3a2c2375e6a869bc1616de429014bd3e
Hassan Karim <144518310+Hassan-kareem@users.noreply.github.com>
2023-10-23 13:11:50 +0530

    mido-PixelExperience-thirteen-plus-hassan-kareem

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b thirteen-plus -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Hassan-kareem/local_manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_a5lte-userdebug
+lunch aosp_mido-userdebug
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
