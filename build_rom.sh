6a4779d55384f0d341906a014384e07f8c4dd84d
Hassan Karim <144518310+Hassan-kareem@users.noreply.github.com>
2023-10-24 23:33:36 +0530

    mido-LineageOS-lineage-20.0-hassan-kareem

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_mido-user
+lunch lineage_mido-userdebug
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
