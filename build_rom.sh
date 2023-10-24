5b1c2efe26bb2afd4e56ec8f2ce6f7c2b50dda3a
Ritikk11 <144951533+Ritikk11@users.noreply.github.com>
2023-10-24 18:24:06 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.git -b ten -g default,-mips,-darwin,-notdefault
+repo init -u https://github.com/SuperiorOS/manifest.git -b ten -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Ritikk11/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
