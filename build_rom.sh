60c6df723e9ac5a772289dae7b3e375b71c3778e
alecchan02 <132892224+alecchan02@users.noreply.github.com>
2023-10-24 09:59:00 +0800

    RELAX_USES_LIBRARY_CHECK=true

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch arrow_monet-userdebug
+export RELAX_USES_LIBRARY_CHECK=true
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 # 8
