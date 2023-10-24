8e947fb3d80709f4305994628345d4861ae39b69
zaid2sultan <100853058+zaid2sultan@users.noreply.github.com>
2023-10-24 20:30:58 +0530

    SuperiorOS thirteen lavender

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,16 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExtended/manifest -b thunder -g default,-mips,-darwin,-notdefault
-git clone https://github.com/zaid2sultan/local_manifests.git --depth 1 -b aosp13 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
+git clone https://github.com/zaid2sultan/local_manifests.git --depth 1 -b temp-sup .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch aosp_fajita-userdebug
+lunch superior_lavender-userdebug
 export BUILD_USERNAME=Zaid
 export BUILD_HOSTNAME=zaid2sultan
+#export BUILD_WITH_GAPPS=true
 export TZ=Asia/Kolkata #put before last build command
-mka bacon
+m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
