e102aa29f16c91227d040f09d3fc853ffdb76172
JV007xp <114124246+JV007xp@users.noreply.github.com>
2023-10-25 17:45:32 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,18 +1,15 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/JV007xp/local-manifest.git --depth 1 -b Lineage19 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Havoc-OS/android_manifest.git -b eleven -g default,-mips,-darwin,-notdefault
+git clone https://github.com/raidenkkj/local_manifest.git --depth 1 -b a001d-havoc .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
-. build/envsetup.sh
-lunch lineage_A001D-userdebug
+# build rom #2
+source build/envsetup.sh
+lunch havoc_A001D-userdebug
 export TZ=America/Sao_Paulo
-export SELINUX_IGNORE_NEVERALLOWS=true
 export ALLOW_MISSING_DEPENDENCIES=true
-
-make bacon
+m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-
 
