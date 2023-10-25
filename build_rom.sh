00f7190e0016f2a8c0886a40e5a715c62f17fc71
dogpoopy <103020425+dogpoopy@users.noreply.github.com>
2023-10-26 07:06:09 +0800

    fixup

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch blaze_vayu-userdebug
+lunch blaze_vayu-user
+export WITH_GAPPS=true
 export TZ=Asia/Manila
 make bacon
 
