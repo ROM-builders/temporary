c3e5a5e4d3efba190243b4831bd94b044dd97555
omanshkrishn <138091069+omanshkrishn@users.noreply.github.com>
2023-10-25 20:12:32 +0530

    fixed overlays

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,6 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest.
 git clone https://codeberg.org/omansh-krishn/local_manifest.git --depth 1 -b evolution-x .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-
 # build rom
 
 source build/envsetup.sh
