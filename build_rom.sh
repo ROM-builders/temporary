14c822ea9b372aaf73e898a1ac05eb0465da1f8f
iamrh420 <92685435+iamrh420@users.noreply.github.com>
2023-10-26 02:15:03 +0600

    a10-alphadroid-project-alpha-13-iamrh420

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/alphadroid-project/manifest -b alpha-13 --git-lfs -g default,-mips,-darwin,-notdefault
-git clone https://github.com/iamrh420/local_manifest.git --depth 1 -b main .repo/local_manifest
+git clone https://github.com/iamrh420/local_manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
