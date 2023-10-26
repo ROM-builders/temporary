a081349ad0ac5d1861ad03bf745a28783b607e78
Tejas Singh <tejassingh649@rediffmail.com>
2023-10-26 16:13:13 +0530

    trigger

--- build_rom.sh
+++ build_rom.sh
@@ -1,7 +1,8 @@
 # sync
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Testing-Stuffs/Local_Manifest.git --depth 1 -b lineage .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 # build
 export TZ=Asia/Kolkata
