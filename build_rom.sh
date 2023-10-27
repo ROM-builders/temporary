0179f1fee79113bb0d4e41bc89b8f0348ce1bf2c
swapnil vicky <74442290+SwapnilVicky@users.noreply.github.com>
2023-10-27 08:31:27 +0530

    build it

--- build_rom.sh
+++ build_rom.sh
@@ -6,8 +6,9 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom0
 . build/envsetup.sh
 lunch bliss_miatoll-userdebug
+export BLISS_BUILDTYPE=UNOFFICIAL
 export TZ=Asia/Dhaka #put before last build command
-blissify -g miatoll
+blissify -v miatoll
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
