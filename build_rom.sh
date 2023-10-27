6eb03370457fe333651e8d3e71738d667f01032c
Beta-boom <67657179+Beta-boom@users.noreply.github.com>
2023-10-27 20:29:48 +0200

    DERP 14 Zaid Sultan

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-AOSP/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Beta-boom/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest.git -b 14 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Beta-boom/local_manifest.git --depth 1 -b enchilada-derp-14 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
-lunch derp_enchilada-eng
+. build/envsetup.sh
+lunch derp_enchilada-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
