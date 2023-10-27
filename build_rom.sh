603b958c7647efae267803883a9c884721670874
Ankit Savani <70018840+AnkitSavani111@users.noreply.github.com>
2023-10-27 08:15:01 +0530

    Initial udc

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://github.com/AnkitSavani111/local_manifest.git --depth 1 -b Evolution-X .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b udc -g default,-mips,-darwin,-notdefault
+git clone https://github.com/AnkitSavani111/local_manifest.git --depth 1 -b Evolution-X-udc .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
