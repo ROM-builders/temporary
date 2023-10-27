ccbd161a2e4a609693f512eead0d8e176901fa1d
dogpoopy <103020425+dogpoopy@users.noreply.github.com>
2023-10-27 09:06:10 +0800

    build

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom
+## sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest.git -b thirteen-plus -g default,-mips,-darwin,-notdefault
 git clone https://github.com/dogpoopy/android_build_manifest.git --depth 1 -b PE .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
