3325a3f3687a6080dc55109d3b376ff9e8ce8c95
swapnil vicky <74442290+SwapnilVicky@users.noreply.github.com>
2023-10-28 06:14:52 +0530

    build eng

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch derp_miatoll-userdebug
+lunch derp_miatoll-eng
 export TZ=Asia/Kolkata #put before last build command
 mka derp
 
