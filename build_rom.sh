b9da44fffe99226032563fb0a19256cdab4198db
Gio <86983840+gioaprilino@users.noreply.github.com>
2023-10-29 00:20:37 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,17 +1,17 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b udc -g default,-mips,-darwin,-notdefault
-git clone https://github.com/gioaprilino/local_manifest.git --depth 1 -b evox .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest.git -b 14 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/gioaprilino/local_manifest.git --depth 1 -b derpfest .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build evolution
+# build derpfest
 source build/envsetup.sh
-lunch evolution_mojito-userdebug
+lunch derp_mojito-userdebug
 export TZ=Asia/Jakarta #put before last build command
 export KBUILD_BUILD_USER=dioparminggo
-export KBUILD_BUILD_HOST=evolution
+export KBUILD_BUILD_HOST=derp
 export BUILD_USERNAME=dioparminggo
-export BUILD_HOSTNAME=evolution
-mka evolution
+export BUILD_HOSTNAME=derp
+mka derp
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
