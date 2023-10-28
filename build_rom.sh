4cd31fb3c6f1f1233d7cd21030a0d587f33f9079
swapnil vicky <74442290+SwapnilVicky@users.noreply.github.com>
2023-10-28 22:56:34 +0530

    use advanced logcat debug

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifes
 git clone https://github.com/SwapnilVicky/local_manifest --depth 1 -b derp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom#
 . build/envsetup.sh
 lunch derp_miatoll-eng
 export TZ=Asia/Kolkata #put before last build command
