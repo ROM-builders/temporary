61e1c578734d07eb8755c889e4344ccebe11b47b
sdalt <142908089+sdalt@users.noreply.github.com>
2023-10-28 09:49:23 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -uhttps://github.com/DerpFest-AOSP/manifest.git -b 13  -g default,-mips,-darwin,-notdefault
-git clone https://github.com/sounddrill31/local_manifests.git --depth 1 -b derpfest-13-generic .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest.git -b 14  -g default,-mips,-darwin,-notdefault
+git clone https://github.com/BakaFest/local_manifests.git --depth 1 -b derpfest-14 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -12,6 +12,6 @@ mka systemimage
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 mkdir out/target/product/gsi
 cp out/target/product/*/system.img . 
-zip out/target/product/gsi/BakaFest-13-demo-$(date +%Y%m%d)-OkBuddyGSI.zip system.img  
+zip out/target/product/gsi/BakaFest-14-demo-$(date +%Y%m%d)-OkBuddyGSI.zip system.img  
 
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
