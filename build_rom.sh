b9b68699f97f21bdf530c6896e5dc892958b18d8
GANESH-JADHAV <124853762+Notganesh@users.noreply.github.com>
2023-10-30 00:55:13 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 -u https://github.com/DerpFest-AOSP/manifest.git -b 14
+git clone  --depth 1 https://github.com/Notganesh/Local-Manifests.git -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
+lunch derp_lime-user
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
