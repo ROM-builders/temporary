644cc5c12db165c68c5dd641c5c3aaf1fbfe7ede
YUDI <112261445+bdjmfoe@users.noreply.github.com>
2023-10-30 07:44:24 +0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b udc --git-lfs -g default,-mips,-darwin,-notdefault
-git clone https://github.com/bdjmfoe/local_manifests --depth 1 -b evo .repo/local_manifests
+git clone https://github.com/bdjmfoe/local_manifests --depth 1 -b evox .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
