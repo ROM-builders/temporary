51e3ce03f7c0aec951af42b7a4171b812bb19726
alecchan02 <132892224+alecchan02@users.noreply.github.com>
2023-10-30 09:34:32 +0800

    Test

--- build_rom.sh
+++ build_rom.sh
@@ -7,6 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch aospa_monet-eng
 export TZ=Asia/Dhaka #put before last build command
+# 1
 ./rom-build.sh monet -t eng
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
