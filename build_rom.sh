3ec260e8141d1cfcdaa355f6289952e4547981b5
hackerbhind <144337511+hackerbhind@users.noreply.github.com>
2023-10-31 04:51:44 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,10 +1,10 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs -g default,-mips,-darwin,-notdefault
-git clone https://github.com/hackerbhind/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b thirteen --git-lfs -g default,-mips,-darwin,-notdefault
+git clone https://github.com/hackerbhind/local_manifest.git --depth 1 -b pe13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 source build/envsetup.sh
-lunch lineage_oscaro-userdebug
+lunch aosp_oscaro-userdebug
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export TZ=Asia/Dhaka #put before last build command
 make bacon
