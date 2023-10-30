39de64a08b7d563f5dd6e459a9c8249ee386b2a7
Tqmane845 <110730536+tamanegi-mogumogu@users.noreply.github.com>
2023-10-31 00:53:15 +0900

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,12 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Project-Elixir/manifest.git -b UNO -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Beta-boom/local_manifest.git --depth 1 -b enchilada-elixir-14 .repo/local_manifests
+git clone https://github.com/tamanegi-mogumogu/local_manifest.git --depth=1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch aosp_enchilada-userdebug
-export TZ=Asia/Dhaka #put before last build command
+lunch aosp_tamanegi-mogumogu-userdebug
+export TZ=Asia/Tokyo #put before last build command
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
