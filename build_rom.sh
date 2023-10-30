0092d9bfd69bdc5e0ebc4a26ca44b83aa1f789ad
Rom Bdr. Gharti <romgharti@gmail.com>
2023-10-30 08:54:24 +0545

    add fcm and eng build

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch evolution_mojito-userdebug
+lunch evolution_mojito-eng
 # export BUILD_WITH_GAPPS=true
 export TZ=Asia/Dhaka #put before last build command
 mka evolution
