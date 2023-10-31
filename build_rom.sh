556b6ee1ca437eb828fc5b93e61032e44d15e547
Hassan Karim <144518310+Hassan-kareem@users.noreply.github.com>
2023-11-01 00:19:52 +0530

    mido-PixelExperience-thirteen-plus-hassan-kareem

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b thirteen-plus -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Hassan-kareem/local_manifests.git --depth 1 -b PixelExperience .repo/local_manifests
+git clone https://github.com/Hassan-kareem/local_manifests.git --depth 1 -b thirteen-plus .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
