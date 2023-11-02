f1700b055d7d48c141c919a241784defa2a51f69
AmritoRock <amritokarmokar5i@gmail.com>
2023-11-02 14:04:23 +0600

    r5x: init arrow

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manif
 git clone https://github.com/Amritorock/local_manifest.git --depth 1 -b arrow .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+## build rom
 source build/envsetup.sh
 lunch arrow_r5x-userdebug
 export ARROW_GAPPS=true
