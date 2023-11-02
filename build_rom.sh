5b4d46cde584941132bd71e5a5e8e9b910c16b24
aminuddinihsan20 <47616229+aminuddinihsan20@users.noreply.github.com>
2023-11-02 18:04:50 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b udc -g default,-mips,-darwin,-notdefault
-git clone https://github.com/aminuddinihsan20/local_manifests --depth 1 -b main .repo/local_manifests
+git clone https://github.com/aminuddinihsan20/local_manifest --depth 1 -b main .repo/local_manifest
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
