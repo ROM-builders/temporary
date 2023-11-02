31b6ee2692fc2bc9c6931e1b8dfd39c084f46e7e
Rinto Khan <138510229+Rinto02@users.noreply.github.com>
2023-11-02 13:17:17 +0600

    RMX2020-DerpFest-AOSP-14-Rinto02

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest.git -b 14 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Rinto02/Local-Manifest.git --depth 1 -b derp-14 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
+lunch derp_RMX2020-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
