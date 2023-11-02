fb20bbcb252e26b3fef27ededfc589159bbd65ea
alecchan02 <132892224+alecchan02@users.noreply.github.com>
2023-11-02 20:43:13 +0800

    test kernel upstream

--- build_rom.sh
+++ build_rom.sh
@@ -7,40 +7,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch aospa_monet-eng
 export TZ=Asia/Dhaka #put before last build command
-# 1
+# 2
 ./rom-build.sh monet -t eng
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
