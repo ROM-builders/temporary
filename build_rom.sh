3407ac0f470814621acf53748512765ca359f65a
YudhoPatrianto <kydh01123@gmail.com>
2023-11-04 06:27:21 +0700

    updating build host and user

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,10 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch spark_selene-userdebug
+export BUILD_USERNAME=YudhoPatrianto 
+export BUILD_HOSTNAME=YudhoPRJKT
+export KBUILD_BUILD_USER=$BUILD_USERNAME
+export KBUILD_BUILD_HOST=$BUILD_HOSTNAME
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
