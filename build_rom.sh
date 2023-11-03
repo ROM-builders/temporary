2f9e312054491155d316997cd8786d3d4c37a926
ArnabPalXDA <149456734+ArnabPalXDA@users.noreply.github.com>
2023-11-03 19:24:00 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b udc -g default,-mips,-darwin,-notdefault
 git clone https://github.com/ArnabPalXDA/Local_Manifest --depth 1 -b tiramisu .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
