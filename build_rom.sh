6e2070afd1d8f37b7e515667cd0a4a6a2500cbb6
Le Hong Duc <hongducthbk123@gmail.com>
2023-11-03 11:13:38 +0700

    initial tulip build

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b udc -g default,-mips,-darwin,-notdefault
+git clone https://github.com/tedomi2705/local_manifest.git --depth 1 -b blank .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
