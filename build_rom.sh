adbfd3fc2eaa9a55e4824b05efbe86e8523eacbe
YudhoPatrianto <kydh01123@gmail.com>
2023-11-04 05:41:24 +0700

    fix typo branch

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git@github.com:Spark-Rom/manifest.git -b main -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u git@github.com:Spark-Rom/manifest.git -b pyro-next -g default,-mips,-darwin,-notdefault
 git clone https://github.com/YudhoPatrianto/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
