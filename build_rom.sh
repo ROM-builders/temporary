8e84a0c8960f6a449733a2463b1c2be8ac13b4e8
Yukizaki Miyamura <96718991+yamaizano@users.noreply.github.com>
2023-11-04 20:24:12 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/yamaizano/local_manifests.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/yamaizano/local_manifests.git --depth 1 -b los .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
