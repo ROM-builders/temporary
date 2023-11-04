ef432d796a5eaccdf2ac90bd55eaed2983a5d15b
Asce-XOX <143834552+Asce-XOX@users.noreply.github.com>
2023-11-04 12:59:42 +0530

    update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u  https://github.com/DerpFest-AOSP/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u  https://github.com/DerpFest-AOSP/manifest.git -b 14 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Asce-XOX/local_manifest.git --depth 1 -b derp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
