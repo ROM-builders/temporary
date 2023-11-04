ef387beb9d8d57912fb6ef12bc8dccf8e74f9392
YudhoPatrianto <kydh01123@gmail.com>
2023-11-04 15:05:59 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest.git -b tiramisu-jem -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu-jem -g default,-mips,-darwin,-notdefault
 git clone https://github.com/YudhoPatrianto/local_manifests -b evolution .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
