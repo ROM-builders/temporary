c38df7dedc2ce944611b3af196731767a7ea0af0
omanshkrishn <138091069+omanshkrishn@users.noreply.github.com>
2023-11-04 21:42:47 +0530

    trigger

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,6 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/RisingTechOSS/android -b thirteen --git-lfs -g default,-mips,-darwin,-notdefault
 git clone https://codeberg.org/omansh-krishn/local_manifest.git --depth 1 -b risingos-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
 # build rom
 
 source build/envsetup.sh
