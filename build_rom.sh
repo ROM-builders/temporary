42ef4e89c8c27ed0cfe737482c3eb35376dbd574
YudhoPatrianto <kydh01123@gmail.com>
2023-11-04 14:50:17 +0700

    [FIXUP] missing .git

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu-jem -g default,-mips,-darwin,-notdefault
-git clone https://github.com/YudhoPatrianto/local_manifests -b evo .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest.git -b tiramisu-jem -g default,-mips,-darwin,-notdefault
+git clone https://github.com/YudhoPatrianto/local_manifests -b evolution .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
