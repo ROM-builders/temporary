8eaa7467c251eb3ab17ac1b8e1903a024008ce6b
Soo-Hwan Na <whiteshell2544@naver.com>
2023-11-04 16:10:40 +0900

    Update to udc

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Lenovo-SM6225-Fork/local_manifests --depth 1 .repo/local_manifests -b evox-13
+repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b udc -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Lenovo-SM6225-Fork/local_manifests --depth 1 .repo/local_manifests -b evox-14
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
