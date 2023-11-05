82271969939598f344d3875850a58b001eeab06e
yamaizano <muhammad.ikhsan.tkom421@polban.ac.id>
2023-11-05 15:17:42 +0700

    build_rom: whyred: build for crdroid 11.1

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 11.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/yamaizano/local_manifests.git --depth 1 -b los .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
