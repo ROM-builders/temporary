645858db48b814c6dfda45fbb30c9919b95f386a
pawar-mahesh <pawar.mahesh2345@gmail.com>
2023-11-05 14:56:53 +0530

    fix01
    Signed-off-by: pawar-mahesh <pawar.mahesh2345@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b thirteen-plus -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pawar-mahesh/local_manifest.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest.git -b thirteen-plus -g default,-mips,-darwin,-notdefault
+git clone https://github.com/pawar-mahesh/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
