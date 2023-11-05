fcdd16bde9ab97b8460bfa09fc5616fe6b3b7420
Ctps123 <79877353+Ctps123@users.noreply.github.com>
2023-11-05 15:20:51 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/syberia-project/manifest.git -b 13.0 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ArrowOS/android_manifest.git -b 13.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Ctps123/local_manifests --depth 1 -b 13 .repo/local_manifest
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 ./device/motorola/targets/scripts/replace_camera_sepolicy.sh
-lunch syberia_amogus-userdebug
+lunch arrow_amogus-userdebug
 export TZ=Asia/Dhaka #put before last build command
-mka bacon
+m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
