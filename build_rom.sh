66d3ef0a16ffe98d9e61c2fd7a161174e0f67378
snehil456 <149964379+snehil456@users.noreply.github.com>
2023-11-05 10:43:21 -0800

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 13
+git clone https://github.com/snehil456/local__manifest.git --depth 1 -b r5x .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
-lunch derp_vayu-user
+source . build/envsetup.sh
+lunch lunch derp_r5x-user
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
