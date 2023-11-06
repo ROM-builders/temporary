cafb139f9a0176378001eb546d1e1d7a1edc2aa1
mukul-lokash <110097868+mukul-lokash@users.noreply.github.com>
2023-11-06 17:19:40 +0530

    Init for m52xq

--- build_rom.sh
+++ build_rom.sh
@@ -1,8 +1,8 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-AOSP/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/MxkulSharma/local_manifest --depth 1 -b crdroid .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
+q
 # build rom
 source build/envsetup.sh
 lunch derp_vayu-user
