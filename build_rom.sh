7ee5a5ee8123ccc993a6507081b1542c67e97f91
Dwyor-tmx <86127970+Dwyor-tmx@users.noreply.github.com>
2023-11-07 18:57:02 +0700

    Fog ChiperOs Dwyor-tmx
    Build ChiperOs Fog

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch cipher_(fog)-userdebug
-export TZ=Asia/Dhaka #put before last build command
+export TZ=Asia/Dhaka #put before last build comman
 mka bacon -j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
