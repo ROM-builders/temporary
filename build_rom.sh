3b493a0dba7806d70a8b754a16e3ef88e5553dc6
BESHIZUMOTO <132457432+beshizumoto@users.noreply.github.com>
2023-11-07 14:29:06 +0000

    lancelot-DerpFest-AOSP-14

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch derp_lancelot-userdebug 
-export TZ=Asia/Jakarta #put before last build command mneara
+export TZ=Asia/Jakarta #put before last build command mnearsdjha
 mka derp
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
