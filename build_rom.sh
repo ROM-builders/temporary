c8ce666a4dbbc0501e9738813bb5d0dff9783398
dogpoopy <103020425+dogpoopy@users.noreply.github.com>
2023-11-07 20:20:43 +0800

    boot or ur gey

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 ## build rom
 source build/envsetup.sh
-lunch evolution_vayu-eng
+lunch evolution_vayu-userdebug
 # timezone
 export TZ=Asia/Manila
 mka evolution
