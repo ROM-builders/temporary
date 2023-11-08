fd487d8bdddc78d1e394c57042f6ee39634baf11
Rom Bdr. Gharti <romgharti@gmail.com>
2023-11-08 11:32:51 +0545

    retry

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch derp_mojito-eng
-# export BUILD_WITH_GAPPS=true
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
