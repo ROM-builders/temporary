d0d95dd822d5dcb7a231f9159314f2a36759ed9d
Soo-Hwan Na <whiteshell2544@naver.com>
2023-11-08 17:12:45 +0900

    i mean build eng

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch evolution_tb128fu-user
+lunch evolution_tb128fu-eng
 mka evolution
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
