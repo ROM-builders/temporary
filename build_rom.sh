8ad915932e135916bd310063caa3cb136aa28414
Meghthedev <meghthedev@gmail.com>
2023-11-08 09:37:49 +0530

    build?

--- build_rom.sh
+++ build_rom.sh
@@ -3,6 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/androi
 git clone https://github.com/Meghthedev/local_manifest --depth 1 -b cr .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+
 # build rom
 source build/envsetup.sh
 lunch lineage_tissot-userdebug
