7bc916f07ed758a73d0c3de73b324e7e46e4a6cc
Ahmed gamal <87333939+AhmedGamal09@users.noreply.github.com>
2023-11-08 18:59:25 +0200

    upstream source

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/alphadroid-project/ma
 git clone https://github.com/AhmedGamal09/local_manifest.git --depth 1 -b alpha-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom 29
+# build rom 30
 source build/envsetup.sh
 lunch lineage_mojito-userdebug
 export TZ=Asia/Dhaka #put before last build command
