9361cda3f21e8e469ee748609040b6289ca76c90
dogpoopy <103020425+dogpoopy@users.noreply.github.com>
2023-11-08 14:08:31 +0800

    upstream kernel

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest 
 git clone https://github.com/dogpoopy/android_build_manifest.git --depth 1 -b Evolution-X .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j4
 
-## build rom
+# build rom
 source build/envsetup.sh
 lunch evolution_vayu-userdebug
 # timezone
