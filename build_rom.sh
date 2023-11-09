d0c22a7c2e31b36d58cd94ecdc69aca51d0fe407
KiwiBlock27 <143954570+KiwiBlock27@users.noreply.github.com>
2023-11-09 15:39:35 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/alphadroid-project/manifest -b alpha-13 --git-lfs -g default,-mips,-darwin,-notdefault
-git clone https://github.com/KiwiBlock27/local_manifest.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/KiwiBlock27/local_manifest.git --depth 1 -b ELIXIR-13.0 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
