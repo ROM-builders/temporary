427dc9815156cc11689bb164053deff83d1ca19b
AbrarNoobAlt <123289074+AbrarNoobAlt@users.noreply.github.com>
2023-11-10 09:19:33 +0600

    November update

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,6 @@ repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifes
 git clone https://github.com/AbrarNoob/local_manifest --depth 1 -b derp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-
 # build rom
 source build/envsetup.sh
 lunch derp_alioth-userdebug
