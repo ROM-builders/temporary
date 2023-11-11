83b91f775e98b3a4dec18eb843b7282a35e273a3
Ameru <94937447+RuiAmeru@users.noreply.github.com>
2023-11-10 18:13:01 -0800

    Add files via upload

---
+++ build_rom.sh
@@ -0,0 +1,12 @@
+# sync rom
+repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest.git -b udc -g default,-mips,-darwin,-notdefault
+git clone https://github.com/RuiAmeru/local_manifest.git --depth 1 -b 14 .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
+# build rom [14]
+source build/envsetup.sh
+lunch evolution_rosy-userdebug
+mka evolution
+
+# upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
