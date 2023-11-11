89921a9d99e3318835a917bc4e4c4010e4a3dad8
aminuddinihsan20 <47616229+aminuddinihsan20@users.noreply.github.com>
2023-11-11 19:48:35 +0700

    Surya-awaken

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch awaken_surya-userdebug
-export TZ=Asia/Jakarta #put before last build command
+export TZ=Asia/Bandung #put before last build command
 m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
