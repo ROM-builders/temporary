03f07f37478cc6fb5860f115fdaf233b2086699f
Muichiro Tokito <132120605+Muichiro09@users.noreply.github.com>
2023-11-12 13:47:44 +0800

    Surya : Retry again

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b udc -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Muichiro09/local_manifest.git --depth 1 -b a14 .repo/local_manifests
+git clone https://github.com/Muichiro09/local_manifest.git --depth 1 -b 14 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
