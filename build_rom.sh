8d688d88cc1e64c9f9a2e231977047e59a685f53
Yoni Asleno Farid <92686606+aslenofarid@users.noreply.github.com>
2023-11-12 20:54:24 +0700

    Go

--- build_rom.sh
+++ build_rom.sh
@@ -5,11 +5,11 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-breakfast X00TD
+lunch lineage_X00TD-userdebug
 export BUILD_HOSTNAME=android-build
 export BUILD_USERNAME=aslenofarid
 export TZ=Asia/Jakarta #put before last build command
-brunch X00TD
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
