16d3b4aebcf0d694a974b884599694b384221d09
Muichiro Tokito <132120605+Muichiro09@users.noreply.github.com>
2023-11-13 06:20:23 +0800

    Surya : Retry

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch evolution_surya-userdebug
-export TZ=Asia/Manila #put before last build command
+export KBUILD_BUILD_USER=Muichiro
+export TZ=Asia/Manila #put before last build command 
 mka evolution
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
