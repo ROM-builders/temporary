f63566ddfce766c75a5df6102be3e9a89d7871a2
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2023-11-13 05:04:44 +0530

    evo [skip ci]

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,16 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b snow -g default,-mips,-darwin,-notdefault
-git clone https://github.com/rushiranpise/local_manifest.git --depth 1 -b evolos .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b udc -g default,-mips,-darwin,-notdefault
+git clone https://github.com/rushiranpise/local_manifest.git --depth 1 -b evolos13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom 2
 source build/envsetup.sh
-lunch evolution_chef-userdebug
+lunch evolution_chef-user
+export TARGET_BOOT_ANIMATION_RES=true
+export export TARGET_SUPPORTS_QUICK_TAP=true
 export TARGET_USES_MINI_GAPPS=true
+export TARGET_ENABLE_BLUR=true
+export TARGET_FACE_UNLOCK_SUPPORTED=true
 export KBUILD_BUILD_USER=rushiranpise
 export KBUILD_BUILD_HOST=rushiranpise
 export BUILD_USERNAME=rushiranpise
@@ -15,5 +19,4 @@ export TZ=Asia/Kolkata #put before last build command
 mka evolution
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
-# rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-rclone copy out/target/product/chef/*.zip cirrus:chef/evo -P
+rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
