8a262c9f9c0f1a31f167d55b4e0c8d94bc46eaa4
shazu-xd <144534472+shazu-xd@users.noreply.github.com>
2023-11-13 11:21:37 +0530

    fix

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android -b lineage-20.0 default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android -b lineage-20.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/shazu-xd/local-manifest -b lineage-20 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
