8d926d44ecae439c015cc2333e9e677151bb0ce9
leoxsys <86662074+leoxsys@users.noreply.github.com>
2023-11-13 08:13:31 +0530

    spes-Evolution-X-udc-leoxsys

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u  https://github.com/Evolution-X/manifest -b udc -g default,-mips,-darwin,-notdefault
-git clone https://github.com/leoxsys/local_manifest.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/leoxsys/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch evolution_spes-user
+lunch evolution_spes-userdebug
 export TZ=Asia/Dhaka #put before last build command                       
 mka evolution
 
