24e41e8000ff754182672c89d22c478698dc213a
deny <77781846+deny2311@users.noreply.github.com>
2023-11-14 21:19:31 +0700

    sagit-Evolution-X-udc-deny2311

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch evolution_sagit-userdebug
+lunch evolution_sagit-eng
 export TZ=Asia/Dhaka #put before last build command
 mka evolution
 
