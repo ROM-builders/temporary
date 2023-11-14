a0cb45ac201f668b643962247024bb5a533e27b0
Hitesoni | ×͜× <72127977+hsx02@users.noreply.github.com>
2023-11-14 19:07:49 +0530

    initial evo14 bild for spes

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b udc -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Muichiro09/local_manifest.git --depth 1 -b 14 .repo/local_manifests
+git clone https://github.com/hsx02/local_manifest.git --depth 1 -b 14Evo .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch evolution_surya-userdebug
-export KBUILD_BUILD_USER=Muichiro09
+lunch evolution_spes-eng
 export TZ=Asia/Manila #put before last build command 
 mka evolution
 
