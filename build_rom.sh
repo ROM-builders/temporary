ddc2ea205d1db0aba66eef4e97f2d7eed876a9e6
sheshuv <sheshuvadrevu@gmail.com>
2023-11-14 18:04:53 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync Rom    
 repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-AOSP/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/sheshuv/local_manifest.git --depth 1 -b pos .repo/local_manifests
+git clone https://github.com/sheshuv/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
   
 # build rom  
