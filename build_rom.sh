2cef65cfdee0d92fceec6c30251407c60c1f24d6
Yukizaki Miyamura <96718991+yamaizano@users.noreply.github.com>
2023-11-14 14:56:56 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 11.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/yamaizano/local_manifests.git --depth 1 -b 4.19 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-18.1 --git-lfs -g default,-mips,-darwin,-notdefault
+git clone https://github.com/yamaizano/local_manifests.git --depth 1 -b mao .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
