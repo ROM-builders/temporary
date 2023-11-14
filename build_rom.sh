5a5e132c90381b8f81268ff4e2f744a71a0ce3e7
شَکِیْب | Shakib <52566642+Shakib-BD@users.noreply.github.com>
2023-11-14 12:49:42 +0600

    Build broken missing module.

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_rock-userdebug
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export TZ=Asia/Dhaka #put before last build command
 make bacon
 
