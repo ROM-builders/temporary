ee4a0bbc644943f7340a4aa0320894fc4a81fb7f
Gio <86983840+gioaprilino@users.noreply.github.com>
2023-11-15 16:53:25 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,17 +1,17 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest.git -b 14 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/gioaprilino/local_manifest.git --depth 1 -b derpfest .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 13-qpr3 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/gioaprilino/local_manifest.git --depth 1 -b banana .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build derpfest
+# build banana
 source build/envsetup.sh
-lunch derp_mojito-userdebug
+lunch banana_mojito-userdebug
 export TZ=Asia/Jakarta #put before last build command
-export KBUILD_BUILD_USER=dioparminggo
-export KBUILD_BUILD_HOST=derp
-export BUILD_USERNAME=dioparminggo
-export BUILD_HOSTNAME=derp
-m derp
+export KBUILD_BUILD_USER=gioaprilino
+export KBUILD_BUILD_HOST=banana
+export BUILD_USERNAME=gioaprilino
+export BUILD_HOSTNAME=banana
+m banana
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
