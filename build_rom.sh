620fbf0e1486367af15147a1e8c12f4fcc2039cf
BhashaRana <122691639+BhashaRana@users.noreply.github.com>
2023-11-15 18:18:51 +0400

    0.0

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 11 --git-lfs -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 11.0 --git-lfs -g default,-mips,-darwin,-notdefault
 git clone https://github.com/BhashaRana/tissot_manifest --depth 1 -b cr .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
