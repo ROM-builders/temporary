b038ac7f760857dcc5aa7682b3900f40495bcd45
Abhinav Kumar <122148266+acex88@users.noreply.github.com>
2023-11-16 18:18:04 +0530

    Trigger build

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 13.0 --git-lfs -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs -g default,-mips,-darwin,-notdefault
 git clone https://github.com/acex88/local_manifest.git --depth 1 -b crdroid-14 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
     
