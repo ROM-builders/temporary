12f7c7bf92f9e88b6b66da63f94273b073585ec2
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2023-11-16 22:00:42 +0530

    eng build

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_chef-user
+lunch lineage_chef-eng
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
