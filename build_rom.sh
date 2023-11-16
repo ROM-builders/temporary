68d1cf771a29b9cf7958553318cdc699875dd457
DA <91001298+mintss-zez0@users.noreply.github.com>
2023-11-17 02:19:42 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,17 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init -u https://github.com/pixeldust-project-caf/manifest -b tartufo-qpr3 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/mintss-zez0/local_manifest.git --depth 1 -b tartufo-qpr3 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom [07]
 source build/envsetup.sh
-lunch derp_vayu-user
-export TZ=Asia/Dhaka #put before last build command
-mka derp
+lunch pixeldust_sky-userdebug
+export KBUILD_BUILD_USER=arm
+export KBUILD_BUILD_HOST=debug
+export BUILD_USERNAME=arm
+export BUILD_HOSTNAME=debug
+export TZ=Asia/Bangkok # put before last build command
+./rom-build.sh sky -t userdebug
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
