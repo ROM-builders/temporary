21c6a47b5a2b512dcaf979f404584adfc309259c
sheshuv <sheshuvadrevu@gmail.com>
2023-11-16 07:21:35 +0530

    Fix manifest

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/PixelOS-AOSP/manifest
 git clone https://github.com/sheshuv/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
   
-# build rom PixelOS fix kernel path fix safetynet 
+# build rom PixelOS fix kernel path fix safetynet fix manifest
 . build/envsetup.sh  
 lunch aosp_sky-user 
 export BUILD_USERNAME=Sheshu Vadrevu
