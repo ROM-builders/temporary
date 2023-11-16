8270e5992afd321ca0c52f1f3676bf773691c895
Hakan <hknhzl@gmail.com>
2023-11-16 10:17:05 +0300

    Initial 14

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 13.0 --git-lfs -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Lafactorial/local_manifest.git --depth 1 -b cr-13 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Lafactorial/local_manifest.git --depth 1 -b cr-14 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom 
