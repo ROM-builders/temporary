060189bd435298d3d84f82728ab52ae89638d89e
DA <91001298+mintss-zez0@users.noreply.github.com>
2023-11-17 12:55:20 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,16 @@
-# sync rom
+# sync Rom    
 repo init --depth=1 --no-repo-verify -u https://github.com/alphadroid-project/manifest -b alpha-13 --git-lfs -g default,-mips,-darwin,-notdefault
 git clone https://github.com/mintss-zez0/local_manifest.git --depth 1 -b alpha-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
-# build rom
-source build/envsetup.sh
-lunch alphadroid_duck-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
-export TZ=Asia/Jakarta #put before last build command
+  
+# build rom Alphadroid
+. build/envsetup.sh  
+lunch Alphadroid_sky-user 
+export BUILD_USERNAME=Duck Yellow
+export BUILD_HOSTNAME=duck
+export KBUILD_BUILD_USER=Duck Yellow   
+export KBUILD_BUILD_HOST=duck
+export TZ=Asia/Delhi #put before last build command
 m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
