c63deffa28a3339b398d6d27e305e4cda6fcb6dd
DA <91001298+mintss-zez0@users.noreply.github.com>
2023-11-17 12:28:21 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,17 +1,14 @@
 # sync rom
-repo init -u https://github.com/pixeldust-project-caf/manifest -b tartufo-qpr3 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/mintss-zez0/local_manifest.git --depth 1 -b tartufo-qpr3 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/alphadroid-project/manifest -b alpha-13 --git-lfs -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Wrdn28/local_manifest.git --depth 1 -b alpha-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom [07]
+# build rom
 source build/envsetup.sh
-lunch pixeldust_sky-userdebug
-export KBUILD_BUILD_USER=arm
-export KBUILD_BUILD_HOST=debug
-export BUILD_USERNAME=arm
-export BUILD_HOSTNAME=debug
-export TZ=Asia/Bangkok # put before last build command
-./rom-build.sh sky -t userdebug
+lunch alphadroid_duck-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
+export TZ=Asia/Jakarta #put before last build command
+m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
