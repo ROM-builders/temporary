403a75fa10f6f45170883eb4156f42b47d8e06b6
RT1649 <147801718+RT1649@users.noreply.github.com>
2023-11-17 20:50:40 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch superior_fleur-userdebug
-export TZ=Asia/Dhaka #put before last build command
+export TZ=Asia/Dhaka ##put before last build command
 export ALLOW MISSING DEPENDENCIES=true
 mka bacon
 
