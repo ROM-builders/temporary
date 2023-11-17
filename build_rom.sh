834616518b2fef6ee484eb4c6ca5bc39d373c56c
dogpoopy <qtqtron@gmail.com>
2023-11-17 20:50:09 +0800

    fix play integrity

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch aosp_vayu-eng
+lunch aosp_vayu-user
 
 # builder variables
 export BUILD_USERNAME=dogpoopy
