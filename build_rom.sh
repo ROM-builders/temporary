5d42fda0d2d317493809e367ca1cfc0f187a4084
sumon <82557471+mdsumon2001@users.noreply.github.com>
2023-11-18 19:47:23 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_RMX2185-userdebug
-export TZ=Asia/Dhaka #put before last build command
+export TZ=Asia/Dhaka ##put before last build command
 make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
