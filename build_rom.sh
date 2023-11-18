1f240d03953d4a143d7654636d1c5c3e5896b7c3
Love Lohar <lovelohar05@gmail.com>
2023-11-18 21:02:35 +0530

    mido-CherishOS-tiramisu-lovelohar05

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-brunch device-mido
+lunch cherish_mido-userdebug
 export TZ=Asia/Udaipur #put before last build command
 m bacon
 
