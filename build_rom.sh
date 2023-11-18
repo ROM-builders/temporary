77b8a1afcf358333620862f1f57c3f82ab4fe283
凛斗 <138510229+Rinto02@users.noreply.github.com>
2023-11-18 20:35:18 +0600

    Build Crdroid now

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/RisingTechOSS/android -b thirteen --git-lfs -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Rinto02/Local-Manifest.git --depth 1 -b rising .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Rinto02/Local-Manifest.git --depth 1 -b crdroid-14.0 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_RMX2020-user
+lunch lineage_RMX2020-userdebug
 export TZ=Asia/Dhaka #put before last build command
-mka bacon
+m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
