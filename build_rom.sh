bf5c9a6dd6787caaeaf16d157580b3326b042d50
shazu-xd <144534472+shazu-xd@users.noreply.github.com>
2023-11-18 20:59:56 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch lineage_fleur-userdebug
+lunch lineage_fleur-eng
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
