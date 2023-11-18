a0869ea8ebab1192af147318870d8a974b2b4c2d
Kendras056 <88943971+Kendras056@users.noreply.github.com>
2023-11-19 01:15:59 +0700

    fix typo

--- build_rom.sh
+++ build_rom.sh
@@ -8,7 +8,7 @@ source build/envsetup.sh
 brunch X00TD
 export TZ=Europe/Moscow #put before last build command
 mka sign_target_files_apks
-coot
+croot
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
