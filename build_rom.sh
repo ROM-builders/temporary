f08306cce8c90e3757cc14e047a8d827bf97f9f7
D I X I <dikiawan9@gmail.com>
2023-11-19 18:53:35 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,14 @@
 # sync rom
 repo init -u https://github.com/bananadroid/android_manifest.git -b 13 --git-lfs
 git clone https://github.com/diksyfck/Local-Manifests.git --depth 1 -b main .repo/local_manifests
-repo sync -c -j8 --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
 
 # build rom
 . build/envsetup.sh
 lunch banana_lavender-userdebug
+export TZ=Asia/Dhaka #put before last build command
 m banana
+
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
