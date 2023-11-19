6cd098c3c5b6236056943ce956e33f6bee4cdc53
D I X I <dikiawan9@gmail.com>
2023-11-19 18:50:03 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init -u https://github.com/bananadroid/android_manifest.git -b 13 --git-lfs
-git clone https://github.com/diksy9/Local-Manifests.git --depth 1 -b main .repo/local_manifests
+git clone https://github.com/diksyfck/Local-Manifests.git --depth 1 -b main .repo/local_manifests
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune
 
 # build rom
