f1d89fb0bf981b8d1d320fb189bd77f31929d3ac
شَکِیْب | Shakib <52566642+Shakib-BD@users.noreply.github.com>
2023-11-20 09:04:54 +0600

    rock: Fix duplicate path of "vendor/xiaomi/rock/Android.bp:10372:1: module "android.hardware.security.keymint-service.beanpod"

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch lineage_rock-userdebug
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
-export TZ=Asia/Dhaka ##put before last build command
+export TZ=Asia/Dhaka #put before last build command
 make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
