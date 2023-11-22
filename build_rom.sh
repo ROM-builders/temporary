b93638ae6c742ef18503ccd34b30773a002870b0
Galih Raditya <141231416+belugaA330@users.noreply.github.com>
2023-11-22 21:37:56 +0700

    ysl-SuperiorOS-thirteen-belugaA330

--- build_rom.sh
+++ build_rom.sh
@@ -7,15 +7,10 @@ repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 . build/envsetup.sh
 lunch superior_ysl-userdebug
 m bacon -j$(nproc --all)
-
-
 export KBUILD_BUILD_USER=beluga
 export KBUILD_BUILD_HOST=beluga330
 export BUILD_USERNAME=beluga
 export BUILD_HOSTNAME=beluga330
-
-
-
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
