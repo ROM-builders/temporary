df147c706b7af9fba70c82fd0d0713be22d93dcf
Hassan Karim <144518310+Hassan-kareem@users.noreply.github.com>
2023-11-22 06:45:19 +0530

    a5lte-LineageOS-lineage-18.1-hassan-kareem

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_a5lte-userdebug
+export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
