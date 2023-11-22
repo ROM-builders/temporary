25aa351b7dcde2a282ee02aa1d10e04222c79453
omanshkrishn <138091069+omanshkrishn@users.noreply.github.com>
2023-11-22 14:50:01 +0530

    retry

--- build_rom.sh
+++ build_rom.sh
@@ -3,6 +3,7 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/alphadroid-project/manifest -b alpha-13 -g default,-mips,-darwin,-notdefault
 git clone https://codeberg.org/omansh-krishn/local_manifest.git --depth 1 -b alpha-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
 # build rom
 
 source build/envsetup.sh
