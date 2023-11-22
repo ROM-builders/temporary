e33a7bb40824b4072a8b5cef7d003cace6a303e8
Galih Raditya <141231416+belugaA330@users.noreply.github.com>
2023-11-23 01:22:07 +0700

    ysl-superioros-thirteen-belugaa330

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,16 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
+repo init -u https://github.com/SuperiorOS/manifest.git -b thirteen
+git clone https://github.com/belugaA330/local_manifest.git --depth 1 -b main.repo/local_manifest
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 # build rom
-source build/envsetup.sh
-lunch derp_vayu-user
-export TZ=Asia/Dhaka #put before last build command
-mka derp
+. build/envsetup.sh
+lunch superior_ysl-userdebug
+export KBUILD_BUILD_USER=beluga
+export KBUILD_BUILD_HOST=beluga330
+export BUILD_USERNAME=beluga
+export BUILD_HOSTNAME=beluga330
+export TZ=Asia/Jakarta #put before last build command
+m bacon -j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
