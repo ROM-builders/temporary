7d28af763411b55e8c96b1d822be8494692ae50a
Arafattex <141769738+Shas45558@users.noreply.github.com>
2023-11-22 20:04:30 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom 
+# sync rom  
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android -b lineage-18.1 --git-lfs -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Arafattex/local_manifest --depth 1 -b aaa .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
