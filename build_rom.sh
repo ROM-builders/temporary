42d598221b72b32e7be796cb0919be9573e4d9ab
Galih Raditya <141231416+belugaA330@users.noreply.github.com>
2023-11-23 09:24:15 +0700

    ysl-superioros-thirteen-belugaA330

--- build_rom.sh
+++ build_rom.sh
@@ -10,7 +10,7 @@ export KBUILD_BUILD_HOST=beluga330
 export BUILD_USERNAME=beluga
 export BUILD_HOSTNAME=beluga330
 export TZ=Asia/Jakarta #put before last build command
-m bacon -j$(nproc --all)
+m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
