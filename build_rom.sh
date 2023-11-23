40c057ef6464db672e4ecfcd548838f399a67321
Galih Raditya <141231416+belugaA330@users.noreply.github.com>
2023-11-24 03:32:36 +0700

    ysl-bananadroid-13-belugaA330

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init -u https://github.com/SuperiorOS/manifest.git -b thirteen
+repo init -u https://github.com/bananadroid/android_manifest.git -b 13 --git-lfs
 git clone https://github.com/belugaA330/local_manifests.git --depth 1 -b ysl .repo/local_manifests
-repo sync -c --force-sync --no-clone-bundle --no-tags
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune
 # build rom
-source build/envsetup.sh
-lunch superior_ysl-userdebug
+. build/envsetup.sh
+lunch banana_ysl-userdebug
 export TZ=Asia/Jakarta #put before last build command
-m bacon
+m banana
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
