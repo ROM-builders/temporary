d2b60c45de833e7bf5ffa567274258303bdca028
Galih Raditya <141231416+belugaA330@users.noreply.github.com>
2023-11-23 15:55:32 +0700

    ysl-SuperiorOS-thirteen-belugaA330

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,11 @@
 # sync rom
 repo init -u https://github.com/SuperiorOS/manifest.git -b thirteen
-git clone https://github.com/krishnaspeace/local_manifests.git --depth 1 -b ysl .repo/local_manifests
+git clone https://github.com/belugaA330/local_manifests.git --depth 1 -b ysl .repo/local_manifests
 repo sync -c --force-sync --no-clone-bundle --no-tags
-+
 # build rom
 source build/envsetup.sh
 lunch superior_ysl-userdebug
 export TZ=Asia/Jakarta #put before last build command
 m bacon
-
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
