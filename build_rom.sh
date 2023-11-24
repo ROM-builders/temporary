1dd77834d6893c72d5caf9bfb21d80ff9772b695
Galih Raditya <141231416+belugaA330@users.noreply.github.com>
2023-11-25 05:31:32 +0700

    ysl-SuperiorOS-thirteen-belugaA330

--- build_rom.sh
+++ build_rom.sh
@@ -1,18 +1,12 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://github.com/krishnaspeace/local_manifests.git --depth 1 -b ysl .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
+repo init -u https://github.com/SuperiorOS/manifest.git -b thirteen
+git clone https://github.com/belugaA330/local_manifests.git --depth 1 -b ysl .repo/local_manifests
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+    
 # build rom
-source build/envsetup.sh
-lunch evolution_ysl-userdebug
-export KBUILD_BUILD_USER=krishna 
-export KBUILD_BUILD_HOST=krishnaspeace 
-export BUILD_USERNAME=krisha
-export BUILD_HOSTNAME=krishnaspeace
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
-export TZ=Asia/Dhaka #put before last build command
-mka evolution
+. build/envsetup.sh
+lunch superior_ysl-userdebug
+m bacon -j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
