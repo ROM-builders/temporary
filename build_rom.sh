d7dd9a6201fe6d85ada902c1488f4e003294656a
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2023-11-24 09:34:17 +0530

    try with eng

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom 2
 source build/envsetup.sh
-lunch evolution_chef-userdebug
+lunch evolution_chef-eng
 export TARGET_BOOT_ANIMATION_RES=1080
 export TARGET_SUPPORTS_QUICK_TAP=true
 export TARGET_USES_MINI_GAPPS=true
@@ -20,4 +20,4 @@ mka evolution
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
-#7
+#8
