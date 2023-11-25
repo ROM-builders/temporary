6e57df6f9af9974094465a1d7fc331d0e4ba6060
Soo-Hwan Na <whiteshell2544@naver.com>
2023-11-25 10:55:56 +0900

    Update link

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b udc -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Samsung-SM8150-Development/local_manifests --depth 1 -b fourteen .repo/local_manifests
+git clone https://github.com/Roynas-Android-Playground/local_manifests --depth 1 -b r3q-Evolution-X-udc .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
