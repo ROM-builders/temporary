2ff061db6067527358d9e266d85c1654cfb11b8f
Arafattex45558 <146331849+Arafattex45558@users.noreply.github.com>
2023-11-26 21:50:33 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom
+# sync rom 
 repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-14.0 --git-lfs -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Arafattex45558/local_manifest --depth 1 -b Arrow-14.0 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
