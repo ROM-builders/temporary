8ff9f5775f936c6d1b8bfa1473f5ac537ddd9a69
شَکِیْب | Shakib <52566642+Shakib-BD@users.noreply.github.com>
2023-11-26 16:51:04 +0600

    Trying with working dt

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/alphadroid-project/manifest -b alpha-13 --git-lfs -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Shakib-BD/local_manifest.git --depth 1 -b rock-alphadroid-13 .repo/local_manifests
+git clone https://github.com/Shakib-BD/local_manifest.git --depth 1 -b AlphaDroid-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch lineage_rock-user
+lunch lineage_stone-user
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export TZ=Asia/Dhaka ##put before last build command
 make bacon
