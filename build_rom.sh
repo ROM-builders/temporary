c584b90c1a6698100c6e5eed6f8f74d260fd9ba2
RT1649 <147801718+RT1649@users.noreply.github.com>
2023-11-26 09:44:45 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/RT1648/local_manifest-fleurprebuilt.git --depth 1 -b master .repo/local_manifests
+git clone https://github.com/RT1648/local_manifest-fleurprebuilt.git --depth 1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
