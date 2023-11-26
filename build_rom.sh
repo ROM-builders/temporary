ad663338e54a4c0807d53a786c80d5555b15977f
israfilbd2 <143932247+israfilbd2@users.noreply.github.com>
2023-11-27 05:34:43 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/Evolution-X/manifest -b udc -g default,-mips,-darwin,-notdefault
-git clone https://github.com/israfilbd/local_manifest.git --depth 1 -b evox-a14 .repo/local_manifests
+git clone https://github.com/israfilbd2/local_manifest.git --depth 1 -b evox-a14 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j4
 
 # build rom
