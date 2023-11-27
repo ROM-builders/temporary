a2f05857c8975f7b83cd02ef87660240ad5f3cef
Arslan <132056853+arslan1400@users.noreply.github.com>
2023-11-27 15:44:22 +0500

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync roms
+# sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest.git -b 14 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/arslan1400/local_manifest.git --depth 1 -b derp .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
