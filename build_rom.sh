c8e4d1e848c099f998af9df98d5436b5ff0f4735
Hassan karim <mohdhassankareem@gmail.com>
2023-11-27 19:20:31 +0530

    a5lte-LineageOS-linegae-18.1-hassan-kareem

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs -g default,-mips,-darwin,-notdefault
-git clone https://github.com/Hassan-kareem/local_manifests.git --depth 1 -b lineage-20.0 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-18.1 --git-lfs --git-lfs -g default,-mips,-darwin,-notdefault
+git clone https://github.com/Hassan-kareem/local_manifests.git --depth 1 -b lineage-18.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
