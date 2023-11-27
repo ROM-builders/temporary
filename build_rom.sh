2a8834166c4069ec93b7f57c3bc0542b4de69341
israfilbd2 <143932247+israfilbd2@users.noreply.github.com>
2023-11-27 20:30:26 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/Evolution-X/manifest -b udc -g default,-mips,-darwin,-notdefault
-git clone https://github.com/israfilbd2/local_manifest.git --depth 1 -b evox-a14 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 14 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/israfilbd/local_manifest.git --depth 1 -b derp-a14 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j4
 
 # build rom
 source build/envsetup.sh
-lunch evolution_RMX1901-userdebug
+lunch derp_RMX1901-userdebug
 export TZ=Asia/Dhaka #put before last build command
-mka evolution
+mka derp
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
