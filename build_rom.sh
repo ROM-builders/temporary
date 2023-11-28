c7adf615a0009b7d54339a2880e13bb57ab1c92b
Arslan <132056853+arslan1400@users.noreply.github.com>
2023-11-28 08:59:25 +0500

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch derp_a52sxq-userdebug
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+export ALLOW_MISSING_DEPENDENCIES=true
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
