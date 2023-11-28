b89d90dafba713cdbe8445b7baf2a08292f9b308
Arafattex45558 <146331849+Arafattex45558@users.noreply.github.com>
2023-11-28 18:56:34 +0600

    Try go fix kernel compile

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom
+# sync rom 
 repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest.git -b 14 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/Arafattex45558/local_manifest --depth 1 -b Lancelot_losa14r .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
