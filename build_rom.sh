fdead0d27aa6193c0ad77bdb0904e057acb380ea
swapnil vicky <74442290+SwapnilVicky@users.noreply.github.com>
2023-11-29 07:08:08 +0530

    kernel fixup

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git
 git clone https://github.com/SwapnilVicky/local_manifest --depth 1 -b 13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom#
+# build rom
 source build/envsetup.sh
 lunch lineage_miatoll-userdebug
 export TZ=Asia/Dhaka #put before last build command
