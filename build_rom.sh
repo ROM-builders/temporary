f365772604f5c1ae431b9de3df5f236b4df9b69c
VelinXd <153659670+VelinXd@users.noreply.github.com>
2023-12-13 01:15:11 +0700

    RMX2185-Derpfest-VelinXd
    Build Derpfest for RMX2185

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-12/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/VelinXd/local_manifest --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
+lunch derp_RMX2185-user
 export TZ=Asia/Dhaka #put before last build command
 mka derp
 
