f07461830aeb3d6a6d88f0d212936b0503311c22
VelinXd <153659670+VelinXd@users.noreply.github.com>
2023-12-13 01:20:00 +0700

    RMX2185-Derpfest-VelinXd
    Build Derpfest for RMX2185

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-12/manifest.git -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/VelinXd/local_manifest --depth 1 -b master .repo/local_manifests
+git clone https://github.com/VelinXd/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
