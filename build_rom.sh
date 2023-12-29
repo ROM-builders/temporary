7e271675f925feb23f7622b5c8669a0502cf3ad9
Jony <108582874+SimpleJony@users.noreply.github.com>
2023-12-29 22:47:15 +0800

    build pe

--- build_rom.sh
+++ build_rom.sh
@@ -1,5 +1,5 @@
 # sync rom 
-repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b thirteen-plus -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/PixelExperience/manifest -b thirteen -g default,-mips,-darwin,-notdefault
 git clone https://github.com/SimpleJony/local_manifest --depth=1 -b pe .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
