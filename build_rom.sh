a1b09339467874798f3c378995018cd2223cf115
snnbyyds <87487115+snnbyyds@users.noreply.github.com>
2023-12-31 14:29:14 +0800

    user build

--- build_rom.sh
+++ build_rom.sh
@@ -6,13 +6,13 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 croot
-breakfast blossom
+lunch lineage_blossom-user
 export KBUILD_BUILD_USER=snnbyyds
 export KBUILD_BUILD_HOST=uotan
 export BUILD_USERNAME=sn
 export BUILD_HOSTNAME=uotan
 export TZ=Asia/Shanghai #put before last build command
-brunch blossom
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
