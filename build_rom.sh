3db5c2b5cfb98b9117cce47ae014cebf14e657d6
spoil7 <schkiper007@gmail.com>
2024-01-13 23:07:12 +0200

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,14 +1,16 @@
-# sync rom
-repo init -u https://github.com/PixelExperience/manifest -b thirteen-plus
+# Initialize local repository
+repo init -u https://github.com/PixelExperience-Staging/manifest -b fourteen
+
+# Sync
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+ #put before last build cmnd
 
-# build rom
-source build/envsetup.sh
-lunch aosp_flame-userdebug
- #put before last build command
-croot
+# Set up environment
+$ . build/envsetup.sh
+
+# Choose a target
+$ lunch aosp_$flame-userdebug
 export TZ=Asia/Dhaka
-mka bacon -j$(nproc --all)
-
+$ mka bacon -j$
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
