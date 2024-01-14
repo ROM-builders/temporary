3369cf4dea37c55302c893870a1591b7f295b4b2
SSlavyanin <156539470+SSlavyanin@users.noreply.github.com>
2024-01-14 04:03:00 +0200

    v7-Miui-SSlavyanin

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/Miui/android.git -b miui-v7 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/SSlavyanin/local_manifest/tree/main.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
