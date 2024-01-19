0002052fa1b5058447f5b184d08b89bbba403811
Antony Braiano <78004742+Antony-Braiano@users.noreply.github.com>
2024-01-19 01:38:36 -0300

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 . build/envsetup.sh
-lunch superior_spes-userdebug
+lunch lineage_spes-userdebug
 export TZ=Asia/Dhaka #put before last build command
 make
 
