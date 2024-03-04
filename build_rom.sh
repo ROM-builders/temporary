e9153d4dadc2330bd3fbfe295ddc77f88ce92c85
gooseprjkt <162053558+gooseprjkt@users.noreply.github.com>
2024-03-04 21:48:49 +0300

    OmniROM 14.0 (chime)

--- build_rom.sh
+++ build_rom.sh
@@ -1,4 +1,4 @@
-# sync rom
+# sync rom (PR)
 repo init --depth=1 --no-repo-verify -u git://github.com/omnirom/android.git -b android-14.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/gooseprjkt/local_manifest.git --depth 1 -b master .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
