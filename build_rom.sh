553e320c02899d62072dbb337873e7bd0917051f
ek1tty <163760271+ek1tty@users.noreply.github.com>
2024-03-20 22:12:12 +0200

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,14 @@
 # sync rom
-repo init -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-17.1 --git-lfs -g default,-mips,-darwin,-notdefault
 git clone https://github.com/ek1tty/local_manifest/ --depth 1 -b main .repo/local_manifest
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-breakfast fire
-croot
-brunch fire
+lunch lineage_fire-userdebug
+export ALLOW_MISSING_DEPENDENCIES=true
+export TZ=Europe/Kyiv #put before last build command
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
